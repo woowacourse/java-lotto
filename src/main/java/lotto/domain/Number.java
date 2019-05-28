b82707fc4a30fb0a6d72f0d7e2e9248387f05b92
yunheehyeon <ok94life@daum.net>
2019-05-28 14:49:08 +0900

    feat : 유효한 로또 번호인지 체크 (0 ~ 45)

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
 public class Number {
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 0;
+
+    private final int num;
+
+    public Number(final int num) {
+        validate(num);
+        this.num = num;
+    }
+
+    private void validate(final int num) {
+        if (num < MIN_NUMBER || num > MAX_NUMBER) {
+            throw new IllegalArgumentException("유효한 번호가 아닙니다.");
+        }
+    }
 }
--- src/test/java/lotto/domain/NumberTest.java
+++ src/test/java/lotto/domain/NumberTest.java
@@ -1,4 +1,15 @@
 package lotto.domain;
 
-public class Number {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NumberTest {
+    @Test
+    void Number_생성_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Number(-1);
+            new Number(46);
+        });
+    }
 }
