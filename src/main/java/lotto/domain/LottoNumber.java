f89febe94fa02f511f655445bb310372d5d40415
pkch93 <star_final@naver.com>
2019-05-28 15:33:49 +0900

    refactor: LottoNumber.class
    1~45로 범위가 정해져 있으므로 성능상 이점을 위해 미리 1~45까지의 LottoNumber를
    만든 후 매번 새로운 인스턴스를 만드는 것이 아닌 기존의 값을 가져오는 방식으로 변경

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,16 +2,43 @@ package lotto.domain;
 
 import lotto.domain.exception.InvalidLottoNumberException;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class LottoNumber {
+    private static final Map<Integer, LottoNumber> numbers = new HashMap<>();
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_LAST_NUMBER = 45;
     private final int number;
 
-    public LottoNumber(final int number) {
+    static {
+        for (int i = LOTTO_START_NUMBER; i <= LOTTO_LAST_NUMBER; i++) {
+            numbers.put(i, new LottoNumber(i));
+        }
+    }
+
+    private LottoNumber(final int number) {
         if (number < 1 || number > 45) {
             throw new InvalidLottoNumberException(number + "는 유효한 범위가 아닙니다. 로또 번호는 1부터 45 사이여야합니다.");
         }
         this.number = number;
+    }
+
+    public static LottoNumber of(final int number) {
+        return new LottoNumber(number);
+    }
+
+    static LottoNumber getLottoNumber(final int number) {
+        LottoNumber lottoNumber = numbers.get(number);
+        if (lottoNumber == null) {
+            throw new InvalidLottoNumberException(number + "는 유효한 범위가 아닙니다. 로또 번호는 1부터 45 사이여야합니다.");
+        }
+        return lottoNumber;
+    }
+
+    public int getNumber() {
+        return number;
     }
 
     @Override
--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -8,17 +8,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoNumberTest {
     @Test
-    void init() {
-        assertThat(new LottoNumber(10)).isEqualTo(new LottoNumber(10));
+    void 번호_10번이_10을_가지고_있는지_테스트() {
+        assertThat(LottoNumber.of(10)).isEqualTo(LottoNumber.getLottoNumber(10));
     }
 
     @Test
     void lotto숫자가_1보다_작은_경우() {
-        assertThrows(InvalidLottoNumberException.class, () -> new LottoNumber(0));
+        assertThrows(InvalidLottoNumberException.class, () -> LottoNumber.getLottoNumber(0));
     }
 
     @Test
     void lotto숫자가_45보다_큰_경우() {
-        assertThrows(InvalidLottoNumberException.class, () -> new LottoNumber(46));
+        assertThrows(InvalidLottoNumberException.class, () -> LottoNumber.getLottoNumber(46));
     }
 }
