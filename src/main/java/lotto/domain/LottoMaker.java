fc7d5df380e1104d53fbbb813286924672a76581
ddu0422 <ddu0422@naver.com>
2019-05-28 17:35:03 +0900

    refactor : LottoMaker 리팩토링

--- src/main/java/lotto/domain/LottoMaker.java
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -6,25 +6,22 @@ import java.util.List;
 import java.util.Random;
 
 public class LottoMaker {
-    private static final int INIT = 0;
+    private static final int START = 0;
     private static final int LOTTO_SIZE = 6;
+    private static final int FIRST_NUMBER = 1;
+    private static final int LAST_NUMBER = 45;
 
     private static final List<Integer> numbers = new ArrayList<>();
 
     static {
-        for (int i = 1; i <= 45; i++) {
+        for (int i = FIRST_NUMBER; i <= LAST_NUMBER; i++) {
             numbers.add(i);
         }
     }
 
     public static List<Integer> generator() {
-        List<Integer> lottoNumbers = new ArrayList<>();
-
         mixNumbers();
-        for (int i = INIT; i < LOTTO_SIZE; i++) {
-            lottoNumbers.add(numbers.get(i));
-        }
-        return lottoNumbers;
+        return numbers.subList(START, LOTTO_SIZE - 1);
     }
 
     private static void mixNumbers() {
