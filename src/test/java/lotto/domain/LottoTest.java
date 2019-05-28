bd74d5e6e1061d803379d0160e6e24feb4768e00
imkimheejoo <jujudamdadi@naver.com>
2019-05-28 18:17:23 +0900

    feat: LottoTest Class 중복확인 테스트메서드 구현

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,6 +1,17 @@
 package lotto.domain;
 
 
-public class LottoTest {
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LottoTest {
+    @Test
+    void 중복확인() {
+        assertThrows(IllegalArgumentException.class,() -> {
+            new Lotto(Arrays.asList(1,1,2,3,4,5));
+        });
+    }
 }
