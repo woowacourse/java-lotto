73d7de00c79278d1c6822961801bb445f093baeb
mrkwon <erased11@gmail.com>
2019-05-28 15:30:44 +0900

    CREATE : Lotto Class

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<LottoNumber> lotto;
+
+    public Lotto(List<LottoNumber> lotto) {
+        this.lotto = lotto;
+    }
+
+    public int size() {
+        return lotto.size();
+    }
+}
