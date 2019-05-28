6e0fe9b012232109ab50dd0cab49c5c7f152778c
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-28 14:16:36 +0900

    refactor: 로또 번호 범위 상수화
    * 로또 번호 범위를 나타내는 매직넘버를 static final로 상수화한다.

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,15 +1,17 @@
 package lotto.domain;
 
 public class LottoNumber {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
     private final int number;
 
     public LottoNumber(int number) {
-        checkValidRange(number);
         this.number = number;
+        checkValidRange();
     }
 
-    private void checkValidRange(int number) {
-        if (number < 1 || number > 45) {
+    private void checkValidRange() {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException("유효한 로또 번호가 아닙니다.");
         }
     }
