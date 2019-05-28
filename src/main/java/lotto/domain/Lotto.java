aed4f459339e6f6722dc9211ba8ce592c7dc1cb4
andole87 <andole87@gmail.com>
2019-05-28 14:23:40 +0900

    feat: Lotto Override equals() and hashCode()
    로또 객체를 비교한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +17,18 @@ public class Lotto {
 
     public List<Integer> numbers() {
         return this.numbers;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(this.numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.numbers);
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -18,4 +18,11 @@ class LottoTest {
         final Lotto lotto = new Lotto(Arrays.asList(1, 3, 5, 7, 9, 11));
         assertEquals(Arrays.asList(1, 3, 5, 7, 9, 11), lotto.numbers());
     }
+
+    @Test
+    void 비교() {
+        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertEquals(lotto1, lotto2);
+    }
 }
