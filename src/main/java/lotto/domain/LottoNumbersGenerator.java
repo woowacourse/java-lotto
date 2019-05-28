bb9f50bb58796f95b0c0895b24d08fd551df8643
yk1028 <wongeunsong94@gmail.com>
2019-05-28 16:55:45 +0900

    refactor: LottoNumbersGenerator가 LottoNumbers를 반환하도록 수정

--- src/main/java/lotto/domain/LottoNumbersGenerator.java
+++ src/main/java/lotto/domain/LottoNumbersGenerator.java
@@ -10,16 +10,18 @@ public class LottoNumbersGenerator {
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int SUBLIST_LOWER_BOUND = 0;
     private static final int SUBLIST_UPPER_BOUND = 6;
-    private static final List<LottoNumber> lottoNumbers = new ArrayList<>();
+    private static final List<LottoNumber> totalLottoNumbers = new ArrayList<>();
 
     static {
         for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
-            lottoNumbers.add(new LottoNumber(i));
+            totalLottoNumbers.add(new LottoNumber(i));
         }
     }
 
-    public static List<LottoNumber> getLottoNumbers() {
-        Collections.shuffle(lottoNumbers, new Random());
-        return new ArrayList<>(lottoNumbers.subList(SUBLIST_LOWER_BOUND , SUBLIST_UPPER_BOUND));
+    public static LottoNumbers getLottoNumbers() {
+        Collections.shuffle(totalLottoNumbers, new Random());
+        ArrayList<LottoNumber> lottoNumbers = new ArrayList<>(
+                totalLottoNumbers.subList(SUBLIST_LOWER_BOUND , SUBLIST_UPPER_BOUND));
+        return new LottoNumbers(lottoNumbers);
     }
 }
--- src/test/java/lotto/domain/LottoNumbersGeneratorTest.java
+++ src/test/java/lotto/domain/LottoNumbersGeneratorTest.java
@@ -2,21 +2,11 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoNumbersGeneratorTest {
     @Test
-    void 여섯개의_중복없는_로또_번호가_나오는지_확인() {
-        Set<LottoNumber> lottoNumbers = new HashSet<>(LottoNumbersGenerator.getLottoNumbers());
-
-        assertThat(lottoNumbers.size()).isEqualTo(6);
-    }
-
-    @Test
-    void 바뀐_결과가_나오는지_확인() {
-        assertThat(LottoNumbersGenerator.getLottoNumbers().containsAll(LottoNumbersGenerator.getLottoNumbers())).isFalse();
+    void 번호들이_셔플되어_나오는지_확인() {
+        assertThat(LottoNumbersGenerator.getLottoNumbers()).isNotEqualTo(LottoNumbersGenerator.getLottoNumbers());
     }
 }
