9131d3b83057c70c0d4fc17c86590bea25dbc0f9
men7627 <men1219@hanmail.net>
2019-05-28 17:41:44 +0900

    [Add] class : WinnerLottoCreator

---
+++ src/main/java/lotto/model/WinnerLottoCreator.java
@@ -0,0 +1,15 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinnerLottoCreator {
+        public static Lotto create(String[] inputs) {
+                List<LottoNumber> lottoNumbers = new ArrayList<>();
+                for(String input : inputs){
+                        int number = Integer.parseInt(input);
+                        lottoNumbers.add(LottoNumberCreator.create(number));
+                }
+                return new Lotto(lottoNumbers);
+        }
+}
---
+++ src/test/java/lotto/model/WinnerLottoCreatorTest.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+import lotto.model.exception.LottoNumberDuplicationException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class WinnerLottoCreatorTest {
+        @Test
+        void 당첨_로또_생성_및_중복_검사() {
+                List<LottoNumber> lottoNumbers = new ArrayList<LottoNumber>();
+                assertThrows(LottoNumberDuplicationException.class, ()->{
+                        WinnerLottoCreator.create(new String[]{"1","2","3","4","5","1"});
+                });
+        }
+}
