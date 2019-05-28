0a03c26412a63fcb25686e306966db148c21ca07
men7627 <men1219@hanmail.net>
2019-05-28 15:09:47 +0900

    [Add] class : LottoNumber

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoNumber {
+        private final int number;
+        private static final Map<Integer, LottoNumber> MAPPING_LOTTO_NUMBER = new HashMap<Integer, LottoNumber>(){
+                {
+                        for(int number = 1; number <=45; number++){
+                                put(number, new LottoNumber(number));
+                        }
+                }
+        };
+
+        private LottoNumber(int number) {
+                this.number = number;
+        }
+
+        public static LottoNumber create(int number){
+                checkValidLottoNumber(number);
+                return MAPPING_LOTTO_NUMBER.get(number);
+        }
+
+        private static void checkValidLottoNumber(int number) {
+                if(MAPPING_LOTTO_NUMBER.get(number) == null){
+                        throw new InvalidLottoNumberException("로또 숫자 범위를 벗어났습니다.");
+                }
+        }
+}
---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LottoNumberTest {
+        @Test
+        void 로또_숫자_범위_검사_0() {
+                assertThrows(InvalidLottoNumberException.class, ()->{
+                    LottoNumber.create(0);
+                } );
+        }
+
+        @Test
+        void 로또_숫자_범위_검사_46() {
+                assertThrows(InvalidLottoNumberException.class, ()->{
+                        LottoNumber.create(46);
+                } );
+        }
+}
