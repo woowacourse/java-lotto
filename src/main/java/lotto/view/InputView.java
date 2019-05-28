cc4d5faad46e457ec5d8c0a14fc9577a7f11ff3b
men7627 <men1219@hanmail.net>
2019-05-28 17:10:26 +0900

    [Refactor] method : inputWinnerNumbers return type String -> String[]

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,11 +7,11 @@ import java.util.Scanner;
 public class InputView {
         public static final String WINNER_NUMBERS_INPUT_FORM = "(([0-9]+)(,)( )){5}([0-9]+)";
 
-        public static String inputWinnerNumbers() {
+        public static String[] inputWinnerNumbers() {
                 System.out.println("지난 주 당첨 번호를 입력해 주세요.");
                 String input = new Scanner(System.in).nextLine();
                 checkWinnerNumbersInputForm(input);
-                return input;
+                return input.split(", ");
         }
 
         private static void checkWinnerNumbersInputForm(String input) {
