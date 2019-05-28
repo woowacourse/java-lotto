6aa3ae6080135656d59a02daff7a61b7299ada5a
school0bhy <house1217@ajou.ac.kr>
2019-05-28 23:25:42 +0900

    Test : 구입 금액 처리 테스트 코드 작성
    UserMoneyTest 클래스 구현

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -2,22 +2,20 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoNumberTest {
     @Test
     void 로또번호_범위_벗어남1() {
         assertThrows(InvalidLottoNumberException.class, () -> {
-            new LottoNumber(0);
+            LottoNumber.of(0);
         });
     }
 
     @Test
     void 로또번호_범위_벗어남2() {
         assertThrows(InvalidLottoNumberException.class, () -> {
-            new LottoNumber(46);
+            LottoNumber.of(46);
         });
     }
 }
