7e3615f0bc9941e8fe7ee9143d70e643f4606f88
men7627 <men1219@hanmail.net>
2019-05-28 17:38:46 +0900

    [Bug Fix] Lotto : checkDuplication Logic (contains misuse)

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,6 +5,8 @@ import lotto.model.exception.LottoNumberDuplicationException;
 import java.util.List;
 
 public class Lotto {
+        private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+
         private final List<LottoNumber> lottoNumbers;
 
         public Lotto(List<LottoNumber> lottoNumbers) {
@@ -13,13 +15,19 @@ public class Lotto {
         }
 
         private void checkLottoNumbersDuplication(List<LottoNumber> lottoNumbers) {
-                for (LottoNumber lottoNumber : lottoNumbers) {
-                        CheckContainLottoNumber(lottoNumbers, lottoNumber);
+                for (int index = 0 ; index < NUMBER_OF_LOTTO_NUMBERS ; index++ ) {
+                        CheckOneLottoNumberDuplication(lottoNumbers, index);
                 }
         }
 
-        private void CheckContainLottoNumber(List<LottoNumber> lottoNumbers, LottoNumber lottoNumber) {
-                if (lottoNumbers.contains(lottoNumber)) {
+        private void CheckOneLottoNumberDuplication(List<LottoNumber> lottoNumbers, int index) {
+                for (int i = index + 1 ; i < NUMBER_OF_LOTTO_NUMBERS ; i++){
+                        checkContainLottoNumber(lottoNumbers, index, i);
+                }
+        }
+
+        private void checkContainLottoNumber(List<LottoNumber> lottoNumbers, int index, int i) {
+                if(lottoNumbers.get(index).equals(lottoNumbers.get(i))){
                         throw new LottoNumberDuplicationException("로또 숫자가 중복되었습니다.");
                 }
         }
--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -4,7 +4,9 @@ import lotto.model.exception.WinnerNumbersInputFormException;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputViewTest {
@@ -18,6 +20,15 @@ public class InputViewTest {
         }
 
         @Test
+        void 당첨_로또_split_검사() {
+                assertThrows(NumberFormatException.class, () -> {
+                        ByteArrayInputStream input = new ByteArrayInputStream("1, 2, 3, 4, 5, 6".getBytes());
+                        System.setIn(input);
+                        assertThat(InputView.inputPayment()).isEqualTo(new String[]{"1","2","3","4","5","6"});
+                });
+        }
+
+        @Test
         void 구매_금액_정수_검사() {
                 assertThrows(NumberFormatException.class, () -> {
                         ByteArrayInputStream input = new ByteArrayInputStream("15,000".getBytes());
@@ -25,4 +36,5 @@ public class InputViewTest {
                         InputView.inputPayment();
                 });
         }
+
 }
