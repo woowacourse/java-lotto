2b643920d641f2c76688f729855dbdde9a2eefa4
skygl <jupiter9501@gmal.com>
2019-05-28 17:11:24 +0900

    [ADD] Lotto 당첨 순위에 대한 정보를 갖는 Enum 추가
    - 로또 객체 두개로 동일한 번호를 몇 개 가지고 있는지 반환하는 메소드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,8 +2,10 @@ package lotto.domain;
 
 import lotto.utils.LottoNoGenerator;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     public static final int COUNT_OF_NO = 6;
@@ -11,6 +13,7 @@ public class Lotto {
     private List<LottoNo> lottoNo;
 
     static Lotto of(List<LottoNo> lottoNo) {
+        lottoNo.sort(LottoNo::compareTo);
         return new Lotto(lottoNo);
     }
 
@@ -19,9 +22,9 @@ public class Lotto {
     }
 
     private Lotto(List<LottoNo> lottoNo) {
-        validateCountOfNums(lottoNo);
+        validateCountOfNo(lottoNo);
         validateDuplicatedNo(lottoNo);
-        this.lottoNo = lottoNo;
+        this.lottoNo = Collections.unmodifiableList(lottoNo);
     }
 
     private void validateDuplicatedNo(List<LottoNo> lottoNo) {
@@ -30,9 +33,17 @@ public class Lotto {
         }
     }
 
-    private void validateCountOfNums(List<LottoNo> lottoNo) {
+    private void validateCountOfNo(List<LottoNo> lottoNo) {
         if (lottoNo.size() != COUNT_OF_NO) {
             throw new InvalidNumberException("로또 번호의 갯수가 " + COUNT_OF_NO + "개가 아닙니다.");
         }
+    }
+
+    private boolean matchNo(LottoNo lotto) {
+        return lottoNo.contains(lotto);
+    }
+
+    int getCountOfMatchNo(Lotto lotto) {
+        return lottoNo.stream().filter(lotto::matchNo).collect(Collectors.toList()).size();
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoTest {
@@ -22,5 +23,18 @@ class LottoTest {
                         , new LottoNo(2), new LottoNo(10)
                         , new LottoNo(15), new LottoNo(22)
                         , new LottoNo(22))));
+    }
+
+    @Test
+    void 로또_인스턴스끼리_중복된_숫자의_갯수_테스트() {
+        Lotto lotto1 = Lotto.of(Arrays.asList(new LottoNo(1)
+                , new LottoNo(2), new LottoNo(3)
+                , new LottoNo(4), new LottoNo(5)
+                , new LottoNo(6)));
+        Lotto lotto2 = Lotto.of(Arrays.asList(new LottoNo(1)
+                , new LottoNo(2), new LottoNo(3)
+                , new LottoNo(15), new LottoNo(22)
+                , new LottoNo(25)));
+        assertThat(lotto1.getCountOfMatchNo(lotto2)).isEqualTo(3);
     }
 }
