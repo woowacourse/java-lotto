80454e5a260f298db1a1b829cdc84541dcddce43
men7627 <men1219@hanmail.net>
2019-05-28 16:17:59 +0900

    [Add] class : LottoNumberDuplicationException

---
+++ src/main/java/lotto/model/LottoNumberDuplicationException.java
@@ -0,0 +1,11 @@
+package lotto.model;
+
+public class LottoNumberDuplicationException extends RuntimeException {
+        public LottoNumberDuplicationException() {
+                super();
+        }
+
+        public LottoNumberDuplicationException(String message) {
+                super(message);
+        }
+}
