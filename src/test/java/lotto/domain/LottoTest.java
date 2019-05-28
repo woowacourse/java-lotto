23ead93cb3d8eced17f6650b9e5c8bc1757a58d8
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-28 14:45:59 +0900

    feat: 로또 예외 테스트 코드 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -14,4 +14,11 @@ public class LottoTest {
             new Lotto(Arrays.asList(1, 2, 3, 4, 6, 6));
         });
     }
+
+    @Test
+    void 유효하지_않은_번호를_갖는_로또_예외() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(Arrays.asList(-1, 2, 3, 4, 5, 6));
+        });
+    }
 }
