58a599b24be34bb520b48f76fa2f2d0e0fcfac83
yunheehyeon <ok94life@daum.net>
2019-05-28 14:54:37 +0900

    feat : 유효한 로또 번호인지 체크 (6 자리)

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lotto {
+    List<Number> lotto;
+
+    public Lotto(List<Number> lotto) {
+        validate(lotto);
+        this.lotto = lotto;
+    }
+
+    private void validate(List<Number> lotto) {
+        if (lotto.size() != 6) {
+            throw new IllegalArgumentException("로또는 6자리 입니다.");
+        }
+    }
+
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,4 +1,21 @@
-package domain;
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoTest {
+    @Test
+    void 로또번호_6자리_체크() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(Arrays.asList(
+                    new Number(1),
+                    new Number(2),
+                    new Number(3),
+                    new Number(4)
+            ));
+        });
+    }
 }
