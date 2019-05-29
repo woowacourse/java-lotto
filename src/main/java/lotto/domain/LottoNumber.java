04da4439bfa86a168cdc77b7ea8f1a313c5289f8
school0bhy <house1217@ajou.ac.kr>
2019-05-29 11:06:50 +0900

    Refactor : 로또 번호 객체 매번 생성하지 않고, 생성된 객체를 리턴하도록 수정
    LottoNumber 클래스에 LOTTO_NUMBER_POOL 갖고, 생성자를 private으로 변경
    of 메소드에서 LottoNumber 객체를 반환하도록 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,22 +1,39 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class LottoNumber {
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
+    static final int MIN_LOTTO_NUMBER = 1;
+    static final int MAX_LOTTO_NUMBER = 45;
+    private static final List<LottoNumber> LOTTO_NUMBER_POOL;
+
+    static {
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
+            lottoNumbers.add(new LottoNumber(i));
+        }
+        LOTTO_NUMBER_POOL = Collections.unmodifiableList(lottoNumbers);
+    }
 
     private final int lottoNumber;
 
-    public LottoNumber(int number) {
+    private LottoNumber(int number) {
         checkRange(number);
         this.lottoNumber = number;
     }
 
-    private void checkRange(int number) {
+    private static void checkRange(int number) {
         if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new InvalidLottoNumberException("로또 번호의 범위에 벗어납니다.");
         }
+    }
+
+    public static LottoNumber of(int number) {
+        checkRange(number);
+        return LOTTO_NUMBER_POOL.get(number - 1);
     }
 
     @Override
