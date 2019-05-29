b8704a7b73ddbe3ee62fad57440f93a020dea879
yunheehyeon <ok94life@daum.net>
2019-05-29 14:00:54 +0900

    feat : 자동 로또 출력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,15 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static Money createMoney() {
-        return null;
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            return new Money(Integer.parseInt(scanner.nextLine()));
+        } catch (NumberFormatException e) {
+            System.out.println("숫자를 입력하세요");
+            return createMoney();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createMoney();
+        }
     }
 }
