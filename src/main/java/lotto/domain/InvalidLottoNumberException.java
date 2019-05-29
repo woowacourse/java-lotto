a4afaf05984c95daa86cc2ea88588ec6eb9efab3
backlo <hjun4512@naver.com>
2019-05-29 17:32:09 +0900

    AutoGenerateLotto 클래스 구현

--- src/main/java/lotto/domain/InvalidLottoNumberException.java
+++ src/main/java/lotto/domain/InvalidLottoNumberException.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 public class InvalidLottoNumberException extends RuntimeException{
-
     public InvalidLottoNumberException(String message) {
         super(message);
     }
---
+++ src/main/java/lotto/domain/lotto/AutoGenerateLotto.java
@@ -0,0 +1,32 @@
+package lotto.domain.lotto;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class AutoGenerateLotto {
+
+    private final Lotto lotto;
+
+    public AutoGenerateLotto(){
+        this.lotto = makeAutoLotto(createNumber());
+    }
+
+    private Lotto makeAutoLotto(List<LottoNumber> createNumber){
+        Collections.shuffle(createNumber);
+        return new Lotto(createNumber.stream()
+                .limit(6)
+                .collect(Collectors.toList()));
+    }
+
+    private List<LottoNumber> createNumber(){
+        return IntStream.rangeClosed(1,45)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
+
+    public Lotto getAutoLotto() {
+        return lotto;
+    }
+}
