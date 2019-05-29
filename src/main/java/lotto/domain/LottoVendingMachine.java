6228522a27f11423ee6e33b2a99e81e3e023f620
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-05-29 12:17:24 +0900

    add: class Lottos

--- src/main/java/lotto/domain/LottoVendingMachine.java
+++ src/main/java/lotto/domain/LottoVendingMachine.java
@@ -5,20 +5,38 @@ import java.util.List;
 
 public class LottoVendingMachine {
     static final int LOTTO_PRICE = 1000;
+    private final int numOfRandomLottos;
+    private final int numOfCustomLottos;
 
-    public static List<Lotto> getRandomLottos(int money) {
-        List<Lotto> lottos = new ArrayList<>();
-        Money lottoBuyingMoney = new Money(money);
-        if (!lottoBuyingMoney.isMultipleOf(new Money(LOTTO_PRICE))) {
+    public LottoVendingMachine(int money, int numOfCustomLottos) {
+        if (money % LOTTO_PRICE != 0) {
             throw new InvalidLottoBuyingMoneyException("로또 금액(" + LOTTO_PRICE + ") 의 배수에 해당하는 돈을 입력하셔야 합니다.");
         }
-        for (int i=0; i<lottoBuyingMoney.divideBy(new Money(LOTTO_PRICE)); i++) {
+        if (money / LOTTO_PRICE < numOfCustomLottos) {
+            throw new InvalidNumOfCustomLottosException("수동으로 구매할 로또 수는 구매 가능한 로또 수보다 클 수 없습니다.");
+        }
+        this.numOfRandomLottos = (money / LOTTO_PRICE) - numOfCustomLottos;
+        this.numOfCustomLottos = numOfCustomLottos;
+    }
+
+    public Lottos getLottos(List<List<Integer>> lottoNumbers) {
+        List<Lotto> userLottos = getCustomLottos(lottoNumbers);
+        userLottos.addAll(getRandomLottos());
+        return new Lottos(userLottos);
+    }
+
+    public List<Lotto> getRandomLottos() {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i=0; i<numOfRandomLottos; i++) {
             lottos.add(new Lotto());
         }
         return lottos;
     }
 
-    public static List<Lotto> getCustomLottos(List<List<Integer>> lottoNumbers) {
+    public List<Lotto> getCustomLottos(List<List<Integer>> lottoNumbers) {
+        if (lottoNumbers.size() != numOfCustomLottos) {
+            throw new InvalidCustomLottoNumbersException("수동으로 구매할 로또 수(" + numOfCustomLottos + "개)와 일치하지 않습니다.");
+        }
         List<Lotto> lottos = new ArrayList<>();
         for (List<Integer> numbers : lottoNumbers) {
             lottos.add(new Lotto(numbers));
---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        if (lottos == null || lottos.contains(null)) {
+            throw new NullArgumentException("생성자의 인자로 null 을 넘길 수 없습니다.");
+        }
+        this.lottos = lottos;
+    }
+
+    public List<Rank> match(WinningLotto winningLotto) {
+        List<Rank> ranks = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            ranks.add(winningLotto.match(lotto));
+        }
+        return ranks;
+    }
+}
