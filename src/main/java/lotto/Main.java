4407ff489fc5b207a1d371b7e434d3c135533f4c
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-29 14:27:30 +0900

    refactor: 금액 입력 오류시 재입력 받기

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,21 +1,26 @@
 package lotto;
 
-import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
-import lotto.domain.RandomNumberGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Money money = new Money(Integer.parseInt(InputView.inputMoney()));
+        Money money = createMoney();
         int numberOfLotto = money.getNumberOfLotto();
         Lottos lottos = new Lottos(numberOfLotto);
         OutputView.outputLotto(numberOfLotto, lottos);
+    }
+
+    private static Money createMoney() {
+        try {
+            Money money = new Money(InputView.inputMoney());
+            return money;
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            return createMoney();
+        }
     }
 }
--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,21 +2,25 @@ package lotto.domain;
 
 public class Money {
 
-    private static final int LOTTO_PRICE = 1000;
+    private static final int MONEY_PER_LOTTO = 1000;
     private final int money;
 
-    public Money(int money) {
-        this.money = money;
+    public Money(String money) {
+        try {
+            this.money = Integer.parseInt(money);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자가 아닙니다. 금액은 숫자를 입력해야 합니다.");
+        }
         checkMinimumMoney();
     }
 
     private void checkMinimumMoney() {
-        if (money < LOTTO_PRICE) {
+        if (money < MONEY_PER_LOTTO) {
             throw new IllegalArgumentException("로또의 최소 가격은 1,000원 입니다. 그 이상을 입력해주세요!");
         }
     }
 
     public int getNumberOfLotto() {
-        return money / LOTTO_PRICE;
+        return money / MONEY_PER_LOTTO;
     }
 }
