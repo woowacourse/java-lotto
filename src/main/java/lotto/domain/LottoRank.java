161e96faa578ed6759e3b0df4abbb1eb500cfa24
school0bhy <house1217@ajou.ac.kr>
2019-05-29 11:56:21 +0900

    Add : 등수 매칭 구현
    LottoRank enum 클래스 구현

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    FAIL(0, 0);
+
+    private static final int WINNING_MIN_COUNT = 3;
+
+    private final int countOfMatch;
+    private final int reward;
+
+    LottoRank(int countOfMatch, int reward) {
+        this.countOfMatch = countOfMatch;
+        this.reward = reward;
+    }
+
+    public static LottoRank rankOf(int countOfMatch) {
+        if (countOfMatch < WINNING_MIN_COUNT) {
+            return FAIL;
+        }
+        return Arrays.stream(values())
+                .filter(rank -> rank.countOfMatch == countOfMatch)
+                .findAny()
+                .get();
+    }
+}
