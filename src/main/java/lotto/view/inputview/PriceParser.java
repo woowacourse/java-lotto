1ce730bca20face519da437c2903a0b06299c81c
강연욱 <fiasboc23@gmail.com>
2019-05-29 16:57:43 +0900

    refactor : Refactor the PriceParser Class

--- src/main/java/lotto/view/inputview/PriceParser.java
+++ src/main/java/lotto/view/inputview/PriceParser.java
@@ -1,6 +1,5 @@
 package lotto.view.inputview;
 
-import java.nio.file.Path;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,29 +7,65 @@ public class PriceParser {
 
     private static final String ERROR_NULL_OR_NO_INPUT = "입력이 없습니다.";
     private static final String ERROR_MINIMUM_LIMIT_PRICE = "구매 금액은 1000원 이상입니다.";
+    private static final String ERROR_PRICE_UNIT = "구매 금액은 1,000원 단위 입니다.";
+    private static final String PATTERN_ALLOWED_LAST_CHARACTER = "^([0-9]*)원$";
+    private static final int REMOVED_INPUT_LAST_CHARACTER = 1;
+    private static final int MINIMUM_LIMIT_PRICE = 1000;
+    private static final int PRICE_UNIT = 1000;
+    private static final int DIVISIBLE = 0;
 
     public static int getLottoAmount(String input) {
+        int price = toInts(getValidInput(input));
+        checkValidPrice(price);
 
-        if(input == null || input.isEmpty()){
+        return calculateAmount(price);
+    }
+
+    private static String getValidInput(String input) {
+        checkNullEmpty(input);
+        return getCheckedPatternInput(input);
+    }
+
+    private static void checkNullEmpty(String input) {
+        if (input == null || input.isEmpty()) {
             throw new NullPointerException(ERROR_NULL_OR_NO_INPUT);
         }
+    }
 
-        Pattern pattern = Pattern.compile("^([0-9]*)원$");
+    private static String getCheckedPatternInput(String input) {
+        Pattern pattern = Pattern.compile(PATTERN_ALLOWED_LAST_CHARACTER);
         Matcher matcher = pattern.matcher(input);
-        if(matcher.find()){
-            input = matcher.group(1);
+        String checkedInput = input;
+
+        if (matcher.find()) {
+            checkedInput = matcher.group(REMOVED_INPUT_LAST_CHARACTER);
         }
 
-        int lottoAmount = Integer.parseInt(input);
+        return checkedInput;
+    }
 
-        if(lottoAmount < 1000){
+    private static int toInts(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private static void checkValidPrice(int price) {
+        checkMinimumLimit(price);
+        checkDivisiblePriceUnit(price);
+    }
+
+    private static void checkMinimumLimit(int price) {
+        if (price < MINIMUM_LIMIT_PRICE) {
             throw new IllegalArgumentException(ERROR_MINIMUM_LIMIT_PRICE);
         }
+    }
 
-        if(lottoAmount % 1000 != 0){
-            throw new IllegalArgumentException("구매 금액은 1,000원 단위 입니다.");
+    private static void checkDivisiblePriceUnit(int price) {
+        if (price % PRICE_UNIT != DIVISIBLE) {
+            throw new IllegalArgumentException(ERROR_PRICE_UNIT);
         }
+    }
 
-        return lottoAmount / 1000;
+    private static int calculateAmount(int price) {
+        return price / PRICE_UNIT;
     }
 }
