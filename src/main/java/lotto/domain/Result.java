74647e0faa65c8806d2e1977f3036504c6579864
pkch93 <star_final@naver.com>
2019-05-29 11:59:49 +0900

    refactor: Result.class
    상수 MIN_PRIZE_NUMBER와 MAX_PRIZE_NUMBER의 접근제어자 private -> public으로
    변경
    getCountOfPrize를 수익률 계산 뿐 아니라 OutputView에서도 쓸 수 있을 것이라 판단하여
    public으로 접근제어자 변경

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -3,9 +3,9 @@ package lotto.domain;
 import java.util.Map;
 
 public class Result {
+    public static final int MIN_PRIZE_NUMBER = 3;
+    public static final int MAX_PRIZE_NUMBER = 6;
     private static final double ONE_HUNDRED_PERCENT = 100.0;
-    private static final int MIN_PRIZE_NUMBER = 3;
-    private static final int MAX_PRIZE_NUMBER = 6;
     private final Map<Prize, Integer> result;
 
     public Result(final Map<Prize, Integer> result) {
@@ -21,7 +21,7 @@ public class Result {
         return (double) sum / buyPrice * ONE_HUNDRED_PERCENT;
     }
 
-    private int getCountOfPrize(Prize prize) {
+    public int getCountOfPrize(Prize prize) {
         Integer countOfPrize = result.get(prize);
         if (countOfPrize == null) {
             return 0;
