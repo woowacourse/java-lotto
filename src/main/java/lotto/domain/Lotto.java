15c01338164dfc953fb092cebd59abdeb61be156
김성훈 <seonghun127@gmail.com>
2019-05-29 14:37:01 +0900

    feat : 로또 6자리를 담고 관리하는 Lotto 클래스 추가
    1. LottoNumber 타입의 리스트를 인자로 받고 6자리가 맞는지 확인한다
    	- 6개의 숫자가 아니거나 중복일 경우 예외를 던진다

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+
+public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+
+    private final List<LottoNumber> numbers;
+
+    public Lotto(List<LottoNumber> numbers) {
+        if(new HashSet<>(numbers).size() != LOTTO_SIZE ){
+            throw new IllegalArgumentException("중복된 숫자가 있습니다. 다시 확인해주세요.");
+        }
+        this.numbers = numbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LottoTest {
+    @Test
+    public void 로또_생성_테스트() {
+        List<LottoNumber> numbers = Arrays.asList(
+                LottoNumber.get(1),
+                LottoNumber.get(2),
+                LottoNumber.get(3),
+                LottoNumber.get(4),
+                LottoNumber.get(5),
+                LottoNumber.get(6)
+        );
+        assertThat(new Lotto(numbers)).isEqualTo(new Lotto(numbers));
+    }
+
+    @Test
+    public void 로또_생성시_6자리가_아닌_경우에_대한_예외처리_테스트() {
+        List<LottoNumber> numbers = Arrays.asList(
+                LottoNumber.get(1),
+                LottoNumber.get(2),
+                LottoNumber.get(3),
+                LottoNumber.get(4),
+                LottoNumber.get(5)
+        );
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(numbers);
+        });
+    }
+}
