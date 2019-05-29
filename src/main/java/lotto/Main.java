a62231b638185467c5d02ecbd210140d1f3b0788
hyun <jhs695@naver.com>
2019-05-29 14:54:09 +0900

    refactor Main Class

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -9,25 +9,43 @@ public class Main {
         Money money = new Money(InputView.insertMoney());
         int countOfCustomLottos = InputView.countOfCustomLottos();
 
-        CustomLottoPaper customLottoPaper = new CustomLottoPaper();
-
-        for (int i = 0; i < countOfCustomLottos; i++) {
-            customLottoPaper.addCustomLotto(InputView.inputCustomLotto());
-        }
-
-        LottoVendingMachine lottoVendingMachine = new LottoVendingMachine();
-        LottoPaper lottoPaper = lottoVendingMachine.buyLotto(money, customLottoPaper.getPaper());
+        LottoPaper lottoPaper = createLottoPaper(money, countOfCustomLottos);
 
         OutputView.printPurchase(money, countOfCustomLottos);
         OutputView.printLottoPaper(lottoPaper);
 
-        Lotto Lotto = new Lotto(customLottoPaper.makeNumbers(InputView.inputWinningLotto()));
-        LottoNumber bonusNumber = new LottoNumber(InputView.inputBonusNumber());
-
-        WinningLotto winningLotto = new WinningLotto(Lotto, bonusNumber);
+        WinningLotto winningLotto = insertWinningLotto();
 
         Statistics statistics = new Statistics(lottoPaper.matchLotto(winningLotto));
 
         OutputView.printStatistics(statistics);
+    }
+
+    private static LottoPaper createLottoPaper(Money money, int countOfCustomLottos) {
+        try {
+            CustomLottoPaper customLottoPaper = new CustomLottoPaper();
+
+            for (int i = 0; i < countOfCustomLottos; i++) {
+                customLottoPaper.addCustomLotto(InputView.inputCustomLotto());
+            }
+
+            LottoVendingMachine lottoVendingMachine = new LottoVendingMachine();
+
+            return lottoVendingMachine.buyLotto(money, customLottoPaper.getPaper());
+        } catch (Exception e) {
+            return createLottoPaper(money, countOfCustomLottos);
+        }
+    }
+
+    private static WinningLotto insertWinningLotto() {
+        try {
+            Lotto Lotto = CustomLottoGenerator.makeLotto(InputView.inputWinningLotto());
+            LottoNumber bonusNumber = new LottoNumber(InputView.inputBonusNumber());
+
+            return new WinningLotto(Lotto, bonusNumber);
+        } catch (Exception e) {
+            return insertWinningLotto();
+        }
+
     }
 }
