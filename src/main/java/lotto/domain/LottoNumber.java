2b30cf07fc67d1f7b61465af5217e46e40bea4e9
dasistHYOJIN <hgenie14@gmail.com>
2019-05-29 20:30:05 +0900

    refactor: merge LottoNumber class Number class

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,24 +1,37 @@
 package lotto.domain;
 
-import java.util.Objects;
+import lotto.exception.IllegalNumberBoundException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LottoNumber {
+    private static final int FIRST_BOUND_OF_LOTTO_NUMBER = 1;
+    private static final int LAST_BOUND_OF_LOTTO_NUMBER = 45;
+    private static final Map<Integer, LottoNumber> numbers = new HashMap<>();
+
     private final int number;
+
+    static {
+        for (int i = FIRST_BOUND_OF_LOTTO_NUMBER; i <= LAST_BOUND_OF_LOTTO_NUMBER; i++) {
+            numbers.put(i, new LottoNumber(i));
+        }
+    }
 
     public LottoNumber(final int number) {
         this.number = number;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LottoNumber number1 = (LottoNumber) o;
-        return number == number1.number;
+    public static LottoNumber getNumber(int key) throws IllegalNumberBoundException {
+        validateNumberBound(key);
+
+        return numbers.get(key);
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(number);
+    private static void validateNumberBound(int key) {
+        if (key < FIRST_BOUND_OF_LOTTO_NUMBER || key >= LAST_BOUND_OF_LOTTO_NUMBER) {
+            throw new IllegalNumberBoundException("로또 번호의 범위에 벗어납니다.");
+        }
     }
+
 }
--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -6,19 +6,19 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class NumberTest {
+public class LottoNumberTest {
     @Test
     void 범위밖의_로또_번호에_대한_예외() {
-        assertThrows(IllegalNumberBoundException.class, () -> { Number.getNumber(0); });
+        assertThrows(IllegalNumberBoundException.class, () -> { LottoNumber.getNumber(0); });
     }
 
     @Test
     void 같은_숫자일때_메모리주소_테스트() {
-        assertThat(Number.getNumber(1) == Number.getNumber(1)).isTrue();
+        assertThat(LottoNumber.getNumber(1) == LottoNumber.getNumber(1)).isTrue();
     }
 
     @Test
     void 다른_숫자일때_메모리주소_테스트() {
-        assertThat(Number.getNumber(1) == Number.getNumber(2)).isFalse();
+        assertThat(LottoNumber.getNumber(1) == LottoNumber.getNumber(2)).isFalse();
     }
 }
