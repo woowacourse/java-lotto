bc3192b9596134e0133d4af5a13b67265f4d4fca
hyun <jhs695@naver.com>
2019-05-29 14:37:43 +0900

    add CustomLottoGenerator Class

---
+++ src/main/java/lotto/domain/CustomLottoGenerator.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class CustomLottoGenerator {
+    public static Lotto makeLotto(String[] userNumbers) {
+        return new Lotto(Arrays.stream(userNumbers)
+                .map(number -> new LottoNumber(Integer.parseInt(number)))
+                .collect(Collectors.toList()));
+    }
+}
--- src/main/java/lotto/domain/CustomLottoPaper.java
+++ src/main/java/lotto/domain/CustomLottoPaper.java
@@ -1,9 +1,7 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CustomLottoPaper {
     private List<Lotto> lottos;
@@ -13,13 +11,7 @@ public class CustomLottoPaper {
     }
 
     public void addCustomLotto(String[] userNumbers) {
-        lottos.add(new Lotto(makeNumbers(userNumbers)));
-    }
-
-    public List<LottoNumber> makeNumbers(String[] userNumbers) {
-        return Arrays.stream(userNumbers)
-                .map(number -> new LottoNumber(Integer.parseInt(number)))
-                .collect(Collectors.toList());
+        lottos.add(CustomLottoGenerator.makeLotto(userNumbers));
     }
 
     public LottoPaper getPaper() {
