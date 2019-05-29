0368064e80e10ed1b7d30b846e847d0490079d0c
ddu0422 <ddu0422@naver.com>
2019-05-29 10:52:52 +0900

    add : 당첨번호를 가지고 있는 클래스 생성

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -1,4 +1,28 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class Winning {
+    private final Lotto lotto;
+
+    private Winning(Lotto lotto) {
+        this.lotto = lotto;
+    }
+
+    public static Winning of(Lotto lotto) {
+        return new Winning(lotto);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Winning winning = (Winning) o;
+        return Objects.equals(lotto, winning.lotto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lotto);
+    }
 }
---
+++ src/test/java/lotto/domain/WinningTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningTest {
+    @Test
+    void 생성() {
+        List<Integer> lottos = Arrays.asList(1,2,3,4,5,6);
+        Winning winning = Winning.of(Lotto.of(lottos));
+        assertThat(winning).isEqualTo(Winning.of(Lotto.of(lottos)));
+    }
+}
