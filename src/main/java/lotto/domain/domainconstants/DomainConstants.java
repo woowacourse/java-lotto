15d18dde637b1beb0cee3f11740d8ed9ece30160
hyun <jhs695@naver.com>
2019-05-30 15:03:46 +0900

    refactor 상수 이동

--- src/main/java/lotto/domain/domainconstants/DomainConstants.java
+++ src/main/java/lotto/domain/domainconstants/DomainConstants.java
@@ -1,6 +1,7 @@
 package lotto.domain.domainconstants;
 
 public class DomainConstants {
+    public static final int LOTTO_SIZE = 6;
     public static final int MINIMUM_LOTTO_NUMBER = 1;
     public static final int MAXIMUM_LOTTO_NUMBER = 45;
     public static final int LOTTO_PRICE = 1000;
