95b8532bb2a9537570e73686395f648ee7750f72
pkch93 <star_final@naver.com>
2019-05-30 15:39:27 +0900

    test: WinningLotto, Lotto
    로직 변경하면서 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import lotto.domain.exception.LottoSizeException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,6 +11,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoTest {
+    Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto(Arrays.asList(
+                getLottoNumber(1), getLottoNumber(2), getLottoNumber(3),
+                getLottoNumber(4), getLottoNumber(5), getLottoNumber(6)));
+    }
+
     @Test
     void init() {
         assertThat(new Lotto(Arrays.asList(
@@ -31,9 +41,12 @@ public class LottoTest {
 
     @Test
     void lotto안에_5가_있는지_여부_테스트() {
-        Lotto lotto = new Lotto(Arrays.asList(
-                getLottoNumber(1), getLottoNumber(2), getLottoNumber(3),
-                getLottoNumber(4), getLottoNumber(5), getLottoNumber(6)));
         assertThat(lotto.hasNumber(getLottoNumber(5))).isEqualTo(1);
+    }
+
+    @Test
+    void 보너스_번호가_lotto번호로_있는_경우_테스트() {
+        BonusBall bonusBall = new BonusBall(5);
+        assertThat(lotto.hasBonusBall(bonusBall)).isTrue();
     }
 }
