80b0531819d8aa546927586d3372681f14c2cb7c
DaejunBae <dpudpu11@gmail.com>
2019-05-31 01:33:50 +0900

    fix: CountOfManual.class 예외처리 수정

--- src/main/java/lotto/domain/CountOfManual.java
+++ src/main/java/lotto/domain/CountOfManual.java
@@ -16,7 +16,7 @@ public class CountOfManual {
         if (countOfManual < 0) {
             throw new IllegalArgumentException("수동 구매 횟수 음수 입력은 불가능합니다");
         }
-        if (countOfManual < countOfPurchase) {
+        if (countOfManual > countOfPurchase) {
             throw new IllegalArgumentException("수동 구매 횟수는 총 구매 횟수보다 많으면 안됩니다.");
         }
     }
