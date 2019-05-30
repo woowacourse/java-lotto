f91cc42bf1720a33bc65f7c7a966a4434b7ae264
Soojin Roh <sooreal91@gmail.com>
2019-05-30 11:27:20 +0900

    feat WinningLotto 클래스 구현
    지난주 당첨 번호와 보너스 볼 넘버를 가지고 있는 WinningLotto 클래스 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+public class WinningLotto {
+
+    private Lotto winningLotto;
+    private Number bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, Number bonusNumber) {
+        this.winningLotto = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int matchCount(Lotto purchasedLotto) {
+        purchasedLotto.getLotto().retainAll(winningLotto.getLotto());
+        return purchasedLotto.getLotto().size();
+    }
+
+    public boolean matchBonusNumber(Lotto purchasedLotto) {
+        return purchasedLotto.getLotto().contains(bonusNumber);
+    }
+}
---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,46 @@
+package lotto.domain;
+
+import lotto.domain.utils.ManualLottoGenerator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningLottoTest {
+
+    List<Number> inputWinningLottoNumber = new ArrayList<>();
+    List<Number> purchasedLottoNumber = new ArrayList<>();
+    Lotto myLotto;
+    WinningLotto winningLotto;
+    Number bonusNumber;
+
+    @Before
+    public void setUp() {
+        for (int i = 1; i <= 6; i++) {
+            inputWinningLottoNumber.add(NumberSet.of(i));
+        }
+
+        for (int i = 3; i <= 7; i++) {
+            purchasedLottoNumber.add(NumberSet.of(i));
+        }
+
+        myLotto = ManualLottoGenerator.makeLotto(purchasedLottoNumber);
+        bonusNumber = NumberSet.of(7);
+        winningLotto = new WinningLotto(ManualLottoGenerator.makeLotto(inputWinningLottoNumber), bonusNumber);
+    }
+
+    @Test
+    public void 당첨_번호_갯수_확인() {
+        int match = winningLotto.matchCount(myLotto);
+        assertThat(match).isEqualTo(4);
+    }
+
+    @Test
+    public void 보너스볼_당첨_확인() {
+        boolean matchBonus = winningLotto.matchBonusNumber(myLotto);
+        assertThat(matchBonus).isEqualTo(true);
+    }
+}
