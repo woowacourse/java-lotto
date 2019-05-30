41f7a1c1f41ce0bc7bc526472f79b053a0efdb64
hyojaekim <hj00686@naver.com>
2019-05-30 12:18:27 +0900

    feat(LottoNumber) : add LottoNumber check number size

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LottoNumberTest {
+    @Test
+    void 로또_숫자_생성하는_테스트() {
+        LottoNumber lottoNumber = new LottoNumber(45);
+        assertThat(lottoNumber).isEqualTo(new LottoNumber(45));
+    }
+
+    @Test
+    void 로또번호가_45_보다_큰_경우_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new LottoNumber(46);
+        });
+    }
+
+    @Test
+    void 로또번호가_1_보다_작은_경우_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new LottoNumber(0);
+        });
+    }
+}
