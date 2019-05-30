43b602fea313dd199a0a7f279aec526ba6b3f2b0
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-30 13:55:59 +0900

    refactor: 테스트 코드 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,16 +1,16 @@
 package lotto.domain;
 
-        import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-        import java.util.Arrays;
+import java.util.Arrays;
 
-        import static org.assertj.core.api.Assertions.assertThat;
-        import static lotto.domain.Rank.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static lotto.domain.Rank.*;
 
 public class WinningLottoTest {
 
     @Test
-    void 로또_등수_계산_보너스_번호_추가() {
+    void 로또_등수_계산_보너스_번호_추가_1등() {
         Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
         LottoNumber bonus = LottoNumber.get(7);
         WinningLotto winningLotto = new WinningLotto(lotto, bonus);
@@ -24,5 +24,14 @@ public class WinningLottoTest {
         LottoNumber bonus = LottoNumber.get(10);
         WinningLotto winningLotto = new WinningLotto(beforeWinningLotto, bonus);
         assertThat(SECOND).isEqualTo(winningLotto.getWinning(currentLotto));
+    }
+
+    @Test
+    void 로또_등수_계산_보너스_번호_추가_3등() {
+        Lotto currentLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 10));
+        Lotto beforeWinningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 9));
+        LottoNumber bonus = LottoNumber.get(7);
+        WinningLotto winningLotto = new WinningLotto(beforeWinningLotto, bonus);
+        assertThat(THIRD).isEqualTo(winningLotto.getWinning(currentLotto));
     }
 }
