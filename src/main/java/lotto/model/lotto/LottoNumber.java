bdd175d2e55a7d1048ee4a9dc179cc83b5303eee
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-05-30 11:28:16 +0900

    [REFACTOR] Modify protected access modifier
    상속 시 사용하던 protected lottoNumber를 더이상 사용하지 않아 private으로 대체

--- src/main/java/lotto/model/lotto/LottoNumber.java
+++ src/main/java/lotto/model/lotto/LottoNumber.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class LottoNumber implements Comparable<LottoNumber> {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
-    protected final int lottoNumber;
+    private final int lottoNumber;
 
     public LottoNumber(int lottoNumber) {
         checkValidLottoNumber(lottoNumber);
