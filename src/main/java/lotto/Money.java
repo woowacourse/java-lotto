d8bdabe8dd7cad7a6526fde80049ee32f6b98739
stopsilver13 <stopsilver13>
2019-05-30 13:53:06 +0900

    [FEATURE] Catch exception at Money's constructor
    Money 예외처리 구현 + 테스트코드 작성
    > StringUtils 임포트
    > 빈문자열/공백 입력시 예외처리
    > 숫자가 아닌 값 입력시 예외처리
    > 천원~십만원 아닌 값 입력시 예외처리

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,9 +1,38 @@
 package lotto;
 
+import lotto.exception.InvalidPaymentException;
+import org.apache.commons.lang3.StringUtils;
+
 public class Money {
     private final int money;
 
     public Money(String input) {
-        this.money = Integer.parseInt(input);
+        checkIsBlank(input);
+        int money = parseInt(input);
+        checkBuyableMoney(money);
+        this.money = money;
+    }
+
+    private void checkIsBlank(String input) {
+        if (StringUtils.isBlank(input)) {
+            throw new InvalidPaymentException("아무것도 입력하지 않으셨습니다.");
+        }
+    }
+
+    private int parseInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new InvalidPaymentException("금액을 숫자로 입력해주세요.");
+        }
+    }
+
+    private void checkBuyableMoney(int money) {
+        if (money < 1000) {
+            throw new InvalidPaymentException("1개 이상의 로또 구입을 위해서는 1000원 이상 필요합니다.");
+        }
+        if (money > 100000) {
+            throw new InvalidPaymentException("1인당 최대 10만원까지 구입 가능합니다.");
+        }
     }
 }
---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import lotto.exception.InvalidPaymentException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MoneyTest {
+    @Test
+    void 제대로된_금액을_입력받은_경우() {
+        assertDoesNotThrow(() -> new Money("1000"));
+    }
+
+    @Test
+    void 빈_문자열을_입력받은_경우_예외_발생() {
+        assertThrows(InvalidPaymentException.class, () -> new Money(""));
+    }
+
+    @Test
+    void 공백을_입력받은_경우_예외_발생() {
+        assertThrows(InvalidPaymentException.class, () -> new Money(" "));
+    }
+
+    @Test
+    void 숫자가_아닌_값을_입력받은_경우_예외_발생() {
+        assertThrows(InvalidPaymentException.class, () -> new Money("abc"));
+    }
+
+    @Test
+    void 천원_미만의_값을_입력받은_경우_예외_발생() {
+        assertThrows(InvalidPaymentException.class, () -> new Money("999"));
+    }
+
+    @Test
+    void 십만원_초과의_값을_입력받은_경우_예외_발생() {
+        assertThrows(InvalidPaymentException.class, () -> new Money("100001"));
+    }
+}
