348cbb254f09c688bc86c797ea599a7754255a03
ddu0422 <ddu0422@naver.com>
2019-05-30 20:32:00 +0900

    add : Controller 작성

--- src/main/java/lotto/contoller/Main.java
+++ src/main/java/lotto/contoller/Main.java
@@ -4,17 +4,50 @@ import lotto.domain.*;
 import lotto.view.input.InputView;
 import lotto.view.output.OutputView;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        PurchaseAmount purchaseAmount = createMoney();
-        Lottos lottos = Lottos.generate(purchaseAmount);
+        PurchaseCount purchaseCount = createManualCount(createPurchaseAmount());
+        Lottos lottos = createLottos(purchaseCount);
 
-        OutputView.outputLottoCount(purchaseAmount);
+        OutputView.outputLottoCount(purchaseCount);
         OutputView.outputLottos(lottos);
 
         Winning winning = createWinningLotto(createLotto());
 
         OutputView.outputResult(LottoResult.of(winning, lottos));
+    }
+
+    private static Lottos createLottos(PurchaseCount purchaseCount) {
+        try {
+            return LottosGenerator.generate(purchaseCount, createManualLottos(purchaseCount));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createLottos(purchaseCount);
+        }
+    }
+
+    private static List<List<Integer>> createManualLottos(PurchaseCount purchaseCount) {
+        return InputView.inputLottos(purchaseCount);
+    }
+
+    private static PurchaseCount createManualCount(PurchaseAmount purchaseAmount) {
+        try {
+            return PurchaseCount.of(purchaseAmount, InputView.inputManualCount());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createManualCount(purchaseAmount);
+        }
+    }
+
+    private static PurchaseAmount createPurchaseAmount() {
+        try {
+            return PurchaseAmount.of(InputView.inputMoney());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createPurchaseAmount();
+        }
     }
 
     private static Lotto createLotto() {
@@ -32,15 +65,6 @@ public class Main {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return createWinningLotto(lotto);
-        }
-    }
-
-    private static PurchaseAmount createMoney() {
-        try {
-            return PurchaseAmount.of(InputView.inputMoney(), 0);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return createMoney();
         }
     }
 }
