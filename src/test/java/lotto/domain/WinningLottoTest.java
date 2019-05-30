4414eedd73d904969609b4dd0f7bdad4f5b69e3f
김성훈 <seonghun127@gmail.com>
2019-05-30 11:59:28 +0900

    refactor : WinningLottoTest 클래스 리펙토링
    1. numbers Set 구조 생성의 중복을 없애기 위해 setUp 메소드와 tearDown 메소드를 추가한다

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,37 +1,43 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningLottoTest {
-    @Test
-    public void 당첨_번호_로또_생성_테스트() {
-        List<LottoNumber> numbers = Arrays.asList(
+    private Set<LottoNumber> numbers;
+
+    @BeforeEach
+    public void setUp() {
+        numbers = new HashSet<>(Arrays.asList(
                 LottoNumber.get(1),
                 LottoNumber.get(2),
                 LottoNumber.get(3),
                 LottoNumber.get(4),
                 LottoNumber.get(5),
                 LottoNumber.get(6)
-        );
+        ));
+    }
+
+    @Test
+    public void 당첨_번호_로또_생성_테스트() {
         assertThat(new WinningLotto(new Lotto(numbers))).isEqualTo(new WinningLotto(new Lotto(numbers)));
     }
 
     @Test
     public void 로또_당첨_결과_계산_테스트() {
-        List<LottoNumber> numbers = Arrays.asList(
-                LottoNumber.get(1),
-                LottoNumber.get(2),
-                LottoNumber.get(3),
-                LottoNumber.get(4),
-                LottoNumber.get(5),
-                LottoNumber.get(6)
-        );
         assertThat(new WinningLotto(new Lotto(numbers)).calculateRank(new Lotto(numbers)))
                 .isEqualTo(LottoRank.FIRST);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        numbers = null;
     }
 }
