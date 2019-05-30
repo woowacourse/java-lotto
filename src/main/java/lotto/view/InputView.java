1c2989ca33780e5ea3e6cce6a5c29d6b42d0f7d6
김성훈 <seonghun127@gmail.com>
2019-05-30 20:59:50 +0900

    feat : NumberException 예외 처리 클래스 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.domain.NumberException;
+
 import java.util.Scanner;
 
 public class InputView {
@@ -7,7 +9,11 @@ public class InputView {
 
     public static int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(SCANNER.nextLine().replaceAll(" ", ""));
+        try {
+            return Integer.parseInt(SCANNER.nextLine().replaceAll(" ", ""));
+        } catch (NumberFormatException e) {
+            throw new NumberException();
+        }
     }
 
     public static String inputWinningLotto() {
@@ -17,14 +23,23 @@ public class InputView {
 
     public static int inputBonusNumber() {
         System.out.println("보너스 볼을 입력해주세요.");
-        return Integer.parseInt(SCANNER.nextLine().replaceAll(" ", ""));
+        try{
+            return Integer.parseInt(SCANNER.nextLine().replaceAll(" ", ""));
+        } catch(NumberFormatException e){
+            throw new NumberException();
+        }
     }
 
     public static int inputManualPurchaseCount() {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
-        return Integer.parseInt(SCANNER.nextLine().replaceAll(" ", ""));
+        try{
+            return Integer.parseInt(SCANNER.nextLine().replaceAll(" ", ""));
+        } catch(NumberFormatException e){
+            throw new NumberException();
+        }
     }
 
+    // TODO 여러줄 한번에 입력받는 형식을 생각해볼 것
     public static String inputManualLottoNumbers() {
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
         return SCANNER.nextLine().replaceAll(" ", "");
