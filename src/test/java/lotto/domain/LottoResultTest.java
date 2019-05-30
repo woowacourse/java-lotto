5411d98c9c84c998a98a02fab533a25eeb8bd838
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-30 14:00:51 +0900

    refactor: 사용하지 않는 import문 제거

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static lotto.domain.Rank.*;
