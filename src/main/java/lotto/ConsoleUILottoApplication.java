6967994a14dff561535849dc51c2f10e7c366937
Seongmo Jeon <oeeen3@gmail.com>
2019-06-01 19:46:41 +0900

    feat: add exception handling

--- src/main/java/lotto/ConsoleUILottoApplication.java
+++ src/main/java/lotto/ConsoleUILottoApplication.java
@@ -11,20 +11,25 @@ import java.util.List;
 
 public class ConsoleUILottoApplication {
     public static void main(String[] args) {
-        int inputMoney = InputView.receiveLottoMoney();
-        LottoMoney lottoMoney = new LottoMoney(inputMoney);
+        try {
+            int inputMoney = InputView.receiveLottoMoney();
+            LottoMoney lottoMoney = new LottoMoney(inputMoney);
 
-        List<String> manualLottos = InputView.receiveManualLotto();
+            List<String> manualLottos = InputView.receiveManualLotto();
 
-        Lottos totalLottos = new Lottos(manualLottos, lottoMoney.getCountOfTicket());
-        OutputView.printLottos(totalLottos, manualLottos.size());
+            Lottos totalLottos = new Lottos(manualLottos, lottoMoney.getCountOfTicket());
+            OutputView.printLottos(totalLottos, manualLottos.size());
 
-        String winningLottoInput =  InputView.receiveWinning();
-        int bonusBall = InputView.receiveBonus();
-        WinningLotto winningLotto = new WinningLotto(winningLottoInput, bonusBall);
+            String winningLottoInput =  InputView.receiveWinning();
+            int bonusBall = InputView.receiveBonus();
+            WinningLotto winningLotto = new WinningLotto(winningLottoInput, bonusBall);
 
 
-        LottoResult lottoResult = new LottoResult(totalLottos, winningLotto);
-        OutputView.printLottoResult(lottoResult);
+            LottoResult lottoResult = new LottoResult(totalLottos, winningLotto);
+            OutputView.printLottoResult(lottoResult);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
