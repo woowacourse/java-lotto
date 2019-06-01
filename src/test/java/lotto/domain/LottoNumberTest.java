8cde97c6e0e24751606c4a970f55c5694abacd6a
starkim06 <starkim06@naver.com>
2019-06-01 15:04:17 +0900

    refactor: 테스트 메서드 이름 수정

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -3,18 +3,18 @@ package lotto.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoNumberTest {
     @Test
-    void 범위밖의수_입력1() {
+    void 범위밖의수_입력_범위초과_오류() {
         assertThrows(IllegalArgumentException.class, () -> {
             LottoNumber.valueOf(46);
         });
     }
 
     @Test
-    void 범위밖의수_입력2() {
+    void 범위밖의수_입력_범위미만_오류() {
         assertThrows(IllegalArgumentException.class, () -> {
             LottoNumber.valueOf(0);
         });
