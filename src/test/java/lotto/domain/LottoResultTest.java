4e1179450578b4756c4539ad6a1a8a073b676eb3
Seongmo Jeon <oeeen3@gmail.com>
2019-06-01 18:56:45 +0900

    test: 수익률 계산 테스트 추가

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -14,4 +14,12 @@ public class LottoResultTest {
         LottoResult lottoResult = new LottoResult(lottos, winningLotto);
         assertThat(lottoResult).isEqualTo(new LottoResult(lottos, winningLotto));
     }
+
+    @Test
+    void 수익률계산() {
+        Lottos lottos = new Lottos(Arrays.asList("1,2,3,4,5,6", "10,11,12,13,14,15"), 2);
+        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6");
+        LottoResult lottoResult = new LottoResult(lottos, winningLotto);
+        assertThat(lottoResult.getYield()).isEqualTo(1000000.0);
+    }
 }
