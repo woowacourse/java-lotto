f7e353ada46b698f9bbfd7e76a8343deefdf1e5d
Seongmo Jeon <oeeen3@gmail.com>
2019-06-01 19:22:51 +0900

    feat: add console InputView

--- src/main/java/lotto/ConsoleUILottoApplication.java
+++ src/main/java/lotto/ConsoleUILottoApplication.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.domain.LottoMoney;
+import lotto.domain.Lottos;
+import lotto.view.InputView;
+
+import java.util.List;
+
 public class ConsoleUILottoApplication {
     public static void main(String[] args) {
+        int inputMoney = InputView.receiveLottoMoney();
+        LottoMoney lottoMoney = new LottoMoney(inputMoney);
 
+        List<String> manualLottos = InputView.receiveManualLotto();
+
+        Lottos totalLottos = new Lottos(manualLottos, lottoMoney.getCountOfTicket());
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,25 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static int receiveLottoMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static List<String> receiveManualLotto() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int manualLottoCount = Integer.parseInt(SCANNER.nextLine());
+        List<String> manualLottos = new ArrayList<>();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        for (int i = 0; i < manualLottoCount; i++) {
+            manualLottos.add(SCANNER.nextLine());
+        }
+        return manualLottos;
+    }
 }
