28bcecf9340db5a6be07712e42ffbe7ada1333eb
yunheehyeon <ok94life@daum.net>
2019-06-01 22:33:28 +0900

    refactor : 중복된 로또 숫자 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,23 +1,27 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     public static final int LOTTO_SIZE = 6;
 
-    private final List<Number> lotto;
+    private final Set<Number> lotto;
 
     public Lotto(final List<Number> lotto) {
-        validate(lotto);
-        Collections.sort(lotto);
-        this.lotto = lotto;
+        validateSize(lotto);
+        this.lotto = new TreeSet<>(lotto);
+        validateOverlap();
     }
 
-    private void validate(final List<Number> lotto) {
+    private void validateSize(List<Number> lotto) {
         if (lotto.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("로또는 6자리 입니다.");
+        }
+    }
+
+    private void validateOverlap() {
+        if (this.lotto.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또는 중복 없이 6자리 입니다.");
         }
     }
 
