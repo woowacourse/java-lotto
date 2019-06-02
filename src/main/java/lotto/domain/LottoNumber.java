e5fb2477476c653c96f6dc74f9dc006896ce0fa5
starkim06 <starkim06@naver.com>
2019-06-02 19:59:09 +0900

    refactor: 상수로 추출

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 public class LottoNumber implements Comparable<LottoNumber> {
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
+    private static final int INDEX_MINUS_ONE = 1;
 
     private final int lottoNumber;
 
@@ -17,7 +18,7 @@ public class LottoNumber implements Comparable<LottoNumber> {
 
     public static LottoNumber valueOf(final int number) {
         if (number >= LOTTO_MIN_NUMBER && number <= LOTTO_MAX_NUMBER) {
-            return LottoNumberCache.cache.get(number - 1);
+            return LottoNumberCache.cache.get(number - INDEX_MINUS_ONE);
         }
         return new LottoNumber(number);
     }
