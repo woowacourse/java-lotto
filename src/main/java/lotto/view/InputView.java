ed8d5a81a78cea4fb42b9a032b50cc3d66e2740e
heebong <heebg2@naver.com>
2019-06-02 17:00:27 +0900

    feat 수동 입력시 0 미만 체크 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -16,7 +16,7 @@ public class InputView {
     private static final String INPUT_PRICE = "구입금액을 입력해 주세요.";
     private static final String INPUT_MANUAL = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String EX_USER_PRICE_FORMAT_MESSAGE = "1000 이상의 숫자만 입력해주세요.";
-    private static final String EX_MANUAL_COUNT_RANGE_MESSAGE = "구입 금액을 초과하지 않는 숫자를 입력해주세요(로또는 한장에 1000원입니다.)";
+    private static final String EX_MANUAL_COUNT_RANGE_MESSAGE = "0 이상의 구입 금액을 초과하지 않는 숫자를 입력해주세요(로또는 한장에 1000원입니다.)";
     private static final int MIN_USER_PRICE_RANGE = 1000;
     private static final int ONE_LOTTO_PRICE = MIN_USER_PRICE_RANGE;
     private static final String EX_LOTTO_FORMAT_RANGE_MESSAGE = "1~45 사이의 숫자를 입력해주세요";
@@ -72,7 +72,7 @@ public class InputView {
     }
 
     private static void checkManualCount(double manualCount, double userPrice) {
-        if (manualCount * ONE_LOTTO_PRICE > userPrice) {
+        if (manualCount * ONE_LOTTO_PRICE > userPrice || manualCount < 0) {
             throw new IllegalArgumentException();
         }
     }
