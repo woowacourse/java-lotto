8987dc1cc4fcef7e8d102565b652b23cffca049b
men7627 <men1219@hanmail.net>
2019-06-03 16:00:29 +0900

    [Refactor] delete WinningInfoCreator

--- src/main/java/lotto/model/object/WinningInfo.java
+++ src/main/java/lotto/model/object/WinningInfo.java
@@ -1,13 +1,21 @@
 package lotto.model.object;
 
+import lotto.model.exception.WinningLottoBonusBallDuplicationException;
+
 public class WinningInfo {
         private Lotto winningLotto;
         private BonusBall bonusBall;
 
-
         public WinningInfo(final Lotto winningLotto, final BonusBall bonusBall) {
+                checkWinningLottoBonusBallDuplication(winningLotto, bonusBall);
                 this.winningLotto = winningLotto;
                 this.bonusBall = bonusBall;
+        }
+
+        private static void checkWinningLottoBonusBallDuplication(final Lotto winningLotto, final BonusBall bonusBall) {
+                if (winningLotto.hasBonusBall(bonusBall)) {
+                        throw new WinningLottoBonusBallDuplicationException("당첨 번호와 보너스볼이 중복됩니다.");
+                }
         }
 
         public int getMatchNumber(final Lotto purchasedLotto) {
