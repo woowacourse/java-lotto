78a52da50194b54e6f111973a840c3d72dda7aa2
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-06-04 00:53:52 +0900

    Fix exception messages

--- src/main/java/lotto/model/exceptions/IllegalNumberCombinationException.java
+++ src/main/java/lotto/model/exceptions/IllegalNumberCombinationException.java
@@ -1,6 +1,9 @@
 package lotto.model.exceptions;
 
 public class IllegalNumberCombinationException extends RuntimeException {
+    private static String MESSAGE = "잘못도니 숫자 조합입니다";
+
     public IllegalNumberCombinationException() {
+        super(MESSAGE);
     }
 }
