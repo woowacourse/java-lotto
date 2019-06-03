ed2b352f389ed050eb6725d3c882f5171fdf1144
jinwook <jin942002@naver.com>
2019-06-03 14:48:38 +0900

    add bonus number info at Prize class

--- src/main/java/lotto/domain/money/Prize.java
+++ src/main/java/lotto/domain/money/Prize.java
@@ -4,30 +4,34 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public enum Prize {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
-    MISS(0, 0);
+    FIRST(6, 2000000000, false),
+    SECOND(5, 30000000, true),
+    THIRD(5, 1500000, false),
+    FOURTH(4, 50000, false),
+    FIFTH(3, 5000, false),
+    MISS(0, 0, false);
 
+    private static int MATCH_NUMBER_LIMIT_TO_HAVE_PRIZE = 3;
     private int matchCount;
     private int prizeMoney;
+    private boolean hasBonusNumber;
 
-    Prize(int matchCount, int prizeMoney) {
+    Prize(int matchCount, int prizeMoney, boolean hasBonusNumber) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.hasBonusNumber = hasBonusNumber;
     }
 
     public static Prize getPrizeRank(int matchCount, boolean hasBonusNumber) {
-        try {
-            if (matchCount == THIRD.matchCount && !hasBonusNumber) {
-                return THIRD;
-            }
-            return Arrays.stream(values()).filter(prize -> prize.matchCount == matchCount).findAny().get();
-        } catch (NoSuchElementException e) {
+        if (matchCount < MATCH_NUMBER_LIMIT_TO_HAVE_PRIZE) {
             return MISS;
         }
+
+        if (matchCount == THIRD.matchCount && !hasBonusNumber) {
+                return THIRD;
+        }
+        return Arrays.stream(values()).filter(prize -> prize.matchCount == matchCount).findAny().get();
+
     }
 
     public int getPrizeMoney() {
