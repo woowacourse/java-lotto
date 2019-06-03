2b1ddc66f99bf091042906cc680d4a5608bd44c7
men7627 <men1219@hanmail.net>
2019-06-03 16:21:15 +0900

    [Refactor] delete ManualPurchaseNumberCreator

--- src/main/java/lotto/model/object/ManualPurchaseNumber.java
+++ src/main/java/lotto/model/object/ManualPurchaseNumber.java
@@ -1,13 +1,22 @@
 package lotto.model.object;
 
+import lotto.model.exception.InvalidManualPurchaseNumberException;
+
 public class ManualPurchaseNumber {
         private final int number;
 
-        public ManualPurchaseNumber(final int number) {
+        public ManualPurchaseNumber(final int number, final Payment payment) {
+                checkValidManualPurchaseNumber(number, payment);
                 this.number = number;
         }
 
         public int getNumber() {
                 return number;
+        }
+
+        private static void checkValidManualPurchaseNumber(final int number, final Payment payment) {
+                if (number < 0 || !payment.isPurchasable(number)) {
+                        throw new InvalidManualPurchaseNumberException("수동으로 구매할 로또 수는 0이상, 총 구매 수 이하입니다.");
+                }
         }
 }
