77869b6fc7854303cc25b89dfde03c73cf9a62cb
heebong <heebg2@naver.com>
2019-06-03 15:44:09 +0900

    refact OuptputView에서 문자열 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,7 +4,6 @@ import lotto.domain.Lotteries;
 import lotto.domain.Lotto;
 import lotto.domain.Rank;
 
-import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -14,9 +13,13 @@ import java.util.Map;
 public class OutputView {
     private static final String OUTPUT_BUY_LOTTO_COUNT = "수동으로 %d장, 자동으로 %d장을 구매하셨습니다.\n";
     private static final String TITLE_INPUT_AUTO_LOTTO = "수동으로 구매할 번호를 입력해 주세요.";
-    public static final String START_SYMBOL = "[";
-    public static final String END_SYMBOL = "]";
-    public static final String SEPARATOR = ",";
+    private static final String START_SYMBOL = "[";
+    private static final String END_SYMBOL = "]";
+    private static final String SEPARATOR = ",";
+    private static final String TITLE_RATE_OF_JACKPOT = "\n당첨 통계\n---------";
+    private static final String OUTPUT_SECOND_RANK = "%d개 일치, 보너스 볼 일치(%d원) - %d개";
+    private static final String OUTPUT_DEFALUT_RANK = "%d개 일치 (%d)- %d개";
+    private static final String OUTPUT_RATE = "총 수익률은 %.0f%s입니다.";
 
     public static void outputUserBuyLottoCount(long manualCount, long autoCount) {
         System.out.printf(OUTPUT_BUY_LOTTO_COUNT, manualCount, autoCount);
@@ -33,7 +36,7 @@ public class OutputView {
     }
 
     public static void outputLotteriesResult(Map<Rank, Integer> rankResult) {
-        System.out.println("\n당첨 통계\n---------");
+        System.out.println(TITLE_RATE_OF_JACKPOT);
         for (Map.Entry<Rank, Integer> rankIntegerEntry : rankResult.entrySet()) {
             generateResultSentence(rankIntegerEntry.getKey(), rankIntegerEntry.getValue());
         }
@@ -44,13 +47,13 @@ public class OutputView {
             return;
         }
         if (rank.equals(Rank.SECOND)) {
-            System.out.println(String.format("%d개 일치, 보너스 볼 일치(%d원) - %d개", rank.getCountOfMatch(), rank.getWinningMoney(), matchCount));
+            System.out.println(String.format(OUTPUT_SECOND_RANK, rank.getCountOfMatch(), rank.getWinningMoney(), matchCount));
             return;
         }
-        System.out.println(String.format("%d개 일치 (%d)- %d개", rank.getCountOfMatch(), rank.getWinningMoney(), matchCount));
+        System.out.println(String.format(OUTPUT_DEFALUT_RANK, rank.getCountOfMatch(), rank.getWinningMoney(), matchCount));
     }
 
     public static void outputLotteriesRate(float rate) {
-        System.out.println(String.format("총 수익률은 %.0f%s입니다.", rate, "%"));
+        System.out.println(String.format(OUTPUT_RATE, rate, "%"));
     }
 }
