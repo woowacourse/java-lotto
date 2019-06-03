3e3a5d3f989315ada10043530425c19540e52eb6
men7627 <men1219@hanmail.net>
2019-06-03 14:34:49 +0900

    [Refactor] add Factory method & modify map (public -> private)

--- src/main/java/lotto/model/object/LottoNumber.java
+++ src/main/java/lotto/model/object/LottoNumber.java
@@ -4,20 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LottoNumber {
-        public static final int MAX_LOTTO_NUMBER = 45;
+        private static final int MAX_LOTTO_NUMBER = 45;
         private final int number;
 
         private LottoNumber(int number) {
                 this.number = number;
         }
 
-        public static final Map<Integer, LottoNumber> MAPPING_LOTTO_NUMBER = new HashMap<Integer, LottoNumber>() {
+        private static final Map<Integer, LottoNumber> MAPPING_LOTTO_NUMBER = new HashMap<Integer, LottoNumber>() {
                 {
                         for (int number = 1; number <= MAX_LOTTO_NUMBER; number++) {
                                 put(number, new LottoNumber(number));
                         }
                 }
         };
+
+        public LottoNumber getInstance(int number){
+                return MAPPING_LOTTO_NUMBER.get(number);
+        }
 
         public int getNumber() {
                 return number;
