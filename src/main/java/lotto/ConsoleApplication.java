2e92bd3668a4c5507bd2c10c71fffa05513e10ea
yk1028 <wongeunsong94@gmail.com>
2019-06-03 15:19:08 +0900

    refactor: 에러 발생시 다시 입력받도록 수정

--- src/main/java/lotto/ConsoleApplication.java
+++ src/main/java/lotto/ConsoleApplication.java
@@ -21,41 +21,56 @@ public class ConsoleApplication {
     private static PurchaseInformation setUpPurchaseInformation() {
         try {
             Money money = new Money(InputView.inputMoney());
-            LottoCount lottoCount =
-                    new LottoCount(InputView.inputNumberOfManualLottos(), money);
-            PurchaseInformation purchaseInformation = new PurchaseInformation(lottoCount);
-            if (purchaseInformation.hasManualLottos()) {
-                registerManualLottosNumbers(purchaseInformation);
-            }
-            return purchaseInformation;
+            LottoCount lottoCount = new LottoCount(InputView.inputNumberOfManualLottos(), money);
+            return new PurchaseInformation(lottoCount);
+        } catch (NumberFormatException e) {
+            System.err.println("잘못된 입력입니다.");
+            return setUpPurchaseInformation();
         } catch (Exception e) {
             System.err.println(e.getMessage());
             return setUpPurchaseInformation();
         }
     }
 
+    private static Lottos purchaseLottos(PurchaseInformation purchaseInformation) {
+        try {
+            registerManualLottosNumbers(purchaseInformation);
+
+            Lottos lottos = LottoMachine.buyLottos(purchaseInformation);
+
+            OutputView.outputLottosPurchaseMessage(purchaseInformation);
+            OutputView.outputLottos(lottos);
+            return lottos;
+        } catch (NumberFormatException e) {
+            System.err.println("잘못된 입력입니다.");
+            return purchaseLottos(purchaseInformation);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return purchaseLottos(purchaseInformation);
+        }
+    }
+
     private static void registerManualLottosNumbers(PurchaseInformation purchaseInformation) {
+        if (!purchaseInformation.hasManualLottos()) {
+            return;
+        }
+
         OutputView.requestManualLottosMessage();
         for (int i = 0; i < purchaseInformation.getManualLottoCount(); i++) {
             purchaseInformation.addManualLottoNumbers(InputView.inputLottoNumbers());
         }
     }
 
-    private static Lottos purchaseLottos(PurchaseInformation purchaseInformation) {
-        Lottos lottos = LottoMachine.buyLottos(purchaseInformation);
-
-        OutputView.outputLottosPurchaseMessage(purchaseInformation);
-        OutputView.outputLottos(lottos);
-        return lottos;
-    }
-
     private static LottoGame setUpLottoGame() {
-        OutputView.requestWinningNumbersMessage();
         try {
+            OutputView.requestWinningNumbersMessage();
+
             ManualLottoNumbersGenerator manualLottoNumbersGenerator = ManualLottoNumbersGenerator.getInstance();
             manualLottoNumbersGenerator.register(InputView.inputLottoNumbers());
             LottoNumbers winningNumbers = manualLottoNumbersGenerator.generate();
+
             LottoNumber bonusNumber = LottoNumber.valueOf(InputView.inputBonusBall());
+
             WinningInformation winningInformation = new WinningInformation(winningNumbers, bonusNumber);
             return new LottoGame(winningInformation);
         } catch (Exception e) {
