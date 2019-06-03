7dddc814bb40e9b4acf90ececb4ac0ac6596bdff
yejilee <yjll1019@naver.com>
2019-06-03 14:53:34 +0900

    Refactor : add test code in LottoRankTest class

--- src/test/java/com/woowacourse/lotto/domain/LottoRankTest.java
+++ src/test/java/com/woowacourse/lotto/domain/LottoRankTest.java
@@ -15,4 +15,29 @@ class LottoRankTest {
 		assertTrue(FIFTH.getPrice() == 5_000);
 		assertTrue(ZERO.getPrice() == 0);
 	}
+
+	@Test
+	void getCount() {
+		assertTrue(FIRST.getCount() == 6);
+		assertTrue(SECOND.getCount() == 5);
+		assertTrue(THIRD.getCount() == 5);
+		assertTrue(FOURTH.getCount() == 4);
+		assertTrue(FIFTH.getCount() == 3);
+		assertTrue(ZERO.getCount() == 0);
+	}
+
+	@Test
+	void checkValueOf() {
+		assertTrue(LottoRank.valueOf(6, false) == FIRST);
+		assertTrue(LottoRank.valueOf(5, true) == SECOND);
+		assertTrue(LottoRank.valueOf(5, false) == THIRD);
+		assertTrue(LottoRank.valueOf(4, true) == FOURTH);
+		assertTrue(LottoRank.valueOf(4, false) == FOURTH);
+		assertTrue(LottoRank.valueOf(3, true) == FIFTH);
+		assertTrue(LottoRank.valueOf(3, false) == FIFTH);
+		assertTrue(LottoRank.valueOf(2, true) == ZERO);
+		assertTrue(LottoRank.valueOf(2, false) == ZERO);
+		assertTrue(LottoRank.valueOf(1, true) == ZERO);
+		assertTrue(LottoRank.valueOf(1, false) == ZERO);
+	}
 }
