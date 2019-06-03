01914f763f8f9049193b878d0a512e1a80f763df
DaejunBae <dpudpu11@gmail.com>
2019-06-03 12:20:37 +0900

    Test RankTest 테스트케이스 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -8,7 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class RankTest {
     @Test
     void SECOND_THIRD_등수_확인() {
+        assertThat(Rank.FIRST).isEqualTo(Rank.valueOf(6, true));
         assertThat(Rank.SECOND).isEqualTo(Rank.valueOf(5, true));
         assertThat(Rank.THIRD).isEqualTo(Rank.valueOf(5, false));
+        assertThat(Rank.FOURTH).isEqualTo(Rank.valueOf(4, false));
+        assertThat(Rank.FIFTH).isEqualTo(Rank.valueOf(3, true));
+        assertThat(Rank.MISS).isEqualTo(Rank.valueOf(2, false));
+
     }
 }
