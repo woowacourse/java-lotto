d8fc8606737d9e92c07ea0054b5839a49fbf005e
ep1stas1s <masloz@konkuk.ac.kr>
2019-06-04 16:33:29 +0900

    feat: Add InputView class

--- src/main/java/lotto/ConsoleUILottoApplication.java
+++ src/main/java/lotto/ConsoleUILottoApplication.java
@@ -1,46 +1,87 @@
 package lotto;
 
 import lotto.domain.*;
+import lotto.domain.lottogenerator.LottoGenerator;
 import lotto.domain.lottogenerator.ManualLottoGeneratingStrategy;
 import lotto.domain.lottogenerator.RandomLottoGeneratingStrategy;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ConsoleUILottoApplication {
     public static void main(String[] args) {
-        Payment payment = new Payment(5000);
-        int countOfManualLotto = 2;
+        Payment payment = inputPayment();
+        CountOfLotto countOfLotto = inputCountOfManualLotto(payment);
 
         LottoRepository lottoRepository = new LottoRepository();
-        // 수동 로또 생성
-        for (int i = 0; i < countOfManualLotto; i++) {
-            List<Integer> input = Arrays.asList(1, 2, 3, 4, 5, 7);
-            lottoRepository.register(new ManualLottoGeneratingStrategy(input));
+        for (int i = 0; i < countOfLotto.getCountOfManualLotto(); i++) {
+            inputLottoNumber(lottoRepository);
         }
 
-        // random lotto 생성
-        for (int i = 0; i < 5 - countOfManualLotto; i++) {
+        for (int i = 0; i < countOfLotto.getCountOfRandomLotto(); i++) {
             lottoRepository.register(new RandomLottoGeneratingStrategy());
         }
 
         LottoTickets lottoTickets = new LottoTickets(lottoRepository);
+        OutputView.printLotto(countOfLotto, lottoTickets);
 
-        // winning Lotto 입력
-        WinningLotto winningLotto = new WinningLotto(new Lotto(Arrays.asList(
-                LottoNumber.getNumber(1),
-                LottoNumber.getNumber(2),
-                LottoNumber.getNumber(3),
-                LottoNumber.getNumber(4),
-                LottoNumber.getNumber(5),
-                LottoNumber.getNumber(6))),
-                new BonusNumber(7));
-
-        OutputView.printLottoTickets(lottoTickets);
+        WinningLotto winningLotto = inputWinningLotto();
 
         Result result = winningLotto.produceResult(lottoTickets);
-
         OutputView.printResult(result, payment);
+    }
+
+    public static Payment inputPayment() {
+        try {
+            String input = InputView.inputPayment();
+            return new Payment(Integer.parseInt(input));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return inputPayment();
+        }
+    }
+
+    public static CountOfLotto inputCountOfManualLotto(Payment payment) {
+        try {
+            String input = InputView.inputCountOfManualLotto();
+            return new CountOfLotto(payment, Integer.parseInt(input));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return inputCountOfManualLotto(payment);
+        }
+    }
+
+    public static void inputLottoNumber(LottoRepository lottoRepository) {
+        try {
+            String input = InputView.inputLottoNumber();
+            List<Integer> list = Arrays.stream(input.split(", "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            lottoRepository.register(new ManualLottoGeneratingStrategy(list));
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            inputLottoNumber(lottoRepository);
+        }
+    }
+
+    public static WinningLotto inputWinningLotto() {
+        try {
+            String inputLotto = InputView.inputWinningLottoNumber();
+            List<Integer> list = Arrays.stream(inputLotto.split(", "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            Lotto lotto = LottoGenerator.create(new ManualLottoGeneratingStrategy(list));
+
+            String inputBonusBall = InputView.inputBonusBall();
+            return new WinningLotto(lotto, new BonusNumber(Integer.parseInt(inputBonusBall)));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return inputWinningLotto();
+        }
+
     }
 }
