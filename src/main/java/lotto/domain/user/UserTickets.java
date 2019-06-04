0a1aaf95a699ca0b5ea2461ca56d08adf9a030dd
강연욱 <fiasboc23@gmail.com>
2019-06-04 19:08:00 +0900

    refactor : complete the second step

--- src/main/java/lotto/domain/user/UserTickets.java
+++ src/main/java/lotto/domain/user/UserTickets.java
@@ -2,6 +2,7 @@ package lotto.domain.user;
 
 import lotto.domain.Rank;
 import lotto.domain.WinningLotto;
+import lotto.domain.lottomanager.BonusBall;
 import lotto.domain.lottomanager.LottoCreator;
 import lotto.domain.lottomanager.LottoTicket;
 import lotto.domain.lottomanager.shufflerule.Shuffle;
@@ -10,12 +11,11 @@ import lotto.utils.NullCheckUtil;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class UserTickets {
     private List<LottoTicket> userLottoTickets;
 
-    public UserTickets(PurchaseAmount purchaseAmount, Shuffle shuffle) {
+    private UserTickets(PurchaseAmount purchaseAmount, Shuffle shuffle) {
         List<LottoTicket> lottoTickets = getCreatedLottoTickets(purchaseAmount, shuffle);
         NullCheckUtil.checkNullLottoTickets(lottoTickets);
         this.userLottoTickets = lottoTickets;
@@ -35,14 +35,42 @@ public class UserTickets {
         return purchaseAmount.isEqualsAmount(lottoTickets.size());
     }
 
+    public static UserTickets createUserTickets(PurchaseAmount purchaseAmount, Shuffle shuffle){
+        NullCheckUtil.checkNullPurchaseAmount(purchaseAmount);
+        NullCheckUtil.checkNullShuffle(shuffle);
+
+        return new UserTickets(purchaseAmount, shuffle);
+    }
+
     public List<LottoTicket> getUserLottoTickets() {
         return Collections.unmodifiableList(userLottoTickets);
     }
 
-    public List<Rank> getMatchedRanks(WinningLotto winningLotto) {
-        return userLottoTickets.stream()
-                .map(winningLotto::getMatchedWinningNumbersCount)
-                .map(Rank::valueOf)
-                .collect(Collectors.toList());
+    public List<Rank> getMatchedRanks(WinningLotto winningLotto, BonusBall bonusBall) {
+        NullCheckUtil.checkNullWinningLotto(winningLotto);
+        NullCheckUtil.checkNullBonusBall(bonusBall);
+
+        return Collections.unmodifiableList(getFilledRanks(winningLotto, bonusBall));
+    }
+
+    private List<Rank> getFilledRanks(WinningLotto winningLotto, BonusBall bonusBall) {
+        List<Rank> matchedRanks = new ArrayList<>();
+
+        for (LottoTicket userTicket : userLottoTickets){
+            matchedRanks.add(getRank(winningLotto, bonusBall, userTicket));
+        }
+        return matchedRanks;
+    }
+
+    private Rank getRank(WinningLotto winningLotto, BonusBall bonusBall, LottoTicket userTicket) {
+        return Rank.valueOf(getMatchOfCount(winningLotto, userTicket), getMatchBonus(bonusBall, userTicket));
+    }
+
+    private Integer getMatchOfCount(WinningLotto winningLotto, LottoTicket userTicket) {
+        return winningLotto.getMatchedWinningNumbersCount(userTicket);
+    }
+
+    private Boolean getMatchBonus(BonusBall bonusBall, LottoTicket userTicket) {
+        return userTicket.isContainedNumbers(bonusBall);
     }
 }
