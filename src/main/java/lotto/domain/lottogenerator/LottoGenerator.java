180eda33cd1c16158e128e86ab08c80fc8099d24
ep1stas1s <masloz@konkuk.ac.kr>
2019-06-05 03:33:45 +0900

    refactor: Block creating constructor

--- src/main/java/lotto/domain/lottogenerator/LottoGenerator.java
+++ src/main/java/lotto/domain/lottogenerator/LottoGenerator.java
@@ -4,12 +4,15 @@ import lotto.domain.Lotto;
 
 import java.util.Objects;
 
-public class LottoGenerator {
+public abstract class LottoGenerator {
+    private LottoGenerator() {
+        throw new AssertionError();
+    }
+
     public static Lotto create(LottoGeneratingStrategy strategy) {
         if (Objects.isNull(strategy)) {
             throw new NullPointerException();
         }
-
         return new Lotto(strategy.generate());
     }
 }
