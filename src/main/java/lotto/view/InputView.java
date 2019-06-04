380c0223f1aa16e6863fe8850d0e2e08da609735
choihz <choixhz@gmail.com>
2019-06-04 16:13:45 +0900

    feat: OutputView Class
    수동으로 구매할 로또 수 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -17,4 +17,25 @@ public class InputView {
             return getMoney();
         }
     }
+
+    public static int getNumberOfManualLotto(Money money) {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        try {
+            int numberOfManualLotto = Integer.parseInt(SCANNER.nextLine());
+            validateNumberOfManualLotto(numberOfManualLotto, money);
+            return numberOfManualLotto;
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            return getNumberOfManualLotto(money);
+        }
+    }
+
+    private static void validateNumberOfManualLotto(int numberOfManualLotto, Money money) {
+        if (numberOfManualLotto < 0) {
+            throw new IllegalArgumentException("0 이상의 숫자만 입력할 수 있습니다.");
+        }
+        if (numberOfManualLotto > money.getNumberOfLotto()) {
+            throw new IllegalArgumentException("구매 금액을 초과하는 숫자입니다.");
+        }
+    }
 }
