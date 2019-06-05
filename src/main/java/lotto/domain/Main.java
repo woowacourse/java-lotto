bf45de878be8dac0a193b1ff03c36ec8b583bef4
Soojin Roh <sooreal91@gmail.com>
2019-06-05 20:27:57 +0900

    refactor Main에서 가지고 있던 메서드들을 각각의 객체로 분산

--- src/main/java/lotto/domain/Main.java
+++ src/main/java/lotto/domain/Main.java
@@ -2,12 +2,12 @@ package lotto.domain;
 
 import lotto.domain.model.Number;
 import lotto.domain.model.*;
+import lotto.domain.utils.AutoLottoGenerator;
+import lotto.domain.utils.ManualLottoGenerator;
 import lotto.domain.utils.ShuffledNumberGenerator;
 import lotto.domain.view.InputView;
 import lotto.domain.view.OutputView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,67 +18,18 @@ public class Main {
         Money money = new Money(InputView.inputMoney());                           // 구입 금액
         int manualLottoSize = InputView.inputManualLottoSize(money);               // 수동 로또 갯수
 
-        purchasedLottos.addLottos(makeManualLotto(manualLottoSize));               // 수동 로또 생성
-        purchasedLottos.addLottos(makeAutoLotto(money, manualLottoSize));          // 자동 로또 생성
-        OutputView.printPurchasedLottoResult(purchasedLottos, manualLottoSize);    // 로또 구입 결과 출력
-
-        WinningLotto winningLotto = makeWinningLotto();                            // 지난 주 당첨 로또 생성
-
-        List<Rank> ranks = judgeRank(purchasedLottos, winningLotto);               // 당첨 확인, 등수 판단
-        Map<Rank, Integer> prizeResult = calculatePrize(ranks);                    // 상금 계산
-        OutputView.printLottoResult(money, prizeResult);                           // 결과 출력
-    }
-
-    private static List<Lotto> makeAutoLotto(Money money, int manualLottoSize) {
-        List<Lotto> autoLottos = new ArrayList<>();
-        int autoLottoSize = money.availablePurchseTicketCount() - manualLottoSize;
-
-        for (int i = 1; i <= autoLottoSize; i++) {
-            autoLottos.add(new Lotto(ShuffledNumberGenerator.getShuffledNumbers()));
-        }
-        return autoLottos;
-    }
-
-    private static List<Lotto> makeManualLotto(int manualLottoSize) {
         InputView.printInputManualLottoMessage();
+        purchasedLottos.addLottos(new ManualLottoGenerator(manualLottoSize).generate(InputView.inputLottoNumber()));                               // 수동 로또 생성
+        purchasedLottos.addLottos(new AutoLottoGenerator(money, manualLottoSize).generate(ShuffledNumberGenerator.getShuffledNumbers()));          // 자동 로또 생성
+        OutputView.printPurchasedLottoResult(purchasedLottos, manualLottoSize);  // 로또 구입 결과 출력
 
-        List<Lotto> manualLottos = new ArrayList<>();
-        Lotto lotto;
-
-        for (int i = 1; i <= manualLottoSize; i++) {
-            lotto = new Lotto(InputView.inputLottoNumber());
-            manualLottos.add(lotto);
-        }
-        return manualLottos;
-    }
-
-    private static WinningLotto makeWinningLotto() {
         InputView.printInputWinningLottoMessage();
         Lotto winLotto = new Lotto(InputView.inputLottoNumber());
         Number bonusNumber = InputView.inputBonusNumber(winLotto);
-        return new WinningLotto(winLotto, bonusNumber);
+        WinningLotto winningLotto = new WinningLotto(winLotto, bonusNumber);  // 지난 주 당첨 로또 생성
+        List<Rank> ranks = winningLotto.match(purchasedLottos);  // 당첨 확인, 등수 판단
+        Map<Rank, Integer> prizeResult = winningLotto.calculatePrize(ranks);  // 상금 계산
+        OutputView.printLottoResult(money, prizeResult);  // 결과 출력
     }
-
-    private static Map<Rank, Integer> calculatePrize(List<Rank> ranks) {
-        Map<Rank, Integer> winResult = new HashMap<>();
-        for (Rank rank : Rank.values()) {
-            winResult.put(rank, 0);
-        }
-
-        for (Rank rank : ranks) {
-            winResult.put(rank, (winResult.get(rank) + rank.getPrize()));
-        }
-        return winResult;
-    }
-
-    private static List<Rank> judgeRank(PurchasedLottos purchasedLottos, WinningLotto winningLotto) {
-        List<Rank> ranks = new ArrayList<>();
-
-        for (Lotto lotto : purchasedLottos.getLottos()) {
-            ranks.add(Rank.valueOf(winningLotto.matchCount(lotto), winningLotto.matchBonusNumber(lotto)));
-        }
-        return ranks;
-    }
-
 }
 
