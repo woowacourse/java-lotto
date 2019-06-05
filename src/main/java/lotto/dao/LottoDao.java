4c9594ca0b7b06759de7eec75d27032d8677c35a
DaejunBae <dpudpu11@gmail.com>
2019-06-05 17:25:03 +0900

    feat: LottoDao.class 로또 추가, 조회

---
+++ src/main/java/lotto/dao/LottoDao.java
@@ -0,0 +1,65 @@
+package lotto.dao;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoNo;
+import lotto.domain.generator.LottoNosManualGenerator;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoDao {
+
+    //TODO 중간에 하나라도 실패하면 롤백은 어떻게?
+    public boolean add(List<Lotto> lottos, int round) {
+        Connection conn = DBUtils.getConnection();
+        PreparedStatement ps = null;
+        int count = 0;
+        try {
+            for (final Lotto lotto : lottos) {
+                String sql = "INSERT INTO lotto (numbers, round) VALUES(?, ?)";
+                ps = conn.prepareStatement(sql);
+                ps.setString(1, lotto.toString());
+                ps.setInt(2, round);
+                count += ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.close(conn, ps);
+        }
+
+        return count == lottos.size();
+    }
+
+    public List<Lotto> findByRound(int round) {
+        Connection conn = DBUtils.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Lotto> lottos = new ArrayList<>();
+
+        try {
+            String sql = "SELECT numbers FROM lotto WHERE round = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, round);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Lotto lotto = Lotto.of(convertToLottoNos(rs.getString("numbers")));
+                lottos.add(lotto);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.close(conn, ps, rs);
+        }
+        return lottos;
+    }
+
+    private List<LottoNo> convertToLottoNos(final String numbers) {
+        List<LottoNo> lottoNos = new LottoNosManualGenerator(numbers.substring(1, numbers.length() - 1)).generate();
+        return lottoNos;
+    }
+}
---
+++ src/test/java/lotto/dao/LottoDaoTest.java
@@ -0,0 +1,28 @@
+package lotto.dao;
+
+import lotto.domain.Lotto;
+import lotto.domain.generator.LottoNosManualGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoDaoTest {
+
+    @Test
+    void addTest() {
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(Lotto.of(new LottoNosManualGenerator("1,2,3,4,5,6").generate()));
+        lottos.add(Lotto.of(new LottoNosManualGenerator("1,2,3,4,12,19").generate()));
+
+        assertTrue(new LottoDao().add(lottos,0));
+    }
+
+    @Test
+    void findByRoundTest() {
+        assertThat(0).isNotEqualTo(new LottoDao().findByRound(0).size());
+    }
+}
