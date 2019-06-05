4335f896905149a96413666f076f6ac64cced4c4
choihz <choixhz@gmail.com>
2019-06-05 14:18:24 +0900

    feat: ConsoleUIApplication Class
    컨트롤러 추가

---
+++ src/main/java/lotto/ConsoleUIApplication.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class ConsoleUIApplication {
+    public static void main(String[] args) {
+        Money money = InputView.getMoney();
+        int numberOfManualLotto = InputView.getNumberOfManualLotto(money);
+
+        LottoTickets lottoTickets = getLottoTickets(money, numberOfManualLotto);
+
+        OutputView.printLottoNumbers(money, numberOfManualLotto);
+        OutputView.printLottoTickets(lottoTickets);
+
+        WinningLotto winningLotto = InputView.getWinningLotto();
+        LottoResult lottoResult = lottoTickets.getLottoResult(winningLotto);
+
+        OutputView.printLottoStatistics(lottoResult);
+    }
+
+    private static LottoTickets getLottoTickets(Money money, int numberOfManualLotto) {
+        if (numberOfManualLotto > 0) {
+            List<Lotto> manualLotto = InputView.getManualLotto(numberOfManualLotto);
+            return LottoMachine.generateTickets(money, manualLotto);
+        }
+
+        return LottoMachine.generateTickets(money);
+    }
+}
