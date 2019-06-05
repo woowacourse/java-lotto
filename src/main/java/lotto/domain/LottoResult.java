6b6e3df08e65b55bc1bf6270a87fb4c66e253cc8
choihz <choixhz@gmail.com>
2019-06-05 14:07:55 +0900

    feat: LottoResult Class
    로또 결과를 의미하는 LottoResult 구현 및 테스트

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,58 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoResult {
+    private static final int ZERO = 0;
+    private static final int MONEY_PER_LOTTO = 1_000;
+
+    private Map<Rank, Integer> lottoResult;
+
+    LottoResult() {
+        lottoResult = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            lottoResult.put(rank, ZERO);
+        }
+    }
+
+    void add(Rank rank) {
+        lottoResult.put(rank, lottoResult.get(rank) + 1);
+    }
+
+    private String getEachResult(Rank rank) {
+        if (rank.equals(Rank.SECOND)) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%d원)- %d개\n", rank.getCountOfMatch(), rank.getWinningMoney(), lottoResult.get(rank));
+        }
+        if (rank.equals(Rank.MISS)) {
+            return "";
+        }
+        return String.format("%d개 일치 (%d원)- %d개\n", rank.getCountOfMatch(), rank.getWinningMoney(), lottoResult.get(rank));
+    }
+
+    double getRateOfReturn() {
+        long winningMoney = 0;
+        for (Rank rank : lottoResult.keySet()) {
+            winningMoney += (long) rank.getWinningMoney() * lottoResult.get(rank);
+        }
+        return (double) winningMoney / (getNumberOfLotto() * MONEY_PER_LOTTO);
+    }
+
+    private int getNumberOfLotto() {
+        int numberOfLotto = 0;
+        for (Integer value : lottoResult.values()) {
+            numberOfLotto += value;
+        }
+        return numberOfLotto;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Rank rank : Rank.values()) {
+            stringBuilder.append(getEachResult(rank));
+        }
+        stringBuilder.append(String.format("총 수익률은 %.1f%%입니다.", getRateOfReturn() * 100));
+        return stringBuilder.toString();
+    }
+}
---
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoResultTest {
+    @Test
+    public void 수익률_계산_테스트() {
+        LottoResult lottoResult = new LottoResult();
+        lottoResult.add(Rank.FIFTH);
+        lottoResult.add(Rank.FIFTH);
+
+        assertThat(lottoResult.getRateOfReturn()).isEqualTo(5);
+    }
+}
