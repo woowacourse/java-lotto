6766919ca16d29dd9e348d58839ced6e698a8265
stopsilver13 <stopsilver13>
2019-06-05 15:08:18 +0900

    [REFACTOR] Fix bug from Combine LottoNumber and LottoNumbers at LottoCreator
    수동 로또 생성 로직 구현
    > 이전에 주석처리 한 코드가 실수로 커밋 됨

--- src/main/java/lotto/AutoLottoCreator.java
+++ src/main/java/lotto/AutoLottoCreator.java
@@ -4,18 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AutoLottoCreator implements LottoCreator {
-    private LottoNumbers numbers;
-
-    public AutoLottoCreator() {
-        this.numbers = new LottoNumbers();
-    }
+    public AutoLottoCreator() {}
 
     @Override
     public List<Lotto> createLottos(int lottoQuantity) {
         List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 0; i < lottoQuantity; i++) {
-            lottos.add(new Lotto(numbers.getLottoNumbers()));
+            lottos.add(new Lotto(LottoNumber.getLottoNumbers()));
         }
         return lottos;
     }
--- src/main/java/lotto/ManualLottoCreator.java
+++ src/main/java/lotto/ManualLottoCreator.java
@@ -1,44 +1,52 @@
 package lotto;
 
+import lotto.exception.InvalidLottoNumbersException;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class ManualLottoCreator {
+public class ManualLottoCreator implements LottoCreator {
     private List<List<LottoNumber>> numbers = new ArrayList<>();
 
-    /*public ManualLottoCreator(List<String> inputs) {
-        LottoNumbers lottoNumbers = new LottoNumbers();
-
+    public ManualLottoCreator(List<String> inputs) {
         for (String input : inputs) {
-            List<LottoNumber> inputNumbers = lottoNumbers.getLottoNumbers(parseInts(input));
+            List<LottoNumber> inputNumbers = LottoNumber.getLottoNumbers(parseInts(input));
             numbers.add(inputNumbers);
         }
-    }*/
+    }
 
-    /*private List<Integer> parseInts(String input) {
+    private List<Integer> parseInts(String input) {
         List<Integer> numbers = new ArrayList<>();
 
         for (String s : input.split(",")) {
             numbers.add(parseInt(s));
         }
         return numbers;
-    }*/
+    }
 
-    /*private int parseInt(String input) {
+    private int parseInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new ;
         }
-    }*/
+    }
 
-    /*@Override
+    @Override
     public List<Lotto> createLottos(int lottoQuantity) {
+        checkNumberOfInputLotto(lottoQuantity);
+
         List<Lotto> lottos = new ArrayList<>();
 
-        for (int i = 0; i < lottoQuantity; i++) {
-            lottos.add(new Lotto());
+        for (List<LottoNumber> numbers : numbers) {
+            lottos.add(new Lotto(numbers));
         }
         return lottos;
-    }*/
+    }
+
+    private void checkNumberOfInputLotto(int lottoQuantity) {
+        if (numbers.size() != lottoQuantity) {
+            throw new InvalidLottoNumbersException("입력하신 수동 로또 수와 입력하신 번호 셋트의 수가 일치하지 않습니다.");
+        }
+    }
 }
