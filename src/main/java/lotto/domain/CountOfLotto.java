14d6df416d3ec84f1708fb690f56839f67b1b75d
gch01410 <gch01410@naver.com>
2019-06-06 16:55:54 +0900

    refactor: Extract check method from constructor

--- src/main/java/lotto/domain/CountOfLotto.java
+++ src/main/java/lotto/domain/CountOfLotto.java
@@ -11,21 +11,27 @@ public class CountOfLotto {
     private final int countOfRandomLotto;
 
     public CountOfLotto(Payment payment, String countOfManualLotto) {
-        if (Objects.isNull(payment) || StringUtils.isBlank(countOfManualLotto)) {
+        if (Objects.isNull(payment)) {
             throw new NullPointerException();
         }
 
+        int totalLotto = payment.calculateCountOfLotto();
+        checkCountOfManualLotto(totalLotto, countOfManualLotto);
+
+        this.countOfManualLotto = Integer.parseInt(countOfManualLotto);
+        this.countOfRandomLotto = totalLotto - this.countOfManualLotto;
+    }
+
+    private void checkCountOfManualLotto(int totalLotto, String countOfManualLotto) {
+        if (StringUtils.isBlank(countOfManualLotto)) {
+            throw new NullPointerException();
+        }
         if (!StringUtils.isNumeric(countOfManualLotto)) {
             throw new NaturalNumberException("수동생성할 로또 개수는 0이상 정수만 가능합니다");
         }
-
-        int totalLotto = payment.calculateCountOfLotto();
-
         if (totalLotto < Integer.parseInt(countOfManualLotto)) {
             throw new PaymentOutOfBoundsException("입력한 로또 개수가 지불한 금액을 초과합니다");
         }
-        this.countOfManualLotto = Integer.parseInt(countOfManualLotto);
-        this.countOfRandomLotto = totalLotto - this.countOfManualLotto;
     }
 
     public int getCountOfManualLotto() {
--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -12,18 +12,20 @@ public class Payment {
     private final int payment;
 
     public Payment(String payment) {
+        checkPayment(payment);
+        this.payment = Integer.parseInt(payment);
+    }
+
+    private void checkPayment(String payment) {
         if (StringUtils.isBlank(payment)) {
             throw new NullPointerException();
         }
-
         if (!StringUtils.isNumeric(payment)) {
             throw new NaturalNumberException("구입금액은 숫자를 입력해야합니다");
         }
-
         if (Integer.parseInt(payment) < LOTTO_PRICE) {
             throw new PaymentOutOfBoundsException(String.format("로또가격(%d)보다 높은 금액을 입력하세요", LOTTO_PRICE));
         }
-        this.payment = Integer.parseInt(payment);
     }
 
     public int calculateCountOfLotto() {
