d5e93e8182e5df2cf66398c93c4f016109cf41bd
deocksoo <dusol92@snu.ac.kr>
2019-06-06 22:03:27 +0900

    [STYLE] PurchseAmount 클래스의 메소드 순서를 변경합니다
    가독성을 위해 생성자에서 호출하는 순서에 맞게 수정하였습니다.

--- src/main/java/domain/PurchaseAmount.java
+++ src/main/java/domain/PurchaseAmount.java
@@ -16,15 +16,15 @@ public class PurchaseAmount {
         return new PurchaseAmount(amountOfMoney);
     }
 
-    private static void validateIfMultipleOfPricePerLotto(int amountOfMoney) {
-        if (amountOfMoney % Lotto.PRICE != FIT_AMOUNT) {
-            throw new IllegalArgumentException(Lotto.PRICE + "원의 배수만 입력할 수 있습니다.");
-        }
-    }
-
     private static void validateAmountOfMoney(int amountOfMoney) {
         if (amountOfMoney < Lotto.PRICE) {
             throw new IllegalArgumentException("고작 그정도 돈으로는 로또를 살 수 없습니다");
+        }
+    }
+
+    private static void validateIfMultipleOfPricePerLotto(int amountOfMoney) {
+        if (amountOfMoney % Lotto.PRICE != FIT_AMOUNT) {
+            throw new IllegalArgumentException(Lotto.PRICE + "원의 배수만 입력할 수 있습니다.");
         }
     }
 
