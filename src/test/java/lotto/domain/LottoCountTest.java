e521d5bb0e7ad3335c6a67eb95a0aa6060508f3d
hyperpace <cnk9109@gmail.com>
2019-06-06 23:20:24 +0900

    feat(LottoCount): create LottoCount adding create Exception

---
+++ src/test/java/lotto/domain/LottoCountTest.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class LottoCountTest {
+    @Test
+    void 로또_개수_금액_초과_테스트() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            LottoCountParser.parse(new Money(5000), "6");
+        }).withMessage("금액이 부족합니다.");
+    }
+}
