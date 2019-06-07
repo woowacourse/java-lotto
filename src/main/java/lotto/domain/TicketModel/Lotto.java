bececee491a9b8d7906b189ed68b0e2a513a4379
andole87 <andole87@gmail.com>
2019-06-08 00:23:44 +0900

    feat: Lotto 생성
    default modifier.
    세부 구현은 컨트롤러 등에게서 숨긴다.

---
+++ src/main/java/lotto/domain/TicketModel/Lotto.java
@@ -0,0 +1,42 @@
+package lotto.domain.TicketModel;
+
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lotto implements Ticket {
+    private final List<Integer> numbers;
+
+    Lotto(LottoNumbers lottoNumbers) {
+        this.numbers = lottoNumbers.numbers();
+    }
+
+    @Override
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+
+    @Override
+    public List<Integer> numbers() {
+        return numbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return numbers.equals(lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return "Lotto " +
+                numbers;
+    }
+}
