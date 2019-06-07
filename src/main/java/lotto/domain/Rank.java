f0bffe854e2c554516cb754118118defcb1d61eb
andole87 <andole87@gmail.com>
2019-06-08 00:31:57 +0900

    feat: Rank enum 생성
    로또 등수에 따른 보상을 관리할 클래스.
    보너스유무를 enum 필드로 넣었다.
    toString 오버라이드 예정.

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    LOSE(2, false, 0);
+
+
+    private int match;
+    private boolean bonus;
+    private int money;
+
+    Rank(int match, boolean bonus, int money) {
+        this.match = match;
+        this.bonus = bonus;
+        this.money = money;
+    }
+
+    public static Rank rank(int match, boolean bonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.match == match && rank.bonus == bonus)
+                .findAny()
+                .orElse(LOSE);
+    }
+
+    public int money() {
+        return money;
+    }
+
+    @Override
+    public String toString() {
+        return "Rank{" +
+                "match=" + match +
+                ", money=" + money +
+                '}';
+    }
+}
