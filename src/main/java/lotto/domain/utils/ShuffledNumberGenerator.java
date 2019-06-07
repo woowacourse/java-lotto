a2377e36be03c2a61bcb039657f8a1695dc75294
Soojin Roh <sooreal91@gmail.com>
2019-06-07 15:56:53 +0900

    refactor getShuffledNumbers() 메서드 수정
    기존에 numberPoll.subList()로 리턴해 사용하던 것을 새로운 List에 numberPoll.subList()를 넣어 리턴하는 것으로 수정.

--- src/main/java/lotto/domain/utils/ShuffledNumberGenerator.java
+++ src/main/java/lotto/domain/utils/ShuffledNumberGenerator.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class ShuffledNumberGenerator {
-    private static final List<Number> numberPoll = new ArrayList<>();
+    private static List<Number> numberPoll = new ArrayList<>();
 
     static {
         for (int key : NumberSet.lottoNumbers.keySet()) {
@@ -20,6 +20,7 @@ public class ShuffledNumberGenerator {
 
     public static List<Number> getShuffledNumbers() {
         Collections.shuffle(numberPoll);
-        return numberPoll.subList(0, LOTTO_SIZE);
+        List<Number> shuffledNumber = new ArrayList<>(numberPoll.subList(0, LOTTO_SIZE));
+        return shuffledNumber;
     }
 }
