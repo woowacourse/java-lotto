3ff10d579602aefc7fe61332edceee7118aa7dee
andole87 <andole87@gmail.com>
2019-06-08 00:13:38 +0900

    feat: UserLottoDto 생성
    역할
    문자열로 받은 로또 관련 정보를 파싱한다.
    UserLottos의 초기화에 필요한 정보를 제공한다.

---
+++ src/main/java/lotto/domain/Dto/UserLottoDto.java
@@ -0,0 +1,51 @@
+package lotto.domain.Dto;
+
+import lotto.domain.Exceptions.LottoNumberException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserLottoDto {
+    private int buyMoney;
+    private int manualCount;
+    private List<List<Integer>> manualNumbers;
+
+    public UserLottoDto(String buyMoney, String manualCount, List<String> manualNumbers) {
+        try {
+            this.buyMoney = parseNumber(buyMoney);
+            this.manualCount = parseNumber(manualCount);
+            this.manualNumbers = parseNumber(manualNumbers);
+        } catch (NumberFormatException e) {
+            throw new LottoNumberException();
+        }
+    }
+
+    private List<List<Integer>> parseNumber(List<String> manualNumbers) {
+        List<List<Integer>> numbers = new ArrayList<>();
+        for (String manualNumber : manualNumbers) {
+            numbers.add(
+                    Arrays.stream(manualNumber.split(","))
+                            .map(this::parseNumber)
+                            .collect(Collectors.toList()));
+        }
+        return numbers;
+    }
+
+    private int parseNumber(String text) {
+        return Integer.parseInt(text);
+    }
+
+    public int getBuyMoney() {
+        return buyMoney;
+    }
+
+    public int getManualCount() {
+        return manualCount;
+    }
+
+    public List<List<Integer>> getManualNumbers() {
+        return manualNumbers;
+    }
+}
