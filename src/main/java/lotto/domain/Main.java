4bb6009f0576d4675bf066948a72fd3ffa929983
Soojin Roh <sooreal91@gmail.com>
2019-06-07 16:17:01 +0900

    refactor Main클래스 로직 수정
    LottoGenerator를 없애고 Main 안에서 Lotto 생성을 수행

--- src/main/java/lotto/domain/Main.java
+++ src/main/java/lotto/domain/Main.java
@@ -2,34 +2,45 @@ package lotto.domain;
 
 import lotto.domain.model.Number;
 import lotto.domain.model.*;
-import lotto.domain.utils.AutoLottoGenerator;
-import lotto.domain.utils.ManualLottoGenerator;
 import lotto.domain.utils.ShuffledNumberGenerator;
 import lotto.domain.view.InputView;
 import lotto.domain.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        PurchasedLottos purchasedLottos = new PurchasedLottos();
+        PurchasedLotto purchasedLotto = new PurchasedLotto();
+        List<Lotto> manualLotto = new ArrayList<>();
+        List<Lotto> autoLotto = new ArrayList<>();
+        Money money = new Money(InputView.inputMoney());
+        int manualLottoSize = InputView.inputManualLottoSize(money);
 
-        Money money = new Money(InputView.inputMoney());                           // 구입 금액
-        int manualLottoSize = InputView.inputManualLottoSize(money);               // 수동 로또 갯수
+        InputView.printInputManualLottoMessage(manualLottoSize);
+        // 수동, 자동 로또 생성
 
-        InputView.printInputManualLottoMessage();
-        purchasedLottos.addLottos(new ManualLottoGenerator(manualLottoSize).generate(InputView.inputLottoNumber()));                               // 수동 로또 생성
-        purchasedLottos.addLottos(new AutoLottoGenerator(money, manualLottoSize).generate(ShuffledNumberGenerator.getShuffledNumbers()));          // 자동 로또 생성
-        OutputView.printPurchasedLottoResult(purchasedLottos, manualLottoSize);  // 로또 구입 결과 출력
+        while (manualLotto.size() < manualLottoSize) {
+            System.out.println("로또 번호를 입력하세요." + manualLotto.size() + "/" + manualLottoSize);
+            manualLotto.add(new Lotto(InputView.inputLottoNumber()));
+        }
+
+        while (autoLotto.size() < (money.availablePurchaseCount() - manualLottoSize)) {
+            autoLotto.add(new Lotto(ShuffledNumberGenerator.getShuffledNumbers()));
+        }
+
+        purchasedLotto.addLotto(manualLotto);
+        purchasedLotto.addLotto(autoLotto);
+        OutputView.printPurchasedLottoResult(purchasedLotto, manualLottoSize);
 
         InputView.printInputWinningLottoMessage();
         Lotto winLotto = new Lotto(InputView.inputLottoNumber());
         Number bonusNumber = InputView.inputBonusNumber(winLotto);
-        WinningLotto winningLotto = new WinningLotto(winLotto, bonusNumber);  // 지난 주 당첨 로또 생성
-        List<Rank> ranks = winningLotto.match(purchasedLottos);  // 당첨 확인, 등수 판단
-        Map<Rank, Integer> prizeResult = winningLotto.calculatePrize(ranks);  // 상금 계산
-        OutputView.printLottoResult(money, prizeResult);  // 결과 출력
+        WinningLotto winningLotto = new WinningLotto(winLotto, bonusNumber);
+
+        List<Rank> ranks = winningLotto.match(purchasedLotto);
+        Map<Rank, Integer> prizeResult = winningLotto.calculatePrize(ranks);
+        OutputView.printLottoResult(money, prizeResult);
     }
 }
-
