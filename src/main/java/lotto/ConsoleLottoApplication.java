f2577dc67dce6d4ce47d27c149b31761b4c2d587
hyperpace <cnk9109@gmail.com>
2019-06-08 18:22:39 +0900

    refactor(ConsoleLottoApplication): remove run method

--- src/main/java/lotto/ConsoleLottoApplication.java
+++ src/main/java/lotto/ConsoleLottoApplication.java
@@ -6,26 +6,22 @@ import lotto.view.OutputView;
 
 public class ConsoleLottoApplication {
     public static void main(String[] args) {
-        run();
-    }
-
-    private static void run() {
         try {
             Money money = MoneyParser.parse(InputView.inputMoney());
             LottoCount lottoManualCount = LottoCountParser.parse(money, InputView.inputManualLottoCount());
             LottoTickets lottoTickets = LottoVendingMachine.purchase(money, createLottoGenerators(lottoManualCount));
             OutputView.printLottoTickets(lottoManualCount, lottoTickets);
-            WinningLottoState winningLottoState = new WinningLottoState(createWinningLotto(), lottoTickets);
+            WinningLottoState winningLottoState = createWinningLotto().match(lottoTickets);
             OutputView.printWinningLottoState(winningLottoState, money);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            run();
+            main(args);
         }
     }
 
-    private static LottoGenerators createLottoGenerators(LottoCount lottoManualCount) {
+    private static LottoNoGenerators createLottoGenerators(LottoCount lottoManualCount) {
         try {
-            return LottoGeneratorsParser.parse(InputView.inputManualLotto(lottoManualCount));
+            return LottoNoGeneratorsParser.parse(InputView.inputManualLotto(lottoManualCount));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return createLottoGenerators(lottoManualCount);
