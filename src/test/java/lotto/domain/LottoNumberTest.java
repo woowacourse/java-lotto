f33833693fe945af4e291d1311a64731c1255450
dasistHYOJIN <hgenie14@gmail.com>
2019-06-08 23:54:43 +0900

    test: add additional test for boundary value of LottoNumber

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -4,12 +4,20 @@ import lotto.exception.UnexpectedInputRangeException;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoNumberTest {
     @Test
-    void 범위밖의_로또_번호에_대한_예외() {
+    void 경계값_밖의_로또_번호에_대한_예외() {
         assertThrows(UnexpectedInputRangeException.class, () -> { LottoNumber.getInstance(0); });
+        assertThrows(UnexpectedInputRangeException.class, () -> { LottoNumber.getInstance(46); });
+    }
+
+    @Test
+    void 경계값_안의_로또_번호에_대한_예외() {
+        assertDoesNotThrow(() -> { LottoNumber.getInstance(1); });
+        assertDoesNotThrow(() -> { LottoNumber.getInstance(45); });
     }
 
     @Test
