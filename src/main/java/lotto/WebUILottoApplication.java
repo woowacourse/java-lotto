e04d30ee7af7b74ff158bbae48bb925adcb3c75c
SUNGBUMPARK <psbum1609@gmail.com>
2019-06-08 20:23:55 +0900

    feat: 웹 로또 입력 추가
    * form.html: 웹에서 로또 금액, 수동 로또 개수, 수동 로또 번호를 입력하는
    기능을 구현하였다.
    
    * WebUILottoApplication.java: form.html에서 입력받은 정보로 Lottos를
    생성하였다.

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -1,16 +1,39 @@
 package lotto;
 
+import lotto.domain.LottoCount;
+import lotto.domain.Lottos;
+import lotto.domain.LottosFactory;
+import lotto.domain.Money;
+import lotto.view.OutputConsole;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebUILottoApplication {
     public static void main(String[] args) {
-        get("/", (req, res) -> {
+        initExceptionHandler((e) -> System.out.println("Uh-oh"));
+        staticFiles.location("/static");
+
+        post("/winningLotto", (req, res) -> {
+            Money money = new Money(Integer.parseInt(req.queryParams("money")));
+            LottoCount lottoCount = new LottoCount(money, Integer.parseInt(req.queryParams("numberOfManualLotto")));
+            List<List<Integer>> manualLottoNumbers = new ArrayList<>();
+            List<String> inputManualLottoNumbers = Arrays.asList(req.queryParams("manualLottoNumbers").split("\r\n"));
+            for (int i = 0; i < lottoCount.getManualCount(); i++) {
+                List<String> stringNumbers = Arrays.asList(inputManualLottoNumbers.get(i).split(","));
+                List<Integer> integers = new ArrayList<>();
+                for (String stringNumber : stringNumbers) {
+                    integers.add(Integer.parseInt(stringNumber));
+                }
+                manualLottoNumbers.add(integers);
+            }
+            LottosFactory lottosFactory = new LottosFactory(manualLottoNumbers, lottoCount);
+            Lottos lottos = lottosFactory.getLottos();
+
+            OutputConsole.outputLotto(lottos, lottoCount);
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
         });
