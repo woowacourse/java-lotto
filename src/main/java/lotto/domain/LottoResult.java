dc4628a4a6d7b1d54a2b882e01c2f519fb4b2e99
andole87 <andole87@gmail.com>
2019-06-08 17:50:16 +0900

    refactor: LottoResult.toString 오버라이드
    통계결과도 함께 출력하도록 변경.

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -4,7 +4,8 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class LottoResult {
-    private Map<Rank, Integer> results;
+    private static final int LOTTO_MONEY = 1000;
+    private final Map<Rank, Integer> results;
 
     public LottoResult(Map<Rank, Integer> results) {
         this.results = results;
@@ -21,10 +22,24 @@ public class LottoResult {
         results.put(rank, results.get(rank) + 1);
     }
 
+    private double summury() {
+        double sumOfRank = 0.0;
+        int sumOfTickets = 0;
+        for (Map.Entry<Rank, Integer> entry : results.entrySet()) {
+            sumOfRank += entry.getKey().money() * entry.getValue();
+            sumOfTickets += entry.getValue();
+        }
+        return sumOfRank / sumOfTickets / LOTTO_MONEY;
+    }
+
     @Override
     public String toString() {
-        return "LottoResult{" +
-                "results=" + results +
-                '}';
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("당첨통계\n").append("---------\n");
+        for (Map.Entry<Rank, Integer> entry : results.entrySet()) {
+            stringBuilder.append(entry.getKey().toString()).append(" - ").append(entry.getValue()).append("개\n");
+        }
+        stringBuilder.append("총 수익률은 ").append(summury()).append(" 입니다.");
+        return stringBuilder.toString();
     }
 }
