dfba9533a6f177ce3be18c29f7b71c8a7eaf2a7e
dasistHYOJIN <hgenie14@gmail.com>
2019-06-10 00:16:49 +0900

    refactor: rename a variable of WinningLotto class

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -7,11 +7,11 @@ import lotto.exception.ExceptionMessage;
 public class WinningLotto {
     private static WinningLotto WINNING_LOTTO = null;
 
-    private final LottoTicket lottoNumbers;
+    private final LottoTicket winningNumbers;
     private final LottoNumber bonusBall;
 
     private WinningLotto(final String winningLotto, final int bonusBall) {
-        this.lottoNumbers = LottoTicketFactory.getInstance().create(winningLotto);
+        this.winningNumbers = LottoTicketFactory.getInstance().create(winningLotto);
         this.bonusBall = LottoNumber.getInstance(bonusBall);
 
         validateDistinctNumber(bonusBall);
@@ -26,7 +26,7 @@ public class WinningLotto {
     }
 
     public int getMatchingCount(LottoTicket lottoTicket) {
-        return lottoNumbers.getMatchingCount(lottoTicket);
+        return winningNumbers.getMatchingCount(lottoTicket);
     }
 
     public boolean matchesBonusBall(LottoTicket lottoTicket) {
@@ -35,7 +35,7 @@ public class WinningLotto {
 
 
     private void validateDistinctNumber(int lottoNumber) {
-        boolean isDistinct = this.lottoNumbers.hasSameNumber(LottoNumber.getInstance(lottoNumber));
+        boolean isDistinct = this.winningNumbers.hasSameNumber(LottoNumber.getInstance(lottoNumber));
 
         if (isDistinct) {
             throw new DuplicatedInputException(ExceptionMessage.DUPLICATED_NUMBER_EXCEPTION);
