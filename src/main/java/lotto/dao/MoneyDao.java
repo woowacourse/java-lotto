ad290522260069d6e84ce26cf964356db5a32f0f
yunheehyeon <ok94life@daum.net>
2019-06-10 17:25:43 +0900

    feat : money find by times

--- src/main/java/lotto/dao/MoneyDao.java
+++ src/main/java/lotto/dao/MoneyDao.java
@@ -1,9 +1,15 @@
 package lotto.dao;
 
+import com.google.gson.Gson;
 import lotto.DataBase;
+import lotto.domain.Lotto;
 import lotto.domain.Money;
+import lotto.domain.Number;
+import lotto.domain.WinningLotto;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MoneyDao {
     private final DataBase dataBase;
@@ -22,5 +28,16 @@ public class MoneyDao {
         pstmt.setInt(4, money.getMoney());
 
         return pstmt.executeUpdate();
+    }
+
+    public Money findByTimes(int times) throws SQLException {
+        String query = "SELECT * FROM money WHERE times = ?";
+        PreparedStatement pstmt = dataBase.getConnection().prepareStatement(query);
+        pstmt.setInt(1, times);
+        ResultSet rs = pstmt.executeQuery();
+
+        if (!rs.next()) return null;
+
+        return new Money(rs.getInt("money"));
     }
 }
--- src/test/java/lotto/dao/MoneyDaoTest.java
+++ src/test/java/lotto/dao/MoneyDaoTest.java
@@ -5,7 +5,7 @@ import lotto.domain.Money;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MoneyDaoTest {
     private MoneyDao moneyDao;
@@ -17,6 +17,11 @@ class MoneyDaoTest {
 
     @Test
     void addMoney() throws Exception {
-        moneyDao.addMoney(new Money(10000), 1);
+        moneyDao.addMoney(new Money(10000), 0);
+    }
+
+    @Test
+    void findByTimes() throws Exception {
+        assertThat(moneyDao.findByTimes(1)).isEqualTo(new Money(10000));
     }
 }
