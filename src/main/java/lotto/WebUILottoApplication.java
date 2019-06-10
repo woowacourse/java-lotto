f45e455594623bcfa03f0c77ca1cba3bf3fa116f
backlo <hjun4512@naver.com>
2019-06-10 16:12:16 +0900

    html header, footer 구현

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -6,14 +6,36 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebUILottoApplication {
     public static void main(String[] args) {
+        port(8080);
+
+        staticFileLocation("templates");
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
+            return render(model, "view/lotto_home.html");
         });
+
+        get("/lotto_rule", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "view/lotto_rule.html");
+        });
+
+        get("/lotto_contact", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "view/lotto_contact.html");
+        });
+
+
+//        post("/users", (request, response) -> {
+//
+//            Map<String, Object> model = new HashMap<>();
+////            model.put("users", users);
+//
+//            return render(model,"result.html");
+//        });
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
