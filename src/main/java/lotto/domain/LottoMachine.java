3c8553c3a5f8ee511923fc1377657899aafd2990
choihz <choixhz@gmail.com>
2019-06-11 11:49:12 +0900

    refactor: ConsoleUIApplication, LottoMachine Class
    로또를 만드는 기능을 모두 LottoMachine 클래스로 위임

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,10 +1,20 @@
 package lotto.domain;
 
+import lotto.view.InputView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
-    public static LottoTickets generateTickets(Money money) {
+    public static LottoTickets getLottoTickets(Money money, int numberOfManualLotto) {
+        if (numberOfManualLotto > 0) {
+            List<Lotto> manualLotto = InputView.getManualLotto(numberOfManualLotto);
+            return generateTickets(money, manualLotto);
+        }
+        return generateTickets(money);
+    }
+
+    static LottoTickets generateTickets(Money money) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < money.getNumberOfLotto(); i++) {
             lottos.add(RandomLottoGenerator.generate());
@@ -12,7 +22,7 @@ public class LottoMachine {
         return new LottoTickets(lottos);
     }
 
-    public static LottoTickets generateTickets(Money money, List<Lotto> manualLotto) {
+    static LottoTickets generateTickets(Money money, List<Lotto> manualLotto) {
         List<Lotto> lottos = manualLotto;
         int numberOfRandomLotto = money.getNumberOfLotto() - manualLotto.size();
         for (int i = 0; i < numberOfRandomLotto; i++) {
