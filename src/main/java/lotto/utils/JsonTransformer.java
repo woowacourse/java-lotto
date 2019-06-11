09115d197e67072a0498426d28b8ec81df42d3e7
pkch93 <star_final@naver.com>
2019-06-11 16:45:28 +0900

    feat: class JsonTransformer
    output 값을 json 형태로 변환해 주는 JsonTransformer 구현
    이때 gson 라이브러리를 사용

---
+++ src/main/java/lotto/utils/JsonTransformer.java
@@ -0,0 +1,14 @@
+package lotto.utils;
+
+import com.google.gson.Gson;
+import spark.ResponseTransformer;
+
+public class JsonTransformer implements ResponseTransformer {
+
+    private Gson gson = new Gson();
+
+    @Override
+    public String render(final Object model) {
+        return gson.toJson(model);
+    }
+}
