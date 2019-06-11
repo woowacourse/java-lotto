12494cd61c3600d2186be5b9d5b0af2ae7b13993
ep1stas1s <masloz@konkuk.ac.kr>
2019-06-11 15:00:33 +0900

    refactor: Apply functional interface

--- src/main/java/lotto/domain/lottogenerator/LottoGeneratingStrategy.java
+++ src/main/java/lotto/domain/lottogenerator/LottoGeneratingStrategy.java
@@ -2,6 +2,7 @@ package lotto.domain.lottogenerator;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface LottoGeneratingStrategy {
     List<Integer> generate();
 }
--- src/test/java/lotto/domain/lottogenerator/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/lottogenerator/LottoGeneratorTest.java
@@ -1,10 +1,14 @@
 package lotto.domain.lottogenerator;
 
 import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,5 +19,29 @@ class LottoGeneratorTest {
 
         assertThat(LottoGenerator.create(new ManualLottoGeneratingStrategy(Arrays.asList(1, 2, 3, 4, 5, 6))))
                 .isEqualTo(new Lotto(lottoNumbers));
+    }
+
+    @Test
+    void 수동_로또_생성() {
+        Lotto lotto = LottoGenerator.create(() -> Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThat(lotto).isEqualTo(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+    }
+
+    @Test
+    void 자동_로또_생성() {
+        Lotto lotto = LottoGenerator.create(() -> {
+            List<Integer> allLottoNumbers = IntStream.rangeClosed(LottoNumber.MIN_BOUNDARY, LottoNumber.MAX_BOUNDARY)
+                    .boxed()
+                    .collect(Collectors.toList());
+
+            Collections.shuffle(allLottoNumbers);
+
+            return allLottoNumbers.stream()
+                    .limit(Lotto.LOTTO_NUMBER_SIZE)
+                    .sorted()
+                    .collect(Collectors.toList());
+        });
+
+        assertThat(lotto).isInstanceOf(Lotto.class);
     }
 }
