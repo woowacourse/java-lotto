74653d6ddaca38f7fa47d541a885f12007e8e035
dasistHYOJIN <hgenie14@gmail.com>
2019-06-11 18:01:50 +0900

    feat: add purchasing lotto web UI & add routing function

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.path;
 
 public class WebUILottoApplication {
     public static void main(String[] args) {
@@ -14,6 +15,31 @@ public class WebUILottoApplication {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
         });
+
+        path("/purchase", () -> {
+            get("", (req, res) -> {
+                Map<String, Object> model = new HashMap<>();
+                return render(model, "purchasing_lotto.html");
+            });
+
+            get("/ticket", (req, res) -> {
+                Map<String, Object> model = new HashMap<>();
+                return render(model, "lotto_tickets.html");
+            });
+        });
+
+        path("/statistics", () -> {
+            get("/win", (req, res) -> {
+                Map<String, Object> model = new HashMap<>();
+                return render(model, "purchasing_lotto.html");
+            });
+
+            get("/ticket", (req, res) -> {
+                Map<String, Object> model = new HashMap<>();
+                return render(model, "lotto_tickets.html");
+            });
+        });
+
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
