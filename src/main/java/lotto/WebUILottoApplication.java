a372a8f40a060d65ea4e27a1661955e3700d5fdd
SUNGBUMPARK <psbum1609@gmail.com>
2019-06-12 15:07:41 +0900

    refactor: 메서드 추출

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -32,14 +32,12 @@ public class WebUILottoApplication {
                 rounds.add(i.toString());
             }
 
-            Map<String, Object> model = new HashMap<>();
-            model.put("rounds", rounds);
-            return render(model, "show.html");
+            return render(getRoundsModel(rounds), "show.html");
         });
 
         get("/showLottoInfo", (req, res) -> {
-            // String lottoRound = req.queryParams("lottoRound");
-            return getModel(connection, req.queryParams("lottoRound"));
+            return render(getLottoInfoModel(connection, req.queryParams("lottoRound")),
+                    "showLottoInfo.html");
         });
 
         post("/winningLotto", (req, res) -> {
@@ -53,29 +51,20 @@ public class WebUILottoApplication {
             DBAccessor.loadDBRoundTable(connection, round);
             DBAccessor.loadDBLottoTable(connection, round, lottos);
 
-            Map<String, Object> model = new HashMap<>();
-            model.put("lottos", currentLottos);
-            model.put("AutoCount", lottoCount.getAutoCount());
-            model.put("ManualCount", lottoCount.getManualCount());
-            return render(model, "winningLotto.html");
+            return render(getLottoModel(lottoCount, currentLottos), "winningLotto.html");
         });
 
         post("/lottoResult", (req, res) -> {
             WinningLotto winningLotto = new WinningLotto(new Lotto(ConvertLottoNumber.run(req.queryParams("winningNumbers"))),
                     LottoNumber.getInstance(Integer.parseInt(req.queryParams("bonusNumber"))));
-            DBAccessor.loadDBWinningLottoTable(connection, round, winningLotto);
             LottoResult lottoResult = new LottoResult(winningLotto, lottos);
+
+            DBAccessor.loadDBWinningLottoTable(connection, round, winningLotto);
             DBAccessor.loadDBLottoResultTable(connection, round, lottoResult);
+
             DBManager.endTransection(connection);
 
-            Map<String, Object> model = new HashMap<>();
-            model.put("first", lottoResult.getCountOfRank(FIRST));
-            model.put("second", lottoResult.getCountOfRank(SECOND));
-            model.put("third", lottoResult.getCountOfRank(THIRD));
-            model.put("fourth", lottoResult.getCountOfRank(FOURTH));
-            model.put("fifth", lottoResult.getCountOfRank(FIFTH));
-            model.put("totalEarningRate", lottoResult.getEarningsRate());
-            return render(model, "lottoResult.html");
+            return render(getLottoResultModel(lottoResult), "lottoResult.html");
         });
 
         exception(IllegalArgumentException.class, (exception, request, response) -> {
@@ -86,13 +75,38 @@ public class WebUILottoApplication {
         });
     }
 
-    private static String getModel(Connection connection, String lottoRound) throws SQLException {
+    private static Map<String, Object> getRoundsModel(List<String> rounds) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("rounds", rounds);
+        return model;
+    }
+
+    private static Map<String, Object> getLottoInfoModel(Connection connection, String lottoRound) throws SQLException {
         Map<String, Object> model = new HashMap<>();
         model.put("lottoRound", lottoRound);
         model.put("lottos", DBAccessor.getDBLottos(connection, lottoRound));
         model.put("winningLotto", DBAccessor.getDBWinningLotto(connection, lottoRound));
         model.put("lottoResult", DBAccessor.getDBLottoResultDTO(connection, lottoRound));
-        return render(model, "showLottoInfo.html");
+        return model;
+    }
+
+    private static Map<String, Object> getLottoModel(LottoCount lottoCount, Lottos currentLottos) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("lottos", currentLottos);
+        model.put("AutoCount", lottoCount.getAutoCount());
+        model.put("ManualCount", lottoCount.getManualCount());
+        return model;
+    }
+
+    private static Map<String, Object> getLottoResultModel(LottoResult lottoResult) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("first", lottoResult.getCountOfRank(FIRST));
+        model.put("second", lottoResult.getCountOfRank(SECOND));
+        model.put("third", lottoResult.getCountOfRank(THIRD));
+        model.put("fourth", lottoResult.getCountOfRank(FOURTH));
+        model.put("fifth", lottoResult.getCountOfRank(FIFTH));
+        model.put("totalEarningRate", lottoResult.getEarningsRate());
+        return model;
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
