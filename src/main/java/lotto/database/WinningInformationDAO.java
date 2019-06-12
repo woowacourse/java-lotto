edbb68a916369988521655f2b129a958c9b8f45b
yk1028 <wongeunsong94@gmail.com>
2019-06-12 14:50:11 +0900

    feat: 특정 회차의 당첨 정보를 데이터 베이스에서 불러오는 기능 구현

--- src/main/java/lotto/database/WinningInformationDAO.java
+++ src/main/java/lotto/database/WinningInformationDAO.java
@@ -1,24 +1,29 @@
 package lotto.database;
 
+import lotto.domain.LottoNumber;
 import lotto.domain.WinningInformation;
+import lotto.domain.generator.ManualLottoNumbersGenerator;
+import lotto.exception.RoundNotFoundException;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningInformationDAO {
     private static final int WINNING_NUMBER_START_INDEX = 1;
     private static final int BONUS_BALL_INDEX = 7;
-    private static final int ROUND_INDEX = 1;
+    private static final int INSERT_ROUND_INDEX = 1;
+    private static final int SELECT_ROUND_INDEX = 1;
     private static final String INSERT_WINNING_INFO_QUERY =
             "INSERT INTO winningInfo(" +
-            "winning_number1," +
-            " winning_number2," +
-            " winning_number3," +
-            " winning_number4," +
-            " winning_number5," +
-            " winning_number6," +
-            " bonus)" +
-            " VALUES (?, ?, ?, ?, ?, ?, ?)";
+                    "winning_number1," +
+                    " winning_number2," +
+                    " winning_number3," +
+                    " winning_number4," +
+                    " winning_number5," +
+                    " winning_number6," +
+                    " bonus)" +
+                    " VALUES (?, ?, ?, ?, ?, ?, ?)";
 
     private static WinningInformationDAO winningInformationDAO;
     private static Connection connection;
@@ -36,6 +41,7 @@ public class WinningInformationDAO {
 
     /**
      * 당첨 정보를 데이터베이스에 저장하고 당첨 정보의 회차를 반환한다.
+     *
      * @param winningInformation
      * @return 당첨 정보의 회차를 반환
      * @throws SQLException
@@ -47,12 +53,48 @@ public class WinningInformationDAO {
             pstmt.setInt(i + WINNING_NUMBER_START_INDEX, numbers.get(i));
         }
         pstmt.setInt(BONUS_BALL_INDEX, winningInformation.getBonusNumber());
+
         pstmt.executeUpdate();
 
         ResultSet resultSet = pstmt.getGeneratedKeys();
         if (!resultSet.next()) {
             throw new SQLException();
         }
-        return resultSet.getInt(ROUND_INDEX);
+        return resultSet.getInt(INSERT_ROUND_INDEX);
+    }
+
+    public WinningInformation findWinningInformationByRound(int round) throws SQLException {
+        String query = "SELECT * FROM winningInfo WHERE round = ?";
+        PreparedStatement pstmt = connection.prepareStatement(query);
+        pstmt.setInt(SELECT_ROUND_INDEX, round);
+        ResultSet resultSet = pstmt.executeQuery();
+        if (!resultSet.next()) {
+            throw new RoundNotFoundException();
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 2; i < 8; i++) {
+            numbers.add(resultSet.getInt(i));
+        }
+        ManualLottoNumbersGenerator manualLottoNumbersGenerator = ManualLottoNumbersGenerator.getInstance(numbers);
+        LottoNumber lottoNumber = LottoNumber.valueOf(resultSet.getInt(8));
+        return new WinningInformation(manualLottoNumbersGenerator.generate(), lottoNumber);
+    }
+
+    public void clear() throws SQLException {
+        deleteAllWinningInfo();
+        initializeAutoIncrement();
+    }
+
+    private void deleteAllWinningInfo() throws SQLException {
+        String query = "DELETE FROM winningInfo";
+        PreparedStatement pstmt = connection.prepareStatement(query);
+        pstmt.executeUpdate();
+    }
+
+    private void initializeAutoIncrement() throws SQLException {
+        String query = "ALTER TABLE winningInfo AUTO_INCREMENT=1";
+        PreparedStatement pstmt = connection.prepareStatement(query);
+        pstmt.executeUpdate();
     }
 }
--- src/main/java/lotto/domain/WinningInformation.java
+++ src/main/java/lotto/domain/WinningInformation.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.exception.DuplicateBonusBallException;
 
 import java.util.List;
+import java.util.Objects;
 
 public class WinningInformation {
     private final LottoNumbers winningLottoNumbers;
@@ -26,5 +27,19 @@ public class WinningInformation {
 
     public int getBonusNumber() {
         return bonusBall.getNumber();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WinningInformation that = (WinningInformation) o;
+        return Objects.equals(winningLottoNumbers, that.winningLottoNumbers) &&
+                Objects.equals(bonusBall, that.bonusBall);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winningLottoNumbers, bonusBall);
     }
 }
