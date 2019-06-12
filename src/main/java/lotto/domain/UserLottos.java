b6bfce2ae99a15587aa700e05b0886980996209f
andole87 <andole87@gmail.com>
2019-06-12 23:44:28 +0900

    feat: UserLottoSeed 생성
    UserLottos 생성자 로직 변경.
    UserLottoSeed가 생성에 필요한 정보를 넘겨준다.

--- src/main/java/lotto/domain/UserLottos.java
+++ src/main/java/lotto/domain/UserLottos.java
@@ -1,55 +1,25 @@
 package lotto.domain;
 
-import lotto.domain.exceptions.ExceptionMessages;
-import lotto.domain.exceptions.LottoTicketException;
-import lotto.domain.ticket.LottoCreator;
 import lotto.domain.ticket.Ticket;
-import lotto.domain.ticket.TicketCreator;
 import lotto.domain.ticket.WinningTicket;
-import lotto.dto.UserLottoDto;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class UserLottos {
-    private final TicketCreator ticketCreator;
-    private List<Ticket> tickets;
+    private final List<Ticket> tickets;
+    private LottoResult results;
 
-    public UserLottos(UserLottoDto dto) {
-        ticketCreator = new LottoCreator();
-        generateManual(dto.getManualCount(), dto.getManualNumbers());
-        generateAuto(dto.getBuyMoney());
-    }
-
-    private void generateManual(int count, List<List<Integer>> manualNumbers) {
-        validate(count, manualNumbers);
-        List<Ticket> tickets = new ArrayList<>();
-        for (int i = 0; i < manualNumbers.size(); i++) {
-            tickets.add(ticketCreator.create(manualNumbers.get(i)));
-        }
+    public UserLottos(List<Ticket> tickets) {
         this.tickets = tickets;
     }
 
-    private void generateAuto(int buyMoney) {
-        int iterate = buyMoney / ticketCreator.unitPrice();
-        for (int i = tickets.size(); i < iterate; i++) {
-            tickets.add(ticketCreator.create());
-        }
-    }
-
-    private void validate(int count, List<List<Integer>> manualNumbers) {
-        if (count != manualNumbers.size()) {
-            throw new LottoTicketException(ExceptionMessages.TICKET.message());
-        }
-    }
-
     public LottoResult result(WinningTicket winningTicket) {
-        LottoResult lottoResult = new LottoResult();
+        this.results = new LottoResult();
         for (Ticket ticket : tickets) {
-            lottoResult.plus(Rank.rank(winningTicket.match(ticket), winningTicket.bonus(ticket)));
+            results.plus(Rank.rank(winningTicket.match(ticket), winningTicket.bonus(ticket)));
         }
-        return lottoResult;
+        return results;
     }
 
     public List<Ticket> tickets() {
