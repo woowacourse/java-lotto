5d5d3474c142d2b960b49c19e014b078ce49d3f4
Jinwoo Shin <bluesky0255@gmail.com>
2019-06-12 15:46:29 +0900

    Apply feedbacks
    * Changed to use constant
    * Added cache to use instances of fixed number.

--- src/main/java/com/woowacourse/lotto/domain/LottoNumber.java
+++ src/main/java/com/woowacourse/lotto/domain/LottoNumber.java
@@ -1,27 +1,34 @@
 package com.woowacourse.lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class LottoNumber implements Comparable<LottoNumber> {
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
+    public static final Map<Integer, LottoNumber> cache = new HashMap<>();
+
+    static {
+        for (int i = LOTTO_NUMBER_MIN; i <= LOTTO_NUMBER_MAX; i++) {
+            cache.put(i, new LottoNumber(i));
+        }
+    }
 
     private final int number;
 
     private LottoNumber(int number) {
         this.number = number;
-
-        assertValidRange(number);
-    }
-
-    private void assertValidRange(int n) {
-        if (n < 1 || n > 45) {
-            throw new IllegalArgumentException("로또 숫자는 1 ~ 45 사이의 숫자여야 합니다.");
-        }
     }
 
     public static LottoNumber of(int num) {
-        return new LottoNumber(num);
+        LottoNumber got = cache.get(num);
+        if (got == null) {
+            throw new IllegalArgumentException(String.format("로또 숫자는 %d ~ %d 사이의 숫자여야 합니다.",
+                LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX));
+        }
+
+        return got;
     }
 
     public int toInt() {
