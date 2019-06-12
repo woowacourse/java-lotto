857f82d212cc1177491fadcfe733cabd86631946
starkim06 <starkim06@naver.com>
2019-06-12 20:57:31 +0900

    refactor: 테스트코드 데이터베이스 롤백 추가

--- src/test/java/lotto/dao/LottoTicketsDaoTest.java
+++ src/test/java/lotto/dao/LottoTicketsDaoTest.java
@@ -4,6 +4,7 @@ import lotto.DatabaseConnection;
 import lotto.domain.LottoTicket;
 import lotto.domain.LottoTickets;
 import lotto.domain.factory.ManualTicketFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
@@ -14,11 +15,18 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTicketsDaoTest {
-    Connection connection = new DatabaseConnection().getConnection();
-    LottoTicketsDao lottoTicketsDao = new LottoTicketsDao(connection);
+    Connection connection;
+    LottoTicketsDao lottoTicketsDao;
+
+    @BeforeEach
+    void setUp() {
+        Connection connection = new DatabaseConnection().getConnection();
+        lottoTicketsDao = new LottoTicketsDao(connection);
+    }
 
     @Test
     void 로또_추가_테스트() throws SQLException {
+        connection.setAutoCommit(false);
         List<LottoTicket> lottoTicketList = new ArrayList<>();
         lottoTicketList.add(new ManualTicketFactory("1,2,3,4,5,6").create());
         lottoTicketList.add(new ManualTicketFactory("1,2,3,4,5,6").create());
@@ -27,10 +35,6 @@ class LottoTicketsDaoTest {
         lottoTicketsDao.addLottoTickets(round, lottoTickets);
 
         assertThat(lottoTicketsDao.findLottoByRound(round)).isEqualTo(lottoTickets);
-        lottoTicketsDao.deleteLottoByRound(round);
-
-        //TODO NULL이 아니라 []가 나오는 문제 해결
-//        assertThat(lottoTicketsDao.findLottoByRound(round)).isNull();
+        connection.rollback();
     }
-
 }
