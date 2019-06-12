5ee3d4be96c35cdb4322da8f691ed1ae45963a55
skygl <jupiter9501@gmal.com>
2019-06-12 16:18:35 +0900

    [ADD] 웹 UI에서 구입금액, 로또 생성을 해주고 출력해주는 기능 추가

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -1,22 +1,28 @@
 package lotto;
 
+import lotto.domain.controller.LottoPurchaseController;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebUILottoApplication {
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
-        });
+        port(8080);
+
+        staticFiles.location("/static");
+
+        get("/input", LottoPurchaseController.serveInputPage);
+
+        post("/budget", LottoPurchaseController.inputBudget);
+        post("/manualLotto", LottoPurchaseController.inputManualLotto);
+        post("/autoLotto", LottoPurchaseController.inputAutoLotto);
+        post("/showLotto", LottoPurchaseController.showLotto);
     }
 
-    private static String render(Map<String, Object> model, String templatePath) {
+    public static String render(Map<String, Object> model, String templatePath) {
         return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
