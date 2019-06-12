ba23d6f50738229750b79e9773305ec18ba26a13
imkimheejoo <jujudamdadi@naver.com>
2019-06-12 10:41:03 +0900

    refactor: LottoNumber Class의 예외처리 메서드 분리

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -14,14 +14,18 @@ public class LottoNumber {
     private final int number;
 
     private LottoNumber(final int number) {
-        if (number > MAX_LOTTO_NUMBER || number < MIN_LOTTO_NUMBER) {
-            throw new NumberValidException("로또번호에 해당되지 않는 숫자입니다.");
-        }
         this.number = number;
     }
 
     static LottoNumber valueOf(int number) {
+        checkValidLottoNumber(number);
         return LottoNumbers.lottoNumbers.get(number - 1);
+    }
+
+    private static void checkValidLottoNumber(int number) {
+        if (number > MAX_LOTTO_NUMBER || number < MIN_LOTTO_NUMBER) {
+            throw new NumberValidException("로또번호에 해당되지 않는 숫자입니다.");
+        }
     }
 
     private static class LottoNumbers {
