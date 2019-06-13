009a7c39b3175128403625d62c7d17dce9a89a94
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-06-13 21:29:04 +0900

    [hotfix] sql 수정

--- src/main/resources/lotto.sql
+++ src/main/resources/lotto.sql
@@ -1,74 +1,23 @@
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
---
--- Host: localhost    Database: lotto_db
--- ------------------------------------------------------
--- Server version	8.0.16
+CREATE DATABASE  IF NOT EXISTS `lotto_db`;
+USE `lotto_db`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
  SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `lottos`
---
-
-DROP TABLE IF EXISTS `lottos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `lottos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `numbers` varchar(20) NOT NULL,
   `round` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3384 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=3423 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `lottos`
---
 
-LOCK TABLES `lottos` WRITE;
-/*!40000 ALTER TABLE `lottos` DISABLE KEYS */;
-INSERT INTO `lottos` VALUES (3367,'1,2,16,6,22,25',1),(3368,'27,14,39,29,4,33',1),(3369,'32,29,41,14,18,27',1),(3370,'39,13,20,35,18,36',1),(3371,'9,40,38,23,6,22',1),(3372,'43,35,6,42,39,37',1),(3373,'1,2,3,16,18,12',2),(3374,'6,17,14,11,5,29',2),(3375,'19,43,14,41,6,10',2),(3376,'23,27,11,42,43,3',2),(3377,'14,7,2,35,8,29',2),(3378,'5,36,12,31,17,14',2),(3379,'1,2,3,4,5,8',3),(3380,'4,9,15,6,37,41',3),(3381,'5,9,19,2,42,38',3),(3382,'23,16,15,17,18,27',3),(3383,'3,18,11,8,12,21',3);
-/*!40000 ALTER TABLE `lottos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `round`
---
-
-DROP TABLE IF EXISTS `round`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `round` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `round`
---
-
-LOCK TABLES `round` WRITE;
-/*!40000 ALTER TABLE `round` DISABLE KEYS */;
-INSERT INTO `round` VALUES (1),(2),(3);
-/*!40000 ALTER TABLE `round` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `stat`
---
-
-DROP TABLE IF EXISTS `stat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `stat` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -81,25 +30,8 @@ CREATE TABLE `stat` (
   `profit` double NOT NULL,
   `turn` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=468 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=474 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `stat`
---
-
-LOCK TABLES `stat` WRITE;
-/*!40000 ALTER TABLE `stat` DISABLE KEYS */;
-INSERT INTO `stat` VALUES (465,0,0,0,0,0,6,0,1),(466,0,0,0,0,1,5,83.33333333333334,2),(467,0,0,0,1,0,4,1000,3);
-/*!40000 ALTER TABLE `stat` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `winning_lotto`
---
-
-DROP TABLE IF EXISTS `winning_lotto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `winning_lotto` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -107,26 +39,5 @@ CREATE TABLE `winning_lotto` (
   `bonus_number` int(11) NOT NULL,
   `turn` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=492 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=498 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `winning_lotto`
---
-
-LOCK TABLES `winning_lotto` WRITE;
-/*!40000 ALTER TABLE `winning_lotto` DISABLE KEYS */;
-INSERT INTO `winning_lotto` VALUES (489,'2,3,4,5,8,9',17,1),(490,'2,3,4,6,1,8',18,2),(491,'2,3,4,6,1,13',17,3);
-/*!40000 ALTER TABLE `winning_lotto` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-06-13 16:10:10
