2532094c895fd1c648c3b64af3526a29d3c1173a
yk1028 <wongeunsong94@gmail.com>
2019-06-13 12:21:20 +0900

    refactor: Exception message 상수로 관리하도록 수정

--- src/main/java/lotto/exception/InvalidPurchaseInformationException.java
+++ src/main/java/lotto/exception/InvalidPurchaseInformationException.java
@@ -1,6 +1,9 @@
 package lotto.exception;
 
 public class InvalidPurchaseInformationException extends RuntimeException {
+    public static final String LOTTO_COUNT_BIGGER_THAN_ZERO_MSG = "수동 로또의 개수는 0이상이여야 합니다.";
+    public static final String NOT_ENOUGH_MONEY_MSG = "구입 금액이 부족합니다.";
+
     public InvalidPurchaseInformationException(String message) {
         super(message);
     }
