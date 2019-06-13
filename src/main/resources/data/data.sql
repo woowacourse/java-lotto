03927baadb77a871c93d3c7d5b8771814671898b
pkch93 <star_final@naver.com>
2019-06-13 17:49:30 +0900

    add: schema.sql, data.sql

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO lotto_game VALUES (1, 2000, 2);
+
+INSERT INTO bought_lotto VALUES (null, 1, 2, 3, 4, 5, 6, 1);
+INSERT INTO bought_lotto VALUES (null, 3, 4, 5, 6, 7, 8, 1);
+
+INSERT INTO winning_lotto VALUES (1, 1, 2, 3, 4, 5, 6, 7);
+
+INSERT INTO result VALUES (1, 2003000000, 1, 0, 1, 0, 0, 0);
