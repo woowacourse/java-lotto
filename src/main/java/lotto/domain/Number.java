3e65f7159c51400cc3f747e8e417ea7c6a8b69f8
이영현 <leey9494@icloud.com>
2019-06-13 13:44:58 +0900

    MOD : 넘버 재사용

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Number implements Comparable<Number> {
     private static final String ERROR_MESSAGE = "숫자의 범위는 1-45입니다.";
@@ -9,9 +13,25 @@ public class Number implements Comparable<Number> {
 
     private final int number;
 
-    public Number(int number) {
+    private static Map<Integer, Number> cacheMap;
+
+    static {
+        cacheMap = IntStream.rangeClosed(MIN, MAX)
+                .boxed()
+                .collect(Collectors.toMap(Function.identity(), Number::new));
+/*
+.groupBy() <- 같은 key 값을 가지고 있는 Collection으로 묶는 함수 (e.g. Map<Key, Collection<Value>)
+.toMap() <- 단순한 Map
+ */
+    }
+
+    private Number(int number) {
         this.number = number;
         validOverBound();
+    }
+
+    public static Number of(int number) {
+        return cacheMap.get(number);
     }
 
     private void validOverBound() {
