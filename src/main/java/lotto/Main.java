0313ec7b90cd4cb5f598281ae31976c090fbdb0a
이영현 <leey9494@icloud.com>
2019-06-13 13:46:25 +0900

    MOD : 클래스 추가

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,52 +1,22 @@
 package lotto;
 
 import lotto.domain.*;
-import lotto.domain.Number;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-    private static final String DELIMITER = ",";
-
     public static void main(String[] args) {
         int round = new Money(InputView.inputMoney()).getRound();
         int manualRound = InputView.inputManualRound(round);
         String[] number = InputView.inputManualNumbers(manualRound);
-        Lottoes lottoes = new Lottoes(createManualLottos(number), round - manualRound, LottoNumberGenerator.create());
-        OutputView.printMyLotto(lottoes, manualRound);
+        UserLotto userLotto = new UserLotto(Parser.parseLotto(number), round - manualRound, new LottoNumberGenerator());
+        OutputView.printMyLotto(userLotto, manualRound);
 
         WinningLotto winningLotto = new WinningLotto
-                (parseWinningLotto(InputView.inputWinnerNumber())
+                (Parser.parseWinningLotto(InputView.inputWinnerNumber())
                         , InputView.inputBonusBall());
 
-        Winners winners = Winners.create(lottoes, winningLotto);
+        Winners winners = new Winners(winningLotto.makeRankResultList(userLotto));
         OutputView.printResultStatus(winners.getRankResult(), winners.calculateResultRate(round));
     }
-
-    private static Lotto parseWinningLotto(String numbers) {
-        List<Number> lottos = new ArrayList<>();
-        String[] winnerNumbers = numbers.split(DELIMITER);
-
-        for (String s : winnerNumbers) {
-            lottos.add(new Number(Integer.parseInt(s)));
-        }
-
-        return new Lotto(lottos);
-    }
-
-    private static List<Lotto> createManualLottos(String[] handleNumbers) {
-        List<Lotto> lottos = new ArrayList<>();
-
-        for (String handleNumber : handleNumbers) {
-            String[] oneNumbers = handleNumber.split(DELIMITER);
-            lottos.add(new Lotto(oneNumbers));
-        }
-
-        return lottos;
-    }
-
-
 }
---
+++ src/main/java/lotto/domain/Parser.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+    private static final String DELIMITER = ",";
+
+    public static Lotto parseWinningLotto(String numbers) {
+        List<Number> userLotto = new ArrayList<>();
+        String[] winnerNumbers = numbers.split(DELIMITER);
+
+        for (String number : winnerNumbers) {
+            userLotto.add(Number.of(Integer.parseInt(number)));
+        }
+
+        return new Lotto(userLotto);
+    }
+
+    public static List<Lotto> parseLotto(String[] numbers) {
+        List<Lotto> userLotto = new ArrayList<>();
+
+        for (String number : numbers) {
+            String[] oneNumbers = number.split(DELIMITER);
+            userLotto.add(new Lotto(oneNumbers));
+        }
+
+        return userLotto;
+    }
+}
