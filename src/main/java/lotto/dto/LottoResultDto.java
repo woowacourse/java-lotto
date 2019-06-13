1f384d7fec02e54ae77b138b524155747027182f
andole87 <andole87@gmail.com>
2019-06-13 20:17:54 +0900

    refactor: UserLottos.result() 변경
    LottoResultDto가 UserLottos를 참조하도록 변경.

--- src/main/java/lotto/dto/LottoResultDto.java
+++ src/main/java/lotto/dto/LottoResultDto.java
@@ -1,37 +1,22 @@
 package lotto.dto;
 
-import lotto.domain.Rank;
+import lotto.domain.LottoResult;
+import lotto.domain.core.Rank;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Map;
 
 public class LottoResultDto {
-    private static final int LOTTO_UNIT_PRICE = 1000;
-    private static final int DECIMAL_POINT = 3;
     private final Map<Rank, Integer> results;
+    private final BigDecimal summary;
 
-    public LottoResultDto(Map<Rank, Integer> results) {
+    public LottoResultDto(Map<Rank, Integer> results, BigDecimal summary) {
         this.results = results;
+        this.summary = summary;
     }
 
-    private BigDecimal summary() {
-        int sumOfRank = 0;
-        int sumOfTickets = 0;
-        for (Map.Entry<Rank, Integer> entry : results.entrySet()) {
-            sumOfRank += entry.getKey().money() * entry.getValue();
-            sumOfTickets += entry.getValue();
-        }
-        return toBigDecimal(sumOfRank, sumOfTickets);
-    }
-
-    private BigDecimal toBigDecimal(int numerator, int denominator) {
-        if (denominator == 0) {
-            return new BigDecimal(numerator);
-        }
-        return new BigDecimal(numerator)
-                .divide(new BigDecimal(denominator), DECIMAL_POINT, RoundingMode.CEILING)
-                .divide(new BigDecimal(LOTTO_UNIT_PRICE), DECIMAL_POINT, RoundingMode.CEILING);
+    public LottoResultDto(LottoResult lottoResult) {
+        this(lottoResult.results(), lottoResult.calculate().summary());
     }
 
     public Map<Rank, Integer> getResults() {
@@ -39,6 +24,6 @@ public class LottoResultDto {
     }
 
     public BigDecimal getSummary() {
-        return summary();
+        return summary;
     }
 }
