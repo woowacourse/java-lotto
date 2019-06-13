cd619727f838d70ad18aa5b08d0b904e0c131948
andole87 <andole87@gmail.com>
2019-06-13 20:32:41 +0900

    refactor: 패키지 정리

--- src/main/java/lotto/domain/ticket/TicketNumber.java
+++
@@ -1,6 +0,0 @@
-package lotto.domain.ticket;
-
-public interface TicketNumber extends Comparable<TicketNumber> {
-
-    int get();
-}
--- src/main/java/lotto/domain/ticket/WinningTicket.java
+++
@@ -1,8 +0,0 @@
-package lotto.domain.ticket;
-
-public interface WinningTicket {
-
-    public int match(Ticket ticket);
-
-    public boolean bonus(Ticket ticket);
-}
