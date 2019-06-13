ed0c042c3272a55aabda0d2c8c9b8d663771dffc
pkch93 <star_final@naver.com>
2019-06-13 15:25:17 +0900

    feat: class LottoGameDao
    당첨번호, 당첨결과 추가 로직 구현

--- src/main/java/lotto/dao/LottoGameDao.java
+++ src/main/java/lotto/dao/LottoGameDao.java
@@ -12,12 +12,10 @@ import java.util.*;
 
 import static lotto.dao.DataConnection.closeConnection;
 import static lotto.dao.DataConnection.getConnection;
-import static lotto.dao.LottoDao.BOUGHT_LOTTO_NUMBER_FROM_INDEX;
-import static lotto.dao.LottoDao.BOUGHT_LOTTO_NUMBER_TO_INDEX;
 
 public class LottoGameDao {
-
-    private static final int WINNING_BONUS_INDEX = 8;
+    private static final int WINNING_START_INDEX = 2;
+    private static final int WINNING_LAST_INDEX = 8;
     private static final List<String> PRIZE_NAMES = Arrays.asList("first", "second", "third", "fourth", "fifth", "none");
 
     public void removeRound(final int testRound) throws SQLException {
@@ -86,12 +84,12 @@ public class LottoGameDao {
         rs.next();
         List<Integer> winningLottoNumbers = new ArrayList<>();
 
-        for (int i = BOUGHT_LOTTO_NUMBER_FROM_INDEX + 2; i < BOUGHT_LOTTO_NUMBER_TO_INDEX + 2; i++) {
+        for (int i = WINNING_START_INDEX; i < WINNING_LAST_INDEX; i++) {
             winningLottoNumbers.add(rs.getInt(i));
         }
 
         winningNumberDto.setNumbers(winningLottoNumbers);
-        winningNumberDto.setBonusBall(rs.getInt(WINNING_BONUS_INDEX));
+        winningNumberDto.setBonusBall(rs.getInt(WINNING_LAST_INDEX));
         return winningNumberDto;
     }
 
@@ -120,5 +118,43 @@ public class LottoGameDao {
         resultDto.setPrize(prize);
         resultDto.setWinningMoney(rs.getInt("winning_money"));
         return resultDto;
+    }
+
+    public void addWinningNumber(int round, WinningNumberDto winningNumberDto) throws SQLException {
+        String sql = "INSERT INTO winning_lotto VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        Connection con = getConnection();
+        PreparedStatement pstmt = con.prepareStatement(sql);
+
+        parseValuesForQuery(round, winningNumberDto, pstmt);
+
+        pstmt.executeUpdate();
+        closeConnection(con);
+    }
+
+    private void parseValuesForQuery(final int round, final WinningNumberDto winningNumberDto,
+                                     final PreparedStatement pstmt) throws SQLException {
+        pstmt.setInt(1, round);
+        List<Integer> winningNumbers = winningNumberDto.getNumbers();
+        for (int i = WINNING_START_INDEX; i < WINNING_LAST_INDEX; i++) {
+            pstmt.setInt(i, winningNumbers.get(i - 2));
+        }
+        pstmt.setInt(WINNING_LAST_INDEX, winningNumberDto.getBonusBall());
+    }
+
+    public void addResult(int round, ResultDto resultDto) throws SQLException {
+        String sql = "INSERT INTO result VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        Connection con = getConnection();
+        PreparedStatement pstmt = con.prepareStatement(sql);
+
+        pstmt.setInt(1, round);
+        pstmt.setInt(2, resultDto.getWinningMoney());
+        Map<String, Integer> prize = resultDto.getPrize();
+        for (int i = 0; i < 6; i++) {
+            String prizeName = PRIZE_NAMES.get(i);
+            pstmt.setInt(i + 2, prize.get(prizeName));
+        }
+
+        pstmt.executeUpdate();
+        closeConnection(con);
     }
 }
