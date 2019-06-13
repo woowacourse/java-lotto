5370d0994336d04b35e89eb402df307daf936f9c
yejilee <yjll1019@naver.com>
2019-06-13 20:04:45 +0900

    Refactor : modified DBConnectorTest class

--- src/test/java/com/woowacourse/lotto/dao/DBConnectorTest.java
+++ src/test/java/com/woowacourse/lotto/dao/DBConnectorTest.java
@@ -1,28 +1,14 @@
 package com.woowacourse.lotto.dao;
 
 import java.sql.Connection;
-import java.util.*;
-
-import com.woowacourse.lotto.domain.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DBConnectorTest {
-	private DBConnector DBConnector;
-	private WinningLotto winningLotto;
-
-	@BeforeEach
-	void init() {
-		DBConnector = new DBConnector();
-		List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "6");
-		winningLotto = new WinningLotto(numbers, 7);
-	}
-
 	@Test
 	void connention() {
-		Connection connection = DBConnector.getConnection();
+		Connection connection = new DBConnector().getConnection();
 		assertNotNull(connection);
 	}
 }
