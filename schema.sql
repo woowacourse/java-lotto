16ab0c27bd72b37289ba2e982303f302cf2a9e2e
jinwook <jin942002@naver.com>
2019-06-14 13:11:55 +0900

    add user information and database name info at table schema file

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
+create user 'Ole'@'localhost' identified by '123';
+grant all privileges on *.* to 'Ole'@'localhost';
+CREATE DATABASE lottoGame DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 CREATE TABLE IF NOT EXISTS winning_lotto (
 	id INT AUTO_INCREMENT primary key NOT NULL,
 	lotto_numbers VARCHAR(17) NOT NULL,
