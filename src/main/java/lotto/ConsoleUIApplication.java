8cc5cd498dc7d446b214cb53b7f19dfa86fdadb9
choihz <choixhz@gmail.com>
2019-06-14 16:31:13 +0900

    refactor: LottoMachine, ConsoleUIApplication Class
    View 관련 로직을 도메인에서 컨트롤러로 이동

--- src/main/java/lotto/ConsoleUIApplication.java
+++ src/main/java/lotto/ConsoleUIApplication.java
@@ -4,12 +4,14 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class ConsoleUIApplication {
     public static void main(String[] args) {
         Money money = InputView.getMoney();
         int numberOfManualLotto = InputView.getNumberOfManualLotto(money);
 
-        LottoTickets lottoTickets = LottoMachine.getLottoTickets(money, numberOfManualLotto);
+        LottoTickets lottoTickets = getLottoTickets(money, numberOfManualLotto);
 
         OutputView.printLottoNumbers(money, numberOfManualLotto);
         OutputView.printLottoTickets(lottoTickets);
@@ -18,5 +20,13 @@ public class ConsoleUIApplication {
         LottoResult lottoResult = lottoTickets.getLottoResult(winningLotto);
 
         OutputView.printLottoStatistics(lottoResult);
+    }
+
+    private static LottoTickets getLottoTickets(Money money, int numberOfManualLotto) {
+        if (numberOfManualLotto > 0) {
+            List<Lotto> manualLotto = InputView.getManualLotto(numberOfManualLotto);
+            return LottoMachine.generateTickets(money, manualLotto);
+        }
+        return LottoMachine.generateTickets(money);
     }
 }
--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,20 +1,10 @@
 package lotto.domain;
 
-import lotto.view.InputView;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
-    public static LottoTickets getLottoTickets(Money money, int numberOfManualLotto) {
-        if (numberOfManualLotto > 0) {
-            List<Lotto> manualLotto = InputView.getManualLotto(numberOfManualLotto);
-            return generateTickets(money, manualLotto);
-        }
-        return generateTickets(money);
-    }
-
-    static LottoTickets generateTickets(Money money) {
+    public static LottoTickets generateTickets(Money money) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < money.getNumberOfLotto(); i++) {
             lottos.add(RandomLottoGenerator.generate());
@@ -22,9 +12,8 @@ public class LottoMachine {
         return new LottoTickets(lottos);
     }
 
-    static LottoTickets generateTickets(Money money, List<Lotto> manualLotto) {
-        List<Lotto> lottos = manualLotto;
-        int numberOfRandomLotto = money.getNumberOfLotto() - manualLotto.size();
+    public static LottoTickets generateTickets(Money money, List<Lotto> lottos) {
+        int numberOfRandomLotto = money.getNumberOfLotto() - lottos.size();
         for (int i = 0; i < numberOfRandomLotto; i++) {
             lottos.add(RandomLottoGenerator.generate());
         }
