c5c981ef780e7c5c53ad997f276116db72495f34
SunHo Van <vsh123@naver.com>
2019-06-14 09:30:20 +0900

    refactor : LottoResultController 리팩토링

--- src/main/java/lotto/controller/LottoResultController.java
+++ src/main/java/lotto/controller/LottoResultController.java
@@ -19,18 +19,23 @@ public class LottoResultController {
         int round = req.session().attribute("round");
         WinningLotto winningLotto = WinningLotto.generate(ManualLottoGenerator.create(req.queryParams("winning-lotto")),
                 Integer.parseInt(req.queryParams("bonus-number")));
-        WinningLottoService.insertWinningLotto(round, winningLotto);
         LottoTickets lottoTickets = LottoTicketsService.findTicketsByRound(round);
-        int price = lottoTickets.numberOfLottos() * 1000;
+        int price = getPrice(res, lottoTickets);
         Money money = Money.generate(price);
         LottoResult lottoResult = LottoResultGenerator.create(lottoTickets, winningLotto);
+
+        updateResult(round, winningLotto, lottoResult);
+        req.session().attribute("round", LottoResultService.findPresentRound());
+
+        model.put("price", price);
+        setMatchResult(model, lottoResult, money);
+        return render(model, "lottoresult.html");
+    }
+
+    private static void updateResult(int round, WinningLotto winningLotto, LottoResult lottoResult) {
+        WinningLottoService.insertWinningLotto(round, winningLotto);
         LottoResultService.updateLottoResult(round, lottoResult);
         LottoResultService.insertNewLottoRound();
-        req.session().attribute("round", LottoResultService.findPresentRound());
-        model.put("price", price);
-        model.put("rateOfLotto", lottoResult.getRateOfLotto(money));
-        setMatchResult(model, lottoResult);
-        return render(model, "lottoresult.html");
     }
 
 
@@ -38,37 +43,67 @@ public class LottoResultController {
         Map<String, Object> model = new HashMap<>();
         int round = req.session().attribute("round");
         List<Integer> rounds = LottoResultService.findAllRound(round);
+
         if (rounds.size() == 0) {
-            return render(model, "noselect");
+            return render(model, "noselect.html");
         }
+
         model.put("rounds", rounds);
         return render(model, "selectround.html");
     }
 
     public static Object showResultOfRound(Request req, Response res) {
         Map<String, Object> model = new HashMap<>();
+        Map<String, Object> objects = new HashMap<>();
         int round = Integer.parseInt(req.queryParams("selected-round"));
-        LottoTickets lottoTickets = LottoTicketsService.findTicketsByRound(round);
-        WinningLotto winningLotto = WinningLottoService.findWinningLottoByRound(round);
-        LottoResult lottoResult = LottoResultService.findLottoResultByRound(round);
-        int price = lottoTickets.numberOfLottos() * 1000;
-        Money money = Money.generate(price);
-        model.put("round", round);
-        model.put("numberOfTicket", lottoTickets.numberOfLottos());
-        model.put("lottos", lottoTickets.getLottos());
-        model.put("winninglotto", winningLotto.getWinningLotto());
-        model.put("bonus", winningLotto.getBonusNumber());
-        model.put("price", price);
-        model.put("rateOfLotto", lottoResult.getRateOfLotto(money));
-        setMatchResult(model, lottoResult);
+
+        objects.put("round", round);
+        objects.put("lottoTickets", LottoTicketsService.findTicketsByRound(round));
+        objects.put("winningLotto", WinningLottoService.findWinningLottoByRound(round));
+        objects.put("lottoResult", LottoResultService.findLottoResultByRound(round));
+        setResultOfRound(model, objects, res);
+
         return render(model, "resultofround.html");
     }
 
-    private static void setMatchResult(Map<String, Object> model, LottoResult lottoResult) {
+    private static void setResultOfRound(Map<String, Object> model, Map<String, Object> objects, Response res) {
+        LottoTickets lottoTickets = (LottoTickets) objects.get("lottoTickets");
+        int price = getPrice(res, lottoTickets);
+        Money money = Money.generate(price);
+
+        model.put("round", objects.get("round"));
+        model.put("price", price);
+        setLottoTickets(model, lottoTickets);
+        setWinningLotto(model, (WinningLotto) objects.get("winningLotto"));
+        setMatchResult(model, (LottoResult) objects.get("lottoResult"), money);
+    }
+
+    private static int getPrice(Response res, LottoTickets lottoTickets) {
+        int price = lottoTickets.numberOfLottos() * 1000;
+
+        if (price == 0) {
+            res.redirect("nopurchaselotto.html");
+        }
+
+        return price;
+    }
+
+    private static void setWinningLotto(Map<String, Object> model, WinningLotto winningLotto) {
+        model.put("winninglotto", winningLotto.getWinningLotto());
+        model.put("bonus", winningLotto.getBonusNumber());
+    }
+
+    private static void setLottoTickets(Map<String, Object> model, LottoTickets lottoTickets) {
+        model.put("numberOfTicket", lottoTickets.numberOfLottos());
+        model.put("lottos", lottoTickets.getLottos());
+    }
+
+    private static void setMatchResult(Map<String, Object> model, LottoResult lottoResult, Money money) {
         model.put("first", LottoResultService.getResultOf(lottoResult, Rank.FIRST));
         model.put("second", LottoResultService.getResultOf(lottoResult, Rank.SECOND));
         model.put("third", LottoResultService.getResultOf(lottoResult, Rank.THIRD));
         model.put("fourth", LottoResultService.getResultOf(lottoResult, Rank.FOURTH));
         model.put("fifth", LottoResultService.getResultOf(lottoResult, Rank.FIFTH));
+        model.put("rateOfLotto", lottoResult.getRateOfLotto(money));
     }
 }
