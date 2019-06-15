8c233997986b2b36a097a8224cc3f394f52621f2
gch01410 <gch01410@naver.com>
2019-06-15 14:37:36 +0900

    fix: Resolve error of selecting to round from winning Lotto table

--- src/main/java/lotto/domain/lotto/WinningLottoDAO.java
+++ src/main/java/lotto/domain/lotto/WinningLottoDAO.java
@@ -3,6 +3,7 @@ package lotto.domain.lotto;
 import lotto.utils.DBUtils;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
@@ -11,15 +12,14 @@ import java.util.stream.Collectors;
 
 public class WinningLottoDAO {
     public int addWinningLotto(WinningLotto winningLotto) throws SQLException {
-        String[] string = winningLotto.getWinningLotto().toString().replace("[", "").replace("]", "").split(",");
-        List<Integer> winningLottoNumbers = Arrays.stream(string)
+        List<Integer> winningLottoNumbers = Arrays.stream(splitLottoNumbers(winningLotto))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
         String query = "INSERT INTO winning_lotto(winning_number_1, winning_number_2,winning_number_3," +
                 "winning_number_4, winning_number_5, winning_number_6, bonus_number) VALUES(?,?,?,?,?,?,?)";
-        PreparedStatement pstmt = DBUtils.getConnection().prepareStatement(query);
+        PreparedStatement pstmt = DBUtils.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
         pstmt.setInt(1, winningLottoNumbers.get(0));
         pstmt.setInt(2, winningLottoNumbers.get(1));
         pstmt.setInt(3, winningLottoNumbers.get(2));
@@ -29,6 +29,20 @@ public class WinningLottoDAO {
         pstmt.setInt(7, Integer.parseInt(winningLotto.getBonusNumber().toString()));
         pstmt.executeUpdate();
 
-        return Statement.RETURN_GENERATED_KEYS;
+        ResultSet resultSet = pstmt.getGeneratedKeys();
+
+        if (!resultSet.next()) {
+            return -1;
+        }
+
+        return resultSet.getInt(1);
+    }
+
+    private String[] splitLottoNumbers(WinningLotto winningLotto) {
+        return winningLotto.getWinningLotto()
+                .toString()
+                .replace("[", "")
+                .replace("]", "")
+                .split(",");
     }
 }
---
+++ src/test/java/lotto/domain/lotto/WinningLottoDAOTest.java
@@ -0,0 +1,19 @@
+package lotto.domain.lotto;
+
+import lotto.domain.lottogenerator.LottoGenerator;
+import lotto.domain.lottogenerator.ManualLottoGeneratingStrategy;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+
+public class WinningLottoDAOTest {
+    @Test
+    void DB에_당첨번호를_insert하는지_확인() throws SQLException {
+        Lotto lotto = LottoGenerator.create(new ManualLottoGeneratingStrategy(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        WinningLotto winningLotto = new WinningLotto(lotto, LottoNumber.getNumber(7));
+
+        WinningLottoDAO winningLottoDAO = new WinningLottoDAO();
+        winningLottoDAO.addWinningLotto(winningLotto);
+    }
+}
