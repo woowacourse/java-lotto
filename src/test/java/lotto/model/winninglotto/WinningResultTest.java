58151e5047a0ab5a6e2c47d324043533da27eec0
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-06-16 18:14:56 +0900

    [FIX] Remove BonusNumber

--- src/test/java/lotto/model/winninglotto/WinningResultTest.java
+++ src/test/java/lotto/model/winninglotto/WinningResultTest.java
@@ -1,7 +1,6 @@
 package lotto.model.winninglotto;
 
 import lotto.model.lotto.LottoNumber;
-import lotto.model.lotto.LottoNumberRepository;
 import lotto.model.lotto.LottoTicket;
 import lotto.model.lotto.LottoTickets;
 import org.junit.jupiter.api.Test;
@@ -10,27 +9,28 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
 
+import static lotto.model.lotto.LottoNumberRepository.fromNumber;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningResultTest {
-    List<LottoNumber> lottoNumbers = Arrays.asList(LottoNumberRepository.fromNumber(1)
-            , LottoNumberRepository.fromNumber(2)
-            , LottoNumberRepository.fromNumber(3)
-            , LottoNumberRepository.fromNumber(4)
-            , LottoNumberRepository.fromNumber(5)
-            , LottoNumberRepository.fromNumber(6));
+    List<LottoNumber> lottoNumbers = Arrays.asList(fromNumber(1)
+            , fromNumber(2)
+            , fromNumber(3)
+            , fromNumber(4)
+            , fromNumber(5)
+            , fromNumber(6));
 
     @Test
     void 수익률_계산_5등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), LottoNumberRepository.fromNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(7)
-                , LottoNumberRepository.fromNumber(8)
-                , LottoNumberRepository.fromNumber(9));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(7)
+                , fromNumber(8)
+                , fromNumber(9));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
         assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(500);
@@ -39,14 +39,14 @@ public class WinningResultTest {
     @Test
     void 수익률_계산_4등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), LottoNumberRepository.fromNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(4)
-                , LottoNumberRepository.fromNumber(8)
-                , LottoNumberRepository.fromNumber(9));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(4)
+                , fromNumber(8)
+                , fromNumber(9));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
         assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(5000);
@@ -55,14 +55,14 @@ public class WinningResultTest {
     @Test
     void 수익률_계산_3등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), LottoNumberRepository.fromNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(4)
-                , LottoNumberRepository.fromNumber(5)
-                , LottoNumberRepository.fromNumber(9));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(4)
+                , fromNumber(5)
+                , fromNumber(9));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
         assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(150000);
@@ -71,14 +71,14 @@ public class WinningResultTest {
     @Test
     void 수익률_계산_2등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), LottoNumberRepository.fromNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(4)
-                , LottoNumberRepository.fromNumber(5)
-                , LottoNumberRepository.fromNumber(45));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(4)
+                , fromNumber(5)
+                , fromNumber(45));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
         assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(Long.valueOf(3000000L));
@@ -87,14 +87,14 @@ public class WinningResultTest {
     @Test
     void 수익률_계산_1등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), LottoNumberRepository.fromNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(4)
-                , LottoNumberRepository.fromNumber(5)
-                , LottoNumberRepository.fromNumber(6));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(4)
+                , fromNumber(5)
+                , fromNumber(6));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
         assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(200000000L);
--- src/test/java/lotto/model/winninglotto/WinningStatisticsTest.java
+++ src/test/java/lotto/model/winninglotto/WinningStatisticsTest.java
@@ -1,8 +1,6 @@
 package lotto.model.winninglotto;
 
-import lotto.model.lotto.BonusNumber;
 import lotto.model.lotto.LottoNumber;
-import lotto.model.lotto.LottoNumberRepository;
 import lotto.model.lotto.LottoTicket;
 import lotto.model.lotto.LottoTickets;
 import org.junit.jupiter.api.Test;
@@ -11,93 +9,95 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
 
+
+import static lotto.model.lotto.LottoNumberRepository.fromNumber;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningStatisticsTest {
-    List<LottoNumber> lottoNumbers = Arrays.asList(LottoNumberRepository.fromNumber(1)
-            , LottoNumberRepository.fromNumber(2)
-            , LottoNumberRepository.fromNumber(3)
-            , LottoNumberRepository.fromNumber(4)
-            , LottoNumberRepository.fromNumber(5)
-            , LottoNumberRepository.fromNumber(6));
+    List<LottoNumber> lottoNumbers = Arrays.asList(fromNumber(1)
+            , fromNumber(2)
+            , fromNumber(3)
+            , fromNumber(4)
+            , fromNumber(5)
+            , fromNumber(6));
 
     @Test
     void 수익률_계산_5등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), new BonusNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(7)
-                , LottoNumberRepository.fromNumber(8)
-                , LottoNumberRepository.fromNumber(9));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(7)
+                , fromNumber(8)
+                , fromNumber(9));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
-        assertThat(WinningStatistics.of(lottoTickets, winningLotto).calculateROI()).isEqualTo(500);
+        assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(500);
     }
 
     @Test
     void 수익률_계산_4등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), new BonusNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(4)
-                , LottoNumberRepository.fromNumber(8)
-                , LottoNumberRepository.fromNumber(9));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(4)
+                , fromNumber(8)
+                , fromNumber(9));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
-        assertThat(WinningStatistics.of(lottoTickets, winningLotto).calculateROI()).isEqualTo(5000);
+        assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(5000);
     }
 
     @Test
     void 수익률_계산_3등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), new BonusNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(4)
-                , LottoNumberRepository.fromNumber(5)
-                , LottoNumberRepository.fromNumber(9));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(4)
+                , fromNumber(5)
+                , fromNumber(9));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
-        assertThat(WinningStatistics.of(lottoTickets, winningLotto).calculateROI()).isEqualTo(150000);
+        assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(150000);
     }
 
     @Test
     void 수익률_계산_2등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), new BonusNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(4)
-                , LottoNumberRepository.fromNumber(5)
-                , LottoNumberRepository.fromNumber(45));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(4)
+                , fromNumber(5)
+                , fromNumber(45));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
-        assertThat(WinningStatistics.of(lottoTickets, winningLotto).calculateROI()).isEqualTo(Long.valueOf(3000000L));
+        assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(Long.valueOf(3000000L));
     }
 
     @Test
     void 수익률_계산_1등() {
 
-        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), new BonusNumber(45));
+        WinningLotto winningLotto = WinningLotto.of(LottoTicket.from(new TreeSet<>(lottoNumbers)), fromNumber(45));
 
-        List<LottoNumber> lottoNumber = Arrays.asList(LottoNumberRepository.fromNumber(1)
-                , LottoNumberRepository.fromNumber(2)
-                , LottoNumberRepository.fromNumber(3)
-                , LottoNumberRepository.fromNumber(4)
-                , LottoNumberRepository.fromNumber(5)
-                , LottoNumberRepository.fromNumber(6));
+        List<LottoNumber> lottoNumber = Arrays.asList(fromNumber(1)
+                , fromNumber(2)
+                , fromNumber(3)
+                , fromNumber(4)
+                , fromNumber(5)
+                , fromNumber(6));
         LottoTickets lottoTickets = LottoTickets.from(Arrays.asList(LottoTicket.from(new TreeSet<>(lottoNumber))));
 
-        assertThat(WinningStatistics.of(lottoTickets, winningLotto).calculateROI()).isEqualTo(200000000L);
+        assertThat(WinningResult.of(lottoTickets, winningLotto).getROI()).isEqualTo(200000000L);
     }
 }
