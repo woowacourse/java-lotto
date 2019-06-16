e4f4bf95df48f99ec828949eacff9785e99f5ded
이영현 <leey9494@icloud.com>
2019-06-16 21:33:56 +0900

    ADD : Test 추가

--- src/test/java/lotto/dao/LottoDaoTest.java
+++ src/test/java/lotto/dao/LottoDaoTest.java
@@ -1,21 +1,22 @@
 package lotto.dao;
 
-import lotto.domain.Lotto;
-import lotto.domain.LottoNumberGenerator;
+import lotto.domain.*;
 import lotto.domain.Number;
-import lotto.domain.UserLotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LottoDaoTest {
     private List<Lotto> lotto;
+    private List<Number> lottoNumbers;
 
     @BeforeEach
     void setUp() {
-        List<Number> lottoNumbers = new ArrayList<>();
+        lottoNumbers = new ArrayList<>();
 
         for (int i = 1; i <= 6; i++) {
             lottoNumbers.add(Number.of(i));
@@ -30,6 +31,26 @@ class LottoDaoTest {
     void addLotto() {
         UserLotto userLotto = new UserLotto(lotto, 4, new LottoNumberGenerator());
         LottoDao.addLotto(userLotto, 1);
+    }
+
+    @Test
+    void addWinningLotto() {
+        Lotto lotto = new Lotto(lottoNumbers);
+        WinningLotto winningLotto = new WinningLotto(lotto, Number.of(9));
+        LottoDao.addWinningLotto(winningLotto, 9);
+    }
+
+    @Test
+    void addResult() {
+        List<String> results = new ArrayList<>();
+        results.add("FIRST");
+        results.add("SECOND");
+        LottoDao.addResult(results, 7);
+    }
+
+    @Test
+    void addResultInfo() {
+        LottoDao.addResultInfo(4, 2.0, "300000");
     }
 
     @Test
