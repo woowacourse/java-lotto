f46cf71beac6247916afe9cbf8ff45eb827d69ff
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-06-17 23:55:22 +0900

    [REFACTOR] Modify assemble method using setter to constructor

--- src/main/java/lotto/service/winningresultservice/WinningResultAssembler.java
+++ src/main/java/lotto/service/winningresultservice/WinningResultAssembler.java
@@ -5,24 +5,16 @@ import lotto.model.winninglotto.WinningResult;
 
 public class WinningResultAssembler {
     public static WinningResultDTO toDTO(WinningResult from, int lottoRoundId){
-        WinningResultDTO winningResultDTO = new WinningResultDTO();
-        long first = from.getRankCount("FIRST");
-        long second = from.getRankCount("SECOND");
-        long third = from.getRankCount("THIRD");
-        long fourth = from.getRankCount("FOURTH");
-        long fifth = from.getRankCount("FIFTH");
-        long miss = from.getRankCount("MISS");
-        long roi = from.getROI();
 
-        winningResultDTO.setLottoRoundId(lottoRoundId);
-        winningResultDTO.setFirst(first);
-        winningResultDTO.setSecond(second);
-        winningResultDTO.setThird(third);
-        winningResultDTO.setFourth(fourth);
-        winningResultDTO.setFifth(fifth);
-        winningResultDTO.setMiss(miss);
-        winningResultDTO.setRoi(roi);
-
-        return winningResultDTO;
+        return new WinningResultDTO(
+                lottoRoundId,
+                from.getRankCount("FIRST"),
+                from.getRankCount("SECOND"),
+                from.getRankCount("THIRD"),
+                from.getRankCount("FOURTH"),
+                from.getRankCount("FIFTH"),
+                from.getRankCount("MISS"),
+                from.getROI()
+        );
     }
 }
