c31105c8bcc330f5efafbd4238ef9da61d099106
school0bhy <house1217@ajou.ac.kr>
2019-06-17 15:10:46 +0900

    Add : 로또 구매 페이지 구현

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -1,22 +1,43 @@
 package lotto;
 
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class WebUILottoApplication {
     public static void main(String[] args) {
-        get("/", (req, res) -> {
+        port(8080);
+        Spark.staticFileLocation("/public");
+
+        get("/", WebUILottoApplication::purchaseLotto);
+    }
+
+    private static String purchaseLotto(Request request, Response response) {
+        try {
             Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
-        });
+            return render(model, "purchaseLotto.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return handleError(e.getMessage());
+        }
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
         return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
+
+    private static String handleError(String message) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("error", message);
+        return render(model, "error.html");
+    }
+
 }
