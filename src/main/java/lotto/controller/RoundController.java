0548fdbc5ba1ab7ab3909399d15c01e728163ed4
imkimheejoo <jujudamdadi@naver.com>
2019-06-18 15:43:11 +0900

    refactor: RoundController 일부기능 RoundService로 분리

--- src/main/java/lotto/controller/RoundController.java
+++ src/main/java/lotto/controller/RoundController.java
@@ -1,26 +1,25 @@
 package lotto.controller;
 
-import lotto.dao.RoundDao;
-import lotto.db.DatabaseConnection;
+import lotto.service.RoundService;
 import lotto.utils.ViewUtils;
 import spark.Route;
 
-import java.sql.Connection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class RoundController {
+
     public static Route makeRoundPage = (req, res) -> {
-        Connection conn = new DatabaseConnection().getConnection();
-        RoundDao roundDao = new RoundDao(conn);
+        RoundService roundService = new RoundService();
         Map<String, Object> model = new HashMap<>();
-        int presentRound = roundDao.findLatestRound() + 1;
-        List<Integer> rounds = roundDao.findAllRound();
+
+        int presentRound = roundService.getPresentRound();
         model.put("present", presentRound);
-        model.put("rounds", rounds);
-        model.put("message", req.queryParams("message"));
         req.session().attribute("round", presentRound);
+
+        model.put("rounds", roundService.getAllRound());
+        model.put("message", req.queryParams("message"));
+
         return ViewUtils.render(model, "home.html");
     };
 }
---
+++ src/main/java/lotto/service/RoundService.java
@@ -0,0 +1,26 @@
+package lotto.service;
+
+import lotto.dao.RoundDao;
+import lotto.db.DatabaseConnection;
+import lotto.domain.Price;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class RoundService {
+    Connection conn = new DatabaseConnection().getConnection();
+    RoundDao roundDao = new RoundDao(conn);
+
+    public int getPresentRound() throws SQLException {
+        return roundDao.findLatestRound();
+    }
+
+    public List<Integer> getAllRound() throws SQLException{
+        return roundDao.findAllRound();
+    }
+
+    public Price getPrice(String price) {
+        return new Price(Integer.parseInt(price));
+    }
+}
