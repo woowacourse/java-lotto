a8812e1e4fc7b4a72798f2f2591707cbe6e66323
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-06-24 01:06:26 +0900

    refactor: abstract controller into multiple controller classes

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -1,13 +1,11 @@
 package lotto;
 
-import lotto.service.*;
-import org.javatuples.Pair;
+import lotto.presentation.HomeController;
+import lotto.presentation.LottoController;
+import lotto.presentation.ResultController;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -16,53 +14,16 @@ public class WebUILottoApplication {
     public static void main(String[] args) {
         externalStaticFileLocation("src/main/resources/templates");
 
-        get("/home", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
-        });
+        HomeController homeController = new HomeController();
+        get(HomeController.HOME_URL, (req, res) -> homeController.get());
+        post(HomeController.HOME_URL, homeController::post);
 
-        post("/home", (req, res) -> {
-            int lottoBuyingMoney = LottoBuyingMoneyService.validateLottoBuyingMoney(req.queryParams("lottoBuyingMoney"));
-            Pair<Integer, Integer> numOfLottos = LottoCountService.calculateNumOfLottos(lottoBuyingMoney, req.queryParams("numOfCustomLottos"));
-            int numOfCustomLottos = numOfLottos.getValue0();
-            int numOfAutoLottos = numOfLottos.getValue1();
+        LottoController lottoController = new LottoController();
+        get(LottoController.LOTTO_URL, (req, res) -> lottoController.get(req));
+        post(LottoController.LOTTO_URL, lottoController::post);
 
-            req.session().attribute("lottoBuyingMoney", lottoBuyingMoney);
-            req.session().attribute("numOfCustomLottos", numOfCustomLottos);
-            req.session().attribute("numOfAutoLottos", numOfAutoLottos);
-
-            res.redirect("/lotto");
-            return null;
-        });
-
-        get("/lotto", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("numOfCustomLottos", req.session().attribute("numOfCustomLottos"));
-            return render(model, "lotto.html");
-        });
-
-        post("/lotto", (req, res) -> {
-            int lottoBuyingMoney = req.session().attribute("lottoBuyingMoney");
-            int numOfCustomLottos = req.session().attribute("numOfCustomLottos");
-
-            String winningLotto = req.queryParams("winningLotto");
-            List<String> customLottos = new ArrayList<>();
-            for (int i = 0; i < numOfCustomLottos; i++) {
-                customLottos.add(req.queryParams("lotto" + i));
-            }
-
-            LottoService.saveResult(lottoBuyingMoney, numOfCustomLottos, winningLotto, customLottos);
-            res.redirect("/result/" + RoundService.getThisLottoRoundId());
-            return null;
-        });
-
-        get("/result/:roundId", (req, res) -> {
-            int thisRoundId = Integer.parseInt(req.params("roundId"));
-            ResultDTO result = ResultService.getResultByRoundId(thisRoundId);
-            Map<String, Object> model = new HashMap<>();
-            model.put("result", result);
-            return render(model, "result.html");
-        });
+        ResultController resultController = new ResultController();
+        get(ResultController.RESULT_URL, (req, res) -> resultController.get(req));
 
         exception(Exception.class, (e, req, res) -> {
             e.printStackTrace();
