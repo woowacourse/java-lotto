dd04a1d1a255ff5454f01c34ac8eb2538ec1e08c
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-06-23 19:03:17 +0900

    [REFACTOR] Separate DB properties from source to external file

--- src/main/java/lotto/dao/DBCPDataSource.java
+++ src/main/java/lotto/dao/DBCPDataSource.java
@@ -2,29 +2,31 @@ package lotto.dao;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBCPDataSource {
     private static BasicDataSource basicDataSource = new BasicDataSource();
 
-    private static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
-    private static final String DBMS = "mysql";
-    private static final String SERVER = "localhost";
-    private static final String DATABASE = "lottodb";
-    private static final String CONNECTION_PROPERTIES = "?characterEncoding=UTF-8&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
-    private static final String URL = "jdbc:" + DBMS + "://" + SERVER + "/" + DATABASE + CONNECTION_PROPERTIES;
-    private static final String USER_NAME = "bmo";
-    private static final String PASSWORD = "0000";
+    private static final String PROPERTIES_FILE_NAME = "mysql.properties";
+    private static final String PROPERTY_DRIVER = "jdbc.driver";
+    private static final String PROPERTY_URL = "jdbc.url";
+    private static final String PROPERTY_USERNAME = "jdbc.username";
+    private static final String PROPERTY_PASSWORD = "jdbc.password";
     private static final int MIN_IDLE = 5;
     private static final int MAX_IDLE = 10;
     private static final int MAX_OPEN_PREPARED_STATEMENTS = 100;
 
     static {
-        basicDataSource.setDriverClassName(DRIVER_CLASS_NAME);
-        basicDataSource.setUrl(URL);
-        basicDataSource.setUsername(USER_NAME);
-        basicDataSource.setPassword(PASSWORD);
+        Properties properties = loadProperties();
+
+        basicDataSource.setDriverClassName(properties.getProperty(PROPERTY_DRIVER));
+        basicDataSource.setUrl(properties.getProperty(PROPERTY_URL));
+        basicDataSource.setUsername(properties.getProperty(PROPERTY_USERNAME));
+        basicDataSource.setPassword(properties.getProperty(PROPERTY_PASSWORD));
         basicDataSource.setMinIdle(MIN_IDLE);
         basicDataSource.setMaxIdle(MAX_IDLE);
         basicDataSource.setMaxOpenPreparedStatements(MAX_OPEN_PREPARED_STATEMENTS);
@@ -32,5 +34,18 @@ public class DBCPDataSource {
 
     public static Connection getConnection() throws SQLException {
         return basicDataSource.getConnection();
+    }
+
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+        ClassLoader classLoader = DBCPDataSource.class.getClassLoader();
+
+        try (InputStream fileInputStream = classLoader.getResourceAsStream(PROPERTIES_FILE_NAME)) {
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
     }
 }
