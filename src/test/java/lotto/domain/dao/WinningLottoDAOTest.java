cc4ed89a9d6437868771ea462d268ff720492e02
Soojin Roh <sooreal91@gmail.com>
2019-06-23 12:59:00 +0900

    test WinningLottoDAO test 코드 추가
    WinningLottoDAO의 addWinningLotto와 getWinningLotto의 테스트 코드 추가

--- src/test/java/lotto/domain/dao/WinningLottoDAOTest.java
+++ src/test/java/lotto/domain/dao/WinningLottoDAOTest.java
@@ -11,33 +11,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningLottoDAOTest {
     private WinningLottoDAO winningLottoDAO;
-
+    private WinningLottoDTO winningLottoDTO;
     @Before
     public void setUp() {
         winningLottoDAO = new WinningLottoDAO();
+        winningLottoDTO = new WinningLottoDTO();
+        winningLottoDTO.setRound(0);
+        winningLottoDTO.setFirstNum(NumberSet.of(1));
+        winningLottoDTO.setSecondNum(NumberSet.of(2));
+        winningLottoDTO.setThirdNum(NumberSet.of(3));
+        winningLottoDTO.setForthNum(NumberSet.of(4));
+        winningLottoDTO.setFifthNum(NumberSet.of(5));
+        winningLottoDTO.setSixthNum(NumberSet.of(6));
+        winningLottoDTO.setBonusNum(NumberSet.of(7));
     }
 
     @Test
-    public void addWinningLotto() {
-        try {
-            WinningLottoDTO winningLottoDTO = new WinningLottoDTO();
-            winningLottoDTO.setRound(0);
-            winningLottoDTO.setFirstNum(NumberSet.of(1));
-            winningLottoDTO.setSecondNum(NumberSet.of(2));
-            winningLottoDTO.setThirdNum(NumberSet.of(3));
-            winningLottoDTO.setForthNum(NumberSet.of(4));
-            winningLottoDTO.setFifthNum(NumberSet.of(5));
-            winningLottoDTO.setSixthNum(NumberSet.of(6));
-            winningLottoDTO.setBonusNum(NumberSet.of(7));
-            winningLottoDAO.addWinningLotto(winningLottoDTO);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public void addWinningLotto_테스트() throws SQLException {
+        winningLottoDAO.addWinningLotto(winningLottoDTO);
+        winningLottoDAO.deleteWinningLotto(0);
     }
 
     @Test
-    public void getLatestRoundFromWinningLotto() throws SQLException {
-        int round = winningLottoDAO.getNewRound();
-        assertThat(round).isEqualTo(2);
+    public void getNewRound_테스트() throws SQLException {
+        assertThat(winningLottoDAO.getNewRound()).isNotNull();
+    }
+
+    @Test
+    public void getWinningLotto_테스트() throws SQLException {
+        winningLottoDAO.addWinningLotto(winningLottoDTO);
+        WinningLottoDTO winningLotto = winningLottoDAO.getWinningLotto(0);
+        assertThat(winningLotto.getRound()).isEqualTo(0);
+        winningLottoDAO.deleteWinningLotto(0);
     }
 }
