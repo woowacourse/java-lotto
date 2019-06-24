e26b310cddba7f24bad7e550a3822ab654e14931
deocksoo <dusol92@snu.ac.kr>
2019-06-25 02:11:36 +0900

    [REFACTOR] InputView에서 parsing 기능을 분리합니다.
    StringParser 클래스를 만들어 분리하였습니다.

---
+++ src/main/java/utils/StringParser.java
@@ -0,0 +1,34 @@
+package utils;
+
+import domain.PurchaseAmount;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class StringParser {
+    public static int parsePurchaseAmount(String moneyFromPlayer) {
+        return Optional.ofNullable(moneyFromPlayer)
+                .map(Integer::parseInt)
+                .orElse(PurchaseAmount.MINIMUM_AMOUNT);
+    }
+
+    public static List<Integer> parseNumbers(String lottoNumbers) {
+        List<String> splitedNmbers = Optional.ofNullable(lottoNumbers)
+                .map(numbers -> Arrays.asList(numbers.split(",\\s*")))
+                .orElse(new ArrayList<>());
+
+        return splitedNmbers.stream()
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public static int parseInt(String inputNumber) {
+        return Optional.ofNullable(inputNumber)
+                .map(Integer::parseInt)
+                .orElse(0);
+    }
+}
