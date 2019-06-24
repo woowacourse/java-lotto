622a5a51a1e27fa44261f865c1c7089a2b2723b9
school0bhy <house1217@ajou.ac.kr>
2019-06-24 12:59:31 +0900

    Refactor : 드라이버 로딩, 드라이버 연결 메서드 분리

--- src/main/java/lotto/dao/DBUtil.java
+++ src/main/java/lotto/dao/DBUtil.java
@@ -5,13 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    public static Connection getConnection() {
-        Connection con = null;
-        String server = "127.0.0.1"; // MySQL 서버 주소
-        String database = "lotto"; // MySQL DATABASE 이름
-        String userName = "lotto_olaf"; //  MySQL 서버 아이디
-        String password = "lotto_olaf"; // MySQL 서버 비밀번호
+    static {
+        loadDriver();
+    }
 
+    private static void loadDriver() {
         // 드라이버 로딩
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -19,6 +17,14 @@ public class DBUtil {
             System.err.println(" !! JDBC Driver load 오류: " + e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    public static Connection getConnection() {
+        Connection con = null;
+        String server = "127.0.0.1"; // MySQL 서버 주소
+        String database = "lotto"; // MySQL DATABASE 이름
+        String userName = "lotto_olaf"; //  MySQL 서버 아이디
+        String password = "lotto_olaf"; // MySQL 서버 비밀번호
 
         // 드라이버 연결
         try {
@@ -28,7 +34,6 @@ public class DBUtil {
             System.err.println("연결 오류:" + e.getMessage());
             e.printStackTrace();
         }
-
         return con;
     }
 
