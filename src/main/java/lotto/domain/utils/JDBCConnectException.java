92d28af0f4115a7edbf0f3bc0086a50a40e47f4b
Soojin Roh <sooreal91@gmail.com>
2019-06-24 12:25:21 +0900

    style comma 추가

--- src/main/java/lotto/domain/utils/JDBCConnectException.java
+++ src/main/java/lotto/domain/utils/JDBCConnectException.java
@@ -2,6 +2,6 @@ package lotto.domain.utils;
 
 public class JDBCConnectException extends Throwable {
     public JDBCConnectException(final String message) {
-        super(message)
+        super(message);
     }
 }
