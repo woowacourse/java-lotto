b95ba429eda4f16db6d438124a7e52636ea6be7e
stopsilver13 <stopsilver13>
2019-06-25 23:07:48 +0900

    [REVIEW] Redistribute responsibility from App to Controller

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -1,157 +1,41 @@
 package lotto;
 
-import lotto.controller.*;
-import lotto.domain.LottosResult;
-import lotto.domain.Money;
-import lotto.domain.Rank;
-import lotto.domain.lotto.Lotto;
-import lotto.domain.lotto.Lottos;
-import lotto.domain.lotto.WinningLotto;
-import lotto.domain.util.CustomStringUtils;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static spark.Spark.*;
 
 public class WebUILottoApplication {
-    private static int ADD_ONE_BEFORE_ADD_LOTTO_GAME = 1;
-    private static int ADD_ONE_FOR_INCLUDE_LIMIT_NUMBER = 1;
-
     public static void main(String[] args) {
         staticFiles.location("/");
 
         get("/", (req, res) -> render(null, "home.html"));
 
-        get("/money", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("round", LottoGameController.getLastRound() + ADD_ONE_BEFORE_ADD_LOTTO_GAME);
+        get("/money", (req, res) -> render(WebUILottoController.getFutureRound(), "paymoney.html"));
 
-            return render(model, "paymoney.html");
-        });
+        post("/money", (req, res) -> WebUILottoController.addGameAndMoney(req, res));
 
-        post("/money", (req, res) -> {
-            LottoGameController.addLottoGame();
-            MoneyController.addMoney(req);
+        get("/manual-quantity", (req, res) -> render(WebUILottoController.getBuyableQuantity(), "inputmanualquantity.html"));
 
-            res.redirect("/manual-quantity");
-            return "";
-        });
+        post("/manual-quantity", (req, res) -> WebUILottoController.addAutoLottos(req, res));
 
-        get("/manual-quantity", (req, res) -> {
-            Money money = MoneyController.getMoney();
+        get("/manual-numbers", (req, res) -> render(WebUILottoController.getManualQuantity(req), "inputmanualnumbers.html"));
 
-            Map<String, Object> model = new HashMap<>();
-            model.put("totalQuantity", money.getBuyableLottoQuantity());
+        post("/manual-numbers", (req, res) -> WebUILottoController.addManualLottos(req, res));
 
-            return render(model, "inputmanualquantity.html");
-        });
+        get("/winning", (req, res) -> render(WebUILottoController.getLottosInfo(), "inputwinningnumbers.html"));
 
-        post("/manual-quantity", (req, res) -> {
-            String inputQuantity = req.queryParams("manual-quantity");
-            CustomStringUtils.checkIsBlank(inputQuantity);
+        post("/winning", (req, res) -> WebUILottoController.addLottoResult(req, res));
 
-            LottoController.addAutoLottos(inputQuantity);
+        get("/result", (req, res) -> render(WebUILottoController.getResultInfo(), "reportresult.html"));
 
-            res.redirect(String.format("/manual-numbers?quantity=%s", inputQuantity));
+        get("/lookup", (req, res) -> render(WebUILottoController.getLastRound(), "lookupresult.html"));
 
-            return "";
-        });
+        post("/lookup", (req, res) -> WebUILottoController.deliverRoundInfo(req, res));
 
-        get("/manual-numbers", (req, res) -> {
-            String quantity = req.queryParams("quantity");
-            List<Integer> iterator = IntStream.range(1, Integer.parseInt(quantity) + ADD_ONE_FOR_INCLUDE_LIMIT_NUMBER)
-                    .boxed().collect(Collectors.toList());
-
-            Map<String, Object> model = new HashMap<>();
-            model.put("quantity", quantity);
-            model.put("iterator", iterator);
-
-            return render(model, "inputmanualnumbers.html");
-        });
-
-        post("/manual-numbers", (req, res) -> {
-            String manualQuantity = req.queryParams("quantity");
-
-            LottoController.addManualLottos(req, manualQuantity);
-
-            res.redirect("/winning");
-            return "";
-        });
-
-        get("/winning", (req, res) -> {
-            List<Lotto> lottos = LottoController.getBoughtLottos();
-            long manualQuantity = lottos.stream().filter(lotto -> lotto.isAuto() == false).count();
-            long autoQuantity = lottos.stream().filter(lotto -> lotto.isAuto() == true).count();
-
-            Map<String, Object> model = new HashMap<>();
-            model.put("round", LottoGameController.getLastRound());
-            model.put("lottos", lottos);
-            model.put("manualQuantity", manualQuantity);
-            model.put("autoQuantity", autoQuantity);
-
-            return render(model, "inputwinningnumbers.html");
-        });
-
-        post("/winning", (req, res) -> {
-            LottoResultController.addLottoResult(req);
-
-            res.redirect("/result");
-            return "";
-        });
-
-        get("/result", (req, res) -> {
-            LottosResult result = LottoResultController.getLottoResult();
-
-            List<Integer> values = new ArrayList<>();
-            for (Rank rank : Rank.values()) {
-                values.add(result.valueOf(rank));
-            }
-
-            Map<String, Object> model = new HashMap<>();
-            model.put("round", LottoGameController.getLastRound());
-            model.put("values", values);
-            model.put("result", result);
-
-            return render(model, "reportresult.html");
-        });
-
-        get("/lookup", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("maxRound", LottoGameController.getLastRound());
-
-            return render(model, "lookupresult.html");
-        });
-
-        post("/lookup", (req, res) -> {
-            res.redirect(String.format("/report?round=%s", req.queryParams("round")));
-            return "";
-        });
-
-        get("/report", (req, res) -> {
-            int round = CustomStringUtils.parseInt(req.queryParams("round"));
-
-            List<Lotto> lottos = LottoController.getBoughtLottos(round);
-            WinningLotto winningLotto = WinningLottoController.getWinningLotto(round);
-            LottosResult result = new LottosResult(winningLotto, new Lottos(lottos));
-
-            List<Integer> values = new ArrayList<>();
-            for (Rank rank : Rank.values()) {
-                values.add(result.valueOf(rank));
-            }
-
-            Map<String, Object> model = new HashMap<>();
-            model.put("round", round);
-            model.put("lottos", lottos);
-            model.put("winning", winningLotto);
-            model.put("values", values);
-            model.put("result", result);
-
-            return render(model, "reportlookup.html");
-        });
+        get("/report", (req, res) -> render(WebUILottoController.getAllGameInfo(req), "reportlookup.html"));
 
         exception(Exception.class, (exception, request, response) -> {
             response.body(String.format("<script>alert('%s'); history.back();</script>", exception.getMessage(), request.pathInfo()));
