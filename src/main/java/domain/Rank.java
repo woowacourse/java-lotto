93cbbedf9efb4fd9a24c54f14a55b4a80ba4d211
deocksoo <dusol92@snu.ac.kr>
2019-06-26 08:27:40 +0900

    [REFACTOR] Rank의 펙토리 메서드를 수정합니다
    Rank의 instance들을 생성할 때, bonus 숫자의 매칭 여부를 생성시 명시적으로 드러날
    수 있게 하기 위해 전략 패턴을 사용하였습니다.

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -3,37 +3,32 @@ package domain;
 import domain.money.Money;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum Rank {
-    FIRST(6, Money.amountOf(2_000_000_000)),
-    SECOND(5, Money.amountOf(30_000_000)),
-    THIRD(5, Money.amountOf(1_500_000)),
-    FOURTH(4, Money.amountOf(50_000)),
-    FIFTH(3, Money.amountOf(5_000)),
-    MISS(0, Money.amountOf(0));
+    FIRST(6, Money.amountOf(2_000_000_000), (umberOfMatching, bonus) -> umberOfMatching.equals(6)),
+    SECOND(5, Money.amountOf(30_000_000), (umberOfMatching, bonus) -> umberOfMatching.equals(5)
+                                                                                            && bonus.equals(true)),
+    THIRD(5, Money.amountOf(1_500_000), (umberOfMatching, bonus) -> umberOfMatching.equals(5)),
+    FOURTH(4, Money.amountOf(50_000), (umberOfMatching, bonus) -> umberOfMatching.equals(4)),
+    FIFTH(3, Money.amountOf(5_000), (umberOfMatching, bonus) -> umberOfMatching.equals(3)),
+    MISS(0, Money.amountOf(0), (umberOfMatching, bonus) -> umberOfMatching.equals(0));
 
-    private int numberOfMatching;
+    public final int numberOfMatching;
     private Money winningMoney;
+    private BiFunction<Integer, Boolean, Boolean> matchingStrategy;
 
-    Rank(int numberOfMatching, Money winningMoney) {
+    Rank(int numberOfMatching, Money winningMoney, BiFunction<Integer, Boolean, Boolean> matchingStrategy) {
         this.numberOfMatching = numberOfMatching;
         this.winningMoney = winningMoney;
+        this.matchingStrategy = matchingStrategy;
     }
 
-    public static Rank of(int numberOfMatching, boolean BonusNumberMatch) {
-        if (numberOfMatching == SECOND.numberOfMatching && BonusNumberMatch) {
-            return SECOND;
-        }
-
+    public static Rank of(int numberOfMatching, Boolean bonus) {
         return Arrays.stream(values())
-                .filter(rank -> rank != SECOND)
-                .filter(rank -> rank.hasSame(numberOfMatching))
+                .filter(rank -> rank.matchingStrategy.apply(numberOfMatching, bonus))
                 .findFirst()
                 .orElse(MISS);
-    }
-
-    private boolean hasSame(int numberOfMatching) {
-        return this.numberOfMatching == numberOfMatching;
     }
 
     public Money getWinningMoney() {
