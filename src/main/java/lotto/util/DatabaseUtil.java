f834a908a746eff795c7e1ff0c2cfaf44adcdf6f
men7627 <men1219@hanmail.net>
2019-06-27 13:59:55 +0900

    [Add] implement DatabasesUtil

--- src/main/java/lotto/util/DatabaseUtil.java
+++ src/main/java/lotto/util/DatabaseUtil.java
@@ -4,18 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseUtil {
+        private static String url = "jdbc:mysql://localhost/lotto?serverTimezone=UTC";
+        private static String id = "hgkim";
+        private static String password = "1234";
 
         public static Connection getConnection() {
-                String url = "jdbc:mysql://localhost/lotto?serverTimezone=UTC";
-                String id = "hgkim";
-                String password = "1234";
-
                 try {
                         Class.forName("com.mysql.cj.jdbc.Driver");
                         return DriverManager.getConnection(url, id, password);
                 } catch (Exception e) {
                         System.err.println(e.getMessage());
-                        return null;
                 }
+                return null;
         }
 }
--- src/test/java/lotto/util/DatabaseUtilTest.java
+++ src/test/java/lotto/util/DatabaseUtilTest.java
@@ -8,8 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DatabaseUtilTest {
         @Test
-        void DB_연결_테스트() {
-                Connection conn = DatabaseUtil.getConnection();
+        void DB_연결_테스트() throws Exception {
+                Connection conn = DatabaseUtil.getInstance().getConnection();
                 assertNotNull(conn);
         }
 }
