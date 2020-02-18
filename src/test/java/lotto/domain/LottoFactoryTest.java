4ae6229b3522a696fd57fd5ecb447f5bcdeaa52f
YerinCho <forjustice9839@gmail.com>
2020-02-18 17:19:47 +0900

    [Lotto] feat : LottoFactory
     로또 번호 생성 기능 추가
     무작위로 6개 숫자 선택해 로또 생성

---
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+public class LottoFactoryTest {
+
+    @Test
+    @DisplayName("로또 번호 생성")
+    void createLottoNumbersTest(){
+        LottoFactory lottoFactory=new LottoFactory();
+        List<Integer> lotto = LottoFactory.createLottoNumbers();
+        assertThat(lotto.size()).isEqualTo(6);
+    }
+}
