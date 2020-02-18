24f72b7c881d10d5f4c4b213729d6dfa9d395081
sujin <sujinemilycho@gmail.com>
2020-02-18 18:04:00 +0900

    feat: 로또 구매 금액 입력 기능 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void inputAmmount() {
+
+    }
+}
