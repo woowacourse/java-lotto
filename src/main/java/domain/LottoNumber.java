2395c337bcef386719b3a72062bcb3c7f51b4560
KimSeongGyu1 <KimSeongGyu1@github.com>
2020-02-18 16:20:23 +0900

    [feature] 로또 숫자 생성 예외처리 기능 구현

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -3,10 +3,19 @@ package domain;
 import java.util.Objects;
 
 public class LottoNumber {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
     private final int number;
 
     public LottoNumber(int number) {
+        validateLottoNumber(number);
         this.number = number;
+    }
+
+    private void validateLottoNumber(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("1~45 사이의 숫자만 가능합니다.");
+        }
     }
 
     @Override
--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -4,9 +4,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("로또 숫자 테스트")
 public class LottoNumberTest {
+    @Test
+    @DisplayName("로또 숫자 생성 예외처리 테스트")
+    void lottoNumberTest() {
+        assertThatThrownBy(() -> new LottoNumber(46))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1~45 사이의 숫자만 가능합니다.");
+    }
+
     @Test
     @DisplayName("로또 숫자 비교 테스트")
     void lottoNumberEqualTest() {
