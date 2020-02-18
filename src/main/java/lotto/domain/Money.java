b57f191d659190384cc77aa6425d5898c4c5f5de
LTTTTTE <10100aaaaa@gmail.com>
2020-02-18 17:54:35 +0900

    feat: 구입 금액이 자연수인지 확인하는 기능

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,17 +1,25 @@
 package lotto.domain;
 
+import java.util.Optional;
+
 public class Money {
 	private int money;
 
 	public Money(String money) {
 		try {
-			this.money = Integer.parseInt(money);
+			this.money = Optional.of(Integer.parseInt(money))
+				.filter(this::isNaturalNumber)
+				.orElseThrow(NumberFormatException::new);
 		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("구입금액을 다시 입력하세요");
+			throw new IllegalArgumentException("구입금액은 자연수로 입력해주세요.");
 		}
 	}
 
 	public int getMoney() {
 		return money;
+	}
+
+	private boolean isNaturalNumber(int money) {
+		return money > 0;
 	}
 }
