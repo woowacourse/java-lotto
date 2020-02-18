2d46824d429e59144ac65ab290f829c6a2a8d79a
gunhee lee <dlrjsgml42@naver.com>
2020-02-18 17:26:18 +0900

    [refactor] Money 클래스 refactor
    Money 예외처리 메소드 분리 및 상수 처리

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -1,16 +1,27 @@
 package domain;
 
 public class Money {
+    private static final String ERROR_NOT_POSITIVE_MESSAGE = "금액은 0 이하로 입력될 수 없습니다.";
+    private static final String ERROR_SURPLUS_MESSAGE = "잔돈이 남습니다. 결제할 수 없습니다.";
+    public static final int LOTTO_PRICE = 1000;
     private int money;
 
     public Money(int money) {
-        if (money <= 0) {
-            throw new IllegalArgumentException("금액은 0 이하로 입력될 수 없습니다.");
-        }
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("잔돈이 남습니다. 결제할 수 없습니다.");
-        }
+        validatePositiveMoney(money);
+        validateSurplusMoney(money);
         this.money = money;
+    }
+
+    private void validateSurplusMoney(int money) {
+        if (money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ERROR_SURPLUS_MESSAGE);
+        }
+    }
+
+    private void validatePositiveMoney(int money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException(ERROR_NOT_POSITIVE_MESSAGE);
+        }
     }
 
     public int calculateEarnings(int earningMoney) {
