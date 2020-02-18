313e64415174c8e6fad5b5afd098d832904d161d
taejinseok <stj930@gmail.com>
2020-02-18 18:54:17 +0900

    [Test] 볼 숫자 비교 추가

--- src/test/java/lotto/domain/BallTest.java
+++ src/test/java/lotto/domain/BallTest.java
@@ -4,15 +4,25 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BallTest {
 
 	@DisplayName("볼 가져오기 범위 외 숫자")
 	@ParameterizedTest
-	@ValueSource(ints = {-1, 46})
+	@ValueSource(ints = {0, 46})
 	void getBallOutOfRange(int number) {
 		assertThatThrownBy(() -> Ball.of(number))
 		.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("볼 비교")
+	@ParameterizedTest
+	@CsvSource(value = {"1,1,true", "40,40,true", "1,30,false", "40,45,false"})
+	void vsBall(int firstBallNo, int secondBallNo, boolean expected) {
+		Ball firstBall = Ball.of(firstBallNo);
+		Ball secondBall = Ball.of(secondBallNo);
+		assertThat(firstBall.equals(secondBall)).isEqualTo(expected);
 	}
 }
