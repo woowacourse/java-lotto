ff618aaf879df20c0565ac9102580fc05df34e5b
chanin <pci2676@gmail.com>
2020-02-18 18:13:34 +0900

    feat : 금액 입력 기능 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class InputView {
     private final static Scanner scanner = new Scanner(System.in);
+    private static final String INPUT_MONEY_MESSAGE = "구입 금액을 입력해 주세요.";
 
-    public static void inputAmmount() {
-
+    public static int inputAmount() {
+        System.out.println(INPUT_MONEY_MESSAGE);
+        return scanner.nextInt();
     }
 }
