adeb5ff5970246c9ddeca88b981f9332da5c2eeb
YerinCho <forjustice9839@gmail.com>
2020-02-18 17:30:27 +0900

    [Lotto] refactor : LottoFactory
     상수화, 불필요한 코드 삭제

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -5,24 +5,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoFactory {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_LENGTH = 6;
     private static List<Integer> lottoNumbers = new ArrayList<>();
 
     public LottoFactory() {
-        for (int i = 1; i < 46; i++) {
+        for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
             lottoNumbers.add(i);
         }
     }
 
     public static List<Integer> createLottoNumbers() {
-        Collections.shuffle(lottoNumbers);
-
         List<Integer> lotto = new ArrayList<>();
-
-        for (int i = 0; i < 6; i++) {
+        Collections.shuffle(lottoNumbers);
+        for (int i = 0; i < LOTTO_LENGTH; i++) {
             lotto.add(lottoNumbers.get(i));
-            System.out.println(lotto.get(i));
         }
-
         return lotto;
     }
 }
