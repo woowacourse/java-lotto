cd6f8025f60badac0cb3ba4f10c4aea1b379f181
lalize <printf@kakao.com>
2020-02-18 15:38:08 +0900

    feat: LottoNumbers 클래스 추가

---
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class LottoNumbers {
+	private static final int SIZE = 6;
+	private static final String DUPLICATED_NUMBER_MESSAGE = "로또 번호가 중복됩니다.";
+	private static final String INVALID_SIZE_MESSAGE = "로또 번호가 존재하지 않습니다.";
+
+	private final List<LottoNumber> numbers = new ArrayList<>();
+
+	public LottoNumbers(List<LottoNumber> numbers) {
+		validate(numbers);
+		this.numbers.addAll(numbers);
+		Collections.sort(this.numbers);
+	}
+
+	private void validate(List<LottoNumber> numbers) {
+		validateSize(numbers);
+		validateDuplicate(numbers);
+	}
+
+	private void validateSize(List<LottoNumber> numbers) {
+		if (Objects.isNull(numbers) || numbers.size() != SIZE) {
+			throw new IllegalArgumentException(INVALID_SIZE_MESSAGE);
+		}
+	}
+
+	private void validateDuplicate(List<LottoNumber> numbers) {
+		Set<LottoNumber> distinctNumbers = new HashSet<>(numbers);
+		if (distinctNumbers.size() != numbers.size()) {
+			throw new IllegalArgumentException(DUPLICATED_NUMBER_MESSAGE);
+		}
+	}
+
+	public List<LottoNumber> getNumbers() {
+		return Collections.unmodifiableList(numbers);
+	}
+}
---
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -0,0 +1,82 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class LottoNumbersTest {
+	@Test
+	@DisplayName("선택된 로또 번호 리스트가 정상적으로 생성된 경우")
+	void constructor() {
+		List<LottoNumber> lottoNumbers = Arrays.asList(LottoNumber.of(1),
+				LottoNumber.of(2),
+				LottoNumber.of(3),
+				LottoNumber.of(43),
+				LottoNumber.of(44),
+				LottoNumber.of(45));
+		assertThat(new LottoNumbers(lottoNumbers)).isInstanceOf(LottoNumbers.class);
+	}
+
+	static Stream<List<LottoNumber>> invalidSize() {
+		return Stream.of(
+				null,
+				new ArrayList<>(),
+				Arrays.asList(LottoNumber.of(1),
+						LottoNumber.of(2),
+						LottoNumber.of(3),
+						LottoNumber.of(43),
+						LottoNumber.of(44)),
+				Arrays.asList(LottoNumber.of(1),
+						LottoNumber.of(2),
+						LottoNumber.of(3),
+						LottoNumber.of(4),
+						LottoNumber.of(43),
+						LottoNumber.of(44),
+						LottoNumber.of(45)));
+	}
+
+	@ParameterizedTest
+	@DisplayName("로또 번호 리스트의 크기가 올바르지 않은 경우")
+	@MethodSource("invalidSize")
+	void constructor_로또_번호_리스트의_크기가_올바르지_않은_경우(List<LottoNumber> lottoNumbers) {
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new LottoNumbers(lottoNumbers));
+	}
+
+	@Test
+	@DisplayName("중복된 수가 있는 경우")
+	void constructor_중복된_수가_있는_경우() {
+		List<LottoNumber> lottoNumbers = Arrays.asList(LottoNumber.of(1),
+				LottoNumber.of(2),
+				LottoNumber.of(2),
+				LottoNumber.of(43),
+				LottoNumber.of(44),
+				LottoNumber.of(45));
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new LottoNumbers(lottoNumbers));
+	}
+
+	@Test
+	@DisplayName("수가 정렬되어 있는지 확인")
+	void constructor_수가_정렬되어_있는지_확인() {
+		LottoNumbers lottoNumbers = new LottoNumbers(Arrays.asList(LottoNumber.of(45),
+				LottoNumber.of(44),
+				LottoNumber.of(43),
+				LottoNumber.of(1),
+				LottoNumber.of(2),
+				LottoNumber.of(3)));
+		List<LottoNumber> expected = Arrays.asList(LottoNumber.of(1),
+				LottoNumber.of(2),
+				LottoNumber.of(3),
+				LottoNumber.of(43),
+				LottoNumber.of(44),
+				LottoNumber.of(45));
+		assertThat(lottoNumbers.getNumbers()).isEqualTo(expected);
+	}
+}
