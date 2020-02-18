22b069e5adc93d6a7d86836922265b1573dbd723
LTTTTTE <10100aaaaa@gmail.com>
2020-02-18 17:38:32 +0900

    feat: 수량에 맞는 로또를 생성하는 기능 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+	private List<Integer> numbers;
+
+	public Lotto(List<Integer> numbers) {
+		this.numbers = numbers;
+	}
+}
--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -1,9 +1,24 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LottoManager {
 	private static final int LOTTO_PRICE = 1000;
+	private static final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
 
 	public static int calculateLottoAmount(Money money) {
 		return money.getMoney() / LOTTO_PRICE;
+	}
+
+	public static Lotto generateSingleLotto() {
+		return new Lotto(lottoNumberGenerator.generate());
+	}
+
+	public static List<Lotto> generateLottoByAmount(int amount) {
+		return IntStream.range(0, amount)
+			.mapToObj(x -> generateSingleLotto())
+			.collect(Collectors.toList());
 	}
 }
