f37d3bd1287b7e27d5d60b2c98172cebbc6207fc
KIMSIYOUNG <tldud2404@gmail.com>
2020-02-18 17:14:19 +0900

    feat: 구입 금액 검증(단위, 문자열)
    구입 금액이 1000원 단위로 들어오는 경우 예외처리
    구입 금액에 문자열을 입력받은 경우 예외처리

---
+++ src/main/java/lotto/IllegalMoneyUnitException.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class IllegalMoneyUnitException extends RuntimeException{
+
+	public IllegalMoneyUnitException() {
+	}
+
+	public IllegalMoneyUnitException(String message) {
+		super(message);
+	}
+}
