f6b098f45cc52f9f9b0694a09d66d97f4655c4cb
sujin <sujinemilycho@gmail.com>
2020-02-18 17:58:11 +0900

    feat: LottoTicket equals 기능 구현

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,11 +1,25 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class LottoTicket {
     private List<LottoBall> lottoBalls;
 
     public LottoTicket(List<LottoBall> lottoBalls) {
         this.lottoBalls = lottoBalls;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoTicket that = (LottoTicket) o;
+        return Objects.equals(lottoBalls, that.lottoBalls);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoBalls);
     }
 }
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -7,15 +7,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LottoTicketTest {
 
     @DisplayName("로또 티켓 생성하기")
     @Test
     void test1() {
-        List<LottoBall> lottoBalls = new ArrayList<>(Arrays.asList(new LottoBall()));
+        List<LottoBall> lottoBalls = new ArrayList<>(Arrays.asList(new LottoBall(1),
+                new LottoBall(2),
+                new LottoBall(3),
+                new LottoBall(4),
+                new LottoBall(5),
+                new LottoBall(6)));
 
         LottoTicket lottoTicket = new LottoTicket(lottoBalls);
 
-
+        LottoTicket expectedTicket = new LottoTicket(Arrays.asList(new LottoBall(1),
+                new LottoBall(2),
+                new LottoBall(3),
+                new LottoBall(4),
+                new LottoBall(5),
+                new LottoBall(6)));
+        assertThat(lottoTicket).isEqualTo(expectedTicket);
     }
 }
