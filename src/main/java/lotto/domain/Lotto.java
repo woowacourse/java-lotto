f8ac34f1e4c084c95a1aa0ea69cc76dd499f15d6
hj <a301dks@naver.com>
2020-02-19 12:03:23 +0900

    feat: Lotto에서 공 갯수가 맞지 않는 경우 예외처리 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+	private static final int LOTTO_SIZE = 6;
 	private final List<Integer> lottoNumber;
 
 	public Lotto(final List<Integer> lottoNumber) {
@@ -11,12 +12,19 @@ public class Lotto {
 	}
 
 	private void validate(List<Integer> lottoNumber) {
-		validateNullOrEmpty(lottoNumber);
+		validateNullAndEmpty(lottoNumber);
+		validateSizeMismatch(lottoNumber);
 	}
 
-	private void validateNullOrEmpty(List<Integer> lottoNumber) {
+	private void validateNullAndEmpty(List<Integer> lottoNumber) {
 		if (lottoNumber == null || lottoNumber.isEmpty()) {
 			throw new IllegalArgumentException("null이나 빈 값은 들어올 수 없습니다!");
+		}
+	}
+
+	private void validateSizeMismatch(List<Integer> lottoNumber) {
+		if (lottoNumber.size() != LOTTO_SIZE) {
+			throw new IllegalArgumentException("로또 번호는 " + LOTTO_SIZE + "개여야 합니다!");
 		}
 	}
 
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,9 +4,12 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class LottoTest {
@@ -17,11 +20,24 @@ public class LottoTest {
 		assertThat(lotto.contains(value)).isEqualTo(expected);
 	}
 
+	static Stream<Arguments> generateInput() {
+		return Stream.of(Arguments.of(Arrays.asList(1, 2, 3, 4, 5)),
+			Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
+	}
+
 	@ParameterizedTest
 	@NullAndEmptySource
 	void 로또가_null이나_빈값인_경우(List<Integer> value) {
 		assertThatThrownBy(() -> new Lotto(value))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining("null이나 빈 값");
+	}
+
+	@ParameterizedTest
+	@MethodSource("generateInput")
+	void 로또_공_갯수가_맞지_않는_경우(List<Integer> value) {
+		assertThatThrownBy(() -> new Lotto(value))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("개여야 합니다");
 	}
 }
