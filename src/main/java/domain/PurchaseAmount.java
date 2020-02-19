7cc6586b3b16bb0bea36d18a0a81bbbc1d3b19f6
yeonnseok <ljinsk3@gmail.com>
2020-02-19 17:30:27 +0900

    feat: 구매금액이 음수인 경우 예외처리 기능 구현

--- src/main/java/domain/PurchaseAmount.java
+++ src/main/java/domain/PurchaseAmount.java
@@ -1,11 +1,25 @@
 package domain;
 
 public class PurchaseAmount {
+    private int amount;
+
     public PurchaseAmount(String inputMoney) {
+        checkNotNumber(inputMoney);
+        this.amount = Integer.parseInt(inputMoney);
+        checkNegativeAmount(amount);
+    }
+
+    private void checkNotNumber(String inputMoney) {
         try {
             Integer.parseInt(inputMoney);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("구매 금액은 숫자여야합니다.");
+        }
+    }
+
+    private void checkNegativeAmount(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("구매 금액은 음수일 수 없습니다.");
         }
     }
 }
--- src/test/java/lotto/PurchaseAmountTest.java
+++ src/test/java/lotto/PurchaseAmountTest.java
@@ -14,5 +14,12 @@ public class PurchaseAmountTest {
                 .hasMessage("구매 금액은 숫자여야합니다.");
     }
 
+    @Test
+    void 구매금액이_음수인지_검증() {
+        assertThatThrownBy(() -> {
+            new PurchaseAmount("-1");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구매 금액은 음수일 수 없습니다.");
+    }
 
 }
