c53586849dbd062b3059c5ceaeebad695d4e076a
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-02-19 16:51:06 +0900

    [feat] 어플리케이션 메인 로직 생성

---
+++ src/main/java/lotto/ConsoleLottoApplication.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsoleLottoApplication {
+    private static RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    public static void main(String[] args) {
+        PurchaseNumber purchaseNumber = InputView.inputPurchaseMoney();
+        OutputView.printPurchaseNumber(purchaseNumber);
+        List<LottoNumbers> lottoNumbersList = new ArrayList<>();
+        for (int i = 0; i < purchaseNumber.getPurchaseNumber(); i++) {
+            List<LottoNumberGroup> randomNumbers = randomNumberGenerator.generateNumbers();
+            lottoNumbersList.add(new LottoNumbers(randomNumbers));
+        }
+        AllLottoNumbers allLottoNumbers = new AllLottoNumbers(lottoNumbersList);
+    }
+}
