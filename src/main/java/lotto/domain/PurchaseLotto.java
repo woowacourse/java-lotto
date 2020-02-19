96566b64c88c12ad0523fa7a8fc76c69b3633e2f
YerinCho <forjustice9839@gmail.com>
2020-02-19 10:49:20 +0900

    [Lotto] feat : PurchaseLotto 입력 예외처리
     입력한 금액이 1000원 미만이면 예외처리

--- src/main/java/lotto/domain/PurchaseLotto.java
+++ src/main/java/lotto/domain/PurchaseLotto.java
@@ -1,9 +1,16 @@
 package lotto.domain;
 
 public class PurchaseLotto {
-    public static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_PRICE = 1000;
 
     public static int purchase(int money) {
+        purchaseException(money);
         return money / LOTTO_PRICE;
+    }
+
+    static void purchaseException(int money) {
+        if (money < LOTTO_PRICE) {
+            throw new IllegalArgumentException("돈이 부족해 로또를 살 수 없습니다.");
+        }
     }
 }
--- src/test/java/lotto/domain/PurchaseLottoTest.java
+++ src/test/java/lotto/domain/PurchaseLottoTest.java
@@ -1,16 +1,26 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PurchaseLottoTest {
     @ParameterizedTest
     @DisplayName("로또 구매 매수 계산")
-    @CsvSource(value = {"10,0","1000,1","2000,2","3333,3"})
+    @CsvSource(value = {"10,0", "1000,1", "2000,2", "3333,3"})
     void purchaseTest(int money, int result) {
         assertThat(PurchaseLotto.purchase(money)).isEqualTo(result);
     }
+
+    @DisplayName("금액 1000원 미만 예외처리 테스트")
+    @Test
+    void purchaseExceptionTest() {
+        assertThatThrownBy(() -> PurchaseLotto.purchaseException(999))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
+
