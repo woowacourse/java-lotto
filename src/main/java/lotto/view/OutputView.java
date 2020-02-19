d47f4e4cfdc77a8e245b2ac9ae3824c7bb503d05
hwanghe159 <hwanghe159@gmail.com>
2020-02-19 15:30:01 +0900

    feat : 로또 한 줄의 숫자를 출력하는 메서드를 뷰에 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,28 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
 public class OutputView {
     public static final String COMMA = ", ";
 
     private OutputView() { /* prevent creating OutputView instance */ }
+
+    public static void printLottoNumbers(Lotto lotto) {
+        List<Integer> numbers = lotto.getNumbers();
+        String result = numbers.stream()
+                .map(String::valueOf)
+                .collect(joining(COMMA));
+        StringBuilder sb = new StringBuilder();
+        sb.append("[")
+                .append(result)
+                .append("]");
+
+        System.out.println(sb.toString());
+    }
 
     public static void printLottoCount(int lottoCount) {
         System.out.println(String.format("%d개를 구매했습니다.", lottoCount));
