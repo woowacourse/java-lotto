fcc5162482af43234bce53a2aa9cd575efcd72b2
Yeji Ahn <yeppilisa@naver.com>
2020-02-19 10:34:33 +0900

    feature : 로또 티켓 콜렉션 검증 및 예외처리 추가

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -6,12 +6,25 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import lotto.exceptions.InvalidLottoTicketException;
+
 public class LottoTicket {
     private static final int LOTTO_NUMBER_COUNT = 6;
     private final List<LottoNumber> lottoTicket;
 
-    private LottoTicket(final List<LottoNumber> lottoTicket) {
+    public LottoTicket(final List<LottoNumber> lottoTicket) {
+        validate(lottoTicket);
         this.lottoTicket = Collections.unmodifiableList(lottoTicket);
+    }
+
+    private void validate(List<LottoNumber> lottoTicket) {
+        if (lottoTicket.size() != LOTTO_NUMBER_COUNT) {
+            throw new InvalidLottoTicketException();
+        }
+        if (lottoTicket.size() != lottoTicket.stream().distinct().count()) {
+            throw new InvalidLottoTicketException();
+        }
+
     }
 
     static LottoTicket create() {
--- src/main/java/lotto/exceptions/InvalidLottoTicketException.java
+++ src/main/java/lotto/exceptions/InvalidLottoTicketException.java
@@ -1,4 +1,7 @@
 package lotto.exceptions;
 
-public class InvalidLottoTicketException extends Throwable {
+public class InvalidLottoTicketException extends RuntimeException {
+    public InvalidLottoTicketException() {
+        super("유효하지 않은 당첨 번호 값입니다.");
+    }
 }
