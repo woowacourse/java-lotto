a4db7b1e9a73d071e2381a9c0d8a742b4d8bdae9
LTTTTTE <10100aaaaa@gmail.com>
2020-02-19 16:25:08 +0900

    refactor: 구입한 로또가 정렬될수 있도록 수정

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class LottoNumberGenerator implements NumberGenerator {
 	public static final int LOTTO_NUMBER_SIZE = 6;
-	public static final int MAX_LOTTO_NUMBER_RANGE = 45;
+	public static final int MAX_LOTTO_NUMBER_RANGE = 46;
 	public static final int MIN_LOTTO_NUMBER_RANGE = 1;
 
 	@Override
@@ -15,6 +15,7 @@ public class LottoNumberGenerator implements NumberGenerator {
 			.ints(MIN_LOTTO_NUMBER_RANGE, MAX_LOTTO_NUMBER_RANGE)
 			.distinct()
 			.limit(LOTTO_NUMBER_SIZE)
+			.sorted()
 			.boxed()
 			.collect(Collectors.toList());
 	}
