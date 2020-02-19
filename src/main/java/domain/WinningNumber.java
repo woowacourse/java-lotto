ba33534dc3ab8a952615071179f950c20c1fc4a1
yeonnseok <ljinsk3@gmail.com>
2020-02-19 19:35:27 +0900

    feat: 보너스번호 생성 및 유효성 검증 구현

--- src/main/java/domain/WinningNumber.java
+++ src/main/java/domain/WinningNumber.java
@@ -5,13 +5,15 @@ import java.util.Set;
 
 public class WinningNumber {
     private Lotto winningNumbers;
+    private LottoNumber bonusNumber;
 
-    public WinningNumber(String[] numbers) {
+    public WinningNumber(String[] numbers, String bonusNumber) {
         Set<LottoNumber> winningNumbers = new HashSet<>();
         for (String number : numbers) {
             winningNumbers.add(new LottoNumber(number));
         }
         this.winningNumbers = new Lotto(winningNumbers);
+        this.bonusNumber = new LottoNumber(bonusNumber);
     }
 }
 
--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -10,7 +10,7 @@ public class WinningNumberTest {
     void 입력된_번호가_문자인지_확인(){
         assertThatThrownBy(() -> {
             String[] numbers = {"1", "2", "3", "4", "d", "6"};
-            new WinningNumber(numbers);
+            new WinningNumber(numbers, "30");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(String.format("로또 넘버는 숫자여야 합니다. 입력한 문자 : %s", "d"));
     }
@@ -19,7 +19,7 @@ public class WinningNumberTest {
     void 당첨_로또_번호가_로또의_범위_안_인지_확인(){
         assertThatThrownBy(() -> {
             String[] numbers = {"1", "2", "3", "4", "55", "6"};
-            new WinningNumber(numbers);
+            new WinningNumber(numbers, "30");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 숫자 범위를 넘어섰습니다.");
     }
@@ -28,8 +28,25 @@ public class WinningNumberTest {
     void 당첨_로또_번호의_개수가_6개인지_확인(){
         assertThatThrownBy(() -> {
             String[] numbers = {"1", "2", "3", "4", "6"};
-            new WinningNumber(numbers);
+            new WinningNumber(numbers, "30");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또의 번호는 6개의 숫자로 이루어져 있어야 합니다.");
+    }
+
+    @Test
+    void 보너스_번호_유효성검증(){
+        assertThatThrownBy(() -> {
+            String[] numbers = {"1", "2", "3", "5", "4", "6"};
+            String bonusNumber = "가";
+            new WinningNumber(numbers, bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("로또 넘버는 숫자여야 합니다. 입력한 문자 : %s", "가"));
+
+        assertThatThrownBy(() -> {
+            String[] numbers = {"1", "2", "3", "5", "4", "6"};
+            String bonusNumber = "50";
+            new WinningNumber(numbers, bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 숫자 범위를 넘어섰습니다.");
     }
 }
