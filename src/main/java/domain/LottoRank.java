ef794903da0f99fdb0c9a122490ea378e6d0abfd
gunhee lee <dlrjsgml42@naver.com>
2020-02-19 17:12:04 +0900

    [feat] LottoRank enum 구현
    LottoRank 게임 결과값을 이용해 Rank 생성

---
+++ src/main/java/domain/LottoRank.java
@@ -0,0 +1,36 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIRST(6, false, 2_000_000_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NOTHING(-1, false, 0);
+
+    private final int hitCount;
+    private final boolean bonus;
+    private final int winning;
+
+    private LottoRank(int hitCount, boolean bonus, int winning) {
+        this.hitCount = hitCount;
+        this.bonus = bonus;
+        this.winning = winning;
+    }
+
+    private static LottoRank calculateRankWithoutBonus(int hitCount) {
+        return Arrays.stream(LottoRank.values())
+                .filter(rank -> rank.hitCount == hitCount && !rank.bonus)
+                .findAny()
+                .orElse(NOTHING);
+    }
+
+    public static LottoRank calculateRank(int hitCount, boolean bonus) {
+        return Arrays.stream(LottoRank.values())
+                .filter(rank -> rank.bonus == bonus && rank.hitCount == hitCount)
+                .findAny()
+                .orElse(calculateRankWithoutBonus(hitCount));
+    }
+}
