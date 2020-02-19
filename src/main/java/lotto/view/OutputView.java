2640a0a626ef7e0cfb33ee79256a90d4ab45106a
LTTTTTE <10100aaaaa@gmail.com>
2020-02-19 15:08:59 +0900

    feat: 당첨 통계 출력하는 기능 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,39 @@
+package lotto.view;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import lotto.domain.Money;
+import lotto.domain.Rank;
+
+public class OutputView {
+	public static void showEarningRate(Money money, List<Rank> ranks) {
+		int sum = ranks.stream()
+			.mapToInt(Rank::getReward)
+			.sum();
+		System.out.printf("총 수익률은 %.2f %%입니다.", sum / money.getMoney() * 100.0);
+	}
+
+	public static void showStatistics(List<Rank> ranks) {
+		Map<Rank, Long> rankCounts = ranks.stream()
+			.collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+
+		for (Rank rank : Rank.values()) {
+			rankCounts.putIfAbsent(rank, 0L);
+		}
+
+		rankCounts.keySet().forEach(rank -> System.out.println(
+			rank.getHitCount() + "개 일치 " + printBonus(rank) + rank.getReward() +
+				"원 - " + rankCounts.get(rank) + "개"));
+	}
+
+	public static String printBonus(Rank rank) {
+		if (rank == Rank.SECOND) {
+			return "보너스 볼 일치 ";
+		}
+
+		return "";
+	}
+
+}
