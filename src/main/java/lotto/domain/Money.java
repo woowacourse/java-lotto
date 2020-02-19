c2a9385d47e20e84d7badbde4c3897c2662f0b6e
kouz <lastpuzly@gmail.com>
2020-02-19 10:49:46 +0900

    feat: 금액 객체 구현 및 유효성 검사

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public class Money {
+	private final int lottoMoneyValue;
+
+	public Money(final String lottoMoneyValue) {
+		checkValidationOf(lottoMoneyValue);
+		this.lottoMoneyValue = Integer.parseInt(lottoMoneyValue);
+	}
+
+	private void checkValidationOf(final String lottoMoneyValue) {
+		if (isNotNumber(lottoMoneyValue)) {
+			throw new IllegalArgumentException("금액은 숫자이어야 합니다.");
+		}
+		if (isEmpty(lottoMoneyValue)) {
+			throw new IllegalArgumentException("금액은 공백이 될 수 없습니다.");
+		}
+	}
+
+	private boolean isNotNumber(final String lottoMoneyValue) {
+		return lottoMoneyValue.chars()
+			.anyMatch(c -> !Character.isDigit(c));
+	}
+
+	private boolean isEmpty(final String lottoMoneyValue) {
+		return lottoMoneyValue.equals("");
+	}
+}
