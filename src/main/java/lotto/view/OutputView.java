2a0dfcfd414c72d8c459581e7be466a11b7231e0
lxxjn0 <ljy3510@gmail.com>
2020-02-19 17:02:03 +0900

    feat : 구매한 로또의 개수를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,13 @@
 package lotto.view;
 
 public class OutputView {
+	public static final String PURCHASE_COMPLETE_MESSAGE = "%d개를 구매했습니다.\n";
+
 	public static void printExceptionMessage(String exceptionMessage) {
 		System.out.println(exceptionMessage);
+	}
+	
+	public static void printPurchaseCompleteMessage(int numberOfLotto) {
+		System.out.printf(PURCHASE_COMPLETE_MESSAGE, numberOfLotto);
 	}
 }
