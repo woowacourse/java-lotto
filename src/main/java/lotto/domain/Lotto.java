bec721f12776dbec5d0b790d6107a7e9e1a16c7e
hj <a301dks@naver.com>
2020-02-19 11:53:22 +0900

    feat: Lotto에서 null이나 빈 값 예외처리 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,7 +6,18 @@ public class Lotto {
 	private final List<Integer> lottoNumber;
 
 	public Lotto(final List<Integer> lottoNumber) {
+		validate(lottoNumber);
 		this.lottoNumber = lottoNumber;
+	}
+
+	private void validate(List<Integer> lottoNumber) {
+		validateNullOrEmpty(lottoNumber);
+	}
+
+	private void validateNullOrEmpty(List<Integer> lottoNumber) {
+		if (lottoNumber == null || lottoNumber.isEmpty()) {
+			throw new IllegalArgumentException("null이나 빈 값은 들어올 수 없습니다!");
+		}
 	}
 
 	public boolean contains(int value) {
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,9 +3,11 @@ package lotto.domain;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class LottoTest {
 	@ParameterizedTest
@@ -13,5 +15,13 @@ public class LottoTest {
 	void 로또_생성(int value, boolean expected) {
 		Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
 		assertThat(lotto.contains(value)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@NullAndEmptySource
+	void 로또가_null이나_빈값인_경우(List<Integer> value) {
+		assertThatThrownBy(() -> new Lotto(value))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("null이나 빈 값");
 	}
 }
