44404dcf8253cce1a659fe7312888717e34c2aaa
KimSeongGyu1 <KimSeongGyu1@github.com>
2020-02-19 15:55:55 +0900

    [feature] InputView 당첨번호와 보너스볼 입력 구현

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -1,16 +1,42 @@
+import domain.LottoNumber;
 import domain.Money;
 import view.InputView;
+
+import java.util.Set;
 
 public class LottoApplication {
 
     public static void main(String[] args) {
         Money money = enterMoney();
+        Set<LottoNumber> lastWeekWinningNumbers = enterLastWeekWinningNumber();
+        LottoNumber bonusNumber = enterBonusNumber();
+
     }
 
     private static Money enterMoney() {
         while (true) {
             try {
                 return InputView.enterMoney();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static Set<LottoNumber> enterLastWeekWinningNumber() {
+        while (true) {
+            try {
+                return InputView.enterLastWeekWinningNumbers();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static LottoNumber enterBonusNumber() {
+        while (true) {
+            try {
+                return InputView.enterBonusNumber();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,10 @@
 package view;
 
+import domain.LottoNumber;
 import domain.Money;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String NUMBER_REGEX = "^[0-9]+$";
@@ -11,11 +13,11 @@ public class InputView {
 
     public static Money enterMoney() {
         System.out.println("구입 금액을 입력해주세요.");
-        int money = parseMoney(scanner.nextLine());
+        int money = parseInt(scanner.nextLine());
         return new Money(money);
     }
 
-    private static int parseMoney(String input) {
+    private static int parseInt(String input) {
         validateNumber(input);
         return Integer.parseInt(input);
     }
@@ -24,5 +26,28 @@ public class InputView {
         if (!input.matches(NUMBER_REGEX)) {
             throw new NumberFormatException("숫자를 입력해주세요.");
         }
+    }
+
+    public static Set<LottoNumber> enterLastWeekWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        Set<LottoNumber> lastWeekWinningNumbers = parseWinningNumbers(scanner.nextLine());
+        return lastWeekWinningNumbers;
+    }
+
+    private static Set<LottoNumber> parseWinningNumbers(String input) {
+        List<LottoNumber> lottoNumbers = Arrays.asList(input.split(",")).stream()
+                .map(InputView::parseLottoNumber)
+                .collect(Collectors.toList());
+        return new HashSet<>(lottoNumbers);
+    }
+
+    public static LottoNumber enterBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return parseLottoNumber(scanner.nextLine());
+    }
+
+    private static LottoNumber parseLottoNumber(String input) {
+        int parsedInt = parseInt(input);
+        return LottoNumber.getLottoNumber(parsedInt);
     }
 }
