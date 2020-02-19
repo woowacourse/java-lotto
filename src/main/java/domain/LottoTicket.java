86647f5caadd8cd7250270b2aa522314c6a694cc
giantim <giantim132@gmail.com>
2020-02-19 11:23:12 +0900

    feat : LottoTicket 클래스 추가 / LottoTicket 생성자 테스트 코드 추가

---
+++ src/main/java/domain/LottoTicket.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoTicket {
+    private static final int MAX_LOTTO_TICKET_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER_RANGE = 1;
+    private static final int MAX_LOTTO_NUMBER_RANGE = 45;
+
+    List<Integer> lottoTicket;
+
+    public LottoTicket(List<Integer> lottoTicket) {
+        validateLottoTicketSize(lottoTicket);
+        validateLottoTicketNumberRange(lottoTicket);
+        validateDuplicateLottoNumber(lottoTicket);
+        this.lottoTicket = lottoTicket;
+    }
+
+    private void validateDuplicateLottoNumber(List<Integer> lottoTicket) {
+        Set<Integer> duplicateLottoTicket = new HashSet<>(lottoTicket);
+        if (duplicateLottoTicket.size() != lottoTicket.size()) {
+            throw new IllegalArgumentException("중복된 로또 숫자를 입력하였습니다.");
+        }
+    }
+
+    private void validateLottoTicketNumberRange(List<Integer> lottoTicket) {
+        if (lottoTicket.stream().anyMatch(this::isLottoNumberRange)) {
+            throw new IllegalArgumentException("범위를 벗어나는 로또 숫자를 입력하였습니다.");
+        }
+    }
+
+    private boolean isLottoNumberRange(int number) {
+        return number < MIN_LOTTO_NUMBER_RANGE || number > MAX_LOTTO_NUMBER_RANGE;
+    }
+
+    private void validateLottoTicketSize(List<Integer> lottoTicket) {
+        if (lottoTicket.size() != MAX_LOTTO_TICKET_SIZE) {
+            throw new IllegalArgumentException("로또 숫자의 개수가 잘못되었습니다.");
+        }
+    }
+}
---
+++ src/test/java/domain/LottoTicketTest.java
@@ -0,0 +1,32 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LottoTicketTest {
+
+    private static Stream<List<Integer>> lottoTicketSetUp() {
+        return Stream.of(
+                new ArrayList(Arrays.asList(1, 2, 3, 4, 5)),
+                new ArrayList(Arrays.asList(1, 2, 2, 3, 4, 5)),
+                new ArrayList(Arrays.asList(0, 1, 2, 3, 4, 46)),
+                new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 6, 7))
+        );
+    }
+
+    @DisplayName("LottoTicket 생성자 테스트")
+    @ParameterizedTest
+    @MethodSource("lottoTicketSetUp")
+    public void lottoTicketConstructorTest(List<Integer> input) {
+        Assertions.assertThatThrownBy(() -> {
+            new LottoTicket(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
