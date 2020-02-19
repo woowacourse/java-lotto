84257f7ae23345333a82ab1e5d876b4cadfb1449
hj <a301dks@naver.com>
2020-02-19 17:50:35 +0900

    feat: 로또 구매 개수 출력 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,13 +2,16 @@ package lotto.controller;
 
 import java.io.IOException;
 
+import lotto.dto.LottoCountDto;
 import lotto.utils.InputUtil;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
 	public static void run() {
+		LottoCountDto lottoCountDto = new LottoCountDto(readMoney());
 		// LottoGame lottoGame = new LottoGame(new LottoCountDto(readMoney()).getLottoCount());
+		OutputView.printLottoCount(lottoCountDto.getLottoCount());
 		//
 		// WinningNumber wn = new WinningNumber(readWinningNumber());
 	}
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
 	public static void printWrongMoneyInput() {
 		System.out.println("금액이 정수가 아닙니다. 다시 입력해주세요.");
 	}
+
+	public static void printLottoCount(int lottoCount) {
+		System.out.println(lottoCount + "개를 구매했습니다.");
+	}
 }
