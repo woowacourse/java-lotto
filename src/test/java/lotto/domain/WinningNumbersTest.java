d58b22fc66171a95af4afe553d845e1952ad2b6c
Tak Hyungmin <milkho11@naver.com>
2020-02-19 20:48:41 +0900

    [refact] WinningNumbersTest 타입 리팩토링
    LottoNumber의 타입을 변경하면서 발생하는 에러를 리팩토링하였다.

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -10,23 +10,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WinningNumbersTest {
-    List<Integer> inputNumbers;
+    List<LottoNumber> inputNumbers;
 
     @BeforeEach
     void initiate() {
         inputNumbers = new ArrayList<>();
-        inputNumbers.add(1);
-        inputNumbers.add(2);
-        inputNumbers.add(3);
-        inputNumbers.add(4);
-        inputNumbers.add(5);
-        inputNumbers.add(6);
+        inputNumbers.add(LottoNumber.of(1));
+        inputNumbers.add(LottoNumber.of(2));
+        inputNumbers.add(LottoNumber.of(3));
+        inputNumbers.add(LottoNumber.of(4));
+        inputNumbers.add(LottoNumber.of(5));
+        inputNumbers.add(LottoNumber.of(6));
     }
 
     @Test
     @SuppressWarnings("NonAsciiCharacters")
     void 정상_입력_생성자_테스트() {
-        Assertions.assertThat(new WinningNumbers(inputNumbers, 7))
+        Assertions.assertThat(new WinningNumbers(inputNumbers, LottoNumber.of(7)))
                 .isInstanceOf(WinningNumbers.class);
     }
 
@@ -34,14 +34,14 @@ public class WinningNumbersTest {
     @SuppressWarnings("NonAsciiCharacters")
     @ValueSource(ints = {0, 46})
     void 로또_숫자_범위_외의_값이_들어온_경우(int bonusBall) {
-        Assertions.assertThatThrownBy(() -> new WinningNumbers(inputNumbers, bonusBall))
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(inputNumbers, LottoNumber.of(bonusBall)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @SuppressWarnings("NonAsciiCharacters")
     void 중복된_숫자가_들어온_경우() {
-        Assertions.assertThatThrownBy(() -> new WinningNumbers(inputNumbers, 1))
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(inputNumbers, LottoNumber.of(1)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
