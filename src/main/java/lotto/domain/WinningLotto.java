3f9a7e72e6e9c1620c2dc19b6a1409f8549e476d
YerinCho <forjustice9839@gmail.com>
2020-02-19 13:12:25 +0900

    [Lotto] feat : WinningLotto 보너스볼 중복검사
     생성자 수정
     Lotto.lottoNumbers 접근제어자 변경

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -6,11 +6,20 @@ public class WinningLotto extends Lotto {
 
     private int bonusNumber;
 
-    public WinningLotto(List<Integer> numbers) {
+    public WinningLotto(List<Integer> numbers, int bonusNumber) {
         super(numbers);
+        validateBonusNumberScope(bonusNumber);
+        validateBonusNumberDuplication(bonusNumber);
+        this.bonusNumber = bonusNumber;
     }
 
     public static void validateBonusNumberScope(int bonusNumber) {
         validateNumberScope(bonusNumber);
+    }
+
+    public void validateBonusNumberDuplication(int bonusNumber) {
+        if (lottoNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningLottoTest {
@@ -13,6 +15,13 @@ public class WinningLottoTest {
         assertThatThrownBy(() -> WinningLotto.validateBonusNumberScope(0))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> WinningLotto.validateBonusNumberScope(46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("보너스볼 중복 확인")
+    void validateBonusNumberDuplicationTest() {
+        assertThatThrownBy(() -> new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
