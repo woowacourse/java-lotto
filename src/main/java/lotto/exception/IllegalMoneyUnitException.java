3293674f4f335b373128026d543f635a764ba40f
KIMSIYOUNG <tldud2404@gmail.com>
2020-02-19 19:13:07 +0900

    copy branch

--- src/main/java/lotto/exception/IllegalMoneyUnitException.java
+++ src/main/java/lotto/exception/IllegalMoneyUnitException.java
@@ -1,10 +1,6 @@
 package lotto.exception;
 
 public class IllegalMoneyUnitException extends RuntimeException{
-
-	public IllegalMoneyUnitException() {
-	}
-
 	public IllegalMoneyUnitException(String message) {
 		super(message);
 	}
