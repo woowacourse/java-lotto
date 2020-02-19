9f652db0429d293df92bf1ab27114e2724a60471
kouz <lastpuzly@gmail.com>
2020-02-19 15:42:13 +0900

    feat: 당첨 번호가 정수가 아닌 값이 입력될 때 유효성 검사

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -2,11 +2,13 @@ package lotto.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class WinningNumbers {
 	public static final String DELIMITER = ", ";
 	public static final int WINNING_NUMBER_SIZE = 6;
+	public static final int ZERO = 0;
 
 	private final List<LottoNumber> winningNumbers;
 
@@ -24,6 +26,19 @@ public class WinningNumbers {
 		if (isNotMatchSize(winningNumbersValue)) {
 			throw new IllegalArgumentException("당첨 번호는 보너스 번호를 제외하고 6자리 이어야 합니다");
 		}
+		if (isNotNumberFormat(winningNumbersValue)) {
+			throw new IllegalArgumentException("당첨 번호는 정수만 가능합니다");
+		}
+	}
+
+	private boolean isNotNumberFormat(List<String> winningNumbersValue) {
+		return winningNumbersValue.stream()
+			.anyMatch(isNotDigit());
+	}
+
+	private Predicate<String> isNotDigit() {
+		return str -> str.chars()
+				.anyMatch(ch -> !Character.isDigit(ch));
 	}
 
 	private boolean isNotMatchSize(List<String> winningNumbersValue) {
--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -6,18 +6,23 @@ import org.junit.jupiter.api.Test;
 
 public class WinningNumbersTest {
 	@Test
-	void checkValidatorWhenOverNumbersSize() {
+	void checkValidationWhenOverNumbersSize() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-			new WinningNumbers("1,2,3,4,5,6,7")
+			new WinningNumbers("1, 2, 3, 4, 5, 6, 7")
 		).withMessage("당첨 번호는 보너스 번호를 제외하고 6자리 이어야 합니다");
 	}
 
 	@Test
-	void checkValidatorWhenUnderNumbersSize() {
+	void checkValidationWhenUnderNumbersSize() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-			new WinningNumbers("1,2,3,4,5")
+			new WinningNumbers("1, 2, 3, 4, 5")
 		).withMessage("당첨 번호는 보너스 번호를 제외하고 6자리 이어야 합니다");
 	}
 
-
+	@Test
+	void checkValidationWhenNotNumber() {
+		assertThatIllegalArgumentException().isThrownBy(() ->
+			new WinningNumbers("w, r, o, o, n, g")
+		).withMessage("당첨 번호는 정수만 가능합니다");
+	}
 }
