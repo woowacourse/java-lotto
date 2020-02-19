e396ad9c814f5f6b86cfa34a3b94dda8e4ed820c
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-02-19 15:35:28 +0900

    [fix] WinningNumbers의 생성 중 중복 체크 중 추가되는 버그 수정

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,24 +1,26 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class WinningNumbers {
     private static final int WINNING_NUMBER_SIZE = 7;
-    private final List<LottoNumberGroup> winningNumbers;
+    private final LottoNumbers winningNumbers;
     private final LottoNumberGroup bonusBall;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusBall) {
         validateDuplicate(winningNumbers, bonusBall);
-        this.winningNumbers = winningNumbers.stream()
+        this.winningNumbers = new LottoNumbers(winningNumbers.stream()
                 .map(LottoNumberGroup::parseLottoNumberGroup)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
         this.bonusBall = LottoNumberGroup.parseLottoNumberGroup(bonusBall);
     }
 
     private void validateDuplicate(List<Integer> winningNumbers, int bonusBall) {
-        winningNumbers.add(bonusBall);
-        boolean isDuplicate = winningNumbers.stream()
+        List<Integer> tempWinningNumbers = new ArrayList<>(winningNumbers);
+        tempWinningNumbers.add(bonusBall);
+        boolean isDuplicate = tempWinningNumbers.stream()
                 .distinct()
                 .count() != WINNING_NUMBER_SIZE;
         if (isDuplicate) {
