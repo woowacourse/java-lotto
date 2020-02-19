dede280a31b9720812829a55f9e0697f21fcb52e
LTTTTTE <10100aaaaa@gmail.com>
2020-02-19 16:24:01 +0900

    feat: 사용자로부터 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,17 @@ public class InputView {
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static String inputAsMoney() {
+		System.out.println("구입금액을 입력해 주세요.");
+		return scanner.nextLine();
+	}
+
+	public static String inputAsWinningNumbers() {
+		System.out.println("지난 주 당첨번호를 입력해 주세요.");
+		return scanner.nextLine();
+	}
+
+	public static String inputAsBonusNumber() {
+		System.out.println("보너스 번호를 입력해 주세요.");
 		return scanner.nextLine();
 	}
 }
