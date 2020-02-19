cf6480bcc4eecad6793bd2e6530645b4d623a4ae
sujin <sujinemilycho@gmail.com>
2020-02-19 18:17:23 +0900

    feat: 당첨 번호, 보너스 볼 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,9 +5,21 @@ import java.util.Scanner;
 public class InputView {
     private final static Scanner scanner = new Scanner(System.in);
     private static final String INPUT_MONEY_MESSAGE = "구입 금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
 
     public static int inputAmount() {
         System.out.println(INPUT_MONEY_MESSAGE);
+        return scanner.nextInt();
+    }
+
+    public static String inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return scanner.nextInt();
     }
 }
