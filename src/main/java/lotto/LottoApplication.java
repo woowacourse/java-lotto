368224ff58e273e0005d856bc8c7c88faeacc0dd
hwanghe159 <hwanghe159@gmail.com>
2020-02-19 17:32:41 +0900

    feat : 당첨 번호를 입력받아 검증하는 WinningNumbers 클래스 생성, 테스트코드 작성

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -2,19 +2,22 @@ package lotto;
 
 import lotto.domain.Lottos;
 import lotto.domain.PurchasePrice;
+import lotto.domain.WinningNumbers;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoApplication {
     public static void main(String[] args) {
         String purchasePriceInput = InputView.requestPurchasePriceInput();
-
         PurchasePrice purchasePrice = new PurchasePrice(purchasePriceInput);
-        int lottoCount = purchasePrice.calculateLottoCount();
 
+        int lottoCount = purchasePrice.calculateLottoCount();
         OutputView.printLottoCount(lottoCount);
+
         Lottos lottos = new Lottos(lottoCount);
         OutputView.printLottos(lottos);
 
+        String winningNumbersInput = InputView.requestWinningNumbersInput();
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumbersInput);
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,4 +11,14 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextLine();
     }
+
+    public static String requestWinningNumbersInput() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public static String requestBonusNumberInput() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return scanner.nextLine();
+    }
 }
