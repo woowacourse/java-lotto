8334e626039f88cf02172b8a1c414380afd52e1b
kouz <lastpuzly@gmail.com>
2020-02-19 16:10:18 +0900

    feat: 보너스 번호를 입력 받을 때 유효성 검사

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.function.Predicate;
@@ -11,21 +10,28 @@ import java.util.stream.Collectors;
 public class WinningNumbers {
 	public static final String DELIMITER = ", ";
 	public static final int WINNING_NUMBER_SIZE = 6;
-	public static final int ZERO = 0;
 
 	private final List<LottoNumber> winningNumbers;
+	private final LottoNumber bonusNumber;
 
-	public WinningNumbers(String winningNumbers) {
+	public WinningNumbers(String winningNumbers, String bonusNumber) {
 		List<String> winningNumbersValues = Arrays.asList(winningNumbers.split(DELIMITER));
-		checkValidationOf(winningNumbersValues);
+		checkValidationOf(winningNumbersValues, bonusNumber);
 
 		this.winningNumbers = winningNumbersValues.stream()
 			.mapToInt(Integer::parseInt)
 			.mapToObj(LottoNumber::new)
 			.collect(Collectors.toList());
+
+		this.bonusNumber = new LottoNumber(Integer.parseInt(bonusNumber));
 	}
 
-	private void checkValidationOf(List<String> winningNumbersValue) {
+	private void checkValidationOf(List<String> winningNumbersValue, String bonusNumber) {
+		checkValid(winningNumbersValue);
+		checkValid(winningNumbersValue, bonusNumber);
+	}
+
+	private void checkValid(List<String> winningNumbersValue) {
 		if (isNotMatchSize(winningNumbersValue)) {
 			throw new IllegalArgumentException("당첨 번호는 보너스 번호를 제외하고 6자리 이어야 합니다");
 		}
@@ -34,6 +40,15 @@ public class WinningNumbers {
 		}
 		if (isDuplicatedNumber(winningNumbersValue)) {
 			throw new IllegalArgumentException("중복된 번호는 허용하지 않습니다");
+		}
+	}
+
+	private void checkValid(List<String> winningNumbersValue, String bonusNumber) {
+		if (winningNumbersValue.contains(bonusNumber)) {
+			throw new IllegalArgumentException("보너스 번호 일반 당첨번호와 중복될수 없습니다.");
+		}
+		if (isNotDigit().test(bonusNumber)) {
+			throw new IllegalArgumentException("보너스 번호는 정수만 가능합니다");
 		}
 	}
 
--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -9,42 +9,54 @@ public class WinningNumbersTest {
 	@Test
 	void checkValidationWhenOverNumbersSize() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-			new WinningNumbers("1, 2, 3, 4, 5, 6, 7")
+			new WinningNumbers("1, 2, 3, 4, 5, 6, 7", "10")
 		).withMessage("당첨 번호는 보너스 번호를 제외하고 6자리 이어야 합니다");
 	}
 
 	@Test
 	void checkValidationWhenUnderNumbersSize() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-			new WinningNumbers("1, 2, 3, 4, 5")
+			new WinningNumbers("1, 2, 3, 4, 5", "11")
 		).withMessage("당첨 번호는 보너스 번호를 제외하고 6자리 이어야 합니다");
 	}
 
 	@Test
 	void checkValidationWhenNotNumber() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-			new WinningNumbers("w, r, o, o, n, g")
+			new WinningNumbers("w, r, o, o, n, g", "1")
 		).withMessage("당첨 번호는 정수만 가능합니다");
 	}
 
 	@Test
 	void checkValidationWhenDuplicateNumber() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-			new WinningNumbers("1, 1, 2, 3, 4, 5")
+			new WinningNumbers("1, 1, 2, 3, 4, 5", "13")
 		).withMessage("중복된 번호는 허용하지 않습니다");
 	}
 
 	@Test
 	void checkValidationWhenEachUnderRange() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-			new WinningNumbers("0, 1, 23, 34, 22, 3")
+			new WinningNumbers("0, 1, 23, 34, 22, 3", "4")
 		).withMessage("로또 숫자는 0이하 일 수 없습니다.");
 	}
 
 	@Test
 	void checkValidationWhenEachOverRange() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-			new WinningNumbers("46, 47123, 48234345, 78123123, 893, 90134234")
+			new WinningNumbers("46, 47123, 48234345, 78123123, 893, 90134234", "23")
 		).withMessage("로또 숫자는 45를 넘기면 안됩니다.");
+	}
+	@Test
+	void checkValidationWhenBonusNumberNotInteger() {
+		assertThatIllegalArgumentException().isThrownBy(() ->
+			new WinningNumbers("1, 2, 3, 4, 5, 6", "a")
+		).withMessage("보너스 번호는 정수만 가능합니다");
+	}
+	@Test
+	void checkValidationWhenBonusNumberDuplicateLottoNumbers() {
+		assertThatIllegalArgumentException().isThrownBy(() ->
+			new WinningNumbers("1, 2, 3, 4, 5, 6","2")
+		).withMessage("보너스 번호 일반 당첨번호와 중복될수 없습니다.");
 	}
 }
