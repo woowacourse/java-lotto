9e98375de86b7090e2fd917856508ff11a7749a8
LTTTTTE <10100aaaaa@gmail.com>
2020-02-19 16:47:53 +0900

    test: Lotto 객체 생성 테스트 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,16 +9,18 @@ public class Lotto {
 		if (isInvalidNumberRange(numbers)) {
 			throw new IllegalArgumentException("1~45 범위의 숫자만 로또 번호가 될 수 있습니다.");
 		}
-		if (wrongAmountOfNumbers(numbers)) {
+		if (wrongAmountOfNumbers(numbers) || hasDuplicatedNumbers(numbers)) {
 			throw new IllegalArgumentException("잘못된 로또 번호입니다. 중복 안됨, 갯수는 6개");
 		}
 		this.numbers = numbers;
 	}
 
+	private boolean hasDuplicatedNumbers(List<Integer> numbers) {
+		return 6 != numbers.stream().distinct().count();
+	}
+
 	private boolean wrongAmountOfNumbers(List<Integer> numbers) {
-		return 6 != numbers.stream()
-			.distinct()
-			.count();
+		return 6 != numbers.size();
 	}
 
 	private boolean isInvalidNumberRange(List<Integer> numbers) {
