d08f15572b05b63f246b05d6924889c9e147f861
Sony <sonypark0204@gmail.com>
2020-02-19 15:49:23 +0900

    feat: 임의로 1부터 45까지의 숫자를 6개 생성하여 리스트로 만든다

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,4 +1,18 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class NumberGenerator {
+    private static Random random = new Random();
+
+    static List<Integer> generate() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            int number = random.nextInt(44) + 1;
+            numbers.add(number);
+        }
+        return numbers;
+    }
 }
