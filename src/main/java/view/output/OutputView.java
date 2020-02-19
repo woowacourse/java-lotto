dbec0990e178882ce2615126995b475a5a3bda35
lowoon <sangbo0229@gmail.com>
2020-02-19 17:42:33 +0900

    feat: 게임 출력을 위한 OutputView 객체를 생성했습니다.
    	- LottoTickets를 출력하는 기능
    	- LottoResult를 출력하는 기능

---
+++ src/main/java/view/output/OutputView.java
@@ -0,0 +1,33 @@
+package view.output;
+
+import java.util.List;
+
+import domain.lotto.LottoTicket;
+import domain.lotto.LottoTickets;
+import domain.result.LottoResult;
+import domain.result.Rank;
+
+public class OutputView {
+    public static void printLottoTickets(LottoTickets lottoTickets) {
+        List<LottoTicket> lottoTicketList = lottoTickets.getLottoTickets();
+        System.out.println("로또를 구매했습니다.");
+
+        lottoTicketList.forEach(System.out::println);
+    }
+
+    public static void printLottoResult(LottoResult lottoResult) {
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.MISS) {
+                continue;
+            }
+            System.out.println(rankToString(rank) + lottoResult.count(rank) + "개");
+        }
+    }
+
+    private static String rankToString(Rank rank) {
+        if (rank == Rank.SECOND) {
+            return rank.getCountOfMatches() + "개 일치, 보너스볼 일 (" + rank.getWinningMoney() + "원) - ";
+        }
+        return rank.getCountOfMatches() + "개 일치 (" + rank.getWinningMoney() + "원) - ";
+    }
+}
