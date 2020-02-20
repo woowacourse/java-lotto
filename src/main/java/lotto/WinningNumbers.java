399005078b337373d7243c386418cd4ca9fe9245
moon <mjj920930@gmail.com>
2020-02-20 13:07:10 +0900

    feat: 당첨번호와 보너스번호 입력기능 추가

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,9 @@
+package lotto;
+
+import java.util.List;
+
+public class WinnigNumbers {
+    public WinnigNumbers(List<Integer> sixNumbers, Integer bonusNumber) {
+
+    }
+}
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,24 @@
 package view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-
     public static int inputPurchaseMoney() {
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<Integer> inputSixNumbers() {
+        return Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public static int inputBonusNumber() {
         return Integer.parseInt(scanner.nextLine());
     }
 }
