1f0c84604438002bdd6779017c43002c8fe7c1fd
YerinCho <forjustice9839@gmail.com>
2020-02-20 10:57:10 +0900

    [Lotto] refactor : Lotto.getLottoNumbers 불변성 보장

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
 
@@ -41,7 +42,7 @@ public class Lotto {
     }
 
     public List<Integer> getLottoNumbers() {
-        return lottoNumbers;
+        return Collections.unmodifiableList(lottoNumbers);
     }
 
 }
