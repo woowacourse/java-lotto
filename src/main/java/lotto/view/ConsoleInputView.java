65a8c78ccf1b42c225e7f0dc5b28f7d725ffcf60
lxxjn0 <ljy3510@gmail.com>
2020-02-20 13:59:36 +0900

    feat : 6개의 당첨 번호를 입력하는 기능 구현

--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class ConsoleInputView {
 	public static final String INPUT_MONEY_MESSAGE = "구입 금액을 입력해주세요.";
+	public static final String INPUT_WINNING_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static String inputMoney() {
 		System.out.println(INPUT_MONEY_MESSAGE);
+		return scanner.nextLine();
+	}
+
+	public static String inputWinningLottoNumber() {
+		System.out.println(INPUT_WINNING_LOTTO_NUMBER_MESSAGE);
 		return scanner.nextLine();
 	}
 }
