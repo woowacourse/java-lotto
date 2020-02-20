40c0c438bbd20da14d94b0972ad1208a73b93b1c
soring <jnsorn@gmail.com>
2020-02-20 22:11:31 +0900

    refactor : 사용하지 않는 클래스 정리

--- src/main/java/lotto/domain/InvalidWinningLottoException.java
+++
@@ -1,9 +0,0 @@
-package lotto.domain;
-
-public class InvalidWinningLottoException extends IllegalArgumentException {
-	public static final String DUPLICATED_BONUS_NUMBER = "당첨번호와 보너스번호가 중복되었습니다.";
-
-	public InvalidWinningLottoException(String s) {
-		super(s);
-	}
-}
