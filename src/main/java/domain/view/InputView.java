ed751177607d3e25d335792f073649515934f822
aegis1920 <aegis1920@gmail.com>
2020-02-20 15:01:43 +0900

    feat : InputView 클래스 추가

---
+++ src/main/java/domain/view/InputView.java
@@ -0,0 +1,23 @@
+package domain.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputBuyMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public static String inputWinningNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return scanner.nextLine();
+    }
+}
