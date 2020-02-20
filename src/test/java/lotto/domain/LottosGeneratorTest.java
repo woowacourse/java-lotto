ca32f27e93f56c4b3dd660aab444b68928086b07
Sony <sonypark0204@gmail.com>
2020-02-20 11:55:41 +0900

    refac: LottosGenerator의 generate() 인자값 변경
    Lotto 생성자 변경에 따라 LottosGenerator의 generate() 인자값을 변경함

--- src/test/java/lotto/domain/LottosGeneratorTest.java
+++ src/test/java/lotto/domain/LottosGeneratorTest.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -11,9 +13,10 @@ class LottosGeneratorTest {
     @Test
     void 로또_갯수만큼_로또_생성() {
         //given
-        int expectedLottosSize = 10;
+        int expectedLottosSize = 1;
         //when
-        List<Lotto> lottos = LottosGenerator.generate(expectedLottosSize);
+        List<Lotto> lottos = LottosGenerator.generate(expectedLottosSize,
+            new ArrayList<>(Arrays.asList(Arrays.asList(1, 2, 3, 4, 5, 6))));
         //then
         assertThat(lottos.size()).isEqualTo(expectedLottosSize);
     }
