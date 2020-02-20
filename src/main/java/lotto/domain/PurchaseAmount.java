8eda42cb00ac7a6ac47f20b0091c3218c4f80106
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-02-20 17:57:34 +0900

    [refactor] 불필요한 메서드 삭제

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+public class PurchaseAmount {
+    private static final int MINIMUM_MONEY = 1000;
+    private static final int MINIMUM_AMOUNT = 1;
+    private static final int LOTTO_PRICE = 1000;
+    private static final String INVALID_PURCHASE_MONEY_EXCEPTION_MESSAGE = "Input money out of range (minimum - 1000).";
+
+    private final int purchaseAmount;
+
+    private PurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public static PurchaseAmount calculate(int purchaseMoney) {
+        validatePurchaseMoney(purchaseMoney);
+        return new PurchaseAmount(purchaseMoney / LOTTO_PRICE);
+    }
+
+    private static void validatePurchaseMoney(int purchaseMoney) {
+        if (purchaseMoney < MINIMUM_MONEY) {
+            throw new IllegalArgumentException(INVALID_PURCHASE_MONEY_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public int getPurchaseNumber() {
+        return purchaseAmount;
+    }
+}
--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,27 +1,25 @@
-package domain;
+package lotto.domain;
 
-import lotto.domain.PurchaseNumber;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class PurchaseNumbersTest {
+public class PurchaseAmountTest {
 
     @Test
     @SuppressWarnings("NonAsciiCharacters")
     void calculate_테스트() {
-        Assertions.assertThat(PurchaseNumber.calculate(2000))
-                .isInstanceOf(PurchaseNumber.class);
+        Assertions.assertThat(PurchaseAmount.calculate(2000))
+                .isInstanceOf(PurchaseAmount.class);
     }
 
     @ParameterizedTest
     @SuppressWarnings("NonAsciiCharacters")
     @ValueSource(ints = {999, 0})
     void 최소_구매_금액보다_작은_입력이_들어온_경우_calculate_테스트(int value) {
-        Assertions.assertThatThrownBy(() -> PurchaseNumber.calculate(value))
-                .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> PurchaseAmount.calculate(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Input money out of range (minimum - 1000).");
     }
-
-
 }
