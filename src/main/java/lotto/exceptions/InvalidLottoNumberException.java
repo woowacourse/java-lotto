96df44fe7c840d47d2d11e9400103d6456126569
Yeji Ahn <yeppilisa@naver.com>
2020-02-20 14:29:05 +0900

    feature : 입력 예외 처리 구현

--- src/main/java/lotto/exceptions/InvalidLottoNumberException.java
+++ src/main/java/lotto/exceptions/InvalidLottoNumberException.java
@@ -1,7 +1,7 @@
 package lotto.exceptions;
 
 public class InvalidLottoNumberException extends RuntimeException {
-    public InvalidLottoNumberException() {
-        super("잘못된 로또 번호를 입력하셨습니다.");
+    public InvalidLottoNumberException(String message) {
+        super(message);
     }
 }
--- src/main/java/lotto/exceptions/InvalidLottoTicketException.java
+++ src/main/java/lotto/exceptions/InvalidLottoTicketException.java
@@ -1,12 +1,7 @@
 package lotto.exceptions;
 
 public class InvalidLottoTicketException extends RuntimeException {
-
     public InvalidLottoTicketException(String message) {
         super(message);
-    }
-
-    public InvalidLottoTicketException() {
-        super("유효하지 않은 당첨 번호 값입니다.");
     }
 }
