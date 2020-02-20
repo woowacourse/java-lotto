0d03a59098368f6bef74c51207170f4686a967a1
kouz <lastpuzly@gmail.com>
2020-02-20 17:50:23 +0900

    refactor: 가독성 개선을 위한 메서드 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,7 +8,7 @@ import lotto.domain.Rank;
 import lotto.domain.Ranks;
 
 public class OutputView {
-	public static final int PERCENT = 100;
+	public static final int MULTIPLIER_FOR_PERCENT = 100;
 
 	public static void printResult(final Ranks ranks) {
 		Rank.getOrderReversed().stream()
@@ -22,25 +22,30 @@ public class OutputView {
 
 	private static void printEachResult(final Rank rank, final Ranks ranks) {
 		if (rank.equals(Rank.SECOND)) {
-			System.out.printf("%d개 일치, 보너스 볼 일치 (%d원)- %d개",
-				rank.getMatchCount(), rank.getAmount(), ranks.getCountOf(rank));
-			emptyLine();
+			printRank("%d개 일치, 보너스 볼 일치 (%d원)- %d개", rank, ranks);
 			return;
 		}
-		System.out.printf("%d개 일치 (%d원)- %d개", rank.getMatchCount(), rank.getAmount(), ranks.getCountOf(rank));
+		printRank("%d개 일치 (%d원)- %d개", rank, ranks);
+	}
+
+	private static void printRank(final String message, final Rank rank, final Ranks ranks) {
+		System.out.printf(message, rank.getMatchCount(), rank.getAmount(), ranks.getCountOf(rank));
+		emptyLine();
+	}
+
+	public static void printLottoAmount(final LottoTickets lottoTickets) {
+		System.out.printf("%d개를 구매했습니다.", lottoTickets.stream().count());
 		emptyLine();
 	}
 
 	public static void printLottoState(final LottoTickets lottoTickets) {
-		System.out.printf("%d개를 구매했습니다.", lottoTickets.stream().count());
-		emptyLine();
 		lottoTickets.stream()
 			.map(LottoTicket::toString)
 			.forEach(System.out::println);
 	}
 
 	public static void printProfit(double calculate) {
-		System.out.printf("총 수익률은 %.1f%%입니다.", calculate * PERCENT);
+		System.out.printf("총 수익률은 %.1f%%입니다.", calculate * MULTIPLIER_FOR_PERCENT);
 	}
 
 	private static void emptyLine() {
