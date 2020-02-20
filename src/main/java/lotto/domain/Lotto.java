58b8bab4643d6f524d6d02a635e9c7f3fd891b41
Sony <sonypark0204@gmail.com>
2020-02-20 11:46:59 +0900

    feat: 생성된 로또와 지난 주 당첨번호를 비교한다

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,24 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lotto {
     private final int PRICE = 1_000;
+    private List<Integer> lottoNumbers;
+
+    Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
 
     int convertMoneyToLottosSize(int money) {
         return money / PRICE;
+    }
+
+    int match(List<Integer> winningNumbers, int bonusBall) {
+        // 질문: List구현체가 ArrayList일 경우, parallel을 쓰면 속도가 빨라진다고 해서 써 봤는데요. 혹시 잘못 쓴 것은 아닌지, parallel을 언제 쓰면 좋은 지 궁금합니다.
+        return (int)lottoNumbers.stream()
+            .parallel()
+            .filter(lottoNumber -> winningNumbers.contains(lottoNumber))
+            .count();
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,6 +2,11 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
@@ -9,11 +14,24 @@ class LottoTest {
     @Test
     void 구매_금액을_생성할_로또들_개수로_변환() {
         //given
-        Lotto lotto = new Lotto();
+        Lotto lotto = new Lotto(new ArrayList<>());
         int money = 14000;
         //when
         int lottosSize = lotto.convertMoneyToLottosSize(money);
         //then
         assertThat(lottosSize).isEqualTo(14);
+    }
+
+    @Test
+    void 당첨숫자와_일치하는_로또번호_갯수_반환() {
+        //given
+        List<Integer> lottoNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 40));
+        int bonusBall = 45;
+        Lotto lotto = new Lotto(lottoNumbers);
+        //when
+        int matchSize = lotto.match(winningNumbers, bonusBall);
+        //then
+        assertThat(matchSize).isEqualTo(5);
     }
 }
