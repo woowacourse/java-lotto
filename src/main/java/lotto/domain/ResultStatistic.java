e733d0bc09252f3c53ff1927de253a1bca2bf67a
toneyparky <qkrtmddhks95@gmail.com>
2020-02-20 15:48:48 +0900

    feat: ResultStatistic 클래스 구현

---
+++ src/main/java/lotto/domain/ResultStatistic.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 클래스 이름 : .java
+ *
+ * @author
+ * @version 1.0
+ * <p>
+ * 날짜 : 2020/02/20
+ */
+public class ResultStatistic {
+	private static final int ONE = 1;
+	private static final int DEFAULT_COUNT = 0;
+
+	private Map<Rank, Integer> results;
+
+	private ResultStatistic(Map<Rank, Integer> input) {
+		results = input;
+	}
+
+	public static ResultStatistic calculate(Lottos lottos, WinningLotto winningLotto, BonusLottoNumber bonus){
+		Map<Rank, Integer> results = createInitialResult();
+
+		for (Lotto lotto : lottos.getLottos()) {
+			PaidLotto paidLotto = (PaidLotto) lotto;
+			Rank rank = paidLotto.getRank(winningLotto, bonus);
+			int count = results.get(rank) + ONE;
+			results.put(rank, count);
+		}
+
+		return new ResultStatistic(results);
+	}
+
+	private static Map<Rank, Integer> createInitialResult() {
+		Map<Rank, Integer> initialResult = new HashMap<>();
+
+		for (Rank rank : Rank.values()) {
+			initialResult.put(rank, DEFAULT_COUNT);
+		}
+
+		return initialResult;
+	}
+
+	public Map<Rank, Integer> getResults() {
+		return this.results;
+	}
+}
---
+++ src/test/java/lotto/domain/ResultStatisticTest.java
@@ -0,0 +1,93 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 클래스 이름 : .java
+ *
+ * @author
+ * @version 1.0
+ * <p>
+ * 날짜 : 2020/02/20
+ */
+public class ResultStatisticTest {
+	private Lottos lottos;
+	private WinningLotto winningLotto;
+	private BonusLottoNumber bonusLottoNumber;
+
+	@BeforeEach
+	void setUp() {
+		List<Lotto> tempLottos = new ArrayList<>();
+
+		List<LottoNumber> winningLottoNumbers = Arrays.asList(
+				LottoNumber.ONE,
+				LottoNumber.TWO,
+				LottoNumber.THREE,
+				LottoNumber.FOUR,
+				LottoNumber.FIVE,
+				LottoNumber.SIX
+		);
+
+		winningLotto = (WinningLotto) LottoFactory.getLottoManual(LottoType.WINNING_LOTTO, winningLottoNumbers);
+
+		List<LottoNumber> lottoNumbersFistPrize = Arrays.asList(
+				LottoNumber.ONE,
+				LottoNumber.TWO,
+				LottoNumber.THREE,
+				LottoNumber.FOUR,
+				LottoNumber.FIVE,
+				LottoNumber.SIX
+		);
+		List<LottoNumber> lottoNumbersForthPrize = Arrays.asList(
+				LottoNumber.ONE,
+				LottoNumber.TWO,
+				LottoNumber.THREE,
+				LottoNumber.FOUR,
+				LottoNumber.EIGHT,
+				LottoNumber.NINE
+		);
+
+		List<LottoNumber> lottoNumbersSixthPrize = Arrays.asList(
+				LottoNumber.TEN,
+				LottoNumber.ELEVEN,
+				LottoNumber.TWELVE,
+				LottoNumber.THIRTEEN,
+				LottoNumber.EIGHT,
+				LottoNumber.NINE
+		);
+
+		tempLottos.add(LottoFactory.getLottoManual(LottoType.PAID_LOTTO, lottoNumbersFistPrize));
+		tempLottos.add(LottoFactory.getLottoManual(LottoType.PAID_LOTTO, lottoNumbersForthPrize));
+		tempLottos.add(LottoFactory.getLottoManual(LottoType.PAID_LOTTO, lottoNumbersSixthPrize));
+		lottos = new Lottos(tempLottos);
+
+		bonusLottoNumber = new BonusLottoNumber(7, winningLotto);
+	}
+
+	@Test
+	void calculate_ResultStatistic_생성_확인() {
+		ResultStatistic resultStatistic = ResultStatistic.calculate(lottos, winningLotto, bonusLottoNumber);
+		assertThat(resultStatistic).isInstanceOf(ResultStatistic.class);
+	}
+
+	@Test
+	void calculate_올바른_리턴_결과_확인() {
+		ResultStatistic resultStatistic = ResultStatistic.calculate(lottos, winningLotto, bonusLottoNumber);
+		Map<Rank, Integer> resultMap = resultStatistic.getResults();
+
+		assertThat(resultMap.get(Rank.FIRST)).isEqualTo(1);
+		assertThat(resultMap.get(Rank.SECOND)).isEqualTo(0);
+		assertThat(resultMap.get(Rank.THIRD)).isEqualTo(0);
+		assertThat(resultMap.get(Rank.FOURTH)).isEqualTo(1);
+		assertThat(resultMap.get(Rank.FIFTH)).isEqualTo(0);
+		assertThat(resultMap.get(Rank.SIXTH)).isEqualTo(1);
+	}
+}
