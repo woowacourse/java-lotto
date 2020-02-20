999b7b9bd7cd6ae072ad6963ce6adf29b5f8024c
Eun Seok <jeseok95@gmail.com>
2020-02-20 14:07:22 +0900

    [test] 원하는 로또값이 나오는지 확인 테스트

--- src/test/java/lotto/domain/LottoBallFactoryTest.java
+++ src/test/java/lotto/domain/LottoBallFactoryTest.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LottoBallFactoryTest {
     @Test
-    void 로또값_생성_확인() {
-        System.out.println(LottoBallFactory.getInstance());
+    void 원하는_로또값_생성_확인() {
+        int lottoBallNumber = 1;
+        Assertions.assertThat(LottoBallFactory.findByLottoBall(1).getLottoNumber()).isEqualTo(1);
     }
 }
--- src/test/java/lotto/domain/LottoBallTest.java
+++ src/test/java/lotto/domain/LottoBallTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.Exception.NumberOutOfRangeException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,6 @@ public class LottoBallTest {
     @Test
     void 로또볼_값_올바른범위안에_없을경우_검증() {
         int lottoBall = -1;
-        Assertions.assertThatThrownBy(()->new LottoBall(lottoBall)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(()->new LottoBall(lottoBall)).isInstanceOf(NumberOutOfRangeException.class);
     }
 }
