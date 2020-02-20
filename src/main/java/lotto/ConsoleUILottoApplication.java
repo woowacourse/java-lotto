98344e56f4a4b7264970cf6348f85705caf47337
lxxjn0 <ljy3510@gmail.com>
2020-02-20 14:27:20 +0900

    refactor : WinningLotto 객체 생성 및 예외처리 부분을 메서드 분리

--- src/main/java/lotto/ConsoleUILottoApplication.java
+++ src/main/java/lotto/ConsoleUILottoApplication.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import lotto.controller.LottoController;
 import lotto.domain.InvalidLottoMoneyException;
+import lotto.domain.InvalidWinningLottoException;
 import lotto.domain.Lotto;
 import lotto.domain.LottoMoney;
+import lotto.domain.LottoNumber;
+import lotto.domain.WinningLotto;
+import lotto.domain.WinningLottoParser;
 import lotto.view.ConsoleInputView;
 import lotto.view.ConsoleOutputView;
 
@@ -19,8 +23,20 @@ public class ConsoleUILottoApplication {
 		List<Lotto> lottos = lottoController.start(numberOfLotto);
 		ConsoleOutputView.printPurchasedLotto(lottos);
 
-		String inputWinningLottoNumber = ConsoleInputView.inputWinningLottoNumber();
-		String inputBonusLottoNumber = ConsoleInputView.inputBonusLottoNumber();
+		WinningLotto winningLotto = receiveWinningLotto();
+
+	}
+
+	private static WinningLotto receiveWinningLotto() {
+		try {
+			String inputWinningLottoNumber = ConsoleInputView.inputWinningLottoNumber();
+			String inputBonusLottoNumber = ConsoleInputView.inputBonusLottoNumber();
+			return new WinningLotto(new Lotto(WinningLottoParser.parser(inputWinningLottoNumber)),
+				LottoNumber.valueOf(inputBonusLottoNumber));
+		} catch (InvalidWinningLottoException iwle) {
+			ConsoleOutputView.printExceptionMessage(iwle.getMessage());
+			return receiveWinningLotto();
+		}
 	}
 
 	private static LottoMoney receiveInputMoney() {
