cfe832a9ba5e1c3167c1bc908a32f1df889f6564
김효찬 <kom1230@hanyang.ac.kr>
2020-02-21 00:03:06 +0900

    [fix] 클래스 테스트 메소드 동시 테스트시 exception 뜨는 것 해결

--- src/test/java/lotto/domain/ResultCalculatorTest.java
+++ src/test/java/lotto/domain/ResultCalculatorTest.java
@@ -1,16 +1,24 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResultCalculatorTest {
+    static int count = 0;
+
+    @BeforeEach
+    void initiate() {
+        count++;
+    }
+
     @Test
     @DisplayName("순위 계산 테스트")
     void calculateRank() {
-        ResultCalculator.calculateRank(6, false);
-        assertThat(Rank.FIRST_RANK.count)
+        ResultCalculator.calculateRank(3, false);
+        assertThat(Rank.FIFTH_RANK.count)
                 .isEqualTo(1);
     }
 
@@ -18,7 +26,7 @@ public class ResultCalculatorTest {
     @DisplayName("수익률 계산 테스트")
     void calculateEarningRate() {
         ResultCalculator.calculateRank(3, false);
-        PurchaseAmount purchaseAmount = PurchaseAmount.calculate(1000);
+        PurchaseAmount purchaseAmount = PurchaseAmount.calculate(count * 1000);
         assertThat(ResultCalculator.calculateEarningRate(purchaseAmount)).isEqualTo(400);
     }
 }
