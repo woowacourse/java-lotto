d12ec3d6bd5bdf7b69c131e2c57c708b0a400711
YerinCho <forjustice9839@gmail.com>
2020-02-20 13:43:19 +0900

    [Lotto] fix : 입력 버퍼 오류 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,27 +7,27 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static int inputMoney() {
+        System.out.println("구입금액을 입력해주세요.");
         try {
-            System.out.println("구입금액을 입력해주세요.");
-            return scanner.nextInt();
-        } catch (InputMismatchException e) {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
             System.out.println("숫자를 입력해주세요.");
-            scanner.nextLine();
             return inputMoney();
         }
     }
 
     public static String inputWinningLottoNumbers() {
+        System.out.println("당첨번호를 입력해주세요.");
         return scanner.nextLine()
-                .trim();
+                .replace(" ", "");
     }
 
     public static int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해주세요.");
         try {
-            return scanner.nextInt();
+            return Integer.parseInt(scanner.nextLine());
         } catch (InputMismatchException e) {
             System.out.println("숫자를 입력해주세요.");
-            scanner.nextLine();
             return inputBonusNumber();
         }
     }
