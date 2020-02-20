6c36e85cbe56e19bd43f03e886c1a836d8bcd6a3
kouz <lastpuzly@gmail.com>
2020-02-20 15:24:50 +0900

    feat: LottoNumber 저장소 생성

--- src/main/java/lotto/domain/CreateRandomTicketsStrategy.java
+++ src/main/java/lotto/domain/CreateRandomTicketsStrategy.java
@@ -1,43 +1,22 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class CreateRandomTicketsStrategy implements CreateNumbersStrategy {
-	private static final int MAX_NUMBER = 45;
-	private static final int MIN_NUMBER = 1;
 	private static final int LOTTO_NUMBER_SIZE = 6;
-
-	private final List<LottoNumber> numberPool;
-
-	public CreateRandomTicketsStrategy() {
-		this.numberPool = createRandomNumberPool();
-	}
-
-	private List<LottoNumber> createRandomNumberPool() {
-		List<LottoNumber> numberPool = new ArrayList<>();
-		IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
-			.mapToObj(LottoNumber::new)
-			.forEach(numberPool::add);
-		return numberPool;
-	}
 
 	@Override
 	public LottoTickets create(PurchasingAmount purchasingAmount) {
 		List<LottoTicket> lottoTickets = new ArrayList<>();
-		purchasingAmount.forEachRemaining((count) -> {
-			Collections.shuffle(numberPool);
-			createLottoTicket(lottoTickets);
-		});
+		purchasingAmount.forEachRemaining(count -> createLottoTicket(lottoTickets));
 		return new LottoTickets(lottoTickets);
 	}
 
 	private void createLottoTicket(List<LottoTicket> lottoTickets) {
 		lottoTickets.add(new LottoTicket(
-			numberPool.stream()
+			LottoNumberRepository.shuffledLottoNumbers().stream()
 				.limit(LOTTO_NUMBER_SIZE)
 				.collect(Collectors.toList())
 		));
---
+++ src/main/java/lotto/domain/LottoNumberRepository.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoNumberRepository {
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 45;
+	private static final List<LottoNumber> lottoNumbers = new ArrayList<>();
+
+	static {
+		IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+			.mapToObj(LottoNumber::new)
+			.forEach(lottoNumbers::add);
+	}
+
+	public static List<LottoNumber> shuffledLottoNumbers() {
+		Collections.shuffle(lottoNumbers);
+		return Collections.unmodifiableList(lottoNumbers);
+	}
+}
