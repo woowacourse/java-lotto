39993ff1c91c5b63c453c3e55b50f015c0a9cb2f
moonyoungCHAE <xpf_fl@naver.com>
2020-02-20 11:26:53 +0900

    refactor: 클래스 이름 변경 및 random 숫자 생성 방법 수정

--- src/main/java/lotto/utils/LottoGenerator.java
+++ src/main/java/lotto/utils/LottoGenerator.java
@@ -2,17 +2,18 @@ package lotto.utils;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
-import lotto.domain.Payment;
+import lotto.domain.PaidPrice;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LottoFactory {
+public class LottoGenerator {
+    private static final int LOTTO_NUMBER_SIZE = 6;
     private static RandomGenerator randomGenerator = new RandomGenerator();
-    public static List<Lotto> createLotties(Payment payment) {
+    public static List<Lotto> createLotties(PaidPrice paidPrice) {
         List<Lotto> lotties = new ArrayList<>();
 
-        for (int i = 0; i < payment.getLottoCount(); i++) {
+        for (int i = 0; i < paidPrice.getLottoCount(); i++) {
             lotties.add(new Lotto(createLottoNumbers()));
         }
         return lotties;
@@ -21,7 +22,7 @@ public class LottoFactory {
     public static List<LottoNumber> createLottoNumbers() {
         List<Integer> randomNumbers = randomGenerator.getRandomNumbers();
         List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < LOTTO_NUMBER_SIZE; i++) {
             lottoNumbers.add(new LottoNumber(randomNumbers.get(i)));
         }
         return lottoNumbers;
--- src/main/java/lotto/utils/RandomGenerator.java
+++ src/main/java/lotto/utils/RandomGenerator.java
@@ -1,22 +1,29 @@
 package lotto.utils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class RandomGenerator {
+    private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int LOTTO_NUMBER_SIZE = 6;
     private Random random;
+    private List<Integer> totalRandomNumbers;
 
     public RandomGenerator() {
         this.random = new Random();
+        for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
+            this.totalRandomNumbers.add(i);
+        }
     }
 
     public List<Integer> getRandomNumbers(){
+        Collections.shuffle(this.totalRandomNumbers);
         List<Integer> randomNumbers = new ArrayList<>();
         for (int i = 0; i < LOTTO_NUMBER_SIZE; i++) {
-            randomNumbers.add(random.nextInt(MAX_LOTTO_NUMBER) + 1);
+            randomNumbers.add(totalRandomNumbers.get(i));
         }
         return randomNumbers;
     }
