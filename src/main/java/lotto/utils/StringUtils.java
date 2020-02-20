9a118e1000b3f7b749f7dd53ffe8510a52b7cd1e
JunSeong <ebseud6135@naver.com>
2020-02-20 14:36:32 +0900

    feat : StringUtils.java
     당첨번호에 숫자가 아닌 문자 입력시 예외처리

--- src/main/java/lotto/utils/StringUtils.java
+++ src/main/java/lotto/utils/StringUtils.java
@@ -5,15 +5,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import lotto.domain.LottoNo;
+import lotto.validator.Validator;
 
 public class StringUtils {
 	private static String COMMA = ",";
 
 	public static List<LottoNo> split(String input) {
 		String[] inputs = input.split(COMMA);
-		return Arrays.stream(inputs)
-			.map(Integer::parseInt)
-			.map(LottoNo::new)
-			.collect(Collectors.toList());
+		try {
+			return Arrays.stream(inputs)
+				.map(Integer::parseInt)
+				.map(LottoNo::new)
+				.collect(Collectors.toList());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(Validator.ERROR_MESSAGE_NOT_INTEGER);
+		}
 	}
 }
