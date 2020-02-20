d1ece4a2b5f7cbd726db5e5fede98f38a323e7b4
lxxjn0 <ljy3510@gmail.com>
2020-02-20 17:44:00 +0900

    feat : 당첨 통계를 출력하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,16 +2,33 @@ package lotto.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoGenerator;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoRank;
 
 public class LottoController {
-	public List<Lotto> start(int numberOfLotto) {
+	public static final int INIT_COUNT = 1;
+
+	public List<Lotto> purchaseLotto(int numberOfLotto) {
 		List<Lotto> lottos = new ArrayList<>();
 		for (int i = 0; i < numberOfLotto; i++) {
 			lottos.add(LottoGenerator.generate());
 		}
 		return lottos;
+	}
+
+	public Map<LottoRank, Integer> getLottoRankCount(List<Lotto> lottos, Lotto winningLotto,
+		LottoNumber bonusLottoNumber) {
+		Map<LottoRank, Integer> lottoRankCount = new TreeMap<>();
+
+		for (Lotto lotto : lottos) {
+			LottoRank lottoRank = LottoRank.of(lotto.getMatchCount(winningLotto), lotto.isContains(bonusLottoNumber));
+			lottoRankCount.merge(lottoRank, INIT_COUNT, Integer::sum);
+		}
+		return lottoRankCount;
 	}
 }
--- src/main/java/lotto/view/ConsoleOutputView.java
+++ src/main/java/lotto/view/ConsoleOutputView.java
@@ -1,14 +1,20 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
+import lotto.domain.LottoRank;
 
 public class ConsoleOutputView {
 	public static final String PURCHASE_COMPLETE_MESSAGE = "%d개를 구매했습니다.\n";
 	public static final String DELIMITER = ",";
+	public static final String STATISTICS_MESSAGE_1 = "당첨 통계";
+	public static final String STATISTICS_MESSAGE_2 = "---------";
+	public static final String STATISTICS_RESULT = "%d개 일치 (%d원) - %d개\n";
+	public static final String STATISTICS_SECOND_RANK_RESULT = "%d개 일치, 보너스 볼 일치(%d원) - %d개\n";
 
 	public static void printExceptionMessage(String exceptionMessage) {
 		System.out.println(exceptionMessage);
@@ -33,7 +39,33 @@ public class ConsoleOutputView {
 	}
 
 	public static void printStatisticsMessage() {
-		System.out.println("당첨 통계");
-		System.out.println("---------");
+		System.out.println(STATISTICS_MESSAGE_1);
+		System.out.println(STATISTICS_MESSAGE_2);
 	}
+
+	public static void printStatisticsResult(Map<LottoRank, Integer> lottoRankCount) {
+		for (Map.Entry<LottoRank, Integer> lottoRankEntry : lottoRankCount.entrySet()) {
+			if (lottoRankEntry.getKey().isLottoRankOf(LottoRank.SECOND)) {
+				printSecondRankResult(lottoRankEntry);
+				continue;
+			}
+			if (lottoRankEntry.getKey().isLottoRankOf(LottoRank.MISS)) {
+				return;
+			}
+			printRankResult(lottoRankEntry);
+		}
+	}
+
+	private static void printRankResult(Map.Entry<LottoRank, Integer> lottoRankEntry) {
+		System.out.printf(STATISTICS_RESULT, lottoRankEntry.getKey().getMatchCount(),
+			lottoRankEntry.getKey().getWinningMoney().getMoney(),
+			lottoRankEntry.getValue());
+	}
+
+	private static void printSecondRankResult(Map.Entry<LottoRank, Integer> lottoRankEntry) {
+		System.out.printf(STATISTICS_SECOND_RANK_RESULT, lottoRankEntry.getKey().getMatchCount(),
+			lottoRankEntry.getKey().getWinningMoney().getMoney(),
+			lottoRankEntry.getValue());
+	}
+
 }
