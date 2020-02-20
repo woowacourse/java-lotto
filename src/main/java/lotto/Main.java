68084ca67425abf74036516a73bad03b8f209a6f
moon <mjj920930@gmail.com>
2020-02-20 15:10:28 +0900

    refactor: 컨트롤러의 문맥에 따라 개행 수정

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -9,10 +9,15 @@ public class Main {
     public static void main(String[] args) {
         int purchaseMoney = InputView.inputPurchaseMoney();
         List<Lotto> lottos =  LottoFactory.createLottos(purchaseMoney);
+
         OutputView.printLottos(lottos);
 
-        WinningNumbers winningNumbers = new WinningNumbers(InputView.inputSixNumbers(), InputView.inputBonusNumber());
+        List<Integer> sixNumbers = InputView.inputSixNumbers();
+        int bonusNumber = InputView.inputBonusNumber();
+        WinningNumbers winningNumbers = new WinningNumbers(sixNumbers, bonusNumber);
+
         List<Rank> ranks = winningNumbers.compareLottos(lottos);
+
         OutputView.printResult(ranks);
         OutputView.printProfit(purchaseMoney, ranks);
     }
