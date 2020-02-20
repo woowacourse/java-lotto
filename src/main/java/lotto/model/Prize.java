720bc8d50c23d91bb8d1f4a5c73e46614533841d
kimjunseok <jnunseok@gmail.com>
2020-02-20 16:32:03 +0900

    refactor : Prize stream 사용

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -2,12 +2,9 @@ package lotto.model;
 
 public class Prize {
 
-    private static double prize = 0;
-
     public static double sumPrize() {
-        for (String key : LottoResultMap.resultCount.keySet()) {
-            prize += LottoResultMap.resultCount.get(key).prizeResult();
-        }
-        return prize;
+        return LottoResultMap.resultCount.keySet().stream()
+            .mapToDouble(lottoResult -> LottoResultMap.resultCount.get(lottoResult).prizeResult())
+            .sum();
     }
 }
