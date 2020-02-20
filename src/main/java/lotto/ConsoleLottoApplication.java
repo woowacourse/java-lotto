959b2e87521667cb7c213a0994b3127c56fee257
Tak Hyungmin <milkho11@naver.com>
2020-02-20 15:42:45 +0900

    [refact] main 메서드 분리 리팩토링

--- src/main/java/lotto/ConsoleLottoApplication.java
+++ src/main/java/lotto/ConsoleLottoApplication.java
@@ -13,28 +13,37 @@ import java.util.List;
 
 public class ConsoleLottoApplication {
     public static void main(String[] args) {
+        PurchaseNumber purchaseNumber = inputPurchaseNumber();
+        AllLottoNumbers allLottoNumbers = createAllLottoNumbers(purchaseNumber);
+        OutputView.printAllLottoNumbers(allLottoNumbers);
+        WinningNumbers winningNumbers = InputView.inputWinningNumbers();
+
+        GameResults gameResults = createGameResultsAndPrint(purchaseNumber, allLottoNumbers, winningNumbers);
+        OutputView.printGameResults(gameResults);
+
+        Yield yield = Yield.calculateYield(purchaseNumber, gameResults);
+        OutputView.printYield(yield);
+    }
+
+    private static PurchaseNumber inputPurchaseNumber() {
         // 구입 금액 입력
         PurchaseNumber purchaseNumber = InputView.inputPurchaseMoney();
         // 구입 개수 출력
         OutputView.printPurchaseNumber(purchaseNumber);
+        return purchaseNumber;
+    }
 
-        // 랜덤한 번호를 담은 List 출력하기
+    private static AllLottoNumbers createAllLottoNumbers(PurchaseNumber purchaseNumber) {
         List<LottoNumbers> lottoNumbersList = new ArrayList<>();
         for (int i = 0; i < purchaseNumber.getPurchaseNumber(); i++) {
             RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
             List<LottoNumber> randomNumbers = randomNumberGenerator.generateNumbers();
             lottoNumbersList.add(new LottoNumbers(randomNumbers));
         }
-        // 모든 로또를 담은 LottoNumbers 생성
-        AllLottoNumbers allLottoNumbers = new AllLottoNumbers(lottoNumbersList);
+        return new AllLottoNumbers(lottoNumbersList);
+    }
 
-        // 출력하는 로직
-        OutputView.printAllLottoNumbers(allLottoNumbers);
-
-        // 당첨번호, 보너스 볼 입력 로직
-        WinningNumbers winningNumbers = InputView.inputWinningNumbers();
-
-        // 당첨 통계 계산 로직
+    private static GameResults createGameResultsAndPrint(PurchaseNumber purchaseNumber, AllLottoNumbers allLottoNumbers, WinningNumbers winningNumbers) {
         List<GameResult> gameResultList = new ArrayList<>();
         for (int i = 0; i < purchaseNumber.getPurchaseNumber(); i++) {
             LottoNumbers presentLottoNumbers = allLottoNumbers.getAllLottoNumbers().get(i);
@@ -43,13 +52,6 @@ public class ConsoleLottoApplication {
             GameResult gameResult = GameResult.calculateRank(correctNumber, isCorrectBonusNumber);
             gameResultList.add(gameResult);
         }
-        GameResults gameResults = new GameResults(gameResultList);
-
-        // 당첨 통계 출력
-        OutputView.printGameResults(gameResults);
-
-        // 수익률 계산
-        Yield yield = Yield.calculateYield(purchaseNumber, gameResults);
-        OutputView.printYield(yield);
+        return new GameResults(gameResultList);
     }
 }
