cd880b451c61f9bcec1e99a7489d50d17c1257bf
lalize <printf@kakao.com>
2020-02-20 16:21:04 +0900

    refactor: 사용하지 않는 import 삭제

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -2,21 +2,20 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import lotto.domain.exception.InvalidLottoException;
 import lotto.domain.exception.InvalidLottosException;
 
 public class LottosTest {
 	@Test
 	@DisplayName("로또 여러 장이 정상적으로 생성된 경우")
 	void constructor() {
-		assertThat(new Lottos(Arrays.asList(Lotto.of(1,2,3,4,5,6), Lotto.of(2,3,4,5,6,7)))).isInstanceOf(Lottos.class);
+		assertThat(new Lottos(Arrays.asList(Lotto.of(1, 2, 3, 4, 5, 6), Lotto.of(2, 3, 4, 5, 6, 7)))).isInstanceOf(
+				Lottos.class);
 	}
 
 	@Test
