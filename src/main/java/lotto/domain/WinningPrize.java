385bfc2f6da14b5e0aff0f601f9bd264158ff44e
hj <a301dks@naver.com>
2020-02-20 15:55:39 +0900

    feat: WinningPrize enum 생성

---
+++ src/main/java/lotto/domain/WinningPrize.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum WinningPrize {
+	NO_PRIZE(0, 0, "미당첨"),
+	FIFTH(3, 5_000, "5등"),
+	FOURTH(4, 50_000, "4등"),
+	THIRD(5, 1_500_000, "3등"),
+	SECOND(5, 30_000_000, "2등(보너스볼 일치)"),
+	FIRST(6, 2_000_000_000, "1등");
+
+	private int matchCount;
+	private int prize;
+	private String prizeName;
+
+	WinningPrize(int matchCount, int prize, String prizeName) {
+		this.matchCount = matchCount;
+		this.prize = prize;
+		this.prizeName = prizeName;
+	}
+
+	public static WinningPrize of(int matchCount, boolean bonusMatch) {
+		if (matchCount == 5 && bonusMatch) {
+			return SECOND;
+		}
+		return Arrays.stream(values())
+			.filter(prize -> prize.matchCount == matchCount)
+			.findFirst()
+			.orElse(NO_PRIZE);
+	}
+
+	public int getMatchCount() {
+		return matchCount;
+	}
+
+	public int getPrize() {
+		return prize;
+	}
+
+	public String getPrizeName() {
+		return prizeName;
+	}
+}
---
+++ src/test/java/lotto/domain/WinningPrizeTest.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class WinningPrizeTest {
+	@ParameterizedTest
+	@CsvSource(value = {"6,false,1등", "5,true,2등(보너스볼 일치)", "3,true,5등", "2,false,미당첨"})
+	void 등수_확인(int matchCount, boolean bonusMatch, String expected) {
+		WinningPrize prize = WinningPrize.of(matchCount, bonusMatch);
+		assertEquals(expected, prize.getPrizeName());
+	}
+}
