9c09349242c155749bd80564375b9bfbf7954fe4
Sony <sonypark0204@gmail.com>
2020-02-21 18:24:08 +0900

    refac: EarningRateCalculator 상수명 변경

--- src/main/java/lotto/domain/EarningRateCalculator.java
+++ src/main/java/lotto/domain/EarningRateCalculator.java
@@ -2,9 +2,12 @@ package lotto.domain;
 
 public class EarningRateCalculator {
 
-    private static final int STANDARD = 100;
+    /* PERCENTILE은 백분위를 의미합니다 */
+    private static final int PERCENTILE = 100;
 
     public static int calculate(double totalWinningMoney, int purchaseAmount) {
-        return (int)((totalWinningMoney / purchaseAmount) * STANDARD);
+        return (int)((totalWinningMoney / purchaseAmount) * PERCENTILE);
     }
 }
+
+
