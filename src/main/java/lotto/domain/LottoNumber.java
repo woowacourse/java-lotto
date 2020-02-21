ae47c603b818a14c1e537051f715d5b5b64a6d9f
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-02-21 15:32:33 +0900

    [refactor] 불필요한 메소드 삭제

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -21,14 +21,6 @@ public class LottoNumber {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LottoNumber that = (LottoNumber) o;
-        return lottoNumber == that.lottoNumber;
-    }
-
-    @Override
     public int hashCode() {
         return Objects.hash(lottoNumber);
     }
