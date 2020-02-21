147c994f9fc6fe6eca54c76997ad6217619ea2f3
Seyun <ksy90101@gmail.com>
2020-02-21 16:20:07 +0900

    [refactor] 1등부터 꼴등까지 뽑는 테스트 리팩토링

--- src/test/java/lotto/domain/WinningRankTest.java
+++ src/test/java/lotto/domain/WinningRankTest.java
@@ -5,19 +5,23 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class WinningRankTest {
-
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}")
     @DisplayName("1등부터 꼴등까지 골라내는 테스트")
-    @CsvSource(value = {"1,2,3,4,5,6 FIRST_RANK","1,2,3,4,5,7 SECOND_RANK"
-            ,"1,2,3,4,5,10 THIRD_RANK","1,2,3,4,44,45 FOURTH_RANK"
-            ,"1,2,3,43,44,45 FIFTH_RANK", "1,2,40,41,42,43 NO_RANK"}, delimiter = ' ')
-    void select_rank_test(String input,WinningRank winningRank) {
+    @CsvSource(value = {
+            "1등_테스트 1,2,3,4,5,6 FIRST_RANK",
+            "2등_테스트 1,2,3,4,5,7 SECOND_RANK",
+            "3등_테스트 1,2,3,4,5,10 THIRD_RANK",
+            "4등_테스트 1,2,3,4,44,45 FOURTH_RANK",
+            "5등_테스트 1,2,3,43,44,45 FIFTH_RANK",
+            "미당첨_테스트 1,2,40,41,42,43 NO_RANK"}, delimiter = ' ')
+    void select_rank_test( String header, String input, WinningRank winningRank) {
         String[] lottoTicketNumbers = input.split(",");
         int[] winningBallInputs = {1, 2, 3, 4, 5, 6};
         int bonus = 7;
