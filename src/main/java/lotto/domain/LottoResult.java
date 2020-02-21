0d14efb1d780a97b1c9a360feaaf549ce04b5fa8
JunSeong <ebseud6135@naver.com>
2020-02-21 17:12:09 +0900

    refactor: LottoResult.java
    뷰에서 쓰기위한 객체 추가

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoResult {
+	private List<Rank> ranks;
+	private Money money;
+
+	public LottoResult(List<Rank> ranks, Money money) {
+		this.ranks = ranks;
+		this.money = money;
+	}
+
+	public Map<Rank, Integer> getResult() {
+		Map<Rank, Integer> counts = new HashMap<>();
+		Arrays.stream(Rank.values())
+			.forEach(rank -> counts.put(rank, 0));
+
+		ranks.stream()
+			.forEach(rank -> counts.replace(rank, counts.get(rank) + 1));
+		return counts;
+	}
+
+	public int getIncomeRate() {
+		return money.calculateIncomeRate(getTotalReward());
+	}
+
+	private long getTotalReward() {
+		return ranks.stream()
+			.map(Rank::getReward)
+			.reduce((x, y) -> x + y)
+			.get();
+	}
+
+}
