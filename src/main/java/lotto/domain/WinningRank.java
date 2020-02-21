604b9fbbdbe1e1e2c0a158d1a154e596757552ee
Seyun <ksy90101@gmail.com>
2020-02-21 10:29:05 +0900

    [refactor] enum Convention 수정

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -3,12 +3,12 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum WinningRank {
-    FIRST_RANK(2_000_000_000,6)
-    ,SECOND_RANK(30_000_000,5)
-    , THIRD_RANK(1_500_000,5)
-    , FOURTH_RANK(50_000,4)
-    , FIFTH_RANK(5_000,3)
-    ,NO_RANK(0,0);
+    FIRST_RANK(2_000_000_000,6),
+    SECOND_RANK(30_000_000,5),
+    THIRD_RANK(1_500_000,5),
+    FOURTH_RANK(50_000,4),
+    FIFTH_RANK(5_000,3),
+    NO_RANK(0,0);
 
     private final int winningMoney;
     private final int winningBallCount;
