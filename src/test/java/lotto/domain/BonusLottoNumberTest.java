1233053f01a2792f4409c0e06374e9dd5a14f4b7
김예림 <kiel0103@naver.com>
2020-02-21 23:52:16 +0900

    style: 사용되지 않는 import 문 제거

--- src/test/java/lotto/domain/BonusLottoNumberTest.java
+++ src/test/java/lotto/domain/BonusLottoNumberTest.java
@@ -2,8 +2,6 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
