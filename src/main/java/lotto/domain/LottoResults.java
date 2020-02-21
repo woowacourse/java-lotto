30f0d1336041b94b3932e5b816c3c5cf3e01c1f5
lalize <printf@kakao.com>
2020-02-22 02:05:15 +0900

    refactor: 코드 컨벤션

--- src/main/java/lotto/domain/LottoResults.java
+++ src/main/java/lotto/domain/LottoResults.java
@@ -32,10 +32,12 @@ public class LottoResults {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o)
+		if (this == o) {
 			return true;
-		if (o == null || getClass() != o.getClass())
+		}
+		if (o == null || getClass() != o.getClass()) {
 			return false;
+		}
 		LottoResults that = (LottoResults)o;
 		return Objects.equals(lottoResults, that.lottoResults);
 	}
