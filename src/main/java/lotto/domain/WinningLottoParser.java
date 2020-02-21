7997d8322d050333b55d17c467f9acc238ac78b1
begaonnuri <turtlenam0928@gmail.com>
2020-02-21 23:45:35 +0900

    feat : WinningLottoParser클래스의 paserToLottoNumberSet메소드에 검증 기능 추가 및 테스트코드 작성
     - null, 빈 문자열 검증 기능 추가
     - 값 개수 검증 기능 추가
     - 값 타입 검증 기능 추가

--- src/main/java/lotto/domain/WinningLottoParser.java
+++ src/main/java/lotto/domain/WinningLottoParser.java
@@ -6,12 +6,37 @@ import java.util.stream.Collectors;
 
 public class WinningLottoParser {
 	private static final String DELIMITER = ",";
+	private static final int LOTTO_NUMBER_COUNT = 6;
 
 	public static Set<LottoNumber> parseToLottoNumberSet(String inputWinningLotto) {
+		validateNullAndEmpty(inputWinningLotto);
+		validateCount(inputWinningLotto);
+		validateInteger(inputWinningLotto);
+
 		return Arrays.stream(inputWinningLotto.split(DELIMITER))
 			.map(String::trim)
 			.map(Integer::parseInt)
 			.map(LottoNumber::valueOf)
 			.collect(Collectors.toSet());
+	}
+
+	private static void validateInteger(String inputWinningLotto) {
+		try {
+			Arrays.stream(inputWinningLotto.split(DELIMITER)).forEach(Integer::parseInt);
+		} catch (NumberFormatException ne) {
+			throw new InvalidWinningLottoException(InvalidWinningLottoException.NOT_INTEGER);
+		}
+	}
+
+	private static void validateCount(String inputWinningLotto) {
+		if (inputWinningLotto.split(DELIMITER).length != LOTTO_NUMBER_COUNT) {
+			throw new InvalidWinningLottoException(InvalidWinningLottoException.INVALID_COUNT);
+		}
+	}
+
+	private static void validateNullAndEmpty(String inputWinningLotto) {
+		if (inputWinningLotto == null || inputWinningLotto.isEmpty()) {
+			throw new InvalidWinningLottoException(InvalidWinningLottoException.NULL_AND_EMPTY_STRING);
+		}
 	}
 }
--- src/test/java/lotto/domain/WinningLottoParserTest.java
+++ src/test/java/lotto/domain/WinningLottoParserTest.java
@@ -1,18 +1,22 @@
 package lotto.domain;
 
+import static lotto.domain.WinningLottoParser.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class WinningLottoParserTest {
+	@DisplayName("parseToLottoNumberSet에 번호 6개를 입력하면 LottoNumberSet 생성")
 	@Test
-	void parser_StringInputWinningLotto_CreateWinningLotto() {
-		String inputWinningLotto = "1, 2, 3, 4, 5, 6";
-
-		Set<LottoNumber> winningLottoNumbers = WinningLottoParser.parseToLottoNumberSet(inputWinningLotto);
+	void parseToLottoNumberSet_StringInputWinningLotto_WinningLottoNumberSet() {
+		Set<LottoNumber> winningLottoNumbers = parseToLottoNumberSet("1, 2, 3, 4, 5, 6");
 
 		Set<LottoNumber> expected = new HashSet<>();
 		expected.add(LottoNumber.valueOf(1));
@@ -23,5 +27,32 @@ class WinningLottoParserTest {
 		expected.add(LottoNumber.valueOf(6));
 
 		assertThat(winningLottoNumbers).isEqualTo(expected);
+	}
+
+	@DisplayName("parseToLottoNumberSet에 null이나 빈 문자열을 입력하면 InvalidWinningLottoException 발생")
+	@ParameterizedTest
+	@NullAndEmptySource
+	void parseToLottoNumberSet_NullAndEmptyString_ExceptionThrown(String input) {
+		assertThatThrownBy(() -> parseToLottoNumberSet(input))
+			.isInstanceOf(InvalidWinningLottoException.class)
+			.hasMessage(InvalidWinningLottoException.NULL_AND_EMPTY_STRING);
+	}
+
+	@DisplayName("parseToLottoNumberSet에 정수가 아닌 값을 입력하면 InvalidWinningLottoException 발생")
+	@ParameterizedTest
+	@ValueSource(strings = {"a,a,a,a,a,a", "1.1,2.2,3.3,4.4,5.5,6.6"})
+	void parseToLottoNumberSet_NotInteger_WinningLottoNumberSet(String input) {
+		assertThatThrownBy(() -> parseToLottoNumberSet(input))
+			.isInstanceOf(InvalidWinningLottoException.class)
+			.hasMessage(InvalidWinningLottoException.NOT_INTEGER);
+	}
+
+	@DisplayName("parseToLottoNumberSet에 6개가 아닌 값을 입력하면 InvalidWinningLottoException 발생")
+	@ParameterizedTest
+	@ValueSource(strings = {"1,2,3,4,5", "1,2,3,4,5,6,7"})
+	void parseToLottoNumberSet_InvalidCount_WinningLottoNumberSet(String input) {
+		assertThatThrownBy(() -> parseToLottoNumberSet(input))
+			.isInstanceOf(InvalidWinningLottoException.class)
+			.hasMessage(InvalidWinningLottoException.INVALID_COUNT);
 	}
 }
