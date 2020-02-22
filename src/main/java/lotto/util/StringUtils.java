2d9ba5c6d18132b294771ef42f18f92978974d88
toneyparky <qkrtmddhks95@gmail.com>
2020-02-22 13:47:27 +0900

    feat: splitIntoLottoNumbers 유틸에 입력 검증 추가

--- src/main/java/lotto/util/StringUtils.java
+++ src/main/java/lotto/util/StringUtils.java
@@ -17,16 +17,30 @@ import java.util.stream.Collectors;
  */
 public class StringUtils {
 	private static final String DELIMITER = ",";
+	private static final String INPUT_LOTTO_NUMBER_VALIDATION_PATTERN = "^((( *\\d* *, *){5})\\d+)";
+	private static final String NULL_INPUT_EXCEPTION_MESSAGE = "로또번호로 null 이 입력될 수 없습니다.";
+	private static final String EMPTY_INPUT_EXCEPTION_MESSAGE = "아무것도 입력하지 않으셨습니다.";
+	private static final String INVALID_INPUT_EXCEPTION_MESSAGE = "잘못된 형식으로 입력하셨습니다.";
+
 
 	private StringUtils() {}
 
-	public static List<LottoNumber> splitIntoLottoNumbers(final String lottoNumbersInput) {
-		Objects.requireNonNull(lottoNumbersInput, "로또번호로 null 이 입력될 수 없습니다.");
-
-		return Arrays.stream(lottoNumbersInput.split(DELIMITER))
+	public static List<LottoNumber> splitIntoLottoNumbers(final String inputLottoNumbers) {
+		Objects.requireNonNull(inputLottoNumbers, NULL_INPUT_EXCEPTION_MESSAGE);
+		validateInputLottoNumber(inputLottoNumbers);
+		return Arrays.stream(inputLottoNumbers.split(DELIMITER))
 				.map(String::trim)
 				.map(Integer::parseInt)
 				.map(LottoNumber::of)
 				.collect(Collectors.toList());
+	}
+
+	private static void validateInputLottoNumber(final String inputLottoNumbers) {
+		if (inputLottoNumbers.isEmpty()) {
+			throw new IllegalArgumentException(EMPTY_INPUT_EXCEPTION_MESSAGE);
+		}
+		if (!inputLottoNumbers.matches(INPUT_LOTTO_NUMBER_VALIDATION_PATTERN)) {
+			throw new IllegalArgumentException(INVALID_INPUT_EXCEPTION_MESSAGE);
+		}
 	}
 }
