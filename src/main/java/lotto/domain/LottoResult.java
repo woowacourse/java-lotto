1a2268e0037e95e6eed15d8d2b985dae286419fe
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-22 16:13:47 +0900

    feat: LottoResult의 getter가 unmodifiable map을 반환하도록 수정

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,19 +1,26 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoResult {
-	public static final int PERCENT_MULTIPLIER = 100;
+	static final int PERCENT_MULTIPLIER = 100;
+
 	private final Map<WinningType, Integer> lottoResult;
 
 	public LottoResult(final Map<WinningType, Integer> lottoResult) {
 		this.lottoResult = lottoResult;
 	}
 
-	public Map<WinningType, Integer> getLottoResult() {
-		return lottoResult;
+	public double calculateEarningRate(PurchaseMoney purchaseMoney) {
+		double totalEarning = 0;
+		for (WinningType winningType : lottoResult.keySet()) {
+			totalEarning += winningType.calculateEarning(lottoResult.get(winningType));
+		}
+
+		return purchaseMoney.divideBy(totalEarning) * PERCENT_MULTIPLIER;
 	}
 
 	public List<WinningType> getWinningKeys() {
@@ -24,12 +31,7 @@ public class LottoResult {
 				.collect(Collectors.toUnmodifiableList());
 	}
 
-	public double calculateEarningRate(PurchaseMoney purchaseMoney) {
-		double totalEarning = 0;
-		for (WinningType winningType : lottoResult.keySet()) {
-			totalEarning += winningType.calculateEarning(lottoResult.get(winningType));
-		}
-
-		return purchaseMoney.divideBy(totalEarning) * PERCENT_MULTIPLIER;
+	public Map<WinningType, Integer> getLottoResult() {
+		return Collections.unmodifiableMap(lottoResult);
 	}
 }
