89007bdad47e05a5b4a34ad8e557202dbea902d9
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-22 18:26:36 +0900

    style: 스페이스를 탭으로 대체

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -3,8 +3,8 @@ package lotto;
 import lotto.controller.LottoController;
 
 public class WebUILottoApplication {
-    public static void main(String[] args) {
-        LottoController.run();
-    }
+	public static void main(String[] args) {
+		LottoController.run();
+	}
 
 }
