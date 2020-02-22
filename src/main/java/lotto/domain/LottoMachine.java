a3b7ccd587e0e990223f7f3f9b8be0f9c7f09ae4
hj <a301dks@naver.com>
2020-02-22 14:31:09 +0900

    refactor: LottoMachine을 싱글톤객체로 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -6,32 +6,40 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoMachine {
-	private static final int MAX_LOTTO_NUMBER = 45;
-	private static final int MIN_LOTTO_NUMBER = 1;
+	private final LottoNumbers allLottoNumbers;
 
-	private final List<Integer> lottoBalls;
+	private LottoMachine() {
+		List<LottoNumber> initialSetting = new ArrayList<>();
 
-	public LottoMachine() {
-		lottoBalls = new ArrayList<>();
-		for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
-			lottoBalls.add(i);
+		for (int i = LottoNumber.MINIMUM; i <= LottoNumber.MAXIMUM; i++) {
+			initialSetting.add(new LottoNumber(i));
 		}
+		this.allLottoNumbers = new LottoNumbers(initialSetting);
+	}
+
+	public static LottoMachine getInstance() {
+		return LottoMachineSingletonHolder.instance;
 	}
 
 	public List<Lotto> makeRandomLottos(int lottoCount) {
 		List<Lotto> lottos = new ArrayList<>();
 
 		for (int i = 0; i < lottoCount; i++) {
-			lottos.add(new Lotto(pickRandomBalls()));
+			lottos.add(new Lotto(new LottoNumbers(pickRandomNumbers())));
 		}
 		return lottos;
 	}
 
-	private List<Integer> pickRandomBalls() {
-		Collections.shuffle(lottoBalls);
-		return lottoBalls.stream()
-			.limit(6)
+	private List<LottoNumber> pickRandomNumbers() {
+		Collections.shuffle(allLottoNumbers.getLottoNumbers());
+		return allLottoNumbers.getLottoNumbers()
+			.stream()
+			.limit(Lotto.SIZE)
 			.sorted()
 			.collect(Collectors.toList());
+	}
+
+	private static class LottoMachineSingletonHolder {
+		private static final LottoMachine instance = new LottoMachine();
 	}
 }
