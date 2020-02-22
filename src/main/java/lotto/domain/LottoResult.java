9e9ad79fa7bd603060092f0a4c3410406298120a
kuenhwi <kueniyam@naver.com>
2020-02-22 11:02:52 +0900

    test: LottoResult 클래스 테스트 구현

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LottoResult {
@@ -32,5 +33,18 @@ public class LottoResult {
 		}
 
 		return purchaseMoney.divideBy(totalEarning) * PERCENT_MULTIPLIER;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		LottoResult that = (LottoResult) o;
+		return Objects.equals(lottoResult, that.lottoResult);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(lottoResult);
 	}
 }
--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,11 +1,25 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LottoResultTest {
 	@Test
 	void LottoResult() {
 		// given
-//		LottoResult lottoResult = new LottoResult(lottoResult);
+		Map<WinningType, Integer> map = new HashMap<>();
+		map.put(WinningType.FIFTH_PLACE, 5);
+		map.put(WinningType.NONE, 4);
+		map.put(WinningType.FIRST_PLACE, 0);
+
+		// when
+		LottoResult result = new LottoResult(map);
+
+		// then
+		Assertions.assertThat(result.getLottoResult())
+				.isEqualTo(map);
 	}
 }
