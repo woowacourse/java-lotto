ece165e574d5d0487323902d8dfa096d3e61f363
gunhee lee <dlrjsgml42@naver.com>
2020-02-22 17:00:07 +0900

    [refactor] LottoTicketTest 중복코드 메서드 분리
    LottoTicketTest 중복되는 생성 코드를 메서드로 분리

--- src/test/java/domain/lottonumber/LottoTicketTest.java
+++ src/test/java/domain/lottonumber/LottoTicketTest.java
@@ -8,18 +8,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.*;
 
 public class LottoTicketTest {
+    private TreeSet makeTestTicket() {
+        return new TreeSet(Arrays.asList(
+                LottoNumberFactory.getInstance(1), LottoNumberFactory.getInstance(2), LottoNumberFactory.getInstance(3),
+                LottoNumberFactory.getInstance(4), LottoNumberFactory.getInstance(5), LottoNumberFactory.getInstance(6))
+        );
+    }
+
     @Test
     void 생성_테스트() {
-        SortedSet<LottoNumber> numbers = new TreeSet(Arrays.asList(
-                LottoNumberFactory.getInstance(1), LottoNumberFactory.getInstance(2), LottoNumberFactory.getInstance(3),
-                LottoNumberFactory.getInstance(4), LottoNumberFactory.getInstance(5), LottoNumberFactory.getInstance(6)));
+        SortedSet<LottoNumber> numbers = makeTestTicket();
         LottoTicket lottoTicket = new LottoTicket(numbers);
-        Assertions.assertThat(lottoTicket.contains(LottoNumberFactory.getInstance(1))).isTrue();
-        Assertions.assertThat(lottoTicket.contains(LottoNumberFactory.getInstance(2))).isTrue();
-        Assertions.assertThat(lottoTicket.contains(LottoNumberFactory.getInstance(3))).isTrue();
-        Assertions.assertThat(lottoTicket.contains(LottoNumberFactory.getInstance(4))).isTrue();
-        Assertions.assertThat(lottoTicket.contains(LottoNumberFactory.getInstance(5))).isTrue();
-        Assertions.assertThat(lottoTicket.contains(LottoNumberFactory.getInstance(6))).isTrue();
+        for (int i = 1; i <= 6; i++) {
+            Assertions.assertThat(lottoTicket.contains(LottoNumberFactory.getInstance(i))).isTrue();
+        }
     }
 
     @Test
@@ -43,12 +45,11 @@ public class LottoTicketTest {
 
     @Test
     void 비교_테스트() {
-        SortedSet<LottoNumber> numbers = new TreeSet(Arrays.asList(
-                LottoNumberFactory.getInstance(1), LottoNumberFactory.getInstance(2), LottoNumberFactory.getInstance(3),
-                LottoNumberFactory.getInstance(4), LottoNumberFactory.getInstance(5), LottoNumberFactory.getInstance(6)));
+        SortedSet<LottoNumber> numbers = makeTestTicket();
         SortedSet<LottoNumber> numbers2 = new TreeSet(Arrays.asList(
                 LottoNumberFactory.getInstance(1), LottoNumberFactory.getInstance(2), LottoNumberFactory.getInstance(3),
-                LottoNumberFactory.getInstance(9), LottoNumberFactory.getInstance(45), LottoNumberFactory.getInstance(8)));
+                LottoNumberFactory.getInstance(7), LottoNumberFactory.getInstance(8), LottoNumberFactory.getInstance(9))
+        );;
         LottoTicket lottoTicket = new LottoTicket(numbers);
         LottoTicket lottoTicket2 = new LottoTicket(numbers);
         LottoTicket lottoTicket3 = new LottoTicket(numbers2);
@@ -60,8 +61,9 @@ public class LottoTicketTest {
     @Test
     void 중복이_있으면_예외_처리() {
         SortedSet<LottoNumber> numbers = new TreeSet(Arrays.asList(
-                LottoNumberFactory.getInstance(1), LottoNumberFactory.getInstance(1), LottoNumberFactory.getInstance(3),
-                LottoNumberFactory.getInstance(4), LottoNumberFactory.getInstance(5), LottoNumberFactory.getInstance(6)));
+                LottoNumberFactory.getInstance(1), LottoNumberFactory.getInstance(2), LottoNumberFactory.getInstance(3),
+                LottoNumberFactory.getInstance(3), LottoNumberFactory.getInstance(4), LottoNumberFactory.getInstance(5))
+        );
         Assertions.assertThatThrownBy(() -> new LottoTicket(numbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("인자의 갯수가 올바르지 않습니다.");
