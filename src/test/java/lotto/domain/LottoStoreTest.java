0bf88c68816d90be83eb3b981d761aca655e2822
Seyun <ksy90101@gmail.com>
2020-02-23 13:31:10 +0900

    [test] 로또가 자동으로 생성되는지 확인하는 테스트 추가

---
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoStoreTest {
+    @Test
+    @DisplayName("로또가 자동으로 생성되는지 확인하는 테스트")
+    void correct_lotto_ticket_test() {
+        PurchaseAmount purchaseAmount = new PurchaseAmount("5000");
+        LottoStore lottoStore = new LottoStore(purchaseAmount);
+        LottoTickets lottoTickets = lottoStore.getLottoTickets();
+        assertThat(lottoTickets.getLottoTickets()).hasSize(5);
+    }
+}
