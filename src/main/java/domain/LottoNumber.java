9d294dcf829009ec922eb5329024f9adeecbf156
giantim <giantim132@gmail.com>
2020-02-23 09:19:30 +0900

    refactor: 로또에 사용될 숫자를 생성시 int 를 매개변수로 변경

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -1,23 +1,16 @@
 package domain;
 
-import spark.utils.StringUtils;
-
 public class LottoNumber {
     private int lottoNumber;
 
-    public LottoNumber(String number) {
-        if (StringUtils.isBlank(number)) {
-            throw new IllegalArgumentException("잘못된 복권 번호를 입력하였습니다.");
+    public LottoNumber(int number) {
+        validateLottoNumberRange(number);
+        this.lottoNumber = number;
+    }
+
+    private void validateLottoNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("범위를 벗어나는 로또 번호 입니다.");
         }
-        int parseNumber;
-        try {
-            parseNumber = Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력해 주십시오.");
-        }
-        if (!LottoNumberBundle.contains(parseNumber)) {
-            throw new IllegalArgumentException("범위를 벗어나는 복권 번호를 입력하였습니다.");
-        }
-        this.lottoNumber = parseNumber;
     }
 }
--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class LottoNumberTest {
     @DisplayName("LottoNumber 생성자 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"", " ", "-1", "46", "0"})
-    public void lottoNumberConstructorTest(String input) {
+    @ValueSource(ints = {0, 46})
+    public void lottoNumberConstructorTest(int input) {
         Assertions.assertThatThrownBy(() -> {
             new LottoNumber(input);
         }).isInstanceOf(IllegalArgumentException.class);
