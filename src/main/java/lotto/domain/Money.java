02c6365bee04f42c6c61bdd26e83ac272df4a65f
ordinCode <ysh1346@naver.com>
2020-02-23 11:30:23 +0900

    refactor : LottoFactoryTest.java
     예외 발생가능한 상황에 대한 테스트 (돈이 1000원 이하, 0원 등)

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -6,7 +6,7 @@ public class Money {
     public static final String ERROR_MESSAGE_NULL_POINT_MONEY = "입력값이 비었습니다.";
     private static final int LOTTO_PRICE = 1000;
     private static final int TO_PERCENT_VALUE = 100;
-    private static final String ERROR_MESSAGE_MIN_MONEY = "천원 이상의 금액만 가능합니다.";
+    public static final String ERROR_MESSAGE_MIN_MONEY = "천원 이상의 금액만 가능합니다.";
     private final int money;
 
     public Money(String inputMoney) {
--- src/test/java/lotto/domain/LottoFactoryTest.java
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -11,7 +11,7 @@ public class LottoFactoryTest {
 
 	@DisplayName("사용자가 입력한 돈에 맞게 로또 생성")
 	@Test
-	void createLotteries() {
+	void createLotteries1() {
 		String userInputMoney = "13000";
 		List<Lotto> lottoList = LottoFactory.createLotteries(new Money(userInputMoney));
 		assertThat(lottoList.size()).isEqualTo(13);
@@ -19,5 +19,22 @@ public class LottoFactoryTest {
 		userInputMoney = "5000";
 		List<Lotto> lottoList2 = LottoFactory.createLotteries(new Money(userInputMoney));
 		assertThat(lottoList2.size()).isEqualTo(5);
+	}
+
+	@DisplayName("1000원 이하의 금액을 입력했을 경우")
+	@Test
+	void createLotteries2() {
+		String userInputMoneyZero = "0";
+		assertThatThrownBy(() -> LottoFactory.createLotteries(new Money(userInputMoneyZero)))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage(Money.ERROR_MESSAGE_MIN_MONEY);
+	}
+
+	@DisplayName("입력값이 비었을 경우")
+	@Test
+	void nullTest() {
+		assertThatThrownBy(() -> LottoFactory.createLotteries(new Money(null)))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage(Money.ERROR_MESSAGE_NULL_POINT_MONEY);
 	}
 }
