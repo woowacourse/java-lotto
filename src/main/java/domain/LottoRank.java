4c349127b4fca84fdb7f5f53c593f8259323b89e
ParkDooWon <pass345@naver.com>
2020-02-23 22:13:00 +0900

    refactor : 한번의 순회로 당첨 번호와 보너스 번호를 매칭하여 해당 등수 반환하도록 수정

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -3,34 +3,28 @@ package domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(6, false, 2_000_000_000, "6개 일치(2000000000원) - "),
-    SECOND(5, true, 30_000_000, "5개 일치, 보너스볼 일치(30000000원) - "),
-    THIRD(5, false, 1_500_000, "5개 일치(1500000원) - "),
-    FOURTH(4, false, 50_000, "4개 일치(50000원) - "),
-    FIFTH(3, false, 5000, "3개 일치(5000원) - ");
+    FIRST(6, BonusNeed.DONT_CARE, 2_000_000_000, "6개 일치(2000000000원) - "),
+    SECOND(5, BonusNeed.NEED, 30_000_000, "5개 일치, 보너스볼 일치(30000000원) - "),
+    THIRD(5, BonusNeed.NOT_NEED, 1_500_000, "5개 일치(1500000원) - "),
+    FOURTH(4, BonusNeed.DONT_CARE, 50_000, "4개 일치(50000원) - "),
+    FIFTH(3, BonusNeed.DONT_CARE, 5000, "3개 일치(5000원) - ");
 
     private int winningMatchCount;
-    private boolean isBonusMatch;
+    private BonusNeed bonusNeed;
     private int winningMoney;
     private String resultMessage;
 
-    LottoRank(int winningMatchCount, boolean isBonusMatch, int winningMoney, String resultMessage){
+    LottoRank(int winningMatchCount, BonusNeed bonusNeed, int winningMoney, String resultMessage){
         this.winningMatchCount = winningMatchCount;
-        this.isBonusMatch = isBonusMatch;
+        this.bonusNeed = bonusNeed;
         this.winningMoney = winningMoney;
         this.resultMessage = resultMessage;
     }
 
-    public static LottoRank findRank(int winningMatchCount, boolean bonusMatchCount) {
-        if (winningMatchCount < 5) {
-            return Arrays.stream(LottoRank.values())
-                    .filter(result -> result.winningMatchCount == winningMatchCount)
-                    .findFirst()
-                    .orElse(null);
-        }
+    public static LottoRank findRank(int winningBalls, boolean isBonusMatch) {
         return Arrays.stream(LottoRank.values())
-                .filter(result -> result.winningMatchCount == winningMatchCount)
-                .filter(result -> result.isBonusMatch == bonusMatchCount)
+                .filter(o -> o.winningMatchCount == winningBalls)
+                .filter(o -> o.bonusNeed.isNeedBonus(isBonusMatch))
                 .findFirst()
                 .orElse(null);
     }
