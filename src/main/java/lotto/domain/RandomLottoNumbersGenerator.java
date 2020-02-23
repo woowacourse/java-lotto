00bd2b9aff8394580a5e3a8a57160f0e1897af2a
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-23 14:40:25 +0900

    refactor: 모든 로또번호를 한번만 할당하게 변경

--- src/main/java/lotto/domain/RandomLottoNumbersGenerator.java
+++ src/main/java/lotto/domain/RandomLottoNumbersGenerator.java
@@ -8,9 +8,11 @@ public class RandomLottoNumbersGenerator implements RandomGenerator {
 	private static final int ZERO_INDEX = 0;
 	private static final int SIX_INDEX = 6;
 
+	private static final List<LottoNumber> allLottoNumbers
+			= new ArrayList<>(AllLottoNumbers.getAll());
+
 	@Override
 	public List<LottoNumber> generateSixNumbers() {
-		List<LottoNumber> allLottoNumbers = new ArrayList<>(AllLottoNumbers.getAll());
 		Collections.shuffle(allLottoNumbers);
 
 		return allLottoNumbers.subList(ZERO_INDEX, SIX_INDEX);
