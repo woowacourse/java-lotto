6e268314f3c0c295978dbf7a1ec4e62b52eb6dd0
yeonnseok <ljinsk3@gmail.com>
2020-02-24 01:20:24 +0900

    feat: manualCount 생성자 구현

---
+++ src/main/java/domain/ManualCount.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class ManualCount {
+    public static final int NEGATIVR_CRITERIA_POINT = 0;
+    private int manualCount;
+
+    public ManualCount(String manualCount, int lottoCount) {
+        checkNotNumber(manualCount);
+        this.manualCount = Integer.parseInt(manualCount);
+        checkManualAvailableRange(lottoCount);
+    }
+
+    private void checkManualAvailableRange(int lottoCount) {
+        if (isManualAvailableRange(lottoCount)) {
+            throw new IllegalArgumentException(String.format("수동으로 구매 가능한 로또 개수가 아닙니다. 현재 입력 : %s ", manualCount));
+        }
+    }
+
+    private boolean isManualAvailableRange(int lottoCount) {
+        return manualCount < NEGATIVR_CRITERIA_POINT || manualCount > lottoCount;
+    }
+
+    private void checkNotNumber(final String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(String.format("문자는 입력될 수 없습니다. 현재 입력 : %s ", "수동"));
+        }
+    }
+}
---
+++ src/test/java/lotto/ManualCountTest.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import domain.ManualCount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ManualCountTest {
+
+    @Test
+    @DisplayName("문자 입력 시 예외 발생")
+    void 수동_구매_로또_수_테스트() {
+        assertThatThrownBy(() -> {
+            new ManualCount("수동", 10);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(String.format("문자는 입력될 수 없습니다. 현재 입력 : %s ", "수동"));
+    }
+
+    @ParameterizedTest
+    @DisplayName("0이상 lottoCount 보다 작은 범위 벗어나면 예외 발생")
+    @ValueSource(strings = {"-1", "11"})
+    void 수동_구매_로또_입력_범위_테스트(String manualCount){
+        assertThatThrownBy(() -> {
+            System.out.println(manualCount);
+            new ManualCount(manualCount, 10);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("수동으로 구매 가능한 로또 개수가 아닙니다. 현재 입력 : %s ", manualCount));
+    }
+
+}
