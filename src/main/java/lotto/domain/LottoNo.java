abe6134ae1cd69571b53744cd3ec35951317ef96
ordinCode <ysh1346@naver.com>
2020-02-24 00:01:06 +0900

    refactor : LottoNo.java
     인자 변수명 변경

--- src/main/java/lotto/domain/LottoNo.java
+++ src/main/java/lotto/domain/LottoNo.java
@@ -3,45 +3,45 @@ package lotto.domain;
 import java.util.Objects;
 
 public class LottoNo implements Comparable<LottoNo> {
-	private static final String ERROR_MESSAGE_LOTTO_RANGE = "1이상 45이하의 숫자를 입력하세요.";
-	private static final int MIN_LOTTO_NO = 1;
-	private static final int MAX_LOTTO_NO = 45;
+    private static final String ERROR_MESSAGE_LOTTO_RANGE = "1이상 45이하의 숫자를 입력하세요.";
+    private static final int MIN_LOTTO_NO = 1;
+    private static final int MAX_LOTTO_NO = 45;
 
-	private final int number;
+    private final int number;
 
-	public LottoNo(int number) {
-		validateLottoNo(number);
-		this.number = number;
-	}
+    public LottoNo(int number) {
+        validateLottoNo(number);
+        this.number = number;
+    }
 
-	private void validateLottoNo(int i) {
-		if (i < MIN_LOTTO_NO || i > MAX_LOTTO_NO) {
-			throw new IllegalArgumentException(ERROR_MESSAGE_LOTTO_RANGE);
-		}
-	}
+    private void validateLottoNo(int lottoNumber) {
+        if (lottoNumber < MIN_LOTTO_NO || lottoNumber > MAX_LOTTO_NO) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_LOTTO_RANGE);
+        }
+    }
 
-	@Override
-	public int compareTo(LottoNo other) {
-		return this.number - other.number;
-	}
+    @Override
+    public int compareTo(LottoNo other) {
+        return this.number - other.number;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		LottoNo lottoNo = (LottoNo)o;
-		return number == lottoNo.number;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        LottoNo lottoNo = (LottoNo) o;
+        return number == lottoNo.number;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(number);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 
-	@Override
-	public String toString() {
-		return number + "";
-	}
+    @Override
+    public String toString() {
+        return number + "";
+    }
 }
