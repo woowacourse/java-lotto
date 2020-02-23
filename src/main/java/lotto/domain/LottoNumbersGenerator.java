cd7c4c706668eb03d093cdd2fc9ce5ce08658670
Sony <sonypark0204@gmail.com>
2020-02-23 21:58:49 +0900

    refac: 일급 콜렉션 적용: LottoNumberBox

--- src/main/java/lotto/domain/LottoNumbersGenerator.java
+++ src/main/java/lotto/domain/LottoNumbersGenerator.java
@@ -3,17 +3,11 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoNumbersGenerator {
-    private static final int LOTTO_MAX_NUMBER = 45;
-    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final List<LottoNumber> lottoNumbers = LottoNumberBox.get();
     static final int LOTTO_NUMBER_SIZE = 6;
     private static final int ONE = 1;
-    private static final List<LottoNumber> LOTTO_NUMBERS_BOX = IntStream.range(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER + ONE)
-        .mapToObj(LottoNumber::new)
-        .collect(Collectors.toList());
 
     static List<List<LottoNumber>> generate(int lottosSize) {
         List<List<LottoNumber>> lottoNumbers = new ArrayList<>(new ArrayList<>());
@@ -27,9 +21,9 @@ public class LottoNumbersGenerator {
     }
 
     private static List<LottoNumber> generateLottoNumbers() {
-        Collections.shuffle(LOTTO_NUMBERS_BOX);
+        Collections.shuffle(lottoNumbers);
         return new ArrayList<>(
-            Collections.unmodifiableCollection(LOTTO_NUMBERS_BOX.subList(ONE, ONE + LOTTO_NUMBER_SIZE)));
+            Collections.unmodifiableCollection(lottoNumbers.subList(ONE, ONE + LOTTO_NUMBER_SIZE)));
     }
 
     private static boolean isDuplicated(List<LottoNumber> lottoNumbers, int lottoNumber) {
