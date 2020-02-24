1c747f6c3ba1a55aa148e77e030ca5fdf5cd0af8
hongbin <khbasd@gmail.com>
2020-02-24 16:37:44 +0900

    test : PurchaseMoney 테스트코드 추가

---
+++ src/test/java/domain/PurchaseMoneyTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import exception.LackOfMoneyException;
+
+class PurchaseMoneyTest {
+	@Test
+	void validate() {
+		assertThatThrownBy(() -> {
+			new PurchaseMoney(999);
+		}).isInstanceOf(LackOfMoneyException.class)
+			.hasMessage("로또 비용이 부족합니다.");
+	}
+}
