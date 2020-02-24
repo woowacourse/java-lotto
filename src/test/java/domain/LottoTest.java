7a022bfe6cb42292443127f14ff8395518d1a3dd
hongbin <khbasd@gmail.com>
2020-02-24 15:57:43 +0900

    test: 로또 테스트 추가

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -2,18 +2,53 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import exception.LottoInputException;
 
 class LottoTest {
 
+	private static Stream<Arguments> createNumbers() {
+		return Stream.of(
+			Arguments.of(
+				"1, 2, 2, 3, 4, 5"
+			), Arguments.of(
+				"1, 2, 3, 4, 5"
+			)
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("createNumbers")
+	void createTest(String numbers) {
+		assertThatThrownBy(() -> {
+			new Lotto(numbers);
+		})
+			.isInstanceOf(LottoInputException.class)
+			.hasMessage("로또 번호가 중복되거나, 6개를 입력하지 않았습니다.");
+	}
+
+	@Test
+	void isContains() {
+		// given
+		Lotto lotto = new Lotto("1, 2, 3, 4, 5, 6");
+		LottoNumber containNumber = LottoNumber.get(1);
+		// then
+		assertThat(lotto.isContains(containNumber)).isTrue();
+	}
+
 	@Test
 	void compare() {
-		//given
-		Lotto lotto = LottoFactory.createSelfNumberLotto(1, 2, 3, 4, 5, 6);
-		Lotto winningTicket = LottoFactory.createSelfNumberLotto(1, 2, 3, 7, 8, 9);
-		LottoNumber bonus = LottoNumber.get(13);
-		//when
-		//then
-		assertThat(lotto.compare(winningTicket, bonus)).isEqualTo(Rank.FIFTH);
+		// given
+		Lotto lotto = new Lotto("1, 2, 3, 4, 5, 6");
+		Lotto winningLotto = new Lotto("1, 2, 3, 4, 5, 7");
+		LottoNumber bonusNumber = LottoNumber.get(6);
+		// then
+		assertThat(lotto.compare(winningLotto, bonusNumber)).isEqualTo(Rank.SECOND);
 	}
 }
