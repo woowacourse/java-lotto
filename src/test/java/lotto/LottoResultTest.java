cfc1543f51a59b40493294c1c67c671393cebc54
yeonnseok <ljinsk3@gmail.com>
2020-02-24 15:47:05 +0900

    test: 로또 결과 계산 테스트 추가

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -1,19 +1,52 @@
 package lotto;
 
-import domain.LottoRank;
-import domain.LottoResult;
+import domain.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoResultTest {
+
+    @BeforeAll
+    static void setUp() {
+        String[] winningNumbers = {"1", "2", "3", "5", "4", "6"};
+        String bonusNumber = "7";
+        WinningNumber.inputWinningNumbers(winningNumbers);
+        WinningNumber.inputBonusNumber(bonusNumber);
+
+        List<LottoNumber> myLottoNumbers = new ArrayList<>();
+        myLottoNumbers.add(new LottoNumber(1));
+        myLottoNumbers.add(new LottoNumber(2));
+        myLottoNumbers.add(new LottoNumber(3));
+        myLottoNumbers.add(new LottoNumber(4));
+        myLottoNumbers.add(new LottoNumber(5));
+        myLottoNumbers.add(new LottoNumber(7));
+        Lotto myLotto = new Lotto(myLottoNumbers);
+        Lottos.addLotto(myLotto);
+    }
+
     @Test
     void 로또_결과의_초기값_확인() {
-        LottoResult lottoResult = new LottoResult();
-        assertThat(lottoResult.getSize()).isEqualTo(5);
-
+        assertThat(LottoResult.getSize()).isEqualTo(5);
         for (LottoRank rank : LottoRank.values()) {
-            assertThat(lottoResult.getRankCount(rank)).isEqualTo(0);
+            assertThat(LottoResult.getRankCount(rank)).isEqualTo(0);
         }
+    }
+
+    @Test
+    void 당첨결과_누적_테스트() {
+        LottoResult.countWinningLotto();
+        assertThat(LottoResult.getRankCount(LottoRank.SECOND)).isEqualTo(1);
+    }
+
+    @Test
+    void 총_수익_계산_테스트() {
+        LottoResult.countWinningLotto();
+        int totalProfit = LottoResult.calculateTotalProfit();
+        assertThat(totalProfit).isEqualTo(30_000_000);
     }
 }
