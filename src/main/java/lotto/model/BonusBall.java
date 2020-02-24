0b8ae4bf28542473f299be4fa2ffa8d3ab873da5
hwangbo <taekwonod714@naver.com>
2020-02-24 12:18:48 +0900

    refactor : BonusBall 클래스 리팩토링

--- src/main/java/lotto/model/BonusBall.java
+++ src/main/java/lotto/model/BonusBall.java
@@ -3,15 +3,15 @@ package lotto.model;
 import lotto.exception.OverlapWinNumberException;
 
 public class BonusBall {
-    private static final String IS_CONTAIN_WIN_NUMBER_EXCEPTION_MESSAGE = "당첨번호와 중복되는 숫자가 있습니다.";
-    private static int bonusball;
+    private final String IS_CONTAIN_WIN_NUMBER_EXCEPTION_MESSAGE = "당첨번호와 중복되는 숫자가 있습니다.";
+    private int bonusball;
 
     public BonusBall(WinNumber winNumber, int bonusNumber) {
-        isContainsWinNumber(winNumber, bonusNumber);
+        checkContainsWinNumber(winNumber, bonusNumber);
         bonusball = bonusNumber;
     }
 
-    private void isContainsWinNumber(WinNumber winNumber,int bonusNumber) {
+    private void checkContainsWinNumber(WinNumber winNumber,int bonusNumber) {
         if (winNumber.contains(bonusNumber)) {
             throw new OverlapWinNumberException(IS_CONTAIN_WIN_NUMBER_EXCEPTION_MESSAGE);
         }
