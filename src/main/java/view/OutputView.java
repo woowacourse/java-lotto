293011e6d0304f12b267683c15777aa0f5f57fd6
yeonnseok <ljinsk3@gmail.com>
2020-02-24 09:39:51 +0900

    refactor: UI 공백라인 위치 조정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -20,15 +20,15 @@ public class OutputView {
     }
 
     public static void printInputManualCountMessage() {
-        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        System.out.println(NEW_LINE + "수동으로 구매할 로또 수를 입력해 주세요.");
     }
 
     public static void printInputManualLottoNumbers() {
-        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        System.out.println(NEW_LINE + "수동으로 구매할 번호를 입력해 주세요.");
     }
 
     public static void printPurchaseCountMessage(ManualCount manualCount, int lottoCount) {
-        System.out.println(String.format("수동으로 %d장 자동으로 %d개를 구매했습니다." + NEW_LINE,
+        System.out.println(NEW_LINE + String.format("수동으로 %d장 자동으로 %d개를 구매했습니다.",
                 manualCount.getManualCount(), lottoCount - manualCount.getManualCount()));
     }
 
@@ -47,7 +47,7 @@ public class OutputView {
     }
 
     public static void printInputWinningNumbersMessage() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println(NEW_LINE + "지난 주 당첨 번호를 입력해 주세요.");
     }
 
     public static void printInputBonusNumberMessage() {
@@ -56,7 +56,8 @@ public class OutputView {
 
     public static void printResult(final LottoResult lottoResult) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("당첨 통계")
+        stringBuilder.append(NEW_LINE)
+                    .append("당첨 통계")
                     .append(NEW_LINE)
                     .append(DASHES)
                     .append(NEW_LINE);
