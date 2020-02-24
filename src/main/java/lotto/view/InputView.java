45ae1c5423213d735347b402133afb40cdbf02cb
icyMojito <dev.icymojito@gmail.com>
2020-02-24 11:12:11 +0900

    [refactor] InputView에서 구분자를 다루도록 수정
    구분자는 InputView의 관심사이기 때문에 InputView에서 구분자 정보를 처리하도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,18 +3,20 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
+    public static final String LOTTO_NUMBERS_DELIMITER = ",";
+
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() { /* prevent creating InputView instance */ }
 
-    public static String requestPurchasePriceInput() {
+    public static String requestLottoMoneyInput() {
         System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextLine();
     }
 
-    public static String requestWinningNumbersInput() {
+    public static String[] requestWinningLottoInput() {
         System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
-        return scanner.nextLine();
+        return scanner.nextLine().split(LOTTO_NUMBERS_DELIMITER);
     }
 
     public static String requestBonusNumberInput() {
