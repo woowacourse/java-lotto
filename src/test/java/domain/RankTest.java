bb2903de2f5f96a41674d4567c67a7c29bd23e64
hongbin <khbasd@gmail.com>
2020-02-24 16:42:32 +0900

    refactor : Rank 테스트코드 추가

--- src/test/java/domain/RankTest.java
+++ src/test/java/domain/RankTest.java
@@ -2,22 +2,40 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class RankTest {
 
-	private static Stream<Arguments> createCountAndRank() {
+	static Stream<Arguments> createCountAndRank() {
 		return Stream.of(
-			Arguments.of(5, Rank.THIRD),
-			Arguments.of(6, Rank.FIRST),
-			Arguments.of(3, Rank.FIFTH)
+			Arguments.of(
+				-123, Rank.NO_RANK
+			),
+			Arguments.of(
+				2000, Rank.NO_RANK
+			),
+			Arguments.of(
+				1, Rank.NO_RANK
+			),
+			Arguments.of(
+				2, Rank.NO_RANK
+			),
+			Arguments.of(
+				3, Rank.FIFTH
+			),
+			Arguments.of(
+				4, Rank.FOURTH
+			),
+			Arguments.of(
+				5, Rank.THIRD
+			),
+			Arguments.of(
+				6, Rank.FIRST
+			)
 		);
 	}
 
@@ -25,12 +43,5 @@ class RankTest {
 	@MethodSource("createCountAndRank")
 	void of(int count, Rank expected) {
 		assertThat(Rank.of(count)).isEqualTo(expected);
-	}
-
-	@Test
-	void sumWinningMoney() {
-		List<Rank> ranks = Arrays.asList(Rank.FIFTH, Rank.SECOND, Rank.FOURTH);
-		int expected = 30055000;
-		assertThat(Rank.sumWinningMoney(ranks).getMoney()).isEqualTo(expected);
 	}
 }
