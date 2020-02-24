1a92ab98aa91e687ca193ba32837b7619d92330f
hwangbo <taekwonod714@naver.com>
2020-02-24 12:28:54 +0900

    refactor : Prize 클래스 리팩토링

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -4,11 +4,9 @@ public class Prize {
     private static double prize = 0;
 
     public static double sumPrize(LottoResult lottoResult) {
-        prize += LottoRank.FIRST.prizeResult(lottoResult.getKey(LottoRank.FIRST.getRank()));
-        prize += LottoRank.SECOND.prizeResult(lottoResult.getKey(LottoRank.SECOND.getRank()));
-        prize += LottoRank.THIRD.prizeResult(lottoResult.getKey(LottoRank.THIRD.getRank()));
-        prize += LottoRank.FOURTH.prizeResult(lottoResult.getKey(LottoRank.FOURTH.getRank()));
-        prize += LottoRank.FIFTH.prizeResult(lottoResult.getKey(LottoRank.FIFTH.getRank()));
+        for (LottoRank lottoRank : LottoRank.values()) {
+            prize += lottoRank.prizeResult(lottoResult.getKey(lottoRank.getRank()));
+        }
         return prize;
     }
 
