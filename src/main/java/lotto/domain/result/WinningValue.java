5b3ca692367262c41b9504a31ca8eb85d9d44f6c
hwookim <gusdn9754@gmail.com>
2020-02-24 13:14:48 +0900

    [Lotto] refactor : 가독성 향상을 위한 _ 사용

--- src/main/java/lotto/domain/result/WinningValue.java
+++ src/main/java/lotto/domain/result/WinningValue.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public enum WinningValue {
 
     ZERO(0, 0, ""),
-    FIFTH(3, 5000, "3개 일치"),
-    FORTH(4, 50000, "4개 일치"),
-    THIRD(5, 1500000, "5개 일치"),
-    SECOND(5, 30000000, "5개 일치, 보너스번호 일치"),
-    FIRST(6, 2000000000, "6개 일치");
+    FIFTH(3, 5_000, "3개 일치"),
+    FORTH(4, 50_000, "4개 일치"),
+    THIRD(5, 1_500_000, "5개 일치"),
+    SECOND(5, 30_000_000, "5개 일치, 보너스번호 일치"),
+    FIRST(6, 2_000_000_000, "6개 일치");
 
     private final int hitCount;
     private final int reward;
