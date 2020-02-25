cdbb2f59bf028f3a3d69e3c31f1d3ccb60697c0e
minuyim <minu_yim@naver.com>
2020-02-25 13:15:49 +0900

    [feat] RepeatCount 클래스 추가
    RepeatCount 로또 게임의 반복 횟수를 담은 객체
    RepeatCountTest RepeatCount 테스트

---
+++ src/main/java/domain/RepeatCount.java
@@ -0,0 +1,41 @@
+package domain;
+
+import java.util.stream.IntStream;
+
+public class RepeatCount {
+    private static final String ERROR_NEGATIVE_MESSAGE = "반복 횟수는 음수일 수 없습니다.";
+    private static final String ERROR_EXCEED_MESSAGE = "수동 횟수가 총 횟수를 초과할 수 없습니다.";
+    private int repeatCount;
+
+    public RepeatCount(int repeatCount) {
+        validateRepeatCount(repeatCount);
+        this.repeatCount = repeatCount;
+    }
+
+    private void validateRepeatCount(int repeatCount) {
+        if (repeatCount < 0) {
+            throw new IllegalArgumentException(ERROR_NEGATIVE_MESSAGE);
+        }
+    }
+
+    public RepeatCount split(int repeatCount) {
+        validateSplitCount(repeatCount);
+        this.repeatCount -= repeatCount;
+        return new RepeatCount(repeatCount);
+    }
+
+    private void validateSplitCount(int repeatCount) {
+        if (this.repeatCount - repeatCount < 0) {
+            throw new IllegalArgumentException(ERROR_EXCEED_MESSAGE);
+        }
+    }
+
+    public IntStream toIntStream() {
+        return IntStream.range(0, repeatCount);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(repeatCount);
+    }
+}
---
+++ src/test/java/domain/RepeatCountTest.java
@@ -0,0 +1,43 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class RepeatCountTest {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void RepeatCount생성_확인(int repeatCount) {
+        RepeatCount count = new RepeatCount(repeatCount);
+        Assertions.assertThat(count)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("repeatCount", repeatCount);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -10})
+    void 잘못된생성_예외처리_확인(int totalRepeatCount) {
+        Assertions.assertThatThrownBy(() -> new RepeatCount(totalRepeatCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("반복 횟수는 음수일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,1,0", "2,0,2", "3,1,2"})
+    void 자동_구매수_반환_확인(int totalCount, int userCount, int expected) {
+        RepeatCount count = new RepeatCount(totalCount);
+        RepeatCount userRepeatCount = count.split(userCount);
+        Assertions.assertThat(count).hasFieldOrPropertyWithValue("repeatCount", expected);
+        Assertions.assertThat(userRepeatCount).hasFieldOrPropertyWithValue("repeatCount", userCount);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,1", "2,3", "10, 20"})
+    void 자동_구매수_예외_확인(int totalCount, int userCount) {
+        RepeatCount count = new RepeatCount(totalCount);
+        Assertions.assertThatThrownBy(() -> count.split(userCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수동 횟수가 총 횟수를 초과할 수 없습니다.");
+    }
+}
