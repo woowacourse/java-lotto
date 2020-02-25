befd6bfe52963c8c9fedc766a25ea439b809f8c8
begaonnuri <turtlenam0928@gmail.com>
2020-02-25 16:36:37 +0900

    feat : 수동으로 구매할 로또 수를 입력받는 기능 구현

--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -9,6 +9,7 @@ import lotto.domain.LottoMoney;
 
 public class ConsoleInputView {
 	private static final String INPUT_MONEY_MESSAGE = "구입 금액을 입력해주세요.";
+	private static final String INPUT_MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해주세요.";
 	private static final String INPUT_WINNING_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 	private static final String INPUT_BONUS_LOTTO_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
 
@@ -25,6 +26,11 @@ public class ConsoleInputView {
 
 	private static String inputMoney() {
 		System.out.println(INPUT_MONEY_MESSAGE);
+		return scanner.nextLine();
+	}
+
+	public static String inputManuaLottoCount() {
+		System.out.println(INPUT_MANUAL_LOTTO_COUNT_MESSAGE);
 		return scanner.nextLine();
 	}
 
