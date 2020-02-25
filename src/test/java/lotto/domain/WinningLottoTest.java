e5e76fe7525991331989391f8d8aea89bdb133e5
jamie9504 <jamie9504@kakao.com>
2020-02-25 16:52:32 +0900

    [리팩터링] 테스트 개선

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -3,8 +3,10 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,14 +28,18 @@ public class WinningLottoTest {
     @DisplayName("로또 당첨 내역 랭크 반환")
     @Test
     void getResult() {
+        List<String> rawLottos = new ArrayList<>();
+        rawLottos.add("1, 2, 3, 4, 5, 6");
+        rawLottos.add("1, 2, 3, 4, 5, 6");
+        LottoCount lottoCount = new LottoCount(2, 2);
+        Lottos lottos = LottosFactory.createLottos(rawLottos, lottoCount);
+
         Set<Ball> balls = new HashSet<>(
-            Arrays.asList(Ball.of(1), Ball.of(2), Ball.of(3), Ball.of(4), Ball.of(5), Ball.of(7)));
+            Arrays.asList(Ball.of(1), Ball.of(2), Ball.of(3), Ball.of(4), Ball.of(5), Ball.of(6)));
         Lotto lotto = new Lotto(balls);
-        Ball ball = Ball.of(40);
+        Ball ball = Ball.of(7);
+
         WinningLotto winningLotto = new WinningLotto(lotto, ball);
-
-        Lottos lottos = LottosFactory.createAutoLottos(new LottoCount(2));
-
         TotalResult totalResult = winningLotto.getResult(lottos);
         assertThat(totalResult.getProfitRate()).isEqualTo(200000000);
     }
