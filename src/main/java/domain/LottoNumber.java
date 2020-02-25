03dece5438a5afb6987bc0d7b5858177e4ca85b0
YerinCho <forjustice9839@gmail.com>
2020-02-25 15:53:35 +0900

    [Lotto] feat : LottoNumber
     로또 숫자의 유효성 검사 추가

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -2,10 +2,18 @@ package domain;
 
 public class LottoNumber {
 
-    private int lottoNumber;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private final int lottoNumber;
 
     public LottoNumber(int lottoNumber) {
-        this.lottoNumber=lottoNumber;
+        validateLottoNumber(lottoNumber);
+        this.lottoNumber = lottoNumber;
     }
 
+    void validateLottoNumber(int lottoNumber) {
+        if (lottoNumber > MAX_LOTTO_NUMBER || lottoNumber < MIN_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("로또 숫자의 범위가 올바르지 않습니다.");
+        }
+    }
 }
