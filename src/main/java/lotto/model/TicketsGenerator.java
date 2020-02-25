9098a790461f7d6f5884a54c3014e0579cf7e36f
kimjunseok <jnunseok@gmail.com>
2020-02-25 13:59:57 +0900

    refactor : shuffle를 사용하여 무작위숫자 6개 리턴

--- src/main/java/lotto/model/TicketsGenerator.java
+++ src/main/java/lotto/model/TicketsGenerator.java
@@ -1,13 +1,16 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TicketsGenerator {
 
     private final static Random RANDOM = new Random();
+    private static List<Integer> lottoNumbers;
 
     public static List<Ticket> createAutoTickets(int autoNumbers) {
         List<Ticket> tickets = new ArrayList<>();
@@ -18,11 +21,9 @@ public class TicketsGenerator {
     }
 
     private static List<LottoNumber> createAutoTicket() {
-        return RANDOM.ints(LottoNumber.MINIMUM_NUMBER, LottoNumber.MAXIMUM_NUMBER + 1)
-            .distinct()
-            .limit(WinNumbers.LOTTO_NUMBER_LENGTH)
-            .sorted()
-            .mapToObj(LottoNumber::getLottoNumber)
+        lottoNumbers = IntStream.range(1, 46).boxed().collect(Collectors.toList());
+        Collections.shuffle(lottoNumbers);
+        return lottoNumbers.stream().limit(6).sorted().map(LottoNumber::getLottoNumber)
             .collect(Collectors.toList());
     }
 }
