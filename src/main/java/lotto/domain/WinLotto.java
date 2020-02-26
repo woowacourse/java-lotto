f06dfb5824bf5fb769f032f1c30af47b03938df0
ordinCode <ysh1346@naver.com>
2020-02-26 09:53:42 +0900

    refactor : WinLotto.java
     보너스볼과 당첨번호 중복 예외처리

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -3,19 +3,29 @@ package lotto.domain;
 import lotto.util.LottoUtils;
 
 public class WinLotto {
-    public static final String ERROR_MESSAGE_SIZE = "6개의 숫자가 아닙니다.";
+    private static final String ERROR_MESSAGE_SIZE = "6개의 숫자가 아닙니다.";
+    private static final String ERROR_MESSAGE_BONUS_OVERLAP = "당첨번호에 보너스볼이 포함되어 있습니다.";
     private final Lotto winLotto;
     private final BonusBall bonusBall;
 
     public WinLotto(String[] winLotto, String bonusBall) {
-        validateSize(winLotto);
+        validate(winLotto, bonusBall);
         this.winLotto = new Lotto(LottoUtils.toLottoNoSet(winLotto));
         this.bonusBall = new BonusBall(bonusBall);
     }
 
-    private void validateSize(String[] winLotto) {
+    private void validate(String[] winLotto, String bonusBall) {
         if (winLotto.length != 6) {
             throw new IllegalArgumentException(ERROR_MESSAGE_SIZE);
+        }
+        for (String winNumber : winLotto) {
+            isBonusOverlap(bonusBall, winNumber);
+        }
+    }
+
+    private void isBonusOverlap(String bonusBall, String number) {
+        if (number.equals(bonusBall)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BONUS_OVERLAP);
         }
     }
 
--- src/test/java/lotto/domain/WinLottoTest.java
+++ src/test/java/lotto/domain/WinLottoTest.java
@@ -40,11 +40,21 @@ public class WinLottoTest {
 	@DisplayName("전달받은 로또에서 보너스볼과 일치하는게 있는지 테스트")
 	@Test
 	void isMatchBonus() {
-        Set<LottoNo> numbers = IntStream.range(7, 13)
-                .boxed()
-                .map(LottoNo::new)
-                .collect(Collectors.toSet());
-        Lotto lotto = new Lotto(numbers);
-        assertThat(winLotto.isMatchBonus(lotto)).isTrue();
-    }
+		Set<LottoNo> numbers = IntStream.range(7, 13)
+				.boxed()
+				.map(LottoNo::new)
+				.collect(Collectors.toSet());
+		Lotto lotto = new Lotto(numbers);
+		assertThat(winLotto.isMatchBonus(lotto)).isTrue();
+	}
+
+	@DisplayName("보너스볼과 당첨번호가 중복 예외처리")
+	@Test
+	void BonusBallInWinLotto() {
+		String[] winLotto = new String[]{"1", "2", "3", "4", "5", "6"};
+		String bonusBall = "4";
+		assertThatThrownBy(() -> new WinLotto(winLotto, bonusBall))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("당첨번호에 보너스볼이 포함되어 있습니다.");
+	}
 }
