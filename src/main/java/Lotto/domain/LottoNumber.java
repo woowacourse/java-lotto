b37c4fa4e3789fbb17cc0bb9d87db77e291d36c5
chws <rebeccachoosir@gmail.com>
2020-02-26 12:26:03 +0900

    feat: 로또넘버 클래스 생성 기능 구현

---
+++ src/main/java/Lotto/domain/LottoNumber.java
@@ -0,0 +1,20 @@
+package Lotto.domain;
+
+public class LottoNumber {
+    private final static int MIN_LOTTO_NUMBER = 1;
+    private final static int MAX_LOTTO_NUMBER = 45;
+    private final static String OUT_OF_RANGE = "로또번호는 1~45의 범위입니다.";
+
+    private int number;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(OUT_OF_RANGE);
+        }
+    }
+}
