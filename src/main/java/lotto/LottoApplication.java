90eaf2ae0ff766ca95408d2a3981eba091a334ef
juyeongjung <juyeong.dev@gmail.com>
2020-02-26 11:11:58 +0900

    refactor : 수동 번호로부터 로또 생성하는 기능 추가

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import lotto.controller.RankCalculator;
 import lotto.domain.Lotto;
+import lotto.domain.LottoAmount;
 import lotto.domain.LottoNumber;
 import lotto.domain.Money;
 import lotto.domain.Rank;
@@ -19,19 +20,32 @@ import lotto.view.OutputView;
 public class LottoApplication {
 	public static void main(String[] args) {
 		Money budget = new Money(NumberParser.parseNumber(InputView.inputAsMoney()));
-		int purchasedLottoAmount = calculateLottoAmount(budget);
+		int manualLottoAmount = NumberParser.parseNumber(InputView.inputAsManualLottoAmount());
+		LottoAmount purchasedLottoAmount = new LottoAmount(calculateLottoAmount(budget),
+			manualLottoAmount);
 		OutputView.showPurchasedLottoCount(purchasedLottoAmount);
 
-		NumberGenerator autoLottoNumberGenerator = new LottoNumberGenerator();
-		List<Lotto> purchasedLotto = generateLottoByAmount(purchasedLottoAmount, autoLottoNumberGenerator);
-		OutputView.showPurchasedLottoNumbers(purchasedLotto);
+		List<Lotto> manualLotto = generateManualLotto(purchasedLottoAmount.getManualLottoAmount());
+		List<Lotto> autoLotto = generateAutoLottoByAmount(purchasedLottoAmount);
+		List<Lotto> totalLotto = concatTotalLotto(manualLotto, autoLotto);
 
-		List<Rank> resultRanks = RankCalculator.calculateMultiple(purchasedLotto, produceWinningNumber());
+		OutputView.showPurchasedLottoNumbers(totalLotto);
+		showResultRanks(budget, totalLotto);
+	}
+
+	private static void showResultRanks(Money budget, List<Lotto> totalLotto) {
+		List<Rank> resultRanks = RankCalculator.calculateMultiple(totalLotto, produceWinningNumber());
 		OutputView.showResult(budget, resultRanks);
 	}
 
+	private static List<Lotto> generateAutoLottoByAmount(LottoAmount purchasedLottoAmount) {
+		NumberGenerator autoLottoNumberGenerator = new LottoNumberGenerator();
+		return generateLottoByAmount(purchasedLottoAmount.getAutoLottoAmount(),
+			autoLottoNumberGenerator);
+	}
+
 	private static WinningNumber produceWinningNumber() {
-		List<Integer> inputWinningNumbers = NumberParser.winningNumberParse(InputView.inputAsWinningNumbers());
+		List<Integer> inputWinningNumbers = NumberParser.lottoNumberParse(InputView.inputAsWinningNumbers());
 		Lotto winningNumberLotto = new Lotto(inputWinningNumbers);
 		LottoNumber bonusNumber = new LottoNumber(NumberParser.parseNumber(InputView.inputAsBonusNumber()));
 
--- src/main/java/lotto/controller/LottoManager.java
+++ src/main/java/lotto/controller/LottoManager.java
@@ -7,11 +7,13 @@ import java.util.stream.IntStream;
 import lotto.domain.Lotto;
 import lotto.domain.Money;
 import lotto.utils.NumberGenerator;
+import lotto.utils.NumberParser;
+import lotto.view.InputView;
 
 public class LottoManager {
 	private static final Money LOTTO_PRICE = new Money(1_000);
 
-	public static int calculateLottoAmount(Money money) {
+	public static long calculateLottoAmount(Money money) {
 		return money.getQuotient(LOTTO_PRICE);
 	}
 
@@ -23,5 +25,18 @@ public class LottoManager {
 		return IntStream.range(0, amount)
 			.mapToObj(x -> generateSingleLotto(lottoNumberGenerator))
 			.collect(Collectors.toList());
+	}
+
+	public static List<Lotto> generateManualLotto(int manualLottoAmount) {
+		return InputView.inputAsManualLottoNumbers(manualLottoAmount)
+			.stream()
+			.map(NumberParser::lottoNumberParse)
+			.map(Lotto::new)
+			.collect(Collectors.toList());
+	}
+
+	public static List<Lotto> concatTotalLotto(List<Lotto> autoLotto, List<Lotto> manualLotto) {
+		autoLotto.addAll(manualLotto);
+		return autoLotto;
 	}
 }
