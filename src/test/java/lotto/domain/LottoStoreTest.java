390b78f395197dcb8bb0950dbfb2d339fd6c5bc6
Seyun <ksy90101@gmail.com>
2020-02-26 14:47:41 +0900

    [refator] 로또가 수동으로 생성되는지 확인하는 테스트 추가

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -2,19 +2,34 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoStoreTest {
     @Test
     @DisplayName("로또가 자동으로 생성되는지 확인하는 테스트")
-    void correct_lotto_ticket_test() {
+    void correct_automatic_lotto_ticket_test() {
         PurchaseAmount purchaseAmount = new PurchaseAmount("5000");
         LottoTicketNumber lottoTicketNumber = new LottoTicketNumber(purchaseAmount.giveLottoTicketNumber(), 0);
         LottoStore lottoStore = new LottoStore(lottoTicketNumber);
         LottoTickets lottoTickets = lottoStore.generateLottoTickets(new ArrayList<>());
         assertThat(lottoTickets.getLottoTickets()).hasSize(5);
+    }
+
+    @Test
+    @DisplayName("로또가 수동으로 생성되는지 확인하는 테스트")
+    void correct_manual_lotto_ticket_test() {
+        List<String> manualLottoBallsInputs = new ArrayList<>();
+        manualLottoBallsInputs.add("1,2,3,4,5,6");
+        PurchaseAmount purchaseAmount = new PurchaseAmount("1000");
+        LottoTicketNumber lottoTicketNumber = new LottoTicketNumber(purchaseAmount.giveLottoTicketNumber(), 1);
+        LottoStore lottoStore = new LottoStore(lottoTicketNumber);
+        LottoTickets lottoTickets = lottoStore.generateLottoTickets(manualLottoBallsInputs);
+        assertThat(lottoTickets.getLottoTickets()).hasSize(1);
     }
 }
