29da01e49b0a212e25d92f7e15b0fbddd5b1836c
chws <rebeccachoosir@gmail.com>
2020-02-26 17:00:18 +0900

    feat: 로또 넘버 객체가 같은지 확인해주는 equals/hashCode 메소드 오버라이딩

--- src/main/java/Lotto/domain/LottoNumber.java
+++ src/main/java/Lotto/domain/LottoNumber.java
@@ -1,20 +1,35 @@
 package Lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
     private final static int MIN_LOTTO_NUMBER = 1;
     private final static int MAX_LOTTO_NUMBER = 45;
     private final static String OUT_OF_RANGE = "로또번호는 1~45의 범위입니다.";
 
-    private int number;
+    private int lottoNumber;
 
     public LottoNumber(int number) {
         validate(number);
-        this.number = number;
+        this.lottoNumber = number;
     }
 
     private void validate(int number) {
         if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException(OUT_OF_RANGE);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
     }
 }
