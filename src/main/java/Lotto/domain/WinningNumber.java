9e46fc019a3f0b5df2c35e1ea9685b0e65b2a409
chws <rebeccachoosir@gmail.com>
2020-02-26 23:12:35 +0900

    feat: 이번주 당첨번호와 보너스넘버로 WinningNumber 인스턴스 생성하는 기능 구현

---
+++ src/main/java/Lotto/domain/WinningNumber.java
@@ -0,0 +1,18 @@
+package Lotto.domain;
+
+public class WinningNumber {
+    private Lotto winningLotto;
+    private LottoNumber bonusNumber;
+
+    public WinningNumber(Lotto winningLotto, LottoNumber bonusNumber) {
+        this.winningLotto = winningLotto;
+        if(bonusNumberDuplicatedWithWinningNumber(winningLotto, bonusNumber)) {
+            throw new IllegalArgumentException("보너스넘버는 당첨번호와 중복될 수 없습니다.");
+        }
+        this.bonusNumber = bonusNumber;
+    }
+
+    private boolean bonusNumberDuplicatedWithWinningNumber(Lotto winningLotto, LottoNumber bonusNumber) {
+        return winningLotto.hasBonusNumber(bonusNumber);
+    }
+}
--- src/test/java/utils/NumberParserTest.java
+++ src/test/java/utils/NumberParserTest.java
@@ -2,11 +2,9 @@ package utils;
 
 import Lotto.utils.NumberParser;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
