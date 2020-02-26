c34ce2caeb8b60fad8c72c53e73af9af9f1b6379
kimjunseok <jnunseok@gmail.com>
2020-02-26 15:37:20 +0900

    refactor : 1단계 피드백 반영 - 요구사항에 없는 조건 제거

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -3,24 +3,20 @@ package lotto.model;
 import java.util.Arrays;
 import lotto.exception.NotMultipleOfThousandException;
 import lotto.exception.NotNumberException;
-import lotto.exception.OverRangeException;
 
 public class Money {
 
     public static final int PAYMENT_UNIT = 1_000;
-    private static final int MINIMUM_PAYMENT = 1_000;
-    private static final int MAXIMUM_PAYMENT = 100_000;
     private static final int PERCENT = 100;
     private static final int ZERO = 0;
     public static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "숫자를 입력하세요.";
     public static final String OVER_RANGE_EXCEPTION_MESSAGE = "범위를 벗어났습니다.";
     private static final String UNIT_EXCEPTION_MESSAGE = "천 단위로 입력하세요.";
 
-    private int money = 0;
+    private int money;
 
     public Money(String input) {
         int money = validateNotNumber(input);
-        validateValueRange(money);
         validateUnitThousand(money);
         this.money = money;
     }
@@ -30,12 +26,6 @@ public class Money {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new NotNumberException(NUMBER_FORMAT_EXCEPTION_MESSAGE);
-        }
-    }
-
-    private void validateValueRange(int payment) {
-        if (!(payment >= MINIMUM_PAYMENT && payment <= MAXIMUM_PAYMENT)) {
-            throw new OverRangeException(OVER_RANGE_EXCEPTION_MESSAGE);
         }
     }
 
