be82404d3bea74c23f0c06bc1c52af03bbf20c44
kimjunseok <jnunseok@gmail.com>
2020-02-26 20:36:50 +0900

    feat : 받을 수 있는 최대값인지 판단하는 메서드 구현

--- src/main/java/lotto/model/ManualNumber.java
+++ src/main/java/lotto/model/ManualNumber.java
@@ -1,12 +1,17 @@
 package lotto.model;
 
 import lotto.exception.NotNumberException;
+import lotto.exception.OverRangeException;
 
 public class ManualNumber {
+
+    private static final String MAXIMUM_MANUAL_NUMBER = "수동으로 구매할 수 있는 수가 너무 큽니다.";
     private int manualNumber;
 
-    public ManualNumber(String input) {
+    public ManualNumber(String input, int ticketCount) {
         int manualNumber = validateNumber(input);
+        validateMaximumNumber(manualNumber, ticketCount);
+        this.manualNumber = manualNumber;
     }
 
     private int validateNumber(String input) {
@@ -14,6 +19,12 @@ public class ManualNumber {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new NotNumberException(Money.NUMBER_FORMAT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateMaximumNumber(int manualNumber, int ticketCount) {
+        if (manualNumber > ticketCount) {
+            throw new OverRangeException(MAXIMUM_MANUAL_NUMBER);
         }
     }
 }
--- src/test/java/lotto/model/ManualNumberTest.java
+++ src/test/java/lotto/model/ManualNumberTest.java
@@ -3,17 +3,28 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.exception.NotNumberException;
+import lotto.exception.OverRangeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ManualNumberTest {
 
     @Test
-    @DisplayName("입력이 숫자인지 판단")
-    void ManualNumber_isNumber() {
+    @DisplayName("입력이 숫자인지 판단하는 테스트")
+    void ManualNumber_Is_Number() {
         assertThatThrownBy(() -> {
-            new ManualNumber("q");
+            new ManualNumber("q", 1);
         }).isInstanceOf(NotNumberException.class)
             .hasMessage("숫자를 입력하세요.");
+    }
+
+    @Test
+    @DisplayName("최대값이 지불한 돈보다 큰지 판단하는 테스트")
+    void ManualNumber_Is_Maximum_Number() {
+        assertThatThrownBy(() -> {
+            Money money = new Money("10000");
+            ManualNumber manualNumber = new ManualNumber("11", money.getMoney() / Money.PAYMENT_UNIT);
+        }).isInstanceOf(OverRangeException.class)
+            .hasMessage("수동으로 구매할 수 있는 수가 너무 큽니다.");
     }
 }
