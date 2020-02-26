22476ed77e35b36e71733457c96dd1d688d37b06
juyeongjung <juyeong.dev@gmail.com>
2020-02-26 11:13:15 +0900

    refactor : 수동번호 구매 갯수도 출력할수 있도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoAmount;
 import lotto.domain.Money;
 import lotto.domain.Rank;
 
@@ -14,18 +15,18 @@ public class OutputView {
 	private static final String EMPTY_STRING = "";
 	private static final String EARNING_RATE_MESSAGE = "총 수익률은 %d %%입니다.";
 	private static final String SECOND_RANK_ADDITIONAL_MESSAGE = "보너스 볼 일치 ";
-	private static final String PURCHASED_LOTTO_MESSAGE = "개를 구매했습니다.";
+	private static final String PURCHASED_LOTTO_MESSAGE = "수동으로 %d개, 자동으로 %d를 구매했습니다.";
 	private static final String NEW_LINE = "\n";
 	private static final String STATISTICS_FORMAT = "%d개 일치 %s%s원 - %d개%s";
 
 	public static void showEarningRate(Money boughtLottoMoney, List<Rank> ranks) {
-		int earningRate = ranks.stream()
+		long earningRate = ranks.stream()
 			.filter(Objects::nonNull)
 			.map(Rank::getReward)
 			.reduce(Money::sum)
 			.map(sum -> sum.multiple(PERCENTAGE_MULTIPLE))
 			.map(sum -> sum.getQuotient(boughtLottoMoney))
-			.orElse(0);
+			.orElse(0L);
 
 		System.out.printf(EARNING_RATE_MESSAGE, earningRate);
 	}
@@ -52,8 +53,9 @@ public class OutputView {
 		return EMPTY_STRING;
 	}
 
-	public static void showPurchasedLottoCount(int purchasedLottoCount) {
-		System.out.println(purchasedLottoCount + PURCHASED_LOTTO_MESSAGE);
+	public static void showPurchasedLottoCount(LottoAmount purchasedLottoAmount) {
+		System.out.printf(PURCHASED_LOTTO_MESSAGE, purchasedLottoAmount.getManualLottoAmount(),
+			purchasedLottoAmount.getManualLottoAmount());
 	}
 
 	public static void showPurchasedLottoNumbers(List<Lotto> purchasedLottoNumbers) {
