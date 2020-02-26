e59b5f70319979ff59db2207247f3ddb3c4717fd
chws <rebeccachoosir@gmail.com>
2020-02-26 12:20:13 +0900

    refactor: 구입 금액 생성자 입력 수정 및 그에 따른 테스트코드 리팩토링

--- src/main/java/Lotto/domain/Money.java
+++ src/main/java/Lotto/domain/Money.java
@@ -5,24 +5,18 @@ public class Money {
 
     private final int money;
 
-    public Money(String moneyInput) {
-        validate(moneyInput);
-        this.money = Integer.parseInt(moneyInput);
+    public Money(int money) {
+        validate(money);
+        this.money = money;
     }
 
-    private void validate(String moneyInput) {
-        try {
-            Integer.parseInt(moneyInput);
-        } catch (IllegalArgumentException e){
-            throw new IllegalArgumentException("구입 금액은 정수입니다.");
-        }
-
-        if(isNegativeNumber(moneyInput)) {
+    private void validate(int money) {
+        if(isNegativeNumber(money)) {
             throw new IllegalArgumentException("구입 금액은 0보다 커야합니다.");
         }
     }
 
-    private boolean isNegativeNumber(String moneyInput) {
-        return Integer.parseInt(moneyInput) <= MIN_MONEY_AMOUNT;
+    private boolean isNegativeNumber(int money) {
+        return money <= MIN_MONEY_AMOUNT;
     }
 }
--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -2,7 +2,6 @@ package domain;
 
 import Lotto.domain.Money;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,26 +9,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MoneyTest {
-    @Test
-    @DisplayName("구입 금액이 옳게 입력된 것 확인 테스트")
-    void rightInputTest() {
-        Money money = new Money("10000");
+    @ParameterizedTest
+    @DisplayName("구입 금액이 올바른 범위 안에 있을 때")
+    @ValueSource(ints = {1, 25, 500000, 39939})
+    void rightInput(int input) {
+        Money money = new Money(input);
         assertThat(money).isNotNull();
     }
 
     @ParameterizedTest
-    @DisplayName("구입 금액이 정수가 아닐 때")
-    @ValueSource(strings = {" ", "n", "3.3"})
-    void inputNotNumber(String input) {
+    @DisplayName("구입 금액이 0보다 작을 때")
+    @ValueSource(ints = {-1, 0})
+    void inputNotNaturalNumber(int input) {
         assertThatThrownBy(() -> new Money(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자");
-    }
-
-    @Test
-    @DisplayName("구입 금액이 음수일 때")
-    void inputNotNaturalNumber() {
-        assertThatThrownBy(() -> new Money("-1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("0보다");
     }
