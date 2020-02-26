4c925272a5dc1e3d4ded6a68cec63e8972d1f3c6
kouz <lastpuzly@gmail.com>
2020-02-26 11:56:59 +0900

    feat: 생성 전략 삭제, 자동 티켓 생성 팩토리 생성

--- src/main/java/lotto/domain/AutoTicketsFactory.java
+++ src/main/java/lotto/domain/AutoTicketsFactory.java
@@ -4,17 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CreateRandomTicketsStrategy implements CreateNumbersStrategy {
+public class AutoTicketsFactory {
 	private static final int LOTTO_NUMBER_SIZE = 6;
 
-	@Override
-	public LottoTickets create(PurchasingAmount purchasingAmount) {
+	public static LottoTickets create(PurchasingAmount purchasingAmount) {
 		List<LottoTicket> lottoTickets = new ArrayList<>();
 		purchasingAmount.forEachRemaining(count -> createLottoTicket(lottoTickets));
 		return new LottoTickets(lottoTickets);
 	}
 
-	private void createLottoTicket(List<LottoTicket> lottoTickets) {
+	private static void createLottoTicket(List<LottoTicket> lottoTickets) {
 		final List<LottoNumber> randomNumbers = LottoNumberRepository.shuffledLottoNumbers();
 		lottoTickets.add(new LottoTicket(
 			randomNumbers.stream()
