0b043665883056abb8c621d4947b6b831fdca837
JunSeong <ebseud6135@naver.com>
2020-02-26 20:55:34 +0900

    refactor : inputView 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,21 +7,29 @@ public class InputView {
 
 	public static String inputBuyMoney() {
 		System.out.println("구입금액을 입력해 주세요.");
-		return scanner.nextLine();
+		return input();
 	}
 
 	public static String inputBonusBall() {
 		System.out.println("보너스 볼을 입력해 주세요.");
-		return scanner.nextLine();
+		return input();
 	}
 
 	public static String inputWinNumber() {
 		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-		return scanner.nextLine();
+		return input();
 	}
 
 	public static String inputManualLottoCount() {
 		System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+		return input();
+	}
+
+	public static void inputManualLotto() {
+		System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+	}
+
+	public static String input() {
 		return scanner.nextLine();
 	}
 }
