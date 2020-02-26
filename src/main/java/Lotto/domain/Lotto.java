d5f93b0214c5b5bfaeac7bb439a5d90981fba133
chws <rebeccachoosir@gmail.com>
2020-02-26 22:38:42 +0900

    feat: 로또넘버로 로또 생성하는 기능 구현

---
+++ src/main/java/Lotto/domain/Lotto.java
@@ -0,0 +1,28 @@
+package Lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private static final String LOTTO_NUMBER_DUPLICATED_MESSAGE = "잘못된 로또 번호입니다. 중복 안됨, 갯수는 6개";
+    private static final int LOTTO_NUMBER_AMOUNT = 6;
+
+    private List<LottoNumber> lottoNumbers;
+
+    public Lotto(List<LottoNumber> lottoNumbers) {
+        Set<LottoNumber> lottoNumberWithoutDuplication = new HashSet<LottoNumber>(lottoNumbers);
+        validate(lottoNumberWithoutDuplication);
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validate(Set<LottoNumber> lottoNumberWithoutDuplication) {
+        if (wrongAmountOfNumbers(lottoNumberWithoutDuplication)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATED_MESSAGE);
+        }
+    }
+
+    private boolean wrongAmountOfNumbers(Set<LottoNumber> lottoNumberWithoutDuplication) {
+        return lottoNumberWithoutDuplication.size() != LOTTO_NUMBER_AMOUNT;
+    }
+}
