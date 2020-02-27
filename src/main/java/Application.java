40c640c445a27ee8873e5971f46e255941ae4fff
toneyparky <qkrtmddhks95@gmail.com>
2020-02-27 22:31:03 +0900

    refactor: 예외처리를 checked exception까지 하도록 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -13,7 +13,7 @@ public class Application {
 	public static void main(String[] args) {
 		try {
 			LottoController.run();
-		} catch (RuntimeException e) {
+		} catch (Exception e) {
 			OutputView.printExceptionMessage(e);
 		}
 	}
