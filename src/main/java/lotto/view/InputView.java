e32020c17f4ee3872ef7e075e64664872efed3ee
moon <mjj920930@gmail.com>
2020-02-27 17:58:23 +0900

    fix : 로또 수동 구매를 위한 입력 기능 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
@@ -36,23 +39,32 @@ public class InputView {
         return scanner.nextLine();
     }
 
-    public static Integer inputManualLottoTicketCount() {
+    public static int inputManualLottoTicketCount() {
         try {
-            return generateIntegerFromInput();
+            return inputManualLottoTicketCountWithMessage();
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
             return inputManualLottoTicketCount();
         }
     }
 
-    private static Integer generateIntegerFromInput() {
-        String inputForManualLottoTicketCount = inputManualLottoTicketCountWithMessage();
-        return Integer.parseInt(inputForManualLottoTicketCount);
+    private static int inputManualLottoTicketCountWithMessage() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요");
+        return Integer.parseInt(scanner.nextLine());
     }
 
-    private static String inputManualLottoTicketCountWithMessage() {
-        System.out.println("수동으로 구매할 로또 수를 입력해 주세요");
-        return scanner.nextLine();
+    public static List<String> inputManualLottoNumbers(int manualTicketCount) {
+        try {
+            return inputManualLottoNumbersWithMessage(manualTicketCount);
+        } catch (IllegalArgumentException e) {
+            return inputManualLottoNumbers(manualTicketCount);
+        }
+    }
+
+    private static List<String> inputManualLottoNumbersWithMessage(int manualTicketCount) {
+        System.out.println("수동으로 구매할 로또 번호를 입력해 주세요.");
+        return Stream.generate(scanner::nextLine).limit(manualTicketCount)
+                .collect(toList());
     }
 
     public static WinningNumbers inputWinningNumbers() {
