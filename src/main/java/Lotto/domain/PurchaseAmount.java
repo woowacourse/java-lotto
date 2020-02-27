c33b1fe270829d11c31702de1d6e2898fd6aae21
chws <rebeccachoosir@gmail.com>
2020-02-27 10:48:27 +0900

    refactor: 구입 금액 클래스 이름 변경 Money -> PurchaseAmount

---
+++ src/main/java/Lotto/domain/PurchaseAmount.java
@@ -0,0 +1,26 @@
+package Lotto.domain;
+
+public class PurchaseAmount implements Money{
+    private static final int LOTTO_PRICE = 1_000;
+
+    private final int purchaseAmount;
+
+    public PurchaseAmount(int purchaseAmount) {
+        validate(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public void validate(int purchaseAmount) {
+        if(isLessThanMinPrice(purchaseAmount)) {
+            throw new IllegalArgumentException("구입 금액은 "+ LOTTO_PRICE +"보다 커야합니다.");
+        }
+    }
+
+    private boolean isLessThanMinPrice(int purchaseAmount) {
+        return purchaseAmount <= LOTTO_PRICE;
+    }
+
+    public int calculateLottoAmount(int purchaseAmount) {
+        return purchaseAmount / LOTTO_PRICE;
+    }
+}
--- src/test/java/domain/PurchaseAmountTest.java
+++ src/test/java/domain/PurchaseAmountTest.java
@@ -1,6 +1,6 @@
 package domain;
 
-import Lotto.domain.Money;
+import Lotto.domain.PurchaseAmount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -8,20 +8,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class MoneyTest {
+class PurchaseAmountTest {
     @ParameterizedTest
     @DisplayName("구입 금액이 올바른 범위 안에 있을 때")
     @ValueSource(ints = {1, 25, 500000, 39939})
     void rightInput(int input) {
-        Money money = new Money(input);
-        assertThat(money).isNotNull();
+        PurchaseAmount purchaseAmount = new PurchaseAmount(input);
+        assertThat(purchaseAmount).isNotNull();
     }
 
     @ParameterizedTest
     @DisplayName("구입 금액이 0보다 작을 때")
     @ValueSource(ints = {-1, 0})
     void inputNotNaturalNumber(int input) {
-        assertThatThrownBy(() -> new Money(input))
+        assertThatThrownBy(() -> new PurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("0보다");
     }
