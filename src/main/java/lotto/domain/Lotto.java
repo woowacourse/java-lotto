fbb850c3bc686136c62adfb0be28a40ff09feb53
ordinCode <ysh1346@naver.com>
2020-02-27 16:38:44 +0900

    style : googleStyle 적용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,57 +7,60 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Lotto {
-    public static final int LOTTO_SIZE = 6;
-    public static final String ERROR_MESSAGE_SIZE = "6개의 숫자가 아닙니다.";
-    private static final String ERROR_MESSAGE_REPEAT_NUMBER = "숫자는 중복될 수 없습니다.";
-    private static final String ERROR_MESSAGE_NULL_POINT_LOTTO = "입력값이 비어있습니다.";
 
-    private final Set<LottoNo> lottoNumbers;
+  public static final int LOTTO_SIZE = 6;
+  public static final String ERROR_MESSAGE_SIZE = "6개의 숫자가 아닙니다.";
+  private static final String ERROR_MESSAGE_REPEAT_NUMBER = "숫자는 중복될 수 없습니다.";
+  private static final String ERROR_MESSAGE_NULL_POINT_LOTTO = "입력값이 비어있습니다.";
 
-    public Lotto(List<LottoNo> lottoNumbers) {
-        validate(lottoNumbers);
-        this.lottoNumbers = new TreeSet<>(lottoNumbers);
+  private final Set<LottoNo> lottoNumbers;
+
+  public Lotto(List<LottoNo> lottoNumbers) {
+    validate(lottoNumbers);
+    this.lottoNumbers = new TreeSet<>(lottoNumbers);
+  }
+
+  private void validate(List<LottoNo> lottoNumbers) {
+    if (lottoNumbers == null) {
+      throw new IllegalArgumentException(ERROR_MESSAGE_NULL_POINT_LOTTO);
     }
-
-    private void validate(List<LottoNo> lottoNumbers) {
-        if (lottoNumbers == null) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_NULL_POINT_LOTTO);
-        }
-        if (lottoNumbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_SIZE);
-        }
-        if (new HashSet<>(lottoNumbers).size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_REPEAT_NUMBER);
-        }
+    if (lottoNumbers.size() != LOTTO_SIZE) {
+      throw new IllegalArgumentException(ERROR_MESSAGE_SIZE);
     }
-
-    public boolean contains(LottoNo lottoNo) {
-        return lottoNumbers.contains(lottoNo);
+    if (new HashSet<>(lottoNumbers).size() != LOTTO_SIZE) {
+      throw new IllegalArgumentException(ERROR_MESSAGE_REPEAT_NUMBER);
     }
+  }
 
-    public int calculateHitCount(Lotto winLotto) {
-        return (int) lottoNumbers.stream()
-                .filter(x -> winLotto.contains(x))
-                .count();
-    }
+  public boolean contains(LottoNo lottoNo) {
+    return lottoNumbers.contains(lottoNo);
+  }
 
-    @Override
-    public String toString() {
-        return lottoNumbers.toString();
-    }
+  public int calculateHitCount(Lotto winLotto) {
+    return (int) lottoNumbers.stream()
+        .filter(x -> winLotto.contains(x))
+        .count();
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        Lotto lotto = (Lotto) o;
-        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
-    }
+  @Override
+  public String toString() {
+    return lottoNumbers.toString();
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(lottoNumbers);
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Lotto lotto = (Lotto) o;
+    return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lottoNumbers);
+  }
 }
