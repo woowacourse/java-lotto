01276300ac6da0c7aae78db6a96782650e0a4332
sorin <jnsorn@gmail.com>
2020-02-27 13:44:29 +0900

    feat : 수동으로 구매할 로또 수를 입력 받는 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,5 @@
 package lotto.controller;
 
-import static lotto.view.ConsoleInputView.*;
-import static lotto.view.ConsoleOutputView.*;
-
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoParser;
 import lotto.domain.lotto.Lottos;
@@ -13,36 +10,54 @@ import lotto.domain.lottonumber.LottoNumber;
 import lotto.domain.result.LottoWinningResult;
 import lotto.domain.result.WinningLotto;
 
+import static lotto.view.ConsoleInputView.*;
+import static lotto.view.ConsoleOutputView.*;
+
 public class LottoController {
-	public void run() {
-		LottoMoney inputLottoMoney = receiveInputMoney();
-		int numberOfLotto = inputLottoMoney.calculateNumberOfLotto();
-		printPurchaseCompleteMessage(numberOfLotto);
-<<<<<<< HEAD
 
-=======
->>>>>>> jnsorn
-		Lottos lottos = LottosGenerator.generate(numberOfLotto);
-		printPurchasedLotto(lottos);
+    private static final String NUMBER_OF_MANUAL_IS_WRONG_RANGE_MESSAGE = "수동 로또 갯수는 0보다 작거나 구매한 로또 갯수보다 클 수 없습니다.";
+    private static final String NUMBER_OF_MANUAL_IS_WRONG_TYPE_MESSAGE = "숫자를 입력해주세요";
 
-		WinningLotto winningLotto = receiveWinningLotto();
-		LottoWinningResult winningResult = new LottoWinningResult(lottos, winningLotto);
-		printWinningResult(winningResult.getLottoRankCount());
-		printWinningRatio(winningResult.calculateWinningRatio(inputLottoMoney));
-	}
+    public void run() {
+        LottoMoney inputLottoMoney = receiveInputMoney();
+        int numberOfLotto = inputLottoMoney.calculateNumberOfLotto();
+        int numberOfManualLotto = receiveNumberOfManualLotto(numberOfLotto);
+        printPurchaseCompleteMessage(numberOfLotto);
+        Lottos lottos = LottosGenerator.generate(numberOfLotto);
+        printPurchasedLotto(lottos);
 
-	private static LottoMoney receiveInputMoney() {
-		try {
-			return new LottoMoney(inputMoney());
-		} catch (InvalidLottoMoneyException ime) {
-			printExceptionMessage(ime.getMessage());
-			return receiveInputMoney();
-		}
-	}
+        WinningLotto winningLotto = receiveWinningLotto();
+        LottoWinningResult winningResult = new LottoWinningResult(lottos, winningLotto);
+        printWinningResult(winningResult.getLottoRankCount());
+        printWinningRatio(winningResult.calculateWinningRatio(inputLottoMoney));
+    }
 
-	private static WinningLotto receiveWinningLotto() {
-		Lotto inputWinningLotto = new Lotto(LottoParser.parser(inputWinningLottoNumber()));
-		LottoNumber inputBonusNumber = LottoNumber.valueOf(inputBonusLottoNumber());
-		return new WinningLotto(inputWinningLotto, inputBonusNumber);
-	}
+    private static LottoMoney receiveInputMoney() {
+        try {
+            return new LottoMoney(inputMoney());
+        } catch (InvalidLottoMoneyException ime) {
+            printExceptionMessage(ime.getMessage());
+            return receiveInputMoney();
+        }
+    }
+
+    private static int receiveNumberOfManualLotto(int numberOfLotto) {
+        int numberOfManualLotto;
+        try {
+            numberOfManualLotto = Integer.parseInt(inputNumberOfManualLotto());
+        } catch (IllegalArgumentException e) {
+            printExceptionMessage(NUMBER_OF_MANUAL_IS_WRONG_TYPE_MESSAGE);
+            return receiveNumberOfManualLotto(numberOfLotto);
+        }
+        if (numberOfManualLotto < 0 || numberOfManualLotto > numberOfLotto) {
+            throw new IllegalArgumentException(NUMBER_OF_MANUAL_IS_WRONG_RANGE_MESSAGE);
+        }
+        return numberOfManualLotto;
+    }
+
+    private static WinningLotto receiveWinningLotto() {
+        Lotto inputWinningLotto = new Lotto(LottoParser.parser(inputWinningLottoNumber()));
+        LottoNumber inputBonusNumber = LottoNumber.valueOf(inputBonusLottoNumber());
+        return new WinningLotto(inputWinningLotto, inputBonusNumber);
+    }
 }
--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -6,6 +6,7 @@ public class ConsoleInputView {
 	private static final String INPUT_MONEY_MESSAGE = "구입 금액을 입력해주세요.";
 	private static final String INPUT_WINNING_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 	private static final String INPUT_BONUS_LOTTO_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+	private static final String INPUT_NUMBER_OF_MANUAL_LOTTO = "수동으로 구매할 로또 수를 입력해주세요.";
 
 	private static Scanner SCANNER = new Scanner(System.in);
 
@@ -24,6 +25,11 @@ public class ConsoleInputView {
 
 	public static String inputBonusLottoNumber() {
 		System.out.println(INPUT_BONUS_LOTTO_NUMBER_MESSAGE);
+		return SCANNER.nextLine();
+	}
+
+	public static String inputNumberOfManualLotto() {
+		System.out.println(INPUT_NUMBER_OF_MANUAL_LOTTO);
 		return SCANNER.nextLine();
 	}
 }
