d73a3c7119dfdaa2390e44f5be61bd0893cb1be6
taeheon <dqrd123@gmail.com>
2020-02-27 18:38:14 +0900

    refactor : 피드백 반영
    컨벤션에 맞게 띄어쓰기, 중괄호 수정
    메서드, 변수명 명확하게 수정
    LottoFactory 내의 메소드 makeManualLottos 추출, addAutoLotto 추출

---
+++ src/main/java/lotto/domain/result/Count.java
@@ -0,0 +1,13 @@
+package lotto.domain.result;
+
+public class Count {
+    private int count = 0;
+
+    public void addCount() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
