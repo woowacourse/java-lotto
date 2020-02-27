b312233ce10340dace9a8db449394df66085feab
chws <rebeccachoosir@gmail.com>
2020-02-27 12:17:41 +0900

    feat: 구입금액, 당첨 로또, 보너스넘버 입력 기능 구현

---
+++ src/main/java/Lotto/views/InputView.java
@@ -0,0 +1,25 @@
+package Lotto.views;
+import java.util.Scanner;
+
+public class InputView {
+    private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputAsPurchaseAmount() {
+        System.out.println(INPUT_MONEY_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputAsWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputAsBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return scanner.nextLine();
+    }
+}
