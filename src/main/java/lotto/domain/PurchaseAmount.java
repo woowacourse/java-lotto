10f52f91c2f227c8c4f8d21695ee62f78ddd3bad
김효찬 <kom1230@hanyang.ac.kr>
2020-02-27 16:32:31 +0900

    [refactor] 로또 구매 양을 수동과 자동으로 구분

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -3,28 +3,37 @@ package lotto.domain;
 import lotto.domain.exception.OutOfRangeException;
 
 public class PurchaseAmount {
-    private static final int MINIMUM_MONEY = 1000;
-    private static final int LOTTO_PRICE = 1000;
-    private static final String OUT_OF_RANGE_PURCHASE_MONEY_EXCEPTION_MESSAGE = "Input money out of range (minimum - 1000).";
+    private static final int MINIMUM_AMOUNT = 1;
+    private static final String OUT_OF_RANGE_PURCHASE_MONEY_EXCEPTION_MESSAGE = "Input money out of range (minimum : 1000).";
+    private static final String OUT_OF_RANGE_LOTTO_AMOUNT_EXCEPTION_MESSAGE = "Lotto amount out of range (minimum : 0).";
 
-    private final int purchaseAmount;
+    private final int randomLottoAmount;
+    private final int manualLottoAmount;
 
-    private PurchaseAmount(int purchaseAmount) {
-        this.purchaseAmount = purchaseAmount;
+    public PurchaseAmount(int randomLottoAmount, int manualLottoAmount) {
+        validatePurchaseMoney(randomLottoAmount, manualLottoAmount);
+        this.randomLottoAmount = randomLottoAmount;
+        this.manualLottoAmount = manualLottoAmount;
     }
 
-    public static PurchaseAmount calculate(int purchaseMoney) {
-        validatePurchaseMoney(purchaseMoney);
-        return new PurchaseAmount(purchaseMoney / LOTTO_PRICE);
-    }
-
-    private static void validatePurchaseMoney(int purchaseMoney) {
-        if (purchaseMoney < MINIMUM_MONEY) {
+    private static void validatePurchaseMoney(int randomLottoAmount, int manualLottoAmount) {
+        if (randomLottoAmount + manualLottoAmount < MINIMUM_AMOUNT) {
             throw new OutOfRangeException(OUT_OF_RANGE_PURCHASE_MONEY_EXCEPTION_MESSAGE);
+        }
+        if (randomLottoAmount < 0 || manualLottoAmount < 0) {
+            throw new OutOfRangeException(OUT_OF_RANGE_LOTTO_AMOUNT_EXCEPTION_MESSAGE);
         }
     }
 
     public int getPurchaseNumber() {
-        return purchaseAmount;
+        return randomLottoAmount + manualLottoAmount;
+    }
+
+    public int getRandomLottoAmount() {
+        return randomLottoAmount;
+    }
+
+    public int getManualLottoAmount() {
+        return manualLottoAmount;
     }
 }
