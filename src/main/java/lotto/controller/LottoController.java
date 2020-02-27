eca951471ed3177bc1d42659aa1448b768bece15
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-27 12:42:38 +0900

    feat: 컨트롤러 및 메인 함수 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,111 @@
+package lotto.controller;
+
+import lotto.domain.money.Money;
+import lotto.domain.number.LottoNumber;
+import lotto.domain.number.SerialLottoNumber;
+import lotto.domain.number.SerialLottoNumberFactory;
+import lotto.domain.random.RandomLottoNumberGenerator;
+import lotto.domain.result.LottoResult;
+import lotto.domain.result.LottoTickets;
+import lotto.domain.result.Winning;
+import lotto.exceptions.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+	public static void run() {
+		Money purchaseMoney = validPurchaseMoney();
+
+		int manualTicketCount = validManualTicketCount(purchaseMoney);
+		int autoTicketCount = purchaseMoney.totalTicketCount() - manualTicketCount;
+
+		LottoTickets lottoTickets = new LottoTickets(createManualLottoTickets(manualTicketCount),
+				createAutoLottoTickets(autoTicketCount));
+		OutputView.printLottoTicketsCount(manualTicketCount, autoTicketCount);
+		OutputView.printLottoTickets(lottoTickets);
+
+		Winning winning = validWinning();
+
+		LottoResult lottoResult = LottoResult.of(winning, lottoTickets);
+		OutputView.printLottoResult(lottoResult);
+		OutputView.printProfitRate(lottoResult.calculateProfitRate(purchaseMoney));
+	}
+
+	private static Money validPurchaseMoney() {
+		try {
+			return new Money(InputView.inputPurchaseMoney());
+		} catch (MoneyIllegalException e) {
+			OutputView.printExceptionMessage(e.getMessage());
+			return validPurchaseMoney();
+		}
+	}
+
+	private static int validManualTicketCount(Money purchaseMoney) {
+		try {
+			int manualTicketCount = InputView.inputManualTicketCount();
+			purchaseMoney.checkCanBuy(manualTicketCount);
+			return manualTicketCount;
+		} catch (TicketCountIllegalException e) {
+			OutputView.printExceptionMessage(e.getMessage());
+			return validManualTicketCount(purchaseMoney);
+		}
+	}
+
+	private static List<SerialLottoNumber> createManualLottoTickets(int manualTicketCount) {
+		List<SerialLottoNumber> manualLottoTickets = new ArrayList<>();
+
+		for (int i = 0; i < manualTicketCount; i++) {
+			manualLottoTickets.add(validManualLottoTicket());
+		}
+		return manualLottoTickets;
+	}
+
+	private static SerialLottoNumber validManualLottoTicket() {
+		try {
+			return SerialLottoNumberFactory.of(InputView.inputManualLottoTicket());
+		} catch (NumberFormatException | NotSixSizeException | LottoNumberIllegalException e) {
+			OutputView.printExceptionMessage(e.getMessage());
+			return validManualLottoTicket();
+		}
+	}
+
+	private static List<SerialLottoNumber> createAutoLottoTickets(int autoTicketCount) {
+		RandomLottoNumberGenerator randomLottoNumberGenerator = new RandomLottoNumberGenerator();
+		List<SerialLottoNumber> autoLottoTickets = new ArrayList<>();
+
+		for (int i = 0; i < autoTicketCount; i++) {
+			autoLottoTickets.add(SerialLottoNumberFactory.of(randomLottoNumberGenerator));
+		}
+		return autoLottoTickets;
+	}
+
+	private static Winning validWinning() {
+		try {
+			return new Winning(validWinningNumbers(), validBonusNumber());
+		} catch (DuplicateWinningNumberException e) {
+			OutputView.printExceptionMessage(e.getMessage());
+			return validWinning();
+		}
+	}
+
+	private static SerialLottoNumber validWinningNumbers() {
+		try {
+			return SerialLottoNumberFactory.of(InputView.inputWinningNumbers());
+		} catch (NumberFormatException | NotSixSizeException | LottoNumberIllegalException e) {
+			OutputView.printExceptionMessage(e.getMessage());
+			return validWinningNumbers();
+		}
+	}
+
+	private static LottoNumber validBonusNumber() {
+		try {
+			return LottoNumber.of(InputView.inputBonusNumber());
+		} catch (LottoNumberIllegalException e) {
+			OutputView.printExceptionMessage(e.getMessage());
+			return validBonusNumber();
+		}
+	}
+}
