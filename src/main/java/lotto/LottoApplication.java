3108d19e74bed428ba845abd3581fa1ad6d1ac8d
ordinCode <ysh1346@naver.com>
2020-02-28 09:43:46 +0900

    refactor : LottoApplication.java
     LottoGenerator 를 리스트로 구현

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.domain.Customer;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGame;
+import lotto.domain.LottoGenerator;
 import lotto.domain.LottoGeneratorAuto;
 import lotto.domain.LottoGeneratorManual;
 import lotto.domain.LottoManager;
@@ -11,9 +12,14 @@ import lotto.util.StringUtils;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoApplication {
+    private static final List<LottoGenerator> lottoGenerator =
+            Arrays.asList(new LottoGeneratorManual(), new LottoGeneratorAuto());
+
     public static void main(String[] args) {
         int inputMoney = StringUtils.ToInt(InputView.inputMoney());
         int inputUserLottoCount = StringUtils.ToInt(InputView.inputManualLottoCount());
@@ -34,11 +40,10 @@ public class LottoApplication {
     }
 
     private static List<Lotto> createUserLottos(Customer customer) {
-        LottoGame manualLottoGame = new LottoGame(customer, new LottoGeneratorManual());
-        LottoGame AutoLottoGame = new LottoGame(customer, new LottoGeneratorAuto());
-
-        List<Lotto> lottos = manualLottoGame.lottoGenerate();
-        AutoLottoGame.lottoGenerate().stream().forEach(x -> lottos.add(x));
+        List<Lotto> lottos = new ArrayList<>();
+        for (LottoGenerator generator : lottoGenerator) {
+            lottos.addAll(new LottoGame(customer, generator).lottoGenerate());
+        }
         return lottos;
     }
 
