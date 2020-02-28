713c6eb6a21da335a1102200e866a9e8db0a66a5
giantim <giantim132@gmail.com>
2020-02-28 12:42:41 +0900

    refactor: LottoTicketsFixture 를 만들어서 LottoTickets 객체를 생성해 테스트 코드에 사용

---
+++ src/test/java/domain/LottoTicketsFixture.java
@@ -0,0 +1,67 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LottoTicketsFixture {
+    private static LottoTickets lottoTickets;
+
+    static {
+        List<LottoTicket> lottoTicketList = new ArrayList<>();
+        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6)))));
+        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(7)))));
+        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(8)))));
+        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(8),
+                new LottoNumber(9)))));
+        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(8),
+                new LottoNumber(9),
+                new LottoNumber(10)))));
+        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(11),
+                new LottoNumber(8),
+                new LottoNumber(9),
+                new LottoNumber(10)))));
+        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(12),
+                new LottoNumber(11),
+                new LottoNumber(8),
+                new LottoNumber(9),
+                new LottoNumber(10)))));
+        lottoTickets = new LottoTickets(lottoTicketList);
+    }
+
+    public static LottoTickets getLottoTickets() {
+        return lottoTickets;
+    }
+}
--- src/test/java/domain/WinningCalculatorTest.java
+++ src/test/java/domain/WinningCalculatorTest.java
@@ -5,74 +5,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class WinningCalculatorTest {
     private WinningLottoTicket winningLottoTicket;
-    private List<LottoTicket> lottoTicketList;
 
     @BeforeEach
     private void setUp() {
         winningLottoTicket = new WinningLottoTicket("1, 2, 3, 4, 5, 6");
         winningLottoTicket.initializeBonusBall(new LottoNumber(7));
-        lottoTicketList = new ArrayList<>();
     }
 
     @DisplayName("WinningCalculator 가 LottoTickets 와 WinningTicket 을 이용해 당첨된 복권이 몇개인지 계산하는 기능 테스트")
     @Test
     void getPrizeCountTest() {
-        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(6)))));
-        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(7)))));
-        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(8)))));
-        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(8),
-                new LottoNumber(9)))));
-        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(8),
-                new LottoNumber(9),
-                new LottoNumber(10)))));
-        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(11),
-                new LottoNumber(8),
-                new LottoNumber(9),
-                new LottoNumber(10)))));
-        lottoTicketList.add(new LottoTicket(new ArrayList(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(12),
-                new LottoNumber(11),
-                new LottoNumber(8),
-                new LottoNumber(9),
-                new LottoNumber(10)))));
-        LottoTickets lottoTickets = new LottoTickets(lottoTicketList);
+        LottoTickets lottoTickets = LottoTicketsFixture.getLottoTickets();
 
         WinningCalculator winningCalculator = new WinningCalculator();
         winningCalculator.calculateWinningCount(lottoTickets, winningLottoTicket);
