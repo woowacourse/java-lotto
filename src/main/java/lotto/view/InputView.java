768440fb4006f35980bb40922c42ee2a4e0aaa1f
junk-sound <wom2277@naver.com>
2020-02-28 12:02:43 +0900

    refac: 매직 넘버 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,13 +8,15 @@ import java.util.stream.Collectors;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
-import lotto.domain.LottosGenerator;
 import lotto.domain.Money;
 import lotto.utils.StringUtils;
 import lotto.view.errors.InvalidInputException;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int MIN_NUMBER_TO_BUY_MANUALLY = 0;
+
 
     public static Money inputPurchaseAmount() {
         System.out.println("구입금액을 입력해주세요");
@@ -33,8 +35,8 @@ public class InputView {
         try {
             String input = scanner.nextLine();
             List<Integer> lottoNumbers = StringUtils.parseWithDelimiter(input);
-            if (lottoNumbers.size() != 6) {
-                throw new InvalidInputException(String.format("로또 번호의 개수는 %d개여야 합니다.", 6));
+            if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
+                throw new InvalidInputException(String.format("로또 번호의 개수는 %d개여야 합니다.", LOTTO_NUMBERS_SIZE));
             }
             return Lotto.createWinningLotto(lottoNumbers);
         } catch (NumberFormatException e) {
@@ -47,8 +49,8 @@ public class InputView {
         try {
             int numberToBuyManually = StringUtils.parseInt(scanner.nextLine());
 
-            if (numberToBuyManually < 0) {
-                throw new InvalidInputException(String.format("%d보다 작은 숫자의 로또를 수동으로 구매할 수 없습니다.", 0));
+            if (numberToBuyManually < MIN_NUMBER_TO_BUY_MANUALLY) {
+                throw new InvalidInputException(String.format("%d보다 작은 숫자의 로또를 수동으로 구매할 수 없습니다.", MIN_NUMBER_TO_BUY_MANUALLY));
             }
 
             if (lottosSize < numberToBuyManually) {
@@ -66,8 +68,8 @@ public class InputView {
         try {
             for (int i = 0; i < numberToBuy; i++) {
                 Set<LottoNumber> lottoNumbers = StringUtils.parseWithDelimiter(scanner.nextLine()).stream().map(LottoNumber::new).collect(Collectors.toSet());
-                if (lottoNumbers.size() != 6) {
-                    throw new InvalidInputException(String.format("로또 번호의 개수는 %d개여야 합니다.", 6));
+                if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
+                    throw new InvalidInputException(String.format("로또 번호의 개수는 %d개여야 합니다.", LOTTO_NUMBERS_SIZE));
                 }
                 lottoNumbersBasket.add(lottoNumbers);
             }
