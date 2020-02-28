6652da7fa1561a3a55b3228782cbfb64659f095e
icyMojito <dev.icymojito@gmail.com>
2020-02-29 03:41:30 +0900

    refactor : 로또 게임의 컨트롤러 역할을 하는 LottoApplication 클래스 정리

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -9,18 +9,29 @@ import java.util.List;
 public class LottoApplication {
     public static void main(String[] args) {
         LottoMoney lottoMoney = new LottoMoney(InputView.requestLottoMoneyInput());
-        LottoCount lottoCount = new LottoCount(lottoMoney, InputView.requestManualLottoCount());
-        List<String[]> manualLottos = InputView.requestManualLottoInput(lottoCount);
-        Lottos lottos = new Lottos(lottoCount, manualLottos);
+        LottoCount lottoCount = lottoMoney.toLottoCount(InputView.requestManualLottoCount());
+
+        Lottos lottos = createLottos(lottoCount);
         OutputView.printLottoCountAndLottos(lottoCount, lottos);
 
-        Lotto winningLottoLine = Lotto.from(InputView.requestWinningLottoInput());
-        LottoNumber bonusNumber = LottoNumber.of(InputView.requestBonusNumberInput());
-        WinningLotto winningLotto = new WinningLotto(winningLottoLine, bonusNumber);
+        WinningLotto winningLotto = createWinningLotto();
 
-        MatchResults matchResults = lottos.toMatchResults(winningLotto, bonusNumber);
+        MatchResults matchResults = lottos.toMatchResults(winningLotto);
         int earningsRate = calculateEarningsRate(matchResults, lottoMoney);
         OutputView.printLottoResultAndEarningsRate(matchResults, earningsRate);
+    }
+
+    private static Lottos createLottos(LottoCount lottoCount) {
+        List<String[]> manualLottoLines = InputView.requestManualLottoInput(lottoCount);
+        Lottos autoLottos = new AutoLottos(lottoCount).generate();
+        Lottos manualLottos = new ManualLottos(manualLottoLines).generate();
+        return Lottos.combine(autoLottos, manualLottos);
+    }
+
+    private static WinningLotto createWinningLotto() {
+        Lotto winningLottoLine = Lotto.from(InputView.requestWinningLottoInput());
+        LottoNumber bonusNumber = LottoNumber.of(InputView.requestBonusNumberInput());
+        return new WinningLotto(winningLottoLine, bonusNumber);
     }
 
     private static int calculateEarningsRate(MatchResults matchResults, LottoMoney lottoMoney) {
