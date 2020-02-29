e8c8d7c7b3af1ca05808f3ddd59f920e8c2f372b
hwookim <gusdn9754@gmail.com>
2020-02-29 16:16:20 +0900

    [Lotto] refactor : 테스트코드 수정
     DisplayName 추가
     테스트 케이스 상황별 분리

--- src/test/java/lotto/domain/lottoTicket/LottoAmountTest.java
+++ src/test/java/lotto/domain/lottoTicket/LottoAmountTest.java
@@ -1,12 +1,14 @@
 package lotto.domain.lottoTicket;
 
 import lotto.exception.InvalidManualLottoAmountException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoAmountTest {
     @Test
+    @DisplayName("LottoAmount 생성 확인")
     void initFailTest() {
         assertThatThrownBy(() -> new LottoAmount(10, 11))
                 .isInstanceOf(InvalidManualLottoAmountException.class);
