7346176f6403c7f5685239d74df336c3f3f63d4a
lalize <printf@kakao.com>
2020-02-29 15:17:28 +0900

    refactor: buyAutoAndManual -> buyManualAndAuto 테스트 메소드 이름 수정

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 public class LottoStoreTest {
 	@Test
 	@DisplayName("로또 수동 + 자동 구매 테스트")
-	void buyAutoAndManual() {
+	void buyManualAndAuto() {
 		LottoPurchaseMoney lottoPurchaseMoney = new LottoPurchaseMoney("5000");
 		LottoBuyCount lottoBuyCount = lottoPurchaseMoney.getBuyCount(2);
 		List<String> manualLottos = new ArrayList<>(Arrays.asList("1,2,3,4,5,6", "2,3,4,5,6,7"));
