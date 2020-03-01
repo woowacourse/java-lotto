b223c7f7083025cbe43a68a31d331d7bccf94da1
icyMojito <dev.icymojito@gmail.com>
2020-03-01 17:54:38 +0900

    refactor : 예외 메시지 내용을 변수로 만들어 사용
    가독성을 높이기 위해 장문인 예외 메시지 내용을 변수로 만듦

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -21,11 +21,10 @@ public class LottoCount {
     private static void validate(String manualLottoCount, int lottoCount) {
         validateNullAndEmptyValue(manualLottoCount, "수동으로 구매할 로또 수를 입력해 주세요.");
         validateNumberFormat(manualLottoCount, "수동으로 구매할 로또 수는 숫자만 입력 가능합니다.");
-        validateNumberRange(manualLottoCount, lottoCount, String.format("수동으로 구매할 로또 수는 %d 이상 %d" +
-                                                                                "(=구매한 총 로또 수) " +
-                                                                                "이하여야 합니다.",
-                                                                        MIN_MANUAL_LOTTO_COUNT,
-                                                                        lottoCount));
+        String message = String.format("수동으로 구매할 로또 수는 %d 이상 %d(=구매한 총 로또 수) 이하여야 합니다.",
+                                       MIN_MANUAL_LOTTO_COUNT,
+                                       lottoCount);
+        validateNumberRange(manualLottoCount, lottoCount, message);
     }
 
     private static void validateNumberRange(String manualLottoCount, int lottoCount,
