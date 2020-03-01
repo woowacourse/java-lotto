be5769ae625943085bdd52c1e1efaa6b8aa474df
jaeju.jang <jjj0611@gmail.com>
2020-03-02 01:44:01 +0900

    refactoring. count를 test하는 메서드들의 의도를 명확하게 드러냄

--- src/test/java/lotto/domain/count/CountTest.java
+++ src/test/java/lotto/domain/count/CountTest.java
@@ -1,5 +1,6 @@
 package lotto.domain.count;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,26 +9,30 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class CountTest {
 
     @Test
-    void testCount() {
+    @DisplayName("count는 전체 횟수와 수동 구매 횟수를 통해 생성")
+    void createCount() {
         Count count = new Count(10, 4);
         assertThat(count.getManualCounts()).isEqualTo(4);
         assertThat(count.getAutoCounts()).isEqualTo(6);
     }
 
     @Test
-    void testException() {
+    @DisplayName("count에서 수동 구매 횟수가 전체 횟수보다 많으면 예외 발생")
+    void totalCountLessThanManualThrowsException() {
         assertThatThrownBy(() -> new Count(1, 2))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void testNegativeTotalCount() {
+    @DisplayName("count에서 전체 구매 횟수가 음수이면 예외 발생")
+    void negativeTotalCountThrowsException() {
         assertThatThrownBy(() -> new Count(-1, -1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void testNegativeManualCount() {
+    @DisplayName("count에서 수동 구매 횟수가 음수이면 예외 발생")
+    void negativeManualCountThrowsException() {
         assertThatThrownBy(() -> new Count(1, -1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
