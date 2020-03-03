750579abb5df26fd795b95a96e988685530d6e15
hongbin23 <khbasd@gmail.com>
2020-03-03 18:03:18 +0900

    test : LottoFactory 변경에 따른 Test코드 변경

--- src/test/java/domain/LottoFactoryTest.java
+++ src/test/java/domain/LottoFactoryTest.java
@@ -2,21 +2,49 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
-import exception.LackOfMoneyException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import exception.InvalidLottoAmountException;
 
 class LottoFactoryTest {
 
+	private static Stream<Arguments> createSelfNumberAndMessage() {
+		return Stream.of(
+			Arguments.of(
+				Arrays.asList("1, 2, 3, 4"), "로또 번호 갯수가 6개가 아닙니다."
+			), Arguments.of(
+				Arrays.asList("1, 2, 3, 4, 5, 5"), "로또 번호가 중복됩니다."
+			), Arguments.of(
+				Arrays.asList("1, 2, 3, 4, 5, -1"), "로또번호는 1~45의 수가 필요합니다."
+			)
+		);
+	}
+
 	@Test
-	void createLottos() {
+	void createAutoLottos() {
 		// given
-		Money purchaseMoney = new Money(999);
+		int amount = -1;
 		// when
 		// then
 		assertThatThrownBy(() -> {
-			LottoFactory.createAutoLottos(purchaseMoney);
-		}).isInstanceOf(LackOfMoneyException.class)
-			.hasMessage("로또 비용이 부족합니다.");
+			LottoFactory.createAutoLottos(amount);
+		}).isInstanceOf(InvalidLottoAmountException.class)
+			.hasMessage("로또의 갯수는 음수가 올 수 없습니다.");
+	}
+
+	@ParameterizedTest
+	@MethodSource("createSelfNumberAndMessage")
+	void createSelfLottos(List<String> numbers, String message) {
+		assertThatThrownBy(() -> {
+			LottoFactory.createSelfLottos(numbers);
+		}).isInstanceOf(Exception.class)
+			.hasMessage(message);
 	}
 }
