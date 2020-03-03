00d944b575f75e7639bb2bfc352c89dc242daa33
hongbin23 <khbasd@gmail.com>
2020-03-03 18:25:04 +0900

    test : LottoGame 변경에 따른 Test코드 변경

--- src/test/java/domain/LottoGameTest.java
+++ src/test/java/domain/LottoGameTest.java
@@ -2,31 +2,65 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import exception.LottoNumberDuplicateException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoGameTest {
-
 	private static LottoGame lottoGame;
+
+	private static Lotto createLotto(int... numbers) {
+		return new Lotto(Arrays.stream(numbers)
+			.mapToObj(LottoNumber::createNumber)
+			.collect(Collectors.toList()));
+	}
+
+	private static List<Lotto> createLottos() {
+		return Arrays.asList(
+			createLotto(1, 2, 3, 4, 5, 6),
+			createLotto(1, 2, 3, 4, 5, 7),
+			createLotto(1, 2, 3, 4, 5, 10));
+	}
 
 	@BeforeAll
 	static void beforeAll() {
-		lottoGame = new LottoGame(new Money(5000), selfLottos);
+		lottoGame = new LottoGame(
+			new Lottos(createLottos(), new ArrayList<>()),
+			new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 7)
+		);
 	}
 
-	@Test
-	void playException() {
-		String[] numbers = IntStream.of(1, 2, 3, 4, 5, 6)
-			.mapToObj(Integer::toString)
-			.toArray(String[]::new);
+	private static Stream<Arguments> createRankAndCount() {
+		return Stream.of(
+			Arguments.of(
+				Rank.FIRST, 1
+			),
+			Arguments.of(
+				Rank.SECOND, 1
+			),
+			Arguments.of(
+				Rank.THIRD, 1
+			),
+			Arguments.of(
+				Rank.NO_RANK, 0
+			)
+		);
+	}
 
-		assertThatThrownBy(() -> {
-			lottoGame.play(numbers, "1");
-		}).isInstanceOf(LottoNumberDuplicateException.class)
-			.hasMessage("로또 번호가 중복됩니다.");
+	@ParameterizedTest
+	@MethodSource("createRankAndCount")
+	void addRanks(Rank rank, int expected) {
+		Map<Rank, Integer> ranks = new HashMap<>();
+		lottoGame.addRanks(ranks);
+		assertThat(ranks.getOrDefault(rank, 0)).isEqualTo(expected);
 	}
 }
