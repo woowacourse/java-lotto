13a0d4a5705569bd6e2fdd435ee530193940fa1a
moonyoungCHAE <xpf_fl@naver.com>
2020-03-03 10:20:59 +0900

    refactor: 예외 처리 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -2,31 +2,42 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class Lottos {
+    private static final String INCORRECT_INDEX_MSG = "로또 번호에 잘못된 접근입니다.";
+    private static final String EMPTY_INPUT_MSG = "%s가 입력되지 않았습니다.";
     public static final int PRICE_PER_LOTTO = 1_000;
 
     private List<Lotto> lottos;
 
-    public Lottos() { this.lottos = new ArrayList<>(); }
+    public Lottos() {
+        this.lottos = new ArrayList<>();
+    }
 
     public Lottos(List<Lotto> lottos) {
-        this.lottos = new ArrayList(lottos);
+        this.lottos = new ArrayList<>(lottos);
     }
 
     public List<Lotto> getLottos() {
         return this.lottos;
     }
 
-    public int getLottosSize() { return this.lottos.size(); }
+    public int getLottosSize() {
+        return this.lottos.size();
+    }
 
-    public Lottos addLottos(Lottos lottosToAdd) {
-        this.lottos.addAll(lottosToAdd.getLottos());
+    public Lottos addLottos(Lottos lottos) {
+        Objects.requireNonNull(lottos, String.format(EMPTY_INPUT_MSG, "로또"));
+        this.lottos.addAll(lottos.getLottos());
         return this;
     }
 
     public Set<LottoNumber> getLottoNumbers(int index) {
-        return this.lottos.get(index).getLottoNumbers();
+        if (index < 0 || index >= this.lottos.size()) {
+            throw new IllegalArgumentException(INCORRECT_INDEX_MSG);
+        }
+         return this.lottos.get(index).getLottoNumbers();
     }
 }
