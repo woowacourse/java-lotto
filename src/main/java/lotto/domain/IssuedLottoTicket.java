15400517149c4a6749b7f66e44b618d369d9903d
YerinCho <forjustice9839@gmail.com>
2020-03-04 11:18:25 +0900

    [Lotto] feat : IssuedLottoTicket
     입력된 로또 발행 개수에 따라 로또 발행

---
+++ src/main/java/lotto/domain/IssuedLottoTicket.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import lotto.view.InputView;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IssuedLottoTicket {
+
+    private Set<LottoTicket> issuedLottoTicket;
+
+    public IssuedLottoTicket(int allLottoTicketCounts, int manualLottoTicketCounts) {
+        this.issuedLottoTicket = new HashSet<>();
+        int autoLottoTicketCounts = allLottoTicketCounts - manualLottoTicketCounts;
+        issueManualLottoTicket(manualLottoTicketCounts);
+        issueAutoLottoTicket(autoLottoTicketCounts);
+    }
+
+    private void issueAutoLottoTicket(int autoLottoTicketCounts) {
+        for (int i = 0; i < autoLottoTicketCounts; i++) {
+            this.issuedLottoTicket.add(new LottoTicket(LottoFactory.createRandomLottoNumbers()));
+        }
+    }
+
+    private void issueManualLottoTicket(int manualLottoTicketCounts) {
+        for (int i = 0; i < manualLottoTicketCounts; i++) {
+            Set<LottoNumber> manualLottoNumbers = LottoFactory.createManualLottoNumbers(InputView.inputManualLottoNumbers());
+            this.issuedLottoTicket.add(new LottoTicket(manualLottoNumbers));
+        }
+
+    }
+
+    public Set<LottoTicket> getIssuedLottoTicket() {
+        return issuedLottoTicket;
+    }
+}
