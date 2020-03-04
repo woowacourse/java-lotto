ae96dc8e52220597c49257d8fa3c511c46a7ac11
Andy H. Jung <commaniakr@gmail.com>
2020-03-04 19:07:09 +0900

    refactor: 목적에 충실하게 간결하게. 불필요한 상수화 수정

--- src/main/java/lotto/utils/InputUtil.java
+++ src/main/java/lotto/utils/InputUtil.java
@@ -14,15 +14,12 @@ public class InputUtil {
 	private static final String SPLIT_DELIMITER = ",";
 	private static final String EMPTY = "";
 	private static final String SPACE = " ";
-	public static final String INPUT_MONEY = "구매 금액을 입력해 주세요.";
-	public static final String INPUT_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
-	public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
 	private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
 	public static int inputMoney() {
 		try {
-			System.out.println(INPUT_MONEY);
+			System.out.println("구매 금액을 입력해 주세요.");
 			return Integer.parseInt(bufferedReader.readLine());
 		} catch (NumberFormatException | IOException e) {
 			OutputView.printWrongIntegerInput();
@@ -32,7 +29,7 @@ public class InputUtil {
 
 	public static int inputManualLottoCount() {
 		try {
-			System.out.println(INPUT_MANUAL_LOTTO_COUNT);
+			System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
 			return Integer.parseInt(bufferedReader.readLine());
 		} catch (NumberFormatException | IOException e) {
 			OutputView.printWrongIntegerInput();
@@ -54,7 +51,7 @@ public class InputUtil {
 	}
 
 	public static int inputBonusNumber() throws IOException {
-		System.out.println(INPUT_BONUS_NUMBER);
+		System.out.println("보너스 번호를 입력해 주세요.");
 		return Integer.parseInt(bufferedReader.readLine());
 	}
 
