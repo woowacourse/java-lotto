66556cbfa5e583df8b418780fa5358f8e026fe73
Sony <sonypark0204@gmail.com>
2020-03-05 14:12:04 +0900

    refac: WinningLotto 스트림으로 정리

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WinningLotto {
     private static final int DEFAULT_WINNING_TICKET_SIZE = 0;
@@ -17,29 +21,36 @@ public class WinningLotto {
     }
 
     WinningRanks match(Lottos lottos) {
-        Map<Rank, Integer> winningRanks = new HashMap<>();
-        for (Lotto lotto : lottos.getLottos()) {
-            int matchNumber = countMatchNumber(lotto);
-            Rank rank = Rank.valueOf(matchNumber, matchBonusNumber(bonusNumber));
-            Integer winningTicketSize = winningRanks.get(rank);
-            winningTicketSize = initTicketSize(winningTicketSize);
-
-            winningRanks.put(rank, winningTicketSize + WINNING_AMOUNT);
-        }
+        Supplier<Stream<Rank>> ranks = matchRanks(lottos);
+        Map<Rank, Integer> winningRanks = calculateWinningRanks(ranks);
         return new WinningRanks(winningRanks);
     }
 
-    private Integer initTicketSize(Integer winningTicketSize) {
-        if (winningTicketSize == null) {
-            winningTicketSize = DEFAULT_WINNING_TICKET_SIZE;
+    private Map<Rank, Integer> calculateWinningRanks(Supplier<Stream<Rank>> ranks) {
+        Map<Rank, Integer> winningRanks = new HashMap<>();
+
+        List<Integer> winningTicketSizes = ranks.get()
+            .map(rank -> winningRanks.getOrDefault(rank, DEFAULT_WINNING_TICKET_SIZE))
+            .collect(Collectors.toList());
+
+        for (int winningTicketSize : winningTicketSizes) {
+            ranks.get()
+                .forEach(rank -> winningRanks.put(rank, winningTicketSize + WINNING_AMOUNT));
         }
-        return winningTicketSize;
+        return winningRanks;
+    }
+
+    private Supplier<Stream<Rank>> matchRanks(Lottos lottos) {
+        return () -> lottos.getLottos()
+            .stream()
+            .map(this::countMatchNumber)
+            .map(matchNumber -> Rank.valueOf(matchNumber, matchBonusNumber(bonusNumber)));
     }
 
     private int countMatchNumber(Lotto lotto) {
         return (int)lottoNumbers.getLottoNumbers().stream()
-                    .filter(lotto::contains)
-                    .count();
+            .filter(lotto::contains)
+            .count();
     }
 
     private boolean matchBonusNumber(LottoNumber bonusNumber) {
