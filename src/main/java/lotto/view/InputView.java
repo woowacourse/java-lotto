513e7b73756268ea7ecf4fe88f9e45f98af289bd
junk-sound <wom2277@naver.com>
2020-03-08 20:07:09 +0900

    refac: 피드백 반영 - 출력문들을 상수로 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,13 +1,24 @@
 package lotto.view;
 
 import java.util.*;
+
 import lotto.utils.StringUtils;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final String INVALID_LOTTO_NUMBERS_SIZE_ERROR = String.format("로또 번호의 개수는 %d개여야 합니다.", LOTTO_NUMBERS_SIZE);
+    private static final String LOTTO_NUMBERS_SIZE_ERROR = String.format("로또 번호의 개수는 %d개여야 합니다.", LOTTO_NUMBERS_SIZE);
     private static final int MIN_NUMBER_TO_BUY_MANUALLY = 0;
+    private static final String INVALID_MANUAL_LOTTOS_SIZE_ERROR = String.format("%d보다 작은 숫자의 로또를 수동으로 구매할 수 없습니다.", MIN_NUMBER_TO_BUY_MANUALLY);
     private static final String REQUEST_FOR_PURCHASE_AMOUNT = "구입금액을 입력해주세요";
+    private static final String NUMBER_FORMAT_ERROR = "숫자가 아닌 문자를 입력하였습니다.";
+    private static final String WINNING_NUMBERS_REQUEST = "지난주 당첨번호을 입력해주세요.";
+    private static final String NUMBER_TO_BUY_MANUALLY_REQUEST = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String EXCEED_MANUAL_LOTTOS_SIZE_ERROR = "수동으로 구매할 수 있는 로또 개수(%s개)를 초과하였습니다.";
+    private static final String MANUAL_LOTTO_NUMBERS_REQUEST = "수동으로 구매할 번호를 입력해 주세요.";
+    private static final String BONUS_BALL_REQUEST = "보너스볼을 입력해주세요";
+    private static final String NEGATIVE_NOT_ALLOWED_ERROR = "음수는 입력할 수 없습니다";
 
 
     public static int inputPurchaseAmount() {
@@ -17,58 +28,58 @@ public class InputView {
     }
 
     public static List<Integer> inputLastWeekWinningNumbers() {
-        System.out.println("지난주 당첨번호을 입력해주세요.");
+        System.out.println(WINNING_NUMBERS_REQUEST);
         try {
             String input = scanner.nextLine();
             List<Integer> lottoNumbers = StringUtils.parseWithDelimiter(input);
             if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
-                throw new IllegalArgumentException(String.format("로또 번호의 개수는 %d개여야 합니다.", LOTTO_NUMBERS_SIZE));
+                throw new IllegalArgumentException(LOTTO_NUMBERS_SIZE_ERROR);
             }
             return lottoNumbers;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 문자를 입력하였습니다.");
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
         }
     }
 
     public static int inputNumberToBuyManually(Integer lottosSize) {
-        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        System.out.println(NUMBER_TO_BUY_MANUALLY_REQUEST);
         try {
             int numberToBuyManually = StringUtils.parseInt(scanner.nextLine());
 
             if (numberToBuyManually < MIN_NUMBER_TO_BUY_MANUALLY) {
-                throw new IllegalArgumentException(String.format("%d보다 작은 숫자의 로또를 수동으로 구매할 수 없습니다.", MIN_NUMBER_TO_BUY_MANUALLY));
+                throw new IllegalArgumentException(INVALID_MANUAL_LOTTOS_SIZE_ERROR);
             }
 
             if (lottosSize < numberToBuyManually) {
-                throw new IllegalArgumentException(String.format("수동으로 구매할 수 있는 로또 개수(%s개)를 초과하였습니다.", lottosSize));
+                throw new IllegalArgumentException(String.format(EXCEED_MANUAL_LOTTOS_SIZE_ERROR, lottosSize));
             }
             return numberToBuyManually;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 문자를 입력하였습니다.");
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
         }
     }
 
     public static List<Set<Integer>> inputnumbersBasketManually(int numberToBuy) {
-        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        System.out.println(MANUAL_LOTTO_NUMBERS_REQUEST);
         List<Set<Integer>> lottoNumbersBasket = new ArrayList<>();
         try {
             for (int i = 0; i < numberToBuy; i++) {
                 Set<Integer> lottoNumbers = new HashSet<>(StringUtils.parseWithDelimiter(scanner.nextLine()));
                 if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
-                    throw new IllegalArgumentException(String.format("로또 번호의 개수는 %d개여야 합니다.", LOTTO_NUMBERS_SIZE));
+                    throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS_SIZE_ERROR);
                 }
                 lottoNumbersBasket.add(lottoNumbers);
             }
 
             return lottoNumbersBasket;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 문자를 입력하였습니다.");
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
         }
 
     }
 
     public static int inputBonusNumber() {
-        System.out.println("보너스볼을 입력해주세요");
+        System.out.println(BONUS_BALL_REQUEST);
         return inputNumber();
     }
 
@@ -76,11 +87,11 @@ public class InputView {
         try {
             int bonusNumber = StringUtils.parseInt(scanner.nextLine());
             if (bonusNumber <= 0) {
-                throw new IllegalArgumentException("음수는 입력할 수 없습니다");
+                throw new IllegalArgumentException(NEGATIVE_NOT_ALLOWED_ERROR);
             }
             return bonusNumber;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 문자를 입력하였습니다.");
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
         }
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,19 +1,19 @@
 package lotto.view;
 
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import lotto.domain.*;
 
 public class OutputView {
-
-    public static void printRetryRequestWithMessage(String message) {
-        System.out.println(String.format("다음과 같은 이유로 다시 시도해 주세요 - %s", message));
-    }
+    private static final String BUY_LOTTOS_SUCCESS = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
+    private static final String STATISTICS_ANNOUNCE = "당첨 통계\n---------";
+    private static final String RETRY_REQUEST = "다음과 같은 이유로 다시 시도해 주세요 - %s";
+    private static final String EARNING_RATE_ANNOUNCE = "총 수익률은 %d%%입니다.";
+    private static final String RESULT_STATISTICS_ANNOUNCE = "%d개 일치(%d원)- %d개";
 
     public static void printLottos(Lottos lottosManual, Lottos lottosAutomatic) {
-        System.out.println(String.format("수동으로 %d장, 자동으로 %d개를 구매했습니다.", lottosManual.size(), lottosAutomatic.size()));
+        System.out.println(String.format(BUY_LOTTOS_SUCCESS, lottosManual.size(), lottosAutomatic.size()));
         for (Lotto lottoManual : lottosManual.getLottos()) {
             printLotto(lottoManual);
         }
@@ -24,19 +24,23 @@ public class OutputView {
     }
 
     public static void printResult(Result result) {
-        System.out.println("당첨 통계\n---------");
+        System.out.println(STATISTICS_ANNOUNCE);
         printStatistics(result.getWinningRanks());
         printEarningRate(result.getEarningRate());
     }
 
+    static void printRetryRequestWithMessage(String message) {
+        System.out.println(String.format(RETRY_REQUEST, message));
+    }
+
     private static void printEarningRate(int earningRate) {
-        System.out.println(String.format("총 수익률은 %d%%입니다.", earningRate));
+        System.out.println(String.format(EARNING_RATE_ANNOUNCE, earningRate));
     }
 
     private static void printStatistics(WinningRanks winningRanks) {
         Map<Rank, Integer> winningResults = winningRanks.getWinningRanks();
         for (Rank rank : winningResults.keySet()) {
-            String resultMessage = String.format("%d개 일치(%d원)- %d개", rank.getMatchNumber(),
+            String resultMessage = String.format(RESULT_STATISTICS_ANNOUNCE, rank.getMatchNumber(),
                     rank.calculateWinningMoney().getValue(), winningResults.get(rank));
             System.out.println(resultMessage);
         }
