5ffe76e6eda6075db829d415c4123e9e1cb5ad36
sori <bosl95@naver.com>
2021-02-16 15:46:28 +0900

    feat : Lotto 객체 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.*;
+
+public class Lotto {
+    final List<Integer> numbers;
+
+    public Lotto(List<Integer> values) {
+        numbers = values;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+}
