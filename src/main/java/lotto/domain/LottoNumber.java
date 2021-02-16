faf44667527ae048556997a6635ca4d93ac76883
cjy17 <cjy1705@naver.com>
2021-02-16 16:54:19 +0900

    refactor(LottoNumber) : 하드코딩 수정

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -5,25 +5,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumber {
+    private static final int START_INDEX = 0;
+    private static final int END_INDEX = 6;
+    private static final int MAXIMUM_CANDIDATE_NUMBER = 45;
 
     public LottoNumber(){
     }
 
     public List<Integer> make() {
-        List<Integer> numbers = new ArrayList<>();
-        for(int i=1;i<=45;i++){
-            numbers.add(i);
-        }
+        List<Integer> numbers = createCandidates();
+
         Collections.shuffle(numbers);
 
-        List<Integer> lottoNumber = new ArrayList<>();
+        List<Integer> lottoNumber = numbers.subList(START_INDEX, END_INDEX);
 
-        for(int i=0;i<6;i++){
-            lottoNumber.add(numbers.get(i));
-        }
         Collections.sort(lottoNumber);
 
         return lottoNumber;
     }
 
+    private List<Integer> createCandidates() {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 1; i <= MAXIMUM_CANDIDATE_NUMBER; i++) {
+            numbers.add(i);
+        }
+        return numbers;
+    }
 }
