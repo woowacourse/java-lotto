7997712432978828b1a8468af79381fa103f3ff0
bomin <bomin_93@naver.com>
2021-02-16 21:03:02 +0900

    feat(LottoSeller): 가격을 받아 로또를 판매하는 객체 구현 및 예외처리

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,2 +1,27 @@
-package lotto.domain;public class LottoSeller {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.util.LottoGenerator;
+
+public class LottoSeller {
+  private static final int LOTTO_PRICE = 1000;
+  private final LottoGenerator lottoGenerator;
+
+  public LottoSeller(LottoGenerator lottoGenerator) {
+    this.lottoGenerator = lottoGenerator;
+  }
+
+  public List<Lotto> sellLotto(int price) {
+    int count = price / LOTTO_PRICE;
+    if(count < 1) {
+      throw new IllegalArgumentException();
+    }
+
+    List<Lotto> lottos = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      lottos.add(lottoGenerator.generate());
+    }
+    return lottos;
+  }
 }
--- src/test/java/lotto/domain/LottoSellerTest.java
+++ src/test/java/lotto/domain/LottoSellerTest.java
@@ -1,4 +1,33 @@
+package lotto.domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import lotto.util.LottoGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class LottoSellerTest {
-  
+
+  private LottoSeller lottoSeller;
+
+  @BeforeEach
+  void setUp() {
+    lottoSeller = new LottoSeller(new LottoGenerator());
+  }
+
+  @Test
+  void createLotto_enoughMoney() {
+    List<Lotto> lottoGroup = lottoSeller.sellLotto(2000);
+    Assertions.assertThat(lottoGroup.size()).isEqualTo(2);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {500, -100})
+  void createLotto_notEnoughMoney(int price) {
+    Assertions.assertThatThrownBy(() -> lottoSeller.sellLotto(price)).isInstanceOf(IllegalArgumentException.class);
+  }
 }
