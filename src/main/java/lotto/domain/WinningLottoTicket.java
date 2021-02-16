b71797ab6498bbd611b9495f1ea7afe4352f1c58
Jinho <jh8579@gmail.com>
2021-02-16 22:04:56 +0900

    debug : 당첨로또 보너스 번호 확인 오류 수정

--- src/main/java/lotto/domain/WinningLottoTicket.java
+++ src/main/java/lotto/domain/WinningLottoTicket.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningLottoTicket extends LottoTicket {
+
     private final LottoNumber bonusNumber;
 
     public WinningLottoTicket(List<Integer> winningNumber, int bonusNumber) {
@@ -15,30 +16,30 @@ public class WinningLottoTicket extends LottoTicket {
             .filter(lottoNumber -> this.list().contains(lottoNumber))
             .count();
 
-        boolean isBonus = this.list().stream()
+        boolean isBonus = lottoTicket.list().stream()
             .anyMatch(winningNumber -> winningNumber.equals(bonusNumber));
 
         return getResult(winningCount, isBonus);
     }
 
     public String getResult(long winningCount, boolean isBonus) {
-        if(winningCount == 3) {
+        if (winningCount == 3) {
             return "5등!";
         }
 
-        if(winningCount == 4) {
+        if (winningCount == 4) {
             return "4등!";
         }
 
-        if(winningCount == 5 && isBonus) {
+        if (winningCount == 5 && isBonus) {
             return "2등!";
         }
 
-        if(winningCount == 5) {
+        if (winningCount == 5) {
             return "3등!";
         }
 
-        if(winningCount == 6) {
+        if (winningCount == 6) {
             return "1등!";
         }
 
