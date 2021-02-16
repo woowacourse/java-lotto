60db648b4ffa791215c86fd6bd46f4da5159b190
xrabcde <xrabcde@gmail.com>
2021-02-16 18:04:10 +0900

    feat(LottoView): 지난주 로또번호 입력받는 기능 구현

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,6 +1,7 @@
 package lotto;
 
 public class LottoController {
+    private static final String DELIMITER = ",";
     private static Lottos lottos;
 
     public void startLotto() {
@@ -8,6 +9,19 @@ public class LottoController {
         LottoView.buyLotto(lottos.getCount());
         lottos.generateLottos();
         LottoView.printLottos(lottos);
+        Lotto winLotto = new Lotto(changeToArrayList());
     }
 
+    private ArrayList<Integer> changeToArrayList() {
+        String numberInput = LottoView.requestWinningNumber();
+        List<String> parsedNumbers = Arrays.stream(numberInput.split(DELIMITER, -1))
+                    .map(String::trim)
+                    .collect(Collectors.toList());
+        List<Integer> winningNumbers = parsedNumbers.stream()
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        ArrayList<Integer> winningNums = new ArrayList<Integer>();
+        winningNums.addAll(winningNumbers);
+        return winningNums;
+    }
 }
--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 public class LottoView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해주세요";
 
     public static String requestMoney() {
-        System.out.println("구입금액을 입력해주세요");
+        System.out.println(INPUT_MONEY_MESSAGE);
         return SCANNER.nextLine();
     }
 
@@ -26,5 +27,10 @@ public class LottoView {
             System.out.print(lotto.get(i).get(5));
             System.out.print("]\n");
         }
+    }
+
+    public static String requestWinningNumber() {
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
+        return SCANNER.nextLine();
     }
 }
