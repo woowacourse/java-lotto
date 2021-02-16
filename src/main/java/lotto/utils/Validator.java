dd15fd0a21d7b10ecaaa2f9a250353e3bde1f648
Sehwan_Jang <whilter08@gmail.com>
2021-02-16 17:47:22 +0900

    feat : 로또티켓 숫자가 유효한 범위 내에 속하는지 검증하는 기능

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,13 +1,16 @@
 package lotto.utils;
 
 import lotto.exception.IllegalLottoTicketDuplicateNumber;
+import lotto.exception.IllegalLottoTicketNumbers;
 import lotto.exception.IllegalLottoTicketSizeError;
 
 import java.util.HashSet;
 import java.util.List;
 
 public class Validator {
-    public static final int LOTTO_TICKET_SIZE = 6;
+    private static final int LOTTO_TICKET_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 49;
 
     private Validator() {
 
@@ -16,10 +19,11 @@ public class Validator {
     public static void validateLottoNumbers(List<Integer> numbers) {
         validateLottoSize(numbers);
         validateDuplicateNumber(numbers);
+        validateLottoNumbersRange(numbers);
     }
 
     private static void validateDuplicateNumber(List<Integer> numbers) {
-        if(new HashSet<>(numbers).size() != numbers.size()){
+        if (new HashSet<>(numbers).size() != numbers.size()) {
             throw new IllegalLottoTicketDuplicateNumber();
         }
     }
@@ -28,5 +32,16 @@ public class Validator {
         if (numbers.size() < LOTTO_TICKET_SIZE) {
             throw new IllegalLottoTicketSizeError();
         }
+    }
+
+    private static void validateLottoNumbersRange(List<Integer> numbers) {
+        if (isValidNumberRange(numbers)) {
+            throw new IllegalLottoTicketNumbers();
+        }
+    }
+
+    private static boolean isValidNumberRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER);
     }
 }
