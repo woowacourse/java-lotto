37cddd1317e191ef0b097f3ea4eb8702cd69ea5e
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-02-16 16:12:52 +0900

    feat: 로또 티켓 구매 금액 생성 및 유효성 로직 기능 구현

---
+++ src/main/java/lotto/domain/PurchasingPrice.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public class PurchasingPrice {
+    private static final int MINIMUM_TICKET_PRICE = 1000;
+    private static final String INVALID_TICKET_PRICE = "로또 티켓 최소 구매 금액은 1000원입니다.";
+
+    private final int purchasingPrice;
+
+    public PurchasingPrice(int purchasingPrice) {
+        if (purchasingPrice < MINIMUM_TICKET_PRICE) {
+            throw new IllegalArgumentException(INVALID_TICKET_PRICE);
+        }
+        this.purchasingPrice = purchasingPrice;
+    }
+
+    public int getPurchasingPrice() {
+        return purchasingPrice;
+    }
+}
---
+++ src/test/java/lotto/domain/PurchasingPriceTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class PurchasingPriceTest {
+
+    @DisplayName("로또 구입 금액은 1000원 미만인 경우 객체 생성 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(ints = {-10, 0, 999})
+    void cannotMakePurchasingPrice(int purchasingPrice) {
+        assertThatCode(() -> {
+            new PurchasingPrice(purchasingPrice);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 티켓 최소 구매 금액은 1000원입니다.");
+    }
+
+    @DisplayName("로또 구입 금액이 1000원 이상이면 객체가 정상 생성된다")
+    @Test
+    void makePurchasingPrice() {
+        PurchasingPrice purchasingPrice = new PurchasingPrice(1000);
+
+        int value = purchasingPrice.getPurchasingPrice();
+
+        assertThat(value).isEqualTo(1000);
+    }
+}
