33379431008288217f57d41a6295c79447a02d6a
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-02-16 17:33:32 +0900

    refactor: LottoTicket 검ìì성 검증 로직 메서드 분리

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -4,7 +4,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class LottoTicket {
-
+    private static final int VALID_LOTTO_NUMBER_COUNTS = 6;
+    private static final String INVALID_LOTTO_NUMBER_COUNTS = "로또 티켓은 중복되지 않은 6자리의 숫자로 구성되어야 합니다.";
+    
     private final List<LottoNumber> lottoNumbers;
 
     private LottoTicket(List<LottoNumber> lottoNumbers) {
@@ -12,19 +14,28 @@ public class LottoTicket {
     }
 
     public static LottoTicket generateTicket(List<Integer> numbers) {
-        Set<Integer> distinctNumbers = new HashSet<>(numbers);
-        int numberCounts = numbers.size();
-        int distinctNumberCounts = distinctNumbers.size();
-        if (numberCounts != 6 || numberCounts != distinctNumberCounts) {
-            throw new IllegalArgumentException("로또 티켓은 중복되지 않은 6자리의 숫자로 구성되어야 합니다.");
-        }
+        validateNumbers(numbers);
         List<LottoNumber> lottoNumbers = numbers.stream()
                 .map(LottoNumber::new)
                 .collect(Collectors.toList());
         return new LottoTicket(lottoNumbers);
     }
 
-    public List<LottoNumber> getLottoNumbers() {
-        return lottoNumbers;
+    private static void validateNumbers(List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        int numberCounts = numbers.size();
+        int distinctNumberCounts = distinctNumbers.size();
+        if (numberCounts != VALID_LOTTO_NUMBER_COUNTS || numberCounts != distinctNumberCounts) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_COUNTS);
+        }
     }
+
+    public List<LottoNumber> getLottoNumbers() {
+        return Collections.unmodifiableList(lottoNumbers);
+    }
+    
+    /*
+        public L
+    
+     */
 }
