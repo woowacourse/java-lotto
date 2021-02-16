c109a41bf36c2ec921e611f32b3a75f51c9adf85
xrabcde <xrabcde@gmail.com>
2021-02-16 17:26:28 +0900

    test: 당첨번호 유효성 검증 테스트코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,10 +8,17 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
+    private static final String NUMBER_COUNT_ERROR = "[ERROR] 6개의 숫자를 입력해주세요";
+    private static final String NUMBER_DUPLICATE_ERROR = "[ERROR] 숫자는 중복될 수 없습니다";
+    private static final String NUMBER_RANGE_ERROR = "[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요";
+
     @Test
     void 숫자가_6개인지_확인() {
-        Lotto lotto = new Lotto();
-        assertThat(lotto.getLotto().size()).isEqualTo(6);
+        ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        assertThatThrownBy(() -> {
+            new Lotto(nums);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NUMBER_COUNT_ERROR);
     }
 
     @Test
