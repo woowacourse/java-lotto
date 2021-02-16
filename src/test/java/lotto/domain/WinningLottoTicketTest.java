9dd632a91b8da83b737e544d8928544a9420562b
Jinho <jh8579@gmail.com>
2021-02-16 22:06:13 +0900

    test : 로또 당첨여부 확인 테스트 케이스에 모든 당첨 경우 추가

--- src/test/java/lotto/domain/WinningLottoTicketTest.java
+++ src/test/java/lotto/domain/WinningLottoTicketTest.java
@@ -1,28 +1,35 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class WinningLottoTicketTest {
 
-    @DisplayName("당첨 번호 비교 결과 테스트")
-    @Test
-    void 당첨_번호_비교_결과_테스트(){
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 8, 9, 10);
-        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+    private static WinningLottoTicket winningTicket;
 
+    @BeforeAll
+    static void setUp() {
         List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
+        winningTicket = new WinningLottoTicket(winningNumbers, bonusNumber);
+    }
 
-        WinningLottoTicket winningTicket = new WinningLottoTicket(winningNumbers, bonusNumber);
+    @DisplayName("당첨 번호 비교 결과 테스트")
+    @ParameterizedTest
+    @CsvSource({"1,2,3,8,9,10,5등!", "1,2,3,4,8,9,4등!", "1,2,3,4,5,8,3등!", "1,2,3,4,5,7,2등!", "1,2,3,4,5,6,1등!","7,8,9,10,11,12,꽝.."})
+    void 당첨_번호_비교_결과_테스트(int first, int second, int third, int fourth, int fifth, int sixth,
+        String expectedResult) {
+        List<Integer> lottoNumbers = Arrays.asList(first, second, third, fourth, fifth, sixth);
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
 
         assertThat(winningTicket.compareNumbers(lottoTicket))
-            .isEqualTo("5등!");
+            .isEqualTo(expectedResult);
     }
 
 
