fbb7288493cf09b4cac442719ae98eca5806f98f
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-16 20:41:35 +0900

    feat: LottoTickets.java 로또 티켓들을 가진 객체를 생성

---
+++ src/main/java/lotto/LottoTickets.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import lotto.LottoTicket;
+
+import java.util.List;
+import java.util.Objects;
+
+public class LottoTickets {
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoTickets that = (LottoTickets) o;
+        return Objects.equals(lottoTickets, that.lottoTickets);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoTickets);
+    }
+}
---
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -0,0 +1,77 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTicketsTest {
+    @Test  //이 코드를 좀 더 깔끔하게 쓰는법 혹시 없을까요?
+    @DisplayName("구매한 여러장의 로또 티켓들을 가진 객체를 생성한다.")
+    public void createLottoTicketsTest() {
+        List<LottoNumber> lottoNumbers1 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+        List<LottoNumber> lottoNumbers2 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+        List<LottoNumber> lottoNumbers3 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+        LottoTickets lottoTickets = new LottoTickets(Arrays.asList(
+                new LottoTicket(lottoNumbers1),
+                new LottoTicket(lottoNumbers2),
+                new LottoTicket(lottoNumbers3)
+        ));
+
+        List<LottoNumber> expectedLottoNumbers1 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+        List<LottoNumber> expectedLottoNumbers2 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+        List<LottoNumber> expectedLottoNumbers3 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+        LottoTickets expectedLottoTickets = new LottoTickets(Arrays.asList(
+                new LottoTicket(expectedLottoNumbers1),
+                new LottoTicket(expectedLottoNumbers2),
+                new LottoTicket(expectedLottoNumbers3)
+        ));
+
+        assertThat(lottoTickets).isEqualTo(expectedLottoTickets);
+    }
+}
