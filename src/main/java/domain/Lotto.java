6118d7c1e1b7be32f1dc8b5cb38a5f4425e4c938
ecsimsw <ecsimsw@gmail.com>
2021-02-16 17:32:37 +0900

    feat : Lotto 번호의 길이가 6이 아닌 경우 예외 발생

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final int LENGTH = 6;
 
     private final List<Integer> lottoNumbers;
 
@@ -15,9 +16,20 @@ public class Lotto {
     }
 
     private void validateLottoNumbers(List lottoNumbers) {
+        validateDuplicatedLottoNumbers(lottoNumbers);
+        validateLottoNumbersLength(lottoNumbers);
+    }
+
+    private void validateDuplicatedLottoNumbers(List lottoNumbers) {
         Set<Integer> distinctLottoNumbers = new HashSet(lottoNumbers);
         if (distinctLottoNumbers.size() != lottoNumbers.size()) {
             throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateLottoNumbersLength(List lottoNumbers) {
+        if (lottoNumbers.size() != LENGTH) {
+            throw new IllegalArgumentException("로또 번호의 개수는 6자리입니다.");
         }
     }
 }
--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -2,9 +2,13 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,11 +30,24 @@ class LottoTest {
 
     @DisplayName("Lotto 번호에 중복이 있는 경우")
     @Test
-    void generateWithDuplicatedLottoNumbers(){
+    void generateWithDuplicatedLottoNumbers() {
         //given
         List<Integer> lottoNumbers = Arrays.asList(1, 1, 3, 4, 5, 6);
 
         //when //then
-        assertThatThrownBy(()-> new Lotto(lottoNumbers)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(lottoNumbers)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Lotto 번호의 길이가 6이 아닌 경우")
+    @ParameterizedTest
+    @MethodSource
+    void generateIfLottoNumbersLengthNotSatisfied(List<Integer> input) {
+        //when //then
+        assertThatThrownBy(() -> new Lotto(input)).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    private static Stream<List<Integer>> generateIfLottoNumbersLengthNotSatisfied() {
+        return Stream.of(Arrays.asList(1, 2, 3, 4, 5), Arrays.asList(1, 2, 3, 4, 5, 6, 7));
     }
 }
