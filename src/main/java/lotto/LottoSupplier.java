df810c26baa3a2f3d828342c1e32134946170e1c
pjy1368 <basejin0810@naver.com>
2021-02-16 17:37:16 +0900

    refactor(LottoSupplier): 상수 처리

--- src/main/java/lotto/LottoSupplier.java
+++ src/main/java/lotto/LottoSupplier.java
@@ -8,13 +8,20 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoSupplier implements Supplier<List<Integer>> {
-    private final List<Integer> lottoNumbers = IntStream.rangeClosed(1, 45).boxed().collect(
-        Collectors.toList());
+
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    private final List<Integer> lottoNumbers = IntStream
+        .rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+        .boxed()
+        .collect(Collectors.toList());
 
     @Override
     public List<Integer> get() {
         List<Integer> numbers = new ArrayList<>(lottoNumbers);
         Collections.shuffle(numbers);
-        return numbers.subList(0, 6);
+        return new ArrayList<>(numbers.subList(0, LOTTO_NUMBER_COUNT));
     }
 }
