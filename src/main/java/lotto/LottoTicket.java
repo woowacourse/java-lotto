93fe0f8bc288ad3827b5283013d9399ecf3fdc2c
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-16 20:27:14 +0900

    feat: 로또 티켓 생성시 중복 처리 예외 검사 및 정렬 수행

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,20 +1,33 @@
 package lotto;
 
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class LottoTicket {
     private static final int VALID_NUMBER_COUNT = 6;
     private static final String COUNT_ERROR_MESSAGE = "숫자는 6개여야 합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "중복된 숫자가 존재합니다.";
 
-    private final Set<LottoNumber> lottoNumbers;
+    private final List<LottoNumber> lottoNumbers;
 
-    public LottoTicket(Set<LottoNumber> lottoNumbers) {
+    public LottoTicket(List<LottoNumber> lottoNumbers) {
         validateCount(lottoNumbers);
+        validateDuplicate(lottoNumbers);
+        Collections.sort(lottoNumbers); //이거 위치가 어디로 가야될지?
         this.lottoNumbers = lottoNumbers;
     }
 
-    public void validateCount(Set<LottoNumber> lottoNumbers) {
+    public List<LottoNumber> getLottoNumbers() {
+        return Collections.unmodifiableList(lottoNumbers);
+    }
+
+    private void validateDuplicate(List<LottoNumber> lottoNumbers) {
+        Set<LottoNumber> uniqueLottoNumbers = new HashSet<>(lottoNumbers);
+        if (uniqueLottoNumbers.size() != lottoNumbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateCount(List<LottoNumber> lottoNumbers) {
         if (lottoNumbers.size() == VALID_NUMBER_COUNT) {
             return;
         }
--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.TreeSet;
-import java.util.Set;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -15,22 +14,22 @@ public class LottoTicketTest {
     @Test
     @DisplayName("로또 티켓을 생성한다.")
     public void createLottoTicketTest() {
-        Set<LottoNumber> lottoNumbers1 = new TreeSet<>(Arrays.asList(
+        List<LottoNumber> lottoNumbers1 = Arrays.asList(
                 new LottoNumber(1),
                 new LottoNumber(2),
                 new LottoNumber(3),
                 new LottoNumber(4),
                 new LottoNumber(5),
                 new LottoNumber(45)
-        ));
-        Set<LottoNumber> lottoNumbers2 = new TreeSet<>(Arrays.asList(
+        );
+        List<LottoNumber> lottoNumbers2 = Arrays.asList(
                 new LottoNumber(1),
                 new LottoNumber(2),
                 new LottoNumber(3),
                 new LottoNumber(4),
                 new LottoNumber(5),
                 new LottoNumber(45)
-        ));
+        );
 
         LottoTicket lottoTicket = new LottoTicket(lottoNumbers1);
         assertThat(lottoTicket).isEqualTo(new LottoTicket(lottoNumbers2));
@@ -39,14 +38,14 @@ public class LottoTicketTest {
     @Test
     @DisplayName("중복된 숫자가 있는지 검사")
     public void duplicateNumberTest() {
-        Set<LottoNumber> lottoNumbers = new TreeSet<>(Arrays.asList(
+        List<LottoNumber> lottoNumbers = Arrays.asList(
                 new LottoNumber(1),
                 new LottoNumber(2),
                 new LottoNumber(3),
                 new LottoNumber(4),
                 new LottoNumber(5),
                 new LottoNumber(5)
-        ));
+        );
         assertThatThrownBy(() -> {
             new LottoTicket(lottoNumbers);
         }).isInstanceOf(RuntimeException.class);
@@ -55,7 +54,7 @@ public class LottoTicketTest {
     @Test
     @DisplayName("숫자가 6개를 초과하면 예외발생 검사")
     public void numberCountGreaterThanSixThrowsExceptionTest() {
-        Set<LottoNumber> lottoNumbers = new TreeSet<>(Arrays.asList(
+        List<LottoNumber> lottoNumbers = Arrays.asList(
                 new LottoNumber(1),
                 new LottoNumber(2),
                 new LottoNumber(3),
@@ -63,7 +62,7 @@ public class LottoTicketTest {
                 new LottoNumber(5),
                 new LottoNumber(6),
                 new LottoNumber(45)
-        ));
+        );
         assertThatThrownBy(() -> {
             new LottoTicket(lottoNumbers);
         }).isInstanceOf(RuntimeException.class);
@@ -72,15 +71,37 @@ public class LottoTicketTest {
     @Test
     @DisplayName("숫자가 6개미만이면 예외발생 검사")
     public void numberCountLessThanSixThrowsExceptionTest() {
-        Set<LottoNumber> lottoNumbers = new TreeSet<>(Arrays.asList(
+        List<LottoNumber> lottoNumbers = Arrays.asList(
                 new LottoNumber(1),
                 new LottoNumber(2),
                 new LottoNumber(3),
                 new LottoNumber(4),
                 new LottoNumber(5)
-        ));
+        );
         assertThatThrownBy(() -> {
             new LottoTicket(lottoNumbers);
         }).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("로또 티켓 생성시 입력받은 List가 잘 정렬 되있는지 검사")
+    public void lottoTicketSortTest() {
+        List<LottoNumber> lottoNumbers = Arrays.asList(
+                new LottoNumber(3),
+                new LottoNumber(2),
+                new LottoNumber(5),
+                new LottoNumber(4),
+                new LottoNumber(1),
+                new LottoNumber(44)
+        );
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+        assertThat(lottoTicket.getLottoNumbers()).isEqualTo(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(44)
+        ));
     }
 }
