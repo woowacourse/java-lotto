1f9ab07e937d6636f4472fe99434219b14e31b6c
Junroot <junroot0909@gmail.com>
2021-02-16 19:26:55 +0900

    feat(lotto): 랜덤 숫자 생성 구현

--- src/main/java/lotto/domain/lotto/LottoGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoGenerator.java
@@ -2,30 +2,41 @@ package lotto.domain.lotto;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import lotto.domain.number.LottoNumber;
 
 public class LottoGenerator {
 
-    public LottoGenerator() {
+    private static final int LOTTO_NUMBER_MINIMUM = 1;
+    private static final int LOTTO_NUMBER_MAXIMUM = 45;
+    private static final int LOTTO_COUNT_MAXIMUM = 6;
 
+    private final List<LottoNumber> lottoNumbers;
+
+    public LottoGenerator() {
+        lottoNumbers = new ArrayList<>();
+
+        for (int i = LOTTO_NUMBER_MINIMUM; i <= LOTTO_NUMBER_MAXIMUM; i++) {
+            lottoNumbers.add(
+                new LottoNumber(i)
+            );
+        }
     }
 
     public LottoGroup generateLotties(int count) {
         List<LottoNumbers> lotties = new ArrayList();
 
         for (int i = 0; i < count; i++) {
-            LottoNumbers lottoNumbers = new LottoNumbers(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(6))
-            );
+            LottoNumbers lottoNumbers = new LottoNumbers(generateRandomLottoNumber());
             lotties.add(lottoNumbers);
         }
 
         return new LottoGroup(lotties);
+    }
+
+    private List<LottoNumber> generateRandomLottoNumber() {
+        Collections.shuffle(lottoNumbers);
+        return lottoNumbers.subList(0, LOTTO_COUNT_MAXIMUM);
     }
 }
