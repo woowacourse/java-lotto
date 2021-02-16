3a2c5ad5ca1a6aa09c731ed34b97a910bcd2a41b
hyeon9mak <jinha3507@gmail.com>
2021-02-16 20:52:52 +0900

    feat(LottoNumber): 로또 넘버 검증 기능 구현

---
+++ src/main/java/LottoNumber.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private static final int START_NUMBER = 1;
+    private static final int FINISH_NUMBER = 45;
+
+    private final int value;
+
+    public LottoNumber(final String value) {
+        this.value = Integer.parseInt(value);
+        validateRange(this.value);
+    }
+
+    private void validateRange(final int value) {
+        if (value < START_NUMBER || FINISH_NUMBER < value) {
+            throw new IllegalArgumentException("로또 숫자 범위를 벗어났습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber lottoNumber = (LottoNumber) o;
+        return value == lottoNumber.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/LottoNumberTest.java
@@ -0,0 +1,34 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoNumberTest {
+
+    @Test
+    @DisplayName("LottoNumber를 문자로 생성하려 했을 때")
+    void lottoNumberValueIsString() {
+        String value = "abc";
+        assertThatThrownBy(() -> {
+            LottoNumber lottoNumber = new LottoNumber(value);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("LottoNumber를 -1로 생성하려 했을 때")
+    void lottoNumberValueIsNegativeNumber(){
+        String value = "-1";
+        assertThatThrownBy(() -> {
+            LottoNumber lottoNumber = new LottoNumber(value);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("LottoNumber를 1로 생성하려 했을 때")
+    void lottoNumberValueIsOne() {
+        String value = "1";
+        LottoNumber lottoNumber1 = new LottoNumber(value);
+        assertThat(lottoNumber1).isEqualTo(new LottoNumber(value));
+    }
+}
