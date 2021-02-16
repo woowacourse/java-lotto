10f34b1d2e66eb3ae35e2808d5fc5bf399c01b01
Jinho <jh8579@gmail.com>
2021-02-16 22:19:42 +0900

    feat : 보너스 숫자와 당첨 숫자 간 중복에 대한 validation

--- src/main/java/lotto/domain/WinningLottoTicket.java
+++ src/main/java/lotto/domain/WinningLottoTicket.java
@@ -8,7 +8,19 @@ public class WinningLottoTicket extends LottoTicket {
 
     public WinningLottoTicket(List<Integer> winningNumber, int bonusNumber) {
         super(winningNumber);
+        validateDuplicatedBonusNumber(bonusNumber);
         this.bonusNumber = new LottoNumber(bonusNumber);
+    }
+
+    private void validateDuplicatedBonusNumber(int bonusNumber) {
+        LottoNumber bonusLottoNumber = new LottoNumber(bonusNumber);
+
+        boolean isDuplicated = this.list().stream()
+            .anyMatch(winningNumber -> winningNumber.equals(bonusLottoNumber));
+
+        if (isDuplicated) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public String compareNumbers(LottoTicket lottoTicket) {
--- src/test/java/lotto/domain/WinningLottoTicketTest.java
+++ src/test/java/lotto/domain/WinningLottoTicketTest.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -32,5 +34,12 @@ class WinningLottoTicketTest {
             .isEqualTo(expectedResult);
     }
 
-
+    @DisplayName("보너스 숫자와 당첨 숫자 중복 테스트")
+    @Test
+    public void 보너스_숫자와_당첨_숫자_중복_테스트() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 7);
+        int bonusNumber = 7;
+        assertThatThrownBy(() -> new WinningLottoTicket(winningNumbers, bonusNumber))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }
