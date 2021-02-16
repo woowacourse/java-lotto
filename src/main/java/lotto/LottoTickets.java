68cb67d6e047431d77d9dec2c678637cac36ce0a
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-16 20:46:26 +0900

    feat: 유저가 구매한 수량만큼 로또 티켓이 생성되었나 확인한다.

--- src/main/java/lotto/LottoTickets.java
+++ src/main/java/lotto/LottoTickets.java
@@ -12,6 +12,10 @@ public class LottoTickets {
         this.lottoTickets = lottoTickets;
     }
 
+    public List<LottoTicket> getLottoTickets() {
+        return this.lottoTickets;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/lotto/LottoTicketsTest.java
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -74,4 +74,40 @@ public class LottoTicketsTest {
 
         assertThat(lottoTickets).isEqualTo(expectedLottoTickets);
     }
+
+    @Test  //이 코드를 좀 더 깔끔하게 쓰는법 혹시 없을까요?
+    @DisplayName("구매한 로또 티켓의 갯수만큼 로또가 생성 되었는지 확인한다.")
+    public void lottoTicketsCountTest() {
+        List<LottoNumber> lottoNumbers1 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+        List<LottoNumber> lottoNumbers2 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+        List<LottoNumber> lottoNumbers3 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(45)
+        );
+
+        LottoTickets lottoTickets = new LottoTickets(Arrays.asList(
+                new LottoTicket(lottoNumbers1),
+                new LottoTicket(lottoNumbers2),
+                new LottoTicket(lottoNumbers3)
+        ));
+        assertThat(lottoTickets.getLottoTickets().size()).isEqualTo(3);
+    }
 }
