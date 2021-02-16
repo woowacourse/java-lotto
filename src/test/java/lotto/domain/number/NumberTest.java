b5a796b4b932c9fa64302caf9d6b3d94be11e4c7
Junroot <junroot0909@gmail.com>
2021-02-16 17:03:06 +0900

    test(lotto): Integer 범위 밖의 입력 테스트

--- src/test/java/lotto/domain/number/NumberTest.java
+++ src/test/java/lotto/domain/number/NumberTest.java
@@ -10,14 +10,21 @@ public class NumberTest {
 
     @Test
     @DisplayName("숫자가 아닌 경우")
-    void noNumberInput() {
+    void inputNoNumber() {
         assertThatIllegalArgumentException()
             .isThrownBy(() -> new Number("abc"));
     }
 
     @Test
+    @DisplayName("Integer 범위 밖인 경우")
+    void inputOutOfInteger() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Number("9876543211324"));
+    }
+
+    @Test
     @DisplayName("정상적인 숫자 생성")
-    void normalInput() {
+    void inputNormal() {
         Number number = new Number(1);
         assertThat(number.equals(1)).isTrue();
 
