0e27e7634ae5f3d99e79903f334e64f617da8322
Gomding <backchida@gmail.com>
2021-02-16 17:36:43 +0900

    (feat) : LottoNumber 숫자는 1~45 사이의 숫자여야한다.

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -3,10 +3,22 @@ package lotto;
 import java.util.Objects;
 
 public class LottoNumber {
-    int number;
+    private static final int MIN_NUMBER_RANGE = 1;
+    private static final int MAX_NUMBER_RANGE = 45;
+    private static final String RANGE_ERROR_MESSAGE = "숫자는 1~45 사이의 숫자여야한다.";
+
+    private final int number;
 
     public LottoNumber(int number) {
+        validateRange(number);
         this.number = number;
+    }
+
+    private void validateRange(int number) {
+        if (number >= MIN_NUMBER_RANGE && number <= MAX_NUMBER_RANGE) {
+            return;
+        }
+        throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
     }
 
     @Override
--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoNumberTest {
 
@@ -12,5 +13,13 @@ public class LottoNumberTest {
     public void createNumberTest() {
         LottoNumber number = new LottoNumber(5);
         assertThat(number).isEqualTo(new LottoNumber(5));
+    }
+
+    @Test
+    @DisplayName("로또 숫자는 1~45 사이의 숫자여야 한다.")
+    public void validateNumberTest() {
+        assertThatThrownBy(() -> {
+            new LottoNumber(46);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
