121e98e9b0f3c48dc835f5b9419407b5ef2d52fa
bomin <bomin_93@naver.com>
2021-02-16 21:47:53 +0900

    feat(LottoRank): 로또 랭크를 담은 enum클래스 기능 구현

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,2 +1,41 @@
-package lotto.domain;public enum LottoRank {
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+  FIRST(6, 2_000_000_000),
+  SECOND(5, 30_000_000),
+  THIRD(5, 1_500_000),
+  FOURTH(4, 50_000),
+  FIFTH(3, 5_000),
+  NONE(0, 0);
+
+  private static final int MIN_MATCH = 3;
+
+  private final int matchCount;
+  private final int winningMoney;
+
+  LottoRank(int matchCount, int winningMoney) {
+    this.matchCount = matchCount;
+    this.winningMoney = winningMoney;
+  }
+
+  public static LottoRank of(int matchCount, boolean bonusMatch) {
+    if(matchCount < MIN_MATCH) {
+      return NONE;
+    }
+
+    if(THIRD.matchCount == matchCount && !bonusMatch) {
+      return THIRD;
+    }
+
+    return Arrays.stream(values())
+        .filter(value -> value.matchCount == matchCount)
+        .findFirst()
+        .orElseThrow(() -> new IllegalStateException("값이 잘못되었습니다."));
+  }
+
+  public int getWinningMoney() {
+    return winningMoney;
+  }
 }
--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -1,4 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 class LottoRankTest {
-  
+
+  @ParameterizedTest
+  @CsvSource(value = {"6:false:FIRST", "5:true:SECOND", "5:false:THIRD", "4:false:FOURTH",
+      "3:false:FIFTH", "2:false:NONE", "1:false:NONE", "0:false:NONE"}, delimiter = ':')
+  void test(int matchCount, boolean bonusMatch, LottoRank expectedRank) {
+    LottoRank rank = LottoRank.of(matchCount, bonusMatch);
+    Assertions.assertThat(rank).isEqualTo(expectedRank);
+  }
 }
