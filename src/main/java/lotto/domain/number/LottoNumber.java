47c89a873cd067dc2a94f27bf890daae87e7bbb3
Junroot <junroot0909@gmail.com>
2021-02-16 18:10:42 +0900

    feat(lotto): 로또 번호 생성 구현

---
+++ src/main/java/lotto/domain/number/LottoNumber.java
@@ -0,0 +1,12 @@
+package lotto.domain.number;
+
+public class LottoNumber extends Number{
+
+    public LottoNumber(int value) {
+        super(value);
+    }
+
+    public LottoNumber(String value) {
+        super(value);
+    }
+}
--- src/test/java/lotto/domain/number/PayOutTest.java
+++ src/test/java/lotto/domain/number/PayOutTest.java
@@ -2,7 +2,6 @@ package lotto.domain.number;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
