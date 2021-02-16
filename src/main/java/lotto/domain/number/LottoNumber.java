1029108ffb57adac027fa412093cb15cc578ece0
Junroot <junroot0909@gmail.com>
2021-02-16 18:24:17 +0900

    feat(lotto): 범위 밖의 로또 번호 생성시 예외처리

--- src/main/java/lotto/domain/number/LottoNumber.java
+++ src/main/java/lotto/domain/number/LottoNumber.java
@@ -2,11 +2,21 @@ package lotto.domain.number;
 
 public class LottoNumber extends Number{
 
-    public LottoNumber(int value) {
-        super(value);
-    }
+    private static int NUMBER_MINIMUM = 1;
+    private static int NUMBER_MAXIMUM = 45;
 
     public LottoNumber(String value) {
         super(value);
+        validateRange();
+    }
+
+    public LottoNumber(int value) {
+        this(String.valueOf(value));
+    }
+
+    private void validateRange() {
+        if (value < NUMBER_MINIMUM || value > NUMBER_MAXIMUM) {
+            throw new IllegalArgumentException("범위 밖의 로또 번호 입니다.");
+        }
     }
 }
