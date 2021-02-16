3a6e99c6f9cc50f4e7550a75cfb6cb5346a23292
Junroot <junroot0909@gmail.com>
2021-02-16 17:45:52 +0900

    test(lotto): 지불 금액이 음수인 경우 테스트

--- src/test/java/lotto/domain/number/PayOutTest.java
+++ src/test/java/lotto/domain/number/PayOutTest.java
@@ -2,6 +2,7 @@ package lotto.domain.number;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PayOutTest {
@@ -14,5 +15,11 @@ public class PayOutTest {
         payOut = new PayOut("10000");
         assertThat(payOut.equals(10000)).isTrue();
     }
-}
 
+    @Test
+    void negativeNumber() {
+        assertThatIllegalArgumentException().isThrownBy(
+            () -> new PayOut(-1)
+        ).withMessage("입력값이 음수 입니다.");
+    }
+}
