c963859bc1f7070fdab934c06b750dbf7ad8adfb
Junroot <junroot0909@gmail.com>
2021-02-16 17:31:55 +0900

    feat(lotto): 구입 금액 입력 구현

---
+++ src/main/java/lotto/domain/number/PayOut.java
@@ -0,0 +1,12 @@
+package lotto.domain.number;
+
+public class PayOut extends Number {
+
+    public PayOut(int value) {
+        super(value);
+    }
+
+    public PayOut(String value) {
+        super(value);
+    }
+}
