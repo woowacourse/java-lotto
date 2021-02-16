79f224b5ad4c251e475838bdf2bbb3279881ac1e
xrabcde <xrabcde@gmail.com>
2021-02-16 15:57:06 +0900

    feat(Lottos): 구입개수 출력하는 getCount 메소드 구현

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,41 @@
+package lotto;
+
+public class Lottos {
+
+    private int money;
+    private int count;
+
+    public Lottos(String input) {
+        int money = changeToInt(input);
+        validateRange(money);
+        validateUnit(money);
+        this.money = money;
+        this.count = this.money / 1000;
+    }
+
+    private int changeToInt(String input) {
+        int money;
+        try {
+            money = Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다");
+        }
+        return money;
+    }
+
+    private void validateRange(int money) {
+        if (money < 1000) {
+            throw new IllegalArgumentException("[ERROR] 금액을 1000원 이상 입력해주세요");
+        }
+    }
+
+    private void validateUnit(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액을 1000단위로 입력해주세요");
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
