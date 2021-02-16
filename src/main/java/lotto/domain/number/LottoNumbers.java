80ae2bff30e2ae99d031b9494e02bf05f0705819
Junroot <junroot0909@gmail.com>
2021-02-16 18:48:12 +0900

    feat(lotto): 로또 번호가 6개가 아닌 경우 예외 처리

--- src/main/java/lotto/domain/number/LottoNumbers.java
+++ src/main/java/lotto/domain/number/LottoNumbers.java
@@ -4,16 +4,26 @@ import java.util.List;
 
 public class LottoNumbers {
 
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
     private final List<LottoNumber> lottoNumbers;
 
     public LottoNumbers(List<LottoNumber> lottoNumbers) {
         validateDuplicate(lottoNumbers);
+        validateLottoNumberCount(lottoNumbers);
+
         this.lottoNumbers = lottoNumbers;
     }
 
     private void validateDuplicate(List<LottoNumber> lottoNumbers) {
         if (lottoNumbers.stream().distinct().count() != lottoNumbers.size()) {
             throw new IllegalArgumentException("로또 넘버에 중복이 있습니다.");
+        }
+    }
+
+    private void validateLottoNumberCount(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또 넘버가 6개가 아닙니다.");
         }
     }
 }
