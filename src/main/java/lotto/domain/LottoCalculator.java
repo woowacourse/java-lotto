e645523a70fb57ee188c489dae9d72c62d29bcb3
mungto <daum7766@naver.com>
2021-02-17 14:50:30 +0900

    feat(LottoCalculator): Add LottoCalculator class

---
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+public class LottoCalculator {
+
+    public static double divide(long money, long winningMoney) {
+
+        return (double) winningMoney / (double) money;
+    }
+}
