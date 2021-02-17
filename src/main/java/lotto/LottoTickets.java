8ce4ea2e496abf1a959f83ec9c365682ae5dda61
Gomding <backchida@gmail.com>
2021-02-17 17:31:12 +0900

    refactor : LottoTickets null 과 empty 값을 생성자의  매개변수로 받을 때 검사 구현

--- src/main/java/lotto/LottoTickets.java
+++ src/main/java/lotto/LottoTickets.java
@@ -7,14 +7,25 @@ import java.util.Objects;
 
 public class LottoTickets {
 
+    private static final String NULL_ERROR_MESSAGE = "null 값은 허용하지 않습니다.";
+    private static final String EMPTY_ERROR_MESSAGE = "로또는 한장 이상 구매해야 합니다.";
+
     private final List<LottoTicket> lottoTickets;
 
-    public LottoTickets(List<LottoTicket> lottoTickets) {
+    public LottoTickets(final List<LottoTicket> lottoTickets) {
+        Objects.requireNonNull(lottoTickets, NULL_ERROR_MESSAGE);
+        validateEmptyTickets(lottoTickets);
         this.lottoTickets = lottoTickets;
     }
 
     public List<LottoTicket> getLottoTickets() {
         return this.lottoTickets;
+    }
+
+    private void validateEmptyTickets(final List<LottoTicket> lottoTickets) {
+        if (lottoTickets.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_ERROR_MESSAGE);
+        }
     }
 
     @Override
--- src/test/java/lotto/LottoTicketsTest.java
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -3,11 +3,15 @@ package lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTicketsTest {
     public LottoTickets lottoTickets;
@@ -44,6 +48,23 @@ public class LottoTicketsTest {
                 new LottoTicket(lottoNumbers3)
         ));
     }
+
+    @ParameterizedTest
+    @NullSource
+    public void nullParameterTest(List<LottoTicket> lottoTickets) {
+        assertThatThrownBy(() -> {
+            new LottoTickets(lottoTickets);
+        }).isInstanceOf(NullPointerException.class).hasMessage("null 값은 허용하지 않습니다.");
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    public void emptyParameterTest(List<LottoTicket> lottoTickets) {
+        assertThatThrownBy(() -> {
+            new LottoTickets(lottoTickets);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("로또는 한장 이상 구매해야 합니다.");
+    }
+
 
     @Test  //이 코드를 좀 더 깔끔하게 쓰는법 혹시 없을까요?
     @DisplayName("구매한 여러장의 로또 티켓들을 가진 객체를 생성한다.")
