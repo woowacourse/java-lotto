22dad54b86c34676cdb715b3d52e97c1557af9a0
perenok <perenoke@gmail.com>
2021-02-17 15:13:47 +0900

    feat(LottoNumber) : implements LottoNumber
    LottoNumber
     - int number를 인스턴스 변수로 가진 클래스
     - 원시값인 int를 포장한다
    validateNumber
     - LottoNumber의 범위(1~45)에 속하는지 검증하는 메소드

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private static final int MIN_NUMBER_OF_LOTTO = 1;
+    private static final int MAX_NUMBER_OF_LOTTO = 45;
+    private final int number;
+
+    public LottoNumber(int number) {
+        validateNumber(number);
+        this.number = number;
+    }
+
+    private void validateNumber(int number) {
+        if (number < MIN_NUMBER_OF_LOTTO || number > MAX_NUMBER_OF_LOTTO) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(number);
+    }
+}
