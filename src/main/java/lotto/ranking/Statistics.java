d4e7f1b80ff68e89ca44a0d803cc92b6b672ffee
Junseo <air.junseo@gmail.com>
2021-02-17 18:12:28 +0900

    feat: 당첨 통계 생성

---
+++ src/main/java/lotto/ranking/Statistics.java
@@ -0,0 +1,26 @@
+package lotto.ranking;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Statistics {
+    private final Map<Ranking, Integer> statistics;
+
+    public Statistics(List<Ranking> rankings) {
+        statistics = new HashMap<>(5);
+        statistics.put(Ranking.FIRST, 0);
+        statistics.put(Ranking.SECOND, 0);
+        statistics.put(Ranking.THIRD, 0);
+        statistics.put(Ranking.FORTH, 0);
+        statistics.put(Ranking.FIFTH, 0);
+
+        for (Ranking ranking : rankings) {
+            statistics.computeIfPresent(ranking, (Ranking key, Integer value) -> ++value);
+        }
+    }
+
+    public int findRankingCount(Ranking ranking) {
+        return statistics.get(ranking);
+    }
+}
---
+++ src/test/java/lotto/ranking/StatisticsTest.java
@@ -0,0 +1,20 @@
+package lotto.ranking;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StatisticsTest {
+    @Test
+    @DisplayName("당첨 통계 생성")
+    void statisticsCreate() {
+        Statistics statistics = new Statistics(Arrays.asList(Ranking.FIRST, Ranking.FORTH, Ranking.FORTH, Ranking.FORTH));
+        assertThat(statistics.findRankingCount(Ranking.FIRST)).isEqualTo(1);
+        assertThat(statistics.findRankingCount(Ranking.SECOND)).isEqualTo(0);
+        assertThat(statistics.findRankingCount(Ranking.THIRD)).isEqualTo(0);
+        assertThat(statistics.findRankingCount(Ranking.FORTH)).isEqualTo(3);
+    }
+}
