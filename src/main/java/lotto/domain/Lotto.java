f30734e72b05ce6998d08c40b097c953eac1c744
mungto <daum7766@naver.com>
2021-02-17 15:32:36 +0900

    feat(Lotto): Add validate code

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,21 +5,37 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import lotto.exception.DuplicateLottoNumberException;
+import lotto.exception.InvalidLottoNumberException;
 
 public class Lotto {
 
     private static final int LOTTO_SIZE = 6;
+    private static final int MIN_BOUND = 1;
+    private static final int MAX_BOUND = 45;
 
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> numbers) {
-        validateDuplicatedNumber(numbers);
+        validates(numbers);
         this.lottoNumbers = new ArrayList<>(numbers);
+    }
+
+    private void validates(List<Integer> values) {
+        validateDuplicatedNumber(values);
+        for (int value : values) {
+            validateBoundNumber(value);
+        }
     }
 
     private void validateDuplicatedNumber(List<Integer> values) {
         if (new HashSet<>(values).size() != LOTTO_SIZE) {
             throw new DuplicateLottoNumberException();
+        }
+    }
+
+    protected void validateBoundNumber(int value) {
+        if (value < MIN_BOUND || value > MAX_BOUND) {
+            throw new InvalidLottoNumberException();
         }
     }
 
