9fe9d0db331755febc8a68dc9cb249843808af59
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-17 17:04:15 +0900

    feat: InputView 추가

--- src/main/java/lotto/controller/LottoMain.java
+++ src/main/java/lotto/controller/LottoMain.java
@@ -1,19 +1,23 @@
 package lotto.controller;
 
+import lotto.domain.LottoComparator;
+import lotto.domain.LottoMachine;
+import lotto.domain.PurchasedLottoTickets;
+import lotto.domain.UserPurchase;
 import lotto.domain.WinningLottoNumbers;
+import lotto.view.InputView;
 
 public class LottoMain {
     public static void main(String[] args) {
-//        UserPurchase userPurchase = InputView.getUserPurchase();
-//
-//        LottoMachine lottoMachine = new LottoMachine();
-//
-//        PurchasedLottoTickets purchasedLottoTickets = lottoMachine.purchaseLottoTicket(userPurchase);
+        UserPurchase userPurchase = InputView.getUserPurchase();
+        LottoMachine lottoMachine = new LottoMachine();
+
+        PurchasedLottoTickets purchasedLottoTickets = lottoMachine.purchaseLottoTicket(userPurchase);
 //        OutputView.printPurchasedLottoTickets(purchasedLottoTickets);
 //
-//        WinningLottoNumbers winningLottoNumbers = InputView.getWinningLottoNumbers();
+        WinningLottoNumbers winningLottoNumbers = InputView.getWinningLottoNumbers();
 //
-//        LottoComparator lottoComparator = new LottoComparator(winningLottoNumbers);
+        LottoComparator lottoComparator = new LottoComparator(winningLottoNumbers);
 //        OutputView(lottoComparator.getResult(purchasedLottoTickets));
     }
 }
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,43 @@
+package lotto.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
+import lotto.domain.UserPurchase;
+import lotto.domain.WinningLottoNumbers;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static UserPurchase getUserPurchase() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchasePriceInput = scanner.nextLine();
+
+        int purchasePrice = Integer.parseInt(purchasePriceInput);
+        return new UserPurchase(purchasePrice);
+    }
+
+    public static WinningLottoNumbers getWinningLottoNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String winningNumbersInput = scanner.nextLine();
+
+        List<LottoNumber> lottoNumbers = Arrays.stream(winningNumbersInput.split(", "))
+            .map(inputNumber -> new LottoNumber(Integer.parseInt(inputNumber)))
+            .collect(Collectors.toList());
+
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+
+        System.out.println("보너스 볼을 입력해 주세요.");
+        String bonusNumberInput = scanner.nextLine();
+
+        LottoNumber bonusNumber = new LottoNumber(Integer.parseInt(bonusNumberInput));
+
+        return new WinningLottoNumbers(lottoTicket, bonusNumber);
+    }
+}
