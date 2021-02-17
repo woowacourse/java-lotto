c2c0e9c4532d538e5a8b4506e369d4d219b54ef6
YeongBeom Heo <hybeom@gmail.com>
2021-02-17 16:37:53 +0900

    feat(OutputView) : 당첨 통계를 출력한다.

--- src/main/java/lotto/viewer/OutputView.java
+++ src/main/java/lotto/viewer/OutputView.java
@@ -1,14 +1,33 @@
 package lotto.viewer;
 
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
 
 public class OutputView {
+
+    private static final String FIFTH_RANK = "3개 일치 (5000원)- %d개";
+    private static final String FOURTH_RANK = "4개 일치 (50000원)- %d개";
+    private static final String THIRD_RANK = "5개 일치 (1500000원)- %d개";
+    private static final String SECOND_RANK = "5개 일치, 보너스 볼 일치(3000000000원)- %d개";
+    private static final String FIRST_RANK = "6개 일치 (2000000000원)- %d개";
+
     public void printPurchasedLottos(List<Lotto> lottoBunch) {
         System.out.printf("%d를 구매하였습니다." + System.lineSeparator(), lottoBunch.size());
         for (Lotto lotto : lottoBunch) {
             System.out.println(lotto.getNumbers());
         }
         System.out.println();
+    }
+
+    public void printLottoStatistics(Map<String, Integer> statistics, double profitRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.printf(FIFTH_RANK + System.lineSeparator(), statistics.get("FIFTH"));
+        System.out.printf(FOURTH_RANK + System.lineSeparator(), statistics.get("FIFTH"));
+        System.out.printf(THIRD_RANK + System.lineSeparator(), statistics.get("FIFTH"));
+        System.out.printf(SECOND_RANK + System.lineSeparator(), statistics.get("FIFTH"));
+        System.out.printf(FIRST_RANK + System.lineSeparator(), statistics.get("FIFTH"));
+        System.out.printf("총 수익률은 %.2f입니다.", profitRate);
     }
 }
