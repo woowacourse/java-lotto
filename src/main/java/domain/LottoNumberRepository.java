ec0d1310c57e3dcb6a3d3c64cb651ef990096730
Kimun Kim <kkm97351@gmail.com>
2021-02-17 12:02:24 +0900

    feat: LottoNumberRepository 생성

---
+++ src/main/java/domain/LottoNumberRepository.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoNumberRepository {
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+    private static final List<LottoNumber> lottoNumbers = new ArrayList<>();
+    private static final int MINIMUM_RANGE = 0;
+    private static final int MAXIMUM_RANGE = 6;
+
+    static {
+        IntStream.rangeClosed(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+                .mapToObj(Integer::toString)
+                .map(LottoNumber::new)
+                .forEach(lottoNumbers::add);
+    }
+
+    public static List<LottoNumber> shuffleLottoNumbers() {
+        Collections.shuffle(lottoNumbers);
+        return Collections.unmodifiableList(lottoNumbers.subList(MINIMUM_RANGE, MAXIMUM_RANGE));
+    }
+}
