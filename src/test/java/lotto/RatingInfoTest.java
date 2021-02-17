4b7d64a5c331c77e3d41b7c7be53eb7861ccd555
pjy1368 <basejin0810@naver.com>
2021-02-17 16:20:21 +0900

    style(RatingInfoTest): 오타 수정

--- src/test/java/lotto/RatingInfoTest.java
+++ src/test/java/lotto/RatingInfoTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class RatingInfoTest {
     @Test
-    @DisplayName("등수 정보 업데이")
+    @DisplayName("등수 정보 업데이트")
     void updateRatingInfo() {
         RatingInfo ratingInfo = new RatingInfo();
         Rating rating = Rating.FIRST;
