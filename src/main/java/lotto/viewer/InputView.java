5f332ec1add26b6fb09003831e9585b268bcad02
YeongBeom Heo <hybeom@gmail.com>
2021-02-17 16:10:24 +0900

    feat(InputView) : 당첨 번호 및 보너스 번호를 입력 받는다.

--- src/main/java/lotto/viewer/InputView.java
+++ src/main/java/lotto/viewer/InputView.java
@@ -1,6 +1,8 @@
 package lotto.viewer;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     final Scanner scanner;
@@ -11,6 +13,20 @@ public class InputView {
 
     public int purchaseMoney() {
         System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public Object inputWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요");
+        String rawWinningNumbers = scanner.nextLine();
+        String[] splittedWinningNumbers = rawWinningNumbers.split(",");
+        return Arrays.stream(splittedWinningNumbers)
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+    }
+
+    public int inputBonusNumber() {
+        System.out.println("보너스 볼을 입력해주세요");
         return scanner.nextInt();
     }
 }
