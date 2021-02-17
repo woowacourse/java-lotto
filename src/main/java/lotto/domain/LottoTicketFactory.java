2d0c457a904f1b4f517b389d4555abd93d2f83a5
jujubebat <bbwwpark@naver.com>
2021-02-17 17:17:47 +0900

    feat: 금액에 따른 로또 티켓을 생성하는 LottoTicketFactory 구현

---
+++ src/main/java/lotto/domain/LottoTicketFactory.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+
+public class LottoTicketFactory {
+    private static final int PRICE_EACH_LOTTO = 1000;
+    private static final RandomLottoGenerator randomLottoGenerator = new RandomLottoGenerator();
+
+    public static LottoTicket createLottoTicket(int money) {
+        int numberOfLottoLine = money / PRICE_EACH_LOTTO;
+
+        if (numberOfLottoLine <= 0) {
+            throw new IllegalArgumentException("[ERROR] 돈이 부족합니다.");
+        }
+
+        ArrayList<LottoLine> lottoLineList = new ArrayList<LottoLine>();
+        for (int i = 0; i < numberOfLottoLine; i++) {
+            lottoLineList.add(new LottoLine(randomLottoGenerator.createLottoLine()));
+        }
+        return new LottoTicket(lottoLineList);
+    }
+}
