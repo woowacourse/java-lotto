df38f720d492f09fa0dc5190cb339bcd641b98f3
pjy1368 <basejin0810@naver.com>
2021-02-17 17:47:41 +0900

    feat(InputView): 입력 로직 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,51 @@
+package lotto.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_MONEY_ERROR_MESSAGE = "구입금액은 숫자여야 합니다.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String NUMBER_DELIMITER = ",";
+    private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
+
+    private InputView() {
+    }
+
+    public static int getInputMoney() {
+        try {
+            OutputView.getMessage(INPUT_MONEY_MESSAGE);
+            String input = scanner.nextLine();
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_MONEY_ERROR_MESSAGE);
+        }
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        try {
+            OutputView.getMessage(INPUT_WINNING_NUMBER_MESSAGE);
+            String input = scanner.nextLine();
+            return Arrays.stream(input.split(NUMBER_DELIMITER))
+                .map(Integer::new)
+                .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_MONEY_ERROR_MESSAGE);
+        }
+    }
+
+    public static int getBonusBall() {
+        try {
+            OutputView.getMessage(INPUT_BONUS_BALL_MESSAGE);
+            String input = scanner.nextLine();
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_MONEY_ERROR_MESSAGE);
+        }
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+public class OutputView {
+
+    public static void getMessage(String message) {
+        System.out.println(message);
+    }
+}
