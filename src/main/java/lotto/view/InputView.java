ab0279b08d083141c6c77f3b2b1b1ce10bda53bd
Gomding <backchida@gmail.com>
2021-02-17 18:05:32 +0900

    feat : InputView.java 유저 입력 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public String getUserInput() throws IOException {
+        try {
+            return br.readLine();
+        } catch (IOException ie) {
+            getUserInput();
+        }
+        return null;
+    }
+}
