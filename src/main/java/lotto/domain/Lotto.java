7bbfa866bd0c38e799077fe3ff2bb07635eef716
mungto <daum7766@naver.com>
2021-02-17 15:20:32 +0900

    feat(Lotto): Add validate code

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,15 +1,26 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import lotto.exception.DuplicateLottoNumberException;
 
 public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
 
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> numbers) {
+        validateDuplicatedNumber(numbers);
         this.lottoNumbers = new ArrayList<>(numbers);
+    }
+
+    private void validateDuplicatedNumber(List<Integer> values) {
+        if (new HashSet<>(values).size() != LOTTO_SIZE) {
+            throw new DuplicateLottoNumberException();
+        }
     }
 
     @Override
