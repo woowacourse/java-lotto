bac2011428e45a90d46851838dc26fd17786fe76
bomin <bomin_93@naver.com>
2021-02-17 20:15:50 +0900

    refactor: 필요없는 import 삭제 및 포맷 교정

--- src/test/java/lotto/util/LottoGeneratorTest.java
+++ src/test/java/lotto/util/LottoGeneratorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +12,7 @@ class LottoGeneratorTest {
   @Test
   @DisplayName("수동 로또 번호 생성")
   void createInputLotto() {
-    Lotto lotto = LottoGenerator.generate(() -> LottoNumber.asList(1,2,3,4,5,6));
+    Lotto lotto = LottoGenerator.generate(() -> LottoNumber.asList(1, 2, 3, 4, 5, 6));
     int expectedMatchCount = 6;
     int matchCount = lotto.matchCount(new Lotto(LottoNumber.asList(1, 2, 3, 4, 5, 6)));
     assertThat(matchCount).isEqualTo(expectedMatchCount);
--- todo.md
+++
@@ -1,9 +0,0 @@
-#ToDo
-@DisplayName
-메서드 순서
-LottoGenerator 리팩토링
-#Done
-Lotto 클래스의 MatchCount, Contains 메서드 테스트
-필요없는 import제거
-예외 처리 하기
-예외처리 메세지 클래스 생성
