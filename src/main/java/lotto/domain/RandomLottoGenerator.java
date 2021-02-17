7a8936ae5df01a879588d1a2c5e17fc1dd7158c0
jujubebat <bbwwpark@naver.com>
2021-02-17 17:15:26 +0900

    feat: 랜덤으로 로또 번호를 결정하는 기능 구현

---
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomLottoGenerator {
+    private static final int START = 1;
+    private static final int END = 45;
+
+    private static final ArrayList<LottoNumber> totalLottoNumberList = new ArrayList<>();
+
+    public RandomLottoGenerator() {
+        for (int i = START; i <= END; i++) {
+            totalLottoNumberList.add(new LottoNumber(i));
+        }
+    }
+
+    public List<LottoNumber> createLottoLine() {
+        Collections.shuffle(totalLottoNumberList);
+        List<LottoNumber> picked = totalLottoNumberList.subList(0, 6);
+        Collections.sort(totalLottoNumberList);
+        return picked;
+    }
+}
