60a601d58e7d65ff33336798abcdd78fbf72c6fb
Naeun <knae11@naver.com>
2021-02-17 17:12:23 +0900

    feat: Rank Enum 생성 및 테스트

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public enum Rank {
+    FIRST_PLACE((val, isBonus) -> val == 6),
+    SEC0ND_PLACE((val, isBonus) -> val == 5 && isBonus),
+    THIRD_PLACE((val, isBonus) -> val == 5 && !isBonus),
+    FOURTH_PLACE((val, isBonus) -> val == 4),
+    FIFTH_PLACE((val, isBonus) -> val == 3),
+    UNRANKED((val, isBonus) -> val < 3);
+
+    private final BiFunction<Integer, Boolean, Boolean> expression;
+
+    Rank(BiFunction<Integer, Boolean, Boolean> expression) {
+        this.expression = expression;
+    }
+
+    public static Rank getInstance(int val, boolean isBonus) {
+        return Arrays.stream(Rank.values())
+            .filter(rank -> rank.expression.apply(val, isBonus))
+            .findFirst()
+            .orElse(UNRANKED);
+    }
+
+}
+
+
