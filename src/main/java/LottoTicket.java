820e8e72b65ea093e3289906fe92d217c307b603
hyeon9mak <jinha3507@gmail.com>
2021-02-17 12:44:09 +0900

    feat(LottoTicket): 중복되지 않는 로또번호 6개 생성하는 기능 구현

---
+++ src/main/java/LottoTicket.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoTicket {
+
+    public static final int START_LOTTO_NUMBER = 1;
+    public static final int FINISH_LOTTO_NUMBER = 45;
+
+    private final LottoNumbers lottoNumbers = new LottoNumbers();
+
+    public void issue() {
+        for (int number : getShuffledNumbers(new ArrayList<>())) {
+            lottoNumbers.add(new LottoNumber(number + ""));
+        }
+    }
+
+    private List<Integer> getShuffledNumbers(List<Integer> numbers) {
+        Collections.shuffle(initNumbers(numbers));
+        return numbers.subList(0, 6);
+    }
+
+    public List<Integer> initNumbers(List<Integer> numbers) {
+        for (int i = START_LOTTO_NUMBER; i <= FINISH_LOTTO_NUMBER; i++) {
+            numbers.add(i);
+        }
+        return numbers;
+    }
+
+    public List<LottoNumber> getLottoNumbers() {
+        return Collections.unmodifiableList(lottoNumbers.toList());
+    }
+}
