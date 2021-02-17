b77186163ce1d17312f13b949212623c5a118490
hyeon9mak <jinha3507@gmail.com>
2021-02-17 15:24:42 +0900

    feat(LottoTickets): 로또 티켓 리스트 일급컬렉션화

---
+++ src/main/java/lottogame/domain/LottoTickets.java
@@ -0,0 +1,18 @@
+package lottogame.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoTickets {
+
+    List<LottoTicket> lottoTickets = new ArrayList<>();
+
+    public void add(final LottoTicket lottoTicket) {
+        lottoTickets.add(lottoTicket);
+    }
+
+    public List<LottoTicket> toList() {
+        return Collections.unmodifiableList(lottoTickets);
+    }
+}
---
+++ src/test/java/lottogame/domain/LottoTicketsTest.java
@@ -0,0 +1,21 @@
+package lottogame.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTicketsTest {
+
+    @Test
+    @DisplayName("LottoTicket 삽입 테스트")
+    void lottoTicketsAddTest() {
+        LottoTicket lottoTicket1 = new LottoTicket();
+        lottoTicket1.issue();
+
+        LottoTickets lottoTickets = new LottoTickets();
+        lottoTickets.add(lottoTicket1);
+
+        assertThat(lottoTickets.toList().size()).isEqualTo(1);
+    }
+}
