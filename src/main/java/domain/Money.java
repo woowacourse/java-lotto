9706067bdcdc1fa406ec3008d548cde68ba3d648
Kimun Kim <kkm97351@gmail.com>
2021-02-17 10:57:26 +0900

    feat: Money 클래스 생성자 및 검증 로직 구현

---
+++ src/main/java/domain/Money.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.util.Objects;
+
+public class Money {
+    private static final String INTEGER_REGULAR_EXPRESSION = "^-?[0-9]+$";
+    private static final int ZERO = 0;
+
+    private final int money;
+
+    public Money(String input) {
+        validateInputFormat(input);
+        int money = Integer.parseInt(input);
+        validatePositiveNumber(money);
+        this.money = money;
+    }
+
+    private void validateInputFormat(String input) {
+        if (input.matches(INTEGER_REGULAR_EXPRESSION)) {
+            return;
+        }
+        throw new InvalidInputException();
+    }
+
+    private void validatePositiveNumber(int input) {
+        if (input < ZERO) {
+            throw new IllegalArgumentException("양수만 입력해주세요.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money1 = (Money) o;
+        return money == money1.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
+    }
+}
---
+++ src/test/java/domain/MoneyTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MoneyTest {
+    @DisplayName("같은 금액의 Money객체는 equals에 true를 반환한다.")
+    @Test
+    public void equalsTest() {
+        Money money1 = new Money("1000");
+        Money money2 = new Money("1000");
+        assertThat(money1).isEqualTo(money2);
+    }
+
+    @DisplayName("숫자가 아닌 인자를 생성자에 넘길 경우 예외를 발생시킨다.")
+    @Test
+    public void validateMoneyFormatTest() {
+        assertThatThrownBy(() -> new Money("a")).isInstanceOf(InvalidInputException.class);
+    }
+
+    @DisplayName("음수가 입력된 경우 예외를 발생시킨다.")
+    @Test
+    public void validatePositiveNumber() {
+        assertThatThrownBy(() -> new Money("-1")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
