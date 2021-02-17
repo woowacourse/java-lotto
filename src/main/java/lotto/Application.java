8b98402c9679d557d320fb2f36fcbfd072439e65
bomin <bomin_93@naver.com>
2021-02-17 16:59:27 +0900

    feat(LottoManager): 전체적인 로또 기능 통합 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,22 +4,18 @@ import lotto.domain.LottoGroup;
 import lotto.domain.LottoRank;
 import lotto.domain.LottoResult;
 import lotto.domain.WinningLotto;
-import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
+
   public static void main(String[] args) {
+    LottoGroup lottoGroup = LottoManager.createLotto();
+    WinningLotto winningLotto = LottoManager.getWinningLotto();
+    LottoManager.printResult(lottoGroup, winningLotto);
 
-    LottoGroup lottoGroup = InputView.lottoGroup();
-    OutputView.printBoughtLotto(lottoGroup);
 
-    WinningLotto winningLotto = InputView.winningLotto();
-
-    LottoResult lottoResult = new LottoResult();
-    for (int i = 0; i < lottoGroup.lottoGroup().size(); i++) {
-      LottoRank lottoRank = winningLotto.matchRank(lottoGroup.lottoGroup().get(i));
-      lottoResult.add(lottoRank);
-    }
-    OutputView.printLottoResult(lottoResult);
+//    LottoResult lottoResult = new LottoResult();
+//    lottoResult.add(LottoRank.SECOND);
+//    OutputView.printLottoResult(lottoResult);
   }
 }
---
+++ src/main/java/lotto/LottoManager.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import lotto.domain.LottoGroup;
+import lotto.domain.LottoResult;
+import lotto.domain.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoManager {
+
+  public static LottoGroup createLotto() {
+    LottoGroup lottoGroup = InputView.lottoGroup();
+    OutputView.printBoughtLotto(lottoGroup);
+
+    return lottoGroup;
+  }
+
+  public static WinningLotto getWinningLotto() {
+    return InputView.winningLotto();
+  }
+
+  public static void printResult(LottoGroup lottoGroup, WinningLotto winningLotto) {
+    LottoResult lottoResult = new LottoResult();
+    lottoGroup.lottoGroup()
+        .stream()
+        .map(winningLotto::matchRank)
+        .forEach(lottoResult::add);
+    OutputView.printLottoResult(lottoResult);
+  }
+
+}
