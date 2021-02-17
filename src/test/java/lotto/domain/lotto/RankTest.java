f1516e3f22d0eb342f69ae549cf17be73625da56
Minsung <sns2831@gmail.com>
2021-02-17 21:51:34 +0900

    test(lotto): 조건에 따른 랭킹 반환 테스트 작성

--- src/test/java/lotto/domain/lotto/RankTest.java
+++ src/test/java/lotto/domain/lotto/RankTest.java
@@ -24,4 +24,19 @@ public class RankTest {
                 () -> Rank.createRanking(10, 0L)
         ).withMessage("매칭되는 등수가 없습니다.");
     }
+
+    @Test
+    @DisplayName("조건에 맞는 랭킹 반환")
+    void getRank_returnRankOfCondition() {
+        assertThat(Rank.getRank(6, false).getRank()).isEqualTo(1);
+        assertThat(Rank.getRank(5, true).getRank()).isEqualTo(2);
+        assertThat(Rank.getRank(5, false).getRank()).isEqualTo(3);
+        assertThat(Rank.getRank(4, false).getRank()).isEqualTo(4);
+        assertThat(Rank.getRank(3, false).getRank()).isEqualTo(5);
+
+        assertThat(Rank.getRank(6, true).getRank()).isEqualTo(1);
+        assertThat(Rank.getRank(4, true).getRank()).isEqualTo(4);
+        assertThat(Rank.getRank(3, true).getRank()).isEqualTo(5);
+
+    }
 }
