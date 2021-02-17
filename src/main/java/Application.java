4f18aa28c93d897351895aeba9f28bc0ed64184b
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-17 20:20:13 +0900

    feat: 유저에게 인풋을 받아서 로또 몇개 구매했는지 출력하는부분까지 완료

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,8 @@
+import lotto.controller.LottoController;
+
 public class Application {
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.lottoStart();
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,4 +11,12 @@ public class OutputView {
     private static final String RESULT_LINE = "---------";
     private static final String EACH_RESULT = "%d개 일치 (%d원)- %d개";
     private static final String FINAL_RESULT = "총 수익률은 %d%입니다.";
+
+    public static void askHowMuchToBuy(){
+        System.out.println(ASK_HOW_MUCH_TO_BUY);
+    }
+
+    public static void howMuchYouBought(int count){
+        System.out.printf(HOW_MUCH_YOU_BOUGHT, count);
+    }
 }
