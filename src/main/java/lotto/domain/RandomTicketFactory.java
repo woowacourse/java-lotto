73d72b4ebc48dd749b758f50dd252af9f9c037af
RinSabbit <celena1004@naver.com>
2021-02-17 11:04:44 +0900

    feat: RandomTicketFactory에서 로또 번호를 셔플해 세트를 반환하는 기능 구현

---
+++ src/main/java/lotto/domain/RandomTicketFactory.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class RandomTicketFactory {
+
+    public static final int LOTTO_NUMBER_MAX_LIMIT = 45;
+    public static final int LOTTO_NUMBER_MIN_LIMIT = 1;
+    public static Map<Integer, LottoNumber> lottoNumberFactory = new HashMap<>();
+
+    static {
+        for (int i = LOTTO_NUMBER_MIN_LIMIT; i < LOTTO_NUMBER_MAX_LIMIT; i++) {
+            lottoNumberFactory.put(i, new LottoNumber(i));
+        }
+    }
+
+    private RandomTicketFactory() {}
+
+    public static Set<LottoNumber> shuffle() {
+        List<Integer> numbers = new ArrayList(lottoNumberFactory.keySet());
+        Set<LottoNumber> ticketNumbers = new HashSet();
+        Collections.shuffle(numbers);
+        for (int i = 0; i < 6; i++) {
+            ticketNumbers.add(lottoNumberFactory.get(numbers.get(i)));
+        }
+        return ticketNumbers;
+    }
+
+}
---
+++ src/test/java/lotto/domain/RandomTicketFactoryTest.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomTicketFactoryTest {
+    @DisplayName("티켓 팩토리에서 생성한 티켓 넘버의 개수가 6개인지 확인")
+    @Test
+    void checkSizeOfTicketNumbers() {
+        Set<LottoNumber> lottoNumbers = RandomTicketFactory.shuffle();
+        assertThat(lottoNumbers.size()).isEqualTo(6);
+    }
+}
