a7f617d8b1a979947910090a2316c456f6944fda
Sehwan_Jang <whilter08@gmail.com>
2021-02-18 00:24:44 +0900

    refactor : 컨트롤러 내 메소드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -9,17 +9,33 @@ import java.util.stream.Collectors;
 public class LottoController {
     public void run() {
         Money money = new Money(InputView.inputMoney());
+        LottoTickets lottoTickets = getLottoTickets(money);
+        LottoTicket winningTicket = getWinningTicket(lottoTickets);
+        LottoResult lottoResult = getLottoResult(lottoTickets, winningTicket);
+        showResult(lottoResult, money);
+    }
+
+    private LottoTickets getLottoTickets(Money money) {
         LottoTicketFactory lottoTicketFactory = new LottoTicketFactory();
-        LottoTickets lottoTickets = new LottoTickets(lottoTicketFactory.buyLottoTickets(money));
+        return new LottoTickets(lottoTicketFactory.buyLottoTickets(money));
+    }
+
+    private LottoTicket getWinningTicket(LottoTickets lottoTickets) {
         OutputView.printLottoTicketsCount(lottoTickets);
         OutputView.printLottoTickets(lottoTickets);
-        LottoTicket winningTicket = new LottoTicket(
+        return new LottoTicket(
                 InputView.inputWinningNumbers().stream()
                         .map(LottoNumber::new)
                         .collect(Collectors.toList()));
+    }
+
+    private LottoResult getLottoResult(LottoTickets lottoTickets, LottoTicket winningTicket) {
         LottoNumber bonusNumber = new LottoNumber(InputView.inputBonusNumber());
         WinningLotto winningLotto = new WinningLotto(winningTicket, bonusNumber);
-        LottoResult lottoResult = new LottoResult(lottoTickets.checkWinningTickets(winningLotto));
+        return new LottoResult(lottoTickets.checkWinningTickets(winningLotto));
+    }
+
+    private void showResult(LottoResult lottoResult, Money money) {
         OutputView.printResultStatistic(lottoResult);
         OutputView.printProfitRate(lottoResult, money);
     }
