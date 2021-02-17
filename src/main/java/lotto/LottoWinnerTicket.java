50bcec0039a0e39a6388736f619eed5a6f20a8aa
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-17 12:16:40 +0900

    feat: 로또 당첨번호 생성

---
+++ src/main/java/lotto/LottoWinnerTicket.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoWinnerTicket extends LottoTicket {
+
+    public LottoWinnerTicket(List<LottoNumber> lottoNumbers) {
+        super(lottoNumbers);
+    }
+}
