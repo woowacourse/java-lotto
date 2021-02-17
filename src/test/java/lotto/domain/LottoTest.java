5e4747e809051ac30c842a8524a0459ef0585361
mungto <daum7766@naver.com>
2021-02-17 15:15:18 +0900

    feat(LottoTest): Add duplicateValidate Test code

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
+import lotto.exception.DuplicateLottoNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +18,11 @@ public class LottoTest {
         assertThat(lotto).isEqualTo(new Lotto(Arrays.asList(11, 25, 32, 41, 7, 3)));
     }
 
-
-
+    @DisplayName("로또번호 에러테스트")
+    @Test
+    void duplicateException() {
+        assertThatThrownBy(() ->
+            new Lotto(Arrays.asList(11, 25, 32, 41, 11, 3))
+        ).isInstanceOf(DuplicateLottoNumberException.class);
+    }
 }
