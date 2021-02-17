6b2b7bc0f497a4801cac5b9b15365beb998a6b07
Sehwan_Jang <whilter08@gmail.com>
2021-02-17 18:38:29 +0900

    feat : 당첨번호 공백제거 및 스플릿 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,21 @@
 package lotto.view;
 
-public class InputView {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    public static final String DELIMITER = ",";
+    public static final String BLANK = " ";
+    public static final String REPLACEMENT = "";
+
+    private InputView() {
+    }
+
+    public static List<String> inputWinningNumbers() {
+        return Arrays.asList(SCANNER.nextLine()
+                .replace(BLANK, REPLACEMENT)
+                .split(DELIMITER));
+    }
 }
