5c21d80ae9461a60055f340784a45b6ed235ecab
mungto <daum7766@naver.com>
2021-02-17 15:34:34 +0900

    refactor(LottoCalculator): code refactoring

--- src/main/java/lotto/domain/LottoCalculator.java
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -3,7 +3,6 @@ package lotto.domain;
 public class LottoCalculator {
 
     public static double divide(long money, long winningMoney) {
-
         return (double) winningMoney / (double) money;
     }
 }
