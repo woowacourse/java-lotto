29d42e367552ac8a3c3bca49dd8b4a57c0c6354f
cjy17 <cjy1705@naver.com>
2021-02-17 16:21:00 +0900

    feat : controller 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+import lotto.domain.Result;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String input = InputView.takeMoneyInput(scanner);
+        Money money = new Money(input);
+        Lottos lottos = new Lottos(money.getMoney());
+        OutputView.lottosResult(lottos);
+
+        List<Integer> winningNumbers = InputView.winningNumbers();
+        int bonusNumber = InputView.bonusNumber();
+
+        List<Result> results = lottos.getResults(winningNumbers, bonusNumber);
+        OutputView.result(results);
     }
 }
