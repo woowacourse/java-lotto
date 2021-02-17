eaf4a0116b990d3b68e2ffe400d33287e441ac12
xrabcde <xrabcde@gmail.com>
2021-02-17 15:31:53 +0900

    test: 당첨여부 계산하는 테스트코드 작성

---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RankTest {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "5,false,THIRD",
+            "5,true,SECOND",
+    })
+    void 등수가_맞게_나오는지_확인(int match, boolean bonusMatch, Rank expected) {
+        Rank rank = Rank.makeRankByMatch(match, bonusMatch);
+        assertThat(rank).isEqualTo(expected);
+    }
+}
