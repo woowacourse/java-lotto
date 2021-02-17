8117a34abc3854a6f618573f0df341a19b341797
Junroot <junroot0909@gmail.com>
2021-02-17 12:33:51 +0900

    test(lotto): 보너스 볼이 하나의 숫자가 아닐 경우 테스트

--- src/test/java/lotto/domain/lotto/WinningNumberTest.java
+++ src/test/java/lotto/domain/lotto/WinningNumberTest.java
@@ -1,17 +1,20 @@
 package lotto.domain.lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.Arrays;
 import lotto.domain.number.LottoNumber;
 import lotto.domain.number.Number;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningNumberTest {
 
     @Test
+    @DisplayName(",를 기준으로 당첨번호를 입력받는다.")
     void getStatistics() {
-        WinningNumber winningNumber = new WinningNumber("1, 2, 3, 4, 5, 6");
+        WinningNumber winningNumber = new WinningNumber("1, 2, 3, 4, 5, 6", "7");
         LottoNumbers expected = new LottoNumbers(
                 Arrays.asList(
                     new LottoNumber(new Number(1)),
@@ -24,5 +27,13 @@ public class WinningNumberTest {
             );
 
         assertThat(expected).isEqualTo(winningNumber.getLottoNumbers());
+    }
+
+    @Test
+    @DisplayName("하나의 숫자가 아닌 경우 예외")
+    void getBonusNumberFromStringInput() {
+        assertThatIllegalArgumentException().isThrownBy(
+            () -> new WinningNumber("1,2,3,4,5,6", "1, 3")
+        ).withMessage("보너스 볼의 하나의 숫자로 이루어져야 합니다.");
     }
 }
