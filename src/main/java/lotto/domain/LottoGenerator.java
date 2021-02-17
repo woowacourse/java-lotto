6b44f8656367188acbb0055dc24f534fc78a9a10
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-17 16:13:46 +0900

    refactor: LottoGenerator 리팩토링

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -7,25 +7,21 @@ import java.util.List;
 public class LottoGenerator {
     private static final int ZERO = 0;
     private static final int SIZE = 6;
-    private static final List<LottoNumber> allNumbers = new ArrayList<>();
+    private final List<LottoNumber> allNumbers;
 
-    static {
+    public LottoGenerator() {
+        this.allNumbers = new ArrayList<>();
         for (int i = LottoNumber.MINIMUM; i <= LottoNumber.MAXIMUM; i++) {
             allNumbers.add(new LottoNumber(i));
         }
     }
 
-    public List<LottoTicket> generatePurchasedTickets(int numberOfTicket) {
-        List<LottoTicket> purchasedTickets = new ArrayList<>();
+    public PurchasedLottoTickets generatePurchasedTickets(int numberOfTicket) {
+        PurchasedLottoTickets purchasedLottoTickets = new PurchasedLottoTickets();
         for (int i = 0; i < numberOfTicket; i++) {
             Collections.shuffle(allNumbers);
-            purchasedTickets.add(new LottoTicket(new ArrayList<>(allNumbers.subList(ZERO, SIZE))));
+            purchasedLottoTickets.add(new LottoTicket(new ArrayList<>(allNumbers.subList(ZERO, SIZE))));
         }
-        return purchasedTickets;
-    }
-
-    public WinningLottoNumbers generateWinningLottoNumbers(LottoTicket lottoTicket,
-        LottoNumber bonusNumber) {
-        return new WinningLottoNumbers(lottoTicket, bonusNumber);
+        return purchasedLottoTickets;
     }
 }
--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -19,25 +19,4 @@ public class LottoGeneratorTest {
         )
     );
 
-    @DisplayName("구입 개수만큼의 로또 생성")
-    @Test
-    void Should_EqualToExpected_When_Purchased() {
-        LottoGenerator lottoGenerator = new LottoGenerator();
-        Assertions.assertThat(lottoGenerator.generatePurchasedTickets(14).size()).isEqualTo(14);
-    }
-
-    @DisplayName("당첨 번호와 보너스 번호 생성")
-    @Test
-    void Should_Return_WinningLottoNumbers_When_WinnerNumberDecided() {
-        LottoGenerator lottoGenerator = new LottoGenerator();
-        LottoNumber bonusNumber = new LottoNumber(7);
-
-        WinningLottoNumbers winningLottoNumbers = lottoGenerator
-            .generateWinningLottoNumbers(lottoTicket, bonusNumber);
-        assertThat(winningLottoNumbers.getWinningTicket().getLottoTicketNumbers())
-            .containsAll(lottoTicket.getLottoTicketNumbers());
-        assertThat(winningLottoNumbers.getBonusNumber()).isEqualTo(bonusNumber);
-        assertThat(winningLottoNumbers.getWinningTicket().getLottoTicketNumbers().size())
-            .isEqualTo(lottoTicket.getLottoTicketNumbers().size());
-    }
 }
