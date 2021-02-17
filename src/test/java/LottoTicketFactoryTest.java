a57c40b6051c404f4cca753158dc2ef5f3658cbc
jujubebat <bbwwpark@naver.com>
2021-02-17 17:18:39 +0900

    feat: LottoTicketFactory 테스트 코드 구현

---
+++ src/test/java/LottoTicketFactoryTest.java
@@ -0,0 +1,17 @@
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoTicketFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTicketFactoryTest {
+
+    @Test
+    @DisplayName("전달 된 돈 만큼의 로또 번호 라인를 가진 로또 티켓을 발행한다")
+    public void create_lotto_ticket_by_sending_money(){
+        LottoTicket lottoTicket = LottoTicketFactory.createLottoTicket(14000);
+        assertThat(lottoTicket.getLength()).isEqualTo(14);
+    }
+}
+
