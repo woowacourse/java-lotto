053b5b09223f0c270fd85c0b18c99bb8ff25c47e
bomin <bomin_93@naver.com>
2021-02-17 16:00:24 +0900

    refactor(Lotto): 메서드 진행 순서 리팩토링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,32 +7,26 @@ import lotto.exception.LottoNumberException;
 
 public class Lotto {
 
+  private static final String JOIN_DELIMITER = ", ";
   private static final int LOTTO_NUMBERS_SIZE = 6;
   private final List<LottoNumber> lottoNumbers;
 
-  public Lotto(List<LottoNumber> lottoNumbers) {
+  public Lotto(final List<LottoNumber> lottoNumbers) {
+    validate(lottoNumbers);
+    Collections.sort(lottoNumbers);
+    this.lottoNumbers = lottoNumbers;
+  }
+
+  private void validate(final List<LottoNumber> lottoNumbers) {
     if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
       throw new LottoNumberException("총 6 개의 숫자가 필요합니다.");
     }
     if (isDuplicated(lottoNumbers)) {
       throw new LottoNumberException("중복된 숫자가 입력이 되었습니다.");
     }
-    Collections.sort(lottoNumbers);
-    this.lottoNumbers = lottoNumbers;
   }
 
-  public boolean contains(LottoNumber lottoNumber) {
-    return lottoNumbers.contains(lottoNumber);
-  }
-
-  public int matchCount(Lotto lotto) {
-    return (int) lottoNumbers
-        .stream()
-        .filter(lotto::contains)
-        .count();
-  }
-
-  private boolean isDuplicated(List<LottoNumber> lottoNumbers) {
+  private boolean isDuplicated(final List<LottoNumber> lottoNumbers) {
     long count = lottoNumbers.stream()
         .mapToInt(LottoNumber::get)
         .distinct()
@@ -40,10 +34,21 @@ public class Lotto {
     return count != lottoNumbers.size();
   }
 
+  public boolean contains(final LottoNumber lottoNumber) {
+    return lottoNumbers.contains(lottoNumber);
+  }
+
+  public int matchCount(final Lotto lotto) {
+    return (int) lottoNumbers
+        .stream()
+        .filter(lotto::contains)
+        .count();
+  }
+
   @Override
   public String toString() {
     return lottoNumbers.stream()
         .map(LottoNumber::toString)
-        .collect(Collectors.joining(", "));
+        .collect(Collectors.joining(JOIN_DELIMITER));
   }
 }
--- todo.md
+++ todo.md
@@ -1,7 +1,8 @@
 #ToDo
-예외처리 메세지 클래스 생성
 @DisplayName
-예외 처리 하기!
+메서드 순서
 #Done
 Lotto 클래스의 MatchCount, Contains 메서드 테스트
 필요없는 import제거
+예외 처리 하기
+예외처리 메세지 클래스 생성
