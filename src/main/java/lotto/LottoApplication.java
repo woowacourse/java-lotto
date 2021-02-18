5c48d6d92613bd9a06363758a31d15d5d3ce18dc
jaeseongDev <qkrwotjd1445@naver.com>
2021-02-18 18:06:45 +0900

    refactor : Application 클래스 이름 변경

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -2,7 +2,7 @@ package lotto;
 
 import lotto.domain.LottoMachine;
 
-public class WebUILottoApplication {
+public class LottoApplication {
 
     public static void main(String[] args) {
         LottoMachine lottoMachine = new LottoMachine();
