0c823512db08f782d824fb056536296f99189497
jujubebat <bbwwpark@naver.com>
2021-02-18 17:12:29 +0900

    refactor: 도메인 수정에 따른 컨트롤러, 뷰 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -7,34 +7,20 @@ import lotto.domain.lotto.LottoLine;
 import lotto.domain.lotto.LottoNumber;
 import lotto.domain.lotto.LottoResult;
 import lotto.domain.lotto.LottoTicket;
-import lotto.domain.lotto.LottoTicketFactory;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
 
     public void run() {
-        try {
-            LottoTicket lottoTicket = getLottoTicket();
-            OutputView.printLottoTicket(lottoTicket);
-            LottoResult lottoResult = checkLottoTicket(lottoTicket, new LottoLine(InputView.getLottoLine()),
-                new LottoNumber(InputView.getBonusLottoNumber()));
-            OutputView.printResult(lottoResult);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
+        Money money = InputView.getMoney();
+        LottoTicket lottoTicket = new LottoTicket(money.getValue());
+        OutputView.printLottoTicket(lottoTicket);
 
-    private LottoTicket getLottoTicket() {
-        Money money = new Money(InputView.getMoney());
-        return LottoTicketFactory.createLottoTicket(money.getValue());
-    }
-
-    private LottoResult checkLottoTicket(LottoTicket lottoTicket, LottoLine winLottoLine,
-        LottoNumber bonusBallNumber) {
-        List<Rank> rankList = lottoTicket
-            .matchLottoLines(winLottoLine.getValues(), bonusBallNumber);
-        return new LottoResult(rankList);
+        LottoLine winningLottoLine = InputView.getLottoLine();
+        LottoNumber bonusLottoNumber = InputView.getBonusLottoNumber();
+        List<Rank> ranks = lottoTicket.matchLottoLines(winningLottoLine.getValues(), bonusLottoNumber);
+        OutputView.printResult(new LottoResult(ranks));
     }
 
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,45 +3,51 @@ package lotto.view;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import lotto.domain.Money;
+import lotto.domain.lotto.LottoLine;
 import lotto.domain.lotto.LottoNumber;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String getMoney() {
+    public static Money getMoney() {
         try {
             System.out.println(InputViewMessages.REQUEST_PURCHASE_MONEY.getMessage());
-            return scanner.nextLine();
+            int inputValue = Integer.parseInt(scanner.nextLine());
+            return new Money(inputValue);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return getMoney();
         }
     }
 
-    public static List<LottoNumber> getLottoLine() {
+    public static LottoLine getLottoLine() {
         try {
             System.out.println(InputViewMessages.REQUEST_LAST_WIN_LOTTO_NUMBERS.getMessage());
-            String lottoNumbersInput = scanner.nextLine();
-            String[] splitLottoNumbersInput = lottoNumbersInput.replace(" ", "").split(",");
-            ArrayList<LottoNumber> lottoNumberList = new ArrayList();
-
-            System.out.println(splitLottoNumbersInput.length);
-            for (int i = 0; i < splitLottoNumbersInput.length; i++) {
-                lottoNumberList.add(new LottoNumber(splitLottoNumbersInput[i]));
-            }
-            return lottoNumberList;
+            String inputValue = scanner.nextLine();
+            List<LottoNumber> lottoNumbers = parseLottoNumber(inputValue);
+            return new LottoLine(lottoNumbers);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return getLottoLine();
         }
     }
 
-    public static int getBonusLottoNumber() {
+    private static List<LottoNumber> parseLottoNumber(String inputValue){
+        String[] parsedInputValue = inputValue.replace(" ", "").split(",");
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (String s : parsedInputValue) {
+            lottoNumbers.add(new LottoNumber(Integer.parseInt(s)));
+        }
+        return lottoNumbers;
+    }
+
+    public static LottoNumber getBonusLottoNumber() {
         try {
             System.out.println(InputViewMessages.REQUEST_LAST_WIN_BONUS_BALL.getMessage());
-            int number = Integer.parseInt(scanner.nextLine());
-            return number;
+            int inputValue = Integer.parseInt(scanner.nextLine());
+            return new LottoNumber(inputValue);
         } catch (Exception e) {
             System.out.printf(e.getMessage());
             return getBonusLottoNumber();
