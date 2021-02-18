dcfc5b599dd9d3943d74967a0160efb57bccabe6
sakjung <ssangyu123@gmail.com>
2021-02-19 00:39:58 +0900

    fix: 당첨 통계 출력 메세지가 잘못 나오는 것을 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,14 +5,13 @@ import lotto.domain.Lottos;
 import lotto.domain.Result;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static lotto.domain.Result.RESULT_END_INDEX;
-import static lotto.domain.Result.SECOND_INDEX;
+import static lotto.domain.Result.getResultsFromFifthToFirst;
 
 public class OutputView {
-    public static final int NUMBER_OF_RESULTS_TO_PRINT = 5;
+    public static final int INDEX_OF_SECOND_RESULT = 3;
 
     public static void showLottos(Lottos lottos) {
         System.out.printf("%d개를 구매했습니다.\n", lottos.getNumberOfLotto());
@@ -26,24 +25,18 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---------");
         List<Integer> statistics = Result.getStatistics(results);
-        List<Result> fifthToFirst = getResultFromFifthToFirst();
+        List<Result> fifthToFirst = getResultsFromFifthToFirst();
         String[] additionalExplanation = getAdditionalExplanation();
-        for (int i = 0; i < Result.values().length - 1; i++) {
+        for (int i = 0; i < statistics.size(); i++) {
             System.out.printf("%d개 일치%s(%d원)- %d개\n", fifthToFirst.get(i).getCount(), additionalExplanation[i], fifthToFirst.get(i).getPrize(), statistics.get(i));
         }
         System.out.printf("총 수익률은 %.2f입니다.\n", profit);
     }
 
-    private static List<Result> getResultFromFifthToFirst() {
-        List<Result> values = Arrays.asList(Result.values());
-        Collections.reverse(values);
-        return values;
-    }
-
     private static String[] getAdditionalExplanation() {
-        String[] bonus = new String[NUMBER_OF_RESULTS_TO_PRINT];
+        String[] bonus = new String[RESULT_END_INDEX];
         Arrays.fill(bonus, " ");
-        bonus[SECOND_INDEX] = ", 보너스 볼 일치";
+        bonus[INDEX_OF_SECOND_RESULT] = ", 보너스 볼 일치";
         return bonus;
     }
 }
