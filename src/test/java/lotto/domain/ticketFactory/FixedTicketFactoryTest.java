618ed71294503fe58bf7716df73f42e8fb1c2375
RinSabbit <celena1004@naver.com>
2021-02-18 12:19:08 +0900

    feat: FixedlottoTicket에 적정한 값이 입력됐는지 확인하는 TC 구현

---
+++ src/test/java/lotto/domain/ticketFactory/FixedTicketFactoryTest.java
@@ -0,0 +1,23 @@
+package lotto.domain.ticketFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import lotto.domain.LottoTicket;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FixedTicketFactoryTest {
+
+    @DisplayName("로또 번호를 생성했을 때, 서로 다른 6개 번호로 구성된 티켓이 반환되어야한다.")
+    @Test
+    void makeTicket() {
+        Set<String> lottoNumbers = new HashSet<>(Arrays.asList("1","2","3","4","5","6"));
+        LottoTicket lottoTicket = FixedTicketFactory.makeTicket(lottoNumbers);
+
+        Set<Integer> actualLottoNumbers = new HashSet<>(Arrays.asList(1,2,3,4,5,6));
+        assertThat(lottoTicket.getLottoNumbers()).isEqualTo(actualLottoNumbers);
+    }
+}
