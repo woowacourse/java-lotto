9b4c669f6dae99d6056faa5ba168ff12acff91aa
joanne <mathmjseo@naver.com>
2021-02-18 23:47:59 +0900

    refactor: InputView에서 parseInt 검증을 수행하도록 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,15 @@
 package lotto.view;
 
+import lotto.exception.LottoCustomException;
+
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InputView {
+
+    private static final String SEPARATOR = ",";
 
     private final Scanner scanner;
 
@@ -10,7 +17,29 @@ public class InputView {
         this.scanner = new Scanner(System.in);
     }
 
-    public String inputValue() {
-        return scanner.nextLine();
+    public static int parseInt(final String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new LottoCustomException("구입 금액은 숫자만 가능합니다.");
+        }
+    }
+
+    public int inputMoney() {
+        return parseInt(scanner.nextLine());
+    }
+
+    public Set<String> inputWinningTicket() {
+        return splitWinningNumbers(scanner.nextLine());
+    }
+
+    public int inputBonusNumber() {
+        return parseInt(scanner.nextLine());
+    }
+
+    private Set<String> splitWinningNumbers(String winningNumbers) {
+        return Arrays.stream(winningNumbers.split(SEPARATOR))
+                .map(String::trim)
+                .collect(Collectors.toSet());
     }
 }
