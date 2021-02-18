1ecda7f6057a2742ef9e1899378cd102d26cc186
jujubebat <bbwwpark@naver.com>
2021-02-18 17:30:33 +0900

    style: 코드 인덴트 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -19,28 +19,22 @@ public class OutputView {
 
     public static void printResult(LottoResult lottoResult) {
         System.out.println(OUTPUT_WIN_STATISTICS_MESSAGES.getMessage());
-        System.out.printf((OUTPUT_FIFTH_RESULT_FORMAT.getMessage()) + "%n",
-            lottoResult.findNumberOfRank(Rank.FIFTH));
-        System.out.printf((OUTPUT_FORTH_RESULT_FORMAT.getMessage()) + "%n",
-            lottoResult.findNumberOfRank(Rank.FOURTH));
-        System.out.printf((OUTPUT_THIRD_RESULT_FORMAT.getMessage()) + "%n",
-            lottoResult.findNumberOfRank(Rank.THIRD));
-        System.out.printf((OUTPUT_SECOND_RESULT_FORMAT.getMessage()) + "%n",
-            lottoResult.findNumberOfRank(Rank.SECOND));
-        System.out.printf((OUTPUT_FIRST_RESULT_FORMAT.getMessage()) + "%n",
-            lottoResult.findNumberOfRank(Rank.FIRST));
-        System.out.printf((OUTPUT_PROFIT_RESULT_FORMAT.getMessage()) + "%n",
-            lottoResult.calculateProfitRate());
+        System.out.printf((OUTPUT_FIFTH_RESULT_FORMAT.getMessage()) + "%n", lottoResult.getNumberOfRank(Rank.FIFTH));
+        System.out.printf((OUTPUT_FORTH_RESULT_FORMAT.getMessage()) + "%n", lottoResult.getNumberOfRank(Rank.FOURTH));
+        System.out.printf((OUTPUT_THIRD_RESULT_FORMAT.getMessage()) + "%n", lottoResult.getNumberOfRank(Rank.THIRD));
+        System.out.printf((OUTPUT_SECOND_RESULT_FORMAT.getMessage()) + "%n", lottoResult.getNumberOfRank(Rank.SECOND));
+        System.out.printf((OUTPUT_FIRST_RESULT_FORMAT.getMessage()) + "%n", lottoResult.getNumberOfRank(Rank.FIRST));
+        System.out.printf((OUTPUT_PROFIT_RESULT_FORMAT.getMessage()) + "%n", lottoResult.calculateProfitRate());
     }
 
     public static void printLottoTicket(LottoTicket lottoTicket) {
         for (LottoLine line : lottoTicket.getLottoLines()) {
 
-            List<String> lottoNumbers = line.getValues().stream()
-                .sorted()
+            List<String> lottoNumbers = line.getValues().stream().sorted()
                 .map(lottoNumber -> String.valueOf(lottoNumber.getValue()))
                 .collect(Collectors.toList());
             System.out.println("[" + String.join(",", lottoNumbers) + "]");
         }
     }
+
 }
