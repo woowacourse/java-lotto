24307fae8897952393063dc413d2a8ed6d56a29d
Naeun <knae11@naver.com>
2021-02-18 17:21:25 +0900

    feat: 구입금액 Money 원시값 포장

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.math.BigDecimal;
+import java.util.regex.Pattern;
+import lotto.utils.CustomException;
+
+public class Money {
+    private static final String IS_NUMBER = "\\d+";
+
+    private final BigDecimal money;
+
+    public Money(String moneyValue) {
+        validateIsNumber(moneyValue);
+        this.money = new BigDecimal(moneyValue);
+    }
+
+    private void validateIsNumber(String moneyValue) {
+        if (!Pattern.matches(IS_NUMBER, moneyValue)) {
+            throw new CustomException("구입금액은 숫자이어야 합니다.");
+        }
+    }
+
+    public int getPossibleTicketCount(){
+        return money.intValue()/LottoTicket.PRICE;
+    }
+
+}
