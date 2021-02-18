f19eece347579313ddab53d955e2c895edccf7ae
Eugine Park <pkeugine@gmail.com>
2021-02-18 17:43:45 +0900

    fix(WinningNumbers): WinningNumbers 검증 누락 수정

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -10,6 +10,8 @@ public class WinningNumbers {
     private static final int WINNING_TICKET_SIZE = 6;
     private static final int BONUS_NUMBER_SIZE = 1;
 
+    private static final String WINNING_TICKET_SIZE_ERROR = "당첨 번호의 개수가 맞지 않습니다.";
+    private static final String BONUS_DUPLICATE_ERROR = "당첨 번호에 보너스 번호가 포함되어 있습니다.";
     private final LottoTicket winningTicket;
     private final LottoNumber bonusNumber;
 
@@ -19,16 +21,24 @@ public class WinningNumbers {
     }
 
     public static WinningNumbers valueOf(final List<Integer> winningNumbers, final int bonusNumber) {
-        validateDistinctBonus(winningNumbers, bonusNumber);
+        validate(winningNumbers, bonusNumber);
         return new WinningNumbers(winningNumbers, bonusNumber);
     }
 
-    private static void validateDistinctBonus(final List<Integer> winningNumbers, final int bonusNumber) {
-        final Set<Integer> numbers = new HashSet<>(winningNumbers);
-        numbers.add(bonusNumber);
+    private static void validate(final List<Integer> winningNumbers, final int bonusNumber) {
+        validateSize(winningNumbers);
+        validateDistinctBonus(winningNumbers, bonusNumber);
+    }
 
-        if (numbers.size() != WINNING_TICKET_SIZE + BONUS_NUMBER_SIZE) {
-            throw new IllegalArgumentException("당첨 번호에 보너스 번호가 포함되어 있습니다.");
+    private static void validateSize(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != WINNING_TICKET_SIZE) {
+            throw new IllegalArgumentException(WINNING_TICKET_SIZE_ERROR);
+        }
+    }
+
+    private static void validateDistinctBonus(final List<Integer> winningNumbers, final int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_DUPLICATE_ERROR);
         }
     }
 
