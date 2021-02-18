91f0abc81f76d9c491ac32d8b443a8097d135d2c
sakjung <ssangyu123@gmail.com>
2021-02-18 17:29:26 +0900

    refactor: lottos관련 테스트 리팩토링

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -10,17 +10,13 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottosTest {
-    private Lottos lottos;
-
-    @BeforeEach
-    void setUp() {
-        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 20, 21, 40));
-        Lotto lotto2 = new Lotto(Arrays.asList(1, 2, 20, 25, 29, 45));
-        lottos = new Lottos(Arrays.asList(lotto1, lotto2));
-    }
-
+    @DisplayName("가지고 있는 로또와 당첨번호를 비교해서 결과를 제대로 반환 하는지")
     @Test
     void testEntireLottoMatching() {
+        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 20, 21, 40));
+        Lotto lotto2 = new Lotto(Arrays.asList(1, 2, 20, 25, 29, 45));
+        Lottos lottos = new Lottos(Arrays.asList(lotto1, lotto2));
+
         List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         int bonusNumber = 20;
         List<Result> results = lottos.getResults(new WinningLotto(winningNumbers, bonusNumber));
@@ -33,8 +29,8 @@ public class LottosTest {
     @DisplayName("로또들을 만들어 주는 테스트")
     @Test
     void makeLottos() {
-        Lottos lottos1 = new Lottos(3);
+        Lottos lottos = new Lottos(3);
 
-        assertThat(lottos1.getNumberOfLotto()).isEqualTo(3);
+        assertThat(lottos.getNumberOfLotto()).isEqualTo(3);
     }
 }
