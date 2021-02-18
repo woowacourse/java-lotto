1c401496afef21fdd97b177aee92938e02546972
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-02-18 14:55:06 +0900

    feat: 구매한 로또 번호들 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import lotto.Money;
 import lotto.domain.lotto.Lottos;
 import lotto.domain.lotto.Lotto;
@@ -8,15 +12,20 @@ import lotto.domain.lotto.LottoNumber;
 
 public class OutputView {
 
-    public static void numPurchasedLotto(Integer numLotto){
+    public static void numPurchasedLotto(Integer numLotto) {
         System.out.println(numLotto + "개를 구매했습니다.");
-    };
+    }
 
     public static void inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
     public static void lottosPrint(Lottos purchasedLottos) {
-        System.out.println(purchasedLottos);
+        for (Lotto lotto : purchasedLottos.getLottos()) {
+            System.out.println(lotto.getNumbers()
+                .stream()
+                .map(LottoNumber::getValue)
+                .collect(Collectors.toList()));
+        }
     }
 }
