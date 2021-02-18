b3b90dfa171e7896b4812f4c42a21722676d507e
Sehwan_Jang <whilter08@gmail.com>
2021-02-18 11:11:37 +0900

    feat : 로또 번호 입력 오류 출력 기능 구현

--- src/main/java/lotto/exception/IllegalLottoNumber.java
+++ src/main/java/lotto/exception/IllegalLottoNumber.java
@@ -1,4 +1,9 @@
 package lotto.exception;
 
+import lotto.view.ErrorView;
+
 public class IllegalLottoNumber extends IllegalArgumentException {
+    public IllegalLottoNumber() {
+        ErrorView.printIllegalLottoNumberMessage();
+    }
 }
