ecfc34253a8cab6e4d4b0c1b7946a5c1e899420f
mungto <daum7766@naver.com>
2021-02-18 16:53:50 +0900

    refactor: Modify LottoTicketsTest test cade

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -20,7 +20,7 @@ public class LottoTicketsTest {
     @DisplayName("같은 번호가 생성되었는지 테스트")
     @Test
     void numberSort() {
-        LottoTickets lottoTickets = new LottoTickets(1, Arrays.asList(2, 4, 6, 2, 3, 1));
+        LottoTickets lottoTickets = new LottoTickets(1, Arrays.asList(2, 4, 6, 5, 3, 1));
         List<Lotto> expectedLottoTickets = new ArrayList<>();
 
         expectedLottoTickets.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
