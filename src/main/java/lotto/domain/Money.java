bb986d601cdf8f49647ae394c7d3501c15c9cdf0
perenok <perenoke@gmail.com>
2021-02-18 14:53:43 +0900

    style(Money) : add error message

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -25,19 +25,19 @@ public class Money {
         try {
             Integer.parseInt(money);
         } catch (NumberFormatException e) {
-            throw new NumberFormatException();
+            throw new NumberFormatException("숫자만 입력 가능합니다.");
         }
     }
 
     private void validateEmpty(String money) {
         if (money.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
         }
     }
 
     private void validatePrice(String money) {
         if (Integer.parseInt(money) < PRICE_OF_LOTTO) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또를 구매에는 최소 1000원 이상의 금액이 필요합니다.");
         }
     }
 }
