63f043c3795f0eee056001465804b4b9d6709e4f
perenok <perenoke@gmail.com>
2021-02-18 14:52:25 +0900

    feat(Lotto) : add validate logic

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,26 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class Lotto {
 
+    public static final int LOTTO_NUMBER_SIZE = 6;
     private final List<LottoNumber> lottoNumbers = new ArrayList<>();
 
     public Lotto() {
     }
 
     public Lotto(List<LottoNumber> lottoNumbers) {
+        validateNumberSize(lottoNumbers);
+        validateDuplicate(lottoNumbers);
         this.lottoNumbers.addAll(lottoNumbers);
+    }
+
+    public List<LottoNumber> lotto() {
+        return Collections.unmodifiableList(lottoNumbers);
     }
 
     public boolean containNumber(LottoNumber number) {
@@ -23,6 +31,22 @@ public class Lotto {
         return (int) lottoNumbers.stream()
                 .filter(lotto::containNumber)
                 .count();
+    }
+
+    private void validateNumberSize(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("로또의 번호 개수가 맞지 않습니다.");
+        }
+    }
+
+    private void validateDuplicate(List<LottoNumber> lottoNumbers) {
+        int numberAfterDistinct = (int) lottoNumbers.stream()
+                .mapToInt(LottoNumber::getNumber)
+                .distinct()
+                .count();
+        if (numberAfterDistinct != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("중복된 번호가 있습니다.");
+        }
     }
 
     @Override
@@ -36,10 +60,5 @@ public class Lotto {
     @Override
     public int hashCode() {
         return Objects.hash(lottoNumbers);
-    }
-
-    @Override
-    public String toString() {
-        return "lottoNumbers = " + lottoNumbers;
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -11,29 +11,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LottoTest {
 
-    Lotto lotto = new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(3)));
+    Lotto lotto = new Lotto(Arrays.asList(new LottoNumber("1"), new LottoNumber("3"),
+            new LottoNumber("4"), new LottoNumber("5"), new LottoNumber("6"), new LottoNumber("7")));
 
     @DisplayName("로또에 있는 번호가 왔을 때 테스트")
     @Test
     void testContainNumber() {
-        LottoNumber number = new LottoNumber(1);
+        LottoNumber number = new LottoNumber("1");
         assertTrue(lotto.containNumber(number));
     }
 
     @DisplayName("로또에 없는 번호가 왔을 때 테스트")
     @Test
     void testNotContainNumber() {
-        LottoNumber number = new LottoNumber(2);
+        LottoNumber number = new LottoNumber("2");
         assertFalse(lotto.containNumber(number));
     }
 
     @DisplayName("다른 로또와 번호가 몇 개 일치하는지 테스")
     @Test
     void testCountOfMatchNumber() {
-        Lotto lotto1 = new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(3), new LottoNumber(3)));
-        Lotto lotto2 = new Lotto(Arrays.asList(new LottoNumber(2), new LottoNumber(3), new LottoNumber(34)));
+        Lotto otherLotto = new Lotto(Arrays.asList(new LottoNumber("1"), new LottoNumber("2"), new LottoNumber("3"),
+                                                new LottoNumber("4"), new LottoNumber("5"), new LottoNumber("6")));
 
-        assertThat(lotto1.countOfMatchNumber(lotto2)).isEqualTo(2);
+        assertThat(otherLotto.countOfMatchNumber(lotto)).isEqualTo(5);
     }
-
 }
