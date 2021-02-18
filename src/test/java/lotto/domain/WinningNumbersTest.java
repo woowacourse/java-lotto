199015ca1cd62a9ee9182fdb1d8fec1e16213e6f
김주원 <kjw11077@naver.com>
2021-02-18 17:20:50 +0900

    refactor: WinningNumbersTest 테스트코드 리팩토링

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,143 +1,69 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class WinningNumbersTest {
 
-    @Test
-    void generate_winning_numbers() {
-        assertThatCode(() -> {
-            WinningNumbers winningNumber = new WinningNumbers("1,2,3,4,5,6", "7");
-        }).doesNotThrowAnyException();
-    }
-
-    @Test
-    @DisplayName("실패 - 보너스볼과 로또넘버가 중복되는 경우")
-    void generate_duplicated_case() {
-        assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,6", "6"))
+    @ParameterizedTest
+    @MethodSource("provideDuplicatedNumbersTestCase")
+    void fail_duplicatedNumbers(String lottoNumbersValue, String bonusBallValue) {
+        assertThatThrownBy(() -> new WinningNumbers(lottoNumbersValue, bonusBallValue))
                 .isInstanceOf(RuntimeException.class);
     }
 
-    @Test
-    @DisplayName("실패 - 보너스볼이 숫자가 아닌경우")
-    void generate_duplicated_case2() {
-        assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,6", "s"))
+    private static Stream<Arguments> provideDuplicatedNumbersTestCase() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", "6"),
+                Arguments.of("1,2,3,4,6,6", "7")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidInputTestCase")
+    void fail_invalidInput(String lottoNumbersValue, String bonusBallValue) {
+        assertThatThrownBy(() -> new WinningNumbers(lottoNumbersValue, bonusBallValue))
                 .isInstanceOf(RuntimeException.class);
     }
 
-    @Test
-    @DisplayName("실패 - 로또넘버가 6개가 아닌 경우")
-    void generate_duplicated_case3() {
-        assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5", "6"))
-                .isInstanceOf(RuntimeException.class);
+    private static Stream<Arguments> provideInvalidInputTestCase() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", "s"),
+                Arguments.of("1,2,3,4,5", "7"),
+                Arguments.of("1,2,3,4,5,6,7", "8"),
+                Arguments.of("1,2,3,4,5,6", "7,8"),
+                Arguments.of("1,2,3,4,5,6", "46"),
+                Arguments.of("1,2,3,4,5,6", "0"),
+                Arguments.of("", ""),
+                Arguments.of("1", "2"),
+                Arguments.of("1", ""),
+                Arguments.of("", "1")
+        );
     }
 
-    @Test
-    @DisplayName("실패 - 보너스볼이 1개가 아닌 경우")
-    void generate_duplicated_case4() {
-        assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,6", "7,8"))
-                .isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    @DisplayName("실패 - 보너스볼 값이 1~45 사이가 아닌 경우")
-    void generate_duplicated_case5() {
-        assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,6", "46"))
-                .isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    @DisplayName("일치하는 갯수 체크 - 6")
-    void count_same_numbers1() {
+    @ParameterizedTest
+    @MethodSource("provideGetRankTestCase")
+    void getRank(String lottoTicketValue, Rank expectedRank) {
         WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("1,2,3,4,5,6");
+        LottoTicket lottoTicket = new LottoTicket(lottoTicketValue);
 
-        assertThat(winningNumbers.countMatches(lottoTicket)).isEqualTo(6);
+        assertThat(winningNumbers.getRank(lottoTicket)).isEqualTo(expectedRank);
     }
 
-    @Test
-    @DisplayName("일치하는 갯수 체크 - 5")
-    void count_same_numbers2() {
-        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("1,2,3,4,5,8");
-
-        assertThat(winningNumbers.countMatches(lottoTicket)).isEqualTo(5);
-    }
-
-    @Test
-    @DisplayName("일치하는 갯수 체크 - 0")
-    void count_same_numbers3() {
-        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("11,12,13,14,15,16");
-
-        assertThat(winningNumbers.countMatches(lottoTicket)).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("일치하는 갯수 체크 - 6")
-    void count_same_numbers4() {
-        WinningNumbers winningNumbers = new WinningNumbers("3,2,1,6,5,4", "7");
-        LottoTicket lottoTicket = new LottoTicket("1,2,3,4,5,6");
-
-        assertThat(winningNumbers.countMatches(lottoTicket)).isEqualTo(6);
-    }
-
-    @Test
-    @DisplayName("1등 랭크 반환")
-    void getRank1() {
-        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("1,2,3,4,5,6");
-
-        assertThat(winningNumbers.getRank(lottoTicket)).isEqualTo(Rank.FIRST_PLACE);
-    }
-
-    @Test
-    @DisplayName("2등 랭크 반환")
-    void getRank2() {
-        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("1,2,3,4,5,7");
-
-        assertThat(winningNumbers.getRank(lottoTicket)).isEqualTo(Rank.SEC0ND_PLACE);
-    }
-
-    @Test
-    @DisplayName("3등 랭크 반환")
-    void getRank3() {
-        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("1,2,3,4,5,12");
-
-        assertThat(winningNumbers.getRank(lottoTicket)).isEqualTo(Rank.THIRD_PLACE);
-    }
-
-    @Test
-    @DisplayName("4등 랭크 반환")
-    void getRank4() {
-        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("1,2,3,4,12,13");
-
-        assertThat(winningNumbers.getRank(lottoTicket)).isEqualTo(Rank.FOURTH_PLACE);
-    }
-
-    @Test
-    @DisplayName("5등 랭크 반환")
-    void getRank5() {
-        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("1,2,3,12,13,14");
-
-        assertThat(winningNumbers.getRank(lottoTicket)).isEqualTo(Rank.FIFTH_PLACE);
-    }
-
-    @Test
-    @DisplayName("NONE 랭크 반환")
-    void getRank_none() {
-        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "7");
-        LottoTicket lottoTicket = new LottoTicket("11,12,13,14,15,16");
-
-        assertThat(winningNumbers.getRank(lottoTicket)).isEqualTo(Rank.UNRANKED);
+    private static Stream<Arguments> provideGetRankTestCase() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", Rank.FIRST_PLACE),
+                Arguments.of("1,2,3,4,5,7", Rank.SEC0ND_PLACE),
+                Arguments.of("1,2,3,4,5,12", Rank.THIRD_PLACE),
+                Arguments.of("1,2,3,4,12,13", Rank.FOURTH_PLACE),
+                Arguments.of("1,2,3,12,13,14", Rank.FIFTH_PLACE),
+                Arguments.of("1,2,13,14,15,16", Rank.UNRANKED),
+                Arguments.of("11,12,13,14,15,16", Rank.UNRANKED)
+        );
     }
 }
