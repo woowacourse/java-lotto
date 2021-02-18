553d97fa4d6b6aaddb2a14ea3a8741df29b97459
mungto <daum7766@naver.com>
2021-02-18 12:28:35 +0900

    style: Add LottoTicketsTest code in Blank

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -16,14 +16,14 @@ public class LottoTicketsTest {
         LottoTickets lottoTickets = new LottoTickets(14);
         assertThat(lottoTickets.getLottoTickets().size()).isEqualTo(14);
     }
-    
+
     @DisplayName("같은 번호가 생성되었는지 테스트")
     @Test
     void numberSort() {
-        LottoTickets lottoTickets = new LottoTickets(1, Arrays.asList(2,4,6,2,3,1));
+        LottoTickets lottoTickets = new LottoTickets(1, Arrays.asList(2, 4, 6, 2, 3, 1));
         List<Lotto> expectedLottoTickets = new ArrayList<>();
 
-        expectedLottoTickets.add(new Lotto(Arrays.asList(1,2,3,4,5,6)));
+        expectedLottoTickets.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
         assertThat(lottoTickets.getLottoTickets()).isEqualTo(expectedLottoTickets);
     }
 }
