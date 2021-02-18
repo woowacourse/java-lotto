f6df0a5b50fc805a75c37bbb5b0dbf7b1dd36e75
RinSabbit <celena1004@naver.com>
2021-02-18 11:36:38 +0900

    feat: FixedTicketFactory에서 입력된 당첨 번호가 1-45사이의 숫자인지 확인하는 기능 구현

--- src/main/java/lotto/domain/FixedTicketFactory.java
+++ src/main/java/lotto/domain/FixedTicketFactory.java
@@ -9,7 +9,7 @@ public class FixedTicketFactory {
 
     public static LottoTicket makeTicket(Set<String> lottoNumbers) {
         if(!isProperSize(lottoNumbers)){
-            throw new LottoCustomException("당첨번호는 6개여야 합니다.");
+            throw new LottoCustomException("당첨번호는 중복되지 않은 숫자들로 총 6개이어야 합니다.");
         }
         return new LottoTicket(getFixedLottoNumbers(lottoNumbers));
     }
--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.exception.LottoCustomException;
 import lotto.utils.ValidateUtils;
 
 public class LottoNumber {
@@ -7,11 +8,18 @@ public class LottoNumber {
     private final int number;
 
     public LottoNumber(final int number) {
+        if(isNotProperRange(number)){
+            throw new LottoCustomException("로또 번호는 1-45사이의 숫자이어야 합니다");
+        }
         this.number = number;
     }
 
     LottoNumber(final String number) {
         this(ValidateUtils.parseInt(number));
+    }
+
+    private boolean isNotProperRange(int number) {
+        return !(number >= 1 && number <= 45);
     }
 
     public Integer getNumber() {
