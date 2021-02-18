d1fc0588fba036fee64b3b570e654e9a51c98eb0
perenok <perenoke@gmail.com>
2021-02-18 17:54:56 +0900

    refactor(Rank) : change enum order

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,12 +3,12 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
+    NOTHING(0, 0),
     FIFTH(3, 5000),
-    NOTHING(0, 0);
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000);
 
     private int countOfMatch;
     private int reward;
@@ -31,13 +31,10 @@ public enum Rank {
             return THIRD;
         }
         return Arrays.stream(values())
+                .filter(rank -> rank != THIRD)
                 .filter(rank -> rank.matchCount(countOfMatch))
                 .findFirst()
                 .orElse(NOTHING);
-    }
-
-    public boolean matchRank(Rank rank) {
-        return this.reward == rank.reward;
     }
 
     private boolean matchCount(int countOfMatch) {
