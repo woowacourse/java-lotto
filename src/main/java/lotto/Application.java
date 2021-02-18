4c117fe371f7a604f1aac9a694947a6fcf9c2ee9
DWL5 <ssop6403@gmail.com>
2021-02-18 16:34:53 +0900

    style: 의미없는 줄바꿈 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,10 +3,9 @@ package lotto;
 import lotto.controller.LottoController;
 
 public class Application {
-
+    
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
         lottoController.run();
     }
-
 }
