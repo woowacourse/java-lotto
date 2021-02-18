4cb04186b0c4ff961fdfdda99f99fc1213652ded
jujubebat <bbwwpark@naver.com>
2021-02-18 11:14:09 +0900

    refactor: 로또 우승상금 금액 수정함

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public enum Rank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(5, 50_000),
-    FOURTH(4, 5_000),
-    FIFTH(3, 0),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     NO_MATCH(0, 0);
 
     private int matchCount;
