7d4ed644d90c8d2393516ecb424276416a49768b
perenok <perenoke@gmail.com>
2021-02-18 17:49:02 +0900

    feat(LottoGameController) : implements LottoGameController
    LottoGameController
     - 도메인과 뷰를 연결
    buyAutoLotto
     - 돈을 입력받아 금액에 맞는 로또들을 반환
    makeWinningLotto
     - WinningLotto를 반환
    showGameResult
     - 구입한 로또들과 당첨 로또를 비교하여 OutPutView에 인자 반환

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,50 @@
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+
+    private final LottoGame lottoGame = new LottoGame();
+
+    public void run() {
+        Lottos lottos = buyAutoLotto();
+
+        WinningLotto lastWinningLotto =
+                makeWinningLotto(lastWinningLotto(), InputView.askBonusNumber());
+
+        showGameResult(lottos, lastWinningLotto);
+    }
+
+    private Lottos buyAutoLotto() {
+        Money money = InputView.askMoney();
+        Lottos lottos = lottoGame.buyLottos(money);
+        OutputView.printEachLotto(lottos);
+        return lottos;
+    }
+
+    private WinningLotto makeWinningLotto(Lotto winningLotto, LottoNumber bonusNumber) {
+        try {
+            return new WinningLotto(winningLotto, bonusNumber);
+        } catch (Exception e) {
+            OutputView.printError(e);
+            bonusNumber = InputView.askBonusNumber();
+            return makeWinningLotto(winningLotto, bonusNumber);
+        }
+    }
+
+    private Lotto lastWinningLotto() {
+        try {
+            return new Lotto(InputView.askLastWinningLottoNumber());
+        } catch (Exception e) {
+            OutputView.printError(e);
+            return lastWinningLotto();
+        }
+    }
+
+    private void showGameResult(Lottos lottos, WinningLotto lastWinningLotto) {
+        LottoGameResult lottoGameResult = lottoGame.compareWithWinningLotto(lottos, lastWinningLotto);
+        OutputView.printLottoGameResult(lottoGameResult);
+    }
+}
