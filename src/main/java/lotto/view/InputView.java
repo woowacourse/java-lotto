445b837506135b95051bde7a1e383f5e154adf54
jujubebat <bbwwpark@naver.com>
2021-02-18 11:14:59 +0900

    feat: 로또 입력 뷰를 구현함

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,36 @@
+package lotto.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import lotto.domain.lotto.LottoNumber;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getMoney() {
+        System.out.println(InputViewMessages.REQUEST_PURCHASE_MONEY.getMessage());
+        return scanner.nextLine();
+    }
+
+    public static List<LottoNumber> getLottoLine() {
+        System.out.println(InputViewMessages.REQUEST_LAST_WIN_LOTTO_NUMBERS.getMessage());
+        String lottoNumbersInput = scanner.nextLine();
+        String[] splitLottoNumbersInput = lottoNumbersInput.split(", ");
+
+        ArrayList<LottoNumber> lottoNumberList = new ArrayList();
+        for (int i = 0; i < splitLottoNumbersInput.length; i++) {
+            lottoNumberList.add(new LottoNumber(splitLottoNumbersInput[i]));
+        }
+
+        return lottoNumberList;
+    }
+
+    public static int getBonusLottoNumber() {
+        System.out.println(InputViewMessages.REQUEST_LAST_WIN_BONUS_BALL.getMessage());
+        int number = Integer.parseInt(scanner.nextLine());
+        return number;
+    }
+
+}
