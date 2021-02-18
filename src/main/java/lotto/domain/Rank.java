da38e5f5c5964c8671faafa8ecfa35eaf7182628
thisisyoungbin <thisisyoungbin@gmail.com>
2021-02-19 00:25:27 +0900

    style: 돈의 단위 구분을 위해 _ 구분자 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public enum Rank {
     NOTHING(0, 0),
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 30000000),
-    FIRST(6, 2000000000);
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
 
     private int countOfMatch;
     private int reward;
@@ -30,6 +30,7 @@ public enum Rank {
         if (!bonusNumber && THIRD.matchCount(countOfMatch)) {
             return THIRD;
         }
+
         return Arrays.stream(values())
                 .filter(rank -> rank != THIRD)
                 .filter(rank -> rank.matchCount(countOfMatch))
