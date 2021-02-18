0b5dc2e802c11ec5b3cc47fd9094a790746d024f
pjy1368 <basejin0810@naver.com>
2021-02-18 11:34:48 +0900

    test(LottoRepositoryTest): 로또 생성 정보 비교 테스트 코드 작성

--- src/test/java/lotto/domain/lotto/LottoRepositoryTest.java
+++ src/test/java/lotto/domain/lotto/LottoRepositoryTest.java
@@ -2,6 +2,8 @@ package lotto.domain.lotto;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.domain.lotto.LottoRepository;
 import lotto.domain.lottomachine.RandomLottoMachine;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +17,15 @@ public class LottoRepositoryTest {
         LottoRepository lottoRepository = new LottoRepository();
         lottoRepository.generateLottoByTicket(new RandomLottoMachine(), 5);
         assertThat(lottoRepository.toList().size()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("로또 생성 정보 비교")
+    void compareLottoByTicket() {
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        LottoRepository lottoRepository = new LottoRepository();
+        lottoRepository.generateLottoByTicket(() -> expected, 5);
+        assertThat(lottoRepository.toList().get(0).getNumbers()).isEqualTo(expected);
     }
 
 }
