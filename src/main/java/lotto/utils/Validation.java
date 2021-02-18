32731e8cb84287af179a9be5e1d0495eb893a0b0
jujubebat <bbwwpark@naver.com>
2021-02-18 13:24:42 +0900

    feat: 로또 숫자번호 검증 책임을 갖는 클래스구현

--- src/main/java/lotto/utils/Validation.java
+++ src/main/java/lotto/utils/Validation.java
@@ -2,8 +2,6 @@ package lotto.utils;
 
 public class Validation {
 
-    public static final int START = 1;
-    public static final int END = 45;
 
     public static Boolean isNumeric(String value) {
         try {
