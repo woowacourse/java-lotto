e5d922994b32e652e40d453570a0811a921fb62e
Gomding <backchida@gmail.com>
2021-02-19 03:06:16 +0900

    refactor : LottoWinner 클래스의 equals 와 hashcode 오버라이딩 메서드 제거

--- src/main/java/lotto/domain/LottoWinner.java
+++ src/main/java/lotto/domain/LottoWinner.java
@@ -36,18 +36,4 @@ public class LottoWinner {
     public LottoWinnerBonusNumber getLottoWinnerBonusNumber() {
         return lottoWinnerBonusNumber;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LottoWinner that = (LottoWinner) o;
-        return Objects.equals(lottoWinnerTicket, that.lottoWinnerTicket) &&
-                Objects.equals(lottoWinnerBonusNumber, that.lottoWinnerBonusNumber);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(lottoWinnerTicket, lottoWinnerBonusNumber);
-    }
 }
