7083756ad81a5f0029c9aba34f5e33a8597d6af1
Jinhong <56240505+xlffm3@users.noreply.github.com>
2021-02-18 14:07:53 +0900

    refactor: 로또 번호 생성기 클래스 상수 분리

--- src/main/java/lotto/domain/RandomLottoNumberGenerator.java
+++ src/main/java/lotto/domain/RandomLottoNumberGenerator.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -9,12 +10,14 @@ public class RandomLottoNumberGenerator implements LottoNumberGenerator {
     private static final List<Integer> NUMBERS = IntStream.rangeClosed(1, 45)
             .boxed()
             .collect(Collectors.toList());
+    private static final int FROM_INDEX = 0;
+    private static final int TO_INDEX = 6;
 
     @Override
     public List<Integer> generate() {
         Collections.shuffle(NUMBERS);
-        List<Integer> numbers = NUMBERS.subList(0, 6);
-        Collections.sort(numbers);
+        List<Integer> numbers = NUMBERS.subList(FROM_INDEX, TO_INDEX);
+        numbers.sort(Comparator.naturalOrder());
         return numbers;
     }
 }
