fb6a6e2faa3459d013d744d880b5a746d4274c0c
DWL5 <ssop6403@gmail.com>
2021-02-18 16:26:23 +0900

    refactor: inputview try-catch 문 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,42 +11,27 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String getMoney() {
-        try {
-            System.out.println(InputViewMessages.REQUEST_PURCHASE_MONEY.getMessage());
-            return scanner.nextLine();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return getMoney();
-        }
+        System.out.println(InputViewMessages.REQUEST_PURCHASE_MONEY.getMessage());
+        return scanner.nextLine();
     }
 
     public static List<LottoNumber> getLottoLine() {
-        try {
-            System.out.println(InputViewMessages.REQUEST_LAST_WIN_LOTTO_NUMBERS.getMessage());
-            String lottoNumbersInput = scanner.nextLine();
-            String[] splitLottoNumbersInput = lottoNumbersInput.replace(" ", "").split(",");
-            ArrayList<LottoNumber> lottoNumberList = new ArrayList();
+        System.out.println(InputViewMessages.REQUEST_LAST_WIN_LOTTO_NUMBERS.getMessage());
+        String lottoNumbersInput = scanner.nextLine();
+        String[] splitLottoNumbersInput = lottoNumbersInput.replace(" ", "").split(",");
+        ArrayList<LottoNumber> lottoNumberList = new ArrayList();
 
-            System.out.println(splitLottoNumbersInput.length);
-            for (int i = 0; i < splitLottoNumbersInput.length; i++) {
-                lottoNumberList.add(new LottoNumber(splitLottoNumbersInput[i]));
-            }
-            return lottoNumberList;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return getLottoLine();
+        System.out.println(splitLottoNumbersInput.length);
+        for (int i = 0; i < splitLottoNumbersInput.length; i++) {
+            lottoNumberList.add(new LottoNumber(splitLottoNumbersInput[i]));
         }
+        return lottoNumberList;
     }
 
     public static int getBonusLottoNumber() {
-        try {
-            System.out.println(InputViewMessages.REQUEST_LAST_WIN_BONUS_BALL.getMessage());
-            int number = Integer.parseInt(scanner.nextLine());
-            return number;
-        } catch (Exception e) {
-            System.out.printf(e.getMessage());
-            return getBonusLottoNumber();
-        }
+        System.out.println(InputViewMessages.REQUEST_LAST_WIN_BONUS_BALL.getMessage());
+        int number = Integer.parseInt(scanner.nextLine());
+        return number;
     }
-
+    
 }
