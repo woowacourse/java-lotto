47824ee9ad570dbe03e4b8599307f4430744a5d4
pjy1368 <basejin0810@naver.com>
2021-02-18 11:12:35 +0900

    refactor(Counter): 당첨된 등수의 횟수를 불변객체로 생성

--- src/main/java/lotto/domain/rating/RatingInfo.java
+++ src/main/java/lotto/domain/rating/RatingInfo.java
@@ -4,20 +4,23 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class RatingInfo {
-    private Map<Rating, Integer> ratings;
+
+    private Map<Rating, Counter> ratings;
 
     public RatingInfo() {
         ratings = new EnumMap<>(Rating.class);
         for (Rating rating : Rating.values()) {
-            ratings.put(rating, 0);
+            ratings.put(rating, new Counter());
         }
     }
 
     public void update(final Rating rating) {
-        ratings.put(rating, ratings.get(rating) + 1);
+        Counter currentCounter = ratings.get(rating);
+        ratings.put(rating, currentCounter.plus());
     }
 
     public int get(final Rating rating) {
-        return ratings.get(rating);
+        Counter counter = ratings.get(rating);
+        return counter.getCount();
     }
 }
