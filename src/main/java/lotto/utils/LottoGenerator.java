5824b5af46d16ac1374514bc66ae15f0a29a71ea
perenok <perenoke@gmail.com>
2021-02-18 14:57:32 +0900

    refactor(LottoGenerator) : change parameter type and move LOTTO_NUMBER_SIZE const to Lotto

--- src/main/java/lotto/utils/LottoGenerator.java
+++ src/main/java/lotto/utils/LottoGenerator.java
@@ -11,7 +11,6 @@ public class LottoGenerator implements RandomGenerator {
 
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
-    public static final int LOTTO_NUMBER_SIZE = 6;
     public static final int FROM_INDEX = 0;
 
     private List<LottoNumber> numbers = new ArrayList<>();
@@ -22,15 +21,14 @@ public class LottoGenerator implements RandomGenerator {
 
     private void createNumber() {
         for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
-            numbers.add(new LottoNumber(i));
+            numbers.add(new LottoNumber(Integer.toString(i)));
         }
     }
-
 
     @Override
     public Lotto generate() {
         Collections.shuffle(numbers);
-        List<LottoNumber> lottoNumbers = numbers.subList(FROM_INDEX, LOTTO_NUMBER_SIZE);
+        List<LottoNumber> lottoNumbers = numbers.subList(FROM_INDEX, Lotto.LOTTO_NUMBER_SIZE);
         Collections.sort(lottoNumbers);
         return new Lotto(lottoNumbers);
     }
