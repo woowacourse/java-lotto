7032134f3ad3d974bb62ca4700a4a5634a3e7d09
Jinhong <56240505+xlffm3@users.noreply.github.com>
2021-02-18 14:16:10 +0900

    refactor: 수익률 계산 기능 메서드 분리 및 반복문 삭제

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import java.util.Map;
-import java.util.Set;
 
 public class LottoStatistics {
     private static final long ZERO = 0;
@@ -17,12 +16,15 @@ public class LottoStatistics {
     }
 
     public double calculateYield(PurchasingPrice purchasingPrice) {
-        Set<LottoRank> lottoRanks = statisticsMap.keySet();
-        long priceTotal = ZERO;
-        for (LottoRank lottoRank : lottoRanks) {
-            priceTotal += lottoRank.getPrizeMoney() * statisticsMap.get(lottoRank);
-        }
-        int denominator = purchasingPrice.getPrice();
-        return (((double) priceTotal) / denominator);
+        long prizeMoneyTotal = calculatePrizeMoneyTotal();
+        int denominatorCost = purchasingPrice.getPrice();
+        return (((double) prizeMoneyTotal) / denominatorCost);
+    }
+
+    private long calculatePrizeMoneyTotal() {
+        return statisticsMap.keySet()
+                .stream()
+                .mapToLong(lottoRank -> lottoRank.getPrizeMoney() * statisticsMap.get(lottoRank))
+                .sum();
     }
 }
