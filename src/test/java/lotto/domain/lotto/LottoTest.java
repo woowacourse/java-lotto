6fd9a8b49b3ddc28a6e6bed14084cd22c48f816f
binghe819 <binghe819@naver.com>
2021-02-18 14:48:59 +0900

    test: 랜덤 로또 번호 생성 테스트 코드 추가

--- src/test/java/lotto/domain/lotto/LottoTest.java
+++ src/test/java/lotto/domain/lotto/LottoTest.java
@@ -1,8 +1,10 @@
 package lotto.domain.lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.Arrays;
+import java.util.List;
 import lotto.domain.lotto.lottogenerator.RandomLottoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
@@ -13,29 +15,18 @@ public class LottoTest {
     @DisplayName("로또 번호는 6개로 구성되어있어야한다.")
     @Test
     void 로또_번호_길이_테스트() {
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5);
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            Lotto lotto = Lotto.generatedBy(Arrays.asList(1, 2, 3, 4, 5));
+            Lotto lotto = Lotto.of(lottoNumbers);
         });
     }
 
     @DisplayName("6개의 로또 번호는 서로 다른 번호여야한다.")
     @Test
     void 로또_번호_중복_테스트() {
+        List<Integer> lottoNumbers = Arrays.asList(1, 1, 2, 3, 4, 5);
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            Lotto lotto = Lotto.generatedBy(Arrays.asList(1, 1, 2, 3, 4, 5));
+            Lotto lotto = Lotto.of(lottoNumbers);
         });
-    }
-
-    @RepeatedTest(10)
-    void 로또_번호_랜덤_생성_테스트() {
-        // given, when
-        Lotto lotto1 = Lotto.generatedBy(new RandomLottoGenerator());
-
-        // then
-        lotto1.getNumbers()
-            .stream()
-            .map(number -> number.getValue())
-            .forEach(number -> System.out.print(number + " "));
-        System.out.println();
     }
 }
---
+++ src/test/java/lotto/domain/lotto/lottogenerator/RandomLottoGeneratorTest.java
@@ -0,0 +1,32 @@
+package lotto.domain.lotto.lottogenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import lotto.domain.lotto.LottoNumber;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RandomLottoGeneratorTest {
+
+    List<LottoNumber> lottoNumbers;
+
+    @BeforeEach
+    void setUp() {
+        lottoNumbers = new RandomLottoGenerator().generateLottoNumbers();
+    }
+
+    @DisplayName("랜덤으로 생성된 번호들은 6개여야 한다.")
+    @Test
+    void 번호_길이_테스트() {
+        assertThat(lottoNumbers.size()).isEqualTo(6);
+    }
+
+    @DisplayName("랜덤으로 생성된 번호들은 서로 중복되면 안된다.")
+    @Test
+    void 번호_중복_테스트() {
+        assertThat(lottoNumbers).extracting(lottoNumber -> lottoNumber.getValue()).doesNotHaveDuplicates();
+    }
+}
