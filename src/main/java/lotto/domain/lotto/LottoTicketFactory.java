c4736be212ff9e1fab975f3dfe393527943040f5
DWL5 <ssop6403@gmail.com>
2021-02-18 16:33:57 +0900

    refactor: 로또 라인의 개수 구하는 기능 함수로 분리

--- src/main/java/lotto/domain/lotto/LottoTicketFactory.java
+++ src/main/java/lotto/domain/lotto/LottoTicketFactory.java
@@ -11,16 +11,20 @@ public class LottoTicketFactory {
     private static final RandomLottoGenerator randomLottoGenerator = new RandomLottoGenerator();
 
     public static LottoTicket createLottoTicket(int money) {
-        int numberOfLottoLine = money / PRICE_EACH_LOTTO;
-
-        if (numberOfLottoLine <= 0) {
-            throw new IllegalArgumentException(ErrorMessages.ERROR_LOTTO_MONEY_NOT_ENOUGH.getMessage());
-        }
+        int numberOfLottoLine = getNumberOfLottoLine(money);
 
         ArrayList<LottoLine> lottoLineList = new ArrayList<LottoLine>();
         for (int i = 0; i < numberOfLottoLine; i++) {
             lottoLineList.add(new LottoLine(randomLottoGenerator.createLottoLine()));
         }
         return new LottoTicket(lottoLineList);
+    }
+
+    private static int getNumberOfLottoLine(int money) {
+        int numberOfLottoLine = money / PRICE_EACH_LOTTO;
+        if (numberOfLottoLine <= 0) {
+            throw new IllegalArgumentException(ErrorMessages.ERROR_LOTTO_MONEY_NOT_ENOUGH.getMessage());
+        }
+        return numberOfLottoLine;
     }
 }
