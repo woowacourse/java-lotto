4ae0ccdd3a3e3706c65df5dc84855c93b58369a0
RinSabbit <celena1004@naver.com>
2021-02-19 01:11:33 +0900

    feat: Money에서 구입한 티켓 갯수 구하는 기능과 수익률을 계산하는 기능 TC 구현

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -24,4 +24,17 @@ public class MoneyTest {
         assertThatIllegalArgumentException()
             .isThrownBy(() -> new Money(999));
     }
+
+    @DisplayName("입력금액을 1000으로 나눈 몫만큼 티켓을 만들 수 있다.")
+    @Test
+    void countTickets() {
+        assertThat(new Money(3000).countTickets()).isEqualTo(3);
+        assertThat(new Money(4500).countTickets()).isEqualTo(4);
+    }
+
+    @DisplayName("구입 금액과 리워드를 이용해 수익률을 계산할 수 있다.")
+    @Test
+    void calculateProfit() {
+        assertThat(new Money(5000).calculateProfit(5000)).isEqualTo(1.00f);
+    }
 }
