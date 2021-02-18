ecff8b4847348e3fb464b3853e660f864433d752
jujubebat <bbwwpark@naver.com>
2021-02-18 17:48:16 +0900

    refactor: 메세지 클래스들 수정

--- src/main/java/lotto/view/ErrorMessages.java
+++ src/main/java/lotto/view/ErrorMessages.java
@@ -1,9 +1,7 @@
 package lotto.view;
 
 public enum ErrorMessages {
-    ERROR_LOTTO_NUMBER_DUPLICATED("[Error] 로또번호 라인은 중복되지 않은 6개의 숫자여야 합니다."),
-    ERROR_LOTTO_NUMBER_OUT_OF_BOUND("[Error] 로또 번호는 1부터 45까지 입니다."),
-    ERROR_LOTTO_MONEY_NOT_ENOUGH("[ERROR] 돈이 부족합니다.");
+    ERROR_LOTTO_NUMBER_DUPLICATED("[Error] 로또번호 라인은 중복되지 않은 6개의 숫자여야 합니다.");
 
     private final String message;
 
--- src/main/java/lotto/view/OutputMessages.java
+++ src/main/java/lotto/view/OutputMessages.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 public enum OutputMessages {
+
     OUTPUT_WIN_STATISTICS_MESSAGES("당첨 통계\n---------"),
     OUTPUT_FIFTH_RESULT_FORMAT("3개 일치 (5000원)- %d개"),
     OUTPUT_FORTH_RESULT_FORMAT("4개 일치 (50000원)- %d개"),
@@ -8,7 +9,6 @@ public enum OutputMessages {
     OUTPUT_SECOND_RESULT_FORMAT("5개 일치, 보너스 볼 일치 (30000000원)- %d개"),
     OUTPUT_FIRST_RESULT_FORMAT("6개 일치 (2000000000원)- %d개"),
     OUTPUT_PROFIT_RESULT_FORMAT("총 수익률은 %.2f 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
-
 
     private final String message;
 
