8e0558df2a82500bdab28a7fc6b4de83246ab956
yoonsung.jung <ybook2012@gmail.com>
2021-02-19 16:45:35 +0900

    refactor(LottoGameResult) : 당첨된 티켓의 개수와 당첨 정보를 담는 map을 일급 컬렉션화

--- src/main/java/lottogame/domain/LottoGame.java
+++ src/main/java/lottogame/domain/LottoGame.java
@@ -10,10 +10,8 @@ import lottogame.domain.ticket.LottoTicketResult;
 import lottogame.domain.ticket.LottoTickets;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class LottoGame {
 
@@ -22,6 +20,7 @@ public class LottoGame {
     private LottoNumbers drawnWinningNumbers;
     private LottoNumber drawnBonusNumber;
     private LottoWinningMachine lottoWinningMachine;
+    private LottoGameResult lottoGameResult;
 
     public void buyTickets(final Money money) {
         this.initMoney = new Money(Integer.toString(money.getValue()));
@@ -38,16 +37,6 @@ public class LottoGame {
         return lottoTickets;
     }
 
-    public Map<Rank, Integer> getLottoGameResult() {
-        Map<Rank, Integer> lottoGameResult = initLottoGameResult();
-        for (LottoTicketResult winningTicket : getWinningTickets()) {
-            Rank rank = Rank.getRank(winningTicket);
-            lottoGameResult.put(rank, lottoGameResult.get(rank) + 1);
-        }
-        lottoGameResult.remove(Rank.FAIL);
-        return lottoGameResult;
-    }
-
     private List<LottoTicketResult> getWinningTickets() {
         lottoWinningMachine = new LottoWinningMachine(drawnWinningNumbers, drawnBonusNumber);
         List<LottoTicketResult> lottoTicketResults = new ArrayList<>();
@@ -60,19 +49,12 @@ public class LottoGame {
         return lottoTicketResults;
     }
 
-    private Map<Rank, Integer> initLottoGameResult() {
-        Map<Rank, Integer> lottoGameResult = new LinkedHashMap<>();
-        for (Rank rank : Rank.values()) {
-            lottoGameResult.put(rank, 0);
-        }
-        return lottoGameResult;
+    public Map<Rank, Integer> getLottoGameResult() {
+        lottoGameResult = new LottoGameResult(getWinningTickets());
+        return lottoGameResult.toMap();
     }
 
     public double getLottoGameYield() {
-        double priceAmount = 0.0;
-        for (Entry<Rank, Integer> rank : getLottoGameResult().entrySet()) {
-            priceAmount += rank.getKey().getPrice() * rank.getValue();
-        }
-        return priceAmount / initMoney.getValue();
+        return lottoGameResult.getPriceAmount() / initMoney.getValue();
     }
 }
---
+++ src/main/java/lottogame/domain/LottoGameResult.java
@@ -0,0 +1,39 @@
+package lottogame.domain;
+
+import lottogame.domain.ticket.LottoTicketResult;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoGameResult {
+    private final Map<Rank, Integer> lottoGameResult;
+
+    {
+        lottoGameResult = new LinkedHashMap<>();
+        for (Rank rank : Rank.values()) {
+            lottoGameResult.put(rank, 0);
+        }
+    }
+
+    public LottoGameResult(final List<LottoTicketResult> winningTickets) {
+        for (LottoTicketResult winingTicket : winningTickets) {
+            Rank rank = Rank.getRank(winingTicket);
+            lottoGameResult.put(rank, lottoGameResult.get(rank) + 1);
+        }
+        lottoGameResult.remove(Rank.FAIL);
+    }
+
+    public Map<Rank, Integer> toMap() {
+        return Collections.unmodifiableMap(lottoGameResult);
+    }
+
+    public double getPriceAmount() {
+        double priceAmount = 0.0;
+        for (Map.Entry<Rank, Integer> rank : lottoGameResult.entrySet()) {
+            priceAmount += rank.getKey().getPrice() * rank.getValue();
+        }
+        return priceAmount;
+    }
+}
