9d1a069a14a9216ed4b95ecef6fa47c8c16342b4
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-20 05:57:34 +0900

    refactor(LottoMatchType): 우승 상금 가독성 위해 '_' 추가

--- src/main/java/lotto/type/LottoMatchType.java
+++ src/main/java/lotto/type/LottoMatchType.java
@@ -5,15 +5,15 @@ import java.util.List;
 import lotto.domain.LottoNumber;
 
 public enum LottoMatchType {
-    THREE_MATCH(3, 5000,
+    THREE_MATCH(3, 5_000,
         "3개 일치 (5000원) - %d개"),
-    FOUR_MATCH(4, 50000,
+    FOUR_MATCH(4, 50_000,
         "4개 일치 (50000원) - %d개"),
-    FIVE_MATCH(5, 1500000,
+    FIVE_MATCH(5, 1_500_000,
         "5개 일치 (1500000원) - %d개"),
-    FIVE_AND_BONUS_MATCH(6, 30000000,
+    FIVE_AND_BONUS_MATCH(6, 30_000_000,
         "5개 일치, 보너스 볼 일치(30000000원) - %d개"),
-    SIX_MATCH(6, 2000000000,
+    SIX_MATCH(6, 2_000_000_000,
         "6개 일치 (2000000000원) - %d개");
 
     private final int countMatchedNumbers;
