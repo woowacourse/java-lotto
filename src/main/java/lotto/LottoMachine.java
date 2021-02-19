e7d68511ad931007c1f4b9eef659c7cc64a6044a
Seed <hsik0225@gmail.com>
2021-02-19 15:55:14 +0900

    style : reformat code

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,37 +1,45 @@
 package lotto;
 
-import java.util.List;
-
-import lotto.domain.*;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoStatisticResult;
+import lotto.domain.Lottos;
+import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class LottoMachine {
-
+    
     private final InputView inputView;
-
+    
     private final OutputView outputView;
-
+    
     public LottoMachine() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
     }
-
-    public Lottos purchase() {
-        int purchaseCount = inputView.readPaymentAmount().getPurchaseCount();
-        Lottos lottos = Lottos.makeLottos(purchaseCount);
-        outputView.printPurchaseLottos(purchaseCount, lottos);
     
+    public Lottos purchase() {
+        int purchaseCount = inputView.readPaymentAmount()
+                                     .getPurchaseCount();
+        
+        Lottos lottos = Lottos.makeLottos(purchaseCount);
+        
+        outputView.printPurchaseLottos(purchaseCount, lottos);
+        
         return lottos;
     }
     
     public void seeResults(Lottos lottos) {
         List<Integer> lotto = inputView.readWinningLotto();
+        
         LottoNumber bonusNumber = inputView.readBonusNumber();
-    
+        
         WinningLotto winningLotto = WinningLotto.of(lotto, bonusNumber);
-        LottoStatisticResult result = Rank.match(lottos, winningLotto);
-    
-        outputView.printStatistics(result);
+        
+        LottoStatisticResult result = lottos.retrieveResults(winningLotto);
+        
+        outputView.printStatistic(result);
     }
 }
