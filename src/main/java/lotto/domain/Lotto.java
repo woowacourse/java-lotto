c8bad45a7e5ab2cb3ca6a909541f9c492ea0b402
cjy17 <cjy1705@naver.com>
2021-02-19 14:24:16 +0900

    feat(Lotto) : 로또 생성시 유효성 검사 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,7 +8,18 @@ public class Lotto {
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        validateLottoNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validateLottoNumbers(List<Integer> lottoNumbers) {
+        if(lottoNumbers.size() != 6){
+            throw new IllegalArgumentException("로또는 6개의 번호가 있어야합니다.");
+        }
+
+        if(!lottoNumbers.stream().allMatch((i) -> i >= 1 && i<= 45)){
+            throw new IllegalArgumentException("유효하지 않은 로또 번호입니다.");
+        }
     }
 
     public int matchingCount(List<Integer> numbers) {
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LottoTest {
@@ -15,6 +17,26 @@ public class LottoTest {
     @BeforeEach
     void setUp() {
         lotto = new Lotto(Arrays.asList(1, 2, 3, 20, 21, 40));
+    }
+
+    @DisplayName("6개 아닌 로또 번호 생성 오류 확인")
+    @Test
+    void lottoLengthCheck(){
+        assertThatThrownBy(()->{
+            new Lotto(Arrays.asList(1,2));
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또는 6개의 번호가 있어야합니다.");
+
+    }
+
+    @DisplayName("유효하지 않는 로또 번호 생성 오류 확인")
+    @Test
+    void validateCheck(){
+        assertThatThrownBy(()->{
+            new Lotto(Arrays.asList(-1, 2, 3, 20, 21, 40));
+            new Lotto(Arrays.asList(100, 2, 3, 20, 21, 40));
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 로또 번호입니다.");
     }
 
     @Test
