87ae9d1f3df0f6901c1d04f14821ebe31ec82bbc
김주원 <kjw11077@naver.com>
2021-02-19 17:28:10 +0900

    test: EarningRate 테스트 진행

--- src/main/java/lotto/domain/result/EarningRate.java
+++ src/main/java/lotto/domain/result/EarningRate.java
@@ -6,8 +6,8 @@ public class EarningRate {
 
     private final BigInteger earningRate;
 
-    public EarningRate(BigInteger totalPrize, BigInteger buyPrice) {
-        this.earningRate = (totalPrize.multiply(BigInteger.valueOf(100)).divide(buyPrice));
+    public EarningRate(BigInteger totalPrize, BigInteger paymentAmount) {
+        this.earningRate = (totalPrize.multiply(BigInteger.valueOf(100)).divide(paymentAmount));
     }
 
     public BigInteger toBigInteger() {
--- src/test/java/lotto/domain/result/EarningRateTest.java
+++ src/test/java/lotto/domain/result/EarningRateTest.java
@@ -1,5 +1,33 @@
 package lotto.domain.result;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigInteger;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class EarningRateTest {
 
+    @ParameterizedTest
+    @MethodSource("calculate_earningRate_testcase")
+    void calculate_earningRate(BigInteger totalPrize, BigInteger paymentAmount, BigInteger expected) {
+        EarningRate earningRate = new EarningRate(totalPrize, paymentAmount);
+
+        assertThat(earningRate.toBigInteger()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> calculate_earningRate_testcase() {
+        // totalPrize, paymentAmount, expected
+        return Stream.of(
+                Arguments.of(toBigInteger(1000), toBigInteger(1000), toBigInteger(100)),
+                Arguments.of(toBigInteger(0), toBigInteger(1000), toBigInteger(0)),
+                Arguments.of(toBigInteger(500), toBigInteger(1000), toBigInteger(50))
+        );
+    }
+
+    private static BigInteger toBigInteger(long i) {
+        return BigInteger.valueOf(i);
+    }
 }
