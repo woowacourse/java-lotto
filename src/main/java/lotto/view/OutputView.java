cccadf17239e903e57e2ca94eb309892dae75ef9
Seed <hsik0225@gmail.com>
2021-02-19 14:05:26 +0900

    refactor(output-view):문자열 상수로 전환 및 출력 메소드 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,11 +10,24 @@ import java.util.Collections;
 
 public class OutputView {
     
-    public void printPurchasingLotto(int purchasingLottoCount) {
-        System.out.println(purchasingLottoCount + "개를 구매했습니다.");
+    private static final String VIEW_OF_LOTTO_RESULT = "%d개 일치 (%d원) - %d개%s";
+    private static final String VIEW_OF_PURCHASE_COUNT = "%d개를 구매했습니다.";
+    private static final String VIEW_OF_INCOME_RATE = "총 수익률은 %.2f입니다.";
+    private static final String VIEW_OF_LOTTO_STATISTICS_CONTENT = "%s%s%s";
+    private static final String VIEW_OF_LOTTO_STATISTICS = "당첨 통계";
+    private static final String VIEW_OF_SEPARATOR = "---------";
+    
+    
+    public void printPurchasedLottos(int purchasingLottoCount, Lottos lottos) {
+        printPurchasedCount(VIEW_OF_PURCHASE_COUNT, purchasingLottoCount);
+        printPurchasedLottos(lottos);
     }
     
-    public void printLottos(Lottos lottos) {
+    private void printPurchasedCount(String viewOfPurchaseCount, double purchasingLottoCount2) {
+        System.out.printf(viewOfPurchaseCount, purchasingLottoCount2);
+    }
+    
+    private void printPurchasedLottos(Lottos lottos) {
         lottos.toInts()
               .forEach(lotto -> {
                   Collections.sort(lotto);
@@ -24,16 +37,25 @@ public class OutputView {
         System.out.println();
     }
     
-    public void printStatisticResult(LottoStatisticResult result) {
-        System.out.println();
-        System.out.println("당첨 통계");
-        System.out.println("---------");
-        
+    public void printStatistics(LottoStatisticResult result) {
+        printStatisticsTitle();
+        printStatisticsContent(result);
+        printIncomeRate(result.calculateIncomeRate());
+    }
+    
+    private void printStatisticsTitle() {
+        System.out.printf(VIEW_OF_LOTTO_STATISTICS_CONTENT, System.lineSeparator(), VIEW_OF_LOTTO_STATISTICS,
+                VIEW_OF_SEPARATOR);
+    }
+    
+    private void printStatisticsContent(LottoStatisticResult result) {
         Arrays.stream(Rank.values())
               .sorted(RankComparator.INSTANCE)
-              .forEach(rank -> System.out.printf("%d개 일치 (%d원) - %d개%s", rank.getMatchCount(), rank.getReward(),
+              .forEach(rank -> System.out.printf(VIEW_OF_LOTTO_RESULT, rank.getMatchCount(), rank.getReward(),
                       result.get(rank), System.lineSeparator()));
-        
-        System.out.printf("총 수익률은 %.2f입니다.", result.calculateIncomeRate());
+    }
+    
+    private void printIncomeRate(double incomeRate) {
+        System.out.printf(VIEW_OF_INCOME_RATE, incomeRate);
     }
 }
