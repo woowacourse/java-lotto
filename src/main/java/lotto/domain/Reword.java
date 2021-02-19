f448a0a864ab3777ca20060d0f12d268fe4bacbe
lns13301 <rkdrbgks22@ks.ac.kr>
2021-02-19 22:03:40 +0900

    refactor: 출력 규격에 맞추기 위한 Reword 재배치

--- src/main/java/lotto/domain/Reword.java
+++ src/main/java/lotto/domain/Reword.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 
 public enum Reword {
 
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
+    NONE(0, 0),
     FIFTH(3, 5_000),
-    NONE(0, 0);
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
 
     private final int hitCount;
     private final int winningMoney;
