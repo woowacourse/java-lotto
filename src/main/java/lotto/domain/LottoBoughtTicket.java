59c87a5c3f5b8b5371195928c75f2e2a316e2acd
Gomding <backchida@gmail.com>
2021-02-19 20:35:10 +0900

    refactor : LottoBoughtTicket 내부에서 로또 번호가 당첨번호와 얼마나 일치하는지 계산하는 로직 변경

--- src/main/java/lotto/domain/LottoBoughtTicket.java
+++ src/main/java/lotto/domain/LottoBoughtTicket.java
@@ -1,12 +1,8 @@
 package lotto.domain;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class LottoBoughtTicket extends LottoTicket {
-
-    private static final int TOTAL_NUMBER_COUNT = 12;
 
     public LottoBoughtTicket(final List<LottoNumber> lottoNumbers) {
         super(lottoNumbers);
@@ -19,8 +15,9 @@ public class LottoBoughtTicket extends LottoTicket {
     }
 
     private int calculateMatchCount(final LottoWinnerTicket lottoWinnerTicket) {
-        Set<LottoNumber> matchingCheckContainer = new HashSet<>(this.getLottoNumbers());
-        matchingCheckContainer.addAll(lottoWinnerTicket.getLottoNumbers());
-        return TOTAL_NUMBER_COUNT - matchingCheckContainer.size();
+        return (int) lottoWinnerTicket.getLottoNumbers()
+                .stream()
+                .filter(this::isContain)
+                .count();
     }
 }
