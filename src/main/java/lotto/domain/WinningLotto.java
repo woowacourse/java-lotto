fc597b26bd2614983175d657776bc4bac3650e43
thisisyoungbin <thisisyoungbin@gmail.com>
2021-02-20 19:36:44 +0900

    test: WinningLottoTest 수정

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,30 +1,40 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class WinningLotto {
 
     private final Lotto winningLotto;
     private final LottoNumber bonusNumber;
-
     public WinningLotto(Lotto lotto, LottoNumber bonusNumber) {
         validateBonusNumber(lotto, bonusNumber);
         this.winningLotto = lotto;
         this.bonusNumber = bonusNumber;
     }
-
     public WinningLotto(Lotto lotto, int number) {
         this(lotto, new LottoNumber(number));
     }
-
     public Rank findRank(Lotto lotto) {
         int countOfMatch = winningLotto.countOfMatchNumber(lotto);
         boolean matchBonusNumber = lotto.containNumber(bonusNumber);
-
         return Rank.rankOf(countOfMatch, matchBonusNumber);
     }
-
     private void validateBonusNumber(Lotto lotto, LottoNumber bonusNumber) {
         if (lotto.containNumber(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호는 로또 번호와 중복될 수 없습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WinningLotto that = (WinningLotto) o;
+        return Objects.equals(winningLotto, that.winningLotto) && Objects.equals(bonusNumber, that.bonusNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winningLotto, bonusNumber);
     }
 }
--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 public class WinningLottoTest {
 
-    static Lotto Lotto;
+    static Lotto lotto;
 
     @BeforeAll
     static void beforeAll() {
@@ -23,29 +23,33 @@ public class WinningLottoTest {
         LottoNumber number5 = new LottoNumber("5");
         LottoNumber number6 = new LottoNumber("6");
         List<LottoNumber> lottoNumbers = Arrays.asList(number1, number2, number3, number4, number5, number6);
-        Lotto = new Lotto(lottoNumbers);
-    }
-
-    @Test
-    @DisplayName("당첨 번호와 보너스 번호가 중복되는 경우 테스트")
-    void name() {
-        LottoNumber bonusNumber = new LottoNumber("2");
-        assertThatIllegalArgumentException().isThrownBy(() -> new WinningLotto(Lotto, bonusNumber));
+        lotto = new Lotto(lottoNumbers);
     }
 
     @Test
     @DisplayName("당첨 번호와 보너스 번호가 제대로 들어갔는지 테스트")
-    void name2() {
+    void testCreateWinningLotto() {
         LottoNumber bonusNumber = new LottoNumber("7");
-        WinningLotto winningLotto = new WinningLotto(Lotto, bonusNumber);
-        assertThat(winningLotto.toString()).isEqualTo(Lotto.toString() + ", " + bonusNumber.toString());
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+
+        List<Integer> winningNumbers = Arrays.asList(1,2,3,4,5,6);
+
+        assertThat(winningLotto)
+                .isEqualTo(new WinningLotto(Lotto.ofLotto(winningNumbers), 7));
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 보너스 번호가 중복되는 경우 테스트")
+    void testValidateBonusNumber() {
+        LottoNumber bonusNumber = new LottoNumber("2");
+        assertThatIllegalArgumentException().isThrownBy(() -> new WinningLotto(lotto, bonusNumber));
     }
 
     @Test
     @DisplayName("당첨 숫자에 따른 등수 테스트")
     void testSeekRank() {
         LottoNumber bonusNumber = new LottoNumber("8");
-        WinningLotto winningLotto = new WinningLotto(Lotto, bonusNumber);
-        assertThat(winningLotto.findRank(Lotto)).isEqualTo(Rank.FIRST);
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        assertThat(winningLotto.findRank(lotto)).isEqualTo(Rank.FIRST);
     }
 }
