9a6801ae83ef371b06649249f149886c0335ef9a
sakjung <ssangyu123@gmail.com>
2021-02-20 23:32:43 +0900

    refactor: custom winning lotto를 만드는 로직 리팩토링

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -4,20 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static lotto.domain.ManualLottoGeneratorTest.createCustomLotto;
 import static lotto.view.InputView.convertStringsToInts;
 
 class WinningLottoTest {
     public static WinningLotto createCustomWinningLotto(String customNumbers, String bonusNumber) {
-        int[] numbers = convertStringsToInts(customNumbers.split(", "));
-        return new WinningLotto(new Lotto(
-                Arrays.asList(
-                        new LottoNumber(numbers[0]),
-                        new LottoNumber(numbers[1]),
-                        new LottoNumber(numbers[2]),
-                        new LottoNumber(numbers[3]),
-                        new LottoNumber(numbers[4]),
-                        new LottoNumber(numbers[5]))),
-                new LottoNumber(Integer.parseInt(bonusNumber)));
+        return new WinningLotto(createCustomLotto(customNumbers), new LottoNumber(Integer.parseInt(bonusNumber)));
     }
 
     //TODO:
