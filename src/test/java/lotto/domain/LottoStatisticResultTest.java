683fa07c1918d095616d9d9c4a5c3a7248e3388f
Seed <hsik0225@gmail.com>
2021-02-20 23:24:16 +0900

    test(rank):searchRank() 테스트 코드 작성

--- src/test/java/lotto/domain/LottoStatisticResultTest.java
+++ src/test/java/lotto/domain/LottoStatisticResultTest.java
@@ -20,6 +20,18 @@ public class LottoStatisticResultTest {
     
     private static final List<Lotto> LOTTO_GROUP = new ArrayList<>();
     
+    @ParameterizedTest
+    @MethodSource("generateLottoStaticResult")
+    @DisplayName("당첨 확인 테스트")
+    void searchRankTest(LottoStatisticResult result, Rank rank) {
+        
+        // when
+        Long count = result.get(rank);
+        
+        // then
+        assertThat(count).isEqualTo(1L);
+    }
+    
     private static Stream<Arguments> generateLottoStaticResult() {
         LottoStatisticResult firstRankResult
                 = generateLottoStatisticResult(Lotto.fromNumbers(Arrays.asList(1, 2, 3, 4, 5, 6)));
@@ -40,9 +52,12 @@ public class LottoStatisticResultTest {
                 = generateLottoStatisticResult(Lotto.fromNumbers(Arrays.asList(1, 2, 42, 43, 44, 45)));
         
         return Stream.of(
-                Arguments.of(firstRankResult, Rank.FIRST), Arguments.of(secondRankResult, Rank.SECOND),
-                Arguments.of(thirdRankResult, Rank.THIRD), Arguments.of(fourthRankResult, Rank.FOURTH),
-                Arguments.of(fifthRankResult, Rank.FIFTH), Arguments.of(nothingRankResult, Rank.NOTHING)
+                Arguments.of(firstRankResult, Rank.FIRST),
+                Arguments.of(secondRankResult, Rank.SECOND),
+                Arguments.of(thirdRankResult, Rank.THIRD),
+                Arguments.of(fourthRankResult, Rank.FOURTH),
+                Arguments.of(fifthRankResult, Rank.FIFTH),
+                Arguments.of(nothingRankResult, Rank.NOTHING)
         );
     }
     
@@ -53,18 +68,6 @@ public class LottoStatisticResultTest {
         Lottos lottos = new Lottos(LOTTO_GROUP);
         
         return lottos.retrieveResults(WINNING_LOTTO);
-    }
-    
-    @ParameterizedTest
-    @MethodSource("generateLottoStaticResult")
-    @DisplayName("당첨 확인 테스트")
-    void searchRankTest(LottoStatisticResult result, Rank rank) {
-        
-        // when
-        Long count = result.get(rank);
-        
-        // then
-        assertThat(count).isEqualTo(1L);
     }
     
     @Test
