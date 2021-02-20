d980b7d78136ba58cf251557e41b03c177b7027f
김주원 <kjw11077@naver.com>
2021-02-21 00:59:41 +0900

    feat: EarningRate 예외 상황 처리 - totalPrize가 0일 경우

--- src/main/java/lotto/domain/result/EarningRate.java
+++ src/main/java/lotto/domain/result/EarningRate.java
@@ -4,10 +4,20 @@ import java.math.BigInteger;
 
 public class EarningRate {
 
+    public static final BigInteger HUNDRED = BigInteger.valueOf(100);
+
     private final BigInteger earningRate;
 
     public EarningRate(BigInteger totalPrize, BigInteger paymentAmount) {
-        this.earningRate = (totalPrize.multiply(BigInteger.valueOf(100)).divide(paymentAmount));
+        this.earningRate = calculateEarningRate(totalPrize, paymentAmount);
+    }
+
+    private BigInteger calculateEarningRate(BigInteger totalPrize, BigInteger paymentAmount) {
+        if (BigInteger.ZERO.equals(paymentAmount)) {
+            return HUNDRED;
+        }
+
+        return totalPrize.multiply(HUNDRED).divide(paymentAmount);
     }
 
     public BigInteger toBigInteger() {
--- src/test/java/lotto/domain/result/EarningRateTest.java
+++ src/test/java/lotto/domain/result/EarningRateTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigInteger;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,19 +12,21 @@ import org.junit.jupiter.params.provider.MethodSource;
 class EarningRateTest {
 
     @ParameterizedTest
+    @DisplayName("수익률 계산")
     @MethodSource("calculate_earningRate_testcase")
-    void calculate_earningRate(BigInteger totalPrize, BigInteger paymentAmount, BigInteger expected) {
+    void calculate_earningRate(BigInteger totalPrize, BigInteger paymentAmount,
+            BigInteger expected) {
         EarningRate earningRate = new EarningRate(totalPrize, paymentAmount);
 
         assertThat(earningRate.toBigInteger()).isEqualTo(expected);
     }
 
     private static Stream<Arguments> calculate_earningRate_testcase() {
-        // totalPrize, paymentAmount, expected
         return Stream.of(
                 Arguments.of(toBigInteger(1000), toBigInteger(1000), toBigInteger(100)),
                 Arguments.of(toBigInteger(0), toBigInteger(1000), toBigInteger(0)),
-                Arguments.of(toBigInteger(500), toBigInteger(1000), toBigInteger(50))
+                Arguments.of(toBigInteger(500), toBigInteger(1000), toBigInteger(50)),
+                Arguments.of(toBigInteger(0), toBigInteger(0), toBigInteger(100))
         );
     }
 
