c1a162794fe494839175ba08557812aaf1737239
Minsung <sns2831@gmail.com>
2021-02-20 10:03:44 +0900

    refactor(lotto): 테스트 메소드의 이름 의미 명확히, 컨벤션 준수

--- src/test/java/lotto/domain/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/lotto/LottoGeneratorTest.java
@@ -3,7 +3,6 @@ package lotto.domain.lotto;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import lotto.domain.number.LottoNumber;
 import lotto.domain.number.PayOut;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,14 +14,14 @@ public class LottoGeneratorTest {
 
     @Test
     @DisplayName("로또 그룹 생성")
-    void generateLottoGroup() {
+    void generateAutoLottoGroup() {
         LottoGenerator lottoGenerator = new LottoGenerator();
         assertThat(lottoGenerator.generateLottos(3).getCount()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("수동 로또번호를 입력하면, 수동번호의 로또그를을 생성한다")
-    void generateLottos() {
+    void generateManualLottos() {
         List<String> manualStringLottoNumbers = Arrays.asList(
                 "1,2,3,4,5,6",
                 "1,3,4,7,34,6",
@@ -36,12 +35,17 @@ public class LottoGeneratorTest {
         PayOut zeroPayOut = PayOut.ZERO;
 
         LottoGenerator lottoGenerator = new LottoGenerator();
-        assertThat(lottoGenerator.generateLottosWithManualLottoNumbers(manualLottoNumbers, zeroPayOut).getCount()).isEqualTo(3);
+        assertThat(
+                lottoGenerator.generateLottosWithManualLottoNumbers(
+                        manualLottoNumbers,
+                        zeroPayOut
+                ).getCount()
+        ).isEqualTo(3);
     }
 
     @Test
     @DisplayName("수동 로또번호를 입력하면, 수동번호의 로또그를을 생성한다")
-    void generateLottos() {
+    void generateManualLottosWithAutoLottos() {
         List<String> manualStringLottoNumbers = Arrays.asList(
                 "1,2,3,4,5,6",
                 "1,3,4,7,34,6",
@@ -55,6 +59,11 @@ public class LottoGeneratorTest {
         PayOut payOut = new PayOut(3000);
 
         LottoGenerator lottoGenerator = new LottoGenerator();
-        assertThat(lottoGenerator.generateLottosWithManualLottoNumbers(manualLottoNumbers, payOut).getCount()).isEqualTo(3);
+        assertThat(
+                lottoGenerator.generateLottosWithManualLottoNumbers(
+                        manualLottoNumbers,
+                        payOut
+                ).getCount()
+        ).isEqualTo(6);
     }
 }
