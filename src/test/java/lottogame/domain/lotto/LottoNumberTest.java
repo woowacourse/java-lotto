01d6c6e443279860019fea912316b3440cc09ffe
sor <bosl95@naver.com>
2021-02-21 06:07:03 +0900

    refactor : 랜덤 로또 생성 테스트 구현

--- src/test/java/lottogame/domain/lotto/LottoNumberTest.java
+++ src/test/java/lottogame/domain/lotto/LottoNumberTest.java
@@ -7,9 +7,17 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoNumberTest {
+    @DisplayName("기본 생성자는 랜덤 로또 번호 6개를 생성한다")
+    @Test
+    void 로또_번호_생성_테스트() {
+        LottoNumber lottoNumber = new LottoNumber();
+        assertThat(lottoNumber.values()).hasSize(6);
+    }
+
     @DisplayName("로또 번호가 범위를 넘어 가는 경우 예외 처리")
     @Test
     void 로또_번호_범위_테스트() {
