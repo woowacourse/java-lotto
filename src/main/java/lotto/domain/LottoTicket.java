dda0886aa8eeceec64cdfcd594fd03a749012135
Jinhong <56240505+xlffm3@users.noreply.github.com>
2021-02-20 19:55:39 +0900

    refactor: LottoTicket 내부 컬렉션을 list에서 set으로 변경

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,32 +1,32 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoTicket {
     private static final int VALID_LOTTO_NUMBER_COUNTS = 6;
     private static final String INVALID_LOTTO_NUMBER_COUNTS = "로또 티켓은 중복되지 않은 6자리의 숫자로 구성되어야 합니다.";
 
-    private final List<LottoNumber> lottoNumbers;
+    private final Set<LottoNumber> lottoNumbers;
 
-    private LottoTicket(List<LottoNumber> lottoNumbers) {
+    private LottoTicket(Set<LottoNumber> lottoNumbers) {
+        validateNumberCounts(lottoNumbers.size());
         this.lottoNumbers = lottoNumbers;
     }
 
     public static LottoTicket from(List<Integer> numbers) {
-        validateNumbers(numbers);
-        List<LottoNumber> lottoNumbers = numbers.stream()
+        validateNumberCounts(numbers.size());
+        Set<LottoNumber> lottoNumbers = numbers.stream()
                 .map(LottoNumber::from)
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
         return new LottoTicket(lottoNumbers);
     }
 
-    private static void validateNumbers(List<Integer> numbers) {
-        int numberCounts = numbers.size();
-        int distinctNumberCounts = new HashSet<>(numbers).size();
-        if (numberCounts != VALID_LOTTO_NUMBER_COUNTS || numberCounts != distinctNumberCounts) {
+    private static void validateNumberCounts(int numberCounts) {
+        if (numberCounts != VALID_LOTTO_NUMBER_COUNTS) {
             throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_COUNTS);
         }
     }
@@ -42,6 +42,6 @@ public class LottoTicket {
     }
 
     public List<LottoNumber> getLottoNumbers() {
-        return Collections.unmodifiableList(lottoNumbers);
+        return Collections.unmodifiableList(new ArrayList<>(lottoNumbers));
     }
 }
