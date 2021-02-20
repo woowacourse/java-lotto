06b5489adaff208069ec884d5afea4a07288c496
sakjung <ssangyu123@gmail.com>
2021-02-20 23:17:37 +0900

    refactor: lottosTest 리팩토링

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -7,23 +7,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import static lotto.domain.LottoTest.createCustomLotto;
+import static lotto.domain.WinningLottoTest.createCustomWinningLotto;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottosTest {
-    /*TODO:
-     *  생성자 관련 테스트 추가 (예외 처리)*/
-
     @DisplayName("가지고 있는 로또와 당첨번호를 비교해서 결과를 제대로 반환 하는지")
     @Test
-    void testEntireLottoMatching() {
+    void getResults() {
         Lotto lotto1 = createCustomLotto("1, 2, 3, 19, 20, 40");
         Lotto lotto2 = createCustomLotto("1, 2, 20, 25, 29, 45");
-
         Lottos lottos = new Lottos(Arrays.asList(lotto1, lotto2));
 
-        Lotto winningNumbers = createCustomLotto("1, 2, 3, 4, 5, 6");
-        LottoNumber bonusNumber = new LottoNumber(20);
-        List<Rank> ranks = lottos.getResults(new WinningLotto(winningNumbers, bonusNumber));
+        WinningLotto winningLotto = createCustomWinningLotto("1, 2, 3, 4, 5, 6", "20");
+        List<Rank> ranks = lottos.getResults(winningLotto);
 
         List<Rank> expectedRanks = Arrays.asList(Rank.FIFTH, Rank.NONE);
 
