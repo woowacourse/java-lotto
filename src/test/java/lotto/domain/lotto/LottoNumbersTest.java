a6fff87b5943024eccafbdcdcb6ba62f6c07c7e1
Minsung <sns2831@gmail.com>
2021-02-20 09:19:13 +0900

    test(lotto): 로또 넘버를 String으로 넣으면 LottoNumbers 반환 테스트

--- src/test/java/lotto/domain/lotto/LottoNumbersTest.java
+++ src/test/java/lotto/domain/lotto/LottoNumbersTest.java
@@ -1,5 +1,6 @@
 package lotto.domain.lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.Arrays;
@@ -37,5 +38,21 @@ public class LottoNumbersTest {
                     new LottoNumber(new Number(3)),
                     new LottoNumber(new Number(4))
                 ))).withMessage("로또 넘버가 6개가 아닙니다.");
+    }
+    
+    @Test
+    @DisplayName("로또번호들이 String으로 들어오면 LottoNumbers 반환")
+    public void crateLottoNumbersUsingStringInput() {
+        LottoNumbers lottoNumbers = LottoNumbers.valueOf("1 ,2 ,3 ,4 ,5, 6");
+        assertThat(lottoNumbers.getMatchCount(new LottoNumbers(
+                Arrays.asList(
+                        new LottoNumber(new Number(1)),
+                        new LottoNumber(new Number(2)),
+                        new LottoNumber(new Number(3)),
+                        new LottoNumber(new Number(4)),
+                        new LottoNumber(new Number(5)),
+                        new LottoNumber(new Number(6))
+                )
+        ))).isEqualTo(6);
     }
 }
