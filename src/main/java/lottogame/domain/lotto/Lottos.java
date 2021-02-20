c9f68b587f502f83dceb7515776dd4a271e7c4bd
sor <bosl95@naver.com>
2021-02-21 04:42:45 +0900

    refactor : lottos 테스트코드의 가시성을 높일 수 있도록 수정

--- src/main/java/lottogame/domain/lotto/Lottos.java
+++ src/main/java/lottogame/domain/lotto/Lottos.java
@@ -4,6 +4,7 @@ import lottogame.domain.dto.LottoResult;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Lottos {
@@ -27,5 +28,18 @@ public class Lottos {
             lottoResults.add(new LottoResult(count, bonus));
         }
         return lottoResults;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lottos lottos1 = (Lottos) o;
+        return Objects.equals(lottos, lottos1.lottos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottos);
     }
 }
--- src/test/java/lottogame/domain/lotto/LottosTest.java
+++ src/test/java/lottogame/domain/lotto/LottosTest.java
@@ -2,81 +2,123 @@ package lottogame.domain.lotto;
 
 import lottogame.domain.Money;
 import lottogame.domain.Rank;
-import lottogame.domain.stats.LottoResults;
+import lottogame.domain.dto.LottoResult;
+import lottogame.domain.dto.LottoResults;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.transform.Result;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottosTest {
+    @DisplayName("로또들이 Lotts 객체 안에 잘 들어갔는 지 테스트")
     @Test
-    void 객체_생성() {
-        LottoGenerator.generate();
-        List<Lotto> lottoGroup = new ArrayList<>();
-        lottoGroup.add(new Lotto(LottoGenerator.makeNumbers()));
-        lottoGroup.add(new Lotto(LottoGenerator.makeNumbers()));
-        lottoGroup.add(new Lotto(LottoGenerator.makeNumbers()));
-        lottoGroup.add(new Lotto(LottoGenerator.makeNumbers()));
-
+    void 로또들을_관리하는_로또_일급컬렉션() {
+        List<Lotto> lottoGroup = Arrays.asList(
+                new Lotto(new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                new Lotto(new LottoNumber(Arrays.asList(2, 3, 4, 5, 6, 7))),
+                new Lotto(new LottoNumber(Arrays.asList(3, 4, 5, 6, 7, 8))),
+                new Lotto(new LottoNumber(Arrays.asList(4, 5, 6, 7, 8, 9))),
+                new Lotto(new LottoNumber(Arrays.asList(5, 6, 7, 8, 9, 10))));
         Lottos lottos = new Lottos(lottoGroup);
-        for (Lotto lotto : lottoGroup) {
-            assertThat(lottos.values()).contains(lotto);
-        }
+        assertThat(lottos).isEqualTo(new Lottos(lottoGroup));
     }
 
+    @DisplayName("순위별 당첨 통계가 제대로 저장되는 지 확인")
     @Test
-    void 로또_매칭_테스트_1() {
-        matchTest(new String[]{"1, 2, 3, 4, 45, 44", "1, 2, 3, 43, 44, 45", "1, 2, 3, 4, 5, 7", "1, 2, 3, 4, 5, 44"},
-                new WinningLotto("1, 2, 3, 4, 5, 6", "7"),
-                "4000",
-                new int[]{1, 1, 1, 1, 0});
+    void 로또_순위별_당첨_통계() {
+        List<Lotto> lottoGroup = Arrays.asList(
+                new Lotto(new LottoNumber(Arrays.asList(1, 2, 3, 4, 45, 44))),
+                new Lotto(new LottoNumber(Arrays.asList(1, 2, 3, 43, 44, 45))),
+                new Lotto(new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 7))),
+                new Lotto(new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 44))));
+        Lottos lottos = new Lottos(lottoGroup);
+        WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+
+        List<LottoResult> expected = Arrays.asList(
+                new LottoResult(4, false),
+                new LottoResult(3, false),
+                new LottoResult(5, true),
+                new LottoResult(5, false));
+
+        assertThat(lottos.matchesLottos(winningLotto)).isEqualTo(expected);
+// 구매 금액 : 4000 , 결과 통계 new int[]{1, 1, 1, 1, 0
     }
 
+    @DisplayName("순위별 당첨 통계가 제대로 저장되는 지 확인 ver2")
     @Test
-    void 로또_매칭_테스트케이스_2() {
-        matchTest(new String[]{
-                        "8, 21, 23, 41, 42, 43", "3, 5, 11, 16, 32, 38", "7, 11, 16, 35, 36, 44",
-                        "1, 8, 11, 31, 41, 42", "13, 14, 16, 38, 42, 45", "7, 11, 30, 40, 42, 43",
-                        "2, 13, 22, 32, 38, 45", "23, 25, 33, 36, 39, 41", "1, 3, 5, 14, 22, 45",
-                        "5, 9, 38, 41, 43, 44", "2, 8, 9, 18, 19, 21", "13, 14, 18, 21, 23, 35",
-                        "17, 21, 29, 37, 42, 45", "3, 8, 27, 30, 35, 44"},
-                new WinningLotto("1, 2, 3, 4, 5, 6", "7"),
-                "14000",
-                new int[]{1, 0, 0, 0, 0});
+    void 로또_순위별_당첨_통계_ver2() {
+        List<Lotto> lottoGroup = Arrays.asList(
+                new Lotto(new LottoNumber(Arrays.asList(8, 21, 23, 41, 42, 43))),
+                new Lotto(new LottoNumber(Arrays.asList(3, 5, 11, 16, 32, 38))),
+                new Lotto(new LottoNumber(Arrays.asList(7, 11, 16, 35, 36, 44))),
+                new Lotto(new LottoNumber(Arrays.asList(1, 8, 11, 31, 41, 42))),
+                new Lotto(new LottoNumber(Arrays.asList(13, 14, 16, 38, 42, 45))),
+                new Lotto(new LottoNumber(Arrays.asList(7, 11, 30, 40, 42, 43))),
+                new Lotto(new LottoNumber(Arrays.asList(2, 13, 22, 32, 38, 45))),
+                new Lotto(new LottoNumber(Arrays.asList(23, 25, 33, 36, 39, 41))),
+                new Lotto(new LottoNumber(Arrays.asList(1, 3, 5, 14, 22, 45))),
+                new Lotto(new LottoNumber(Arrays.asList(5, 9, 38, 41, 43, 44))),
+                new Lotto(new LottoNumber(Arrays.asList(2, 8, 9, 18, 19, 21))),
+                new Lotto(new LottoNumber(Arrays.asList(13, 14, 18, 21, 23, 35))),
+                new Lotto(new LottoNumber(Arrays.asList(17, 21, 29, 37, 42, 45))),
+                new Lotto(new LottoNumber(Arrays.asList(3, 8, 27, 30, 35, 44))));
+        Lottos lottos = new Lottos(lottoGroup);
+        WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+
+        List<LottoResult> expected = Arrays.asList(
+                new LottoResult(0, false),
+                new LottoResult(2, false),
+                new LottoResult(0, true),
+                new LottoResult(1, false),
+                new LottoResult(0, false),
+                new LottoResult(0, true),
+                new LottoResult(1, false),
+                new LottoResult(0, false),
+                new LottoResult(3, false),
+                new LottoResult(1, false),
+                new LottoResult(1, false),
+                new LottoResult(0, false),
+                new LottoResult(0, false),
+                new LottoResult(1, false));
+
+        assertThat(lottos.matchesLottos(winningLotto)).isEqualTo(expected);
+// 구매 금액 : 14000 , 결과 통계 new int[]{1, 0, 0, 0, 0}
     }
 
-    private void matchTest(String[] lottoNumbersGroup, WinningLotto winningLotto, String money, int[] expectedCount) {
-        Lottos lottos = new Lottos(makeLottos(lottoNumbersGroup));
-
-        LottoResults lottoResults = lottos.findMatchLottos(winningLotto, new Money(money));
-        Map<Rank, Integer> sameResult = new LinkedHashMap<>();
-
-        sameResult.put(Rank.FIFTH, expectedCount[0]);
-        sameResult.put(Rank.FOURTH, expectedCount[1]);
-        sameResult.put(Rank.THIRD, expectedCount[2]);
-        sameResult.put(Rank.SECOND, expectedCount[3]);
-        sameResult.put(Rank.FIRST, expectedCount[4]);
-        assertThat(lottoResults.values()).isEqualTo(sameResult);
-
-        float expected = (5000 * expectedCount[0]
-                + 50000 * expectedCount[1]
-                + 1500000 * expectedCount[2]
-                + 30000000 * expectedCount[3]
-                + 2000000000 * expectedCount[4]) / Float.valueOf(money);
-        assertThat(lottoResults.getProfit()).isEqualTo(expected);
-    }
-
-    List<Lotto> makeLottos(String[] inputs) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (String input : inputs) {
-            String[] numbers = input.split(", ");
-            List<Integer> nums = Arrays.stream(numbers)
-                    .map(x -> Integer.parseInt(x))
-                    .collect(Collectors.toList());
-            lottos.add(new Lotto(nums));
-        }
-        return lottos;
-    }
+//    private void matchTest(String[] lottoNumbersGroup, WinningLotto winningLotto, int money, int[] expectedCount) {
+//        Lottos lottos = new Lottos(makeLottos(lottoNumbersGroup));
+//
+//        LottoResults lottoResults = lottos.findMatchLottos(winningLotto, new Money(money));
+//        Map<Rank, Integer> sameResult = new LinkedHashMap<>();
+//
+//        sameResult.put(Rank.FIFTH, expectedCount[0]);
+//        sameResult.put(Rank.FOURTH, expectedCount[1]);
+//        sameResult.put(Rank.THIRD, expectedCount[2]);
+//        sameResult.put(Rank.SECOND, expectedCount[3]);
+//        sameResult.put(Rank.FIRST, expectedCount[4]);
+//        assertThat(lottoResults.values()).isEqualTo(sameResult);
+//
+//        float expected = (5000 * expectedCount[0]
+//                + 50000 * expectedCount[1]
+//                + 1500000 * expectedCount[2]
+//                + 30000000 * expectedCount[3]
+//                + 2000000000 * expectedCount[4]) / Float.valueOf(money);
+//        assertThat(lottoResults.getProfit()).isEqualTo(expected);
+//    }
+//
+//    List<Lotto> makeLottos(String[] inputs) {
+//        List<Lotto> lottos = new ArrayList<>();
+//        for (String input : inputs) {
+//            String[] numbers = input.split(", ");
+//            List<Integer> nums = Arrays.stream(numbers)
+//                    .map(x -> Integer.parseInt(x))
+//                    .collect(Collectors.toList());
+//            lottos.add(new Lotto(nums));
+//        }
+//        return lottos;
+//    }
 }
