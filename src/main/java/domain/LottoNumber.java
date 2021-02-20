66bd6c3500635abbcd16fa8a5a831a64bc1137ca
seunghyun <ggyool@naver.com>
2021-02-21 01:24:33 +0900

    refactor(LottoNumber): 캐싱 기능 추가

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -1,26 +1,32 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class LottoNumber {
-    private static final int MIN_NUMBER_VALUE = 1;
-    private static final int MAX_NUMBER_VALUE = 45;
+    public static final int MIN_NUMBER_VALUE = 1;
+    public static final int MAX_NUMBER_VALUE = 45;
+    private static final Map<Integer, LottoNumber> CACHE = new HashMap<>();
 
     private final int value;
+
+    static {
+        IntStream.rangeClosed(MIN_NUMBER_VALUE, MAX_NUMBER_VALUE)
+                .forEach(i -> CACHE.put(i, new LottoNumber(i)));
+    }
 
     private LottoNumber(final int value) {
         this.value = value;
     }
 
     public static LottoNumber valueOf(final int value) {
-        validateRange(value);
-        return new LottoNumber(value);
-    }
-
-    private static void validateRange(final int value) {
-        if (value < MIN_NUMBER_VALUE || value > MAX_NUMBER_VALUE) {
-            throw new IllegalArgumentException("1에서 45 범위에서 벗어났습니다.");
-        }
+        return CACHE.computeIfAbsent(value, k -> {
+            throw new IllegalArgumentException(
+                    String.format("%d~%d의 범위에서 벗어났습니다: %d", MIN_NUMBER_VALUE, MAX_NUMBER_VALUE, value)
+            );
+        });
     }
 
     public int getValue() {
@@ -28,19 +34,15 @@ public class LottoNumber {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        LottoNumber that = (LottoNumber) o;
-        return value == that.value;
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final LottoNumber that = (LottoNumber) o;
+        return getValue() == that.getValue();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value);
+        return Objects.hash(getValue());
     }
 }
--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -1,13 +1,17 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.IntStream;
+
+import static domain.LottoNumber.MAX_NUMBER_VALUE;
+import static domain.LottoNumber.MIN_NUMBER_VALUE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class LottoNumberTest {
     @DisplayName("유효한 값이면 객체 생성 성공")
@@ -31,6 +35,17 @@ public class LottoNumberTest {
     @Test
     void equals() {
         final LottoNumber lottoNumber = LottoNumber.valueOf(1);
-        assertEquals(lottoNumber, LottoNumber.valueOf(1));
+        assertThat(lottoNumber).isEqualTo(LottoNumber.valueOf(1));
+    }
+
+    @DisplayName("캐싱 동작 확인")
+    @Test
+    void caching() {
+        IntStream.rangeClosed(MIN_NUMBER_VALUE, MAX_NUMBER_VALUE)
+                .forEach(i -> {
+                    final LottoNumber lottoNumber = LottoNumber.valueOf(i);
+                    assertThat(lottoNumber)
+                            .isSameAs(LottoNumber.valueOf(i));
+                });
     }
 }
