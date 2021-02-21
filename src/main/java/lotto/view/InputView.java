5478196573b732988b6955634b682a54a6fa5d75
Jinhong <56240505+xlffm3@users.noreply.github.com>
2021-02-21 15:00:26 +0900

    feat: InputView 수동 구매 로또 수 및 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,17 +1,23 @@
 package lotto.view;
 
+import lotto.domain.ManualTicketNumbers;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_PURCHASING_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_MANUAL_TICKET_COUNTS_MESSAGE = "\n수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String INPUT_MANUAL_TICKET_NUMBERS_MESSAGE = "\n수동으로 구매할 번호를 입력해 주세요.";
     private static final String INPUT_WINNING_TICKET_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String COMMA_DELIMITER = ", ";
     private static final int SPLIT_LIMIT_THRESHOLD = -1;
+    private static final int ZERO = 0;
 
     private InputView() {
     }
@@ -23,6 +29,17 @@ public class InputView {
     private static int nextIntWithInstructionMessage(String instructionMessage) {
         System.out.println(instructionMessage);
         return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static List<ManualTicketNumbers> inputManualTicketNumbers() {
+        int manualTicketCounts = nextIntWithInstructionMessage(INPUT_MANUAL_TICKET_COUNTS_MESSAGE);
+        if (manualTicketCounts > ZERO) {
+            System.out.println(INPUT_MANUAL_TICKET_NUMBERS_MESSAGE);
+        }
+        return Stream.generate(() -> SCANNER.nextLine().split(COMMA_DELIMITER, SPLIT_LIMIT_THRESHOLD))
+                .map(ManualTicketNumbers::from)
+                .limit(manualTicketCounts)
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> inputWinningTicketNumbers() {
