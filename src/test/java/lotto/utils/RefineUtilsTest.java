8d71f1e77163e7d70159213a984640aab8ddd0f1
RinSabbit <celena1004@naver.com>
2021-02-21 16:19:40 +0900

    feat: RefineUtils에서 문자열->정수 변환이 제대로 이뤄지는지 검사하는 TC 구현

---
+++ src/test/java/lotto/utils/RefineUtilsTest.java
@@ -0,0 +1,25 @@
+package lotto.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RefineUtilsTest {
+
+    @DisplayName("입력된 문자열에 정수만 적혀있으면 정수를 반환한다.")
+    @Test
+    void refine(){
+        String value = "3";
+        String value2 = "d";
+        String value3 = " 5  ";
+        String value4 = "";
+
+        assertThat(RefineUtils.refineIntegerValue(value)).isEqualTo(3);
+        assertThatIllegalArgumentException().isThrownBy(()->RefineUtils.refineIntegerValue(value2));
+        assertThat(RefineUtils.refineIntegerValue(value3)).isEqualTo(5);
+        assertThatIllegalArgumentException().isThrownBy(()->RefineUtils.refineIntegerValue(value4));
+    }
+
+}
