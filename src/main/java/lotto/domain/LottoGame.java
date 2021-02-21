fd057eb46395da43f566f6b790aac27e4154e5ff
thisisyoungbin <thisisyoungbin@gmail.com>
2021-02-22 01:09:09 +0900

    feat: 게임 결과 비교에 수동 로또도 포함되도록 인자 추가 및 메서드 분리

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -7,13 +7,18 @@ public class LottoGame {
         return new Lottos(autoAmount);
     }
 
-    public LottoGameResult compareWithWinningLotto(Lottos lottos, WinningLotto winningLotto) {
+    public LottoGameResult compareWithWinningLotto(Lottos manual, Lottos auto, WinningLotto winningLotto) {
         LottoGameResult lottoGameResult = new LottoGameResult();
 
-        for (Lotto lotto : lottos.toList()) {
-            lottoGameResult.add(winningLotto.findRank(lotto));
-        }
+        addMatchLotto(manual, winningLotto, lottoGameResult);
+        addMatchLotto(auto, winningLotto, lottoGameResult);
 
         return lottoGameResult;
+    }
+
+    private void addMatchLotto(Lottos manual, WinningLotto winningLotto, LottoGameResult lottoGameResult) {
+        for (Lotto manualLotto : manual.toList()) {
+            lottoGameResult.add(winningLotto.findRank(manualLotto));
+        }
     }
 }
