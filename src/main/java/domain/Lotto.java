5ff10a8b089d88b2629e33bb6c697836515fcc67
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-21 12:49:51 +0900

    feat: 로또 한 줄을 담당하는 Lotto 클래스 생성

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+    private static final int LOTTO_BALLS_NUMBER = 6;
+
+    private final List<LottoBall> lotto;
+
+    public Lotto(final List<LottoBall> lotto) {
+        validateLotto(lotto);
+        this.lotto = new ArrayList<>(lotto);
+    }
+
+    private void validateLotto(final List<LottoBall> lotto) {
+        validateLottoSize(lotto);
+        validateDuplicate(lotto);
+    }
+
+    private void validateLottoSize(final List<LottoBall> lotto) {
+        if (lotto.size() != LOTTO_BALLS_NUMBER) {
+            throw new IllegalArgumentException(LOTTO_BALLS_NUMBER + "개의 로또 번호가 필요합니다.");
+        }
+    }
+
+    private void validateDuplicate(final List<LottoBall> lotto) {
+        final int lottoSizeWithoutDuplicate = (int)lotto.stream().distinct().count();
+
+        if (lotto.size() != lottoSizeWithoutDuplicate) {
+            throw new IllegalArgumentException("로또 번호가 중복됩니다");
+        }
+    }
+}
---
+++ src/test/java/domain/LottoTest.java
@@ -0,0 +1,53 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class LottoTest {
+
+    @DisplayName("로또가 정상적으로 생성되는지 테스트")
+    @Test
+    void LottoConstructorTest() {
+        assertThatCode(() -> new Lotto(Arrays.asList(
+                    LottoBall.valueOf(1),
+                    LottoBall.valueOf(2),
+                    LottoBall.valueOf(3),
+                    LottoBall.valueOf(4),
+                    LottoBall.valueOf(5),
+                    LottoBall.valueOf(6))))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("로또 번호가 6개가 아니라면 에러를 발생시킨다")
+    @Test
+    void LottoCheckContainSixLottoBalls() {
+        assertThatThrownBy(() -> new Lotto(Arrays.asList(
+                LottoBall.valueOf(1),
+                LottoBall.valueOf(2),
+                LottoBall.valueOf(3),
+                LottoBall.valueOf(4),
+                LottoBall.valueOf(5))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("6개의 로또 번호가 필요합니다.");
+    }
+
+    @DisplayName("로또 번호가 중복된다면 에러를 발생시킨다")
+    @Test
+    void LottoCheckDuplicate() {
+        assertThatThrownBy(() -> new Lotto(Arrays.asList(
+                LottoBall.valueOf(1),
+                LottoBall.valueOf(1),
+                LottoBall.valueOf(2),
+                LottoBall.valueOf(3),
+                LottoBall.valueOf(4),
+                LottoBall.valueOf(5))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호가 중복됩니다");
+    }
+
+}
