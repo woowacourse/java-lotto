d8830dd65a904c14dcefa199ef14c8bddad0dfd7
seunghyun <ggyool@naver.com>
2021-02-21 18:00:30 +0900

    test(LottoTicket-test): 필요하다고 느끼는 테스트들 추가

--- src/test/java/domain/ticket/LottoTicketTest.java
+++ src/test/java/domain/ticket/LottoTicketTest.java
@@ -1,50 +1,85 @@
 package domain.ticket;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
-import java.util.Arrays;
-import java.util.List;
-
-import domain.ticket.LottoTicket;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static domain.ticket.Ticket.LOTTO_TICKET_SIZE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class LottoTicketTest {
     @DisplayName("유효한 값이면 객체 생성 성공")
     @Test
-    void valueOf_validInput_success() {
+    void create_validInput_success() {
         final List<Integer> lottoNumbers = getValidNumbers();
 
-        assertThatCode(() -> LottoTicket.valueOf(lottoNumbers))
+        assertThatCode(() -> new LottoTicket(lottoNumbers))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("반자동 티켓 객체 생성 성공")
+    @Test
+    void create_semiAutomatic_success() {
+        final List<Integer> fewerLottoNumbers = getFewerNumbers();
+
+        assertThatCode(() -> new LottoTicket(fewerLottoNumbers))
                 .doesNotThrowAnyException();
     }
 
     @DisplayName("객체 생성 실패 : 중복된 번호")
     @Test
-    void valueOf_duplicateNumber_exceptionThrown() {
+    void create_duplicateNumber_exceptionThrown() {
         final List<Integer> lottoNumbers = getDuplicateNumbers();
 
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> LottoTicket.valueOf(lottoNumbers))
+                .isThrownBy(() -> new LottoTicket(lottoNumbers))
                 .withMessageContaining("중복");
     }
 
-    @DisplayName("객체 생성 실패 : 숫자가 6개가 아닌 경우")
+    @DisplayName("객체 생성 실패 : 숫자가 6개 보다 많은 경우")
     @Test
-    void valueOf_incorrectSize_exceptionThrown() {
-        final List<Integer> fewerLottoNumbers = getFewerNumbers();
+    void create_incorrectSize_exceptionThrown() {
         final List<Integer> moreLottoNumbers = getMoreNumbers();
 
-        assertAll(
-                () -> assertThatIllegalArgumentException()
-                        .isThrownBy(() -> LottoTicket.valueOf(fewerLottoNumbers))
-                        .withMessageContaining("개수가 6이 아닙니다."),
-                () -> assertThatIllegalArgumentException()
-                        .isThrownBy(() -> LottoTicket.valueOf(moreLottoNumbers))
-                        .withMessageContaining("개수가 6이 아닙니다.")
-        );
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoTicket(moreLottoNumbers))
+                .withMessageContaining(
+                        String.format("개수가 %d이 아닙니다", LOTTO_TICKET_SIZE)
+                );
+    }
+
+    @DisplayName("객체 생성 실패 : 선택한 번호에 중복이 있는 경우")
+    @Test
+    void create_duplicateSelection_exceptionThrown() {
+        final List<Integer> lottoNumbers = getFewerAndDuplicateNumbers();
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoTicket(lottoNumbers))
+                .withMessageContaining("선택한 번호 중에 중복 숫자가 존재합니다");
+    }
+
+    @DisplayName("동등 비교")
+    @Test
+    void equals() {
+        final LottoTicket lottoTicket = new LottoTicket(getValidNumbers());
+        assertThat(lottoTicket)
+                .isEqualTo(new LottoTicket(getValidNumbers()));
+    }
+
+    @DisplayName("오름차순 정렬 되어 저장되면 성공")
+    @Test
+    void shuffled_equals() {
+        final List<Integer> shuffledValidNumbers = getValidNumbers();
+        Collections.shuffle(shuffledValidNumbers);
+        final LottoTicket lottoTicket = new LottoTicket(shuffledValidNumbers);
+
+        assertThat(lottoTicket)
+                .isEqualTo(new LottoTicket(getValidNumbers()));
     }
 
     private List<Integer> getValidNumbers() {
@@ -61,5 +96,9 @@ public class LottoTicketTest {
 
     private List<Integer> getMoreNumbers() {
         return Arrays.asList(1, 3, 5, 10, 13, 20, 30);
+    }
+
+    private List<Integer> getFewerAndDuplicateNumbers() {
+        return Arrays.asList(1, 1);
     }
 }
