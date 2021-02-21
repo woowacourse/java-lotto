93c6ea9046f8d40fb1401b3a95fc6c0fa280f6a4
pika96 <52442949+pika96@users.noreply.github.com>
2021-02-21 23:53:29 +0900

    refactor: InputView에서 동작하는 Domain 분리

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,31 +1,43 @@
 package lotto;
 
-import lotto.domain.LottoGroup;
-import lotto.domain.LottoResult;
-import lotto.domain.WinningLotto;
+import lotto.domain.*;
+import lotto.exception.LottoException;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoManager {
 
-  public static LottoGroup createLotto() {
-    LottoGroup lottoGroup = InputView.lottoGroup();
-    OutputView.printBoughtLotto(lottoGroup);
+    public static LottoGroup createLotto() {
+        LottoGroup lottoGroup = createLottoGroup(InputView.getMoney());
+        OutputView.printBoughtLotto(lottoGroup);
+        return lottoGroup;
+    }
 
-    return lottoGroup;
-  }
+    public static WinningLotto getWinningLotto() {
+        try {
+            return new WinningLotto(InputView.getWinningNumbers(), InputView.getBonusNumber());
+        } catch (LottoException e) {
+            OutputView.printMessage(e.getMessage());
+            return getWinningLotto();
+        }
+    }
 
-  public static WinningLotto getWinningLotto() {
-    return InputView.winningLotto();
-  }
+    public static void printResult(LottoGroup lottoGroup, WinningLotto winningLotto) {
+        LottoResult lottoResult = new LottoResult();
+        lottoGroup.lottoGroup()
+                .stream()
+                .map(winningLotto::matchRank)
+                .forEach(lottoResult::add);
+        OutputView.printLottoResult(lottoResult);
+    }
 
-  public static void printResult(LottoGroup lottoGroup, WinningLotto winningLotto) {
-    LottoResult lottoResult = new LottoResult();
-    lottoGroup.lottoGroup()
-        .stream()
-        .map(winningLotto::matchRank)
-        .forEach(lottoResult::add);
-    OutputView.printLottoResult(lottoResult);
-  }
-
+    private static LottoGroup createLottoGroup(Money money) {
+        try {
+            LottoSeller lottoSeller = new LottoSeller();
+            return lottoSeller.sellLotto(money);
+        } catch (LottoException e) {
+            OutputView.printMessage(e.getMessage());
+            return createLottoGroup(money);
+        }
+    }
 }
