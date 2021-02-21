a6d099c504b45f8eec9cc3cc7c6ec7b21f51ec9e
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-21 12:10:28 +0900

    feat: 로또 번호를 담당하는 LottoBall 클래스 생성

---
+++ src/main/java/domain/LottoBall.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class LottoBall {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int ARRAY_EXTRA_INDEX = 1;
+
+    private static final LottoBall[] lottoBalls = new LottoBall[MAX_NUMBER + ARRAY_EXTRA_INDEX];
+
+    private final int number;
+
+    private LottoBall(final int number) {
+        this.number = number;
+    }
+
+    static {
+        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+            lottoBalls[i] = new LottoBall(i);
+        }
+    }
+
+    public static LottoBall valueOf(final int number) {
+        validateLottoBall(number);
+        return lottoBalls[number];
+    }
+
+    private static void validateLottoBall(final int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/domain/LottoBallTest.java
@@ -0,0 +1,39 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class LottoBallTest {
+
+    @DisplayName("LottoBall 생성 테스트")
+    @Test
+    void LottoBallConstructorTest() {
+        assertThatCode(() -> LottoBall.valueOf(1))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("LottoBall 생성시 1~45 범위 테스트")
+    @ParameterizedTest()
+    @ValueSource(ints = {-10, -5, 50, 55})
+    void LottoBallParameterTest(int number) {
+        assertThatThrownBy(() -> LottoBall.valueOf(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("LottoBall 싱글톤 변환 테스트")
+    @Test
+    void LottoBallSingletonTest() {
+        for (int i = 1; i <= 45; i++) {
+            LottoBall lottoBall1 = LottoBall.valueOf(i);
+            LottoBall lottoBall2 = LottoBall.valueOf(i);
+            assertThat(lottoBall1).isSameAs(lottoBall2);
+            assertThat(lottoBall1).isEqualTo(lottoBall2);
+        }
+    }
+}
