fda92c9e074001a5a36b4fba330ade41672fb183
seunghyun <ggyool@naver.com>
2021-02-21 23:32:03 +0900

    feat(WinningStatics): 당첨 통계를 관련 클래스 추가

---
+++ src/main/java/domain/WinningStatics.java
@@ -0,0 +1,41 @@
+package domain;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class WinningStatics {
+    private final Map<Prize, Integer> numberPerPrize = new LinkedHashMap<>();
+
+    {
+        numberPerPrize.put(Prize.FIFTH, 0);
+        numberPerPrize.put(Prize.FOURTH, 0);
+        numberPerPrize.put(Prize.THIRD, 0);
+        numberPerPrize.put(Prize.SECOND, 0);
+        numberPerPrize.put(Prize.FIRST, 0);
+        numberPerPrize.put(Prize.NOTHING, 0);
+    }
+
+    public WinningStatics(final List<Prize> prizes) {
+        prizes.stream()
+                .forEach(prize -> numberPerPrize.
+                        computeIfPresent(prize, (Prize key, Integer value) -> value + 1));
+    }
+
+    public double calculateProfitRate(final LottoMoney lottoMoney) {
+        final long totalProfit = calculateTotalProfit();
+        return lottoMoney.toScale(totalProfit);
+    }
+
+    private long calculateTotalProfit() {
+        long totalProfit = 0L;
+
+        for (Entry<Prize, Integer> entry : numberPerPrize.entrySet()) {
+            long money = entry.getKey().getMoney();
+            int number = entry.getValue();
+            totalProfit += money * number;
+        }
+        return totalProfit;
+    }
+}
---
+++ src/test/java/domain/WinningStaticsTest.java
@@ -0,0 +1,45 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+class WinningStaticsTest {
+    private List<Prize> prizes;
+
+    @BeforeEach
+    void setUp() {
+        prizes = Arrays.asList(
+                Prize.FIRST,
+                Prize.FIRST,
+                Prize.FIFTH,
+                Prize.NOTHING
+        );
+    }
+
+    @DisplayName("객체 생성 성공")
+    @Test
+    void create() {
+        assertThatCode(() -> new WinningStatics(prizes))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("총 수익률 계산 비교 성공")
+    @Test
+    void calculateProfitRate() {
+        final int money = 100_000;
+        final long totalProfit = prizes.stream()
+                .mapToLong(Prize::getMoney)
+                .sum();
+        final WinningStatics winningStatics = new WinningStatics(prizes);
+
+        final double profitRate = winningStatics.calculateProfitRate(LottoMoney.valueOf(String.valueOf(money)));
+        assertThat(profitRate).isEqualTo((double) totalProfit / money);
+    }
+}
