07fa523eb03aa977073718b940734d969a8d5b5a
Jinhong <56240505+xlffm3@users.noreply.github.com>
2021-02-21 16:27:36 +0900

    feat; LottoController 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,26 +1,10 @@
 package lotto;
 
-import lotto.domain.*;
-import lotto.view.InputView;
-import lotto.view.OutputView;
-
-import java.util.List;
+import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        PurchasingPrice purchasingPrice = new PurchasingPrice(InputView.inputPurchasingPrice());
-        List<ManualTicketNumbers> manualTicketsNumbers = InputView.inputManualTicketNumbers();
-        LottoMachine lottoMachine = new LottoMachine(new RandomLottoNumberGenerator());
-        LottoTickets lottoTickets = lottoMachine.issueLottoTickets(purchasingPrice, manualTicketsNumbers);
-        OutputView.printPurchasedLottoTicketCounts(manualTicketsNumbers.size(), lottoTickets.getTicketCounts());
-        OutputView.printAllLottoTicketNumbers(lottoTickets);
-
-        List<Integer> winningTicketNumbers = InputView.inputWinningTicketNumbers();
-        int bonusBallNumber = InputView.inputBonusBallNumber();
-        WinningLottoTicket winningLottoTicket = WinningLottoTicket.of(winningTicketNumbers, bonusBallNumber);
-        LottoResult lottoResult = lottoTickets.checkResult(winningLottoTicket);
-        int purchasingPriceTotal = lottoMachine.calculatePurchasingPrice(lottoTickets);
-        double yield = lottoResult.calculateYield(purchasingPriceTotal);
-        OutputView.printLottoResult(lottoResult, yield);
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,35 @@
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+
+    public void run() {
+        LottoMachine lottoMachine = new LottoMachine(new RandomLottoNumberGenerator());
+        LottoTickets lottoTickets = buyLottoTickets(lottoMachine);
+        WinningLottoTicket winningLottoTicket = generateWinningLottoTicket();
+        LottoResult lottoResult = lottoTickets.checkResult(winningLottoTicket);
+        int purchasingPriceTotal = lottoMachine.calculatePurchasingPrice(lottoTickets);
+        double yield = lottoResult.calculateYield(purchasingPriceTotal);
+        OutputView.printLottoResult(lottoResult, yield);
+    }
+
+    private LottoTickets buyLottoTickets(LottoMachine lottoMachine) {
+        PurchasingPrice purchasingPrice = new PurchasingPrice(InputView.inputPurchasingPrice());
+        List<ManualTicketNumbers> manualTicketsNumbers = InputView.inputManualTicketNumbers();
+        LottoTickets lottoTickets = lottoMachine.issueLottoTickets(purchasingPrice, manualTicketsNumbers);
+        OutputView.printPurchasedLottoTicketCounts(manualTicketsNumbers.size(), lottoTickets.getTicketCounts());
+        OutputView.printAllLottoTicketNumbers(lottoTickets);
+        return lottoTickets;
+    }
+
+    private WinningLottoTicket generateWinningLottoTicket() {
+        List<Integer> winningTicketNumbers = InputView.inputWinningTicketNumbers();
+        int bonusBallNumber = InputView.inputBonusBallNumber();
+        return WinningLottoTicket.of(winningTicketNumbers, bonusBallNumber);
+    }
+}
