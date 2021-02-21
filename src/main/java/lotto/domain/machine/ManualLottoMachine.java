ae97e4f4891966831b94598f6c74a95565d2643e
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-02-22 00:42:48 +0900

    feat : 수동 로또 기계에서 개수 validation 추가

--- src/main/java/lotto/domain/machine/ManualLottoMachine.java
+++ src/main/java/lotto/domain/machine/ManualLottoMachine.java
@@ -7,6 +7,8 @@ import lotto.domain.ticket.LottoTickets;
 import java.util.List;
 
 public class ManualLottoMachine extends LottoMachine {
+    public static final String TICKETS_SIZE_ERROR_MSG_FORMAT = "수동 로또의 개수와 입력한 수동 로또 번호의 숫자가 같지 않습니다. 개수 : %d, 입력한 수동 번호의 수 : %d";
+
     private final LottoTicketFactory lottoTicketFactory = new LottoTicketFactory();
 
     public ManualLottoMachine(int lottoPrice) {
@@ -15,6 +17,13 @@ public class ManualLottoMachine extends LottoMachine {
 
     @Override
     public LottoTickets createTickets(int numberOfTickets, List<LottoNumbers> lottoNumbersBundle) {
+        validateTicketsSize(numberOfTickets, lottoNumbersBundle.size());
         return null;
+    }
+
+    private void validateTicketsSize(int numberOfTickets, int size) {
+        if (numberOfTickets != size) {
+            throw new IllegalArgumentException(String.format(TICKETS_SIZE_ERROR_MSG_FORMAT, numberOfTickets, size));
+        }
     }
 }
--- src/test/java/lotto/domain/machine/ManualLottoMachineTest.java
+++ src/test/java/lotto/domain/machine/ManualLottoMachineTest.java
@@ -1,13 +1,52 @@
 package lotto.domain.machine;
 
+import lotto.domain.number.LottoNumbers;
+import lotto.domain.ticket.LottoTicket;
+import lotto.domain.ticket.LottoTickets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ManualLottoMachineTest {
+    private LottoMachine manualLottoMachine = new ManualLottoMachine(1000);
+    private List<LottoNumbers> lottoNumbersBundle;
+    private int expectedSize;
 
+    @BeforeEach
+    void setUp() {
+        List<List<Integer>> testNumbers = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5, 6),
+                Arrays.asList(2, 3, 4, 5, 6, 7),
+                Arrays.asList(2, 3, 4, 5, 6, 7),
+                Arrays.asList(10, 11, 13, 14, 15, 16));
+        expectedSize = testNumbers.size();
+
+        lottoNumbersBundle = testNumbers.stream()
+                .map(LottoNumbers::new)
+                .collect(Collectors.toList());
+
+    }
+
+    @DisplayName("수동 로또가 생성되는지 확인")
     @Test
-    void createTicketsByMoney() {
+    void createTicketsByMoneyTest() {
+        LottoTickets lottoTickets = manualLottoMachine.createTicketsByMoney(expectedSize, lottoNumbersBundle);
 
+        assertThat(lottoTickets.size()).isEqualTo(expectedSize);
+        assertThat(lottoTickets.list().get(0)).isEqualTo(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)));
+    }
+
+    @DisplayName("수동 로또 개수 예외처리 확인")
+    @Test
+    void 수동_로또_개수_예외처리() {
+        assertThatThrownBy(() -> manualLottoMachine.createTicketsByMoney(expectedSize - 1, lottoNumbersBundle)).
+                isInstanceOf(IllegalArgumentException.class);
     }
 }
