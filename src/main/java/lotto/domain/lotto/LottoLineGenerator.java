c69b72a4bf73d2451964d4c06e54427fe126b8b1
jujubebat <bbwwpark@naver.com>
2021-02-21 14:28:03 +0900

    refactor: 매직넘버를 상수로 수정 및 상수 변수명을 대문자 관례로 수정

--- src/main/java/lotto/domain/lotto/LottoLineGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoLineGenerator.java
@@ -9,17 +9,20 @@ import java.util.List;
 
 public class LottoLineGenerator {
 
-    private static final List<LottoNumber> lottoNumbers = new ArrayList<>();
+    private static final List<LottoNumber> LOTTO_NUMBERS = new ArrayList<>();
+    private static final int PICKED_LOTTO_NUMBERS_FROM = 0;
+    private static final int PICKED_LOTTO_NUMBERS_TO = 6;
 
     public LottoLineGenerator() {
         for (int i = LOTTO_NUMBER_START; i <= LOTTO_NUMBER_END; i++) {
-            lottoNumbers.add(new LottoNumber(i));
+            LOTTO_NUMBERS.add(new LottoNumber(i));
         }
     }
 
     public LottoLine createLottoLine() {
-        Collections.shuffle(lottoNumbers);
-        List<LottoNumber> picked = lottoNumbers.subList(0, 6);
+        Collections.shuffle(LOTTO_NUMBERS);
+        List<LottoNumber> picked = LOTTO_NUMBERS
+            .subList(PICKED_LOTTO_NUMBERS_FROM, PICKED_LOTTO_NUMBERS_TO);
         return new LottoLine(picked);
     }
 
