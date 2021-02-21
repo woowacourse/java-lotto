fa55d9685b0d7f5551db22f96cb95e413448876d
mungto <daum7766@naver.com>
2021-02-21 16:25:59 +0900

    refactor: 등수가 변경되어도 함수가 늘어나지 않도록 수정

--- src/main/java/lotto/domain/Rewords.java
+++ src/main/java/lotto/domain/Rewords.java
@@ -2,8 +2,12 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.exception.InvalidLottoRankException;
 
 public class Rewords {
+
+    private static final int FIFTH = 5;
+    private static final int FIRST = 1;
 
     private final List<Reword> rewords;
 
@@ -11,24 +15,9 @@ public class Rewords {
         this.rewords = new ArrayList<>(rewords);
     }
 
-    public int getFirst() {
-        return matchCount(Reword.FIRST);
-    }
-
-    public int getSecond() {
-        return matchCount(Reword.SECOND);
-    }
-
-    public int getThird() {
-        return matchCount(Reword.THIRD);
-    }
-
-    public int getFourth() {
-        return matchCount(Reword.FOURTH);
-    }
-
-    public int getFifth() {
-        return matchCount(Reword.FIFTH);
+    public int getRankCount(int rank) {
+        validate(rank);
+        return matchCount(Reword.values()[rank - 1]);
     }
 
     public double profit(final int money) {
@@ -37,6 +26,12 @@ public class Rewords {
             .sum();
 
         return LottoCalculator.divide(money, profit);
+    }
+
+    private void validate(int rank) {
+        if (rank < FIRST || rank > FIFTH) {
+            throw new InvalidLottoRankException();
+        }
     }
 
     private int matchCount(final Reword reword) {
