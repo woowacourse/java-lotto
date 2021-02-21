6d1f41193024832a40801a030948264d62c67738
sakjung <ssangyu123@gmail.com>
2021-02-21 15:17:40 +0900

    refactor: rank enum에서 랭크별 메세지를 담당하는 기능을 뷰로 옮겨줌

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,28 +4,23 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static lotto.domain.LottoStatistics.DEFAULT_STATISTICS_FORMAT;
-import static lotto.domain.LottoStatistics.STATISTICS_FORMAT_FOR_SECOND;
-
 public enum Rank {
-    FIRST(6, 2_000_000_000, DEFAULT_STATISTICS_FORMAT),
-    SECOND(5, 30_000_000, STATISTICS_FORMAT_FOR_SECOND),
-    THIRD(5, 1_500_000, DEFAULT_STATISTICS_FORMAT),
-    FOURTH(4, 50_000, DEFAULT_STATISTICS_FORMAT),
-    FIFTH(3, 5_000, DEFAULT_STATISTICS_FORMAT),
-    NONE(0, 0, DEFAULT_STATISTICS_FORMAT);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
 
     public static final int RANK_START_INDEX = 0;
     public static final int RANK_END_INDEX = 5;
 
     private final int count;
     private final int prize;
-    private final String message;
 
-    Rank(int count, int prize, String message) {
+    Rank(int count, int prize) {
         this.count = count;
         this.prize = prize;
-        this.message = String.format(message, this.count, this.prize);
     }
 
     public static Rank getCorrespondingRank(int matchCount, boolean bonusMatch) {
@@ -42,11 +37,11 @@ public enum Rank {
         return values;
     }
 
-    public int getPrize() {
-        return prize;
+    public int getCount() {
+        return count;
     }
 
-    public String getMessage() {
-        return message;
+    public int getPrize() {
+        return prize;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,19 @@
 package lotto.view;
 
-import lotto.domain.*;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoStatistics;
+import lotto.domain.Lottos;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static lotto.domain.Rank.getRanksForStatistics;
+import static lotto.view.RankMessage.getRankMessages;
 
 public class OutputView {
+    public static final String DEFAULT_STATISTICS_FORMAT = "%d개 일치 (%d원)- ";
+    public static final String STATISTICS_FORMAT_FOR_SECOND = "%d개 일치, 보너스 볼 일치(%d원)- ";
+
     private OutputView() {
     }
 
@@ -34,9 +40,9 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---------");
         List<Integer> statistics = lottoStatistics.getWinCountByRank();
-        List<Rank> ranksForStatistics = getRanksForStatistics();
+        List<String> rankMessages = getRankMessages();
         for (int i = 0; i < statistics.size(); i++) {
-            System.out.printf("%s %d개\n", ranksForStatistics.get(i).getMessage(), statistics.get(i));
+            System.out.printf("%s %d개\n", rankMessages.get(i), statistics.get(i));
         }
         System.out.printf("총 수익률은 %.2f입니다.\n", lottoStatistics.getProfitRate());
     }
