3794b0df8f258ee79f23cb4a3a17f763db8f8a33
DWL5 <ssop6403@gmail.com>
2021-02-22 03:22:03 +0900

    test: 각 당첨 순위별로 테스트 케이스 추가

--- src/test/java/domain/lotto/LottoLineTest.java
+++ src/test/java/domain/lotto/LottoLineTest.java
@@ -5,9 +5,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+
 import lotto.domain.lotto.LottoLine;
 import lotto.domain.lotto.LottoNumber;
 import lotto.domain.Rank;
+import lotto.domain.lotto.WinningNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,11 +19,11 @@ public class LottoLineTest {
     @DisplayName("LottoNumbers 에는 중복된 LottoNubmer를 가지면 안된다")
     public void lottoNumbers_do_not_have_duplicated_lotto_number() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> {
-                LottoLine lottoLine = new LottoLine(Arrays
-                    .asList(new LottoNumber(1), new LottoNumber(1), new LottoNumber(2),
-                        new LottoNumber(2), new LottoNumber(5), new LottoNumber(6)));
-            });
+                .isThrownBy(() -> {
+                    LottoLine lottoLine = new LottoLine(Arrays
+                            .asList(new LottoNumber(1), new LottoNumber(1), new LottoNumber(2),
+                                    new LottoNumber(2), new LottoNumber(5), new LottoNumber(6)));
+                });
     }
 
     @Test
@@ -29,37 +31,91 @@ public class LottoLineTest {
     public void lottoNumbers_must_have_6_numbers() {
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> {
-                LottoLine lottoLine = new LottoLine(Arrays
-                    .asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                        new LottoNumber(4),
-                        new LottoNumber(5), new LottoNumber(6), new LottoNumber(7)));
-            });
+                .isThrownBy(() -> {
+                    LottoLine lottoLine = new LottoLine(Arrays
+                            .asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                                    new LottoNumber(4),
+                                    new LottoNumber(5), new LottoNumber(6), new LottoNumber(7)));
+                });
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> {
-                LottoLine lottoLine = new LottoLine(Arrays
-                    .asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                        new LottoNumber(4),
-                        new LottoNumber(5)));
-            });
+                .isThrownBy(() -> {
+                    LottoLine lottoLine = new LottoLine(Arrays
+                            .asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                                    new LottoNumber(4),
+                                    new LottoNumber(5)));
+                });
     }
 
     @Test
-    @DisplayName("로또 번호 매칭 테스트")
-    void lotto_number_match_test() {
+    @DisplayName("로또 번호 매칭 테스트 5등")
+    void lotto_number_match_test_fifth() {
         List<LottoNumber> lottoNumbers = Arrays.asList(new LottoNumber(1)
-            , new LottoNumber(2), new LottoNumber(3), new LottoNumber(4)
-            , new LottoNumber(5), new LottoNumber(6)
+                , new LottoNumber(2), new LottoNumber(3), new LottoNumber(4)
+                , new LottoNumber(5), new LottoNumber(6)
         );
 
         List<LottoNumber> answerLottoNumbers = Arrays.asList(new LottoNumber(1)
-            , new LottoNumber(2), new LottoNumber(3), new LottoNumber(12)
-            , new LottoNumber(13), new LottoNumber(14)
+                , new LottoNumber(2), new LottoNumber(3), new LottoNumber(12)
+                , new LottoNumber(13), new LottoNumber(14)
         );
 
         LottoLine lottoLine = new LottoLine(lottoNumbers);
-        assertThat(lottoLine.matchLottoNumbers(lottoNumbers, new LottoNumber(3)  ,answerLottoNumbers))
-            .isEqualTo(Rank.FIFTH);
+        WinningNumbers winningNumbers = new WinningNumbers(new LottoLine(answerLottoNumbers), new LottoNumber(9));
+        assertThat(lottoLine.matchLottoNumbers(lottoNumbers, winningNumbers)).isEqualTo(Rank.FIFTH);
+    }
+
+    @Test
+    @DisplayName("로또 번호 매칭 테스트 4등")
+    void lotto_number_match_test_forth() {
+        List<LottoNumber> lottoNumbers = Arrays.asList(new LottoNumber(1)
+                , new LottoNumber(2), new LottoNumber(3), new LottoNumber(4)
+                , new LottoNumber(5), new LottoNumber(6)
+        );
+
+        List<LottoNumber> answerLottoNumbers = Arrays.asList(new LottoNumber(1)
+                , new LottoNumber(2), new LottoNumber(3), new LottoNumber(4)
+                , new LottoNumber(13), new LottoNumber(14)
+        );
+
+        LottoLine lottoLine = new LottoLine(lottoNumbers);
+        WinningNumbers winningNumbers = new WinningNumbers(new LottoLine(answerLottoNumbers), new LottoNumber(9));
+        assertThat(lottoLine.matchLottoNumbers(lottoNumbers, winningNumbers)).isEqualTo(Rank.FOURTH);
+    }
+
+    @Test
+    @DisplayName("로또 번호 매칭 테스트 3등")
+    void lotto_number_match_test_third() {
+        List<LottoNumber> lottoNumbers = Arrays.asList(new LottoNumber(1)
+                , new LottoNumber(2), new LottoNumber(3), new LottoNumber(4)
+                , new LottoNumber(5), new LottoNumber(6)
+        );
+
+        List<LottoNumber> answerLottoNumbers = Arrays.asList(new LottoNumber(1)
+                , new LottoNumber(2), new LottoNumber(3), new LottoNumber(4)
+                , new LottoNumber(5), new LottoNumber(14)
+        );
+
+        LottoLine lottoLine = new LottoLine(lottoNumbers);
+        WinningNumbers winningNumbers = new WinningNumbers(new LottoLine(answerLottoNumbers), new LottoNumber(9));
+        assertThat(lottoLine.matchLottoNumbers(lottoNumbers, winningNumbers)).isEqualTo(Rank.THIRD);
+    }
+
+    @Test
+    @DisplayName("로또 번호 매칭 테스트 2등")
+    void lotto_number_match_test_second() {
+        List<LottoNumber> lottoNumbers = Arrays.asList(new LottoNumber(1)
+                , new LottoNumber(2), new LottoNumber(3), new LottoNumber(4)
+                , new LottoNumber(5), new LottoNumber(9)
+        );
+
+        List<LottoNumber> answerLottoNumbers = Arrays.asList(new LottoNumber(1)
+                , new LottoNumber(2), new LottoNumber(3), new LottoNumber(4)
+                , new LottoNumber(5), new LottoNumber(14)
+        );
+
+        LottoLine lottoLine = new LottoLine(lottoNumbers);
+        WinningNumbers winningNumbers = new WinningNumbers(new LottoLine(answerLottoNumbers), new LottoNumber(9));
+        assertThat(lottoLine.matchLottoNumbers(lottoNumbers, winningNumbers)).isEqualTo(Rank.SECOND);
     }
 }
