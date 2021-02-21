24c4dff860a0b2c84bb7e33f6b473fcb944c11e4
perenok <perenoke@gmail.com>
2021-02-21 22:25:00 +0900

    refactor(Lottos) : move compareWithWinningLotto method from LottoGame

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -24,6 +24,14 @@ public class Lottos {
         return Collections.unmodifiableList(lottos);
     }
 
+    public LottoGameResult compareWithWinningLotto(WinningLotto winningLotto) {
+        LottoGameResult lottoGameResult = new LottoGameResult();
+        lottos().stream()
+                .map(winningLotto::findRank)
+                .forEach(lottoGameResult::add);
+        return lottoGameResult;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
