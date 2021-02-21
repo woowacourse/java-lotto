17b2c54ef87b0fed1385b79ba2d67b3caeae63d3
Seed <hsik0225@gmail.com>
2021-02-22 07:32:36 +0900

    rafactor(lotto-machine):Application을 Controller로 수정 및 그에 따른 LottoMachine의 코드를 Application으로 이동

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,39 +1,14 @@
 package lotto;
 
 import lotto.domain.*;
-import lotto.view.InputView;
-import lotto.view.OutputView;
-
-import java.util.List;
 
 public class LottoMachine {
     
-    private final InputView inputView;
-    
-    private final OutputView outputView;
-    
-    public LottoMachine() {
-        this.inputView = new InputView();
-        this.outputView = new OutputView();
+    public Lottos purchase(PaymentAmount paymentAmount) {
+        return LottoFactory.makeLottos(paymentAmount);
     }
     
-    public Lottos purchase() {
-        PaymentAmount paymentAmount = inputView.readPaymentAmount();
-        
-        Lottos lottos = LottoFactory.makeLottos(paymentAmount);
-        
-        outputView.printPurchaseLottos(paymentAmount, lottos);
-        
-        return lottos;
-    }
-    
-    public void seeResults(Lottos lottos) {
-        List<Integer> lotto = inputView.readWinningLotto();
-        LottoNumber bonusNumber = inputView.readBonusNumber();
-        WinningLotto winningLotto = WinningLotto.of(lotto, bonusNumber);
-        
-        LottoStatisticResult result = lottos.retrieveResults(winningLotto);
-        
-        outputView.printStatistic(result);
+    public LottoStatisticResult seeResults(Lottos lottos, WinningLotto winningLotto) {
+        return lottos.retrieveResults(winningLotto);
     }
 }
