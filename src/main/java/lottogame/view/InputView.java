9329f13d09b334ba0101e42e973c7ffe3407584d
hyeon9mak <jinha3507@gmail.com>
2021-02-21 13:21:09 +0900

    refactor(InputView): delete constant value, add validate integer logic

--- src/main/java/lottogame/view/InputView.java
+++ src/main/java/lottogame/view/InputView.java
@@ -4,24 +4,28 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String INPUT_REQUEST_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
-    private static final String INPUT_REQUEST_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
-    private static final String INPUT_REQUEST_MONEY = "구입금액을 입력해 주세요.";
-
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String getMoneyInput() {
-        System.out.println(INPUT_REQUEST_MONEY);
-        return scanner.nextLine();
+    public static int getInputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = scanner.nextLine();
+        validateInteger(money);
+        return Integer.parseInt(money);
+    }
+
+    private static void validateInteger(String money) {
+        if (!money.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("구입 금액은 정수로만 입력 가능합니다.");
+        }
     }
 
     public static String getWinningNumbersInput() {
-        System.out.println(INPUT_REQUEST_WINNING_NUMBERS);
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return scanner.nextLine();
     }
 
     public static String getBonusNumberInput() {
-        System.out.println(INPUT_REQUEST_BONUS_NUMBER);
+        System.out.println("보너스 볼을 입력해 주세요.");
         return scanner.nextLine();
     }
 }
