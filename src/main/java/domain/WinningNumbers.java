ac941128997caafcee99799beb1d7f4d9a53d4d6
seunghyun <ggyool@naver.com>
2021-02-21 19:57:24 +0900

    refactor(WinningNumbers): 필요 없는메서드 삭제, 코드 정리

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -1,50 +1,27 @@
 package domain;
 
-import domain.ticket.LottoTicket;
+import domain.ticket.Ticket;
+import domain.ticket.WinningTicket;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WinningNumbers {
-    private static final int WINNING_TICKET_SIZE = 6;
-    private static final String WINNING_TICKET_SIZE_ERROR = "당첨 번호의 개수가 맞지 않습니다.";
-    private static final String BONUS_DUPLICATE_ERROR = "당첨 번호에 보너스 번호가 포함되어 있습니다.";
-
-    private final LottoTicket winningTicket;
+    private final Ticket winningTicket;
     private final LottoNumber bonusNumber;
 
-    private WinningNumbers(final List<Integer> winningNumbers, final int bonusNumber) {
-        this.winningTicket = LottoTicket.valueOf(winningNumbers);
+    public WinningNumbers(final List<Integer> winningNumbers, final int bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+        this.winningTicket = new WinningTicket(winningNumbers);
         this.bonusNumber = LottoNumber.valueOf(bonusNumber);
     }
 
-    public static WinningNumbers valueOf(final List<Integer> winningNumbers, final int bonusNumber) {
-        validate(winningNumbers, bonusNumber);
-        return new WinningNumbers(winningNumbers, bonusNumber);
-    }
-
-    private static void validate(final List<Integer> winningNumbers, final int bonusNumber) {
-        validateSize(winningNumbers);
+    private void validate(final List<Integer> winningNumbers, final int bonusNumber) {
         validateDistinctBonus(winningNumbers, bonusNumber);
     }
 
-    private static void validateSize(final List<Integer> winningNumbers) {
-        if (winningNumbers.size() != WINNING_TICKET_SIZE) {
-            throw new IllegalArgumentException(WINNING_TICKET_SIZE_ERROR);
-        }
-    }
-
-    private static void validateDistinctBonus(final List<Integer> winningNumbers, final int bonusNumber) {
+    private void validateDistinctBonus(final List<Integer> winningNumbers, final int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(BONUS_DUPLICATE_ERROR);
+            throw new IllegalArgumentException("당첨 번호에 보너스 번호가 포함되어 있습니다:" + bonusNumber);
         }
-    }
-
-    public List<Ranking> getRankings(final List<LottoTicket> lottoTickets) {
-        List<Ranking> rankings = new ArrayList<>();
-        for (LottoTicket lottoTicket : lottoTickets) {
-            rankings.add(lottoTicket.checkRanking(winningTicket, bonusNumber));
-        }
-        return rankings;
     }
 }
--- src/test/java/domain/WinningNumbersTest.java
+++ src/test/java/domain/WinningNumbersTest.java
@@ -1,13 +1,13 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class WinningNumbersTest {
     @DisplayName("유효한 값이면 객체 생성 성공")
@@ -16,18 +16,18 @@ public class WinningNumbersTest {
         final List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         final int bonusNumber = 7;
 
-        assertThatCode(() -> WinningNumbers.valueOf(winningNumbers, bonusNumber))
+        assertThatCode(() -> new WinningNumbers(winningNumbers, bonusNumber))
                 .doesNotThrowAnyException();
     }
 
     @DisplayName("객체 생성 실패 : 당첨 번호에 보너스 번호가 포함 되어 있는 경우 ")
     @Test
     void valueOf_includeBonusNumber_exceptionThrown() {
-        final List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         final int bonusNumber = 6;
+        final List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, bonusNumber);
 
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> WinningNumbers.valueOf(winningNumbers, bonusNumber))
-                .withMessageContaining("포함");
+                .isThrownBy(() -> new WinningNumbers(winningNumbers, bonusNumber))
+                .withMessageContaining("당첨 번호에 보너스 번호가 포함되어 있습니다");
     }
 }
