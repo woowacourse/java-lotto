d8ee8983278a75f6b6802ca4c6b449ae649648b4
hyeon9mak <jinha3507@gmail.com>
2021-02-21 13:19:58 +0900

    refactor(Money): fix constructor parameter data type

--- src/main/java/lottogame/domain/Money.java
+++ src/main/java/lottogame/domain/Money.java
@@ -4,18 +4,18 @@ public class Money {
 
     private int value;
 
-    public Money(final String money) {
-        this.value = Integer.parseInt(money);
+    public Money(final int value) {
+        this.value = value;
         validateNegative(this.value);
     }
 
     private void validateNegative(final int value) {
         if (value < 0) {
-            throw new IllegalArgumentException("돈 입력은 양수이여야 합니다.");
+            throw new IllegalArgumentException("돈 입력은 0 이상이어야 합니다.");
         }
     }
 
-    public boolean isCanBuyAmount(final int amount) {
+    public boolean canBuyAmount(final int amount) {
         return this.value >= amount;
     }
 
--- src/test/java/lottogame/domain/MoneyTest.java
+++ src/test/java/lottogame/domain/MoneyTest.java
@@ -1,6 +1,7 @@
 package lottogame.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,16 +9,16 @@ import org.junit.jupiter.api.Test;
 public class MoneyTest {
 
     @Test
-    @DisplayName("Money에 문자를 넣었을때")
-    void moneyValueIsString() {
-        assertThatThrownBy(() -> new Money("abc"))
-            .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("Money가 정상적으로 생성되었는지 테스트")
+    void moneyValueIsValid() {
+        assertThat(new Money(1000).getValue()).isEqualTo(1000);
+        assertThat(new Money(2000).getValue()).isNotEqualTo(3000);
     }
 
     @Test
-    @DisplayName("Money에 음수를 넣었을때")
+    @DisplayName("Money에 음수가 입력되었을 때 예외처리 진행")
     void moneyValueIsNegativeNumber() {
-        assertThatThrownBy(() -> new Money("-1"))
-            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Money(-1)).isInstanceOf(IllegalArgumentException.class);
     }
+
 }
