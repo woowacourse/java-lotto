d0b9fbb75289aa31ee8b6bdffa82a926609d272e
Minsung Son <sns2831@gmail.com>
2021-02-22 01:45:06 +0900

    test(lotto): 수익률 게산 테스트 추가

--- src/test/java/lotto/domain/lotto/WinningNumberTest.java
+++ src/test/java/lotto/domain/lotto/WinningNumberTest.java
@@ -1,13 +1,14 @@
 package lotto.domain.lotto;
 
 import static java.util.stream.Collectors.toList;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+
 import lotto.domain.number.LottoNumber;
 import lotto.domain.number.Number;
+import lotto.domain.number.PayOut;
 import lotto.domain.rank.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,14 +20,14 @@ public class WinningNumberTest {
     void getWinningNumber() {
         WinningNumber winningNumber = new WinningNumber("1, 2, 3, 4, 5, 6", "7");
         LottoNumbers expected = new LottoNumbers(
-            Arrays.asList(
-                new LottoNumber(new Number(1)),
-                new LottoNumber(new Number(2)),
-                new LottoNumber(new Number(3)),
-                new LottoNumber(new Number(4)),
-                new LottoNumber(new Number(5)),
-                new LottoNumber(new Number(6))
-            )
+                Arrays.asList(
+                        new LottoNumber(new Number(1)),
+                        new LottoNumber(new Number(2)),
+                        new LottoNumber(new Number(3)),
+                        new LottoNumber(new Number(4)),
+                        new LottoNumber(new Number(5)),
+                        new LottoNumber(new Number(6))
+                )
         );
 
         assertThat(expected).isEqualTo(winningNumber.getLottoNumbers());
@@ -36,7 +37,7 @@ public class WinningNumberTest {
     @DisplayName("하나의 숫자가 아닌 경우 예외")
     void getBonusNumberFromStringInput() {
         assertThatIllegalArgumentException().isThrownBy(
-            () -> new WinningNumber("1,2,3,4,5,6", "1, 3")
+                () -> new WinningNumber("1,2,3,4,5,6", "1, 3")
         ).withMessage("보너스 볼의 하나의 숫자로 이루어져야 합니다.");
     }
 
@@ -44,7 +45,7 @@ public class WinningNumberTest {
     @DisplayName("보너스 번호가 당첨 번호와 중복이 되는 경우 예외")
     void duplicateBonusNumberWithLottoNumbers() {
         assertThatIllegalArgumentException().isThrownBy(
-            () -> new WinningNumber("1,2,3,4,5,6", "3")
+                () -> new WinningNumber("1,2,3,4,5,6", "3")
         ).withMessage("보너스 번호는 로또 번호와 달라야 합니다.");
     }
 
@@ -55,36 +56,36 @@ public class WinningNumberTest {
         List<Long> expected = Arrays.asList(1L, 1L, 1L, 0L, 0L);
 
         LottoGroup lottoGroup = new LottoGroup(Arrays.asList(
-            new LottoNumbers(
-                Arrays.asList(
-                    new LottoNumber(new Number(1)),
-                    new LottoNumber(new Number(2)),
-                    new LottoNumber(new Number(3)),
-                    new LottoNumber(new Number(4)),
-                    new LottoNumber(new Number(5)),
-                    new LottoNumber(new Number(6))
+                new LottoNumbers(
+                        Arrays.asList(
+                                new LottoNumber(new Number(1)),
+                                new LottoNumber(new Number(2)),
+                                new LottoNumber(new Number(3)),
+                                new LottoNumber(new Number(4)),
+                                new LottoNumber(new Number(5)),
+                                new LottoNumber(new Number(6))
+                        )
+                ),
+                new LottoNumbers(
+                        Arrays.asList(
+                                new LottoNumber(new Number(1)),
+                                new LottoNumber(new Number(2)),
+                                new LottoNumber(new Number(3)),
+                                new LottoNumber(new Number(4)),
+                                new LottoNumber(new Number(5)),
+                                new LottoNumber(new Number(34))
+                        )
+                ),
+                new LottoNumbers(
+                        Arrays.asList(
+                                new LottoNumber(new Number(1)),
+                                new LottoNumber(new Number(2)),
+                                new LottoNumber(new Number(3)),
+                                new LottoNumber(new Number(4)),
+                                new LottoNumber(new Number(5)),
+                                new LottoNumber(new Number(7))
+                        )
                 )
-            ),
-            new LottoNumbers(
-                Arrays.asList(
-                    new LottoNumber(new Number(1)),
-                    new LottoNumber(new Number(2)),
-                    new LottoNumber(new Number(3)),
-                    new LottoNumber(new Number(4)),
-                    new LottoNumber(new Number(5)),
-                    new LottoNumber(new Number(34))
-                )
-            ),
-            new LottoNumbers(
-                Arrays.asList(
-                    new LottoNumber(new Number(1)),
-                    new LottoNumber(new Number(2)),
-                    new LottoNumber(new Number(3)),
-                    new LottoNumber(new Number(4)),
-                    new LottoNumber(new Number(5)),
-                    new LottoNumber(new Number(7))
-                )
-            )
         ));
 
         List<Long> actual = winningNumber.
@@ -94,5 +95,46 @@ public class WinningNumberTest {
                 .collect(toList());
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("수익률 계산")
+    void getYield() {
+        WinningNumber winningNumber = new WinningNumber("1, 2, 3, 4, 5, 6", "7");
+        LottoGroup lottoGroup = new LottoGroup(Arrays.asList(
+                new LottoNumbers(
+                        Arrays.asList(
+                                new LottoNumber(new Number(1)),
+                                new LottoNumber(new Number(2)),
+                                new LottoNumber(new Number(3)),
+                                new LottoNumber(new Number(4)),
+                                new LottoNumber(new Number(5)),
+                                new LottoNumber(new Number(6))
+                        )
+                ),
+                new LottoNumbers(
+                        Arrays.asList(
+                                new LottoNumber(new Number(1)),
+                                new LottoNumber(new Number(2)),
+                                new LottoNumber(new Number(3)),
+                                new LottoNumber(new Number(4)),
+                                new LottoNumber(new Number(5)),
+                                new LottoNumber(new Number(34))
+                        )
+                ),
+                new LottoNumbers(
+                        Arrays.asList(
+                                new LottoNumber(new Number(1)),
+                                new LottoNumber(new Number(2)),
+                                new LottoNumber(new Number(3)),
+                                new LottoNumber(new Number(4)),
+                                new LottoNumber(new Number(5)),
+                                new LottoNumber(new Number(7))
+                        )
+                )
+        ));
+
+        assertThat(winningNumber.getYield(lottoGroup, new PayOut(3000)))
+                .isEqualTo(2031500000D / 3000D, withPrecision(2d));
     }
 }
