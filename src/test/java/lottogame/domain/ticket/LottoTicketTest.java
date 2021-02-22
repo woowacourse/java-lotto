af0822996e4f983e1943e4ff5388160cba59b197
hyeon9mak <jinha3507@gmail.com>
2021-02-22 14:50:58 +0900

    test(LottoTicketTest): add setUp method, refactor test methods

--- src/test/java/lottogame/domain/ticket/LottoTicketTest.java
+++ src/test/java/lottogame/domain/ticket/LottoTicketTest.java
@@ -1,29 +1,40 @@
 package lottogame.domain.ticket;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
 import lottogame.domain.number.LottoNumber;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoTicketTest {
 
-    @Test
-    @DisplayName("뽑힌 LottoNumber가 6개인지")
-    void issueLottoTicket() {
-        LottoTicket lottoTicket = new LottoTicket();
-        assertThat(lottoTicket.getLottoNumbers().size()).isNotEqualTo(5);
-        assertThat(lottoTicket.getLottoNumbers().size()).isEqualTo(6);
-        assertThat(lottoTicket.getLottoNumbers().size()).isNotEqualTo(7);
+    private LottoTicket lottoTicket;
+
+    @BeforeEach
+    void setUp() {
+        Set<LottoNumber> lottoNumberSet = new HashSet<>();
+        lottoNumberSet.add(new LottoNumber(1));
+        lottoNumberSet.add(new LottoNumber(2));
+        lottoNumberSet.add(new LottoNumber(3));
+        lottoNumberSet.add(new LottoNumber(4));
+        lottoNumberSet.add(new LottoNumber(5));
+        lottoNumberSet.add(new LottoNumber(6));
+        this.lottoTicket = new LottoTicket(lottoNumberSet);
     }
 
     @Test
-    @DisplayName("뽑힌 LottoNumber에 중복이 없는지")
-    void issueLottoTicketDuplicate() {
-        LottoTicket lottoTicket = new LottoTicket();
-        Set<LottoNumber> lottoNumberSet = new HashSet<>(lottoTicket.getLottoNumbers());
-        assertThat(lottoNumberSet.size()).isEqualTo(lottoTicket.getLottoNumbers().size());
+    @DisplayName("뽑힌 LottoNumber가 6개인지인 확인")
+    void testIssuedLottoNumberCount() {
+        assertThat(lottoTicket.getLottoNumbers().size()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("LottoNumber가 LottoTicket에 있는지 없는지 확인")
+    void testIssueLottoTicketDuplicate() {
+        assertThat(lottoTicket.contains(new LottoNumber(6))).isTrue();
+        assertThat(lottoTicket.contains(new LottoNumber(7))).isFalse();
     }
 }
