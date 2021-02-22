688278eeb2ab5c376cd20b8f5e261b994bd39f10
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-22 17:24:10 +0900

    refactor: LottoTicket 상수 네이밍 수정

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 public class LottoTicket {
 
-    private static final int TOTAL_NUMBER_COUNT = 12;
-    private static final int VALID_NUMBER_COUNT = 6;
+    private static final int LOTTO_NUMBER_COUNT = 6;
     private static final String NULL_ERROR_MESSAGE = "null값은 허용하지 않습니다.";
     private static final String EMPTY_ERROR_MESSAGE = "숫자는 하나 이상이어야 합니다.";
     private static final String COUNT_ERROR_MESSAGE = "숫자는 6개여야 합니다.";
@@ -38,7 +37,7 @@ public class LottoTicket {
     private int calculateMatchCount(LottoWinnerTicket lottoWinnerTicket) {
         Set<LottoNumber> matchingCheckContainer = new HashSet<>(lottoNumbers);
         matchingCheckContainer.addAll(lottoWinnerTicket.getLottoNumbers());
-        return TOTAL_NUMBER_COUNT - matchingCheckContainer.size();
+        return LOTTO_NUMBER_COUNT * 2 - matchingCheckContainer.size();
     }
 
     private void validateEmptyTicket(List<LottoNumber> lottoNumbers) {
@@ -55,7 +54,7 @@ public class LottoTicket {
     }
 
     private void validateCount(List<LottoNumber> lottoNumbers) {
-        if (lottoNumbers.size() == VALID_NUMBER_COUNT) {
+        if (lottoNumbers.size() == LOTTO_NUMBER_COUNT) {
             return;
         }
         throw new RuntimeException(COUNT_ERROR_MESSAGE);
