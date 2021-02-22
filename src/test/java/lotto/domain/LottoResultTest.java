b743faa1528f92acd954198adab92a9bbfd9c32f
RinSabbit <celena1004@naver.com>
2021-02-22 17:34:42 +0900

    refactor: 각 메소드에서 공통적으로 쓰는 부분을 BeforeEach로 이동

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,33 +1,37 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 import lotto.domain.ticketFactory.TicketFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoResultTest {
-
     LottoResult lottoResult = new LottoResult();
+    LottoTickets lottoTickets;
+    WinningLotto winningLotto;
+
+    @BeforeEach
+    void init() {
+        LottoTicket lottoTicket = TicketFactory.makeFixedTicket(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lottoTickets = new LottoTickets(Collections.singletonList(lottoTicket));
+        LottoTicket winningTicket = TicketFactory.makeFixedTicket(Arrays.asList(1, 2, 3, 4, 5, 7));
+        winningLotto = new WinningLotto(winningTicket, new LottoNumber(6));
+    }
 
     @DisplayName("당첨티켓과 구매티켓들을 각각 비교하여 각 등수별 당첨횟수를 구한다.")
     @Test
-    void checkResult(){
-        LottoTicket lottoTicket = TicketFactory.makeFixedTicket(Arrays.asList(1,2,3,4,5,6));
-        LottoTickets lottoTickets = new LottoTickets(Arrays.asList(lottoTicket));
-        LottoTicket winningTicket = TicketFactory.makeFixedTicket(Arrays.asList(1,2,3,4,5,7));
-        WinningLotto winningLotto = new WinningLotto(winningTicket,new LottoNumber(6));
-
+    void checkResult() {
         lottoResult.checkWinnings(lottoTickets, winningLotto);
     }
 
     @DisplayName("저장된 등수별 당첨횟수를 이용해 총 당청금을 게산한다.")
     @Test
-    void calculate(){
-        checkResult();
+    void calculate() {
+        lottoResult.checkWinnings(lottoTickets, winningLotto);
         assertThat(lottoResult.calculateTotalReward()).isEqualTo(30_000_000);
     }
-
 }
