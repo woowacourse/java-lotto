64d0d2668e61dd7b41cf914141fcdbcb9daf9185
Kimun Kim <kkm97351@gmail.com>
2021-02-22 16:16:29 +0900

    refactor: WinningNumbersTest Fixture를 이용해 리팩토링

--- src/test/java/domain/WinningNumbersTest.java
+++ src/test/java/domain/WinningNumbersTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +10,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningNumbersTest {
+    private WinningNumbers winningNumbers;
+
+    @BeforeEach
+    public void setUp() {
+        winningNumbers = new WinningNumbers(
+                LottoTicket.valueOf(Arrays.asList("1", "2", "3", "4", "5", "6")),
+                new LottoNumber("7"));
+    }
+
     @DisplayName("당첨 번호와 보너스 볼을 묶은 객체를 생성한다")
     @Test
     public void createWinningNumbers() {
-        WinningNumbers winningNumber = new WinningNumbers(
-                LottoTicket.valueOf(Arrays.asList("3", "4", "7", "8", "10", "16")),
-                new LottoNumber("22"));
-        assertThat(winningNumber).isInstanceOf(WinningNumbers.class);
+        WinningNumbers winningNumberTest = new WinningNumbers(
+                LottoTicket.valueOf(Arrays.asList("1", "2", "3", "4", "5", "6")),
+                new LottoNumber("7"));
+        assertThat(winningNumberTest).isInstanceOf(WinningNumbers.class);
     }
 
     @DisplayName("당첨 번호에 보너스 볼 번호가 있는 경우 예외를 발생시킨다")
@@ -23,38 +33,29 @@ public class WinningNumbersTest {
     public void createWinningNumbersException() {
         assertThatThrownBy(() -> {
             new WinningNumbers(
-                LottoTicket.valueOf(Arrays.asList("3", "4", "7", "8", "10", "16")),
-                new LottoNumber("10"));
+                    LottoTicket.valueOf(Arrays.asList("1", "2", "3", "4", "5", "6")),
+                    new LottoNumber("6"));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("맞춘 개수에 맞는 Rank객체가 리턴된다. - 1등")
     @Test
     public void calculateLottoFirstRank() {
-        WinningNumbers winningNumbers = new WinningNumbers(
-                LottoTicket.valueOf(Arrays.asList("3", "4", "7", "8", "10", "16")),
-                new LottoNumber("22"));
-        Rank rank = winningNumbers.calculateRank(LottoTicket.valueOf(Arrays.asList("3", "4", "7", "8", "10", "16")));
+        Rank rank = winningNumbers.calculateRank(LottoTicket.valueOf(Arrays.asList("1", "2", "3", "4", "5", "6")));
         assertThat(rank).isEqualTo(Rank.FIRST);
     }
 
     @DisplayName("맞춘 개수에 맞는 Rank객체가 리턴된다. - 2등")
     @Test
     public void calculateLottoSecondRank() {
-        WinningNumbers winningNumbers = new WinningNumbers(
-                LottoTicket.valueOf(Arrays.asList("3", "4", "7", "8", "10", "16")),
-                new LottoNumber("22"));
-        Rank rank = winningNumbers.calculateRank(LottoTicket.valueOf(Arrays.asList("3", "4", "7", "8", "10", "22")));
+        Rank rank = winningNumbers.calculateRank(LottoTicket.valueOf(Arrays.asList("1", "2", "3", "4", "5", "7")));
         assertThat(rank).isEqualTo(Rank.SECOND);
     }
 
     @DisplayName("맞춘 개수에 맞는 Rank객체가 리턴된다. - 없는 경우")
     @Test
     public void calculateLottoNoneRank() {
-        WinningNumbers winningNumbers = new WinningNumbers(
-                LottoTicket.valueOf(Arrays.asList("3", "4", "7", "8", "10", "16")),
-                new LottoNumber("22"));
-        Rank rank = winningNumbers.calculateRank(LottoTicket.valueOf(Arrays.asList("1", "2", "5", "7", "11", "15")));
+        Rank rank = winningNumbers.calculateRank(LottoTicket.valueOf(Arrays.asList("8", "9", "10", "11", "12", "13")));
         assertThat(rank).isEqualTo(Rank.NOTHING);
     }
 }
