9cc1222c8b89a9731f2f7dffa9c0626f08718c0f
hyeon9mak <jinha3507@gmail.com>
2021-02-22 14:49:02 +0900

    refactor(OutputView): divide print methods

--- src/main/java/lottogame/view/OutputView.java
+++ src/main/java/lottogame/view/OutputView.java
@@ -1,39 +1,43 @@
 package lottogame.view;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
+import lottogame.domain.LottoGameResult;
 import lottogame.domain.Rank;
 
 public class OutputView {
 
-    private static final String TICKETS_COUNT_MESSAGE = "개를 구매했습니다.";
-    private static final String AMOUNT_YIELD_FORMAT = "총 수익률은 %.2f입니다.";
+    private static final String DEFAULT_RANK_PRINT_FORMAT = "%d개 일치 (%d원)- %d개";
+    private static final String BONUS_RANK_PRINT_FORMAT = "%d개 일치, 보너스 볼 일치(%d원) - %d개";
 
     public static void printLottoTickets(final List<String> lottoTickets) {
-        System.out.println(lottoTickets.size() + TICKETS_COUNT_MESSAGE);
+        System.out.println(lottoTickets.size() + "개를 구매했습니다.");
         for (String lottoTicket : lottoTickets) {
             System.out.println(lottoTicket);
         }
         System.out.println();
     }
 
-    public static void printLottoGameResult(final Map<Rank, Integer> ranks) {
+    public static void printLottoGameResult(final LottoGameResult lottoGameResult) {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---------");
-        printWinningResult(ranks);
-    }
-
-    private static void printWinningResult(final Map<Rank, Integer> ranks) {
-        for (Entry<Rank, Integer> rank : ranks.entrySet()) {
-            System.out.format(rank.getKey().getMessageFormat(), rank.getValue());
-            System.out.println();
+        for (Entry<Rank, Integer> rank : lottoGameResult.getRanks().entrySet()) {
+            System.out.println(printLottoRank(rank.getKey(), rank.getValue()));
         }
     }
 
+    private static String printLottoRank(Rank rank, Integer count) {
+        if (rank.isBonusMatch()) {
+            return String.format(BONUS_RANK_PRINT_FORMAT,
+                rank.getMatchCount(), rank.getPrice(), count);
+        }
+        return String.format(DEFAULT_RANK_PRINT_FORMAT,
+            rank.getMatchCount(), rank.getPrice(), count);
+    }
+
     public static void printLottoGameYield(final double yield) {
-        System.out.format(AMOUNT_YIELD_FORMAT, yield);
+        System.out.format("총 수익률은 %.2f입니다.", yield);
         System.out.println();
     }
 }
