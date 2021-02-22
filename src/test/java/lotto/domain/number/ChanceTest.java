fa604282b369c1fdcd46f6b42129d0313a4c770e
Junroot <junroot0909@gmail.com>
2021-02-22 19:56:29 +0900

    test(lotto): 수동 로또 개수 입력 테스트

--- src/test/java/lotto/domain/number/ChanceTest.java
+++ src/test/java/lotto/domain/number/ChanceTest.java
@@ -1,10 +1,7 @@
 package lotto.domain.number;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import lotto.domain.number.Chance;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
