5674b4692fc3280dace7cbdfed4e23c1e91b6678
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-22 17:21:18 +0900

    refactor: LottoRank 필드의 new Integer로 새로 선언하던 부분 수정

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public enum LottoRank {
-    FIRST_PLACE(new Integer(6), 2000000000),
-    SECOND_PLACE(new Integer(5), 30000000),
-    THIRD_PLACE(new Integer(5), 1500000),
-    FOURTH_PLACE(new Integer(4), 50000),
-    FIFTH_PLACE(new Integer(3), 5000),
-    SIXTH_PLACE(new Integer(0), 0);
+    FIRST_PLACE(6, 2000000000),
+    SECOND_PLACE(5, 30000000),
+    THIRD_PLACE(5, 1500000),
+    FOURTH_PLACE(4, 50000),
+    FIFTH_PLACE(3, 5000),
+    SIXTH_PLACE(0, 0);
 
     private int matches;
     private int reward;
