9c4ec45a1781db7e48f1667598a953409deb6f41
hyeon9mak <jinha3507@gmail.com>
2021-02-22 14:54:08 +0900

    refactor(LottoTicketIssueMachine): fix constructor
    add parameter in constructor
    add validate min amount logic

--- src/main/java/lottogame/domain/machine/LottoTicketIssueMachine.java
+++ src/main/java/lottogame/domain/machine/LottoTicketIssueMachine.java
@@ -1,6 +1,8 @@
 package lottogame.domain.machine;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import lottogame.domain.Money;
 import lottogame.domain.number.LottoNumber;
@@ -10,25 +12,39 @@ import lottogame.domain.ticket.LottoTickets;
 public class LottoTicketIssueMachine {
 
     private static final int TICKET_PRICE = 1000;
-    public static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
-    private LottoTicketIssueMachine() {
+    private final Money money;
+
+    public LottoTicketIssueMachine(final Money money) {
+        this.money = money;
+        validateMinPurchaseAmount(this.money);
     }
 
-    public static LottoTickets issueTickets(final Money money) {
-        LottoTickets lottoTickets = new LottoTickets();
-        while (money.canBuyAmount(TICKET_PRICE)) {
-            money.spent(TICKET_PRICE);
+    private void validateMinPurchaseAmount(final Money money) {
+        if (!money.canBuyAmount(TICKET_PRICE)) {
+            throw new IllegalArgumentException("최소 입력 금액은 1000원 이상입니다.");
+        }
+    }
+
+    public LottoTickets issueTickets() {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        while (this.money.canBuyAmount(TICKET_PRICE)) {
+            this.money.spent(TICKET_PRICE);
             lottoTickets.add(new LottoTicket(issueNumbers()));
         }
-        return lottoTickets;
+        return new LottoTickets(lottoTickets);
     }
 
-    private static Set<LottoNumber> issueNumbers() {
+    private Set<LottoNumber> issueNumbers() {
         Set<LottoNumber> lottoNumbers = new HashSet<>();
         while (lottoNumbers.size() < LOTTO_NUMBER_COUNT) {
             lottoNumbers.add(new LottoNumber());
         }
         return lottoNumbers;
+    }
+
+    public static int getTicketPrice() {
+        return TICKET_PRICE;
     }
 }
--- src/test/java/lottogame/domain/machine/LottoTicketIssueMachineTest.java
+++ src/test/java/lottogame/domain/machine/LottoTicketIssueMachineTest.java
@@ -1,6 +1,7 @@
 package lottogame.domain.machine;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lottogame.domain.Money;
 import org.junit.jupiter.api.DisplayName;
@@ -10,12 +11,21 @@ public class LottoTicketIssueMachineTest {
 
     @Test
     @DisplayName("구입 금액에 해당하는 만큼 로또 티켓 발급")
-    void lottoTicketMachineInsertMoney() {
-        assertThat(LottoTicketIssueMachine.issueTickets(new Money(999))
-            .getLottoTickets().size()).isEqualTo(0);
-        assertThat(LottoTicketIssueMachine.issueTickets(new Money(1000))
+    void testInsertMoney() {
+        assertThat(new LottoTicketIssueMachine(new Money(1000)).issueTickets()
             .getLottoTickets().size()).isEqualTo(1);
-        assertThat(LottoTicketIssueMachine.issueTickets(new Money(3000))
-            .getLottoTickets().size()).isEqualTo(3);
+        assertThat(new LottoTicketIssueMachine(new Money(1999)).issueTickets()
+            .getLottoTickets().size()).isEqualTo(1);
+        assertThat(new LottoTicketIssueMachine(new Money(2000)).issueTickets()
+            .getLottoTickets().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("입력된 금액이 최소 구입 금액보다 적을시 예외처리")
+    void testMinPurchaseAmountException() {
+        assertThatThrownBy(()->new LottoTicketIssueMachine(new Money(0)))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->new LottoTicketIssueMachine(new Money(999)))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
