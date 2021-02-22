8b34e11ca3371c90329c1ae34f3f2acfa9afd840
Sehwan_Jang <whilter08@gmail.com>
2021-02-22 16:38:51 +0900

    feat : 자동로또 개수가 돈의 범위를 초과하는 오류 검증 기능 구현

--- src/main/java/lotto/domain/ManualLottoAmount.java
+++ src/main/java/lotto/domain/ManualLottoAmount.java
@@ -5,16 +5,29 @@ import lotto.exception.IllegalManualLottoAmountException;
 import java.util.regex.Pattern;
 
 public class ManualLottoAmount {
+    private static final Pattern NUMBER_FORMAT = Pattern.compile("^[0-9]*$");
+
     private final int value;
 
-    public ManualLottoAmount(String amount, int autoLottoCount) {
-        validateNumberByFormat(amount);
-        this.value = Integer.parseInt(amount);
+    public ManualLottoAmount(String input, int purchasableLottoCount) {
+        validateManualLottoAmount(input, purchasableLottoCount);
+        this.value = Integer.parseInt(input);
     }
 
-    private void validateNumberByFormat(String amount) {
-        if (!Pattern.compile("^[0-9]*$").matcher(amount).matches()) {
+    private void validateManualLottoAmount(String amount, int purchasableLottoCount) {
+        validateInputByFormat(amount);
+        validateInputByMoney(amount, purchasableLottoCount);
+    }
+
+    private void validateInputByFormat(String amount) {
+        if (!NUMBER_FORMAT.matcher(amount).matches()) {
             throw new IllegalManualLottoAmountException(amount + " : 올바른 형식이 아닙니다.");
+        }
+    }
+
+    private void validateInputByMoney(String amount, int purchasableLottoCount) {
+        if (Integer.parseInt(amount) > purchasableLottoCount) {
+            throw new IllegalManualLottoAmountException(amount + " : 소지금으로 구매할 수 있는 범위를 초과합니다.");
         }
     }
 
