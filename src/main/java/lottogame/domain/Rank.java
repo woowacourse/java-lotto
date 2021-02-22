7d40936587c94f9712b70e2228096d2fd33c186d
hyeon9mak <jinha3507@gmail.com>
2021-02-22 14:48:02 +0900

    refactor(Rank): delete print message format

--- src/main/java/lottogame/domain/Rank.java
+++ src/main/java/lottogame/domain/Rank.java
@@ -1,42 +1,50 @@
 package lottogame.domain;
 
 import java.util.Arrays;
-import lottogame.domain.ticket.LottoWinTicket;
 
 public enum Rank {
-    FAIL(0, false, 0, "0개 일치 (0원)- %d개"),
-    RANK5(3, false, 5_000, "3개 일치 (5000원)- %d개"),
-    RANK4(4, false, 50_000, "4개 일치 (50000원)- %d개"),
-    RANK3(5, false, 1_500_000, "5개 일치 (1500000원)- %d개"),
-    RANK2(5, true, 30_000_000, "5개 일치, 보너스 볼 일치(30000000원) - %d개"),
-    RANK1(6, false, 2_000_000_000, "6개 일치 (2000000000원)- %d개");
+    FAIL(0, false, 0),
+    RANK5(3, false, 5_000),
+    RANK4(4, false, 50_000),
+    RANK3(5, false, 1_500_000),
+    RANK2(5, true, 30_000_000),
+    RANK1(6, false, 2_000_000_000);
 
     private final int matchCount;
     private final boolean isBonusMatch;
     private final int price;
-    private final String messageFormat;
 
-    Rank(final int matchCount, final boolean isBonusMatch, final int price,
-        final String messageFormat) {
+    Rank(final int matchCount, final boolean isBonusMatch, final int price) {
         this.matchCount = matchCount;
         this.isBonusMatch = isBonusMatch;
         this.price = price;
-        this.messageFormat = messageFormat;
     }
 
-    public static Rank getRank(final LottoWinTicket lottoWinTicket) {
+    public static Rank of(final int matchCount, final boolean isBonusMatch) {
         return Arrays.stream(values())
-            .filter(rank -> lottoWinTicket.isSameMatchCount(rank.matchCount))
-            .filter(rank -> lottoWinTicket.isSameBonusMatch(rank.isBonusMatch))
+            .filter(rank -> rank.isSameMatchCount(matchCount))
+            .filter(rank -> rank.isSameBonusMatch(isBonusMatch))
             .findAny()
             .orElse(FAIL);
     }
 
-    public int getPrice() {
-        return this.price;
+    private boolean isSameMatchCount(final int matchCount) {
+        return this.matchCount == matchCount;
     }
 
-    public String getMessageFormat() {
-        return this.messageFormat;
+    private boolean isSameBonusMatch(boolean isBonusMatch) {
+        return this.isBonusMatch == isBonusMatch;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return isBonusMatch;
+    }
+
+    public int getPrice() {
+        return this.price;
     }
 }
--- src/test/java/lottogame/domain/RankTest.java
+++ src/test/java/lottogame/domain/RankTest.java
@@ -1,21 +1,19 @@
 package lottogame.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import lottogame.domain.ticket.LottoWinTicket;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RankTest {
 
     @Test
-    @DisplayName("등수 확인 테스트")
-    void getPrice() {
-        assertThat(Rank.getRank(new LottoWinTicket(6, false))).isEqualTo(Rank.RANK1);
-        assertThat(Rank.getRank(new LottoWinTicket(5, true))).isEqualTo(Rank.RANK2);
-        assertThat(Rank.getRank(new LottoWinTicket(5, false))).isEqualTo(Rank.RANK3);
-        assertThat(Rank.getRank(new LottoWinTicket(4, false))).isEqualTo(Rank.RANK4);
-        assertThat(Rank.getRank(new LottoWinTicket(3, false))).isEqualTo(Rank.RANK5);
-        assertThat(Rank.getRank(new LottoWinTicket(2, false))).isEqualTo(Rank.FAIL);
+    @DisplayName("맞춘 개수별 등수 확인")
+    void testGetPrice() {
+        assertThat(Rank.of(6, false)).isEqualTo(Rank.RANK1);
+        assertThat(Rank.of(5, true)).isEqualTo(Rank.RANK2);
+        assertThat(Rank.of(5, false)).isEqualTo(Rank.RANK3);
+        assertThat(Rank.of(4, false)).isEqualTo(Rank.RANK4);
+        assertThat(Rank.of(3, false)).isEqualTo(Rank.RANK5);
     }
 }
