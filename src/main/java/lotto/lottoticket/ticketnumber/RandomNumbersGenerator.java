634d8bb7a2af94f0378246e8966f852e1c79733a
y_woo :) <dusdn1702@naver.com>
2021-02-22 18:54:19 +0900

    refactor: 1~45 상수 리스트 상수화 취소
    스레드 안전을 위해서

--- src/main/java/lotto/lottoticket/ticketnumber/RandomNumbersGenerator.java
+++ src/main/java/lotto/lottoticket/ticketnumber/RandomNumbersGenerator.java
@@ -13,26 +13,27 @@ public class RandomNumbersGenerator implements NumbersGenerator {
     public static final int MAXIMUM_NUMBER = 45;
     public static final int NUMBER_COUNT_IN_LOTTO = 6;
 
-    private static final List<Integer> NUMBERS_IN_RANGE = IntStream.rangeClosed(MINIMUM_NUMBER, MAXIMUM_NUMBER)
-            .boxed()
-            .collect(Collectors.toList());
-
     @Override
     public List<LottoNumber> generate() {
+        final List<Integer> numbers = IntStream.rangeClosed(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
+        Collections.shuffle(numbers);
+        return makeLottoNumbers(makeAscendingNumbers(numbers));
+    }
+
+    private List<Integer> makeAscendingNumbers(List<Integer> numbers) {
         List<Integer> randomNumbers = new ArrayList<>();
-        Collections.shuffle(NUMBERS_IN_RANGE);
         for (int i = 0; i < NUMBER_COUNT_IN_LOTTO; i++) {
-            randomNumbers.add(NUMBERS_IN_RANGE.get(i));
+            randomNumbers.add(numbers.get(i));
         }
         Collections.sort(randomNumbers);
-        return makeLottoNumbers(randomNumbers);
+        return randomNumbers;
     }
 
     private List<LottoNumber> makeLottoNumbers(List<Integer> randomNumbers) {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (Integer number : randomNumbers) {
-            lottoNumbers.add(new LottoNumber(number));
-        }
-        return lottoNumbers;
+        return randomNumbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 }
