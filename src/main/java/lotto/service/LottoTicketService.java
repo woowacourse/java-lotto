25eeeff910e338c8d4a98cefc7326aff10b0b182
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-22 17:31:59 +0900

    refactor: LottoTicketService에서 createLottoTicket 메서드 수정

--- src/main/java/lotto/service/LottoTicketService.java
+++ src/main/java/lotto/service/LottoTicketService.java
@@ -21,12 +21,10 @@ public class LottoTicketService {
     public static LottoTicket createLottoTicket() {
         List<LottoNumber> lottoNumbers = LottoNumber.getCache();
         Collections.shuffle(lottoNumbers);
-        return new LottoTicket(
-                lottoNumbers
-                        .stream()
-                        .limit(6)
-                        .sorted()
-                        .collect(Collectors.toList()));
+        return lottoNumbers.stream()
+                .limit(LOTTO_NUMBER_COUNT)
+                .sorted()
+                .collect(Collectors.collectingAndThen(Collectors.toList(), LottoTicket::new));
     }
 
     public static LottoWinnerTicket createLottoWinnerTicket(String input) {
