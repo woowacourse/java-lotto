92f97375432f52ef0b742e346fae2a2a02cccbbd
TaewanKimmmm <ktww88@gmail.com>
2021-02-22 09:40:57 +0900

    refactor: 불필요한 getter 제거

--- src/main/java/lotto/type/LottoMatchType.java
+++ src/main/java/lotto/type/LottoMatchType.java
@@ -5,30 +5,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum LottoMatchType {
-    THREE_MATCH(3, 5000,
-        "3개 일치 (5000원) - %d개"),
-    FOUR_MATCH(4, 50000,
-        "4개 일치 (50000원) - %d개"),
-    FIVE_MATCH(5, 1500000,
-        "5개 일치 (1500000원) - %d개"),
-    FIVE_AND_BONUS_MATCH(5, 30000000,
-        "5개 일치, 보너스 볼 일치(30000000원) - %d개"),
-    SIX_MATCH(6, 2000000000,
-        "6개 일치 (2000000000원) - %d개");
+    THREE_MATCH(3, 5_000,
+            "3개 일치 (5000원) - %d개"),
+    FOUR_MATCH(4, 50_000,
+            "4개 일치 (50000원) - %d개"),
+    FIVE_MATCH(5, 1_500_000,
+            "5개 일치 (1500000원) - %d개"),
+    FIVE_AND_BONUS_MATCH(5, 30_000_000,
+            "5개 일치, 보너스 볼 일치(30000000원) - %d개"),
+    SIX_MATCH(6, 2_000_000_000,
+            "6개 일치 (2000000000원) - %d개");
 
     private final int countMatchedNumbers;
     private final int prizeMoney;
     private final String matchCountMessage;
 
     LottoMatchType(int countMatchedNumbers, int prizeMoney,
-        String matchCountMessage) {
+                   String matchCountMessage) {
         this.countMatchedNumbers = countMatchedNumbers;
         this.prizeMoney = prizeMoney;
         this.matchCountMessage = matchCountMessage;
-    }
-
-    public int getCountMatchedNumbers() {
-        return countMatchedNumbers;
     }
 
     public int getPrizeMoney() {
@@ -41,8 +37,7 @@ public enum LottoMatchType {
 
     public static List<LottoMatchType> getLottoMatchType(int countMatchedNumbers) {
         return Arrays.stream(LottoMatchType.values())
-            .filter(
-                lottoMatchType -> lottoMatchType.getCountMatchedNumbers() == countMatchedNumbers)
-            .collect(Collectors.toList());
+                .filter(lottoMatchType -> lottoMatchType.countMatchedNumbers == countMatchedNumbers)
+                .collect(Collectors.toList());
     }
 }
