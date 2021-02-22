edd8fdd6069185ab7ab32190cd7d21ca682530f1
sor <bosl95@naver.com>
2021-02-23 02:50:11 +0900

    refactor(lottomachine) : lottonumber 수정에 관련 기능 리팩토링

--- src/main/java/lottogame/domain/LottoMachine.java
+++ src/main/java/lottogame/domain/LottoMachine.java
@@ -2,6 +2,7 @@ package lottogame.domain;
 
 import lottogame.domain.lotto.Lotto;
 import lottogame.domain.lotto.LottoNumber;
+import lottogame.domain.lotto.LottoNumberGenerator;
 import lottogame.utils.CannotBuyLottoException;
 
 import java.util.ArrayList;
@@ -9,6 +10,11 @@ import java.util.List;
 
 public class LottoMachine {
     private static final int LOTTO_PRICE = 1000;
+    private final LottoNumberGenerator numberGenerator;
+
+    public LottoMachine() {
+        numberGenerator = new LottoNumberGenerator(LottoNumber.LOTTO_MIN, LottoNumber.LOTTO_MAX);
+    }
 
     public int purchaseQuantity(Money money) {
         int quantity = money.buyLotto(LOTTO_PRICE);
@@ -27,6 +33,24 @@ public class LottoMachine {
     }
 
     private Lotto makeLotto() {
-        return new Lotto(new LottoNumber());
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        while (lottoNumbers.size() < Lotto.LOTTO_NUMBER_VOLUME) {
+            makeRandomNumber(lottoNumbers);
+        }
+        return new Lotto(lottoNumbers);
+    }
+
+    private void makeRandomNumber(List<LottoNumber> lottoNumbers) {
+        int random = numberGenerator.generate();
+        if (isPossible(random, lottoNumbers)) {
+            lottoNumbers.add(new LottoNumber(random));
+        }
+    }
+
+    private boolean isPossible(int random, List<LottoNumber> lottoNumbers) {
+        return !lottoNumbers.stream()
+                .filter(lottoNumber -> lottoNumber.equals(random))
+                .findAny()
+                .isPresent();
     }
 }
--- src/test/java/lottogame/domain/LottoMachineTest.java
+++ src/test/java/lottogame/domain/LottoMachineTest.java
@@ -1,12 +1,18 @@
 package lottogame.domain;
 
+import lottogame.domain.lotto.Lotto;
+import lottogame.domain.lotto.LottoNumber;
 import lottogame.utils.CannotBuyLottoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoMachineTest {
@@ -32,5 +38,24 @@ class LottoMachineTest {
     void 로또_구매_기능_테스트(int value, int expected) {
         Money money = new Money(value);
         assertEquals(expected, lottoMachine.purchaseQuantity(money));
+    }
+
+    @DisplayName("로또가 정상 구매 되는 지 확인")
+    @Test
+    void 로또_구매_기능_테스트() {
+        List<Lotto> lottos = lottoMachine.buyLotto(4);
+        assertThat(lottos).hasSize(4);
+    }
+
+    @DisplayName("로또 생성 시 중복 숫자를 방지하는 기능이 잘 동작하는 지 확인")
+    @Test
+    void 로또_생성_기능_테스트() {
+        List<Lotto> lottos = lottoMachine.buyLotto(3);
+        assertThat(lottos).hasSize(3);
+
+        for (Lotto lotto : lottos) {
+            List<LottoNumber> lottoNumbers = lotto.values();
+            assertThat(lottoNumbers.stream().distinct().count()).isEqualTo(lottoNumbers.size());
+        }
     }
 }
