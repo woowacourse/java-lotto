d2c6f89b47f4c41107eabdc6ac8269c5817e29bc
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-23 23:43:54 +0900

    refactor(ManualTicketsSizeTest): ManualTicketsSize 생성 및 사이즈 관련 함수 테스트 리팩토링

--- src/test/java/lotto/domain/ticketpurchase/ManualTicketsSizeTest.java
+++ src/test/java/lotto/domain/ticketpurchase/ManualTicketsSizeTest.java
@@ -1,43 +1,90 @@
 package lotto.domain.ticketpurchase;
 
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
+import lotto.domain.exception.InvalidManualTicketsSizeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ManualTicketsSizeTest {
-    @DisplayName("0개 이상, 총 구매 티켓 개수 이하의 티켓을 수동구매할 때 정상")
+    @DisplayName("수동구매 티켓 개수가 0개 이상, 총 구매 티켓 개수 이하일 때 정상")
     @ParameterizedTest
     @ValueSource(ints = {0, 5, 10})
     void Should_Not_ThrowException_When_ManualTicketsSizeIsValid(int manualTicketsSize) {
         PurchasePrice purchasePrice = new PurchasePrice(10_000);
 
-        Assertions.assertThatCode(
+        assertThatCode(
             () -> new ManualTicketsSize(manualTicketsSize, purchasePrice)
         ).doesNotThrowAnyException();
     }
 
-    @DisplayName("0개 미만의 티켓을 수동구매할 때 예외 발생")
+    @DisplayName("수동구매 티켓 개수가 0개 미만일 때 예외 발생")
     @Test
     void Should_ThrowException_When_ManualTicketsSizeIsLessThanZero() {
         int manualTicketsSize = -1;
-        PurchasePrice purchasePrice = new PurchasePrice(1000);
+        PurchasePrice purchasePrice = new PurchasePrice(1_000);
 
-        Assertions.assertThatThrownBy(
+        assertThatThrownBy(
             () -> new ManualTicketsSize(manualTicketsSize, purchasePrice)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(InvalidManualTicketsSizeException.class);
     }
 
-    @DisplayName("총 구매 티켓 개수 초과의 티켓을 수동구매할 때 예외 발생")
+    @DisplayName("수동구매 티켓 개수가 총 구매 티켓 개수보다 클 때 예외 발생")
     @Test
-    void Should_ThrowException_When_ManualTicketsSizeIsOverThanAllTickets() {
+    void Should_ThrowException_When_ManualTicketsSizeIsGreaterThanAllTickets() {
         int manualTicketsSize = 2;
-        PurchasePrice purchasePrice = new PurchasePrice(1000);
+        PurchasePrice purchasePrice = new PurchasePrice(1_000);
 
-        Assertions.assertThatThrownBy(
+        assertThatThrownBy(
             () -> new ManualTicketsSize(manualTicketsSize, purchasePrice)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(InvalidManualTicketsSizeException.class);
+    }
+
+    @DisplayName("수동구매 티켓 개수 확인 테스트")
+    @Test
+    void Should_Return_ExpectedSize_When_GetSize() {
+        // given
+        int manualTicketsSizeInput = 2;
+        PurchasePrice purchasePrice = new PurchasePrice(5_000);
+
+        // when
+        ManualTicketsSize manualTicketsSize
+            = new ManualTicketsSize(manualTicketsSizeInput, purchasePrice);
+
+        // then
+        assertThat(manualTicketsSize.size()).isEqualTo(manualTicketsSizeInput);
+    }
+
+    @DisplayName("수동구매 티켓이 0개일 때")
+    @Test
+    void Should_Return_False_When_ManualTicketsSizeZero() {
+        // given
+        int manualTicketsSizeInput = 0;
+        PurchasePrice purchasePrice = new PurchasePrice(5_000);
+
+        // when
+        ManualTicketsSize manualTicketsSize
+            = new ManualTicketsSize(manualTicketsSizeInput, purchasePrice);
+
+        // then
+        assertThat(manualTicketsSize.isNotZero()).isFalse();
+    }
+
+    @DisplayName("수동구매 티켓이 0개가 아닐 때")
+    @Test
+    void Should_Return_True_When_ManualTicketsSizeNotZero() {
+        // given
+        int manualTicketsSizeInput = 1;
+        PurchasePrice purchasePrice = new PurchasePrice(5_000);
+
+        // when
+        ManualTicketsSize manualTicketsSize
+            = new ManualTicketsSize(manualTicketsSizeInput, purchasePrice);
+
+        // then
+        assertThat(manualTicketsSize.isNotZero()).isTrue();
     }
 }
