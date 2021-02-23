815dde128ee5118d5f7a23e7e44a5a5009839bba
김주원 <kjw11077@naver.com>
2021-02-23 22:47:44 +0900

    feat: InputView 안내메시지 출력 형태 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,27 +9,27 @@ public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static String getMoneyInput() {
-        System.out.println("구입금액을 입력해 주세요.");
+        printNotice("구입금액을 입력해 주세요.");
         return SCANNER.nextLine();
     }
 
     public static String getWinningNumbersInput() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        printNotice("지난 주 당첨 번호를 입력해 주세요.");
         return SCANNER.nextLine();
     }
 
     public static String getBonusBallInput() {
-        System.out.println("보너스 볼을 입력해 주세요.");
+        printNotice("보너스 볼을 입력해 주세요.");
         return SCANNER.nextLine();
     }
 
     public static String getManuallyBuyAmountInput() {
-        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        printNotice("수동으로 구매할 로또 수를 입력해 주세요.");
         return SCANNER.nextLine();
     }
 
     public static List<String> getManualLottoTicketsInput(int manuallyBuyCount) {
-        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        printNotice("수동으로 구매할 번호를 입력해 주세요.");
 
         List<String> lottoTicketsInput = new ArrayList<>();
         for (int i = 0; i < manuallyBuyCount; i++) {
@@ -37,5 +37,9 @@ public class InputView {
         }
 
         return lottoTicketsInput;
+    }
+
+    private static void printNotice(String notice) {
+        System.out.println("\n" + notice);
     }
 }
