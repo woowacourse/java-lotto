6493f605692601cd2810290f1d126c1b741efbfc
mungto <daum7766@naver.com>
2021-02-23 16:33:14 +0900

    refactor: LottoTickets 입력받는 구조 변경

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -14,23 +14,29 @@ public class LottoTickets {
     private static final int END_LOTTO_NUMBER = 45;
     private static final int LOTTO_SIZE = 6;
     private static final List<Integer> ALL_LOTTO_NUMBERS = IntStream
-        .range(START_LOTTO_NUMBER, END_LOTTO_NUMBER + 1).boxed().collect(Collectors.toList());
+        .range(START_LOTTO_NUMBER, END_LOTTO_NUMBER + 1)
+        .boxed()
+        .collect(Collectors.toList());
 
     private final List<Lotto> lottoTickets;
 
     public LottoTickets(final int buyLottoSize) {
-        this(buyLottoSize, ALL_LOTTO_NUMBERS);
+        this(buyLottoSize, new ArrayList<>());
     }
 
-    public LottoTickets(final int buyLottoSize, final List<Integer> LottoNumbers) {
-        lottoTickets = createLottoTickets(buyLottoSize, LottoNumbers);
+    public LottoTickets(final int buyLottoSize, final List<List<Integer>> lottoNumbers) {
+        lottoTickets = createLottoTickets(buyLottoSize, lottoNumbers);
     }
 
-    private List<Lotto> createLottoTickets(final int buyLottoSize, final List<Integer> LottoNumbers) {
-        List<Lotto> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < buyLottoSize; i++) {
-            lottoTickets
-                .add(new Lotto(RandomUtils.generateRandomNumbers(LottoNumbers, LOTTO_SIZE)));
+    private List<Lotto> createLottoTickets(final int buyLottoSize, final List<List<Integer>> lottoNumbers) {
+        final List<Lotto> lottoTickets = new ArrayList<>();
+        for (List<Integer> numbers : lottoNumbers) {
+            lottoTickets.add(new Lotto(numbers));
+        }
+        for (int i = 0; i < buyLottoSize - lottoNumbers.size(); i++) {
+            lottoTickets.add(
+                new Lotto(RandomUtils.generateRandomNumbers(ALL_LOTTO_NUMBERS, LOTTO_SIZE))
+            );
         }
         return lottoTickets;
     }
--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -17,10 +17,12 @@ public class LottoTicketsTest {
         assertThat(lottoTickets.getLottoTickets().size()).isEqualTo(14);
     }
 
-    @DisplayName("같은 번호가 생성되었는지 테스트")
+    @DisplayName("같은 번호가 생성되었는지 테스트(정렬")
     @Test
     void numberSort() {
-        LottoTickets lottoTickets = new LottoTickets(1, Arrays.asList(2, 4, 6, 5, 3, 1));
+        List<List<Integer>> numbers = new ArrayList<>();
+        numbers.add(Arrays.asList(2, 4, 6, 5, 3, 1));
+        LottoTickets lottoTickets = new LottoTickets(1, numbers);
         List<Lotto> expectedLottoTickets = new ArrayList<>();
 
         expectedLottoTickets.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
