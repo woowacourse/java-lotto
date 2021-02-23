9541470178119930e64701d33bb4c1de916360c1
RinSabbit <celena1004@naver.com>
2021-02-23 20:18:06 +0900

    feat: 티켓에 찾는 번호가 있는지 검사하는 기능 TC 구현

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -16,4 +16,14 @@ public class LottoTicketTest {
         LottoTicket lottoTicket = new LottoTicket(numbers);
         assertThat(lottoTicket).isEqualTo(new LottoTicket(numbers));
     }
+
+    @DisplayName("티켓에 찾는 번호가 있으면 참을 반환한다.")
+    @Test
+    public void contain(){
+        LottoTicket lottoTicket = new LottoTicket(Arrays.asList(1,2,3,4,5,6));
+        LottoNumber number = new LottoNumber(1);
+        LottoNumber excludedNumber = new LottoNumber(8);
+        assertThat(lottoTicket.hasNumber(number)).isTrue();
+        assertThat(lottoTicket.hasNumber(excludedNumber)).isFalse();
+    }
 }
