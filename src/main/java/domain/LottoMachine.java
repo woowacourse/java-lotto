e97b43086026a4eaf9ba2bcc62d4b34eabddb91c
daeun <leede0418@likelion.org>
2021-02-23 16:36:51 +0900

    style(lotto-machine): final static을 static final로 수정

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
-    private final static int PRICE = 1000;
+    private static final int PRICE = 1000;
 
     public static List<LottoTicket> buy(Money money) {
         List<LottoTicket> lottoTickets = new ArrayList<>();
