33f4d47f3cd7b73bced2c8991049de03fd78d2ff
김주원 <kjw11077@naver.com>
2021-02-23 20:28:53 +0900

    feat: InputView 수동 티켓 입력 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -19,5 +21,21 @@ public class InputView {
     public static String getBonusBallInput() {
         System.out.println("보너스 볼을 입력해 주세요.");
         return SCANNER.nextLine();
+    }
+
+    public static String getManuallyBuyAmountInput() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return SCANNER.nextLine();
+    }
+
+    public static List<String> getManualLottoTicketsInput(int manuallyBuyCount) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+
+        List<String> lottoTicketsInput = new ArrayList<>();
+        for (int i = 0; i < manuallyBuyCount; i++) {
+            lottoTicketsInput.add(SCANNER.nextLine());
+        }
+
+        return lottoTicketsInput;
     }
 }
