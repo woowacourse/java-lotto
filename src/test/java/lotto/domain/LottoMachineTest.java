1edeff68010e92a1c894b81412df9426a749795a
lns13301 <rkdrbgks22@ks.ac.kr>
2021-02-23 18:36:05 +0900

    test: LottoMachineTest 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -2,7 +2,7 @@ package lotto.domain;
 
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoMachine;
-import lotto.domain.lotto.Lottos;
+import lotto.domain.lotto.WinningLotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,5 +32,21 @@ public class LottoMachineTest {
 
         assertThat(lottoMachine.createAndPutLotto(lotto, numbers))
                 .isEqualTo(Collections.singletonList(new Lotto(lottoNumbers)));
+    }
+
+    @DisplayName("로또 번호 숫자 리스트 생성 테스트")
+    @Test
+    void createLottoNumbers() {
+        String numbers = "1,2,3,4,5,45";
+        List<Integer> lottoNumbers = Arrays.asList(1,2,3,4,5,45);
+
+        assertThat(lottoMachine.createLottoNumbers(numbers)).isEqualTo(lottoNumbers);
+    }
+
+    @DisplayName("당첨 번호 생성 테스트")
+    @Test
+    void createWinningLotto() {
+        assertThat(lottoMachine.createWinningLotto(Arrays.asList(1,2,3,4,5,45), "44"))
+                .isEqualTo(new WinningLotto(Arrays.asList(1,2,3,4,5,45), 44));
     }
 }
