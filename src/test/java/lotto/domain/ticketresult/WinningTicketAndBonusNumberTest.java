783b330d58f1ab8794c06bb0afaca0cd2f6a039a
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-24 00:25:36 +0900

    test: WinningTicketAndBonusNumberTest 보완

--- src/test/java/lotto/domain/ticketresult/WinningTicketAndBonusNumberTest.java
+++ src/test/java/lotto/domain/ticketresult/WinningTicketAndBonusNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.domain.ticketresult;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningTicketAndBonusNumberTest {
+    private WinningTicketAndBonusNumber winningTicketAndBonusNumber;
     private LottoTicket winningTicket;
 
     @BeforeEach
@@ -26,6 +28,8 @@ public class WinningTicketAndBonusNumberTest {
                 LottoNumbers.get(6)
             )
         );
+        LottoNumber bonusNumber = LottoNumbers.get(7);
+        winningTicketAndBonusNumber = new WinningTicketAndBonusNumber(winningTicket, bonusNumber);
     }
 
     @DisplayName("우승 로또 번호 정상 생성")
@@ -44,5 +48,51 @@ public class WinningTicketAndBonusNumberTest {
         assertThatThrownBy(
             () -> new WinningTicketAndBonusNumber(winningTicket, bonusNumber)
         ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨된 번호들 반환 테스트 - 3개")
+    @Test
+    void Should_Return_Matched3LottoNumbers_When_Get() {
+        // given
+        LottoTicket lottoTicket = new LottoTicket(
+            Arrays.asList(
+                LottoNumbers.get(1),
+                LottoNumbers.get(2),
+                LottoNumbers.get(3),
+                LottoNumbers.get(11),
+                LottoNumbers.get(12),
+                LottoNumbers.get(13)
+            )
+        );
+
+        // when
+        MatchedLottoNumbers matchedLottoNumbers
+            = winningTicketAndBonusNumber.getMatchedLottoNumbers(lottoTicket);
+
+        // then
+        assertThat(matchedLottoNumbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("당첨된 번호들 반환 테스트 - 5개")
+    @Test
+    void Should_Return_Matched5LottoNumbers_When_Get() {
+        // given
+        LottoTicket lottoTicket = new LottoTicket(
+            Arrays.asList(
+                LottoNumbers.get(1),
+                LottoNumbers.get(2),
+                LottoNumbers.get(3),
+                LottoNumbers.get(4),
+                LottoNumbers.get(5),
+                LottoNumbers.get(13)
+            )
+        );
+
+        // when
+        MatchedLottoNumbers matchedLottoNumbers
+            = winningTicketAndBonusNumber.getMatchedLottoNumbers(lottoTicket);
+
+        // then
+        assertThat(matchedLottoNumbers.size()).isEqualTo(5);
     }
 }
