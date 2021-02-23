12dc1d26a6457005a18dc8db56f8034cb245518f
xrabcde <xrabcde@gmail.com>
2021-02-23 14:35:19 +0900

    refactor: BonusBall 변수명 리팩토링

--- src/main/java/lotto/domain/BonusBall.java
+++ src/main/java/lotto/domain/BonusBall.java
@@ -6,7 +6,7 @@ public class BonusBall {
     public static final String SAME_NUMBER_ERROR = "[ERROR] 보너스볼 숫자는 당첨번호와 중복될 수 없습니다";
     public static final int MAXIMUM_NUMBER = 45;
     public static final int MINIMUM_NUMBER = 1;
-    private int bonusBall;
+    private int bonusNumber;
 
     public BonusBall(Lotto winLotto, String input) {
         validateBonusBallType(input);
@@ -16,25 +16,25 @@ public class BonusBall {
 
     private void validateBonusBallType(String input) {
         try {
-            bonusBall = Integer.parseInt(input);
+            bonusNumber = Integer.parseInt(input);
         } catch (Exception e) {
             throw new IllegalArgumentException(NUMBER_TYPE_ERROR);
         }
     }
 
     private void validateBonusBallRange() {
-        if (bonusBall < MINIMUM_NUMBER || bonusBall > MAXIMUM_NUMBER) {
+        if (bonusNumber < MINIMUM_NUMBER || bonusNumber > MAXIMUM_NUMBER) {
             throw new IllegalArgumentException(NUMBER_RANGE_ERROR);
         }
     }
 
     private void validateDuplicate(Lotto winLotto) {
-        if (winLotto.isContainNum(bonusBall)) {
+        if (winLotto.isContainNum(bonusNumber)) {
             throw new IllegalArgumentException(SAME_NUMBER_ERROR);
         }
     }
 
     public boolean hasBonusBall(Lotto lotto) {
-        return lotto.isContainNum(bonusBall);
+        return lotto.isContainNum(bonusNumber);
     }
 }
