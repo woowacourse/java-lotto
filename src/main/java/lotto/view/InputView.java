17540a9eddc19d00fc22bbc20b5dd04188f982db
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-02-23 17:30:35 +0900

    feat: 수동 생성 관련 InputView 메소드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -8,6 +9,8 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_PURCHASING_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_MANUAL_TICKET_COUNTS_MESSAGE = "\n수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String INPUT_MANUAL_LOTTO_NUMBERS_MESSAGE = "\n수동으로 구매할 번호를 입력해 주세요.";
     private static final String INPUT_WINNING_TICKET_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String COMMA_DELIMITER = ", ";
@@ -34,5 +37,21 @@ public class InputView {
 
     public static int inputBonusBallNumber() {
         return nextIntWithInstructionMessage(INPUT_BONUS_BALL_NUMBER_MESSAGE);
+    }
+
+    public static int inputManualTicketCounts() {
+        System.out.println(INPUT_MANUAL_TICKET_COUNTS_MESSAGE);
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static List<List<Integer>> inputManualLottoNumbers(int manualTicketCounts) {
+        System.out.println(INPUT_MANUAL_LOTTO_NUMBERS_MESSAGE);
+        List<List<Integer>> numberGroup = new ArrayList<>();
+        for (int i = 0; i < manualTicketCounts; i++) {
+            numberGroup.add(Arrays.stream(SCANNER.nextLine().split(COMMA_DELIMITER, SPLIT_LIMIT_THRESHOLD))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList()));
+        }
+        return numberGroup;
     }
 }
