f3cb2f14d075ca3cab13ba5714980d1df59c625d
김주원 <kjw11077@naver.com>
2021-02-23 22:08:52 +0900

    feat: LottoApplication2 로또 수동생성 구현 위한 클래스 생성

---
+++ src/main/java/lotto/LottoApplication2.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import java.util.List;
+import lotto.domain.lotto.LottoMachine;
+import lotto.domain.lotto.LottoTicket;
+import lotto.domain.lotto.Money;
+import lotto.domain.result.LottoMatcher;
+import lotto.domain.result.Result;
+import lotto.utils.RandomLottoGenerator;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+import lotto.view.TicketsView;
+
+public class LottoApplication2 {
+
+    public static void main(String[] args) {
+        Money money = getMoneyByInput();
+
+        List<LottoTicket> lottoTickets = buyLottoTickets(money);
+        TicketsView.printTickets(lottoTickets);
+
+        Result result = getMatchedLottoResult(money, lottoTickets);
+        ResultView.printResult(result);
+    }
+
+    private static Money getMoneyByInput() {
+        try {
+            return Money.valueOf(InputView.getMoneyInput());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return getMoneyByInput();
+        }
+    }
+
+    private static List<LottoTicket> buyLottoTickets(Money money) {
+        LottoMachine lottoMachine = new LottoMachine(money);
+        return lottoMachine.buyTickets(new RandomLottoGenerator());
+    }
+
+    private static Result getMatchedLottoResult(Money money, List<LottoTicket> lottoTickets) {
+        try {
+            LottoMatcher lottoMatcher = new LottoMatcher(
+                    InputView.getWinningNumbersInput(),
+                    InputView.getBonusBallInput(), lottoTickets, money);
+            return lottoMatcher.getResult();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return getMatchedLottoResult(money, lottoTickets);
+        }
+    }
+}
