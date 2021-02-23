dd51900aa1f76c3c9244bde05824f95b4047599f
DWL5 <ssop6403@gmail.com>
2021-02-24 02:16:30 +0900

    feat: Money 에러 메시지 출력 하도록 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -3,28 +3,22 @@ package lotto.domain;
 import static lotto.utils.Validation.isNumeric;
 
 public class Money {
+    private static final String ERROR_MONEY_IS_NOT_NUMERIC = "[ERROR] 구입 금액은 숫자여야 합니다.";
+    private static final String ERROR_MONEY_IS_NOT_POSITIVE = "[ERROR] 구입 금액은 음수여서는 안됩니다.";
 
     private final int value;
 
     public Money(String moneyInput) {
         if (!isNumeric(moneyInput)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MONEY_IS_NOT_NUMERIC);
         }
 
         int money = Integer.parseInt(moneyInput);
         if (money < 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MONEY_IS_NOT_POSITIVE);
         }
 
         value = money;
-    }
-
-    public Money(int moneyInput) {
-        if (moneyInput < 0) {
-            throw new IllegalArgumentException();
-        }
-
-        value = moneyInput;
     }
 
     public int getValue() {
