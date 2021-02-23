c53e2065ae244dea04632cdcb37bd129d3f08e14
김주원 <kjw11077@naver.com>
2021-02-23 22:46:23 +0900

    feat: LottoMachine2 수동구매기능 추가

--- src/main/java/lotto/domain/lotto/LottoMachine2.java
+++ src/main/java/lotto/domain/lotto/LottoMachine2.java
@@ -3,22 +3,56 @@ package lotto.domain.lotto;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.result.UsersLottoTickets;
 import lotto.utils.LottoGenerator;
+import lotto.utils.RandomLottoGenerator;
 
 public class LottoMachine2 {
 
-    private final Money money;
+    private Money money;
+    private final LottoGenerator lottoGenerator;
 
-    public LottoMachine2(Money money) {
+    public LottoMachine2(Money money, LottoGenerator lottoGenerator) {
         this.money = money;
+        this.lottoGenerator = lottoGenerator;
     }
 
-    public List<LottoTicket> buyTickets(LottoGenerator lottoGenerator) {
+    public LottoMachine2(Money money) {
+        this(money, new RandomLottoGenerator());
+    }
+
+    public UsersLottoTickets buyTickets(List<String> manualTicketsValue) {
+        List<LottoTicket> manualTickets = getManualTickets(manualTicketsValue);
+        List<LottoTicket> autoTickets = getAutoTickets();
+
+        return new UsersLottoTickets(manualTickets, autoTickets);
+    }
+
+    private List<LottoTicket> getManualTickets(List<String> manualTicketsValue) {
+        List<LottoTicket> manualLottoTickets = manualTicketsValue.stream()
+                .map(LottoTicket::new)
+                .collect(Collectors.toList());
+
+        decreaseMoney(getPrice(manualLottoTickets));
+
+        return manualLottoTickets;
+    }
+
+    private BigInteger getPrice(List<LottoTicket> manualLottoTickets) {
+        return LottoTicket.PRICE.multiply(BigInteger.valueOf(manualLottoTickets.size()));
+    }
+
+    private void decreaseMoney(BigInteger amount) {
+        money = money.subtract(amount);
+    }
+
+    private List<LottoTicket> getAutoTickets() {
         ArrayList<LottoTicket> lottoTickets = new ArrayList<>();
 
-        for (BigInteger bi = getTicketCount();
-                bi.compareTo(BigInteger.ZERO) > 0;
-                bi = bi.subtract(BigInteger.ONE)) {
+        for (BigInteger ticketCount = getTicketCount();
+                ticketCount.compareTo(BigInteger.ZERO) > 0;
+                ticketCount = ticketCount.subtract(BigInteger.ONE)) {
 
             lottoTickets.add(lottoGenerator.generateLottoTicket());
         }
