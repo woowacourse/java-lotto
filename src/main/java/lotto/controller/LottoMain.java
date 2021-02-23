7eda8f686752262ab8a7ffdf36b50a78e4d47792
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-23 22:55:54 +0900

    refactor: 함수 내의 코드가 10줄이 넘어가지 않도록 리팩토링

--- src/main/java/lotto/controller/LottoMain.java
+++ src/main/java/lotto/controller/LottoMain.java
@@ -11,20 +11,23 @@ import lotto.view.OutputView;
 public class LottoMain {
     public static void main(String[] args) {
         UserPurchase userPurchase = InputView.getUserPurchase();
-        LottoTickets lottoTickets = new LottoTickets();
-        if (userPurchase.isPurchaseManually()) {
-            lottoTickets.addAll(InputView.purchaseManually(userPurchase));
-        }
-        LottoTicketsRandomGenerator lottoTicketsRandomGenerator = new LottoTicketsRandomGenerator();
-        if (userPurchase.isPurchaseRandomly()) {
-            lottoTickets.addAll(lottoTicketsRandomGenerator.generate(userPurchase));
-        }
+        LottoTickets lottoTickets = purchaseTickets(userPurchase);
         OutputView.printAllLottoTickets(lottoTickets, userPurchase);
-
         WinningTicketAndBonusNumber winningTicketAndBonusNumber
             = InputView.getWinningTicketAndBonusNumber();
         LottoComparator lottoComparator
             = new LottoComparator(winningTicketAndBonusNumber, userPurchase);
         OutputView.printResult(lottoComparator.getLottoResult(lottoTickets));
+    }
+
+    private static LottoTickets purchaseTickets(UserPurchase userPurchase) {
+        LottoTickets lottoTickets = new LottoTickets();
+        if (userPurchase.isPurchaseManually()) {
+            lottoTickets.addAll(InputView.purchaseManually(userPurchase));
+        }
+        if (userPurchase.isPurchaseRandomly()) {
+            lottoTickets.addAll(new LottoTicketsRandomGenerator().generate(userPurchase));
+        }
+        return lottoTickets;
     }
 }
