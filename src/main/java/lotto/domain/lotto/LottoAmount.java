5ab920d5f724a5b97487f81a9a481d32a40f3d3f
DWL5 <ssop6403@gmail.com>
2021-02-24 02:20:13 +0900

    refactor: LottoAmount 데이터 불변하게 수정

--- src/main/java/lotto/domain/lotto/LottoAmount.java
+++ src/main/java/lotto/domain/lotto/LottoAmount.java
@@ -6,7 +6,7 @@ public class LottoAmount {
     private static final String ERROR_LOTTO_AMOUNT_NOT_NUMERIC = "[ERROR] 로또 구매 수량은 숫자여야 합니다.";
     private static final String ERROR_LOTTO_AMOUNT_NEGATIVE = "[ERROR] 로또 구매 수량은 음수가 될 수 없습니다.";
 
-    private int value;
+    private final int value;
 
     public LottoAmount(String lottoAmountInput) {
         if (!isNumeric(lottoAmountInput)) {
