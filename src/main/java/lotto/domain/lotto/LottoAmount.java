175a72908e2f96838321d02018e6aed8e656393c
DWL5 <ssop6403@gmail.com>
2021-02-24 01:50:42 +0900

    feat: 로또 구매 갯수 LottoAmount 객체 추가

---
+++ src/main/java/lotto/domain/lotto/LottoAmount.java
@@ -0,0 +1,37 @@
+package lotto.domain.lotto;
+
+import static lotto.utils.Validation.isNumeric;
+
+public class LottoAmount {
+    private static final String ERROR_LOTTO_AMOUNT_NOT_NUMERIC = "[ERROR] 로또 구매 수량은 숫자여야 합니다.";
+    private static final String ERROR_LOTTO_AMOUNT_NEGATIVE = "[ERROR] 로또 구매 수량은 음수가 될 수 없습니다.";
+
+    private int value;
+
+    public LottoAmount(String lottoAmountInput) {
+        if (!isNumeric(lottoAmountInput)) {
+            throw new IllegalArgumentException();
+        }
+
+        int lottoAmount = Integer.parseInt(lottoAmountInput);
+        if (lottoAmount < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        value = lottoAmount;
+    }
+
+
+    public LottoAmount(int lottoAmount) {
+        if (lottoAmount < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        value = lottoAmount;
+    }
+
+
+    public int getValue() {
+        return value;
+    }
+}
