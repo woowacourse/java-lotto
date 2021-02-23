3becbf60feb6d61bfbb9268924cb0e6b2e3a2413
bepoz <kangsy763@naver.com>
2021-02-23 16:55:33 +0900

    refactor: 수동으로 구매할 로또 수를 입력한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,9 +8,15 @@ public class InputView {
     public static final String INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요";
     public static final String INPUT_BONUS_BALL_NUMBER = "보너스 볼을 입력해주세요";
     public static final String INPUT_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
+    public static final String INPUT_MANUAL_LOTTO_NUMBER = "수동으로 구매할 로또 수를 입력해 주세요.";
 
     public static int inputMoney() {
         OutputView.printInputMessage(INPUT_MONEY);
+        return scanner.nextInt();
+    }
+
+    public static int inputManualLottoCount() {
+        OutputView.printInputMessage(INPUT_MANUAL_LOTTO_NUMBER);
         return scanner.nextInt();
     }
 
