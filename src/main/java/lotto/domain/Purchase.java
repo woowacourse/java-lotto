a1e84ed6fc8e55b92e0a3c561e40e4a04a488222
joanne <mathmjseo@naver.com>
2021-02-23 16:32:49 +0900

    feat[Purchase]: 구매 개수 관련 Purchase 클래스 추가

---
+++ src/main/java/lotto/domain/Purchase.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import lotto.exception.LottoCustomException;
+
+public class Purchase {
+    private final Money money;
+    private final int autoPurchase;
+    private final int manualPurchase;
+
+    public Purchase(Money money, int manualPurchase) {
+        this.money = money;
+        validate(manualPurchase);
+        this.manualPurchase = manualPurchase;
+        this.autoPurchase = calculate(manualPurchase);
+    }
+
+    private int calculate(int manualPurchase) {
+        return money.getTotalPurchaseCount() - manualPurchase;
+    }
+
+    private void validate(int manualPurchase) {
+        if (money.isCountExceedLimit(manualPurchase)){
+            throw new LottoCustomException("수동으로 구매할 로또의 개수가 금액을 초과합니다.");
+        }
+    }
+
+}
