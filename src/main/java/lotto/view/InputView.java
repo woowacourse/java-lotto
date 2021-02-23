1ea57de5aea17a85b9bb35c62d1e6efc373fab27
mungto <daum7766@naver.com>
2021-02-23 16:36:34 +0900

    feat: 수동구매 관련 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,21 +6,29 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_MONEY_MESSAGE = "구매금액을 입력해 주세요.";
-    private static final String LOTTO_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String INPUT_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
 
     public static String getInputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
-        return SCANNER.nextLine();
+        String value = SCANNER.nextLine();
+        System.out.println();
+        return value;
     }
 
     public static String getLottoNumbers() {
-        System.out.println(LOTTO_NUMBERS_MESSAGE);
         return SCANNER.nextLine();
     }
 
     public static String getBonusBallNumber() {
         System.out.println(INPUT_BONUS_BALL_MESSAGE);
+        String value = SCANNER.nextLine();
+        System.out.println();
+        return value;
+    }
+
+    public static String getBuySelfLottoCount() {
+        System.out.println(INPUT_LOTTO_COUNT_MESSAGE);
         String value = SCANNER.nextLine();
         System.out.println();
         return value;
