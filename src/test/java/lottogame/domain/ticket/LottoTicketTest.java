da1d0b8231475498b52c2e024cabe2551f7c7a39
yoonsung.jung <ybook2012@gmail.com>
2021-02-23 17:16:09 +0900

    test(LottoTicketTest) : 로또 티켓의 타입이 맞는지 확인하는 기능

--- src/test/java/lottogame/domain/ticket/LottoTicketTest.java
+++ src/test/java/lottogame/domain/ticket/LottoTicketTest.java
@@ -1,10 +1,13 @@
 package lottogame.domain.ticket;
 
 import lottogame.domain.number.LottoNumber;
+import lottogame.domain.number.LottoNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,5 +29,19 @@ public class LottoTicketTest {
         LottoTicket lottoTicket = new LottoAutoTicket();
         Set<LottoNumber> lottoNumberSet = new HashSet<>(lottoTicket.getLottoNumbers());
         assertThat(lottoNumberSet.size()).isEqualTo(lottoTicket.getLottoNumbers().size());
+    }
+
+    @Test
+    @DisplayName("티켓을 뽑고 맞는 타입인지 확인하면 옳은 결과를 가져온다.")
+    void checkTicketType() {
+        LottoTicket lottoAutoTicket = new LottoAutoTicket();
+        assertThat(lottoAutoTicket.isAutoTicket()).isTrue();
+
+        List<LottoNumber> lottoNumberGroup = new ArrayList<>();
+        for (int i = 1; i <= 6; ++i) {
+            lottoNumberGroup.add(new LottoNumber(i));
+        }
+        LottoManualTicket lottoManualTicket = new LottoManualTicket(new LottoNumbers(lottoNumberGroup));
+        assertThat(lottoManualTicket.isAutoTicket()).isFalse();
     }
 }
