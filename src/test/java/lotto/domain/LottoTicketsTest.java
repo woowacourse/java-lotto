032086e77ce43d713ea3dd715fa313bf87f31964
RinSabbit <celena1004@naver.com>
2021-02-23 20:17:42 +0900

    feat: 생성된 로또티켓들을 lottotickets에 추가하는 기능 TC 구현

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -2,24 +2,46 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoTicketsTest {
 
-    @DisplayName("로또 티켓들 생성하기")
-    @Test
-    public void create() {
+    private LottoTickets lottoTickets;
+
+    @BeforeEach
+    void init() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         List<Integer> numbers2 = Arrays.asList(1, 2, 3, 4, 5, 7);
 
-        LottoTickets lottoTickets = new LottoTickets(Arrays.asList
+        lottoTickets = new LottoTickets(Arrays.asList
             (new LottoTicket(numbers),
                 new LottoTicket(numbers2)));
-
-        assertThat(lottoTickets.getLottoTickets())
-            .contains(new LottoTicket(numbers), new LottoTicket(numbers2));
     }
+
+    @DisplayName("로또 티켓들 생성하기")
+    @Test
+    public void create() {
+        assertThat(lottoTickets.getLottoTickets())
+            .contains(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 7)));
+    }
+
+    @DisplayName("새 로또 티켓(들)을 생성하면 기존 로또티켓 목록에 추가한다.")
+    @Test
+    void add() {
+        List<LottoTicket> moreTickets = new ArrayList<>(
+            Collections.singletonList(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 8))));
+        lottoTickets.addTickets(moreTickets);
+        assertThat(lottoTickets.getLottoTickets())
+            .contains(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 7)),
+                new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 8)));
+    }
+
 }
