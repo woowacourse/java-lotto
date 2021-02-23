0178685b5128cd8659127d884355c46332f7092b
mungto <daum7766@naver.com>
2021-02-23 14:31:01 +0900

    feat: 문자열을 IntegerList로 바꿔주는 기능 추가

--- src/main/java/lotto/utils/ParseUtils.java
+++ src/main/java/lotto/utils/ParseUtils.java
@@ -1,5 +1,8 @@
 package lotto.utils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import lotto.exception.IllegalTypeException;
 
 public class ParseUtils {
@@ -7,6 +10,17 @@ public class ParseUtils {
     public static int parseInt(String value) {
         try {
             return Integer.parseInt(value);
+        } catch (Exception e) {
+            throw new IllegalTypeException();
+        }
+    }
+
+    public static List<Integer> parseIntegerList(String value, String regex) {
+        try {
+            return Arrays.stream(value.split(regex)).
+                mapToInt(number -> Integer.parseInt(number))
+                .boxed()
+                .collect(Collectors.toList());
         } catch (Exception e) {
             throw new IllegalTypeException();
         }
