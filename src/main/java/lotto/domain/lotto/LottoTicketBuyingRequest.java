8a6591f88ea6181ae57bac472d6d88bd14c352a9
DWL5 <ssop6403@gmail.com>
2021-02-24 01:51:42 +0900

    refactor: LottoAmount에서 구매 수량에 관한 validation 하도록 변경

--- src/main/java/lotto/domain/lotto/LottoTicketBuyingRequest.java
+++ src/main/java/lotto/domain/lotto/LottoTicketBuyingRequest.java
@@ -2,32 +2,43 @@ package lotto.domain.lotto;
 
 import lotto.domain.Money;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.domain.lotto.LottoTicketGenerator.PRICE_EACH_LOTTO;
 
+
 public class LottoTicketBuyingRequest {
-    private static final String ERROR_MANUAL_LOTTO_PRICE_OVER = "총 구입 금액보다 수동구입량이 많습니다.";
+    private static final String ERROR_MANUAL_LOTTO_PRICE_OVER = "[ERROR] 총 구입 금액보다 수동구입량이 많습니다.";
+
     private Money money;
-    private int numberOfManualLotto;
+    private LottoAmount manualLottoAmount;
     private List<LottoLine> manualLottoLineList;
 
-    public LottoTicketBuyingRequest(Money money, int numberOfManualLotto) {
-        if (numberOfManualLotto * PRICE_EACH_LOTTO > money.getValue()) {
+    public LottoTicketBuyingRequest(Money money, LottoAmount manualLottoAmount) {
+        if (manualLottoAmount.getValue() * PRICE_EACH_LOTTO > money.getValue()) {
             throw new IllegalArgumentException(ERROR_MANUAL_LOTTO_PRICE_OVER);
         }
 
         this.money = money;
-        this.numberOfManualLotto = numberOfManualLotto;
-        this.manualLottoLineList = new ArrayList<>();
+        this.manualLottoAmount = manualLottoAmount;
+        this.manualLottoLineList = new ArrayList();
     }
 
     public void submitManualLottoLine(LottoLine manualLottoLine) {
         manualLottoLineList.add(manualLottoLine);
     }
 
+    public List<LottoLine> getManualLottoLineList() {
+        return manualLottoLineList;
+    }
+
     public Money getRemainMoney() {
-        return new Money(money.getValue() - numberOfManualLotto * PRICE_EACH_LOTTO);
+        return new Money(money.getValue() - manualLottoAmount.getValue() * PRICE_EACH_LOTTO);
+    }
+
+    public LottoAmount getAutoLottoAmount() {
+        return new LottoAmount(money.getValue() / PRICE_EACH_LOTTO - manualLottoAmount.getValue());
     }
 }
