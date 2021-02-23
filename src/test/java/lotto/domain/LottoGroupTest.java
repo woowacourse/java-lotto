0716a891c02d7f12dcdb4ea88e2154982c6ff3ce
pika96 <52442949+pika96@users.noreply.github.com>
2021-02-23 23:07:18 +0900

    feat(LottoGroupTest): LottoGroup 도메인에 대한 테스트 구현

--- src/test/java/lotto/domain/LottoGroupTest.java
+++ src/test/java/lotto/domain/LottoGroupTest.java
@@ -1,11 +1,46 @@
 package lotto.domain;
 
+import lotto.util.LottoGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoGroupTest {
 
-    @Test
-    void name() {
+    private LottoGroup lottoGroup;
+    private List<Lotto> lottos;
 
+    @BeforeEach
+    void setUp() {
+        lottos = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            lottos.add(LottoGenerator.generate());
+        }
+    }
+
+    @Test
+    @DisplayName("LottoGroup 생성 성공 테스트")
+    void createLottoGroup() {
+        lottoGroup = new LottoGroup(lottos);
+        assertThat(lottoGroup).isNotNull();
+    }
+
+    @Test
+    @DisplayName("LottoGroup 사이즈가 일치하는 지 테스트")
+    void checkLottoGroupSize() {
+        lottoGroup = new LottoGroup(lottos);
+        assertThat(lottoGroup.size()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("Lotto를 저장하는 리스트인 lottos 변수를 반환하는지 테스트")
+    void checkReturnLottosVariable() {
+        lottoGroup = new LottoGroup(lottos);
+        assertThat(lottoGroup.lottoGroup()).isEqualTo(lottos);
     }
 }
