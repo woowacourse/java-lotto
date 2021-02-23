5471d1e27b09b13061140f581cf37571cc6189f4
yjksw <cairofaith@gmail.com>
2021-02-23 23:48:41 +0900

    refactor(OutputView.java): String.format 으로 출력 문구 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,18 +11,15 @@ public class OutputView {
 
     private static final String WINNING_STATISTICS_TITLE = "당첨 통계";
     private static final String DIVIDER = "---------";
-    private static final String NUMBER_OF_TICKETS = "개를 구매했습니다.";
+    private static final String NUMBER_OF_TICKETS = "%d 개를 구매했습니다.";
     private static final String COMMA = ", ";
-    private static final String LEFT_BRACKET = "[";
-    private static final String RIGHT_BRACKET = "]";
+    private static final String BRACKET = "[%s]";
     private static final String RANK_RESULT_FORMAT = "%d개 일치 %s(%d원) - %d개";
-    private static final String TOTAL_PROFIT_RATE_PREFIX = "총 수익률은 ";
-    private static final String TOTAL_PROFIT_RATE_SUFFIX = "입니다.";
+    private static final String TOTAL_PROFIT_RATE = "총 수익률은 %f 입니다.";
     private static final String BONUS_BALL_FORMAT = ", 보너스 볼 일치";
-    private static final String EMPTY_STRING = "";
 
     public static void printNumberOfTickets(final int lottoQuantity) {
-        System.out.println(lottoQuantity + NUMBER_OF_TICKETS);
+        System.out.printf((NUMBER_OF_TICKETS) + "%n", lottoQuantity);
     }
 
     public static void printLottoTickets(final List<LottoTicket> lottoTickets) {
@@ -34,7 +31,7 @@ public class OutputView {
             .map(LottoNumber::getValue)
             .map(String::valueOf)
             .collect(Collectors.joining(COMMA));
-        return LEFT_BRACKET + numbers + RIGHT_BRACKET;
+        return String.format(BRACKET, numbers);
     }
 
     public static void printRankResultTitle() {
@@ -44,7 +41,7 @@ public class OutputView {
 
     public static void printIndividualRankResult(final int countNumberOfRank,
                                                  final Ranking ranking) {
-        String bonusMessage = EMPTY_STRING;
+        String bonusMessage = "";
         if (ranking == Ranking.SECOND) {
             bonusMessage = BONUS_BALL_FORMAT;
         }
@@ -56,7 +53,7 @@ public class OutputView {
     }
 
     public static void printTotalProfitRate(double profitRate) {
-        System.out.println(TOTAL_PROFIT_RATE_PREFIX + profitRate + TOTAL_PROFIT_RATE_SUFFIX);
+        System.out.printf((TOTAL_PROFIT_RATE) + "%n", profitRate);
     }
 
     public static void printError(Exception e) {
