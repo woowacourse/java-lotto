08b7cfbf7667e8733b6f09a8022105e1fc84bd60
sakjung <ssangyu123@gmail.com>
2021-02-24 12:54:30 +0900

    refactor: winning lotto에 대한 테스트에서 로또를 만들어주는 메서드 적용

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -15,54 +15,39 @@ class WinningLottoTest {
 
 	@BeforeEach
 	void setUp() {
-		winingLotto = new WinningLotto(
-				new Lotto(Arrays.asList(
-						new LottoNumber(1),
-						new LottoNumber(2),
-						new LottoNumber(3),
-						new LottoNumber(4),
-						new LottoNumber(5),
-						new LottoNumber(6))),
-				new LottoNumber(20));
+		Lotto winningLotto = createCustomLotto(1, 2, 3, 4, 5, 6);
+		winingLotto = new WinningLotto(winningLotto, new LottoNumber(20));
 	}
 
 	@DisplayName("보너스가 당첨 번호와 중복 되는 것이 있는지 검증")
 	@Test
 	void validateDuplicatesWithWinningNumbers() {
-		assertThatThrownBy(() -> new WinningLotto(
-				new Lotto(Arrays.asList(
-						new LottoNumber(1),
-						new LottoNumber(2),
-						new LottoNumber(3),
-						new LottoNumber(4),
-						new LottoNumber(5),
-						new LottoNumber(6))),
-				new LottoNumber(5)))
-				.isInstanceOf(IllegalArgumentException.class)
+		Lotto winningLotto = createCustomLotto(1, 2, 3, 4, 5, 6);
+		assertThatThrownBy(() -> new WinningLotto(winningLotto, new LottoNumber(5))).isInstanceOf(IllegalArgumentException.class)
 				.hasMessage(DUPLICATE_BONUS_NUMBER_ERROR);
 	}
 
 	@DisplayName("당첨 번호와 로또와의 매칭 확인 제대로 하는지")
 	@Test
 	void countMatchingNumbers() {
-		assertThat(winingLotto.countMatchingNumbersWith(Arrays.asList(
-				new LottoNumber(1),
-				new LottoNumber(2),
-				new LottoNumber(3),
-				new LottoNumber(13),
-				new LottoNumber(14),
-				new LottoNumber(16)))).isEqualTo(3);
+		Lotto lotto = createCustomLotto(1, 2, 3, 13, 14, 16);
+		assertThat(winingLotto.countMatchingNumbersWith(lotto)).isEqualTo(3);
 	}
 
 	@DisplayName("보너스 번호와 로또와의 매칭 확인 제대로 하는지")
 	@Test
 	void hasBonusMatch() {
-		assertThat(winingLotto.hasBonusMatchWith(Arrays.asList(
-				new LottoNumber(1),
-				new LottoNumber(2),
-				new LottoNumber(3),
-				new LottoNumber(13),
-				new LottoNumber(14),
-				new LottoNumber(20)))).isTrue();
+		Lotto lotto = createCustomLotto(1, 2, 3, 13, 14, 20);
+		assertThat(winingLotto.hasBonusMatchWith(lotto)).isTrue();
+	}
+
+	private Lotto createCustomLotto(final int... numbers) {
+		return new Lotto(Arrays.asList(
+				new LottoNumber(numbers[0]),
+				new LottoNumber(numbers[1]),
+				new LottoNumber(numbers[2]),
+				new LottoNumber(numbers[3]),
+				new LottoNumber(numbers[4]),
+				new LottoNumber(numbers[5])));
 	}
 }
