44364aac56e1ba0f5f0f2244542a5096e248dd61
yoonsung.jung <ybook2012@gmail.com>
2021-02-24 17:15:12 +0900

    refactor(view) : view 수정 및 구현

--- src/main/java/lottogame/view/InputView.java
+++ src/main/java/lottogame/view/InputView.java
@@ -3,25 +3,33 @@ package lottogame.view;
 import java.util.Scanner;
 
 public class InputView {
-
-    public static final String INPUT_REQUEST_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
-    public static final String INPUT_REQUEST_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
-    private static final String INPUT_REQUEST_MONEY = "구입금액을 입력해 주세요.";
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String getMoneyInput() {
-        System.out.println(INPUT_REQUEST_MONEY);
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public static String getManualLottoTicketCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public static void printManualNumbersInputRequestMessage() {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+    }
+
+    public static String getManualNumbersInput() {
         return scanner.nextLine();
     }
 
     public static String getWinningNumbersInput() {
-        System.out.println(INPUT_REQUEST_WINNING_NUMBERS);
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return scanner.nextLine();
     }
 
     public static String getBonusNumberInput() {
-        System.out.println(INPUT_REQUEST_BONUS_NUMBER);
+        System.out.println("보너스 볼을 입력해 주세요.");
         return scanner.nextLine();
     }
 }
