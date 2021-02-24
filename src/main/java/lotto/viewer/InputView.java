984df2fd3d2d8cdb9c46984b1456c4029ed955f4
YeongBeom Heo <hybeom@gmail.com>
2021-02-25 01:02:35 +0900

    fix(InputException) : 자료형 검증에 대한 Input 예외 처리

--- src/main/java/lotto/viewer/InputView.java
+++ src/main/java/lotto/viewer/InputView.java
@@ -10,6 +10,7 @@ import lotto.domain.Money;
 import lotto.domain.Piece;
 import lotto.domain.generator.LottoManualGenerator;
 import lotto.exception.LottoAnnouncementException;
+import lotto.exception.LottoException;
 import lotto.exception.PieceException;
 
 public class InputView {
@@ -18,13 +19,37 @@ public class InputView {
     private static final String MANUAL_PIECE_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String INPUT_WINNERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요";
     private static final String INPUT_BONUS_MESSAGE = "보너스 볼을 입력해주세요";
-    private static final String PURCAHSE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String NOT_INTEGER_INPUT = "정수 입력이 아닙니다.";
+    public static final String NOT_VALID_LOTTO_NUMBERS = "올바르지 않는 로또 번호 입력입니다.";
     private static final String DELIMITER = ", ";
 
     final Scanner scanner;
 
     public InputView(Scanner scanner) {
         this.scanner = scanner;
+    }
+
+    public Money purchaseMoney() {
+        int rawMoney;
+        try {
+            rawMoney = receiveInteger(PURCHASE_MONEY_MESSAGE);
+        } catch (LottoException lottoException) {
+            System.out.println(lottoException.getMessage());
+            rawMoney = receiveInteger(PURCHASE_MONEY_MESSAGE);
+        }
+        return new Money(rawMoney);
+    }
+
+    public Piece inputManualPieces(Money money) throws PieceException {
+        int rawAutoPieces;
+        try {
+            rawAutoPieces = receiveInteger(MANUAL_PIECE_MESSAGE);
+        } catch (LottoException lottoException) {
+            System.out.println(lottoException.getMessage());
+            rawAutoPieces = receiveInteger(MANUAL_PIECE_MESSAGE);
+        }
+        return new Piece(money, rawAutoPieces);
     }
 
     public LottoAnnouncement inputAnnouncement() throws LottoAnnouncementException {
@@ -37,46 +62,63 @@ public class InputView {
 
     private List<Integer> inputWinningNumbers() {
         System.out.println(INPUT_WINNERS_MESSAGE);
-        return parseToWinner();
+        return getValidLottoNumbers();
     }
 
     private int inputBonusNumber() {
-        System.out.println(INPUT_BONUS_MESSAGE);
-        int bonusNumber= scanner.nextInt();
-        scanner.nextLine();
-        return bonusNumber;
+        int rawMoney;
+        try {
+            rawMoney = receiveInteger(INPUT_BONUS_MESSAGE);
+        } catch (LottoException lottoException) {
+            System.out.println(lottoException.getMessage());
+            rawMoney = receiveInteger(INPUT_BONUS_MESSAGE);
+        }
+        return rawMoney;
+    }
+
+    private int receiveInteger(String receiveTargetMessage) throws LottoException {
+        System.out.println(receiveTargetMessage);
+        String rawInteger;
+        try {
+            rawInteger = scanner.nextLine();
+            return Integer.parseInt(rawInteger);
+        } catch (Exception integerException) {
+            throw new LottoException(NOT_INTEGER_INPUT);
+        }
     }
 
     public LottoManualGenerator lottoManualGenerator(Piece manualPiece) {
         System.out.println(MANUAL_LOTTO_INPUT_MESSAGE);
         List<List<Integer>> inputWinningNumbers = new ArrayList<>();
         for (int i = 0; i < manualPiece.getPieceNumber(); i++) {
-            inputWinningNumbers.add(parseToWinner());
+            List<Integer> lottoNumbers= getValidLottoNumbers();
+            inputWinningNumbers.add(lottoNumbers);
         }
         return new LottoManualGenerator(inputWinningNumbers);
     }
 
-    private List<Integer> parseToWinner() {
-        String rawWinningNumbers = scanner.nextLine();
-        String[] splittedWinningNumbers = rawWinningNumbers.split(DELIMITER);
-        return Arrays.stream(splittedWinningNumbers)
-            .map(String::trim)
-            .map(Integer::parseInt)
-            .collect(Collectors.toList());
+    private List<Integer> getValidLottoNumbers() {
+        List<Integer> lottoNumbers;
+        try {
+            lottoNumbers = receiveWinners();
+        } catch (LottoException lottoException) {
+            System.out.println(lottoException.getMessage());
+            lottoNumbers = receiveWinners();
+        }
+        return lottoNumbers;
     }
 
-    public Money purchaseMoney() {
-        System.out.println(PURCAHSE_MONEY_MESSAGE);
-        int rawMoney = scanner.nextInt();
-        Money money = new Money(rawMoney);
-        scanner.nextLine();
-        return money;
+    private List<Integer> receiveWinners() throws LottoException {
+        try {
+            String rawWinningNumbers = scanner.nextLine();
+            String[] splittedWinningNumbers = rawWinningNumbers.split(DELIMITER);
+            return Arrays.stream(splittedWinningNumbers)
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        } catch (Exception exception) {
+            throw new LottoException(NOT_VALID_LOTTO_NUMBERS);
+        }
     }
 
-    public Piece inputManualPieces(Money money) throws PieceException {
-        System.out.println(MANUAL_PIECE_MESSAGE);
-        int inputAutoPieces = scanner.nextInt();
-        scanner.nextLine();
-        return new Piece(money, inputAutoPieces);
-    }
 }
