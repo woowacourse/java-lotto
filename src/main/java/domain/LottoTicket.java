ba57b4d9fcb56dda24adcee44e89b11fc7b8b46e
yjksw <cairofaith@gmail.com>
2021-02-24 16:08:44 +0900

    refactor(LottoTicket.java): 불필요한 메소드 삭제

--- src/main/java/domain/LottoTicket.java
+++ src/main/java/domain/LottoTicket.java
@@ -44,19 +44,4 @@ public class LottoTicket {
     public Set<LottoNumber> toSet() {
         return Collections.unmodifiableSet(lottoNumbers);
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        LottoTicket that = (LottoTicket) o;
-        return Objects.equals(lottoNumbers, that.lottoNumbers);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(lottoNumbers);
-    }
 }
