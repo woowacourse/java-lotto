196eece5ac241c8e6287ebcba97d023dccbc6ab2
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-02-24 16:39:58 +0900

    debug : 2등이 아니고 보너스번호가 같을 때(4,5등) 결과가 없다고 나오던 오류 수정

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -3,30 +3,39 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Prize {
-    FIRST(1, 2_000_000_000, 6, false),
-    SECOND(2, 30_000_000, 5, true),
-    THIRD(3, 1_500_000, 5, false),
-    FOURTH(4, 50_000, 4, false),
-    FIFTH(5, 5_000, 3, false),
-    LOSING(-1, 0, -1, false);
+    FIRST(1, 2_000_000_000, 6),
+    SECOND(2, 30_000_000, 5),
+    THIRD(3, 1_500_000, 5),
+    FOURTH(4, 50_000, 4),
+    FIFTH(5, 5_000, 3),
+    LOSING(-1, 0, -1);
 
     private final int rank;
     private final int prizeMoney;
     private final int matchCount;
-    private final boolean isBonus;
 
-    Prize(int rank, int prizeMoney, int matchCount, boolean isBonus) {
+    Prize(int rank, int prizeMoney, int matchCount) {
         this.rank = rank;
         this.prizeMoney = prizeMoney;
         this.matchCount = matchCount;
-        this.isBonus = isBonus;
     }
 
     public static Prize findByMatchCount(int matchCount, boolean isBonus) {
+        if (matchCount == SECOND.matchCount) {
+            return secondOrThird(isBonus);
+        }
+
         return Arrays.stream(Prize.values())
-                .filter(prize -> prize.matchCount == matchCount && prize.isBonus == isBonus)
+                .filter(prize -> prize.matchCount == matchCount)
                 .findFirst()
                 .orElse(LOSING);
+    }
+
+    private static Prize secondOrThird(boolean isBonus) {
+        if (isBonus) {
+            return SECOND;
+        }
+        return THIRD;
     }
 
     public int getRank() {
