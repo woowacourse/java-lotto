6d028a4f6b80e77397f34d68210acaba1eaae276
binghe819 <binghe819@naver.com>
2021-02-25 00:13:25 +0900

    test: 수동과 자동으로 구매한 로또 당첨금 계산 테스트 추가

--- src/test/java/lotto/model/LottoResultsTest.java
+++ src/test/java/lotto/model/LottoResultsTest.java
@@ -6,31 +6,40 @@ import java.util.Arrays;
 import lotto.domain.WinningLotto;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.Lottos;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoResultsTest {
 
-    @DisplayName("두 LottoResult를 합쳐서 하나의 결과가 나와야한다")
-    @Test
-    void 로또_결과_머지_테스트() {
-        // given
+    Lottos lottos1;
+    Lottos lottos2;
+    LottoResults lottoResults;
+
+    @BeforeEach
+    void setUp() {
         Lotto userLotto1 = Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto userLotto2 = Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 7));
         Lotto userLotto3 = Lotto.of(Arrays.asList(1, 2, 10, 33, 35, 38));
         WinningLotto winningLotto = new WinningLotto(Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6)), 7);
 
-        Lottos lottos1 = Lottos.from(Arrays.asList(userLotto1, userLotto2));
-        Lottos lottos2 = Lottos.from(Arrays.asList(userLotto3));
+        lottos1 = Lottos.from(Arrays.asList(userLotto1, userLotto2));
+        lottos2 = Lottos.from(Arrays.asList(userLotto3));
+        lottoResults = LottoResults.of(lottos1.match(winningLotto), lottos2.match(winningLotto));
+    }
 
-        // when
-        LottoResult lottoResult1 = lottos1.match(winningLotto);
-        LottoResult lottoResult2 = lottos2.match(winningLotto);
-        LottoResults lottoResults = LottoResults.of(lottoResult1, lottoResult2);
-
-        // then
+    @DisplayName("두 LottoResult를 합쳐도 하나의 당첨 결과가 나와야한다")
+    @Test
+    void 로또_결과_머지_테스트() {
         assertThat(lottoResults.findNumOfMatchByKey(LottoRank.FIRST)).isEqualTo(1);
-        assertThat(lottoResults.findNumOfMatchByKey(LottoRank.NO_MATCH)).isEqualTo(1);
         assertThat(lottoResults.findNumOfMatchByKey(LottoRank.SECOND)).isEqualTo(1);
+        assertThat(lottoResults.findNumOfMatchByKey(LottoRank.NO_MATCH)).isEqualTo(1);
+    }
+
+    @Test
+    void 수익률_확인_테스트() {
+        int numOfLotto = 3;
+        int totalPrize = LottoRank.FIRST.getPrize() + LottoRank.SECOND.getPrize();
+        assertThat(lottoResults.getEarningsRate()).isEqualTo( (double) totalPrize / (double) (numOfLotto * Lotto.LOTTO_PRICE));
     }
 }
