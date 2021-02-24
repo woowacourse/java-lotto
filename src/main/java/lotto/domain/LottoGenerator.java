eb4639f88b07e4057b7a2df4b1327961613e60df
Sol ji Choi <bssj9307@gmail.com>
2021-02-24 20:54:09 +0900

    refactor: LottoGenerator 수정 - LottoNumber는 Set을 생성할 때만 만들도록 변경 - int 리스트를 갖도록 변경

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,31 +1,35 @@
 package lotto.domain;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
 
     private static final int MAXIMUM_NUMBER = 45;
     private static final int LOTTO_NUMBER_LIMIT = 6;
     private static final String DELIMITER = ",";
-    private final Set<LottoNumber> lottoNumbers = new HashSet<>();
+    private final List<Integer> numbers = new ArrayList<>();
 
     public LottoGenerator() {
         for (int i = 1; i <= MAXIMUM_NUMBER; i++) {
-            lottoNumbers.add(LottoNumber.of(i));
+            numbers.add(i);
         }
     }
 
     public Set<LottoNumber> generateAuto() {
-        List<LottoNumber> lottoNumber = new ArrayList<>(lottoNumbers);
-        Collections.shuffle(lottoNumber);
-        return new HashSet<>(lottoNumber.subList(0, LOTTO_NUMBER_LIMIT));
+        List<Integer> lottoNumbers = new ArrayList<>(numbers);
+        Collections.shuffle(lottoNumbers);
+        lottoNumbers = lottoNumbers.subList(0, LOTTO_NUMBER_LIMIT);
+        return lottoNumbers.stream()
+                .map(LottoNumber::of)
+                .collect(Collectors.toSet());
     }
 
     public Set<LottoNumber> generateManual(String input) {
         Set<LottoNumber> lottoNumber = new HashSet<>();
-        for (String number : input.split(DELIMITER, -1)) {
-            number = number.trim();
-            lottoNumber.add(LottoNumber.of(number));
+        for (String value : input.split(DELIMITER, -1)) {
+            value = value.trim();
+            lottoNumber.add(LottoNumber.of(value));
         }
         return lottoNumber;
     }
--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,8 +1,5 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class WinningLotto {
 
     private static final String DUPLICATED_NUMBER_ERROR = "[ERROR] 보너스볼 숫자는 당첨번호와 중복될 수 없습니다";
@@ -22,9 +19,9 @@ public class WinningLotto {
     }
 
     private int howManyWins(Lotto lotto) {
-        List<LottoNumber> wins = new ArrayList<>(winLotto.getLottoNumbers());
-        wins.retainAll(lotto.getLottoNumbers());
-        return wins.size();
+        return lotto.getLottoNumbers()
+                .stream().filter(winLotto::isContain)
+                .toArray().length;
     }
 
     public Rank findRank(Lotto lotto) {
