5a5f00f1bae1dcf0329f60f8324087ecc1e85134
thisisyoungbin <thisisyoungbin@gmail.com>
2021-02-24 14:01:12 +0900

    refactor: Controller 내에 인스턴스 변수 제거 및 메서드 분리

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -8,43 +8,40 @@ import java.util.List;
 
 public class LottoGameController {
 
-    private LottoGame lottoGame = new LottoGame();
-    private LottoGameResult lottoGameResult = new LottoGameResult();
-    private Lottos manualLottos;
-    private Lottos autoLottos;
-
     public void run() {
         LottoAmount lottoAmount = makeLottoAmount();
 
-        buyAndPrintLottos(lottoAmount.toManualAmountNumber(), lottoAmount.toAutoAmountNumber());
+        LottoGame lottoGame = buyTwoTypeLottos(lottoAmount);
+        printEachLottos(lottoGame);
 
         WinningLotto lastWinningLotto = askWinningLotto();
-
-        lottoGameResult
-                = lottoGame.compareWithWinningLotto(manualLottos, autoLottos, lastWinningLotto);
+        LottoGameResult lottoGameResult = getRankofLottoGame(lottoGame, lastWinningLotto);
 
         OutputView.printLottoGameResult(lottoGameResult);
     }
 
-    private LottoAmount makeLottoAmount() {
-        Money money = InputView.askMoney();
+    private LottoGame buyTwoTypeLottos(LottoAmount lottoAmount) {
+        LottoGame lottoGame = new LottoGame();
+        List<Lotto> manualLottos = InputView.askManualLottoNumbers(lottoAmount.toManualAmountNumber());
+        lottoGame.buyManualLottos(manualLottos);
+        lottoGame.buyAutoLottos(lottoAmount.toAutoAmountNumber());
 
-        return InputView.askLottoAmount(money);
+        return lottoGame;
     }
 
-    private void buyAndPrintLottos(int manualAmount, int autoAmount) {
-        manualLottos = InputView.askManualLottoNumbers(manualAmount);
-        autoLottos = lottoGame.buyAutoLottos(autoAmount);
-
-        OutputView.printTotalNumberOfLotto(manualLottos, autoLottos);
-        OutputView.printEachLotto(manualLottos, autoLottos);
+    private void printEachLottos(LottoGame lottoGame) {
+        OutputView.printTotalNumberOfLotto(lottoGame.toManualLottos(), lottoGame.toAutoLottos());
+        OutputView.printEachLotto(lottoGame.toManualLottos(), lottoGame.toAutoLottos());
     }
 
-    private WinningLotto askWinningLotto() {
-        Lotto winningLotto = askWinningLottoNumbers();
-        LottoNumber bonusNumber = InputView.askBonusNumber();
-
-        return makeWinningLotto(winningLotto, bonusNumber);
+    private Lotto askWinningLottoNumbers() {
+        try {
+            List<LottoNumber> winningNumbers = InputView.askLastWinningLottoNumber();
+            return new Lotto(winningNumbers);
+        } catch (Exception e) {
+            OutputView.printError(e.getMessage());
+            return askWinningLottoNumbers();
+        }
     }
 
     private WinningLotto makeWinningLotto(Lotto winningLotto, LottoNumber bonusNumber) {
@@ -56,13 +53,24 @@ public class LottoGameController {
         }
     }
 
-    private Lotto askWinningLottoNumbers() {
-        try {
-            List<LottoNumber> winningNumbers = InputView.askLastWinningLottoNumber();
-            return new Lotto(winningNumbers);
-        } catch (Exception e) {
-            OutputView.printError(e.getMessage());
-            return askWinningLottoNumbers();
-        }
+    private LottoGameResult getRankofLottoGame(LottoGame lottoGame, WinningLotto lastWinningLotto) {
+        LottoGameResult lottoGameResult = new LottoGameResult();
+        lottoGame.toManualLottos().addMatchLotto(lastWinningLotto, lottoGameResult);
+        lottoGame.toAutoLottos().addMatchLotto(lastWinningLotto, lottoGameResult);
+
+        return lottoGameResult;
+    }
+
+    private LottoAmount makeLottoAmount() {
+        Money money = InputView.askMoney();
+
+        return InputView.askLottoAmount(money);
+    }
+
+    private WinningLotto askWinningLotto() {
+        Lotto winningLotto = askWinningLottoNumbers();
+        LottoNumber bonusNumber = InputView.askBonusNumber();
+
+        return makeWinningLotto(winningLotto, bonusNumber);
     }
 }
