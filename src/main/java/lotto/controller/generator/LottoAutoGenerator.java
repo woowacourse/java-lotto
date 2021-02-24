ce4dfb8d9cad3d4cc385002770fe474f62796304
YeongBeom Heo <hybeom@gmail.com>
2021-02-24 12:34:26 +0900

    fix(LottoAutoGenerator) : Thread-Safe 고려한 List 깊은 복사

--- src/main/java/lotto/controller/generator/LottoAutoGenerator.java
+++ src/main/java/lotto/controller/generator/LottoAutoGenerator.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class LottoAutoGenerator implements LottoGenerator {
 
@@ -22,9 +23,10 @@ public class LottoAutoGenerator implements LottoGenerator {
 
     @Override
     public List<Integer> generateNumbers() {
-        Collections.shuffle(candidateNumbers);
+        final List<Integer> shuffledNumbers = new CopyOnWriteArrayList<>(candidateNumbers);
+        Collections.shuffle(shuffledNumbers);
         Set<Integer> lottoNumbers =
-            new TreeSet<>(candidateNumbers.subList(0, LOTTO_POSSESSION_NUMBER));
-        return Collections.synchronizedList(new ArrayList<>(lottoNumbers));
+            new TreeSet<>(shuffledNumbers.subList(0, LOTTO_POSSESSION_NUMBER));
+        return new ArrayList<>(lottoNumbers);
     }
 }
