48c43e13c4ace628eda25ea8eca5be2a798546d7
Gomding <backchida@gmail.com>
2021-02-24 15:26:29 +0900

    refactor : ManualCount value  필드 final 로 변경

--- src/main/java/lotto/domain/ManualCount.java
+++ src/main/java/lotto/domain/ManualCount.java
@@ -6,7 +6,7 @@ public class ManualCount {
 
     public static final String INVALID_COUNT_ERROR_MESSAGE = "수동 구매 개수는 0미만이거나 구매가능 개수인 %d개를 초과할 수 없습니다.";
 
-    private int value;
+    private final int value;
 
     public ManualCount(int value, Money money) {
         validateManualCount(value, money);
