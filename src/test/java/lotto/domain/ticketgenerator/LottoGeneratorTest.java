34811fc38ccacef8975d71be4b0ca55a6e532ea1
TaewanKimmmm <ktww88@gmail.com>
2021-02-25 04:41:07 +0900

    test: 수동 티켓 생성 테스트 추가

--- src/test/java/lotto/domain/ticketgenerator/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/ticketgenerator/LottoGeneratorTest.java
@@ -1,39 +1,60 @@
 package lotto.domain.ticketgenerator;
 
-import java.util.Arrays;
 import lotto.domain.LottoNumber;
 import lotto.domain.LottoTicket;
 import lotto.domain.LottoTickets;
 import lotto.domain.ticketpurchase.UserPurchase;
 import lotto.strategy.AutoStrategy;
 import lotto.strategy.ManualStrategy;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoGeneratorTest {
+    private LottoGenerator lottoGenerator;
+    private List<LottoTicket> tickets;
+
+    @BeforeEach
+    void setUp() {
+        lottoGenerator = new LottoGenerator();
+        LottoTicket ticket1 = new LottoTicket(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6)
+        ));
+
+        LottoTicket ticket2 = new LottoTicket(Arrays.asList(
+                new LottoNumber(31),
+                new LottoNumber(32),
+                new LottoNumber(33),
+                new LottoNumber(34),
+                new LottoNumber(35),
+                new LottoNumber(36)
+        ));
+        tickets = Arrays.asList(ticket1, ticket2);
+    }
+
+    @DisplayName("수동 티켓 생성 테스트")
+    @Test
+    void createManualTicketTest() {
+        lottoGenerator.setGenerateStrategy(new ManualStrategy(tickets));
+        assertThat(lottoGenerator.generateTickets().size()).isEqualTo(2);
+    }
 
     @DisplayName("구입 금액에 따른 티켓 다발 생성")
     @Test
     void Should_ReturnPurchasedTickets_When_Purchased() {
         UserPurchase userPurchase = new UserPurchase(18000, 3);
-        LottoGenerator lottoGenerator = new LottoGenerator();
         lottoGenerator.setGenerateStrategy(new AutoStrategy(15));
         LottoTickets lottoTickets = lottoGenerator.generateTickets();
         assertThat(lottoTickets.size()).isEqualTo(18);
     }
-
-
-    private final LottoTicket lottoTicket = new LottoTicket(
-        Arrays.asList(
-            new LottoNumber(1),
-            new LottoNumber(2),
-            new LottoNumber(3),
-            new LottoNumber(4),
-            new LottoNumber(5),
-            new LottoNumber(6)
-        )
-    );
-
 }
