e70eb904ce79e5d7caf722270514006e3e571d5c
seunghyun <ggyool@naver.com>
2021-02-25 15:53:39 +0900

    refactor(Prize): isMatched 메서드 다형성을 이용하도록 변경

--- src/main/java/domain/Prize.java
+++ src/main/java/domain/Prize.java
@@ -5,12 +5,7 @@ import domain.ticket.LottoTicket;
 import java.util.Arrays;
 
 public enum Prize {
-    FIRST(6, false, 2_000_000_000L) {
-        @Override
-        boolean isMatched(final int matching, final boolean bonusMatching) {
-            return this.matching == matching;
-        }
-    },
+    FIRST(6, false, 2_000_000_000L),
     SECOND(5, true, 30_000_000L) {
         @Override
         boolean isMatched(final int matching, final boolean bonusMatching) {
@@ -23,24 +18,9 @@ public enum Prize {
             return this.matching == matching && this.bonusMatching == bonusMatching;
         }
     },
-    FOURTH(4, false, 50_000L) {
-        @Override
-        boolean isMatched(final int matching, final boolean bonusMatching) {
-            return this.matching == matching;
-        }
-    },
-    FIFTH(3, false, 5000L) {
-        @Override
-        boolean isMatched(final int matching, final boolean bonusMatching) {
-            return this.matching == matching;
-        }
-    },
-    NOTHING(0, false, 0L) {
-        @Override
-        boolean isMatched(final int matching, final boolean bonusMatching) {
-            return false;
-        }
-    };
+    FOURTH(4, false, 50_000L),
+    FIFTH(3, false, 5000L),
+    NOTHING(0, false, 0L);
 
     protected final int matching;
     protected final boolean bonusMatching;
@@ -52,7 +32,9 @@ public enum Prize {
         this.money = money;
     }
 
-    abstract boolean isMatched(final int matching, final boolean bonusMatching);
+    boolean isMatched(final int matching, final boolean bonusMatching) {
+        return this.matching == matching;
+    }
 
     public static Prize of(final WinningNumbers winningNumbers, final LottoTicket lottoTicket) {
         final int matching = winningNumbers.countMatching(lottoTicket);
