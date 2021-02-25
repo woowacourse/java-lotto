ba6dbec3282997585d89ccd931a314d41de8af64
sjpark-dev <sjpark.dev@gmail.com>
2021-02-25 18:57:40 +0900

    refactor(ManualLottoGenerator) : 사용하지 않는 import 제거

--- src/main/java/lottogame/utils/ManualLottoGenerator.java
+++ src/main/java/lottogame/utils/ManualLottoGenerator.java
@@ -5,7 +5,6 @@ import lottogame.domain.lotto.LottoNumber;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
