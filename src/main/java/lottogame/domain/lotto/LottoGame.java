0d5232f29fff37138297a3a696fc27f03497b411
sjpark-dev <sjpark.dev@gmail.com>
2021-02-25 18:53:48 +0900

    refactor(LottoGame) : 테스트 작성

--- src/main/java/lottogame/domain/lotto/LottoGame.java
+++ src/main/java/lottogame/domain/lotto/LottoGame.java
@@ -2,6 +2,8 @@ package lottogame.domain.lotto;
 
 import lottogame.domain.stats.LottoResults;
 
+import java.util.Objects;
+
 public class LottoGame {
     private Lottos lottos;
     private WinningLotto winningLotto;
@@ -13,5 +15,18 @@ public class LottoGame {
 
     public LottoResults results() {
         return lottos.matchLottos(winningLotto);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoGame lottoGame = (LottoGame) o;
+        return Objects.equals(lottos, lottoGame.lottos) && Objects.equals(winningLotto, lottoGame.winningLotto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottos, winningLotto);
     }
 }
--- src/test/java/lottogame/domain/lotto/LottoGameTest.java
+++ src/test/java/lottogame/domain/lotto/LottoGameTest.java
@@ -1,19 +1,20 @@
 package lottogame.domain.lotto;
 
-import lottogame.domain.stats.Rank;
 import lottogame.domain.stats.LottoResults;
+import lottogame.domain.stats.Rank;
 import lottogame.utils.ManualLottoGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.EnumMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class LottosTest {
+class LottoGameTest {
     private ManualLottoGenerator manualLottoGenerator;
-    private Lottos lottos;
+    private LottoGame lottoGame;
 
     @BeforeEach
     void setUp() {
@@ -21,7 +22,10 @@ class LottosTest {
         manualLottoGenerator.addResources(Arrays.asList(
                 "1, 2, 3, 4, 45, 44", "1, 2, 3, 43, 44, 45",
                 "1, 2, 3, 4, 5, 7", "1, 2, 3, 4, 5, 44"));
-        lottos = new Lottos(manualLottoGenerator.generateLottos());
+        Lottos lottos = new Lottos(manualLottoGenerator.generateLottos());
+        manualLottoGenerator = new ManualLottoGenerator("1, 2, 5, 20, 21, 22");
+        WinningLotto winningLotto = new WinningLotto(manualLottoGenerator.generateLotto(), LottoNumber.of(25));
+        lottoGame = new LottoGame(lottos, winningLotto);
     }
 
     @Test
@@ -32,14 +36,15 @@ class LottosTest {
                 "1, 2, 3, 4, 45, 44", "1, 2, 3, 43, 44, 45",
                 "1, 2, 3, 4, 5, 7", "1, 2, 3, 4, 5, 44"));
         Lottos newLottos = new Lottos(manualLottoGenerator.generateLottos());
-        assertEquals(newLottos, lottos);
+        manualLottoGenerator = new ManualLottoGenerator("1, 2, 5, 20, 21, 22");
+        WinningLotto newWinningLotto = new WinningLotto(manualLottoGenerator.generateLotto(), LottoNumber.of(25));
+        LottoGame newLottoGame = new LottoGame(newLottos, newWinningLotto);
+        assertEquals(newLottoGame, lottoGame);
     }
 
     @Test
     @DisplayName("올바른 결과를 만드는지 확인")
-    void matchLottos() {
-        manualLottoGenerator = new ManualLottoGenerator("1, 2, 5, 20, 21, 22");
-        WinningLotto winningLotto = new WinningLotto(manualLottoGenerator.generateLotto(), LottoNumber.of(25));
+    void results() {
         EnumMap<Rank, Integer> results = new EnumMap<>(Rank.class);
         results.put(Rank.NOT_FOUND, 0);
         results.put(Rank.FIFTH, 2);
@@ -48,6 +53,6 @@ class LottosTest {
         results.put(Rank.SECOND, 0);
         results.put(Rank.FIRST, 0);
         LottoResults exectedlottoResults = new LottoResults(results);
-        assertEquals(exectedlottoResults, lottos.matchLottos(winningLotto));
+        assertEquals(exectedlottoResults, lottoGame.results());
     }
 }
