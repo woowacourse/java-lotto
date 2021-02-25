15855280e1d9687b07a4e8dea7a8e12ec4ac469b
Gomding <backchida@gmail.com>
2021-02-25 18:52:35 +0900

    refactor : 등수 출력 순서를 역전

--- src/main/java/lotto/domain/lottos/rank/LottoRank.java
+++ src/main/java/lotto/domain/lottos/rank/LottoRank.java
@@ -3,12 +3,12 @@ package lotto.domain.lottos.rank;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST_PLACE(6, false, 2_000_000_000),
-    SECOND_PLACE(5, true, 30_000_000),
-    THIRD_PLACE(5, false, 1_500_000),
-    FOURTH_PLACE(4, false, 50_000),
+    SIXTH_PLACE(0, false, 0),
     FIFTH_PLACE(3, false, 5_000),
-    SIXTH_PLACE(0, false, 0);
+    FOURTH_PLACE(4, false, 50_000),
+    THIRD_PLACE(5, false, 1_500_000),
+    SECOND_PLACE(5, true, 30_000_000),
+    FIRST_PLACE(6, false, 2_000_000_000);
 
     private final int matches;
     private final boolean matchBonusNumber;
