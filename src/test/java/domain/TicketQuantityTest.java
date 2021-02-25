cdcfb86d3f48f958a33114bc63f3b3dbbc3c09b4
seunghyun <ggyool@naver.com>
2021-02-26 00:39:01 +0900

    refactor(Lotto): 불필요한 import 제거

--- src/test/java/domain/TicketQuantityTest.java
+++ src/test/java/domain/TicketQuantityTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
