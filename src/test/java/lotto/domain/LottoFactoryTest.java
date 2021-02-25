676be97497e59f231fbc6b6bee0ba2110d48c156
joanne <mathmjseo@naver.com>
2021-02-25 17:19:38 +0900

    test[LottoFactoryTest]: LottoFactory에서 수동으로 티켓 생성 및 자동 생성 테스트 추가

---
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoFactoryTest {
+    private Lottos lottos;
+    private Lotto lotto;
+    private Set<LottoNumber> lottoNumbers;
+
+    @BeforeEach
+    void setUp() {
+        lottos = new Lottos();
+
+        LottoNumber lotto1 = new LottoNumber(1);
+        LottoNumber lotto2 = new LottoNumber(2);
+        LottoNumber lotto3 = new LottoNumber(3);
+        LottoNumber lotto4 = new LottoNumber(4);
+        LottoNumber lotto5 = new LottoNumber(5);
+        LottoNumber lotto6 = new LottoNumber(6);
+        lottoNumbers = new HashSet<>(Arrays.asList(lotto1, lotto2, lotto3, lotto4, lotto5, lotto6));
+        lotto = new Lotto(lottoNumbers);
+    }
+
+    @Test
+    void createManualLottos() {
+        lottos = LottoFactory.createManualLottos(lottos, Arrays.asList(lotto));
+        assertThat(lottos.toList())
+                .contains(new Lotto(lottoNumbers));
+    }
+
+    @Test
+    void createAutoLottos() {
+        Purchase purchase = new Purchase(new Money(2000), 0);
+        lottos = LottoFactory.createAutoLottos(lottos, purchase);
+        assertThat(lottos.toList().size()).isEqualTo(2);
+    }
+}
