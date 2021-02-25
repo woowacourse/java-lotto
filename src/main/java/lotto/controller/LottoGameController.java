149c850ebc603f94b59682cbd134d5d85e1e2df6
perenok <perenoke@gmail.com>
2021-02-25 11:46:40 +0900

    feat(LottoGameController) : 수동 로또 구매 기능 추가에 따른 메소드 변경 및 추가
    createLottoQuantity를 통해 수동과 자동의 로또 개수 저장
    buyFixedLotto 메소드를 추가해 수동 로또 구매

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -2,47 +2,79 @@ package lotto.controller;
 
 import lotto.domain.*;
 import lotto.utils.AutoLottoGenerator;
+import lotto.utils.FixedLottoGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.List;
+
+import static lotto.domain.LottoQuantity.ZERO_COUNT;
 
 public class LottoGameController {
 
     private final LottoGame lottoGame = new LottoGame();
 
     public void run() {
-        buyAutoLotto();
-        Lotto lotto = InputView.askLastWinningLotto();
-        WinningLotto lastWinningLotto = createLastWinningLotto(lotto);
-        showLottoGameResult(lastWinningLotto);
+        Money totalMoney = InputView.askMoney();
+        LottoQuantity lottoQuantity = createLottoQuantity(totalMoney);
+        buyLottos(lottoQuantity);
+
+        LottoNumbers lastWinningLottoNumbers = InputView.askLastWinningLottoNumbers();
+        LottoNumber bonusNumber = createBonusNumber(lastWinningLottoNumbers);
+        WinningLotto lastWinningLotto = lottoGame.createWinningLotto(lastWinningLottoNumbers, bonusNumber);
+
+        LottoGameResult lottoGameResult = lottoGame.calculateLottoGameResult(lastWinningLotto);
+        OutputView.printLottoGameResult(lottoGameResult);
     }
 
-    private void buyAutoLotto() {
-        Money money = InputView.askMoney();
-        lottoGame.buyLottos(money, new AutoLottoGenerator());
+    private LottoQuantity createLottoQuantity(Money totalMoney) {
+        try {
+            return lottoGame.createLottoQuantity(totalMoney, InputView.askFixedLottoQuantity());
+        } catch (Exception e) {
+            OutputView.printError(e);
+            return createLottoQuantity(totalMoney);
+        }
+    }
+
+    private void buyLottos(LottoQuantity lottoQuantity) {
+        if (lottoQuantity.getFixedLottoQuantity() > ZERO_COUNT) {
+            buyFixedLotto(lottoQuantity);
+        }
+        if (lottoQuantity.getAutoLottoQuantity() > ZERO_COUNT) {
+            buyAutoLotto(lottoQuantity);
+        }
         OutputView.printEachLotto(lottoGame.myLottos());
     }
 
-    private WinningLotto createLastWinningLotto(Lotto lotto) {
-        LottoNumber bonusNumber = createBonusNumber();
-        try {
-            return new WinningLotto(lotto, bonusNumber);
-        } catch (Exception e) {
-            OutputView.printError(e);
-            return createLastWinningLotto(lotto);
+    private void buyFixedLotto(LottoQuantity lottoQuantity) {
+        List<LottoNumbers> fixedLottoNumbersBundle = createLottoNumbersBundle(lottoQuantity);
+        if (lottoQuantity.getFixedLottoQuantity() != ZERO_COUNT) {
+            lottoGame.buyLottos(lottoQuantity.calculateFixedLottoPrice(),
+                    new FixedLottoGenerator(fixedLottoNumbersBundle));
         }
     }
 
-    private LottoNumber createBonusNumber() {
+    private void buyAutoLotto(LottoQuantity lottoQuantity) {
+        lottoGame.buyLottos(lottoQuantity.calculateAutoLottoPrice(), new AutoLottoGenerator());
+    }
+
+    private List<LottoNumbers> createLottoNumbersBundle(LottoQuantity lottoQuantity) {
         try {
-            return InputView.askBonusNumber();
+            return InputView.askFixLottoNumbersBundle(lottoQuantity.getFixedLottoQuantity());
         } catch (Exception e) {
             OutputView.printError(e);
-            return createBonusNumber();
+            return createLottoNumbersBundle(lottoQuantity);
         }
     }
 
-    private void showLottoGameResult(WinningLotto lastWinningLotto) {
-        LottoGameResult lottoGameResult = lottoGame.calculateLottoGameResult(lastWinningLotto);
-        OutputView.printLottoGameResult(lottoGameResult);
+    private LottoNumber createBonusNumber(LottoNumbers lottoNumbers) {
+        LottoNumber bonusNumber = InputView.askBonusNumber();
+        try {
+            lottoNumbers.checkBonusNumber(bonusNumber);
+            return bonusNumber;
+        } catch (Exception e) {
+            OutputView.printError(e);
+            return createBonusNumber(lottoNumbers);
+        }
     }
 }
