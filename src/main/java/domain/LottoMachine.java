b4935c65e386c7b6fa8b9ec2b4ff5e0b3016f039
daeun <leede0418@likelion.org>
2021-02-25 13:01:35 +0900

    feat(lotto-machine): 수동으로 로또를 구매하는 기능 구현
    - 숫자가 아닌 경우 예외 발생
    - 공백인 경우 예외 발생
    - 1~45의 범위가 아닌 경우 예외 발생
    - 6개보다 적거나 많은 숫자를 입력한 경우 예외 발생
    - 중복되는 숫자를 입력한 경우 예외 발생

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
-    private static final int PRICE = 1000;
-
-    public static List<LottoTicket> buy(Money money) {
+    public static List<LottoTicket> buyManual(List<List<Integer>> manualNumbers) {
         List<LottoTicket> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < money.toLong() / PRICE; i++) {
+        for (List<Integer> manualNumber : manualNumbers) {
+            lottoTickets.add(LottoTicket.of(manualNumber));
+        }
+        return lottoTickets;
+    }
+
+    public static List<LottoTicket> buyAuto(long autoCount) {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        for (int i = 0; i < autoCount; i++) {
             lottoTickets.add(new LottoTicket(LottoNumberRepository.shuffleLottoNumbers()));
         }
         return lottoTickets;
--- src/test/java/domain/LottoMachineTest.java
+++ src/test/java/domain/LottoMachineTest.java
@@ -1,20 +1,39 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoMachineTest {
-    @DisplayName("금액이 입력되면 금액에 맞게 로또 티켓을 구매한다.")
-    @Test
-    public void purchaseLottoTicketTest() {
-        Money money = new Money(3_000);
+    private Quantity quantity;
 
-        List<LottoTicket> lottoTickets = LottoMachine.buy(money);
+    @BeforeEach
+    public void setUp() {
+        quantity = new Quantity(new Money(12_000), 3);
+    }
+
+    @DisplayName("수동으로 로또 티켓을 구매한다.")
+    @Test
+    public void buyManualLottoTickets() {
+        List<LottoTicket> lottoTickets = LottoMachine.buyManual(Arrays.asList(
+                Arrays.asList(9, 12, 6, 2, 1, 8),
+                Arrays.asList(19, 3, 5, 4, 7, 1),
+                Arrays.asList(2, 3, 6, 8, 21, 4)
+        ));
 
         assertThat(lottoTickets.size()).isEqualTo(3);
+    }
+
+    @DisplayName("자동으로 로또 티켓을 구매한다.")
+    @Test
+    public void buyAutoLottoTickets() {
+        List<LottoTicket> lottoTickets = LottoMachine.buyAuto(quantity.auto());
+
+        assertThat(lottoTickets.size()).isEqualTo(9);
     }
 }
