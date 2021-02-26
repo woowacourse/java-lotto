a6b1b1f167f14e6326234c73c02ded3dae417333
DWL5 <ssop6403@gmail.com>
2021-02-26 17:19:44 +0900

    refactor: 불필요한 상수 제거

--- src/main/java/lotto/domain/lotto/LottoAmount.java
+++ src/main/java/lotto/domain/lotto/LottoAmount.java
@@ -1,11 +1,6 @@
 package lotto.domain.lotto;
 
-import static lotto.utils.Validation.isNumeric;
-
 public class LottoAmount {
-    private static final String ERROR_LOTTO_AMOUNT_NOT_NUMERIC = "[ERROR] 로또 구매 수량은 숫자여야 합니다.";
-    private static final String ERROR_LOTTO_AMOUNT_NEGATIVE = "[ERROR] 로또 구매 수량은 음수가 될 수 없습니다.";
-
     private final int value;
 
     public LottoAmount(int lottoAmount) {
