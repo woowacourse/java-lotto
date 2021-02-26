828c5a395903b3268e2551f8e54a7f91d6911fd3
Eugine Park <pkeugine@gmail.com>
2021-02-26 12:28:55 +0900

    test(LottoNumber): 로또 리스트 생성 테스트 추가

--- src/test/java/domain/lotto/LottoNumberTest.java
+++ src/test/java/domain/lotto/LottoNumberTest.java
@@ -1,9 +1,11 @@
 package domain.lotto;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashSet;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,10 +13,10 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoNumberTest {
 
-    @DisplayName("유효한 값이면 객체 생성 성공")
+    @DisplayName("객체 생성 성공 : 1 ~ 45 인 값 입력")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 45})
-    void valueOf_validValue_success(final int value) {
+    void of_validValue_successful(final int value) {
         assertThatCode(() -> LottoNumber.of(value))
                 .doesNotThrowAnyException();
     }
@@ -22,10 +24,23 @@ public class LottoNumberTest {
     @DisplayName("객체 생성 실패 : 1 ~ 45 가 아닌 값 입력")
     @ParameterizedTest
     @ValueSource(ints = {0, 46})
-    void valueOf_outOfRange_exceptionThrown(final int value) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> LottoNumber.of(value))
-                .withMessageContaining("범위");
+    void of_outOfRange_exceptionThrown(final int value) {
+        // TODO
+    }
+
+    @DisplayName("로또 넘버 리스트 생성 성공 : 사이즈 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {6, 7, 8})
+    void generateList_size_successful(final int value) {
+        assertThat(LottoNumber.generateList(value).size()).isEqualTo(value);
+    }
+
+    @DisplayName("로또 넘버 리스트 생성 성공 : 중복 없음")
+    @ParameterizedTest
+    @ValueSource(ints = {6, 7, 8})
+    void generateList_noDuplicate_successful(final int value) {
+        List<LottoNumber> numbers = LottoNumber.generateList(value);
+        assertThat(numbers.size()).isEqualTo(new HashSet<>(numbers).size());
     }
 
     @DisplayName("동일한 객체 비교")
