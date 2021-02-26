da7b8b6d5b65af04178c7f44b0594e457418318d
Eugine Park <pkeugine@gmail.com>
2021-02-26 12:32:26 +0900

    test(LottoTicket): contains 테스트 추가

--- src/test/java/domain/lotto/LottoTicketTest.java
+++ src/test/java/domain/lotto/LottoTicketTest.java
@@ -3,17 +3,18 @@ package domain.lotto;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LottoTicketTest {
+class LottoTicketTest {
 
-    @DisplayName("유효한 값이면 객체 생성 성공")
+    @DisplayName("객체 생성 성공 : 수동 생성")
     @Test
-    void valueOf_validInput_success() {
+    void generate_validInput_successful() {
         final List<Integer> lottoNumbers = getValidNumbers();
 
         assertThatCode(() -> LottoTicket.generateManual(lottoNumbers))
@@ -22,7 +23,7 @@ public class LottoTicketTest {
 
     @DisplayName("객체 생성 실패 : 중복된 번호")
     @Test
-    void valueOf_duplicateNumber_exceptionThrown() {
+    void generate_duplicateNumber_exceptionThrown() {
         final List<Integer> lottoNumbers = getDuplicateNumbers();
 
         assertThatIllegalArgumentException()
@@ -32,7 +33,7 @@ public class LottoTicketTest {
 
     @DisplayName("객체 생성 실패 : 숫자가 6개가 아닌 경우")
     @Test
-    void valueOf_incorrectSize_exceptionThrown() {
+    void generate_incorrectSize_exceptionThrown() {
         final List<Integer> fewerLottoNumbers = getFewerNumbers();
         final List<Integer> moreLottoNumbers = getMoreNumbers();
 
@@ -44,6 +45,12 @@ public class LottoTicketTest {
                         .isThrownBy(() -> LottoTicket.generateManual(moreLottoNumbers))
                         .withMessageContaining("개수가 6이 아닙니다.")
         );
+    }
+
+    @DisplayName("객체 안에 숫자 존재 확인")
+    @Test
+    void contains_successful() {
+        assertTrue(LottoTicket.generateManual(getValidNumbers()).contains(LottoNumber.of(1)));
     }
 
     private List<Integer> getValidNumbers() {
