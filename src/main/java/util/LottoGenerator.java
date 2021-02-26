e0026ae4b01d40a21ba40c999d356dc59dc9e699
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-26 22:05:51 +0900

    refactor(LottoGenerator): domain과 의존성 제거

--- src/main/java/util/LottoGenerator.java
+++ src/main/java/util/LottoGenerator.java
@@ -1,12 +1,8 @@
 package util;
 
-import domain.lotto.Lotto;
-import domain.lotto.LottoBall;
-import domain.lotto.LottoBundle;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoGenerator {
@@ -18,30 +14,12 @@ public class LottoGenerator {
     private LottoGenerator() {
     }
 
-    public static LottoBundle createRandomLottoBundle(final int number) {
-        final List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < number; i++) {
-            lottos.add(createRandomLotto());
-        }
-        return new LottoBundle(lottos);
-    }
-
-    public static LottoBundle createRandomLottoBundle(final List<Lotto> lotto, final int number) {
-        final List<Lotto> lottos = new ArrayList<>(lotto);
-        for (int i = 0; i < number; i++) {
-            lottos.add(createRandomLotto());
-        }
-        return new LottoBundle(lottos);
-    }
-
-    private static Lotto createRandomLotto() {
-        final List<LottoBall> lottoBalls = new ArrayList<>();
+    public static List<Integer> createRandomLottoNumber() {
         IntStream intStream = new Random().ints(MIN_NUMBER, MAX_NUMBER + EXTRA_BOUND);
-        intStream.distinct()
+        return intStream.distinct()
                 .limit(LOTTO_BALLS_IN_LOTTO)
                 .sorted()
-                .forEach(number -> lottoBalls.add(LottoBall.valueOf(number)));
-        return new Lotto(lottoBalls);
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
-
