18d9265e1e95418edc1db295d6b1c971c892d985
rok93 <goodboy302@naver.com>
2021-02-27 19:15:49 +0900

    fix: 잘못된 입력 안내문구 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -21,7 +21,7 @@ public class InputView {
     }
 
     public static CountOfPurchasingLotto inputPurchasingPassiveLottoNumber() {
-        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
         return new CountOfPurchasingLotto(SCANNER.nextInt());
     }
 
