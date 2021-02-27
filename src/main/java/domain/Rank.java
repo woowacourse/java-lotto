f36be2dbcb9d79744f0619ae1df5d32a9dd5be7e
Kimun Kim <kkm97351@gmail.com>
2021-02-28 00:10:53 +0900

    refactor: Rank 어트리뷰트에 보너스 번호 일치 여부 추가
    - 어트리뷰트에 보너스볼 일치 여부를 추가하여, enum 객체 중 2등과 3등이 상태에 따라 어떻게 구분되는지를 더 확연히 했습니다.
    - 또한 Rank.from 메소드 내부의 if 조건절을 삭제할 수 있도록 간략화했습니다.

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -10,29 +10,27 @@ import java.util.Arrays;
  * @author Daeun Lee, github.com/da-nyee
  */
 public enum Rank {
-    FIRST(6, Money.from(2_000_000_000)),
-    SECOND(5, Money.from(30_000_000)),
-    THIRD(5, Money.from(1_500_000)),
-    FOURTH(4, Money.from(50_000)),
-    FIFTH(3, Money.from(5_000)),
-    NOTHING(0, Money.from(0));
-
-    private static final long FIVE = 5;
+    FIRST(6, false, Money.from(2_000_000_000)),
+    SECOND(5, true, Money.from(30_000_000)),
+    THIRD(5, false, Money.from(1_500_000)),
+    FOURTH(4, false, Money.from(50_000)),
+    FIFTH(3, false, Money.from(5_000)),
+    NOTHING(0, false, Money.from(0));
 
     private final long count;
+    private final boolean hasBonusBallMatch;
     private final Money reward;
 
-    Rank(long count, Money reward) {
+    Rank(long count, boolean hasBonusBallMatch, Money reward) {
         this.count = count;
         this.reward = reward;
+        this.hasBonusBallMatch = hasBonusBallMatch;
     }
 
-    public static Rank from(long count, boolean bonusBallMatch) {
-        if (bonusBallMatch && count == FIVE) {
-            return Rank.SECOND;
-        }
+    public static Rank from(long count, boolean hasBonusBallMatch) {
         return Arrays.stream(Rank.values())
                 .filter(item -> item.getCount() == count)
+                .filter(item -> item.hasBonusBallMatch == hasBonusBallMatch)
                 .findAny()
                 .orElse(NOTHING);
     }
--- src/test/java/domain/RankTest.java
+++ src/test/java/domain/RankTest.java
@@ -6,11 +6,48 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RankTest {
-    @DisplayName("로또 번호를 맞춘 개수에 맞는 Rank 객체가 리턴되는지 테스트한다.")
+    @DisplayName("로또 번호를 맞춘 개수에 맞는 Rank 객체가 리턴되는지 테스트한다. - 1등")
     @Test
-    public void rankFromTest() {
+    public void firstRankFromTest() {
         Rank rank = Rank.from(6, false);
-        assertThat(rank).isInstanceOf(Rank.class);
+        assertThat(rank).isEqualTo(Rank.FIRST);
+    }
+
+    @DisplayName("로또 번호를 맞춘 개수에 맞는 Rank 객체가 리턴되는지 테스트한다. - 2등")
+    @Test
+    public void secondRankFromTest() {
+        Rank rank = Rank.from(5, true);
+        assertThat(rank).isEqualTo(Rank.SECOND);
+    }
+
+    @DisplayName("로또 번호를 맞춘 개수에 맞는 Rank 객체가 리턴되는지 테스트한다. - 3등")
+    @Test
+    public void thirdRankFromTest() {
+        Rank rank = Rank.from(5, false);
+        assertThat(rank).isEqualTo(Rank.THIRD);
+    }
+
+    @DisplayName("로또 번호를 맞춘 개수에 맞는 Rank 객체가 리턴되는지 테스트한다. - 4등")
+    @Test
+    public void fourthRankFromTest() {
+        Rank rank = Rank.from(4, false);
+        assertThat(rank).isEqualTo(Rank.FOURTH);
+    }
+
+    @DisplayName("로또 번호를 맞춘 개수에 맞는 Rank 객체가 리턴되는지 테스트한다. - 5등")
+    @Test
+    public void fifthRankFromTest() {
+        Rank rank = Rank.from(3, false);
+        assertThat(rank).isEqualTo(Rank.FIFTH);
+    }
+
+    @DisplayName("로또 번호를 맞춘 개수에 맞는 Rank 객체가 리턴되는지 테스트한다. - 0 ~ 2개 맞출 시 없음")
+    @Test
+    public void noRankFromTwoMatchingCountTest() {
+        for (int i = 0; i < 3; i++) {
+            Rank rank = Rank.from(i, false);
+            assertThat(rank).isEqualTo(Rank.NOTHING);
+        }
     }
 
     @DisplayName("1등 상금 리턴 테스트")
@@ -27,10 +64,33 @@ public class RankTest {
         assertThat(rank.getReward()).isEqualTo(Money.from(30_000_000));
     }
 
-    @DisplayName("상금이 없는 경우")
+    @DisplayName("3등 상금 리턴 테스트")
     @Test
-    public void compareNoRewardTest() {
-        Rank rank = Rank.from(1, false);
-        assertThat(rank.getReward()).isEqualTo(Money.from(0));
+    public void compareThirdRewardTest() {
+        Rank rank = Rank.from(5, false);
+        assertThat(rank.getReward()).isEqualTo(Money.from(1_500_000));
+    }
+
+    @DisplayName("4등 상금 리턴 테스트")
+    @Test
+    public void compareFourthRewardTest() {
+        Rank rank = Rank.from(4, false);
+        assertThat(rank.getReward()).isEqualTo(Money.from(50_000));
+    }
+
+    @DisplayName("5등 상금 리턴 테스트")
+    @Test
+    public void compareFifthRewardTest() {
+        Rank rank = Rank.from(3, false);
+        assertThat(rank.getReward()).isEqualTo(Money.from(5_000));
+    }
+
+    @DisplayName("Nothing Rank 상금 리턴 테스트")
+    @Test
+    public void noRewardOnNothingRankTest() {
+        for (int i = 0; i < 3; i++) {
+            Rank rank = Rank.from(i, false);
+            assertThat(rank.getReward()).isEqualTo(Money.from(0));
+        }
     }
 }
