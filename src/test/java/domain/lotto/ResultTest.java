fe9baf314b9166ff0e3dad4a53542484108ab125
Eugine Park <pkeugine@gmail.com>
2021-03-01 18:48:39 +0900

    test(Result): Result 관련 객체 생성 및 당첨 등수 계산 테스트케이스 추가

--- src/test/java/domain/lotto/ResultTest.java
+++ src/test/java/domain/lotto/ResultTest.java
@@ -1,28 +1,57 @@
 package domain.lotto;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ResultTest {
 
-    private Result result;
+    @DisplayName("객체 생성 성공")
+    @Test
+    void generate_validInput_successful() {
+        assertThatCode(() -> new Result(getLottoNumbers(), getValidBonusNumber()))
+                .doesNotThrowAnyException();
+    }
 
-//    @BeforeEach
-//    void setUp() {
-//        result = new Result(createValidWinningNumbers(), createValidLottoTickets());
-//    }
+    @DisplayName("객체 생성 실패 : 보너스 번호가 당첨 번호에 포함")
+    @Test
+    void generate_duplicateNumber_exceptionThrown() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Result(getLottoNumbers(), getInvalidBonusNumber()))
+                .withMessageContaining("포함");
+    }
 
-//    @DisplayName("총 수익률 계산 성공")
-//    @Test
-//    void getProfitRate_success() {
-//        assertThat(result.calculateProfitRate()).isEqualTo(2_030_005_000.0 / 3000.0);
-//    }
+    @DisplayName("당첨 번호와 보너스 번호를 이용하여 계산 성공")
+    @Test
+    void calculate_successful() {
+        final Result result = new Result(getLottoNumbers(), getValidBonusNumber());
+        assertThatCode(() -> result.calculate(getValidLottoTickets()))
+                .doesNotThrowAnyException();
+    }
 
-    List<LottoTicket> createValidLottoTickets() {
+    private List<Integer> getLottoNumbers() {
+        return Arrays.asList(1, 2, 3, 4, 5, 6);
+    }
+
+    private int getValidBonusNumber() {
+        return 10;
+    }
+
+    private int getInvalidBonusNumber() {
+        return 1;
+    }
+
+    private List<List<Integer>> getLottoTicketsNumbers() {
         return Arrays.asList(
-                LottoTicket.generateManual(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                LottoTicket.generateManual(Arrays.asList(4, 5, 6, 7, 8, 9)),
-                LottoTicket.generateManual(Arrays.asList(1, 2, 3, 4, 5, 9))
+                getLottoNumbers()
         );
+    }
+
+    private LottoTickets getValidLottoTickets() {
+        return new LottoTickets(getLottoTicketsNumbers(), 1);
     }
 }
