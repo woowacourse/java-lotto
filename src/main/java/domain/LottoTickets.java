7207b7194fc927f1f28c1cb75b3921966983bd91
Kimun Kim <kkm97351@gmail.com>
2021-03-04 23:11:00 +0900

    feat: LottoTickets 클래스 생성
    - LottoTicket들을 담은 일급컬렉션 LottoTickets를 생성하였습니다.

---
+++ src/main/java/domain/LottoTickets.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+
+public class LottoTickets {
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+}
