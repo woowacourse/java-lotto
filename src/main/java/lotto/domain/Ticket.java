55cb6b15641c76ced1a521ca4663aa9bd69af8bd
summerlunaa <summerlunaa9@gmail.com>
2022-02-22 15:47:10 +0900

    feat: LottoTicket 생성 기능

---
+++ src/main/java/lotto/domain/Ticket.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Ticket {
+
+    private static final int PICKUP_COUNT = 6;
+
+    private final List<LottoNumber> ticket;
+
+    private Ticket(List<LottoNumber> ticket) {
+        this.ticket = ticket;
+    }
+
+    public Ticket getTicketByAuto() {
+        List<LottoNumber> ticket = IntStream.range(0, PICKUP_COUNT)
+                .mapToObj(shuffleLottoNumbers()::get)
+                .collect(Collectors.toList());
+        return new Ticket(ticket);
+    }
+
+    private List<LottoNumber> shuffleLottoNumbers() {
+        List<LottoNumber> lottoNumbers = LottoNumber.getLottoNumbers();
+        Collections.shuffle(lottoNumbers);
+        return lottoNumbers;
+    }
+}
