15d6152d1791f622f8df46f328eeffc366c6ca44
sure-why-not <pkse1213@gmail.com>
2022-02-22 21:24:34 +0900

    feat : 구매 금액 만큼의 로또를 발생시키는 기능 구현

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -2,13 +2,24 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class LottoGame {
 
 	public static final int LOTTO_NUMBER_SIZE = 6;
+	public static final int LOTTO_PRICE = 1000;
 
-	public Lotto createLotto() {
+	public List<Lotto> purchaseLottos(int money) {
+		int lottoCount = money / LOTTO_PRICE;
+		List<Lotto> lottos = new ArrayList<>();
+		for (int i = 0; i < lottoCount; i++) {
+			lottos.add(createLotto());
+		}
+		return lottos;
+	}
+
+	private Lotto createLotto() {
 		Set<LottoNumber> lottoNumbers = new HashSet<>();
 
 		while (lottoNumbers.size() != LOTTO_NUMBER_SIZE) {
--- src/test/java/domain/LottoGameTest.java
+++ src/test/java/domain/LottoGameTest.java
@@ -2,18 +2,32 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoGameTest {
 
 	@Test
-	@DisplayName("랜덤 방식의 로또를 생성하는 경우")
+	@DisplayName("랜덤 방식의 로또를 한 개 생성하는 경우")
 	void createRandomLotto() {
 		LottoGame lottoGame = new LottoGame();
+		int money = 1000;
 
-		Lotto lotto = lottoGame.createLotto();
+		List<Lotto> lottos = lottoGame.purchaseLottos(money);
 
-		assertThat(lotto).isNotNull();
+		assertThat(lottos).isNotNull();
+	}
+
+	@Test
+	@DisplayName("구입 금액 만큼의 로또를 생성하는 경우")
+	void createRandomLottoByMoney() {
+		LottoGame lottoGame = new LottoGame();
+		int money = 5000;
+
+		List<Lotto> lottos = lottoGame.purchaseLottos(money);
+
+		assertThat(lottos.size()).isEqualTo(money / 1000);
 	}
 }
