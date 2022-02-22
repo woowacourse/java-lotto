c611e43dd16fba5d9bb596f74eaf758cfb76f8a6
skullKim <kyk990328@gmail.com>
2022-02-22 19:27:55 +0900

    feat: 로또 번호 검증 기능 구현

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.List;
+
+public class Lotto {
+
+	private final List<Integer> numbers;
+
+	public Lotto(List<Integer> numbers) {
+		validateNumbers(numbers);
+		this.numbers = numbers;
+	}
+
+	private void validateNumbers(List<Integer> numbers) {
+		validateSize(numbers.size());
+		validateRange(numbers);
+	}
+
+	private void validateSize(int size) {
+		if (size != 6) {
+			throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+		}
+	}
+
+	private void validateRange(List<Integer> numbers) {
+		numbers.forEach(this::checkNumberRange);
+	}
+
+
+	private void checkNumberRange(final int numbers) {
+		if(1 > numbers || numbers > 45) {
+			throw new IllegalArgumentException("로또의 각 번호는 1~45 사이여야 합니다");
+		}
+	}
+}
---
+++ src/test/java/LottoGameTest.java
@@ -0,0 +1,56 @@
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import domain.Lotto;
+
+public class LottoGameTest {
+
+	@Nested
+	@DisplayName("로또 용지 하나에 포함된 숫자가")
+	class CheckNumbersInLottoTest {
+		@Test
+		@DisplayName("6개이면 올바른 로또이다.")
+		void valid_lotto_number(){
+			assertThatNoException().isThrownBy(() -> {
+				new Lotto(Arrays.asList(1,2,3,4,5,6));
+			});
+		}
+
+		@Test
+		@DisplayName("6개가 아니면 올바르지 않은 로또이다.")
+		void invalid_lotto_number() {
+			assertThatThrownBy(() -> {
+				new Lotto(Arrays.asList(1,2,3,4,5));
+			}).isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("로또 번호는 6개여야 합니다.");
+		}
+	}
+
+	@Nested
+	@DisplayName("로또 용지에 포함되 있는 숫자들이")
+	class CheckNumberRangeTest {
+
+		@Test
+		@DisplayName("1~45 사이이면 통과")
+		void valid_lotto_number_range() {
+			assertThatNoException().isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 4, 5, 6, 45)));
+		}
+
+		@Test
+		@DisplayName("1~45 사이가 아니면 실패")
+		void invalid_lotto_number_range() {
+			assertThatThrownBy(() -> {
+				new Lotto(Arrays.asList(-1, 0, 46, 3, 4, 5));
+			}).isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("로또의 각 번호는 1~45 사이여야 합니다");
+		}
+	}
+
+
+
+}
