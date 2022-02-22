8dbfc8ab2d7e06e0a921772938a35ec418e521d8
유영우 <fbduddn97@naver.com>
2022-02-22 18:58:00 +0900

    feat: 로또 목록 생성 기능 구현

---
+++ src/main/java/domain/Lottos.java
@@ -0,0 +1,22 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+	private final LottoGenerator lottoGenerator = new RandomLottoGenerator();
+	private final List<Lotto> lottos;
+
+	public Lottos(int count) {
+		this.lottos = generateLottos(count);
+	}
+
+	private List<Lotto> generateLottos(int count) {
+		List<Lotto> lottos = new ArrayList<>();
+		for (int i = 0; i < count; i++) {
+			lottos.add(new Lotto(lottoGenerator));
+		}
+		return lottos;
+	}
+}
---
+++ src/test/java/domain/LottosTest.java
@@ -0,0 +1,15 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottosTest {
+
+	@DisplayName("로또목록 생성 테스트")
+	@Test
+	void initTest() {
+		assertDoesNotThrow(() -> new Lottos(14));
+	}
+}
