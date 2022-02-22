c62c935d7bace295f4999c2860882c9907201cdc
yhh1056 <yhh1056@naver.com>
2022-02-22 17:08:08 +0900

    test: 로또 번호 자릿수 엣지 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,8 +8,16 @@ import org.junit.jupiter.api.Test;
 class LottoTest {
 
     @Test
-    @DisplayName("로또 번호는 6자리 숫자가 전달되지 않으면 예외가 발생한다.")
-    void throwExceptionWhenNotSixSizeNumbers() {
+    @DisplayName("로또 번호는 6자리 숫자로 생성된다.")
+    void createSixSizeNumbers() {
+        List<Number> numbers = List.of(number(1), number(2), number(3), number(4), number(5), number(6));
+
+        assertThat(new Lotto(numbers)).isNotNull();
+    }
+
+    @Test
+    @DisplayName("로또 번호는 5자리 숫자가 전달되면 예외가 발생한다.")
+    void throwExceptionWhenFiveSizeNumbers() {
         List<Number> numbers = List.of(number(1), number(2), number(3), number(4), number(5));
 
         assertThatIllegalArgumentException()
@@ -18,11 +26,13 @@ class LottoTest {
     }
 
     @Test
-    @DisplayName("로또 번호는 6자리 숫자로 생성된다.")
-    void createSixSizeNumbers() {
-        List<Number> numbers = List.of(number(1), number(2), number(3), number(4), number(5), number(6));
+    @DisplayName("로또 번호는 7자리 숫자가 전달되면 예외가 발생한다.")
+    void throwExceptionWhenSevenSizeNumbers() {
+        List<Number> numbers = List.of(number(1), number(2), number(3), number(4), number(5), number(6), number(7));
 
-        assertThat(new Lotto(numbers)).isNotNull();
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Lotto(numbers))
+            .withMessageMatching("로또 번호는 6자리 이어야 한다.");
     }
 
     private Number number(int number) {
