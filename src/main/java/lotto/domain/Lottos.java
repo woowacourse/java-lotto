5bd9118c498a604deb7fe51d8c8197f3e1c30051
Hyunta <mohai2618@gmail.com>
2022-02-22 16:45:56 +0900

    feat(Lottos) : 입력 금액으로 구매할 개수를 구하는 기능 구현

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private List<Lotto> lottos;
+
+    public Lottos(Money money) {
+        this.lottos = new ArrayList<>();
+        purchaseLotto(money);
+    }
+
+    private void purchaseLotto(Money money) {
+        for (int i = 0; i < getLottoCount(money); i++) {
+            lottos.add(new Lotto());
+        }
+    }
+
+    private int getLottoCount(Money money) {
+        return money.getMoney() / 1000;
+    }
+
+    public int getLottosSize() {
+        return lottos.size();
+    }
+}
