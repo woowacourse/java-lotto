3660dcd411ce7f9836a713238e2ca40f7fab14d9
jaejae-yoo <wotj102@gmail.com>
2022-02-22 16:35:26 +0900

    feat: 투입 금액이 천원 단위인지 검증
    refactor: 문자열 상수화

--- src/main/java/model/LottoCount.java
+++ src/main/java/model/LottoCount.java
@@ -1,15 +1,29 @@
 package model;
 
 public class LottoCount {
-    final String REGEX = "[0-9]+";
+    private static final String REGEX_NUMBER = "[0-9]+";
+    private static final int UNIT = 1000;
+    private int money;
 
     public LottoCount(String number) {
         validateInsertMoneyBlank(number);
         validateInputMoneyNumber(number);
+        this.money = makeMoneyToNumber(number);
+    }
+
+    private int makeMoneyToNumber(String number) {
+        validateThousandUnitInputMoney(number);
+        return Integer.parseInt(number);
+    }
+
+    private void validateThousandUnitInputMoney(String number) {
+        if (Integer.parseInt(number) % UNIT != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validateInputMoneyNumber(String number) {
-        if (!number.matches(REGEX)) {
+        if (!number.matches(REGEX_NUMBER)) {
             throw new IllegalArgumentException();
         }
     }
@@ -18,6 +32,10 @@ public class LottoCount {
         if (number == null || number.isBlank()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public int getMoney() {
+        return money;
     }
 }
 
--- src/test/java/model/LottoCountTest.java
+++ src/test/java/model/LottoCountTest.java
@@ -1,8 +1,10 @@
 package model;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -21,8 +23,23 @@ public class LottoCountTest {
     @ParameterizedTest
     @ValueSource(strings ={"azpi","++","greeanlawn","1dksl","-1"})
     @DisplayName("투입 금액이 숫자가 아닌 경우 검증")
-    void validateInputMoneyIsNumber(String number){
+    void validateInputMoneyIsNumber(String number) {
         assertThatThrownBy(() -> new LottoCount(number))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100","1001","100001"})
+    @DisplayName("투입 금액이 천원 단위가 아닌 경우인")
+    void validateNotThousandUnitInputMoney(String number) {
+        assertThatThrownBy(() -> new LottoCount(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("투입 금액이 천원 단위 경우")
+    void validateThousandUnitInputMoney() {
+        LottoCount lottoCount = new LottoCount("1000");
+        assertThat(lottoCount.getMoney()).isEqualTo(1000);
     }
 }
