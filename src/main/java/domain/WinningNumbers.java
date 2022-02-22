147cd165fda39f0be95d6a59ba6ee6604a2c2728
이한울 <dkfghg3@naver.com>
2022-02-22 22:48:27 +0900

    feat: 당첨 번호 중복 예외 처리 구현

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -1,60 +1,19 @@
 package domain;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class WinningNumbers {
 
-	private final List<Integer> winningNumbers;
+	private final Lotto winningNumbers;
+	private final Number bonusNumber;
 
-	private WinningNumbers(List<Integer> winningNumbers) {
-		checkWinningNumbersRange(winningNumbers);
-		checkWinningNumbersSize(winningNumbers);
-		checkDuplicateWinningNumbers(winningNumbers);
+	public WinningNumbers(Lotto winningNumbers, Number bonus) {
+		checkDuplicateBonusNumber(winningNumbers, bonus);
 		this.winningNumbers = winningNumbers;
+		this.bonusNumber = bonus;
 	}
 
-	public static WinningNumbers from(String[] userInput) {
-		checkNotDigit(userInput);
-		return new WinningNumbers(toWinningNumbersList(userInput));
-	}
-
-	private void checkDuplicateWinningNumbers(List<Integer> winningNumbers) {
-		boolean duplicated = winningNumbers.stream()
-			.distinct()
-			.count() != winningNumbers.size();
-
-		if (duplicated) {
-			throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다");
-		}
-	}
-
-	private void checkWinningNumbersSize(List<Integer> winningNumbers) {
-		if (winningNumbers.size() != 6) {
-			throw new IllegalArgumentException("당첨 번호는 6개의 숫자여야 합니다");
-		}
-	}
-
-	private static List<Integer> toWinningNumbersList(String[] userInput) {
-		return Arrays.stream(userInput)
-			.mapToInt(Integer::parseInt)
-			.boxed()
-			.collect(Collectors.toList());
-	}
-
-	private static void checkWinningNumbersRange(List<Integer> winningNumbers) {
-		boolean isOutOfRange = winningNumbers.stream().anyMatch(number -> number < 1 || number > 45);
-		if (isOutOfRange) {
-			throw new IllegalArgumentException("당첨 번호는 1 ~ 45의 숫자여야 합니다");
-		}
-	}
-
-	private static void checkNotDigit(String[] userInput) {
-		boolean isDigit = Arrays.stream(userInput).allMatch(input -> input.matches("^[0-9]*$"));
-
-		if (!isDigit) {
-			throw new IllegalArgumentException("당첨 번호는 숫자여야 합니다");
+	private void checkDuplicateBonusNumber(Lotto winningNumbers, Number bonus) {
+		if (winningNumbers.isContain(bonus)) {
+			throw new IllegalArgumentException("당첨 번호와 보너스 번호는 중복될 수 없습니다");
 		}
 	}
 }
--- src/test/java/domain/WinningNumbersTest.java
+++ src/test/java/domain/WinningNumbersTest.java
@@ -2,56 +2,24 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningNumbersTest {
 
 	@Test
-	@DisplayName("숫자가 아닌 경우")
-	void checkNotDigit() {
-		//given
-		String[] input = new String[] {"1", "a", "11", "b", "45", "c"};
+	@DisplayName("당첨 번호와 보너스 넘버가 중복되면 예외를 발생")
+	void checkDuplicateBonusNumber() {
+		Lotto lotto = new Lotto(Stream.of(1, 5, 9, 11, 16, 35)
+			.map(Number::new)
+			.collect(Collectors.toList()));
 
-		//then
-		assertThatThrownBy(() -> WinningNumbers.from(input))
+		Number bonus = new Number(11);
+		assertThatThrownBy(() -> new WinningNumbers(lotto,bonus))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("당첨 번호는 숫자여야 합니다");
-	}
-
-	@Test
-	@DisplayName("당첨 번호의 범위를 벗어난 경우")
-	void checkWinningNumbersRange() {
-		//given
-		String[] input = new String[] {"1", "7", "11", "30", "45", "80"};
-
-		//then
-		assertThatThrownBy(() -> WinningNumbers.from(input))
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("당첨 번호는 1 ~ 45의 숫자여야 합니다");
-	}
-
-	@Test
-	@DisplayName("입력한 숫자가 6개가 아닌 경우")
-	void checkWinningNumbersSize() {
-		//given
-		String[] input = new String[] {"1", "7", "11", "30"};
-
-		//then
-		assertThatThrownBy(() -> WinningNumbers.from(input))
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("당첨 번호는 6개의 숫자여야 합니다");
-	}
-
-	@Test
-	@DisplayName("중복된 숫자를 입력한 경우")
-	void checkDuplicateWinningNumbers() {
-		//given
-		String[] input = new String[] {"1", "7", "11", "30", "30", "34"};
-
-		//then
-		assertThatThrownBy(() -> WinningNumbers.from(input))
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("중복된 숫자를 입력할 수 없습니다");
+			.hasMessage("당첨 번호와 보너스 번호는 중복될 수 없습니다");
 	}
 }
