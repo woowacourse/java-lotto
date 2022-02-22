4ec5ca7960ef54513e67db1a5d3563b941b28fb5
jayjaehunchoi <wogns0108@naver.com>
2022-02-22 17:12:37 +0900

    refactor : 숫자 범위 검증 메소드 분리

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -5,9 +5,13 @@ public class LottoNumber {
     private final int number;
 
     public LottoNumber(final int number) {
+        checkLottoNumberRange(number);
+        this.number = number;
+    }
+
+    private void checkLottoNumberRange(final int number) {
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException("");
         }
-        this.number = number;
     }
 }
