5682d30b1ccd6de10ac01444a4e2a82a8de1dddc
= <rerub0831@gmail.com>
2022-02-22 17:48:26 +0900

    feat: 로또 숫자 객체 구현

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -1,4 +1,37 @@
 package domain;
 
-public class LottoNumber {
+import java.util.Objects;
+
+public class LottoNumber implements Comparable<LottoNumber> {
+    private final int number;
+
+    public LottoNumber(int number) {
+        this.number = number;
+    }
+
+    private int getNumber() {
+        return this.number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public int compareTo(LottoNumber o) {
+        return java.lang.Integer.compare(number, o.getNumber());
+    }
 }
---
+++ src/test/java/domain/LottoNumberTest.java
@@ -0,0 +1,15 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class LottoNumberTest {
+    @Test
+    void 로또_번호_일치_여부_검사() {
+        LottoNumber lottoNumber = new LottoNumber(1);
+        LottoNumber lottoNumber2 = new LottoNumber(1);
+
+        assertThat(lottoNumber).isEqualTo(lottoNumber2);
+    }
+}
