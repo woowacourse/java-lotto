b5c147357c043773d685b799ba116d373068ed2d
Richard JEON <ztzy1907@gmail.com>
2022-02-22 17:31:44 +0900

    feat(lottonumber): 로또넘버 생성 및 동등성 검증 기능 추가

---
+++ src/main/java/domain/LottoNumber.java
@@ -0,0 +1,53 @@
+package domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    private static final String ERROR_MESSAGE_FOR_OUT_OF_RANGE_NUMBER = "1에서 45 사이의 값을 입력해주세요";
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
+    private final int number;
+
+    public LottoNumber(int number) {
+        validateRange(number);
+        this.number = number;
+    }
+
+    private void validateRange(int number) {
+        if (isInvalidRange(number)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_OUT_OF_RANGE_NUMBER);
+        }
+    }
+
+    private boolean isInvalidRange(int number) {
+        return MAXIMUM_LOTTO_NUMBER < number || number < MINIMUM_LOTTO_NUMBER;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) object;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumber{" +
+                "number=" + number +
+                '}';
+    }
+}
---
+++ src/test/java/domain/LottoNumberTest.java
@@ -0,0 +1,42 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LottoNumberTest {
+
+    @ParameterizedTest(name = "1 ~ 45 사이 로또 넘버 생성 : {0}")
+    @ValueSource(ints = {1, 45})
+    void createLottoNumber(int number) {
+        // given & when
+        LottoNumber lottoNumber = new LottoNumber(number);
+
+        // then
+        assertThat(lottoNumber.getNumber()).isEqualTo(number);
+    }
+
+    @ParameterizedTest(name = "1 ~ 45 범위 외 숫자가 전달되면, IAE를 던진다")
+    @ValueSource(ints = {-1, 0, 46})
+    void createLottoNumberOutOfRangeShouldFail(int number) {
+        assertThatThrownBy(() -> new LottoNumber(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("1에서 45 사이의 값을 입력해주세요");
+    }
+
+    @Test
+    void lottoNumberEqualityTest() {
+        // given & when
+        LottoNumber lottoNumber1 = new LottoNumber(1);
+        LottoNumber lottoNumber2 = new LottoNumber(1);
+
+        // then
+        assertThat(lottoNumber1).isEqualTo(lottoNumber2);
+    }
+
+}
