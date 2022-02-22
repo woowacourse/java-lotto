ecd8d6f571eb461d3c1c591a721ebaafaef28b7a
jayjaehunchoi <wogns0108@naver.com>
2022-02-22 23:59:53 +0900

    refactor : calculateYield 메서드 분리

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -12,14 +12,21 @@ public class LottoResult {
     }
 
     public double calculateYield() {
-        final long totalReward = rankResults.entrySet().stream()
-                .map(result -> Rank.calculateMoney(result.getKey(), result.getValue()))
-                .reduce(0L, Long::sum);
-
-        final int purchaseMoney = rankResults.entrySet().stream()
-                .map(Entry::getValue)
-                .reduce(0, Integer::sum) * Lotto.LOTTO_PURCHASE_MONEY;
+        final long totalReward = calculateTotalReward();
+        final int purchaseMoney = calculatePurchaseMoney();
 
         return totalReward / (double) purchaseMoney;
+    }
+
+    private Long calculateTotalReward() {
+        return rankResults.entrySet().stream()
+                .map(result -> Rank.calculateMoney(result.getKey(), result.getValue()))
+                .reduce(0L, Long::sum);
+    }
+
+    private int calculatePurchaseMoney() {
+        return rankResults.entrySet().stream()
+                .map(Entry::getValue)
+                .reduce(0, Integer::sum) * Lotto.LOTTO_PURCHASE_MONEY;
     }
 }
