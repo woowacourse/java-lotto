41444710ffd3586fc7809b4a7a863b37e7622bae
sure-why-not <pkse1213@gmail.com>
2022-02-23 00:23:39 +0900

    feat : 당첨 기준 정보와 금액을 담고있는 로또 리워드 기능 구현

---
+++ src/main/java/domain/LottoReward.java
@@ -0,0 +1,33 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum LottoReward {
+
+	FIRST(6, 2000000000),
+	SECOND(5, 30000000),
+	THIRD(5, 1500000),
+	FOURTH(4, 50000),
+	FIFTH(3, 5000),
+	NONE(0, 0);
+
+	private final int matchCount;
+	private final int price;
+
+	LottoReward(int matchCount, int price) {
+		this.matchCount = matchCount;
+		this.price = price;
+	}
+
+	public static LottoReward find(int matchCount, boolean hasBonus) {
+		return Arrays.stream(values())
+			.filter(reward -> reward.matchCount == matchCount)
+			.filter(reward -> !reward.equals(SECOND) || hasBonus)
+			.findFirst()
+			.orElse(NONE);
+	}
+
+	public int getPrice() {
+		return price;
+	}
+}
---
+++ src/test/java/domain/LottoRewardTest.java
@@ -0,0 +1,19 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LottoRewardTest {
+
+	@ParameterizedTest
+	@CsvSource(value = {"6:2000000000:false", "5:30000000:true", "5:1500000:false", "0:0:false"}, delimiter = ':')
+	@DisplayName("유효한 로또 리워드를 반환하는 경우")
+	void returnLottoReward(int matchCount, int price, boolean hasBonus) {
+		LottoReward lottoReward = LottoReward.find(matchCount, hasBonus);
+
+		assertThat(lottoReward.getPrice()).isEqualTo(price);
+	}
+}
