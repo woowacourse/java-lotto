f0f230ec6f33c0e88423969d3cf37701ff126fc8
wilgur513 <wilgur513@naver.com>
2022-02-22 14:57:23 +0900

    feat: 중복된 당첨 번호 분리 실패

--- src/main/java/LottoNumberParser.java
+++ src/main/java/LottoNumberParser.java
@@ -4,17 +4,29 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class LottoNumberParser {
+
     private static final int LOTTO_NUMBER_LENGTH = 6;
+
+    static final String INVALID_LOTTO_NUMBER_LENGTH_MESSAGE = "당첨 번호는 6개여야 합니다.";
+    static final String DUPLICATED_LOTTO_NUMBER_MESSAGE = "중복된 당첨 번호는 허용하지 않습니다.";
 
     public List<Integer> parse(String numbers) {
         String[] tokens = numbers.split(", ");
         checkLottoNumberLength(tokens);
-        return Stream.of(tokens).map(Integer::valueOf).collect(toList());
+        List<Integer> result = Stream.of(tokens).map(Integer::valueOf).collect(toList());
+        checkDuplicatedLottoNumber(result);
+        return result;
     }
 
     private void checkLottoNumberLength(String[] tokens) {
         if (tokens.length != LOTTO_NUMBER_LENGTH) {
-            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_LENGTH_MESSAGE);
+        }
+    }
+
+    private void checkDuplicatedLottoNumber(List<Integer> result) {
+        if (result.stream().distinct().count() != result.size()) {
+            throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBER_MESSAGE);
         }
     }
 }
--- src/test/java/LottoNumberParserTest.java
+++ src/test/java/LottoNumberParserTest.java
@@ -1,36 +1,40 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoNumberParserTest {
+
+    private LottoNumberParser parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new LottoNumberParser();
+    }
 
     @Test
     @DisplayName("로또 당첨 번호 분리")
     void splitWinningNumber() {
-        LottoNumberParser parser = new LottoNumberParser();
-
         assertThat(parser.parse("1, 2, 3, 4, 5, 6")).contains(1, 2, 3, 4, 5, 6);
     }
 
-    @Test
-    @DisplayName("6개 이하 당첨 번호 분리 실패")
-    void splitWinningNumberUnderSix() {
-        LottoNumberParser parser = new LottoNumberParser();
-
-        assertThatThrownBy(() -> parser.parse("1, 2, 3, 4, 5"))
+    @ParameterizedTest(name = "잘못된 길이의 당첨 번호 분리 실패 : 입력값 - {0}")
+    @ValueSource(strings = {"1, 2, 3, 4, 5", "1, 2, 3, 4, 5, 6, 7"})
+    void splitWinningNumberUnderSix(String numbers) {
+        assertThatThrownBy(() -> parser.parse(numbers))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("당첨 번호는 6개여야 합니다.");
+                .hasMessage(LottoNumberParser.INVALID_LOTTO_NUMBER_LENGTH_MESSAGE);
     }
 
     @Test
-    @DisplayName("6개 이상 당첨 번호 분리 실패")
-    void splitWinningNumberOverSix() {
-        LottoNumberParser parser = new LottoNumberParser();
-
-        assertThatThrownBy(() -> parser.parse("1, 2, 3, 4, 5, 6, 7"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("당첨 번호는 6개여야 합니다.");
+    @DisplayName("중복된 당첨 번호 분리 실패")
+    void failSplitWithDuplicateLottoNumber() {
+        assertThatThrownBy(() -> parser.parse("1, 2, 3, 3, 4, 5"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(LottoNumberParser.DUPLICATED_LOTTO_NUMBER_MESSAGE);
     }
 }
