1b5064140df476fcd612ac8612e31075983a38e9
양동주 <djwhy5510@gmail.com>
2022-02-22 14:44:18 +0900

    feat: 당첨 번호 갯수 6개 아닌 경우 예외 발생

--- src/main/java/LottoNumberParser.java
+++ src/main/java/LottoNumberParser.java
@@ -4,9 +4,17 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class LottoNumberParser {
+    private static final int LOTTO_NUMBER_LENGTH = 6;
 
     public List<Integer> parse(String numbers) {
         String[] tokens = numbers.split(", ");
+        checkLottoNumberLength(tokens);
         return Stream.of(tokens).map(Integer::valueOf).collect(toList());
+    }
+
+    private void checkLottoNumberLength(String[] tokens) {
+        if (tokens.length != LOTTO_NUMBER_LENGTH) {
+            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        }
     }
 }
--- src/test/java/LottoNumberParserTest.java
+++ src/test/java/LottoNumberParserTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,4 +14,23 @@ public class LottoNumberParserTest {
         assertThat(parser.parse("1, 2, 3, 4, 5, 6")).contains(1, 2, 3, 4, 5, 6);
     }
 
+    @Test
+    @DisplayName("6개 이하 당첨 번호 분리 실패")
+    void splitWinningNumberUnderSix() {
+        LottoNumberParser parser = new LottoNumberParser();
+
+        assertThatThrownBy(() -> parser.parse("1, 2, 3, 4, 5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("당첨 번호는 6개여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("6개 이상 당첨 번호 분리 실패")
+    void splitWinningNumberOverSix() {
+        LottoNumberParser parser = new LottoNumberParser();
+
+        assertThatThrownBy(() -> parser.parse("1, 2, 3, 4, 5, 6, 7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("당첨 번호는 6개여야 합니다.");
+    }
 }
