9fe71c04186e8fcbab9fc186eed38ecda693bee1
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-22 16:41:59 +0900

    feat(OutputView): print lotto purchase info

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,40 @@
+package view;
+
+import domain.Lotto;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+
+    private static final String lineSeparator = System.lineSeparator();
+
+    public static void printPurchaseInfo(List<Lotto> lottos) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(lottos.size())
+                .append("개를 구매했습니다.")
+                .append(lineSeparator);
+
+       lottos.stream()
+               .map(OutputView::formatLottoNumbers)
+               .forEach(builder::append);
+
+        builder.append(lineSeparator);
+
+        print(builder.toString());
+    }
+
+    private static String formatLottoNumbers(Lotto lotto) {
+
+        String lottoNumFormat = lotto.getNumbers()
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+
+        return "[" + lottoNumFormat + "]";
+    }
+
+    private static void print(String value) {
+        System.out.println(value);
+    }
+}
