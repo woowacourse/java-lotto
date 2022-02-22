11f1b8ada1819cbcd84229ae0ffdbc5d5f12b306
yhh1056 <yhh1056@naver.com>
2022-02-22 17:00:37 +0900

    refactor: 로또의 숫자를 Number 클래스로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,27 +5,12 @@ import java.util.List;
 public class Lotto {
 
     private static final int LOTTO_SIZE = 6;
-    private static final int MINIMUM_NUMBER = 1;
-    private static final int MAXIMUM_NUMBER = 45;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Number> numbers) {
         validateSize(numbers);
-        validateRangeOfNumbers(numbers);
     }
 
-    private void validateRangeOfNumbers(List<Integer> numbers) {
-        for (int number : numbers) {
-            validateRangeOfNumber(number);
-        }
-    }
-
-    private void validateRangeOfNumber(int number) {
-        if (number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER) {
-            throw new IllegalArgumentException("로또 번호는 1이상 45이하이어야 한다.");
-        }
-    }
-
-    private void validateSize(List<Integer> numbers) {
+    private void validateSize(List<Number> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6자리 이어야 한다.");
         }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -10,7 +10,7 @@ class LottoTest {
     @Test
     @DisplayName("로또 번호는 6자리 숫자가 전달되지 않으면 예외가 발생한다.")
     void throwExceptionWhenNotSixSizeNumbers() {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        List<Number> numbers = List.of(number(1), number(2), number(3), number(4), number(5));
 
         assertThatIllegalArgumentException()
             .isThrownBy(() -> new Lotto(numbers))
@@ -20,18 +20,12 @@ class LottoTest {
     @Test
     @DisplayName("로또 번호는 6자리 숫자로 생성된다.")
     void createSixSizeNumbers() {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Number> numbers = List.of(number(1), number(2), number(3), number(4), number(5), number(6));
 
         assertThat(new Lotto(numbers)).isNotNull();
     }
 
-    @Test
-    @DisplayName("로또 번호는 1이상 45이하가 아니면 예외가 발생한다.")
-    void throwExceptionWhenOutOfRange() {
-        List<Integer> numbers = List.of(-1, 46, 3, 4, 5, 7);
-
-        assertThatIllegalArgumentException()
-            .isThrownBy(() -> new Lotto(numbers))
-            .withMessageMatching("로또 번호는 1이상 45이하이어야 한다.");
+    private Number number(int number) {
+        return new Number(number);
     }
 }
