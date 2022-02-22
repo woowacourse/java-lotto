b63a199ccc7f21293715c431721bb35c1eb73370
= <rerub0831@gmail.com>
2022-02-22 18:13:30 +0900

    feat(MainController): 구현할 기능 주석처리

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import domain.LottoNumbers;
+import java.util.List;
+import view.InputView;
+
 public class MainController {
+    public void run(){
+        // money
+        int money = InputView.inputMoney();
+
+        // create lotto
+        List<LottoNumbers> lottoTickets = createLottoTickets(money);
+
+        // print lotto
+        // OutputView.printLottoTickets();
+        // lotto numbers
+        // bonus number
+        // result
+    }
+
+    private List<LottoNumbers> createLottoTickets(int money) {
+        return null;
+    }
 }
