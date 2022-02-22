5264da7682fae85ea060480c948a2296333d0a8d
hyunrrr <whgusrms96@gmail.com>
2022-02-22 19:51:10 +0900

    feat(InputView): add askWinningNumbers function

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
     private final static String QUESTION_MONEY_INPUT = "구매금액을 입력해 주세요.";
+    private final static String QUESTION_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
 
     Scanner scanner = new Scanner(System.in);
 
     public int askMoneyInput() {
         System.out.println(QUESTION_MONEY_INPUT);
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public List<Integer> askWinningNumbers() {
+        System.out.println(QUESTION_WINNING_NUMBERS);
+        String[] numbers = scanner.nextLine().split(", ");
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String number : numbers) {
+            winningNumbers.add(Integer.parseInt(number));
+        }
+        return winningNumbers;
     }
 }
 
