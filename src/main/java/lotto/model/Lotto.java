82d95a28cbd4fd424e2b725b2d3b374aa156ae2e
bomi <bonita9931@gmail.com>
2022-02-22 23:33:41 +0900

    feat: 로또의 번호는 정렬하는 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,11 +1,13 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        Collections.sort(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
