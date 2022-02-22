943c0eeb4a7924724991d334a312ef8f08132047
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-22 21:16:53 +0900

    feat(InputView): request winning numbers and bonus number
    - refactor with readline

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,10 +3,25 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int requestUserMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(scanner.nextLine());
+        return Integer.parseInt(readline());
+    }
+    
+    public static String requestWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return readline();
+    }
+    
+    public static int requestBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return Integer.parseInt(readline());
+    }
+
+    private static String readline() {
+        return scanner.nextLine();
     }
 }
