70c597ccd2ceebdec39270f7edc6c2536767b9a7
jayjaehunchoi <wogns0108@naver.com>
2022-02-22 22:38:35 +0900

    feat : Rank 일치 결과 받아 등수 반환 기능 구현 및 테스트

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,21 +1,34 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
 public enum Rank {
 
-    FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FOURTH(50000),
-    FIFTH(5000),
+    FIRST(2000000000, (hitCounts, bonus) -> hitCounts == 6),
+    SECOND(30000000, (hitCounts, bonus) -> hitCounts == 5 && bonus),
+    THIRD(1500000, (hitCounts, bonus) -> hitCounts == 5 && !bonus),
+    FOURTH(50000, (hitCounts, bonus) -> hitCounts == 4),
+    FIFTH(5000, (hitCounts, bonus) -> hitCounts == 3),
+    NOT_THING(0, (hitCounts, bonus) -> hitCounts < 3 && hitCounts >= 0),
     ;
 
     private final long reward;
+    private final BiFunction<Integer, Boolean, Boolean> expression;
 
-    Rank (final long reward) {
+    Rank(final long reward, final BiFunction<Integer, Boolean, Boolean> expression) {
         this.reward = reward;
+        this.expression = expression;
     }
 
     public static long calculateMoney(Rank currentRank, long count) {
         return currentRank.reward * count;
+    }
+
+    public static Rank calculateCurrentRank(int hitCounts, boolean bonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.expression.apply(hitCounts, bonus))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당하는 랭크가 없습니다."));
     }
 }
--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -3,7 +3,10 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RankTest {
 
@@ -50,5 +53,66 @@ public class RankTest {
         final long count = 2;
 
         assertThat(Rank.calculateMoney(fifth, count)).isEqualTo(10000);
+    }
+
+    @DisplayName("맞은 갯수와 보너스 여부를 가지고 랭크를 반환할 수 있다.")
+    @Nested
+    class CalculateRank {
+
+        @DisplayName("1등 반환")
+        @Test
+        void firstRank() {
+            final Rank rank = Rank.calculateCurrentRank(6, false);
+
+            assertThat(rank).isEqualTo(Rank.FIRST);
+        }
+
+        @DisplayName("2등 반환")
+        @Test
+        void secondRank() {
+            final Rank rank = Rank.calculateCurrentRank(5, true);
+
+            assertThat(rank).isEqualTo(Rank.SECOND);
+        }
+
+        @DisplayName("3등 반환")
+        @Test
+        void thirdRank() {
+            final Rank rank = Rank.calculateCurrentRank(5, false);
+
+            assertThat(rank).isEqualTo(Rank.THIRD);
+        }
+
+        @DisplayName("4등 반환")
+        @Test
+        void fourthRank() {
+            final Rank rank = Rank.calculateCurrentRank(4, false);
+
+            assertThat(rank).isEqualTo(Rank.FOURTH);
+        }
+
+        @DisplayName("5등 반환")
+        @Test
+        void fifthRank() {
+            final Rank rank = Rank.calculateCurrentRank(3, false);
+
+            assertThat(rank).isEqualTo(Rank.FIFTH);
+        }
+
+        @DisplayName("Nothing 반환")
+        @Test
+        void NothingRank() {
+            final Rank rank = Rank.calculateCurrentRank(2, false);
+
+            assertThat(rank).isEqualTo(Rank.NOT_THING);
+        }
+
+        @DisplayName("케이스에 존재하지 않음")
+        @Test
+        void exceptionRank() {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Rank.calculateCurrentRank(-1, false))
+                    .withMessage("[ERROR] 해당하는 랭크가 없습니다.");
+        }
     }
 }
