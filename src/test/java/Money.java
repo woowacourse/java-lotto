8eb9337415b9325f97d0b04d07710119940b2cba
tonic523 <diddmlvkf@gmail.com>
2022-02-22 16:46:31 +0900

    feat: Money 생성자 구현

---
+++ src/test/java/Money.java
@@ -0,0 +1,47 @@
+public class Money {
+
+    private static final int DIVIDED_STANDARD = 1000;
+    private static final int MIN_VALUE = 1000;
+    private static final int MAX_VALUE = 2_000_000_000;
+    private static final int REMAINDER = 0;
+    private static final String TO_INT_ERROR_MESSAGE = "숫자여야 합니다.";
+    private static final String VALUE_RANGE_ERROR_MESSAGE = "1000부터 20억의 숫자여야 합니다.";
+    private static final String VALUE_DIVIDED_BY_ERROR_MESSAGE = "1000으로 나누어 떨어져야 합니다.";
+
+    private final int value;
+
+    public Money(String text) {
+        int number = toInt(text);
+        validate(number);
+        this.value = number;
+    }
+
+    private int toInt(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(TO_INT_ERROR_MESSAGE);
+        }
+    }
+
+    private void validate(int number) {
+        validateValueRange(number);
+        validateDividedBy(number);
+    }
+
+    private void validateValueRange(int number) {
+        if (MIN_VALUE > number || number > MAX_VALUE) {
+            throw new IllegalArgumentException(VALUE_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDividedBy(int number) {
+        if (number % DIVIDED_STANDARD != REMAINDER) {
+            throw new IllegalArgumentException(VALUE_DIVIDED_BY_ERROR_MESSAGE);
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
