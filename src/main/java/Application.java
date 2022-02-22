4fb860cf7daf899e68e96eed8241e8411ba7bcad
= <rerub0831@gmail.com>
2022-02-22 18:13:47 +0900

    feat: Application 생성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,8 @@
+import controller.MainController;
+
 public class Application {
+    public static void main(String[] args) {
+        MainController mainController = new MainController();
+        mainController.run();
+    }
 }
