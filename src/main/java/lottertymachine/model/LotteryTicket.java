704ab5f471c2052f513b170be9a5cd5b89827f49
jochungbeom <cndqjacndqja@gmail.com>
2022-02-22 19:46:57 +0900

    feat: 로또 번호의 보너스 번호 보유 여부 확인

--- src/main/java/lottertymachine/model/LotteryTicket.java
+++ src/main/java/lottertymachine/model/LotteryTicket.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 public class LotteryTicket {
 
-  private final List<Integer> numbers;
+    private final List<Integer> numbers;
 
-  public LotteryTicket(final List<Integer> numbers) {
-    this.numbers = numbers;
-  }
+    public LotteryTicket(final List<Integer> numbers) {
+        this.numbers = numbers;
+    }
 
-  public int countMatchingNumbers(List<Integer> numbers) {
-    return (int) numbers.stream()
-        .filter(this.numbers::contains)
-        .count();
-  }
+    public int countMatchingNumbers(List<Integer> numbers) {
+        return (int) numbers.stream()
+                .filter(this.numbers::contains)
+                .count();
+    }
+
+    public boolean containsNumber(int number) {
+        return this.numbers.contains(number);
+    }
 }
