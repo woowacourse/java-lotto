ba90755bcfcb883fd67ba7c7763cd458f2d1caf6
= <rerub0831@gmail.com>
2022-02-22 18:11:35 +0900

    feat: 당첨로또 객체 구현

--- src/main/java/domain/WinLottoNumbers.java
+++ src/main/java/domain/WinLottoNumbers.java
@@ -1,4 +1,14 @@
 package domain;
 
-public class WinLottoNumbers {
+import java.util.List;
+
+public class WinLottoNumbers extends LottoNumbers {
+
+    public WinLottoNumbers(List<Integer> lottoNumbers) {
+        super(lottoNumbers);
+    }
+
+    public boolean isInNumber(LottoNumber lottoNumber) {
+        return super.lottoNumbers.contains(lottoNumber);
+    }
 }
---
+++ src/test/java/domain/WinLottoNumbersTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class WinLottoNumbersTest {
+    private WinLottoNumbers winLottoNumbers;
+
+    @BeforeEach
+    void 당첨번호_생성(){
+        winLottoNumbers = new WinLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4})
+    void 로또_숫자_확인(int input) {
+        assertThat(winLottoNumbers.isInNumber(new LottoNumber(input))).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7,8,9,10})
+    void 로또_숫자_포함안될때_검사(int input) {
+        assertThat(winLottoNumbers.isInNumber(new LottoNumber(input))).isFalse();
+    }
+}
