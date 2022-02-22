42a471c0a58311a2a41d267ea2271de6efe6dfce
jayjaehunchoi <wogns0108@naver.com>
2022-02-22 21:24:46 +0900

    test : 2~4등 상금 계산 기능 테스트 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
-import java.util.Arrays;
-
 public enum Rank {
 
     FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000),
     ;
 
     private final long reward;
--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -15,4 +15,40 @@ public class RankTest {
 
         assertThat(Rank.calculateMoney(first, count)).isEqualTo(6000000000L);
     }
+
+    @DisplayName("2등의 상금을 계산할 수 있다.")
+    @Test
+    void calculateSecondRankMoney() {
+        final Rank second = Rank.SECOND;
+        final long count = 2;
+
+        assertThat(Rank.calculateMoney(second, count)).isEqualTo(60000000L);
+    }
+
+    @DisplayName("3등의 상금을 계산할 수 있다.")
+    @Test
+    void calculateThirdRankMoney() {
+        final Rank third = Rank.THIRD;
+        final long count = 4;
+
+        assertThat(Rank.calculateMoney(third, count)).isEqualTo(6000000L);
+    }
+
+    @DisplayName("4등의 상금을 계산할 수 있다.")
+    @Test
+    void calculateFourthRankMoney() {
+        final Rank fourth = Rank.FOURTH;
+        final long count = 2;
+
+        assertThat(Rank.calculateMoney(fourth, count)).isEqualTo(100000);
+    }
+
+    @DisplayName("5등의 상금을 계산할 수 있다.")
+    @Test
+    void calculateFifthRankMoney() {
+        final Rank fifth = Rank.FIFTH;
+        final long count = 2;
+
+        assertThat(Rank.calculateMoney(fifth, count)).isEqualTo(10000);
+    }
 }
