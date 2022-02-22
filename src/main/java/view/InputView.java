9da1628e34b09b46a466ad7ce801a78f21b00da7
bcc0830 <bcc0830@naver.com>
2022-02-22 22:17:06 +0900

    feat : 사용자 입력 받는 기능 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,58 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+	private static final String NUMBER_REGEX = "^[0-9]+$";
+
+	public static int inputMoney() {
+		System.out.println("구입금액을 입력해 주세요.");
+		return inputSingleNumber();
+	}
+
+	public static int inputBonusNumber() {
+		System.out.println("보너스 볼을 입력해 주세요.");
+		return inputSingleNumber();
+	}
+
+	public static List<Integer> inputAnsNumbers() {
+		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+		return inputMultipleNumber();
+	}
+
+	private static String inputLine() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
+
+	private static int inputSingleNumber() {
+		String userInput = inputLine();
+		validateEmpty(userInput);
+		validateAllNumber(userInput);
+		return Integer.parseInt(userInput);
+	}
+
+	private static List<Integer> inputMultipleNumber() {
+		List<Integer> answers = new ArrayList<>();
+		String[] parsedUserInput = inputLine().split(", ");
+		for (String eachInput : parsedUserInput) {
+			validateAllNumber(eachInput);
+			answers.add(Integer.parseInt(eachInput));
+		}
+		return answers;
+	}
+
+	private static void validateAllNumber(String userInput) {
+		if (!userInput.matches(NUMBER_REGEX)) {
+			throw new IllegalArgumentException("[ERROR] 입력은 반드시 숫자여야 합니다.");
+		}
+	}
+
+	private static void validateEmpty(String userInput) {
+		if (userInput.isEmpty()) {
+			throw new IllegalArgumentException("[ERROR] 입력은 빈 입력일 수 없습니다.");
+		}
+	}
+}
