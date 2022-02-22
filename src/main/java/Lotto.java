fe9561842720f5dd4f2a5347a29bdb42645538e6
hyunrrr <whgusrms96@gmail.com>
2022-02-22 15:39:54 +0900

    feat(lotto): add checkWinning function

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -7,15 +7,50 @@ import java.util.stream.IntStream;
 
 public class Lotto {
 
+    private List<Integer> lottoNumbers;
+
+    public Lotto() {}
+
+    public Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
 
     public List<Integer> generateNumber() {
 
         List<Integer> numbers = IntStream.range(1, 46)
                 .boxed().collect(Collectors.toList());
         Collections.shuffle(numbers);
+        lottoNumbers = numbers.subList(0, 6);
 
         return numbers.subList(0, 6);
+    }
 
+    public int checkWinning(List<Integer> winningNumbers, Integer bonusNumber) {
+
+        int sameCount = 0;
+
+        for (Integer winningNumber : winningNumbers) {
+
+            if (lottoNumbers.contains(winningNumber)) {
+                sameCount++;
+            }
+
+        }
+
+        if (sameCount == 5 && lottoNumbers.contains(bonusNumber)) {
+            return 2;
+        }
+
+        if (sameCount == 5) {
+            return 3;
+        }
+
+        if (sameCount < 3) {
+            return -1;
+        }
+
+        return 7 - sameCount;
     }
 
 }
--- src/test/java/LottoTest.java
+++ src/test/java/LottoTest.java
@@ -1,5 +1,8 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,4 +15,54 @@ public class LottoTest {
 
         assertThat(lotto.generateNumber().size()).isEqualTo(6);
     }
+
+    @Test
+    @DisplayName("1등 당첨인지 확인하는 기능 테스트")
+    void checkWinning() {
+        Lotto lotto = new Lotto(Arrays.asList(3, 5, 6, 7, 8, 9));
+
+        List<Integer> winningNumber = Arrays.asList(3, 5, 6, 7, 8, 9);
+
+        Integer bonusNumber = 11;
+        assertThat(lotto.checkWinning(winningNumber, bonusNumber))
+                .isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("2등 당첨인지 확인하는 기능 테스트")
+    void checkWinning2() {
+        Lotto lotto = new Lotto(Arrays.asList(3, 5, 6, 7, 8, 9));
+
+        List<Integer> winningNumber = Arrays.asList(3, 5, 6, 7, 8, 10);
+
+        Integer bonusNumber = 9;
+
+        assertThat(lotto.checkWinning(winningNumber, bonusNumber)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("3등 당첨인지 확인하는 테스트 ")
+    void checkWinning3() {
+        Lotto lotto = new Lotto(Arrays.asList(3, 5, 6, 7, 8, 9));
+
+        List<Integer> winningNumber = Arrays.asList(3, 5, 6, 7, 8, 10);
+
+        Integer bonusNumber = 12;
+
+        assertThat(lotto.checkWinning(winningNumber, bonusNumber)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("당첨 안됐을때 확인하는 기능 테스트")
+    void checkWinning4() {
+        Lotto lotto = new Lotto(Arrays.asList(11, 12, 13, 14, 8, 9));
+
+        List<Integer> winningNumber = Arrays.asList(3, 5, 6, 7, 8, 10);
+
+        Integer bonusNumber = 9;
+
+        assertThat(lotto.checkWinning(winningNumber, bonusNumber)).isEqualTo(-1);
+    }
+
+
 }
