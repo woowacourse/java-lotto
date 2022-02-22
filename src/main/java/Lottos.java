68d165e3899f4eecb8bfebe1b37f4d64ff520756
hyunrrr <whgusrms96@gmail.com>
2022-02-22 16:24:55 +0900

    feat(Lottos): add compareAllLotto function

---
+++ src/main/java/Lottos.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Lottos {
+
+    //int lottoAmount;
+    List<Lotto> lottos;
+
+    public Lottos(int lottoAmount) {
+        //this.lottoAmount = lottoAmount;
+        for (int i = 0; i<lottoAmount; ++i) {
+            lottos.add(new Lotto());
+        }
+
+    }
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public Map<Integer, Integer> compareAllLotto(List<Integer> winningNumbers, int bonusNumber) {
+
+        Map<Integer, Integer> results = new HashMap<>();
+        for (int i = 0; i<lottos.size(); ++i) {
+            int winningRanking = lottos.get(i).checkWinning(winningNumbers, bonusNumber);
+            results.put(winningRanking, results.getOrDefault(winningRanking, 0) + 1);
+        }
+
+
+        return results;
+    }
+}
---
+++ src/test/java/LottosTest.java
@@ -0,0 +1,27 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottosTest {
+
+    @Test
+    @DisplayName("또로첨 당등 1 갯수 확인하는 기능")
+    void compareAllLottoTest1() {
+        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9));
+
+        Lottos lottos = new Lottos(Arrays.asList(lotto1, lotto2));
+
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 30;
+        Map<Integer, Integer> results = lottos.compareAllLotto(winningNumbers, bonusNumber);
+
+
+        assertThat(results.get(1)).isEqualTo(1);
+    }
+
+}
