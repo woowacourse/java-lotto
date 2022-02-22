dc3fc06a46f3b59c0e665f9658b18d543ebe015a
Gyuchool <balance0591@naver.com>
2022-02-22 17:55:23 +0900

    feat: 보너스 볼 입력 기능

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static Scanner scanner = new Scanner(System.in);
 
     public static String askInputMoney() {
@@ -12,6 +13,11 @@ public class InputView {
 
     public static String askInputWinningNumber() {
         System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String askInputBonusBall() {
+        System.out.println(INPUT_BONUS_BALL_MESSAGE);
         return scanner.nextLine();
     }
 }
