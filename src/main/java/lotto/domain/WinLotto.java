9c5687dbda8bc3f87270d4fcdbbc00098722e8cd
jayjaehunchoi <wogns0108@naver.com>
2022-02-22 17:32:23 +0900

    feat : WinLotto에 LottoNumber 적용

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -4,29 +4,29 @@ import java.util.List;
 
 public class WinLotto {
 
-    private final List<Integer> winNumbers;
-    private final int bonusNumber;
+    private final List<LottoNumber> winNumbers;
+    private final LottoNumber bonusNumber;
 
-    public WinLotto(final List<Integer> winNumbers, final int bonusNumber) {
+    public WinLotto(final List<LottoNumber> winNumbers, final LottoNumber bonusNumber) {
         checkDuplicateWinNumbers(winNumbers);
         checkDuplicateBonusNumber(winNumbers, bonusNumber);
         this.winNumbers = winNumbers;
         this.bonusNumber = bonusNumber;
     }
 
-    private void checkDuplicateWinNumbers(final List<Integer> winNumbers) {
+    private void checkDuplicateWinNumbers(final List<LottoNumber> winNumbers) {
         if (winNumbers.size() != calculateDistinctSize(winNumbers)) {
             throw new IllegalArgumentException("[ERROR] 중복된 당첨 번호가 존재합니다.");
         }
     }
 
-    private long calculateDistinctSize(final List<Integer> winNumbers) {
+    private long calculateDistinctSize(final List<LottoNumber> winNumbers) {
         return winNumbers.stream()
                 .distinct()
                 .count();
     }
 
-    private void checkDuplicateBonusNumber(final List<Integer> winNumbers, final int bonusNumber) {
+    private void checkDuplicateBonusNumber(final List<LottoNumber> winNumbers, final LottoNumber bonusNumber) {
         if (winNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 볼이 당첨 번호와 중복됩니다.");
         }
--- src/test/java/lotto/domain/WinLottoTest.java
+++ src/test/java/lotto/domain/WinLottoTest.java
@@ -2,8 +2,9 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,21 +13,25 @@ public class WinLottoTest {
     @DisplayName("중복된 당첨 번호가 존재하는 경우 에러 발생")
     @Test
     void duplicateLottoNumber() {
-        final List<Integer> duplicateNumbers = Arrays.asList(1, 1, 1, 1, 1, 1);
+        final List<LottoNumber> duplicateNumbers = Stream.of(1, 1, 1, 1, 1, 1)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new WinLotto(duplicateNumbers, 5))
+                .isThrownBy(() -> new WinLotto(duplicateNumbers, new LottoNumber(5)))
                 .withMessage("[ERROR] 중복된 당첨 번호가 존재합니다.");
     }
 
     @DisplayName("보너스 볼과 당첨 번호가 중복되는 경우 에러 발생")
     @Test
     void duplicateBonusBallNumber() {
-        final List<Integer> winNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        final int duplicateBonusNumber = 1;
+        final List<LottoNumber> winNumbers = Stream.of(1, 2, 3, 4, 5, 6)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+        final LottoNumber duplicateBonusLottoNumber = new LottoNumber(1);
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new WinLotto(winNumbers, duplicateBonusNumber))
+                .isThrownBy(() -> new WinLotto(winNumbers, duplicateBonusLottoNumber))
                 .withMessage("[ERROR] 보너스 볼이 당첨 번호와 중복됩니다.");
     }
 }
