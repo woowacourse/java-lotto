12f1baf3a14c84607c13cfe53df8bf524fa1f7ee
jayjaehunchoi <wogns0108@naver.com>
2022-02-22 23:04:38 +0900

    test : LottoNumbers 공통 생성부분 메소드 분리

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,9 +17,7 @@ public class LottoTest {
     @DisplayName("구매 로또 생성시 숫자가 6개가 입력되지 않으면 에러 발생")
     @Test
     void lottoCreateExceptionBySize() {
-        final List<LottoNumber> lottoNumbers = Stream.of(1, 2, 3, 4, 5)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
+        final List<LottoNumber> lottoNumbers = createLottoNumbers(1, 2, 3, 4, 5);
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Lotto(lottoNumbers))
                 .withMessage("[ERROR] 로또 넘버는 6개가 입력되어야 합니다.");
@@ -27,11 +26,8 @@ public class LottoTest {
     @DisplayName("구매 로또가 중복될 경우 예외가 발생해야 한다.")
     @Test
     void duplicateLottoException() {
-        final List<LottoNumber> lottoNumbers = Stream.of(1, 1, 1, 1, 1, 1)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Lotto(lottoNumbers))
+                .isThrownBy(() -> new Lotto(createLottoNumbers(1, 1, 1, 1, 1, 1)))
                 .withMessage("[ERROR] 로또 넘버는 중복될 수 없습니다.");
     }
 
@@ -43,9 +39,7 @@ public class LottoTest {
 
         @BeforeEach
         void setUp() {
-            lotto = new Lotto(Stream.of(1, 2, 3, 4, 5, 6)
-                    .map(LottoNumber::new)
-                    .collect(Collectors.toList()));
+            lotto = new Lotto(createLottoNumbers(1, 2, 3, 4, 5, 6));
         }
 
         @DisplayName("보너스 넘버를 포함하는 경우")
@@ -65,14 +59,15 @@ public class LottoTest {
 
     @Test
     void matchLotto() {
-        final Lotto lotto = new Lotto(Stream.of(1, 2, 3, 4, 5, 6)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList()));
-
-        final Lotto compareLotto = new Lotto(Stream.of(1, 2, 3, 4, 5, 7)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList()));
+        final Lotto lotto = new Lotto(createLottoNumbers(1, 2, 3, 4, 5, 6));
+        final Lotto compareLotto = new Lotto(createLottoNumbers(1, 2, 3, 4, 5, 7));
 
         assertThat(lotto.match(compareLotto)).isEqualTo(5);
+    }
+
+    private List<LottoNumber> createLottoNumbers(int... number) {
+        return Arrays.stream(number)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 }
