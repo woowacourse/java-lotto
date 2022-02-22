dd5b61b95e0024c0522d7d186cd8e6972a55febd
yxxnghwan <younghwan960@gmail.com>
2022-02-22 14:54:48 +0900

    feat: 구입 금액이 1000의 배수인지 검증

--- src/main/java/validator/PurchaseMoneyValidator.java
+++ src/main/java/validator/PurchaseMoneyValidator.java
@@ -2,12 +2,28 @@ package validator;
 
 public class PurchaseMoneyValidator {
 
+    private static final String NOT_MULTIPLES_OF_1000_ERROR_MESSAGE = "금액을 1,000의 배수로 입력해주세요.";
+    private static final String NOT_INTEGER_ERROR_MESSAGE = "금액을 정수로 입력해주세요.";
+
     public static void validate(String purchaseMoneyString) {
+        int purchaseMoney = checkInteger(purchaseMoneyString);
+
+        if (isMultiplesOf1000(purchaseMoney)) {
+            throw new IllegalArgumentException(NOT_MULTIPLES_OF_1000_ERROR_MESSAGE);
+        }
+    }
+
+    private static boolean isMultiplesOf1000(int purchaseMoney) {
+        return purchaseMoney % 1000 != 0;
+    }
+
+    private static int checkInteger(String purchaseMoneyString) {
         int purchaseMoney;
         try {
             purchaseMoney = Integer.parseInt(purchaseMoneyString);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_INTEGER_ERROR_MESSAGE);
         }
+        return purchaseMoney;
     }
 }
--- src/test/java/validator/PurchaseMoneyValidatorTest.java
+++ src/test/java/validator/PurchaseMoneyValidatorTest.java
@@ -10,17 +10,19 @@ public class PurchaseMoneyValidatorTest {
 
     @Test
     @DisplayName("구입 금액이 정수인지 확인한다.")
-    void checkPurchaseMoneyMultiplesOf1000() {
+    void checkPurchaseMoneyInteger() {
         String purchaseMoney = "1700dd";
         assertThatThrownBy(() ->
             PurchaseMoneyValidator.validate(purchaseMoney)
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
-    /*@Test
+    @Test
     @DisplayName("구입 금액이 1,000의 배수인지 확인한다.")
     void checkPurchaseMoneyMultiplesOf1000() {
-        purchaseMoney = 17000;
-
-    }*/
+        String purchaseMoney = "1700";
+        assertThatThrownBy(() ->
+                PurchaseMoneyValidator.validate(purchaseMoney)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
 }
