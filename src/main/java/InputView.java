a73369cdeb421b90bf0f4d14c9c2e667411dbe56
Gyuchool <balance0591@naver.com>
2022-02-22 17:54:04 +0900

    feat: 지난주 당첨 번호 입력 기능

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static Scanner scanner = new Scanner(System.in);
 
     public static String askInputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String askInputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         return scanner.nextLine();
     }
 }
