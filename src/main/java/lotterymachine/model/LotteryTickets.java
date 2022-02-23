ed2985ae6c8537e2ab2fbdf4e3ddcefb4698f471
jochungbeom <cndqjacndqja@gmail.com>
2022-02-23 14:52:24 +0900

    refactor: 불필요한 코드 삭제

--- src/main/java/lotterymachine/model/LotteryTickets.java
+++ src/main/java/lotterymachine/model/LotteryTickets.java
@@ -11,39 +11,19 @@ public class LotteryTickets {
         this.lotteryTickets = lotteryTickets;
     }
 
-    public int countMatchingNumberOfThree(List<Integer> winningNumbers) {
-        int count = 0;
-        for (LotteryTicket lotteryTicket : lotteryTickets) {
-            if (lotteryTicket.countMatchingNumbers(winningNumbers) == 3) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public int countMatchingNumberOfFour(List<Integer> winningNumbers, int findMatch) {
-        int count = 0;
-        for (LotteryTicket lotteryTicket : lotteryTickets) {
-            if (lotteryTicket.countMatchingNumbers(winningNumbers) == findMatch) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public Map<WinningLottery, Integer> getWinningLotteries(List<Integer> numbers, int bonusNumber) {
+    public Map<WinningLottery, Integer> getLotteriesResult(List<Integer> numbers, int bonusNumber) {
         Map<WinningLottery, Integer> winningLotteries = new HashMap<>();
         for (LotteryTicket lotteryTicket : lotteryTickets) {
             int matchingNumbers = lotteryTicket.countMatchingNumbers(numbers);
             boolean containsBonus = lotteryTicket.containsNumber(bonusNumber);
-            WinningLottery winningLottery = getWinningLottery(containsBonus, matchingNumbers);
+            WinningLottery winningLottery = findWinningLottery(containsBonus, matchingNumbers);
             int value = winningLotteries.getOrDefault(winningLottery, 0);
             winningLotteries.put(winningLottery, value + 1);
         }
         return winningLotteries;
     }
 
-    private WinningLottery getWinningLottery(boolean bonus, int matchingCnt) {
+    private WinningLottery findWinningLottery(boolean bonus, int matchingCnt) {
         if (matchingCnt == 5 && bonus) {
             return WinningLottery.BONUS_FIVE;
         }
