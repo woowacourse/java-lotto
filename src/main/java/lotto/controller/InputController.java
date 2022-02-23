a2da1bf4fd42bacb7157cbc3aa99dc3253f418f2
brorae <jhr3239@gmail.com>
2022-02-23 12:02:07 +0900

    test : 입력값 테스트 추가

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,26 +1,24 @@
 package lotto.controller;
 
-import lotto.view.InputView;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputController {
 
-    public int scanLottoCount() {
-        return Integer.parseInt(InputView.inputPrice()) / 1000;
+    public int countLotto(String price) {
+        return Integer.parseInt(price) / 1000;
     }
 
-    public List<Integer> splitWinningNumbers() {
-        String[] s = InputView.inputWinningNumbers().split(", ");
+    public List<Integer> splitWinningNumbers(String winningNumbers) {
+        String[] s = winningNumbers.split(", ");
         return Arrays.stream(s)
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toList());
     }
 
-    public int scanBonusNumber() {
-        return Integer.parseInt(InputView.inputBonusNumber());
+    public int toIntBonusNumber(String bonusNumber) {
+        return Integer.parseInt(bonusNumber);
     }
 }
---
+++ src/test/java/lotto/controller/InputControllerTest.java
@@ -0,0 +1,34 @@
+package lotto.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputControllerTest {
+
+    InputController inputController = new InputController();
+
+    @Test
+    void 입력한_금액을_개수로_변환1() {
+        String value = "10000";
+        assertThat(inputController.countLotto(value)).isEqualTo(10);
+    }
+
+    @Test
+    void 입력한_금액을_개수로_변환2() {
+        String value = "1500";
+        assertThat(inputController.countLotto(value)).isEqualTo(1);
+    }
+
+    @Test
+    void 당첨번호_분리_성공() {
+        String value = "1, 2, 3, 4, 5, 6";
+        assertThat(inputController.splitWinningNumbers(value)).contains(1,2,3,4,5,6);
+    }
+
+    @Test
+    void 보너스번호_테스트() {
+        String value = "44";
+        assertThat(inputController.toIntBonusNumber(value)).isEqualTo(44);
+    }
+}
