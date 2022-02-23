8089a6e38d695ce2755f0c9092f510b265d6ff68
hyeonic <dev.hyeonic@gmail.com>
2022-02-23 20:15:02 +0900

    test: 당첨 정보의 검증을 위한 테스트 추가
    당첨 정보에 해당하는 열거형(Rank)를 생성하고 제대로 반환하는지 테스트

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -17,4 +17,47 @@ class RankTest {
         assertThat(rank).isEqualTo(Rank.SECOND);
         assertThat(rank.getPrizeMoney()).isEqualTo(30_000_000);
     }
+
+    @DisplayName("보너스 볼을 포함하지 않고 5개를 맞춘 경우 3등이어야한다.")
+    @Test
+    void 삼등_보너스_미포함_당첨() {
+        // given
+        Rank rank = Rank.of(5, false);
+
+        // when & then
+        assertThat(rank).isEqualTo(Rank.THIRD);
+        assertThat(rank.getPrizeMoney()).isEqualTo(1_500_000);
+    }
+
+    @DisplayName("보너스볼 포함하여 6개 맞춘 경우 1등이어야 한다.")
+    @Test
+    void 일등_보너스_포함_당첨() {
+        // given
+        Rank rank = Rank.of(6, false);
+
+        // when & then
+        assertThat(rank).isEqualTo(Rank.FIRST);
+        assertThat(rank.getPrizeMoney()).isEqualTo(2_000_000_000);
+    }
+
+    @DisplayName("보너스볼 미포함하여 6개 맞춘 경우 1등이어야 한다.")
+    @Test
+    void 일등_보너스_미포함_당첨() {
+        // given
+        Rank rank = Rank.of(6, true);
+
+        // when & then
+        assertThat(rank).isEqualTo(Rank.FIRST);
+        assertThat(rank.getPrizeMoney()).isEqualTo(2_000_000_000);
+    }
+
+    @DisplayName("2개 맞춘 경우 NOTTING 이다.")
+    @Test
+    void 두개_맞은_경우_NOTTING() {
+        // given
+        Rank rank = Rank.of(2, true);
+
+        // when & then
+        assertThat(rank).isEqualTo(Rank.NOTTING);
+    }
 }
