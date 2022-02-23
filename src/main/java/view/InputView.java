0a3e09172d5c2ddcdd1d50ebb211ea2eec828c09
Wishoon <heonga2@gmail.com>
2022-02-23 22:00:24 +0900

    feat : 구매된 로또를 출력하는 기능의 검증 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String REQUEST_MESSAGE_INPUT_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
+    private static final String ERROR_MESSAGE_TYPE_OF_MONEY = "금액은 숫자가 아닐 수 없습니다.";
+    private static final String ERROR_MESSAGE_RANGE_OF_MONEY = "금액은 0이하일 수 없습니다.";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -22,13 +24,20 @@ public class InputView {
 
     private static void validatePurchaseMoney(String value) {
         validateNumber(value);
+        validateRange(value);
     }
 
     private static void validateNumber(String value) {
         try {
             Integer.parseInt(value);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE_TYPE_OF_MONEY);
+        }
+    }
+
+    private static void validateRange(String value) {
+        if (Integer.parseInt(value) <= 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_RANGE_OF_MONEY);
         }
     }
 }
