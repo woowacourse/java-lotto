3f3727cf62c2d9796aa7d8e625ecc24466388cd2
양동주 <djwhy5510@gmail.com>
2022-02-23 21:04:08 +0900

    refactor: 테스트 클래스 이름 변경

--- src/test/java/model/RandomLottoNumbersGeneratorTest.java
+++ src/test/java/model/RandomLottoNumbersGeneratorTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class RandomDistinctNumberQueueTest {
+public class RandomLottoNumbersGeneratorTest {
 
     @ParameterizedTest
     @MethodSource("provideRangeAndSizeAndExpectedSet")
