3eb8f58c3a20649761a5e212e6a969abc9d82074
bcc0830 <bcc0830@naver.com>
2022-02-24 01:55:02 +0900

    refactor : 일반 로또 생성자 수정 및 해당 로또의 당첨 번호 갯수와 보너스 당첨 여부 기능 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -3,28 +3,17 @@ package domain;
 import java.util.List;
 
 public class Lotto {
-	List<Integer> numbers;
+	private final List<LottoNumber> lottoNumbers;
 
-	public Lotto(LottoNumbersGenerator lottoNumbersGenerator) {
-		this.numbers = lottoNumbersGenerator.generate();
+	public Lotto() {
+		this.lottoNumbers = LottoGenerator.generateRandomLottoNumbers();
 	}
 
-	public List<Integer> getNumbers() {
-		return numbers;
+	public int calculateInAnswerNumbers(AnswerLotto answerLotto) {
+		return (int) this.lottoNumbers.stream().map(answerLotto::isInAnswerNumbers).count();
 	}
 
-	public int getNumbersSize() {
-		return this.numbers.size();
-	}
-
-	public ResultStatics calculate(AnswerLotto answerLotto) {
-		int count = (int) answerLotto.getNumbers().stream().filter(num -> this.numbers.contains(num)).count();
-		boolean bonus = false;
-
-		if (count == 5) {
-			bonus = this.numbers.contains(answerLotto.getBonusNumber());
-		}
-
-		return ResultStatics.of(count, bonus);
+	public boolean isHitBonusNumber(AnswerLotto answerLotto) {
+		return this.lottoNumbers.stream().anyMatch(answerLotto::isSameWithBonusNumber);
 	}
 }
