0db02342bb9cfa6faafde27d5d1c85614bdb339e
summerlunaa <summerlunaa9@gmail.com>
2022-02-23 12:31:07 +0900

    feat: 로또 번호 유효성 검사 기능

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -3,7 +3,9 @@ package lotto.domain;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import lotto.exception.LottoNumberException;
 
 public enum LottoNumber {
 
@@ -16,6 +18,10 @@ public enum LottoNumber {
     NUMBER_31(31), NUMBER_32(32), NUMBER_33(33), NUMBER_34(34), NUMBER_35(35),
     NUMBER_36(36), NUMBER_37(37), NUMBER_38(38), NUMBER_39(39), NUMBER_40(40),
     NUMBER_41(41), NUMBER_42(42), NUMBER_43(43), NUMBER_44(44), NUMBER_45(45);
+
+    private static final String REGEX_FOR_NATURAL_NUMBER = "^[1-9][0-9]*$";
+    public static final int MINIMUM_RANGE = 1;
+    public static final int MAXIMUM_RANGE = 45;
 
     private final int number;
 
@@ -31,5 +37,31 @@ public enum LottoNumber {
 
     private static List<LottoNumber> getLottoNumbers() {
         return Arrays.stream(LottoNumber.values()).collect(Collectors.toList());
+    }
+
+    public static LottoNumber getLottoNumber(String input) {
+        checkNaturalNumber(input);
+        checkRange(input);
+        return LottoNumber.valueOf("NUMBER_" + input);
+    }
+
+    private static void checkNaturalNumber(String input) {
+        if(!isNaturalNumber(input)) {
+            throw new LottoNumberException(LottoNumberException.LOTTO_NUMBER_NATURAL_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    private static boolean isNaturalNumber(String input) {
+        return Pattern.compile(REGEX_FOR_NATURAL_NUMBER).matcher(input).find();
+    }
+
+    private static void checkRange(String input) {
+        if (!isCorrectRange(Integer.parseInt(input))) {
+            throw new LottoNumberException(LottoNumberException.LOTTO_NUMBER_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    private static boolean isCorrectRange(int input) {
+        return input >= MINIMUM_RANGE && input <= MAXIMUM_RANGE;
     }
 }
---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import lotto.exception.LottoNumberException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LottoNumberTest {
+
+    @ParameterizedTest(name = "로또 번호가 자연수가 아닌 경우 예외 발생 - case : {0}")
+    @ValueSource(strings = {"-1", "0", "A"})
+    void checkNaturalNumber(String input) {
+        Assertions.assertThatThrownBy(() -> LottoNumber.getLottoNumber(input))
+                .isInstanceOf(LottoNumberException.class)
+                .hasMessage("로또 번호는 자연수여야 합니다.");
+    }
+
+    @ParameterizedTest(name = "로또 번호가 범위를 벗어나는 경우 예외 발생 - case : {0}")
+    @ValueSource(strings = {"100", "46"})
+    void checkRange(String input) {
+        Assertions.assertThatThrownBy(() -> LottoNumber.getLottoNumber(input))
+                .isInstanceOf(LottoNumberException.class)
+                .hasMessage("로또 번호는 1 ~ 45 사이여야 합니다.");
+    }
+}
