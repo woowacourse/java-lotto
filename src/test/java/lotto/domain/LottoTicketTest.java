5eda59250a55bc1be18219db6737569742fd1491
hyeonic <dev.hyeonic@gmail.com>
2022-02-23 14:25:26 +0900

    feat: LottoTicket 생성 기능 추가
    LottoTicket 생성 시점에 로또 번호 생성을 위한 전략 활용

---
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTicketTest {
+
+    @DisplayName("로또 티켓 생성 시점에 로또 번호 생성을 위한 전략을 활용한다.")
+    @Test
+    void 로또_티켓_정상_생성() {
+        // given & when & then
+        assertThatCode(() -> new LottoTicket(new AutoLottoNumberGenerator()))
+                .doesNotThrowAnyException();
+    }
+}
