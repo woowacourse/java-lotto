329b69ed2c96e8ebc05b5d3c52bac0942dd2b8f8
Richard JEON <ztzy1907@gmail.com>
2022-02-23 11:09:51 +0900

    feat(inputview): 로또 구매 수량 출력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String MESSAGE_TO_GET_INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_FOR_LOTTO_COUNT = "%d개를 구매했습니다.%n";
 
     public static int scanInputMoney() {
         System.out.println(MESSAGE_TO_GET_INPUT_MONEY);
@@ -13,5 +14,9 @@ public class InputView {
 
     public static void printException(Exception exception) {
         System.out.println(exception.getMessage());
+    }
+
+    public static void printLottoCount(int count) {
+        System.out.printf(MESSAGE_FOR_LOTTO_COUNT, count);
     }
 }
