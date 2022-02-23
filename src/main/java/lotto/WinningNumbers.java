9a3c828a86fe9791219c117f83713d47996d8595
yh20studio(tiki) <yh20studio@gmail.com>
2022-02-23 21:44:20 +0900

    feat: 당첨 번호가 6개가 아닐 때 예외 처리 구현

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class WinningNumbers {
 
     private static final String ERROR_DUPLICATION_WINNING_NUMBERS = "당첨 번호가 서로 중복되었습니다.";
+    private static final String ERROR_NOT_MATCH_NUMBER_COUNT = "당첨 번호는 6개를 입력해주세요.";
 
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
@@ -12,12 +13,20 @@ public class WinningNumbers {
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+
         validateIsDuplicatedNumbers();
+        validateNumberCount();
     }
 
     private void validateIsDuplicatedNumbers() {
         if (isDuplicate()) {
             throw new RuntimeException(ERROR_DUPLICATION_WINNING_NUMBERS);
+        }
+    }
+
+    private void validateNumberCount() {
+        if (winningNumbers.size() != 6) {
+            throw new RuntimeException(ERROR_NOT_MATCH_NUMBER_COUNT);
         }
     }
 
--- src/test/java/lottoTest/WinningNumbersTest.java
+++ src/test/java/lottoTest/WinningNumbersTest.java
@@ -3,8 +3,13 @@ package lottoTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 import lotto.WinningNumbers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -15,5 +20,20 @@ class WinningNumbersTest {
         assertThatThrownBy(() -> new WinningNumbers(Arrays.asList(1, 1, 3, 4, 5, 6), 7))
                 .hasMessageContaining("당첨 번호가 서로 중복되었습니다.")
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideWinningNumbers")
+    void 당첨번호가_6개가_아닌_경우(List<Integer> winningNumbers, int bonusNumber) {
+        assertThatThrownBy(() -> new WinningNumbers(winningNumbers, bonusNumber))
+                .hasMessageContaining("당첨 번호는 6개를 입력해주세요.")
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    private static Stream<Arguments> provideWinningNumbers() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 7),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5), 7)
+        );
     }
 }
