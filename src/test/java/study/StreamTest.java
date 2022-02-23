fce02bc4dee02229d38ae119d62a294381d8e658
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-23 19:08:08 +0900

    feat(study): toMap can initialize Map with values of stream

--- src/test/java/study/StreamTest.java
+++ src/test/java/study/StreamTest.java
@@ -4,16 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 public class StreamTest {
 
     @Test
-    void joining() {
+    void join() {
         List<String> list = Arrays.asList("a", "bcd", "efgh");
 
-        String result = list.stream().collect(Collectors.joining(", "));
+        String result = String.join(", ", list);
 
         assertThat(result).isEqualTo("a, bcd, efgh");
     }
@@ -27,5 +29,20 @@ public class StreamTest {
                 .collect(Collectors.joining(", "));
 
         assertThat(result).isEqualTo("1, 2, 3");
+    }
+
+    @Test
+    void toMap_initializesMapWithValuesInStream() {
+        Map<String, String> map = Stream
+                .of(new String[][]{{"Hello", "World"}, {"John", "Doe"},})
+                .collect(Collectors.toMap(data -> data[0], data -> data[1]));
+
+        Map<OrderEnum, Integer> map2 = Arrays
+                .stream(OrderEnum.values())
+                .collect(Collectors.toMap(lottoResult -> lottoResult, i -> 0));
+    }
+
+    private enum OrderEnum {
+        FIRST, SECOND, THIRD, FOURTH, FIFTH
     }
 }
