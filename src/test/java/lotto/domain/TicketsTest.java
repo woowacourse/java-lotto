0c7e12d73f9b64544115d42318168e0ecc98a18c
summerlunaa <summerlunaa9@gmail.com>
2022-02-23 12:34:50 +0900

    chore: 줄바꿈

--- src/test/java/lotto/domain/TicketsTest.java
+++ src/test/java/lotto/domain/TicketsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TicketsTest {
+
     @Test()
     @DisplayName("구입금액 만큼 로또티켓을 구입")
     void buyTicketsByAuto() {
