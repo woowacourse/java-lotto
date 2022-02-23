916f5e455d68bf66f69fa9a1e7775e7c98ca1c1c
Gyuchool <balance0591@naver.com>
2022-02-23 14:57:00 +0900

    feat: 당첨 번호에 보너스 볼과 중복되면 예외 발생

--- src/main/java/WinningNumber.java
+++ src/main/java/WinningNumber.java
@@ -1,13 +1,20 @@
 import java.util.List;
 
 public class WinningNumber {
+    private static final String DUPLICATE_BONUS_BALL_MESSAGE = "[ERROR] 보너스 볼은 당첨 번호와 중복될수 없습니다.";
     List<Integer> winningNumbers;
-    public WinningNumber(List<Integer> winningNumbers){
+
+    public WinningNumber(List<Integer> winningNumbers) {
         this.winningNumbers = winningNumbers;
     }
 
-
     public int size() {
         return winningNumbers.size();
+    }
+
+    public void checkBonusBall(int bonusBall) {
+        if (winningNumbers.contains(bonusBall)) {
+            throw new IllegalArgumentException(DUPLICATE_BONUS_BALL_MESSAGE);
+        }
     }
 }
--- src/test/java/WinningNumberTest.java
+++ src/test/java/WinningNumberTest.java
@@ -1,16 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningNumberTest {
 
+    private WinningNumber winningNumber;
+    @BeforeEach
+    public void setUp(){
+        winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
+    }
     @Test
     @DisplayName("당첨 번호가 6자리 숫자인지 테스트")
-    public void checkWinningNumberIs6Test(){
-        WinningNumber winningNumber = new WinningNumber(List.of(1,2,3,4,5,6));
+    public void checkWinningNumberIs6Test() {
         assertThat(winningNumber.size()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("당첨 번호에 보너스 볼이 있으면 예외 발생")
+    public void checkBonusBallInWinningNumbersTest() {
+        int bonusBall = 3;
+        assertThatThrownBy(
+                ()->winningNumber.checkBonusBall(bonusBall)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
