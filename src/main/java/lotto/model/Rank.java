3dddf58a75a41014ec4cec55b8722b535ec31c92
brorae <jhr3239@gmail.com>
2022-02-23 16:22:08 +0900

    refactor : enum Rank 구조 수정

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -8,7 +8,8 @@ public enum Rank {
     SECOND(5, true, 30_000_000),
     THIRD(5, false, 1_500_000),
     FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000);
+    FIFTH(3, false, 5_000),
+    LOSER(0, false, 0);
 
     private final int count;
     private final boolean winBonusNumber;
@@ -25,6 +26,6 @@ public enum Rank {
                 .filter(rank -> rank.count == count)
                 .filter(rank -> rank.winBonusNumber == winBonusNumber)
                 .findFirst()
-                .orElseThrow();
+                .orElse(LOSER);
     }
 }
