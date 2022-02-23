c405ba95a9f609266bd87f0e53eaf74d4d129e87
cjlee38 <cjlee38@github.com>
2022-02-23 11:11:15 +0900

    refactor(NumberGenerator): 테스트 코드가 LottoNumber에 비종속적이도록 수정

--- src/test/java/lotto/domain/NumberGeneratorTest.java
+++ src/test/java/lotto/domain/NumberGeneratorTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.assertj.core.api.Assertions;
@@ -10,10 +11,10 @@ import org.junit.jupiter.api.Test;
 class NumberGeneratorTest {
 
     @Test
-    @DisplayName("랜덤으로 번호를 생성한다.")
+    @DisplayName("랜덤으로 여러 번호를 생성한다.")
     void createRandomNumber() {
         // given
-        NumberGenerator numberGenerator = new RandomNumberGenerator(LottoNumber.MIN, LottoNumber.MAX);
+        NumberGenerator numberGenerator = new RandomNumberGenerator(1, 5);
         // when
         List<Integer> numbers = numberGenerator.generate(6);
         // then
@@ -21,16 +22,16 @@ class NumberGeneratorTest {
     }
 
     @Test
-    @DisplayName("번호는 0 이상 45 이하의 값을 생성한다.")
+    @DisplayName("임의의 범위의 값을 생성한다.")
     void testRandomNumberRange() {
         // given
-        NumberGenerator numberGenerator = new RandomNumberGenerator(LottoNumber.MIN, LottoNumber.MAX);
+        NumberGenerator numberGenerator = new RandomNumberGenerator(1, 5);
         // when
         List<Integer> numbers = numberGenerator.generate(6);
         // then
-        Assertions.assertThat(IntStream.range(1, 46)
+        Assertions.assertThat(IntStream.range(1, 5)
                 .boxed()
-                .toArray(Integer[]::new))
+                .collect(Collectors.toList()))
             .contains(numbers.toArray(Integer[]::new));
     }
 }
