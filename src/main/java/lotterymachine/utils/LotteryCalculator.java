fc9c6f3069914364d296e3519cae43a0f6a287e0
jochungbeom <cndqjacndqja@gmail.com>
2022-02-23 14:47:08 +0900

    feat: (당첨금액 / 구입금액)으로 수익률을 계산

--- src/main/java/lotterymachine/utils/LotteryCalculator.java
+++ src/main/java/lotterymachine/utils/LotteryCalculator.java
@@ -1,9 +1,16 @@
 package lotterymachine.utils;
 
+import static java.lang.Math.round;
+
 public class LotteryCalculator {
     private static final int LOTTERY_PRICE = 1000;
+    private static final double DECIMAL_PLACE_SAVER = 100.0;
 
     public static int divideByLotteryPrice(int amount) {
         return amount / LOTTERY_PRICE;
+    }
+
+    public static double calculateProfitRate(double winningLotteryAmount, int amount) {
+        return round(winningLotteryAmount / amount * DECIMAL_PLACE_SAVER)  / DECIMAL_PLACE_SAVER;
     }
 }
--- src/test/java/lotterymachine/utils/LotteryCalculatorTest.java
+++ src/test/java/lotterymachine/utils/LotteryCalculatorTest.java
@@ -8,10 +8,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LotteryCalculatorTest {
 
     @Test
-    @DisplayName("구입 금액에서 최대한으로 구매할 수 있는 로또 개수 계산")
+    @DisplayName("구입 금액에서 최대한으로 구매할 수 있는 로또 개수 계산한다.")
     void calculateMaximumLottery() {
         int input = 15300;
         int lottery = LotteryCalculator.divideByLotteryPrice(input);
         assertThat(lottery).isEqualTo(15);
+    }
+
+    @Test
+    @DisplayName("당첨금액, 구입금액을 입력하여 수익률을 계산한다.")
+    void calculateProfitRate() {
+        int amount = 15000;
+        int winningLotteryAmount = 1000;
+        double result = LotteryCalculator.calculateProfitRate(winningLotteryAmount, amount);
+        assertThat(result).isEqualTo(0.07);
     }
 }
