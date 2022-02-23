5092ea2a8c6104c21d271298469d526ec40ed6c5
YJGwon <yj970125@gmail.com>
2022-02-23 19:36:03 +0900

    feat: 당첨 번호에 대한 개수와 중복에 대한 예외처리

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -3,12 +3,40 @@ package lotto;
 import java.util.List;
 
 public class WinningNumbers {
-    private List<Integer> winningNumbers;
-    private int bonusNumber;
+    private static final String ERROR_SIZE = "[ERROR] 당첨 번호는 6개여야 합니다";
+    private static final String ERROR_DUPLICATE = "[ERROR] 당첨 번호는 중복되면 안됩니다";
+    private static final int WINNING_NUMBERS_SIZE = 6;
+
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        validate(winningNumbers);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> winningNumbers) {
+        checkSize(winningNumbers);
+        checkDuplicate(winningNumbers);
+    }
+
+    private void checkSize(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != WINNING_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(ERROR_SIZE);
+        }
+    }
+
+    private void checkDuplicate(List<Integer> winningNumbers) {
+        if (getDistinctCount(winningNumbers) != winningNumbers.size()) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE);
+        }
+    }
+
+    private int getDistinctCount(List<Integer> winningNumbers) {
+        return (int) winningNumbers.stream()
+                .distinct()
+                .count();
     }
 
     public boolean match(int number) {
--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -1,24 +1,70 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningNumbersTest {
+    private List<Integer> standardNumbers;
+
+    @BeforeEach
+    public void initializeStandardNumbers() {
+        standardNumbers = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            standardNumbers.add(i);
+        }
+    }
+
+    @DisplayName("숫자가 7개일 때 예외가 발생한다")
+    @Test
+    public void size_7_exception() {
+        standardNumbers.add(7);
+        assertThatThrownBy(() -> {
+            new WinningNumbers(standardNumbers, 8);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 6개여야 합니다");
+    }
+
+    @DisplayName("숫자가 5개일 때 예외가 발생한다")
+    @Test
+    public void size_5_exception() {
+        standardNumbers.remove(0);
+        assertThatThrownBy(() -> {
+            new WinningNumbers(standardNumbers, 6);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 6개여야 합니다");
+    }
+
+    @DisplayName("숫자가 중복될 때 예외가 발생한다")
+    @Test
+    public void duplicate_exception() {
+        final int duplicatedNumber = 5;
+
+        standardNumbers.remove(standardNumbers.indexOf(duplicatedNumber) + 1);
+        standardNumbers.add(duplicatedNumber);
+
+        assertThatThrownBy(() -> {
+            new WinningNumbers(standardNumbers, 6);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 중복되면 안됩니다");
+    }
 
     @DisplayName("당첨된 번호에 포함된 숫자일 때 true를 반환한다")
     @Test
     public void match_true() {
-        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        WinningNumbers winningNumbers = new WinningNumbers(standardNumbers, 7);
         assertThat(winningNumbers.match(1)).isTrue();
     }
 
     @DisplayName("당첨된 번호에 포함된 숫자가 아닐 때 false를 반환한다")
     @Test
     public void match_false() {
-        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        WinningNumbers winningNumbers = new WinningNumbers(standardNumbers, 7);
         assertThat(winningNumbers.match(7)).isFalse();
     }
 }
