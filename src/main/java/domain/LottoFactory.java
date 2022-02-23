fb6ca8ec50955a07558ab8f7938eb05b04c0e345
is2js <tingstyle1@gmail.com>
2022-02-23 11:18:43 +0900

    feat: 자동 로또 발급 기능 구현

--- src/main/java/domain/LottoFactory.java
+++ src/main/java/domain/LottoFactory.java
@@ -1,8 +1,29 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class LottoFactory {
 
-    public int calculateCount(final Money money) {
-        return money.calculateCounts();
+    public Count calculateCount(final Money money) {
+        return new Count(money.calculateCounts());
+    }
+
+    public LottoNumbers generateAutoLottoNumbers() {
+        HashSet<LottoNumber> autoLottoNumbers = new HashSet<>();
+        while (autoLottoNumbers.size() < 6) {
+            autoLottoNumbers.add(generateAutoLottoNumber(ThreadLocalRandom.current().nextInt(45) + 1));
+        }
+
+        return new LottoNumbers(new ArrayList<>(autoLottoNumbers));
+    }
+
+
+    //ThreadLocalRandom.current().nextInt(45) + 1
+    public LottoNumber generateAutoLottoNumber(final int number) {
+        final int autoLottoNumber = number;
+        return new LottoNumber(autoLottoNumber);
+
     }
 }
--- src/test/java/domain/LottoFactoryTest.java
+++ src/test/java/domain/LottoFactoryTest.java
@@ -2,18 +2,42 @@ package domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoFactoryTest {
+    LottoFactory lottoFactory;
+
+    @BeforeEach
+    void setUp() {
+        lottoFactory = new LottoFactory();
+    }
+
     @DisplayName("구입 금액을 로또 발급 갯수로 반환을 확인한다.")
     @ParameterizedTest
     @CsvSource(value = {"1000,1", "1100,1", "3001,3"}, delimiter = ',')
     void money_to_counts(String inputMoney, int counts) {
-        LottoFactory lottoFactory = new LottoFactory();
         final Money money = new Money(inputMoney);
 
         assertThat(lottoFactory.calculateCount(money)).isEqualTo(counts);
+    }
+
+    @DisplayName("개별 로또 번호 생성을 확인한다.")
+    @Test
+    void generate_autoLottoNumber_correct() {
+        final LottoNumber lottoNumber = lottoFactory.generateAutoLottoNumber(4);
+
+        assertThat(lottoNumber).isEqualTo(new LottoNumber(4));
+    }
+
+    @DisplayName("자동 로또 발급시 번호 갯수가 6개인지 확인한다.")
+    @Test
+    void autoLottoNumbers_size_correct() {
+        final LottoNumbers lottoNumbers = lottoFactory.generateAutoLottoNumbers();
+
+        assertThat(lottoNumbers.size()).isEqualTo(6);
     }
 }
