a4c7f6d226e9d0772406422d423e7b742035c805
= <rerub0831@gmail.com>
2022-02-23 18:15:17 +0900

    feat(Rank): 로직 변경

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,14 +1,16 @@
 package domain;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public enum Rank {
-    FIRST(2000000000, 6),
-    SECOND(30000000, 5),
-    THIRD(1500000, 5),
-    FOURTH(50000, 4),
+    DEFAULT(0, 0),
     FIFTH(5000, 3),
-    DEFAULT(0, 0);
+    FOURTH(50000, 4),
+    THIRD(1500000, 5),
+    SECOND(30000000, 5),
+    FIRST(2000000000, 6);
 
     private final int prizeMoney;
     private final int matchCount;
@@ -21,9 +23,15 @@ public enum Rank {
     public static Rank of(int matchCount, boolean isBonus) {
         return Arrays.stream(Rank.values())
                 .filter(rank -> rank.isMatch(matchCount))
-                .filter(rank -> !rank.equals(SECOND) || isBonus)
+                .filter(rank -> rank.equals(SECOND) || !isBonus)
                 .findFirst()
                 .orElse(DEFAULT);
+    }
+
+    public static List<Rank> getWithoutDefault() {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> !rank.equals(DEFAULT))
+                .collect(Collectors.toList());
     }
 
     private boolean isMatch(int matchCount) {
@@ -32,5 +40,9 @@ public enum Rank {
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
     }
 }
