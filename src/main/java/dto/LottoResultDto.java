7552a1107406bfeabda720c65a93a30a51c7a71d
Richard JEON <ztzy1907@gmail.com>
2022-02-23 17:34:39 +0900

    feat(lotto-result): 당첨 통계 출력 구현

--- src/main/java/dto/LottoResultDto.java
+++ src/main/java/dto/LottoResultDto.java
@@ -1,5 +1,64 @@
 package dto;
 
-public class LottoResultDto {
+import domain.Rank;
+import domain.TrialNumber;
+import domain.WinningCount;
+import java.util.Map;
+import java.util.Objects;
 
+public class LottoResultDto {
+    private final Map<Rank, WinningCount> lottoWinningResult;
+    private final double profitRatio;
+
+    private LottoResultDto(Map<Rank, WinningCount> lottoWinningResult, double profitRatio) {
+        this.lottoWinningResult = lottoWinningResult;
+        this.profitRatio = profitRatio;
+    }
+
+    public static LottoResultDto from(Map<Rank, WinningCount> lottoWinningResult, TrialNumber trialNumber) {
+        Long totalPrize = lottoWinningResult.entrySet()
+                .stream()
+                .map((entrySet) -> entrySet.getKey().getPrize() * entrySet.getValue().getCount())
+                .reduce(0L, Long::sum);
+
+        return new LottoResultDto(lottoWinningResult, getProfitRatio(totalPrize, trialNumber));
+    }
+
+    private static double getProfitRatio(long totalPrize, TrialNumber trialNumber) {
+        return Math.round(totalPrize / (trialNumber.getTrialNumber() * 1000.0) * 100) / 100.0;
+    }
+
+    public WinningCount getWinningCountByRank(Rank rank) {
+        return lottoWinningResult.get(rank);
+    }
+
+    public double getProfitRatio() {
+        return profitRatio;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        LottoResultDto that = (LottoResultDto) object;
+        return Double.compare(that.profitRatio, profitRatio) == 0 && Objects.equals(lottoWinningResult,
+                that.lottoWinningResult);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoWinningResult, profitRatio);
+    }
+
+    @Override
+    public String toString() {
+        return "LottoResultDto{" +
+                "lottoWinningResult=" + lottoWinningResult +
+                ", profitRatio=" + profitRatio +
+                '}';
+    }
 }
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,4 +1,20 @@
 package view;
 
+import domain.Rank;
+import dto.LottoResultDto;
+
 public class ResultView {
+    public static void printResult(LottoResultDto lottoResultDto) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.println("3개 일치 (5000원) - " + lottoResultDto.getWinningCountByRank(Rank.FIFTH).getCount() + "개");
+        System.out.println("4개 일치 (50000원) - " + lottoResultDto.getWinningCountByRank(Rank.FOURTH).getCount() + "개");
+        System.out.println("5개 일치 (1500000원) - " + lottoResultDto.getWinningCountByRank(Rank.THIRD).getCount() + "개");
+        System.out.println(
+                "5개 일치, 보너스 볼 일치(30000000원) - " + lottoResultDto.getWinningCountByRank(Rank.SECOND).getCount() + "개");
+        System.out.println(
+                "6개 일치 (2000000000원) - " + lottoResultDto.getWinningCountByRank(Rank.FIRST).getCount() + "개");
+        System.out.println("총 수익률은 " + lottoResultDto.getProfitRatio() + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+    }
 }
+
