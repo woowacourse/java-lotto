202a31741680601f817da11b07f7ed07fdb68844
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-23 11:46:33 +0900

    refactor(validator): exception messages to constants
    - reduce depth with stream

--- src/main/java/validator/LottoNumberValidators.java
+++ src/main/java/validator/LottoNumberValidators.java
@@ -1,5 +1,10 @@
 package validator;
 
+import static constant.ExceptionMessages.DUPLICATE_WINNING_NUMBER_EXCEPTION_MESSAGE;
+import static constant.ExceptionMessages.INVALID_LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE;
+import static constant.ExceptionMessages.INVALID_NUMBER_INPUT_EXCEPTION_MESSAGE;
+import static constant.ExceptionMessages.NOT_UNIQUE_BONUS_NUMBER_EXCEPTION_MESSAGE;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,13 +15,13 @@ public class LottoNumberValidators {
         try {
             return Integer.parseInt(value);
         } catch (Exception e) {
-            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException(INVALID_NUMBER_INPUT_EXCEPTION_MESSAGE);
         }
     }
 
     public static void validateLottoNumberRange(Integer num) {
         if (num < 1 || num > 45) {
-            throw new IllegalArgumentException("1과 45 사이의 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE);
         }
     }
 
@@ -24,15 +29,15 @@ public class LottoNumberValidators {
         Set<Integer> numsSet = new HashSet<>(nums);
 
         if (nums.size() != numsSet.size()) {
-            throw new IllegalArgumentException("당첨 번호는 서로 달라야 합니다.");
+            throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
     public static void validateNoDuplicateInList(int target, List<Integer> nums) {
-        for (int num : nums) {
-            if (num == target) {
-                throw new IllegalArgumentException("보너스 볼의 값은 당첨 번호와 달라야 합니다.");
-            }
+        if (nums.stream().anyMatch(num -> num == target)) {
+            throw new IllegalArgumentException(NOT_UNIQUE_BONUS_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 }
+
+
