2ad10fd104ada2ace6b880e7d05c6d3a75a87a89
leokim <kunga077@gmail.com>
2022-02-23 20:07:08 +0900

    feat: LottoRank 순서와 Rank 판단 로직 수정

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -1,19 +1,16 @@
 package domain;
 
-import java.util.Arrays;
-
 public enum LottoRank {
 
-    RANK_1(6, 2000000000),
-    RANK_2(5, 30000000),
-    RANK_3(5, 1500000),
-    RANK_4(4, 50000),
     RANK_5(3, 5000),
+    RANK_4(4, 50000),
+    RANK_3(5, 1500000),
+    RANK_2(5, 30000000),
+    RANK_1(6, 2000000000),
     RANK_NOTHING(0, 0);
 
     private final int count;
     private final int price;
-
 
     LottoRank(int count, int price) {
         this.count = count;
@@ -23,11 +20,11 @@ public enum LottoRank {
     public static LottoRank getRankByCountAndBonus(int count, boolean bonus) {
         for (LottoRank rank : LottoRank.values()) {
             if (rank.count == count) {
-                if (count == 6) {
+                if (count == 5) {
                     if (bonus) {
                         return RANK_2;
                     }
-                    return RANK_1;
+                    return RANK_3;
                 }
                 return rank;
             }
@@ -37,5 +34,9 @@ public enum LottoRank {
 
     public int getPrice() {
         return price;
+    }
+
+    public int getCount() {
+        return count;
     }
 }
