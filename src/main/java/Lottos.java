fa9eb3199044f6437b4140f105d2daf3b3439e53
tonic523 <diddmlvkf@gmail.com>
2022-02-23 14:59:07 +0900

    feat: 주어진 개수만큼 로또 생성

--- src/main/java/Lottos.java
+++ src/main/java/Lottos.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Lottos {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(int amount) {
+        this.lottos = new ArrayList<>();
+        while (amount-- > 0) {
+            Lotto lotto = new Lotto();
+            lottos.add(lotto);
+        }
+    }
+
+    int getCount() {
+        return lottos.size();
+    }
 }
--- src/test/java/LottosTest.java
+++ src/test/java/LottosTest.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class LottosTest {
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LottosTest {
+
+    @Nested
+    @DisplayName("Lottos는")
+    class NewLottos {
+
+        @Nested
+        @DisplayName("구입금액이 주어지면")
+        class Context_with_money {
+
+            @ParameterizedTest
+            @ValueSource(ints = {1, 2})
+            @DisplayName("구입금액에 맞는 개수의 로또를 생성한다.")
+            void It_create_lottos(int value) {
+                Lottos lottos = new Lottos(value);
+
+                assertThat(lottos.getCount()).isEqualTo(value);
+            }
+        }
+    }
 }
