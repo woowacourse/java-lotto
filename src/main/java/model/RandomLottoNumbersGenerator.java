9d88299c813b9df7b205f8930fc32f81cd4bd59d
양동주 <djwhy5510@gmail.com>
2022-02-23 16:33:15 +0900

    refactor: 로또 번호 난수 생성 리팩토링

--- src/main/java/model/RandomLottoNumbersGenerator.java
+++ src/main/java/model/RandomLottoNumbersGenerator.java
@@ -2,6 +2,7 @@ package model;
 
 import static java.util.stream.Collectors.toList;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -15,38 +16,33 @@ public class RandomLottoNumbersGenerator extends LottoNumbersGenerator{
     private final List<Integer> numberPool;
 
     public RandomLottoNumbersGenerator(int start, int end) {
-        numberPool = createNumbers(start, end);
+        numberPool = createNumberPool(start, end);
     }
 
-    protected LottoNumbers createLottoNumbers() {
-        Set<Integer> lottoNumbers = nextSixNumbers();
-        Iterator<Integer> iterator = lottoNumbers.iterator();
-        return LottoNumbers.withSixNumbers(iterator.next(), iterator.next(), iterator.next(),
-            iterator.next(), iterator.next(), iterator.next());
-    }
-
-    private List<Integer> createNumbers(int start, int end) {
+    private List<Integer> createNumberPool(int start, int end) {
         return IntStream.rangeClosed(start, end).boxed().collect(toList());
     }
 
-    private Set<Integer> nextSixNumbers() {
-        return get(6);
+    protected LottoNumbers createLottoNumbers() {
+        shuffleNumberPool();
+        List<Integer> sixNumbers = nextSixNumbers(createQueueByNumberPool());
+        return LottoNumbers.withSixNumbers(sixNumbers.get(0), sixNumbers.get(1), sixNumbers.get(2),
+                sixNumbers.get(3), sixNumbers.get(4), sixNumbers.get(5));
     }
 
-    public Set<Integer> get(int quantity) {
-        shuffle(numberPool);
-        Queue<Integer> queue = new LinkedList<>(numberPool);
-        Set<Integer> result = new HashSet<>();
-
-        for (int i = 0; i < quantity; i++) {
-            result.add(queue.remove());
-        }
-
+    private List<Integer> nextSixNumbers(Queue<Integer> queue) {
+        List<Integer> result = new ArrayList<>();
+        IntStream.range(0, 6)
+                .forEach(i -> result.add(queue.remove()));
         return result;
     }
 
-    private List<Integer> shuffle(List<Integer> list) {
-        Collections.shuffle(list);
-        return list;
+    private Queue<Integer> createQueueByNumberPool() {
+        Queue<Integer> queue = new LinkedList<>(numberPool);
+        return queue;
+    }
+
+    private void shuffleNumberPool() {
+        Collections.shuffle(numberPool);
     }
 }
