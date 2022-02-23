3a16462fd11df215a4d8cbce8881797f81264258
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-24 00:06:20 +0900

    refactor : LOTTO_NUMBERS 네이밍 변경

--- src/main/java/lotto/domain/RandomLottoMachine.java
+++ src/main/java/lotto/domain/RandomLottoMachine.java
@@ -8,22 +8,22 @@ import java.util.stream.IntStream;
 
 public class RandomLottoMachine {
 
-    private static final List<LottoNumber> lottoNumbers = new ArrayList<>();
+    private static final List<LottoNumber> LOTTO_NUMBERS = new ArrayList<>();
 
     static {
         IntStream.rangeClosed(LottoNumber.MIN_LOTTO_NUMBER, LottoNumber.MAX_LOTTO_NUMBER)
                 .mapToObj(LottoNumber::valueOf)
-                .forEach(lottoNumbers::add);
+                .forEach(LOTTO_NUMBERS::add);
     }
 
     private RandomLottoMachine() {
     }
 
     public static List<LottoNumber> createRandomLottoNumbers() {
-        Collections.shuffle(lottoNumbers);
+        Collections.shuffle(LOTTO_NUMBERS);
         return IntStream.rangeClosed(0, 5)
                 .boxed()
-                .map(lottoNumbers::get)
+                .map(LOTTO_NUMBERS::get)
                 .sorted()
                 .collect(Collectors.toList());
     }
