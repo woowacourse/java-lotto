87dc522d296ff89ca8c5ef1941a739723ec257af
jayjaehunchoi <wogns0108@naver.com>
2022-02-23 21:31:50 +0900

    refactor : 캐싱 자료구조 배열로 변경

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -10,11 +10,11 @@ public class LottoNumber implements Comparable<LottoNumber> {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
 
-    public static final Map<Integer, LottoNumber> LOTTO_NUMBER_CACHE = new HashMap<>();
+    public static final LottoNumber[] LOTTO_NUMBER_CACHE = new LottoNumber[MAX_LOTTO_NUMBER + 1];
 
     static {
         IntStream.rangeClosed(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
-                .forEach(number -> LOTTO_NUMBER_CACHE.put(number, new LottoNumber(number)));
+                .forEach(number -> LOTTO_NUMBER_CACHE[number] =  new LottoNumber(number));
     }
 
     private final int number;
@@ -27,7 +27,7 @@ public class LottoNumber implements Comparable<LottoNumber> {
         if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 입력값이 1 이상 45 이하여야 합니다.");
         }
-        return LOTTO_NUMBER_CACHE.get(number);
+        return LOTTO_NUMBER_CACHE[number];
     }
 
     public int getNumber() {
