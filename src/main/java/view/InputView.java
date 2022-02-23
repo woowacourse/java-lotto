9693de3846a43c7bddb77bd753234a5709d301a0
Richard JEON <ztzy1907@gmail.com>
2022-02-23 12:43:46 +0900

    feat(inputview): 당첨 로또 번호 입력 정규표현식으로 검사

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,6 +3,7 @@ package view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
@@ -12,6 +13,9 @@ public class InputView {
     private static final String MESSAGE_FOR_WINNING_LOTTO_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String SPLIT_DELIMITER = ", ";
     private static final String MESSAGE_FOR_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+    private static final Pattern WINNING_LOTTO_PATTERN = Pattern.compile("(., )+.");
+    private static final String ERROR_MESSAGE_FOR_INVALID_INPUT_FORMAT = "6개의 숫자를 ', ' 로 구분해서 입력해주세요. 😆";
 
     public static int scanInputMoney() {
         System.out.println(MESSAGE_TO_GET_INPUT_MONEY);
@@ -19,7 +23,7 @@ public class InputView {
     }
 
     public static void printException(Exception exception) {
-        System.out.println(exception.getMessage());
+        System.out.println(ERROR_MESSAGE_PREFIX + exception.getMessage());
     }
 
     public static void printLottoCount(int count) {
@@ -29,11 +33,21 @@ public class InputView {
     public static List<Integer> scanWinningNumbers() {
         System.out.print(System.lineSeparator());
         System.out.println(MESSAGE_FOR_WINNING_LOTTO_NUMBERS);
-        String[] splitNumbers = SCANNER.nextLine().split(SPLIT_DELIMITER);
 
-        return Arrays.stream(splitNumbers)
+        String userInput = SCANNER.nextLine();
+        validateInputFormat(userInput);
+
+        return Arrays.stream(userInput.split(SPLIT_DELIMITER))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    private static void validateInputFormat(String userInput) {
+        boolean matches = WINNING_LOTTO_PATTERN.matcher(userInput).matches();
+
+        if (!matches) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_INVALID_INPUT_FORMAT);
+        }
     }
 
     public static int scanBonusNumber() {
