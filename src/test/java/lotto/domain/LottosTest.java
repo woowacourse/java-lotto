563c0b6f972b42bc396ebfe994d15cc78df861d6
jayjaehunchoi <wogns0108@naver.com>
2022-02-23 11:36:33 +0900

    test : 미사용 import 제거 및 공통 부분 통합

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -3,30 +3,32 @@ package lotto.domain;
 import static lotto.domain.LottoTest.createLottoNumbers;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LottosTest {
 
-    @Test
-    void constructor() {
-        final Lottos lottos = new Lottos(Stream.of(createLottoNumbers(1, 2, 3, 4, 5, 6),
+    private Lottos lottos;
+
+    @BeforeEach
+    void setUp() {
+        lottos = new Lottos(Stream.of(createLottoNumbers(1, 2, 3, 4, 5, 6),
                         createLottoNumbers(2, 3, 4, 5, 6, 7))
                 .map(Lotto::new)
                 .collect(Collectors.toList()));
+    }
+
+    @Test
+    void constructor() {
         assertThat(lottos).isInstanceOf(Lottos.class);
     }
 
     @Test
     void createLottoResult() {
         // given
-        final Lottos lottos = new Lottos(Stream.of(createLottoNumbers(1, 2, 3, 4, 5, 6),
-                        createLottoNumbers(2, 3, 4, 5, 6, 7))
-                .map(Lotto::new)
-                .collect(Collectors.toList()));
         final WinLotto winLotto = new WinLotto(new Lotto(createLottoNumbers(1, 2, 3, 4, 5, 6)), new LottoNumber(7));
 
         final Map<Rank, Integer> resultMap = Rank.initResultMap();
--- src/test/java/lotto/domain/WinLottoTest.java
+++ src/test/java/lotto/domain/WinLottoTest.java
@@ -1,11 +1,10 @@
 package lotto.domain;
 
+import static lotto.domain.LottoTest.createLottoNumbers;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +13,7 @@ public class WinLottoTest {
     @DisplayName("보너스 볼과 당첨 번호가 중복되는 경우 에러 발생")
     @Test
     void duplicateBonusBallNumber() {
-        final List<LottoNumber> winNumbers = Stream.of(1, 2, 3, 4, 5, 6)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
+        final List<LottoNumber> winNumbers = createLottoNumbers(1, 2, 3, 4, 5, 6);
         final LottoNumber duplicateBonusLottoNumber = new LottoNumber(1);
 
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -27,9 +24,7 @@ public class WinLottoTest {
     @DisplayName("로또 맞춘 개수에 다라 랭크를 계산한다.")
     @Test
     void calculateMatchNumber() {
-        final List<LottoNumber> winNumbers = Stream.of(1, 2, 3, 4, 5, 6)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
+        final List<LottoNumber> winNumbers = createLottoNumbers(1, 2, 3, 4, 5, 6);
         final LottoNumber bonusLottoNumber = new LottoNumber(7);
         final WinLotto winLotto = new WinLotto(new Lotto(winNumbers), bonusLottoNumber);
 
