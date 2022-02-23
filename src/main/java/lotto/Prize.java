85cadf002ddeea2a4b35d553aaa8e2fa05d35df6
YJGwon <yj970125@gmail.com>
2022-02-23 14:50:16 +0900

    feat: 보너스볼 포함한 일치 개수에 따른 등수 반환

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public enum Prize {
     FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000);
@@ -16,10 +17,20 @@ public enum Prize {
         this.amount = amount;
     }
 
-    public static Prize getPrize(int matchCount) {
+    public static Prize getPrize(int matchCount, boolean bonus) {
+        if (matchCount != SECOND.matchCount) {
+            return getPrize(matchCount);
+        }
+        if (bonus) {
+            return SECOND;
+        }
+        return THIRD;
+    }
+
+    private static Prize getPrize(int matchCount) {
         return Arrays.stream(values())
-                .filter(prize -> prize.matchCount == matchCount)
-                .findFirst()
-                .get();
+            .filter(prize -> prize.matchCount == matchCount)
+            .findFirst()
+            .get();
     }
 }
--- src/test/java/lotto/PrizeTest.java
+++ src/test/java/lotto/PrizeTest.java
@@ -10,24 +10,30 @@ public class PrizeTest {
     @DisplayName("3개가 일치하면 5등을 반환한다")
     @Test
     void match_3_fifth() {
-        assertThat(Prize.getPrize(3)).isEqualTo(Prize.FIFTH);
+        assertThat(Prize.getPrize(3, false)).isEqualTo(Prize.FIFTH);
     }
 
     @DisplayName("4개가 일치하면 4등을 반환한다")
     @Test
     void match_4_fourth() {
-        assertThat(Prize.getPrize(4)).isEqualTo(Prize.FOURTH);
+        assertThat(Prize.getPrize(4, false)).isEqualTo(Prize.FOURTH);
     }
 
     @DisplayName("5개가 일치하면 3등을 반환한다")
     @Test
     void match_5_third() {
-        assertThat(Prize.getPrize(5)).isEqualTo(Prize.THIRD);
+        assertThat(Prize.getPrize(5, false)).isEqualTo(Prize.THIRD);
     }
 
     @DisplayName("6개가 일치하면 1등을 반환한다")
     @Test
     void match_6_first() {
-        assertThat(Prize.getPrize(6)).isEqualTo(Prize.FIRST);
+        assertThat(Prize.getPrize(6, false)).isEqualTo(Prize.FIRST);
+    }
+
+    @DisplayName("5개가 일치하고 보너스가 일치하면 2등을 반환한다")
+    @Test
+    void match_5_bonus_second() {
+        assertThat(Prize.getPrize(5, true)).isEqualTo(Prize.SECOND);
     }
 }
