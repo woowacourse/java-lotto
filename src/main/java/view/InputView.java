0bbc814d099863047d42ee27a7995680e3f42937
Richard JEON <ztzy1907@gmail.com>
2022-02-23 11:03:13 +0900

    feat(inputview): 로또 구입 금액을 입력 받음

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,17 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String MESSAGE_TO_GET_INPUT_MONEY = "구입금액을 입력해 주세요.";
+
+    public static int scanInputMoney() {
+        System.out.println(MESSAGE_TO_GET_INPUT_MONEY);
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static void printException(Exception exception) {
+        System.out.println(exception.getMessage());
+    }
 }
