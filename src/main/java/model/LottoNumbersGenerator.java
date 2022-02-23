b47836e230585834828c2c5a6ea25316adaa0ef4
wilgur513 <wilgur513@naver.com>
2022-02-23 15:05:34 +0900

    test: 로또 번호 생성을 위한 스텁 클래스 생성

--- src/main/java/model/LottoNumbersGenerator.java
+++ src/main/java/model/LottoNumbersGenerator.java
@@ -3,6 +3,10 @@ package model;
 import java.util.List;
 
 public class LottoNumbersGenerator {
+
+    public LottoNumbersGenerator(NumberQueue numberQueue) {
+    }
+
     public List<LottoNumbers> generate(int quantity) {
         return null;
     }
--- src/test/java/model/LottoNumbersGeneratorTest.java
+++ src/test/java/model/LottoNumbersGeneratorTest.java
@@ -9,11 +9,32 @@ import org.junit.jupiter.api.Test;
 
 public class LottoNumbersGeneratorTest {
 
+    static class NumberQueueStub implements NumberQueue {
+
+        private final int[] numbers = {
+            1, 2, 3, 4, 5, 6,
+            7, 8, 9, 10, 11, 12,
+            13, 14, 15, 16, 17, 18
+        };
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public int get() {
+            return 0;
+        }
+    }
+
+
     @Test
     @Disabled
     @DisplayName("입력한 수 만큼 로또 발급 테스트")
     void createLottoNumbersTest() {
-        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
+        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator(
+            new NumberQueueStub());
         List<LottoNumbers> lottoNumbersList = lottoNumbersGenerator.generate(3);
         assertThat(lottoNumbersList).hasSize(3);
     }
