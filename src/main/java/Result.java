4edcf15072bdc0647847f5c01ee199ab97f69e7d
tonic523 <diddmlvkf@gmail.com>
2022-02-23 15:51:02 +0900

    feat: 당첨 결과를 순위와 개수로 저장

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Result {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Result {
+
+    private static final int INIT_COUNT = 0;
+    private static final int ADD_NUMBER = 1;
+
+    private final Map<WinningPrice, Integer> result;
+
+    public Result() {
+        result = new HashMap<>();
+        for (WinningPrice value : WinningPrice.values()) {
+            result.put(value, INIT_COUNT);
+        }
+    }
+
+    public int getCount(WinningPrice key) {
+        return result.get(key);
+    }
+
+    public void add(WinningPrice key) {
+        result.put(key, result.get(key) + ADD_NUMBER);
+    }
 }
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class ResultTest {
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class ResultTest {
+
+    @Nested
+    @DisplayName("결과는")
+    class NewResult {
+
+        @Nested
+        @DisplayName("생성될 때")
+        class Context_with_empty {
+
+            @Test
+            @DisplayName("순위 별 개수를 0으로 초기화한다.")
+            void it_create_ok() {
+                Result result = new Result();
+
+                for (WinningPrice value : WinningPrice.values()) {
+                    assertThat(result.getCount(value)).isEqualTo(0);
+                }
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("등수의 개수를 올리는 메소드는")
+    class AddCount {
+
+        @Nested
+        @DisplayName("WinningPrice가 주어지면")
+        class Context_with_winning_price {
+
+            @Test
+            @DisplayName("해당 WinningPrice의 개수가 1 올라간다.")
+            void it_add_count() {
+                Result result = new Result();
+
+                result.add(WinningPrice.All);
+
+                assertThat(result.getCount(WinningPrice.All)).isEqualTo(1);
+            }
+        }
+    }
 }
