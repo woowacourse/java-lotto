aa801eca193efa335c26f8f5689bffa5d3729676
jojogreen91 <iawbg13@gmail.com>
2022-02-24 00:24:10 +0900

    feat: 당첨 번호와 보너스 번호를 입력받는 기능

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -3,7 +3,7 @@ package lotto.controller;
 import java.util.Scanner;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
-import lotto.exception.MoneyException;
+import lotto.domain.WinningNumbers;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -14,14 +14,24 @@ public class Controller {
         Lottos lottos = Lottos.buyLottosByAuto(money);
         OutputView.printTotalLottoCount(lottos.getTotalLottoCount());
         OutputView.printLottos(lottos);
+        WinningNumbers winningNumbers = getWinningNumbers(scanner);
     }
 
     private Money getMoney(Scanner scanner) {
         try {
             return new Money(InputView.inputMoney(scanner));
-        } catch (MoneyException exception) {
+        } catch (IllegalArgumentException exception) {
             OutputView.printErrorMessage(exception.getMessage());
             return getMoney(scanner);
+        }
+    }
+
+    private WinningNumbers getWinningNumbers(Scanner scanner) {
+        try {
+            return new WinningNumbers(InputView.inputWinningLotto(scanner), InputView.inputBonusNumber(scanner));
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception.getMessage());
+            return getWinningNumbers(scanner);
         }
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,23 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
+
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_LOTTO_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
 
     public static String inputMoney(Scanner scanner) {
         System.out.println(INPUT_MONEY_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputWinningLotto(Scanner scanner) {
+        System.out.println(INPUT_WINNING_LOTTO_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputBonusNumber(Scanner scanner) {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return scanner.nextLine();
     }
 }
