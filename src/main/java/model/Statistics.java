43ab9d33787d51c71dd06c66611d5c4d56fa8927
jaejae-yoo <wotj102@gmail.com>
2022-02-23 17:34:31 +0900

    feat: 통계를 위한 enum 객체 생성

---
+++ src/main/java/model/Statistics.java
@@ -0,0 +1,26 @@
+package model;
+
+public enum Statistics {
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    BONUS(5, 30000000),
+    SIX(6, 2000000000);
+
+    private final int matchNumber;
+    private final int value;
+    private int count;
+
+    Statistics(int matchNumber, int value) {
+        this.matchNumber = matchNumber;
+        this.value = value;
+    }
+
+    public void addCount() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
