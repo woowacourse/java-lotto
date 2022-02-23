450d22342297a831249153e244d8d8b69d977155
hyeonic <dev.hyeonic@gmail.com>
2022-02-23 15:26:24 +0900

    feat: LottoTickets 일급컬렉션 추가
    LottoTicket 리스트를 관리하는 일급컬렉션을 추가

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.generator.LottoNumberGenerator;
+
+public class LottoTickets {
+
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(int lottoCount, LottoNumberGenerator lottoNumberGenerator) {
+        this.lottoTickets = generateTickets(lottoCount, lottoNumberGenerator);
+    }
+
+    private List<LottoTicket> generateTickets(int lottoCount, LottoNumberGenerator lottoNumberGenerator) {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            LottoTicket lottoTicket = new LottoTicket(lottoNumberGenerator);
+            lottoTickets.add(lottoTicket);
+        }
+
+        return lottoTickets;
+    }
+}
