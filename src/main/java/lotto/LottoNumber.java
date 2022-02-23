9c947de14f1bda755eeb9c63fd07f53f334b35be
yh20studio(tiki) <yh20studio@gmail.com>
2022-02-23 21:58:41 +0900

    feat: 로또 번호가 범위를 벗어나는 경우 예외 처리 구현

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,21 @@
+package lotto;
+
+public class LottoNumber {
+
+    private static final String ERROR_NUMBER_RANGE = "로또 번호가 범위를 벗어났습니다.";
+    private static final int MINIMUM_RANGE = 1;
+    private static final int MAXIMUM_RANGE = 45;
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        this.number = number;
+        validateRange();
+    }
+
+    private void validateRange() {
+        if (number < MINIMUM_RANGE || number > MAXIMUM_RANGE) {
+            throw new RuntimeException(ERROR_NUMBER_RANGE);
+        }
+    }
+}
