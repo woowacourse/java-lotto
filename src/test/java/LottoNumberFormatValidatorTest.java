1220299297b0d1c25af11ebdcb1f3be6dc1b231d
양동주 <djwhy5510@gmail.com>
2022-02-23 11:36:22 +0900

    test: 로또 번호 포맷 검증 예외 발생 테스트

--- src/test/java/LottoNumberFormatValidatorTest.java
+++ src/test/java/LottoNumberFormatValidatorTest.java
@@ -1,18 +1,34 @@
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoNumberFormatValidatorTest {
+    private LottoNumberFormatValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new LottoNumberFormatValidator();
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"1, 2, 3, 4, 5, 6", "1,   2,  3   , 4,5,6"})
     @DisplayName("올바른 로또 번호 포맷 검증")
     void validateValidLottoNumberFormat(String lottoNumbers) {
-        LottoNumberFormatValidator validator = new LottoNumberFormatValidator();
-
         assertThatCode(() -> validator.validate(lottoNumbers))
             .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100,200,3,4,5,6", "1,2,3,4,5"})
+    @DisplayName("잘못된 로또 번호 포맷 검증")
+    void validateInvalidLottoNumberFormat(String invalidLottoNumbers) {
+        assertThatThrownBy(() -> validator.validate(invalidLottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LottoNumberFormatValidator.INVALID_LOTTO_NUMBER_FORMAT_MESSAGE);
     }
 }
