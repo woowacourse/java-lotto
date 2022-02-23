07417f6e597a0bab33679158fc2ffb7000ba5959
YJGwon <yj970125@gmail.com>
2022-02-24 00:50:08 +0900

    feat: 입력 로직 추가
    1. 구입 금액
    2. 당첨 번호
    3. 보너스 번호

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String MESSAGE_MONEY_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String MESSAGE_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
+
+    private final Scanner scanner;
+
+    public InputView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String askMoneyAmount() {
+        System.out.println(MESSAGE_MONEY_AMOUNT);
+        return scanner.nextLine();
+    }
+
+    public String askWinningNumbers() {
+        System.out.println(MESSAGE_WINNING_NUMBERS);
+        return scanner.nextLine();
+    }
+
+    public String askBonusNumber() {
+        System.out.println(MESSAGE_BONUS_NUMBER);
+        return scanner.nextLine();
+    }
+}
