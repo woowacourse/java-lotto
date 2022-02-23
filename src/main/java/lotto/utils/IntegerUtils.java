78c91f21670cc911f280da0d6ac94574aada4ef1
cjlee38 <cjlee38@github.com>
2022-02-23 17:59:49 +0900

    feat(utils): 정수로 변환하는 유틸성 메소드 추가

---
+++ src/main/java/lotto/utils/IntegerUtils.java
@@ -0,0 +1,12 @@
+package lotto.utils;
+
+public class IntegerUtils {
+
+    public static int parse(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+    }
+}
