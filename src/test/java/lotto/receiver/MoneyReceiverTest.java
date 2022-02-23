88ad353119da4c1cdeb918c9af9e61ea9489b339
summerlunaa <summerlunaa9@gmail.com>
2022-02-23 13:46:47 +0900

    test: receiver에 대한 테스트 추가

--- src/test/java/lotto/receiver/MoneyReceiverTest.java
+++ src/test/java/lotto/receiver/MoneyReceiverTest.java
@@ -2,10 +2,18 @@ package lotto.receiver;
 
 import lotto.exception.MoneyException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoneyReceiverTest {
+
+    @Test
+    @DisplayName("구입금액을 검증하여 정수로 반환한다.")
+    void receive() {
+        Assertions.assertThat(MoneyReceiver.receive("14000")).isEqualTo(14000);
+    }
 
     @ParameterizedTest(name = "자연수가 아닐 경우 예외 발생 - case : {0}")
     @ValueSource(strings = {"1.2", "test", "-300", "0", "0123", "00000"})
--- src/test/java/lotto/receiver/WinningNumbersReceiverTest.java
+++ src/test/java/lotto/receiver/WinningNumbersReceiverTest.java
@@ -1,11 +1,26 @@
 package lotto.receiver;
 
+import lotto.domain.LottoNumber;
 import lotto.exception.WinningNumbersException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class WinningNumbersReceiverTest {
+
+    @Test
+    @DisplayName("구입금액을 검증하여 정수로 반환한다.")
+    void receive() {
+        Assertions.assertThat(WinningNumbersReceiver.receive("45, 1, 10, 4, 20, 30"))
+                .containsExactly(LottoNumber.NUMBER_1,
+                        LottoNumber.NUMBER_4,
+                        LottoNumber.NUMBER_10,
+                        LottoNumber.NUMBER_20,
+                        LottoNumber.NUMBER_30,
+                        LottoNumber.NUMBER_45);
+    }
 
     @ParameterizedTest(name = "숫자가 6개가 아닌 경우 예외 발생 - case : {0}")
     @ValueSource(strings = {"1, 2, 3, 4, 5", "1, 2, 3, 4, 5, 6, 7"})
