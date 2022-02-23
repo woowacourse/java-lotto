765622e2ef76dd6347c9ad0092deedac220fbabd
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-23 23:21:27 +0900

    feat(LottoNumber): use cache for already created number, add test case

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -2,19 +2,39 @@ package domain;
 
 import static validator.NumberValidators.validateLottoNumberRange;
 
-import java.util.Objects;
+import java.util.HashMap;
 
 public class LottoNumber implements Comparable<LottoNumber> {
 
+    private static final HashMap<Integer, LottoNumber> cache = new HashMap<>();
+
     private final int number;
 
-    private LottoNumber (int value) {
+    private LottoNumber(int value) {
         number = value;
     }
 
     public static LottoNumber of(int value) {
+        LottoNumber lottoNumber = getCacheIfExists(value);
+
+        if (lottoNumber != null) {
+            return lottoNumber;
+        }
+
         validateLottoNumberRange(value);
-        return new LottoNumber(value);
+        return createNewCache(value);
+    }
+
+    private static LottoNumber getCacheIfExists(int num) {
+        return cache.get(num);
+    }
+
+    private static LottoNumber createNewCache(int num) {
+        LottoNumber lottoNumber = new LottoNumber(num);
+
+        cache.put(num, new LottoNumber(num));
+
+        return lottoNumber;
     }
 
     public int getNumber() {
@@ -24,22 +44,5 @@ public class LottoNumber implements Comparable<LottoNumber> {
     @Override
     public int compareTo(LottoNumber lottoNumber) {
         return this.number - lottoNumber.getNumber();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        LottoNumber that = (LottoNumber) o;
-        return number == that.number;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(number);
     }
 }
--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -1,6 +1,8 @@
 package domain;
 
 import static constant.ExceptionMessages.INVALID_LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE;
+import static constant.LottoConstants.MAXIMUM_LOTTO_NUMBER;
+import static constant.LottoConstants.MINIMUM_LOTTO_NUMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.params.ParameterizedTest.ARGUMENTS_PLACEHOLDER;
@@ -22,10 +24,18 @@ public class LottoNumberTest {
     }
 
     @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
-    @ValueSource(ints = {0, 46})
+    @ValueSource(ints = {MINIMUM_LOTTO_NUMBER - 1, MAXIMUM_LOTTO_NUMBER + 1})
     void of_failsOnInvalidRangeInteger(int value) {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> LottoNumber.of(value))
                 .withMessageMatching(INVALID_LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void of_returnsCacheForAlreadyCreatedNumber() {
+        LottoNumber newLottoNum = LottoNumber.of(10);
+        LottoNumber cachedLottoNum = LottoNumber.of(10);
+
+        assertThat(newLottoNum).isEqualTo(cachedLottoNum);
     }
 }
