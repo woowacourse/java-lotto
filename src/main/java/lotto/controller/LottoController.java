fea0d0c566f7b3d4679e8f449c8bad5e26a78115
hyeonic <dev.hyeonic@gmail.com>
2022-02-23 18:36:06 +0900

    feat: 당첨번호 컨트롤러 구현
    당첨번호 및 보너스 볼을 처리하는 컨트롤러 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,12 @@
 package lotto.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import lotto.domain.LottoMachine;
 import lotto.domain.LottoTickets;
+import lotto.domain.WinningNumbers;
 
 public class LottoController {
 
@@ -15,7 +19,23 @@ public class LottoController {
         System.out.println(lottoTickets.totalCount() + "개를 구매했습니다.");
         System.out.println(lottoTickets.getTicketsInfo());
 
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        WinningNumbers winningNumbers = createWinningNumbers(scanner);
 
+    }
+
+    private WinningNumbers createWinningNumbers(Scanner scanner) {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String inputWinningNumbers = scanner.nextLine();
+        String[] splitWinningNumbers = inputWinningNumbers.split(", ");
+
+        List<Integer> winningNumbers = Arrays.stream(splitWinningNumbers)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        System.out.println("보너스 볼을 입력해 주세요.");
+
+        int bonusNumber = scanner.nextInt();
+
+        return new WinningNumbers(winningNumbers, bonusNumber);
     }
 }
