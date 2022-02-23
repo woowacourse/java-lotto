f45e09beab4530b1395cfeff1169c13e8ee12f39
tonic523 <diddmlvkf@gmail.com>
2022-02-23 18:17:45 +0900

    feat: 입력 요청 view 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,29 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static String inputMoney() {
+        System.out.println(INPUT_MONEY_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputLastWeekWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return scanner.nextLine();
+    }
+}
