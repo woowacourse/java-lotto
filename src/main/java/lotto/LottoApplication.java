8e1ef02597e72db799925714de3ebd8af092e9c2
jayjaehunchoi <wogns0108@naver.com>
2022-02-23 12:47:47 +0900

    feat : 당첨번호 입력 기능 구현

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,13 +1,14 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.domain.RandomLottoMachine;
+import lotto.domain.WinLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -18,12 +19,23 @@ public class LottoApplication {
         final int buyCounts = money.calculateLottoCount();
         OutputView.outputBuyLottoCounts(buyCounts);
 
-        Lottos lottos = new Lottos(buyRandomLottos(buyCounts));
+        final Lottos lottos = new Lottos(buyRandomLottos(buyCounts));
         OutputView.outputLottos(lottos.getLottos());
 
+        final WinLotto winLotto = new WinLotto(inputWinLotto(), inputBonusNumber());
     }
 
-    private static List<Lotto> buyRandomLottos(int buyCounts) {
+    private static Lotto inputWinLotto() {
+        return new Lotto(InputView.inputWinLotto().stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList()));
+    }
+
+    private static LottoNumber inputBonusNumber() {
+        return new LottoNumber(InputView.inputBonusNumber());
+    }
+
+    private static List<Lotto> buyRandomLottos(final int buyCounts) {
         return IntStream.rangeClosed(0, buyCounts)
                 .mapToObj(index -> new Lotto(RandomLottoMachine.createRandomLottoNumbers()))
                 .collect(Collectors.toList());
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,19 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final String INPUT_PURCHASE_MONEY = "구매금액을 입력해 주세요.";
+    private static final String INPUT_WIN_LOTTO_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_LOTTO_NUMBER = "보너스 볼을 입력해 주세요.";
+
+    private static final String INPUT_WIN_LOTTO_NUMBERS_DELIMITER = ",";
+    private static final String INPUT_BLANK = " ";
+    private static final String INPUT_NOT_BLANK = "";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -13,6 +22,23 @@ public class InputView {
 
     public static int inputMoney() {
         System.out.println(INPUT_PURCHASE_MONEY);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<Integer> inputWinLotto() {
+        System.out.println(INPUT_WIN_LOTTO_NUMBERS);
+        return Arrays.stream(scanner.nextLine().split(INPUT_WIN_LOTTO_NUMBERS_DELIMITER))
+                .map(InputView::removeBlank)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static String removeBlank(String input) {
+        return input.replaceAll(INPUT_BLANK, INPUT_NOT_BLANK);
+    }
+
+    public static int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_LOTTO_NUMBER);
         return Integer.parseInt(scanner.nextLine());
     }
 }
