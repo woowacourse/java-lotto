ad67772d8fbacdcdca3db8f983a442fa21eaf7eb
woong7 <driedseas@gmail.com>
2022-02-23 15:01:30 +0900

    feat(BonusNumber): 입력값을 검증하고 보너스넘버 인스턴스 생성

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class BonusNumber {
+    private int bonusNumber;
+
+    public BonusNumber(String input, PickedNumbers pickedNumbers) {
+        bonusNumber = Integer.parseInt(input);
+
+        validateRange();
+        validateDuplicate(pickedNumbers.getPickedNumbers());
+    }
+
+    private void validateDuplicate(List<Integer> pickedNumbers) {
+        if (pickedNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("중복값이 있습니다");
+        }
+    }
+
+    private void validateRange() {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("범위내에 없습니다");
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
