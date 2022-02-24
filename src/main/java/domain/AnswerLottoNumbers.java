a23858867daeca4bf94b43511a2c5d0e7e48064b
ulimy <ulim130@gmail.com>
2022-02-24 23:31:28 +0900

    refactor : 매직넘버 상수화 및 메서드명 변경

--- src/main/java/domain/AnswerLottoNumbers.java
+++ src/main/java/domain/AnswerLottoNumbers.java
@@ -9,14 +9,14 @@ import static constant.LottoConstant.*;
 
 public class AnswerLottoNumbers {
 
-	private static final String NUMBER_IN_RANGE = START_ERROR + "1이상 45 이하의 숫자만 허용됩니다.";
+	private static final String NUMBER_IN_RANGE = START_ERROR + MIN_NUMBER + "이상 " + MAX_NUMBER + "이하의 숫자만 허용됩니다.";
 	private static final String NUMBER_OF_NUMBERS_IS_NOT_CORRECT = START_ERROR + NUMBER_OF_NUMBERS + "개의 숫자만 허용됩니다.";
 	private static final String NUMBERS_MUST_NOT_DUPLICATED = START_ERROR + "지난 주 당첨 번호 숫자들은 중복일 수 없습니다.";
 
 	private final List<Integer> numbers;
 
 	public AnswerLottoNumbers(List<Integer> numbers) {
-		validateAllCondition(numbers);
+		validateNumbers(numbers);
 		this.numbers = numbers;
 	}
 
@@ -28,24 +28,24 @@ public class AnswerLottoNumbers {
 		return Collections.unmodifiableList(this.numbers);
 	}
 
-	private void validateAllCondition(List<Integer> numbers) {
-		validateLength(numbers);
-		validateNumbers(numbers);
-		validateDuplicateInNumbers(numbers);
-	}
-
 	private void validateNumbers(List<Integer> numbers) {
-		int upperCount = (int) numbers.stream().filter(number -> number > MAX_NUMBER).count();
-		int lowerCount = (int) numbers.stream().filter(number -> number < MIN_NUMBER).count();
-
-		if (upperCount > 0 || lowerCount > 0) {
-			throw new IllegalArgumentException(NUMBER_IN_RANGE);
-		}
+		validateLength(numbers);
+		validateNumbersInRange(numbers);
+		validateDuplicateInNumbers(numbers);
 	}
 
 	private void validateLength(List<Integer> numbers) {
 		if (numbers.size() != NUMBER_OF_NUMBERS) {
 			throw new IllegalArgumentException(NUMBER_OF_NUMBERS_IS_NOT_CORRECT);
+		}
+	}
+
+	private void validateNumbersInRange(List<Integer> numbers) {
+		int upperCount = (int) numbers.stream().filter(number -> number > MAX_NUMBER).count();
+		int lowerCount = (int) numbers.stream().filter(number -> number < MIN_NUMBER).count();
+
+		if (upperCount > 0 || lowerCount > 0) {
+			throw new IllegalArgumentException(NUMBER_IN_RANGE);
 		}
 	}
 
