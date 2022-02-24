219b9cc36e5eb24248c246e9f02e2418a51d7499
Chris <bjuuuu98@gmail.com>
2022-02-24 14:56:55 +0900

    refactor: 예외메시지 상수화

--- src/main/java/lotto/domain/LottoMatchKind.java
+++ src/main/java/lotto/domain/LottoMatchKind.java
@@ -10,6 +10,8 @@ public enum LottoMatchKind {
     FIVE_BONUS(5, true, 30000000),
     SIX(6, false, 2000000000);
 
+    private static final String MATCH_KIND_NOT_FOUND_EXCEPTION_MESSAGE = "존재하지 않는 당첨 종류가 발생했습니다.";
+
     private final int matchCount;
     private final boolean bonus;
     private final long winningAmount;
@@ -19,13 +21,13 @@ public enum LottoMatchKind {
         this.bonus = bonus;
         this.winningAmount = winningAmount;
     }
-// TODO : 예외 메시지 상수화
+
     public static LottoMatchKind from(final int matchCount, final boolean bonus) {
         return Arrays.stream(values())
                 .filter(matchKind -> matchKind.matchCount == matchCount)
                 .filter(matchKind -> matchKind.bonus == bonus)
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("매치 개수는 3개 미만이거나 6개 초과입니다."));
+                .orElseThrow(() -> new IllegalArgumentException(MATCH_KIND_NOT_FOUND_EXCEPTION_MESSAGE));
     }
 
     public long getProfit(final int countOfMatchedLottoNumbers) {
