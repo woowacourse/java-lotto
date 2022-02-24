174dc5e27d770cb05d97269da3200f0266993df3
SuyeonChoi <tndus970120@gmail.com>
2022-02-24 11:53:19 +0900

    refactor(LotteryMachine): main 함수 분리

--- src/main/java/lotterymachine/LotteryMachine.java
+++ src/main/java/lotterymachine/LotteryMachine.java
@@ -1,10 +1,13 @@
 package lotterymachine;
 
+import static lotterymachine.utils.LotteryCalculator.calculateProfitRate;
+import static lotterymachine.utils.LotteryCalculator.divideByLotteryPrice;
+import static lotterymachine.utils.LotteryNumbersGenerator.generate;
+import static lotterymachine.utils.LotteryRule.TICKET_PRICE;
+
 import lotterymachine.dto.LotteryResultDto;
 import lotterymachine.model.LotteryTicket;
 import lotterymachine.model.LotteryTickets;
-import lotterymachine.utils.LotteryCalculator;
-import lotterymachine.utils.LotteryNumbersGenerator;
 import lotterymachine.view.InputView;
 import lotterymachine.view.OutputView;
 
@@ -14,30 +17,41 @@ import java.util.List;
 public class LotteryMachine {
     public static void main(String[] args) {
         int amount = InputView.getAmount();
-        int numberOfTickets = LotteryCalculator.divideByLotteryPrice(amount);
+        int numberOfTickets = divideByLotteryPrice(amount);
         OutputView.printNumberOfTicket(numberOfTickets);
-        LotteryTickets lotteryTickets = new LotteryTickets(createLotteryTickets(amount, numberOfTickets));
+
+        LotteryTickets lotteryTickets = new LotteryTickets(createLotteryTickets(numberOfTickets));
         OutputView.printLotteryTickets(lotteryTickets.getLotteryTickets());
-        List<Integer> winningNumbers = InputView.getWinningLotteryNumbers();
-        int bonusNumber = InputView.getBonusNumber(winningNumbers);
-        List<LotteryResultDto> lotteryResult = LotteryResultDto.createList(lotteryTickets.getLotteriesResult(winningNumbers, bonusNumber));
-        OutputView.printStatistics(lotteryResult);
-        OutputView.printProfitRate(LotteryCalculator.calculateProfitRate(winningLotteryAmount(lotteryResult), numberOfTickets * 1000));
+
+        List<LotteryResultDto> lotteryResult = getLotteryResult(lotteryTickets);
+        printResult(numberOfTickets, lotteryResult);
     }
 
-    private static int winningLotteryAmount(List<LotteryResultDto> lotteryResult) {
-        int sum = 0;
-        for (LotteryResultDto lotteryResultDto : lotteryResult) {
-            sum += lotteryResultDto.sumIncome();
-        }
-        return sum;
-    }
-
-    private static List<LotteryTicket> createLotteryTickets(int amount, int numberOfTickets) {
+    private static List<LotteryTicket> createLotteryTickets(int numberOfTickets) {
         List<LotteryTicket> lotteryTickets = new ArrayList<>();
         for (int i = 0; i < numberOfTickets; i++) {
-            lotteryTickets.add(new LotteryTicket(LotteryNumbersGenerator.generate()));
+            lotteryTickets.add(new LotteryTicket(generate()));
         }
         return lotteryTickets;
+    }
+
+    private static List<LotteryResultDto> getLotteryResult(LotteryTickets lotteryTickets) {
+        List<Integer> winningNumbers = InputView.getWinningLotteryNumbers();
+        int bonusNumber = InputView.getBonusNumber(winningNumbers);
+        return LotteryResultDto.createList(lotteryTickets.getLotteriesResult(winningNumbers, bonusNumber));
+    }
+
+    private static void printResult(int numberOfTickets, List<LotteryResultDto> lotteryResult) {
+        OutputView.printStatistics(lotteryResult);
+        int ticketAmount = numberOfTickets * TICKET_PRICE.getNumber();
+        int winningLotteryAmount = getWinningLotteryAmount(lotteryResult);
+        OutputView.printProfitRate(calculateProfitRate(winningLotteryAmount, ticketAmount));
+    }
+
+    private static int getWinningLotteryAmount(List<LotteryResultDto> lotteryResult) {
+        return lotteryResult.stream()
+                .map(LotteryResultDto::sumIncome)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
--- src/main/java/lotterymachine/utils/LotteryRule.java
+++ src/main/java/lotterymachine/utils/LotteryRule.java
@@ -3,7 +3,8 @@ package lotterymachine.utils;
 public enum LotteryRule {
     MINIMUM(1),
     MAXIMUM(45),
-    TICKET_SIZE(6);
+    TICKET_SIZE(6),
+    TICKET_PRICE(1000);
 
     private final int number;
 
