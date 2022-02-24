d0eaa4c37e3fa844d75fed7b0623270a943ad5d4
Juhyung990122 <harukawa99@naver.com>
2022-02-24 15:02:56 +0900

    refactor: 출력 개행 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -86,6 +86,7 @@ public class OutputView {
         for (LottoDto lottoDto : lottosDto.getLottoDtos()) {
             printLottoNumbers(lottoDto);
         }
+        System.out.print(System.lineSeparator());
     }
 
     public static void printLottoNumbers(LottoDto lottoDto) {
@@ -94,6 +95,7 @@ public class OutputView {
     }
 
     public static void printResult(RanksDto ranksDto) {
+        System.out.print(System.lineSeparator());
         System.out.println(RESULT_MESSAGE);
         System.out.println(DELIMITER);
         for (RankDto rankDto : ranksDto.getRankDtos()) {
