823e66271679e6389bbea90f139b5dc501df0da4
SeungCheol <ssc6839@gmail.com>
2022-02-24 15:51:29 +0900

    refactor(lotto): 입력받은 데이터 형식 관련 로직 InputView 클래스로 이동

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,23 +1,31 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해주세요";
-    private static final String INPUT_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+
+    private static final String SEPARATOR = ",";
+    private static final int LIMIT = -1;
 
     private static final Scanner input = new Scanner(System.in);
 
-    public static String inputPrice() {
-        System.out.println(INPUT_PRICE_MESSAGE);
+    public static String inputPurchaseAmount() {
+        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
         return input.nextLine();
     }
 
-    public static String inputWinningNumbers() {
+    public static List<String> inputWinningNumbers() {
         System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
-        return input.nextLine();
+        return Arrays.stream(input.nextLine().split(SEPARATOR, LIMIT))
+            .map(String::trim)
+            .collect(Collectors.toList());
     }
 
     public static String inputBonusNumber() {
