f71a0023fec7005503168bd9261bda6dc1b41965
sinb57 <sinb57@naver.com>
2022-02-24 10:53:09 +0900

    test: 랜덤 로또 생성 기능에 대해, 범위 및 크기 확인 테스트 보충

--- src/test/java/lotto/domain/ticket/generator/RandomTicketGeneratorTest.java
+++ src/test/java/lotto/domain/ticket/generator/RandomTicketGeneratorTest.java
@@ -7,16 +7,28 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import lotto.domain.ticket.generator.RandomTicketGenerator;
-
 class RandomTicketGeneratorTest {
 
-	@DisplayName("렌덤 로또 번호 생성 테스트")
-	@Test
-	void generate() {
-		final List<Integer> numbers = (new RandomTicketGenerator()).generate();
+	private static final int TICKET_DEFAULT_SIZE = 6;
+	private static final int TICKET_NUMBER_RANGE_INCLUSIVE_START = 1;
+	private static final int TICKET_NUMBER_RANGE_EXCLUSIVE_END = 45;
 
-		assertThat(numbers.size()).isEqualTo(6);
+	private final TicketGenerator ticketGenerator = new RandomTicketGenerator();
+
+	@DisplayName("랜덤 생성된 숫자 요소는 6개여야 합니다.")
+	@Test
+	void sizeTest() {
+		final List<Integer> numbers = ticketGenerator.generate();
+		assertThat(numbers.size()).isEqualTo(TICKET_DEFAULT_SIZE);
+	}
+
+	@DisplayName("랜덤 생성된 숫자 요소는 1부터 45까지의 범위에 속해야 합니다.")
+	@Test
+	void rangeTest() {
+		final List<Integer> numbers = ticketGenerator.generate();
+		numbers.forEach(number -> {
+			assertThat(number).isBetween(TICKET_NUMBER_RANGE_INCLUSIVE_START, TICKET_NUMBER_RANGE_EXCLUSIVE_END);
+		});
 	}
 
 }
