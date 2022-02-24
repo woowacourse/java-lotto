20bb2efcc47437232ae940b4aaa4d2f0c3e3bf41
sinb57 <sinb57@naver.com>
2022-02-24 14:03:11 +0900

    refactor: 구매 금액 분할 단위(1000) 상수를 enum 클래스로 분리

--- src/main/java/lotto/domain/credit/Credit.java
+++ src/main/java/lotto/domain/credit/Credit.java
@@ -5,8 +5,6 @@ import lotto.exception.credit.CreditMoneyExceptionStatus;
 
 public class Credit {
 
-    private static final int CREDIT_BASE_UNIT = 1000;
-
     private final int money;
 
     public Credit(final int money) {
@@ -15,13 +13,13 @@ public class Credit {
     }
 
     private void validateMoneyIsDivisible(final int money) {
-        if (money % CREDIT_BASE_UNIT != 0) {
+        if (CreditUnit.isNotDivisible(money)) {
             throw new LottoException(CreditMoneyExceptionStatus.MONEY_IS_NOT_DIVISIBLE);
         }
     }
 
     public int getQuotient() {
-        return money / CREDIT_BASE_UNIT;
+        return CreditUnit.divide(money);
     }
 
     public int getMoney() {
---
+++ src/main/java/lotto/domain/credit/CreditUnit.java
@@ -0,0 +1,21 @@
+package lotto.domain.credit;
+
+public enum CreditUnit {
+
+    DIVIDING_UNIT(1000);
+
+    private final int unit;
+
+    CreditUnit(final int unit) {
+        this.unit = unit;
+    }
+
+    public static boolean isNotDivisible(final int creditMoney) {
+        return (creditMoney % DIVIDING_UNIT.unit) != 0;
+    }
+
+    public static int divide(final int creditMoney) {
+        return creditMoney / DIVIDING_UNIT.unit;
+    }
+
+}
