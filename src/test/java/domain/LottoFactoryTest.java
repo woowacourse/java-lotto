f638e97cb30e464fde87634b507f99c6b89a3ee7
DWL21 <nggus5@naver.com>
2022-02-24 14:49:47 +0900

    refactor: money 불필요한 변수 삭제, final 키워드 추가

--- src/test/java/domain/LottoFactoryTest.java
+++ src/test/java/domain/LottoFactoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -19,6 +20,12 @@ class LottoFactoryTest {
         lottoFactory = new LottoFactory(money);
     }
 
-        assertThat(lottoFactory.calculateCount(money)).isEqualTo(counts);
+    @DisplayName("구입 금액만큼 발급 받은 로또의 갯수를 확인한다.")
+    @Test
+    void issueLotto_count_correct() {
+        lottoFactory.issueLotto();
+        final List<Lotto> lottoTickets = lottoFactory.getLotto();
+
+        assertThat(lottoTickets.size()).isEqualTo(100000);
     }
 }
