e28730506a2de66cf6df42cd273b9c450dcb813e
BeomsooKim <kbsat@naver.com>
2022-02-24 10:30:21 +0900

    refactor(Money): 필드 이름 변경 및 equals & hashCode 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,14 +1,16 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class Money {
 
     private static final String ERROR_NEGATIVE_INPUT_MESSAGE = "금액은 음수를 입력하면 안됩니다.";
 
-    private int money;
+    private final int amount;
 
-    public Money(int money) {
-        validateNegative(money);
-        this.money = money;
+    public Money(int amount) {
+        validateNegative(amount);
+        this.amount = amount;
     }
 
     private void validateNegative(int number) {
@@ -17,11 +19,28 @@ public class Money {
         }
     }
 
-    public int canBuyNumber(Money amount) {
-        return money / amount.money;
+    public int canBuyNumber(Money itemMoney) {
+        return this.amount / itemMoney.amount;
     }
 
     public int get() {
-        return money;
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
