c40a090b951798947097a8f55c6646b9e1d899e7
cjlee38 <cjlee38@github.com>
2022-02-24 11:11:05 +0900

    feat(ErrorView): 에러를 출력하는 view 생성

--- src/main/java/lotto/client/ConsoleErrorClient.java
+++ src/main/java/lotto/client/ConsoleErrorClient.java
@@ -2,10 +2,8 @@ package lotto.client;
 
 public class ConsoleErrorClient implements ErrorClient {
 
-	private static final String PREFIX = "[ERROR] ";
-
 	@Override
 	public void error(String message) {
-		System.out.print(PREFIX + message);
+		System.out.print(message);
 	}
 }
---
+++ src/main/java/lotto/view/ErrorView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+import lotto.client.ErrorClient;
+
+public class ErrorView {
+
+    private static final String PREFIX = "[ERROR] ";
+
+    private final ErrorClient errorClient;
+
+    public ErrorView(ErrorClient errorClient) {
+        this.errorClient = errorClient;
+    }
+
+    public void error(String message) {
+        errorClient.error(PREFIX + message);
+    }
+}
