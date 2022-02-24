f3cd6cfb3a892712b02fbaac228b30d1a2c0fb8d
sinb57 <sinb57@naver.com>
2022-02-24 15:50:12 +0900

    test: Ball 로또 번호 테스트 보충

--- src/test/java/lotto/domain/ticket/BallTest.java
+++ src/test/java/lotto/domain/ticket/BallTest.java
@@ -2,11 +2,10 @@ package lotto.domain.ticket;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import lotto.exception.LottoException;
@@ -14,8 +13,8 @@ import lotto.exception.ball.BallNumberExceptionStatus;
 
 class BallTest {
 
-    @DisplayName("볼은 1~45 범위의 숫자여야 한다.")
-    @ParameterizedTest
+    @DisplayName("로또 번호는 1~45 범위의 숫자여야 한다.")
+    @ParameterizedTest(name = "[{index}] 로또 번호 : {0}")
     @ValueSource(ints = {-2, -1, 0, 46, 47, 48})
     void rangeOutExceptionTest(final int number) {
         assertThatThrownBy(() -> new Ball(number))
@@ -24,17 +23,29 @@ class BallTest {
     }
 
     @DisplayName("생성자 기능 테스트")
-    @ParameterizedTest
-    @ValueSource(ints = {43, 44, 45})
+    @ParameterizedTest(name = "[{index}] 로또 번호 : {0}")
+    @ValueSource(ints = {1, 2, 3, 43, 44, 45})
     void initTest(final int number) {
-        assertDoesNotThrow(() -> new Ball(number));
+        final Ball ball = new Ball(number);
+        assertThat(ball.getBallNumber()).isEqualTo(number);
     }
 
-    @DisplayName("숫자 비교 기능 테스트")
-    @Test
-    void equalsNumberTest() {
-        final Ball ball = new Ball(40);
-        final Ball ball2 = new Ball(40);
-        assertThat(ball.equals(ball2)).isTrue();
+    @DisplayName("숫자 비교 기능 성공 테스트")
+    @ParameterizedTest(name = "[{index}] 번호1 : {0}, 번호2 : {1}")
+    @CsvSource(value = {"1,1", "2,2", "33,33", "45,45"}, delimiter = ',')
+    void equalsSuccessTest(final int number1, final int number2) {
+        final Ball ball1 = new Ball(number1);
+        final Ball ball2 = new Ball(number2);
+        assertThat(ball1.equals(ball2)).isTrue();
     }
+
+    @DisplayName("숫자 비교 기능 실패 테스트")
+    @ParameterizedTest(name = "[{index}] 번호1 : {0}, 번호2 : {1}")
+    @CsvSource(value = {"1,2", "2,1", "33,45", "45,33"}, delimiter = ',')
+    void equalsFalseTest(final int number1, final int number2) {
+        final Ball ball1 = new Ball(number1);
+        final Ball ball2 = new Ball(number2);
+        assertThat(ball1.equals(ball2)).isFalse();
+    }
+
 }
