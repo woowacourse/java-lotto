186a9dc6d540a41b1be8633e11414edf514c003b
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-24 14:05:58 +0900

    refactor(controller): remove run and use controller methods and all the views at main

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,26 @@
+import static view.InputView.requestBonusNumber;
+import static view.InputView.requestTotalLottoPrice;
+import static view.InputView.requestWinningNumbers;
+import static view.OutputView.printLottoResults;
+import static view.OutputView.printPurchaseInfo;
+
 import controller.LottoController;
+import domain.LottoGame;
+import domain.LottoReferee;
+import domain.Lottos;
 
 public class Application {
 
+    private static final LottoController controller = new LottoController();
+
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.run();
+        Lottos lottos = controller.initCustomerLottos(requestTotalLottoPrice());
+        printPurchaseInfo(lottos.getLottos());
+
+        LottoReferee referee = controller.initLottoReferee(requestWinningNumbers(), requestBonusNumber());
+
+        LottoGame lottoGame = new LottoGame(lottos, referee);
+        printLottoResults(lottoGame.getResultStatistics());
+        printLottoResults(lottoGame.calculateProfitRatio());
     }
 }
--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -6,7 +6,6 @@ import static validator.NumberValidators.validateNoDuplicates;
 import static validator.NumberValidators.validateTotalLottoPriceUnit;
 import static validator.NumberValidators.validateWinningNumbersSize;
 
-import domain.LottoGame;
 import domain.LottoNumber;
 import domain.LottoReferee;
 import domain.Lottos;
@@ -14,43 +13,26 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import validator.NumberValidators;
-import view.InputView;
-import view.OutputView;
 
 public class LottoController {
 
     public static final String WINNING_NUMBERS_DELIMITER = ", ";
 
-    public void run() {
-        Lottos lottos = initCustomerLottos();
-        LottoReferee referee = initLottoReferee();
-
-        LottoGame lottoGame = new LottoGame(lottos, referee);
-
-        OutputView.printLottoResults(lottoGame.getResultStatistics());
-        OutputView.printLottoResults(lottoGame.calculateProfitRatio());
-    }
-
-    private Lottos initCustomerLottos() {
-        int totalLottoPrice = InputView.requestTotalLottoPrice();
+    public Lottos initCustomerLottos(int totalLottoPrice) {
         validateTotalLottoPriceUnit(totalLottoPrice);
 
-        Lottos lottos = Lottos.of(totalLottoPrice / LOTTO_PRICE);
-
-        OutputView.printPurchaseInfo(lottos.getLottos());
-
-        return lottos;
+        return Lottos.of(totalLottoPrice / LOTTO_PRICE);
     }
 
-    private LottoReferee initLottoReferee() {
-        List<LottoNumber> winningNumbers = registerWinningNumbers();
-        LottoNumber bonusNumber = registerBonusNumber(winningNumbers);
+    public LottoReferee initLottoReferee(String winningNumbersInput, int bonusBallValue) {
+        List<LottoNumber> winningNumbers = registerWinningNumbers(winningNumbersInput);
+        LottoNumber bonusNumber = registerBonusNumber(winningNumbers, bonusBallValue);
 
         return new LottoReferee(winningNumbers, bonusNumber);
     }
 
-    private List<LottoNumber> registerWinningNumbers() {
-        List<LottoNumber> winningNumbers = getWinningNumbers(InputView.requestWinningNumbers());
+    private List<LottoNumber> registerWinningNumbers(String winningNumbersInput) {
+        List<LottoNumber> winningNumbers = getWinningNumbers(winningNumbersInput);
 
         validateWinningNumbersSize(winningNumbers);
         validateNoDuplicates(winningNumbers);
@@ -65,8 +47,8 @@ public class LottoController {
                 .collect(Collectors.toList());
     }
 
-    private LottoNumber registerBonusNumber(List<LottoNumber> winningNumbers) {
-        LottoNumber bonusNumber = LottoNumber.of(InputView.requestBonusNumber());
+    private LottoNumber registerBonusNumber(List<LottoNumber> winningNumbers, int bonusBallValue) {
+        LottoNumber bonusNumber = LottoNumber.of(bonusBallValue);
 
         validateNoDuplicateInList(bonusNumber, winningNumbers);
 
