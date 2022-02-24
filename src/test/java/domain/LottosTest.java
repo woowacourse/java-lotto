81526ab2e3ec0e763880cfd39ed6d2f9e63fbff2
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-25 01:16:46 +0900

    test(Lottos): throws exception on modifying unmodifiableList

--- src/test/java/domain/LottosTest.java
+++ src/test/java/domain/LottosTest.java
@@ -1,8 +1,11 @@
 package domain;
 
+import static common.TestUtils.createNewLotto;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +25,16 @@ public class LottosTest {
         Set<Lotto> noDuplicateLottoSet = new HashSet<>(lottos.getLottos());
         assertThat(noDuplicateLottoSet.size())
                 .isEqualTo(lottos.getLottos().size());
+    }
+
+    @Test
+    void getLottos_throwsExceptionOnModify() {
+        List<Lotto> lottos = Lottos.of(20)
+                .getLottos();
+
+        Lotto newLotto = createNewLotto(1, 2, 3, 4, 5, 6);
+
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> lottos.add(newLotto));
     }
 }
