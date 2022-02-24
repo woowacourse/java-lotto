84cfc7fd46b99c67f9fc4dec8728c72f3a1f69b5
bomi <bonita9931@gmail.com>
2022-02-24 14:30:09 +0900

    refactor: 변수 inline화

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,17 +5,13 @@ import lotto.model.Lottos;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
-import java.util.List;
-
 public class LottoController {
     public static void runGame() {
         Integer money = InputView.insertMoney();
         Lottos lottos = LottoGame.buyLottos(money);
         ResultView.printGeneratedLottos(lottos.getLottos());
 
-        List<Integer> winningNumbers = InputView.inputWinningNumbers();
-        Integer bonusNumber = InputView.inputBonusNumber();
-        LottoGame lottoGame = new LottoGame(winningNumbers, bonusNumber);
+        LottoGame lottoGame = new LottoGame(InputView.inputWinningNumbers(), InputView.inputBonusNumber());
 
         ResultView.printResultStatistics(lottoGame.generateLottoResult(lottos));
         ResultView.printYield(lottoGame.calculateYield(money));
