3d4fa8c717aa79427af3cefebbcd19022d1b54bf
sinb57 <sinb57@naver.com>
2022-02-24 15:44:48 +0900

    test: Credit 구입 금액 테스트 보충

--- src/test/java/lotto/domain/credit/CreditTest.java
+++ src/test/java/lotto/domain/credit/CreditTest.java
@@ -1,9 +1,12 @@
 package lotto.domain.credit;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import lotto.exception.LottoException;
 import lotto.exception.credit.CreditMoneyExceptionStatus;
@@ -11,13 +14,20 @@ import lotto.exception.credit.CreditMoneyExceptionStatus;
 class CreditTest {
 
     @DisplayName("구입 금액은 1000으로 나누어 떨어져야 한다.")
-    @Test
-    void moneyIsDivisibleExceptionTest() {
-        final int money = 1410;
-        assertThatThrownBy(() -> new Credit(money))
+    @ParameterizedTest(name = "[{index}] 구입 금액 : {0}")
+    @ValueSource(ints = {100, 1100, 200001})
+    void moneyIsDivisibleExceptionTest(final int creditMoney) {
+        assertThatThrownBy(() -> new Credit(creditMoney))
                 .isInstanceOf(LottoException.class)
                 .hasMessageContaining(CreditMoneyExceptionStatus.MONEY_IS_NOT_DIVISIBLE.getMessage());
+    }
 
+    @DisplayName("구입 금액 생성자 기능 테스트")
+    @ParameterizedTest(name = "[{index}] 구입 금액 : {0}")
+    @ValueSource(ints = {1000, 12000, 1300000})
+    void initTest(final int creditMoney) {
+        final Credit credit = new Credit(creditMoney);
+        assertThat(credit.getMoney()).isEqualTo(creditMoney);
     }
 
 }
