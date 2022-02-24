02bff93e37ccaf51aee609e43508c9686b28e182
Kim Seunglae <obey1342@gmail.com>
2022-02-24 15:44:47 +0900

    refactor(WinningTicket):  보너스 번호 중복 검사 추가

--- src/main/java/lotto/domain/WinningTicket.java
+++ src/main/java/lotto/domain/WinningTicket.java
@@ -2,12 +2,19 @@ package lotto.domain;
 
 public class WinningTicket {
 
-    private LottoTicket winningTicket;
-    private LottoNumber bonusBall;
+    private final LottoTicket winningTicket;
+    private final LottoNumber bonusBall;
 
     public WinningTicket(LottoTicket lottoTicket, LottoNumber bonusBall) {
+        validate(lottoTicket, bonusBall);
         winningTicket = lottoTicket;
         this.bonusBall = bonusBall;
+    }
+
+    private void validate(LottoTicket lottoTicket, LottoNumber bonusBall) {
+        if (lottoTicket.isMatch(bonusBall)) {
+            throw new IllegalArgumentException("보너스 숫자가 로또 번호에 포함되어 있습니다.");
+        }
     }
 
     public LottoRank compare(LottoTicket lottoTicket) {
