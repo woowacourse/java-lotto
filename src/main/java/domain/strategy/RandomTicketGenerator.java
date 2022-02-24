41d65d6c79e9276896e941b597da212abf811e42
유영우 <fbduddn97@naver.com>
2022-02-25 00:02:41 +0900

    refactor: 숫자 범위 start와 end 동일한 기준으로 수정

--- src/main/java/domain/strategy/RandomTicketGenerator.java
+++ src/main/java/domain/strategy/RandomTicketGenerator.java
@@ -7,11 +7,11 @@ import java.util.stream.IntStream;
 
 public class RandomTicketGenerator implements TicketGenerator {
 	private static final int TOTAL_START_RANGE = 1;
-	private static final int TOTAL_END_RANGE = 46;
+	private static final int TOTAL_END_RANGE = 45;
 	private static final int LOTTO_PARSE_START_RANGE = 0;
 	private static final int LOTTO_PARSE_END_RANGE = 6;
 
-	private static final List<Integer> numbers = IntStream.range(TOTAL_START_RANGE, TOTAL_END_RANGE)
+	private static final List<Integer> numbers = IntStream.range(TOTAL_START_RANGE, TOTAL_END_RANGE + 1)
 		.boxed()
 		.collect(Collectors.toList());
 
