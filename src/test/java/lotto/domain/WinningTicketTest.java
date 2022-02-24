bdab9d34015df3c83fa54ed7267d4f4f133843dc
Kim Seunglae <obey1342@gmail.com>
2022-02-24 17:08:01 +0900

    test(WinningTicketTest): 당첨번호와 보너스볼 중복 테스트 추가

--- src/test/java/lotto/domain/WinningTicketTest.java
+++ src/test/java/lotto/domain/WinningTicketTest.java
@@ -35,11 +35,27 @@ public class WinningTicketTest {
                 new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))
         );
         LottoTicket lottoTicket = new LottoTicket(numbers);
-        LottoNumber bonusball = new LottoNumber(7);
-        WinningTicket winningTicket = new WinningTicket(lottoTicket, bonusball);
+        LottoNumber bonusBall = new LottoNumber(7);
+        WinningTicket winningTicket = new WinningTicket(lottoTicket, bonusBall);
         // when
         LottoRank result = winningTicket.compare(lottoTicket);
         // then
         assertThat(result).isEqualTo(LottoRank.FIRST);
+    }
+
+    @Test
+    @DisplayName("당첨번호와 보너스번호가 중복이면 예외 발생.")
+    void validateDistinctByBonusBall() {
+        //given
+        List<LottoNumber> numbers = new ArrayList<>(
+            List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))
+        );
+        LottoTicket lottoTicket = new LottoTicket(numbers);
+        LottoNumber bonusBall = new LottoNumber(6);
+        //then
+        assertThatThrownBy(() -> new WinningTicket(lottoTicket, bonusBall))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("보너스 숫자가 로또 번호에 포함되어 있습니다.");
     }
 }
