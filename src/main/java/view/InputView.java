7abe1a526372c37a01d2791e541bfbf48afefe6e
Juhyung990122 <harukawa99@naver.com>
2022-02-24 12:54:49 +0900

    refactor: 입력 유효성 검사 static으로 변경 및 적용

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,15 @@
 package view;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import utils.InputValidator;
 
 =======
 >>>>>>> 31d7c6e (feat: WinningLotto 객체 생성)
+=======
+import utils.InputValidator;
+
+>>>>>>> 701f681 (refactor: 입력 유효성 검사 static으로 변경 및 적용)
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -25,7 +30,16 @@ public class InputView {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     private static final InputView inputView = new InputView();
+=======
+    private static String input() {
+        String input = scanner.nextLine();
+        InputValidator.validateNull(input);
+        InputValidator.validateEmpty(input);
+        return scanner.nextLine();
+    }
+>>>>>>> 701f681 (refactor: 입력 유효성 검사 static으로 변경 및 적용)
 
     private InputView() {
     }
@@ -50,6 +64,7 @@ public class InputView {
 >>>>>>> 31d7c6e (feat: WinningLotto 객체 생성)
     public static int inputPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+<<<<<<< HEAD
         return Integer.parseInt(scanner.nextLine());
 >>>>>>> 1f14e05 (feat: Money 객체 생성)
     }
@@ -59,13 +74,16 @@ public class InputView {
         System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
         List<String> numberValues = toStrings(input());
         return toNumbers(numberValues);
+=======
+        return Integer.parseInt(input());
+>>>>>>> 701f681 (refactor: 입력 유효성 검사 static으로 변경 및 적용)
     }
 
     private List<String> toStrings(String stringArray) {
 =======
     public static List<String> inputWinningNumber() {
         System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
-        return toStringList(scanner.nextLine());
+        return toStringList(input());
     }
 
     private static List<String> toStringList(String stringArray) {
@@ -92,7 +110,11 @@ public class InputView {
     public static int inputBonusBall() {
 >>>>>>> 31d7c6e (feat: WinningLotto 객체 생성)
         System.out.println(BONUS_BALL_INPUT_MESSAGE);
+<<<<<<< HEAD
         return Integer.parseInt(scanner.nextLine());
 >>>>>>> 1f14e05 (feat: Money 객체 생성)
+=======
+        return Integer.parseInt(input());
+>>>>>>> 701f681 (refactor: 입력 유효성 검사 static으로 변경 및 적용)
     }
 }
