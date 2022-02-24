1246739bc2e87cb1a624d84047317785af2f838e
shindong96 <2006peter@naver.com>
2022-02-24 14:04:47 +0900

    refactor: 메서드 순서 변경

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -12,67 +12,69 @@ import view.InputView;
 import view.OutputView;
 
 public class LottoController {
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
+	private final InputView inputView = new InputView();
+	private final OutputView outputView = new OutputView();
 
-    private LottoStorage lottoStorage;
-    private LottoWinningNumber lottoWinningNumber;
-    private BonusBall bonusBall;
-    private RateOfReturn rateOfReturn;
+	private LottoStorage lottoStorage;
+	private LottoWinningNumber lottoWinningNumber;
+	private BonusBall bonusBall;
+	private RateOfReturn rateOfReturn;
 
-    public void playGame() {
-        makeLottos();
-        storeWinningNumber();
-        storeBonusBall();
-        compareLottoWithWinningNumber();
-        showResult();
-    }
+	public void playGame() {
+		makeLottos();
+		storeWinningNumber();
+		storeBonusBall();
+		compareLottoWithWinningNumber();
+		showResult();
+	}
 
-    private void showResult() {
-        outputView.printResultMessage();
-        Arrays.stream(Statistics.values())
-                        .forEach(statistics -> outputView.printResult(statistics.getMatchNumber(), statistics.getValue(), statistics.getCount(),
-                                Statistics.BONUS.getValue()));
-        outputView.printRateOfReturn(rateOfReturn.getRateOfReturn());
-    }
+	private void makeLottos() {
+		try {
+			LottoCount lottoCount = new LottoCount(inputView.inputMoney());
+			storeMoneyInRateOfReturn(lottoCount);
+			lottoStorage = new LottoStorage(lottoCount);
+			outputView.printLottos(lottoStorage.getLottoStorageDTO());
+		} catch (IllegalArgumentException e) {
+			outputView.printErrorMessage(e.getMessage());
+			makeLottos();
+		}
+	}
 
-    private void makeLottos() {
-        try {
-            LottoCount lottoCount = new LottoCount(inputView.inputMoney());
-            storeMoneyInRateOfReturn(lottoCount);
-            lottoStorage = new LottoStorage(lottoCount);
-            outputView.printLottos(lottoStorage.getLottoStorageDTO());
-        } catch (IllegalArgumentException e) {
-            outputView.printErrorMessage(e.getMessage());
-            makeLottos();
-        }
-    }
+	private void storeMoneyInRateOfReturn(LottoCount lottoCount) {
+		rateOfReturn = new RateOfReturn(lottoCount);
+	}
 
-    private void storeMoneyInRateOfReturn(LottoCount lottoCount) {
-        rateOfReturn = new RateOfReturn(lottoCount);
-    }
+	private void storeWinningNumber() {
+		try {
+			lottoWinningNumber = new LottoWinningNumber(inputView.inputWinningNumbers());
+		} catch (IllegalArgumentException e) {
+			outputView.printErrorMessage(e.getMessage());
+			storeWinningNumber();
+		}
+	}
 
-    private void storeWinningNumber() {
-        try {
-            lottoWinningNumber = new LottoWinningNumber(inputView.inputWinningNumbers());
-        } catch (IllegalArgumentException e) {
-            outputView.printErrorMessage(e.getMessage());
-            storeWinningNumber();
-        }
-    }
+	private void storeBonusBall() {
+		try {
+			String input = inputView.inputBonusBall();
+			bonusBall = new BonusBall(input);
+			lottoWinningNumber.validateReduplicationWithBonusBall(input);
+		} catch (IllegalArgumentException e) {
+			outputView.printErrorMessage(e.getMessage());
+			storeBonusBall();
+		}
+	}
 
-    private void storeBonusBall() {
-        try {
-            String input = inputView.inputBonusBall();
-            bonusBall = new BonusBall(input);
-            lottoWinningNumber.validateReduplicationWithBonusBall(input);
-        } catch (IllegalArgumentException e) {
-            outputView.printErrorMessage(e.getMessage());
-            storeBonusBall();
-        }
-    }
+	private void compareLottoWithWinningNumber() {
+		lottoStorage.compare(bonusBall.getBonusBallDTO(), lottoWinningNumber.getWinningNumbersDTO());
+	}
 
-    private void compareLottoWithWinningNumber() {
-        lottoStorage.compare(bonusBall.getBonusBallDTO(), lottoWinningNumber.getWinningNumbersDTO());
-    }
+	private void showResult() {
+		outputView.printResultMessage();
+		Arrays.stream(Statistics.values())
+			.forEach(statistics -> outputView.printResult(statistics.getMatchNumber(), statistics.getValue(),
+				statistics.getCount(),
+				Statistics.BONUS.getValue()));
+		outputView.printRateOfReturn(rateOfReturn.getRateOfReturn());
+	}
+
 }
