1a5434820cad8ec7b6de245ddd36d4b39d39b3f6
Chris <bjuuuu98@gmail.com>
2022-02-24 13:16:13 +0900

    feat: 당첨 번호들과 보너스 번호 간에 중복이 있으면 예외를 발생시키는 기능 구현

--- src/main/java/lotto/domain/TargetLottoNumbers.java
+++ src/main/java/lotto/domain/TargetLottoNumbers.java
@@ -3,12 +3,22 @@ package lotto.domain;
 import lotto.domain.vo.LottoNumber;
 
 public class TargetLottoNumbers {
+    private static final String TARGET_BONUS_DUPLICATION_EXCEPTION_MESSAGE =
+            "당첨 번호와 보너스 번호에 중복이 있으면 안됩니다.";
+
     private final LottoNumbers targetLottoNumbers;
     private final LottoNumber bonusLottoNumber;
 
     public TargetLottoNumbers(final LottoNumbers targetLottoNumbers, final LottoNumber bonusLottoNumber) {
+        validateDuplication(targetLottoNumbers, bonusLottoNumber);
         this.targetLottoNumbers = targetLottoNumbers;
         this.bonusLottoNumber = bonusLottoNumber;
+    }
+
+    private void validateDuplication(final LottoNumbers targetLottoNumbers, final LottoNumber bonusLottoNumber) {
+        if (targetLottoNumbers.hasSameNumberWith(bonusLottoNumber)) {
+            throw new IllegalArgumentException(TARGET_BONUS_DUPLICATION_EXCEPTION_MESSAGE);
+        }
     }
 
     public LottoMatchKind getLottoMatchResult(final LottoNumbers lottoNumbers) {
--- src/test/java/lotto/domain/TargetLottoNumbersTest.java
+++ src/test/java/lotto/domain/TargetLottoNumbersTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import lotto.domain.vo.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,8 +12,22 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TargetLottoNumbersTest {
+    @Test
+    @DisplayName("당첨 번호와 보너스 번호에 중복이 있으면 예외를 발생시킨다.")
+    void create_ExceptionByDuplicationOfTargetNumbersAndBonusNumber() {
+        //given
+        final LottoNumbers targetNumbers = new LottoNumbers(Arrays.asList("1", "2", "3", "4", "5", "6"));
+        final LottoNumber bonusNumber = LottoNumber.ONE;
+        final String expectedExceptionMessage = "당첨 번호와 보너스 번호에 중복이 있으면 안됩니다.";
+        //when then
+        assertThatThrownBy(() -> new TargetLottoNumbers(targetNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(expectedExceptionMessage);
+    }
+
     @ParameterizedTest
     @DisplayName("로또 숫자 6개를 받아 당첨 번호와 보너스 번호를 비교해 당첨 종류를 반환한다.")
     @MethodSource("provideLottoNumbersAndMatchKind")
