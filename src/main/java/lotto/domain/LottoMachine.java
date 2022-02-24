d3a2ae4ec6ab8f355414d2e4fbbda016004461eb
hyeonic <dev.hyeonic@gmail.com>
2022-02-24 13:51:04 +0900

    refactor: LottoMachine 불필요 지역변수 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -5,8 +5,6 @@ import lotto.domain.generator.AutoLottoNumberGenerator;
 public class LottoMachine {
 
     public LottoTickets purchase(Money money) {
-        int lottoCount = money.calculate();
-
-        return new LottoTickets(lottoCount, new AutoLottoNumberGenerator());
+        return new LottoTickets(money.calculate(), new AutoLottoNumberGenerator());
     }
 }
