d0e3d31a4665831a75bdb4b02550d85bc342b622
cjlee38 <cjlee38@github.com>
2022-02-24 16:42:51 +0900

    feat(NumberGenerator): 문자열 기반 숫자 생성기 추가

---
+++ src/main/java/lotto/domain/generator/StringInputNumberGenerator.java
@@ -0,0 +1,29 @@
+package lotto.domain.generator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lotto.utils.IntegerUtils;
+
+public class StringInputNumberGenerator implements NumberGenerator {
+
+    private static final String DELIMITER = ", ";
+
+    private List<Integer> numbers;
+
+    public StringInputNumberGenerator(String inputNumbers) {
+        this.numbers = toIntNumbers(inputNumbers);
+    }
+
+    private List<Integer> toIntNumbers(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+            .map(IntegerUtils::parse)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Integer> generate(int size) {
+        return numbers.subList(0, size);
+    }
+}
--- src/test/java/lotto/domain/NumberGeneratorTest.java
+++ src/test/java/lotto/domain/NumberGeneratorTest.java
@@ -1,12 +1,18 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import lotto.domain.generator.NumberGenerator;
+import lotto.domain.generator.RandomNumberGenerator;
+import lotto.domain.generator.StringInputNumberGenerator;
 
 class NumberGeneratorTest {
 
@@ -18,20 +24,34 @@ class NumberGeneratorTest {
         // when
         List<Integer> numbers = numberGenerator.generate(6);
         // then
-        Assertions.assertThat(numbers.size()).isEqualTo(6);
+        assertThat(numbers.size()).isEqualTo(6);
     }
 
     @Test
     @DisplayName("임의의 범위의 값을 생성한다.")
-    void testRandomNumberRange() {
+    void testRandomNumberGeneration() {
         // given
         NumberGenerator numberGenerator = new RandomNumberGenerator(1, 10);
         // when
         List<Integer> numbers = numberGenerator.generate(6);
         // then
-        Assertions.assertThat(IntStream.rangeClosed(1, 10)
-                .boxed()
-                .collect(Collectors.toList()))
+        assertThat(IntStream.rangeClosed(1, 10)
+            .boxed()
+            .collect(Collectors.toList()))
             .contains(numbers.toArray(Integer[]::new));
+    }
+
+    @Test
+    @DisplayName("문자열을 기반으로 값을 생성한다.")
+    public void testStringInputNumberGeneration() {
+        // given
+        NumberGenerator generator = new StringInputNumberGenerator("1, 2, 3, 4, 5, 6");
+        // when
+        List<Integer> numbers = generator.generate(3);
+        // then
+        Assertions.assertAll(
+            () -> assertThat(numbers.size()).isEqualTo(3),
+            () -> assertThat(numbers).contains(1, 2, 3)
+        );
     }
 }
