75c56f63bdf16cacf4fb73281f9020f2e8a65fd9
injoon2019 <ison@sfu.ca>
2022-02-24 11:53:10 +0900

    feat(IntegerUtils): 인스턴스화 방지

--- src/main/java/lotto/utils/IntegerUtils.java
+++ src/main/java/lotto/utils/IntegerUtils.java
@@ -2,6 +2,10 @@ package lotto.utils;
 
 public class IntegerUtils {
 
+    // 기본 생성자 생성 방지 (인스턴스화 방지)
+    private IntegerUtils() {
+    }
+
     public static int parse(String input) {
         try {
             return Integer.parseInt(input);
