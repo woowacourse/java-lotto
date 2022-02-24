302bd169085843a1f48c2d34b6d60c53f8afefb0
cjlee38 <cjlee38@github.com>
2022-02-24 16:37:10 +0900

    feat(winningTicket): 당첨번호와 보너스볼 번호 중복 검사 로직 추가

--- src/main/java/lotto/domain/WinningTicket.java
+++ src/main/java/lotto/domain/WinningTicket.java
@@ -5,9 +5,16 @@ public class WinningTicket {
     private LottoTicket winningTicket;
     private LottoNumber bonusBall;
 
-    public WinningTicket(LottoTicket lottoTicket, LottoNumber bonusBall) {
-        winningTicket = lottoTicket;
+    public WinningTicket(LottoTicket winningTicket, LottoNumber bonusBall) {
+        validateBonusDistinct(winningTicket, bonusBall);
+        this.winningTicket = winningTicket;
         this.bonusBall = bonusBall;
+    }
+
+    private void validateBonusDistinct(LottoTicket winningTicket, LottoNumber bonusBall) {
+        if (winningTicket.contains(bonusBall)) {
+            throw new IllegalArgumentException("보너스볼은 당첨 번호와 중복될 수 없습니다.");
+        }
     }
 
     public LottoRank compare(LottoTicket lottoTicket) {
--- src/test/java/lotto/domain/WinningTicketTest.java
+++ src/test/java/lotto/domain/WinningTicketTest.java
@@ -4,40 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningTicketTest {
 
+    private LottoTicket baseLottoTicket;
+
+    @BeforeEach
+    public void setUp() {
+        baseLottoTicket = new LottoTicket(new ArrayList<>(
+            List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))
+        ));
+    }
+
     @Test
     @DisplayName("당첨번호를 생성한다.")
     public void createWinningTicket() {
         // given
-        List<LottoNumber> numbers = new ArrayList<>(
-            List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))
-        );
-        LottoTicket lottoTicket = new LottoTicket(numbers);
         LottoNumber bonusball = new LottoNumber(7);
         // then
-        Assertions.assertThatCode(() -> new WinningTicket(lottoTicket, bonusball))
+        Assertions.assertThatCode(() -> new WinningTicket(baseLottoTicket, bonusball))
             .doesNotThrowAnyException();
+    }
 
+    @Test
+    @DisplayName("보너스볼과 로또번호는 중복될 수 없다.")
+    public void throwsExceptionWithDuplicatedLottoNumberAndBonusBall() {
+        // when
+        LottoNumber bonusBall = new LottoNumber(6);
+
+        // then
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new WinningTicket(baseLottoTicket, bonusBall));
     }
 
     @Test
     @DisplayName("당첨번호와 로또번호를 비교할 수 있다.")
     public void compareWinningTicketWithLottoTicket() {
         // given
-        List<LottoNumber> numbers = new ArrayList<>(
-            List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))
-        );
-        LottoTicket lottoTicket = new LottoTicket(numbers);
         LottoNumber bonusball = new LottoNumber(7);
-        WinningTicket winningTicket = new WinningTicket(lottoTicket, bonusball);
+        WinningTicket winningTicket = new WinningTicket(baseLottoTicket, bonusball);
         // when
-        LottoRank result = winningTicket.compare(lottoTicket);
+        LottoTicket purchaseTicket = new LottoTicket(List.of(
+            new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+            new LottoNumber(4), new LottoNumber(5), new LottoNumber(6)
+        ));
+        LottoRank result = winningTicket.compare(purchaseTicket);
         // then
         Assertions.assertThat(result).isEqualTo(LottoRank.FIRST);
     }
