03fcaf61ad0479105e89b54d742076b360fc21ac
Hyunta <mohai2618@gmail.com>
2022-02-24 16:59:35 +0900

    feat(OutputView): 출력 메세지 상수로 처리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,37 +6,45 @@ import lotto.domain.LottoResult;
 import lotto.domain.LottoDto;
 
 public class OutputView {
+
+    private static final String PURCHASE_AMOUNT_MESSAGE = "%d개를 구매했습니다.%n";
+    private static final String WINNING_STATISTIC_MESSAGE = "\n당첨 통계\n---------\n";
+    private static final String WINNING_STATISTIC_FORMAT = "%d개 일치 %s(%d원) - %d개";
+    private static final String BONUS_BALL_MESSAGE = ", 보너스 볼 일치";
+    private static final String EMPTY_STRING = "";
+    private static final String YIELD_MESSAGE_FORMAT = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)%n";
+
     public void printErrorMessage(Exception exception) {
         System.out.println(exception.getMessage());
     }
 
     public void printLotto(LottoDto lottoDto) {
-        System.out.printf("%d개를 구매했습니다.%n", lottoDto.getLottoSize());
+        System.out.printf(PURCHASE_AMOUNT_MESSAGE, lottoDto.getLottoSize());
         for (ChoiceNumber choiceNumber : lottoDto.getLotto()) {
             System.out.println(choiceNumber);
         }
     }
 
     public void printResult(LottoResult result) {
-        System.out.println("당첨 통계");
-        System.out.println("---------");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(WINNING_STATISTIC_MESSAGE);
         for (LottoRank lottoRank : result.getResult().keySet()) {
-            String message = String.format("%d개 일치%s(%d원) - %d개", lottoRank.getCorrectNumber(),
+            String message = String.format(WINNING_STATISTIC_FORMAT, lottoRank.getCorrectNumber(),
                     getBonusMessage(lottoRank), lottoRank.getPrizeAmount(),
                     result.getResult().get(lottoRank));
-            System.out.println(message);
+            stringBuilder.append(message).append("\n");
         }
+        System.out.print(stringBuilder);
     }
 
     private String getBonusMessage(LottoRank lottoRank) {
         if (lottoRank == LottoRank.RANK_SECOND) {
-            return ", 보너스 볼 일치";
+            return BONUS_BALL_MESSAGE;
         }
-        return " ";
+        return EMPTY_STRING;
     }
 
     public void printYield(double yield) {
-        System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)%n", yield);
-
+        System.out.printf(YIELD_MESSAGE_FORMAT, yield);
     }
 }
