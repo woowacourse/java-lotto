23e7b2143bd94734fd9cb6781bfdf00dffb33c8f
BeomsooKim <kbsat@naver.com>
2022-02-24 11:12:02 +0900

    test(Money): canBuyNumber 메서드 테스트 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -5,19 +5,29 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class MoneyTest {
+class MoneyTest {
 
-    @DisplayName("Money 생성자 테스트")
+    @DisplayName("Money 생성자는 음수가 아닌 정수를 입력받아 amount 값을 초기화한다.")
     @Test
-    void money_constructor_test() {
-        Money money = new Money(10000);
+    void money_constructor() {
+        assertThatNoException().isThrownBy(() -> new Money(10000));
     }
 
-    @DisplayName("Money 생성자 음수 입력 예외 테스트")
+    @DisplayName("Money 생성자는 음수를 입력받으면 예외가 발생한다.")
     @Test
-    void money_constructor_error_on_negative_test() {
+    void money_constructorErrorOnNegative() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Money(-1000))
                 .withMessage("금액은 음수를 입력하면 안됩니다.");
+    }
+
+    @DisplayName("canBuyNumber 메서드는 구매하려는 item의 Money를 입력받아 구매 가능한 개수를 반환한다.")
+    @Test
+    void canBuyNumber() {
+        // given
+        Money itemMoney = new Money(1000);
+        Money myMoney = new Money(10000);
+        // then
+        assertThat(myMoney.canBuyNumber(itemMoney)).isEqualTo(10);
     }
 }
