fb8031255e52e64ff2d0bb689362d9c3b01df596
yoon <duddbs774@gmail.com>
2022-02-24 15:36:47 +0900

    refactor: 예외문구 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import java.util.Optional;
 
 public class WinningNumbers {
+    private static final String DUPLICATE_ERROR = "보너스 번호는 당첨 번호와 중복될 수 없습니다";
+
     private LottoNumbers lottoNumbers;
     private LottoNumber bonusNumber;
 
@@ -14,7 +16,7 @@ public class WinningNumbers {
 
     private void validateDuplicateNumber(LottoNumbers lottoNumbers, LottoNumber bonusNumber) {
         if (lottoNumbers.isContain(bonusNumber)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATE_ERROR);
         }
     }
 
