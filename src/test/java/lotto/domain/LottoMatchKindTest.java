922062414247731166abce5cbfdc642fa5a86013
Chris <bjuuuu98@gmail.com>
2022-02-24 14:07:10 +0900

    fix: 오류가 발생하던 테스트 소스 수정

--- src/test/java/lotto/domain/LottoMatchKindTest.java
+++ src/test/java/lotto/domain/LottoMatchKindTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoMatchKindTest {
     @ParameterizedTest
     @DisplayName("매치 개수를 받아 개수에 따른 당첨 종류 객체를 반환한다.")
-    @CsvSource({"3, false, THREE", "4, false, FOUR", "5, false ,FIVE", "6, true, FIVE_BONUS", "6, false ,SIX"})
+    @CsvSource({"3, false, THREE", "4, false, FOUR", "5, false ,FIVE", "5, true, FIVE_BONUS", "6, false ,SIX"})
     void from(final int matchCount, final boolean bonus, final LottoMatchKind expected) {
         //when
         final LottoMatchKind actual = LottoMatchKind.from(matchCount, bonus);
