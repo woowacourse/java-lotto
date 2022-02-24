095df088b3ccec3eacf128c5368e49e2644b1282
sinb57 <sinb57@naver.com>
2022-02-24 15:40:55 +0900

    refactor: OutputView 문자열 매직넘버 제거 및 Enum 클래스 생성

--- src/main/java/lotto/view/output/OutputView.java
+++ src/main/java/lotto/view/output/OutputView.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import lotto.domain.rank.Rank;
 import lotto.dto.AnalysisDto;
 import lotto.dto.TicketDto;
+import lotto.utils.Delimiter;
 
 public class OutputView {
 
@@ -62,25 +63,34 @@ public class OutputView {
 			.map(String::valueOf)
 			.collect(Collectors.toUnmodifiableList());
 
-		return "[" + String.join(",", ticketBalls) + "]";
+		final String joinedBallNumbers = Delimiter.joinWithComma(ticketBalls);
+		return String.format(TICKET_FORMAT.getMessage(), joinedBallNumbers);
 	}
 
 	public void printAnalysis(final AnalysisDto analysisDto) {
 		final Map<Rank, Integer> rankCounts = analysisDto.getRankCounts();
 
-		for (Rank rank : rankCounts.keySet()) {
-			final int count = rankCounts.get(rank);
-			String message = String.format("%d개 일치", rank.getMatchCount());
-			if (rank.getBonusMatched()) {
-				message += ", 보너스 볼 일치";
-			}
-			message += String.format("(%d원) - %d개", rank.getPrizeMoney(), count);
-			printMessage(message);
-		}
+		rankCounts.forEach(this::printAnalysisRank);
 
 		final double rate = analysisDto.getProfitRate();
 		final String message = String.format(PROFIT_RAGE_FORMAT.getMessage(), rate);
 		printMessage(message);
+	}
+
+	private void printAnalysisRank(final Rank rank, final int rankCount) {
+		final int matchCount = rank.getMatchCount();
+		final long prizeMoney = rank.getPrizeMoney();
+
+		final String messageFormat = selectMessageFormatOfAnalysisRank(rank.getBonusMatched());
+		final String message = String.format(messageFormat, matchCount, prizeMoney, rankCount);
+		printMessage(message);
+	}
+
+	private String selectMessageFormatOfAnalysisRank(final boolean isExtra) {
+		if (isExtra) {
+			return ANALYSIS_EXTRA_FORMAT.getMessage();
+		}
+		return ANALYSIS_FORMAT.getMessage();
 	}
 
 }
