6938d3336cf6a205f99cb5fea4fed8794cf08e40
Richard JEON <ztzy1907@gmail.com>
2022-02-25 06:16:05 +0900

    refactor(random-lotto-generator): 자동 로또 생성 전략 구현체 내부에 1~45 숫자 리스트를 갖도록 수정

--- src/main/java/domain/strategy/RandomLottoNumberGenerateStrategy.java
+++ src/main/java/domain/strategy/RandomLottoNumberGenerateStrategy.java
@@ -1,6 +1,5 @@
 package domain.strategy;
 
-import constants.LottoNumbers;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,14 +7,22 @@ import java.util.List;
 public class RandomLottoNumberGenerateStrategy implements LottoNumberGenerateStrategy {
     private static final int RANDOM_LOTTO_NUMBER_START_INDEX = 0;
     private static final int RANDOM_LOTTO_NUMBER_END_INDEX = 6;
+    private static final List<Integer> lottoNumbers;
+
+    static {
+        lottoNumbers = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            lottoNumbers.add(i);
+        }
+    }
 
     @Override
     public List<Integer> generateLottoNumbers() {
-        List<Integer> lottoNumbers = new ArrayList<>(LottoNumbers.LOTTO_NUMBERS);
         Collections.shuffle(lottoNumbers);
-        List<Integer> shuffled = lottoNumbers.subList(RANDOM_LOTTO_NUMBER_START_INDEX, RANDOM_LOTTO_NUMBER_END_INDEX);
-        Collections.sort(shuffled);
+        List<Integer> pickedNumbers = lottoNumbers.subList(RANDOM_LOTTO_NUMBER_START_INDEX,
+                RANDOM_LOTTO_NUMBER_END_INDEX);
+        Collections.sort(pickedNumbers);
 
-        return Collections.unmodifiableList(shuffled);
+        return Collections.unmodifiableList(pickedNumbers);
     }
 }
--- src/test/java/domain/strategy/LottoNumberGeneratorStrategyTest.java
+++ src/test/java/domain/strategy/LottoNumberGeneratorStrategyTest.java
@@ -2,7 +2,7 @@ package domain.strategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import constants.LottoNumbers;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
@@ -13,11 +13,15 @@ public class LottoNumberGeneratorStrategyTest {
     @DisplayName("랜덤 생성한 로또 번호는 1 ~ 45사이여야 한다")
     void randomLottoNumberGenerateTest() {
         LottoNumberGenerateStrategy lottoNumberGenerateStrategy = new RandomLottoNumberGenerateStrategy();
+        List<Integer> lottoNumbers = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            lottoNumbers.add(i);
+        }
 
         IntStream.range(0, 100000)
                 .forEach(i -> {
                     List<Integer> generatedWinningNumbers = lottoNumberGenerateStrategy.generateLottoNumbers();
-                    assertThat(LottoNumbers.LOTTO_NUMBERS.containsAll(generatedWinningNumbers)).isTrue();
+                    assertThat(lottoNumbers.containsAll(generatedWinningNumbers)).isTrue();
                 });
     }
 }
