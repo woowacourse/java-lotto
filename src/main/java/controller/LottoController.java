3c375d1f0bda2dcb3cda78eb42237bf1e00bde6e
이한울 <dkfghg3@naver.com>
2022-02-24 15:14:16 +0900

    refactor: 메서드 분리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -23,23 +23,40 @@ public class LottoController {
 	}
 
 	public void run() {
-		outputView.printRequestMoney();
-		Money money = Money.from(inputView.requestMoney());
+		Money money = insertMoney();
+		LottoTicket lottoTicket = buyLottoTicket(money);
+		WinningNumbers winningNumbers = generateWinningNumbers();
+		WinningResult winningResult = checkWinningResult(lottoTicket, winningNumbers);
+		announceWinningResult(money, winningResult);
+	}
 
-		LottoTicket lottoTicket = new LottoTicket(lottoFactory.generateLottoTicket(money));
-		outputView.printPurchasedLottoTicket(lottoTicket.getLottoTicket());
+	private void announceWinningResult(Money money, WinningResult winningResult) {
+		outputView.printWinningResult(winningResult.getWinningResult());
+		outputView.printRateOfProfit(winningResult.getRateOfProfit(money));
+	}
 
+	private WinningResult checkWinningResult(LottoTicket lottoTicket, WinningNumbers winningNumbers) {
+		return WinningResult.createWinningResult(lottoTicket, winningNumbers);
+	}
+
+	private WinningNumbers generateWinningNumbers() {
 		outputView.printRequestWinningNumbers();
 		Lotto winningLotto = Lotto.from(inputView.requestWinningNumbers());
 
 		outputView.printRequestBonusNumber();
 		Number bonusNumber = Number.from(inputView.requestBonusNumber());
 
-		WinningNumbers winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+		return new WinningNumbers(winningLotto, bonusNumber);
+	}
 
-		WinningResult winningResult = WinningResult.createWinningResult(lottoTicket, winningNumbers);
-		outputView.printWinningResult(winningResult.getWinningResult());
+	private LottoTicket buyLottoTicket(Money money) {
+		LottoTicket lottoTicket = new LottoTicket(lottoFactory.generateLottoTicket(money));
+		outputView.printPurchasedLottoTicket(lottoTicket.getLottoTicket());
+		return lottoTicket;
+	}
 
-		outputView.printRateOfProfit(winningResult.getRateOfProfit(money));
+	private Money insertMoney() {
+		outputView.printRequestMoney();
+		return Money.from(inputView.requestMoney());
 	}
 }
