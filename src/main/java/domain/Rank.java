0872363d305c53f7c57716c02efe4b2d37ea071f
devHudi <devhudi@gmail.com>
2022-02-24 16:41:14 +0900

    refactor(rank): 긴 숫자에 underscore(_) 추가

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -3,11 +3,11 @@ package domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000L),
-    SECOND(5, 30000000L),
-    THIRD(5, 1500000L),
-    FOURTH(4, 50000L),
-    FIFTH(3, 5000L),
+    FIRST(6, 2_000_000_000L),
+    SECOND(5, 30_000_000L),
+    THIRD(5, 1_500_000L),
+    FOURTH(4, 50_000L),
+    FIFTH(3, 5_000L),
     NO_MATCH(0, 0L);
 
     private final int sameNumberCount;
