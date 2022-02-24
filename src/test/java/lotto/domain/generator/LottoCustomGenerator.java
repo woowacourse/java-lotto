4d0b3f68c6e08a347d6cb9a8cc0476c0e8e60d77
Chris <bjuuuu98@gmail.com>
2022-02-24 15:11:57 +0900

    refactor: 매직넘버 상수로 치환

--- src/test/java/lotto/domain/generator/LottoCustomGenerator.java
+++ src/test/java/lotto/domain/generator/LottoCustomGenerator.java
@@ -11,13 +11,15 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoCustomGenerator implements LottoGenerator {
+    private static final int MAX_GENERATING_LOTTO_NUMBERS_COUNT = 39;
     private static final String TEST_LOTTO_NUMBERS_COUNT_EXCEED_EXCEPTION_MESSAGE =
             "테스트 시에는 로또 번호들을 39개까지 생성할 수 있습니다.";
+    private static final int SIZE_OF_EACH_LOTTO_NUMBERS = 6;
     private final List<LottoNumber> lottoNumbers = Arrays.asList(LottoNumber.values());
 
     @Override
     public List<LottoNumbers> generateLottoNumbersGroup(int numberOfGenerating) {
-        if (numberOfGenerating > 39) {
+        if (numberOfGenerating > MAX_GENERATING_LOTTO_NUMBERS_COUNT) {
             throw new TestAbortedException(TEST_LOTTO_NUMBERS_COUNT_EXCEED_EXCEPTION_MESSAGE);
         }
         return IntStream.range(0, numberOfGenerating)
@@ -26,7 +28,7 @@ public class LottoCustomGenerator implements LottoGenerator {
     }
 
     private LottoNumbers generateLottoNumbers(final int firstValue) {
-        final Set<LottoNumber> numbers = IntStream.range(firstValue, firstValue + 6)
+        final Set<LottoNumber> numbers = IntStream.range(firstValue, firstValue + SIZE_OF_EACH_LOTTO_NUMBERS)
                 .mapToObj(lottoNumbers::get)
                 .collect(Collectors.toUnmodifiableSet());
         return new LottoNumbers(numbers);
