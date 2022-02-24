76df546253ad2c9df1abfd105e1dc4231f7efd12
summerlunaa <summerlunaa9@gmail.com>
2022-02-24 11:54:21 +0900

    feat: 당첨 개수 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.Arrays;
 import lotto.domain.Lottos;
 import lotto.domain.Rank;
 import lotto.domain.RankCount;
@@ -8,6 +9,9 @@ public class OutputView {
 
     private static final String TOTAL_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STATISTIC_TITLE = "당첨 통계\n" + "---------";
+    private static final String RANK_MESSAGE_FORMAT = "%s개 일치%s(%s원) - %d개\n";
+    private static final String BONUS_NUMBER_MATCH_MESSAGE = ", 보너스 볼 일치";
+    private static final String BONUS_NUMBER_MISMATCH_MESSAGE = " ";
 
     public static void printErrorMessage(String message) {
         System.out.println(message);
@@ -23,21 +27,18 @@ public class OutputView {
 
     public static void printWinningStatistic(RankCount rankCount) {
         System.out.println(WINNING_STATISTIC_TITLE);
-        Rank[] values = Rank.values();
-        for (int i = 0; i < 5; i++) {
-            Rank rank = values[i];
-            printRank(rankCount, rank);
-        }
+        Arrays.stream(Rank.values())
+                .filter(rank -> !rank.equals(Rank.RANK_OUT))
+                .forEach(rank -> System.out.printf((RANK_MESSAGE_FORMAT),
+                        rank.getWinningLottoMatchCountToString(), getBonusNumberMessage(rank), rank.getPrizeToString(),
+                        rankCount.getCount(rank)));
     }
 
-    private static void printRank(RankCount rankCount, Rank rank) {
+    private static String getBonusNumberMessage(Rank rank) {
         if (rank.isBonusNumberMatch()) {
-            System.out.println(
-                    rank.isBonusNumberMatch() + "개 일치, 보너스 볼 일치(" + rank.getPrizeToString() + "원) - " + rankCount.getCount(rank) + "개");
-            return;
+            return BONUS_NUMBER_MATCH_MESSAGE;
         }
-        System.out.println(
-                rank.isBonusNumberMatch() + "개 일치 (" + rank.getPrizeToString() + "원) - " + rankCount.getCount(rank) + "개");
+        return BONUS_NUMBER_MISMATCH_MESSAGE;
     }
 
     public static void printNewLine() {
