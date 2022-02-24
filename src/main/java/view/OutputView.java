bde0092a5695a262f4110d7ae0a0188877e9f3cc
hyunrrr <whgusrms96@gmail.com>
2022-02-24 11:33:30 +0900

    refactor(OutputView): change for loop to steam

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,10 +3,11 @@ package view;
 import domain.Lotto;
 import domain.Lottos;
 import domain.Rewards;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
-    private static final String MESSAGE_LOTTOS_NUMBER = "%d개를 구매했습니다.";
+    private static final String MESSAGE_LOTTOS_NUMBER = "%d개를 구매했습니다.\n";
     private static final String MESSAGE_WINNING_STATISTIC = "당첨 통계";
     private static final String SEPERATOR_LINE = "------------";
     private static final String[] MESSAGE_WINNING_RANKING = {"3개 일치 (5000원)- %d개\n",
@@ -15,16 +16,19 @@ public class OutputView {
             "5개 일치, 보너스 볼 일치(30000000)- %d개\n",
             "6개 일치 (2000000000원)- %d개\n"};
     private static final String MESSAGE_YIELD = "총 수익률은 %f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+    private static final String NUMBER_DELIMITER = ", ";
+    private static final String LOTTO_NUMBER_FORMAT = "[%s]\n";
 
     public static void printLottosInformations(Lottos lottos) {
 
         System.out.printf(MESSAGE_LOTTOS_NUMBER, lottos.getLottos().size());
+
         for (Lotto lotto : lottos.getLottos()) {
-            System.out.println();
-            for (Integer number : lotto.getLottoNumbers()) {
-                System.out.printf("%d, ", number);
-            }
-            System.out.println();
+            String str = lotto.getLottoNumbers()
+                    .stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(NUMBER_DELIMITER));
+            System.out.printf(LOTTO_NUMBER_FORMAT, str);
         }
     }
 
@@ -33,7 +37,7 @@ public class OutputView {
         System.out.println(SEPERATOR_LINE);
 
         for (int i = 0; i < 5; i++) {
-            System.out.printf(MESSAGE_WINNING_RANKING[i], Rewards.getCount(Rewards.values()[4-i]));
+            System.out.printf(MESSAGE_WINNING_RANKING[i], Rewards.getCount(Rewards.values()[4 - i]));
         }
     }
 
