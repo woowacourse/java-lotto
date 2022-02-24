34f38e5f7a9bf8115bc8e381de702cfe3ff4ab4f
brorae <jhr3239@gmail.com>
2022-02-24 15:40:54 +0900

    test : 로또 당첨 테스트 케이스 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -19,16 +20,61 @@ public class LottoTest {
     }
 
     @Test
-    void 로또_3등_당첨테스트() {
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,6,8));
-        lotto.calculateRank(Arrays.asList(1,2,3,4,5,6), 7);
+    void 로또_1등_당첨_테스트() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto.calculateRank(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+        assertThat(lotto.getRank()).isEqualTo((Rank.FIRST));
+    }
+
+    @Test
+    void 로또_2등_당첨_테스트() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto.calculateRank(Arrays.asList(1, 2, 3, 4, 5, 7), 6);
+        assertThat(lotto.getRank()).isEqualTo(Rank.SECOND);
+    }
+
+    @Test
+    void 로또_3등_당첨_테스트() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto.calculateRank(Arrays.asList(1, 2, 3, 4, 5, 7), 8);
         assertThat(lotto.getRank()).isEqualTo((Rank.THIRD));
     }
 
     @Test
-    void 로또_2등_당첨테스트() {
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,6,7));
-        lotto.calculateRank(Arrays.asList(1,2,3,4,5,6), 7);
-        assertThat(lotto.getRank()).isEqualTo(Rank.SECOND);
+    void 로또_4등_당첨_테스트() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto.calculateRank(Arrays.asList(1, 2, 3, 4, 7, 8), 9);
+        assertThat(lotto.getRank()).isEqualTo((Rank.FOURTH));
+    }
+
+    @Test
+    void 로또_5등_당첨_테스트() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto.calculateRank(Arrays.asList(1, 2, 3, 7, 8, 9), 10);
+        assertThat(lotto.getRank()).isEqualTo((Rank.FIFTH));
+    }
+
+    @DisplayName("0개 일치")
+    @Test
+    void 로또_낙첨_테스트1() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto.calculateRank(Arrays.asList(7, 8, 9, 10, 11, 12), 13);
+        assertThat(lotto.getRank()).isEqualTo((Rank.LOSER));
+    }
+
+    @DisplayName("1개 일치")
+    @Test
+    void 로또_낙첨_테스트2() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto.calculateRank(Arrays.asList(1, 8, 9, 10, 11, 12), 13);
+        assertThat(lotto.getRank()).isEqualTo((Rank.LOSER));
+    }
+
+    @DisplayName("2개 일치")
+    @Test
+    void 로또_낙첨_테스트3() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto.calculateRank(Arrays.asList(1, 2, 9, 10, 11, 12), 13);
+        assertThat(lotto.getRank()).isEqualTo((Rank.LOSER));
     }
 }
