73f461def62afec0abf147ae5237f145a683a15e
shindong96 <2006peter@naver.com>
2022-02-24 16:04:23 +0900

    feat: 투입 금액이 0원인 경우 예외 처리
    docs: 기능 구현 목록 추가

--- src/main/java/model/lotto/LottoCount.java
+++ src/main/java/model/lotto/LottoCount.java
@@ -3,41 +3,41 @@ package model.lotto;
 import utils.InputValidateUtils;
 
 public class LottoCount {
-    private static final int ZERO = 0;
-    private static final int UNIT = 1000;
-    private static final String LOTTO_COUNT_BLANK_ERROR_MESSAGE = "[Error]: 금액을 입력해주세요.";
-    private static final String LOTTO_COUNT_NUMBER_ERROR_MESSAGE = "[Error]: 금액은 숫자를 입력해주세요.";
-    private static final String LOTTO_COUNT_UNIT_ERROR_MESSAGE = "[Error]: 금액은 천원 단위여야 합니다.";
+	private static final int ZERO = 0;
+	private static final int UNIT = 1000;
+	private static final String LOTTO_COUNT_BLANK_ERROR_MESSAGE = "[Error]: 금액을 입력해주세요.";
+	private static final String LOTTO_COUNT_NUMBER_ERROR_MESSAGE = "[Error]: 금액은 숫자를 입력해주세요.";
+	private static final String LOTTO_COUNT_UNIT_ERROR_MESSAGE = "[Error]: 금액은 천원 단위여야 합니다.";
 
-    private int count;
+	private int count;
 
-    public LottoCount(String money) {
-        InputValidateUtils.inputBlank(money, LOTTO_COUNT_BLANK_ERROR_MESSAGE);
-        InputValidateUtils.inputNumber(money, LOTTO_COUNT_NUMBER_ERROR_MESSAGE);
-        this.count = makeMoneyToNumber(money);
-    }
+	public LottoCount(String money) {
+		InputValidateUtils.inputBlank(money, LOTTO_COUNT_BLANK_ERROR_MESSAGE);
+		InputValidateUtils.inputNumber(money, LOTTO_COUNT_NUMBER_ERROR_MESSAGE);
+		this.count = makeMoneyToNumber(money);
+	}
 
-    private int makeMoneyToNumber(String money) {
-        validateThousandUnitInputMoney(money);
-        return Integer.parseInt(money) / UNIT;
-    }
+	private int makeMoneyToNumber(String money) {
+		validateThousandUnitInputMoney(money);
+		return Integer.parseInt(money) / UNIT;
+	}
 
-    private void validateThousandUnitInputMoney(String money) {
-        if (Integer.parseInt(money) % UNIT != 0) {
-            throw new IllegalArgumentException(LOTTO_COUNT_UNIT_ERROR_MESSAGE);
-        }
-    }
+	private void validateThousandUnitInputMoney(String money) {
+		if (Integer.parseInt(money) % UNIT != 0 || Integer.parseInt(money) == 0) {
+			throw new IllegalArgumentException(LOTTO_COUNT_UNIT_ERROR_MESSAGE);
+		}
+	}
 
-    public boolean isZero() {
-        return count == ZERO;
-    }
+	public boolean isZero() {
+		return count == ZERO;
+	}
 
-    public void makeLotto() {
-        count--;
-    }
+	public void makeLotto() {
+		count--;
+	}
 
-    public int getCount() {
-        return count;
-    }
+	public int getCount() {
+		return count;
+	}
 }
 
--- src/main/java/model/winningnumber/LottoWinningNumber.java
+++ src/main/java/model/winningnumber/LottoWinningNumber.java
@@ -1,77 +1,77 @@
 package model.winningnumber;
 
-import utils.InputValidateUtils;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import utils.InputValidateUtils;
+
 public class LottoWinningNumber {
-    private static final String WINNING_NUMBER_ERROR_MESSAGE = "[Error]: 당첨 번호는 숫자여야 합니다.";
-    private static final String WINNING_NUMBER_BLANK_ERROR_MESSAGE = "[Error]: 당첨 번호를 입력하세요.";
-    private static final String WINNING_NUMBER_RANGE_ERROR_MESSAGE = "[Error]: 당첨 번호는 1~45 숫자여야 합니다.";
-    private static final String WINNING_NUMBER_SIZE_ERROR_MESSAGE = "[Error]: 당첨 번호는 6개의 숫자여야 합니다.";
-    private static final String WINNING_NUMBER_REDUPLICATION_ERROR_MESSAGE = "[Error]: 당첨 번호는 중복이 있으면 안됩니다";
-    private static final String REDUPLICATION_WITH_BONUS_BALL_ERROR_MESSAGE = "[Error]: 당첨 번호와 보너스 볼이 중복됩니다.";
-    private static final int WINNING_NUMBER_SIZE = 6;
+	private static final String WINNING_NUMBER_ERROR_MESSAGE = "[Error]: 당첨 번호는 숫자여야 합니다.";
+	private static final String WINNING_NUMBER_BLANK_ERROR_MESSAGE = "[Error]: 당첨 번호를 입력하세요.";
+	private static final String WINNING_NUMBER_RANGE_ERROR_MESSAGE = "[Error]: 당첨 번호는 1~45 숫자여야 합니다.";
+	private static final String WINNING_NUMBER_SIZE_ERROR_MESSAGE = "[Error]: 당첨 번호는 6개의 숫자여야 합니다.";
+	private static final String WINNING_NUMBER_REDUPLICATION_ERROR_MESSAGE = "[Error]: 당첨 번호는 중복이 있으면 안됩니다";
+	private static final String REDUPLICATION_WITH_BONUS_BALL_ERROR_MESSAGE = "[Error]: 당첨 번호와 보너스 볼이 중복됩니다.";
+	private static final int WINNING_NUMBER_SIZE = 6;
 
-    private final List<Integer> winningNumbers;
+	private final List<Integer> winningNumbers;
 
-    public LottoWinningNumber(String numbers) {
-        InputValidateUtils.inputBlank(numbers, WINNING_NUMBER_BLANK_ERROR_MESSAGE);
-        InputValidateUtils.inputNumber(makeNumbersToString(numbers), WINNING_NUMBER_ERROR_MESSAGE);
-        validateNumberOutOfRange(numbers);
+	public LottoWinningNumber(String numbers) {
+		InputValidateUtils.inputBlank(numbers, WINNING_NUMBER_BLANK_ERROR_MESSAGE);
+		InputValidateUtils.inputNumber(makeNumbersToString(numbers), WINNING_NUMBER_ERROR_MESSAGE);
+		validateNumberOutOfRange(numbers);
 
-        validateNumberSize(numbers);
-        validateNumberReduplication(numbers);
-        this.winningNumbers = makeWinningNumbers(split(numbers));
-    }
+		validateNumberSize(numbers);
+		validateNumberReduplication(numbers);
+		this.winningNumbers = makeWinningNumbers(split(numbers));
+	}
 
-    private List<String> split(String numbers) {
-        return Arrays.asList(numbers.replace(" ", "").split(","));
-    }
+	private List<String> split(String numbers) {
+		return Arrays.asList(numbers.replace(" ", "").split(","));
+	}
 
-    private String makeNumbersToString(String numbers) {
-        return String.join("", split(numbers));
-    }
+	private String makeNumbersToString(String numbers) {
+		return String.join("", split(numbers));
+	}
 
-    private void validateNumberOutOfRange(String numbers) {
-        split(numbers)
-                .forEach(number -> InputValidateUtils.inputOutOfRange(number, WINNING_NUMBER_RANGE_ERROR_MESSAGE));
-    }
+	private void validateNumberOutOfRange(String numbers) {
+		split(numbers)
+			.forEach(number -> InputValidateUtils.inputOutOfRange(number, WINNING_NUMBER_RANGE_ERROR_MESSAGE));
+	}
 
-    private void validateNumberSize(String numbers) {
-        if (split(numbers).size() != WINNING_NUMBER_SIZE) {
-            throw new IllegalArgumentException(WINNING_NUMBER_SIZE_ERROR_MESSAGE);
-        }
-    }
+	private void validateNumberSize(String numbers) {
+		if (split(numbers).size() != WINNING_NUMBER_SIZE) {
+			throw new IllegalArgumentException(WINNING_NUMBER_SIZE_ERROR_MESSAGE);
+		}
+	}
 
-    private void validateNumberReduplication(String numbers) {
-        long count = split(numbers).stream().distinct().count();
+	private void validateNumberReduplication(String numbers) {
+		long count = split(numbers).stream().distinct().count();
 
-        if (count != split(numbers).size()) {
-            throw new IllegalArgumentException(WINNING_NUMBER_REDUPLICATION_ERROR_MESSAGE);
-        }
-    }
+		if (count != split(numbers).size()) {
+			throw new IllegalArgumentException(WINNING_NUMBER_REDUPLICATION_ERROR_MESSAGE);
+		}
+	}
 
-    private List<Integer> makeWinningNumbers(List<String> numbers) {
-        return numbers.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
+	private List<Integer> makeWinningNumbers(List<String> numbers) {
+		return numbers.stream()
+			.map(Integer::parseInt)
+			.collect(Collectors.toList());
+	}
 
-    public void validateReduplicationWithBonusBall(String number) {
-        if (winningNumbers.contains(Integer.parseInt(number))) {
-            throw new IllegalArgumentException(REDUPLICATION_WITH_BONUS_BALL_ERROR_MESSAGE);
-        }
-    }
+	public void validateReduplicationWithBonusBall(String number) {
+		if (winningNumbers.contains(Integer.parseInt(number))) {
+			throw new IllegalArgumentException(REDUPLICATION_WITH_BONUS_BALL_ERROR_MESSAGE);
+		}
+	}
 
-    public LottoWinningNumberDTO getWinningNumbersDTO() {
-        return new LottoWinningNumberDTO(this.winningNumbers);
-    }
+	public LottoWinningNumberDTO getWinningNumbersDTO() {
+		return new LottoWinningNumberDTO(this.winningNumbers);
+	}
 
-    public List<Integer> getWinningNumbers() {
-        return winningNumbers;
-    }
+	public List<Integer> getWinningNumbers() {
+		return winningNumbers;
+	}
 
 }
