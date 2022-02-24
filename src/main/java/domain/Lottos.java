381eec246856656b2f1510e5f8d392e025302a92
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-25 01:06:07 +0900

    refactor(Lottos): cleanse factory method of with stream
    - move unmodifiableList to constructor

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -1,29 +1,30 @@
 package domain;
 
-import java.util.ArrayList;
+import static constant.LottoConstants.LOTTO_PRICE;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lottos {
 
     private final List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
-        this.lottos = lottos;
+        this.lottos = Collections.unmodifiableList(lottos);
     }
 
     public static Lottos of(int lottoCount) {
-        List<Lotto> lottos = new ArrayList<>();
-
-        for (int i = 0; i < lottoCount; i++) {
-            lottos.add(new Lotto());
-        }
+        List<Lotto> lottos = Stream.generate(Lotto::new)
+                .limit(lottoCount)
+                .collect(Collectors.toList());
 
         return new Lottos(lottos);
     }
 
     public List<Lotto> getLottos() {
-        return Collections.unmodifiableList(lottos);
+        return lottos;
     }
 
     @Override
