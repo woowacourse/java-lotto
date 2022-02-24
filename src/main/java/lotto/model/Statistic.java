c0315c814214a38b5059a3cac0b8018258f807b2
wilgur513 <wilgur513@naver.com>
2022-02-24 21:42:43 +0900

    refactor: 인스턴스 변수 이름 변경, Map 생성을 위해 Stream API 사용

--- src/main/java/lotto/model/Statistic.java
+++ src/main/java/lotto/model/Statistic.java
@@ -1,28 +1,29 @@
 package lotto.model;
 
+import static java.util.stream.Collectors.toMap;
+
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Statistic {
 
     private final Money inputMoney;
-    private final Map<Rank, Integer> resultMap;
+    private final Map<Rank, Integer> rankMap;
 
     public Statistic(Money inputMoney) {
         this.inputMoney = inputMoney;
-        this.resultMap = new HashMap<>(
-            Map.of(Rank.FIRST, 0, Rank.SECOND, 0,
-                Rank.THIRD, 0, Rank.FOURTH, 0,
-                Rank.FIFTH, 0, Rank.NOTHING, 0));
+        this.rankMap = Stream.of(Rank.values())
+            .collect(toMap(Function.identity(), r -> 0));
     }
 
     public int getCountByRank(Rank rank) {
-        return resultMap.get(rank);
+        return rankMap.get(rank);
     }
 
     public void addRank(Rank rank) {
-        resultMap.put(rank, getCountByRank(rank) + 1);
+        rankMap.put(rank, getCountByRank(rank) + 1);
     }
 
     public BigDecimal getProfitRate() {
@@ -30,7 +31,7 @@ public class Statistic {
     }
 
     private Money totalPrize() {
-        return resultMap.keySet().stream()
+        return rankMap.keySet().stream()
             .map(this::eachTotalPrize)
             .reduce(Money.ZERO, Money::plus);
     }
