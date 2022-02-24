c60dc62c6af85bf824a2c620be308de992686897
sinb57 <sinb57@naver.com>
2022-02-24 15:13:00 +0900

    refactor: 로또번호 중복 허용 개수에 대한 상수, enum 클래스 생성

--- src/main/java/lotto/domain/ticket/validator/TicketValidator.java
+++ src/main/java/lotto/domain/ticket/validator/TicketValidator.java
@@ -1,9 +1,9 @@
 package lotto.domain.ticket.validator;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+import lotto.domain.ticket.condition.BallNumberDuplication;
 import lotto.domain.ticket.condition.TicketSize;
 import lotto.exception.LottoException;
 import lotto.exception.ticket.TicketNumbersExceptionStatus;
@@ -15,33 +15,33 @@ public class TicketValidator {
     private TicketValidator() {
     }
 
-    public static void validateTicket(final List<Integer> numbers) {
-        INSTANCE.validateTicketIsNull(numbers);
-        INSTANCE.validateTicketIsOutOfSize(numbers);
-        INSTANCE.validateNumbersDuplicated(numbers);
+    public static void validateTicket(final List<Integer> ballNumbers) {
+        INSTANCE.validateTicketIsNull(ballNumbers);
+        INSTANCE.validateTicketIsOutOfSize(ballNumbers);
+        INSTANCE.validateNumbersDuplicated(ballNumbers);
     }
 
-    private void validateTicketIsNull(final List<Integer> numbers) {
-        if (Objects.isNull(numbers)) {
+    private void validateTicketIsNull(final List<Integer> ballNumbers) {
+        if (Objects.isNull(ballNumbers)) {
             throw new LottoException(TicketNumbersExceptionStatus.NUMBERS_IS_NULL);
         }
     }
 
-    private void validateTicketIsOutOfSize(final List<Integer> numbers) {
-        if (TicketSize.DEFAULT_SIZE.doesNotMatch(numbers.size())) {
+    private void validateTicketIsOutOfSize(final List<Integer> ballNumbers) {
+        if (TicketSize.DEFAULT_SIZE.doesNotMatch(ballNumbers.size())) {
             throw new LottoException(TicketNumbersExceptionStatus.NUMBERS_OUT_OF_SIZE);
         }
     }
 
-    private void validateNumbersDuplicated(final List<Integer> numbers) {
-        if (isNumberDuplicated(numbers)) {
+    private void validateNumbersDuplicated(final List<Integer> ballNumbers) {
+        if (isBallNumberDuplicated(ballNumbers)) {
             throw new LottoException(TicketNumbersExceptionStatus.NUMBERS_DUPLICATED);
         }
     }
 
-    private boolean isNumberDuplicated(final List<Integer> numbers) {
-        return numbers.stream()
-                .anyMatch(number -> Collections.frequency(numbers, number) > 1);
+    private boolean isBallNumberDuplicated(final List<Integer> ballNumbers) {
+        return ballNumbers.stream()
+                .anyMatch(ballNumber -> BallNumberDuplication.isExcessiveDuplicated(ballNumbers, ballNumber));
     }
 
 }
