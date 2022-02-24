59ce29386cbd57b134a6e2fefc664461e138d8e2
wilgur513 <wilgur513@naver.com>
2022-02-24 11:00:27 +0900

    refactor: 정규표현식 정적 메서드 추출

--- src/main/java/view/LottoNumbersParser.java
+++ src/main/java/view/LottoNumbersParser.java
@@ -5,29 +5,21 @@ import static java.util.stream.Collectors.toList;
 import java.util.List;
 
 public class LottoNumbersParser extends Parser<List<Integer>> {
-    private static final String REGEX_DELIMITER = ",";
-    public static final String REGEX_BEGINNING = "^";
-    private static final String REGEX_PREFIX = "\\s*(";
-    public static final String REGEX_END = "$";
-    private static final String REGEX_SUFFIX = ")\\s*";
-    private static final String REGEX_OR = "|";
-    private static final String REGEX_ONE_TO_NINE = "[1-9]";
-    private static final String REGEX_TEN_TO_THIRTY_NINE = "[1-3][0-9]";
-    private static final String REGEX_FORTY_TO_FORTY_FIVE = "4[0-5]";
-    private static final String REGEX_LOTTO_NUMBER_FORMAT =
-            REGEX_BEGINNING + numberRegex() + "(\\s*," + numberRegex() + "){5}" + REGEX_END;
     static final String INVALID_LOTTO_NUMBER_FORMAT_MESSAGE = "당첨 번호는 반드시 6개의 숫자여야 합니다.";
 
     public LottoNumbersParser() {
-        super(REGEX_LOTTO_NUMBER_FORMAT, INVALID_LOTTO_NUMBER_FORMAT_MESSAGE);
+        super(regex(), INVALID_LOTTO_NUMBER_FORMAT_MESSAGE);
     }
 
-    private static String numberRegex() {
-        return new StringBuilder(REGEX_PREFIX)
-                .append(REGEX_ONE_TO_NINE).append(REGEX_OR)
-                .append(REGEX_TEN_TO_THIRTY_NINE).append(REGEX_OR)
-                .append(REGEX_FORTY_TO_FORTY_FIVE).append(REGEX_SUFFIX)
-                .toString();
+    private static String regex() {
+        return new StringBuilder(REGEX_BEGINNING)
+            .append(lottoNumberWithSpacesRegex())
+            .append(REGEX_GROUP_BEGINNING)
+            .append(REGEX_SPACE).append(REGEX_COMMA)
+            .append(lottoNumberWithSpacesRegex())
+            .append(REGEX_GROUP_END).append("{5}")
+            .append(REGEX_END)
+            .toString();
     }
 
     @Override
@@ -39,7 +31,7 @@ public class LottoNumbersParser extends Parser<List<Integer>> {
     }
 
     private List<String> splitNumbers(String numbers) {
-        return List.of(numbers.split(REGEX_DELIMITER));
+        return List.of(numbers.split(REGEX_COMMA));
     }
 
     private List<String> trimNumbers(List<String> numbers) {
--- src/main/java/view/Parser.java
+++ src/main/java/view/Parser.java
@@ -1,6 +1,16 @@
 package view;
 
 public abstract class Parser<T> {
+    protected static final String REGEX_BEGINNING = "^";
+    protected static final String REGEX_END = "$";
+    protected static final String REGEX_GROUP_BEGINNING = "(";
+    protected static final String REGEX_GROUP_END = ")";
+    protected static final String REGEX_COMMA = ",";
+    protected static final String REGEX_SPACE = "\\s*";
+    protected static final String REGEX_OR = "|";
+    protected static final String REGEX_ONE_TO_NINE = "[1-9]";
+    protected static final String REGEX_TEN_TO_THIRTY_NINE = "[1-3][0-9]";
+    protected static final String REGEX_FORTY_TO_FORTY_FIVE = "4[0-5]";
 
     private final String regex;
     private final String errorMessage;
@@ -16,6 +26,21 @@ public abstract class Parser<T> {
         }
 
         return convert(text);
+    }
+
+    protected static String lottoNumberWithSpacesRegex() {
+        return new StringBuilder(REGEX_SPACE)
+            .append(lottoNumberRegex())
+            .append(REGEX_SPACE)
+            .toString();
+    }
+
+    protected static String lottoNumberRegex() {
+        return new StringBuilder(REGEX_GROUP_BEGINNING)
+            .append(REGEX_ONE_TO_NINE).append(REGEX_OR)
+            .append(REGEX_TEN_TO_THIRTY_NINE).append(REGEX_OR)
+            .append(REGEX_FORTY_TO_FORTY_FIVE)
+            .append(REGEX_GROUP_END).toString();
     }
 
     protected abstract T convert(String text);
