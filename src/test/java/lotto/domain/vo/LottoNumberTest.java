35236a3cd2db54460c4a38fc1e1c7f8973a40f58
TaeHyeon Kim <kth990303@naver.com>
2022-02-25 15:39:32 +0900

    test: 45를 초과하는 값으로 LottoNumber 객체 생성하는 테스트 추가

--- src/test/java/lotto/domain/vo/LottoNumberTest.java
+++ src/test/java/lotto/domain/vo/LottoNumberTest.java
@@ -24,7 +24,7 @@ class LottoNumberTest {
 
     @ParameterizedTest
     @DisplayName("1부터 45까지의 자연수가 아닌 값으로 객체를 생성할 경우 예외를 발생시킨다.")
-    @ValueSource(strings = {"0", "-1", "1.2", "a"})
+    @ValueSource(strings = {"0", "-1", "1.2", "a", "46"})
     void create_exceptionByInvalidValue(final String value) {
         // given
         final String expectedExceptionMessage = "로또 번호는 1 ~ 45 사이의 자연수여야합니다.";
