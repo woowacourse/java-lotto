82362d9d099074323006d9af58c1fca2eea44191
DOES <ldk980130@gmail.com>
2022-02-25 13:12:35 +0900

    refactor: fix calculateProfit in WinningStat

--- src/main/java/domain/WinningStat.java
+++ src/main/java/domain/WinningStat.java
@@ -2,40 +2,30 @@ package domain;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class WinningStat {
 
-    public static final int DEFAULT_VALUE = 0;
+	public static final int DEFAULT_VALUE = 0;
 
-    private final Map<LottoRank, Integer> stat;
+	private final Map<LottoRank, Integer> stat;
 
-    public WinningStat(Map<LottoRank, Integer> ranks) {
-        stat = ranks;
-    }
+	public WinningStat(Map<LottoRank, Integer> ranks) {
+		stat = ranks;
+	}
 
-    public Map<LottoRank, Integer> getStat() {
-        return new HashMap<>(stat);
-    }
+	public Map<LottoRank, Integer> getStat() {
+		return new HashMap<>(stat);
+	}
 
-    public double calculateProfit(int ticketPrice) {
-        int num = stat.values().stream()
-                .reduce(DEFAULT_VALUE, Integer::sum);
+	public double calculateProfit(int ticketPrice) {
+		long totalPrize = Arrays.stream(LottoRank.values())
+			.mapToLong(rank -> (long) rank.getPrize() * stat.get(rank))
+			.reduce(DEFAULT_VALUE, Long::sum);
 
-        List<LottoRank> wonLottoRanks = stat.entrySet().stream()
-                .filter(rank -> rank.getValue() != DEFAULT_VALUE)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+		int num = stat.values().stream()
+			.reduce(DEFAULT_VALUE, Integer::sum);
 
-        return (double) sumPrize(wonLottoRanks) / (num * ticketPrice);
-    }
-
-    private long sumPrize(List<LottoRank> wonLottoRanks) {
-        return Arrays.stream(LottoRank.values())
-                .filter(wonLottoRanks::contains)
-                .mapToLong(rank -> (long) rank.getPrize() * stat.get(rank))
-                .reduce(DEFAULT_VALUE, Long::sum);
-    }
+		return (double) totalPrize / (num * ticketPrice);
+	}
 }
