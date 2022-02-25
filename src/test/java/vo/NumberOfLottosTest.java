44fd619373fa00fe645f49397f745f2276a72646
Richard JEON <ztzy1907@gmail.com>
2022-02-26 07:53:11 +0900

    refactor(test): 책임 변화 및 클래스명 변경에 따른 test 코드 수정

--- src/test/java/vo/NumberOfLottosTest.java
+++ src/test/java/vo/NumberOfLottosTest.java
@@ -7,16 +7,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class TrialNumberTest {
+public class NumberOfLottosTest {
     @ParameterizedTest(name = "{0} 로 생성")
     @ValueSource(ints = {1, 5, 7})
     @DisplayName("로또 구매 횟수 생성 테스트")
     void trialNumberCreationTest(int trial) {
         // given
-        TrialNumber trialNumber = new TrialNumber(trial);
+        NumberOfLottos trialNumber = new NumberOfLottos(trial);
 
         // when
-        int extractedTrialNumber = trialNumber.getTrialNumber();
+        int extractedTrialNumber = trialNumber.getNumberOfLottos();
 
         // then
         assertThat(extractedTrialNumber).isEqualTo(trial);
@@ -26,7 +26,7 @@ public class TrialNumberTest {
     @ValueSource(ints = {-1, 0})
     @DisplayName("양수 아닌 값으로 횟수 생성시 IAE 발생")
     void createTrialNumberWithNegativeOrZeroShouldFail(int trialNumber) {
-        assertThatThrownBy(() -> new TrialNumber(trialNumber))
+        assertThatThrownBy(() -> new NumberOfLottos(trialNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageMatching("횟수는 1 보다 작을 수 없습니다 😂");
     }
