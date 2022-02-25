c01daaf0f9e48973fff207e0b7514a8c283c2ad9
DWL21 <nggus5@naver.com>
2022-02-25 15:02:41 +0900

    fix: 지난 주 당첨 번호 입력시 6개가 아닌 경우 다시 입력 받는 기능 수정

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -11,15 +11,24 @@ public class Lotto {
     private static final int INPUT_NUMBER_SPLIT_OPTION = -1;
     private static final int INIT_COUNT = 0;
     private static final int COUNT_INCREASE_UNIT = 1;
+    public static final String ERROR_LOTTO_SIZE_MESSAGE = "입력받은 로또 개수가 6개가 아닙니다.";
+    public static final int LOTTO_SIZE = 6;
     private final List<LottoNumber> lotto;
 
     public Lotto(final String inputLotto) {
         Validator.checkNullOrEmpty(inputLotto);
         List<String> splitNumbers = splitInput(inputLotto);
+        validateLottoSize(splitNumbers);
         Validator.checkDuplication(splitNumbers);
         this.lotto = splitNumbers.stream()
                 .map(number -> new LottoNumber(number))
                 .collect(Collectors.toList());
+    }
+
+    private void validateLottoSize(List<String> splitNumbers) {
+        if (splitNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ERROR_LOTTO_SIZE_MESSAGE);
+        }
     }
 
     public Lotto(final List<LottoNumber> lotto) {
--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -11,27 +11,34 @@ class LottoTest {
 
     @DisplayName("유효한 로또 번호를 발급을 확인한다.")
     @Test
-    void input_lottoNumbers_valid() {
+    void input_lotto_valid() {
         assertDoesNotThrow(() -> new Lotto("1, 2, 3, 4, 5, 6"));
+    }
+
+    @DisplayName("지난 주 당첨 번호 입력시 로또 개수 6개가 아닌 경우 예외를 발생시킨다.")
+    @Test
+    void input_lotto_size() {
+        assertThatThrownBy(() -> new Lotto("1, 2, 3, 4, 5"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("중복된 로또 번호를 발급할시 예외를 발생시킨다.")
     @Test
-    void input_lottoNumbers_duplicated() {
+    void input_lotto_duplicated() {
         assertThatThrownBy(() -> new Lotto("1, 1, 3, 4, 5, 6"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("지난 주 당첨 번호 입력시 숫자가 아닌 경우 예외를 발생시킨다.")
     @Test
-    void input_lottoNumbers_format() {
+    void input_lotto_format() {
         assertThatThrownBy(() -> new Lotto("1, a, 3, 4, 5, 6"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("발급 로또와 당첨 로또를 비교한 뒤 당첨 갯수 반환을 확인한다.")
     @Test
-    void compare_LottoNumbers() {
+    void compare_lotto() {
         final Lotto lotto = new Lotto("1,2,3,4,5,6");
         final Lotto winNumbers = new Lotto("1,2,3,4,5,7");
 
