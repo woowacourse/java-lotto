1a1f0343cf8dfd37101e99a81d6e16d0a60ade5b
Richard JEON <ztzy1907@gmail.com>
2022-02-26 07:44:44 +0900

    refactor(rank): 2등 판정로직을 rank enum이 담당하기 위해 보너스숫자 보유 여부 필드 추가

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -3,33 +3,35 @@ package domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000L),
-    SECOND(5, 30_000_000L),
-    THIRD(5, 1_500_000L),
-    FOURTH(4, 50_000L),
-    FIFTH(3, 5_000L),
-    NO_MATCH(0, 0L);
+    FIRST(6, 2_000_000_000L, false),
+    SECOND(5, 30_000_000L, true),
+    THIRD(5, 1_500_000L, false),
+    FOURTH(4, 50_000L, false),
+    FIFTH(3, 5_000L, false),
+    NO_MATCH(0, 0L, false);
 
     private final int sameNumberCount;
     private final long prize;
+    private final boolean containsBonusNumber;
 
-    Rank(int rank, long prize) {
+    Rank(int rank, long prize, boolean containsBonusNumber) {
         this.sameNumberCount = rank;
         this.prize = prize;
+        this.containsBonusNumber = containsBonusNumber;
     }
 
-    public static Rank of(int sameNumberCount) {
+    public static Rank of(long sameNumberCount, boolean hasBonusNumber) {
         return Arrays.stream(Rank.values())
-                .filter(rank -> rank.isSameNumberCount(sameNumberCount))
+                .filter(rank -> rank.sameNumberCount == sameNumberCount && rank.containsBonusNumber == hasBonusNumber)
                 .findAny()
                 .orElse(NO_MATCH);
     }
 
-    public boolean isSameNumberCount(int count) {
-        return sameNumberCount == count;
-    }
-
     public long getPrize() {
         return prize;
+    }
+
+    public Rank getThis() {
+        return this;
     }
 }
