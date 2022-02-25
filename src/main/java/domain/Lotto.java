fb98ebd63efc6f776cf1285ecdb64b20f25c5753
Gyuchool <balance0591@naver.com>
2022-02-25 17:46:35 +0900

    refactor: 로또 번호에 대한 검증 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,19 +1,40 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
+    private static final String LOTTO_DUPLICATED_MESSAGE = "[ERROR] 로또 번호에 중복된 숫자가 들어가면 안됩니다.";
+    private static final String LOTTO_SIZE_MESSAGE = "[ERROR] 로또의 숫자는 6개여야 합니다.";
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int LOTTO_START = 0;
     private static final int LOTTO_END = 6;
+    private static final int LOTTO_SIZE = 6;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = new ArrayList<>(numbers);
+    }
+
+    private void validateSize(List<Integer> lotto) {
+        if (lotto.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(LOTTO_SIZE_MESSAGE);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException(LOTTO_DUPLICATED_MESSAGE);
+        }
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        return numbers.size() != numbers.stream()
+                .distinct()
+                .count();
     }
 
     public List<Integer> getLotto() {
