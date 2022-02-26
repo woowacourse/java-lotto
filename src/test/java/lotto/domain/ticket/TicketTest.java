6251a316a1d65f77021dd6e14dbf5660e6af8a50
sinb57 <sinb57@naver.com>
2022-02-26 16:02:09 +0900

    test: MethodSource 어노테이션, fullName 작성

--- src/test/java/lotto/domain/ticket/TicketTest.java
+++ src/test/java/lotto/domain/ticket/TicketTest.java
@@ -17,8 +17,6 @@ import lotto.exception.ticket.TicketNumbersExceptionStatus;
 
 class TicketTest {
 
-    private static final String PROVIDER_PATH = "lotto.domain.ticket.provider.TicketTestProvider#";
-
     @DisplayName("로또 번호 묶음은 NULL이 아니어야 합니다.")
     @ParameterizedTest
     @NullSource
@@ -30,7 +28,7 @@ class TicketTest {
 
     @DisplayName("로또 번호 묶음은 6개로 구성되여야 합니다.")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}")
-    @MethodSource(PROVIDER_PATH + "provideForNumbersOutOfSizeExceptionTest")
+    @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForNumbersOutOfSizeExceptionTest")
     void ticketOutOfSizeExceptionTest(final List<Integer> numbers) {
         assertThatThrownBy(() -> new Ticket(numbers))
                 .isInstanceOf(LottoException.class)
@@ -39,7 +37,7 @@ class TicketTest {
 
     @DisplayName("로또 번호 묶음 중 중복 값은 존재할 수 없습니다.")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}")
-    @MethodSource(PROVIDER_PATH + "provideForNumbersDuplicatedExceptionTest")
+    @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForNumbersDuplicatedExceptionTest")
     void ticketNumbersDuplicatedExceptionTest(final List<Integer> numbers) {
         assertThatThrownBy(() -> new Ticket(numbers))
                 .isInstanceOf(LottoException.class)
@@ -48,7 +46,7 @@ class TicketTest {
 
     @DisplayName("특정 번호 포함 여부 확인 테스트")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}, 특정 번호 : {1}")
-    @MethodSource(PROVIDER_PATH + "provideForContainsTest")
+    @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForContainsTest")
     void containsTest(final List<Integer> numbers, final int targetNumber) {
         final Ticket ticket = new Ticket(numbers);
         final Ball targetBall = new Ball(targetNumber);
@@ -58,7 +56,7 @@ class TicketTest {
 
     @DisplayName("당첨 번호와 일치 개수 확인 테스트")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}, 일치 개수 : {2}, 당첨 번호 : {1}")
-    @MethodSource(PROVIDER_PATH + "provideForCountMatchesTest")
+    @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForCountMatchesTest")
     void countMatches(final List<Integer> numbers, final List<Integer> winningNumbers, final int matchCount) {
         final Ticket ticket = new Ticket((numbers));
         final Ticket winningTicket = new Ticket(winningNumbers);
@@ -72,7 +70,7 @@ class TicketTest {
 
     @DisplayName("당첨 등수 확인 테스트")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}, 당첨 번호 : {1}, 보너스 번호 : {2}, 당첨 등수 : {3}")
-    @MethodSource(PROVIDER_PATH + "provideForCalculateRankTest")
+    @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForCalculateRankTest")
     void calculateRankTest(final List<Integer> numbers,
                            final List<Integer> winningNumbers,
                            final int bonusNumber,
--- src/test/java/lotto/domain/ticket/TicketsTest.java
+++ src/test/java/lotto/domain/ticket/TicketsTest.java
@@ -14,13 +14,11 @@ import lotto.dto.TicketDto;
 
 class TicketsTest {
 
-    private static final String PROVIDER_PATH = "lotto.domain.ticket.provider.TicketsTestProvider#";
-
     private final CustomTicketGenerator customTicketGenerator = new CustomTicketGenerator();
 
     @DisplayName("로또 목록 생성, 개수 일치 확인 테스트")
     @ParameterizedTest(name = "[{index}] 로또 개수 : {1}")
-    @MethodSource(PROVIDER_PATH + "provideForGenerateTest")
+    @MethodSource("lotto.domain.ticket.provider.TicketsTestProvider#provideForGenerateTest")
     void generateTicketsSizeCheckTest(final List<TicketDto> generatedTickets, final int ticketCount) {
         customTicketGenerator.initNumbers(generatedTickets);
         final Tickets tickets = new Tickets(ticketCount, customTicketGenerator);
@@ -31,7 +29,7 @@ class TicketsTest {
 
     @DisplayName("당첨 등수 확인 테스트")
     @ParameterizedTest(name = "[{index}] 당첨 등수 : {4}")
-    @MethodSource(PROVIDER_PATH + "provideForCalculateRanksTest")
+    @MethodSource("lotto.domain.ticket.provider.TicketsTestProvider#provideForCalculateRanksTest")
     void calculateRanksTest(final TicketDto winningTicketNumbers,
                             final int bonusNumber,
                             final List<TicketDto> generatedTickets,
