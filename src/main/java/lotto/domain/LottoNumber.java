6fcd294df1af98aaebcf4639a049731dd3172cb8
Donggyu Kim <a29661498@gmail.com>
2022-02-26 12:47:28 +0900

    feat: 로또 숫자 관리를 위한 VO 인 LottoNumber 정의
    로또 숫자 관리를 위한 LottoNumber 라는 VO를 정의하였습니다.

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    public static final int MINIMUM_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        validateNumberBoundary(number);
+        this.number = number;
+    }
+
+    private void validateNumberBoundary(int number) {
+        if (number < MINIMUM_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("1~45의 숫자이어야 합니다.");
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
