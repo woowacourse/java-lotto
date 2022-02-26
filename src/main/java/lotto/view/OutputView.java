f2e1699931f9bca119df2c8f85885cae2175bfdd
Donggyu Kim <a29661498@gmail.com>
2022-02-26 14:51:30 +0900

    refactor: OutputView 리팩터링
    `(기준이 1이기 대문에 결과적으로 손해라는 의미임` 이라는 문구는 요구사항을 위한 메시지이고 사용자에게 보여주기 위한 메시지가 아니므로 삭제하였습니다.
    또한 상금에 대한 부분도 Rank의 getPrizeMoney() 를 활용하도록 하여 상금 변화에 대한 View 변화를 최소화하도록 하였습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -58,11 +58,11 @@ public class OutputView {
     }
 
     public void printYield(Map<Rank, Integer> ranks, double yield) {
-        System.out.println("3개 일치 (5000원)-" + ranks.getOrDefault(FIFTH, 0) + "개");
-        System.out.println("4개 일치 (50000원)-" + ranks.getOrDefault(FORTH, 0) + "개");
-        System.out.println("5개 일치 (1500000원)-" + ranks.getOrDefault(THIRD, 0) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치(30000000원)-" + ranks.getOrDefault(SECOND, 0) + "개");
-        System.out.println("6개 일치 (2000000000원)-" + ranks.getOrDefault(FIRST, 0) + "개");
-        System.out.printf("총 수익률은 %.2f 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)", yield);
+        System.out.println("3개 일치 (" + FIFTH.getPrizeMoney() + "원)-" + ranks.getOrDefault(FIFTH, 0) + "개");
+        System.out.println("4개 일치 (" + FORTH.getPrizeMoney() + "원)-" + ranks.getOrDefault(FORTH, 0) + "개");
+        System.out.println("5개 일치 (" + THIRD.getPrizeMoney() + "원)-" + ranks.getOrDefault(THIRD, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치(" + SECOND.getPrizeMoney() + "원)-" + ranks.getOrDefault(SECOND, 0) + "개");
+        System.out.println("6개 일치 (" + FIRST.getPrizeMoney() + "원)-" + ranks.getOrDefault(FIRST, 0) + "개");
+        System.out.printf("총 수익률은 %.2f 입니다.", yield);
     }
 }
