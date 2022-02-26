1dcb126c9747e5834aea5e2750188190bf749982
meatsby <meatsby@gmail.com>
2022-02-26 16:57:32 +0900

    refactor(WinningLotto): rearranged responsibility of validation

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,12 +1,14 @@
 package domain;
 
-import java.util.List;
-
 public class WinningLotto {
-    private final List<LottoNumber> winningNumbers;
+
+    private static final String NOT_UNIQUE_BONUS_NUMBER_EXCEPTION_MESSAGE = "보너스 번호는 당첨 번호와 달라야 합니다.";
+
+    private final LottoTicket winningNumbers;
     private final LottoNumber bonusNumber;
 
-    public WinningLotto(List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
+    public WinningLotto(LottoTicket winningNumbers, LottoNumber bonusNumber) {
+        validateNoDuplicateInList(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
@@ -19,13 +21,18 @@ public class WinningLotto {
     }
 
     private int getMatchCount(LottoTicket lottoTicket) {
-        return (int) lottoTicket.getNumbers()
-                .stream()
-                .filter(winningNumbers::contains)
+        return (int) lottoTicket.getNumbers().stream()
+                .filter(winningNumbers.getNumbers()::contains)
                 .count();
     }
 
     private boolean containsBonus(LottoTicket lottoTicket) {
         return lottoTicket.getNumbers().contains(bonusNumber);
+    }
+
+    private void validateNoDuplicateInList(LottoTicket lottoTicket, LottoNumber target) {
+        if (lottoTicket.getNumbers().stream().anyMatch(lottoNumber -> lottoNumber == target)) {
+            throw new IllegalArgumentException(NOT_UNIQUE_BONUS_NUMBER_EXCEPTION_MESSAGE);
+        }
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,5 @@
 package view;
 
-import static util.LottoNumberValidator.validateAndParseNumber;
 import static util.UserBalanceValidator.validateUserBalance;
 
 import java.util.Scanner;
@@ -11,20 +10,16 @@ public class InputView {
 
     public static int requestUserMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return validateUserBalance(readLine());
+        return validateUserBalance(scanner.nextLine());
     }
 
     public static String requestWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return readLine();
+        return scanner.nextLine();
     }
 
-    public static int requestBonusNumber() {
+    public static String requestBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
-        return validateAndParseNumber(readLine());
-    }
-
-    private static String readLine() {
         return scanner.nextLine();
     }
 }
