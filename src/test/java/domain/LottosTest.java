445eb9c11547c3038d2c56204f8c061120951128
Wishoon <heonga2@gmail.com>
2022-02-26 13:15:48 +0900

    refactor: 테스트의 가독성을 위해 로또 번호들의 생성 로직을 static 메서드로 변경

--- src/test/java/domain/LottosTest.java
+++ src/test/java/domain/LottosTest.java
@@ -11,6 +11,18 @@ import org.junit.jupiter.api.Test;
 
 public class LottosTest {
 
+    private static List<LottoNumber> createLottoNumbers() {
+        return Stream.of(12, 23, 6, 44, 17, 16)
+            .map(LottoNumber::valueOf)
+            .collect(Collectors.toList());
+    }
+
+    private static List<LottoNumber> createWinnerLottoNumbers() {
+        return Stream.of(12, 23, 6, 44, 17, 16)
+            .map(LottoNumber::valueOf)
+            .collect(Collectors.toList());
+    }
+
     @Test
     @DisplayName("Lottos를 생성하는 경우")
     void createLottos() {
@@ -23,17 +35,10 @@ public class LottosTest {
     @Test
     @DisplayName("보유하고 있는 로또들과 당첨 로또의 매칭 결과를 계산")
     void calculateLottoMatchResult() {
-        List<LottoNumber> winningNumbers = Stream.of(12, 23, 6, 44, 17, 16)
-            .map(LottoNumber::valueOf)
-            .collect(Collectors.toList());
+        LottoNumberGenerator lottoNumberGenerator = LottosTest::createLottoNumbers;
+        WinningLotto winningLotto = new WinningLotto(new Lotto(createWinnerLottoNumbers()), LottoNumber.valueOf(2));
 
-        LottoNumberGenerator lottoNumberGenerator = () -> Stream.of(12, 23, 6, 44, 17, 16)
-            .map(LottoNumber::valueOf)
-            .collect(Collectors.toList());
-
-        Lottos lottos = new Lottos(3, lottoNumberGenerator);
-        WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), LottoNumber.valueOf(2));
-
+        Lottos lottos = new Lottos(1, lottoNumberGenerator);
         List<LottoReward> lottoRewards = lottos.calculateLottoReward(winningLotto);
 
         lottoRewards.forEach(lottoReward -> assertThat(lottoReward).isEqualTo(LottoReward.FIRST));
