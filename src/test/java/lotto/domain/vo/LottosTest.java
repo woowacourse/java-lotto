a6a481155153968a1ebc445462804f63a58510f5
OzRagwort <ozragwort@gmail.com>
2022-02-27 05:03:03 +0900

    refactor(LottosTest): 필수 검증만을 하여 불필요한 private 메서드 제거

--- src/test/java/lotto/domain/vo/LottosTest.java
+++ src/test/java/lotto/domain/vo/LottosTest.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import lotto.domain.LottoPrize;
 import lotto.domain.generator.CustomLottoGenerator;
 import lotto.domain.generator.Generator;
-import lotto.domain.generator.LottoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -33,40 +32,21 @@ public class LottosTest {
     @Test
     void confirmWinnings_test() {
         Generator lottoGenerator = new CustomLottoGenerator();
-        Lottos lottos = new Lottos(new ArrayList<>(){{
+        Lottos lottos = new Lottos(new ArrayList<>() {{
             add(lottoGenerator.generate());
         }});
 
         WinningNumbers winningNumbers = getWinningNumbers();
-
-        Map<LottoPrize, Integer> testMap = initTestMap(lottos, winningNumbers);
         Map<LottoPrize, Integer> lottoMatches = lottos.confirmWinnings(winningNumbers);
 
-        assertThat(testMap).isEqualTo(lottoMatches);
-    }
-
-    private Map<LottoPrize, Integer> initTestMap(Lottos lottos, WinningNumbers winningNumbers) {
-        Map<LottoPrize, Integer> myMap = new HashMap<>();
-        for (LottoPrize prize : LottoPrize.values()) {
-            myMap.put(prize, 0);
-        }
-
-        List<Lotto> myLottos = lottos.getLottos();
-        for (Lotto lotto : myLottos) {
-            LottoPrize prize = lotto.confirmWinning(winningNumbers);
-            myMap.put(prize, myMap.get(prize) + 1);
-        }
-        return myMap;
+        assertThat(lottoMatches.get(LottoPrize.FIRST)).isEqualTo(1);
     }
 
     private WinningNumbers getWinningNumbers() {
-        List<LottoNumber> lottoNumbers;
-        LottoNumber bonusNumber = new LottoNumber(30);
-        lottoNumbers = new ArrayList<>();
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
         for (int i = 1; i <= 6; i++) {
             lottoNumbers.add(new LottoNumber(i));
         }
-        WinningNumbers winningNumbers = new WinningNumbers(new Lotto(lottoNumbers), bonusNumber);
-        return winningNumbers;
+        return new WinningNumbers(new Lotto(lottoNumbers), new LottoNumber(30));
     }
 }
