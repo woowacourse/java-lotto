2728a6738829da70c4a96ba0629aed8936574a2a
Gyuchool <balance0591@naver.com>
2022-02-26 16:44:01 +0900

    refactor: 경계값에 따른 테스트케이스 추가

--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -1,7 +1,6 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,18 +10,28 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class MoneyTest {
 
-    @Test
-    @DisplayName("구입 금액에 따른 생성할 수 있는 로또 개수 테스트")
-    public void generateLottoTest() {
-        int inputMoney = 14000;
-        int unit = 1000;
-        int actualCount = inputMoney / unit;
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 1001, 1500, 1999})
+    @DisplayName("로또 1개 생성되는지 테스트")
+    public void generateLottoOneTest(int inputMoney) {
         Money money = new Money(inputMoney);
         int count = money.generateCount();
-        assertThat(count).isEqualTo(actualCount);
+
+        assertThat(count).isEqualTo(1);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {2000, 2001, 2500, 2999})
+    @DisplayName("로또 2개 생성되는지 테스트")
+    public void generateLottoTwoTest(int inputMoney) {
+        Money money = new Money(inputMoney);
+        int count = money.generateCount();
+
+        assertThat(count).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 999, 1})
     @DisplayName("1000원 미만이 입력되었을 때, 예외 발생 테스트 ")
     public void inputUnder1000Test() {
         int inputMoney = 10;
