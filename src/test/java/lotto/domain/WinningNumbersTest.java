ce10277649d828b04e86716af804f0697641aba7
yeon-06 <rnjstldus2@hs.ac.kr>
2022-02-27 01:31:08 +0900

    test: 테스트 코드 간략화

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -3,71 +3,42 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class WinningNumbersTest {
     private WinningNumbers winningNumbers;
-
-    @BeforeEach
-    void setUp() {
-        LottoNumbers lottoNumbers = new LottoNumbers("1,2,3,4,5,6");
-        LottoNumber bonusNumber = LottoNumber.of(7);
-        winningNumbers = new WinningNumbers(lottoNumbers, bonusNumber);
-    }
 
     @Test
     @DisplayName("중복 문자")
     void test() {
         // given
         LottoNumbers lottoNumbers = new LottoNumbers("1,2,3,4,5,6");
-        LottoNumber bonusNumber = LottoNumber.of(1);
 
         // when
+        LottoNumber bonusNumber = LottoNumber.of(1);
 
         // then
         assertThatThrownBy(() -> new WinningNumbers(lottoNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    @DisplayName("1등 당첨 결과 반환")
-    void firstPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,4,5,6");
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6:FIRST", "1,2,3,4,5,7:SECOND", "1,2,3,4,5,8:THIRD"
+            , "1,2,3,4,8,9:FOURTH", "1,2,3,8,9,10:FIFTH"}, delimiter = ':')
+    @DisplayName("[n]등 당첨 결과 반환")
+    void prize(String input, Ranking expect) {
+        // given
+        LottoNumbers lottoNumbers = new LottoNumbers("1,2,3,4,5,6");
+        LottoNumber bonusNumber = LottoNumber.of(7);
+        winningNumbers = new WinningNumbers(lottoNumbers, bonusNumber);
 
-        assertThat(winningNumbers.calculateRanking(myLotto)).isEqualTo(Ranking.FIRST);
-    }
+        // when
+        LottoNumbers myLotto = new LottoNumbers(input);
 
-    @Test
-    @DisplayName("2등 당첨 결과 반환")
-    void secondPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,4,5,7");
-
-        assertThat(winningNumbers.calculateRanking(myLotto)).isEqualTo(Ranking.SECOND);
-    }
-
-    @Test
-    @DisplayName("3등 당첨 결과 반환")
-    void thirdPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,4,5,8");
-
-        assertThat(winningNumbers.calculateRanking(myLotto)).isEqualTo(Ranking.THIRD);
-    }
-
-    @Test
-    @DisplayName("4등 당첨 결과 반환")
-    void fourthPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,4,8,9");
-
-        assertThat(winningNumbers.calculateRanking(myLotto)).isEqualTo(Ranking.FOURTH);
-    }
-
-    @Test
-    @DisplayName("5등 당첨 결과 반환")
-    void fifthPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,8,9,10");
-
-        assertThat(winningNumbers.calculateRanking(myLotto)).isEqualTo(Ranking.FIFTH);
+        // then
+        assertThat(winningNumbers.calculateRanking(myLotto)).isEqualTo(expect);
     }
 }
