93f3600fb1b54bf2eb3683bb0c3ba9f8b6860722
hamcheeseburger <yoo77hyeon@gmail.com>
2022-02-26 14:10:14 +0900

    fix: 테스트 코드 깨지는 부분 수정

--- src/test/java/lotto/domain/vo/LottoNumberTest.java
+++ src/test/java/lotto/domain/vo/LottoNumberTest.java
@@ -1,6 +1,7 @@
 package lotto.domain.vo;
 
 import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,16 +11,16 @@ public class LottoNumberTest {
 
     @ParameterizedTest(name = "number : {0}")
     @ValueSource(ints = {0, 46})
-    @DisplayName("로또 번호는 1이상 45이하가 아니면 예외가 발생한다.")
+    @DisplayName("번호는 1이상 45이하가 아니면 예외가 발생한다.")
     void throwExceptionWhenOutOfRange(int number) {
         assertThatIllegalArgumentException()
-            .isThrownBy(() -> new Number(number))
-            .withMessageMatching("로또 번호는 1이상 45이하이어야 한다.");
+                .isThrownBy(() -> new Number(number))
+                .withMessageMatching("번호는 1이상 45이하이어야 한다.");
     }
 
     @ParameterizedTest(name = "number : {0}")
     @ValueSource(ints = {1, 45})
-    @DisplayName("로또 번호는 1이상 45이하로 생성된다.")
+    @DisplayName("번호는 1이상 45이하로 생성된다.")
     void createNumber(int number) {
         assertThat(new Number(number)).isNotNull();
     }
