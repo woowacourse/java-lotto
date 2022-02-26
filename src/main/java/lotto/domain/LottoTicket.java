e53ec7d3d093b21295525b69e3c4fee94c7e69b6
Donggyu Kim <a29661498@gmail.com>
2022-02-26 16:04:43 +0900

    refactor: LottoTicket 매직넘버에 상수 적용
    `로또 티켓에서 숫자는 6개여야 한다`를 검증하는 과정에서 사용하는 매직넘버에 대해서 상수 처리 해주었습니다.

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -9,6 +9,7 @@ import lotto.domain.vo.LottoNumber;
 
 public class LottoTicket {
 
+    public static final int LOTTO_TICKET_SIZE = 6;
     private final List<LottoNumber> lottoNumbers;
 
     public LottoTicket(LottoNumberGenerator lottoNumberGenerator) {
@@ -23,7 +24,7 @@ public class LottoTicket {
     }
 
     private void validateLottoNumberSize(List<LottoNumber> lottoNumbers) {
-        if (lottoNumbers.size() != 6) {
+        if (lottoNumbers.size() != LOTTO_TICKET_SIZE) {
             throw new IllegalArgumentException("로또 티켓의 개수가 6개가 아닙니다.");
         }
     }
