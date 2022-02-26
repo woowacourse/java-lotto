89a00fece3a006af226b21706c3e9c78181a494b
meatsby <meatsby@gmail.com>
2022-02-26 20:17:03 +0900

    feat(UserBalance): implement user balance class for validation of initial user balance input
    - add test cases

---
+++ src/main/java/domain/UserBalance.java
@@ -0,0 +1,36 @@
+package domain;
+
+public class UserBalance {
+    private static final String INVALID_NUMBER_INPUT_EXCEPTION_MESSAGE = "구입금액은 숫자여야 합니다.";
+    private static final String INVALID_CURRENCY_EXCEPTION_MESSAGE = "구입금액은 1000원 이상이어야 하며 1000원 미만일 수 없습니다.";
+
+    private final int userBalance;
+
+    public UserBalance(String value) {
+        this.userBalance = validateUserBalance(value);
+    }
+
+    private int validateUserBalance(String value) {
+        int balance = validateAndParseNumber(value);
+        validateBalance(balance);
+        return balance;
+    }
+
+    private int validateAndParseNumber(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(INVALID_NUMBER_INPUT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateBalance(int balance) {
+        if (!(balance > 1000 && balance % 1000 == 0)) {
+            throw new IllegalArgumentException(INVALID_CURRENCY_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public int getUserBalance() {
+        return userBalance;
+    }
+}
