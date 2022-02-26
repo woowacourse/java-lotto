a3afecfa259ac810a010841586cc83179c757fec
yeon-06 <rnjstldus2@hs.ac.kr>
2022-02-27 01:08:27 +0900

    refactor: 하드코딩 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,9 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
     private static final String TYPE_ERROR = "로또 번호는 숫자만 가능합니다.";
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -11,17 +14,17 @@ public class InputView {
     }
 
     public static String inputAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(INPUT_LOTTO_AMOUNT);
         return scanner.nextLine();
     }
 
     public static String inputWinningNumbers() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println(INPUT_WINNING_NUMBERS);
         return scanner.nextLine();
     }
 
     public static int inputBonusBall() {
-        System.out.println("보너스 볼을 입력해 주세요.");
+        System.out.println(INPUT_BONUS_NUMBER);
         String input = scanner.nextLine();
         return convertToInt(input);
     }
