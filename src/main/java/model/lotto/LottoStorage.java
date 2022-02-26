bece63f79bffc549d276548843c4f37c61a0184e
shindong96 <2006peter@naver.com>
2022-02-26 13:43:42 +0900

    refactor: 컨밴션 준수하게 수정

--- src/main/java/model/lotto/LottoStorage.java
+++ src/main/java/model/lotto/LottoStorage.java
@@ -1,35 +1,35 @@
 package model.lotto;
 
-import model.winningnumber.LottoWinningNumberDTO;
-import model.bonusball.BonusBallDTO;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import model.bonusball.BonusBallDTO;
+import model.winningnumber.LottoWinningNumberDTO;
+
 public class LottoStorage {
-    private List<Lotto> lottoStorage;
+	private List<Lotto> lottoStorage;
 
-    public LottoStorage(LottoCount lottoCount) {
-        this.lottoStorage = store(lottoCount);
-    }
+	public LottoStorage(LottoCount lottoCount) {
+		this.lottoStorage = store(lottoCount);
+	}
 
-    private List<Lotto> store(LottoCount lottoCount) {
-        List<Lotto> lottos = new ArrayList<>();
+	private List<Lotto> store(LottoCount lottoCount) {
+		List<Lotto> lottos = new ArrayList<>();
 
-        while (!lottoCount.isZero()) {
-            lottos.add(new Lotto(RandomLottoNumbersGenerator.pickSixNumbers()));
-            lottoCount.makeLotto();
-        }
-        return lottos;
-    }
+		while (!lottoCount.isZero()) {
+			lottos.add(new Lotto(RandomLottoNumbersGenerator.pickSixNumbers()));
+			lottoCount.makeLotto();
+		}
+		return lottos;
+	}
 
-    public List<LottoDTO> getLottoStorageDTO() {
-        List<LottoDTO> lottoDTOS = new ArrayList<>();
-        lottoStorage.forEach(lotto -> lottoDTOS.add(lotto.getLottoDTO()));
-        return lottoDTOS;
-    }
+	public List<LottoDTO> getLottoStorageDTO() {
+		List<LottoDTO> lottoDTOS = new ArrayList<>();
+		lottoStorage.forEach(lotto -> lottoDTOS.add(lotto.getLottoDTO()));
+		return lottoDTOS;
+	}
 
-    public void compare(BonusBallDTO bonusBallDTO, LottoWinningNumberDTO winningNumbersDTO) {
-        lottoStorage.forEach(lotto -> lotto.compare(bonusBallDTO, winningNumbersDTO));
-    }
+	public void compare(BonusBallDTO bonusBallDTO, LottoWinningNumberDTO winningNumbersDTO) {
+		lottoStorage.forEach(lotto -> lotto.compare(bonusBallDTO, winningNumbersDTO));
+	}
 }
