5e9da17f9cd381ca52260876caa4820458c3300b
EastHShin <xldk78@gmail.com>
2022-02-26 18:53:44 +0900

    test(LotteryNumberTest): 로또 번호 생성에 대한 테스트 코드 작성

---
+++ src/test/java/domain/lottery/LotteryNumberTest.java
@@ -0,0 +1,31 @@
+package domain.lottery;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import domain.exception.LotteryExceptionMessages;
+
+public class LotteryNumberTest {
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 45})
+	@DisplayName("로또 번호가 1~45 범위이면 정상적으로 생성")
+	void testValidLotteryNumber(int number) {
+		assertThatNoException().isThrownBy(() ->
+			new LotteryNumber(number)
+		);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {-1, 0, 46})
+	@DisplayName("로또 번호가 1~45 범위가 아니면 오류 발생")
+	void testInvalidLotteryNumber(int number) {
+		assertThatThrownBy(() ->
+			new LotteryNumber(number)
+		).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(LotteryExceptionMessages.INVALID_RANGE_EXCEPTION.getMessage());
+	}
+}
