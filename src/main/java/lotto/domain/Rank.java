147e74986d8e13c540e226f44ef50ec032ccfc38
yh20studio(tiki) <yh20studio@gmail.com>
2022-02-26 18:09:35 +0900

    refactor: Enum Rank의 숫자 자릿수 표현(_) 적용

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public enum Rank {
 
     MISS(0, 0, "3개 미만 일치"),
-    FIFTH(3, 5000, "3개 일치"),
-    FOURTH(4, 50000, "4개 일치"),
-    THIRD(5, 1500000, "5개 일치"),
-    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치"),
-    FIRST(6, 2000000000, "6개 일치");
+    FIFTH(3, 5_000, "3개 일치"),
+    FOURTH(4, 50_000, "4개 일치"),
+    THIRD(5, 1_500_000, "5개 일치"),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, 2_000_000_000, "6개 일치");
 
     private final int matchCount;
     private final int reward;
