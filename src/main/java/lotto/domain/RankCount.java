78e796e9261bb7246c4ee7f1588a0d7db02019e7
jojogreen91 <iawbg13@gmail.com>
2022-02-27 01:27:40 +0900

    refactor: RankCount 생성자 수정

--- src/main/java/lotto/domain/RankCount.java
+++ src/main/java/lotto/domain/RankCount.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.EnumMap;
 import java.util.Map;
 
@@ -10,18 +9,20 @@ public class RankCount {
     private final Map<Rank, Integer> rankCount;
 
     public RankCount(Lottos lottos, WinningLotto winningLotto, BonusNumber bonusNumber) {
-        this.rankCount = Collections.unmodifiableMap(calculateRankCount(lottos, winningLotto, bonusNumber));
+        this.rankCount = new EnumMap<>(calculateRankCount(lottos, winningLotto, bonusNumber));
     }
 
-    private Map<Rank, Integer> calculateRankCount(Lottos lottos, WinningLotto winningLotto, BonusNumber bonusNumber) {
-        Map<Rank, Integer> rankCount = new EnumMap<>(Rank.class);
+    private EnumMap<Rank, Integer> calculateRankCount(Lottos lottos,
+                                                      WinningLotto winningLotto,
+                                                      BonusNumber bonusNumber) {
+        EnumMap<Rank, Integer> rankCount = new EnumMap<>(Rank.class);
         for (Lotto lotto : lottos.getLottos()) {
             increaseCount(rankCount, lotto.getRank(winningLotto, bonusNumber));
         }
         return rankCount;
     }
 
-    private void increaseCount(Map<Rank, Integer> rankCount, Rank rank) {
+    private void increaseCount(EnumMap<Rank, Integer> rankCount, Rank rank) {
         rankCount.putIfAbsent(rank, 0);
         rankCount.computeIfPresent(rank, (key, value) -> value + 1);
     }
