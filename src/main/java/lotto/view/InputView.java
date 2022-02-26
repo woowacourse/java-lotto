d47853c9814c4e9ddb987363930a472ad1fc2620
BeomsooKim <kbsat@naver.com>
2022-02-26 20:20:29 +0900

    style(inputView): 개행으로 에러 메세지와 인풋 메세지 구분

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,12 +9,13 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String ERROR_NO_INPUT_MESSAGE = "입력이 없습니다.";
-    private static final String ERROR_INPUT_EMPTY_MESSAGE = "공백 또는 빈 문자열이 입력하면 안됩니다.";
-    private static final String ERROR_NOT_NUMBER_MESSAGE = "숫자 이외의 문자가 입력하면 안됩니다.";
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String INPUT_WINNING_DELIMITER = ",";
+
+    private static final String ERROR_NO_INPUT_MESSAGE = "입력이 없습니다.";
+    private static final String ERROR_INPUT_EMPTY_MESSAGE = "공백 또는 빈 문자열이 입력하면 안됩니다.";
+    private static final String ERROR_NOT_NUMBER_MESSAGE = "숫자 이외의 문자가 입력하면 안됩니다.";
 
     private static final Scanner scanner = new Scanner(System.in);
 
