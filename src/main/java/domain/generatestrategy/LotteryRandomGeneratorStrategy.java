27e608d34da746d9f03f4c10dada504aaa99a046
skullKim <kyk990328@gmail.com>
2022-02-26 16:34:57 +0900

    refactor(LotteryRandomGeneratroStrategy.java): 매직넘버를 상수로 변경
    코드 가독성을 위해 메서드 내부에서 사용하는 매직 넘버를 상수로 변경
    
    BREAKING CHANGE:
    as-is:
    public class LotteryRandomGeneratorStrategy implements LotteryGenerateFamily{
    
        private static final int MAX_BOUND = 45;
        ...
    
        public LotteryRandomGeneratorStrategy() {
            ...
            for (int i = 1; i <= MAX_BOUND; i++) {
                ...
            }
        }
    
        public List<LotteryNumber> getNumbers() {
            ...
            return numbers.stream()
                .limit(6)
                ...
        }
    }
    
    to-be:
    public class LotteryRandomGeneratorStrategy implements LotteryGenerateFamily{
    
        private static final int MIN_BOUND = 1;
        private static final int MAX_BOUND = 45;
        private static final int PROPER_LOTTERY_NUMBERS = 6;
        ...
    
        public LotteryRandomGeneratorStrategy() {
            ...
            for (int i = MIN_BOUND; i <= MAX_BOUND; i++) {
                ...
            }
        }
    
        public List<LotteryNumber> getNumbers() {
            ...
            return numbers.stream()
                .limit(PROPER_LOTTERY_NUMBERS)
                ...
        }
    }

--- src/main/java/domain/generatestrategy/LotteryRandomGeneratorStrategy.java
+++ src/main/java/domain/generatestrategy/LotteryRandomGeneratorStrategy.java
@@ -9,12 +9,14 @@ import domain.LotteryNumber;
 
 public class LotteryRandomGeneratorStrategy implements LotteryGenerateFamily{
 
+	private static final int MIN_BOUND = 1;
 	private static final int MAX_BOUND = 45;
+	private static final int PROPER_LOTTERY_NUMBERS = 6;
 	private final List<Integer> numbers;
 
 	public LotteryRandomGeneratorStrategy() {
 		numbers = new ArrayList<>();
-		for (int i = 1; i <= MAX_BOUND; i++) {
+		for (int i = MIN_BOUND; i <= MAX_BOUND; i++) {
 			numbers.add(i);
 		}
 	}
@@ -22,7 +24,7 @@ public class LotteryRandomGeneratorStrategy implements LotteryGenerateFamily{
 	public List<LotteryNumber> getNumbers() {
 		Collections.shuffle(numbers);
 		return numbers.stream()
-			.limit(6)
+			.limit(PROPER_LOTTERY_NUMBERS)
 			.sorted()
 			.map(LotteryNumber::new)
 			.collect(Collectors.toList());
