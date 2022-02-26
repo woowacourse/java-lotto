d3443b04cbd0b35d47010d6dc2d7a55f429ae897
Gyuchool <balance0591@naver.com>
2022-02-26 14:12:17 +0900

    refactor: Integer변환 에러 발생시 try catch로 묶기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,16 +9,15 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final String INPUT_MONEY_ONLY_NUMBER_MESSAGE = "[ERROR] 금액은 숫자로만 입력해주세요.";
     private static final String INPUT_BONUS_BALL_ONLY_NUMBER_MESSAGE = "[ERROR] 보너스 볼은 숫자로만 입력해주세요.";
+    private static final String BONUS_BALL_RANGE_MESSAGE = "[ERROR] 보너스 볼은 1부터 45이내의 숫자여야 합니다.";
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final String REGEX_NUMBER = "^[0-9]*$";
-    private static final Pattern pattern = Pattern.compile(REGEX_NUMBER);
-    private static final String BONUS_BALL_RANGE_MESSAGE = "[ERROR] 보너스 볼은 1부터 45이내의 숫자여야 합니다.";
     private static final int MIN_BONUS_BALL = 1;
     private static final int MAX_BONUS_BALL = 45;
     private static final String DELIMITER = ", ";
+
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static int askInputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
@@ -27,10 +26,11 @@ public class InputView {
     }
 
     private static int convertToInt(String input) {
-        if (!pattern.matcher(input).matches()) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INPUT_MONEY_ONLY_NUMBER_MESSAGE);
         }
-        return Integer.parseInt(input);
     }
 
     public static List<Integer> askInputWinningNumber() {
@@ -51,10 +51,11 @@ public class InputView {
     }
 
     private static int convertToIntBonusBall(String input) {
-        if (!pattern.matcher(input).matches()) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INPUT_BONUS_BALL_ONLY_NUMBER_MESSAGE);
         }
-        return Integer.parseInt(input);
     }
 
     private static void checkBonusBallRange(int bonusBall) {
