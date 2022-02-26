c7fc4dfd1a5842f7c30285ef0c3f02d0f107275e
Donggyu Kim <a29661498@gmail.com>
2022-02-26 14:45:19 +0900

    refactor: 로또 티켓 생성시 검증하는 기능을 추가
    로또 티켓 생성시 다음을 검증하도록 수정해보았습니다.
    * 6개의 숫자를 생성하는지
    * 중복되는 숫자가 없는지

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.domain.generator.LottoNumberGenerator;
 import lotto.domain.vo.LottoNumber;
 
@@ -10,7 +12,27 @@ public class LottoTicket {
     private final List<LottoNumber> lottoNumbers;
 
     public LottoTicket(LottoNumberGenerator lottoNumberGenerator) {
+        List<LottoNumber> lottoNumbers = lottoNumberGenerator.generate();
+        validateLottoTicket(lottoNumbers);
         this.lottoNumbers = lottoNumberGenerator.generate();
+    }
+
+    private void validateLottoTicket(List<LottoNumber> lottoNumbers) {
+        validateLottoNumberSize(lottoNumbers);
+        validateDuplication(lottoNumbers);
+    }
+
+    private void validateLottoNumberSize(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("로또 티켓의 개수가 6개가 아닙니다.");
+        }
+    }
+
+    private void validateDuplication(List<LottoNumber> lottoNumbers) {
+        Set<LottoNumber> lottoNumberSet = new HashSet<>(lottoNumbers);
+        if (lottoNumberSet.size() < lottoNumbers.size()) {
+            throw new IllegalArgumentException("중복된 숫자가 존재할 수 없습니다.");
+        }
     }
 
     public List<LottoNumber> getLottoNumbers() {
