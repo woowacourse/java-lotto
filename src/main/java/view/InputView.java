1ffc0cfeed9fb95c7311914c4a349255f530ebbd
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-26 13:55:45 +0900

    feat(InputView): implement requesting manual lottos input

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,11 +2,15 @@ package view;
 
 import static validator.NumberValidators.validateAndParseNumber;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final String REQUEST_TOTAL_LOTTO_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_TOTAL_MANUAL_LOTTO_COUNT_INPUT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String REQUEST_MANUAL_LOTTOS_INPUT_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String REQUEST_WINNING_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String REQUEST_BONUS_BALL_INPUT_MESSAGE = "보너스 볼을 입력해 주세요.";
 
@@ -18,6 +22,22 @@ public class InputView {
     public static int requestTotalLottoPrice() {
         print(REQUEST_TOTAL_LOTTO_PRICE_INPUT_MESSAGE);
         return validateAndParseNumber(readline());
+    }
+
+    public static int requestManualLottoCount() {
+        print(REQUEST_TOTAL_MANUAL_LOTTO_COUNT_INPUT_MESSAGE);
+        return validateAndParseNumber(readline());
+    }
+
+    public static List<String> requestManualLottos(int count) {
+        print(REQUEST_MANUAL_LOTTOS_INPUT_MESSAGE);
+
+        List<String> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(readline());
+        }
+
+        return lottos;
     }
 
     public static String requestWinningNumbers() {
