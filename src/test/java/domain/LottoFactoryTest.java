c389b410bb63b3a858dc6695bc5329b96a4a4c69
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-26 23:31:37 +0900

    refactor: 불필요한 전역변수를 지역변수로 변경

--- src/test/java/domain/LottoFactoryTest.java
+++ src/test/java/domain/LottoFactoryTest.java
@@ -7,15 +7,13 @@ import org.junit.jupiter.api.Test;
 
 public class LottoFactoryTest {
 
-	private LottoFactory lottoFactory;
-
 	@Test
 	@DisplayName("구입한 금액 만큼 로또 발급")
 	void generateLottoByMoney() {
 		String purchaseMoney = "14000";
 		Money money = Money.from(purchaseMoney);
 
-		lottoFactory = new LottoFactory();
+		LottoFactory lottoFactory = new LottoFactory();
 		assertThat(lottoFactory.generateLottoTicket(money).size()).isEqualTo(Integer.parseInt(purchaseMoney) / 1000);
 	}
 }
