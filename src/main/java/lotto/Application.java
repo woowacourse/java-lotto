775ea00df07b35a31b08ee169f2de00ce8ca89f4
hyeonic <dev.hyeonic@gmail.com>
2022-02-27 09:49:27 +0900

    refactor: 기존 실행 로직 main으로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,49 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.console.LottoRunner;
+import lotto.controller.dto.LottoResultDto;
+import lotto.controller.dto.LottoTicketsDto;
+import lotto.controller.dto.WinningNumberDto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        new LottoRunner(new InputView(), new OutputView(), new LottoController()).run();
+        LottoController lottoController = new LottoController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        int money = createMoney(inputView, outputView);
+        int totalCount = lottoController.createTotalCount(money);
+        outputView.printTotalCount(totalCount);
+
+        LottoTicketsDto lottoTicketsDto = lottoController.createLottoTickets(money);
+        outputView.printLottoTicketsInfo(lottoTicketsDto);
+
+        WinningNumberDto winningNumberDto = creatWinningNumber(lottoController, inputView, outputView);
+
+        outputView.printLottoResultMessage();
+        LottoResultDto lottoResultDto = lottoController.createLottoResult(money, winningNumberDto, lottoTicketsDto);
+        outputView.printYield(lottoResultDto);
+    }
+
+    private static int createMoney(InputView inputView, OutputView outputView) {
+        try {
+            return inputView.getMoney();
+        } catch (RuntimeException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return createMoney(inputView, outputView);
+        }
+    }
+
+    private static WinningNumberDto creatWinningNumber(LottoController lottoController, InputView inputView,
+                                                OutputView outputView) {
+        try {
+            return lottoController.createWinningNumber(inputView.getNormalNumbers(), inputView.getBonusNumber());
+        } catch (RuntimeException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return creatWinningNumber(lottoController, inputView, outputView);
+        }
     }
 }
