a44f8727a2431d2be885366616d3d8b40b26ed3e
HyeonbinSa <shb1833@gmail.com>
2022-02-28 00:04:44 +0900

    test: 생성된 로또에 대한 당첨 결과 통계 테스트

--- src/test/java/LottosTest.java
+++ src/test/java/LottosTest.java
@@ -1,8 +1,6 @@
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +14,23 @@ public class LottosTest {
         lottos.generateLottos(count);
 
         assertEquals(lottos.size(), 14);
+    }
+
+    @Test
+    @DisplayName("생성된 로또에 대해 당첨 결과가 모두 계산되었는지 테스트")
+    public void getWinningStatisticsTest() {
+        int count = 5;
+        WinningNumber winningNumber = new WinningNumber(
+            List.of(LottoNumber.valueOf(1), LottoNumber.valueOf(2), LottoNumber.valueOf(3),
+                LottoNumber.valueOf(4), LottoNumber.valueOf(5), LottoNumber.valueOf(6)));
+        LottoNumber bonusBall = LottoNumber.valueOf(7);
+        Lottos lottos = Lottos.generateLottos(count);
+        lottos.generateLottos(count);
+        Statistic statistic = lottos.getWinningStatistics(winningNumber, bonusBall);
+
+        int sumOfValues = statistic.getStatistics().values().stream()
+            .mapToInt(Integer::intValue)
+            .sum();
+        assertEquals(sumOfValues, 5);
     }
 }
