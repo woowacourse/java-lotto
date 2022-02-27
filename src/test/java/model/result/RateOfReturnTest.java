eb5430f6129925f4a790eff663953a6a0dd0ef28
jaejae-yoo <wotj102@gmail.com>
2022-02-27 14:32:04 +0900

    refactor: 수익률 검증 테스트

---
+++ src/test/java/model/result/RateOfReturnTest.java
@@ -0,0 +1,24 @@
+package model.result;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import model.lotto.LottoCount;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RateOfReturnTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {10000, 14000, 50000})
+    @DisplayName("수익률 검증")
+    void checkProfit(int money) {
+        RateOfReturn rateOfReturn = new RateOfReturn(new LottoCount(Integer.toString(money)));
+
+        WinningResult winningResult = new WinningResult();
+        winningResult.addCount(Rank.FOUR);
+
+        assertThat(rateOfReturn.calcRateOfReturn(winningResult)).isEqualTo(Rank.FOUR.getValue()  / (double) money);
+    }
+}
