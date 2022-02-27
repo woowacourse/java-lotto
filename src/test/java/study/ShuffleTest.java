28f492eaf213f43564c119d2e38d24c7f396615c
pup-paw <wldusdl0310@gmail.com>
2022-02-28 00:51:50 +0900

    refactor: 테스트의 공통 given 부분 @BeforeEach 처리

--- src/test/java/study/ShuffleTest.java
+++ src/test/java/study/ShuffleTest.java
@@ -6,26 +6,28 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ShuffleTest {
+	List<Object> intList;
+
+	@BeforeEach
+	void initializeList() {
+		intList = Arrays.asList(1, 2, 3, 4, 5, 6);
+		Collections.shuffle(intList);
+	}
 
 	@DisplayName("셔플된 리스트의 원소가 변경되지 않는다")
 	@Test
 	void shuffle_numbers_contains() {
-		List<Object> intList = Arrays.asList(1, 2, 3, 4, 5, 6);
-		Collections.shuffle(intList);
-
 		assertThat(intList).contains(1, 2, 3, 4, 5, 6);
 	}
 
 	@DisplayName("셔플된 리스트의 원소의 순서가 변경된다")
 	@Test
 	void shuffle_numbers_sequence() {
-		List<Object> intList = Arrays.asList(1, 2, 3, 4, 5, 6);
-		Collections.shuffle(intList);
-
-		assertThat(intList).doesNotContainSequence(1, 2, 3, 4, 5, 6);
+		assertThat(intList).doesNotContainSequence(5, 1, 2, 6, 4, 3);
 	}
 }
