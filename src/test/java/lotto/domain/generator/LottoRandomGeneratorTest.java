f91c3193c8f914e01f4ba624808c48948de6dbbb
TaeHyeon Kim <kth990303@naver.com>
2022-02-28 01:14:04 +0900

    test: 올바른 로또번호들로 만들어지는지 검증하는 테스트 추가
    LottoNumber 생성자에서 검증을 거쳐주므로, 에러 없이 List<LottoNumber>에 모두 삽입되는지 테스트

--- src/test/java/lotto/domain/generator/LottoRandomGeneratorTest.java
+++ src/test/java/lotto/domain/generator/LottoRandomGeneratorTest.java
@@ -1,22 +1,36 @@
 package lotto.domain.generator;
 
 import lotto.domain.LottoNumbers;
+import lotto.domain.vo.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoRandomGeneratorTest {
+    final int numberOfGenerating = 5;
+    final LottoGenerator lottoRandomGenerator = new LottoRandomGenerator();
+    final List<LottoNumbers> generated = lottoRandomGenerator.generateLottoNumbersGroup(numberOfGenerating);
+
+    @Test
+    @DisplayName("올바른 값으로 로또 번호들이 예외 없이 자동으로 만들어지는지 검증한다.")
+    void checkLottoNumberRangeByGenerate() {
+        //given
+        List<LottoNumber> lottoNumberGroup = new ArrayList<>();
+        final int expected = 30;
+        //when
+        generated.forEach(lottoNumbers -> lottoNumberGroup.addAll(lottoNumbers.getValues()));
+        //then
+        assertThat(lottoNumberGroup.size()).isEqualTo(expected);
+    }
+
     @Test
     @DisplayName("숫자 값을 입력받아, 입력받은 값만큼의 로또 개수를 서로 중복없이 생성해 반환한다.")
     void generate() {
-        //given
-        final int numberOfGenerating = 5;
-        final LottoGenerator lottoRandomGenerator = new LottoRandomGenerator();
         //when
-        final List<LottoNumbers> generated = lottoRandomGenerator.generateLottoNumbersGroup(numberOfGenerating);
         final int actualDistinctCount = (int) generated.stream()
                 .distinct()
                 .count();
