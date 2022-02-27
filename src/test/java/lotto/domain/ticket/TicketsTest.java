e77d7c5db05cfb21bc7482a005bb5180af10b02a
sinb57 <sinb57@naver.com>
2022-02-27 15:47:00 +0900

    test: displayName 수정 및 중복 코드 메소드 추출, 테스트 픽스처 추가

--- src/test/java/lotto/domain/ticket/TicketsTest.java
+++ src/test/java/lotto/domain/ticket/TicketsTest.java
@@ -16,26 +16,28 @@ class TicketsTest {
 
     private final CustomTicketGenerator customTicketGenerator = new CustomTicketGenerator();
 
+    private Tickets generateTickets(final List<TicketDto> generatedTickets, final int ticketCount) {
+        customTicketGenerator.initNumbers(generatedTickets);
+        return new Tickets(ticketCount, customTicketGenerator);
+    }
+
     @DisplayName("로또 목록 생성, 개수 일치 확인 테스트")
     @ParameterizedTest(name = "[{index}] 로또 개수 : {1}")
     @MethodSource("lotto.domain.ticket.provider.TicketsTestProvider#provideForGenerateTest")
     void generateTicketsSizeCheckTest(final List<TicketDto> generatedTickets, final int ticketCount) {
-        customTicketGenerator.initNumbers(generatedTickets);
-        final Tickets tickets = new Tickets(ticketCount, customTicketGenerator);
+        final Tickets tickets = generateTickets(generatedTickets, ticketCount);
         assertThat(tickets.getTickets().size()).isEqualTo(ticketCount);
     }
 
-    @DisplayName("당첨 등수 확인 테스트")
-    @ParameterizedTest(name = "[{index}] 당첨 등수 : {4}")
+    @DisplayName("당첨 등수 목록 확인 테스트")
+    @ParameterizedTest(name = "[{index}] 당첨 등수 목록 : {4}")
     @MethodSource("lotto.domain.ticket.provider.TicketsTestProvider#provideForCalculateRanksTest")
     void calculateRanksTest(final TicketDto winningTicketNumbers,
                             final int bonusNumber,
                             final List<TicketDto> generatedTickets,
                             final int ticketCount,
                             final List<Rank> expected) {
-        customTicketGenerator.initNumbers(generatedTickets);
-        final Tickets tickets = new Tickets(ticketCount, customTicketGenerator);
-
+        final Tickets tickets = generateTickets(generatedTickets, ticketCount);
         final Ticket winningTicket = new Ticket(winningTicketNumbers.getBallNumbers());
         final Ball bonusBall = Balls.getBall(bonusNumber);
 
