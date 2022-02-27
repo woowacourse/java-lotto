ff070c9898eac855e2c4042882a141a179436283
pup-paw <wldusdl0310@gmail.com>
2022-02-27 21:03:05 +0900

    refactor: 출력이 아닌 assert 문을 이용해 테스트 진행

--- src/test/java/study/ShuffleTest.java
+++ src/test/java/study/ShuffleTest.java
@@ -1,18 +1,31 @@
 package study;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ShuffleTest {
-    @Test
-    void shuffle_numbers() {
-        List<Object> intList = Arrays.asList(new int[]{1, 2, 3, 4, 5, 6});
-        Collections.shuffle(intList);
-        for (Object integer : intList) {
-            System.out.println((int) integer);
-        }
-    }
+
+	@DisplayName("셔플된 리스트의 원소가 변경되지 않는다")
+	@Test
+	void shuffle_numbers_contains() {
+		List<Object> intList = Arrays.asList(1, 2, 3, 4, 5, 6);
+		Collections.shuffle(intList);
+
+		assertThat(intList).contains(1, 2, 3, 4, 5, 6);
+	}
+
+	@DisplayName("셔플된 리스트의 원소의 순서가 변경된다")
+	@Test
+	void shuffle_numbers_sequence() {
+		List<Object> intList = Arrays.asList(1, 2, 3, 4, 5, 6);
+		Collections.shuffle(intList);
+
+		assertThat(intList).doesNotContainSequence(1, 2, 3, 4, 5, 6);
+	}
 }
