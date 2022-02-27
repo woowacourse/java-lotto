1f42a245dc36194baa7d3b593a7d1d58a774b236
sinb57 <sinb57@naver.com>
2022-02-27 18:52:27 +0900

    refactor: Validator 클래스, 인스턴스 제거

--- src/main/java/lotto/domain/money/validation/MoneyValidator.java
+++ src/main/java/lotto/domain/money/validation/MoneyValidator.java
@@ -6,23 +6,21 @@ import lotto.exception.money.MoneyExceptionStatus;
 
 public class MoneyValidator {
 
-    private static final MoneyValidator INSTANCE = new MoneyValidator();
-
     private MoneyValidator() {
     }
 
     public static void validateCreditMoney(final int money) {
-        INSTANCE.verifyMoneyNotZero(money);
-        INSTANCE.verifyMoneyDivisible(money);
+        verifyMoneyNotZero(money);
+        verifyMoneyDivisible(money);
     }
 
-    private void verifyMoneyNotZero(final int money) {
+    private static void verifyMoneyNotZero(final int money) {
         if (money == 0) {
             throw new LottoException(MoneyExceptionStatus.MONEY_CANNOT_BE_ZERO);
         }
     }
 
-    private void verifyMoneyDivisible(final int money) {
+    private static void verifyMoneyDivisible(final int money) {
         if (MoneyUnit.isNotDivisible(money)) {
             throw new LottoException(MoneyExceptionStatus.MONEY_MUST_BE_DIVISIBLE);
         }
--- src/main/java/lotto/domain/ticket/validation/WinningTicketValidator.java
+++ src/main/java/lotto/domain/ticket/validation/WinningTicketValidator.java
@@ -7,16 +7,14 @@ import lotto.exception.ticket.TicketNumbersExceptionStatus;
 
 public class WinningTicketValidator {
 
-    private static final WinningTicketValidator INSTANCE = new WinningTicketValidator();
-
     private WinningTicketValidator() {
     }
 
     public static void validateWinningTicket(final Ticket ticket, final Ball bonusBall) {
-        INSTANCE.verifyBonusBallNotDuplicated(ticket, bonusBall);
+        verifyBonusBallNotDuplicated(ticket, bonusBall);
     }
 
-    private void verifyBonusBallNotDuplicated(final Ticket ticket, final Ball bonusBall) {
+    private static void verifyBonusBallNotDuplicated(final Ticket ticket, final Ball bonusBall) {
         if (ticket.contains(bonusBall)) {
             throw new LottoException(TicketNumbersExceptionStatus.TICKET_NUMBERS_CANNOT_BE_DUPLICATED);
         }
