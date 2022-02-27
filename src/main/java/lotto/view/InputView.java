de39792529fba9923d707a3e2bccef5045bbdfc4
wilgur513 <wilgur513@naver.com>
2022-02-27 20:37:33 +0900

    refactor: inputTemplate 파라미터 타입 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import static lotto.view.StringFormatValidator.lottoValidator;
 import static lotto.view.StringFormatValidator.moneyValidator;
+import static lotto.view.StringFormatValidator.numberValidator;
 
 import java.util.Scanner;
 import java.util.function.Consumer;
@@ -9,25 +10,21 @@ import java.util.function.Supplier;
 
 public class InputView {
 
-    private static final StringFormatValidator MONEY_VALIDATOR = moneyValidator();
-    private static final StringFormatValidator LOTTO_VALIDATOR = lottoValidator();
-    private static final StringFormatValidator NUMBER_VALIDATOR = StringFormatValidator
-        .numberValidator();
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
     }
 
     public static String inputMoneyText(Consumer<Exception> exceptionHandler) {
-        return inputTemplate("구입금액을 입력해 주세요.", MONEY_VALIDATOR::validate, exceptionHandler,
+        return inputTemplate("구입금액을 입력해 주세요.", moneyValidator(), exceptionHandler,
             () -> inputMoneyText(exceptionHandler));
     }
 
-    private static String inputTemplate(String message, Consumer<String> validator,
+    private static String inputTemplate(String message, StringFormatValidator validator,
         Consumer<Exception> exceptionHandler, Supplier<String> supplier) {
         try {
             String value = inputWithMessage(message);
-            validator.accept(value);
+            validator.validate(value);
             return value;
         } catch (IllegalArgumentException e) {
             exceptionHandler.accept(e);
@@ -35,19 +32,19 @@ public class InputView {
         }
     }
 
+    private static String inputWithMessage(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
     public static String inputLottoText(Consumer<Exception> exceptionHandler) {
-        return inputTemplate("지난 주 당첨 번호를 입력해 주세요.", LOTTO_VALIDATOR::validate, exceptionHandler,
+        return inputTemplate("지난 주 당첨 번호를 입력해 주세요.", lottoValidator(), exceptionHandler,
             () -> inputLottoText(exceptionHandler));
     }
 
     public static String inputBonusText(Consumer<Exception> exceptionHandler) {
-        return inputTemplate("보너스 볼을 입력해 주세요.", NUMBER_VALIDATOR::validate, exceptionHandler,
+        return inputTemplate("보너스 볼을 입력해 주세요.", numberValidator(), exceptionHandler,
             () -> inputBonusText(exceptionHandler));
-    }
-
-    private static String inputWithMessage(String message) {
-        System.out.println(message);
-        return scanner.nextLine();
     }
 
 }
