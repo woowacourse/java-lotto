eff80c58152acfdeb39c102c27dd770ba02b7ef1
sinb57 <sinb57@naver.com>
2022-02-27 17:17:51 +0900

    test: LottoControllerTest 당첨 번호 및 보너스 볼 입력 예외 테스트

--- src/test/java/lotto/controller/provider/LottoControllerTestProvider.java
+++ src/test/java/lotto/controller/provider/LottoControllerTestProvider.java
@@ -1,12 +1,10 @@
 package lotto.controller.provider;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.provider.Arguments;
 
-import lotto.domain.rank.Rank;
 import lotto.dto.TicketDto;
 
 public class LottoControllerTestProvider {
@@ -105,57 +103,5 @@ public class LottoControllerTestProvider {
         );
     }
 
-    public static Stream<Arguments> provideForCheckOutAnalysisTest() {
-        return Stream.of(
-                Arguments.of(
-                        "1000", "1, 2, 3, 4, 5, 6", "7",
-                        List.of(new TicketDto(List.of(11, 12, 13, 14, 15, 16))),
-                        Map.of(
-                                Rank.FIRST_GRADE, 0,
-                                Rank.SECOND_GRADE, 0,
-                                Rank.THIRD_GRADE, 0,
-                                Rank.FOURTH_GRADE, 0,
-                                Rank.FIFTH_GRADE, 0
-                        ),
-                        "0.00"
-                ),
-                Arguments.of(
-                        "3000", "1, 2, 3, 4, 5, 6", "7",
-                        List.of(
-                                new TicketDto(List.of(1, 2, 3, 14, 15, 16)),
-                                new TicketDto(List.of(1, 2, 3, 4, 5, 16)),
-                                new TicketDto(List.of(11, 12, 13, 14, 15, 16))
-                        ),
-                        Map.of(
-                                Rank.FIRST_GRADE, 0,
-                                Rank.SECOND_GRADE, 0,
-                                Rank.THIRD_GRADE, 1,
-                                Rank.FOURTH_GRADE, 0,
-                                Rank.FIFTH_GRADE, 1
-                        ),
-                        "501666.67"
-                ),
-                Arguments.of(
-                        "7000", "1, 2, 3, 4, 5, 6", "7",
-                        List.of(
-                                new TicketDto(List.of(1, 2, 3, 4, 5, 6)),
-                                new TicketDto(List.of(1, 2, 3, 4, 5, 7)),
-                                new TicketDto(List.of(7, 2, 3, 4, 5, 6)),
-                                new TicketDto(List.of(1, 2, 3, 14, 15, 16)),
-                                new TicketDto(List.of(4, 5, 6, 14, 15, 16)),
-                                new TicketDto(List.of(11, 12, 13, 41, 15, 16)),
-                                new TicketDto(List.of(11, 12, 13, 14, 15, 16))
-                        ),
-                        Map.of(
-                                Rank.FIRST_GRADE, 1,
-                                Rank.SECOND_GRADE, 2,
-                                Rank.THIRD_GRADE, 0,
-                                Rank.FOURTH_GRADE, 0,
-                                Rank.FIFTH_GRADE, 2
-                        ),
-                        "294287142.86"
-                )
-        );
-    }
 
 }
