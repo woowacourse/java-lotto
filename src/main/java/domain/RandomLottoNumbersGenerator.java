e4f94ffb6f6058b5bf68ab3cf79be9b71ab43e82
seong-wooo <seongwoo0513@uos.ac.kr>
2022-02-27 19:01:10 +0900

    refactor : LOTTO_SIZE 상수를 다른 클래스에서도 사용하도록 수정

--- src/main/java/domain/RandomLottoNumbersGenerator.java
+++ src/main/java/domain/RandomLottoNumbersGenerator.java
@@ -1,13 +1,12 @@
 package domain;
 
+import static domain.Ticket.LOTTO_SIZE;
+
 import java.util.Set;
 import java.util.TreeSet;
-
 import utils.RandomNumberGenerator;
 
 public class RandomLottoNumbersGenerator implements LottoNumbersGenerator {
-    private static final int LOTTO_SIZE = 6;
-
     @Override
     public Set<LottoNumber> generate() {
         Set<LottoNumber> lottoNumbers = new TreeSet<>();
--- src/main/java/domain/Ticket.java
+++ src/main/java/domain/Ticket.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Ticket {
-    private static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_SIZE = 6;
     private static final String REQUEST_NON_DUPLICATED_NUMBER = String.format("중복되지 않은 숫자 %d개를 입력해주세요.", LOTTO_SIZE);
     private static final String DELIMITER = ", ";
     private final Set<LottoNumber> lottoNumbers;
