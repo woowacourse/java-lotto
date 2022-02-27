c7b10cff0255f81625f655d0b88307a0196a8714
유영우 <fbduddn97@naver.com>
2022-02-27 19:57:21 +0900

    refactor: View toString() 이용하는 것으로 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,26 +1,23 @@
 package view;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import domain.Rank;
-import domain.Ticket;
 import domain.Tickets;
+import domain.WinningAnalyze;
 
 public class OutputView {
-	private static final String COUNT_MESSAGE = "개를 구매했습니다.";
-	private static final String ANALYSIS_TITLE = "당첨 통계";
+	private static final String PAYMENT_COUNT_MESSAGE = "개를 구매했습니다.";
+	private static final String ANALYSIS_TITLE_MESSAGE = "당첨 통계";
 	private static final String DIVIDING_LINE = "---------";
-	private static final String PROFIT_RATE_MESSAGE_FORMAT = "총 수익률은 %.2f입니다.";
-	private static final String ANALYSIS_MATCH_COUNT_MESSAGE = "%d개 일치";
-	private static final String ANALYSIS_BONUS_BALL_COUNT_MESSAGE = ", 보너스 볼 일치";
-	private static final String ANALYSIS_PRIZE_COUNT_MESSAGE = "(%d원) - %d개";
 
-	private static final String START_SIGN = "[";
-	private static final String END_SIGN = "]";
+	public static void printTickets(Tickets tickets) {
+		System.out.println(tickets.size() + PAYMENT_COUNT_MESSAGE);
+		System.out.println(tickets.toString());
+	}
 
-	private static final String ORIGIN_TICKET_DELIMITER = ",";
+	public static void printStatistics(WinningAnalyze winningAnalyze) {
+		System.out.println();
+		System.out.println(ANALYSIS_TITLE_MESSAGE);
+		System.out.println(DIVIDING_LINE);
 
-
+		System.out.println(winningAnalyze.toString());
+	}
 }
