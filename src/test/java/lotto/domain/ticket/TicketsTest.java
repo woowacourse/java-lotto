bfdeb901433451454ffe9e03a18443e19823a7f2
sinb57 <sinb57@naver.com>
2022-02-27 14:24:56 +0900

    refactor: TicketsDto 클래스 제거

--- src/test/java/lotto/domain/ticket/TicketsTest.java
+++ src/test/java/lotto/domain/ticket/TicketsTest.java
@@ -8,41 +8,36 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import lotto.AppConfig;
 import lotto.domain.rank.Rank;
 import lotto.domain.ticket.generator.CustomTicketGenerator;
 import lotto.dto.TicketDto;
 
 class TicketsTest {
 
-    private static final AppConfig APP_CONFIG = AppConfig.getInstance();
-
-    private final CustomTicketGenerator customTicketGenerator = APP_CONFIG.ticketGenerator;
-
-    private Tickets generateTickets(final List<TicketDto> generatedTickets, final int ticketCount) {
-        customTicketGenerator.initTickets(generatedTickets);
-        return new Tickets(ticketCount, customTicketGenerator);
-    }
+    private final CustomTicketGenerator customTicketGenerator = new CustomTicketGenerator();
 
     @DisplayName("로또 목록 생성, 개수 일치 확인 테스트")
     @ParameterizedTest(name = "[{index}] 로또 개수 : {1}")
     @MethodSource("lotto.domain.ticket.provider.TicketsTestProvider#provideForGenerateTest")
     void generateTicketsSizeCheckTest(final List<TicketDto> generatedTickets, final int ticketCount) {
-        final Tickets tickets = generateTickets(generatedTickets, ticketCount);
+        customTicketGenerator.initNumbers(generatedTickets);
+        final Tickets tickets = new Tickets(ticketCount, customTicketGenerator);
         assertThat(tickets.getTickets().size()).isEqualTo(ticketCount);
     }
 
-    @DisplayName("당첨 등수 목록 확인 테스트")
-    @ParameterizedTest(name = "[{index}] 당첨 등수 목록 : {4}")
+    @DisplayName("당첨 등수 확인 테스트")
+    @ParameterizedTest(name = "[{index}] 당첨 등수 : {4}")
     @MethodSource("lotto.domain.ticket.provider.TicketsTestProvider#provideForCalculateRanksTest")
     void calculateRanksTest(final TicketDto winningTicketNumbers,
                             final int bonusNumber,
                             final List<TicketDto> generatedTickets,
                             final int ticketCount,
                             final List<Rank> expected) {
-        final Tickets tickets = generateTickets(generatedTickets, ticketCount);
+        customTicketGenerator.initNumbers(generatedTickets);
+        final Tickets tickets = new Tickets(ticketCount, customTicketGenerator);
+
         final Ticket winningTicket = new Ticket(winningTicketNumbers.getBallNumbers());
-        final Ball bonusBall = Balls.getBall(bonusNumber);
+        final Ball bonusBall = new Ball(bonusNumber);
 
         final List<Rank> actual = tickets.calculateRanks(winningTicket, bonusBall);
         assertThat(actual).isEqualTo(expected);
