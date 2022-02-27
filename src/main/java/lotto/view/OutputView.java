1d113a84e806053df97941ed4c953ab58900da31
dongho <dongho108@naver.com>
2022-02-27 17:01:42 +0900

    refactor : OutputView 리팩터링
    * StringBuilder 도입
    * 메서드 순서 호출 순으로
    * 상수추출

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,23 +11,33 @@ import lotto.domain.Ticket;
 import lotto.domain.Tickets;
 
 public class OutputView {
-
+    private static final StringBuilder stringBuilder = new StringBuilder();
+    
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String LOSS_MESSAGE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
     private static final String DELIMITER = ", ";
-
-    private static void printTicketCount(int ticketCount) {
-        System.out.println(ticketCount + PURCHASE_MESSAGE);
-    }
+    public static final String RESULT_HEADER_MESSAGE = "당첨 통계\n---------\n";
+    public static final int STANDARD = 1;
 
     public static void printTickets(int ticketCount, Tickets tickets) {
-        printTicketCount(ticketCount);
-        List<Ticket> purchasedTickets = tickets.getTickets();
-        for (Ticket ticket : purchasedTickets) {
-            System.out.print("[");
+        initializeStringBuilder();
+        appendTicketCountToStringBuilder(ticketCount);
+        appendTicketsToStringBuilder(tickets);
+        System.out.println(stringBuilder);
+    }
+
+    private static void appendTicketCountToStringBuilder(int ticketCount) {
+        stringBuilder.append(ticketCount)
+            .append(PURCHASE_MESSAGE)
+            .append("\n");
+    }
+
+    private static void appendTicketsToStringBuilder(Tickets tickets) {
+        for (Ticket ticket : tickets.getTickets()) {
             Set<LottoNumber> lottoNumbers = ticket.getLottoNumbers();
-            System.out.print(String.join(DELIMITER, getLottoNumbers(lottoNumbers)));
-            System.out.println("]");
+            stringBuilder.append("[")
+                .append(String.join(DELIMITER, getLottoNumbers(lottoNumbers)))
+                .append("]\n");
         }
     }
 
@@ -38,30 +48,24 @@ public class OutputView {
             .collect(Collectors.toList());
     }
 
-    public static void printErrorMessage(String message) {
-        System.out.println(message);
-    }
-
     public static void printResult(Map<Rank, Integer> result, double yield) {
-        System.out.println("당첨 통계");
-        System.out.println("---------");
-        for (Rank rank : Rank.getRanks()) {
-            System.out.println(rank.getCount()
-                + "개 일치"
-                + getBonus(rank)
-                + "(" + rank.getAmount()
-                + "원) - "
-                + getCount(result, rank)
-                + "개");
-        }
+        initializeStringBuilder();
+        stringBuilder.append(RESULT_HEADER_MESSAGE);
+        appendResultToStringBuilder(result);
+        System.out.print(stringBuilder);
         printYield(yield);
     }
 
-    private static int getCount(Map<Rank, Integer> result, Rank rank) {
-        if (result.containsKey(rank)) {
-            return result.get(rank);
+    private static void appendResultToStringBuilder(Map<Rank, Integer> result) {
+        for (Rank rank : Rank.getRanks()) {
+            stringBuilder.append(rank.getCount())
+                .append("개 일치")
+                .append(getBonus(rank));
+            stringBuilder.append("(")
+                .append(rank.getAmount()).append("원) - ")
+                .append(getCount(result, rank))
+                .append("개\n");
         }
-        return 0;
     }
 
     private static String getBonus(Rank rank) {
@@ -71,11 +75,29 @@ public class OutputView {
         return " ";
     }
 
-    private static void printYield(double yield) {
-        System.out.print("총 수익률은 " + yield + "입니다.");
-        if (yield < 1) {
-            System.out.print(LOSS_MESSAGE);
+    private static int getCount(Map<Rank, Integer> result, Rank rank) {
+        if (result.containsKey(rank)) {
+            return result.get(rank);
         }
-        System.out.println();
+        return 0;
+    }
+
+    private static void printYield(double yield) {
+        initializeStringBuilder();
+        stringBuilder.append("총 수익률은 ")
+                .append(yield)
+                .append("입니다.");
+        if (yield < STANDARD) {
+            stringBuilder.append(LOSS_MESSAGE);
+        }
+        System.out.println(stringBuilder);
+    }
+
+    private static void initializeStringBuilder() {
+        stringBuilder.setLength(0);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(message);
     }
 }
