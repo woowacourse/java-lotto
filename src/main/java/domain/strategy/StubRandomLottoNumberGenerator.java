392fa6050b5fa5289d7c301df36f888d79daa3c5
devHudi <devhudi@gmail.com>
2022-02-27 15:46:41 +0900

    feat: StubRandomLottoNumberGenerator가 순서를 갖도록 개선

--- src/main/java/domain/strategy/StubRandomLottoNumberGenerator.java
+++ src/main/java/domain/strategy/StubRandomLottoNumberGenerator.java
@@ -3,14 +3,18 @@ package domain.strategy;
 import java.util.List;
 
 public class StubRandomLottoNumberGenerator implements LottoNumberGenerateStrategy {
-    private final List<Integer> numbers;
+    private final List<List<Integer>> numberSequences;
+    private int sequenceCursor = 0;
 
-    public StubRandomLottoNumberGenerator(List<Integer> numbers) {
-        this.numbers = numbers;
+    public StubRandomLottoNumberGenerator(List<List<Integer>> numberSequences) {
+        this.numberSequences = numberSequences;
     }
 
     @Override
     public List<Integer> generateLottoNumbers() {
-        return this.numbers;
+        int currentCursor = sequenceCursor;
+        sequenceCursor = (sequenceCursor + 1) % numberSequences.size();
+
+        return numberSequences.get(currentCursor);
     }
 }
--- src/test/java/domain/strategy/StubRandomLottoNumberGeneratorTest.java
+++ src/test/java/domain/strategy/StubRandomLottoNumberGeneratorTest.java
@@ -8,17 +8,21 @@ import org.junit.jupiter.api.Test;
 
 public class StubRandomLottoNumberGeneratorTest {
     @Test
-    @DisplayName("생성자로 전달한 List<Integer> 를 generateLottoNumber 호출 시 그대로 반환")
+    @DisplayName("생성자로 전달한 순서를 generateLottoNumber 호출 시 그대로 순환하며 반환")
     void generateLottoNumberTest() {
         // given
-        LottoNumberGenerateStrategy fixedLottoNumberGenerator = new StubRandomLottoNumberGenerator(
-                List.of(1, 2, 3, 4, 5, 6)
+        List<List<Integer>> numbersSequence = List.of(
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(7, 8, 9, 10, 11, 12),
+                List.of(13, 14, 15, 16, 17, 18)
         );
+        LottoNumberGenerateStrategy fixedLottoNumberGenerator = new StubRandomLottoNumberGenerator(numbersSequence);
 
-        // when
-        List<Integer> actual = fixedLottoNumberGenerator.generateLottoNumbers();
-
-        // then
-        assertThat(actual).containsExactly(1, 2, 3, 4, 5, 6);
+        // when & then
+        for (int i = 0; i < 6; i++) {
+            int cursor = i % numbersSequence.size();
+            List<Integer> actual = fixedLottoNumberGenerator.generateLottoNumbers();
+            assertThat(actual).containsAll(numbersSequence.get(cursor));
+        }
     }
 }
