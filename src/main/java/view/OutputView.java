898b43ae177d3e87d7edfb6cfd70c815d503dc81
hyewoncc <hyewoncc@gmail.com>
2022-02-28 03:20:48 +0900

    refactor: 출력 포맷 적용 개선

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,22 +10,17 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
-    private static final String PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다.%n";
     private static final String LEFT_BRACKET = "[";
     private static final String RIGHT_BRACKET = "]";
     private static final String DELIMITER = ", ";
-    private static final String WINNING_STATISTIC = "당첨 통계";
-    private static final String OUTLINE = "---------";
-    private static final String STATISTICS_PRIZE_MESSAGE = "개 일치 (";
-    private static final String STATISTICS_SECOND_PRIZE_MESSAGE = "5개 일치, 보너스 볼 일치 (";
-    private static final String STATISTICS_WON_MESSAGE = "원)- ";
-    private static final String STATISTICS_COUNT_MESSAGE = "개";
-    private static final String EARNING_RATE_START_MESSAGE = "총 수익률은 ";
-    private static final String EARNING_RATE_END_MESSAGE = "입니다.";
-
+    private static final String WINNING_STATISTIC = "당첨 통계%n---------";
+    private static final String STATISTICS_PRIZE_MESSAGE = "%d개 일치 (%d원)- %d개%n";
+    private static final String STATISTICS_SECOND_PRIZE_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원)- %d개%n";
+    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.2f입니다.";
 
     public static void printPurchasedLotto(LottoGame lottoGame) {
-        System.out.println(lottoGame.getLottos().size() + PURCHASE_COUNT_MESSAGE);
+        System.out.printf(PURCHASE_COUNT_MESSAGE, lottoGame.getLottos().size());
         for (Lotto lotto : lottoGame.getLottos()) {
             System.out.print(LEFT_BRACKET);
             String lottoNumbers = String.join(DELIMITER, lotto.getLottoNumbers().stream()
@@ -39,7 +34,6 @@ public class OutputView {
 
     public static void printFinalStatistic(PrizeResult result) {
         System.out.println(System.lineSeparator() + WINNING_STATISTIC);
-        System.out.println(OUTLINE);
         Map<Prize, Integer> prizeResult = result.getPrizeResult();
         for (Prize winnerPrice : result.validWinnerPrices()) {
             printEachStatistic(winnerPrice, prizeResult);
@@ -48,16 +42,16 @@ public class OutputView {
 
     private static void printEachStatistic(Prize winnerPrize, Map<Prize, Integer> prizeResult) {
         if (winnerPrize == Prize.SECOND) {
-            System.out.print(STATISTICS_SECOND_PRIZE_MESSAGE + winnerPrize.getPrize() + STATISTICS_WON_MESSAGE
-                    + prizeResult.get(winnerPrize) + STATISTICS_COUNT_MESSAGE + System.lineSeparator());
+            System.out.printf(STATISTICS_SECOND_PRIZE_MESSAGE,
+                    winnerPrize.getMatched(), winnerPrize.getPrize(), prizeResult.get(winnerPrize));
             return;
         }
-        System.out.print(winnerPrize.getMatched() + STATISTICS_PRIZE_MESSAGE + winnerPrize.getPrize() + STATISTICS_WON_MESSAGE
-                + prizeResult.get(winnerPrize) + STATISTICS_COUNT_MESSAGE + System.lineSeparator());
+        System.out.printf(STATISTICS_PRIZE_MESSAGE,
+                winnerPrize.getMatched(), winnerPrize.getPrize(), prizeResult.get(winnerPrize));
     }
 
     public static void printEarningRate(double earningRate) {
-        System.out.println(EARNING_RATE_START_MESSAGE + earningRate + EARNING_RATE_END_MESSAGE);
+        System.out.printf(EARNING_RATE_MESSAGE, earningRate);
     }
 
 }
