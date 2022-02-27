20352b0c9b77e1805ba827efcce115ebd7db3e4e
yoon <duddbs774@gmail.com>
2022-02-27 23:34:46 +0900

    refactor: 변수명 변경

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,27 +1,56 @@
 package lotto.domain;
 
+import java.util.Objects;
 import java.util.Optional;
+import lotto.domain.lottoticket.LottoTicket;
 
 public class WinningNumbers {
     private static final String DUPLICATE_ERROR = "보너스 번호는 당첨 번호와 중복될 수 없습니다";
 
-    private final LottoNumbers lottoNumbers;
+    private final LottoTicket lottoNumbers;
     private final LottoNumber bonusNumber;
 
-    public WinningNumbers(LottoNumbers lottoNumbers, LottoNumber bonusNumber) {
+    public WinningNumbers(LottoTicket lottoNumbers, LottoNumber bonusNumber) {
         validateDuplicateNumber(lottoNumbers, bonusNumber);
         this.lottoNumbers = lottoNumbers;
         this.bonusNumber = bonusNumber;
     }
 
-    public Optional<Ranking> calculateRanking(LottoNumbers otherLottoNumbers) {
-        int cnt = lottoNumbers.calculateSameCount(otherLottoNumbers);
-        return Ranking.findRanking(cnt, otherLottoNumbers.isContain(bonusNumber));
+    public Optional<Ranking> calculateRanking(LottoTicket otherLottoNumbers) {
+        int hitCount = lottoNumbers.calculateSameCount(otherLottoNumbers);
+        boolean hasBonusBall = otherLottoNumbers.isContains(bonusNumber);
+        return Ranking.findRanking(hitCount, hasBonusBall);
     }
 
-    private void validateDuplicateNumber(LottoNumbers lottoNumbers, LottoNumber bonusNumber) {
-        if (lottoNumbers.isContain(bonusNumber)) {
+    private void validateDuplicateNumber(LottoTicket lottoNumbers, LottoNumber bonusNumber) {
+        if (lottoNumbers.isContains(bonusNumber)) {
             throw new IllegalArgumentException(DUPLICATE_ERROR);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        WinningNumbers that = (WinningNumbers) o;
+        return Objects.equals(lottoNumbers, that.lottoNumbers) && Objects.equals(bonusNumber,
+                that.bonusNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers, bonusNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "WinningNumbers{" +
+                "lottoNumbers=" + lottoNumbers +
+                ", bonusNumber=" + bonusNumber +
+                '}';
     }
 }
--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -3,71 +3,53 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.domain.lottoticket.LottoTicket;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class WinningNumbersTest {
     private WinningNumbers winningNumbers;
 
     @BeforeEach
     void setUp() {
-        LottoNumbers lottoNumbers = new LottoNumbers("1,2,3,4,5,6");
-        LottoNumber bonusNumber = new LottoNumber("7");
+        LottoTicket lottoNumbers = new LottoTicket("1,2,3,4,5,6");
+        LottoNumber bonusNumber = new LottoNumber(7);
         winningNumbers = new WinningNumbers(lottoNumbers, bonusNumber);
     }
 
     @Test
-    @DisplayName("중복 문자")
+    @DisplayName("1등 당첨번호와 보너스 번호가 중복 될 경우 예외 발생")
     void test() {
-        // given
-        LottoNumbers lottoNumbers = new LottoNumbers("1,2,3,4,5,6");
-        LottoNumber bonusNumber = new LottoNumber("1");
+        LottoTicket lottoNumbers = new LottoTicket("1,2,3,4,5,6");
+        LottoNumber bonusNumber = new LottoNumber(1);
 
-        // when
-
-        // then
         assertThatThrownBy(() -> new WinningNumbers(lottoNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    @DisplayName("1등 당첨 결과 반환")
-    void firstPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,4,5,6");
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,2,3,4,5,6 : FIRST",
+            "1,2,3,4,5,7 : SECOND",
+            "1,2,3,4,5,8 : THIRD",
+            "1,2,3,4,8,9 : FOURTH",
+            "1,2,3,8,9,10 : FIFTH"},
+            delimiter = ':')
+    @DisplayName("1,2,3,4,5 등 당첨 결과 반환")
+    void calculateRanking(String lottoNumbers, Ranking expectedRanking) {
+        LottoTicket myLotto = new LottoTicket(lottoNumbers);
 
-        assertThat(winningNumbers.calculateRanking(myLotto).get()).isEqualTo(Ranking.FIRST);
+        assertThat(winningNumbers.calculateRanking(myLotto).get()).isEqualTo(expectedRanking);
     }
 
     @Test
-    @DisplayName("2등 당첨 결과 반환")
-    void secondPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,4,5,7");
+    @DisplayName("당첨되지 않았을 경우")
+    void noPrize() {
+        LottoTicket myLotto = new LottoTicket("1,2,11,8,9,10");
 
-        assertThat(winningNumbers.calculateRanking(myLotto).get()).isEqualTo(Ranking.SECOND);
-    }
-
-    @Test
-    @DisplayName("3등 당첨 결과 반환")
-    void thirdPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,4,5,8");
-
-        assertThat(winningNumbers.calculateRanking(myLotto).get()).isEqualTo(Ranking.THIRD);
-    }
-
-    @Test
-    @DisplayName("4등 당첨 결과 반환")
-    void fourthPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,4,8,9");
-
-        assertThat(winningNumbers.calculateRanking(myLotto).get()).isEqualTo(Ranking.FOURTH);
-    }
-
-    @Test
-    @DisplayName("5등 당첨 결과 반환")
-    void fifthPrize() {
-        LottoNumbers myLotto = new LottoNumbers("1,2,3,8,9,10");
-
-        assertThat(winningNumbers.calculateRanking(myLotto).get()).isEqualTo(Ranking.FIFTH);
+        assertThat(winningNumbers.calculateRanking(myLotto).isEmpty()).isTrue();
     }
 }
