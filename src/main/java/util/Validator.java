86b8c70fc02ace2834c86862f528bf4fe8b6c419
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-02-28 23:39:11 +0900

    refactor: 미사용 필드 제거

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -3,10 +3,8 @@ package util;
 public class Validator {
 
     private static final String ERROR = "[ERROR] ";
-    private static final String INVALID_WINNING_NUMBER_LENGTH_ERROR = "당첨 번호는 6개의 숫자만 입력 가능합니다.";
     private static final String NEGATIVE_PRICE_ERROR = "음수인 금액은 입력할 수 없습니다.";
     private static final String NOT_INTEGER_ERROR = "정수가 아닌 문자열이 입력되었습니다.";
-    private static final int WINNING_NUMBER_COUNT = 6;
     private static final int LOTTO_NUMBERS_COUNT = 6;
     public static final String INVALID_LOTTO_NUMBER_COUNT = "로또 번호의 수는 6자리를 입력해주어야 합니다.";
 
