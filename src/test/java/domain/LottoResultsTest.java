2d2f2a19e10ce0259530248110d7b0562f4af6f7
seong-wooo <seongwoo0513@uos.ac.kr>
2022-02-28 15:26:55 +0900

    refactor : LottoResultTest 테스트를 ParameterizedTest로 변경

--- src/test/java/domain/LottoResultsTest.java
+++ src/test/java/domain/LottoResultsTest.java
@@ -1,39 +1,48 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
 
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-@SuppressWarnings("NonAsciiCharacters")
 public class LottoResultsTest {
-    @Test
-    void 로또당첨_등수_개수() {
+
+    @ParameterizedTest(name = "{index}: {4}")
+    @MethodSource("validRankCount")
+    @DisplayName("로또당첨 등수 개수 검사")
+    void validResult(Ticket ticket1,
+                     Ticket ticket2,
+                     Ticket ticket3,
+                     int size,
+                     String testName) {
         WinningNumbers winningNumbers = getWinningNumbers();
-
-        Ticket ticketFirst = getFirstTicket();
-        Ticket ticketFirst2 = getFirstTicket();
-        Ticket ticketSecond = getSecondTicket();
-        Ticket ticketThird = getThirdTicket();
-
-        Tickets tickets = new Tickets(Arrays.asList(ticketFirst, ticketSecond, ticketFirst2, ticketThird));
+        Tickets tickets = new Tickets(Arrays.asList(ticket1, ticket2, ticket3));
         LottoResults result = LottoResults.of(winningNumbers, tickets);
 
-        assertThat(result.getLottoResults()).hasSize(3)
-                .contains(entry(Rank.FIRST, 2), entry(Rank.SECOND, 1), entry(Rank.THIRD, 1));
+        assertThat(result.getLottoResults()).hasSize(size);
+    }
+
+    static Stream<Arguments> validRankCount() {
+        return Stream.of(
+                Arguments.of(getOtherTicket(), getOtherTicket(), getOtherTicket(), 1, "등수 개수 1"),
+                Arguments.of(getOtherTicket(), getThirdTicket(), getThirdTicket(), 2, "등수 개수 2"),
+                Arguments.of(getFifthTicket(), getSecondTicket(), getFirstTicket(), 3, "등수 개수 3"));
     }
 
     @ParameterizedTest(name = "{index}: {4}")
     @MethodSource("validParameters")
     @DisplayName("수익률 검사")
-    void validCreate(Ticket ticket1, Ticket ticket2, Ticket ticket3, int amount, double yield, String testName) {
+    void validCreate(Ticket ticket1,
+                     Ticket ticket2,
+                     Ticket ticket3,
+                     int amount,
+                     double yield,
+                     String testName) {
         WinningNumbers winningNumbers = getWinningNumbers();
         Tickets tickets = new Tickets(Arrays.asList(ticket1, ticket2, ticket3));
         LottoResults results = LottoResults.of(winningNumbers, tickets);
