1937fb52e71a461b52880783e5fe23036335c049
Hyunta <mohai2618@gmail.com>
2022-02-28 16:22:54 +0900

    refactor: 하드코딩한 예상값을 @MethodSource를 이용해 검증

--- src/test/java/lotto/domain/WinningNumberTest.java
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -1,16 +1,19 @@
 package lotto.domain;
 
+import static lotto.domain.LottoRank.*;
+
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class WinningNumberTest {
 
     @ParameterizedTest
     @DisplayName("올바른 등수를 판별하는지")
-    @CsvSource(value = {"1,2,3,4,5,6:FIRST", "1,2,3,4,5,7:SECOND", "1,2,3,4,5,8:THIRD",
-            "1,2,3,4,9,7:FOURTH", "1,2,3,11,12,13:FIFTH"}, delimiter = ':')
+    @MethodSource("rankProvider")
     void Decide_Rank(String input, LottoRank inputRank) {
         ChoiceNumber choiceNumber = new ChoiceNumber("1,2,3,4,5,6");
         BonusNumber bonusNumber = new BonusNumber("7", choiceNumber);
@@ -18,4 +21,16 @@ class WinningNumberTest {
 
         Assertions.assertThat(winningNumber.findLottoRank(new ChoiceNumber(input))).isEqualTo(inputRank);
     }
+
+    private static Stream<Arguments> rankProvider() {
+        return Stream.of(
+                Arguments.arguments("1,2,3,4,5,6", FIRST),
+                Arguments.arguments("1,2,3,4,5,7", SECOND),
+                Arguments.arguments("1,2,3,4,5,8", THIRD),
+                Arguments.arguments("1,2,3,4,9,7", FOURTH),
+                Arguments.arguments("1,2,3,11,12,13", FIFTH)
+        );
+    }
+
+
 }
