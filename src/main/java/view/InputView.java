13fdca3219b295cdd7dc9433a2a68fc1a7a1149f
is2js <tingstyle1@gmail.com>
2022-02-28 23:34:59 +0900

    refactor: InputView, OutputView 메서드들 static 제거

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,22 +10,27 @@ public class InputView {
 
     private static final String INPUT_NUMBER_DELIMITER = ",";
     private static final int INPUT_NUMBER_SPLIT_OPTION = -1;
+    private static final String INPUT_MONEY_INSTRUCTION = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WIN_LOTTO_INSTRUCTION = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_INSTRUCTION = "보너스 볼을 입력해 주세요.";
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private static String getInput() {
         return SCANNER.nextLine();
     }
 
-    public static int getMoney() {
-        OutputView.printMoneyInstruction();
+    public int getMoney() {
+        System.out.println(INPUT_MONEY_INSTRUCTION);
         final String input = getInput();
         Validator.checkNullOrEmpty(input);
         Validator.checkFormat(input);
         return Integer.parseInt(input);
     }
 
-    public static List<String> getLastWinLotto() {
-        OutputView.printWinLottoInstruction();
+    public List<String> getLastWinLotto() {
+        System.out.println();
+        System.out.println(INPUT_WIN_LOTTO_INSTRUCTION);
         final String input = getInput();
         Validator.checkNullOrEmpty(input);
         return Arrays.stream(input.split(INPUT_NUMBER_DELIMITER, INPUT_NUMBER_SPLIT_OPTION))
@@ -33,8 +38,9 @@ public class InputView {
             .collect(Collectors.toList());
     }
 
-    public static int getBonusNumber() {
-        OutputView.printBonusInstruction();
+    public int getBonusNumber() {
+        System.out.println();
+        System.out.println(INPUT_BONUS_INSTRUCTION);
         final String input = getInput();
         Validator.checkNullOrEmpty(input);
         Validator.checkFormat(input);
