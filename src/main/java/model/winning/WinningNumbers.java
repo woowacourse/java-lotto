1ff9d86498058d92f3ca8a9a0d3eb846b9a8bb24
koo <gudonghee2000@gmail.com>
2022-02-28 23:31:08 +0900

    refactor: WinningNumbers클래스의 상수명 변경, 함수명 변경

--- src/main/java/model/winning/WinningNumbers.java
+++ src/main/java/model/winning/WinningNumbers.java
@@ -2,34 +2,35 @@ package model.winning;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 import model.lottotickets.vo.Number;
 
 public class WinningNumbers {
-    private static final int TICKET_SIZE = 6;
-    private static final String NOT_CORRECT_TICKET_SIZE_ERROR_MESSAGE = "입력한 당첨 번호가 6개가 아닙니다.";
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final String NOT_CORRECT_LOTTO_NUMBER_COUNT_ERROR_MESSAGE = "입력한 당첨 번호가 6개가 아닙니다.";
 
     private final List<Number> winningNumbers;
     private final Number bonusNumber;
 
     public WinningNumbers(final List<Integer> winningNumbers, final int bonusNumber) {
-        checkSize(winningNumbers);
+        checkCountOfNumbers(winningNumbers);
         this.winningNumbers = winningNumbers.stream()
                 .map(Number::new)
                 .collect(Collectors.toList());
         this.bonusNumber = new Number(bonusNumber);
     }
 
-    private void checkSize(final List<Integer> winningNumbers) {
-        if (winningNumbers.size() != TICKET_SIZE) {
-            throw new IllegalArgumentException(NOT_CORRECT_TICKET_SIZE_ERROR_MESSAGE);
+    private void checkCountOfNumbers(final List<Integer> winningNumbers) {
+        if (winningNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(NOT_CORRECT_LOTTO_NUMBER_COUNT_ERROR_MESSAGE);
         }
-    }
-
-    public Number getBonusNumber() {
-        return bonusNumber;
     }
 
     public List<Number> getWinningNumbers() {
         return winningNumbers;
+    }
+
+    public Number getBonusNumber() {
+        return bonusNumber;
     }
 }
