69a652ef874bbbff4da3a94dd11f2bb18eb8e474
ulimy <ulim130@gmail.com>
2022-02-28 17:07:35 +0900

    feat : 구입금액으로 구매할 수 없는 수동구매개수를 입력할 경우 예외 발생

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -6,12 +6,19 @@ import static constant.LottoConstant.PRICE_OF_LOTTO;
 public class Money {
 
 	private static final String DIVIDE_BY_THOUSAND = START_ERROR + "1000원으로 나누어 떨어지는 금액을 입력해주세요.";
+	private static final String CANT_PURCHASE = START_ERROR + "구입금액이 부족합니다.";
 
 	private final int inputMoney;
 
 	public Money(int inputMoney) {
 		validateMoney(inputMoney);
 		this.inputMoney = inputMoney;
+	}
+
+	public void canPurchase(int count) {
+		if (inputMoney < count * PRICE_OF_LOTTO) {
+			throw new IllegalArgumentException(CANT_PURCHASE);
+		}
 	}
 
 	public int calculateCount() {
--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,7 +13,13 @@ public class MoneyTest {
 	@ValueSource(ints = {0, 1234})
 	@DisplayName("구입금액 : 로또 금액으로 나누어 떨어지지 않는 금액을 입력 한 경우 예외 발생")
 	void validatePrice(int inputMoney) {
-		assertThatThrownBy(() -> new Money(inputMoney)).isInstanceOf(
-			IllegalArgumentException.class);
+		assertThatThrownBy(() -> new Money(inputMoney)).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("수동으로 구매할 로또 개수 : 구입금액으로 구입할 수 없는 경우 예외 발생")
+	void canNotPurchase() {
+		Money money = new Money(5000);
+		assertThatThrownBy(() -> money.canPurchase(10)).isInstanceOf(IllegalArgumentException.class);
 	}
 }
