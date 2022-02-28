a8a07335575e453c75b8f219f2c7b03ac7ba7080
yh20studio(tiki) <yh20studio@gmail.com>
2022-02-28 18:43:27 +0900

    refactor: application 코드 가독성 높임

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.dto.LottoResultDto;
+import lotto.dto.LottoTicketsDto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -8,8 +10,12 @@ public class Application {
 
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        OutputView.displayLottoTickets(lottoController.buyLottoTickets(InputView.requestMoney()));
-        OutputView.displayLottoResult(lottoController.matchLottoTickets(InputView.requestWinningNumbers(),
-                InputView.requestBonusNumber()));
+
+        LottoTicketsDto lottoTicketsDto = lottoController.buyLottoTickets(InputView.requestMoney());
+        OutputView.displayLottoTickets(lottoTicketsDto);
+
+        LottoResultDto lottoResultDto = lottoController.matchLottoTickets(InputView.requestWinningNumbers(),
+                InputView.requestBonusNumber());
+        OutputView.displayLottoResult(lottoResultDto);
     }
 }
