dc439b1cb630ffccb5dac5c2c7b43d244825679b
Hyunta <mohai2618@gmail.com>
2022-02-28 16:17:04 +0900

    refactor(LottoRankTest): 하드코딩한 예상값을 @MethodSource를 이용해 검증

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -1,18 +1,31 @@
 package lotto.domain;
 
+import static lotto.domain.LottoRank.*;
+
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoRankTest {
 
     @ParameterizedTest
     @DisplayName("등수를 올바르게 결정하는지 확인")
-    @CsvSource(value = {"6:false:FIRST", "5:true:SECOND", "5:false:THIRD", "4:false:FOURTH",
-            "4:true:FOURTH", "3:true:FIFTH", "3:false:FIFTH"}, delimiter = ':')
-    void valueOf(int sameCount, boolean bonus, LottoRank expectedLottoRank) {
+    @MethodSource("rankParameterProvider")
+    void Get_Proper_Rank_For_Condition(int sameCount, boolean bonus, LottoRank expectedLottoRank) {
         LottoRank lottoRank = LottoRank.valueOf(sameCount, bonus);
         Assertions.assertThat(lottoRank).isEqualTo(expectedLottoRank);
+    }
+
+    private static Stream<Arguments> rankParameterProvider() {
+        return Stream.of(
+                Arguments.arguments(6, false, FIRST),
+                Arguments.arguments(5, true, SECOND),
+                Arguments.arguments(5, false, THIRD),
+                Arguments.arguments(4, false, FOURTH),
+                Arguments.arguments(3, false, FIFTH)
+        );
     }
 }
