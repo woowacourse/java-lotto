3c1287e317e0b720ff00b725c37d17b88a57be7d
koo <gudonghee2000@gmail.com>
2022-02-28 15:03:14 +0900

    feat: Money 클래스에 투입금액을 검증하는 기능 생성

--- src/main/java/model/money/Money.java
+++ src/main/java/model/money/Money.java
@@ -2,11 +2,27 @@ package model.money;
 
 public class Money {
     private static final int LOTTO_PRICE = 1000;
+    private static final String INPUT_MONEY_UNIT_ERROR_MESSAGE = "[ERROR] 투입 금액은 천원 단위의 금액으로 입력하세요.";
 
     private final int money;
 
     public Money(final int money) {
-        this.money = money;
+        this.money = checkValidMoney(money);
+    }
+
+    private int checkValidMoney(int money) {
+        if (isZeroOrLess(money) || isNotThousandUnits(money)) {
+            throw new IllegalArgumentException(INPUT_MONEY_UNIT_ERROR_MESSAGE);
+        }
+        return money;
+    }
+
+    private boolean isZeroOrLess(int money) {
+        return money <= 0;
+    }
+
+    private boolean isNotThousandUnits(int money) {
+        return money % LOTTO_PRICE != 0;
     }
 
     public int generatePurchaseCount() {
