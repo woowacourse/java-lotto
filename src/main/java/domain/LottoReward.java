b6d16eec63d813f4caf49bbfabeb9b5ccbc4c6ab
sure-why-not <pkse1213@gmail.com>
2022-03-01 02:09:59 +0900

    refactor : LottoReward 가 보너스 일치 정보 (hasBonus) 를 소유하고 관리하도록 변경

--- src/main/java/domain/LottoReward.java
+++ src/main/java/domain/LottoReward.java
@@ -4,25 +4,26 @@ import java.util.Arrays;
 
 public enum LottoReward {
 
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
-    NONE(0, 0);
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
 
     private final int matchCount;
+    private final boolean hasBonus;
     private final int price;
 
-    LottoReward(int matchCount, int price) {
+    LottoReward(int matchCount, boolean hasBonus, int price) {
         this.matchCount = matchCount;
+        this.hasBonus = hasBonus;
         this.price = price;
     }
 
     public static LottoReward find(int matchCount, boolean hasBonus) {
         return Arrays.stream(values())
-            .filter(reward -> reward.matchCount == matchCount)
-            .filter(reward -> !reward.equals(SECOND) || hasBonus)
+            .filter(reward -> reward.matchCount == matchCount && reward.hasBonus == hasBonus)
             .findFirst()
             .orElse(NONE);
     }
