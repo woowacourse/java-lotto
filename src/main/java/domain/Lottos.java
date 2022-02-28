bec556093d31342b8f02b952b0c96cabdc3fbd66
Wishoon <heonga2@gmail.com>
2022-03-01 00:22:30 +0900

    fix : 사용하지 않는 생성자 제거

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -4,22 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import domain.strategy.LottoGeneratorStrategy;
-
 public class Lottos {
 
     private final List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
-    }
-
-    public Lottos(int lottoCount, LottoGeneratorStrategy lottoGeneratorStrategy) {
-        lottos = new ArrayList<>();
-        for (int i = 0; i < lottoCount; i++) {
-            List<LottoNumber> lottoNumbers = lottoGeneratorStrategy.generate();
-            lottos.add(new Lotto(lottoNumbers));
-        }
     }
 
     public List<LottoReward> calculateLottoReward(WinningLotto winningLotto) {
