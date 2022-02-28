4b9d1c227699503888b26d4e4c6b65ef67c89d3b
jayjaehunchoi <wogns0108@naver.com>
2022-02-28 14:37:12 +0900

    refactor : public 으로 선언된 상수 private 으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,9 +8,9 @@ import java.util.stream.IntStream;
 
 public class InputView {
 
-    public static final String INPUT_MANUAL_LOTTO_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String INPUT_PURCHASE_MONEY = "구매금액을 입력해 주세요.";
     private static final String INPUT_PURCHASE_MANUAL_COUNT = "수동으로 구매할 로또 수를 입력해주세요.";
+    private static final String INPUT_MANUAL_LOTTO_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String INPUT_WIN_LOTTO_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_LOTTO_NUMBER = "보너스 볼을 입력해 주세요.";
     private static final String INPUT_WIN_LOTTO_NUMBERS_DELIMITER = ",";
