786b448c6f5fe28612458d74c38cf92efe0b9c92
SeungCheol <ssc6839@gmail.com>
2022-02-28 18:13:22 +0900

    refactor: 중복되는 로직들 반복 테스트로 변경

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -4,53 +4,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LottoTest {
 
     private final WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
 
-    @Test
-    @DisplayName("로또 1등 당첨인 경우")
-    void firstRankTest() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        assertThat(lotto.calculateRank(winningLotto)).isEqualTo(Rank.FIRST);
-    }
+    @ParameterizedTest
+    @DisplayName("로또 1등부터 미당첨인 경우까지 모두 테스트")
+    @CsvSource(value = {
+        "1, 2, 3, 4, 5, 6:FIRST",
+        "1, 2, 3, 4, 5, 7:SECOND",
+        "1, 2, 3, 4, 5, 8:THIRD",
+        "1, 2, 3, 4, 8, 9:FOURTH",
+        "1, 2, 3, 8, 9, 10:FIFTH",
+        "1, 2, 8, 9, 10, 11:LOSER"
+    }, delimiter = ':')
+    void rankTest(String input, String rank) {
+        List<Integer> list = Arrays.stream(input.split(", "))
+            .mapToInt(Integer::parseInt)
+            .boxed()
+            .collect(Collectors.toList());
+        Lotto lotto = new Lotto(list);
 
-    @Test
-    @DisplayName("로또 2등 당첨인 경우")
-    void secondRankTest() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
-        assertThat(lotto.calculateRank(winningLotto)).isEqualTo(Rank.SECOND);
-    }
-
-    @Test
-    @DisplayName("로또 3등 당첨인 경우")
-    void thirdRankTest() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
-        assertThat(lotto.calculateRank(winningLotto)).isEqualTo(Rank.THIRD);
-    }
-
-    @Test
-    @DisplayName("로또 4등 당첨인 경우")
-    void fourthRankTest() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 8, 9));
-        assertThat(lotto.calculateRank(winningLotto)).isEqualTo(Rank.FOURTH);
-    }
-
-    @Test
-    @DisplayName("로또 5등 당첨인 경우")
-    void fifthRankTest() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
-        assertThat(lotto.calculateRank(winningLotto)).isEqualTo(Rank.FIFTH);
-    }
-
-    @Test
-    @DisplayName("로또 미당첨인 경우")
-    void noRankTest() {
-        Lotto lotto = new Lotto(List.of(1, 2, 8, 9, 10, 11));
-        assertThat(lotto.calculateRank(winningLotto)).isEqualTo(Rank.LOSER);
+        assertThat(lotto.calculateRank(winningLotto)).isEqualTo(Rank.valueOf(rank));
     }
 }
