695679cdb05177dfb308271dfd1dbabb2f5bfecf
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-02-28 21:01:57 +0900

    refactor: reflection, ParameterizedTest 도입

--- src/test/java/lotto/view/ResultViewTest.java
+++ src/test/java/lotto/view/ResultViewTest.java
@@ -4,12 +4,16 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import lotto.model.Lotto;
 import lotto.model.LottoMoney;
@@ -42,10 +46,15 @@ class ResultViewTest {
 
     @Test
     @DisplayName("당첨 통계 출력 확인")
-    void printResultStatisticsTest() {
+    void printResultStatisticsTest() throws
+        NoSuchMethodException,
+        InvocationTargetException,
+        InstantiationException,
+        IllegalAccessException {
         Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        LottoNumber bonusNumber = new LottoNumber(7);
-
+        Constructor<LottoNumber> lottoNumberConstructor = LottoNumber.class.getDeclaredConstructor(Integer.class);
+        lottoNumberConstructor.setAccessible(true);
+        LottoNumber bonusNumber = lottoNumberConstructor.newInstance(7);
         ResultView.printResultStatistics(new LottoResult(lottos, winningNumbers, bonusNumber));
 
         assertThat(outputStreamCaptor.toString())
@@ -54,28 +63,25 @@ class ResultViewTest {
             .contains("4개 일치 (50000원)- 0개");
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"5000:총 수익률은 0.36입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)",
+        "14000:총 수익률은 1.00입니다.(기준이 1이기 때문에 결과적으로 이득이라는 의미임)"}, delimiter = ':')
     @DisplayName("손해인 경우 수익률을 출력한다.")
-    void printMinusYieldTest() {
+    void printMinusYieldTest(Long totalWinningMoney, String expectedMessage) throws
+        NoSuchMethodException,
+        InvocationTargetException,
+        InstantiationException,
+        IllegalAccessException {
         LottoMoney lottoMoney = new LottoMoney(14000);
-        Long totalWinningMoney = 5000L;
-        Yield yield = new Yield(lottoMoney, totalWinningMoney);
+
+        Constructor<Yield> yieldConstructor = Yield.class.getDeclaredConstructor(LottoMoney.class, Long.class);
+        yieldConstructor.setAccessible(true);
+        Yield yield = yieldConstructor.newInstance(lottoMoney, totalWinningMoney);
+
         ResultView.printYield(yield);
 
         assertThat(outputStreamCaptor.toString())
-            .contains("총 수익률은 0.36입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
-    }
-
-    @Test
-    @DisplayName("이득인 경우 수익률을 출력한다.")
-    void printPlusYieldTest() {
-        LottoMoney lottoMoney = new LottoMoney(14000);
-        Long totalWinningMoney = 14000L;
-        Yield yield = new Yield(lottoMoney, totalWinningMoney);
-        ResultView.printYield(yield);
-
-        assertThat(outputStreamCaptor.toString())
-            .contains("총 수익률은 1.00입니다.(기준이 1이기 때문에 결과적으로 이득이라는 의미임)");
+            .contains(expectedMessage);
     }
 
     @AfterEach
