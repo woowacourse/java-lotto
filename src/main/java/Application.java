4b2f39c79994a153815f8b5eeb800dcbf57e96ce
Wishoon <heonga2@gmail.com>
2022-02-28 21:28:18 +0900

    refactor : Application 전체 구조 리팩토링

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,3 @@
-import java.util.List;
-import java.util.stream.Collectors;
-
 import domain.Lotto;
 import domain.LottoFactory;
 import domain.LottoNumber;
@@ -16,28 +13,25 @@ import view.OutputView;
 public class Application {
 
     public static void main(String[] args) {
-        final LottoMoney lottoMoney = new LottoMoney(InputView.getPurchaseMoney());
+        final LottoMoney lottoMoney = createLottoMoney();
         final LottoPurchaseCount lottoPurchaseCount = createLottoPurchaseCount(lottoMoney);
+
         final Lottos lottos = createLottos(lottoPurchaseCount);
         OutputView.showPurchasedLottos(lottos.getLottos());
 
         final WinningLotto winningLotto = createWinningLotto();
-        final WinningStatistics winningStatistics = new WinningStatistics(lottos.calculateLottoReward(winningLotto));
+        final WinningStatistics winningStatistics = createWinningStatistics(lottos, winningLotto);
         OutputView.showWinningStatistics(winningStatistics.getWinningStatistics());
         OutputView.showProfitRate(winningStatistics.calculateProfitRate());
     }
 
-    private static WinningLotto createWinningLotto() {
-        final List<Integer> winningNumbers = InputView.getWinningLottoNumbers();
-        List<LottoNumber> winningLottoNumbers = winningNumbers.stream()
-            .map(LottoNumber::valueOf)
-            .collect(Collectors.toList());
-        Lotto winningLotto = new Lotto(winningLottoNumbers);
-
-        final int bonusNumber = InputView.getBonusBall();
-        LottoNumber bonusLottoNumber = LottoNumber.valueOf(bonusNumber);
-
-        return new WinningLotto(winningLotto, bonusLottoNumber);
+    private static LottoMoney createLottoMoney() {
+        try {
+            return new LottoMoney(InputView.getPurchaseMoney());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createLottoMoney();
+        }
     }
 
     private static LottoPurchaseCount createLottoPurchaseCount(LottoMoney lottoMoney) {
@@ -59,5 +53,31 @@ public class Application {
             System.out.println(e.getMessage());
             return createLottos(lottoPurchaseCount);
         }
+    }
+
+    private static WinningLotto createWinningLotto() {
+        return new WinningLotto(createWinningLottoBall(), createBonusBall());
+    }
+
+    private static Lotto createWinningLottoBall() {
+        try {
+            return LottoFactory.createLotto(InputView.getWinningLottoNumbers());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createWinningLottoBall();
+        }
+    }
+
+    private static LottoNumber createBonusBall() {
+        try {
+            return LottoNumber.valueOf(InputView.getBonusBall());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createBonusBall();
+        }
+    }
+
+    private static WinningStatistics createWinningStatistics(Lottos lottos, WinningLotto winningLotto) {
+        return new WinningStatistics(lottos.calculateLottoReward(winningLotto));
     }
 }
