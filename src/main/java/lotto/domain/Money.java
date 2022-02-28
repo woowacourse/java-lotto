23b2777f8b6e2bfb2afdbc3ce6c60ba8803a5566
nailseong <nnis9857@gmail.com>
2022-02-28 20:06:22 +0900

    feat: 수동으로 구매할 로또 수를 가지고 자동으로 생성할 로또 수를 반환하는 기능

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -9,6 +9,8 @@ public class Money {
     private static final double DECIMAL_PLACE = 1000.0;
     private static final String VALUE_RANGE_ERROR_MESSAGE = "1000부터 20억의 숫자여야 합니다.";
     private static final String VALUE_DIVIDED_BY_ERROR_MESSAGE = "1000으로 나누어 떨어져야 합니다.";
+    private static final String QUANTITY_OF_MANUAL_PREFIX = "0개 이상 ";
+    private static final String QUANTITY_OF_MANUAL_SUFFIX = "개 이하의 로또만 수동으로 구매할 수 있습니다.";
 
     private final int value;
 
@@ -31,6 +33,18 @@ public class Money {
     private void validateDividedBy(int number) {
         if (number % DIVIDED_STANDARD != REMAINDER) {
             throw new IllegalArgumentException(VALUE_DIVIDED_BY_ERROR_MESSAGE);
+        }
+    }
+
+    public int getQuantityOfAuto(int quantityOfManual, int price) {
+        final int maxQuantity = getAvailableQuantity(price);
+        validateQuantityOfManual(maxQuantity, quantityOfManual);
+        return maxQuantity - quantityOfManual;
+    }
+
+    private void validateQuantityOfManual(int maxQuantity, int quantityOfManual) {
+        if (quantityOfManual < 0 || quantityOfManual > maxQuantity) {
+            throw new IllegalArgumentException(QUANTITY_OF_MANUAL_PREFIX + maxQuantity + QUANTITY_OF_MANUAL_SUFFIX);
         }
     }
 
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoneyTest {
@@ -38,6 +40,42 @@ public class MoneyTest {
                 assertThatThrownBy(() -> new Money(value))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("1000으로 나누어 떨어져야 합니다.");
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("자동으로 생성할 로또 수를 반환하는 메서드는")
+    class CalculateAmountOfAuto {
+
+        @Nested
+        @DisplayName("수동으로 구매할 로또 수와 로또의 가격이 주어지면")
+        class Context_with_quantity_of_manual_and_lotto_price {
+
+            @ParameterizedTest
+            @CsvSource(value = {"0|10", "3|7", "10|0"}, delimiter = '|')
+            @DisplayName("값을 반환한다.")
+            void It_returns_amount_of_auto(int value, int expected) {
+                final Money money = new Money(10000);
+
+                final int quantityOfAuto = money.getQuantityOfAuto(value, 1000);
+                assertThat(quantityOfAuto).isEqualTo(expected);
+            }
+        }
+
+        @Nested
+        @DisplayName("수동으로 구매할 로또 수가 유효하지 않으면")
+        class Context_with_not_available_amount_of_manual {
+
+            @ParameterizedTest
+            @CsvSource(value = {"-1|10000", "11|10000", "21|20000"}, delimiter = '|')
+            @DisplayName("예외를 던진다.")
+            void It_throws_exception(int quantityOfManual, int moneyValue) {
+                final Money money = new Money(moneyValue);
+
+                assertThatThrownBy(() -> money.getQuantityOfAuto(quantityOfManual, 1000))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("0개 이상 " + moneyValue / 1000 + "개 이하의 로또만 수동으로 구매할 수 있습니다.");
             }
         }
     }
