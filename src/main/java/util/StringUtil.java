215ad622e4074b6f52c434c47aab264ad2a6d2f9
hyunrrr <whgusrms96@gmail.com>
2022-02-28 17:26:37 +0900

    feat: static 메서드만 있는 객체 생성 불가하도록 private 생성자 추가

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -4,6 +4,8 @@ public class StringUtil {
 
     private static final String NONE_NUMERIC_ERROR = "[ERROR] 숫자로 변환하려는 String에 숫자가 아닌 문자가 있습니다.";
 
+    private StringUtil() {}
+
     public static int convertToInteger(String input) {
         try {
             return Integer.parseInt(input);
