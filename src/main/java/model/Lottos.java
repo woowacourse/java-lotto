9323ad5412c884621210361365336d2ce5e23915
koo <gudonghee2000@gmail.com>
2022-02-28 15:40:56 +0900

    refactor: Lottos 클래스의 함수명 변경 및 함수구조 변경

--- src/main/java/model/Lottos.java
+++ src/main/java/model/Lottos.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,17 +9,17 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import model.lottonumbergenerator.Generator;
-import model.lottotickets.LottoTicket;
+import model.lottotickets.Lotto;
 import model.winning.Rank;
 import model.winning.Statistics;
 
 public class Lottos {
-    private final List<LottoTicket> lottoTickets;
+    private final List<Lotto> lottos;
 
     public Lottos(final int purchaseCount, final Generator generator) {
-        lottoTickets = IntStream
+        lottos = IntStream
                 .rangeClosed(1, purchaseCount)
-                .mapToObj(index -> new LottoTicket(generator))
+                .mapToObj(index -> new Lotto(generator))
                 .collect(Collectors.toList());
     }
 
@@ -26,16 +27,14 @@ public class Lottos {
         Map<Rank, Integer> result = new LinkedHashMap<>();
         Arrays.stream(Rank.values()).forEach(rank -> result.put(rank, 0));
 
-        for (LottoTicket lottoTicket : lottoTickets) {
-            Rank key = lottoTicket.selectRank(winningNumbers, bonusNumber);
+        for (Lotto lotto : lottos) {
+            Rank key = lotto.compareWithWinningNumber(winningNumbers, bonusNumber);
             result.put(key, result.get(key) + 1);
         }
         return new Statistics(result);
     }
 
-    public List<List<Integer>> tickets() {
-        return lottoTickets.stream()
-                .map(LottoTicket::lottoNumbers)
-                .collect(Collectors.toList());
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
     }
 }
