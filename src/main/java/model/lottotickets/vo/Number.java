9543fc5739805feebc7f36099ebb2e5d4b8b907f
koo <gudonghee2000@gmail.com>
2022-02-28 16:16:00 +0900

    refactor: Number클래스의 매직넘버 상수화, 함수명 변경

--- src/main/java/model/lottotickets/vo/Number.java
+++ src/main/java/model/lottotickets/vo/Number.java
@@ -5,30 +5,31 @@ import java.util.List;
 public class Number {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
+    private static final String NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1~45까지의 숫자로 입력하세요.";
 
     private final int number;
 
     public Number(final int number) {
-        this.number = checkNumber(number);
+        this.number = checkNumberInLottoRange(number);
     }
 
-    private int checkNumber(final int number) {
-        if (isNotCorrectNumberRange(number)) {
-            throw new IllegalArgumentException();
+    private int checkNumberInLottoRange(final int number) {
+        if (isNumberNotInLottoRange(number)) {
+            throw new IllegalArgumentException(NUMBER_RANGE_ERROR_MESSAGE);
         }
         return number;
     }
 
-    private boolean isNotCorrectNumberRange(final int number) {
+    private boolean isNumberNotInLottoRange(final int number) {
         return number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER;
     }
 
-    public boolean contain(final List<Integer> otherNumbers) {
-        return otherNumbers.contains(number);
+    public boolean contain(final List<Integer> WinningNumbers) {
+        return WinningNumbers.contains(number);
     }
 
-    public boolean contain(final int otherNumber) {
-        return number == otherNumber;
+    public boolean contain(final int bonusNumber) {
+        return number == bonusNumber;
     }
 
     public int getNumber() {
