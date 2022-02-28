c04e900e8935d3ec99e556e4a88d342580eb9a0a
soominsohn <soominsohn@gmail.com>
2022-02-28 22:33:54 +0900

    test(lottos): add lottos test

--- src/test/java/LottosTest.java
+++ src/test/java/LottosTest.java
@@ -11,5 +11,18 @@ import org.junit.jupiter.api.Test;
 
 public class LottosTest {
 
+    @Test
+    @DisplayName("로또 장수 확인")
+    void check_lottos_size() {
+
+        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(Arrays.asList(4, 5, 6, 7, 8, 9));
+        Lotto lotto3 = new Lotto(Arrays.asList(11, 12, 13, 14, 15, 16));
+
+        Lottos lottos = new Lottos(List.of(lotto1, lotto2, lotto3));
+
+        assertThat(lottos.getSize()).isEqualTo(3);
+    }
+
 
 }
