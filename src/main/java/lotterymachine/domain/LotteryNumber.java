0d060235769453da50e87519ff773658ca3bc2d1
jochungbeom <cndqjacndqja@gmail.com>
2022-02-28 16:30:38 +0900

    feat(LotteryNumberTest): 생성 테스트 추가

--- src/main/java/lotterymachine/domain/LotteryNumber.java
+++ src/main/java/lotterymachine/domain/LotteryNumber.java
@@ -2,7 +2,7 @@ package lotterymachine.domain;
 
 import java.util.Objects;
 
-public class LotteryNumber implements Comparable<LotteryNumber>{
+public class LotteryNumber implements Comparable<LotteryNumber> {
     private static final int MINIMUM_LOTTERY_NUMBER = 1;
     private static final int MAXIMUM_LOTTERY_NUMBER = 45;
     private static final String OUT_OF_RANGE = "로또 번호는 1~45 사이의 값이어야 합니다.";
@@ -13,7 +13,6 @@ public class LotteryNumber implements Comparable<LotteryNumber>{
         validateNumber(number);
         this.number = number;
     }
-
 
     public int getNumber() {
         return number;
--- src/test/java/lotterymachine/domain/LotteryNumberTest.java
+++ src/test/java/lotterymachine/domain/LotteryNumberTest.java
@@ -3,20 +3,30 @@ package lotterymachine.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LotteryNumberTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
     @DisplayName("1~45 범위에 벗어난 숫자를 입력 시, 에러 발생")
-    void validateNumber() {
+    void validateNumber(int input) {
         Assertions.assertThatThrownBy(() -> {
-            LotteryNumber lotteryNumber = new LotteryNumber(0);
+            LotteryNumber lotteryNumber = new LotteryNumber(input);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 번호는 1~45 사이의 값이어야 합니다.");
     }
 
-
-
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 45})
+    @DisplayName("1~45 범위의 숫자를 입력 받아 생성한다.")
+    void create(int input) {
+        LotteryNumber lotteryNumber = new LotteryNumber(input);
+        assertThat(lotteryNumber.getNumber()).isEqualTo(input);
+    }
 }
