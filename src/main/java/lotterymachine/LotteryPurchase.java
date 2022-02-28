b4ca1513f66bc9b2a1fbf5904d8b6904967e117b
jochungbeom <cndqjacndqja@gmail.com>
2022-02-28 16:56:55 +0900

    refactor(LotteryPurchase): 상수 매직넘버로 수정

--- src/main/java/lotterymachine/LotteryPurchase.java
+++ src/main/java/lotterymachine/LotteryPurchase.java
@@ -2,6 +2,7 @@ package lotterymachine;
 
 public class LotteryPurchase {
     private static final int PER_LOTTERY_TICKET_PRICE = 1000;
+    private static final String TERMS_OF_PURCHASE = "로또 구매는 기본 1000원 이상부터 할 수 있습니다.";
 
     private int number;
 
@@ -24,7 +25,7 @@ public class LotteryPurchase {
 
     private void validateNumber(int number) {
         if (number < PER_LOTTERY_TICKET_PRICE) {
-            throw new IllegalArgumentException("로또 구매는 기본 1000원 이상부터 할 수 있습니다.");
+            throw new IllegalArgumentException(TERMS_OF_PURCHASE);
         }
     }
 }
