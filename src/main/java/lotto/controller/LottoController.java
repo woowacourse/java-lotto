b5e870620e8077d3d3b4fff6ad4c78d55ab51e72
nailseong <nnis9857@gmail.com>
2022-02-28 22:59:39 +0900

    feat: 수동으로 구매하려는 유효한 로또 수를 입력 받는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,11 @@
 package lotto.controller;
 
+import java.util.Optional;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.domain.Result;
 import lotto.domain.WinningNumbers;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
@@ -17,5 +19,26 @@ public class LottoController {
     public void play(Lottos lottos, WinningNumbers winningNumbers, Money money) {
         final Result result = lottos.getResult(winningNumbers);
         OutputView.printPlayResult(result, money);
+    }
+
+    private int inputQuantityOfManual(Money money) {
+        Optional<Integer> quantityOfManual;
+
+        do {
+            quantityOfManual = getValidQuantityOfManual(money);
+        } while (quantityOfManual.isEmpty());
+        return quantityOfManual.get();
+    }
+
+    private Optional<Integer> getValidQuantityOfManual(Money money) {
+        try {
+            final int quantityOfManual = InputView.inputManualCount();
+            money.validateQuantityOfManual(quantityOfManual);
+
+            return Optional.of(quantityOfManual);
+        } catch (IllegalArgumentException exception) {
+            OutputView.printError(exception.getMessage());
+            return Optional.empty();
+        }
     }
 }
