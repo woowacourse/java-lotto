e3fb6ff2a51978b8b330fc7ac09fae3142b7911c
이한울 <dkfghg3@naver.com>
2022-02-28 18:23:23 +0900

    refactor: 숫자 범위를 벗어난 경우 경계값으로 테스트 진행

--- src/test/java/domain/NumberTest.java
+++ src/test/java/domain/NumberTest.java
@@ -3,14 +3,16 @@ package domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberTest {
 
-    @Test
-    @DisplayName("번호의 범위를 벗어났을 때 예외를 발생시킨다")
-    void checkNumberRange() {
-        assertThatThrownBy(() -> new Number(100))
+    @ParameterizedTest
+    @DisplayName("1번 ~ 45번까지의 범위를 벗어난 경우 예외를 발생시킨다")
+    @ValueSource(ints = {-1, 0, 46})
+    void checkNumberRange(int number) {
+        assertThatThrownBy(() -> new Number(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("번호는 1 ~ 45의 숫자여야 합니다");
     }
