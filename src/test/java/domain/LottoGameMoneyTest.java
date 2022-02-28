b5472c34bbe166e81ee8d10ee76d8619e31f7fe7
sure-why-not <pkse1213@gmail.com>
2022-03-01 01:27:52 +0900

    refactor : LottoGameMoneyTest 테스트들을 경계값 위주로 테스트하도록 수정

--- src/test/java/domain/LottoGameMoneyTest.java
+++ src/test/java/domain/LottoGameMoneyTest.java
@@ -7,18 +7,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class MoneyTest {
+public class LottoGameMoneyTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 14000})
     @DisplayName("로또 게임 머니를 생성하는 기능")
-    void createMoney() {
-        LottoGameMoney money = new LottoGameMoney(1000);
+    void createMoney(int amount) {
+        LottoGameMoney money = new LottoGameMoney(amount);
 
         assertThat(money).isNotNull();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-100, 0, 8800})
+    @ValueSource(ints = {-1, 0, 990, 1500})
     @DisplayName("로또 게임 머니를 생성하지 못하는 경우")
     void createInvalidMoney(int amount) {
         assertThatThrownBy(() -> new LottoGameMoney(amount))
