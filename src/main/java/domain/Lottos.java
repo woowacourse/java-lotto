185f100ee8f76d4d081a64d28b09962443c18568
Richard JEON <ztzy1907@gmail.com>
2022-03-01 23:37:28 +0900

    refactor(lottos): Lottos 생성자 내 unmodifiableList() -> List.copyOf 변경

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -1,6 +1,5 @@
 package domain;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -10,10 +9,11 @@ public class Lottos {
 
     public Lottos(final List<Lotto> lottos) {
         Objects.requireNonNull(lottos, ERROR_MESSAGE_FOR_NULL_OR_EMPTY_LIST);
-        final List<Lotto> unmodifiableLottos = Collections.unmodifiableList(lottos);
+
+        final List<Lotto> copiedLottos = List.copyOf(lottos);
         validateEmpty(lottos);
 
-        this.lottos = unmodifiableLottos;
+        this.lottos = copiedLottos;
     }
 
     private void validateEmpty(List<Lotto> lottos) {
