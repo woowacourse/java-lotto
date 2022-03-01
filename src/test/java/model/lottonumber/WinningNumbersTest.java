42bef190ffdf0369597589fe73b84fad40155b47
koo <gudonghee2000@gmail.com>
2022-03-01 14:22:56 +0900

    test: WinningNumbers클래스의 당첨번호 입력개수를 검증하는 테스트 코드 작성

---
+++ src/test/java/model/lottonumber/WinningNumbersTest.java
@@ -0,0 +1,34 @@
+package model.lottonumber;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumbersTest {
+
+    @Test
+    @DisplayName("당첨 번호가 6개보다 적게 입력되면 오류를 발생한다.")
+    void generateWinningNumbers_underThanSix() {
+        List<Integer> numbers = Arrays.asList(1, 2, 4, 6);
+        int bonusNumber = 7;
+
+        assertThatThrownBy(() -> new WinningNumbers(numbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력한 당첨 번호가 6개가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("당첨 번호가 6개보다 많이 입력되면 오류를 발생한다.")
+    void generateWinningNumbers_overThanSix() {
+        List<Integer> numbers = Arrays.asList(1, 2, 4, 6, 7, 9, 16);
+        int bonusNumber = 21;
+
+        assertThatThrownBy(() -> new WinningNumbers(numbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력한 당첨 번호가 6개가 아닙니다.");
+    }
+}
