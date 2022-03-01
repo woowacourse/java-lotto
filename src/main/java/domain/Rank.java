17519142c9e3ef0853384a7384a68c51103a02af
EastHShin <xldk78@gmail.com>
2022-03-01 17:21:13 +0900

    refactor(domain/Rank): 개행으로 가독성 증진

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -5,8 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum Rank {
-	FIRST(6, 2_000_000_000), SECOND(5, 30_000_000), THIRD(5, 1_500_000),
-	FOURTH(4, 50_000), FIFTH(3, 5_000), NONE(0, 0);
+	FIRST(6, 2_000_000_000),
+	SECOND(5, 30_000_000),
+	THIRD(5, 1_500_000),
+	FOURTH(4, 50_000),
+	FIFTH(3, 5_000),
+	NONE(0, 0);
 
 	private static final int SECOND_AND_THIRD_BALL = 5;
 
