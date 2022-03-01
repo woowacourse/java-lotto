2dd2b257ca820f5a029df5bda36f125804c0284a
Ohzzi <clover7even@naver.com>
2022-03-01 17:15:51 +0900

    test: 로또 주문 테스트 추가

--- src/test/java/model/LottoOrderTest.java
+++ src/test/java/model/LottoOrderTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -17,5 +18,21 @@ class LottoOrderTest {
         assertThatThrownBy(() -> new LottoOrder(purchasingMoney, manualCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(LottoOrder.TOO_MANY_MANUAL);
+    }
+
+    @Test
+    @DisplayName("수동 구매 개수와 자동 구매 개수를 합치면 전체 구매 가능 개수가 된다.")
+    void validateTotalCount() {
+        // given
+        LottoPurchasingMoney purchasingMoney = LottoPurchasingMoney.valueOf(8000);
+        int manualCount = 5;
+        int expected = 8;
+
+        // when
+        LottoOrder lottoOrder = new LottoOrder(purchasingMoney, manualCount);
+        int actual = lottoOrder.getAutoLottoCount() + lottoOrder.getManualLottoCount();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
