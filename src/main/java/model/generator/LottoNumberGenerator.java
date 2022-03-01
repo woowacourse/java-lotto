1c526c12c3c9f7abdff802b56378dcb579813d82
koo <gudonghee2000@gmail.com>
2022-03-01 22:06:53 +0900

    refactor: LottoNumberGenerator클래스 상수 접근지정자 변경

--- src/main/java/model/generator/LottoNumberGenerator.java
+++ src/main/java/model/generator/LottoNumberGenerator.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 public class LottoNumberGenerator implements Generator {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
-    public static final int MAX_SIZE = 6;
+    private static final int MAX_SIZE = 6;
 
     @Override
     public List<Integer> generateNumbers() {
