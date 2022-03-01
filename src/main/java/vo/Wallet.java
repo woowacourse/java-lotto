82e66663fa8a22d2d33a6f59548a3f87e44590a0
Richard JEON <ztzy1907@gmail.com>
2022-03-02 05:59:26 +0900

    refactor(wallet): 잔액이 0인지 확인하는 isEmpty 메서드 추가

--- src/main/java/vo/Wallet.java
+++ src/main/java/vo/Wallet.java
@@ -4,9 +4,11 @@ import static domain.Lotto.LOTTO_PRICE;
 
 public class Wallet {
     private static final int EXPECTED_REMAIN = 0;
+    private static final int ZERO_FOR_EMPTY_WALLET = 0;
     private static final int MINIMUM_MANUAL_LOTTO_QUANTITY_TO_BUY = 1;
-    private static final String ERROR_MESSAGE_FOR_BUYING_LESS_THEN_ONE = "구매 수량은 1장 이상이어야 합니다";
     private static final String ERROR_MESSAGE_FOR_INVALID_INPUT_MONEY = "금액은 1000원 이상, 1000원 단위로 입력해주세요";
+    private static final String ERROR_MESSAGE_FOR_BUYING_LESS_THEN_ONE = "구매 수량은 1장 이상이어야 합니다";
+    private static final String ERROR_MESSAGE_FOR_NOT_ENOUGH_MONEY = " 이상 구매할 수 없습니다 : ";
 
     private int currentBalance;
     private int manualQuantity;
@@ -40,13 +42,18 @@ public class Wallet {
         }
 
         if (this.currentBalance < LOTTO_PRICE * quantity) {
-            throw new IllegalArgumentException(this.currentBalance / LOTTO_PRICE + " 이상 구매할 수 없습니다 : " + quantity);
+            throw new IllegalArgumentException(
+                    this.currentBalance / LOTTO_PRICE + ERROR_MESSAGE_FOR_NOT_ENOUGH_MONEY + quantity);
         }
     }
 
     public void buyAutoLottoWithCurrentBalance() {
         this.autoQuantity = this.currentBalance / LOTTO_PRICE;
         this.currentBalance -= this.autoQuantity * LOTTO_PRICE;
+    }
+
+    public boolean isEmpty() {
+        return this.currentBalance == ZERO_FOR_EMPTY_WALLET;
     }
 
     public int getCurrentBalance() {
