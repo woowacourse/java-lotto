a8bbe95f3342e41ebcb69aa38150ebfcec04c2ac
soominsohn <soominsohn@gmail.com>
2022-03-01 19:08:31 +0900

    refactor(lotto): add exception while generate lotto numbers

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,17 +1,57 @@
 package domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private final int MINIMUM_LOTTO_NUMBER = 1;
+    private final int MAXIMUM_LOTTO_NUMBER = 45;
+    private final int LOTTO_SIZE = 6;
+
+    private final String LOTTO_NUMBER_RANGE_ERROR = "[ERROR] 로또 번호는 1~45 사이 정수만 가능합니다.";
+    private final String LOTTO_SIZE_ERROR = "[ERROR] 로또 번호는 6개만 입력 가능합니다.";
+    private final String LOTTO_NUMBER_NOT_UNIQUE_ERROR = "[ERROR] 로또 번호는 중복될 수 없습니다.";
 
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        checkValidate(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
     public List<Integer> getLottoNumbers() {
         return this.lottoNumbers;
+    }
+
+    private void checkValidate(List<Integer> numbers) {
+        checkRange(numbers);
+        checkSize(numbers);
+        checkUnique(numbers);
+
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        numbers.stream()
+            .filter(number -> number < MINIMUM_LOTTO_NUMBER || number > MAXIMUM_LOTTO_NUMBER)
+            .findAny()
+            .ifPresent(m -> {
+                throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR);
+            });
+    }
+
+    private void checkSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(LOTTO_SIZE_ERROR);
+        }
+    }
+
+    private void checkUnique(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numbers.size() != numberSet.size()) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_NOT_UNIQUE_ERROR);
+        }
     }
 
 }
--- src/test/java/LottoTest.java
+++ src/test/java/LottoTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.Lotto;
 import java.util.List;
@@ -8,12 +9,39 @@ import org.junit.jupiter.api.Test;
 public class LottoTest {
 
     @Test
-    @DisplayName("로또 생성 확인")
-    void lotto_generate() {
+    @DisplayName("로또 생성 성공")
+    void lotto_generate_success() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
         assertThat(lotto.getLottoNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
 
     }
+
+    @Test
+    @DisplayName("로또 숫자 1~45 사이 아닐 시 예외 발생")
+    void check_range_fail() {
+
+        assertThatThrownBy(
+            () -> new Lotto(List.of(1, 2, 3, 4, 5, 46))
+        ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    @DisplayName("로또 숫자가 7개 이상일 시 예외 발생")
+    void check_size_fail() {
+        assertThatThrownBy(
+            () -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7))
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호에 중복이 있을 시 예외 발생")
+    void check_unique_fail() {
+        assertThatThrownBy(
+            () -> new Lotto(List.of(1, 2, 3, 4, 5, 5))
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
