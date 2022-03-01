085f8918a9cf4301819f327f7c531719aaea3dc2
Richard JEON <ztzy1907@gmail.com>
2022-03-02 06:49:59 +0900

    refactor(view): InputView 2단계 구현 후 메소드 사용 순으로 재정렬

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,19 +1,23 @@
 package view;
 
+import static java.util.stream.Collectors.toList;
+
 import domain.Lotto;
 import domain.Lottos;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import vo.Wallet;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String SPLIT_DELIMITER = ", ";
     private static final String MESSAGE_TO_GET_INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String MESSAGE_FOR_MANUAL_LOTTO_QUANTITY = "수동으로 구매할 로또 수를 입력해 주세요.";
-    private static final String MESSAGE_FOR_LOTTO_COUNT = "%d개를 구매했습니다.%s";
+    private static final String MESSAGE_FOR_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
+    private static final String MESSAGE_FOR_PURCHASED_LOTTOS = "수동으로 %d장, 자동으로 %d장을 구매했습니다.%n";
     private static final String MESSAGE_FOR_WINNING_LOTTO_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
-    private static final String SPLIT_DELIMITER = ", ";
     private static final String MESSAGE_FOR_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
 
@@ -27,34 +31,20 @@ public class InputView {
         return Integer.parseInt(SCANNER.nextLine());
     }
 
-    public static void printException(Exception exception) {
-        System.out.println(ERROR_MESSAGE_PREFIX + exception.getMessage());
-    }
-
-    public static void printTrialNumber(int count) {
-        System.out.printf(MESSAGE_FOR_LOTTO_COUNT, count, System.lineSeparator());
-    }
-
-    public static List<Integer> scanWinningNumbers() {
+    public static List<Integer> scanManualLottoNumbers() {
         System.out.print(System.lineSeparator());
-        System.out.println(MESSAGE_FOR_WINNING_LOTTO_NUMBERS);
+        System.out.println(MESSAGE_FOR_MANUAL_LOTTO_NUMBERS);
+
         String userInput = SCANNER.nextLine();
-
-        try {
-            return Arrays.stream(userInput.split(SPLIT_DELIMITER))
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-        } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException("숫자만 입력해주세요");
-        }
+        String[] userInputSplit = userInput.split(SPLIT_DELIMITER);
+        return Arrays.stream(userInputSplit)
+                .map(Integer::parseInt)
+                .collect(toList());
     }
 
-    public static int scanBonusNumber() {
-        System.out.println(MESSAGE_FOR_BONUS_NUMBER);
-        return Integer.parseInt(SCANNER.nextLine());
-    }
+    public static void printLottos(Wallet wallet, Lottos lottos) {
+        System.out.printf(MESSAGE_FOR_PURCHASED_LOTTOS, wallet.getManualQuantity(), wallet.getAutoQuantity());
 
-    public static void printLottos(Lottos lottos) {
         lottos.getLottos()
                 .forEach(InputView::printSingleLotto);
     }
@@ -66,5 +56,28 @@ public class InputView {
                 .collect(Collectors.joining(SPLIT_DELIMITER));
 
         System.out.println("[" + joinedLottoNumbers + "]");
+    }
+
+    public static List<Integer> scanWinningNumbers() {
+        System.out.print(System.lineSeparator());
+        System.out.println(MESSAGE_FOR_WINNING_LOTTO_NUMBERS);
+        String userInput = SCANNER.nextLine();
+
+        try {
+            return Arrays.stream(userInput.split(SPLIT_DELIMITER))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("숫자만 입력해주세요");
+        }
+    }
+
+    public static int scanBonusNumber() {
+        System.out.println(MESSAGE_FOR_BONUS_NUMBER);
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static void printException(Exception exception) {
+        System.out.println(ERROR_MESSAGE_PREFIX + exception.getMessage());
     }
 }
