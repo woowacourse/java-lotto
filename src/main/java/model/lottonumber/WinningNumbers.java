89f422ad2e864661daf6fd95b7b429cbdb6a8c0c
koo <gudonghee2000@gmail.com>
2022-03-01 14:28:30 +0900

    feat: WinningNumbers클래스에 당첨 번호와 보너스볼 숫자가 일치하면 오류를 발생하는 기능 생성

--- src/main/java/model/lottonumber/WinningNumbers.java
+++ src/main/java/model/lottonumber/WinningNumbers.java
@@ -8,16 +8,24 @@ import model.lottonumber.vo.Number;
 public class WinningNumbers {
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static final String NOT_CORRECT_LOTTO_NUMBER_COUNT_ERROR_MESSAGE = "입력한 당첨 번호가 6개가 아닙니다.";
+    private static final String DUPLICATE_WITH_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 입력한 보너스볼 숫자가 당첨 숫자와 중복됩니다.";
 
     private final List<Number> winningNumbers;
     private final Number bonusNumber;
 
     public WinningNumbers(final List<Integer> winningNumbers, final int bonusNumber) {
         checkCountOfNumbers(winningNumbers);
+        checkDuplicateWithWinningNumbers(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers.stream()
                 .map(Number::new)
                 .collect(Collectors.toList());
         this.bonusNumber = new Number(bonusNumber);
+    }
+
+    private void checkDuplicateWithWinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.stream().anyMatch(winningNumber -> winningNumber == bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATE_WITH_WINNING_NUMBER_ERROR_MESSAGE);
+        }
     }
 
     private void checkCountOfNumbers(final List<Integer> winningNumbers) {
--- src/test/java/model/lottonumber/WinningNumbersTest.java
+++ src/test/java/model/lottonumber/WinningNumbersTest.java
@@ -31,4 +31,15 @@ class WinningNumbersTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력한 당첨 번호가 6개가 아닙니다.");
     }
+
+    @Test
+    @DisplayName("당첨 번호와 보너스볼 번호가 겹치면 오류를 발생한다.")
+    void generateWinningNumbers_Duplicate() {
+        List<Integer> numbers = Arrays.asList(1, 2, 4, 6, 9, 16);
+        int bonusNumber = 16;
+
+        assertThatThrownBy(() -> new WinningNumbers(numbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력한 보너스볼 숫자가 당첨 숫자와 중복됩니다.");
+    }
 }
