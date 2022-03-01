65143b2559e2ff8b28d848dc3fb9d7b80528d3c0
yoon <duddbs774@gmail.com>
2022-03-01 21:21:38 +0900

    feat: 수동 로또티켓 개수가 최대값을 넘는 경우 예외 발생 기능 구현

--- src/main/java/lotto/domain/ManualLottoCount.java
+++ src/main/java/lotto/domain/ManualLottoCount.java
@@ -1,2 +1,17 @@
-package lotto.domain;public class ManualLottoCount {
+package lotto.domain;
+
+public class ManualLottoCount {
+    private static final String ERROR_EXCEED_UPPER_BOUND = "최대 구매 개수를 초과했습니다.";
+
+    private final int value;
+
+    private ManualLottoCount(int count) {
+        value = count;
+    }
+
+    public static void from(int inputCount, int upperBound) {
+        if (upperBound < inputCount) {
+            throw new IllegalArgumentException(ERROR_EXCEED_UPPER_BOUND);
+        }
+    }
 }
---
+++ src/test/java/lotto/domain/ManualLottoCountTest.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NoneAsciiCharacters")
+public class ManualLottoCountTest {
+    @Test
+    void 최대개수를_넘는_값을_입력한_경우_예외발생() {
+        int inputCount = 3;
+        int upperBound = 2;
+
+        assertThatThrownBy(() -> ManualLottoCount.from(inputCount, upperBound)).isInstanceOf(
+                IllegalArgumentException.class).hasMessageContaining("최대 구매 개수를 초과했습니다.");
+    }
+
+    @Test
+    void 최대개수를_넘지않는_경우() {
+        int inputCount = 2;
+        int upperBound = 2;
+
+        assertThatNoException().isThrownBy(() -> ManualLottoCount.from(inputCount, upperBound));
+    }
+}
