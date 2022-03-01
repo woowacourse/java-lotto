381266cc074000faf88c0d220a33984ab50167ad
sinb57 <sinb57@naver.com>
2022-03-01 15:54:12 +0900

    test: Ticket DisplayName 수정 및 테스트 로직 변경

--- src/test/java/lotto/domain/ticket/TicketTest.java
+++ src/test/java/lotto/domain/ticket/TicketTest.java
@@ -4,14 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import lotto.domain.ball.Ball;
-import lotto.domain.ball.BallStorage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import lotto.domain.ball.Ball;
+import lotto.domain.ball.BallStorage;
 import lotto.domain.rank.Rank;
 import lotto.exception.LottoException;
 import lotto.exception.LottoExceptionStatus;
@@ -24,21 +23,21 @@ class TicketTest {
                 .hasMessageContaining(exceptionStatus.getMessage());
     }
 
-    @DisplayName("로또 번호 묶음은 6개로 구성되여야 합니다.")
+    @DisplayName("로또 번호 묶음은 6개로 구성되어야 합니다.")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}")
     @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForNumbersOutOfSizeExceptionTest")
     void ticketOutOfSizeExceptionTest(final List<Integer> numbers) {
         ticketExceptionTest(numbers, LottoExceptionStatus.TICKET_NUMBERS_CANNOT_BE_OUT_OF_SIZE);
     }
 
-    @DisplayName("로또 번호 묶음 중 중복 값은 존재할 수 없습니다.")
+    @DisplayName("로또 번호는 중복된 숫자로 구성될 수 없습니다.")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}")
     @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForNumbersDuplicatedExceptionTest")
     void ticketNumbersDuplicatedExceptionTest(final List<Integer> numbers) {
         ticketExceptionTest(numbers, LottoExceptionStatus.TICKET_NUMBERS_CANNOT_BE_DUPLICATED);
     }
 
-    @DisplayName("로또 번호는 기댓값을 지니고 있으면 참을 반환한다.")
+    @DisplayName("로또 번호가 기댓값을 지니고 있으면 true를 반환해야 한다.")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}, 특정 번호 : {1}")
     @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForContainsTrueTest")
     void containsTrueTest(final List<Integer> numbers, final int targetNumber) {
@@ -48,7 +47,7 @@ class TicketTest {
         assertThat(ticket.contains(targetBall)).isTrue();
     }
 
-    @DisplayName("로또 번호는 기댓값을 지니고 있지 않으면 거짓을 반환한다.")
+    @DisplayName("로또 번호가 기댓값을 지니고 있지 않으면 false를 반환해야 한다.")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}, 특정 번호 : {1}")
     @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForContainsFalseTest")
     void containsFalseTest(final List<Integer> numbers, final int targetNumber) {
@@ -58,18 +57,14 @@ class TicketTest {
         assertThat(ticket.contains(targetBall)).isFalse();
     }
 
-    @DisplayName("당첨 번호와의 일치 개수는 기댓값과 일치해야 한다.")
+    @DisplayName("다른 로또 번호와의 일치 개수 계산 결과가 기댓값과 일치해야 한다.")
     @ParameterizedTest(name = "[{index}] 로또 번호 : {0}, 일치 개수 : {2}, 당첨 번호 : {1}")
     @MethodSource("lotto.domain.ticket.provider.TicketTestProvider#provideForCountMatchesTest")
-    void countMatchesTest(final List<Integer> numbers, final List<Integer> winningNumbers, final int matchCount) {
-        final Ticket ticket = new Ticket((numbers));
-        final Ticket winningTicket = new Ticket(winningNumbers);
-        final List<Integer> winningBallNumbers = winningTicket.getBallNumbers();
-        final List<Ball> winningBalls = winningBallNumbers.stream()
-                .map(BallStorage::getBall)
-                .collect(Collectors.toUnmodifiableList());
+    void countMatchesTest(final List<Integer> numbers, final List<Integer> anotherNumbers, final int matchCount) {
+        final Ticket ticket = new Ticket(numbers);
+        final Ticket anotherTicket = new Ticket(anotherNumbers);
 
-        assertThat(ticket.countMatches(winningBalls)).isEqualTo(matchCount);
+        assertThat(ticket.countMatches(anotherTicket)).isEqualTo(matchCount);
     }
 
     @DisplayName("당첨 등수 결과는 기댓값과 일치해야 한다.")
