37541be60848c5cec914447b69e8a21d578e5f91
sinb57 <sinb57@naver.com>
2022-03-01 15:09:30 +0900

    test: Ball 저장소, 캐싱 테스트

---
+++ src/test/java/lotto/domain/ball/BallStorageTest.java
@@ -0,0 +1,30 @@
+package lotto.domain.ball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("Ball 저장소, 캐싱 테스트")
+class BallStorageTest {
+
+    @DisplayName("Ball 객체는 캐싱되어야 한다.")
+    @ParameterizedTest(name = "[{index}] 번호 : {0}")
+    @ValueSource(ints = {1, 2, 44, 45})
+    void ballsCachingTest(final int ballNumber) {
+        final Ball actualBall = BallStorage.getBall(ballNumber);
+        final Ball expectedBall = BallStorage.getBall(ballNumber);
+        assertThat(actualBall).isSameAs(expectedBall);
+    }
+
+    @DisplayName("번호가 동일해도, 캐싱되지 않은 Ball 객체와 일치해선 안된다.")
+    @ParameterizedTest(name = "[{index}] 번호 : {0}")
+    @ValueSource(ints = {1, 2, 44, 45})
+    void ballsCaching2Test(final int ballNumber) {
+        final Ball actualBall = BallStorage.getBall(ballNumber);
+        final Ball expectedBall = new Ball(ballNumber);
+        assertThat(actualBall).isNotSameAs(expectedBall);
+    }
+
+}
