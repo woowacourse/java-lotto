9903ba5e5415f5a7b48aaa0a84a4e0316df79bc5
koo <gudonghee2000@gmail.com>
2022-03-01 15:28:23 +0900

    test: WinningNumbers클래스의 생성자를 테스트하는 코드 작성

--- src/test/java/model/lottonumber/WinningNumbersTest.java
+++ src/test/java/model/lottonumber/WinningNumbersTest.java
@@ -1,20 +1,35 @@
 package model.lottonumber;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
 
+import model.lottonumber.vo.Number;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningNumbersTest {
 
     @Test
+    @DisplayName("당첨 번호와 보너스볼 번호가 생성되는지 확인한다.")
+    void generateWinningNumbers() {
+        final List<Integer> numbers = Arrays.asList(1, 2, 4, 37, 41, 23);
+        final int bonusNumber = 7;
+
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, bonusNumber);
+        for (Number winningNumber : winningNumbers.getWinningNumbers()) {
+            assertThat(numbers).contains(winningNumber.getNumber());
+        }
+        assertThat(winningNumbers.getBonusNumber().getNumber()).isEqualTo(7);
+    }
+
+    @Test
     @DisplayName("당첨 번호가 6개보다 적게 입력되면 오류를 발생한다.")
-    void generateWinningNumbers_underThanSix() {
-        List<Integer> numbers = Arrays.asList(1, 2, 4, 6);
-        int bonusNumber = 7;
+    void generateWinningNumbers_UnderThanSix() {
+        final List<Integer> numbers = Arrays.asList(1, 2, 4, 6);
+        final int bonusNumber = 7;
 
         assertThatThrownBy(() -> new WinningNumbers(numbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -23,9 +38,9 @@ class WinningNumbersTest {
 
     @Test
     @DisplayName("당첨 번호가 6개보다 많이 입력되면 오류를 발생한다.")
-    void generateWinningNumbers_overThanSix() {
-        List<Integer> numbers = Arrays.asList(1, 2, 4, 6, 7, 9, 16);
-        int bonusNumber = 21;
+    void generateWinningNumbers_OverThanSix() {
+        final List<Integer> numbers = Arrays.asList(1, 2, 4, 6, 7, 9, 16);
+        final int bonusNumber = 21;
 
         assertThatThrownBy(() -> new WinningNumbers(numbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -35,8 +50,8 @@ class WinningNumbersTest {
     @Test
     @DisplayName("당첨 번호와 보너스볼 번호가 겹치면 오류를 발생한다.")
     void generateWinningNumbers_Duplicate() {
-        List<Integer> numbers = Arrays.asList(1, 2, 4, 6, 9, 16);
-        int bonusNumber = 16;
+        final List<Integer> numbers = Arrays.asList(1, 2, 4, 6, 9, 16);
+        final int bonusNumber = 16;
 
         assertThatThrownBy(() -> new WinningNumbers(numbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
