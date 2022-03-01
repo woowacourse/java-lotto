5d708a620d3971c2aba5993119d705a2ad16963d
hamcheeseburger <yoo77hyeon@gmail.com>
2022-03-01 18:20:26 +0900

    refactor: matchBonus를 중복시키지 않고 등수를 찾아내게끔 변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,32 +1,46 @@
 package lotto.domain;
 
-import java.util.function.BiPredicate;
 import java.util.*;
 
 public enum Rank {
 
-    FIRST(6, new Reward(2_000_000_000L), (matchCount, matchBonus) -> matchCount == 6),
-    SECOND(5, new Reward(30_000_000L), (matchCount, matchBonus) -> matchCount == 5 && matchBonus),
-    THIRD(5, new Reward(1_500_000L), (matchCount, matchBonus) -> matchCount == 5 && !matchBonus),
-    FOURTH(4, new Reward(50_000L), (matchCount, matchBonus) -> matchCount == 4),
-    FIFTH(3, new Reward(5_000L), (matchCount, matchBonus) -> matchCount == 3),
-    NONE(0, new Reward(0L), (matchCount, matchBonus) -> matchCount <= 2);
+    FIRST(6, new Reward(2_000_000_000L)),
+    SECOND(5, new Reward(30_000_000L)),
+    THIRD(5, new Reward(1_500_000L)),
+    FOURTH(4, new Reward(50_000L)),
+    FIFTH(3, new Reward(5_000L)),
+    NONE(0, new Reward(0L));
 
     private final int matchCount;
     private final Reward reward;
-    private final BiPredicate<Integer, Boolean> matchCalculator;
 
-    Rank(int matchCount, Reward reward, BiPredicate<Integer, Boolean> matchCalculator) {
+    Rank(int matchCount, Reward reward) {
         this.matchCount = matchCount;
         this.reward = reward;
-        this.matchCalculator = matchCalculator;
     }
 
     public static Rank find(int matchCount, boolean matchBonus) {
-        return Arrays.stream(Rank.values())
-                .filter(rank -> rank.matchCalculator.test(matchCount, matchBonus))
+        if (isMatchBonusAvailable(matchCount, matchBonus)) {
+            return Rank.SECOND;
+        }
+        return findWithMatchCount(matchCount);
+    }
+
+    private static boolean isMatchBonusAvailable(int matchCount, boolean matchBonus) {
+        return matchCount == SECOND.matchCount && matchBonus;
+    }
+
+    private static Rank findWithMatchCount(int matchCount) {
+        return exceptMatchBonusAvailableRank().stream()
+                .filter(rank -> rank.matchCount == matchCount)
                 .findAny()
                 .orElse(Rank.NONE);
+    }
+
+    private static List<Rank> exceptMatchBonusAvailableRank() {
+        List<Rank> ranks = new ArrayList<>(List.of(Rank.values()));
+        ranks.remove(Rank.SECOND);
+        return ranks;
     }
 
     public static Reward calculateReward(List<Rank> ranks) {
