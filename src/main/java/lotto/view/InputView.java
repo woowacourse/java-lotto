431317b3f4defab6e16e20b5472c56df40463267
yoon <duddbs774@gmail.com>
2022-03-01 22:07:25 +0900

    feat: 수동 로또 티켓 개수 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,14 +7,18 @@ public class InputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String INPUT_MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String LOTTO_ERROR_PREFIX = "로또 번호는";
     private static final String NOT_NUMBER_ERROR = "숫자로 입력해주세요.";
     private static final String NOT_INSTANTIATION_ERROR = "InputView 객체를 생성할 수 없습니다.";
+    private static final String NOT_POSITIVE_ERROR = "0이 넘는 값을 입력하세요.";
     private static final String EMPTY = "";
     private static final String BLANK = " ";
     private static final String DELIMITER = ",";
+    private static final int POSITIVE_LOWER_BOUND = 1;
 
     private static final Scanner scanner = new Scanner(System.in);
+
 
     private InputView() throws InstantiationException {
         throw new InstantiationException(NOT_INSTANTIATION_ERROR);
@@ -22,15 +26,15 @@ public class InputView {
 
     public static int inputAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
-        return convertToInt(scanner.nextLine(), NOT_NUMBER_ERROR);
+        return convertToNumber(scanner.nextLine(), NOT_NUMBER_ERROR);
     }
 
-    private static int convertToInt(String input, String errorMessage) {
+    private static int convertToNumber(String input, String errorMessage) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             System.out.println(errorMessage);
-            return convertToInt(scanner.nextLine(), errorMessage);
+            return convertToNumber(scanner.nextLine(), errorMessage);
         }
     }
 
@@ -57,6 +61,24 @@ public class InputView {
 
     public static int inputBonusBall() {
         System.out.println(INPUT_BONUS_BALL_MESSAGE);
-        return convertToInt(scanner.nextLine(), NOT_NUMBER_ERROR);
+        return convertToNumber(scanner.nextLine(), NOT_NUMBER_ERROR);
+    }
+
+    public static int inputManualLottoCount() {
+        System.out.println(INPUT_MANUAL_LOTTO_COUNT_MESSAGE);
+        final int manualLottoCount = convertToNumber(scanner.nextLine(), NOT_NUMBER_ERROR);
+        try {
+            checkPositive(manualLottoCount);
+            return manualLottoCount;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputManualLottoCount();
+        }
+    }
+
+    private static void checkPositive(int number) {
+        if (number < POSITIVE_LOWER_BOUND) {
+            throw new IllegalArgumentException(NOT_POSITIVE_ERROR);
+        }
     }
 }
