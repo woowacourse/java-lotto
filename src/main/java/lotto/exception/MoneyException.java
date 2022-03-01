f8c6da36e417e64eb09acdb0befb359464f3444d
jojogreen91 <iawbg13@gmail.com>
2022-03-02 00:52:43 +0900

    refactor: 포매팅 적용

--- src/main/java/lotto/exception/MoneyException.java
+++ src/main/java/lotto/exception/MoneyException.java
@@ -1,6 +1,6 @@
 package lotto.exception;
 
-public class MoneyException extends IllegalArgumentException{
+public class MoneyException extends IllegalArgumentException {
 
     public static final String MONEY_UNIT_ERROR_MESSAGE = "구입금액은 1000원 단위만 가능합니다.";
     public static final String MONEY_ONLY_POSITIVE_NUMBER_ERROR_MESSAGE = "구입금액이 음수일 수 없습니다.";
