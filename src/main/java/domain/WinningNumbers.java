356d7bc5c3749763dec95b679b887b2ee8e62ec8
soominsohn <soominsohn@gmail.com>
2022-03-02 01:09:27 +0900

    test(winningNumbers): add validate test

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -11,7 +11,7 @@ public class WinningNumbers {
     private static final int NUMBER_RANGE_MAXIMUM = 45;
 
     private static final String OUT_OF_RANGE_ERROR = "[ERROR] 1이상 45이하 값을 입력해주세요.";
-    private static final String DUPLICATED_NUMBER_ERROR = "[ERROR] 로또번호는 중복될 수 없습니다.";
+    private static final String DUPLICATED_NUMBER_ERROR = "[ERROR] 당첨번호와 보너스볼은 중복될 수 없습니다.";
 
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
--- src/test/java/WinningNumbersTest.java
+++ src/test/java/WinningNumbersTest.java
@@ -1,3 +1,54 @@
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import domain.WinningNumbers;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class WinningNumbersTest {
+
+    @Test
+    @DisplayName("로또 당첨 번호는 1~45 사이가 아닐 시 실패")
+    void check_range_fail() {
+
+        assertThatThrownBy(
+            () -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 46), 6)
+        ).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 1이상 45이하 값을 입력해주세요.");
+
+    }
+
+    @Test
+    @DisplayName("로또 보너스 번호는 1~45 사이가 아닐 시 실패")
+    void check_bonus_range_fail() {
+
+        assertThatThrownBy(
+            () -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 46)
+        ).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 1이상 45이하 값을 입력해주세요.");
+
+    }
+
+    @Test
+    @DisplayName("로또 당첨 번호 중복 시 실패")
+    void check_unique_fail() {
+
+        assertThatThrownBy(
+            () -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5), 45)
+        ).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 당첨번호와 보너스볼은 중복될 수 없습니다.");
+
+    }
+
+    @Test
+    @DisplayName("로또 당첨 번호와 보너스 볼 중복 시 실패")
+    void check_bonus_unique_fail() {
+
+        assertThatThrownBy(
+            () -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 6)
+        ).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 당첨번호와 보너스볼은 중복될 수 없습니다.");
+
+    }
 
 }
