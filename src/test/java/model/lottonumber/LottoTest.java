50b86b3830947fc6fd37dcef5e0416439ee87c39
koo <gudonghee2000@gmail.com>
2022-03-01 13:55:23 +0900

    test: Lotto클래스의 숫자 생성, 당첨등수선정을 테스트하는 코드 작성

---
+++ src/test/java/model/lottonumber/LottoTest.java
@@ -0,0 +1,101 @@
+package model.lottonumber;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import model.generator.Generator;
+
+import model.rank.Rank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @Test
+    @DisplayName("생성된 난수를 바탕으로 로또 번호를 생성한다.")
+    void generateLottoNumbers_Test() {
+        Generator generator = () -> Arrays.asList(1, 2, 13, 4, 25, 39);
+        Lotto lotto = new Lotto(generator);
+
+        assertThat(lotto.getLottoNumbers()).isEqualTo(Arrays.asList(1, 2, 13, 4, 25, 39));
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 3개 미만으로 일치시 NONE을 반환한다.")
+    void findWinningRank_NONE() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int number = 7;
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, number);
+
+        Generator generator = () -> Arrays.asList(1, 32, 13, 4, 25, 39);
+        Lotto lotto = new Lotto(generator);
+
+        assertThat(lotto.findWinningRank(winningNumbers)).isEqualTo(Rank.NONE);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 3개 일치시 5등을 반환한다.")
+    void findWinningRank_Fifth() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int number = 7;
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, number);
+
+        Generator generator = () -> Arrays.asList(1, 2, 13, 4, 25, 39);
+        Lotto lotto = new Lotto(generator);
+
+        assertThat(lotto.findWinningRank(winningNumbers)).isEqualTo(Rank.FIFTH);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 4개 일치시 4등을 반환한다.")
+    void findWinningRank_Fourth() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int number = 7;
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, number);
+
+        Generator generator = () -> Arrays.asList(1, 2, 3, 4, 25, 39);
+        Lotto lotto = new Lotto(generator);
+
+        assertThat(lotto.findWinningRank(winningNumbers)).isEqualTo(Rank.FOURTH);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 5개 일치, 보너스 볼 불일치한 경우 3등을 반환한다.")
+    void findWinningRank_Third() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int number = 7;
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, number);
+
+        Generator generator = () -> Arrays.asList(1, 2, 3, 4, 39, 6);
+        Lotto lotto = new Lotto(generator);
+
+        assertThat(lotto.findWinningRank(winningNumbers)).isEqualTo(Rank.THIRD);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 5개 일치, 보너스 볼 일치한 경우 2등을 반환한다.")
+    void findWinningRank_Second() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int number = 7;
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, number);
+
+        Generator generator = () -> Arrays.asList(1, 2, 3, 4, 7, 6);
+        Lotto lotto = new Lotto(generator);
+
+        assertThat(lotto.findWinningRank(winningNumbers)).isEqualTo(Rank.SECOND);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 전부 일치할 시 1등을 반환한다.")
+    void findWinningRank_First() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int number = 7;
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, number);
+
+        Generator generator = () -> Arrays.asList(1, 2, 4, 3, 6, 5);
+        Lotto lotto = new Lotto(generator);
+
+        assertThat(lotto.findWinningRank(winningNumbers)).isEqualTo(Rank.FIRST);
+    }
+}
