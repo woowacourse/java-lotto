d9103641d996494d44d807d9150aac0910d256a6
nailseong <nnis9857@gmail.com>
2022-03-01 16:23:22 +0900

    feat: Lottos 정적 팩토리 메서드 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Lottos {
@@ -18,6 +19,18 @@ public class Lottos {
                 .mapToObj(i -> Lotto.createByAuto())
                 .forEach(lottos::add);
         this.lottos = lottos;
+    }
+
+    public static Lottos createByAuto(int quantity) {
+        final List<Lotto> lottos = IntStream.range(0, quantity)
+                .mapToObj(i -> Lotto.createByAuto())
+                .collect(Collectors.toList());
+
+        return new Lottos(lottos);
+    }
+
+    public static Lottos createByManual(List<Lotto> lottos) {
+        return new Lottos(lottos);
     }
 
     public Result getResult(WinningNumbers winningNumbers) {
