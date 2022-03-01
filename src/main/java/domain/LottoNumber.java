314e2f12c6724d8dfd39a13fb9a56de2f723bb39
is2js <tingstyle1@gmail.com>
2022-03-01 15:38:03 +0900

    style: 불필요한 코드 제거 및 warning 해결

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -10,12 +10,12 @@ public class LottoNumber implements Comparable<LottoNumber> {
     private static final int LOTTO_NUMBER_MAX = 45;
     private static final String LOTTO_NUMBER_RANGE_MESSAGE =
         "로또 번호가 유효한 범위(" + LOTTO_NUMBER_MIN + "-" + LOTTO_NUMBER_MAX + ")가 아닙니다.";
-    private static Map<Integer, LottoNumber> LOTTO_NUMBER_CACHE;
+    private static final Map<Integer, LottoNumber> LOTTO_NUMBER_CACHE;
 
     static {
         LOTTO_NUMBER_CACHE = IntStream.rangeClosed(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX)
             .boxed()
-            .collect(Collectors.toMap(number -> number, number -> new LottoNumber(number)));
+            .collect(Collectors.toMap(number -> number, LottoNumber::new));
     }
 
     private final int number;
