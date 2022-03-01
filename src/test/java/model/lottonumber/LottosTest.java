d8a46da89b73f0a4552f52546f810aa95929f684
koo <gudonghee2000@gmail.com>
2022-03-01 14:16:53 +0900

    test: Lottos클래스의 로또 생성을 테스트하는 코드 작성

---
+++ src/test/java/model/lottonumber/LottosTest.java
@@ -0,0 +1,23 @@
+package model.lottonumber;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import model.generator.Generator;
+import model.money.Money;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottosTest {
+
+    @Test
+    @DisplayName("투입 금액만큼의 로또를 생성하는지 확인한다.")
+    void generateLottos_Test() {
+        Money money = new Money(100000);
+        Generator generator = () -> Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        Lottos lottos = new Lottos(money, generator);
+        assertThat(lottos.getLottos().size()).isEqualTo(100);
+    }
+}
