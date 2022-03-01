a2a02685c07615d26320552c1f4e24a1da798e3a
jurlring <78091011+jurlring@users.noreply.github.com>
2022-03-01 15:03:02 +0900

    refactor: 의도에 맞는 변수명으로 변경

--- src/main/java/domain/LottoFactory.java
+++ src/main/java/domain/LottoFactory.java
@@ -9,10 +9,10 @@ import java.util.stream.IntStream;
 public class LottoFactory {
 
 	private static final int FIRST_LOTTO_NUMBER = 1;
-	private static final int NEXT_NUMBER_OF_LAST_LOTTO_NUMBER = 46;
+	private static final int LAST_LOTTO_NUMBER = 45;
 	private static final int LOTTO_MAX_SIZE_INDEX = 6;
 	private static final int INITIAL_INDEX = 0;
-	private final List<Integer> lottoNumbers = IntStream.range(FIRST_LOTTO_NUMBER, NEXT_NUMBER_OF_LAST_LOTTO_NUMBER)
+	private final List<Integer> lottoNumbers = IntStream.rangeClosed(FIRST_LOTTO_NUMBER, LAST_LOTTO_NUMBER)
 		.boxed()
 		.collect(Collectors.toList());
 
