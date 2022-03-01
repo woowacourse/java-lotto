85d881415a2a14902eef5366a6a5fbb5ad585f2e
nailseong <nnis9857@gmail.com>
2022-03-01 18:19:35 +0900

    test: 수익률을 계산하는 메서드의 테스트 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -3,10 +3,15 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoneyTest {
@@ -67,6 +72,35 @@ public class MoneyTest {
                 assertThatThrownBy(() -> money.validateQuantityOfManual(quantityOfManual))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("0개 이상 " + moneyValue / 1000 + "개 이하의 로또만 수동으로 구매할 수 있습니다.");
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("수익률을 계산하는 메서드는")
+    class CalculateRateOfProfit {
+
+        @Nested
+        @TestInstance(Lifecycle.PER_CLASS)
+        @DisplayName("총 당첨금이 주어지면")
+        class Context_with_totla_profit {
+
+            @ParameterizedTest
+            @MethodSource("provideSource")
+            @DisplayName("수익률을 소수점 아래 4번째 자리에서 반올림하여 반환한다.")
+            void It_returns_rate_of_profit(int value, long totalProfit, double expected) {
+                final Money money = new Money(value);
+
+                assertThat(money.calculateRateOfProfit(totalProfit)).isEqualTo(expected);
+            }
+
+            Stream<Arguments> provideSource() {
+                return Stream.of(
+                        Arguments.of(1_000, 5000, 5),
+                        Arguments.of(10_000, 5000, 0.5),
+                        Arguments.of(1_000_000, 5000, 0.005),
+                        Arguments.of(10_000_000, 5000, 0.001)
+                );
             }
         }
     }
