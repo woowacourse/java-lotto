62b2a54bc5d7378c4c1dad78ea786029c7ff015a
Gyuchool <balance0591@naver.com>
2022-03-02 00:32:06 +0900

    test: 로또 번호 정상인 경우 테스트

--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumberTest {
@@ -11,9 +12,19 @@ class LottoNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {0, -1, 46})
     @DisplayName("로또 번호가 1보다 작거나 45보다 크면 예외 발생")
-    void generateLottoNumberTest(int number) {
+    void generateLottoNumberWhenNumberLessThanOneOrMoreThan45FailureTest(int number) {
         assertThatThrownBy(
                 () -> LottoNumber.of(number)
         ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 44, 45})
+    @DisplayName("로또 번호가 1부터 45일때 테스트 - 성공")
+    void generateLottoNumberTest(int number) {
+
+        LottoNumber lottoNumber = LottoNumber.of(number);
+        assertThat(lottoNumber.getLottoNumber()).isEqualTo(number);
+
     }
 }
