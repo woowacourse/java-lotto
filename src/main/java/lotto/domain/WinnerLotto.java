2ebef0158be3766d4e4c5a1d2174e83775c32637
hamcheeseburger <yoo77hyeon@gmail.com>
2022-03-01 18:44:39 +0900

    refactor: WinnerLotto 검증 메소드 분리

--- src/main/java/lotto/domain/WinnerLotto.java
+++ src/main/java/lotto/domain/WinnerLotto.java
@@ -6,11 +6,15 @@ public class WinnerLotto {
     private final LottoNumber bonus;
 
     public WinnerLotto(Lotto winner, LottoNumber bonus) {
+        validateDuplicated(winner, bonus);
+        this.winner = winner;
+        this.bonus = bonus;
+    }
+
+    private void validateDuplicated(Lotto winner, LottoNumber bonus) {
         if (winner.containsNumber(bonus)) {
             throw new IllegalArgumentException("보너스볼은 당첨번호와 중복될 수 없다.");
         }
-        this.winner = winner;
-        this.bonus = bonus;
     }
 
     public Rank findRank(Lotto lotto) {
