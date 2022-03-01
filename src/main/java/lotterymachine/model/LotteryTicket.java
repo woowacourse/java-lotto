b67278dce30f7bad129b945a80f355886fa4f2fa
SuyeonChoi <tndus970120@gmail.com>
2022-03-02 01:13:43 +0900

    feat(LotteryTicket): 로또 티켓의 번호가 6개인지 검증

--- src/main/java/lotterymachine/model/LotteryTicket.java
+++ src/main/java/lotterymachine/model/LotteryTicket.java
@@ -1,13 +1,17 @@
 package lotterymachine.model;
 
+import static lotterymachine.model.ErrorMessage.INVALID_SIZE;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import lotterymachine.vo.Ball;
 
 public class LotteryTicket {
+    private static final int TICKET_SIZE = 6;
     private final List<Ball> balls;
 
     public LotteryTicket(final List<Ball> balls) {
+        validateSize(balls);
         this.balls = balls;
     }
 
@@ -25,5 +29,11 @@ public class LotteryTicket {
         return balls.stream()
                 .map(Ball::getNumber)
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void validateSize(List<Ball> balls) {
+        if (balls.size() != TICKET_SIZE) {
+            throw new IllegalArgumentException(INVALID_SIZE.getMessage());
+        }
     }
 }
