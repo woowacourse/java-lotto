19fa553d8bc459d1ef8fc16b7186f35513f788cf
koo <gudonghee2000@gmail.com>
2022-03-01 13:12:09 +0900

    refactor: IntergerParser클래스의 함수 매개변수 상수화

--- src/main/java/utils/IntegerParser.java
+++ src/main/java/utils/IntegerParser.java
@@ -3,7 +3,7 @@ package utils;
 public class IntegerParser {
     private static final String NO_INTEGER_VALUE_ERROR_MESSAGE = "[ERROR] 입력한 값이 숫자가 아닙니다.";
 
-    public static int parseInteger(String value) {
+    public static int parseInteger(final String value) {
         try {
             return Integer.parseInt(value.trim());
         } catch (NumberFormatException error) {
