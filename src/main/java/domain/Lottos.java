7765723e8f1ee9d45d1e46286c6d5ebf1301059c
devHudi <devhudi@gmail.com>
2022-03-01 20:53:33 +0900

    refactor(lottos): 두 Lottos 를 합치는 기능 추가

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -5,11 +5,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
+    static final String ERROR_MESSAGE_FOR_EMPTY_LOTTO_LIST = "빈 Lotto 리스트로 Lottos 를 생성할 수 없습니다.";
+
     private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        validateLottoListSize(lottos);
+        this.lottos = lottos;
+    }
 
     public Lottos(LottoQuantity lottoQuantity, LottoNumberGenerateStrategy lottoNumberGenerator) {
         this.lottos = generateLottos(lottoQuantity, lottoNumberGenerator);
     }
+
+    public static Lottos concat(Lottos lottos1, Lottos lottos2) {
+        List<Lotto> newLottoValues = new ArrayList<>(lottos1.lottos);
+        newLottoValues.addAll(lottos2.lottos);
+
+        return new Lottos(newLottoValues);
+    }
+
+    private void validateLottoListSize(List<Lotto> lottos) {
+        if (lottos.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_EMPTY_LOTTO_LIST);
+        }
+    }
+
 
     private List<Lotto> generateLottos(LottoQuantity lottoQuantity, LottoNumberGenerateStrategy lottoNumberGenerator) {
         List<Lotto> lottos = new ArrayList<>();
--- src/test/java/domain/LottosTest.java
+++ src/test/java/domain/LottosTest.java
@@ -2,6 +2,7 @@ package domain;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.strategy.LottoNumberGenerateStrategy;
 import domain.strategy.StubRandomLottoNumberGenerator;
@@ -57,5 +58,53 @@ public class LottosTest {
 
         // then
         assertThat(winningCount).isEqualTo(new WinningCount(1));
+    }
+
+    @Test
+    @DisplayName("생성자에 List<Lotto> 를 전달받아 Lottos 를 생성할 수 있다.")
+    void createLottosWithLottoList() {
+        // given
+        List<Lotto> lottoValues = List.of(new Lotto(Set.of(1, 2, 3, 4, 5, 6)));
+
+        // when
+        Lottos lottos = new Lottos(lottoValues);
+
+        // then
+        assertThat(lottos).isNotNull();
+    }
+
+    @Test
+    @DisplayName("생성자에 전달된 List<Lotto> 의 크기가 1보다 작다면 IAE 가 발생한다.")
+    void createLottosWithEmptyLottoListShouldFail() {
+        // given
+        List<Lotto> lottoValues = List.of();
+
+        // when & then
+        assertThatThrownBy(() -> new Lottos(lottoValues))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Lottos.ERROR_MESSAGE_FOR_EMPTY_LOTTO_LIST);
+    }
+
+    @Test
+    @DisplayName("concat 메소드를 사용하여 두개의 Lottos 를 합쳐 새로운 Lottos 를 만들 수 있다.")
+    void concat() {
+        // given
+        Lottos lottos1 = new Lottos(new LottoQuantity(3), lottoNumberGenerateStrategy);
+        Lottos lottos2 = new Lottos(new LottoQuantity(3), lottoNumberGenerateStrategy);
+
+        // when
+        Lottos joinedLottos = Lottos.concat(lottos1, lottos2);
+
+        // then
+        assertThat(joinedLottos.getLottos())
+                .containsExactly(
+                        new Lotto(Set.of(1, 2, 3, 4, 5, 6)),
+                        new Lotto(Set.of(7, 8, 9, 10, 11, 12)),
+                        new Lotto(Set.of(13, 14, 15, 16, 17, 18)),
+                        new Lotto(Set.of(1, 2, 3, 4, 5, 6)),
+                        new Lotto(Set.of(7, 8, 9, 10, 11, 12)),
+                        new Lotto(Set.of(13, 14, 15, 16, 17, 18))
+                );
+
     }
 }
