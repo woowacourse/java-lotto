54b09bea03e503af130a4f8e3344d99a62d3388c
her0807 <her0807@naver.com>
2022-03-02 16:07:14 +0900

    feat : 수동 로또 변환 로직

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Lottos {
 	private final List<Lotto> lottos;
@@ -16,6 +17,14 @@ public class Lottos {
 	public Lottos(List<Lotto> lottos) {
 		Objects.requireNonNull(lottos, "[ERROR] lottos가 null 입니다.");
 		this.lottos = new ArrayList<>(lottos);
+	}
+
+	public static Lottos of(List<String> manualLottos) {
+		List<Lotto> lottos = manualLottos.stream()
+			.map(lotto -> Lotto.of(lotto.split(",")))
+			.collect(Collectors.toUnmodifiableList());
+
+		return new Lottos(lottos);
 	}
 
 	public Map<Rank, Long> countRank(WinningLotto winningLotto) {
