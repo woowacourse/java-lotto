eafee930caaa79594c0171ce38ffeed2eb35e614
Donggyu Kim <a29661498@gmail.com>
2022-03-02 17:24:18 +0900

    fix: LottoResult 변경에 따른 Test 코드 수정
    LottoResult 에서 소수점 아래 2자리에서 짜르지 않고 LottoResult 의 결과에서는 단순 double 형 값을 반환해주므로 이에 따라 수정을 진행하였습니다.

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static lotto.domain.enumeration.Rank.FIFTH;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
@@ -14,7 +15,7 @@ class LottoResultTest {
     @Test
     void checkYield() {
         // given
-        Map<Rank, Integer> ranks = Map.of(Rank.FIFTH, 1);
+        Map<Rank, Integer> ranks = Map.of(FIFTH, 1);
 
         LottoResult lottoResult = new LottoResult(ranks);
 
@@ -24,14 +25,14 @@ class LottoResultTest {
         double result = lottoResult.calculateYield(lottoPurchaseMoney);
 
         // then
-        assertThat(result).isEqualTo(0.35);
+        assertThat(result).isEqualTo((double) FIFTH.getPrizeMoney() / lottoPurchaseMoney.getPrice());
     }
 
     @DisplayName("같은 등수가 여러 개 당첨된 경우 올바른 수익률을 계산하여 반환한다.")
     @Test
     void checkSamePlace() {
         // given
-        Map<Rank, Integer> ranks = Map.of(Rank.FIFTH, 2);
+        Map<Rank, Integer> ranks = Map.of(FIFTH, 2);
 
         LottoResult lottoResult = new LottoResult(ranks);
 
@@ -41,6 +42,6 @@ class LottoResultTest {
         double result = lottoResult.calculateYield(lottoPurchaseMoney);
 
         // then
-        assertThat(result).isEqualTo(0.71);
+        assertThat(result).isEqualTo((double) (FIFTH.getPrizeMoney() * 2) / lottoPurchaseMoney.getPrice());
     }
 }
