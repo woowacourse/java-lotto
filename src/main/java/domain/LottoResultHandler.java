eea28a9ce44a9b7a2a65c9274a2a2da2c5b90e1e
Richard JEON <ztzy1907@gmail.com>
2022-03-03 06:29:41 +0900

    refactor(lotto-result-handler): 당첨 결과 집계 시 LongSummaryStatistics 활용

--- src/main/java/domain/LottoResultHandler.java
+++ src/main/java/domain/LottoResultHandler.java
@@ -1,13 +1,13 @@
 package domain;
 
 import static domain.Lotto.LOTTO_PRICE;
-import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summarizingLong;
 
 import dto.LottoResultDto;
 import java.util.EnumMap;
+import java.util.LongSummaryStatistics;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.function.Function;
 
 public class LottoResultHandler {
@@ -15,29 +15,27 @@ public class LottoResultHandler {
     }
 
     public static LottoResultDto getLottoResultDto(WinningLotto winningLotto, Lottos lottos) {
-        Map<Rank, Long> lottoResult = getLottoResultGroupByRank(winningLotto, lottos);
+        Map<Rank, LongSummaryStatistics> lottoResult = getLottoResultGroupByRank(winningLotto, lottos);
         long totalReturn = getTotalReturnByLottoResult(lottoResult);
         double rateOfReturn = getRateOfReturn(lottos, totalReturn);
 
         return LottoResultDto.of(lottoResult, rateOfReturn);
     }
 
-    private static Map<Rank, Long> getLottoResultGroupByRank(WinningLotto winningLotto, Lottos lottos) {
+    private static Map<Rank, LongSummaryStatistics> getLottoResultGroupByRank(WinningLotto winningLotto,
+                                                                              Lottos lottos) {
         return lottos.getLottos()
                 .stream()
                 .map(winningLotto::getRankByLotto)
-                .collect(groupingBy(Function.identity(), () -> new EnumMap<>(Rank.class), counting()));
+                .collect(groupingBy(Function.identity(), () -> new EnumMap<>(Rank.class),
+                        summarizingLong(Rank::getPrize)));
     }
 
-    private static long getTotalReturnByLottoResult(Map<Rank, Long> lottoResult) {
-        return lottoResult.entrySet()
+    private static long getTotalReturnByLottoResult(Map<Rank, LongSummaryStatistics> lottoResult) {
+        return lottoResult.values()
                 .stream()
-                .mapToLong(LottoResultHandler::getPrizeByRankAndTimes)
+                .mapToLong(LongSummaryStatistics::getSum)
                 .sum();
-    }
-
-    private static long getPrizeByRankAndTimes(Entry<Rank, Long> map) {
-        return map.getKey().getPrize() * map.getValue();
     }
 
     private static double getRateOfReturn(Lottos lottos, long totalReturn) {
--- src/main/java/dto/LottoResultDto.java
+++ src/main/java/dto/LottoResultDto.java
@@ -2,25 +2,24 @@ package dto;
 
 import domain.Rank;
 import java.util.Collections;
+import java.util.LongSummaryStatistics;
 import java.util.Map;
 
 public class LottoResultDto {
-    private static final long ZERO_FOR_INVALID_KEY = 0L;
-
-    private final Map<Rank, Long> winningCountByRank;
+    private final Map<Rank, LongSummaryStatistics> winningCountByRank;
     private final double rateOfReturn;
 
-    private LottoResultDto(Map<Rank, Long> winningCountByRank, double rateOfReturn) {
+    private LottoResultDto(Map<Rank, LongSummaryStatistics> winningCountByRank, double rateOfReturn) {
         this.winningCountByRank = Collections.unmodifiableMap(winningCountByRank);
         this.rateOfReturn = rateOfReturn;
     }
 
-    public static LottoResultDto of(Map<Rank, Long> winningCountByRank, double profitRatio) {
+    public static LottoResultDto of(Map<Rank, LongSummaryStatistics> winningCountByRank, double profitRatio) {
         return new LottoResultDto(winningCountByRank, profitRatio);
     }
 
-    public Long getWinningCountByRank(Rank rank) {
-        return winningCountByRank.getOrDefault(rank, ZERO_FOR_INVALID_KEY);
+    public LongSummaryStatistics getWinningCountByRank(Rank rank) {
+        return winningCountByRank.get(rank);
     }
 
     public double getRateOfReturn() {
