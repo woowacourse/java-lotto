756c554f374257f7c12f2eb6aadf4ba884427847
jojogreen91 <iawbg13@gmail.com>
2022-03-02 19:42:14 +0900

    refactor: 에러메시지 수정

--- src/main/java/lotto/exception/MoneyException.java
+++ src/main/java/lotto/exception/MoneyException.java
@@ -4,7 +4,7 @@ public class MoneyException extends IllegalArgumentException {
 
     public static final String MONEY_UNIT_ERROR_MESSAGE = "투입금액은 1000원 단위만 가능합니다.";
     public static final String MONEY_ONLY_POSITIVE_NUMBER_ERROR_MESSAGE = "투입금액이 음수일 수 없습니다.";
-    public static final String MONEY_SPENT_LIMIT_ERROR_MESSAGE = "사용한 금액이 투입금액을 초과합니다.";
+    public static final String MONEY_SPENT_LIMIT_ERROR_MESSAGE = "투입한 금액을 모두 사용했습니다.";
 
     public MoneyException(String message) {
         super(message);
