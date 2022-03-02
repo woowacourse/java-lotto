dc906c9cdad3a612987c95c6e49bdb0809349895
유영우 <fbduddn97@naver.com>
2022-03-02 18:23:26 +0900

    feat: 구매할 개수와 입력받은 로또 티켓들 검증 로직 추가

--- src/main/java/domain/TicketCount.java
+++ src/main/java/domain/TicketCount.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.List;
+
 public class TicketCount {
 	private static final int TICKET_PRICE = 1000;
 	private static final int ZERO_COUNT = 0;
@@ -7,6 +9,7 @@ public class TicketCount {
 	private static final String INVALID_MONEY_ERROR = "구입 금액은 1000원 이상이어야 합니다.";
 	private static final String NEGATIVE_COUNT_ERROR = "구매할 로또 수는 음수일 수 없습니다.";
 	private static final String OVER_COUNT_ERROR = "구매할 로또 수는 구입 금액으로 살 수 있어야 합니다.";
+	private static final String COUNT_TICKETS_SIZE_NOT_EQUALS_ERROR = "구매할 개수와 입력받은 수동 로또 개수는 같아야 합니다.";
 
 	private final int manualCount;
 	private final int autoCount;
@@ -36,8 +39,10 @@ public class TicketCount {
 		}
 	}
 
-	public int getManualCount() {
-		return manualCount;
+	public void validateManualCountEqualsTicketsSize(List<List<Integer>> lottoTickets) {
+		if(manualCount != lottoTickets.size()) {
+			throw new IllegalArgumentException(COUNT_TICKETS_SIZE_NOT_EQUALS_ERROR);
+		}
 	}
 
 	public int getAutoCount() {
