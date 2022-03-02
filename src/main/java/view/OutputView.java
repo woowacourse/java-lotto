e23936f9711707ef4362cb82e705f04857991b98
Juhyung990122 <harukawa99@naver.com>
2022-03-02 21:38:22 +0900

    feat: 출력 형식 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,7 +7,7 @@ import dto.RanksDto;
 
 public class OutputView {
 
-    public static final String PURCHASE_QUANTITY_FORMAT = "%s개를 구매했습니다.";
+    public static final String PURCHASE_QUANTITY_FORMAT = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private static final int SECOND_RANK = 2;
     private static final String RESULT_MESSAGE = "당첨 통계";
     private static final String DELIMITER = "---------";
@@ -17,7 +17,7 @@ public class OutputView {
     private static final String INCOME_RATE_FORMAT = "총 수익률은 %.2f입니다.";
 
     public static void printPurchasedLotto(LottosDto lottosDto) {
-        System.out.println(String.format(PURCHASE_QUANTITY_FORMAT, lottosDto.getQuantity()));
+        System.out.println(String.format(PURCHASE_QUANTITY_FORMAT, lottosDto.getManualQuantity(), lottosDto.getAutoQuantity()));
         for (LottoDto lottoDto : lottosDto.getLottoDtos()) {
             printLottoNumbers(lottoDto);
         }
