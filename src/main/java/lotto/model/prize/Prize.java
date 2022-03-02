3def6d59c232396586c46b0a632e8dd151603bd2
pup-paw <wldusdl0310@gmail.com>
2022-03-03 02:20:11 +0900

    style: 오름차순으로 재정렬

--- src/main/java/lotto/model/prize/Prize.java
+++ src/main/java/lotto/model/prize/Prize.java
@@ -3,12 +3,12 @@ package lotto.model.prize;
 import java.util.Arrays;
 
 public enum Prize {
-	FIRST(6, 2_000_000_000, false),
-	SECOND(5, 30_000_000, true),
-	THIRD(5, 1_500_000, false),
-	FOURTH(4, 50_000, false),
+	NONE(0, 0, false),
 	FIFTH(3, 5_000, false),
-	NONE(0, 0, false);
+	FOURTH(4, 50_000, false),
+	THIRD(5, 1_500_000, false),
+	SECOND(5, 30_000_000, true),
+	FIRST(6, 2_000_000_000, false);
 
 	private final int matchCount;
 	private final int amount;
