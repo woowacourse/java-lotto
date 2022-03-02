a59b8d50bb9c8cd65143cd2739d74047fe224669
leo0842 <kunga077@gmail.com>
2022-03-02 15:48:15 +0900

    feat: 중복되는 로직 interface 로 일원화

--- src/main/java/util/LottoNumberGenerator.java
+++ src/main/java/util/LottoNumberGenerator.java
@@ -1,10 +1,25 @@
 package util;
 
+import static domain.Lotto.LOTTO_NUMBER_MAXIMUM;
+import static domain.Lotto.LOTTO_NUMBER_MINIMUM;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public interface LottoNumberGenerator {
 
+    List<Integer> CANDIDATE_NUMBERS = IntStream.range(LOTTO_NUMBER_MINIMUM, LOTTO_NUMBER_MAXIMUM + 1)
+            .boxed()
+            .collect(Collectors.toList());
     int LOTTO_NUMBER_LENGTH = 6;
 
-    List<Integer> generate();
+    default List<Integer> generate() {
+        Collections.shuffle(CANDIDATE_NUMBERS);
+        return CANDIDATE_NUMBERS.stream()
+                .limit(LOTTO_NUMBER_LENGTH)
+                .sorted()
+                .collect(Collectors.toList());
+    };
 }
--- src/main/java/util/ManualAndAutoNumberGenerator.java
+++ src/main/java/util/ManualAndAutoNumberGenerator.java
@@ -1,20 +1,13 @@
 package util;
 
-import static domain.Lotto.LOTTO_NUMBER_MAXIMUM;
-import static domain.Lotto.LOTTO_NUMBER_MINIMUM;
-
 import domain.ManualLotto;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class ManualAndAutoNumberGenerator implements LottoNumberGenerator {
 
-    private static final List<Integer> CANDIDATE_NUMBERS = IntStream.range(LOTTO_NUMBER_MINIMUM, LOTTO_NUMBER_MAXIMUM + 1)
-            .boxed()
-            .collect(Collectors.toList());
     private final Iterator<ManualLotto> manualLottoIterator;
 
     public ManualAndAutoNumberGenerator(List<ManualLotto> manualLottos) {
@@ -25,10 +18,6 @@ public class ManualAndAutoNumberGenerator implements LottoNumberGenerator {
         if (manualLottoIterator.hasNext()) {
             return manualLottoIterator.next().getManualLottoNumbers();
         }
-        Collections.shuffle(CANDIDATE_NUMBERS);
-        return CANDIDATE_NUMBERS.stream()
-                .limit(LOTTO_NUMBER_LENGTH)
-                .sorted()
-                .collect(Collectors.toList());
+        return LottoNumberGenerator.super.generate();
     }
 }
