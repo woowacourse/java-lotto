768cc673a86231f6585badbae9eb67d20c2ce862
skullKim <kyk990328@gmail.com>
2022-03-02 16:46:01 +0900

    refactor(InputView.java): 중복 제거
    중복되는 로직 제거
    
    BREAKING CHANGE: 데이터를 입력 받고 유효하지 않을 경우 재입력 받는
    로직에 중복이 발생해 중복 제거
    as-is:
    public class InputView {
    
        ...
    
        public static int inputValidMoney() {
            final String money = inputMoney();
            ...
        }
    
        private static String inputMoney() {
            try {
                System.out.println(LotteryMessage.INPUT_PURCHASE_AMOUNT);
                final String money = scanner.nextLine();
                InputValidator.validateMoney(money);
                return money;
            } catch (IllegalArgumentException exception) {
                System.out.println(exception.getMessage());
                return inputMoney();
            }
        }
    
        public static List<Integer> inputValidLotteryNumber() {
            final String numbers = inputLotteryNumber();
            ...
        }
    
        private static String inputLotteryNumber() {
            try {
                System.out.println(LotteryMessage.LAST_WEEK_WINNING_NUMBERS);
                final String winningNumber = scanner.nextLine();
                InputValidator.validateWinningNumber(winningNumber);
                return winningNumber;
            } catch (IllegalArgumentException exception) {
                System.out.println(exception.getMessage());
                return inputLotteryNumber();
            }
        }
    
        public static int inputValidBonusNumber() {
            return Integer.parseInt(inputBonusNumber());
        }
    
        private static String inputBonusNumber() {
            try {
                System.out.println(LotteryMessage.LAST_WEEK_BONUS_NUMBER);
                final String bonusNumber = scanner.nextLine();
                InputValidator.validateNumber(bonusNumber);
                return bonusNumber;
            } catch (IllegalArgumentException exception) {
                System.out.println(exception.getMessage());
                return inputBonusNumber();
            }
        }
    }
    
    to-be:
    public class InputView {
    
        ...
    
        public static int inputValidMoney() {
            final String money = inputData(LotteryMessage.INPUT_PURCHASE_AMOUNT, InputValidator::validateMoney);
            ...
        }
    
        public static List<Integer> inputValidLotteryNumber() {
            final String numbers = inputData(LotteryMessage.LAST_WEEK_WINNING_NUMBERS, InputValidator::validateWinningNumber);
            ...
        }
    
        public static int inputValidBonusNumber() {
            return Integer.parseInt(inputData(LotteryMessage.LAST_WEEK_BONUS_NUMBER, InputValidator::validateNumber));
        }
    
        private static String inputData(final String inputMessage, final Consumer<String> validation) {
            try {
                System.out.println(inputMessage);
                final String inputtedData = scanner.nextLine();
                validation.accept(inputtedData);
                return inputtedData;
            } catch (IllegalArgumentException exception) {
                System.out.println(exception.getMessage());
                return inputData(inputMessage, validation);
            }
        }
    
    }

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,6 +3,7 @@ package view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import utils.InputValidator;
@@ -14,55 +15,33 @@ public class InputView {
 	private static final String WINNING_NUMBER_DISTRIBUTOR = ",";
 
 	public static int inputValidMoney() {
-		final String money = inputMoney();
+		final String money = inputData(LotteryMessage.INPUT_PURCHASE_AMOUNT, InputValidator::validateMoney);
 		return Integer.parseInt(money);
 	}
 
-	private static String inputMoney() {
-		try {
-			System.out.println(LotteryMessage.INPUT_PURCHASE_AMOUNT);
-			final String money = scanner.nextLine();
-			InputValidator.validateMoney(money);
-			return money;
-		} catch (IllegalArgumentException exception) {
-			System.out.println(exception.getMessage());
-			return inputMoney();
-		}
-	}
-
 	public static List<Integer> inputValidLotteryNumber() {
-		final String numbers = inputLotteryNumber();
+		final String numbers = inputData(LotteryMessage.LAST_WEEK_WINNING_NUMBERS, InputValidator::validateWinningNumber);
 		return Arrays.stream(numbers.split(WINNING_NUMBER_DISTRIBUTOR))
 			.map(String::trim)
 			.map(Integer::parseInt)
 			.collect(Collectors.toList());
 	}
 
-	private static String inputLotteryNumber() {
-		try {
-			System.out.println(LotteryMessage.LAST_WEEK_WINNING_NUMBERS);
-			final String winningNumber = scanner.nextLine();
-			InputValidator.validateWinningNumber(winningNumber);
-			return winningNumber;
-		} catch (IllegalArgumentException exception) {
-			System.out.println(exception.getMessage());
-			return inputLotteryNumber();
-		}
-	}
-
 	public static int inputValidBonusNumber() {
-		return Integer.parseInt(inputBonusNumber());
+		return Integer.parseInt(inputData(LotteryMessage.LAST_WEEK_BONUS_NUMBER, InputValidator::validateNumber));
 	}
 
-	private static String inputBonusNumber() {
+	private static String inputData(final String inputMessage, final Consumer<String> validation) {
 		try {
-			System.out.println(LotteryMessage.LAST_WEEK_BONUS_NUMBER);
-			final String bonusNumber = scanner.nextLine();
-			InputValidator.validateNumber(bonusNumber);
-			return bonusNumber;
+			System.out.println(inputMessage);
+			final String inputtedData = scanner.nextLine();
+			validation.accept(inputtedData);
+			return inputtedData;
 		} catch (IllegalArgumentException exception) {
 			System.out.println(exception.getMessage());
-			return inputBonusNumber();
+			return inputData(inputMessage, validation);
 		}
 	}
+
+
 }
