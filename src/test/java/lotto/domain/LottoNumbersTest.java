829c37d078294b2bbe0657fe430dbc5763974fe9
yeon-06 <rnjstldus2@hs.ac.kr>
2022-03-02 12:48:48 +0900

    test: given, when, then 구분

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -17,7 +17,10 @@ public class LottoNumbersTest {
     @Test
     @DisplayName("중복된 숫자로 생성시 예외 발생")
     void incorrect() {
+        // given
         List<Integer> numbers = getNumbers(2, 2, 3, 4, 5, 6);
+
+        // then
         assertThatThrownBy(() -> new LottoNumbers(numbers)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 개수는 중복이 불가능합니다.");
     }
@@ -25,7 +28,10 @@ public class LottoNumbersTest {
     @Test
     @DisplayName("숫자가 6개 미만")
     void incorrect4() {
+        // given
         List<Integer> numbers = getNumbers(2, 5, 6);
+
+        // then
         assertThatThrownBy(() -> new LottoNumbers(numbers)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 개수는 6개로 제한됩니다.");
     }
@@ -33,19 +39,24 @@ public class LottoNumbersTest {
     @Test
     @DisplayName("중복된 숫자가 없는 경우")
     void correct() {
+        // given
         List<Integer> numbers = getNumbers(1, 2, 3, 4, 5, 6);
+
+        // then
         assertThatNoException().isThrownBy(() -> new LottoNumbers(numbers));
     }
 
     @Test
     @DisplayName("equals 확인")
     void test() {
+        // given
         List<Integer> numbers1 = getNumbers(1, 2, 3, 4, 5, 6);
         List<Integer> numbers2 = getNumbers(5, 2, 1, 3, 4, 6);
 
         LottoNumbers lottoNumbers1 = new LottoNumbers(numbers1);
         LottoNumbers lottoNumbers2 = new LottoNumbers(numbers2);
 
+        // then
         assertThat(lottoNumbers1).isEqualTo(lottoNumbers2);
     }
 
