8c5098017905e11e02e1f6149b46cf687a2c30a6
Hongdonggeon <ghd700@naver.com>
2022-03-02 23:00:59 +0900

    refactor : Interface default 메서드 추가
    이전에는 adapter 클래스를 추가하여 오버라이딩하고 싶은 메서드만 골라 오버라이딩하였다.
    기존의 로또 자동 생성기능은 추상 메서드로 구현하고 새로 추가된 로또 수동생성 메서드는 default 메서드로 구현
    AutoLottoGenerator와 ManualLottoGenerator에서 알맞지 않은 기능을 사용할시 예외 발생

--- src/main/java/domain/LottoGenerator/AutoLottoGenerator.java
+++ src/main/java/domain/LottoGenerator/AutoLottoGenerator.java
@@ -9,9 +9,10 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class AutoLottoGenerator extends LottoGeneratorAdpater {
+public class AutoLottoGenerator implements LottoGenerator {
 
     private static final int LOTTO_SIZE = 6;
+    private static final String MANUAL_LOTTO_GENERATE_LIMIT = "자동 로또 생성기에서는 수동 로또 생성을 할 수 없습니다.";
 
     @Override
     public Lotto generateLotto() {
@@ -24,5 +25,10 @@ public class AutoLottoGenerator extends LottoGeneratorAdpater {
                 .collect(Collectors.toList());
 
         return new Lotto(collect);
+    }
+
+    @Override
+    public Lotto generateLotto(List<Integer> numbers) {
+        throw new IllegalArgumentException(MANUAL_LOTTO_GENERATE_LIMIT);
     }
 }
--- src/main/java/domain/LottoGenerator/LottoGenerator.java
+++ src/main/java/domain/LottoGenerator/LottoGenerator.java
@@ -1,12 +1,20 @@
 package domain.LottoGenerator;
 
 import domain.Lotto.Lotto;
+import domain.Lotto.LottoNumber;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface LottoGenerator {
 
     Lotto generateLotto();
 
-    Lotto generateLotto(List<Integer> numbers);
+    default Lotto generateLotto(List<Integer> numbers){
+        List<LottoNumber> lottoNumbers = numbers.stream()
+                .map(LottoNumber::valueOf)
+                .collect(Collectors.toList());
+
+        return new Lotto(lottoNumbers);
+    }
 }
