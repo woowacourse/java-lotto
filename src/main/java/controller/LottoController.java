66bfdf0c48beeda6c6c56424b4af89d2acfe558f
Juhyung990122 <harukawa99@naver.com>
2022-03-02 20:02:56 +0900

    refactor: 주석 삭제

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -33,31 +33,5 @@ public class LottoController {
         OutputView.printResult(new RanksDto(player.calculateIncomeRate(results)));
 
     }
-//
-//    public LottosDto purchase(int purchaseAmount) {
-//        player = new Player(new Money(purchaseAmount));
-//        player.purchaseLotto(new AutoLottoGenerator(), LottoNumberFactory.makeBoundary());
-//        return LottosDto.from(player.getLottos());
-//    }
-//
-//    public Game run(){
-//        {
-//        LottosDto lottosDto = lottoController.purchase(InputView.inputPurchaseAmount());
-//        OutputView.printPurchasedLotto(lottosDto);
-//
-//        lottoController.determineWinningNumber(InputView.inputWinningNumber(), InputView.inputBonusBall());
-//
-//        List<Result> results = lottoController.judgeLottos();
-//        OutputView.printResult(lottoController.makeResult(results));
-//    }
-//    public List<Result> judgeLottos() {
-//        return player.judgeAll(winningLotto);
-//    }
-//
-//    public RanksDto makeResult(List<Result> judgeLottos) {
-//        double totalIncome = Rank.calculateAllResult(judgeLottos);
-//        double incomeRate = player.calculateIncomeRate(totalIncome);
-//
-//        return new RanksDto(incomeRate);
-//    }
+
 }
