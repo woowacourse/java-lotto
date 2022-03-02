00eb140f57dd06b2c0a8a712f564da4d664347eb
is2js <tingstyle1@gmail.com>
2022-03-02 14:25:56 +0900

    feat: 입력한 수만큼 반복해서 수동 로또를 입력받는 기능 구현
    - 만약 반복 횟수가 0이라면, IntStream.collect(Collectors.toList())는 [] 빈 List를 반환해준다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import utils.Validator;
 
 public class InputView {
@@ -13,9 +14,9 @@ public class InputView {
     private static final String INPUT_MONEY_INSTRUCTION = "구입금액을 입력해 주세요.";
     private static final String INPUT_WIN_LOTTO_INSTRUCTION = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_INSTRUCTION = "보너스 볼을 입력해 주세요.";
-
-    private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_MANUAL_COUNT_INSTRUCTION = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String INPUT_MANUAL_LOTTO_INSTRUCTION = "수동으로 구매할 번호를 입력해 주세요.";
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private static String getInput() {
         return SCANNER.nextLine();
@@ -29,14 +30,26 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
-    public List<String> getLastWinLotto() {
+    public List<List<String>> getManualLotto(final int manualCount) {
         System.out.println();
-        System.out.println(INPUT_WIN_LOTTO_INSTRUCTION);
+        System.out.println(INPUT_MANUAL_LOTTO_INSTRUCTION);
+        return IntStream.range(0, manualCount)
+            .mapToObj(i -> getLotto())
+            .collect(Collectors.toList());
+    }
+
+    private List<String> getLotto() {
         final String input = getInput();
         Validator.checkNullOrEmpty(input);
         return Arrays.stream(input.split(INPUT_NUMBER_DELIMITER, INPUT_NUMBER_SPLIT_OPTION))
             .map(String::trim)
             .collect(Collectors.toList());
+    }
+
+    public List<String> getWinLotto() {
+        System.out.println();
+        System.out.println(INPUT_WIN_LOTTO_INSTRUCTION);
+        return getLotto();
     }
 
     public int getBonusNumber() {
