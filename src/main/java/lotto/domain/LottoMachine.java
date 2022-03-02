7b0e06ab034eff70d0d4d681b9b3b28a78806c98
awesomeo184 <awesomeo184@gmail.com>
2022-03-02 11:12:51 +0900

    refactor(LottoMachine): 상수 중복을 제거

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,36 +1,19 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoMachine {
 
-    private static final int LOTTO_NUMBER_START_INDEX = 0;
-    private static final int LOTTO_NUMBER_END_INDEX = 6;
-    private static final int LOTTO_NUMBER_LOWER_BOUND = 1;
-    private static final int LOTTO_NUMBER_UPPER_BOUND = 46;
-
-    private final List<LottoNumber> numbers;
     private final int totalTicketCount;
     private final int manualTicketCount;
 
     public LottoMachine(int totalTicketCount, int manualTicketCount) {
-        numbers = initNumbers();
-
         if (totalTicketCount < manualTicketCount) {
             throw new IllegalArgumentException("[ERROR] 최대 구입 가능 개수를 넘었습니다. 최대 구입 가능 개수 = " + totalTicketCount);
         }
         this.totalTicketCount = totalTicketCount;
         this.manualTicketCount = manualTicketCount;
-    }
-
-    private List<LottoNumber> initNumbers() {
-        List<LottoNumber> numbers = new ArrayList<>();
-        for (int i = LOTTO_NUMBER_LOWER_BOUND; i < LOTTO_NUMBER_UPPER_BOUND; i++) {
-            numbers.add(new LottoNumber(i));
-        }
-        return numbers;
     }
 
     public List<Lotto> makeLottoTickets(List<Lotto> manualLottos) {
@@ -47,8 +30,7 @@ public class LottoMachine {
     }
 
     private Lotto makeLottoTicket() {
-        Collections.shuffle(numbers);
-        return new Lotto(numbers.subList(LOTTO_NUMBER_START_INDEX, LOTTO_NUMBER_END_INDEX));
+        return new Lotto(LottoNumber.getShuffledNumbers());
     }
 
     public int getManualTicketCount() {
