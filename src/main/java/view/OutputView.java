dec0d86a68bcc1bfc1cf4f0e2e1ede903ef182a4
HyeonbinSa <shb1833@gmail.com>
2022-03-02 22:23:29 +0900

    refactor: OutputView 미사용 상수 및 메서드 제거

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,17 +8,12 @@ import domain.UserCount;
 import java.util.EnumMap;
 
 public class OutputView {
-
-    private static final String COUNT_MESSAGE = "%d개를 구매했습니다.\n";
+    
     private static final String TOTAL_COUNT_MESSAGE = "\n수동으로 %d장, 자동으로 %d개를 구매했습니다.\n";
     private static final String STATISTIC_RESULT_MESSAGE = "\n당첨 통계\n---------";
     private static final String WINNING_MESSAGE = "%d개 일치 (%d원)- %d\n";
     private static final String SECOND_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원)- %d개\n";
     private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.2f입니다.\n";
-
-    public static void printCountOfLotto(int count) {
-        System.out.printf(COUNT_MESSAGE, count);
-    }
 
     public static void printLottos(Lottos lottos, UserCount userCount) {
         System.out.printf(TOTAL_COUNT_MESSAGE, userCount.getManualCount(), userCount.getAutoCount());
