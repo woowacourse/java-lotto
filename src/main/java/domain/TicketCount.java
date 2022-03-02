e33decfb2242f7363fa337b525c89be56e52edbc
유영우 <fbduddn97@naver.com>
2022-03-02 19:46:13 +0900

    refactor: TicketCount 불필요한 코드 제거

--- src/main/java/domain/TicketCount.java
+++ src/main/java/domain/TicketCount.java
@@ -7,16 +7,13 @@ public class TicketCount {
 	private static final String INVALID_MONEY_ERROR = "구입 금액은 1000원 이상이어야 합니다.";
 	private static final String NEGATIVE_COUNT_ERROR = "구매할 로또 수는 음수일 수 없습니다.";
 	private static final String OVER_COUNT_ERROR = "구매할 로또 수는 구입 금액으로 살 수 있어야 합니다.";
-	private static final String COUNT_TICKETS_SIZE_NOT_EQUALS_ERROR = "구매할 개수와 입력받은 수동 로또 개수는 같아야 합니다.";
 
-	private final int manualCount;
 	private final int autoCount;
 
 	public TicketCount(final int money, final int manualCount) {
 		validateMoney(money);
 		validateManualCount(money, manualCount);
 
-		this.manualCount = manualCount;
 		this.autoCount = (money - (manualCount * TICKET_PRICE)) / TICKET_PRICE;
 	}
 
