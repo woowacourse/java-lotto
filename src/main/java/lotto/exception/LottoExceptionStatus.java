5e9cc46c75bdd9d302d79910430dbec97cd19479
sinb57 <sinb57@naver.com>
2022-03-02 14:17:49 +0900

    refactor: 예외 메시지에 TicketSize 값 사용

--- src/main/java/lotto/exception/LottoExceptionStatus.java
+++ src/main/java/lotto/exception/LottoExceptionStatus.java
@@ -2,6 +2,7 @@ package lotto.exception;
 
 import lotto.utils.BallNumberRange;
 import lotto.utils.MoneyUnit;
+import lotto.utils.TicketSize;
 
 public enum LottoExceptionStatus {
 
@@ -10,8 +11,8 @@ public enum LottoExceptionStatus {
     MONEY_MUST_BE_DIVISIBLE(String.format("구입 금액은 %d원으로 나누어 떨어져야 합니다.", MoneyUnit.DIVIDING_UNIT.getUnit())),
 
     TICKET_COUNT_MUST_BE_NUMERIC("로또 개수는 숫자여야 합니다."),
-
-    TICKET_NUMBERS_CANNOT_BE_OUT_OF_SIZE("로또 번호는 6개로 구성되어야 합니다."),
+    TICKET_NUMBERS_CANNOT_BE_OUT_OF_SIZE(
+            String.format("로또 번호는 %d개로 구성되어야 합니다.", TicketSize.DEFAULT_SIZE.getSize())),
     TICKET_NUMBERS_CANNOT_BE_DUPLICATED("로또 번호는 중복될 수 없습니다."),
 
     BALL_NUMBER_MUST_BE_NUMERIC("번호는 숫자여야 합니다."),
