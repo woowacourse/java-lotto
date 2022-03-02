cf4410f888c4f89524db26caf304576fcef0cdbc
yeon-06 <rnjstldus2@hs.ac.kr>
2022-03-02 12:28:43 +0900

    refactor: 인스턴스 캐싱 방식 변경
    - array -> map 활용
    - 미리 전부 생성해두지 않고 필요할 때마다 생성해서 저장

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
-import java.util.stream.IntStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LottoNumber {
     private static final int MIN = 1;
@@ -8,14 +9,9 @@ public class LottoNumber {
 
     private static final String NUMBER_RANGE_ERROR = "로또 숫자는 " + MIN + " 이상 " + MAX + " 이하의 숫자만 가능합니다.";
 
-    private static final LottoNumber[] cacheLottoNumber = new LottoNumber[MAX + 1];
+    private static final Map<Integer, LottoNumber> cacheLottoNumber = new HashMap<>();
 
     private final int value;
-
-    static {
-        IntStream.range(MIN, MAX + 1)
-                .forEach(i -> cacheLottoNumber[i] = new LottoNumber(i));
-    }
 
     private LottoNumber(int value) {
         this.value = value;
@@ -23,13 +19,22 @@ public class LottoNumber {
 
     public static LottoNumber of(int number) {
         validateNumber(number);
-        return cacheLottoNumber[number];
+        return generateLottoNumber(number);
     }
 
     private static void validateNumber(int number) {
         if (number < MIN || number > MAX) {
             throw new IllegalArgumentException(NUMBER_RANGE_ERROR);
         }
+    }
+
+    private static LottoNumber generateLottoNumber(int number) {
+        LottoNumber lottoNumber = cacheLottoNumber.get(number);
+        if (lottoNumber == null) {
+            lottoNumber = new LottoNumber(number);
+            cacheLottoNumber.put(number, lottoNumber);
+        }
+        return lottoNumber;
     }
 
     public int getValue() {
