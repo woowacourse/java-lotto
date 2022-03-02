28b8bd91748c9a2f9f3d60b89b75f752f0a4f4ba
pup-paw <wldusdl0310@gmail.com>
2022-03-02 15:23:35 +0900

    refactor: 패키지 이동 후 불필요한 import 문 삭제

--- src/test/java/lotto/model/MoneyTest.java
+++ src/test/java/lotto/model/MoneyTest.java
@@ -1,9 +1,7 @@
-package lotto;
+package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import lotto.model.Money;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
