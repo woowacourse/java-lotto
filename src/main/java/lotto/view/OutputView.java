e33984b3cb6f02cc6e637f666f4fdef13ac6cd11
yoon <duddbs774@gmail.com>
2022-03-02 13:03:08 +0900

    refactor: 로또 티켓 개수 출력 기능 리펙토링

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.domain.AmountToBuyLotto;
 import lotto.domain.LottoNumber;
 import lotto.domain.LottoTickets;
+import lotto.domain.ManualLottoCount;
 import lotto.domain.Ranking;
 import lotto.domain.WinningResult;
 import lotto.domain.lottoticket.LottoTicket;
@@ -15,7 +17,6 @@ public class OutputView {
     private static final String CLOSE_BRACKET = "]";
     private static final String BLANK = " ";
     private static final String DELIMITER = ",";
-    private static final String TICKET_PURCHASE_SENTENCE = "개를 구매했습니다.";
     private static final String NOT_INSTANTIATION_ERROR = "OutputView 객체를 생성 할 수 없습니다.";
     private static final String RESULT_PREFIX = "당첨 통계";
     private static final String RESULT_SEPARATION_LINE = "---------";
@@ -24,13 +25,24 @@ public class OutputView {
     private static final String SECOND_RANKING_SUFFIX = "보너스 볼 일치";
     private static final String PRIZE_AND_HIT_COUNT_FORMAT = "(%d원)- %d개%n";
     private static final String TOTAL_PROFIT_FORMAT = "총 수익률은 %.2f 입니다.";
+    private static final String MANUAL_TICKET_COUNT_PREFIX = "수동으로";
+    private static final String COUNT_UNIT = "장";
+    private static final String AUTO_TICKET_COUNT_PREFIX = "자동으로";
+    private static final String TICKET_COUNT_SUFFIX = "을 구매했습니다.";
 
     private OutputView() throws InstantiationException {
         throw new InstantiationException(NOT_INSTANTIATION_ERROR);
     }
 
-    public static void printTicketCount(int ticketCount) {
-        System.out.println(ticketCount + TICKET_PURCHASE_SENTENCE);
+    public static void printTicketCount(AmountToBuyLotto amount, ManualLottoCount manualLottoCount) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(MANUAL_TICKET_COUNT_PREFIX + BLANK)
+                .append(manualLottoCount.getValue())
+                .append(COUNT_UNIT + DELIMITER + BLANK + AUTO_TICKET_COUNT_PREFIX + BLANK)
+                .append(amount.calculateAutomaticLottoCount(manualLottoCount))
+                .append(COUNT_UNIT + TICKET_COUNT_SUFFIX);
+
+        System.out.println(stringBuilder);
     }
 
     public static void printTicket(LottoTickets lottoTickets) {
