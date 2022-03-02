232f3ecda4891fdba1d0f2adea593327f6eff63b
yukong <kyb062666@gmail.com>
2022-03-02 12:45:31 +0900

    refactor: 랜덤 로또 생성 로직을 LottoGenerator 로 이동

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,11 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
-    private static final int BALL_COUNT = 6;
+    public static final int BALL_COUNT = 6;
     public static final int LOTTO_PRICE = 1000;
     private static final String ERROR_BALL_COUNT = BALL_COUNT + "개의 숫자를 입력해주세요";
     private static final String ERROR_DUPLICATED_NUMBER = "번호가 중복됩니다!";
@@ -25,21 +23,6 @@ public class Lotto {
 
     public boolean contains(final Ball number) {
         return lotto.contains(number);
-    }
-
-    // TODO: Lotto 의 역할인가?
-    public static List<Ball> selectRandomBalls() {
-        List<Ball> lottoBalls = new ArrayList<>(Ball.getTotalBalls());
-        Collections.shuffle(lottoBalls);
-
-        List<Ball> selectedBalls = selectBalls(lottoBalls);
-        return selectedBalls.stream()
-            .sorted(Comparator.comparing(Ball::getNumber))
-            .collect(Collectors.toList());
-    }
-
-    private static ArrayList<Ball> selectBalls(final List<Ball> lottoBalls) {
-        return new ArrayList<>(lottoBalls.subList(0, BALL_COUNT));
     }
 
     private void validateLotto(final List<Ball> lotto) {
--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
 
@@ -10,8 +13,22 @@ public class LottoGenerator {
     public static Lottos pickAutoLottos(final int lottoCount) {
         List<Lotto> lottos = new ArrayList<>(lottoCount);
         for (int i = 0; i < lottoCount; i++) {
-            lottos.add(new Lotto(Lotto.selectRandomBalls()));
+            lottos.add(new Lotto(selectRandomBalls()));
         }
         return new Lottos(lottos);
+    }
+
+    private static List<Ball> selectRandomBalls() {
+        List<Ball> lottoBalls = new ArrayList<>(Ball.getTotalBalls());
+        Collections.shuffle(lottoBalls);
+
+        List<Ball> selectedBalls = selectBalls(lottoBalls);
+        return selectedBalls.stream()
+                .sorted(Comparator.comparing(Ball::getNumber))
+                .collect(Collectors.toList());
+    }
+
+    private static ArrayList<Ball> selectBalls(final List<Ball> lottoBalls) {
+        return new ArrayList<>(lottoBalls.subList(0, Lotto.BALL_COUNT));
     }
 }
