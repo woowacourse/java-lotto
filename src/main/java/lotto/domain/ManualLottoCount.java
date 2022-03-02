b7a65d2913fb2cdb03015f9efe015315aee83134
yeon-06 <rnjstldus2@hs.ac.kr>
2022-03-02 14:20:00 +0900

    feat: 수동 로또 개수 클래스 생성

---
+++ src/main/java/lotto/domain/ManualLottoCount.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+public class ManualLottoCount {
+    private static final String LOTTO_COUNT_ERROR = "구매 최대 개수는 %s개 입니다.";
+    private static final int MIN_NATURAL_NUMBER = 0;
+
+    private int value;
+
+    public ManualLottoCount(int value, int max) {
+        validation(value, max);
+        this.value = value;
+    }
+
+    private void validation(int value, int max) {
+        if (value < MIN_NATURAL_NUMBER || value > max) {
+            throw new IllegalArgumentException(String.format(LOTTO_COUNT_ERROR, max));
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ManualLottoCount)) {
+            return false;
+        }
+
+        ManualLottoCount that = (ManualLottoCount) o;
+
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
+    }
+}
---
+++ src/test/java/lotto/domain/ManualLottoCountTest.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ManualLottoCountTest {
+    @Test
+    @DisplayName("유효한 개수")
+    public void correct() {
+        // given
+        int max = 10;
+
+        // when
+        int tryCount = 9;
+
+        // then
+        assertDoesNotThrow(() -> new ManualLottoCount(tryCount, max));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 100})
+    @DisplayName("숫자가 범위 내에 존재하지 않는 경우 예외 발생")
+    void incorrect(int tryCount) {
+        // given
+        int max = 10;
+
+        // then
+        assertThatThrownBy(() -> new ManualLottoCount(tryCount, max)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
