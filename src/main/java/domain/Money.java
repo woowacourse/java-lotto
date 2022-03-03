85e822b4e36fb4181de85e8a96754e0023971292
Gyuchool <balance0591@naver.com>
2022-03-03 23:05:07 +0900

    refactor: 에러 메세지 상수화

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class Money {
 
     private static final String MIN_UNIT_MESSAGE = "[ERROR] 구입 금액은 1000원 단위이어야 합니다.";
+    private static final String LOTTO_COUNT_ERROR = "[ERROR] 구매할 로또 수를 다시 입력해주세요.";
     private static final int UNIT = 1000;
 
     private final int money;
@@ -30,7 +31,7 @@ public class Money {
 
     public void validateCountRange(int maxLottoCount, int inputCount) {
         if (inputCount < 0 || inputCount > maxLottoCount) {
-            throw new IllegalArgumentException("구매할 로또 수를 다시 입력해주세요.");
+            throw new IllegalArgumentException(LOTTO_COUNT_ERROR);
         }
     }
 
