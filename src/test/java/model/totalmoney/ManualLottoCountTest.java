8a6c7b30329343f67579ef60e0daf104b3ea144d
koo <gudonghee2000@gmail.com>
2022-03-03 17:31:10 +0900

    test: 수동 구매 로또 개수를 검증하는 테스트 코드 작성

--- src/test/java/model/totalmoney/ManualLottoCountTest.java
+++ src/test/java/model/totalmoney/ManualLottoCountTest.java
@@ -13,7 +13,7 @@ class ManualLottoCountTest {
     @Test
     @DisplayName("수동으로 구매할 로또 개수 객체를 생성한다.")
     void generateManualLottoCount() {
-        ManualLottoCount manualLottoCount = new ManualLottoCount(3, 10);
+        ManualLottoCount manualLottoCount = new ManualLottoCount(3);
 
         assertThat(manualLottoCount.getCount()).isEqualTo(3);
     }
@@ -22,17 +22,8 @@ class ManualLottoCountTest {
     @ValueSource(ints = {-1, -34})
     @DisplayName("수동으로 구매할 로또 개수가 0개보다 적으면 오류를 발생한다.")
     void checkValidCount_UnderMinCount(int count) {
-        assertThatThrownBy(() -> new ManualLottoCount(count, 10))
+        assertThatThrownBy(() -> new ManualLottoCount(count))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 입력한 수동 구매 로또 개수가 최소로 구매할 개수보다 작습니다.");
-    }
-
-    @Test
-    @DisplayName("수동으로 구매할 로또 개수가 최대로 구매 가능한 로또 개수보다 많으면 오류를 발생한다.")
-    void checkValidCount_OverMaxCount() {
-        int purchaseLottoCount = 10;
-        assertThatThrownBy(() -> new ManualLottoCount(11, purchaseLottoCount))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 입력한 수동 구매 로또 개수가 최대로 구매할 수 있는 개수보다 많습니다.");
+                .hasMessage("[ERROR] 입력한 수동 구매 로또 개수가 음수입니다.");
     }
 }
