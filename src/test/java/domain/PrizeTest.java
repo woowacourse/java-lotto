22dbeac7a25a46388048564f7f906f9e235d6d65
DWL21 <nggus5@naver.com>
2022-03-04 01:37:47 +0900

    test: 일치하는 당첨 번호 갯수에 해당하는 상금 반환 테스트 등수마다 추가

--- src/test/java/domain/PrizeTest.java
+++ src/test/java/domain/PrizeTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
