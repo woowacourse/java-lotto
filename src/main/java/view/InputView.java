28676c65dd7584d106e67e0cae5db13788c20883
devHudi <devhudi@gmail.com>
2022-03-03 15:33:31 +0900

    refactor(input-view): 입력 형태 검증 및 에러 메세지 개선

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,9 +10,13 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
+    private static final String NUMBERS_INPUT_FORMAT_REGEX = ".(.)?, .(.)?, .(.)?, .(.)?, .(.)?, .(.)?";
     private static final String SPLIT_DELIMITER = ", ";
 
     private static final String ERROR_MESSAGE_FOR_INVALID_NUMBER = "숫자만 입력해주세요.";
+    private static final String ERROR_MESSAGE_FOR_INVALID_NUMBERS_INPUT_FORMAT = "잘못된 숫자 입력 형식입니다. '1, 2, 3, 4, 5, 6' 의 형태로 입력해주세요.";
+    private static final String ERROR_MESSAGE_FOR_EMPTY_INPUT = "빈 문자열을 입력할 수 없습니다.";
+
     private static final String MESSAGE_TO_GET_INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String MESSAGE_FOR_MANUAL_LOTTO_QUANTITY = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String MESSAGE_FOR_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
@@ -51,22 +55,46 @@ public class InputView {
     }
 
     private static int scanNumber() {
+        String input = scanInput();
+
         try {
-            return Integer.parseInt(SCANNER.nextLine());
+            return Integer.parseInt(input);
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FOR_INVALID_NUMBER);
         }
     }
 
     private static Set<Integer> scanLottoNumbers() {
+        String input = scanInput();
+        validateNumbersInputFormat(input);
+
         try {
-            return convertLottoNumbersInputToSet(SCANNER.nextLine());
+            return generateLottoNumbersFromInput(input);
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FOR_INVALID_NUMBER);
         }
     }
 
-    private static Set<Integer> convertLottoNumbersInputToSet(String input) {
+    private static void validateNumbersInputFormat(String input) {
+        if (!input.matches(NUMBERS_INPUT_FORMAT_REGEX)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_INVALID_NUMBERS_INPUT_FORMAT);
+        }
+    }
+
+    private static String scanInput() {
+        String input = SCANNER.nextLine();
+        validateEmpty(input);
+
+        return input;
+    }
+
+    private static void validateEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_EMPTY_INPUT);
+        }
+    }
+
+    private static Set<Integer> generateLottoNumbersFromInput(String input) {
         return Arrays.stream(input.split(SPLIT_DELIMITER))
                 .map(Integer::parseInt)
                 .collect(Collectors.toSet());
