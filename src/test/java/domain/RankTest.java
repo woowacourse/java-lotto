480eba78be872f77146798d0d1c8e8d186164873
devHudi <devhudi@gmail.com>
2022-03-03 16:14:47 +0900

    test(rank): 클래스에 테스트 이름 추가

--- src/test/java/domain/RankTest.java
+++ src/test/java/domain/RankTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+@DisplayName("Rank 테스트")
 public class RankTest {
     @ParameterizedTest(name = "구매 로또: [{0}, {1}, {2}, {3}, {4}, {5}], 기대 순위: {6}")
     @CsvSource(value = {
