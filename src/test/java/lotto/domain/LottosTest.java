9dee2c800da4d5f7cbe6d190a6e74901bacb0e21
summerlunaa <summerlunaa9@gmail.com>
2022-03-04 01:46:01 +0900

    refactor: stream 사용

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +14,9 @@ public class LottosTest {
     @Test
     @DisplayName("원하는 만큼 수동 + 자동으로 로또티켓을 구입")
     void buyLottos() {
-        List<List<Integer>> manualLottos = Arrays.asList(
-                Arrays.asList(1, 2, 3, 4, 5, 6),
-                Arrays.asList(1, 2, 3, 4, 5, 6),
-                Arrays.asList(1, 2, 3, 4, 5, 6)
-        );
+        List<List<Integer>> manualLottos = IntStream.range(0, 3)
+                .mapToObj(i -> Arrays.asList(1, 2, 3, 4, 5, 6))
+                .collect(Collectors.toList());
         Lottos lottos = new Lottos(manualLottos, 7);
         assertThat(lottos.getLottos().size()).isEqualTo(10);
     }
