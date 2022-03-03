474020e94d7394137415cef24c6cf1640101d04a
Juhyung990122 <harukawa99@naver.com>
2022-03-03 21:40:05 +0900

    refactor: 접근제어자 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,7 +7,7 @@ import dto.RanksDto;
 
 public class OutputView {
 
-    public static final String PURCHASE_QUANTITY_FORMAT = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
+    private static final String PURCHASE_QUANTITY_FORMAT = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private static final int SECOND_RANK = 2;
     private static final String RESULT_MESSAGE = "당첨 통계";
     private static final String DELIMITER = "---------";
