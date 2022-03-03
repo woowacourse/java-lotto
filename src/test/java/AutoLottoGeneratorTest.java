3d73a81d9d20863437d9fba83d3a699c5ac40f13
soominsohn <soominsohn@gmail.com>
2022-03-03 17:06:48 +0900

    test(autoLottoGenerator): add zero amount lotto generate test

--- src/test/java/AutoLottoGeneratorTest.java
+++ src/test/java/AutoLottoGeneratorTest.java
@@ -12,10 +12,23 @@ public class AutoLottoGeneratorTest {
     @Test
     @DisplayName("로또 amount만큼 로또 생성에 성공한다.")
     void lotto_generate() {
+
         LottoGenerator lottoGenerator = new AutoLottoGenerator(3);
+
         List<Lotto> lottos = lottoGenerator.generate();
 
         assertThat(lottos.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("로또 amount가 0이면 빈 리스트를 반환한다.")
+    void lotto_generate_empty() {
+
+        LottoGenerator lottoGenerator = new AutoLottoGenerator(0);
+
+        List<Lotto> lottos = lottoGenerator.generate();
+
+        assertThat(lottos.size()).isEqualTo(0);
     }
 
 }
