752d1389a31b3c158468e52a070997d2e8b2f19b
Hongdonggeon <ghd700@naver.com>
2022-03-03 15:29:34 +0900

    feat : 수동 로또 갯수에 대한 예외 메세지 추가

--- src/main/java/utils/ExceptionMessage.java
+++ src/main/java/utils/ExceptionMessage.java
@@ -10,4 +10,7 @@ public class ExceptionMessage {
 
     public static final String LOTTO_AND_BONUS_BALL_DUPLICATION = "당첨 번호와 보너스 볼은 중복될 수 없다.";
 
+    public static final String MANUAL_LOTTO_COUNT_IS_NOT_UNDER_ZERO = "수동으로 구매할 로또 수는 음수를 입력할 수 없습니다.";
+    public static final String MANUAL_LOTTO_PURCHASE_MONEY_EXCESS_TOTAL_MONEY = "수동 로또 구매 금액이 구입 금액을 초과하였습니다.";
+
 }
