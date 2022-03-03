e1315e90b929693e54dfdc26dc7e3517383bb15c
Hongdonggeon <ghd700@naver.com>
2022-03-03 17:14:20 +0900

    test : 수동 로또 갯수에 대한 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -10,11 +10,13 @@ import domain.player.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import utils.ExceptionMessage;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
 
@@ -52,5 +54,31 @@ class PlayerTest {
         int expected = 15;
 
         assertThat(actual.size()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Player의 총 수익을 계산한다.")
+    void calculateIncomeRateTest() {
+        double totalIncome = 15000;
+        double actual = player.calculateIncomeRate(totalIncome);
+        assertThat(actual).isEqualTo(1.0);
+    }
+
+    @Test
+    @DisplayName("수동 로또 갯수가 총 구매할 수 있는 로또 갯수를 초과할 경우 예외 발생")
+    void manualLottoCountExcessTotalCountTest() {
+        int manualLottoCount = 17;
+        assertThatThrownBy(() -> player.selectLottoCount(manualLottoCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.MANUAL_LOTTO_PURCHASE_MONEY_EXCESS_TOTAL_MONEY);
+    }
+
+    @Test
+    @DisplayName("수동 로또 갯수로 음수를 입력할 경우 예외 발생")
+    void manualLottoCountNegativeTest() {
+        int manualLottoCount = -1;
+        assertThatThrownBy(() -> player.selectLottoCount(manualLottoCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.MANUAL_LOTTO_COUNT_IS_NOT_UNDER_ZERO);
     }
 }
