4278c31b477070b87473f2a0f814e155e30cd7e3
Hongdonggeon <ghd700@naver.com>
2022-03-03 15:28:35 +0900

    feat : 구입 금액으로 구매할 수 있는 로또 총 갯수 반환하기

--- src/main/java/domain/player/Money.java
+++ src/main/java/domain/player/Money.java
@@ -33,4 +33,8 @@ public class Money {
     public boolean isBiggerThanLottoPrice() {
         return amount >= MINIMUM_PURCHASE_AMOUNT;
     }
+
+    public int calculateTotalLottoCount() {
+        return amount / MINIMUM_PURCHASE_AMOUNT;
+    }
 }
