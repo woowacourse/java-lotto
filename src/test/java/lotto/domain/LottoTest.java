f48d6923aec2a5323146e2909d44107461746edd
summerlunaa <summerlunaa9@gmail.com>
2022-03-03 15:05:01 +0900

    style: 포맷팅 적용

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -49,8 +49,8 @@ public class LottoTest {
     @DisplayName("당첨번호와 비교하여 알맞은 당첨 등수 반환")
     void getRank() {
         WinningNumbers winningNumbersInstance
-                = new WinningNumbers(new ArrayList<>(Arrays.asList(1,2,3,4,5,45)), 6);
-        Lotto lotto = Lotto.generateLottoByManual(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+                = new WinningNumbers(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 45)), 6);
+        Lotto lotto = Lotto.generateLottoByManual(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
         Assertions.assertThat(lotto.getRank(winningNumbersInstance)).isEqualTo(Rank.RANK_2);
     }
 }
