11edb63332ce6cc0aadc7c93ffd7ac50169c63ae
skullKim <kyk990328@gmail.com>
2022-03-03 14:55:55 +0900

    refactor(InputView.java): 입력 안내 메시지 출력 로직 간소화
    입력 안내 메시지를 출력하는 용도의 함수형 인터페이스를 정의해 입력 안내
    메시지 출력 로직 간소화
    
    BREAKING CHANGE: inputData 로직 수정
    InputMessage:
    @FunctionalInterface
    public interface InputMessage {
        void printInputMessage();
    }
    
    InputView:
    as-is:
    public class InputView {
    
        ...
    
        public static int inputValidMoney() {
            final String money = inputData(LotteryMessage.INPUT_PURCHASE_AMOUNT, InputValidator::validateMoney);
            ...
        }
    
        public static List<Integer> inputValidLotteryNumber() {
            final String numbers = inputData(LotteryMessage.LAST_WEEK_WINNING_NUMBERS,
                InputValidator::validateLottery);
            ...
        }
    
        public static int inputValidBonusNumber() {
            return Integer.parseInt(inputData(LotteryMessage.LAST_WEEK_BONUS_NUMBER, InputValidator::validateBonusNumber));
        }
    
        public static int inputTheNumberOfValidManualLottery() {
            return Integer.parseInt(inputData(LotteryMessage.THE_NUMBER_OF_MANUAL_LOTTERY_TO_PURCHASE,
                InputValidator::validateTheNumberOfManualLottery));
        }
    
        public static List<List<Integer>> inputValidManualLotteries(final int theNumberOfLottery) {
            ...
            for (int i = 0; i < theNumberOfLottery; i++) {
                final List<Integer> lottery = parseStringToIntegerList(
                    inputData("", InputValidator::validateLottery));
                ...
            }
            ...
        }
    
        ...
    
        private static String inputData(final String inputMessage, final Consumer<String> validation) {
            try {
                System.out.println(inputMessage);
                ...
            } catch (IllegalArgumentException exception) {
                ...
            }
        }
    
    }
    
    to-be:
    public class InputView {
    
        ...
    
        public static int inputValidMoney() {
            final String money = inputData(InputValidator::validateMoney,
                () -> System.out.println(LotteryMessage.INPUT_PURCHASE_AMOUNT));
            ...
        }
    
        public static List<Integer> inputValidLotteryNumber() {
            final String numbers = inputData(InputValidator::validateLottery,
                () -> System.out.println(LotteryMessage.LAST_WEEK_WINNING_NUMBERS));
            ...
        }
    
        public static int inputValidBonusNumber() {
            return Integer.parseInt(inputData(InputValidator::validateBonusNumber,
                () -> System.out.println(LotteryMessage.LAST_WEEK_BONUS_NUMBER)));
        }
    
        public static int inputTheNumberOfValidManualLottery() {
            return Integer.parseInt(inputData(InputValidator::validateTheNumberOfManualLottery,
                () -> System.out.println(LotteryMessage.THE_NUMBER_OF_MANUAL_LOTTERY_TO_PURCHASE)));
        }
    
        public static List<List<Integer>> inputValidManualLotteries(final int theNumberOfLottery) {
            ...
            for (int i = 0; i < theNumberOfLottery; i++) {
                final List<Integer> lottery = parseStringToIntegerList(
                    inputData(InputValidator::validateLottery, () -> {}));
                ...
            }
            ...
        }
    
       ...
    
        private static String inputData(final Consumer<String> validation, final InputMessage inputMessage) {
            try {
                inputMessage.printInputMessage();
                ...
            } catch (IllegalArgumentException exception) {
                ...
            }
        }
    
    }

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -16,23 +16,25 @@ public class InputView {
 	private static final String WINNING_NUMBER_DISTRIBUTOR = ",";
 
 	public static int inputValidMoney() {
-		final String money = inputData(LotteryMessage.INPUT_PURCHASE_AMOUNT, InputValidator::validateMoney);
+		final String money = inputData(InputValidator::validateMoney,
+			() -> System.out.println(LotteryMessage.INPUT_PURCHASE_AMOUNT));
 		return Integer.parseInt(money);
 	}
 
 	public static List<Integer> inputValidLotteryNumber() {
-		final String numbers = inputData(LotteryMessage.LAST_WEEK_WINNING_NUMBERS,
-			InputValidator::validateLottery);
+		final String numbers = inputData(InputValidator::validateLottery,
+			() -> System.out.println(LotteryMessage.LAST_WEEK_WINNING_NUMBERS));
 		return parseStringToIntegerList(numbers);
 	}
 
 	public static int inputValidBonusNumber() {
-		return Integer.parseInt(inputData(LotteryMessage.LAST_WEEK_BONUS_NUMBER, InputValidator::validateBonusNumber));
+		return Integer.parseInt(inputData(InputValidator::validateBonusNumber,
+			() -> System.out.println(LotteryMessage.LAST_WEEK_BONUS_NUMBER)));
 	}
 
 	public static int inputTheNumberOfValidManualLottery() {
-		return Integer.parseInt(inputData(LotteryMessage.THE_NUMBER_OF_MANUAL_LOTTERY_TO_PURCHASE,
-			InputValidator::validateTheNumberOfManualLottery));
+		return Integer.parseInt(inputData(InputValidator::validateTheNumberOfManualLottery,
+			() -> System.out.println(LotteryMessage.THE_NUMBER_OF_MANUAL_LOTTERY_TO_PURCHASE)));
 	}
 
 	public static List<List<Integer>> inputValidManualLotteries(final int theNumberOfLottery) {
@@ -40,7 +42,7 @@ public class InputView {
 		List<List<Integer>> manualLotteries = new ArrayList<>();
 		for (int i = 0; i < theNumberOfLottery; i++) {
 			final List<Integer> lottery = parseStringToIntegerList(
-				inputData("", InputValidator::validateLottery));
+				inputData(InputValidator::validateLottery, () -> {}));
 			manualLotteries.add(lottery);
 		}
 		return manualLotteries;
@@ -53,17 +55,16 @@ public class InputView {
 			.collect(Collectors.toList());
 	}
 
-	private static String inputData(final String inputMessage, final Consumer<String> validation) {
+	private static String inputData(final Consumer<String> validation, final InputMessage inputMessage) {
 		try {
-			System.out.println(inputMessage);
+			inputMessage.printInputMessage();
 			final String inputtedData = scanner.nextLine();
 			validation.accept(inputtedData);
 			return inputtedData;
 		} catch (IllegalArgumentException exception) {
 			System.out.println(exception.getMessage());
-			return inputData(inputMessage, validation);
+			return inputData(validation, inputMessage);
 		}
 	}
-
 
 }
