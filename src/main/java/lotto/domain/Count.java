62bba38cabc4f26dcb6a1095f9ff4f4132917782
Hyunta <mohai2618@gmail.com>
2022-03-03 16:18:10 +0900

    feat(Count): 로또 구매횟수가 음수로 입력됐을 때 예외처리

--- src/main/java/lotto/domain/Count.java
+++ src/main/java/lotto/domain/Count.java
@@ -5,8 +5,15 @@ public class Count {
     private final int autoCount;
 
     public Count(int manualCount, int autoCount) {
+        checkManualCount(manualCount);
         this.manualCount = manualCount;
         this.autoCount = autoCount;
+    }
+
+    private void checkManualCount(int manualCount) {
+        if (manualCount < 0) {
+            throw new IllegalArgumentException("[ERROR] 구매할 로또 수는 음수가 될 수 없습니다.");
+        }
     }
 
     public int getManualCount() {
