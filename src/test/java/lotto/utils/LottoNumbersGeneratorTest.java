87e1cd41a080f907436cc699adf2e9414c14d5b4
BeomsooKim <kbsat@naver.com>
2022-03-03 12:17:08 +0900

    test(LottoNumberGenerator): 수동 로또번호 생성 기능 테스트

--- src/test/java/lotto/utils/LottoNumbersGeneratorTest.java
+++ src/test/java/lotto/utils/LottoNumbersGeneratorTest.java
@@ -16,4 +16,15 @@ class LottoNumbersGeneratorTest {
 
         assertThat(lottoNumbers).hasSize(6);
     }
+
+    @DisplayName("generateManualLottoNumbers 메서드는 입력받은 Integer 리스트를 LottoNumber 리스트로 변환한다.")
+    @Test
+    void generateManualLottoNumbers() {
+        // given
+        List<LottoNumber> lottoNumbers = LottoNumbersGenerator.generateManualLottoNumbers(List.of(1, 2, 3, 4, 5, 6));
+        List<LottoNumber> result = List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6));
+        // then
+        assertThat(lottoNumbers).isEqualTo(result);
+    }
 }
