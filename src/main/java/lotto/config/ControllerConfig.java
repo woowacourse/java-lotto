74649279934a10061749afe864ef5d4d6f5eb1ed
cjlee38 <cjlee38@github.com>
2022-03-03 20:39:57 +0900

    feat(controller): 수동 구매 컨트롤러 추가

--- src/main/java/lotto/config/ControllerConfig.java
+++ src/main/java/lotto/config/ControllerConfig.java
@@ -2,8 +2,14 @@ package lotto.config;
 
 import lotto.controller.AutoPurchaseController;
 import lotto.controller.InputWinningController;
+import lotto.controller.ManualPurchaseController;
 
 public class ControllerConfig {
+
+    public static ManualPurchaseController getManualPurchaseController() {
+        return ManualPurchaseController.getInstance();
+    }
+
     public static AutoPurchaseController getAutoPurchaseController() {
         return AutoPurchaseController.getInstance();
     }
