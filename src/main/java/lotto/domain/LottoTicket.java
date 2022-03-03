141a8a1a113c3abcf2735b17268c2204755d9876
YuJeong <yjfflq4872@gmail.com>
2022-03-03 19:10:50 +0900

    refactor: 로또 번호 개수와 중복 검증 로직 위치 변경

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -5,10 +5,30 @@ import java.util.List;
 
 public class LottoTicket {
 
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+
+    private static final String ERROR_DUPLICATION_WINNING_NUMBERS = "입력 번호가 서로 중복되었습니다.";
+    private static final String ERROR_NOT_MATCH_NUMBER_COUNT = "번호는 6개를 입력해주세요.";
+
     private final List<LottoNumber> numbers;
 
-    public LottoTicket(List<LottoNumber> lottoTicket) {
+    public LottoTicket(List<LottoNumber> lottoTicket) throws RuntimeException {
+        validateLottoNumbersCount(lottoTicket);
+        validateLottoNumbersDuplication(lottoTicket);
+
         this.numbers = lottoTicket;
+    }
+
+    private void validateLottoNumbersCount(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new RuntimeException(ERROR_NOT_MATCH_NUMBER_COUNT);
+        }
+    }
+
+    private void validateLottoNumbersDuplication(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.stream().distinct().count() != lottoNumbers.size()) {
+            throw new RuntimeException(ERROR_DUPLICATION_WINNING_NUMBERS);
+        }
     }
 
     public List<LottoNumber> getNumbers() {
--- src/test/java/lottoTest/domain/LottoTicketTest.java
+++ src/test/java/lottoTest/domain/LottoTicketTest.java
@@ -1,6 +1,7 @@
 package lottoTest.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,12 +11,36 @@ import lotto.domain.LottoNumber;
 import lotto.domain.LottoTicket;
 import lotto.domain.Rank;
 import lotto.domain.WinningNumbers;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 @SuppressWarnings("NonAsciiCharacters")
 class LottoTicketTest {
+
+    @Test
+    void 로또_번호가_서로_중복인_경우() {
+        assertThatThrownBy(
+                () -> new LottoTicket(toLottoNumbers(Arrays.asList(1, 1, 3, 4, 5, 6))))
+                .hasMessageContaining("입력 번호가 서로 중복되었습니다.")
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideWinningNumbers")
+    void 로또_번호가_6개가_아닌_경우(List<LottoNumber> lottoNumbers) {
+        assertThatThrownBy(() -> new LottoTicket(lottoNumbers))
+                .hasMessageContaining("번호는 6개를 입력해주세요.")
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    private static Stream<Arguments> provideWinningNumbers() {
+        return Stream.of(
+                Arguments.of(toLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6, 7))),
+                Arguments.of(toLottoNumbers(Arrays.asList(1, 2, 3, 4, 5)))
+        );
+    }
 
     @ParameterizedTest(name = "[{index}] 당첨 결과 : {3}")
     @MethodSource("provideLottoData")
@@ -30,21 +55,23 @@ class LottoTicketTest {
         assertThat(resultRank).isEqualTo(rank);
     }
 
-    private static List<LottoNumber> toLottoNumbers(List<Integer> integers) {
-        return integers.stream()
-                .map(LottoNumber::valueOf)
-                .collect(Collectors.toList());
-    }
-
     private static Stream<Arguments> provideLottoData() {
         List<LottoNumber> winningNumbersForTest = toLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         return Stream.of(
                 Arguments.of(winningNumbersForTest, LottoNumber.valueOf(7), winningNumbersForTest,
                         Rank.MATCH_SIX_NUMBERS),
-                Arguments.of(winningNumbersForTest, LottoNumber.valueOf(7), toLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 8)),
+                Arguments.of(winningNumbersForTest, LottoNumber.valueOf(7),
+                        toLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 8)),
                         Rank.MATCH_FIVE_NUMBERS),
-                Arguments.of(winningNumbersForTest, LottoNumber.valueOf(8), toLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 8)),
+                Arguments.of(winningNumbersForTest, LottoNumber.valueOf(8),
+                        toLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 8)),
                         Rank.MATCH_FIVE_AND_BONUS_NUMBERS)
         );
+    }
+
+    private static List<LottoNumber> toLottoNumbers(List<Integer> integers) {
+        return integers.stream()
+                .map(LottoNumber::valueOf)
+                .collect(Collectors.toList());
     }
 }
