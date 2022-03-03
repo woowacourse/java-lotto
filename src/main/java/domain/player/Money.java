5ddcb03d95534871fb9b00871b6ec2e45eeae4f4
Hongdonggeon <ghd700@naver.com>
2022-03-03 17:23:17 +0900

    test : money 로직에 대한 테스트

--- src/main/java/domain/player/Money.java
+++ src/main/java/domain/player/Money.java
@@ -67,4 +67,8 @@ public class Money {
     public int calculateTotalLottoCount() {
         return amount / MINIMUM_PURCHASE_AMOUNT;
     }
+
+    public int getAmount() {
+        return amount;
+    }
 }
--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utils.ExceptionMessage;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 =======
 import org.junit.jupiter.api.DisplayName;
@@ -49,4 +50,50 @@ class MoneyTest {
 >>>>>>> d722001 (refactor: 예외 메세지 별도 클래스로 분리)
     }
 
+    @Test
+    @DisplayName("1000원 단위가 아니면 예외가 발생한다.")
+    void create_1000원_단위_아닌경우_예외_테스트() {
+        int lessThan1000 = 1100;
+        assertThatThrownBy(() -> new Money(lessThan1000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.NOT_1000_UNIT);
+    }
+
+    @Test
+    @DisplayName("돈이 잘 차감되는지 확인한다.")
+    void duductMoneyTest() {
+        int amount = 2000;
+        Money money = new Money(amount);
+
+        money.deductMoney();
+        int actual = money.getAmount();
+
+        assertThat(actual).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("로또 금액보다 Money의 상태가 크면 True를 반환한다.")
+    void isBiggerThanLottoPriceTest() {
+        int amount = 2000;
+        Money money = new Money(amount);
+        assertThat(money.isBiggerThanLottoPrice()).isTrue();
+    }
+
+    @Test
+    @DisplayName("로또 금액보다 Money의 상태가 작으면 False를 반환한다.")
+    void isBiggerThanLottoPriceTest_false() {
+        int amount = 1000;
+        Money money = new Money(amount);
+        money.deductMoney();
+        assertThat(money.isBiggerThanLottoPrice()).isFalse();
+    }
+
+    @Test
+    @DisplayName("금액으로 구매할 수 있는 총 로또 갯수를 잘 반환하는지 확인한다.")
+    void calculateTotalLottoCountTest() {
+        int amount = 15000;
+        Money money = new Money(amount);
+        int actual = money.calculateTotalLottoCount();
+        assertThat(actual).isEqualTo(15);
+    }
 }
