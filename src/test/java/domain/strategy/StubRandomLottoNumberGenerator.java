2936ced572ee53de723da487233b9bf6615d9dfb
devHudi <devhudi@gmail.com>
2022-03-03 17:36:38 +0900

    refactor(naming): numberSequences 에서 numbersSequences 로 이름 변경

--- src/test/java/domain/strategy/StubRandomLottoNumberGenerator.java
+++ src/test/java/domain/strategy/StubRandomLottoNumberGenerator.java
@@ -4,18 +4,18 @@ import java.util.List;
 import java.util.Set;
 
 public class StubRandomLottoNumberGenerator implements LottoNumberGenerateStrategy {
-    private final List<Set<Integer>> numberSequences;
+    private final List<Set<Integer>> numbersSequences;
     private int sequenceCursor = 0;
 
-    public StubRandomLottoNumberGenerator(List<Set<Integer>> numberSequences) {
-        this.numberSequences = numberSequences;
+    public StubRandomLottoNumberGenerator(List<Set<Integer>> numbersSequences) {
+        this.numbersSequences = numbersSequences;
     }
 
     @Override
     public Set<Integer> generateNumbers() {
         int currentCursor = sequenceCursor;
-        sequenceCursor = (sequenceCursor + 1) % numberSequences.size();
+        sequenceCursor = (sequenceCursor + 1) % numbersSequences.size();
 
-        return numberSequences.get(currentCursor);
+        return numbersSequences.get(currentCursor);
     }
 }
