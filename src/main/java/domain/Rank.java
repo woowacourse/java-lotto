9f711c8d45344dc911d0eb96985173bc6cd66ed9
devHudi <devhudi@gmail.com>
2022-03-03 13:40:58 +0900

    refactor(rank): Rank 가 Lotto 와 WinningLotto 를 전달받아 직접 순위를 판단해 Rank 를 반환하는 메소드 구현

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -10,12 +10,35 @@ public enum Rank {
     FIFTH(3, 5_000L),
     NO_MATCH(0, 0L);
 
+    private static final int SAME_NUMBER_COUNT_OF_SECOND_AND_THIRD = 5;
     private final int sameNumberCount;
     private final long prize;
 
     Rank(int sameNumberCount, long prize) {
         this.sameNumberCount = sameNumberCount;
         this.prize = prize;
+    }
+
+    public static Rank createByLottoAndWinningLotto(Lotto lotto, WinningLotto winningLotto) {
+        int sameNumberCount = lotto.getSameNumberCount(winningLotto.getLotto());
+        boolean hasBonusNumber = lotto.containsLottoNumber(winningLotto.getBonusNumber());
+
+        if (sameNumberCount != SAME_NUMBER_COUNT_OF_SECOND_AND_THIRD) {
+            return findRankBySameNumberCount(sameNumberCount);
+        }
+
+        if (hasBonusNumber) {
+            return SECOND;
+        }
+
+        return THIRD;
+    }
+
+    private static Rank findRankBySameNumberCount(int sameNumberCount) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.sameNumberCount == sameNumberCount)
+                .findFirst()
+                .orElse(NO_MATCH);
     }
 
     public static Rank of(int sameNumberCount) {
