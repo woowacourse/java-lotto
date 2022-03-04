cd16eb1779c98a28549c944715a24669dcc7afa4
yxxnghwan <younghwan960@gmail.com>
2022-03-04 19:45:19 +0900

    test: WinningTicket.compareMatchCount 테스트 작성

--- src/test/java/domain/WinningTicketTest.java
+++ src/test/java/domain/WinningTicketTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
@@ -11,10 +12,20 @@ import org.junit.jupiter.api.Test;
 class WinningTicketTest {
 
     @Test
-    @DisplayName("로또번호가 6개가 아닌 경우 오류를 발생합니다.")
+    @DisplayName("로또번호가 6개가 아닌 경우 오류를 발생시키는지 확인한다.")
     void checkLottoNumberSize() {
         Set<Integer> numbers = new HashSet<>(Arrays.asList(1, 2, 3));
         assertThatThrownBy(() -> WinningTicket.of(numbers, 10))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage(LottoTicket.LOTTO_TICKET_SIZE_ERROR_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("로또 번호를 비교하여 맞는 갯수를 잘 찾아내는지 확인한다.")
+    void compareMatchCountTest() {
+        Set<Integer> winningNumberValues = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        WinningTicket winningTicket = WinningTicket.of(winningNumberValues, bonusNumber);
+        LottoTicket lottoTicket = LottoTicket.from(winningNumberValues);
+        assertThat(winningTicket.compareMatchCount(lottoTicket)).isEqualTo(6);
     }
 }
