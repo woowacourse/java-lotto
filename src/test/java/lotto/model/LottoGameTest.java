2dcd4d699556db9ce159021e07dbd7beff3bad5d
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-03-05 02:34:33 +0900

    test: 수동 구매, 자동 구매 검증 테스트 추가

--- src/test/java/lotto/model/LottoGameTest.java
+++ src/test/java/lotto/model/LottoGameTest.java
@@ -16,16 +16,26 @@ import lotto.model.numbergenerator.ManualGenerator;
 class LottoGameTest {
 
     @Test
-    @DisplayName("수익률 계산 확인")
-    void calculateYieldTest() {
-        LottoGame lottoGame = new LottoGame(1000, 0, new TestNumberGenerator());
-        LottoResult lottoResult = lottoGame.generateLottoResult(
-            new Lottos(new ManualGenerator(Collections.emptyList()), 0),
-            Arrays.asList(1, 2, 3, 4, 5, 6),
-            7);
-        Yield yield = lottoGame.calculateYield(lottoResult);
+    @DisplayName("수동 구매 검증 테스트")
+    void buyManualLottosTest() {
+        LottoGame lottoGame = new LottoGame(1000, 1, new TestNumberGenerator());
+        List<List<Integer>> rawManualLottos = List.of(List.of(1, 2, 3, 11, 12, 13));
 
-        assertThat(yield.getYield()).isCloseTo(5.00f, Percentage.withPercentage(99));
+        Lottos manualLottos = lottoGame.buyManualLottos(rawManualLottos);
+        List<Lotto> expected = List.of(new Lotto(List.of(1, 2, 3, 11, 12, 13)));
+
+        assertThat(manualLottos.getLottos().toString()).isEqualTo(expected.toString());
+    }
+
+    @Test
+    @DisplayName("자동 구매 검증 테스트")
+    void buyAutoLottosTest() {
+        LottoGame lottoGame = new LottoGame(1000, 0, new TestNumberGenerator());
+
+        List<Lotto> autoLottos = lottoGame.getAutoLottos();
+        List<Lotto> expected = List.of(new Lotto(List.of(1, 2, 3, 11, 12, 13)));
+
+        assertThat(autoLottos.toString()).isEqualTo(expected.toString());
     }
 
     @Test
@@ -37,6 +47,19 @@ class LottoGameTest {
                 Arrays.asList(1, 2, 3, 4, 5, 6), 6))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("보너스 볼 번호가 당첨 번호와 중복입니다.");
+    }
+
+    @Test
+    @DisplayName("수익률 계산 확인")
+    void calculateYieldTest() {
+        LottoGame lottoGame = new LottoGame(1000, 0, new TestNumberGenerator());
+        LottoResult lottoResult = lottoGame.generateLottoResult(
+            new Lottos(new ManualGenerator(Collections.emptyList()), 0),
+            Arrays.asList(1, 2, 3, 4, 5, 6),
+            7);
+        Yield yield = lottoGame.calculateYield(lottoResult);
+
+        assertThat(yield.getYield()).isCloseTo(5.00f, Percentage.withPercentage(99));
     }
 
     static class TestNumberGenerator implements LottoNumberGenerator {
