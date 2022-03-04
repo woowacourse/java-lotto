1c08234e4eaab904f80c46be36a70a10dfc451a4
skullKim <kyk990328@gmail.com>
2022-03-04 16:09:56 +0900

    refactor(InputValidator.java): 불필요한 검증 삭제
    로또 번호 갯수 체크는 이미 Lottery에서 하고 있고 이는 도메인과 관련되
    있기 때문에 InputValidator에서 해당 로직 삭제
    
    BREAKING CHANAGE: checkTheNumberOfNumber 메서드 삭제
    as-is:
    public class InputValidator {
        ...
        private static final int PROPER_LOTTERY_LENGTH = 6;
    
        ...
    
        public static void validateLottery(final String lottery) {
             ...
             checkTheNumberOfNumber(lotteryNumbers.length, LotteryMessage.LOTTERY_SIZE_ERROR);
        }
    
        ...
    
        private static void checkTheNumberOfNumber(int length, final String errorMessage) {
            if (length != PROPER_LOTTERY_LENGTH) {
                throw new IllegalArgumentException(errorMessage);
            }
        }
    }
    
    to-be:
    public class InputValidator {
        ...
    
        public static void validateLottery(final String lottery) {
            ...
        }
    
        ...
    }

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -7,7 +7,6 @@ public class InputValidator {
 	private static final String NUMBER_PATTERN = "^[0-9]*$";
 	private static final Pattern COMPILED_NUMBER_PATTERN = Pattern.compile(NUMBER_PATTERN);
 	private static final String WINNING_NUMBER_DISTRIBUTOR = ",";
-	private static final int PROPER_LOTTERY_LENGTH = 6;
 
 	public static void validateMoney(final String money) {
 		validateNumber(money, LotteryMessage.PURCHASE_AMOUNT_NOT_NUMBER_ERROR);
@@ -26,19 +25,12 @@ public class InputValidator {
 		 for (String number : lotteryNumbers) {
 			 validateNumber(number, LotteryMessage.LOTTERY_NUMBER_NOT_NUMBER_ERROR);
 		 }
-		 checkTheNumberOfNumber(lotteryNumbers.length, LotteryMessage.LOTTERY_SIZE_ERROR);
 	}
 
 	private static void validateNumber(final String number, final String errorMessage) {
 		 if (!COMPILED_NUMBER_PATTERN.matcher(number).matches()) {
 			 throw new IllegalArgumentException(errorMessage);
 		 }
-	}
-
-	private static void checkTheNumberOfNumber(int length, final String errorMessage) {
-		if (length != PROPER_LOTTERY_LENGTH) {
-			throw new IllegalArgumentException(errorMessage);
-		}
 	}
 
 }
