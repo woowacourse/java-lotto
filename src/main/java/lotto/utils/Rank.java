3fbb49183cf16c53d20b687370c9a09c28ee8d49
sinb57 <sinb57@naver.com>
2022-03-04 18:07:20 +0900

    refactor: Rank Enum 클래스, 내부 생성 로직 수정

--- src/main/java/lotto/utils/Rank.java
+++ src/main/java/lotto/utils/Rank.java
@@ -5,51 +5,50 @@ import java.util.Optional;
 
 public enum Rank {
 
-    FIFTH_GRADE(3, 5_000L),
-    FOURTH_GRADE(4, 50_000L),
-    THIRD_GRADE(5, false, 1_500_000L),
-    SECOND_GRADE(5, true, 30_000_000L),
-    FIRST_GRADE(6, 2_000_000_000L);
+    FIFTH_GRADE(BonusMatched.CAN_BE_WHATEVER, 3, 5_000L),
+    FOURTH_GRADE(BonusMatched.CAN_BE_WHATEVER, 4, 50_000L),
+    THIRD_GRADE(BonusMatched.MUST_BE_FALSE, 5, 1_500_000L),
+    SECOND_GRADE(BonusMatched.MUST_BE_TRUE, 5, 30_000_000L),
+    FIRST_GRADE(BonusMatched.CAN_BE_WHATEVER, 6, 2_000_000_000L);
 
+    private final BonusMatched bonusMatched;
     private final int matchCount;
-    private final boolean bonusRequired;
-    private final boolean bonusMatched;
     private final long prizeMoney;
 
-    Rank(final int matchCount, final boolean bonusRequired, final boolean bonusMatched, final long prizeMoney) {
-        this.matchCount = matchCount;
-        this.bonusRequired = bonusRequired;
+    private enum BonusMatched {
+
+        CAN_BE_WHATEVER(Optional.empty()),
+        MUST_BE_FALSE(Optional.of(Boolean.FALSE)),
+        MUST_BE_TRUE(Optional.of(Boolean.TRUE));
+
+        private final Optional<Boolean> matchState;
+
+        BonusMatched(final Optional<Boolean> matchState) {
+            this.matchState = matchState;
+        }
+
+        private boolean matches(final boolean bonusMatched) {
+            return matchState.map(matchState -> matchState == bonusMatched)
+                    .orElse(true);
+        }
+
+        boolean getState() {
+            return matchState.orElse(false);
+        }
+
+    }
+
+    Rank(final BonusMatched bonusMatched, final int matchCount, final long prizeMoney) {
         this.bonusMatched = bonusMatched;
+        this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
     }
 
-    Rank(final int matchCount, final boolean bonusMatched, final long prizeMoney) {
-        this(matchCount, true, bonusMatched, prizeMoney);
-    }
-
-    Rank(final int matchCount, final long prizeMoney) {
-        this(matchCount, false, false, prizeMoney);
-    }
-
     public static Optional<Rank> of(final int matchCount, final boolean bonusMatched) {
-        return Arrays.stream(Rank.values())
-                .filter(rank -> rank.matches(matchCount, bonusMatched))
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .filter(rank -> rank.bonusMatched.matches(bonusMatched))
                 .findAny();
-    }
-
-    private boolean matches(final int matchCount, final boolean bonusMatched) {
-        return matchesCount(matchCount) && matchesBonus(bonusMatched);
-    }
-
-    private boolean matchesCount(final int matchCount) {
-        return matchCount == this.matchCount;
-    }
-
-    private boolean matchesBonus(final boolean bonusMatched) {
-        if (this.bonusRequired) {
-            return bonusMatched == this.bonusMatched;
-        }
-        return true;
     }
 
     public int getMatchCount() {
@@ -57,7 +56,7 @@ public enum Rank {
     }
 
     public boolean getBonusMatched() {
-        return bonusMatched;
+        return bonusMatched.getState();
     }
 
     public long getPrizeMoney() {
