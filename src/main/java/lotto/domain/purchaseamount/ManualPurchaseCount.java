39400f5d75bc6a64a5a573a79ff21522c6fdebb5
Chris <bjuuuu98@gmail.com>
2022-03-04 20:35:32 +0900

    refactor: 수동 구매 개수 검증 로직 수정

--- src/main/java/lotto/domain/purchaseamount/ManualPurchaseCount.java
+++ src/main/java/lotto/domain/purchaseamount/ManualPurchaseCount.java
@@ -1,28 +1,19 @@
 package lotto.domain.purchaseamount;
 
 public class ManualPurchaseCount {
-    private static final String MANUAL_PURCHASE_AMOUNT_NOT_NATURAL_NUMBER_EXCEPTION_MESSAGE =
-            "수동 구매 로또 수는 자연수여야합니다.";
-    private static final String MANUAL_PURCHASE_AMOUNT_HIGHER_THAN_TOTAL_AMOUNT_EXCEPTION_MESSAGE =
-            "총 구매 수보다 수동 구매 수가 큽니다.";
+    private static final String MANUAL_PURCHASE_COUNT_RANGE_EXCEPTION_MESSAGE =
+            "수동 구매 개수는 0 이상, 전체 구매 개수 사이의 정수여야 합니다.";
 
     private final int value;
 
     public ManualPurchaseCount(final int value, final int totalPurchaseCount) {
-        validateRange(value);
-        validateLowerThanTotalPurchaseAmount(value, totalPurchaseCount);
+        validateRange(value, totalPurchaseCount);
         this.value = value;
     }
 
-    private void validateRange(final int value) {
-        if (value < 0) {
-            throw new IllegalArgumentException(MANUAL_PURCHASE_AMOUNT_NOT_NATURAL_NUMBER_EXCEPTION_MESSAGE);
-        }
-    }
-
-    private void validateLowerThanTotalPurchaseAmount(final int value, final int totalPurchaseAmount) {
-        if (totalPurchaseAmount < value) {
-            throw new IllegalArgumentException(MANUAL_PURCHASE_AMOUNT_HIGHER_THAN_TOTAL_AMOUNT_EXCEPTION_MESSAGE);
+    private void validateRange(final int value, final int totalPurchaseCount) {
+        if (value < 0 || totalPurchaseCount < value) {
+            throw new IllegalArgumentException(MANUAL_PURCHASE_COUNT_RANGE_EXCEPTION_MESSAGE);
         }
     }
 
--- src/test/java/lotto/domain/purchaseamount/ManualPurchaseCountTest.java
+++ src/test/java/lotto/domain/purchaseamount/ManualPurchaseCountTest.java
@@ -1,31 +1,22 @@
 package lotto.domain.purchaseamount;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ManualPurchaseCountTest {
     private final int totalPurchaseCount = 15;
-    @Test
-    @DisplayName("수동 구매 개수가 0 보다 작으면 예외를 발생시킨다.")
-    void create_exceptionByNotZeroOrNaturalNumber_Test() {
+    @ParameterizedTest
+    @DisplayName("수동 구매 개수가 0 보다 작거나 전체 구매 개수보다 크면 예외를 발생시킨다.")
+    @ValueSource(ints = {-1, 16})
+    void create_exceptionByNotZeroOrNaturalNumber_Test(final int invalidManualPurchaseCount) {
         //given
-        final int invalidManualPurchaseCount = -1;
+        final String expectedExceptionMessage = "수동 구매 개수는 0 이상, 전체 구매 개수 사이의 정수여야 합니다.";
         //when then
         assertThatThrownBy(() -> new ManualPurchaseCount(invalidManualPurchaseCount, totalPurchaseCount))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("수동 구매 로또 수는 자연수여야합니다.");
-    }
-
-    @Test
-    @DisplayName("수동 구매 개수가 전체 로또 개수보다 크면 예외를 발생시킨다.")
-    void create_exceptionByLowerTest() {
-        //given
-        final int invalidManualPurchaseCount = 16;
-        //when then
-        assertThatThrownBy(() -> new ManualPurchaseCount(invalidManualPurchaseCount, totalPurchaseCount))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("총 구매 수보다 수동 구매 수가 큽니다.");
+                .hasMessage(expectedExceptionMessage);
     }
 }
