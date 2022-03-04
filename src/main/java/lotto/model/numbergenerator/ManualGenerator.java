e5da2f5dab1bb980ff2678642d4516d7d4348617
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-03-05 00:03:37 +0900

    refactor: 받은 수동 번호를 정렬해서 전달하도록 변경

--- src/main/java/lotto/model/numbergenerator/ManualGenerator.java
+++ src/main/java/lotto/model/numbergenerator/ManualGenerator.java
@@ -1,5 +1,6 @@
 package lotto.model.numbergenerator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -8,7 +9,11 @@ public class ManualGenerator implements LottoNumberGenerator {
     private final Iterator<List<Integer>> manualLottos;
 
     public ManualGenerator(List<List<Integer>> manualLottos) {
-        this.manualLottos = manualLottos.iterator();
+        List<List<Integer>> result = new ArrayList<>(manualLottos);
+        for (List<Integer> manualLotto : new ArrayList<>(manualLottos)) {
+            Collections.sort(manualLotto);
+        }
+        this.manualLottos = result.iterator();
     }
 
     @Override
