fbe67a11efd1bfc35e0f2c36b7a81243d3c2d9af
Kim Seunglae <obey1342@gmail.com>
2022-03-05 01:13:26 +0900

    feat(InputView): 수동으로 구매할 번호 입력하는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import static lotto.view.OutputView.NEW_LINE;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import lotto.utils.IntegerUtils;
 
@@ -34,5 +36,18 @@ public class InputView {
         System.out.println(NEW_LINE + "수동으로 구매할 로또 수를 입력해 주세요.");
         String input = scanner.nextLine();
         return IntegerUtils.parse(input);
+    }
+    
+    public List<String> inputManualLottoTickets(int manualCount) {
+        System.out.println(NEW_LINE + "수동으로 구매할 번호를 입력해 주세요.");
+        return getManualLottoTickets(manualCount);
+    }
+
+    private List<String> getManualLottoTickets(int manualCount) {
+        List<String> lottoTickets = new ArrayList<>();
+        for (int lottoCount = manualCount; lottoCount > 0; lottoCount--) {
+            lottoTickets.add(scanner.nextLine());
+        }
+        return lottoTickets;
     }
 }
