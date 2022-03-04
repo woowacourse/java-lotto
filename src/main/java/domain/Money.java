7e7e48ad5fe054e4eda8dcb4749f24f3af899b32
Seongwon97 <osw970325@naver.com>
2022-03-05 01:13:23 +0900

    feat: 돈이 0보다 적을 때 발생하는 예외 추가

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -5,6 +5,7 @@ import static domain.LottoMachine.LOTTO_PRICE;
 public class Money {
 
     private static final String ERROR_LESS_THAN_MINIMUM_MONEY = "가격은 1000원 이상만 가능합니다.";
+    private static final String ERROR_INVALID_MONEY = "보유한 돈은 0보다 적을 수 없습니다.";
 
     private int money;
 
@@ -25,5 +26,12 @@ public class Money {
 
     public void purchaseLotto(int numOfLotto) {
         money -= (LOTTO_PRICE * numOfLotto);
+        checkInvalidMoney();
+    }
+
+    private void checkInvalidMoney() {
+        if (money < 0) {
+            throw new IllegalStateException(ERROR_INVALID_MONEY);
+        }
     }
 }
