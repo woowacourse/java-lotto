73c0e5b3a0c1ffabd7c6bd7c82ce0a6f78c5bf0d
EastHShin <xldk78@gmail.com>
2022-03-05 08:55:43 +0900

    refactor(domain/lottery/LotteryGenerator): 사용하지 않는 로직 제거

--- src/main/java/domain/lottery/LotteryGenerator.java
+++ src/main/java/domain/lottery/LotteryGenerator.java
@@ -1,24 +1,11 @@
 package domain.lottery;
 
-import static domain.lottery.LotteryNumber.*;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class LotteryGenerator {
-
-	private final List<Integer> numbers;
-
-	public LotteryGenerator() {
-		numbers = new ArrayList<>();
-		for (int i = MIN_LOTTERY_NUMBER; i <= MAX_LOTTERY_NUMBER; i++) {
-			numbers.add(i);
-		}
-	}
-
 	public Lottery generateLottery(final List<Integer> lotteryNumbers) {
 		Set<LotteryNumber> collect = lotteryNumbers.stream()
 			.map(LotteryNumber::new)
