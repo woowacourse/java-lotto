f69419d204eb9cbfc4f4db048453592ab3ef8616
wilgur513 <wilgur513@naver.com>
2022-03-04 15:55:45 +0900

    test: 테스트에 필요한 데이터 setUp에서 초기화

--- src/test/java/lotto/model/LottoesTest.java
+++ src/test/java/lotto/model/LottoesTest.java
@@ -3,31 +3,37 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LottoesTest {
-    private static final Lotto[] LOTTOES = new Lotto[]{
-        Lotto.create(List.of(1,2,3,4,5,6)),
-        Lotto.create(List.of(11,12,13,14,15,16)),
-        Lotto.create(List.of(21,22,23,24,25,26)),
-        Lotto.create(List.of(31,32,33,34,35,36))
-    };
+    private Lotto[] lottoes = null;
+
+    @BeforeEach
+    void setUp() {
+        lottoes = new Lotto[]{
+            Lotto.create(List.of(1,2,3,4,5,6)),
+            Lotto.create(List.of(11,12,13,14,15,16)),
+            Lotto.create(List.of(21,22,23,24,25,26)),
+            Lotto.create(List.of(31,32,33,34,35,36))
+        };
+    }
 
     @Test
     void combine() {
-        Lottoes lottoes1 = new Lottoes(List.of(LOTTOES[0], LOTTOES[1]));
-        Lottoes lottoes2 = new Lottoes(List.of(LOTTOES[2], LOTTOES[3]));
+        Lottoes lottoes1 = new Lottoes(List.of(lottoes[0], lottoes[1]));
+        Lottoes lottoes2 = new Lottoes(List.of(lottoes[2], lottoes[3]));
 
         Lottoes actual = lottoes1.combine(lottoes2);
 
         assertThat(actual).hasSize(4);
-        assertThat(actual).contains(LOTTOES);
+        assertThat(actual).contains(lottoes);
     }
 
     @Test
     void getPrice() {
-        Lottoes lottoes = new Lottoes(List.of(LOTTOES[0], LOTTOES[1], LOTTOES[2]));
-        assertThat(lottoes.getPrice()).isEqualTo(Lotto.PRICE.multiply(3));
+        Lottoes lotto = new Lottoes(List.of(lottoes[0], lottoes[1], lottoes[2]));
+        assertThat(lotto.getPrice()).isEqualTo(Lotto.PRICE.multiply(3));
     }
 
 }
