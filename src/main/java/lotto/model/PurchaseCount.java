1fee7be68a564c056f30bcdb1abe95ed2af94d56
YJGwon <yj970125@gmail.com>
2022-03-04 22:23:06 +0900

    feat(PurchaseCount): 음수 입력 예외 처리 추가
    PurchaseCountTest.count_10_manual_3() 오류 수정 (기대값 True -> False)

--- src/main/java/lotto/model/PurchaseCount.java
+++ src/main/java/lotto/model/PurchaseCount.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 public class PurchaseCount {
     private static final String ERROR_COUNT_OVER = "[ERROR] 구매할 수 있는 수량을 초과했습니다";
+    private static final String ERROR_NEGATIVE = "[ERROR] 구매 수량은 0보다 작을 수 없습니다";
     public static final String ERROR_TYPE = "[ERROR] 로또 구매 수량은 숫자로만 입력해주세요";
 
     private int auto;
@@ -9,6 +10,7 @@ public class PurchaseCount {
 
     private PurchaseCount(int total, int manual) {
         checkCount(total, manual);
+        checkNegative(manual);
         this.auto = total - manual;
         this.manual = manual;
     }
@@ -16,6 +18,12 @@ public class PurchaseCount {
     private void checkCount(int total, int manual) {
         if (total < manual) {
             throw new IllegalArgumentException(ERROR_COUNT_OVER);
+        }
+    }
+
+    private void checkNegative(int manual) {
+        if (manual < 0) {
+            throw new IllegalArgumentException(ERROR_NEGATIVE);
         }
     }
 
--- src/test/java/lotto/model/PurchaseCountTest.java
+++ src/test/java/lotto/model/PurchaseCountTest.java
@@ -1,12 +1,37 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PurchaseCountTest {
     public static final int LOTTO_PRICE = 1000;
+
+    @DisplayName("구매할 수 있는 수량보다 수동 구매 수량이 많으면 예외가 발생된다")
+    @Test
+    void error_bigger_than_total() {
+        int count = 3;
+        int manualCount = 4;
+        Money money = Money.from(String.valueOf(count * LOTTO_PRICE));
+
+        assertThatThrownBy(() -> PurchaseCount.of(money, String.valueOf(manualCount)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구매할 수 있는 수량을 초과했습니다");
+    }
+
+    @DisplayName("수동 구매 수량을 음수로 입력하면 예외가 발생한다")
+    @Test
+    void error_negative() {
+        int count = 10;
+        int manualCount = -3;
+        Money money = Money.from(String.valueOf(count * LOTTO_PRICE));
+
+        assertThatThrownBy(() -> PurchaseCount.of(money, String.valueOf(manualCount)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구매 수량은 0보다 작을 수 없습니다");
+    }
 
     @DisplayName("10장 중 3장이 수동이면 자동 로또는 7장이다")
     @Test
@@ -19,6 +44,6 @@ public class PurchaseCountTest {
             purchaseCount.subtractAuto();
         }
 
-        assertThat(purchaseCount.isAutoAvailable()).isTrue();
+        assertThat(purchaseCount.isAutoAvailable()).isFalse();
     }
 }
