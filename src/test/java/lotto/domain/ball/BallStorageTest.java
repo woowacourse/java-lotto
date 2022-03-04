4bac6ef34ff6bdb3310150bea5668dda3f2b5037
sinb57 <sinb57@naver.com>
2022-03-04 16:03:08 +0900

    test: BallStorageTest, 중복 테스트 제거 및 동등성 실패 테스트 수정

--- src/test/java/lotto/domain/ball/BallStorageTest.java
+++ src/test/java/lotto/domain/ball/BallStorageTest.java
@@ -17,22 +17,13 @@ class BallStorageTest {
         assertThat(actualBall).isSameAs(expectedBall);
     }
 
-    @DisplayName("동일한 Ball 객체를 반환해야 한다.")
-    @ParameterizedTest(name = "[{index}] 번호 : {0}")
-    @ValueSource(ints = {1, 2, 44, 45})
-    void cachedBallsSameTest(final int ballNumber) {
-        final Ball actualBall = BallStorage.getBall(ballNumber);
-        final Ball expectedBall = BallStorage.getBall(ballNumber);
-        assertThat(actualBall).isSameAs(expectedBall);
-    }
-
     @DisplayName("번호가 동일해도, 캐싱되지 않은 Ball 객체와 일치해선 안된다.")
     @ParameterizedTest(name = "[{index}] 번호 : {0}")
     @ValueSource(ints = {1, 2, 44, 45})
     void ballsNotSameIfNotGeneratedThroughBallStorageTest(final int ballNumber) {
         final Ball actualBall = BallStorage.getBall(ballNumber);
         final Ball expectedBall = new Ball(ballNumber);
-        assertThat(actualBall).isNotSameAs(expectedBall);
+        assertThat(actualBall).isNotEqualTo(expectedBall);
     }
 
 }
