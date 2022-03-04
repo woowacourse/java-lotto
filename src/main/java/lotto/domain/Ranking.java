cb8cfb6f146e9ad1ad9ddbff7c1e644ab818791d
yeon-06 <rnjstldus2@hs.ac.kr>
2022-03-04 21:33:34 +0900

    style: 1000단위 구분자 표시

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Ranking {
-    FIRST(2000000000, 6, false),
-    SECOND(30000000, 5, true),
-    THIRD(1500000, 5, false),
-    FOURTH(50000, 4, false),
-    FIFTH(5000, 3, false),
+    FIRST(2000_000_000, 6, false),
+    SECOND(30_000_000, 5, true),
+    THIRD(1_500_000, 5, false),
+    FOURTH(50_000, 4, false),
+    FIFTH(5_000, 3, false),
     NONE(0, 0, false);
 
     private final int prize;
