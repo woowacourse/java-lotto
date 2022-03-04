6d446367ca2a26af2fbb0bf9583e05e154d87552
Chris <bjuuuu98@gmail.com>
2022-03-04 20:31:50 +0900

    refactor: ManualPurchaseCount 정수 검증 및 변환 책임 삭제

--- src/main/java/lotto/domain/purchaseamount/ManualPurchaseCount.java
+++ src/main/java/lotto/domain/purchaseamount/ManualPurchaseCount.java
@@ -1,9 +1,6 @@
 package lotto.domain.purchaseamount;
 
-import java.util.regex.Pattern;
-
 public class ManualPurchaseCount {
-    private static final Pattern NATURAL_NUMBER_REGEX = Pattern.compile("^[0-9][0-9]*$");
     private static final String MANUAL_PURCHASE_AMOUNT_NOT_NATURAL_NUMBER_EXCEPTION_MESSAGE =
             "수동 구매 로또 수는 자연수여야합니다.";
     private static final String MANUAL_PURCHASE_AMOUNT_HIGHER_THAN_TOTAL_AMOUNT_EXCEPTION_MESSAGE =
@@ -11,15 +8,14 @@ public class ManualPurchaseCount {
 
     private final int value;
 
-    public ManualPurchaseCount(final String value, final int totalPurchaseAmount) {
-        validateNaturalNumber(value);
-        int numberValue = Integer.parseInt(value);
-        validateLowerThanTotalPurchaseAmount(numberValue, totalPurchaseAmount);
-        this.value = numberValue;
+    public ManualPurchaseCount(final int value, final int totalPurchaseCount) {
+        validateRange(value);
+        validateLowerThanTotalPurchaseAmount(value, totalPurchaseCount);
+        this.value = value;
     }
 
-    private void validateNaturalNumber(final String value) {
-        if (!NATURAL_NUMBER_REGEX.matcher(value).matches()) {
+    private void validateRange(final int value) {
+        if (value < 0) {
             throw new IllegalArgumentException(MANUAL_PURCHASE_AMOUNT_NOT_NATURAL_NUMBER_EXCEPTION_MESSAGE);
         }
     }
--- src/test/java/lotto/domain/purchaseamount/ManualPurchaseCountTest.java
+++ src/test/java/lotto/domain/purchaseamount/ManualPurchaseCountTest.java
@@ -2,19 +2,18 @@ package lotto.domain.purchaseamount;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ManualPurchaseCountTest {
-    private final int totalPurchaseAmount = 14;
-    @ParameterizedTest
-    @DisplayName("수동 구매 개수가 0 혹은 자연수가 아니면 예외를 발생시킨다.")
-    @ValueSource(strings = {"-1", "1.2", "a", "!"})
-    void create_exceptionByNotZeroOrNaturalNumber_Test(final String invalidValue) {
+    private final int totalPurchaseCount = 15;
+    @Test
+    @DisplayName("수동 구매 개수가 0 보다 작으면 예외를 발생시킨다.")
+    void create_exceptionByNotZeroOrNaturalNumber_Test() {
+        //given
+        final int invalidManualPurchaseCount = -1;
         //when then
-        assertThatThrownBy(() -> new ManualPurchaseCount(invalidValue, totalPurchaseAmount))
+        assertThatThrownBy(() -> new ManualPurchaseCount(invalidManualPurchaseCount, totalPurchaseCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("수동 구매 로또 수는 자연수여야합니다.");
     }
@@ -23,9 +22,9 @@ class ManualPurchaseCountTest {
     @DisplayName("수동 구매 개수가 전체 로또 개수보다 크면 예외를 발생시킨다.")
     void create_exceptionByLowerTest() {
         //given
-        final String higherManualPurchaseCountThanTotalCount = "15";
+        final int invalidManualPurchaseCount = 16;
         //when then
-        assertThatThrownBy(() -> new ManualPurchaseCount(higherManualPurchaseCountThanTotalCount, totalPurchaseAmount))
+        assertThatThrownBy(() -> new ManualPurchaseCount(invalidManualPurchaseCount, totalPurchaseCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("총 구매 수보다 수동 구매 수가 큽니다.");
     }
