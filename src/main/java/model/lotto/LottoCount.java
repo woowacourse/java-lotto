38b863081397b1f7a7e34171abed770e0ba42677
shindong96 <2006peter@naver.com>
2022-03-05 14:53:52 +0900

    refactor: 주석 삭제

--- src/main/java/model/lotto/LottoCount.java
+++ src/main/java/model/lotto/LottoCount.java
@@ -6,23 +6,11 @@ public class LottoCount {
 	public static final String CANT_MAKE_AMOUNT_ERROR_MESSAGE = "[Error]: 이 만큼의 로또를 생성할 수 없습니다.";
 
 	private int count;
-	//private int madeCount;
 
 	public LottoCount(int count) {
 		InputValidateUtils.inputNegative(count);
 		this.count = count;
-		//madeCount = 0;
 	}
-
-	// public boolean haveRemainToMake() {
-	// 	return count > madeCount;
-	// }
-	//
-	// public void increaseMadeLottoCount() {
-	// 	if (haveRemainToMake()) {
-	// 		madeCount++;
-	// 	}
-	// }
 
 	public void deductCountForManual(int count) {
 		checkCanMake(count);
@@ -38,8 +26,4 @@ public class LottoCount {
 	public int getCount() {
 		return count;
 	}
-
-	//public int getMadeCount() {
-	// 	return madeCount;
-	// }
 }
