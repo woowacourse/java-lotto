9a3679e7c1c5c567750afe324c8644a14827fdd0
shindong96 <2006peter@naver.com>
2022-03-06 03:28:09 +0900

    refactor: 테스트 케이스 코드 간단화

--- src/test/java/model/lottonumber/LottoNumbersTest.java
+++ src/test/java/model/lottonumber/LottoNumbersTest.java
@@ -2,13 +2,10 @@ package model.lottonumber;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import model.winningnumber.WinningLottoNumberDTO;
 import strategy.TestLottoNumberGenerationStrategy;
 
 public class LottoNumbersTest {
@@ -16,11 +13,10 @@ public class LottoNumbersTest {
 	@DisplayName("로또 넘버와 당첨 번호의 일치 개수 확인")
 	void countLottoNumbersMatchingWinningNumbers() {
 		LottoNumbers lottoNumbers = LottoNumbers.from(new TestLottoNumberGenerationStrategy());
-		List<LottoNumber> winningNumber = Arrays.asList(1, 2, 3, 4, 5, 6)
-			.stream()
-			.map(number -> LottoNumber.valueOf(number))
-			.collect(Collectors.toList());
-		assertThat(lottoNumbers.countMatchedNumbers(winningNumber)).isEqualTo(6);
+		WinningLottoNumberDTO winningLottoNumberDTO = new WinningLottoNumberDTO(
+			LottoNumbers.from(new TestLottoNumberGenerationStrategy()), LottoNumber.valueOf(7));
+		assertThat(lottoNumbers.countMatchedNumbers(winningLottoNumberDTO.getWinningNumbers()))
+			.isEqualTo(6);
 	}
 
 	@Test
