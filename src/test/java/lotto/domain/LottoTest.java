3b1ffedc8234a64820e92ab75262780d1b77c89d
yeongunheo <gjduddns@gmail.com>
2022-03-05 16:27:19 +0900

    test: 자동으로 생성된 로또가 잘 생성되었는지 검증하는 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -20,7 +20,7 @@ public class LottoTest {
     private static final String ERROR_DUPLICATE_LOTTO_NUMBER = "[ERROR] 로또 번호는 서로 중복되지 않아야 합니다.";
 
     @Nested
-    @DisplayName("6개의 숫자로 이루어져 있지 않다면")
+    @DisplayName("6개의 숫자로 이루어져있지 않다면")
     class wrong_lotto_count {
         @Test
         @DisplayName("에러를 출력한다.")
@@ -36,7 +36,7 @@ public class LottoTest {
     }
 
     @Nested
-    @DisplayName("중복된 숫자로 이루어져 있다면")
+    @DisplayName("중복된 숫자를 포함한다면")
     class has_duplicate_number {
         @Test
         @DisplayName("에러를 출력한다.")
@@ -52,7 +52,7 @@ public class LottoTest {
     }
 
     @Nested
-    @DisplayName("중복되지 않은 6개의 숫자로 이루어져 있다면")
+    @DisplayName("중복되지 않은 6개의 숫자를 입력받는다면")
     class right_lotto_number {
         @Test
         @DisplayName("정상적으로 생성된다.")
@@ -73,6 +73,15 @@ public class LottoTest {
                     .map(LottoNumber::new)
                     .collect(Collectors.toList());
             assertThat(lottoNumbers.contains(new LottoNumber(value))).isTrue();
+        }
+    }
+    @Nested
+    @DisplayName("자동이라면")
+    class create_auto_lotto {
+        @Test
+        @DisplayName("정상적으로 생성된다.")
+        void not_return_error() {
+            assertThatCode(() -> Lotto.createAutoLotto()).doesNotThrowAnyException();
         }
     }
 }
