3c6ced2a41ba9472aabc81bb70206b68bed94f97
jochungbeom <cndqjacndqja@gmail.com>
2022-03-05 16:37:33 +0900

    feat: 수동 로또 구매 개수 예외 처리

--- src/main/java/lotterymachine/domain/Count.java
+++ src/main/java/lotterymachine/domain/Count.java
@@ -3,10 +3,18 @@ package lotterymachine.domain;
 public class Count {
     private final int autoValue;
     private final int passivityValue;
+    private static final String INVALID_PASSIVITY_VALUE  = "수동 로또 개수가 총 구매 개수 보다 높습니다.";
 
-    public Count(int autoValue, int totalValue) {
-        this.autoValue = autoValue;
-        this.passivityValue = totalValue - autoValue;
+    public Count(int passivityValue, int totalValue) {
+        validatePassivityValue(passivityValue, totalValue);
+        this.passivityValue = passivityValue;
+        this.autoValue = totalValue - passivityValue;
+    }
+
+    public void validatePassivityValue(int passivityValue, int totalValue) {
+        if (totalValue < passivityValue) {
+            throw new IllegalArgumentException(INVALID_PASSIVITY_VALUE);
+        }
     }
 
     public int getAutoValue() {
--- src/test/java/lotterymachine/domain/CountTest.java
+++ src/test/java/lotterymachine/domain/CountTest.java
@@ -4,15 +4,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CountTest {
 
     @Test
     @DisplayName("Money를 입력 받아 총 로또 구매 개수를 표현하는 객체 Count를 생성한다.")
     void create() {
-        Money money = new Money(14000);
         Count count = new Count(3, 14);
         assertThat(count.getAutoValue()).isEqualTo(3);
         assertThat(count.getPassivityValue()).isEqualTo(11);
+    }
+
+    @Test
+    @DisplayName("수동 구매 개수가 총 로또 구매 개수 보다 높을 시, 에러가 발생한다.")
+    void validatePassivityValue() {
+        assertThatThrownBy(() -> {
+            Count count = new Count(3, 2);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("수동 로또 개수가 총 구매 개수 보다 높습니다.");
     }
 }
