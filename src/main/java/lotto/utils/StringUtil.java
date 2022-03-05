adc53cb8625b9ed3961bfe8204f88e70483d89be
dongho <dongho108@naver.com>
2022-03-05 23:22:36 +0900

    refactor : StringUtil은 6자리 숫자가 필요한지 모름

--- src/main/java/lotto/utils/StringUtil.java
+++ src/main/java/lotto/utils/StringUtil.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class StringUtil {
     private static final String EMPTY_ERROR_MESSAGE = "빈 문자를 입력할 수 없습니다.";
-    private static final String REQUEST_DELIMITER_MESSAGE = ",(콤마)와 공백으로 구분된 숫자 6자리를 입력해주세요. ex) 1, 2, 3, 4, 5, 6";
+    private static final String REQUEST_DELIMITER_MESSAGE = ",(콤마)와 공백으로 구분된 숫자를 입력해주세요. ex) 1, 2, 3, 4, 5, 6";
     private static final String DELIMITER = ", ";
 
     private StringUtil() {
