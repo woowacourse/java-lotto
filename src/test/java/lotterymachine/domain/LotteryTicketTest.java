e580e76a83cc44e75706edc1090d457f85e85f68
jochungbeom <cndqjacndqja@gmail.com>
2022-03-05 17:42:56 +0900

    test(LotteryTicket): 테스트 추가 createAutoLotteryTickets, validateSize, validateDuplication, getNumbers

--- src/test/java/lotterymachine/domain/LotteryTicketTest.java
+++ src/test/java/lotterymachine/domain/LotteryTicketTest.java
@@ -1,16 +1,18 @@
 package lotterymachine.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import lotterymachine.utils.LotteryNumbersGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 class LotteryTicketTest {
 
@@ -49,5 +51,43 @@ class LotteryTicketTest {
         List<List<Integer>> input = Arrays.asList(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList(1, 2, 3, 4, 5, 6));
         List<LotteryTicket> value = LotteryTicket.from(input);
         assertThat(value.size()).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("입력 받은 개수 만큼 자동 로또 번호를 생성하여 LotteryTicket 리스트를 만든다.")
+    void createAutoLotteryTickets(int input) {
+        List<LotteryNumber> lotteryNumbers = LotteryNumber.from(List.of(1, 2, 3, 4, 5, 6));
+        List<LotteryTicket> value = LotteryTicket.createAutoLotteryTickets(input, () -> lotteryNumbers);
+        assertThat(value.size()).isEqualTo(input);
+    }
+
+    @Test
+    @DisplayName("로또 넘버 숫자가 6개 미만일 때, 에러가 발생한다.")
+    void validateSize() {
+        List<LotteryNumber> input = LotteryNumber.from(List.of(1, 2, 3, 4, 5));
+        assertThatThrownBy(() -> {
+            LotteryTicket lotteryTicket = new LotteryTicket(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 숫자는 여섯개를 입력해야합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 넘버가 중복될 시, 에러가 발생한다.")
+    void validateDuplication() {
+        List<LotteryNumber> input = LotteryNumber.from(List.of(1, 2, 3, 4, 5, 5));
+        assertThatThrownBy(() -> {
+            LotteryTicket lotteryTicket = new LotteryTicket(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 숫자를 입력 받았습니다.");
+    }
+
+    @Test
+    @DisplayName("로또 넘버 조회 시, 정렬한 값을 반환한다.")
+    void getNumbers() {
+        List<LotteryNumber> lotteryNumbers = LotteryNumber.from(List.of(6, 5, 4, 3, 2, 1));
+        LotteryTicket lotteryTicket = new LotteryTicket(lotteryNumbers);
+        List<Integer> numbers = lotteryTicket.getNumbers();
+        assertThat(numbers.get(0)).isEqualTo(1);
     }
 }
