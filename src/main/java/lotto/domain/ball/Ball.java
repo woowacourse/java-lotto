a6ee9e258278d7b9bd74bfc95f7d8f49af3d8af4
sinb57 <sinb57@naver.com>
2022-03-06 16:45:56 +0900

    refactor: Ball equals, hashcode 오버라이딩

--- src/main/java/lotto/domain/ball/Ball.java
+++ src/main/java/lotto/domain/ball/Ball.java
@@ -2,6 +2,7 @@ package lotto.domain.ball;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import lotto.domain.ball.validation.BallValidator;
 
@@ -30,6 +31,23 @@ public class Ball {
 
     public int getBallNumber() {
         return this.number;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Ball ball = (Ball) object;
+        return number == ball.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 
 }
