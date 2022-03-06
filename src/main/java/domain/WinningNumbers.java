1efb97c747258d0b3152c059f79acc39404ceafb
Seongwon97 <osw970325@naver.com>
2022-03-06 23:57:49 +0900

    refactor: 당첨번호 클래스에서 Lotto객체를 변수로 사용하도록 수정
    - Set<Number>로 관리하던 당첨 번호를 Lotto객체를 사용하도록 변경

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -2,45 +2,30 @@ package domain;
 
 import domain.generator.ManualLottoGenerator;
 
-import java.util.HashSet;
 import java.util.Set;
-
-import static domain.Lotto.LOTTO_SIZE;
 
 public class WinningNumbers {
 
     private static final String ERROR_DUPLICATE_BONUS = "보너스 번호가 당첨 번호와 중복됩니다.";
-    private static final String ERROR_DUPLICATE_NUMBER = "로또 번호는 중복되지 않은 6자리 숫자여야 합니다.";
 
-    private final Set<Number> winningNumbers;
+    private final Lotto winningNumbers;
     private final Number bonus;
 
     public WinningNumbers(final int[] inputNumbers, final int bonus) {
-        Set<Integer> numbers = new ManualLottoGenerator(inputNumbers).generateNumbers();
-        checkRightSize(numbers);
-        winningNumbers = new HashSet<>();
-        for (Integer number : numbers) {
-            winningNumbers.add(new Number(number));
-        }
-        this.bonus = checkRangeAndDuplicate(bonus);
+        winningNumbers = new Lotto(new ManualLottoGenerator(inputNumbers).generateNumbers());
+        this.bonus = checkBonusRangeAndDuplicate(bonus);
     }
 
-    private void checkRightSize(Set<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException(ERROR_DUPLICATE_NUMBER);
-        }
-    }
-
-    private Number checkRangeAndDuplicate(final int number) {
+    private Number checkBonusRangeAndDuplicate(final int number) {
         final Number bonus = new Number(number);
-        if (winningNumbers.contains(bonus)) {
+        if (winningNumbers.hasMatchedNumber(bonus)) {
             throw new IllegalArgumentException(ERROR_DUPLICATE_BONUS);
         }
         return bonus;
     }
 
     public Set<Number> getWinningNumbers() {
-        return winningNumbers;
+        return winningNumbers.getLottoNumbers();
     }
 
     public Number getBonus() {
