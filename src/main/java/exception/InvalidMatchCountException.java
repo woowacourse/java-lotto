a73bf0f6b1481a15542779f20fb682640499911d
yangdongjue5510 <djwhy5510@gmail.com>
2022-03-07 17:26:47 +0900

    refactor: LottoRank 생성 방식 수정

--- src/main/java/exception/InvalidMatchCountException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class InvalidMatchCountException extends IllegalArgumentException {
-    public InvalidMatchCountException() {
-        super("일치하는 로또 번호 갯수는 0 ~ 6 사이여야 합니다.");
-    }
-}
--- src/test/java/model/LottoRankTest.java
+++ src/test/java/model/LottoRankTest.java
@@ -2,7 +2,6 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import exception.InvalidMatchCountException;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,14 +27,16 @@ public class LottoRankTest {
     @DisplayName("음수 일치 갯수로 생성 시 예외 발생")
     void createByNegativeMatchCount() {
         assertThatThrownBy(() -> LottoRank.of(-1, false))
-                .isInstanceOf(InvalidMatchCountException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("일치하는 로또 번호 갯수는 0 ~ 6 사이여야 합니다.");
     }
 
     @Test
     @DisplayName("일치 갯수가 6 초과되는 값으로 생성 시 예외 발생")
     void createByOverSixMatchCount() {
         assertThatThrownBy(() -> LottoRank.of(7, false))
-                .isInstanceOf(InvalidMatchCountException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("일치하는 로또 번호 갯수는 0 ~ 6 사이여야 합니다.");
     }
 
     @ParameterizedTest
