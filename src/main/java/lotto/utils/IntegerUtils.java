d1b9ff7a1463de11a4966b529824189e9e6e87ca
cjlee38 <cjlee38@github.com>
2022-03-07 15:06:47 +0900

    refactor(utils): 사용하지 않는 메소드 제거

--- src/main/java/lotto/utils/IntegerUtils.java
+++ src/main/java/lotto/utils/IntegerUtils.java
@@ -1,12 +1,6 @@
 package lotto.utils;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class IntegerUtils {
-
-    private static final String DELIMITER = ",";
 
     public static int parse(String input) {
         try {
@@ -14,11 +8,5 @@ public class IntegerUtils {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력한 값이 숫자가 아닙니다.");
         }
-    }
-
-    public static List<Integer> parseAll(String input) {
-        return Arrays.stream(input.split(DELIMITER))
-            .map(string -> IntegerUtils.parse(string.trim()))
-            .collect(Collectors.toList());
     }
 }
