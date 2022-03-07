51494bfccf4a6156f13b345bb2f9acf2c68b2733
cjlee38 <cjlee38@github.com>
2022-03-07 14:32:15 +0900

    refactor: 도메인에 책임을 위임하는 사용자 역할 할당

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,21 +1,16 @@
 package lotto;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import lotto.domain.LottoMachine;
+import lotto.domain.LottoLine;
 import lotto.domain.LottoNumber;
-import lotto.domain.LottoRank;
+import lotto.domain.LottoStatistics;
 import lotto.domain.LottoTicket;
 import lotto.domain.Money;
 import lotto.domain.WinningTicket;
 import lotto.domain.generator.RandomNumberGenerator;
 import lotto.domain.generator.StringInputNumberGenerator;
-import lotto.dto.LottoStatisticsResponse;
-import lotto.dto.LottoTicketResponse;
 import lotto.dto.PurchaseResult;
-import lotto.utils.IntegerUtils;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -46,81 +41,40 @@ public class Application {
     }
 
     private PurchaseResult tryPurchase() {
-        Money money = Money.from(inputView.inputMoney());
-        LottoMachine lottoMachine = new LottoMachine(money);
-        List<LottoTicket> tickets = purchaseLottos(lottoMachine);
-        return new PurchaseResult(money, tickets);
+        try {
+            Money money = Money.from(inputView.inputMoney());
+            List<String> inputLottoLines = inputView.inputLottoLines();
+            PurchaseResult purchaseResult = purchaseLottos(money, inputLottoLines);
+            outputView.outputPurchaseResult(purchaseResult);
+            return purchaseResult;
+        } catch (RuntimeException e) {
+            outputView.outputError(e);
+            return tryPurchase();
+        }
     }
 
-    private List<LottoTicket> purchaseLottos(LottoMachine lottoMachine) {
-        List<String> inputNumbers = inputView.inputLottoNumbers();
+    private PurchaseResult purchaseLottos(Money money, List<String> inputLottoLines) {
+        Money manualMoney = Money.from(inputLottoLines.size() * LottoLine.PRICE);
+        LottoTicket manualTicket = LottoTicket.createLottoTicket(
+            new StringInputNumberGenerator(inputLottoLines), manualMoney);
 
-        List<LottoTicket> manualTickets = purchaseManual(lottoMachine, inputNumbers);
-        List<LottoTicket> autoTickets = purchaseAuto(lottoMachine);
-
-        List<LottoTicketResponse> manualResponses = toTicketResponse(manualTickets);
-        List<LottoTicketResponse> autoResponses = toTicketResponse(autoTickets);
-
-        outputView.outputTickets(manualResponses, autoResponses);
-        return Stream.concat(manualTickets.stream(), autoTickets.stream()).collect(Collectors.toList());
-    }
-
-    private List<LottoTicket> purchaseManual(LottoMachine lottoMachine, List<String> inputNumbers) {
-        return lottoMachine.createLottos(new StringInputNumberGenerator(inputNumbers), inputNumbers.size());
-    }
-
-    private List<LottoTicket> purchaseAuto(LottoMachine lottoMachine) {
-        return lottoMachine.createLottos(
-            new RandomNumberGenerator(LottoNumber.MIN, LottoNumber.MAX), LottoMachine.ALL);
-    }
-
-    private List<LottoTicketResponse> toTicketResponse(List<LottoTicket> tickets) {
-        return tickets.stream()
-            .map(LottoTicketResponse::from)
-            .collect(Collectors.toList());
+        Money remainMoney = money.minus(manualMoney);
+        LottoTicket autoTicket = LottoTicket.createLottoTicket(
+            new RandomNumberGenerator(LottoNumber.MIN, LottoNumber.MAX), remainMoney);
+        return new PurchaseResult(manualTicket, autoTicket);
     }
 
     public void tryDrawLots(PurchaseResult purchaseResult) {
         try {
-            WinningTicket winningTicket = createWinningTicket();
-            showStatistics(winningTicket, purchaseResult);
+            String inputWinningNumber = inputView.inputWinningNumber();
+            String inputBonusBall = inputView.inputBonusBall();
+            WinningTicket winningTicket = WinningTicket.from(inputWinningNumber, inputBonusBall);
+            LottoStatistics statistics =
+                new LottoStatistics(purchaseResult.compareWinningTicket(winningTicket), purchaseResult.sumMoney());
+            outputView.outputStatistics(statistics);
         } catch (IllegalArgumentException e) {
             outputView.outputError(e);
             tryDrawLots(purchaseResult);
         }
     }
-
-    private WinningTicket createWinningTicket() {
-        LottoTicket winningNumber = createWinningNumber();
-        LottoNumber bonusBall = createWinningBonus();
-        return new WinningTicket(winningNumber, bonusBall);
-    }
-
-    private LottoTicket createWinningNumber() {
-        String inputWinningNumber = inputView.inputWinningNumber();
-        List<LottoNumber> numbers = IntegerUtils.parseAll(inputWinningNumber)
-            .stream()
-            .map(LottoNumber::from)
-            .collect(Collectors.toList());
-        return new LottoTicket(numbers);
-    }
-
-    private LottoNumber createWinningBonus() {
-        String inputBonusBall = inputView.inputBonusBall();
-        return LottoNumber.from(IntegerUtils.parse(inputBonusBall));
-    }
-
-    private void showStatistics(WinningTicket winningTicket, PurchaseResult purchaseResult) {
-        LottoStatisticsResponse statistics = toStatisticsResponse(winningTicket, purchaseResult);
-        outputView.outputStatistics(statistics);
-    }
-
-    private LottoStatisticsResponse toStatisticsResponse(
-        WinningTicket winningTicket, PurchaseResult purchaseResult) {
-        List<LottoRank> ranks = purchaseResult.getTickets().stream()
-            .map(winningTicket::compare)
-            .collect(Collectors.toList());
-        return new LottoStatisticsResponse(ranks, purchaseResult.getMoney());
-    }
-
 }
