101f5dbae210ebb78e37ec9b0b32434dffccd026
soominsohn <soominsohn@gmail.com>
2022-03-07 15:46:20 +0900

    refactor(winningNumbers): add exception case

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -7,17 +7,19 @@ import java.util.Set;
 
 public class WinningNumbers {
 
-    private static final int NUMBER_RANGE_MINIMUM = 1;
-    private static final int NUMBER_RANGE_MAXIMUM = 45;
-
-    private static final String OUT_OF_RANGE_ERROR = "[ERROR] 1이상 45이하 값을 입력해주세요.";
+    private static final String NULL_WINNING_NUMBER_ERROR = "[ERROR] 로또 당첨번호에 null 값이 올 수 없습니다.";
+    private static final String NULL_BONUS_NUMBER_ERROR = "[ERROR] 로또 보너스볼에 null 값이 올 수 없습니다.";
+    private static final String EMPTY_WINNING_NUMBER_ERROR = "[ERROR] 로또 당첨번호에 빈 값이 올 수 없습니다.";
     private static final String DUPLICATED_NUMBER_ERROR = "[ERROR] 당첨번호와 보너스볼은 중복될 수 없습니다.";
+
+    private static final int EMPTY = 0;
+
 
     private final List<LottoNumber> winningNumbers;
     private final LottoNumber bonusNumber;
 
     public WinningNumbers(List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
-        checkDuplicate(winningNumbers, bonusNumber);
+        checkValidate(winningNumbers, bonusNumber);
         this.winningNumbers = new ArrayList<>(winningNumbers);
         this.bonusNumber = bonusNumber;
     }
@@ -28,6 +30,35 @@ public class WinningNumbers {
 
     public boolean isEqualToBonusNumber(LottoNumber lottoNumber) {
         return bonusNumber.equals(lottoNumber);
+    }
+
+    private void checkValidate(List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
+        checkNull(winningNumbers, bonusNumber);
+        checkWinningNumbersEmpty(winningNumbers);
+        checkDuplicate(winningNumbers, bonusNumber);
+    }
+
+    private void checkNull(List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
+        checkWinningNumbersNull(winningNumbers);
+        checkBonusNumberNull(bonusNumber);
+    }
+
+    private void checkWinningNumbersNull(List<LottoNumber> winningNumbers) {
+        if (winningNumbers == null) {
+            throw new IllegalArgumentException(NULL_WINNING_NUMBER_ERROR);
+        }
+    }
+
+    private void checkBonusNumberNull(LottoNumber bonusNumber) {
+        if (bonusNumber == null) {
+            throw new IllegalArgumentException(NULL_BONUS_NUMBER_ERROR);
+        }
+    }
+
+    private void checkWinningNumbersEmpty(List<LottoNumber> winningNumbers) {
+        if (winningNumbers.size() == EMPTY) {
+            throw new IllegalArgumentException(EMPTY_WINNING_NUMBER_ERROR);
+        }
     }
 
     private void checkDuplicate(List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
--- src/test/java/WinningNumbersTest.java
+++ src/test/java/WinningNumbersTest.java
@@ -2,49 +2,60 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.LottoNumber;
 import domain.WinningNumbers;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningNumbersTest {
-//
-//    @Test
-//    @DisplayName("로또 당첨 번호는 1~45 사이가 아닐 시 실패")
-//    void check_range_fail() {
-//
-//        assertThatThrownBy(
-//            () -> new WinningNumbers(Stream.of(1, 2, 3, 4, 5, 46).map(LottoNumber::new).collect(
-//                Collectors.toList()), new LottoNumber(6))
-//        ).isInstanceOf(IllegalArgumentException.class)
-//            .hasMessageContaining("[ERROR] 1이상 45이하 값을 입력해주세요.");
-//
-//    }
-//
-//    @Test
-//    @DisplayName("로또 보너스 번호는 1~45 사이가 아닐 시 실패")
-//    void check_bonus_range_fail() {
-//
-//        assertThatThrownBy(
-//            () -> new WinningNumbers(Stream.of(1, 2, 3, 4, 5, 6).map(LottoNumber::new).collect(
-//                Collectors.toList()), new LottoNumber(46))
-//        ).isInstanceOf(IllegalArgumentException.class)
-//            .hasMessageContaining("[ERROR] 1이상 45이하 값을 입력해주세요.");
-//
-//    }
-//
-//    @Test
-//    @DisplayName("로또 당첨 번호 중복 시 실패")
-//    void check_unique_fail() {
-//
-//        assertThatThrownBy(
-//            () -> new WinningNumbers(Stream.of(1, 2, 3, 4, 5, 5).map(LottoNumber::new).collect(
-//                Collectors.toList()), new LottoNumber(6))
-//        ).isInstanceOf(IllegalArgumentException.class)
-//            .hasMessageContaining("[ERROR] 당첨번호와 보너스볼은 중복될 수 없습니다.");
-//
-//    }
+
+    @Test
+    @DisplayName("로또 당첨 번호에 null 값이 올 시 실패")
+    void check_winning_null_fail() {
+
+        assertThatThrownBy(
+            () -> new WinningNumbers(null, new LottoNumber(6))
+        ).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 로또 당첨번호에 null 값이 올 수 없습니다.");
+
+    }
+
+    @Test
+    @DisplayName("로또 보너스 번호에 null 값이 올 시 실패")
+    void check_bonus_null_fail() {
+
+        assertThatThrownBy(
+            () -> new WinningNumbers(Stream.of(1, 2, 3, 4, 5, 5).map(LottoNumber::new).collect(
+                Collectors.toList()), null)
+        ).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 로또 보너스볼에 null 값이 올 수 없습니다.");
+
+    }
+
+    @Test
+    @DisplayName("로또 당첨 번호에 empty 값이 올 시 실패")
+    void check_bonus_empty_fail() {
+
+        assertThatThrownBy(
+            () -> new WinningNumbers(new ArrayList<>(), new LottoNumber(6))
+        ).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 로또 당첨번호에 빈 값이 올 수 없습니다.");
+
+    }
+
+
+    @Test
+    @DisplayName("로또 당첨 번호 중복 시 실패")
+    void check_unique_fail() {
+
+        assertThatThrownBy(
+            () -> new WinningNumbers(Stream.of(1, 2, 3, 4, 5, 5).map(LottoNumber::new).collect(
+                Collectors.toList()), new LottoNumber(6))
+        ).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR] 당첨번호와 보너스볼은 중복될 수 없습니다.");
+
+    }
 
     @Test
     @DisplayName("로또 당첨 번호와 보너스 볼 중복 시 실패")
