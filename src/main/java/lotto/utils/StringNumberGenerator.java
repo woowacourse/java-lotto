210f6bcc209d04264f6807051b3a1146af84a9f5
Kim Seunglae <obey1342@gmail.com>
2022-03-07 20:19:47 +0900

    refactor(StringNumberGenerator): 예외 상황 추가

--- src/main/java/lotto/utils/StringNumberGenerator.java
+++ src/main/java/lotto/utils/StringNumberGenerator.java
@@ -1,6 +1,7 @@
 package lotto.utils;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,16 +22,28 @@ public class StringNumberGenerator implements NumberGenerator {
 
     @Override
     public List<Integer> generate(int size) {
+        if (!iterator.hasNext()) {
+            throw new IllegalArgumentException("읽어들일 숫자가 존재하지 않습니다.");
+        }
         return readLine(iterator.next(), size);
     }
 
-    private List<Integer> readLine(String next, int size) {
-        List<Integer> numbers = toNumbers(next);
-        return numbers.subList(0, size);
+    private List<Integer> readLine(String input, int size) {
+        List<Integer> numbers = toNumbers(input);
+        validateNumbersSize(size, numbers);
+        List<Integer> integers = numbers.subList(0, size);
+        Collections.sort(integers);
+        return integers;
     }
 
-    private List<Integer> toNumbers(String inputs) {
-        return Arrays.stream(inputs.split(NUMBER_DELIMITER))
+    private void validateNumbersSize(int size, List<Integer> numbers) {
+        if (numbers.size() < size) {
+            throw new IllegalArgumentException("입력된 숫자들의 길이가 요구하는 길이보다 작습니다.");
+        }
+    }
+
+    private List<Integer> toNumbers(String input) {
+        return Arrays.stream(input.split(NUMBER_DELIMITER))
             .map(number -> IntegerUtils.parse(number.trim()))
             .collect(Collectors.toList());
     }
--- src/test/java/lotto/utils/NumberGeneratorTest.java
+++ src/test/java/lotto/utils/NumberGeneratorTest.java
@@ -1,17 +1,26 @@
 package lotto.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class NumberGeneratorTest {
+
+    private NumberGenerator generator;
+
+    @BeforeEach
+    void setUp() {
+        generator = new StringNumberGenerator("1,2,3,4,5,6");
+    }
 
     @Test
     @DisplayName("랜덤으로 여러 번호를 생성한다.")
@@ -41,12 +50,30 @@ class NumberGeneratorTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6})
     @DisplayName("문자열을 입력받아 정상적으로 생성되는지 확인")
-    public void createStringNumber(int size) {
-        // given
-        NumberGenerator generator = new StringNumberGenerator("1,2,3,4,5,6");
+    void createStringNumber(int size) {
         // when
         List<Integer> numbers = generator.generate(size);
         // then
         assertThat(numbers.size()).isEqualTo(size);
+    }
+
+    @Test
+    @DisplayName("입력된 숫자들의 길이가 요구하는 길이보다 작으면 예외를 던진다.")
+    void validateNumbersSize() {
+        // then
+        assertThatThrownBy(() -> generator.generate(8))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("입력된 숫자들의 길이가 요구하는 길이보다 작습니다.");
+    }
+
+    @Test
+    @DisplayName("더 이상 읽어들인 숫자들이 존재하지 않으면 예외를 던진다.")
+    void validateReadLine() {
+        // when
+        generator.generate(2);
+        // then
+        assertThatThrownBy(() -> generator.generate(2))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("읽어들일 숫자가 존재하지 않습니다.");
     }
 }
