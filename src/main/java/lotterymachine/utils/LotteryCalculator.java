f799b4589f271bfb3d5495bb1bfdb0b82874ca9d
SuyeonChoi <tndus970120@gmail.com>
2022-03-08 05:29:22 +0900

    refactor(LotteryCalculator): 사용하지 않는 메서드 제거

--- src/main/java/lotterymachine/utils/LotteryCalculator.java
+++ src/main/java/lotterymachine/utils/LotteryCalculator.java
@@ -6,10 +6,6 @@ import lotterymachine.vo.Money;
 public class LotteryCalculator {
     private static final int LOTTERY_PRICE = 1000;
 
-    public static Money getTotalTicketAmount(int numberOfTickets) {
-        return Money.from(numberOfTickets * LOTTERY_PRICE);
-    }
-
     public static Count divideByLotteryPrice(Money money) {
          return Count.from(money.getAmount() / LOTTERY_PRICE);
     }
--- src/test/java/lotterymachine/utils/LotteryCalculatorTest.java
+++ src/test/java/lotterymachine/utils/LotteryCalculatorTest.java
@@ -9,15 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LotteryCalculatorTest {
     @Test
-    @DisplayName("로또 개수로 로또 구입 비용을 계산한다.")
-    void getTotalTicketAmount() {
-        int numberOfTickets = 14;
-        Money totalTicketAmount = LotteryCalculator.getTotalTicketAmount(numberOfTickets);
-        Money expected = Money.from(14000);
-        assertThat(totalTicketAmount).isEqualTo(expected);
-    }
-
-    @Test
     @DisplayName("투입된 금액을 통해 구매할 수 있는 로또 티켓 개수를 구한다.")
     void divideByTicketPrice() {
         Money inputMoney = Money.fromInputAmount(14000);
