d90953aa9016ce1e25ff2ae89eba34073bfff13d
soominsohn <soominsohn@gmail.com>
2022-03-08 00:32:39 +0900

    refactor(outputView): change decimal point numbers

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -15,7 +15,7 @@ public class OutputView {
     private static final String MESSAGE_LOTTOS_NUMBER = "수동으로 %d장, 자동으로 %d개를 구매했습니다.\n";
     private static final String MESSAGE_WINNING_STATISTIC = "당첨 통계";
     private static final String SEPERATOR_LINE = "------------";
-    private static final String MESSAGE_YIELD = "총 수익률은 %2f입니다.(1초과 시 이득, 1미만일 시 손해라는 의미임)";
+    private static final String MESSAGE_YIELD = "총 수익률은 %.2f입니다.(1초과 시 이득, 1미만일 시 손해라는 의미임)";
     private static final String NUMBER_DELIMITER = ", ";
     private static final String LOTTO_NUMBER_FORMAT = "[%s]\n";
 
