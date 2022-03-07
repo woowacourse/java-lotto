e91a310581ab0de877e3c411950b14dbd43d871e
sure-why-not <pkse1213@gmail.com>
2022-03-07 15:14:40 +0900

    refactor : 한 곳에서만 사용하는 상수는 해당 클래스에서만 선언하도록 수정

--- src/main/java/domain/LottoGameMoney.java
+++ src/main/java/domain/LottoGameMoney.java
@@ -1,10 +1,9 @@
 package domain;
 
-import constant.LottoConstant;
-
 public class LottoGameMoney {
 
     private static final int MINIMUM_AMOUNT = 0;
+    private static final int LOTTO_PRICE = 1000;
 
     private final int amount;
 
@@ -17,8 +16,8 @@ public class LottoGameMoney {
         if (amount <= MINIMUM_AMOUNT) {
             throw new IllegalArgumentException("금액은 " + MINIMUM_AMOUNT + "이하일 수 없습니다.");
         }
-        if (amount % LottoConstant.LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("로또 구매 금액은 " + LottoConstant.LOTTO_PRICE + "원 단위여야 합니다.");
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("로또 구매 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.");
         }
     }
 
@@ -34,7 +33,7 @@ public class LottoGameMoney {
     }
 
     private int purchasableLottoCount() {
-        return amount / LottoConstant.LOTTO_PRICE;
+        return amount / LOTTO_PRICE;
     }
 
     public int amount() {
