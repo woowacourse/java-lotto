3883e89265138cd633dfc5509f41d9842929ce16
jochungbeom <cndqjacndqja@gmail.com>
2022-03-07 15:10:32 +0900

    refactor(RandomLotteryNumbersGenerator): NUMBERS 필드 제거

--- src/main/java/lotterymachine/domain/RandomLotteryNumbersGenerator.java
+++ src/main/java/lotterymachine/domain/RandomLotteryNumbersGenerator.java
@@ -1,23 +1,16 @@
 package lotterymachine.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class RandomLotteryNumbersGenerator implements LotteryNumbersGenerator {
-    private static final List<Integer> NUMBERS;
-
-    static {
-        NUMBERS = IntStream.rangeClosed(LotteryNumber.MIN_VALUE, LotteryNumber.MAX_VALUE)
-                .boxed()
-                .collect(Collectors.toList());
-    }
-
     @Override
     public List<LotteryNumber> generate() {
-        Collections.shuffle(NUMBERS);
-        return NUMBERS.stream()
+        ArrayList<Integer> keyOfNumbers = new ArrayList<>(LotteryNumber.NUMBERS.keySet());
+        Collections.shuffle(keyOfNumbers);
+        return keyOfNumbers.stream()
                 .limit(LotteryTicket.TICKET_SIZE)
                 .map(LotteryNumber::from)
                 .collect(Collectors.toList());
