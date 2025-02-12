bf4d5168e303921669afef8044c92f6004256c9a
황재웅 <wodnd0131@naver.com>
2025-02-12 16:55:24 +0900

    feat: Amount 선언
    - 로또 구입 개수를 계산한다.

---
+++ src/main/java/lotto/domain/Amount.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import static lotto.exception.ErrorMessage.*;
+
+public class Amount {
+    private final int money;
+
+    public Amount(int money) {
+        validateZero(money);
+        validateDivided1000(money);
+        this.money = money;
+    }
+
+    private void validateZero(int money) {
+        if(money < 1000) {
+            throw new IllegalArgumentException(ERROR_MONEY_ZERO);
+        }
+    }
+
+    private void validateDivided1000(int money) {
+        if(money % 1000 !=0) {
+            throw new IllegalArgumentException(ERROR_MONEY_NOT_DIVIDED);
+        }
+    }
+
+    public int getAmount() {
+        return money / 1000;
+    }
+}
