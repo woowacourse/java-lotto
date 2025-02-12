b980ad1a4bd868c2a223f42c40333adc5ffebe8a
Dompoo <dlckdrms0517@gmail.com>
2025-02-12 14:20:26 +0900

    feat: 랜덤한 번호 생성 기능 개발

---
+++ src/main/java/util/RandomNumberPicker.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class RandomNumberPicker {
+    
+    private final Random random;
+    
+    public RandomNumberPicker(Random random) {
+        this.random = random;
+    }
+    
+    public List<Integer> pickUnique(int start, int end, int count) {
+        List<Integer> result = new ArrayList<>();
+        
+        while (result.size() < count) {
+            int random = this.random.nextInt(end) + start;
+            if (!result.contains(random)) {
+                result.add(random);
+            }
+        }
+        
+        return Collections.unmodifiableList(result);
+    }
+}
