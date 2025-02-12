2bce2d57f0001374e19df06b6540178ee8cb8ef9
CheChe903 <cheche903@naver.com>
2025-02-12 16:18:16 +0900

    feat(NumberGenerator): 숫자 list 생성 메서드 제거
    숫자의 중복을 체크하는 책임은 숫자 생성기와 맞지 않으므로 제거합니다.

--- src/main/java/lotto/utils/NumberGenerator.java
+++ src/main/java/lotto/utils/NumberGenerator.java
@@ -1,24 +1,11 @@
 package lotto.utils;
 
-import java.util.List;
 import java.util.Random;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 public class NumberGenerator {
 
-    public static List<Integer> numberGenerator(int numberQuantity, int minValue, int maxValue) {
+    public static int numberGenerator(int minValue, int maxValue) {
         Random random = new Random();
-
-        SortedSet<Integer> lottoSet = new TreeSet<>();
-        while (true) {
-            int ranNum = random.nextInt(maxValue - minValue + 1) + minValue;
-            lottoSet.add(ranNum);
-            if (lottoSet.size() == numberQuantity) {
-                break;
-            }
-        }
-
-        return List.copyOf(lottoSet);
+        return random.nextInt(maxValue - minValue - 1) + minValue;
     }
 }
