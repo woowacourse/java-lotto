508c6e60ce146afc3ac35a9c2954ea24ca227d45
wogus <ind07152@naver.com>
2025-02-12 17:42:12 +0900

    test(domain) : 로또 정렬 조회 기능 테스트

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -3,6 +3,8 @@ package domain;
 
 
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -12,20 +14,32 @@ class LottoTest {
     @DisplayName("로또_번호_범위_테스트")
     @Test
     void rangeTest() {
-        Assertions.assertThatThrownBy(() -> new Lotto(List.of(0,1,2,3,4,5)))
+        assertThatThrownBy(() -> new Lotto(List.of(0,1,2,3,4,5)))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("로또 번호 범위 오류");
     }
+
     @DisplayName("로또 번호 중복 테스트")
     @Test
     void duplicateTest() {
-        Assertions.assertThatThrownBy(() -> new Lotto(List.of(1,1,2,3,4,5)))
+        assertThatThrownBy(() -> new Lotto(List.of(1,1,2,3,4,5)))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("로또 번호 중복 오류");
     }
+
     @DisplayName("로또 번호 갯수 테스트")
     @Test
     void sizeTest() {
-        Assertions.assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,6,7)))
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,6,7)))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("로또 번호 갯수 오류");
+    }
+
+    @DisplayName("로또 번호 정렬 테스트")
+    @Test
+    void sortTest(){
+        // given
+        Lotto lotto = new Lotto(List.of(6,5,4,3,2,1));
+        // when & then
+        List<Integer> numbers = lotto.getSortedNumbers();
+        assertThat(numbers).containsExactly(1,2,3,4,5,6);
     }
 
 }
