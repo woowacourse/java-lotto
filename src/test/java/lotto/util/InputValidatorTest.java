2bcf59f1928051fddc0003d9a86838e2f3af9ba9
Mint <audwl03071@gmail.com>
2025-02-12 16:53:19 +0900

    test(inputValidator): inputValidator 테스트 작성

---
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -0,0 +1,32 @@
+package lotto.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputValidatorTest {
+
+    @Test
+    void 입력값이_null이면_예외가_발생한다() {
+        // Given
+
+        // When & Then
+        Assertions.assertThatThrownBy(() -> InputValidator.validate(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력값은 null일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    @ValueSource(strings = {" ", "\t"})
+    void 입력값이_비어있으면_예외가_발생한다(final String input) {
+        // Given
+
+        // When & Then
+        Assertions.assertThatThrownBy(() -> InputValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력값은 빈 값일 수 없습니다.");
+    }
+}
