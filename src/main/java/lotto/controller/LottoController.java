5c0a23fc7085dec708526d94710d17cfd79628ef
bowook <mbosx316@naver.com>
2025-02-12 14:54:44 +0900

    feat(OutputView): 로또 구매 개수 출력 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,14 @@
 package lotto.controller;
 
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
     private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
     public void run() {
         int money = inputView.inputMoney();
+        outputView.printCount(money / 1000);
     }
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+public class OutputView {
+
+    public void printCount(int count) {
+        System.out.println(String.format("%d개를 구매했습니다.", count));
+    }
+}
