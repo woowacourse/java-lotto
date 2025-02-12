76bc1936fa3dd86623b48ea6de2e5cb3ae90a9c1
Firedrago95 <yonghwa95@naver.com>
2025-02-12 17:54:08 +0900

    refactor: InputView 메서드 분리 및 상수화

---
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -0,0 +1,19 @@
+package lotto.constant;
+
+public enum ErrorMessage {
+    EMPTY("빈 값을 입력할 수 없습니다."),
+    NOT_NUMBER("숫자가 아닌 값을 입력할 수 없습니다.")
+    ;
+
+    private static final String PREFIX = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return PREFIX + message;
+    }
+
+}
