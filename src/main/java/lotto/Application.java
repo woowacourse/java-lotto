8862479b0b83f8d281260016b1b56e11b99763a9
ljhee92 <weilainib7@gmail.com>
2025-02-12 15:26:51 +0900

    feat: 구입 금액에 따른 로또 발행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,7 +8,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
-        int amount = scanner.nextInt();
-        List<Set<Integer>> lottos = new LottoManager().purchase(amount);
+        int purchaseAmount = scanner.nextInt();
+        List<Set<Integer>> lottos = new LottoManager().purchase(purchaseAmount);
     }
 }
--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -5,17 +5,25 @@ import java.util.List;
 import java.util.Set;
 
 public class LottoManager {
-    public List<Set<Integer>> purchase(final int amount) {
-        validateAmount(amount);
-        return new ArrayList<>();
+    private static final int LOTTO_UNIT_PRICE = 1000;
+
+    public List<Set<Integer>> purchase(final int purchaseAmount) {
+        validatePurchaseAmount(purchaseAmount);
+        int lottoAmount = purchaseAmount / LOTTO_UNIT_PRICE;
+        List<Set<Integer>> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoAmount; i++) {
+            Set<Integer> lotto = new LottoMachine().createLotto();
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 
-    private static void validateAmount(final int amount) {
-        if (amount % 1000 != 0) {
+    private void validatePurchaseAmount(final int purchaseAmount) {
+        if (purchaseAmount % LOTTO_UNIT_PRICE != 0) {
             throw new IllegalArgumentException("구입금액은 1000원으로 나누어져야 합니다.");
         }
 
-        if (amount <= 0) {
+        if (purchaseAmount <= 0) {
             throw new IllegalArgumentException("구입금액은 양수여야 합니다.");
         }
     }
