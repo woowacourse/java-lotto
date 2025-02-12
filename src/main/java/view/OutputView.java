bfbf136206b1d69eb99ee0fcb45efc6866de8c10
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-12 16:58:15 +0900

    feat(view): 로또 번호 출력 메서드 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,8 @@
 package view;
 
+import dto.LottoDto;
+import java.util.List;
+
 public class OutputView {
     private static final String PURCHASE_COUNT_MESSAGE = "개를 구매하였습니다.";
 
@@ -7,4 +10,10 @@ public class OutputView {
         System.out.printf("%d%s\n", count, PURCHASE_COUNT_MESSAGE);
     }
 
+    public void printLottoNumbers(List<LottoDto> lottoDtos) {
+        for (LottoDto lottoDto : lottoDtos) {
+            List<Integer> numbers = lottoDto.getNumbers();
+            System.out.println(numbers.toString());
+        }
+    }
 }
