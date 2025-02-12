399e1baab65cde05de916829b9d14bb503b531df
saera <yook.saera@gmail.com>
2025-02-12 16:43:58 +0900

    feat: Lottos 모델 구현

---
+++ src/main/java/domain/Lottos.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(final List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos ofSize(final int quantity) {
+        List<Lotto> lottos = new ArrayList<>(quantity);
+        for (int i = 0; i < quantity; i++) {
+            Lotto generatedLotto = Lotto.of(LottoGenerator.generate());
+            lottos.add(generatedLotto);
+        }
+        return new Lottos(lottos);
+    }
+
+
+}
