13c5d695272e6268ad09e60e9c73ff61f2efc357
2jin2.1031 <2jin2.1031@gmail.com>
2025-02-12 17:46:26 +0900

    feat: 보너스 번호 검증 로직 구현

--- src/main/java/model/Bonus.java
+++ src/main/java/model/Bonus.java
@@ -4,16 +4,38 @@ public class Bonus {
 
     private Integer number;
 
-    public Bonus(final String input) {
+    public static Bonus of(final String input, final Lotto lotto) {
         validateInteger(input);
-        this.number = Integer.parseInt(input);
+        return new Bonus(Integer.parseInt(input), lotto);
     }
 
-    private void validateInteger(String input) {
+    public Bonus(final Integer number, final Lotto lotto) {
+        validateRange(number);
+        validateDuplicateWithLotto(number, lotto);
+        this.number = number;
+    }
+
+    private static void validateInteger(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("보너스 번호는 숫자여야 합니다.");
         }
+    }
+
+    private void validateRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("보너스 번호는 1부터 45사이여야 합니다.");
+        }
+    }
+
+    private void validateDuplicateWithLotto(Integer number, Lotto lotto) {
+        if (lotto.getNumbers().contains(number)) {
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복되지 말아야 합니다.");
+        }
+    }
+
+    public Integer getNumber() {
+        return number;
     }
 }
