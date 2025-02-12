efe5f69823724f91b38cefbb066f8cc98d455cf6
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-12 19:06:39 +0900

    test: LottoNumber 테스트 작성
    - 로또 번호 validation 을 테스트.

---
+++ src/test/java/src/model/vo/LottoNumberTest.java
@@ -0,0 +1,16 @@
+package src.model.vo;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static src.model.vo.LottoNumber.MAX_NUMBER_RANGE;
+import static src.model.vo.LottoNumber.MIN_NUMBER_RANGE;
+
+import org.junit.jupiter.api.Test;
+
+class LottoNumberTest {
+
+    @Test
+    void 로또_번호가_1부터_45사이가_아니면_예외_발생() {
+        assertThrows(IllegalArgumentException.class, () -> LottoNumber.from(MIN_NUMBER_RANGE - 1));
+        assertThrows(IllegalArgumentException.class, () -> LottoNumber.from(MAX_NUMBER_RANGE + 1));
+    }
+}
