58a6b7cc1984487c9df538b43428a3a580948bab
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-12 19:04:48 +0900

    feat: 로또 번호 vo 구현
    - 로또 번호 원시값을 포장.
    - 로또 번호에 대한 validation 수행.
    - 상수는 같은 패키지 사용을 위해 protected 로 설정.

---
+++ src/main/java/src/model/vo/LottoNumber.java
@@ -0,0 +1,31 @@
+package src.model.vo;
+
+public class LottoNumber {
+
+    protected static final int MIN_NUMBER_RANGE = 1;
+    protected static final int MAX_NUMBER_RANGE = 45;
+
+    private final int value;
+
+    private LottoNumber(final int value) {
+        this.value = value;
+    }
+
+    public static LottoNumber from(final int value) {
+        validate(value);
+        return new LottoNumber(value);
+    }
+
+    private static void validate(final int value) {
+        if (value < MIN_NUMBER_RANGE || value > MAX_NUMBER_RANGE) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이여야 합니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumber{" +
+                "value=" + value +
+                '}';
+    }
+}
