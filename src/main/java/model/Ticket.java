d2344789756c2d877e6d7fe6cae8551e19589e60
taek2222 <oot309@daum.net>
2025-02-12 16:58:19 +0900

    feat: 구매 문구 출력 기능

--- src/main/java/model/Ticket.java
+++ src/main/java/model/Ticket.java
@@ -1,19 +1,24 @@
 package model;
 
+import dto.TicketAmountResponse;
+
 import static utils.Validator.validateDivide;
 import static utils.Validator.validateRange;
 
 public class Ticket {
 
-    private static final int TICKET_AMOUNT = 1000;
+    private static final int TICKET_PRICE = 1000;
     
     private final int purchaseMoney;
 
     public Ticket(final int purchaseMoney) {
-        validateRange(purchaseMoney, Integer.MAX_VALUE, TICKET_AMOUNT);
-        validateDivide(purchaseMoney, TICKET_AMOUNT);
+        validateRange(purchaseMoney, Integer.MAX_VALUE, TICKET_PRICE);
+        validateDivide(purchaseMoney, TICKET_PRICE);
         this.purchaseMoney = purchaseMoney;
     }
 
 
+    public TicketAmountResponse getTicketAmount() {
+        return new TicketAmountResponse(purchaseMoney / TICKET_PRICE);
+    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,14 @@
 package view;
 
+import dto.TicketAmountResponse;
+
 public class OutputView {
 
     public void printStartMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printTicketPurchaseAmount(TicketAmountResponse response) {
+        System.out.println(response.amount() + "개를 구매했습니다.");
     }
 }
