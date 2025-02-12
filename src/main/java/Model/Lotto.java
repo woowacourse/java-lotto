6df626c42efca9f30d7ecc656cf1e10547781203
jhan0121 <jhan0121@gmail.com>
2025-02-12 14:57:02 +0900

    feat: Lotto 생성 기능 구현
    (페어 2차)

--- src/main/java/Model/Lotto.java
+++ src/main/java/Model/Lotto.java
@@ -1,8 +1,26 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Lotto {
 
     private List<Integer> lottoNumber;
+
+    public Lotto(){
+        this.lottoNumber = new ArrayList<>();
+        createLotto();
+    }
+
+    private void createLotto(){
+        Random random = new Random();
+        for (int i = 0; i < 6; i++){
+            this.lottoNumber.add(random.nextInt(45) + 1);
+        }
+    }
+
+    public List<Integer> getLottoNumber() {
+        return lottoNumber;
+    }
 }
