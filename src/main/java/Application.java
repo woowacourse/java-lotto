7e5d581a2c91ac036bc36220e55a3c872bf6eda0
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-12 16:22:13 +0900

    feat: 메인 메소드 작성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,15 @@
+import controller.Controller;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        Controller controller = new Controller(inputView, outputView);
+        controller.run();
 
     }
 }
