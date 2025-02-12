67b60edd06be0ceb751d6963aacc78fa831cb205
Yeongjae Kim <sees77770@gmail.com>
2025-02-12 17:23:38 +0900

    feat(InputView): 보너스 번호를 입력받는다

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -17,7 +17,7 @@ public class InputView {
         return purchaseAmount;
     }
 
-    public List<String> askWinningNumbers() {
+    public List<Integer> askWinningNumbers() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
@@ -25,5 +25,15 @@ public class InputView {
         sc.close();
 
         return InputConverter.convertWinningNumbers(winningNumbers);
+    }
+
+    public int askBonusNumber() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("보너스 볼을 입력해 주세요.");
+        String bonusNumber = sc.nextLine();
+        sc.close();
+
+        return InputConverter.convertBonusNumber(bonusNumber);
     }
 }
