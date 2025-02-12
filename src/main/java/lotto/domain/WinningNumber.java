2be61a79c47324f97cf5ebe76a9716941e8eaf83
horizonpioneer <quantumjump10@gmail.com>
2025-02-12 17:08:46 +0900

    feat(WinningNumber): 당첨 번호 검증 기능 추가

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumber {
 
@@ -8,12 +10,20 @@ public class WinningNumber {
 
     public WinningNumber(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplicate(numbers);
         this.winningNumbers = numbers;
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != 6) {
+            throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
         }
     }
 
