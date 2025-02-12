39c7190376fd89b64a735e0faca7e4a64827d95b
jhan0121 <jhan0121@gmail.com>
2025-02-12 15:17:06 +0900

    feat: OutputView 구현
    구입금액안내, 개수 확인 문자열 출력

---
+++ src/main/java/View/OutputView.java
@@ -0,0 +1,12 @@
+package View;
+
+public class OutputView {
+
+    public static void inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void printPurchaseCount(int count) {
+        System.out.printf("%d개를 구매했습니다.\n", count);
+    }
+}
