155a781863c1b5dc8016a53f02a530e65130825e
taek2222 <oot309@daum.net>
2025-02-12 19:14:28 +0900

    feat: 보너스 볼 입력 기능
    - 숫자 검증 및 범위 검증 로직을 추가했습니다.

--- src/main/java/model/WinningLotto.java
+++ src/main/java/model/WinningLotto.java
@@ -1,13 +1,19 @@
 package model;
 
+import utils.Validator;
+
 public class WinningLotto {
 
     private Lotto lotto;
     private int bonusNumber;
 
-    public WinningLotto(String input, int bonusNumber) {
+    public WinningLotto(String input, String bonusNumber) {
         lotto = new Lotto(input);
-        this.bonusNumber = bonusNumber;
-    }
 
+        Validator.validateNumeric(bonusNumber);
+        int parsed = Integer.parseInt(bonusNumber);
+
+        Validator.validateRange(parsed, 45, 1);
+        this.bonusNumber = parsed;
+    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,14 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String ENTER_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String ENTER_BONUS_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요";
-
-    Scanner scanner = new Scanner(System.in);
-
-    private String enter(String message) {
-        System.out.printf(message);
-        return scanner.nextLine();
-    }
+    private static final String ENTER_BONUS_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
     public String enterPurchasePrice() {
         return enter(ENTER_PURCHASE_PRICE_MESSAGE);
