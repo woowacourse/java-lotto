e06883b8955b47540293ab04e5bd0180ffdfd927
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-12 14:30:29 +0900

    feat: 로또 번호 model 작성

--- src/main/java/model/Number.java
+++ src/main/java/model/Number.java
@@ -1,6 +1,7 @@
 package model;
 
 public record Number(int value) {
+
     public Number {
         if (value <= 0 || value > 45) {
             throw new IllegalArgumentException("로또 번호는 1 이상 45 이하여야 합니다.");
