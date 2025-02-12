b8081a9810faa67bc07d664acaad1e9c88cd8682
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-12 15:30:27 +0900

    test: 로또 구조 변경에 따른 테스트 변경

--- src/test/java/model/LottoTest.java
+++ src/test/java/model/LottoTest.java
@@ -2,7 +2,7 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
@@ -10,8 +10,13 @@ class LottoTest {
     @Test
     void 로또의_로또_번호는_6개이다() {
         assertThatThrownBy(() -> {
-            Lotto lotto = new Lotto(List.of(
-                    1, 2, 3, 4, 5, 6, 7
+            Lotto lotto = new Lotto(Set.of(
+                    new Number(1),
+                    new Number(2),
+                    new Number(3),
+                    new Number(4),
+                    new Number(5),
+                    new Number(6)
             ));
         });
     }
