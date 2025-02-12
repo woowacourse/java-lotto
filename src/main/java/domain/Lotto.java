f782ed68e396314ab932974414d63e377490e474
ppparkta <pppppark2@gmail.com>
2025-02-12 17:17:16 +0900

    feat: 로또 validation 기능 추가
    - 로또 범위 검증 추가
    - 로또 중복 검증 추가
    - 로또 6개 검증 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,12 +6,41 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    private Lotto(List<Integer> numbers) {
-        this.numbers = numbers;
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers.stream().sorted().toList();
     }
 
     public static Lotto from(NumberGenerator numberGenerator) {
         List<Integer> numbers = numberGenerator.generateNumber();
         return new Lotto(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        // 1~45 체크, 중복체크, 6개 체크
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("");
+        }
+        if (hasDistinctNumber(numbers)) {
+            throw new IllegalArgumentException("");
+        }
+        if (numbers.stream().anyMatch(number -> !isValidNumber(number))) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+
+
+    private boolean isValidNumber(int number) {
+        return number > 0 && number <= 45;
+    }
+
+    private boolean hasDistinctNumber(List<Integer> numbers) {
+        int distinctedSize = numbers.stream()
+                .distinct()
+                .toList()
+                .size();
+
+        return numbers.size() != distinctedSize;
     }
 }
