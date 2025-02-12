5709e42939f2db41b2d266725c9c59d6c9c66122
rladmstn <oz33177351@gmail.com>
2025-02-12 14:37:16 +0900

    feat: 구입금액 음수 검증 기능 구현
    Co-authored-by: YehyeokBang qkddpgur318@gmail.com

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -6,7 +6,15 @@ public class InputView {
 
     public int requestPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        Scanner in = new Scanner(System.in);
+        int purchaseAmount = in.nextInt();
+        validatePositive(purchaseAmount);
+        return purchaseAmount;
+    }
+
+    private void validatePositive(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("음수일 수 없습니다.");
+        }
     }
 }
