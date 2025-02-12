79c9457a9ec0594b214c54f66f301cf98fe3b1a5
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-12 19:50:23 +0900

    feat(WinningNumber): 당첨 번호 도메인 구현

---
+++ src/main/java/domain/WinningNumber.java
@@ -0,0 +1,38 @@
+package domain;
+
+import java.util.List;
+
+public class WinningNumber {
+    // 당첨 번호 Lotto
+    private final Lotto winningLotto;
+    // 보너스 번호
+    private final BonusNumber bonusNumber;
+
+    public WinningNumber(final Lotto winningLotto, final BonusNumber bonusNumber) {
+        validateDuplicate(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    // 중복 검증
+    private void validateDuplicate(final Lotto winningLotto, final BonusNumber bonusNumber) {
+        if (winningLotto.getNumbers().contains(bonusNumber.getValue())) {
+            throw new IllegalArgumentException("보너스 번호는 로또 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    // 랭크 반환
+    public LottoRank calculateRankByLotto(final Lotto lotto) {
+        final List<Integer> numbers = lotto.getNumbers();
+        final boolean isBonus = numbers.contains(this.bonusNumber.getValue());
+        return LottoRank.findByMatchedCountAndIsBonus(countMatchedNumbers(lotto), isBonus);
+    }
+
+    // 일치하는 로또 갯수 계산
+    private int countMatchedNumbers(final Lotto lotto) {
+        final List<Integer> numbers = lotto.getNumbers();
+        return (int) numbers.stream()
+                .filter(number -> winningLotto.getNumbers().contains(number))
+                .count();
+    }
+}
