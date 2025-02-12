224736610e63d6319b3cb538264746840d9c203d
ppparkta <pppppark2@gmail.com>
2025-02-12 17:29:17 +0900

    feat: 당첨 로또 유효성 검사 추가

--- src/main/java/domain/AnswerLotto.java
+++ src/main/java/domain/AnswerLotto.java
@@ -8,7 +8,21 @@ public class AnswerLotto {
     private final int bonusNumber;
 
     public AnswerLotto(Lotto lotto, final int bonusNumber) {
+        validate(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Lotto lotto, final int bonusNumber) {
+        if (lotto.hasDuplicateNumber(bonusNumber)) {
+            throw new IllegalArgumentException("");
+        }
+        if (!isValidNumber(bonusNumber)) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    private boolean isValidNumber(int number) {
+        return number > 0 && number <= 45;
     }
 }
