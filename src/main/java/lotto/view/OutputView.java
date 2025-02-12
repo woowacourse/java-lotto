fa4d59c2e6abb65836c009cbc0644e95ae6cc116
horizonpioneer <quantumjump10@gmail.com>
2025-02-12 15:52:48 +0900

    feat(OutputView): 구매 수량 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void printInputMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void printCountMessage(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
 }
