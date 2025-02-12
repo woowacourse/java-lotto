3de76d7edbdc0c2cf386d3b69770dfe5aea7f7d9
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-12 15:47:13 +0900

    feat: record 비교 구현

--- src/main/java/model/Number.java
+++ src/main/java/model/Number.java
@@ -1,10 +1,15 @@
 package model;
 
-public record Number(int value) {
+public record Number(int value) implements Comparable<Number> {
 
     public Number {
         if (value <= 0 || value > 45) {
             throw new IllegalArgumentException("로또 번호는 1 이상 45 이하여야 합니다.");
         }
+    }
+
+    @Override
+    public int compareTo(Number o) {
+        return this.value - o.value;
     }
 }
