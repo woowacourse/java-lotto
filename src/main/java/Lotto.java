eb5d8fc192c16d6cbdaca6a4e5e6566314e2a232
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-12 15:08:09 +0900

    feat(Lotto): 로또 번호 관련 클래스 생성
    - 로또 번호 검증 추가

---
+++ src/main/java/Lotto.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private static void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
