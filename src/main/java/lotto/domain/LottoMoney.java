c26b5a86d7b5dc6ebd824a6e93c6f370b97b3b98
yeonnhuu <yeonnhuu@gmail.com>
2025-02-12 17:12:43 +0900

    :sparkles: feat(LottoMoney): add LottoMoney and LottoMoneyTest

---
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import static lotto.util.Constant.LOTTO_MONEY_UNIT;
+import static lotto.util.ErrorHandler.INVALID_AMOUNT;
+import static lotto.util.ErrorHandler.INVALID_NUMBER;
+import static lotto.util.ErrorHandler.INVALID_UNIT;
+
+public class LottoMoney {
+
+    private int lottoMoney;
+
+    public LottoMoney(String input) {
+       int amount = parse(input);
+       validate(amount);
+       lottoMoney = amount;
+    }
+
+    private int parse(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw INVALID_NUMBER.getException();
+        }
+    }
+
+    private void validate(int amount) {
+        validateAmount(amount);
+        validateUnit(amount);
+    }
+
+    private void validateAmount(int amount) {
+        if (amount < LOTTO_MONEY_UNIT) {
+            throw INVALID_AMOUNT.getException();
+        }
+    }
+
+    private void validateUnit(int amount) {
+        if (amount % LOTTO_MONEY_UNIT != 0) {
+            throw INVALID_UNIT.getException();
+        }
+    }
+}
