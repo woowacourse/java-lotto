78be41f6899051929e03feaad383054714d28341
jhan0121 <jhan0121@gmail.com>
2025-02-12 18:13:38 +0900

    feat: LottoController run 로직 구현

--- src/main/java/Controller/LottoController.java
+++ src/main/java/Controller/LottoController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.Lotto;
 import Model.Lottos;
 import Model.WinnerNumber;
 import Service.LottoService;
@@ -18,14 +19,19 @@ public class LottoController {
         OutputView.inputPurchaseAmount();
         int price = InputView.inputPrice();
         int lottoAmount = lottoService.lottoCount(price);
-
+        OutputView.printPurchaseCount(lottoAmount);
         Lottos lottos = new Lottos(lottoAmount);
         OutputView.printLottoResults(lottos);
         OutputView.inputWinnerNumbers();
         List<Integer> winnerNumbers = InputView.inputWinnerNumbers();
+        OutputView.inputBonusBall();
         int bonusBall = InputView.inputBonusBall(winnerNumbers);
         WinnerNumber winnerNumber = new WinnerNumber(winnerNumbers, bonusBall);
-
-
+        for (Lotto lotto : lottos.getLottos()) {
+            winnerNumber.compareWinning(lotto);
+        }
+        double result = lottoService.lottoRateOfReturn(price);
+        OutputView.winningStatistics(result);
+        InputView.close();
     }
 }
