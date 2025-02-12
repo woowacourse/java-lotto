b536be26f27c89f18112ee1144fd9df53a7274b3
Yeongjae Kim <sees77770@gmail.com>
2025-02-12 14:52:54 +0900

    feat(Lotto): 랜덤한 숫자들로 로또를 생성한다

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,8 +1,27 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 public class Lotto {
-//    private final List<Integer> numbers;
+
+    private final List<Integer> numbers;
 
     public Lotto() {
+        this.numbers = new ArrayList<>();
+        Random random = new Random();
+        
+        for (int i = 0; i < 6; i++) {
+            int number = random.nextInt(1, 46);
+            numbers.add(number);
+        }
+    }
+
+    public List<String> getNumbers() {
+        return numbers.stream()
+                .map(Object::toString)
+                .toList();
     }
 }
