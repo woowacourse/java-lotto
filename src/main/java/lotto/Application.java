4cbc5d7ef92cd9d94f4318cf77f78601b6bdda50
Mint <audwl03071@gmail.com>
2025-02-12 20:41:26 +0900

    feat(LottoController): 로또 개수 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,14 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        LottoController lottoController = new LottoController(inputView);
+        OutputView outputView = new OutputView();
+        LottoController lottoController = new LottoController(inputView, outputView);
         lottoController.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,23 +3,32 @@ package lotto.controller;
 import lotto.domain.LottoPrice;
 import lotto.util.StringParser;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
     private final InputView inputView;
+    private final OutputView outputView;
 
-    public LottoController(final InputView inputView) {
+    public LottoController(final InputView inputView, final OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void run() {
         LottoPrice lottoPrice = getLottoPrice();
-        System.out.println();
+        int lottoCount = getLottoCount(lottoPrice);
     }
 
     private LottoPrice getLottoPrice() {
         int parsedAmount = StringParser.parseInt(inputView.readPurchasePrice());
         return new LottoPrice(parsedAmount);
+    }
+
+    private int getLottoCount(final LottoPrice lottoPrice) {
+        int lottoCount = lottoPrice.calculateLottoCount();
+        outputView.printLottoCount(lottoCount);
+        return lottoCount;
     }
 
 }
