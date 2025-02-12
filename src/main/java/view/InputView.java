6da9f2aa3761b07f3a803a8cfd2ef7627645ba9a
Yeongjae Kim <sees77770@gmail.com>
2025-02-12 15:30:55 +0900

    feat(InputView): 구입 금액을 입력받는다

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,18 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
+
+    public String askPurchaseAmount() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(PURCHASE_AMOUNT_PROMPT);
+        String purchaseAmount = sc.nextLine();
+        sc.close();
+
+        return purchaseAmount;
+    }
 }
