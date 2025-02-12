8a1fcb0771d86e94bf2b293d3744a7e0cc1afbe0
jhan0121 <jhan0121@gmail.com>
2025-02-12 16:59:47 +0900

    feat: OutputView 당첨 번호 출력 기능 구현

--- src/main/java/Model/Lottos.java
+++ src/main/java/Model/Lottos.java
@@ -10,11 +10,18 @@ public class Lottos {
     public Lottos(int count) {
         this.lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            lottos.add(new Lotto());
+            lottos.add(new Lotto(LottoCreator.createLotto()));
         }
     }
 
     public List<Lotto> getLottos() {
         return lottos;
     }
+
+    public void printLottoNumbers(){
+        for (Lotto l : this.lottos){
+            System.out.println(l.printLottoNumber());
+        }
+    }
+
 }
--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,5 +1,7 @@
 package View;
 
+import Model.Lottos;
+
 public class OutputView {
 
     public static void inputPurchaseAmount() {
@@ -9,4 +11,23 @@ public class OutputView {
     public static void printPurchaseCount(int count) {
         System.out.printf("%d개를 구매했습니다.\n", count);
     }
+
+    public static void printLottoResults(Lottos lottos){
+        lottos.printLottoNumbers();
+    }
+
+    public static void inputWinnerNumbers(){
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    }
+
+    public static void inputBonusBall(){
+        System.out.println("보너스 볼을 입력해 주세요.");
+    }
+
+    public static void winningStatistics(){
+        System.out.println("당첨 통계\n"
+                + "---------");
+
+    }
+
 }
