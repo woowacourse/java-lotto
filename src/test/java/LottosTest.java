8845a56b2e11aed14182a5f67c8f07465ba977ec
supernovaMK <minki533@naver.com>
2025-02-12 17:02:46 +0900

    test(Lottos): 로또들을 관리하는 기능 테스트 추가
    - 로또를 저장하는 기능 검증

---
+++ src/test/java/LottosTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class LottosTest {
+    @Test
+    @DisplayName("여러 로또가 저장되는지 테스트")
+    void 여러_로또가_저장되는지_테스트() {
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> numbers2 = Arrays.asList(1, 2, 3, 41, 5, 6);
+        List<Integer> numbers3 = Arrays.asList(1, 2, 31, 4, 5, 6);
+        List<Integer> numbers4 = Arrays.asList(1, 21, 3, 4, 5, 6);
+
+        Lottos lottos = new Lottos();
+
+        lottos.addLotto(new Lotto(numbers1));
+        lottos.addLotto(new Lotto(numbers2));
+        lottos.addLotto(new Lotto(numbers3));
+        lottos.addLotto(new Lotto(numbers4));
+
+        assertThat(lottos.getLottos().size()).isEqualTo(4);
+    }
+
+}
