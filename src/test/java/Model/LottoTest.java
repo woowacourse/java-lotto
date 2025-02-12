81f2c7c050551f9ec1100c208bd18220a4967da5
jhan0121 <jhan0121@gmail.com>
2025-02-12 16:22:32 +0900

    test: 로또 번호 출력 확인 테스트 생성

--- src/test/java/Model/LottoTest.java
+++ src/test/java/Model/LottoTest.java
@@ -1,7 +1,5 @@
 package Model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class LottoTest {
     @Test
     void 로또_생성_테스트 () throws Exception {
-        Lotto lotto = new Lotto();
+        Lotto lotto = new Lotto(LottoCreator.createLotto());
         List<Integer> testLotto = lotto.getLottoNumber();
 
         Assertions.assertThat(testLotto.size()).isEqualTo(6);
@@ -18,4 +16,10 @@ class LottoTest {
         }
     }
 
+    @Test
+    void 로또_확인(){
+        Lotto lotto = new Lotto(List.of(1,13,5,4,32,39));
+        String result = lotto.printLottoNumber();
+        Assertions.assertThat(result).isEqualTo("[1,4,5,13,32,39]");
+    }
 }
