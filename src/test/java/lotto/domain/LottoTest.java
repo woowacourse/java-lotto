5fa301cdfe97033ddfa3100a0b424aa281c1618e
Firedrago95 <yonghwa95@naver.com>
2025-02-12 15:57:32 +0900

    feat: 로또 번호 등수를 판정한다

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,6 +2,9 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoTest {
@@ -15,5 +18,13 @@ public class LottoTest {
     void 로또_티켓을_랜덤으로_생성한다() {
         Lottos lottos = new Lottos(3000);
         assertThat(lottos.getLottos().get(0).numbers.size()).isEqualTo(6);
+    }
+
+    @Test
+    void 로또_번호의_등수를_판정한다() {
+        WinningNumbers winningNumbers = new WinningNumbers(new Lotto(List.of(1,2,3,4,5,6)), 7);
+        assertThat(winningNumbers.getRank(new Lotto(List.of(1,2,3,4,5,6)))).isEqualTo(Rank.FIRST);
+        assertThat(winningNumbers.getRank(new Lotto(List.of(1,2,3,4,5,7)))).isEqualTo(Rank.SECOND);
+        assertThat(winningNumbers.getRank(new Lotto(List.of(1,2,3,4,5,8)))).isEqualTo(Rank.THIRD);
     }
 }
