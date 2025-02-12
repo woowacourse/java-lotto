58c516fc58dfa8a2dcadd8cd5b1f02b9f3b95d28
jhan0121 <jhan0121@gmail.com>
2025-02-12 15:15:08 +0900

    feat: Lottos 개수만큼 로또 생성 기능 구현

--- src/main/java/Model/Lottos.java
+++ src/main/java/Model/Lottos.java
@@ -1,8 +1,20 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
 
     private List<Lotto> lottos;
+
+    public Lottos(int count) {
+        this.lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto());
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 }
