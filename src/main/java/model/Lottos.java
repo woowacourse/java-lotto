bf0ec07b9f35889988b1e709d6b080b202d094ae
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-12 14:30:09 +0900

    feat: 사용자가 구매한 로또 목록 model 작성

---
+++ src/main/java/model/Lottos.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}
