8a61d7d41633610b165f143d48fcbf4f8988f1ea
ljhee92 <weilainib7@gmail.com>
2025-02-12 15:15:19 +0900

    test: 구입 금액 검증 - 구입 금액이 1000원으로 나누어지지 않으면 IllegalArgumentException을 던진다. - 구입 금액이 양수가 아니라면 IllegalArgumentException을 던진다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
         int amount = scanner.nextInt();
+        List<Set<Integer>> lottos = new LottoManager().purchase(amount);
     }
 }
