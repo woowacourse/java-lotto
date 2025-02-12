022e032117c5a0cc249c453aa731796dc8d122d9
dltmddus <tmddus4671@gmail.com>
2025-02-12 17:23:04 +0900

    feat(LottoMachine): 로또 생성 기능 추가

---
+++ src/main/java/domain/LottoMachine.java
@@ -0,0 +1,28 @@
+package domain;
+
+import static domain.LottoTicket.LOTTO_MAX_NUMBER;
+import static domain.LottoTicket.LOTTO_MIN_NUMBER;
+import static domain.LottoTicket.LOTTO_SIZE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    public LottoTicket generateLottoTicket(IntegerGenerator generator) {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < LOTTO_SIZE; i++) {
+            int number = generator.generateIntegerInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER);
+            lottoNumbers.add(number);
+        }
+        return new LottoTicket(lottoNumbers);
+    }
+
+    public List<LottoTicket> generateLottoTickets(int lottoTicketNumber, IntegerGenerator generator) {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        for (int i = 0; i < lottoTicketNumber; i++) {
+            LottoTicket lottoTicket = generateLottoTicket(generator);
+            lottoTickets.add(lottoTicket);
+        }
+        return lottoTickets;
+    }
+}
