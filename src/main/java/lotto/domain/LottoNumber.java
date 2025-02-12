222772a02e861576561a21c29955221a38bbbba4
kimdev <kim__dev@hufs.ac.kr>
2025-02-12 18:09:25 +0900

    feat(LottoNumber): LottoNumber 객체 생성

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class LottoNumber {
+
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < MINIMUM_NUMBER  || number > MAXIMUM_NUMBER) {
+            throw new IllegalArgumentException("로또 번호는 1과 45 사이여야 합니다.");
+        }
+    }
+}
