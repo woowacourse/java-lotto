f1838d0e98a8f294632fa4da4a854e212c65adbc
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-12 15:00:08 +0900

    feat: 컨트롤러 추가

---
+++ src/main/java/src/controller/LottoController.java
@@ -0,0 +1,15 @@
+package src.controller;
+
+import src.view.input.ConsoleInputView;
+import src.view.input.InputView;
+import src.view.output.ConsoleOutputView;
+import src.view.output.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView = new ConsoleInputView();
+    private final OutputView outputView = new ConsoleOutputView();
+
+    public void run() {
+    }
+}
