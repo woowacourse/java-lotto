600f94017f0fc4d86ff9b93db06b84a8dad3a116
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-12 16:36:16 +0900

    feat(Money): 구입 금액 도메인 구현

---
+++ src/main/java/domain/Money.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class Money {
+
+    private final int amount;
+
+    public Money(final int amount) {
+        validateRange(amount);
+        validateAmount(amount);
+        this.amount = amount;
+    }
+
+    public int getCount() {
+        return amount / 1_000;
+    }
+
+    private void validateAmount(final int amount) {
+        if (amount % 1_000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000 단위 숫자로 입력해야 합니다.");
+        }
+    }
+
+    private void validateRange(final int amount) {
+        if (amount < 1_000) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이어야 합니다.");
+        }
+    }
+
+
+}
