ab1568885f29e409194781e36188f128da6d53e2
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-02-12 16:09:27 +0900

    feat: 로또 번호 6개 검증 추가

--- src/main/java/domain/LottoNumbers.java
+++ src/main/java/domain/LottoNumbers.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Set;
 
 public class LottoNumbers {
+    private static final int LOTTO_VALID_SIZE = 6;
+    private static final String INVALID_LOTTO_SIZE = "로또 번호는 6개여야 합니다.";
     private final List<LottoNumber> lottoNumbers;
     private final String DUPLICATE_LOTTO_NUMBERS = "로또 번호는 중복될 수 없습니다!";
 
@@ -18,6 +20,9 @@ public class LottoNumbers {
         Set<Integer> duplicationSet = new HashSet<>(lottoNumbers);
         if(lottoNumbers.size() != duplicationSet.size()) {
             throw new LottoException(DUPLICATE_LOTTO_NUMBERS);
+        }
+        if(lottoNumbers.size() != LOTTO_VALID_SIZE){
+            throw new LottoException(INVALID_LOTTO_SIZE);
         }
     }
 }
