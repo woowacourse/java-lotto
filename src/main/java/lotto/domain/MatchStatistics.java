74d8347fee3d751ff3f30f1e95b76ab1fa21d628
황재웅 <wodnd0131@naver.com>
2025-02-12 16:57:54 +0900

    feat: MatchStatistics를 선언
    - 당첨 결과에 맞게 변환

---
+++ src/main/java/lotto/domain/MatchStatistics.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.List;
+
+public enum MatchStatistics {
+    MATCH_THREE(3,5000),
+    MATCH_FOUR(4,50000),
+    MATCH_FIVE(5,150000),
+    MATCH_BONUS(5,30000000),
+    MATCH_SIX(6,2000000000),
+    NO_MATCH(0,0);
+    ;
+
+    private int number;
+    private int money;
+    MatchStatistics(int number, int money) {
+    }
+
+    public MatchStatistics getMatchStatistics(int matchNumber, boolean bonus) {
+        if(matchNumber == 5 && bonus) {
+            return MATCH_BONUS;
+        }
+        //당첨 번호 개수  보너스 번호
+        for(MatchStatistics statistics : values()) {
+            if(statistics.number == matchNumber) {
+                return statistics;
+            }
+        }
+        return NO_MATCH;
+    }
+}
