67376983c3c09d46aec103617d17921c35244815
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-12 16:13:46 +0900

    feat(BonusNumber): 보너스 번호 도메인 구현

---
+++ src/main/java/domain/BonusNumber.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class BonusNumber {
+
+    private final int value;
+
+    public BonusNumber(final int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(final int number) {
+        if (number < 0 || number > 45) {
+            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이 숫자여야 합니다.");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
