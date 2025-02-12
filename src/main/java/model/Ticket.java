ead790f32389105945c31e403389c93634ed83bd
taek2222 <oot309@daum.net>
2025-02-12 16:48:31 +0900

    refactor: 구입 금액 객체 생성
    - 객체 분리로, 검증 역할을 내부로 집중시켰습니다.

--- src/main/java/model/Ticket.java
+++ src/main/java/model/Ticket.java
@@ -1,41 +1,19 @@
 package model;
 
-import dto.ReturnOfInvestmentResultResponse;
-import dto.TicketAmountResponse;
-
-import static global.utils.Validator.validateDivisibility;
-import static global.utils.Validator.validateRange;
+import static utils.Validator.validateDivide;
+import static utils.Validator.validateRange;
 
 public class Ticket {
 
-    private static final int TICKET_PRICE = 1_000;
-
+    private static final int TICKET_AMOUNT = 1000;
+    
     private final int purchaseMoney;
 
     public Ticket(final int purchaseMoney) {
-        validatePurchaseMoney(purchaseMoney);
+        validateRange(purchaseMoney, Integer.MAX_VALUE, TICKET_AMOUNT);
+        validateDivide(purchaseMoney, TICKET_AMOUNT);
         this.purchaseMoney = purchaseMoney;
     }
 
-    public int getTicketAmount() {
-        return purchaseMoney / TICKET_PRICE;
-    }
 
-    public TicketAmountResponse createTicketResponse() {
-        return new TicketAmountResponse(getTicketAmount());
-    }
-
-    public ReturnOfInvestmentResultResponse createReturnOfInvestmentResponse(final int totalPrice) {
-        double returnOfInvestment = calculateReturnOfInvestment(totalPrice);
-        return new ReturnOfInvestmentResultResponse(returnOfInvestment, InvestmentOutcome.determine(returnOfInvestment));
-    }
-
-    private static void validatePurchaseMoney(final int purchaseMoney) {
-        validateRange(purchaseMoney, TICKET_PRICE, Integer.MAX_VALUE);
-        validateDivisibility(purchaseMoney, TICKET_PRICE);
-    }
-
-    private double calculateReturnOfInvestment(final int totalPrice) {
-        return Math.round(((double) totalPrice / purchaseMoney) * 100) / 100.0;
-    }
 }
