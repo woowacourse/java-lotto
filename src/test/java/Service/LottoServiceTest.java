c72b69308bb4125889280a4de963aa69309dd75c
jhan0121 <jhan0121@gmail.com>
2025-02-12 18:15:55 +0900

    test: LottoServiceTest 로또 결과 테스트 구현

--- src/test/java/Service/LottoServiceTest.java
+++ src/test/java/Service/LottoServiceTest.java
@@ -1,21 +1,27 @@
 package Service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import Model.Lotto;
+import Model.LottoResult;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 class LottoServiceTest {
 
+    private final LottoService lottoService = new LottoService();
+
     @Test
     void 로또_갯수(){
-        LottoService lottoService = new LottoService();
         int price = 14000;
-
         Assertions.assertThat(lottoService.lottoCount(price)).isEqualTo(14);
 
     }
 
+    @Test
+    void 로또_결과() {
+        int price = 14000;
+        LottoResult.addCount(3, false);
+        Assertions.assertThat(LottoResult.FIFTH.getCount()).isEqualTo(1);
+        double result = lottoService.lottoRateOfReturn(price);
+        Assertions.assertThat(result).isEqualTo(0.35);
+    }
 }
