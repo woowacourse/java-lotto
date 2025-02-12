7201b3bd6e4b9fd8a3c0d59cafd091994cfd7731
ljhee92 <weilainib7@gmail.com>
2025-02-12 15:31:37 +0900

    feat: 구입 금액 입력 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
@@ -7,8 +8,16 @@ import java.util.Set;
 public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
-        Scanner scanner = new Scanner(System.in);
-        int purchaseAmount = scanner.nextInt();
+        int purchaseAmount = getPurchaseAmount();
         List<Set<Integer>> lottos = new LottoManager().purchase(purchaseAmount);
+    }
+
+    private static int getPurchaseAmount() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("구입금액은 숫자여야 합니다.");
+        }
     }
 }
