a2817ade122b84cbc3ee6fb5548e670df6a3510e
ppparkta <pppppark2@gmail.com>
2025-02-12 15:43:58 +0900

    feat: 기본 구성 추가

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -1,10 +1,8 @@
 import controller.LottoController;
-import service.LottoService;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        LottoService lottoService = new LottoService();
-        LottoController lottoController = new LottoController(lottoService);
+        LottoController lottoController = new LottoController();
         lottoController.run();
     }
 }
