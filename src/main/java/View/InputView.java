7655c61550ee60af47882cf958f52ab63fabb0b3
jhan0121 <jhan0121@gmail.com>
2025-02-12 16:59:08 +0900

    feat: InputView 당첨번호 및 보너스 번호 입력 기능 구현

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,10 +1,14 @@
 package View;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class InputView {
 
-    public static int InputPrice() {
+    public static int inputPrice() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         int price = validateInteger(input);
@@ -12,6 +16,65 @@ public class InputView {
         sc.close();
         return price;
     }
+
+    public static List<Integer> inputWinnerNumbers(){
+        Scanner sc = new Scanner(System.in);
+        String input =sc.nextLine();
+        sc.close();
+        validateEmptyInput(input);
+
+        String [] splitInput = input.split(",");
+
+        List<Integer> winnerNumbers = new ArrayList<>();
+        for (String s : splitInput){
+            int number = validateInteger(s);
+            validateNumberRange(number);
+            winnerNumbers.add(number);
+        }
+        validateDuplicateValue(winnerNumbers);
+        return winnerNumbers;
+    }
+
+    public static int inputBonusBall(List<Integer> winnerNumbers){
+        Scanner sc = new Scanner(System.in);
+        String input =sc.nextLine();
+        sc.close();
+
+        validateEmptyInput(input);
+
+        int bonusBall = validateInteger(input);
+
+        validateNumberRange(bonusBall);
+        validateDuplicateBonusBall(winnerNumbers, bonusBall);
+        return bonusBall;
+    }
+
+    private static void validateDuplicateBonusBall(List<Integer> winnerNumbers, int bonusBall){
+        if (winnerNumbers.contains(bonusBall)){
+            throw new IllegalArgumentException("중복된 값을 입력해서는 안됩니다.");
+        }
+    }
+
+    private static void validateEmptyInput(String input){
+        if (input == null || input.isBlank()){
+            throw new IllegalArgumentException("유효하지않는 입력입니다.");
+        }
+    }
+
+    private static void validateNumberRange(int value){
+        if (value < 1 || value > 45){
+            throw new IllegalArgumentException("1에서 45 사이의 정수를 입력해주세요.");
+        }
+    }
+
+    private static void validateDuplicateValue(List<Integer> winnerNumbers){
+        Set<Integer> duplicateCheck = new HashSet<>(winnerNumbers);
+        if (duplicateCheck.size() != winnerNumbers.size()){
+            throw new IllegalArgumentException("중복된 값을 입력해서는 안됩니다.");
+        }
+
+    }
+
 
     private static void validatePrice(int price) {
         if (price % 1000 != 0) {
