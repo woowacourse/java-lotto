6b9a25d07cd7e5b526777d5ac9bd3f00c965a579
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-12 15:48:33 +0900

    feat(domain) : 로또 번호 도메인 추가
    - 로또 검증 기능 추가
     - 범위 검증
     - 중복 검증
     - 갯수 검증

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,37 @@
+package domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
+        validateLottoRange(numbers);
+        validateLottoDuplicate(numbers);
+        validateLottoNumberSize(numbers);
+    }
+
+    private void validateLottoRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("로또 번호 범위 오류");
+            }
+        }
+    }
+
+    private void validateLottoDuplicate(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호 중복 오류");
+        }
+    }
+
+    private void validateLottoNumberSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호 갯수 오류");
+        }
+    }
+
+}
