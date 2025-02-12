255e05a2bce2a6e26ec68b3cabbdb39da2ba88cd
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-12 17:37:28 +0900

    test: 수익률 로직 테스트

--- src/test/java/model/LottosTest.java
+++ src/test/java/model/LottosTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
@@ -28,12 +29,26 @@ class LottosTest {
 
         Map<Prize, Integer> result = lottos.getResult(new WinningLotto(winningLotto, bonus));
 
-        Assertions.assertThat(result.get(Prize._5TH)).isEqualTo(1);
-        Assertions.assertThat(result.get(Prize._4TH)).isEqualTo(2);
-        Assertions.assertThat(result.get(Prize._2ND)).isEqualTo(1);
+        assertThat(result.get(Prize._5TH)).isEqualTo(1);
+        assertThat(result.get(Prize._4TH)).isEqualTo(2);
+        assertThat(result.get(Prize._2ND)).isEqualTo(1);
     }
 
+    @Test
+    void 수익률을_계산한다() {
+        Lotto winningLotto = generateTestLotto(1,2,3,4,5,6);
+        Number bonus = new Number(10);
 
+        Lottos lottos = new Lottos(Arrays.asList(
+                generateTestLotto(1,2,3,6,7,8),
+                generateTestLotto(10,11,12,4,5,6),
+                generateTestLotto(1,2,3,4,10,11),
+                generateTestLotto(1,2,3,4,5,10)
+        ));
+
+        assertThat(lottos.computeProfit(new WinningLotto(winningLotto, bonus))).isEqualTo(7526.25);
+
+    }
 
     public Lotto generateTestLotto(int ... values){
         Set<Number> numbers = new TreeSet<>();
