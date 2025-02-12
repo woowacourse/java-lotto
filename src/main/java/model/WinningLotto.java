5c474e998461ce13849f07b99d9df52427562f6a
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-12 14:27:38 +0900

    feat: 당첨 번호 model 작성

---
+++ src/main/java/model/WinningLotto.java
@@ -0,0 +1,11 @@
+package model;
+
+public class WinningLotto {
+    private final Lotto lotto;
+    private final Number bonus;
+
+    public WinningLotto(Lotto lotto, Number bonus) {
+        this.lotto = lotto;
+        this.bonus = bonus;
+    }
+}
