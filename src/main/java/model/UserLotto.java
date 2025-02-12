f2d0b918dba742c209683392dfb5ae10fd429f16
junhaa <2171326@hansung.ac.kr>
2025-02-12 16:25:53 +0900

    feat(UserLotto): 생성자를 통해 검증 후, 사용자의 입력 정보를 담고 있는 UserLotto 클래스 구현

---
+++ src/main/java/model/UserLotto.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class UserLotto {
+    private List<Integer> winningNumbers;
+
+    public UserLotto(List<Integer> winningNumbers) {
+        validateSize(winningNumbers);
+        validateDuplicate(winningNumbers);
+        validateNumberRange(winningNumbers);
+
+        this.winningNumbers = winningNumbers;
+    }
+
+    private void validateSize(List<Integer> winningNumbers){
+        if(winningNumbers.size() != 6){
+            throw new IllegalArgumentException("6개의 숫자만 입력해주세요.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> winningNumbers){
+        if(new HashSet<>(winningNumbers).size() != winningNumbers.size()){
+            throw new IllegalArgumentException("숫자는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> winningNumbers){
+        for (Integer winningNumber : winningNumbers) {
+            if(winningNumber < 1 || winningNumber > 45){
+                throw new IllegalArgumentException("숫자는 1~45 사이여야 합니다.");
+            }
+        }
+    }
+
+}
