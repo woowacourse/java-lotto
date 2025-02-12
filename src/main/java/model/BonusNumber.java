c5c9a1da7ea59ff08906c256027f7aa38ea7c565
Chaeyoung714 <202102760@hufs.ac.kr>
2025-02-12 19:12:46 +0900

    refactor : Scanner nextLine() 메서드만 사용하도록 변경

--- src/main/java/model/BonusNumber.java
+++ src/main/java/model/BonusNumber.java
@@ -6,14 +6,19 @@ public class BonusNumber {
 
     private final int number;
 
-    public BonusNumber(int bonusNumber, WinningNumber winningNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("보너스 볼은 1~45 사이의 정수로 입력해주세요.");
+    public BonusNumber(String bonusNumberInput, WinningNumber winningNumber) {
+        try {
+            int bonusNumber = Integer.parseInt(bonusNumberInput);
+            if (bonusNumber < 1 || bonusNumber > 45) {
+                throw new IllegalArgumentException("보너스 볼은 1~45 사이의 정수로 입력해주세요.");
+            }
+            if (winningNumber.contains(bonusNumber)) {
+                throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복되지 않게 입력해주세요.");
+            }
+            this.number = bonusNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("보너스 볼은 정수로 입력해주세요.");
         }
-        if (winningNumber.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복되지 않게 입력해주세요.");
-        }
-        this.number = bonusNumber;
     }
 
     public boolean matchesWith(List<Integer> lottoNumbers) {
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,19 +11,23 @@ public class InputView {
         while (true) {
             try {
                 System.out.println("구입금액을 입력해 주세요.");
-                int purchaseAmount = sc.nextInt();
+                String purchaseAmountInput = sc.nextLine();
+                int purchaseAmount = Integer.parseInt(purchaseAmountInput);
                 if (purchaseAmount < 1000 || purchaseAmount > 100000 || purchaseAmount % 1000 != 0) {
                     throw new IllegalArgumentException("1000 이상 100000 이하의 1000으로 나누어 떨어지는 정수를 입력해주세요.");
                 }
                 return purchaseAmount;
-            } catch (IllegalArgumentException e) {
+            }
+            catch (NumberFormatException e) {
+                throw new IllegalArgumentException("구입 금액은 정수로 입력해주세요.");
+            }
+            catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
     public WinningNumber readWinningNumbers() {
-        sc.nextLine();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         while(true) {
             try {
@@ -40,7 +44,7 @@ public class InputView {
         System.out.println("보너스 볼을 입력해 주세요.");
         while (true) {
             try {
-                int bonusNumber = sc.nextInt();
+                String bonusNumber = sc.nextLine();
                 return new BonusNumber(bonusNumber, winningNumber);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
