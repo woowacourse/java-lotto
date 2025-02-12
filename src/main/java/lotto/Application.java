6031db4169ea6a84a3e074c42fa4c5cdd2a40003
ljhee92 <weilainib7@gmail.com>
2025-02-12 14:51:57 +0900

    feat: 구입 금액 입력

---
+++ src/main/java/lotto/Application.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("구입금액을 입력해 주세요.");
+        Scanner scanner = new Scanner(System.in);
+        int amount = scanner.nextInt();
+    }
+}
