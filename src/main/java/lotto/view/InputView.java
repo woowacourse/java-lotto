afcc81763d2eacc210698601f1bc48fa475b897c
AppleMint98 <applemint9821@gmail.com>
2025-02-12 17:31:37 +0900

    feat(View) : 당첨 번호 및 보너스번호 I/O 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,4 +12,14 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         return br.readLine();
     }
+
+    public String winningNumberInput() throws IOException {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return br.readLine();
+    }
+
+    public String bonusNumberInput() throws IOException {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return br.readLine();
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
+import java.util.EnumMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.domain.Rank;
 
 public class OutputView {
 
@@ -16,5 +18,16 @@ public class OutputView {
         System.out.print("]");
 
         System.out.println();
+    }
+
+    public void lottoStatisticsPrint(EnumMap<Rank, Integer> statistics, double profitRate) {
+        System.out.println("3개 일치 (" + Rank.FIFTH_PRIDE.prize + "원)- " + statistics.get(Rank.FIFTH_PRIDE) + "개");
+        System.out.println("4개 일치 (" + Rank.FOURTH_PRIDE.prize + "원)- " + statistics.get(Rank.FOURTH_PRIDE) + "개");
+        System.out.println("5개 일치 (" + Rank.THIRD_PRIDE.prize + "원)- " + statistics.get(Rank.THIRD_PRIDE) + "개");
+        System.out.println("5개 일치 (" + Rank.SECOND_PRIDE.prize + "원)- " + statistics.get(Rank.SECOND_PRIDE) + "개");
+        System.out.println("6개 일치 (" + Rank.FIRST_PRIDE.prize + "원)- " + statistics.get(Rank.FIRST_PRIDE) + "개");
+
+        System.out.println("총 수익률은 " + profitRate + "입니다.");
+
     }
 }
