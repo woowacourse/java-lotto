4112a71fff2e542798a6ba45dc6e0f2587eaf847
horizonpioneer <quantumjump10@gmail.com>
2025-02-13 11:16:31 +0900

    feat(OutputView): 발행한 로또 번호를 정렬해서 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,17 @@
 package lotto.view;
 
-public class OutputView {
+import java.util.Collections;
+import java.util.List;
 
-    public static void printInputMessage() {
-        System.out.println("구입금액을 입력해 주세요.");
-    }
+public class OutputView {
 
     public static void printCountMessage(int count) {
         System.out.println(count + "개를 구매했습니다.");
     }
+
+    public static void printLottos(List<Integer> lottos) {
+        Collections.sort(lottos);
+        System.out.println(lottos);
+    }
+
 }
