6cdbd0ca828f6560a378416a7fd7383f6f811742
CheChe903 <cheche903@naver.com>
2025-02-13 15:46:31 +0900

    feat(AmountPaid): 이익률 계산 로직 구현

--- src/main/java/lotto/domain/AmountPaid.java
+++ src/main/java/lotto/domain/AmountPaid.java
@@ -10,20 +10,24 @@ public class AmountPaid {
         validateNegativeValue();
     }
 
+    public String calculateProfitRate(int totalPrize) {
+        return String.format("%.2f", Math.floor(((double) totalPrize / (double) amount) * 100) * 0.01);
+    }
+
     private void validateDivideByLottoPrice() {
-        if(amount %1000 !=0) {
+        if (amount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 금액은 1000으로 나누어 떨어져야 합니다.");
         }
     }
 
     private void validateNegativeValue() {
-        if(amount <0) {
+        if (amount < 0) {
             throw new IllegalArgumentException("[ERROR] 음수는 입력할 수 없습니다.");
         }
     }
 
     public int getLottoQuantity() {
-        return amount/1000;
+        return amount / 1000;
     }
 
 }
