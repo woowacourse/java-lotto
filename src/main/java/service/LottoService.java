f87f720c963a8581959938fe3e93297b3c0905af
ppparkta <pppppark2@gmail.com>
2025-02-13 16:41:33 +0900

    feat: 로또 당첨 계산 비즈니스 로직 구현

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -4,28 +4,56 @@ import domain.AnswerLotto;
 import domain.Lotto;
 import domain.Lottos;
 import domain.enums.Prize;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class LottoService {
-    private final Map<Prize, Integer> prizeResult = new HashMap<>();
+    private final Map<Prize, Integer> prizeResult = new LinkedHashMap<>();
+
+    public LottoService() {
+        for (Prize prize : Prize.values()) {
+            prizeResult.put(prize, 0);
+        }
+    }
 
     public AnswerLotto getAnswerLotto(List<Integer> selectedNumbers, int bonus) {
         Lotto answerLotto = new Lotto(selectedNumbers);
         return new AnswerLotto(answerLotto, bonus);
     }
 
-    private void calculatePrize(AnswerLotto answerLotto, Lottos lottos) {
-        // 몇 개 맞았는지 세야되고
-        // 보너스 맞았는지 세야되고
-        // 그걸로 Prize 가져와야됨
-        int matchedNumberCount = 0;
-        boolean isBonusMatched = false;
-        for (Lotto lotto : lottos.getLottos()) {
-            int matchedNumberCount = lotto.getHitCountFrom(answerLotto.getLotto());
-            boolean isBonusMatched = lotto.has(answerLotto.getBonusNumber());
+    public Map<Prize, Integer> getPrizeResult() {
+        return Collections.unmodifiableMap(prizeResult);
+    }
 
+    public void calculatePrize(AnswerLotto answerLotto, Lottos lottos) {
+        for (Lotto lotto : lottos.getLottos()) {
+            int matchedCount = lotto.getHitCountFrom(answerLotto.getLotto());
+            boolean isBonusMatched = lotto.has(answerLotto.getBonusNumber());
+            Prize foundPrize = Prize.getPrizeOf(matchedCount, isBonusMatched);
+            updatePrizeResult(foundPrize);
         }
+    }
+
+    public double calculateRateOfReturn() {
+        int totalPrizeCount = 0;
+        int totalEarnedMoney = 0;
+        for (Map.Entry<Prize, Integer> entry : prizeResult.entrySet()) {
+            Prize prize = entry.getKey();
+            int prizeCount = entry.getValue();
+
+            totalPrizeCount += prizeCount;
+            totalEarnedMoney += prize.getPrizeMoney() * prizeCount;
+        }
+
+        int usedMoney = totalPrizeCount * 1000;
+        double rateOfReturn = (double) totalEarnedMoney / usedMoney;
+        return Math.floor(rateOfReturn * 100) / 100.0;
+    }
+
+    private void updatePrizeResult(Prize foundPrize) {
+        Integer prizeCount = prizeResult.getOrDefault(foundPrize, 0);
+        prizeResult.put(foundPrize, prizeCount + 1);
     }
 }
