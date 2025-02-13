1d2c8f669c429c7ff122847a3a326313371fccac
julie <7jujubuilds@gmail.com>
2025-02-13 15:57:54 +0900

    refactor(application): 예외 메세지 열거형 리팩토링
    작업 내용
    - ExceptionMessage 내 미사용 상수 삭제

--- src/main/java/lotto/constant/ExceptionMessage.java
+++ src/main/java/lotto/constant/ExceptionMessage.java
@@ -6,8 +6,7 @@ public enum ExceptionMessage {
     INVALID_NUMBER_COUNT("올바르지 않은 숫자 개수입니다."),
     INVALID_NUMBER_FORMAT("숫자 형식이 아닙니다."),
     INVALID_PURCHASE_AMOUNT("구입 금액은 %d단위 입니다."),
-    INVALID_INPUT("유효하지 않은 입력값입니다."),
-    NOT_FOUND_WINNING_TIER("해당하는 당첨 등수를 찾을 수 없습니다.");
+    INVALID_INPUT("유효하지 않은 입력값입니다.");
 
     private final String content;
 
