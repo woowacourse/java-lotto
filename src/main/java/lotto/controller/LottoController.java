e3292054fcbebcb86dcded8d5e410f87fb881db7
ryan-dia <cjfdnjs158@naver.com>
2025-02-13 15:47:35 +0900

    feat: 로또 컨트롤러 추가
    Co-authored-by: kiwoook <tizmfns1218@Naver.com>

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,87 @@
+package lotto.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGroup;
+import lotto.domain.LottoNumber;
+import lotto.domain.Money;
+import lotto.domain.Profit;
+import lotto.domain.Rank;
+import lotto.domain.WinnerLotto;
+import lotto.utils.RecoveryUtils;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    LottoGroup lottoGroup = new LottoGroup();
+
+    public void run() {
+        Money money = getMoney();
+        generateLotto(money);
+        WinnerLotto winnerLotto = getWinnerLotto();
+
+        Profit profit = calculateProfit(winnerLotto);
+        String profitRate = profit.calculateAverageProfitRate(money);
+
+        OutputView.printResult(profit, profitRate);
+    }
+
+    private Money getMoney() {
+        return RecoveryUtils.executeWithRetry(InputView::readMoney, Money::new);
+    }
+
+    private void generateLotto(Money money) {
+        lottoGroup.generate(money);
+        OutputView.printLottoGroup(lottoGroup);
+    }
+
+    private Profit calculateProfit(WinnerLotto winnerLotto) {
+        Profit profit = new Profit();
+
+        for (Lotto lotto : lottoGroup.getLottoGroup()) {
+            long matchCount = winnerLotto.getMatchCount(lotto);
+            boolean hasBonus = winnerLotto.hasBonus(lotto);
+            Rank rank = Rank.find((int) matchCount, hasBonus);
+            profit.incrementCount(rank);
+        }
+
+        return profit;
+    }
+
+    private WinnerLotto getWinnerLotto() {
+        return RecoveryUtils.executeWithRetry(InputView::readWinnerNumbers, this::readWinnerNumber);
+    }
+
+    private WinnerLotto readWinnerNumber(String input) {
+        List<LottoNumber> winnerNumbers = toLottoNumberList(input);
+        WinnerLotto.validateWinnerNumbers(winnerNumbers);
+
+        return readBonusNumber(winnerNumbers);
+    }
+
+    private WinnerLotto readBonusNumber(List<LottoNumber> winnerNumbers) {
+        LottoNumber bonusNumber = RecoveryUtils.executeWithRetry(InputView::readBonusNumber, LottoNumber::create);
+
+        try {
+            WinnerLotto.validateBonusNumbers(winnerNumbers, bonusNumber);
+            return new WinnerLotto(winnerNumbers, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e);
+            return readBonusNumber(winnerNumbers);
+        }
+    }
+
+    private List<LottoNumber> toLottoNumberList(String input) {
+        if (input == null || input.isBlank() || input.endsWith(", ")) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+        return Arrays.stream(input.split(", "))
+                .map(LottoNumber::new)
+                .toList();
+    }
+
+
+}
