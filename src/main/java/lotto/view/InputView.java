2f3c110d027c416ad9faefacf2144412cd61e37c
kimdev <kim__dev@hufs.ac.kr>
2025-02-13 11:22:45 +0900

    feat(InputView): 지난 주 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,8 +7,16 @@ import lotto.util.InputValidator;
 
 public class InputView {
 
+    private static final String LINE = System.lineSeparator();
+
     public String readPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
+        return getInput();
+    }
+
+    public String readWinningLottoNumbers() {
+
+        System.out.println(LINE + "지난 주 당첨 번호를 입력해 주세요.");
         return getInput();
     }
 
