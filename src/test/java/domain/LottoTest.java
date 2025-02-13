b181701a74198e9981d0e2d4bc4d33e228d8fe1b
sonjh919 <kingjh1125@gmail.com>
2025-02-13 16:18:57 +0900

    test: Add countMatchNumbers

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -3,11 +3,16 @@ package domain;
 import static global.exception.ExceptionMessage.DUPLICATED_NUMBER;
 import static global.exception.ExceptionMessage.INVALID_FORMAT;
 import static global.exception.ExceptionMessage.INVALID_RANGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
 
@@ -83,9 +88,25 @@ class LottoTest {
         }
     }
 
-    @Test
-    void countMatchNumbers() {
+    @ParameterizedTest
+    @MethodSource("testMatchRank")
+    void countMatchNumbers(String numbers, Rank rank) {
+        //given
+        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", "7");
+        Lotto lotto = new Lotto(numbers);
+        //when-then
+        assertThat(lotto.countMatchNumbers(winningLotto)).isEqualTo(rank);
+    }
 
+    private static Stream<Arguments> testMatchRank() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", Rank.FIRST),
+                Arguments.of("1,2,3,4,5,7", Rank.SECOND),
+                Arguments.of("1,2,3,4,5,8", Rank.THIRD),
+                Arguments.of("1,2,3,4,8,9", Rank.FOURTH),
+                Arguments.of("1,2,3,8,9,10", Rank.FIFTH),
+                Arguments.of("1,2,8,9,10,11", Rank.NONE)
+        );
     }
 
 }
