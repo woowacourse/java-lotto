fe10c56bc8f9b1de8f775ebbc5146f7df49d8992
horizonpioneer <quantumjump10@gmail.com>
2025-02-13 17:19:42 +0900

    feat(WinningNumber): 범위 검증 기능 추가

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -7,12 +7,15 @@ import java.util.Set;
 public class WinningNumber {
 
     private static final int WINNING_NUMBER_COUNT = 6;
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
 
     private final List<Integer> winningNumbers;
 
     public WinningNumber(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
         this.winningNumbers = numbers;
     }
 
@@ -36,10 +39,10 @@ public class WinningNumber {
     }
 
     private void validateRange(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("당첨 번호는 1~45 사이의 수여야 합니다.");
-            }
+        if (numbers.stream()
+                .anyMatch(number -> number < LOTTO_START_NUMBER || number > LOTTO_END_NUMBER)) {
+            throw new IllegalArgumentException("당첨 번호는 " + LOTTO_START_NUMBER
+                    + "~" + LOTTO_END_NUMBER + "사이의 수여야 합니다.");
         }
     }
 
