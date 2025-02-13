0933308cd602ba737a67b48ffb27e0dc9d4f3cf0
CheChe903 <cheche903@naver.com>
2025-02-13 11:07:15 +0900

    feat(Parser): Parser 유틸 클래스 구현
    int 형 변환을 위한 유틸 클래스 Parser를 구혆바니다.

---
+++ src/main/java/lotto/utils/Parser.java
@@ -0,0 +1,15 @@
+package lotto.utils;
+
+import java.text.ParseException;
+
+public class Parser {
+
+    public static int parseToInteger(String input) {
+
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력값 입니다.");
+        }
+    }
+}
