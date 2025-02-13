ebe12a2e929ee3abbc8908657fb1c5ece718f8d3
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-13 17:13:17 +0900

    feat(Price): 로또 구입 금액 입력값 검증
    - 1000원 단위, 0원 이상

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -1,5 +1,6 @@
 public class Price {
 
+    public static final int LOTTO_PRICE = 1000;
     private final int value;
 
     public Price(String value) {
@@ -8,7 +9,25 @@ public class Price {
     }
 
     private void validate(String value) {
+        validateLottoPriceUnit(value);
+    }
 
+    private void validateLottoPriceUnit(String value) {
+        int price = validateNumberFormat(value);
+
+        if (price >= LOTTO_PRICE && price % LOTTO_PRICE == 0) {
+            return;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private int validateNumberFormat(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getValue() {
