28e968b0499b635b17f8e2c89beb32632368dfd7
ryan-dia <cjfdnjs158@naver.com>
2025-02-13 15:46:11 +0900

    refactor: 수익률 변수 초기화 수정
    Co-authored-by: kiwoook <tizmfns1218@Naver.com>

--- src/main/java/lotto/domain/Profit.java
+++ src/main/java/lotto/domain/Profit.java
@@ -6,7 +6,14 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class Profit {
-    Map<Rank, Integer> rankMap = new EnumMap<>(Rank.class);
+    Map<Rank, Integer> rankMap;
+
+    public Profit() {
+        this.rankMap = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            rankMap.put(rank, 0);
+        }
+    }
 
     public void incrementCount(Rank key) {
         rankMap.merge(key, 1, Integer::sum);
@@ -24,5 +31,9 @@ public class Profit {
         long totalProfit = calculateTotalProfit();
         return new BigDecimal(totalProfit)
                 .divide(new BigDecimal(amount), 2, RoundingMode.HALF_UP).toString();
+    }
+
+    public Map<Rank, Integer> getRankMap() {
+        return rankMap;
     }
 }
