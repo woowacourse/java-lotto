e8797bc0335e9c03abccb8c5ab6f80ecd0f0f20c
kjyyjk <rlawnsdud920@gmail.com>
2025-02-13 15:31:13 +0900

    refactor: 테스트에서만 사용되는 메서드 제거

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -2,23 +2,14 @@ package lotto;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
-
-    private LottoMachine lottoMachine;
-
-    @BeforeEach
-    void init() {
-        lottoMachine = new LottoMachine();
-    }
-
     @DisplayName("로또는 6개의 번호를 가진다")
     @Test
     void 로또는_6개의_번호를_가진다() {
-        Lotto result = lottoMachine.createLotto();
-        assertThat(result.getNumbersSize()).isEqualTo(6);
+        Lotto result = LottoMachine.createLotto();
+        assertThat(result.getNumbers().size()).isEqualTo(6);
     }
 }
