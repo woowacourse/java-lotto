ed83a80591907cf54255ad9435a66ed1cf75c247
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-13 11:14:33 +0900

    feat(view): 당첨 통계 출력 메서드 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,10 +1,19 @@
 package view;
 
 import dto.LottoDto;
+import dto.StatisticsDto;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import model.PrizeTier;
 
 public class OutputView {
     private static final String PURCHASE_COUNT_MESSAGE = "개를 구매하였습니다.";
+    private static final String STATISTICS_HEADER_MESSAGE = "당첨 통계\n---------";
+    private static final String STATISTICS_FORMAT = "%d개 일치%s (%d원)- %d개\n";
+    private static final String BONUS_MATCHED_MESSAGE = ", 보너스 볼 일치";
+    private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.2f입니다.";
 
     public void printPurchaseCount(int count) {
         System.out.printf("%d%s\n", count, PURCHASE_COUNT_MESSAGE);
@@ -15,5 +24,23 @@ public class OutputView {
             List<Integer> numbers = lottoDto.getNumbers();
             System.out.println(numbers.toString());
         }
+    }
+
+    public void printStatistics(StatisticsDto statisticsDto) {
+        System.out.println(STATISTICS_HEADER_MESSAGE);
+        Map<PrizeTier, Integer> prizeCounts = statisticsDto.getPrizeCounts();
+        PrizeTier[] prizeTiers = PrizeTier.values();
+        Arrays.sort(prizeTiers, Collections.reverseOrder());
+        for (PrizeTier prizeTier : prizeTiers) {
+            int matchedCount = prizeTier.getMatchedCount();
+            int prize = prizeTier.getPrize();
+            int prizeTierCount = prizeCounts.get(prizeTier);
+            if (prizeTier == PrizeTier.SECOND) {
+                System.out.printf(STATISTICS_FORMAT, matchedCount, BONUS_MATCHED_MESSAGE, prize, prizeTierCount);
+                continue;
+            }
+            System.out.printf(STATISTICS_FORMAT, matchedCount, "", prize, prizeTierCount);
+        }
+        System.out.printf(PROFIT_RATE_FORMAT, statisticsDto.getProfitRate());
     }
 }
