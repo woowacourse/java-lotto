dc9752ec4322e4f52c243556e7b373e58777b451
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-13 13:16:53 +0900

    feat: 지난 주 당첨 번호를 입력 받는 함수 구현

--- src/main/java/src/view/input/ConsoleInputView.java
+++ src/main/java/src/view/input/ConsoleInputView.java
@@ -1,10 +1,13 @@
 package src.view.input;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleInputView implements InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String WINNING_LOTTO_DELIMITER = ", ";
 
     @Override
     public int inputPurchaseMoney() {
@@ -12,6 +15,32 @@ public class ConsoleInputView implements InputView {
             return scanner.nextInt();
         } catch (RuntimeException e) {
             throw new IllegalArgumentException("잘못된 입력입니다.", e);
+        }
+    }
+
+    @Override
+    public List<Integer> inputWinningLottoNumbers() {
+        try {
+            scanner.nextLine();
+            String input = scanner.nextLine();
+            List<String> parsedWinningLottoNumbers = parseWinningLottoNumbers(input);
+
+            return parsedWinningLottoNumbers.stream().map(Integer::parseInt).toList();
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException("잘못된 입력입니다.", e);
+        }
+    }
+
+    private List<String> parseWinningLottoNumbers(final String input) {
+        List<String> parsedWinningLottoNumbers = Arrays.stream(input.split(WINNING_LOTTO_DELIMITER)).toList();
+        parsedWinningLottoNumbers.forEach(this::validateWinningLottoInput);
+
+        return parsedWinningLottoNumbers;
+    }
+
+    private void validateWinningLottoInput(final String winningLottoNumber) {
+        if (!winningLottoNumber.matches("[0-9]+")) {
+            throw new IllegalArgumentException("구분자 형식에 맞지 않습니다.");
         }
     }
 }
