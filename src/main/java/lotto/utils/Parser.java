dbdfe88dd14180f32f87e04b7cb46ab7cfeef62d
CheChe903 <cheche903@naver.com>
2025-02-13 14:07:40 +0900

    feat(Parser): 콤마 구분자 Parser 로직 추가 구현
    단일 값 parse가 아닌, 콤마를 구분자로 하는 parsing 메서드를 추가합니다.

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,11 +1,21 @@
 package lotto.utils;
 
+import java.util.List;
+
 public class Parser {
 
     public static int parseToInteger(String input) {
 
         try {
             return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력값 입니다.");
+        }
+    }
+
+    public static List<Integer> parseToIntegers(List<String> input) {
+        try {
+            return input.stream().map(Parser::parseToInteger).toList();
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력값 입니다.");
         }
