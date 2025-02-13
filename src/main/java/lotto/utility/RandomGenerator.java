ba92b1cb26db628f9f68465d0d8ed623f98fae4e
julie <7jujubuilds@gmail.com>
2025-02-13 15:57:03 +0900

    style(application): 스타일 정리

--- src/main/java/lotto/utility/RandomGenerator.java
+++ src/main/java/lotto/utility/RandomGenerator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomGenerator {
+
     private void addNumberToList(List<Integer> list, int number) {
         if (!list.contains(number)) {
             list.add(number);
