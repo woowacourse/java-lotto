c235f93f6bcc12444441cf573d9f1b494d77b042
2jin2.1031 <2jin2.1031@gmail.com>
2025-02-13 11:12:00 +0900

    fix: 입력 스캐너 한줄 처리로 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,16 +11,16 @@ public class InputView {
 
     public static LottoPurchase getPurchaseLotto() {
         System.out.println("구입금액을 입력해 주세요.");
-        return LottoPurchase.of(sc.next());
+        return LottoPurchase.of(sc.nextLine());
     }
 
     public static Lotto getWinningLotto() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return Lotto.of(sc.next());
+        return Lotto.of(sc.nextLine());
     }
 
     public static Bonus getWinningBonus(final Lotto lotto) {
         System.out.println("보너스 볼을 입력해 주세요.");
-        return Bonus.of(sc.next(), lotto);
+        return Bonus.of(sc.nextLine(), lotto);
     }
 }
