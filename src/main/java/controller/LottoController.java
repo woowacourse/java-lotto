1b7bf324d7b03e3f70cd2b90ab5cbc9b3758af05
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-13 15:07:14 +0900

    refactor(LottoController): 메서드 분리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -25,27 +25,34 @@ public class LottoController {
     }
 
     public void run() {
-        outputView.printAskInputMoney();
-        final Money money = inputView.readMoney();
+        final Money money = requestMoney();
 
         final LottoStore lottoStore = new LottoStore(new RandomNumbersGenerator(), money);
         final List<Lotto> lottos = lottoStore.issueLottos();
-
         outputView.printUserLottos(lottos);
 
-        outputView.printAskInputWiningLotto();
-        final List<Integer> numbers = inputView.readWinningNumbers();
-        final Lotto winningLotto = new Lotto(numbers);
-        outputView.printAskInputBonusNumber();
-        final BonusNumber bonusNumber = inputView.readBonusNumber();
-        final WinningNumber winningNumber = new WinningNumber(winningLotto, bonusNumber);
-
+        final WinningNumber winningNumber = requestWinningNumber();
         final WinningResult winningResult = new WinningResult(winningNumber, lottos);
+
         final Map<LottoRank, Integer> countedWinningResult = winningResult.countWinningResult();
         outputView.printWinningResult(countedWinningResult);
 
         final WinningProfit winningProfit = new WinningProfit(countedWinningResult);
         outputView.printWinningProfit(winningProfit.calculateProfitRate(money.getAmount()));
+    }
 
+
+    private WinningNumber requestWinningNumber() {
+        outputView.printAskInputWiningLotto();
+        final List<Integer> numbers = inputView.readWinningNumbers();
+        final Lotto winningLotto = new Lotto(numbers);
+        outputView.printAskInputBonusNumber();
+        final BonusNumber bonusNumber = inputView.readBonusNumber();
+        return new WinningNumber(winningLotto, bonusNumber);
+    }
+
+    private Money requestMoney() {
+        outputView.printAskInputMoney();
+        return inputView.readMoney();
     }
 }
