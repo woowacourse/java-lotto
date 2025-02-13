61f6a99bb66bf08fb113d22473e7bcb2b83a7538
taek2222 <oot309@daum.net>
2025-02-13 16:04:46 +0900

    style: 설명 주석 삭제

--- src/main/java/model/Heuristic.java
+++ src/main/java/model/Heuristic.java
@@ -1,9 +1,5 @@
 package model;
 
-/*
-    총 수익률은 0.35입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)
- */
-
 public enum Heuristic {
     BENEFIT("이익"),
     LOSS("손해"),
@@ -15,7 +11,7 @@ public enum Heuristic {
         this.benefitType = benefitType;
     }
 
-    public static String determine(double roi) {
+    public static String determine(final double roi) {
         if (roi > 1) {
             return BENEFIT.benefitType;
         }
