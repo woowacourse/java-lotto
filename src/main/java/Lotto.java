b3a05326f3958f8a3bed0a9ef238d11d358c0c36
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-13 13:53:53 +0900

    feat(Lotto): 로또 번호 중복 검증 추가

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -7,22 +7,36 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+
         numbers.sort(Comparator.naturalOrder());
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateDistinct(numbers);
+        validateSize(numbers);
+    }
+
+    private void validateDistinct(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() == 6) {
+            return;
         }
+        throw new IllegalArgumentException();
+    }
+
+    private static void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            return;
+        }
+        throw new IllegalArgumentException();
     }
 
     @Override
     public String toString() {
         return "[" + String.join(", ", numbers.stream().map(String::valueOf).toList()) + "]";
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
     }
 }
