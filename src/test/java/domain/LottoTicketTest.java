49c8653f3088b87c86c2253d12d882f316947eb5
dltmddus <tmddus4671@gmail.com>
2025-02-13 15:33:43 +0900

    test: LottoTicket 생성 시 예외 테스트 추가

--- src/test/java/domain/LottoTicketTest.java
+++ src/test/java/domain/LottoTicketTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,4 +23,45 @@ class LottoTicketTest {
         }
         Assertions.assertThat(lottoTicket.getNumbers().stream().distinct()).hasSize(LottoTicket.LOTTO_SIZE);
     }
+
+    @DisplayName("로또 번호의 개수가 6개가 아닌 경우 예외가 발생한다")
+    @Test
+    void 로또_번호의_개수가_6개가_아닌_경우_테스트() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> {
+                    new LottoTicket(numbers);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 6개여야 합니다.");
+    }
+
+    @DisplayName("로또 번호가 1 이상 45 이하가 아닌 경우 예외가 발생한다")
+    @Test
+    void 로또_번호가_1_이상_45_이하가_아닌_경우_테스트() {
+        // given
+        List<Integer> numbers = List.of(0, 1, 2, 3, 4, 55);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> {
+                    new LottoTicket(numbers);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 1 이상 45 이하이다.");
+    }
+
+    @DisplayName("로또 번호가 중복된 경우 예외가 발생한다")
+    @Test
+    void 로또_번호가_중복된_경우_테스트() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 5);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> {
+                    new LottoTicket(numbers);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 번호가 존재합니다.");
+    }
+
+
 }
