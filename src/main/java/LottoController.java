8a42fd3729f5d411a1ae231e6da248354ece2dd5
supernovaMK <minki5336@naver.com>
2025-02-13 16:06:48 +0900

    feat(LottoController): 로또 당첨 계산 및 출력 로직 구현
    - 수익률 포함

--- src/main/java/LottoController.java
+++ src/main/java/LottoController.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoController {
@@ -15,7 +18,7 @@ public class LottoController {
         int ticket = lottoMachine.getTicket();
         System.out.println(ticket + "개를 구매했습니다.");
 
-        Lottos lottos = lottoMachine.generateLotto();
+        Lottos lottos = lottoMachine.generateLottos();
         for (Lotto lotto : lottos.getLottos()) {
             System.out.println(lotto.toString());
         }
@@ -24,12 +27,14 @@ public class LottoController {
                 = new WinningNumberWithBonusNumber(getWinningNumber(bufferedReader), getBonusNumber(bufferedReader));
 
         LottoResult lottoResult = LottoWinningChecker.calculateResult(lottos, winningNumberWithBonusNumber);
-        System.out.println(lottoResult.result().toString());
 
+        printLottoResults(lottoResult);
+
+        printRateOfReturn(lottoResult,price);
 
     }
 
-    private static Lotto getWinningNumber(BufferedReader bufferedReader) throws IOException {
+    private Lotto getWinningNumber(BufferedReader bufferedReader) throws IOException {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return new Lotto(Arrays.stream(bufferedReader.readLine().split(","))
                 .map(String::trim)
@@ -37,8 +42,44 @@ public class LottoController {
                 .collect(Collectors.toList()));
     }
 
-    private static int getBonusNumber(BufferedReader bufferedReader) throws IOException {
+    private int getBonusNumber(BufferedReader bufferedReader) throws IOException {
         System.out.println("보너스 볼을 입력해 주세요.");
         return Integer.parseInt(bufferedReader.readLine());
+    }
+
+    private void printLottoResults(LottoResult lottoResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("당첨 통계\n" +
+                "---------\n");
+        Map<LottoRanking, Integer> result = lottoResult.result();
+        result.remove(LottoRanking.LOSING);
+
+        List<LottoRanking> lottoRankings = result.keySet().stream().collect(Collectors.toList());
+        Collections.reverse(lottoRankings);
+
+        for (LottoRanking lottoRanking : lottoRankings) {
+            if (lottoRanking == LottoRanking.SECOND) {
+                stringBuilder.append(lottoRanking.getCorrectCount() + "개 일치, 보너스 볼 일치(" + lottoRanking.getPrize() + "원)- " + result.get(lottoRanking) + "개\n");
+                continue;
+            }
+            stringBuilder.append(lottoRanking.getCorrectCount() + "개 일치 (" + lottoRanking.getPrize() + "원)- " + result.get(lottoRanking) + "개\n");
+        }
+        System.out.println(stringBuilder);
+    }
+
+    private void printRateOfReturn(LottoResult lottoResult, Price price) {
+        double rateOfReturn = (double) lottoResult.getTotalPrize() / price.getValue();
+        System.out.print("총 수익률은 " + lottoResult.getTotalPrize() / price.getValue() + "입니다.");
+        if (rateOfReturn > 1) {
+            System.out.print("(기준이 1이기 때문에 결과적으로 이득이라는 의미임)");
+            return;
+        }
+        if (rateOfReturn == 1) {
+            System.out.print("(기준이 1이기 때문에 결과적으로 동등이라는 의미임)");
+            return;
+        }
+        if (rateOfReturn < 1) {
+            System.out.print("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        }
     }
 }
