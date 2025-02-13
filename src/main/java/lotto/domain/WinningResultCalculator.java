cfa1536c3025327dd913f58e0bb0071629de3c69
Mint <audwl03071@gmail.com>
2025-02-13 14:52:33 +0900

    feat(WinningResultCalculator): 당첨 로또의 개수를 구하는 기능 구현

--- src/main/java/lotto/domain/WinningResultCalculator.java
+++ src/main/java/lotto/domain/WinningResultCalculator.java
@@ -1,14 +1,38 @@
 package lotto.domain;
 
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
 public class WinningResultCalculator {
 
     private final Lotto winningLotto;
     private final LottoNumber bonusNumber;
 
-    public WinningResultCalculator(Lotto winningLotto, LottoNumber bonusNumber) {
+    public WinningResultCalculator(final Lotto winningLotto, final LottoNumber bonusNumber) {
         validate(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    public Map<LottoAward, Integer> countLottoPrizes(final List<Lotto> lottos) {
+        Map<LottoAward, Integer> winningResult = initializeWinningResult();
+        for (Lotto lotto : lottos) {
+            int matchingCount = winningLotto.countMatchingLottoNumber(lotto);
+            boolean isBonusNumberMatched = lotto.contains(bonusNumber);
+            LottoAward lottoAward = LottoAward.from(matchingCount, isBonusNumberMatched);
+            winningResult.merge(lottoAward, 1, Integer::sum);
+        }
+        return Collections.unmodifiableMap(winningResult);
+    }
+
+    private Map<LottoAward, Integer> initializeWinningResult() {
+        Map<LottoAward, Integer> winningResult = new EnumMap<>(LottoAward.class);
+        for (LottoAward lottoAward : LottoAward.ACTUAL_LOTTO_AWARD) {
+            winningResult.put(lottoAward, 0);
+        }
+        return winningResult;
     }
 
     private void validate(Lotto winningLotto, LottoNumber bonusNumber) {
