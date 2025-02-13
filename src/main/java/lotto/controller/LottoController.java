bedf0acf4c8a50d1b0a5c5c548b249483c4b9902
ryan-dia <cjfdnjs158@naver.com>
2025-02-13 17:04:51 +0900

    refactor: toLottoNumberList 메서드를 Lotto 클래스로 이동
    Co-authored-by: kiwoook <tizmfns1218@Naver.com>

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,5 @@
 package lotto.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGroup;
@@ -57,7 +56,7 @@ public class LottoController {
     }
 
     private WinnerLotto readWinnerNumber(String input) {
-        List<LottoNumber> winnerNumbers = toLottoNumberList(input);
+        List<LottoNumber> winnerNumbers = Lotto.toLottoNumberList(input);
         WinnerLotto.validateWinnerNumbers(winnerNumbers);
 
         return readBonusNumber(winnerNumbers);
@@ -74,16 +73,4 @@ public class LottoController {
             return readBonusNumber(winnerNumbers);
         }
     }
-
-    private List<LottoNumber> toLottoNumberList(String input) {
-        if (input == null || input.isBlank() || input.endsWith(", ")) {
-            throw new IllegalArgumentException("잘못된 입력입니다. 이와 같은 형태로 작성해주세요.(ex. 1, 2, 3, 4, 5, 6)");
-        }
-
-        return Arrays.stream(input.split(", "))
-                .map(LottoNumber::new)
-                .toList();
-    }
-
-
 }
