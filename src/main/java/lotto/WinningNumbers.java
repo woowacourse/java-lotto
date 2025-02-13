9c80c189d9857fbc9d66251650a9d10eb8f99c3b
kjyyjk <rlawnsdud920@gmail.com>
2025-02-13 10:43:59 +0900

    feat: 보너스 번호 중복 검증
    - 입력한 보너스 번호가 당첨 번호와 중복된다면IllegalArgumentException을 발생시키고,
    재입력 받는다.
    
    - ,를 기준으로 자른뒤 공백을 없애주기 위해 trim을 사용한다.

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+
+    public WinningNumbers(final List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    public void validateBonusNumberDuplicated(final int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("당첨 번호와 보너스 번호는 중복될 수 없습니다.");
+        }
+    }
+}
