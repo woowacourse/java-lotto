3667b9ba8d855a988eecf69f957c25e359e1c6a9
taek2222 <oot309@daum.net>
2025-02-13 17:00:55 +0900

    test: 검증 테스트 메시지 확인

--- src/test/java/utils/ValidatorTest.java
+++ src/test/java/utils/ValidatorTest.java
@@ -1,11 +1,13 @@
 package utils;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import global.utils.Validator;
 import org.junit.jupiter.api.Test;
 
 class ValidatorTest {
+
     @Test
     void 숫자가_아닌_경우_예외가_발생한다() {
         assertThatThrownBy(() -> {
@@ -23,7 +25,21 @@ class ValidatorTest {
     @Test
     void 범위에_해당되면_예외가_발생하지_않는다() {
         assertThatNoException().isThrownBy(() -> {
-            Validator.validateRange(10, 100, 1);
+            Validator.validateRange(10, 1, 100);
         });
+    }
+
+    @Test
+    void 금액이_1000원_단위면_예외가_발생하지_않는다() {
+        assertThatNoException().isThrownBy(() -> {
+            Validator.validateDivisibility(5000, 1000);
+        });
+    }
+
+    @Test
+    void 금액이_1000원_단위가_아니면_예외가_발생한다() {
+        assertThatThrownBy((() -> {
+            Validator.validateDivisibility(5300, 1000);
+        }));
     }
 }
