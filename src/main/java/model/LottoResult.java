14b906e9666aced90d21e047c53ae9515e92a4df
taek2222 <oot309@daum.net>
2025-02-13 16:08:55 +0900

    refactor: 기본 NONE 인 경우 출력 안하도록 필터 추가

--- src/main/java/model/LottoResult.java
+++ src/main/java/model/LottoResult.java
@@ -2,9 +2,10 @@ package model;
 
 import dto.LottoResultDetailResponse;
 import dto.LottoResultsResponse;
-import dto.ROIResultResponse;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoResult {
@@ -18,29 +19,32 @@ public class LottoResult {
 
     public LottoResultsResponse createResponse() {
         List<LottoResultDetailResponse> detailResponses = results.entrySet().stream()
+                .filter(result -> result.getKey() != RankType.NONE)
                 .map(result -> new LottoResultDetailResponse(
                         result.getKey().createResponse(),
                         result.getValue()
                 )).toList();
 
         return new LottoResultsResponse(
-            detailResponses
+                detailResponses
         );
     }
 
-    public void sort() { // TODO: 리팩토링 필요
-        results = results.entrySet().stream()
+    public void sort() {
+        results = results.entrySet()
+                .stream()
                 .sorted((r1, r2) -> Integer.compare(r2.getKey().getRank(), r1.getKey().getRank()))
                 .collect(Collectors.toMap(
-                                Map.Entry::getKey,
-                                Map.Entry::getValue,
-                                (e1, e2) -> e1,
-                                LinkedHashMap::new
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
                 ));
     }
 
     public int calculateTotalPrice() {
-        return results.entrySet().stream()
+        return results.entrySet()
+                .stream()
                 .mapToInt(result -> result.getKey().getPrice() * result.getValue())
                 .sum();
     }
