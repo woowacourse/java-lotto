f2809cc249b21becc9dc958c0896522d741c8e3c
CheChe903 <cheche903@naver.com>
2025-02-13 14:09:11 +0900

    feat(LottoService): 당첨 번호 및 보너스 번호 생성 로직 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -6,19 +6,30 @@ import java.util.List;
 import lotto.domain.AmountPaid;
 import lotto.domain.Lotto;
 import lotto.domain.LottoBundle;
+import lotto.domain.WinningNumbers;
 import lotto.utils.NumberGenerator;
+import lotto.utils.Parser;
+import lotto.utils.Splitter;
 
 public class LottoService {
 
     public LottoBundle makeLottoBundle(AmountPaid amount) {
         List<Lotto> lottos = new ArrayList<>();
-        for(int i =0; i<amount.getLottoQuantity(); i++) {
+        for (int i = 0; i < amount.getLottoQuantity(); i++) {
             lottos.add(makeLotto());
         }
         return new LottoBundle(lottos);
     }
 
-    private Lotto makeLotto() {
-        return new Lotto(NumberGenerator.numberGeneratorWithUniqueValues(6,1,45));
+
+    public WinningNumbers makeWinningNumbers(String winningNumber, String bonusNumber) {
+        return new WinningNumbers(Parser.parseToIntegers(Splitter.splitByComma(
+                winningNumber)), Parser.parseToInteger(bonusNumber));
     }
+
+
+    private Lotto makeLotto() {
+        return new Lotto(NumberGenerator.numberGeneratorWithUniqueValues(6, 1, 45));
+    }
+
 }
