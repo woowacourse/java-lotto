7d32298819893b85684b3328be54493c00d8cbf3
2jin2.1031 <2jin2.1031@gmail.com>
2025-02-13 14:31:07 +0900

    refactor: 공백 추가

--- src/main/java/model/Prize.java
+++ src/main/java/model/Prize.java
@@ -9,7 +9,7 @@ public enum Prize {
     match_three("3개 일치 (5000원)", 3, 5_000, false),
     match_four("4개 일치 (50000원)", 4, 50_000, false),
     match_five("5개 일치 (1500000원)", 5, 1_500_000, false),
-    match_five_and_bonus("5개 일치, 보너스 볼 일치(30000000원)", 5, 30_000_000, true),
+    match_five_and_bonus("5개 일치, 보너스 볼 일치 (30000000원)", 5, 30_000_000, true),
     match_six("6개 일치 (2000000000원)", 6, 2_000_000_000, false);
 
     private final String comment;
