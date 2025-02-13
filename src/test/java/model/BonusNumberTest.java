3f8ca06aaed309184fa8daf2765cb4251be4484f
junhaa <2171326@hansung.ac.kr>
2025-02-13 16:47:59 +0900

    test(BonusNumberTest): 보너스 번호 관련 테스트 코드 작성
    - 보너스 번호의 범위를 검증하는 테스트
    - 보너스 번호가 로또 번호에 존재하는지 검증하는 테스트

---
+++ src/test/java/model/BonusNumberTest.java
@@ -0,0 +1,34 @@
+package model;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberTest {
+
+    @Test
+    @DisplayName("보너스 번호는 1~45 사이가 아닌 경우 예외를 발생한다.")
+    void bonus_number_out_of_range_test(){
+        assertThatThrownBy(() -> new BonusNumber(46))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자는 1~45 사이여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 로또 번호 내부에 존재하는 경우 true를 반환한다.")
+    void is_bonus_number_true(){
+        List<Integer> lottoNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(lottoNumbers);
+        BonusNumber bonusNumber = new BonusNumber(5);
+
+        assertThat(bonusNumber.isBonusNumber(lotto)).isTrue();
+    }
+
+
+
+}
