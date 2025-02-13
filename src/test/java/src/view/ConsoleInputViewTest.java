420e7f482e3fbff66d6280d78b8a4b65691780eb
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-13 13:17:26 +0900

    test: 지난 주 당첨 번호를 입력 받는 함수 테스트 작성

--- src/test/java/src/view/ConsoleInputViewTest.java
+++ src/test/java/src/view/ConsoleInputViewTest.java
@@ -3,14 +3,29 @@ package src.view;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import src.view.input.ConsoleInputView;
 
 public class ConsoleInputViewTest {
 
     private static final ConsoleInputView consoleInputView = new ConsoleInputView();
+
+    private static Stream<Arguments> 지난_주_로또_당첨_번호_입력_시_구분자가_쉼표가_아니면_예외_발생_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(1, 2, 3),
+                        List.of(1, 2, 3, 4),
+                        List.of(1, 2, 3, 4, 5)
+                )
+        );
+    }
 
     @Test
     void 구입_금액_입력_시_문자가_입력되면_예외_발생() {
@@ -26,5 +41,33 @@ public class ConsoleInputViewTest {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
         assertThatIllegalArgumentException().isThrownBy(consoleInputView::inputPurchaseMoney);
+    }
+
+    @ParameterizedTest
+    @MethodSource("지난_주_로또_당첨_번호_입력_시_구분자가_쉼표가_아니면_예외_발생_테스트_케이스")
+    void 지난_주_로또_당첨_번호_입력_시_구분자가_쉼표가_아니면_예외_발생(final List<Integer> input) {
+
+        String joined = input.stream().map(String::valueOf).collect(Collectors.joining(", "));
+
+        System.setIn(new ByteArrayInputStream(joined.getBytes()));
+
+        assertThatIllegalArgumentException().isThrownBy(consoleInputView::inputWinningLottoNumbers);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1, 2. 3", "1+2, 3", "1-2, 3", "1,,2", "1, , 2"})
+    void 지난_주_로또_당첨_번호_입력_시_구분자가_쉼표가_아니면_예외_발생(final String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertThatIllegalArgumentException().isThrownBy(consoleInputView::inputWinningLottoNumbers);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a, b, c", "1, 2, a", "+1, 2, 3", "-1, 2, 3"})
+    void 지난_주_로또_당첨_번호_입력_시_숫자가_아니면_예외_발생(final String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertThatIllegalArgumentException().isThrownBy(consoleInputView::inputWinningLottoNumbers);
     }
 }
