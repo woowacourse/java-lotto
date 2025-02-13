146a553440b10b2af628173932f934e8f47c3e54
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-13 14:59:21 +0900

    test(WinningProfitTest): 불필요한 테스트 제거

--- src/test/java/domain/WinningProfitTest.java
+++ src/test/java/domain/WinningProfitTest.java
@@ -8,15 +8,6 @@ import org.junit.jupiter.api.Test;
 
 class WinningProfitTest {
 
-    @DisplayName("총 당첨 금액 계산을 성공한다")
-    @Test
-    void calculateTotalPriceTest() {
-        final Map<LottoRank, Integer> map = Map.of(LottoRank.RANK_4, 1, LottoRank.RANK_5, 1);
-        final WinningProfit winningProfit = new WinningProfit(map);
-        final int totalPrice = winningProfit.calculateTotalPrice();
-        assertThat(totalPrice).isEqualTo(55_000);
-    }
-
     @DisplayName("총 수익률 계산을 성공한다")
     @Test
     void calculateProfitRateTest() {
