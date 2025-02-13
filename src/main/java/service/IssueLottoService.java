cb8812e157f0b9b3ad86e1861cb0a07e912821eb
wogus <ind07152@naver.com>
2025-02-13 16:18:47 +0900

    fix(service): IssueLottoService 버그 수정
    - 금액 범위 검증 추가

--- src/main/java/service/IssueLottoService.java
+++ src/main/java/service/IssueLottoService.java
@@ -9,26 +9,29 @@ import util.RandomNumberGenerator;
 
 public class IssueLottoService {
 
-    public IssuedLottosDto issueLottos(int money){
+    public IssuedLottosDto issueLottos(int money) {
         validateMoney(money);
         int count = money / 1000;
         List<IssuedLottoDto> issuedLottos = new ArrayList<>();
-        while(issuedLottos.size()!= count) {
+        while (issuedLottos.size() != count) {
             Lotto lotto = new Lotto(RandomNumberGenerator.getRandomNumbers(1, 45));
-            if(!isDuplicate(issuedLottos,lotto.getSortedNumbers())){
+            if (!isDuplicate(issuedLottos, lotto.getSortedNumbers())) {
                 issuedLottos.add(new IssuedLottoDto(lotto.getSortedNumbers()));
             }
         }
         return new IssuedLottosDto(issuedLottos);
     }
 
-    private void validateMoney(int money){
-        if(money % 1000 != 0){
+    private void validateMoney(int money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException("금액 범위 오류");
+        }
+        if (money % 1000 != 0) {
             throw new IllegalArgumentException("금액 단위 오류");
         }
     }
 
-    private boolean isDuplicate(List<IssuedLottoDto> lottos, List<Integer> numbers){
+    private boolean isDuplicate(List<IssuedLottoDto> lottos, List<Integer> numbers) {
         IssuedLottoDto issuedLottoDto = new IssuedLottoDto(numbers);
         return lottos.contains(issuedLottoDto);
     }
