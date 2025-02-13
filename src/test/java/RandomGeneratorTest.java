ac42efc347472d921d7048a88822d640f372c18e
supernovaMK <minki5336@naver.com>
2025-02-13 15:58:05 +0900

    test(RandomGenerator): 테스트 도구 변경

--- src/test/java/RandomGeneratorTest.java
+++ src/test/java/RandomGeneratorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,26 +14,23 @@ class RandomGeneratorTest {
     @DisplayName("난수 값 6개 생성 테스트")
     void 난수_값_6개_생성_테스트() {
         List<Integer> numbers = RandomGenerator.generateUniqueRandomNumbers(6, 1, 45);
-        Assertions.assertThat(numbers.size()).isEqualTo(6);
+        assertThat(numbers.size()).isEqualTo(6);
     }
 
     @Test
     @DisplayName("값들의 범위 테스트")
     void 값들의_범위_테스트() {
-
         List<Integer> numbers = RandomGenerator.generateUniqueRandomNumbers(6, 1, 45);
 
         for (int number : numbers) {
-            assertTrue(number >= 1 && number <= 45, "숫자가 범위를 벗어났습니다: " + number);
+            assertThat(number).isBetween(1, 45);
         }
     }
 
     @Test
     @DisplayName("값들의 중복 테스트")
     void 값들의_중복_테스트() {
-
         List<Integer> numbers = RandomGenerator.generateUniqueRandomNumbers(6, 1, 45);
-
-        assertEquals(numbers.size(), numbers.stream().distinct().count(), "중복된 숫자가 생성되었습니다.");
+        assertThat(numbers.size()).isEqualTo(numbers.stream().distinct().count());
     }
 }
