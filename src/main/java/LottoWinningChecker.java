b8ff8d5c027711a8789a0ccd69250e85ae8b6bcd
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-13 14:05:39 +0900

    feat(LottoWinningChecker): 로또 결과 계산 클래스 구현

---
+++ src/main/java/LottoWinningChecker.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoWinningChecker {
+
+    public static LottoResult calculateResult(Lottos lottos, WinningNumberWithBonusNumber winningNumberWithBonusNumber) {
+        LottoResult lottoResult = LottoResult.initialize();
+
+        lottos.getLottos().forEach(
+                lotto -> {
+                    lottoResult.add(check(lotto, winningNumberWithBonusNumber));
+                }
+        );
+
+        return lottoResult;
+    }
+
+
+    private static LottoRanking check(Lotto lotto, WinningNumberWithBonusNumber winningNumberWithBonusNumber) {
+        List<Integer> numbers = new ArrayList<>(lotto.getNumbers());
+        numbers.retainAll(winningNumberWithBonusNumber.winningNumber().getNumbers());
+
+        int collectCount = numbers.size();
+        boolean isBonusCollect = lotto.getNumbers().contains(winningNumberWithBonusNumber.bonusNumber());
+        return LottoRanking.from(collectCount, isBonusCollect);
+    }
+}
