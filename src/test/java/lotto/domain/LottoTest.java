9fa2c2c87a2df472675fbe277f2ee2b04206f799
Mint <audwl03071@gmail.com>
2025-02-13 15:11:00 +0900

    fix(Lotto): 로또 생성시 예외 상황에 대한 검증 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -29,4 +29,24 @@ public class LottoTest {
         // Then
         Assertions.assertThat(count).isEqualTo(5);
     }
+
+    @Test
+    void 로또_번호의_개수가_6개가_아니면_예외가_발생한다() {
+        // Given
+
+        // When & Then
+        Assertions.assertThatThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호의 개수는 6개여야 합니다.");
+    }
+
+    @Test
+    void 로또_번호가_중복될_경우_예외가_발생한다() {
+        // Given
+
+        /// When & Then
+        Assertions.assertThatThrownBy(() -> new Lotto(Arrays.asList(1, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복되지 않은 로또 번호를 입력해 주세요.");
+    }
 }
