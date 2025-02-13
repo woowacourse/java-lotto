dfdfcd572052a2c5e20c5f7264b7d4de8242be02
CheChe903 <cheche903@naver.com>
2025-02-13 14:06:43 +0900

    feat(WinningNumbers): 당첨 번호 및 보너스 번호 검증 구현
    1. 범위 검증
    2. 중복 검증
    3. 사이즈 검증
    4. 보너스 번호, 당첨 번호 중복 검증

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbers {
 
@@ -10,5 +12,45 @@ public class WinningNumbers {
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+        validateWinningNumberRange();
+        validateBonusNumberRange();
+        validateDuplicateWinningNumber();
+        validateWinningNumberSize();
+        validateExistBonusNumber();
     }
+
+    private void validateWinningNumberRange() {
+        for (Integer winningNumber : winningNumbers) {
+            if (winningNumber < 1 || winningNumber > 45) {
+                throw new IllegalArgumentException("[ERROR] 범위를 벗어나는 숫자입니다.");
+            }
+        }
+    }
+
+    private void validateBonusNumberRange() {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 범위를 벗어나는 숫자입니다.");
+        }
+    }
+
+    private void validateDuplicateWinningNumber() {
+        Set<Integer> set = new HashSet<>(winningNumbers);
+        if (set.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateWinningNumberSize() {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 올바른 당첨 번호 갯수를 입력해 주세요");
+        }
+    }
+
+    private void validateExistBonusNumber() {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+
 }
