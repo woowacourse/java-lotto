ae81d6be1f9eafe51c9a8cbea69d632cb98c495c
horizonpioneer <quantumjump10@gmail.com>
2025-02-13 11:21:58 +0900

    refactor(InputView): 구입금액 입력 요청 메시지가 입력 기능에 포함되도록 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.util.Converter;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,6 +10,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static long inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextLong();
     }
 
