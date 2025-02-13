f9de71fba0d8f8f43c2932e59f2bc56bb6490ff8
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-13 17:13:34 +0900

    test(Price): 예외 클래스 변경

--- src/test/java/PriceTest.java
+++ src/test/java/PriceTest.java
@@ -18,7 +18,7 @@ class PriceTest {
     @DisplayName("Price 객체는 정수만 입력할 수 있다.")
     void Price_객체는_정수가_아닌_값을_입력받을_수_없다() {
         assertThatThrownBy(() -> new Price("asdasd"))
-                .isInstanceOf(NumberFormatException.class);
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
