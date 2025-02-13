d1ef76feb378409270fee9144502765db2c38e11
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-13 11:12:44 +0900

    feat(dto): 로또 출력을 위한 LottoDto 구현

--- src/main/java/dto/LottoDto.java
+++ src/main/java/dto/LottoDto.java
@@ -1,14 +1,16 @@
 package dto;
 
 import java.util.List;
-import model.Lotto;
+import model.PrizeTier;
 
 public class LottoDto {
 
     private List<Integer> numbers;
+    private PrizeTier prizeTier;
 
-    public LottoDto(List<Integer> numbers) {
+    public LottoDto(List<Integer> numbers, PrizeTier prizeTier) {
         this.numbers = numbers;
+        this.prizeTier = prizeTier;
     }
 
     public List<Integer> getNumbers() {
