b00f1e4f72375a94e80b8d9d1cff7cb86c0312dc
KJungW <sagakkb0707@naver.com>
2025-02-13 14:54:57 +0900

    test(lotto_service): 등수 계산 테스트
    작업 내용
    - 발행된 로또의 당첨 등수를 찾을 수 있다.

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,15 +1,18 @@
 package lotto.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.costant.WinningTier;
 import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class LottoServiceTest {
+
     private LottoService lottoService;
 
     @BeforeEach
@@ -29,5 +32,22 @@ public class LottoServiceTest {
         int expectedCount = 5;
         List<Lotto> lottos = lottoService.issueLottos(expectedCount);
         assertThat(lottos.size()).isEqualTo(expectedCount);
+    }
+
+    @DisplayName("발행된 로또의 당첨 등수를 찾을 수 있다.")
+    @Test
+    void 발행된_로또의_당첨_등수를_찾을_수_있다() {
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)),
+                new Lotto(List.of(1, 2, 3, 4, 8, 9)),
+                new Lotto(List.of(1, 2, 3, 8, 9, 10)),
+                new Lotto(List.of(8, 9, 10, 11, 12, 13)));
+        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+
+        List<WinningTier> actualTiers = lottoService.findWinningTiers(lottos, winningLotto);
+
+        assertThat(actualTiers).containsExactlyInAnyOrderElementsOf(Arrays.stream(WinningTier.values()).toList());
     }
 }
