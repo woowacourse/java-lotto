d01c0fd5088cc71857e5fd963ac0e2d12b205fc3
Mint <audwl03071@gmail.com>
2025-02-13 14:51:48 +0900

    test(WinningResultCalculator): 당첨 로또 개수를 구하는 테스트 작성

--- src/test/java/lotto/domain/WinningResultCalculatorTest.java
+++ src/test/java/lotto/domain/WinningResultCalculatorTest.java
@@ -1,12 +1,15 @@
 package lotto.domain;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 public class WinningResultCalculatorTest {
-    
+
     @Test
     void 당첨_결과_계산기가_정상적으로_생성된다() {
         // Given
@@ -28,5 +31,31 @@ public class WinningResultCalculatorTest {
         Assertions.assertThatThrownBy(() -> new WinningResultCalculator(winningLotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 번호와 중복되지 않는 보너스 번호를 입력해 주세요.");
+    }
+
+    @Test
+    void 당첨된_로또의_개수를_구한다() {
+        // Given
+        Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        LottoNumber bonusNumber = new LottoNumber(7);
+
+        List<Lotto> lottos = List.of(
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                new Lotto(Arrays.asList(7, 2, 3, 4, 5, 6)),
+                new Lotto(Arrays.asList(8, 2, 3, 4, 5, 6)),
+                new Lotto(Arrays.asList(8, 9, 3, 4, 5, 6))
+        );
+        WinningResultCalculator winningResultCalculator = new WinningResultCalculator(winningLotto, bonusNumber);
+
+        // When
+        Map<LottoAward, Integer> winningResult = winningResultCalculator.countLottoPrizes(lottos);
+
+        // Then
+        assertAll(
+                () -> Assertions.assertThat(winningResult.get(LottoAward.FIRST_RANK)).isEqualTo(1),
+                () -> Assertions.assertThat(winningResult.get(LottoAward.SECOND_RANK)).isEqualTo(1),
+                () -> Assertions.assertThat(winningResult.get(LottoAward.THIRD_RANK)).isEqualTo(1),
+                () -> Assertions.assertThat(winningResult.get(LottoAward.FOURTH_RANK)).isEqualTo(1)
+        );
     }
 }
