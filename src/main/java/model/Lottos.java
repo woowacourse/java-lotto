f4d5e40cc34045625bdef667c859087164f663b3
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-13 10:48:27 +0900

    feat(model): PrizeTier 별 개수 & 총 상금 계산 메서드 구현

--- src/main/java/model/Lottos.java
+++ src/main/java/model/Lottos.java
@@ -10,4 +10,23 @@ public class Lottos {
         lottos.add(lotto);
     }
 
+    public int countTiers(PrizeTier prizeTier) {
+        int count = 0;
+        for (Lotto lotto : lottos) {
+            if (lotto.isTierMatched(prizeTier)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public long calculateTotalPrize() {
+        long totalPrize = 0;
+        for (Lotto lotto : lottos) {
+            totalPrize += lotto.extractPrize();
+        }
+        return totalPrize;
+    }
+
+
 }
