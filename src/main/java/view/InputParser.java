d74a1edc2907b6f365991c9e38c5b235202a8198
Jin409 <sualng123@gmail.com>
2025-02-13 17:13:13 +0900

    refactor : 당첨 번호 구분자 상수화

--- src/main/java/view/InputParser.java
+++ src/main/java/view/InputParser.java
@@ -6,12 +6,14 @@ import java.util.stream.Stream;
 
 public class InputParser {
 
+    private static final String WINNING_NUMBERS_SPLITTER = ", ";
+
     public int parseBonusNumber(String rawBonusNumber) {
         return parsePositiveInteger(rawBonusNumber);
     }
 
     public List<Integer> parseWinningNumbers(String rawWinningNumbers) {
-        String[] splitWinningNumbers = rawWinningNumbers.split(", ");
+        String[] splitWinningNumbers = rawWinningNumbers.split(WINNING_NUMBERS_SPLITTER);
         return Stream.of(splitWinningNumbers)
                 .map(this::parsePositiveInteger)
                 .toList();
@@ -19,8 +21,10 @@ public class InputParser {
 
     private int parsePositiveInteger(String rawNumber) {
         NumberValidator.validateInteger(rawNumber);
+
         int parsedNumber = Integer.parseInt(rawNumber);
         NumberValidator.validatePositive(parsedNumber);
+
         return parsedNumber;
     }
 }
