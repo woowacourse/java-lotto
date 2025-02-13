4b8a64ffaf1f5e805c3dfa40e48a3c1c8defc1aa
taek2222 <oot309@daum.net>
2025-02-13 17:09:46 +0900

    test: 티켓 클래스 테스트 작성

---
+++ src/test/java/model/TicketTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class TicketTest {
+
+    @Test
+    void 로또를_구매하면_금액에_알맞은_티켓_수를_반환한다() {
+        final int price = 5000;
+        Ticket ticket = new Ticket(price);
+
+        assertThat(ticket.getTicketAmount())
+                .isEqualTo(5);
+    }
+
+    @ParameterizedTest(name = "구입 금액 : {0}")
+    @ValueSource(ints = {100, 200, 40500, 56070})
+    void 구입_금액이_올바르지_않으면_예외가_발생한다(int price) {
+        assertThatThrownBy(() -> new Ticket(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
