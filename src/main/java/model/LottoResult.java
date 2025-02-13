2f011342cd520b6e1ff3de493e519089eb42ca23
taek2222 <oot309@daum.net>
2025-02-13 13:59:17 +0900

    feat: 당첨 순위 정렬 추가

--- src/main/java/model/LottoResult.java
+++ src/main/java/model/LottoResult.java
@@ -2,16 +2,25 @@ package model;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoResult {
 
     private Map<RankType, Integer> result;
 
-    public LottoResult() {
-        result = new LinkedHashMap<>();
+    public LottoResult(Lottos lottos, WinningLotto winningLotto) {
+        result = winningLotto.evaluateRank(lottos.getLottos());
+        sort();
     }
 
-    public void processLottoResult(Lottos lottos, WinningLotto winningLotto) {
-        result = winningLotto.evaluateRank(lottos.getLottos());
+    public void sort() { // TODO: 리팩토링 필요
+        result = result.entrySet().stream()
+                .sorted((r1, r2) -> Integer.compare(r2.getKey().getRank(), r1.getKey().getRank()))
+                .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (e1, e2) -> e1,
+                                LinkedHashMap::new
+                ));
     }
 }
