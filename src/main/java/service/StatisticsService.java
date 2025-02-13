8d2fb18ecf24c671709ae3c9920de77a45cbeb4b
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-13 10:48:01 +0900

    feat(service): StatisticsService 구현

---
+++ src/main/java/service/StatisticsService.java
@@ -0,0 +1,21 @@
+package service;
+
+import java.util.HashMap;
+import java.util.Map;
+import model.Lottos;
+import model.PrizeTier;
+import model.Statistics;
+
+public class StatisticsService {
+    public Statistics produceStatistics(Lottos lottos, int purchaseAmount) {
+        Map<PrizeTier, Integer> prizeCounts = new HashMap<>();
+        for (PrizeTier prizeTier : PrizeTier.values()) {
+            int count = lottos.countTiers(prizeTier);
+            prizeCounts.put(prizeTier, count);
+        }
+
+        long totalPrize = lottos.calculateTotalPrize();
+        double profitRate = (double) totalPrize / purchaseAmount;
+        return new Statistics(prizeCounts, totalPrize, profitRate);
+    }
+}
