468852ec132289dec0d23d80718da017a3871dc4
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-13 13:55:59 +0900

    refactor(RandomGenerator): 난수 생성 로직 변경
    - 강력한 SecureRandom

--- src/main/java/RandomGenerator.java
+++ src/main/java/RandomGenerator.java
@@ -1,21 +1,21 @@
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class RandomGenerator {
     private RandomGenerator() {
     }
 
     public static List<Integer> generateUniqueRandomNumbers(int count, int start, int end) {
-        Random random = new Random();
-        random.setSeed(System.currentTimeMillis());
+        SecureRandom random = new SecureRandom();
+
         Set<Integer> numbers = new HashSet<>();
         while (numbers.size() < count) {
             int number = random.nextInt(end - start + 1) + start;
             numbers.add(number);
         }
-        return numbers.stream().collect(Collectors.toList());
+        return new ArrayList<>(numbers);
     }
 }
