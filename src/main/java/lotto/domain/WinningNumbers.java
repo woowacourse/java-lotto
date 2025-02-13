1927b9a01928a7b2249a2af1708871301469953a
Firedrago95 <yonghwa95@naver.com>
2025-02-13 13:46:16 +0900

    feat: 예외 처리 구현

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,11 +1,18 @@
 package lotto.domain;
 
+import lotto.constant.ErrorMessage;
+
+import static lotto.constant.ErrorMessage.*;
+
 public class WinningNumbers {
 
     private Lotto winningLotto;
     private int bonusNumber;
 
     public WinningNumbers(Lotto lotto, int bonusNumber) {
+        if (lotto.containsNumber(bonusNumber)) {
+           throw new IllegalArgumentException(WINNING_NUMBERS_CONTAIN_BONUS_NUMBER.getMessage());
+        }
         this.winningLotto = lotto;
         this.bonusNumber = bonusNumber;
     }
