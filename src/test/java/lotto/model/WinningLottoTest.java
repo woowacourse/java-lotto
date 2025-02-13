c4960e1551a2e8d9fec0d25da9952f6ae4d0a4ad
ykmxxi <ykmxxi97@gmail.com>
2025-02-13 14:37:10 +0900

    test: 당첨 등수 계산 기능 반환 타입 수정

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.tuple;
 
 import java.util.List;
 
@@ -41,7 +42,10 @@ class WinningLottoTest {
         Lottos lottos = new Lottos();
         lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
 
-        assertThat(winningLotto.calculateWinning(lottos)).isEqualTo(Rank.SECOND);
+        assertThat(winningLotto.calculateWinning(lottos)
+                .getResponses())
+                .extracting("matchingCount, hasBonus, winningCount")
+                .contains(tuple(5, true, 1));
     }
 
 }
