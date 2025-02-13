1909d89be561d5647eaeb53f600432a01851cf1f
horizonpioneer <quantumjump10@gmail.com>
2025-02-13 11:05:26 +0900

    feat(LottoService): 구매 금액을 통해 로또 티켓을 발행하는 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,8 +1,24 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumberGenerator;
+import lotto.domain.LottoTicket;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
 
     private static final int LOTTO_PRICE = 1000;
+
+    public LottoTicket issueTicket(long purchaseAmount) {
+        int count = countNumberOfPurchases(purchaseAmount);
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(LottoNumberGenerator.generate()));
+        }
+        return new LottoTicket(lottos);
+    }
 
     public int countNumberOfPurchases(long purchaseAmount) {
         return (int) purchaseAmount / LOTTO_PRICE;
