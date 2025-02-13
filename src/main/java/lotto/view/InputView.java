9e67812f461a8c60fc650bdd4bbbf55416cbdf61
horizonpioneer <quantumjump10@gmail.com>
2025-02-13 11:25:23 +0900

    refactor(InputView): 당첨 번호 입력 메시지 앞에 개행 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -16,7 +16,7 @@ public class InputView {
 
     public static List<Integer> inputWinningNumbers() {
         scanner.nextLine();
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
         return Converter.convertToIntegerList(scanner.nextLine());
     }
 
