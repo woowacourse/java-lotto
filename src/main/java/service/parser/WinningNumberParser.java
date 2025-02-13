5b039beda6bc7ea29518a0f1c493a1bbbc4e0776
GiSeok Kang <ime.rokz@gmail.com>
2025-02-13 15:23:33 +0900

    refactor: WinningNumberParser 검증 로직 메서드 분리

--- src/main/java/service/parser/WinningNumberParser.java
+++ src/main/java/service/parser/WinningNumberParser.java
@@ -1,58 +1,58 @@
 package service.parser;
 
-import static validator.ErrorMessages.LOTTO_NUMBER_COUNT;
-
+import constant.RegexConstants;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 import validator.ErrorMessages;
 import validator.Validator;
 
 public class WinningNumberParser {
-
-    private static final String NUMBER_ONLY_REGEX = "^[0-9]+$"; // 숫자만
     private static final String MULTIPLE_TEMPLATE = "%s{2,}"; // 연속된 구분자
     private static final String EDGE_TEMPLATE = "^%s|%s$"; // 앞뒤 구분자
     private static final String EMPTY_STRING = ""; // 빈문자열
-
-    // 한 종류의 구분자만 사용되는 경우, 해당 구분자를 정리
-    public static String cleanConsecutiveDelimiters(String input, String delimiter) {
-        String escapedDelimiter = Pattern.quote(delimiter); // 특수 문자 처리를 위해 escape
-        String consecutiveDelimitersRegex = String.format(MULTIPLE_TEMPLATE, escapedDelimiter); // 연속된 구분자 표현식
-        String leadingTrailingDelimiterRegex =
-                String.format(EDGE_TEMPLATE, escapedDelimiter, escapedDelimiter); // 앞뒤 구분자 표현식
-
-        return input.replaceAll(consecutiveDelimitersRegex, delimiter) // 연속된 구분자를 하나로 변경
-                .replaceAll(leadingTrailingDelimiterRegex, EMPTY_STRING); // 앞뒤 구분자 제거
-    }
-
-    // 구분자로 해당 문자열 분리 후 문자열 리스트로 반환
-    public static List<String> splitStringByDelimiter(String input, String delimiter) {
-        return Arrays.asList(input.split(delimiter));
-    }
+    private static final int RANGE_START = 1;
+    private static final int RANGE_END = 45;
+    public static final int LOTTO_COUNT = 6;
 
     public static List<Integer> parseWinningNumbers(String input) {
         Validator.validateEmptyInput(input); // 공백 검사
+        List<String> rawNumbers = Arrays.asList(input.split(","));
 
-        String cleanInput = cleanConsecutiveDelimiters(input, ",");
-        List<String> rawNumbers = splitStringByDelimiter(cleanInput, ",");
-
-        // TODO: 유효성 검사
-        if (rawNumbers.size() != 6){
-            throw new IllegalArgumentException(LOTTO_NUMBER_COUNT.getMessage());
-        }
-        for (int i = 0; i < rawNumbers.size(); i++) {
-            Validator.checkInvalidForm(rawNumbers.get(i), NUMBER_ONLY_REGEX, ErrorMessages.NOT_NUMBER.getMessage());
-        }
-        Validator.isDuplicates(rawNumbers);
-
-        List<Integer> numbers =  rawNumbers.stream()
-                .map(num -> Integer.parseInt(num))
-                .toList();
-
-        for (int idx = 0; idx < rawNumbers.size(); idx++)
-            Validator.checkOutOfRange(numbers.get(idx), 1, 45, ErrorMessages.LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
+        validateNotNumber(rawNumbers);
+        List<Integer> numbers = rawToIntegers(rawNumbers);
+        validateLottoNumberCount(numbers);
+        validateDuplicate(numbers);
+        validateOutOfRange(rawNumbers, numbers);
 
         return numbers;
+    }
+
+    private static void validateOutOfRange(List<String> rawNumbers, List<Integer> numbers) {
+        for (int idx = 0; idx < rawNumbers.size(); idx++)
+            Validator.checkOutOfRange(numbers.get(idx), RANGE_START, RANGE_END, ErrorMessages.LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        if (Validator.isDuplicates(numbers)){
+            throw new IllegalArgumentException(ErrorMessages.DUPLICATE_EXIST.getMessage());
+        }
+    }
+
+    private static void validateLottoNumberCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_COUNT){
+            throw new IllegalArgumentException(ErrorMessages.LOTTO_NUMBER_COUNT.getMessage());
+        }
+    }
+
+    private static List<Integer> rawToIntegers(List<String> rawNumbers) {
+        return rawNumbers.stream()
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private static void validateNotNumber(List<String> rawNumbers) {
+        for (int i = 0; i < rawNumbers.size(); i++) {
+            Validator.checkInvalidForm(rawNumbers.get(i), RegexConstants.NUMBER_ONLY_REGEX, ErrorMessages.NOT_NUMBER.getMessage());
+        }
     }
 }
