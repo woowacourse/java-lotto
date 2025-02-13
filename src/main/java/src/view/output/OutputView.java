16b94cfcf63626cd1479dc048ae5e9013706ac27
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-13 10:41:10 +0900

    feat: 구입 로또 출력 함수 시그니처 추가

--- src/main/java/src/view/output/OutputView.java
+++ src/main/java/src/view/output/OutputView.java
@@ -1,6 +1,10 @@
 package src.view.output;
 
+import java.util.List;
+
 public interface OutputView {
 
     void printInputPurchaseMoneyMessage();
+
+    void printPurchasedLottos(List<LottoResponse> responses);
 }
