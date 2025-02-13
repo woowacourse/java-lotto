1d97c12833b8b297cca04f1a6c49c36dd47ec75b
horizonpioneer <quantumjump10@gmail.com>
2025-02-13 16:32:04 +0900

    feat: 보너스 번호 범위 검증 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -7,10 +7,17 @@ public class BonusNumber {
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
+        validateRange(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
     public boolean isMatch(List<Integer> numbers) {
         return numbers.contains(bonusNumber);
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("보너스 번호는 1~45 사이의 수를 입력해야 한다.");
+        }
     }
 }
