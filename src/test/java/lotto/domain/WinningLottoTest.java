ee03bc292382b494718c6862b802f337cad31b0a
kimdev <kim__dev@hufs.ac.kr>
2025-02-13 10:48:40 +0900

    docs(README): 기능 목록 수정

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+    
+    @Test
+    @DisplayName("")
+    void () {
+        // Given
+        
+        // When
+    
+        // Then
+    }
+}
