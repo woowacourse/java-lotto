5df5f4a21be6e2d306df63d7b95b6f473adfaaf9
Han <jumdo12@gmail.com>
2025-02-13 14:00:53 +0900

    test: 기본 테스트 케이스 추가

--- src/test/java/domain/LottoStatsTest.java
+++ src/test/java/domain/LottoStatsTest.java
@@ -24,6 +24,36 @@ class LottoStatsTest {
         Assertions.assertThat(lottoStats.getRankCount(Rank.FIRST)).isEqualTo(3);
     }
 
+    @DisplayName("기본 테스트 케이스")
+    @Test
+    void 기본_테스트_케이스() {
+        List<Lotto> lottos = List.of(
+            new Lotto(List.of(8, 21, 23, 41, 42, 43)),
+            new Lotto(List.of(3, 5, 11, 16, 32, 38)),
+            new Lotto(List.of(7, 11, 16, 35, 36, 44)),
+            new Lotto(List.of(1, 8, 11, 31, 41, 42)),
+            new Lotto(List.of(13, 14, 16, 38, 42, 45)),
+            new Lotto(List.of(7, 11, 30, 40, 42, 43)),
+            new Lotto(List.of(2, 13, 22, 32, 38, 45)),
+            new Lotto(List.of(23, 25, 33, 36, 39, 41)),
+            new Lotto(List.of(1, 3, 5, 14, 22, 45)),
+            new Lotto(List.of(5, 9, 38, 41, 43, 44)),
+            new Lotto(List.of(2, 8, 9, 18, 19, 21)),
+            new Lotto(List.of(13, 14, 18, 21, 23, 35)),
+            new Lotto(List.of(17, 21, 29, 37, 42, 45)),
+            new Lotto(List.of(3, 8, 27, 30, 35, 44)));
+
+        LottoStats lottoStats = new LottoStats(List.of(1,2,3,4,5,6), 7);
+        lottoStats.calculateResult(lottos);
+
+        Assertions.assertThat(lottoStats.getRankCount(Rank.FIRST)).isEqualTo(0);
+        Assertions.assertThat(lottoStats.getRankCount(Rank.SECOND)).isEqualTo(0);
+        Assertions.assertThat(lottoStats.getRankCount(Rank.THIRD)).isEqualTo(0);
+        Assertions.assertThat(lottoStats.getRankCount(Rank.FOURTH)).isEqualTo(0);
+        Assertions.assertThat(lottoStats.getRankCount(Rank.FIFTH)).isEqualTo(1);
+        Assertions.assertThat(lottoStats.getEarningRate(14000)).isEqualTo("0.35");
+    }
+
     @DisplayName("총 상금 계산 테스트")
     @Test
     void 총_상금_계산_테스트() {
