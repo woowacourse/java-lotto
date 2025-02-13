1053c96a696c44d8ac7ad44dbd37d94b00f8129e
dltmddus <tmddus4671@gmail.com>
2025-02-13 15:10:10 +0900

    refactor: final 키워드 추가

--- src/main/java/domain/LottoTicket.java
+++ src/main/java/domain/LottoTicket.java
@@ -9,7 +9,7 @@ public class LottoTicket {
     public static int LOTTO_MIN_NUMBER = 1;
     public static int LOTTO_MAX_NUMBER = 45;
 
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
     public LottoTicket(List<Integer> numbers) {
         this.numbers = numbers;
