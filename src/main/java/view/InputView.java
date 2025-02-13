8616ac3654a0c6fbe3827c50e8c978f3a739cf4f
2jin2.1031 <2jin2.1031@gmail.com>
2025-02-13 11:32:52 +0900

    refactor: 입력 공통 로직 분리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,16 +11,20 @@ public class InputView {
 
     public static LottoPurchase getPurchaseLotto() {
         System.out.println("구입금액을 입력해 주세요.");
-        return LottoPurchase.of(sc.nextLine());
+        return LottoPurchase.of(getInput());
     }
 
     public static Lotto getWinningLotto() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return Lotto.of(sc.nextLine());
+        return Lotto.of(getInput());
     }
 
     public static Bonus getWinningBonus(final Lotto lotto) {
         System.out.println("보너스 볼을 입력해 주세요.");
-        return Bonus.of(sc.nextLine(), lotto);
+        return Bonus.of(getInput(), lotto);
+    }
+
+    private static String getInput() {
+        return sc.nextLine();
     }
 }
