e6e459fb3755dbd86f7ed6bd288da8e384286a7b
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-13 16:28:32 +0900

    feat: 당첨 로또 클래스 구현
    - 당첨 번호 일급 컬렉션, 보너스 번호 vo 를 가짐.

---
+++ src/main/java/src/model/winning_lotto/WinningLotto.java
@@ -0,0 +1,55 @@
+package src.model.winning_lotto;
+
+import java.util.List;
+import src.model.lotto.Lotto;
+import src.model.winning_lotto.vo.BonusNumber;
+import src.model.winning_lotto.vo.WinningLottoNumbers;
+
+public class WinningLotto {
+
+    private final WinningLottoNumbers numbers;
+    private final BonusNumber bonusNumber;
+
+    private WinningLotto(WinningLottoNumbers numbers, BonusNumber bonusNumber) {
+        validate(numbers, bonusNumber);
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningLotto of(List<Integer> numbers, int bonusNumber) {
+        return new WinningLotto(WinningLottoNumbers.from(numbers), BonusNumber.from(bonusNumber));
+    }
+
+    private void validate(WinningLottoNumbers numbers, BonusNumber bonusNumber) {
+        if (numbers.contains(bonusNumber.getValue())) {
+            throw new IllegalArgumentException("당첨 로또 번호는 보너스 번호를 포함할 수 없습니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers.getValues();
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber.getValue();
+    }
+
+    public int getMatchCount(Lotto lotto) {
+
+        return (int) lotto.getNumbers().stream()
+                .filter(numbers::contains)
+                .count();
+    }
+
+    public boolean isBonusMatch(Lotto lotto) {
+        return lotto.contains(bonusNumber.getValue());
+    }
+
+    @Override
+    public String toString() {
+        return "WinningLotto{" +
+                "numbers=" + numbers +
+                ", bonusNumber=" + bonusNumber +
+                '}';
+    }
+}
