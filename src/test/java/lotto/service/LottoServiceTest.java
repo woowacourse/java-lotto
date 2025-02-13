b680cd4dbc03c7ce08672138fdfb2f1a38873e09
julie <7jujubuilds@gmail.com>
2025-02-13 15:03:51 +0900

    test(lotto_service): 로또 서비스 테스트
    작업 내용
    - 수익률을 올바르게 계산할 수 있다

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,15 +1,16 @@
 package lotto.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Arrays;
-import java.util.List;
 import lotto.costant.WinningTier;
 import lotto.domain.Lotto;
 import lotto.domain.WinningLotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoServiceTest {
 
@@ -49,5 +50,15 @@ public class LottoServiceTest {
         List<WinningTier> actualTiers = lottoService.findWinningTiers(lottos, winningLotto);
 
         assertThat(actualTiers).containsExactlyInAnyOrderElementsOf(Arrays.stream(WinningTier.values()).toList());
+    }
+
+    @DisplayName("수익률을 올바르게 계산할 수 있다.")
+    @Test
+    void 수익률을_올바르게_계산할_수_있다() {
+        List<WinningTier> winningTiers = List.of(WinningTier.FOURTH, WinningTier.FIFTH, WinningTier.EMPTY);
+        int purchaseAmount = 5000;
+        double expectedProfit = 11;
+
+        assertThat(lottoService.calculateProfit(winningTiers, purchaseAmount)).isEqualTo(expectedProfit);
     }
 }
