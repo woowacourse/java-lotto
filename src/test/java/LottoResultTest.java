5f975cc0f925f9b7cb477877f284c730eb14eb2e
supernovaMK <minki5336@naver.com>
2025-02-13 16:05:30 +0900

    test(LottoResult): 로또 결과 테스트 추가
    - 초기화된 객체의 Map 검증
    - 결과에 로또 순위 개수 추가 검증

---
+++ src/test/java/LottoResultTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoResultTest {
+
+    @Test
+    @DisplayName("초기화 된 객체의 맵의 키 개수는 로또 순위 개수 만큼 받아 올 수 있다")
+    void 초기화_된_객체의_맵의_키_개수는_로또_순위_개수_만큼_받아_올_수_있다() {
+        LottoResult lottoResult = LottoResult.initialize();
+        assertThat(lottoResult.result().size()).isEqualTo(LottoRanking.values().length);
+    }
+
+    @Test
+    @DisplayName("초기화 된 객체의 맵의 모든 값은 0이다")
+    void 초기화_된_객체의_맵의_모든_값은_0이다() {
+        LottoResult lottoResult = LottoResult.initialize();
+        boolean isAllZero = lottoResult.result().values().stream().filter((value) -> value != 0).toList().isEmpty();
+        assertThat(isAllZero).isTrue();
+    }
+
+    @Test
+    @DisplayName("결과에 로또 순위 개수를 추가할 수 있다")
+    void 결과에_로또_순위_개수를_추가할_수_있다(){
+        LottoResult lottoResult = LottoResult.initialize();
+        lottoResult.add(LottoRanking.FIRST);
+        assertThat(lottoResult.result().get(LottoRanking.FIRST)).isEqualTo(1);
+    }
+}
