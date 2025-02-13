7b04e352fd14162f87c75af47287c3285609b3db
horizonpioneer <quantumjump10@gmail.com>
2025-02-13 17:00:54 +0900

    refactor: 매직넘버 상수화

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class BonusNumber {
 
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
@@ -16,8 +19,9 @@ public class BonusNumber {
     }
 
     private void validateRange(int bonusNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("보너스 번호는 1~45 사이의 수를 입력해야 한다.");
+        if (bonusNumber < LOTTO_START_NUMBER || bonusNumber > LOTTO_END_NUMBER) {
+            throw new IllegalArgumentException("보너스 번호는 " + LOTTO_START_NUMBER + "~" +
+                    LOTTO_END_NUMBER + " 사이의 수를 입력해야 합니다.");
         }
     }
 }
--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -6,6 +6,8 @@ import java.util.Set;
 
 public class WinningNumber {
 
+    private static final int WINNING_NUMBER_COUNT = 6;
+
     private final List<Integer> winningNumbers;
 
     public WinningNumber(List<Integer> numbers) {
@@ -21,15 +23,23 @@ public class WinningNumber {
     }
 
     private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        if (numbers.size() != WINNING_NUMBER_COUNT) {
+            throw new IllegalArgumentException("당첨 번호는 " + WINNING_NUMBER_COUNT + "개여야 합니다.");
         }
     }
 
     private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> set = new HashSet<>(numbers);
-        if (set.size() != 6) {
+        if (set.size() != WINNING_NUMBER_COUNT) {
             throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("당첨 번호는 1~45 사이의 수여야 합니다.");
+            }
         }
     }
 
