f5001fc34e631b71326c930ec01faebac4dae1cf
ryan-dia <cjfdnjs158@naver.com>
2025-02-13 13:53:04 +0900

    feat: 재시도 유틸리티 추가
    Co-authored-by: kiwoook <tizmfns1218@Naver.com>

---
+++ src/main/java/utils/RecoveryUtils.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import view.OutputView;
+
+public class RecoveryUtils {
+
+    private RecoveryUtils() {
+    }
+
+    public static <T, R> R executeWithRetry(Supplier<T> inputSupplier, Function<T, R> processFunction) {
+        while (true) {
+            try {
+                return processFunction.apply(inputSupplier.get());
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e);
+            }
+        }
+    }
+
+    public static <T> void executeWithRetry(Supplier<T> inputSupplier, Consumer<T> processFunction) {
+        while (true) {
+            try {
+                T input = inputSupplier.get();
+                processFunction.accept(input);
+                return;
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e);
+            }
+        }
+    }
+
+    public static <T> T executeWithRetry(Supplier<T> inputSupplier) {
+        while (true) {
+            try {
+                return inputSupplier.get();
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e);
+            }
+        }
+    }
+
+
+}
