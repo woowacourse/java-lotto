049fae681930b9fc84cc2ed5e8a8fa9da19fac37
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-13 10:41:18 +0900

    feat: 구입 로또 출력 함수 구현

--- src/main/java/src/view/output/ConsoleOutputView.java
+++ src/main/java/src/view/output/ConsoleOutputView.java
@@ -1,9 +1,21 @@
 package src.view.output;
 
+import java.util.List;
+
 public class ConsoleOutputView implements OutputView {
 
     @Override
     public void printInputPurchaseMoneyMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    @Override
+    public void printPurchasedLottos(List<LottoResponse> responses) {
+        System.out.printf("%d개를 구매했습니다.\n", responses.size());
+        responses.forEach(this::printLotto);
+    }
+
+    private void printLotto(LottoResponse response) {
+        System.out.println(response.getNumbers().stream().sorted().toList());
     }
 }
