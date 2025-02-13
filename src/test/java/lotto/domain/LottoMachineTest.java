1ede4472f9009bbb7436b85a7c2b0e4f55c8fa23
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-13 16:11:30 +0900

    test(LottoMachineTest): add ticket numbers test

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -9,9 +9,20 @@ public class LottoMachineTest {
 
     @DisplayName("로또 구입 금액만큼 로또 티켓 수 생성 확인")
     @Test
-    public void lottoTicketNumber() {
+    public void lottoTicketAmount() {
         LottoMoney lottoMoney = new LottoMoney("10000");
         LottoMachine lottoMachine = new LottoMachine(lottoMoney);
         assertThat(lottoMachine.getLottoTickets().size()).isEqualTo(10);
+    }
+
+    @DisplayName("로또 번호가 중복 없이 6개 생성 확인")
+    @Test
+    public void lottoTicketNumbers() {
+        LottoMoney lottoMoney = new LottoMoney("1000");
+        LottoMachine lottoMachine = new LottoMachine(lottoMoney);
+        lottoMachine.getLottoTickets().forEach(lottoTickets -> {
+            assertThat(lottoTickets).hasSize(6);
+            assertThat(lottoTickets).doesNotHaveDuplicates();
+        });
     }
 }
