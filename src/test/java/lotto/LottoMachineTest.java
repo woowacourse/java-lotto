27e927e5757b21001723f0606de6ee7fbec991d9
ljhee92 <weilainib7@gmail.com>
2025-02-13 15:45:00 +0900

    docs: 테스트에 대한 고민 TODO

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,9 +1,9 @@
 package lotto;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoMachineTest {
     @DisplayName("로또는 6개의 번호를 가진다")
