6d1395ee45078f9cfe860b883caebdd76e597fec
julie <7jujubuilds@gmail.com>
2025-02-13 14:24:23 +0900

    test(lotto_service): 로또 서비스 테스트
    작업 내용
    - 구매한 개수만큼의 로또를 생성할 수 있다

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,8 +1,11 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,5 +21,13 @@ public class LottoServiceTest {
     @Test
     void 구매_금액에_해당하는_로또_개수를_계산할_수_있다() {
         assertThat(lottoService.purchaseLotto(10000)).isEqualTo(10);
+    }
+
+    @DisplayName("구매한 개수만큼의 로또를 생성할 수 있다.")
+    @Test
+    void 구매한_개수만큼의_로또를_생성할_수_있다() {
+        int expectedCount = 5;
+        List<Lotto> lottos = lottoService.issueLottos(expectedCount);
+        assertThat(lottos.size()).isEqualTo(expectedCount);
     }
 }
