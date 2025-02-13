dc14c3e832c8e3c5613fea3592a511218bf0c180
ppparkta <pppppark2@gmail.com>
2025-02-13 16:40:33 +0900

    feat: Prize 당첨 로직 구현

--- src/main/java/domain/enums/Prize.java
+++ src/main/java/domain/enums/Prize.java
@@ -3,31 +3,33 @@ package domain.enums;
 import java.util.Optional;
 
 public enum Prize {
-    EMPTY("", 0, 0, false),
-    FIFTH("3개 일치", 5_000, 3, false),
-    FOURTH("4개 일치", 50_000, 4, false),
-    THIRD("5개 일치", 150_000, 5, false),
-    SECOND("5개 일치, 보너스 볼 일치", 30_000_000, 5, true),
-    FIRST("6개 일치", 2_000_000_000, 6, false),
+    EMPTY("", 0, 0),
+    FIFTH("3개 일치", 5_000, 3),
+    FOURTH("4개 일치", 50_000, 4),
+    THIRD("5개 일치", 150_000, 5),
+    SECOND("5개 일치, 보너스 볼 일치", 30_000_000, 5),
+    FIRST("6개 일치", 2_000_000_000, 6),
     ;
 
     private String matchedMessage;
     private int prizeMoney;
     private int matchedCount;
-    private boolean isBonusMatched;
 
-    Prize(String matchedMessage, int prizeMoney, int matchedCount, boolean isBonusMatched) {
-        this.matchedMessage = matchedMessage;
-        this.prizeMoney = prizeMoney;
-        this.matchedCount = matchedCount;
-        this.isBonusMatched = isBonusMatched;
+    public String getMatchedMessage() {
+        return matchedMessage;
     }
 
     public int getPrizeMoney() {
         return this.prizeMoney;
     }
 
-    public Prize getPrizeOf(int matchedCount, boolean isBonusMatched) {
+    Prize(String matchedMessage, int prizeMoney, int matchedCount) {
+        this.matchedMessage = matchedMessage;
+        this.prizeMoney = prizeMoney;
+        this.matchedCount = matchedCount;
+    }
+
+    public static Prize getPrizeOf(int matchedCount, boolean isBonusMatched) {
         for (Prize prize : Prize.values()) {
             Optional<Prize> foundPrize = findPrize(matchedCount, isBonusMatched, prize);
             if (foundPrize.isPresent()) {
@@ -37,13 +39,13 @@ public enum Prize {
         return Prize.EMPTY;
     }
 
-    private Optional<Prize> findPrize(int matchedCount, boolean isBonusMatched, Prize prize) {
+    private static Optional<Prize> findPrize(int matchedCount, boolean isBonusMatched, Prize prize) {
         if (prize.matchedCount == matchedCount) {
             if (prize.equals(Prize.THIRD) && isBonusMatched) {
-                return Prize.SECOND;
+                return Optional.of(Prize.SECOND);
             }
-            return prize;
+            return Optional.of(prize);
         }
-        return ;
+        return Optional.empty();
     }
 }
