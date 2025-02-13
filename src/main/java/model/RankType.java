7cc620278e864c20c75dc46f927299d76147ee47
taek2222 <oot309@daum.net>
2025-02-13 11:07:13 +0900

    feat: 당첨 기준 설계 구현
    - 일치하는 볼 개수와 보너스 볼 일치 여부로 판단하는 메서드를 설계했습니다.

--- src/main/java/model/RankType.java
+++ src/main/java/model/RankType.java
@@ -1,47 +1,31 @@
 package model;
 
-import dto.LottoRankDetailResponse;
-
 import java.util.Arrays;
 
-public enum RankType {
-    FIRST(1, 6, false, 2000000000),
-    SECOND(2, 5, true, 30000000),
-    THIRD(3, 5, false, 1500000),
-    FOURTH(4, 4, false, 50000),
-    FIFTH(5, 3, false, 5000),
+public enum RankType { // 등수 5개, (볼 일치, 보너스 볼, 당첨 금액)
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
 
-    NONE(0, 0, false, 0);
+    NONE(0, false, 0);
 
-    private final int rank;
     private final int matchNumber;
     private final boolean isBonusNumber;
     private final int price;
 
-    RankType(final int rank, final int matchNumber, final boolean isBonusNumber, final int price) {
-        this.rank = rank;
+    RankType(int matchNumber, boolean isBonusNumber, int price) {
         this.matchNumber = matchNumber;
         this.isBonusNumber = isBonusNumber;
         this.price = price;
     }
 
-    public static RankType evaluateRank(final int matchNumber, final boolean isBonusNumber) {
+    public static RankType evaluateRank(int matchNumber, boolean isBonusNumber) {
         return Arrays.stream(values())
                 .filter(rankType -> rankType.matchNumber == matchNumber && rankType.isBonusNumber == isBonusNumber)
                 .findFirst()
                 .orElse(NONE);
-    }
-    
-    public LottoRankDetailResponse createResponse() {
-        return new LottoRankDetailResponse(
-                matchNumber,
-                isBonusNumber,
-                price
-        );
-    }
-
-    public int getRank() {
-        return rank;
     }
 
     public int getPrice() {
