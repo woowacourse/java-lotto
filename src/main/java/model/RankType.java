4ac2073e40ad56c4dce468bb0b6023f309c3bc87
taek2222 <oot309@daum.net>
2025-02-13 11:34:59 +0900

    feat: 당첨 기준 랭크 추가
    - 등수를 가리기 위해 랭크를 추가했습니다.

--- src/main/java/model/RankType.java
+++ src/main/java/model/RankType.java
@@ -2,20 +2,22 @@ package model;
 
 import java.util.Arrays;
 
-public enum RankType { // 등수 5개, (볼 일치, 보너스 볼, 당첨 금액)
-    FIRST(6, false, 2000000000),
-    SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000),
-    FOURTH(4, false, 50000),
-    FIFTH(3, false, 5000),
+public enum RankType {
+    FIRST(1, 6, false, 2000000000),
+    SECOND(2, 5, true, 30000000),
+    THIRD(3, 5, false, 1500000),
+    FOURTH(4, 4, false, 50000),
+    FIFTH(5, 3, false, 5000),
 
-    NONE(0, false, 0);
+    NONE(0, 0, false, 0);
 
+    private final int rank;
     private final int matchNumber;
     private final boolean isBonusNumber;
     private final int price;
 
-    RankType(int matchNumber, boolean isBonusNumber, int price) {
+    RankType(int rank, int matchNumber, boolean isBonusNumber, int price) {
+        this.rank = rank;
         this.matchNumber = matchNumber;
         this.isBonusNumber = isBonusNumber;
         this.price = price;
@@ -26,6 +28,10 @@ public enum RankType { // 등수 5개, (볼 일치, 보너스 볼, 당첨 금액
                 .filter(rankType -> rankType.matchNumber == matchNumber && rankType.isBonusNumber == isBonusNumber)
                 .findFirst()
                 .orElse(NONE);
+    }
+
+    public int getRank() {
+        return rank;
     }
 
     public int getPrice() {
