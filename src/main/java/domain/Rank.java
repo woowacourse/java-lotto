4b70dae3423e18decbe9036c7f544c8bffd9450e
ryan-dia <cjfdnjs158@naver.com>
2025-02-13 13:56:16 +0900

    feat: 로또 당첨 관련 도메인 구현
    Co-authored-by: kiwoook <tizmfns1218@Naver.com>

---
+++ src/main/java/domain/Rank.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5000),
+    NO_REWARD(0, false, 0);
+
+    private final int correctCount;
+    private final boolean hasBonusNumber;
+    private final long prize;
+
+    Rank(int correctCount, boolean hasBonusNumber, long prize) {
+        this.correctCount = correctCount;
+        this.hasBonusNumber = hasBonusNumber;
+        this.prize = prize;
+    }
+
+    public static Rank find(int correctCount, boolean hasBonusNumber) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.filter(correctCount, hasBonusNumber))
+                .findFirst()
+                .orElse(NO_REWARD);
+    }
+
+
+    private boolean filter(int correctCount, boolean hasBonusNumber) {
+        if (correctCount == 5) {
+            return correctCount == this.correctCount && hasBonusNumber == this.hasBonusNumber;
+        }
+
+        return correctCount == this.correctCount;
+    }
+
+    public long getPrize() {
+        return prize;
+    }
+}
---
+++ src/main/java/domain/WinnerLotto.java
@@ -0,0 +1,42 @@
+package domain;
+
+import static common.Constants.LOTTO_NUM_SIZE;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class WinnerLotto {
+    private final List<LottoNumber> winnerNumbers;
+    private final LottoNumber bonusNumber;
+
+    public WinnerLotto(final List<LottoNumber> winnerNumbers, final LottoNumber bonusNumber) {
+        this.winnerNumbers = winnerNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    /**
+     * // NOTICE 매개변수 사용에대한 의견 대립 List<LottoNumbers> - 필요한 것만 받을 수 있다. 즉 객체 전체를 넘겨주지 않아도 되기 때문에 Lotto 객체를 은닉화할 수 있다.
+     * Lotto lotto - 강력한 타입 제한, Lotto의 객체를 통해 로또의 List<LottoNumbers>의 제약을 걸 수 있음.
+     */
+    public long getMatchCount(Lotto lotto) {
+        return lotto.getLottoNumbers().stream().filter(winnerNumbers::contains).count();
+    }
+
+    public boolean hasBonus(Lotto lotto) {
+        return lotto.getLottoNumbers().contains(bonusNumber);
+    }
+
+    public static void validateLottoNumbers(List<LottoNumber> lottoNumbers) {
+        if (new HashSet<>(lottoNumbers).size() != LOTTO_NUM_SIZE) {
+            throw new IllegalStateException("중복은 불가능합니다.");
+        }
+    }
+
+    public static void validateBonusNumbers(List<LottoNumber> lottoNumbers, LottoNumber bonusNumber) {
+        if (lottoNumbers.contains(bonusNumber)) {
+            throw new IllegalStateException("보너스 넘버가 당첨 번호에 중복됩니다.");
+        }
+    }
+
+
+}
