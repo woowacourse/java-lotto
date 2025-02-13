aa96b8be86ab122093a9f693e8e1e227ca895047
Mint <audwl03071@gmail.com>
2025-02-13 14:54:15 +0900

    test(Lotto): 두 로또를 비교하여 일치하는 로또 번호 개수를 반환하는 테스트 작성

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -16,4 +16,17 @@ public class LottoTest {
         Assertions.assertThatCode(() -> new Lotto(numbers))
                 .doesNotThrowAnyException();
     }
+
+    @Test
+    void 두_로또를_비교하여_일치하는_로또_번호_개수를_반환한다() {
+        // Given
+        Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(Arrays.asList(7, 2, 3, 4, 5, 6));
+
+        // When
+        int count = winningLotto.countMatchingLottoNumber(lotto);
+
+        // Then
+        Assertions.assertThat(count).isEqualTo(5);
+    }
 }
