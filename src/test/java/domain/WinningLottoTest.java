373bc5b26cec23c6be0aa556e800a8a2ef441ea9
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-02-13 14:47:13 +0900

    test(domain) : 로또 결과 반환 기능 검증

--- src/test/java/domain/WinningLottoTest.java
+++ src/test/java/domain/WinningLottoTest.java
@@ -3,13 +3,17 @@ package domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import constant.WinningCount;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class WinningLottoTest {
 
@@ -35,5 +39,27 @@ class WinningLottoTest {
         assertThatThrownBy(()-> new WinningLotto(numbers,bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 번호 중복 오류");
+    }
+
+    @DisplayName("로또 결과 반환 검증")
+    @ParameterizedTest
+    @MethodSource
+    void lottoResultTest(List<Integer> issuedLotto, WinningCount expectedWinningCount){
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+        WinningCount lottoResult = winningLotto.getLottoResult(issuedLotto);
+        assertThat(lottoResult).isEqualTo(expectedWinningCount);
+    }
+
+    static Stream<Arguments> lottoResultTest(){
+        return Stream.of(
+                Arguments.of(List.of(11, 12, 13, 14, 15, 16), WinningCount.NONE),
+                Arguments.of(List.of(1, 12, 13, 14, 15, 16), WinningCount.NONE),
+                Arguments.of(List.of(1, 2, 13, 14, 15, 16), WinningCount.NONE),
+                Arguments.of(List.of(1, 2, 3, 14, 15, 16), WinningCount.THREE),
+                Arguments.of(List.of(1, 2, 3, 4, 15, 16), WinningCount.FOUR),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 16), WinningCount.FIVE),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), WinningCount.FIVE_BONUS),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), WinningCount.SIX)
+        );
     }
 }
