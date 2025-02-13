d5e4f06741bb377222a8ee9f45bbadb150f13713
CheChe903 <cheche903@naver.com>
2025-02-13 09:26:36 +0900

    feat(Lotto): 로또 사이즈 검증 구현
    로또의 사이즈가 6이 아니라면 예외를 발생하는 검증 로직을 구현합니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -11,6 +11,7 @@ public class Lotto {
     public Lotto(List<Integer> lottoNumbers) {
         this.lottoNumbers = lottoNumbers;
         validateNumberRange();
+        validateLottoSize();
         validateDuplicateNumber();
     }
 
@@ -27,6 +28,16 @@ public class Lotto {
         if(set.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
+    }
+
+    private void validateLottoSize() {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또의 사이즈가 일치하지 않습니다.");
+        }
+    }
+
+    public int getSize() {
+        return lottoNumbers.size();
     }
 
 }
