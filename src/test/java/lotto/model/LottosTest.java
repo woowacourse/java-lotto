75d5d105eadb14edcd8defd06bef60013d4f9c03
ykmxxi <ykmxxi97@gmail.com>
2025-02-14 15:15:23 +0900

    refactor: 사용하지 않는 import문 제거

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,10 +1,9 @@
 package lotto.model;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
