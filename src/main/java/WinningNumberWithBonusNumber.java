002a554215a551760f2068d3ca20e31073acb3ac
supernovaMK <minki5336@naver.com>
2025-02-15 00:17:21 +0900

    refactor(WinningNumberWithBonusNumber): 오류메시지 구체적으로 변경

--- src/main/java/WinningNumberWithBonusNumber.java
+++ src/main/java/WinningNumberWithBonusNumber.java
@@ -15,7 +15,7 @@ public record WinningNumberWithBonusNumber(
 
     private void validateDistinct(Lotto winningNumber, int bonusNumber) {
         if (winningNumber.getNumbers().contains(bonusNumber)) {
-            throw new IllegalArgumentException("Winning number " + winningNumber + " is already in use");
+            throw new IllegalArgumentException("당첨번호 " + winningNumber + "는 이미 존재하는 번호입니다. 보너스 번호와 당첨번호는 중복될 수 없습니다.");
         }
     }
 }
