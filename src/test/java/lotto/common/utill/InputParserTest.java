68551cd2acb9a9c465e2700f9c94a4ba7675c9e9
YuyoungRhee <bbclara@naver.com>
2025-02-14 14:27:25 +0900

    test(InputParser): 리스트 변환 테스트 추가, 테스트 일부 수정
    -parseToInt()뿐만 아니라 pareToList() 메서드의 테스트도 추가
    - testNullError라는 테스트 이름에 맞게 ""이 아닌 null 테스트로 수정

--- src/test/java/lotto/common/utill/InputParserTest.java
+++ src/test/java/lotto/common/utill/InputParserTest.java
@@ -4,8 +4,11 @@ import static lotto.common.utill.InputParser.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputParserTest {
 
@@ -25,9 +28,9 @@ class InputParserTest {
     }
 
     @DisplayName("빈칸를 포함할 때, 예외가 발생한다.")
-    @Test
-    void testBlankError() {
-        String str = " ";
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void testBlankError(String str) {
         assertThatThrownBy(() -> parseToInt(str))
             .isInstanceOf(IllegalArgumentException.class);
     }
@@ -35,8 +38,27 @@ class InputParserTest {
     @DisplayName("Null를 포함할 때, 예외가 발생한다.")
     @Test
     void testNullError() {
-        String str = "";
+        String str = null;
         assertThatThrownBy(() -> parseToInt(str))
             .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("문자열을 리스트로 정상적으로 변환한다.")
+    @Test
+    void testPaserToList() {
+        String str = "1,2,3,4,5,6";
+        List<Integer> list = parseToList(str);
+
+        assertThat(list).isNotNull();
+    }
+
+    @DisplayName("리스트로 변환할 때 잘못된 입력에 대해서 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ",
+            "1,2,3,4,,5,6", "1.2,3,4,5,6"})
+    void testParseToList_notValid(String str) {
+        assertThatThrownBy(() -> parseToList(str))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
