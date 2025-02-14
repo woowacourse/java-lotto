ee25c9d20be509e7ed3c0ba264353ed1b7a5adeb
wogus <ind07152@naver.com>
2025-02-14 20:24:53 +0900

    test(utils): RandomNumber mock 테스트 추가
    - 중복된 숫자 검증

---
+++ src/test/java/util/RandomNumberMockTest.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomNumberMockTest {
+
+    class TestLottoGenerator implements RandomGenerator {
+        private static final List<Integer> numberList = List.of(1, 1, 2, 3, 5, 5, 6, 4);
+        int index = 0;
+
+        @Override
+        public int generate() {
+            return numberList.get(index++);
+        }
+    }
+
+    @DisplayName("중복된 숫자는 피해서 생성된다.")
+    @Test
+    void duplicateMockTest() {
+        // when
+        List<Integer> randomNumbers = RandomNumberGenerator.getRandomNumbers(new TestLottoGenerator());
+        // then
+        Assertions.assertThat(randomNumbers).contains(1, 2, 3, 4, 5, 6);
+    }
+}
