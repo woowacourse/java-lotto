5944ba67bf6432b4fd0fa3f22adf5a3577a82db1
Han <jumdo12@gmail.com>
2025-02-15 02:29:06 +0900

    feat: 예외 handler 추가

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -13,17 +13,22 @@ public class LottoController {
     private static final LottoService lottoService = new LottoService();
 
     public static void run() {
-        int purchaseAmount = InputView.inputPurchaseAmount();
-        List<Lotto> lottos = lottoService.buyLottos(purchaseAmount);
-        OutputView.printLottos(lottos);
+        try{
+            int purchaseAmount = InputView.inputPurchaseAmount();
+            List<Lotto> lottos = lottoService.buyLottos(purchaseAmount);
+            OutputView.printLottos(lottos);
 
-        List<Integer> winningNumbers = InputView.inputWinningNumbers();
-        WinningLotto winningLotto = new WinningLotto(winningNumbers,InputView.inputBonusBall(winningNumbers));
+            List<Integer> winningNumbers = InputView.inputWinningNumbers();
+            WinningLotto winningLotto = new WinningLotto(winningNumbers,InputView.inputBonusBall(winningNumbers));
 
-        LottoStats lottoStats = new LottoStats();
-        lottoService.calcResult(lottoStats,lottos,winningLotto);
+            LottoStats lottoStats = new LottoStats();
+            lottoService.calcResult(lottoStats,lottos,winningLotto);
 
-        OutputView.printLottoStats(lottoStats);
-        OutputView.printEarningRate(lottoStats, purchaseAmount);
+            OutputView.printLottoStats(lottoStats);
+            OutputView.printEarningRate(lottoStats, purchaseAmount);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
