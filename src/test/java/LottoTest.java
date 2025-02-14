631d988afc668c6a3c9e310ec3193b90ecf4cad5
supernovaMK <minki5336@naver.com>
2025-02-14 23:35:15 +0900

    test(Lotto): 출력 로직 테스트 삭제
    - toString 로직이 모두 삭제되었기에, 출력 메서드 테스트 삭제
    - @DisplayName 대신 @DisplayNameGenration 사용으로 변경

--- src/test/java/LottoTest.java
+++ src/test/java/LottoTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,10 +10,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class LottoTest {
 
     @Test
-    @DisplayName("숫자6개를 갖는다")
     void 숫자6개를갖는다() {
         List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         List<Integer> invalidNumbers = Arrays.asList(1, 2, 3, 4, 5);
@@ -24,7 +26,6 @@ class LottoTest {
     }
 
     @Test
-    @DisplayName("로또 숫자는 6개인지 테스트")
     void 로또_숫자는_6개인지_테스트() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         assertThatThrownBy(() -> {
@@ -33,7 +34,6 @@ class LottoTest {
     }
 
     @Test
-    @DisplayName("로또 숫자 정렬 테스트")
     void 로또_숫자_정렬_테스트() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 6, 5);
         List<Integer> sortedNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
@@ -43,16 +43,6 @@ class LottoTest {
     }
 
     @Test
-    @DisplayName("로또 출력 테스트")
-    void 로또_출력_테스트() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 6, 5);
-
-        Lotto lotto = new Lotto(numbers);
-        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
-    }
-
-    @Test
-    @DisplayName("로또 번호 중복 검증")
     void 로또_번호_중복_검증() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5);
         assertThatThrownBy(() -> new Lotto(numbers)).isInstanceOf(IllegalArgumentException.class);
