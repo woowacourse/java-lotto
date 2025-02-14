d4b9dbe9d1ed0e7b4f2965a93c6384c59ba41f65
MinJun Choi <mj043000@naver.com>
2025-02-15 01:00:38 +0900

    refactor: 안 쓰는 코드 제거

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
-
-    public static final int UNIT_PRICE = 1_000;
     private final List<Lotto> lottos = new ArrayList<>();
 
     public void add(Lotto lotto) {
