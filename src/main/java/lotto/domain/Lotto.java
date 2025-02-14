2e1423b2dd39f89b8ecbbe6f206b53d5e70fb3f4
CheChe903 <cheche903@naver.com>
2025-02-14 15:11:28 +0900

    feat: 상수화 static 선언

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -11,9 +11,9 @@ import lotto.exception.LottoException;
 
 public class Lotto {
 
-    private final int MIN_NUMBER = 1;
-    private final int MAX_NUMBER = 45;
-    private final int LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
