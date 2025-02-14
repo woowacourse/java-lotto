3dffd19c89382e09eec024334784867c913f9b2e
yeonnhuu <yeonnhuu@gmail.com>
2025-02-14 10:55:03 +0900

    :recycle: refactor(OutputView): separate methods and update to use StringBuilder

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -12,27 +12,41 @@ public class OutputView {
     }
 
     public static void writeLottoTickets(List<List<Integer>> lottoTickets) {
-        System.out.println(String.format("%d개를 구매했습니다.", lottoTickets.size()));
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%d개를 구매했습니다.\n", lottoTickets.size()));
         for (List<Integer> lottoTicket : lottoTickets) {
-            System.out.println(lottoTicket);
+            sb.append(lottoTicket).append('\n');
         }
+        System.out.println(sb);
     }
 
     public static void writeLottoResult(LottoResult lottoResult) {
         StringBuilder sb = new StringBuilder();
+        writeLottoResultHeader(sb);
+        for (Map.Entry<LottoPrize, Integer>  entry: lottoResult.getLottoResult().entrySet()) {
+            writeLottoResultHit(sb, entry);
+        }
+        writeLottoResultProfitRate(sb, lottoResult.getLottoProfitRate());
+        System.out.println(sb);
+    }
+
+    private static void writeLottoResultHeader(StringBuilder sb) {
         sb.append("\n당첨 통계\n");
         sb.append("---------\n");
-        for (Map.Entry<LottoPrize, Integer>  entry: lottoResult.getLottoResult().entrySet()) {
-            LottoPrize lottoPrize = entry.getKey();
-            if (lottoPrize.equals(LottoPrize.MISS)) {
-                continue;
-            }
-            sb.append(String.format("%d개 일치 (%d원) - %d개\n", lottoPrize.getHitNumbers(), lottoPrize.getPrize(), entry.getValue()));
+    }
+
+    private static void writeLottoResultHit(StringBuilder sb, Map.Entry<LottoPrize, Integer>  entry) {
+        LottoPrize lottoPrize = entry.getKey();
+        if (lottoPrize.equals(LottoPrize.MISS)) {
+            return;
         }
-        sb.append(String.format("총 수익률은 %.2f입니다.", lottoResult.getLottoProfitRate()));
-        String profitStatus = formatProfitStatus(lottoResult.getLottoProfitRate());
+        sb.append(String.format("%d개 일치 (%d원) - %d개\n", lottoPrize.getHitNumbers(), lottoPrize.getPrize(), entry.getValue()));
+    }
+
+    private static void writeLottoResultProfitRate(StringBuilder sb, double profitRate) {
+        sb.append(String.format("총 수익률은 %.2f입니다.", profitRate));
+        String profitStatus = formatProfitStatus(profitRate);
         sb.append(String.format("(기준이 1이기 때문에 결과적으로 %s라는 의미임)", profitStatus));
-        System.out.println(sb);
     }
 
     private static String formatProfitStatus(double profitRate) {
