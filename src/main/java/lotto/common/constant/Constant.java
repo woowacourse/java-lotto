c97e60acb0ac99efdcb4bbe36c3b012b58cd24da
YuyoungRhee <bbclara@naver.com>
2025-02-14 14:44:08 +0900

    refactor(Constant): 상수 이름 네이밍 변경
    - 로또의 최소범위, 최대범위라는 뜻의 네이밍을 더 직관적으로 하고자 RANGE라는 단어를 추가

--- src/main/java/lotto/common/constant/Constant.java
+++ src/main/java/lotto/common/constant/Constant.java
@@ -1,7 +1,7 @@
 package lotto.common.constant;
 
 public class Constant {
-    public static final int LOTTO_MINIMUM = 1;
-    public static final int LOTTO_MAXIMUM = 45;
+    public static final int LOTTO_RANGE_MINIMUM = 1;
+    public static final int LOTTO_RANGE_MAXIMUM = 45;
     public static final int LOTTO_SIZE = 6;
 }
