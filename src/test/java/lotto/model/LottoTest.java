ab5f5e98b307d4c53ec510d98ad9e019b346871b
Yehyeok Bang <qkddpgur318@gmail.com>
2025-02-14 18:06:29 +0900

    refactor: 테스트 메서드명 및 표시 이름 수정

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -16,22 +16,42 @@ class LottoTest {
 
     @DisplayName("중복 없는 6개의 숫자로 로또를 생성할 수 있다.")
     @Test
-    void ok() {
+    void createTest() {
         Set<Integer> numbers = Set.of(1, 2, 3, 4, 5, 6);
 
         assertDoesNotThrow(() -> new Lotto(numbers));
     }
 
+    @DisplayName("로또가 특정 번호를 가지고 있는지 확인할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void containsTest(int checkNumber) {
+        Set<Integer> numbers = Set.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = LottoFixtures.createByNumbers(numbers);
+
+        assertTrue(lotto.contains(checkNumber));
+    }
+
+    @DisplayName("로또를 서로 비교하여 일치하는 숫자의 개수를 확인할 수 있다.")
+    @Test
+    void getMatchCountTest() {
+        Set<Integer> numbers = Set.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto1 = LottoFixtures.createByNumbers(numbers);
+        Lotto lotto2 = LottoFixtures.createByNumbers(numbers);
+
+        assertEquals(6, lotto1.getMatchCount(lotto2));
+    }
+
     @DisplayName("로또 숫자가 6개가 아닌 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {5, 7})
-    void shouldThrowException_WhenNumberCountIsNot6(int count) {
-        Set<Integer> numbers = createNumbers(count);
+    void shouldThrowException_WhenNumberCountIsNot6(int countOfNumbers) {
+        Set<Integer> numbers = createUniqueNumbersByCount(countOfNumbers);
 
         assertThrows(IllegalArgumentException.class, () -> new Lotto(numbers));
     }
 
-    private Set<Integer> createNumbers(int count) {
+    private Set<Integer> createUniqueNumbersByCount(int count) {
         Set<Integer> numbers = new HashSet<>();
         for (int i = 1; i <= count; i++) {
             numbers.add(i);
@@ -39,34 +59,12 @@ class LottoTest {
         return numbers;
     }
 
-    @DisplayName("로또 숫자의 범위가 올바르지 않은 경우 예외가 발생한다.")
-    @Test
-    void shouldThrowException_WhenNumberNotInRange() {
-        Set<Integer> numbers = Set.of(1, 2, 3, 4, 5, 46);
-
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> new Lotto(numbers)
-        );
-    }
-
-    @DisplayName("특정 번호를 가지고 있는지 확인할 수 있다.")
+    @DisplayName("로또 숫자 범위를 벗어나는 숫자가 있는 경우 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
-    void containsTest(int number) {
-        Set<Integer> numbers = Set.of(1, 2, 3, 4, 5, 6);
-        Lotto lotto = new Lotto(numbers);
+    @ValueSource(ints = {0, 46})
+    void shouldThrowException_WhenNumberNotInRange(int number) {
+        Set<Integer> numbers = Set.of(number, 1, 2, 3, 4, 5);
 
-        assertTrue(lotto.contains(number));
-    }
-
-    @DisplayName("로또를 비교할 수 있다.")
-    @Test
-    void matchTest() {
-        Set<Integer> numbers = Set.of(1, 2, 3, 4, 5, 6);
-        Lotto lotto1 = new Lotto(numbers);
-        Lotto lotto2 = new Lotto(numbers);
-
-        assertEquals(6, lotto1.getMatchCount(lotto2));
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(numbers));
     }
 }
