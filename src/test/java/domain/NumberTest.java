d6ccf3e7e97d0e249303754a3fb7135684b06045
jhpark1227 <junhyeok0246@naver.com>
2025-02-14 22:32:22 +0900

    test: 불필요한 테스트 케이스 삭제

--- src/test/java/domain/NumberTest.java
+++ src/test/java/domain/NumberTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class NumberTest {
 
     @ParameterizedTest
-    @CsvSource({"-1", "0", "46", "47"})
+    @CsvSource({"0", "46"})
     void Number의_값이_1부터_45_사이의_수가_아니라면_예외를_반환한다(int value) {
         assertThatThrownBy(() -> new Number(value))
                 .isInstanceOf(IllegalArgumentException.class)
