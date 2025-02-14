642d32e9aded6fe219e9e977e743bc67dbd16ce2
minSsan <msp1125@hanyang.ac.kr>
2025-02-14 16:05:45 +0900

    test(LottoTest): 추가된 메소드에 대한 테스트 코드 추가

--- src/test/java/LottoTest.java
+++ src/test/java/LottoTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoTest {
     @DisplayName("로또 번호는 6개만 가능")
@@ -48,4 +50,101 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호의 존재 여부 파악 가능")
+    @ParameterizedTest
+    @CsvSource(value = {"1, true", "2, true", "3, true", "4, true", "5, true", "6, true", "7, false"})
+    void discoverIsExistNumberOrNot(int number, boolean expected) {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Number compare = new Number(number);
+
+        // when
+        final boolean result = lotto.isExist(compare);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("1등 결과를 올바르게 반환할 수 있다.")
+    @Test
+    void returnFirstPrize() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Number bonusNumber = new Number(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        // when
+        LottoPrize lottoPrize = lotto.getLottoPrize(winningLotto);
+
+        // then
+        assertThat(lottoPrize).isSameAs(LottoPrize.FIRST);
+    }
+
+    @DisplayName("2등 결과를 올바르게 반환할 수 있다.")
+    @Test
+    void returnSecondPrize() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Number bonusNumber = new Number(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        // when
+        LottoPrize lottoPrize = lotto.getLottoPrize(winningLotto);
+
+        // then
+        assertThat(lottoPrize).isSameAs(LottoPrize.SECOND);
+    }
+
+    @DisplayName("3등 결과를 올바르게 반환할 수 있다.")
+    @Test
+    void returnThirdPrize() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Number bonusNumber = new Number(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        // when
+        LottoPrize lottoPrize = lotto.getLottoPrize(winningLotto);
+
+        // then
+        assertThat(lottoPrize).isSameAs(LottoPrize.THIRD);
+    }
+
+    @DisplayName("4등 결과를 올바르게 반환할 수 있다.")
+    @Test
+    void returnFourthPrize() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 8, 9));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Number bonusNumber = new Number(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        // when
+        LottoPrize lottoPrize = lotto.getLottoPrize(winningLotto);
+
+        // then
+        assertThat(lottoPrize).isSameAs(LottoPrize.FOURTH);
+    }
+
+    @DisplayName("5등 결과를 올바르게 반환할 수 있다.")
+    @Test
+    void returnFifthPrize() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Number bonusNumber = new Number(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        // when
+        LottoPrize lottoPrize = lotto.getLottoPrize(winningLotto);
+
+        // then
+        assertThat(lottoPrize).isSameAs(LottoPrize.FIFTH);
+    }
+
+    // TODO: 리뷰 내용 반영 - 당첨 번호에 보너스 볼이 포함되어 있는 경우?
 }
