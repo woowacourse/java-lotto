18e354a97b9b829bee18c98ac9d01d29faccd6d2
jhpark1227 <junhyeok0246@naver.com>
2025-02-14 17:11:50 +0900

    refactor: Lottos 메서드 분리

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -20,12 +20,18 @@ public class Lottos {
     public WinningResult calculateWinning(WinningLotto winningLotto) {
         Map<Rank, Integer> winningResult = new HashMap<>();
         for (Lotto purchaseLotto : lottos) {
-            int matchCount = winningLotto.calculateMatchCount(purchaseLotto);
-            boolean isMatchBonusNumber = winningLotto.containsBonusNumber(purchaseLotto);
-            Rank rank = Rank.findRank(matchCount, isMatchBonusNumber);
+            Rank rank = calculateRank(winningLotto, purchaseLotto);
             winningResult.put(rank, winningResult.getOrDefault(rank, 0) + 1);
         }
         Money purchaseLottoMoney = LottoStore.LOTTO_PRICE.multiply(lottos.size());
         return new WinningResult(purchaseLottoMoney, winningResult);
     }
+
+    private Rank calculateRank(WinningLotto winningLotto, Lotto purchaseLotto) {
+        int matchCount = winningLotto.calculateMatchCount(purchaseLotto);
+        boolean isMatchBonusNumber = winningLotto.containsBonusNumber(purchaseLotto);
+        return Rank.findRank(matchCount, isMatchBonusNumber);
+    }
+
+
 }
