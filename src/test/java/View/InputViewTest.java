fdc96d5b25d40d84fc5da41b9106456aa8d3f04f
jhan0121 <jhan0121@gmail.com>
2025-02-14 16:09:34 +0900

    refactor: InputViewTest 중복 테스트 로직 개선
    중복 로직에 ParameterizedTest를 적용

--- src/test/java/View/InputViewTest.java
+++ src/test/java/View/InputViewTest.java
@@ -1,86 +1,60 @@
 package View;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
     @Test
-    void 로또_구매_입력_테스트(){
+    void 로또_구매_입력_테스트() {
         String input = "14000";
         Assertions.assertThat(InputView.inputPrice(input)).isEqualTo(14000);
     }
 
-    @Test
-    void 로또_구매_입력_오류_테스트(){
-        String inputEmpty = "";
-        String inputNull = null;
-        Assertions.assertThatThrownBy(() -> InputView.inputPrice(inputEmpty))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        Assertions.assertThatThrownBy(() -> InputView.inputPrice(inputNull))
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 로또_구매_입력_오류_테스트(String input) {
+        Assertions.assertThatThrownBy(() -> InputView.inputPrice(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 당첨번호_입력_테스트(){
+    void 당첨번호_입력_테스트() {
         String inputWinnerNumber = "1,2,3,4,5,6";
-        List<Integer> result = List.of(1,2,3,4,5,6);
+        List<Integer> result = List.of(1, 2, 3, 4, 5, 6);
         org.junit.jupiter.api.Assertions
                 .assertEquals(result, InputView.inputWinnerNumbers(inputWinnerNumber));
     }
 
-    @Test
-    void 당첨번호_입력_오류_테스트(){
-        String inputEmpty = "";
-        String inputNull = null;
-        String wrongInputCount = "1,2,3,4,5,6,7";
-        String wrongInputRange = "102,1,2,3,4,5";
-        String wrongDelimiter = "1;2,3,4,5,6";
-        Assertions.assertThatThrownBy(() -> InputView.inputPrice(inputEmpty))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        Assertions.assertThatThrownBy(() -> InputView.inputPrice(inputNull))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        Assertions.assertThatThrownBy(() -> InputView.inputPrice(wrongInputCount))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        Assertions.assertThatThrownBy(() -> InputView.inputPrice(wrongInputRange))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        Assertions.assertThatThrownBy(() -> InputView.inputPrice(wrongDelimiter))
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"1,2,3,4,5,6,7", "102,1,2,3,4,5", "1;2,3,4,5,6"})
+    void 당첨번호_입력_오류_테스트(String input) {
+        Assertions.assertThatThrownBy(() -> InputView.inputPrice(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 보너스_볼_입력_테스트(){
+    void 보너스_볼_입력_테스트() {
         String inputWinnerNumber = "8";
-        List<Integer> result = List.of(1,2,3,4,5,6);
+        List<Integer> result = List.of(1, 2, 3, 4, 5, 6);
 
         Assertions.assertThat(InputView.inputBonusBall(inputWinnerNumber, result)).
                 isEqualTo(8);
     }
 
-    @Test
-    void 보너스_볼_입력_오류_테스트(){
-        String duplicateNumber = "6";
-        String wrongRangeNumber = "102";
-        List<Integer> result = List.of(1,2,3,4,5,6);
-
-        Assertions.assertThatThrownBy(() -> InputView.inputBonusBall(duplicateNumber, result))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        Assertions.assertThatThrownBy(() -> InputView.inputBonusBall(wrongRangeNumber, result))
+    @ParameterizedTest
+    @ValueSource(strings = {"6", "102"})
+    void 보너스_볼_입력_오류_테스트(String input) {
+        List<Integer> result = List.of(1, 2, 3, 4, 5, 6);
+        Assertions.assertThatThrownBy(() -> InputView.inputBonusBall(input, result))
                 .isInstanceOf(IllegalArgumentException.class);
 
 
     }
-
-
-
 
 
 }
