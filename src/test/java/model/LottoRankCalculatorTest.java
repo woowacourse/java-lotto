e7087a6c1fc26aae3ed33d059a98c3581082931f
Jin409 <sualng123@gmail.com>
2025-02-14 15:46:26 +0900

    test : LottoRankCalculator 가 적절한 순위를 반환하는지 검증하는 테스트 작성

---
+++ src/test/java/model/LottoRankCalculatorTest.java
@@ -0,0 +1,43 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LottoRankCalculatorTest {
+
+    private LottoRankCalculator lottoRankCalculator;
+
+    @BeforeEach
+    void setUp() {
+        lottoRankCalculator = new LottoRankCalculator();
+    }
+
+    @Test
+    void 조건에_알맞은_순위를_반환한다() {
+        // given
+        LottoTicket lottoTicket = new LottoTicket(List.of(1, 2, 3, 4, 5, 6));
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        // when
+        LottoRank resultRank = lottoRankCalculator.calculate(lottoTicket, winningLotto);
+
+        // then
+        assertThat(resultRank).isEqualTo(LottoRank.FIRST);
+    }
+
+    @Test
+    void 보너스_번호에_대한_확인이_요구되는_경우_알맞은_순위를_반환한다() {
+        // given
+        LottoTicket lottoTicket = new LottoTicket(List.of(1, 2, 3, 4, 5, 8));
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        // when
+        LottoRank resultRank = lottoRankCalculator.calculate(lottoTicket, winningLotto);
+
+        // then
+        assertThat(resultRank).isEqualTo(LottoRank.THIRD);
+    }
+}
