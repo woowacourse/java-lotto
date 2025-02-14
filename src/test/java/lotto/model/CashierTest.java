5cfbefbdc3d85809a8da6eab2446f94dbbde8cfa
rladmstn <oz33177351@gmail.com>
2025-02-14 13:59:15 +0900

    test: 판매원 테스트 라이브러리 변경

--- src/test/java/lotto/model/CashierTest.java
+++ src/test/java/lotto/model/CashierTest.java
@@ -1,22 +1,26 @@
 package lotto.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CashierTest {
 
     @DisplayName("구입 금액 단위가 일치할 경우, 로또가 정상 발급된다.")
-    @Test
-    void ok() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1_000, 1",
+            "10_000, 10"
+    }, delimiter = ',')
+    void ok(int amount, int count) {
         Cashier cashier = new Cashier();
-        List<Lotto> lottos = cashier.payForLotto(1_000);
-        assertEquals(1, lottos.size());
+        List<Lotto> lottos = cashier.payForLotto(amount);
+
+        assertThat(lottos.size()).isEqualTo(count);
     }
 
     @DisplayName("구입 금액 단위가 일치하지 않을 경우, 예외가 발생한다.")
@@ -24,6 +28,8 @@ class CashierTest {
     @ValueSource(ints = {999, 1_001})
     void shouldThrowException_WhenInvalidUnit(int invalidAmount) {
         Cashier cashier = new Cashier();
-        assertThrows(IllegalArgumentException.class, () -> cashier.payForLotto(invalidAmount));
+
+        assertThatThrownBy(() -> cashier.payForLotto(invalidAmount))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
