c7af7bdc9b3dfe62c22d2d4db087c87c304ab05a
MinJun Choi <mj043000@naver.com>
2025-02-14 15:47:27 +0900

    fix: 랜덤 번호 생성하는 random 객체를 static 인스턴스 변수로 수정

--- src/main/java/lotto/model/RandomNumberGenerator.java
+++ src/main/java/lotto/model/RandomNumberGenerator.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.Random;
 
 public class RandomNumberGenerator {
+    private static final Random random = new Random();
+
     private RandomNumberGenerator() {
     }
 
     public static List<Integer> generate() {
-        Random random = new Random();
         ArrayList<Integer> randomNumbers = new ArrayList<>();
         for (int count = 0; count < Lotto.LOTTO_SIZE; count++) {
             randomNumbers.add(random.nextInt(Lotto.MIN_LOTTO_NUMBER, Lotto.MAX_LOTTO_NUMBER));
