62194fb797f6b795633e6eb7852d0bfa5990a910
sonjh919 <kingjh1125@gmail.com>
2025-02-14 19:31:00 +0900

    fix: 보너스 번호 일치 여부에 따라 랭크가 정상적으로 반환되지 않던 오류 수정

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,31 +1,33 @@
 package domain;
 
-public enum Rank {
-    FIRST(6, 2_000_000_000, false),
-    SECOND(5, 30_000_000, true),
-    THIRD(5, 1_500_000, false),
-    FOURTH(4, 50_000, false),
-    FIFTH(3, 5_000, false),
+import java.util.Arrays;
 
-    NONE(0, 0, false);
+public enum Rank {
+    NONE(0, 0),
+
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
 
     private int count;
     private int prize;
-    private boolean isMatchBonus;
 
-    Rank(int count, int prize, boolean isMatchBonus) {
+    Rank(int count, int prize) {
         this.count = count;
         this.prize = prize;
-        this.isMatchBonus = isMatchBonus;
     }
 
-    public static Rank matchRank(int matchCount, boolean isMatchBonus) {
-        for (Rank rank : Rank.values()) {
-            if (rank.count == matchCount && rank.isMatchBonus == isMatchBonus) {
-                return rank;
-            }
+    public static Rank matchRank(int count, boolean isMatchBonus) {
+        if (count == SECOND.count && isMatchBonus) {
+            return SECOND;
         }
-        return NONE;
+
+        return Arrays.stream(values())
+                .filter(rating -> rating.count == count)
+                .findFirst()
+                .orElse(NONE);
     }
 
     public static long calculateTotalPrize(Rank rank, Integer count) {
