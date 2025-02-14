994713ff28e47df4711db157a054bde1ec178389
wogus <ind07152@naver.com>
2025-02-14 20:22:18 +0900

    refactor(InputConverter): delimiter 수정
    "1, 2, 3, 4, 5, 6" 입력 방식만을 허락한 부분을 공백은 유연하게 허용하도록 수정

--- src/main/java/util/InputConverter.java
+++ src/main/java/util/InputConverter.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputConverter {
-    private static final String DELIMITER = ", ";
+    private static final String DELIMITER = ",";
 
     public static Integer convertNumber(String input) {
         Validator.validateNumberFormat(input.trim());
@@ -18,11 +18,13 @@ public class InputConverter {
     public static List<Integer> convertWinningLotto(String originalInput) {
         String input = originalInput.trim();
         Validator.validateWinningLottoPattern(originalInput);
-        return Arrays.stream(input.split(DELIMITER)).map(Integer::parseInt).toList();
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .map(Integer::parseInt).toList();
     }
 
     static private class Validator {
-        private static final String INPUT_FORMAT_PATTERN = "^\\d+(, \\d+){5}$";
+        private static final String INPUT_FORMAT_PATTERN = "^\\d+(\\s*,\\s*\\d+){5}$";
         private static final String NUMBER_TYPE_PATTERN = "^\\d+$";
 
         private static void validateWinningLottoPattern(String originalInput) {
