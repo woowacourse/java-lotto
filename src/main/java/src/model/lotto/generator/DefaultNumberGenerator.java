5878f2096c1212e68bb161f4d910b28002d9f10e
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-14 13:53:11 +0900

    refactor: 랜덤 번호 생성 코드 IntStream 적용
    - 가독성을 위함.

--- src/main/java/src/model/lotto/generator/DefaultNumberGenerator.java
+++ src/main/java/src/model/lotto/generator/DefaultNumberGenerator.java
@@ -3,17 +3,16 @@ package src.model.lotto.generator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class DefaultNumberGenerator implements NumberGenerator {
 
     @Override
     public List<Integer> generate(int min, int max, int size) {
         List<Integer> numbers = new ArrayList<>(max - min + 1);
-        for (int i = min; i <= max; i++) {
-            numbers.add(i);
-        }
-
+        IntStream.rangeClosed(min, max).forEach(numbers::add);
         Collections.shuffle(numbers);
+
         return numbers.subList(0, size);
     }
 }
