01b4f7ae914cee74a5e2894fdf7f97000681ebc6
YuyoungRhee <bbclara@naver.com>
2025-02-15 03:49:03 +0900

    test(MatchRankTest): @ParameterizedTest를 이용한 테스트로 변경

--- src/test/java/lotto/domain/MatchRankTest.java
+++ src/test/java/lotto/domain/MatchRankTest.java
@@ -1,18 +1,30 @@
 package lotto.domain;
 
-import static lotto.domain.MatchRank.*;
+import static lotto.domain.MatchRank.getMatchRank;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MatchRankTest {
 
-    @Test
     @DisplayName("getMatchStatistics 정상 작동 테스트")
-    void testGetMatchStatistics() {
-        assertThat(getMatchRank(3, false)).isEqualTo(MATCH_THREE);
-        assertThat(getMatchRank(5, false)).isEqualTo(MATCH_FIVE);
-        assertThat(getMatchRank(5, true)).isEqualTo(MATCH_BONUS);
+    @ParameterizedTest
+    @CsvSource({
+            "0, false, NO_MATCH",
+            "1, false, NO_MATCH",
+            "2, false, NO_MATCH",
+            "3, false, MATCH_THREE",
+            "4, false, MATCH_FOUR",
+            "5, false, MATCH_FIVE",
+            "5, true, MATCH_BONUS",
+            "6, false, MATCH_SIX",
+    })
+    void testGetMatchStatistics(int matchCount, boolean isBonusMatched, String expectedRankStr) {
+        MatchRank expectedRank = MatchRank.valueOf(expectedRankStr);
+
+        MatchRank matchRank = getMatchRank(matchCount, isBonusMatched);
+        assertThat(matchRank).isEqualTo(expectedRank);
     }
 }
