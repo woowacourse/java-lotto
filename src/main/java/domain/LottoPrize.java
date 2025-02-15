74d89055e46efbf4fed91452fb5101ac829188ee
threepebbles <threepebbles@naver.com>
2025-02-16 07:30:14 +0900

    feat(LottoPrize): 사용하지 않는 인스턴스(rank) 제거

--- src/main/java/domain/LottoPrize.java
+++ src/main/java/domain/LottoPrize.java
@@ -4,23 +4,21 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public enum LottoPrize {
-    FIRST(2000000000, 6, 0, 1),
-    SECOND(30000000, 5, 1, 2),
-    THIRD(1500000, 5, 0, 3),
-    FOURTH(50000, 4, 0, 4),
-    FIFTH(5000, 3, 0, 5),
-    NOTHING(0, 0, 0, Integer.MAX_VALUE);
+    FIRST(2000000000, 6, 0),
+    SECOND(30000000, 5, 1),
+    THIRD(1500000, 5, 0),
+    FOURTH(50000, 4, 0),
+    FIFTH(5000, 3, 0),
+    NOTHING(0, 0, 0);
 
     private final int money;
     private final int countMatched;
     private final int countBonusNumberMatched;
-    private final int rank;
 
-    LottoPrize(int money, int countMatched, int countBonusNumberMatched, int rank) {
+    LottoPrize(int money, int countMatched, int countBonusNumberMatched) {
         this.money = money;
         this.countMatched = countMatched;
         this.countBonusNumberMatched = countBonusNumberMatched;
-        this.rank = rank;
     }
 
     public static LottoPrize getLottoPrize(int countMatched, boolean isBonusNumberMatched) {
