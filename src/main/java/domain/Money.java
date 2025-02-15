0b6ebd58c3ff328d83d8e6194c3e24b96d98ed18
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-15 19:54:30 +0900

    fix(Money): 배수 검증 수정

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -21,9 +21,10 @@ public class Money {
     }
 
     private static void validateMultipleOfLottoPrice(int price) {
-        if (price < LottoRule.LOTTO_PRICE.getValue()) {
-            throw new IllegalArgumentException("구입 금액은 로또 가격 단위(" + LottoRule.LOTTO_PRICE.getValue() + ")의 배수여야 합니다.");
+        if (price % LottoRule.LOTTO_PRICE.getValue() == 0) {
+            return;
         }
+        throw new IllegalArgumentException("구입 금액은 로또 가격 단위(" + LottoRule.LOTTO_PRICE.getValue() + ")의 배수여야 합니다.");
     }
 
     private static void validateZeroAmount(int amount) {
