78edb10076e2de273b160f52787e898126d9dcc2
Hacanna42 <dtmdgus901@gmail.com>
2025-02-15 20:02:08 +0900

    refactor: 불필요한 validator 삭제
    DTO에 Validator가 있는 것은 부적절하다고 판단, 삭제했습니다.

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -3,14 +3,13 @@ package domain;
 import domain.numbergenerator.NumberGenerator;
 import dto.OutputLottosDto;
 import java.util.List;
-import validator.LottoValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers.stream().sorted().toList();
-        LottoValidator.validate(this.numbers);
+        validate(this.numbers);
     }
 
     public static Lotto from(NumberGenerator numberGenerator) {
@@ -43,5 +42,38 @@ public class Lotto {
             return 1;
         }
         return 0;
+    }
+
+    public void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
+        }
+        if (hasDistinctNumber(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+        if (numbers.stream().anyMatch(number -> !isValidNumber(number))) {
+            throw new IllegalArgumentException("1~45 범위 이내여야 합니다.");
+        }
+        if (!isSorted(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 정렬되어야 합니다.");
+        }
+    }
+
+    private boolean isSorted(List<Integer> numbers) {
+        List<Integer> sortedNumbers = numbers.stream().sorted().toList();
+        return sortedNumbers.equals(numbers);
+    }
+
+    private boolean isValidNumber(int number) {
+        return number > 0 && number <= 45;
+    }
+
+    private boolean hasDistinctNumber(List<Integer> numbers) {
+        int distinctedSize = numbers.stream()
+                .distinct()
+                .toList()
+                .size();
+
+        return numbers.size() != distinctedSize;
     }
 }
