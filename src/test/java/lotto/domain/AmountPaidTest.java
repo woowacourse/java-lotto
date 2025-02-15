802c3b9de0630ef91686d310caab31f3703e065c
AppleMint98 <applemint9821@gmail.com>
2025-02-15 20:12:41 +0900

    test(AmountPaidTest) : 로또 구입 수량 계산 및 입력값 0의 경우의 유효성 검증

--- src/test/java/lotto/domain/AmountPaidTest.java
+++ src/test/java/lotto/domain/AmountPaidTest.java
@@ -12,8 +12,7 @@ class AmountPaidTest {
     @Test
     void 구매_금액은_1000으로_나누어_떨어지지_않으면_예외를_발생한다() {
 
-        assertThatThrownBy(() -> new AmountPaid(15500))
-                .isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> new AmountPaid(15500)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 금액은 1000으로 나누어 떨어져야 합니다.");
     }
 
@@ -21,16 +20,33 @@ class AmountPaidTest {
     @Test
     void 구매_금액은_음수면_예외를_발생한다() {
 
-        assertThatThrownBy(() -> new AmountPaid(-15000))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 음수는 입력할 수 없습니다.");
+        assertThatThrownBy(() -> new AmountPaid(-15000)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 양수만 입력 해 주세요.");
+    }
+
+    @DisplayName("구매 금액이 0일 경우 예외를 발생한다.")
+    @Test
+    void 구매_금액이_0일_경우_예외를_발생한다() {
+
+        assertThatThrownBy(() -> new AmountPaid(0)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 양수만 입력 해 주세요.");
     }
 
     @DisplayName("이익률을 계산한다")
     @Test
     void 이익률을_계산한다() {
+
         AmountPaid amountPaid = new AmountPaid(14000);
 
         assertThat(amountPaid.calculateProfitRate(5000)).isEqualTo("0.35");
+    }
+
+    @DisplayName("로또 구입 수량을 계산한다.")
+    @Test
+    void 로또_구입_수량을_계산한다() {
+
+        AmountPaid amountPaid = new AmountPaid(14000);
+
+        assertThat(amountPaid.getLottoQuantity()).isEqualTo(14);
     }
 }
