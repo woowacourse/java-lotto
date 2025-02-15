7f41970126ca13409cebe6cc12ebca2968f7ef3b
julie <7jujubuilds@gmail.com>
2025-02-15 11:17:15 +0900

    refactor(output): 출력 서비스 삭제
    작업 내용
    - 출력 뷰를 제어하는 `OutputService`를 삭제하고, 해당 서비스의 로직을 뷰로 이동

--- src/main/java/lotto/config/ApplicationConfiguration.java
+++ src/main/java/lotto/config/ApplicationConfiguration.java
@@ -1,11 +1,11 @@
 package lotto.config;
 
-import java.util.Scanner;
 import lotto.service.InputService;
 import lotto.service.LottoService;
-import lotto.service.OutputService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.Scanner;
 
 public class ApplicationConfiguration {
 
@@ -13,15 +13,14 @@ public class ApplicationConfiguration {
     private final InputView inputView = new InputView(scanner);
     private final OutputView outputView = new OutputView();
     private final InputService inputService = new InputService(inputView, outputView);
-    private final OutputService outputService = new OutputService(outputView);
     private final LottoService lottoService = new LottoService();
 
     public InputService getInputService() {
         return inputService;
     }
 
-    public OutputService getOutputService() {
-        return outputService;
+    public OutputView getOutputView() {
+        return outputView;
     }
 
     public LottoService getLottoService() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,62 @@
 package lotto.view;
 
+import lotto.constant.OutputMessage;
+import lotto.constant.WinningTier;
+import lotto.domain.Lotto;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
+
 public class OutputView {
+
+    private void printWinningStatistics(List<WinningTier> winningTiers) {
+        List<WinningTier> tiers = Arrays.stream(WinningTier.values())
+                .toList()
+                .reversed();
+
+        for (WinningTier tier : tiers) {
+            printTier(winningTiers, tier);
+        }
+    }
+
+    private void printTier(List<WinningTier> targets, WinningTier tier) {
+        if (tier == WinningTier.EMPTY) {
+            return;
+        }
+
+        long count = targets.stream()
+                .filter(winningTier -> winningTier == tier)
+                .count();
+
+        if (tier.getHasBonusMatch()) {
+            printBonusTier(tier, count);
+            return;
+        }
+        printRegularTier(tier, count);
+    }
+
+    private void printBonusTier(WinningTier tier, long count) {
+        String template = OutputMessage.BONUS_TIER.getContent();
+        String content = String.format(template, tier.getMatches(), tier.getPrize(), count);
+        System.out.println(content);
+    }
+
+    private void printRegularTier(WinningTier tier, long count) {
+        String template = OutputMessage.REGULAR_TIER.getContent();
+        String content = String.format(template, tier.getMatches(), tier.getPrize(), count);
+        System.out.println(content);
+    }
+
+    private void printProfit(double profit) {
+        DecimalFormat formatter = new DecimalFormat("0.00");
+        String formattedProfit = formatter.format(profit);
+        String content = String.format(OutputMessage.PROFIT.getContent(), formattedProfit);
+        if (profit < 1) {
+            content += OutputMessage.LOSS_DESCRIPTION.getContent();
+        }
+        System.out.println(content);
+    }
 
     public void printLine(String content) {
         System.out.println(content);
@@ -10,4 +66,20 @@ public class OutputView {
         System.out.println();
     }
 
+    public void printLottos(List<Lotto> lottos) {
+        String content = String.format(OutputMessage.PURCHASE_COMPLETE.getContent(), lottos.size());
+        System.out.println(content);
+        for (Lotto lotto : lottos) {
+            String lottoNumbers = lotto.getNumbers().toString();
+            System.out.println(lottoNumbers);
+        }
+        this.printBlankLine();
+    }
+
+    public void printResults(List<WinningTier> winningTiers, double profit) {
+        System.out.println(OutputMessage.RESULTS_HEADER.getContent());
+        this.printWinningStatistics(winningTiers);
+        this.printProfit(profit);
+        this.printBlankLine();
+    }
 }
