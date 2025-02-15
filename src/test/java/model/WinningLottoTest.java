57844b40829976d62e9d5efa674e300bcbce5655
egaeng09 <egaeng09@gmail.com>
2025-02-15 20:30:35 +0900

    test: WinningLotto 클래스에 대한 테스트

---
+++ src/test/java/model/WinningLottoTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @Test
+    void 지난주_당첨_번호가_생성된다() {
+        final String numbers = "1, 2, 3, 4, 5, 6";
+        final String bonusNumber = "7";
+
+        assertThatNoException()
+                .isThrownBy(() -> new WinningLotto(numbers, bonusNumber));
+    }
+}
