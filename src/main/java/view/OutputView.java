795210ee8242252913521e06fde165ec642ad493
soeun2537 <soeun2537@gmail.com>
2025-02-15 14:34:02 +0900

    refactor: OutputView 메서드 리팩토링

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,15 @@
 package view;
 
+import static constant.OutputViewMessage.FIFTH_PLACE_STATISTICS;
+import static constant.OutputViewMessage.FIRST_PLACE_STATISTICS;
+import static constant.OutputViewMessage.FOUR_PLACE_STATISTICS;
+import static constant.OutputViewMessage.LOTTO_PURCHASE_GUIDANCE;
+import static constant.OutputViewMessage.SECOND_PLACE_STATISTICS;
+import static constant.OutputViewMessage.THIRD_PLACE_STATISTICS;
 import static constant.OutputViewMessage.TOTAL_PROFIT_RATE_RESULT;
 import static constant.OutputViewMessage.LOTTO_PURCHASE_RESULT;
-import static constant.OutputViewMessage.WINNING_STATISTIC_ENTRY;
+import static constant.OutputViewMessage.WINNING_BONUS_GUIDANCE;
+import static constant.OutputViewMessage.WINNING_LOTTO_GUIDANCE;
 import static constant.OutputViewMessage.WINNING_STATISTIC_TITLE;
 import static constant.OutputViewMessage.WINNING_STATISTICS_SEPARATOR;
 
@@ -10,45 +17,50 @@ import java.util.EnumMap;
 import java.util.List;
 import model.Lotto;
 import model.Prize;
-import service.LottoService;
+import model.LottoFactory;
 
 public class OutputView {
 
-    private static final String BENEFIT = "이익";
-    private static final String DAMAGE = "손해";
-
-    public static void printLottoCount(final LottoService lottoService) {
-        print(LOTTO_PURCHASE_RESULT.getMessage(lottoService.getTicketNumber()));
+    public static void printLottoPurchaseGuidance() {
+        print(LOTTO_PURCHASE_GUIDANCE.getMessage());
     }
 
-    public static void printLottoTickets(final LottoService lottoService) {
-        List<Lotto> lottoList = lottoService.getIssuedTickets();
-        lottoList.stream()
+    public static void printWinningLottoGuidance() {
+        print(WINNING_LOTTO_GUIDANCE.getMessage());
+    }
+
+    public static void printWinningBonusGuidance() {
+        print(WINNING_BONUS_GUIDANCE.getMessage());
+    }
+
+    public static void printLottoCount(final LottoFactory lottoFactory) {
+        print(LOTTO_PURCHASE_RESULT.getMessage(lottoFactory.getLottoCount()));
+    }
+
+    public static void printLottoTickets(final LottoFactory lottoFactory) {
+        List<Lotto> issuedLottoTickets = lottoFactory.getIssuedLottoTickets();
+        issuedLottoTickets.stream()
                 .map(lotto -> lotto.getNumbers().toString())
                 .forEach(OutputView::print);
+
         printNewLine();
     }
 
-    public static void printStatistics(EnumMap<Prize, Integer> prizeMap) {
+    public static void printStatistics(EnumMap<Prize, Integer> prizes) {
         printNewLine();
         print(WINNING_STATISTIC_TITLE.getMessage());
         print(WINNING_STATISTICS_SEPARATOR.getMessage());
-
-        prizeMap.keySet().stream()
-                .map(prize -> WINNING_STATISTIC_ENTRY.getMessage(prize.getComment(), prizeMap.get(prize)))
-                .forEach(OutputView::print);
+        print(FIFTH_PLACE_STATISTICS.getMessage(prizes.get(Prize.FIFTH_PLACE)));
+        print(FOUR_PLACE_STATISTICS.getMessage(prizes.get(Prize.FOUR_PLACE)));
+        print(THIRD_PLACE_STATISTICS.getMessage(prizes.get(Prize.THIRD_PLACE)));
+        print(SECOND_PLACE_STATISTICS.getMessage(prizes.get(Prize.SECOND_PLACE)));
+        print(FIRST_PLACE_STATISTICS.getMessage(prizes.get(Prize.FIRST_PLACE)));
     }
 
-    public static void printBenefit(double benefit) {
-        String result = checkBenefit(benefit);
-        print(TOTAL_PROFIT_RATE_RESULT.getMessage(benefit, result));
-    }
-
-    private static String checkBenefit(double benefit) {
-        if (benefit >= 1) {
-            return BENEFIT;
-        }
-        return DAMAGE;
+    public static void printBenefit(LottoFactory lottoFactory, EnumMap<Prize, Integer> prizes) {
+        double winningAmount = lottoFactory.getWinningAmount(prizes);
+        String result = lottoFactory.lossOrGain(winningAmount);
+        print(TOTAL_PROFIT_RATE_RESULT.getMessage(winningAmount, result));
     }
 
     private static void print(final String output) {
