c6f61d72c6130ef96f05534c495c28f7c5266538
horizonpioneer <quantumjump10@gmail.com>
2025-02-15 15:46:05 +0900

    refactor: 구입 금액 입력 예외 처리 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -15,11 +15,16 @@ public class InputView {
 
     public static long inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextLong();
+        String input = scanner.nextLine();
+
+        try {
+            return Long.parseLong(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("구매 금액은 정수만 입력 가능합니다");
+        }
     }
 
     public static List<Integer> inputWinningNumbers() {
-        scanner.nextLine();
         System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
         return Converter.convertToIntegerList(scanner.nextLine());
     }
