b903e7916671b4d4febe5c6a8683c0086e044a9f
Yeongjae Kim <sees77770@gmail.com>
2025-02-15 13:59:11 +0900

    test: 로또 번호 범위 예외 테스트를 추가한다

---
+++ src/test/java/domain/BallTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class BallTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void 로또_번호가_범위가_아니면__예외를_던진다() {
+        int number = 0;
+
+        assertThatThrownBy(() -> new Ball(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
