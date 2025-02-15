71a370682aec9b4814138e11e3a9c89b9932984f
eoehd1ek <eoehd2ek@gmail.com>
2025-02-15 19:07:24 +0900

    refactor(MoneyTest): 명확한 테스트 의도를 표현하도록 메소드 이름 수정

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -3,53 +3,40 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MoneyTest {
-    @DisplayName("음수이면 예외")
-    @Test
-    void test1() {
-        // given
-        final int input = -1000;
 
+    @ParameterizedTest
+    @DisplayName("Money를 생성할 때 금액이 양수가 아니면 예외가 발생한다.")
+    @CsvSource(value = {"-1000", "0"})
+    void exception_argv_is_not_positive_when_ctor(final int input) {
         // when & then
-        assertThatThrownBy(()-> {
+        assertThatThrownBy(() -> {
             new Money(input);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("0이면 예외")
-    @Test
-    void test2() {
-        // given
-        final int input = 0;
-
-        // when & then
-        assertThatThrownBy(()-> {
-            new Money(input);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("숫자로 구성된 문자열일 때")
-    @Test
-    void test3() {
-        // given
-        String input = "1";
-
+    @ParameterizedTest
+    @DisplayName("정적 생성자로 Money 생성할 때 숫자로만 구성된 문자열이면 예외가 발생하지 않는다")
+    @CsvSource(value = {"100", "1000", "5000", "10000"})
+    void not_exception_argv_is_only_number_string_when_static_ctor(final String input) {
         // when
         Money money = Money.of(input);
 
         // then
-        assertThat(money.getValue()).isEqualTo(1);
+        assertThat(money.getValue()).isEqualTo(Integer.parseInt(input));
     }
 
-    @DisplayName("숫자가 아닌 문자열일 때 예외")
     @Test
-    void test4() {
+    @DisplayName("정적 생성자로 Money 생성할 때 숫자 이외 문자열이 포함되면 예외가 발생한다")
+    void exception_argv_is_not_only_number_string_when_static_ctor() {
         // given
-        String input = "woowa";
+        final String input = "woowa";
 
         // when & then
-        assertThatThrownBy(()-> {
+        assertThatThrownBy(() -> {
             Money.of(input);
         }).isInstanceOf(IllegalArgumentException.class);
     }
