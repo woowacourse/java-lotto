9e668c82d482d9fe9a6054ec18fe23d692d3ed1d
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-15 19:55:19 +0900

    feat: 전역 예외 소비, 자원 정리
    - try-with-resources (bufferedReader)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,16 +9,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            InputHandler inputHandler = new InputHandler(bufferedReader);
+            OutputHandler outputHandler = new OutputHandler();
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        InputHandler inputHandler = new InputHandler(bufferedReader);
-        OutputHandler outputHandler = new OutputHandler();
+            LottoMachine lottoMachine = new LottoMachine();
+            LottoWinningChecker lottoWinningChecker = new LottoWinningChecker();
 
-        LottoMachine lottoMachine = new LottoMachine();
-        LottoWinningChecker lottoWinningChecker = new LottoWinningChecker();
-
-        LottoController lottoController = new LottoController(inputHandler, outputHandler, lottoMachine, lottoWinningChecker);
-        lottoController.start();
+            LottoController lottoController = new LottoController(inputHandler, outputHandler, lottoMachine, lottoWinningChecker);
+            lottoController.start();
+        } catch (IOException | IllegalArgumentException e) {
+            System.out.println("잘못된 입력이 감지되었습니다: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("예상치 못한 오류가 발생했습니다. 프로그램을 종료합니다.");
+        }
     }
 }
