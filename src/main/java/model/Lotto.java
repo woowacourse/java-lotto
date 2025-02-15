e8fa31f4daef5fb48e43c520c1d60f288d271398
junhaa <2171326@hansung.ac.kr>
2025-02-15 19:14:33 +0900

    style(Lotto): 자바 컨벤션에 맞게 코드 스타일 조정
    - 일반 메서드 생성자 하단으로 이동

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -8,16 +8,16 @@ import java.util.stream.Collectors;
 public class Lotto {
     private final List<Integer> randomNumbers;
 
-    public List<Integer> getRandomNumbers() {
-        return randomNumbers;
-    }
-
     public Lotto(List<Integer> randomNumbers) {
         if (isDuplicate(randomNumbers)) {
             throw new IllegalArgumentException("숫자가 중복되어서는 안됩니다.");
         }
         this.randomNumbers = randomNumbers;
         Collections.sort(randomNumbers);
+    }
+
+    public List<Integer> getRandomNumbers() {
+        return randomNumbers;
     }
 
     private boolean isDuplicate(List<Integer> randomNumbers) {
