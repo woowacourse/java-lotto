c954bfd42adf61ce327b6850fa7aaedaffb83d3e
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-15 16:26:25 +0900

    feat: Main 클래스의 객체 생성, 의존성 주입

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
 import controller.LottoController;
+import service.LottoMachine;
+import service.LottoWinningChecker;
+import view.InputHandler;
+import view.OutputHandler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        LottoController lottoController = new LottoController();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        InputHandler inputHandler = new InputHandler(bufferedReader);
+        OutputHandler outputHandler = new OutputHandler();
+
+        LottoMachine lottoMachine = new LottoMachine();
+        LottoWinningChecker lottoWinningChecker = new LottoWinningChecker();
+
+        LottoController lottoController = new LottoController(inputHandler, outputHandler, lottoMachine, lottoWinningChecker);
         lottoController.start();
     }
 }
