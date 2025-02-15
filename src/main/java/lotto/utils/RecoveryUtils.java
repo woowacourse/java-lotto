1d85db088cdc6c1a354ff91a420ca41d3a97302d
ryan-dia <cjfdnjs158@naver.com>
2025-02-15 16:58:28 +0900

    refactor(RecoveryUtils): 인덴트 1로 맞추기위한 메서드 분리

--- src/main/java/lotto/utils/RecoveryUtils.java
+++ src/main/java/lotto/utils/RecoveryUtils.java
@@ -11,14 +11,17 @@ public final class RecoveryUtils {
 
     public static <T, R> R executeWithRetry(Supplier<T> inputSupplier, Function<T, R> processFunction) {
         while (true) {
-            try {
-                return processFunction.apply(inputSupplier.get());
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e);
-
-            }
+            return tryCatchLoop(inputSupplier, processFunction);
         }
+
     }
 
-
+    private static <T, R> R tryCatchLoop(Supplier<T> inputSupplier, Function<T, R> processFunction) {
+        try {
+            return processFunction.apply(inputSupplier.get());
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e);
+            return tryCatchLoop(inputSupplier, processFunction);
+        }
+    }
 }
