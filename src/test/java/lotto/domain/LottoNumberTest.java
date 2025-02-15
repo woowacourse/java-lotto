ac1923e837c2127a78831ead19c4dd49e70cbab5
7i.w__k <tizmfns1218@Naver.com>
2025-02-15 16:47:04 +0900

    feat: 로또 넘버 테스트

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
+import static lotto.common.ErrorMessage.ONLY_NUMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoNumberTest {
 
@@ -24,4 +28,20 @@ class LottoNumberTest {
 
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46})
+    void 음수와_경계값은_허용하지_않는다(int value) {
+        assertThatThrownBy(() -> new LottoNumber(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ONLY_NUMBER.getMessage());
+    }
+
+    @Test
+    void 숫자_비교가_가능하다() {
+        LottoNumber number1 = LottoNumber.from(4);
+        LottoNumber number2 = LottoNumber.from(5);
+
+        assertThat(number1).isLessThan(number2);
+        assertThat(number2).isGreaterThan(number1);
+    }
 }
