dd8c7a398411a1d332a2dbb443a4105bc58369ab
junhaa <2171326@hansung.ac.kr>
2025-02-15 19:10:03 +0900

    chore: 불필요한 import 문 제거

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
--- src/main/java/model/UserLotto.java
+++ src/main/java/model/UserLotto.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import view.util.LottoConstants;
