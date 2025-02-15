9b98618adcfda26cbe6d989974231b75ba48a837
eoehd1ek <eoehd2ek@gmail.com>
2025-02-15 15:49:51 +0900

    test(Lotto): 로또 범위가 벗어나는 경우 예외 발생 테스트 보완
    기존 방식에서는 0과 46 중 어떤 상황에서 예외가 발생했는지 확인하기 어려움.
    
    이에 0이 포함된 경우와 46이 포함된 경우 2가지 케이스를 각각 검사하도록 함.

--- src/test/java/LottoTest.java
+++ src/test/java/LottoTest.java
@@ -1,9 +1,14 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
     @DisplayName("로또 번호는 6개만 가능")
@@ -40,12 +45,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 범위가 벗어나는 경우 예외")
-    @Test
-    void test4() {
-        // given
-        List<Integer> numbers = List.of(0, 1, 2, 3, 4, 46);
+    static Stream<Arguments> generateOutboundLottoNumber() {
+        return Stream.of(
+                arguments(List.of(0, 1, 2, 3, 4, 5)),
+                arguments(List.of(46, 1, 2, 3, 4, 5))
+        );
+    }
 
+    @DisplayName("로또 범위가 벗어나는 경우 예외")
+    @ParameterizedTest
+    @MethodSource("generateOutboundLottoNumber")
+    void test4(List<Integer> numbers) {
         // when & then
         assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
