ccb4e4503d8dafba7128bc05e2ce81750685bf03
junho <wnsgh732@gmail.com>
2025-02-15 19:19:48 +0900

    style: 개행 수정

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -6,13 +6,9 @@ import java.util.List;
 public enum Rank {
     NONE("", 0L, 0, false),
     FIFTH("3개 일치 (5000원)- ", 5000L, 3, false),
-
     FOURTH("4개 일치 (50000원)- ", 50000L, 4, false),
-
     THIRD("5개 일치 (1500000원)- ", 1500000L, 5, false),
-
     SECOND("5개 일치, 보너스 볼 일치(30000000원)- ", 30000000L, 5, true),
-
     FIRST("6개 일치 (2000000000원)- ", 2000000000L, 6, false);
 
 
