79bf680c9f0e75c074a6ca8ef109872a1498b089
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-02-15 16:21:47 +0900

    refactor(LottoMachine): 로직, 가독성 개선

--- src/main/java/service/LottoMachine.java
+++ src/main/java/service/LottoMachine.java
@@ -10,23 +10,20 @@ import java.util.List;
 
 public class LottoMachine {
 
-    private final int ticket;
+    public Lottos buyLottos(Money money) {
+        int ticket = money.getAmount() / LottoRule.LOTTO_PRICE.getValue();
 
-    public LottoMachine(Money money) {
-        this.ticket = money.getValue() / LottoRule.LOTTO_PRICE.getValue();
-    }
-
-    public Lottos generateLottos() {
         Lottos lottos = new Lottos();
         for (int i = 0; i < ticket; i++) {
-            List<Integer> randomNumbers = RandomGenerator.generateUniqueRandomNumbers(LottoRule.LOTTO_SELECTION_SIZE.getValue(), LottoRule.MIN_LOTTO_NUMBER.getValue(), LottoRule.MAX_LOTTO_NUMBER.getValue());
+            List<Integer> randomNumbers = RandomGenerator.generateUniqueRandomNumbers(
+                            LottoRule.LOTTO_SELECTION_SIZE.getValue(),
+                            LottoRule.MIN_LOTTO_NUMBER.getValue(),
+                            LottoRule.MAX_LOTTO_NUMBER.getValue());
+
             Lotto lotto = new Lotto(randomNumbers);
             lottos.addLotto(lotto);
         }
-        return lottos;
-    }
 
-    public int getTicket() {
-        return ticket;
+        return lottos;
     }
 }
