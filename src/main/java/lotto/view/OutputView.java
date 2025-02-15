0c14ed214c74d82e3c591cb69a5a14712ee67eec
7i.w__k <tizmfns1218@Naver.com>
2025-02-15 20:25:54 +0900

    style: 코드 줄 정리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,10 +7,8 @@ import java.util.stream.IntStream;
 
 public class OutputView {
 
-    private static final List<String> CORRECT_MESSAGE = List.of("",
-            "3개 일치 (5000원)- ", "4개 일치 (50000원)- ", "5개 일치 (1500000원)- ",
-            "5개 일치, 보너스 볼 일치(30000000원)- ", "6개 일치 (2000000000원)- "
-    );
+    private static final List<String> CORRECT_MESSAGE = List.of("", "3개 일치 (5000원)- ", "4개 일치 (50000원)- ",
+            "5개 일치 (1500000원)- ", "5개 일치, 보너스 볼 일치(30000000원)- ", "6개 일치 (2000000000원)- ");
 
     private OutputView() {
     }
@@ -28,13 +26,10 @@ public class OutputView {
     public static void printStatics(List<Integer> correctCountValues, String profitRate) {
         printNoticeResultMessage();
         IntStream.range(1, CORRECT_MESSAGE.size())
-                .forEach(index ->
-                        System.out.println(CORRECT_MESSAGE.get(index) + correctCountValues.get(index) + "개")
-                );
+                .forEach(index -> System.out.println(CORRECT_MESSAGE.get(index) + correctCountValues.get(index) + "개"));
 
         printProfitRate(profitRate);
     }
-
 
     private static void printNoticeResultMessage() {
         System.out.println("당첨 통계");
