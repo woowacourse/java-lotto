945b30b622dc119c8dfb06de67c58c6301e52077
ppparkta <pppppark2@gmail.com>
2025-02-15 22:03:30 +0900

    fix: 오타 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -43,6 +43,6 @@ public class OutputView {
     }
 
     public static void printRateOfReturn(double rateOfReturn) {
-        System.out.println("총 수익률을 " + rateOfReturn + "입니다. (기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        System.out.println("총 수익률은 " + rateOfReturn + "입니다.");
     }
 }
