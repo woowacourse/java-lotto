851bbac52e2399f5ed764375c12f352715c8903e
jhan0121 <jhan0121@gmail.com>
2025-02-15 18:50:22 +0900

    refactor: Lottos 생성자 추가
    List<Lotto>를 매개변수로 한 생성자

--- src/main/java/model/Lottos.java
+++ src/main/java/model/Lottos.java
@@ -7,6 +7,16 @@ public class Lottos {
 
     private final List<Lotto> lottos;
 
+    /**
+     * 지정한 lotto 들을 저장
+     */
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    /**
+     * 입력한 개수만큼 자동으로 lotto 생성
+     */
     public Lottos(int count) {
         this.lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
