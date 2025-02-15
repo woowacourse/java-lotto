11bdd24a9d6b87a85dbe501bc2df5ae46a01563b
horizonpioneer <quantumjump10@gmail.com>
2025-02-15 10:22:22 +0900

    refactor: 구매금액를 통해 구매수량을 계산하는 로직을 도메인에서 담당하도록 수정

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+public class PurchaseAmount {
+
+    private static final int LOTTO_PRICE = 1000;
+    private final long amount;
+
+    public PurchaseAmount(long purchaseAmount) {
+        validateAmount(purchaseAmount);
+        this.amount = purchaseAmount;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public int countNumberOfPurchases() {
+        return (int) amount / LOTTO_PRICE;
+    }
+
+    private void validateAmount(long purchaseAmount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("구매 금액은" + LOTTO_PRICE + "단위어야 합니다.");
+        }
+    }
+}
