6c3ee157c7326e3ac13e00b8445876d96bed35e4
ryan-dia <cjfdnjs158@naver.com>
2025-02-15 18:16:52 +0900

    refactor: 최대 재시도 횟수를 추가하여 무한시도를 할 수 없도록 변경

--- src/main/java/lotto/utils/RecoveryUtils.java
+++ src/main/java/lotto/utils/RecoveryUtils.java
@@ -6,22 +6,32 @@ import lotto.view.OutputView;
 
 public final class RecoveryUtils {
 
+    private static final int MAX_ATTEMPTS = 5;
+
     private RecoveryUtils() {
     }
 
-    public static <T, R> R executeWithRetry(Supplier<T> inputSupplier, Function<T, R> processFunction) {
-        while (true) {
-            return tryCatchLoop(inputSupplier, processFunction);
-        }
-
+    public static <T, R> R executeWithRetry(
+            Supplier<T> inputSupplier,
+            Function<T, R> processFunction
+    ) {
+        return tryCatchLoop(inputSupplier, processFunction, MAX_ATTEMPTS);
     }
 
-    private static <T, R> R tryCatchLoop(Supplier<T> inputSupplier, Function<T, R> processFunction) {
+    private static <T, R> R tryCatchLoop(
+            Supplier<T> inputSupplier,
+            Function<T, R> processFunction,
+            int attemptsLeft
+    ) {
+        if (attemptsLeft <= 0) {
+            throw new IllegalArgumentException("최대 재시도 횟수를 초과했습니다.");
+        }
+
         try {
             return processFunction.apply(inputSupplier.get());
         } catch (IllegalArgumentException e) {
             OutputView.printError(e);
-            return tryCatchLoop(inputSupplier, processFunction);
+            return tryCatchLoop(inputSupplier, processFunction, attemptsLeft - 1);
         }
     }
 }
