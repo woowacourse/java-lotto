2ee36a1ccc54beca14ce01450ec672fdf26cb0da
ppparkta <pppppark2@gmail.com>
2025-02-15 20:57:10 +0900

    refactor: AnswerLotto 테스트명 추가

--- src/test/java/domain/AnswerLottoTest.java
+++ src/test/java/domain/AnswerLottoTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AnswerLottoTest {
-    @DisplayName("")
+    @DisplayName("보너스 숫자는 중복되거나 1~45 범위 외일 수 없다")
     @Test
     void test() {
+        // given
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
+        // when & then
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> new AnswerLotto(lotto, 6));
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> new AnswerLotto(lotto, 46));
     }
