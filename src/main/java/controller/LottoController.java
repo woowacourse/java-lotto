f9e8260fec451209ceb9e55b11a69c3abdd42851
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-15 10:11:00 +0900

    refactor: Application 제어 로직 LottoController 에게 위임

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import exception.LottoException;
 import service.LottoService;
+import view.InputView;
+import view.OutputView;
 
 public class LottoController {
 
@@ -8,6 +11,72 @@ public class LottoController {
 
     public LottoController(LottoService lottoService) {
         this.lottoService = lottoService;
+    }
+
+    public void startLotto() {
+        buyLotto();
+        settingWinningNumbers();
+        settingBonusNumbers();
+        printWinningResult();
+    }
+
+    private void buyLotto() {
+        boolean retry = true;
+        while(retry) {
+            retry = buyLottoInput();
+        }
+        OutputView.printBuyLotto(formattingBuyLottoResult());
+    }
+
+    private boolean buyLottoInput() {
+        try{
+            String inputBuyLottoMoney = InputView.inputBuyLottoMoney();
+            inputBuyLottoMoney(inputBuyLottoMoney);
+        }catch (LottoException lottoException){
+            OutputView.printError(lottoException);
+            return true;
+        }
+        return false;
+    }
+
+    private void settingWinningNumbers() {
+        boolean retry = true;
+        while(retry) {
+            retry = inputWinningNumber();
+        }
+    }
+
+    private boolean inputWinningNumber() {
+        try{
+            String inputWinningNumber = InputView.inputWinningNumber();
+            inputWinningNumber(inputWinningNumber);
+        }catch (LottoException lottoException){
+            OutputView.printError(lottoException);
+            return true;
+        }
+        return false;
+    }
+
+    private void settingBonusNumbers() {
+        boolean retry = true;
+        while(retry) {
+            retry = inputBonusNumber();
+        }
+    }
+
+    private boolean inputBonusNumber() {
+        try{
+            String inputBonusNumber = InputView.inputBonusNumber();
+            inputBonusNumber(inputBonusNumber);
+        }catch (LottoException lottoException){
+            OutputView.printError(lottoException);
+            return true;
+        }
+        return false;
+    }
+
+    private void printWinningResult() {
+        System.out.println(formattingWinningResult());
     }
 
     public void inputBuyLottoMoney(String inputBuyLottoMoney) {
