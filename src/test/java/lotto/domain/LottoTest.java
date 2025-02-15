37ae5f9921f01d64d6872a174ee7d8820502dab1
황재웅 <wodnd0131@naver.com>
2025-02-15 20:31:38 +0900

    refactor: Lotto와 Wallet Test를 개선
    -불필요해진 RandomsWrapperTest를 삭제한다.
    -LottoTest에서 상수를 통해 도메인 기준을 따르도록 한다.
    -Wallet의 모호했던 testToString를 개선한다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static lotto.common.constant.Constant.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -13,23 +14,18 @@ import lotto.dto.MatchCountDto;
 class LottoTest {
 
     @Test
-    @DisplayName("로또 생성 검증")
-    void testLotto() {
-        assertThat(new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)))).isNotNull();
-
-    }
-
-    @Test
-    @DisplayName("로또 번호가 6개가 아니라면 예외를 발생시킨다.")
+    @DisplayName("로또 번호의 갯수가 기준(6)과 다르다면 예외를 발생시킨다.")
     void testLottoNumber_sizeException() {
         assertThatThrownBy(() -> new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5))))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("로또 번호 가 1에서 45 사이가 아니라면 예외를 발생시킨다.")
+    @DisplayName("로또 번호의 범위가 기준에서 벗어난다면 예외를 발생시킨다.")
     void testLottoNumber_rangeException() {
-        assertThatThrownBy(() -> new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 100))))
+        assertThatThrownBy(() -> new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, LOTTO_MAXIMUM + 1))))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(new ArrayList<>(List.of(LOTTO_MINIMUM - 1, 2, 3, 4, 5, 6))))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -51,6 +47,7 @@ class LottoTest {
         ArrayList<Integer> list = new ArrayList<>(List.of(6, 5, 4, 3, 2, 1));
         Lotto lotto = new Lotto(list);
 
-        assertThat(lotto.toString().equals(List.of(6, 5, 4, 3, 2, 1).toString())).isFalse();
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
+
 }
--- src/test/java/lotto/domain/WalletTest.java
+++ src/test/java/lotto/domain/WalletTest.java
@@ -1,18 +1,22 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WalletTest {
 
-    @DisplayName("toString 가공 테스트")
+    @DisplayName("구매한 로또 목록이 요구사항과 동일한 형태로 출력된다.")
     @Test
-    void testToString() {
+    void testWalletToString() {
         Amount amount = new Amount(5000);
 
         Wallet wallet = new Wallet(amount);
 
-        System.out.println(wallet);
+        String output = wallet.toString();
+        String[] lines = output.split("\n");
+        assertThat(lines).hasSize(5);
     }
 
 }
