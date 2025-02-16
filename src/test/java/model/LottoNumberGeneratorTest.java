713e8704d952963740ab6eec8576e82a5a01e235
gabean13 <choegabin171@gmail.com>
2025-02-17 01:29:03 +0900

    test(LottoNumberGenerator) : 무작위 정수 생성 테스트

--- src/test/java/model/LottoNumberGeneratorTest.java
+++ src/test/java/model/LottoNumberGeneratorTest.java
@@ -8,14 +8,15 @@ import org.junit.jupiter.api.Test;
 
 class LottoNumberGeneratorTest {
 
-    @DisplayName("로또 번호 생성 테스트")
+    @DisplayName("1~45사이의 6개의 무작위 정수를 생성한다")
     @Test
     void lottoGenerateTest() {
-        Set<Integer> lotto = new Lotto(new LottoNumberGenerator()).getLotto();
-        assertThat(lotto.size()).isEqualTo(6);
-        for (int number : lotto) {
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        Set<Integer> numbers = lottoNumberGenerator.generateNumbers();
+
+        assertThat(numbers.size()).isEqualTo(6);
+        for (int number : numbers) {
             assertThat(number).isBetween(1, 45);
         }
     }
-
 }
