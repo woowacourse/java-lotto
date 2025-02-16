6812518dcb58a4f1197d45a217267e5c24fa8c70
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-16 22:45:02 +0900

    refactor: 테스트 파라메터화 및 메서드명에서 상수를 사용하지 않도록 변경
    - 1 부터 45 라는 함수명 대신 최솟값, 최댓값을 사용하도록 변경.
    - 파라메터화를 통한 코드 중복 제거.

--- src/test/java/lotto/model/lotto/vo/LottoNumberTest.java
+++ src/test/java/lotto/model/lotto/vo/LottoNumberTest.java
@@ -4,13 +4,14 @@ import static lotto.model.lotto.vo.LottoNumber.MAX;
 import static lotto.model.lotto.vo.LottoNumber.MIN;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoNumberTest {
 
-    @Test
-    void 로또_번호가_1부터_45사이가_아니면_예외_발생() {
-        assertThrows(IllegalArgumentException.class, () -> LottoNumber.from(MIN - 1));
-        assertThrows(IllegalArgumentException.class, () -> LottoNumber.from(MAX + 1));
+    @ParameterizedTest
+    @ValueSource(ints = {MIN - 1, MAX + 1})
+    void 로또_번호가_최솟값_최댓값_사이가_아니면_예외_발생(int value) {
+        assertThrows(IllegalArgumentException.class, () -> LottoNumber.from(value));
     }
 }
