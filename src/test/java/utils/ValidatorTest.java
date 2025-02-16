f1512ddb273a5347e0b0308715817dd4e8e0b2f6
taek2222 <oot309@daum.net>
2025-02-16 23:29:02 +0900

    test: 나누어 떨어지는 테스트 삭제

--- src/test/java/utils/ValidatorTest.java
+++ src/test/java/utils/ValidatorTest.java
@@ -18,28 +18,14 @@ class ValidatorTest {
     @Test
     void 범위에_벗어나면_예외가_발생한다() {
         assertThatThrownBy(() -> {
-            Validator.validateRange(999, Integer.MAX_VALUE, 1000);
+            Validator.validateNumberRange(999, Integer.MAX_VALUE, 1000);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 범위에_해당되면_예외가_발생하지_않는다() {
         assertThatNoException().isThrownBy(() -> {
-            Validator.validateRange(10, 1, 100);
+            Validator.validateNumberRange(10, 1, 100);
         });
-    }
-
-    @Test
-    void 금액이_1000원_단위면_예외가_발생하지_않는다() {
-        assertThatNoException().isThrownBy(() -> {
-            Validator.validateDivisibility(5000, 1000);
-        });
-    }
-
-    @Test
-    void 금액이_1000원_단위가_아니면_예외가_발생한다() {
-        assertThatThrownBy((() -> {
-            Validator.validateDivisibility(5300, 1000);
-        }));
     }
 }
