3f425386d00e1895f0e39fef7c2f6a1945bcd047
junho <wnsgh732@gmail.com>
2025-02-17 01:08:03 +0900

    test: 예외 처리 테스트 코드 추가

--- src/test/java/validation/LottoValidatorTest.java
+++ src/test/java/validation/LottoValidatorTest.java
@@ -1,14 +1,14 @@
-package view;
+package validation;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import validation.LottoValidator;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static validation.LottoValidator.*;
 
-class ValidatorTest {
+class LottoValidatorTest {
     @DisplayName("당첨 번호 6개 예외 처리 테스트")
     @Test
     void 당첨_번호_6개_예외_처리_테스트() {
@@ -16,9 +16,9 @@ class ValidatorTest {
         List<Integer> sixNumbers = List.of(1, 2, 3, 4, 5, 6);
         List<Integer> overSixNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
         assertThatThrownBy(() -> {
-            LottoValidator.validateWinningNumbers(underSixNumbers);
-            LottoValidator.validateWinningNumbers(sixNumbers);
-            LottoValidator.validateWinningNumbers(overSixNumbers);
+            validateWinningNumbers(underSixNumbers);
+            validateWinningNumbers(sixNumbers);
+            validateWinningNumbers(overSixNumbers);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -26,10 +26,10 @@ class ValidatorTest {
     @Test
     void 당첨_번호는_1에서_45_사이의_수_예외_처리_테스트() {
         assertThatThrownBy(() -> {
-            LottoValidator.validateNumber(0);
-            LottoValidator.validateNumber(5);
-            LottoValidator.validateNumber(40);
-            LottoValidator.validateNumber(50);
+            validateNumber(0);
+            validateNumber(5);
+            validateNumber(40);
+            validateNumber(50);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -37,7 +37,15 @@ class ValidatorTest {
     @Test
     void 당첨_번호_중복_예외_처리_테스트() {
         assertThatThrownBy(() -> {
-            LottoValidator.validateWinningNumbers(List.of(1,1,2,3,4,5));
+            validateWinningNumbers(List.of(1, 1, 2, 3, 4, 5));
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호와 보너스볼 중복 예외 처리 테스트")
+    @Test
+    void 로또_번호와_보너스볼_중복_예외_처리_테스트() {
+        assertThatThrownBy(() -> {
+            validateBonusBallUnique(List.of(1, 2, 3, 4, 5, 6), 6);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
