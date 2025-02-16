eb9c45d3becd955cfaa6e260093d231c4be987b2
rladmstn <oz33177351@gmail.com>
2025-02-16 11:23:12 +0900

    refactor: 유틸 클래스 상속 제한 추가

--- src/main/java/lotto/model/RankDeterminer.java
+++ src/main/java/lotto/model/RankDeterminer.java
@@ -2,7 +2,10 @@ package lotto.model;
 
 import lotto.Rank;
 
-public class RankDeterminer {
+public final class RankDeterminer {
+
+    private RankDeterminer() {
+    }
 
     public static Rank determine(Lotto lotto, Lotto winningLotto, LottoNumber bonusNumber) {
         int matchCount = lotto.getMatchCount(winningLotto);
