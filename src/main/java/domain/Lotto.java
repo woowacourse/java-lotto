0a98ca3d5c158cb5ba917e79a3307dc9d9b52c60
wogus <ind07152@naver.com>
2025-02-16 17:40:46 +0900

    refactor(Lotto): LottoNumber 원시값 포장

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -5,47 +5,42 @@ import static constant.LottoConstants.LOTTO_RANGE_MIN;
 import static constant.LottoConstants.LOTTO_SIZE;
 import static exception.ExceptionMessage.LOTTO_NUMBER_DUPLICATED_ERROR;
 import static exception.ExceptionMessage.LOTTO_RANGE_ERROR;
+import static exception.ExceptionMessage.LOTTO_SIZE_ERROR;
 
 import exception.LottoException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateLottoRange(numbers);
-        validateLottoDuplicate(numbers);
-        validateLottoNumberSize(numbers);
-        this.numbers = numbers;
+        List<LottoNumber> lottoNumbers = numbers.stream().map(LottoNumber::new)
+                .collect(Collectors.toList());
+        validateLottoDuplicate(lottoNumbers);
+        validateLottoNumberSize(lottoNumbers);
+        Collections.sort(lottoNumbers);
+        this.numbers = lottoNumbers;
     }
 
-    private void validateLottoRange(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (number < LOTTO_RANGE_MIN.getValue() || number > LOTTO_RANGE_MAX.getValue()) {
-                throw LottoException.from(LOTTO_RANGE_ERROR);
-            }
-        }
-    }
-
-    private void validateLottoDuplicate(List<Integer> numbers) {
-        HashSet<Integer> set = new HashSet<>(numbers);
+    private void validateLottoDuplicate(List<LottoNumber> numbers) {
+        HashSet<LottoNumber> set = new HashSet<>(numbers);
         if (set.size() != numbers.size()) {
             throw LottoException.from(LOTTO_NUMBER_DUPLICATED_ERROR);
         }
     }
 
-    private void validateLottoNumberSize(List<Integer> numbers) {
+    private void validateLottoNumberSize(List<LottoNumber> numbers) {
         if (numbers.size() != LOTTO_SIZE.getValue()) {
-            throw new IllegalArgumentException("로또 번호 갯수 오류");
+            throw LottoException.from(LOTTO_SIZE_ERROR);
         }
     }
 
     public List<Integer> getSortedNumbers() {
-        List<Integer> numbers = new ArrayList<>(this.numbers);
-        Collections.sort(numbers);
+        List<Integer> numbers = this.numbers.stream().map(LottoNumber::getNumber).toList();
         return numbers;
     }
 }
