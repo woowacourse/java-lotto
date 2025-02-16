267e7fb9809f73d89995891e9e527517f9411c3e
songsunkook <songsunkook@gmail.com>
2025-02-16 13:06:46 +0900

    refactor: InputView 출력문구 enum으로 이동

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 public enum OutputMessage {
+    // OutputView
     TICKET_COUNT_MESSAGE("%d개를 구매했습니다.%n"),
     RESULT_TITLE_MESSAGE("%n당첨 통계"),
     DIVIDER("---------"),
@@ -8,6 +9,11 @@ public enum OutputMessage {
     RANK_COUNT_MESSAGE("%d개 일치 (%d원)- %d개%n"),
     RETURN_OF_RATE_MESSAGE("총 수익률은 %.2f입니다.%n"),
     EMPTY(""),
+
+    // InputView
+    PAYMENT_MESSAGE("구입금액을 입력해 주세요."),
+    WINNING_NUMBER_MESSAGE("지난 주 당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_MESSAGE("보너스 볼을 입력해 주세요."),
     ;
 
     private final String message;
