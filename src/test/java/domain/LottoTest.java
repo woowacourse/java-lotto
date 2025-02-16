b530269c4d2e1fef736a7d8c8c3ad280377c7557
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-17 00:46:23 +0900

    test : 로또 번호 간 중복 검증 테스트 코드 추가

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -52,9 +52,17 @@ public class LottoTest {
     @DisplayName("로또 번호와 일치하는 경우 True를 반환한다")
     @Test
     void shouldReturnTrueWhenDuplicateNumbers() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
 
-        Assertions.assertThat(lotto.isUnique(Arrays.asList(1, 2, 3, 4, 5, 6))).isTrue();
+        Assertions.assertThat(lotto1.isUnique(Arrays.asList(1, 2, 3, 4, 5, 6))).isTrue();
+
+        Lotto lotto2 = new Lotto(Arrays.asList(6, 5, 4, 3, 2, 1));
+
+        Assertions.assertThat(lotto2.isUnique(Arrays.asList(1, 2, 3, 4, 5, 6))).isTrue();
+
+        Lotto lotto3 = new Lotto(Arrays.asList(1, 3, 5, 2, 4, 6));
+
+        Assertions.assertThat(lotto3.isUnique(Arrays.asList(1, 2, 3, 4, 5, 6))).isTrue();
     }
 
     @DisplayName("로또 번호와 중복되지 않는 번호를 가진 경우 False를 반환한다")
