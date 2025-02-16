780223993a37bc6805fa9b40da6e7a8d7a7a967f
AJKHJ <gmlwns904@ajou.ac.kr>
2025-02-16 16:57:37 +0900

    test: 로또 결과 정상 반환 테스트 작성

--- src/test/java/src/model/LottoMachineTest.java
+++ src/test/java/src/model/LottoMachineTest.java
@@ -2,13 +2,17 @@ package src.model;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import src.model.lotto.Lotto;
 import src.model.lotto.generator.MockedNumberGenerator;
+import src.model.lotto.generator.NumberGenerator;
+import src.model.winning_lotto.WinningLotto;
 
 class LottoMachineTest {
 
@@ -45,5 +49,34 @@ class LottoMachineTest {
 
         double actualProfitRate = lottoMachine.calculateProfitRate(prizes, purchaseMoney);
         assertEquals(expectedProfitRate, actualProfitRate);
+    }
+
+    @Test
+    void 로또_결과_정상_반환_테스트() {
+        MockedNumberGenerator generator1 = new MockedNumberGenerator();
+        generator1.setNumbersToGenerate(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto1 = Lotto.generateFrom(generator1);
+
+        MockedNumberGenerator generator2 = new MockedNumberGenerator();
+        generator2.setNumbersToGenerate(Arrays.asList(1, 2, 3, 4, 5, 7));
+        Lotto lotto2 = Lotto.generateFrom(generator2);
+
+        MockedNumberGenerator generator3 = new MockedNumberGenerator();
+        generator3.setNumbersToGenerate(Arrays.asList(10, 11, 12, 13, 14, 15));
+        Lotto lotto3 = Lotto.generateFrom(generator3);
+
+        List<Lotto> lottos = Arrays.asList(lotto1, lotto2, lotto3);
+
+        WinningLotto winningLotto = WinningLotto.of(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+
+        LottoMachine lottoMachine = new LottoMachine(new MockedNumberGenerator());
+        List<LottoPrize> results = lottoMachine.getLottoResults(lottos, winningLotto);
+
+        assertNotNull(results);
+        assertEquals(3, results.size());
+
+        assertEquals(LottoPrize.SIX, results.get(0));
+        assertEquals(LottoPrize.FIVE_WITH_BONUS, results.get(1));
+        assertEquals(LottoPrize.ZERO, results.get(2));
     }
 }
