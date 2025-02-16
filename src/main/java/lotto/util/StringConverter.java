74254a2b06a662bbace42e479bc3ef3e8bc1530f
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-17 00:10:21 +0900

    fix(StringConverter): use custom parse method

--- src/main/java/lotto/util/StringConverter.java
+++ src/main/java/lotto/util/StringConverter.java
@@ -8,11 +8,11 @@ public class StringConverter {
     public static List<Integer> parseToIntList(String input, String delimiter) {
         List<String> result = Arrays.stream(input.split(delimiter)).toList();
         return result.stream()
-                .map(s -> Integer.parseInt(s.strip()))
+                .map(s -> parseToInt(s.strip(), ExceptionHandler.INVALID_LOTTO_NUMBER_FORMAT))
                 .toList();
     }
 
-    public static int parseToInt(String input, ErrorHandler errorCode) {
+    public static int parseToInt(String input, ExceptionHandler errorCode) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
