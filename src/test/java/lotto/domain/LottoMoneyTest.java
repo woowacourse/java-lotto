211837830aec630c26289e8631932fc5c1ec1183
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-17 00:32:07 +0900

    test(LottoMoneyTest): remove format test and use integer value

--- src/test/java/lotto/domain/LottoMoneyTest.java
+++ src/test/java/lotto/domain/LottoMoneyTest.java
@@ -7,27 +7,21 @@ import org.junit.jupiter.api.Test;
 
 public class LottoMoneyTest {
 
-    @DisplayName("로또 구입 금액이 숫자가 아닌 예외")
-    @Test
-    public void lottoMoneyNumber() {
-        String input = "money";
-        assertThatThrownBy(() -> new LottoMoney(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @DisplayName("로또 구입 금액이 1000원 미만인 예외")
     @Test
     public void lottoMoneyAmount() {
-        String input = "200";
-        assertThatThrownBy(() -> new LottoMoney(input))
+        int amount = 200;
+
+        assertThatThrownBy(() -> new LottoMoney(amount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구입 금액이 1000원 단위가 아닌 예외")
     @Test
     public void lottoMoneyUnit() {
-        String input = "1200";
-        assertThatThrownBy(() -> new LottoMoney(input))
+        int amount = 1200;
+
+        assertThatThrownBy(() -> new LottoMoney(amount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
