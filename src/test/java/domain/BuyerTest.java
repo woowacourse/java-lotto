019372ef2ed94abb6463159e7337b866efee6d36
DEV_WOONG <ywcsuwon@gmail.com>
2025-02-17 00:21:31 +0900

    test: 로또 생성 테스트

--- src/test/java/domain/BuyerTest.java
+++ src/test/java/domain/BuyerTest.java
@@ -1,7 +1,11 @@
 package domain;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import generator.DefaultLottoNumber;
+import generator.FixedLottoNumber;
 import generator.RandomGenerator;
+import java.util.Arrays;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,5 +20,17 @@ public class BuyerTest {
         Money money = new Money(input);
         Buyer buyer = new Buyer(money, randomGenerator);
         Assertions.assertThat(buyer.getLottoSize()).isEqualTo(14);
+    }
+
+    @Test
+    void testBuyerCreatesLottosWithFixedNumbers() {
+        FixedLottoNumber fixedRandomGenerator = new FixedLottoNumber(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        Money money = new Money(1000);
+        Buyer buyer = new Buyer(money, fixedRandomGenerator);
+
+        String result = buyer.createResult();
+
+        assertTrue(result.contains("[1, 2, 3, 4, 5, 6]"), "로또 번호는 고정된 값이어야 합니다.");
     }
 }
