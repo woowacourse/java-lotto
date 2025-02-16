95519edcd602b4d12b2b46280a47c1cfa1875a32
Mint <audwl03071@gmail.com>
2025-02-16 16:48:17 +0900

    test(LottoNumber): 테스트 케이스 추가 및 누락된 final 키워드 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,21 +1,21 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoNumberTest {
 
-    @Test
-    void 로또_번호가_정상적으로_생성된다() {
-        Assertions.assertThatCode(() -> new LottoNumber(45))
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45})
+    void 로또_번호가_정상적으로_생성된다(final int value) {
+        Assertions.assertThatCode(() -> new LottoNumber(value))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 46})
-    void 번호가_1과_45_사이가_아니면_예외를_발생한다(int number) {
+    void 번호가_1과_45_사이가_아니면_예외를_발생한다(final int number) {
         Assertions.assertThatThrownBy(() -> new LottoNumber(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 번호는 1과 45 사이여야 합니다.");
