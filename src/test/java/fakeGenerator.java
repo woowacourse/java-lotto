1a5fab9df53ca3f51d68b814b865bd48841e1b44
ChoiDongJoon <adong0808@naver.com>
2025-02-16 23:21:01 +0900

    refactor(lotto): Delete file

--- src/test/java/fakeGenerator.java
+++
@@ -1,9 +0,0 @@
-import domain.generator.Generator;
-import java.util.List;
-
-public class fakeGenerator implements Generator {
-    @Override
-    public List<Integer> generate() {
-        return List.of(1,2,3,4,5,6);
-    }
-}
