e9f7511f130df19af795ecc953422690a5844f37
eunseongu <rndmstjs75@gmail.com>
2025-02-17 00:36:27 +0900

    feat : 로또 번호가 쉼표로 구분되지 않은 경우 예외 처리 추가

--- src/main/java/error/ErrorMessage.java
+++ src/main/java/error/ErrorMessage.java
@@ -7,7 +7,8 @@ public enum ErrorMessage {
     BONUS_NUMBER_ALREADY_EXIST("보너스 번호가 당첨번호에 이미 존재합니다."),
     INVALID_LOTTO_PRICE("로또는 1,000원 단위로만 구매 가능합니다."),
     BLANK_INPUT("공백을 입력했습니다."),
-    ONLY_POSITIVE_NUMBER("양수만 입력 가능합니다.");
+    ONLY_POSITIVE_NUMBER("양수만 입력 가능합니다."),
+    INVALID_LOTTO_NUMBER_INPUT("로또 번호는 쉼표로 구분해서 입력하여야 합니다.");
 
     private final String message;
     private final String prefix = "[ERROR] ";
