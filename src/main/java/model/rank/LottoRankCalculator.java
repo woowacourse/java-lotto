68a702fbac53246402cc0de97aa984d506fe6665
Jin409 <sualng123@gmail.com>
2025-02-16 22:17:31 +0900

    refactor : 완료된 TODO 삭제

--- src/main/java/model/rank/LottoRankCalculator.java
+++ src/main/java/model/rank/LottoRankCalculator.java
@@ -5,8 +5,6 @@ import model.numbers.WinningLotto;
 
 public class LottoRankCalculator {
 
-    // TODO: calcualte 에 대한 테스트 추가
-
     public LottoRank calculate(LottoNumbers lottoNumbers, WinningLotto winningLotto) {
         int overlappedCount = winningLotto.countOverlappedNumbers(lottoNumbers.getNumbers());
 
