dea683ec38db9aa2221e0f7e5541efb4b4d9bfc0
Firedrago95 <yonghwa95@naver.com>
2025-02-16 11:49:57 +0900

    refactor: import 와일드카드 제거

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class WinningNumbersTest {
     @Test
