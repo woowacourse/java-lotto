379bf401b5f7a31285c89f9840ef822daddf5374
dltmddus <tmddus4671@gmail.com>
2025-02-16 20:44:23 +0900

    fix: 여러개 인자 테스트 안 되는 오류 수정, 메서드 네이밍 영어로 수정

--- src/test/java/domain/LottoTicketTest.java
+++ src/test/java/domain/LottoTicketTest.java
@@ -4,100 +4,105 @@ import java.util.List;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import view.InputErrorMessage;
 
 class LottoTicketTest {
 
+    @Nested
     @DisplayName("로또머신이 로또 번호를 정상적으로 발행하는지 테스트")
-    @Test
-    void 로또_발행_테스트() {
-        // given
-        LottoMachine lottoMachine = new LottoMachine();
+    class lottoMachineTest {
+        @DisplayName("구매 금액에 따라 올바른 개수의 로또를 생성해야 한다.")
+        @Test
+        void shouldGenerateCorrectNumberOfTickets() {
+            // given
+            int purchaseAmount = 14000;
+            LottoMachine lottoMachine = new LottoMachine();
+            FixedIntegerGenerator generator = new FixedIntegerGenerator();
 
-        // when
-        LottoTicket lottoTicket = lottoMachine.generateLottoTicket(new FixedIntegerGenerator());
+            // when
+            List<LottoTicket> lottoTickets = lottoMachine.generateLottoTickets(purchaseAmount,
+                    generator);
 
-        // then
-        Assertions.assertThat(lottoTicket.getSize()).isEqualTo(LottoTicket.LOTTO_SIZE);
-        for (int number : lottoTicket.getNumbers()) {
-            Assertions.assertThat(number).isGreaterThanOrEqualTo(LottoTicket.LOTTO_MIN_NUMBER);
-            Assertions.assertThat(number).isLessThanOrEqualTo(LottoTicket.LOTTO_MAX_NUMBER);
+            // then
+            Assertions.assertThat(lottoTickets.size()).isEqualTo(14);
         }
-        Assertions.assertThat(lottoTicket.getNumbers().stream().distinct()).hasSize(LottoTicket.LOTTO_SIZE);
+
+        @DisplayName("로또 번호의 개수가 6개가 아닌 경우 예외가 발생한다")
+        @ParameterizedTest
+        @MethodSource("notSixNumbers")
+        void shouldGenerateSixNumbers(List<Integer> numbers) {
+            // when & then
+            Assertions.assertThatThrownBy(() -> {
+                        LottoTicket.from(numbers);
+                    }).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(InputErrorMessage.REQUIRED_SIX_NUMBERS.getMessage());
+        }
+
+        static Stream<Arguments> notSixNumbers() {
+            return Stream.of(
+                    Arguments.of(List.of(1)),
+                    Arguments.of(List.of(1, 2, 3)),
+                    Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7)
+                    ));
+        }
+
+        @DisplayName("로또 번호가 1 이상 45 이하가 아닌 경우 예외가 발생한다")
+        @ParameterizedTest
+        @MethodSource("validateRangeNumbers")
+        void shouldGenerateValidRange(List<Integer> numbers) {
+            // when & then
+            Assertions.assertThatThrownBy(() -> {
+                        LottoTicket.from(numbers);
+                    }).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(InputErrorMessage.NOT_FITTED_RANGE.getMessage());
+        }
+
+        static Stream<Arguments> validateRangeNumbers() {
+            return Stream.of(
+                    Arguments.of(List.of(0, 1, 2, 3, 4, 55)),
+                    Arguments.of(List.of(-1, 1, 2, 3, 4, 5)),
+                    Arguments.of(List.of(1, 2, 3, 4, 5, 100)
+                    ));
+        }
+
+        @DisplayName("로또 번호가 중복된 경우 예외가 발생한다")
+        @ParameterizedTest
+        @MethodSource("duplicatedNumbers")
+        void shouldNotGeneratedDuplicated(List<Integer> numbers) {
+            // when & then
+            Assertions.assertThatThrownBy(() -> {
+                        LottoTicket.from(numbers);
+                    }).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(InputErrorMessage.DUPLICATED.getMessage());
+        }
+
+        static Stream<Arguments> duplicatedNumbers() {
+            return Stream.of(
+                    Arguments.of(List.of(1, 2, 3, 4, 5, 5)),
+                    Arguments.of(List.of(7, 7, 7, 7, 7, 7))
+            );
+        }
     }
 
-    @DisplayName("로또 번호의 개수가 6개가 아닌 경우 예외가 발생한다")
-    @ParameterizedTest
-    @MethodSource("notSixNumbers")
-    void 로또_번호의_개수가_6개가_아닌_경우_테스트(List<Integer> numbers) {
-        // when & then
-        Assertions.assertThatThrownBy(() -> {
-                    new LottoTicket(numbers);
-                }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호는 6개여야 합니다.");
-    }
 
-    static Stream<Arguments> notSixNumbers() {
-        return Stream.of(Arguments.of(
-                List.of(1),
-                List.of(1, 2, 3),
-                List.of(1, 2, 3, 4, 5, 6, 7)
-        ));
-    }
-
-    @DisplayName("로또 번호가 1 이상 45 이하가 아닌 경우 예외가 발생한다")
-    @ParameterizedTest
-    @MethodSource("validateRangeNumbers")
-    void 로또_번호가_1_이상_45_이하가_아닌_경우_테스트(List<Integer> numbers) {
-        // when & then
-        Assertions.assertThatThrownBy(() -> {
-                    new LottoTicket(numbers);
-                }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호는 1 이상 45 이하이다.");
-    }
-
-    static Stream<Arguments> validateRangeNumbers() {
-        return Stream.of(Arguments.of(
-                List.of(0, 1, 2, 3, 4, 55),
-                List.of(-1, 1, 2, 3, 4, 5),
-                List.of(1, 2, 3, 4, 5, 100)
-        ));
-    }
-
-    @DisplayName("로또 번호가 중복된 경우 예외가 발생한다")
-    @ParameterizedTest
-    @MethodSource("duplicatedNumbers")
-    void 로또_번호가_중복된_경우_테스트(List<Integer> numbers) {
-        // when & then
-        Assertions.assertThatThrownBy(() -> {
-                    new LottoTicket(numbers);
-                }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("중복된 번호가 존재합니다.");
-    }
-
-    static Stream<Arguments> duplicatedNumbers() {
-        return Stream.of(Arguments.of(
-                List.of(1, 2, 3, 4, 5, 5),
-                List.of(7, 7, 7, 7, 7, 7)
-        ));
-    }
-
-    @DisplayName("로또 번호의 개수가 1 이상 45 이하일 때 정상적으로 로또를 발행하는지 테스트")
+    @DisplayName("로또 번호가 1 또는 45(경계값)일 때 정상적으로 로또를 발행하는지 테스트")
     @ParameterizedTest
     @MethodSource("randomLottoNumbers")
-    void 로또_테스트(List<Integer> numbers) {
+    void boundaryTest(List<Integer> numbers) {
         // when & then
-        Assertions.assertThatCode(() -> new LottoTicket(numbers))
+        Assertions.assertThatCode(() -> LottoTicket.from(numbers))
                 .doesNotThrowAnyException();
     }
 
     static Stream<Arguments> randomLottoNumbers() {
         return Stream.of(
-                Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
-                        List.of(1, 27, 42, 35, 8, 45)));
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6)),
+                Arguments.of(List.of(11, 12, 13, 14, 15, 45)),
+                Arguments.of(List.of(1, 27, 42, 35, 8, 45)));
     }
 }
