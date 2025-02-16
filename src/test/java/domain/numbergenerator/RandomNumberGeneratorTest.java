906bc19a8edc92126a9ea0f781433b196b39ed29
Hacanna42 <dtmdgus901@gmail.com>
2025-02-16 14:35:38 +0900

    test: RandomNumberGenerator 테스트 추가

--- src/test/java/domain/numbergenerator/RandomNumberGeneratorTest.java
+++ src/test/java/domain/numbergenerator/RandomNumberGeneratorTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RandomNumberGeneratorTest {
-    @DisplayName("중복되지 않는 6개의 숫자가 나온다")
+    @DisplayName("중복되지 않는 6개의 숫자가 나오는지 확인한다.")
     @Test
-    void 중복되지_않는_6개의_숫자가_나온다() {
+    void 중복되지_않는_6개의_숫자_테스트() {
         // given
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
