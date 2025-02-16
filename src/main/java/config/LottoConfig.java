f147f4534e60df0c4138949617f2488fe5041071
eunseongu <rndmstjs75@gmail.com>
2025-02-16 17:05:53 +0900

    style : 불필요한 개행 제거

--- src/main/java/config/LottoConfig.java
+++ src/main/java/config/LottoConfig.java
@@ -7,7 +7,6 @@ import view.InputView;
 import view.OutputView;
 
 public class LottoConfig {
-
     public static LottoController createController() {
         return new LottoController(InputView.create(), OutputView.create(), createService());
     }
