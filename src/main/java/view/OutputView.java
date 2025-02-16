80ccd9256260e8397ae26411d3a8368ef9748530
jhan0121 <jhan0121@gmail.com>
2025-02-16 20:11:30 +0900

    refactor: OutputView 구매 로또, 통계 출력 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,8 @@
 package view;
 
+import java.util.List;
+import model.LottoCalculator;
 import model.LottoResult;
-import model.Lottos;
 
 public class OutputView {
 
@@ -17,8 +18,10 @@ public class OutputView {
         System.out.printf("%d개를 구매했습니다.%n", count);
     }
 
-    public static void printLottoResults(Lottos lottos) {
-        lottos.printLottoNumbers();
+    public static void printLottoResults(List<String> lottoNumbers) {
+        for (String lottoNumber : lottoNumbers) {
+            System.out.println("[" + lottoNumber + "]");
+        }
     }
 
     public static void inputWinnerNumbers() {
@@ -29,15 +32,19 @@ public class OutputView {
         System.out.println("보너스 볼을 입력해 주세요.");
     }
 
-    public static void winningStatistics(double result) {
+    public static void winningStatistics(LottoCalculator lottoCalculator, double result) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.printf("3개 일치 (5000원)- %d개%n", LottoResult.FIFTH.getCount());
-        System.out.printf("4개 일치 (50000원)- %d개%n", LottoResult.FOURTH.getCount());
-        System.out.printf("5개 일치 (1500000원)- %d개%n", LottoResult.THIRD.getCount());
-        System.out.printf("5개 일치, 보너스 볼 일치(30000000원)- %d개%n", LottoResult.SECOND.getCount());
-        System.out.printf("6개 일치 (2000000000원)- %d개%n", LottoResult.FIRST.getCount());
+        System.out.printf("3개 일치 (%d원)- %d개%n", LottoResult.FIFTH.getPrice(),
+                lottoCalculator.findTargetResultCount(LottoResult.FIFTH));
+        System.out.printf("4개 일치 (%d원)- %d개%n", LottoResult.FOURTH.getPrice(),
+                lottoCalculator.findTargetResultCount(LottoResult.FOURTH));
+        System.out.printf("5개 일치 (%d원)- %d개%n", LottoResult.THIRD.getPrice(),
+                lottoCalculator.findTargetResultCount(LottoResult.THIRD));
+        System.out.printf("5개 일치, 보너스 볼 일치(%d원)- %d개%n", LottoResult.SECOND.getPrice(),
+                lottoCalculator.findTargetResultCount(LottoResult.SECOND));
+        System.out.printf("6개 일치 (%d원)- %d개%n", LottoResult.FIRST.getPrice(),
+                lottoCalculator.findTargetResultCount(LottoResult.FIRST));
         System.out.printf("총 수익률은 %.02f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)", result);
     }
-
 }
