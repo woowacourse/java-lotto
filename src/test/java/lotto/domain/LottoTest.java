a33e00786f3c80bc4f264678fce369d95377b7ab
Firedrago95 <yonghwa95@naver.com>
2025-02-16 12:15:47 +0900

    refactor: 테스트 코드의 가독성을 위해 given, when, then 구조 도입

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -10,22 +10,30 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 public class LottoTest {
     @Test
     void 일치하는_숫자_갯수를_구한다() {
+        // given
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
 
+        // when & then
         assertThat(lotto.findMatchCount(winningLotto)).isEqualTo(5);
     }
 
     @Test
     void 번호_포함_여부를_확인한다() {
+        //given
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+
+        // when & then
         assertThat(lotto.containsNumber(new LottoNumber(3)))
             .isTrue();
     }
 
     @Test
     void 로또_번호의_등수를_판정한다() {
+        //given
         WinningNumbers winningNumbers = new WinningNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+
+        //when & then
         assertThat(winningNumbers.getRank(new Lotto(List.of(1, 2, 3, 4, 5, 6)))).isEqualTo(Rank.FIRST);
         assertThat(winningNumbers.getRank(new Lotto(List.of(1, 2, 3, 4, 5, 7)))).isEqualTo(Rank.SECOND);
         assertThat(winningNumbers.getRank(new Lotto(List.of(1, 2, 3, 4, 5, 8)))).isEqualTo(Rank.THIRD);
@@ -33,6 +41,7 @@ public class LottoTest {
 
     @Test
     void 로또_번호_개수가_6개가_아닐_경우_예외를_반환한다() {
+        // when & then
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
             .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -41,6 +50,7 @@ public class LottoTest {
 
     @Test
     void 로또_번호가_중복될_경우_예외를_반환한다() {
+        // when & then
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
             .isInstanceOf(IllegalArgumentException.class);
     }
--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -10,23 +11,32 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 public class LottosTest {
     @Test
     void 당첨_결과를_구한다() {
+        //given
         Lottos lottos = new Lottos(List.of(
             new Lotto(List.of(1,2,3,4,5,6)),
             new Lotto(List.of(7,8,9,10,11,12))
         ));
         WinningNumbers winningNumbers = new WinningNumbers(new Lotto(List.of(1,2,3,4,5,6)), 7);
-        assertThat(lottos.getRankCount(winningNumbers).get(Rank.FIRST))
-            .isEqualTo(1);
+
+        //when
+        Map<Rank, Integer> rankCount = lottos.getRankCount(winningNumbers);
+
+        //then
+        assertThat(rankCount.get(Rank.FIRST)).isEqualTo(1);
     }
 
     @Test
     void 주어진_가격으로_정확한_매수를_구한다() {
+        //given
         Lottos lottos = new Lottos(3000);
+
+        //when & then
         assertThat(lottos.getTicketCount()).isEqualTo(3);
     }
 
     @Test
     void 구입_금액이_1000원으로_나누어떨어지지_않을_경우_예외를_반환한다() {
+        //when & then
         assertThatThrownBy(() -> new Lottos(3))
             .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Lottos(3003))
