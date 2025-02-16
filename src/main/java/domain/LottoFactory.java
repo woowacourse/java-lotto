dd78e5c7fd1338a9f8974b9ad57265bdba045a47
JO-eusan <goodsan0330@gmail.com>
2025-02-16 23:28:51 +0900

    refactor: 불필요한 import 제거

--- src/main/java/domain/LottoFactory.java
+++ src/main/java/domain/LottoFactory.java
@@ -6,7 +6,6 @@ import static util.constant.Values.LOTTO_SIZE;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
