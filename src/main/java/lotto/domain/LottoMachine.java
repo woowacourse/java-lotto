ccd060644d5848b1a3c40723c20ed5b065365811
KJungW <sagakkb0707@naver.com>
2025-02-16 18:18:07 +0900

    refactor(lotto_machine): 구매 금액 검증 추가
    작업 내용
    - LottoMachine에서 구매 금액을 검증하도록 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 import java.util.stream.IntStream;
 import lotto.utility.RandomGenerator;
+import lotto.view.constant.ExceptionMessage;
 
 public class LottoMachine {
 
@@ -30,6 +31,7 @@ public class LottoMachine {
     }
 
     private int calculatePurchaseCount(int purchaseAmount) {
+        validateUnit(purchaseAmount);
         return purchaseAmount / Lotto.LOTTO_PRICE;
     }
 
@@ -46,5 +48,14 @@ public class LottoMachine {
                 .sorted()
                 .toList();
         return new Lotto(randoms);
+    }
+
+    private void validateUnit(int purchaseAmount) {
+        boolean isInValidAmount = purchaseAmount % Lotto.LOTTO_PRICE != 0 || purchaseAmount <= 0;
+        if (isInValidAmount) {
+            String messageTemplate = ExceptionMessage.INVALID_PURCHASE_AMOUNT.getContent();
+            String exceptionMessage = String.format(messageTemplate, Lotto.LOTTO_PRICE);
+            throw new IllegalArgumentException(exceptionMessage);
+        }
     }
 }
