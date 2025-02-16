11c704aeda1edb392eafe9de9f7b53ac5762ea95
GiSeok Kang <ime.rokz@gmail.com>
2025-02-17 01:20:24 +0900

    style: 변수명 변경 및 메소드 분리

--- src/main/java/service/Judgement.java
+++ src/main/java/service/Judgement.java
@@ -13,17 +13,21 @@ public class Judgement {
     public static PrizeResult judge(Lottos lottos, WinningLotto winningLotto) {
         EnumMap<Rank, Integer> prizeCounts = new EnumMap<>(Rank.class);
         for (int idx = 0; idx < lottos.size(); idx++) {
-            Set<Integer> lottoNumbers = new HashSet<>(lottos.getLottoByIndex(idx).getNumbers());
-            Set<Integer> WinningLottoNumbers = new HashSet<>(winningLotto.getNumbers());
-            lottoNumbers.retainAll(WinningLottoNumbers);
-
-            boolean bonus = lottos.containByIndex(idx, winningLotto.getBonusNumber());
-            int match = lottoNumbers.size();
-
-            Rank result = Rank.judge(match, bonus);
+            Rank result = findMatchRank(lottos, winningLotto, idx);
             prizeCounts.put(result, prizeCounts.getOrDefault(result, 0) + 1);
         }
 
         return new PrizeResult(prizeCounts, lottos.size());
+    }
+
+    private static Rank findMatchRank(Lottos lottos, WinningLotto winningLotto, int idx) {
+        Set<Integer> lottoNumbers = new HashSet<>(lottos.getLottoByIndex(idx).getNumbers());
+        Set<Integer> WinningLottoNumbers = new HashSet<>(winningLotto.getNumbers());
+        lottoNumbers.retainAll(WinningLottoNumbers);
+
+        boolean bonus = lottos.containByIndex(idx, winningLotto.getBonusNumber());
+        int matchCount = lottoNumbers.size();
+
+        return Rank.judge(matchCount, bonus);
     }
 }
