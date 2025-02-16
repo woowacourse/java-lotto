0da4ecb2f332be99f37563a5a147b8577f03498a
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-02-17 01:24:28 +0900

    refactor(factory): LottoFactory를 인터페이스 방식으로 변경하여 확장성 개선
    - 기존 static 메서드를 사용하는 LottoFactory를 인터페이스 방식으로 변경
    - AutoLottoFactory, ManualLottoFactory를 구현하여 자동/수동 로또 생성 분리
    - DI를 활용하여 서비스 레이어에서 LottoFactory를 주입받도록 수정

--- src/main/java/factory/LottoFactory.java
+++ src/main/java/factory/LottoFactory.java
@@ -4,13 +4,7 @@ import java.util.List;
 import model.Lotto;
 import util.RandomGenerator;
 
-public class LottoFactory {
-    public static Lotto createCustomLotto(List<Integer> numbers) {
-        return new Lotto(numbers);
-    }
-
-    public static Lotto createRandomLotto() {
-        List<Integer> randomNumbers = RandomGenerator.generateNumbers(1, 45, 6);
-        return new Lotto(randomNumbers);
-    }
+public interface LottoFactory {
+    Lotto createLotto();
+    Lotto createLotto(List<Integer> numbers);
 }
--- src/main/java/service/LottoEvaluationService.java
+++ src/main/java/service/LottoEvaluationService.java
@@ -1,11 +1,20 @@
 package service;
 
+import factory.LottoFactory;
+import java.util.List;
 import model.Lotto;
 import model.Lottos;
 import model.WinningLotto;
 
 public class LottoEvaluationService {
-    public void evaluateLottos(Lottos lottos, Lotto basicLotto, int bonusNumber) {
+    private final LottoFactory lottoFactory;
+
+    public LottoEvaluationService(LottoFactory lottoFactory) {
+        this.lottoFactory = lottoFactory;
+    }
+
+    public void evaluateLottos(Lottos lottos, List<Integer> basicNumbers, int bonusNumber) {
+        Lotto basicLotto = lottoFactory.createLotto(basicNumbers);
         WinningLotto winningLotto = new WinningLotto(basicLotto, bonusNumber);
         lottos.rankAll(winningLotto);
     }
