660daeca437155f531120ab62667c07bc17d72fb
kjyyjk <rlawnsdud920@gmail.com>
2025-02-17 01:27:21 +0900

    test: 발행된 로또의 범위가 올바른지 추가 검증

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -3,6 +3,8 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static lotto.Lotto.MIN_LOTTO_NUMBER;
+import static lotto.Lotto.MAX_LOTTO_NUMBER;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoMachineTest {
@@ -11,5 +13,7 @@ class LottoMachineTest {
     void 로또는_6개의_번호를_가진다() {
         Lotto result = LottoMachine.createLotto();
         assertThat(result.getNumbers().size()).isEqualTo(6);
+        result.getNumbers()
+                .forEach(number -> assertThat(number).isBetween(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
     }
 }
