57857318bf77bc462ebde40859f7cd66ba192587
KJungW <sagakkb0707@naver.com>
2025-02-16 18:08:12 +0900

    refactor(lotto): 검증 로직 추가
    추가 내용
    - Lotto와 관련된 검증은 Lotto에서
      관리하도록 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import lotto.vaildator.CommonValidator;
+import java.util.Set;
+import lotto.view.constant.ExceptionMessage;
 
 public class Lotto {
 
@@ -13,9 +15,9 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        CommonValidator.validateDuplication(numbers);
-        CommonValidator.validateRange(numbers, MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
-        CommonValidator.validateSize(numbers, LOTTO_NUMBER_COUNT);
+        validateDuplication(numbers);
+        validateRange(numbers);
+        validateSize(numbers);
         this.numbers = numbers.stream().sorted().toList();
     }
 
@@ -29,5 +31,28 @@ public class Lotto {
 
     public int findMatches(Lotto lotto) {
         return (int) numbers.stream().filter(lotto::hasNumber).count();
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        Set<Integer> notDuplicatedNumbers = new HashSet<>(numbers);
+        boolean isDuplicated = notDuplicatedNumbers.size() != numbers.size();
+        if (isDuplicated) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_NUMBERS.getContent());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        boolean isInRange = numbers.stream()
+                .allMatch(number -> number >= MIN_LOTTO_NUMBER && number <= MAX_LOTTO_NUMBER);
+        if (!isInRange) {
+            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_RANGE.getContent());
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        boolean isIncorrectSize = numbers.size() != LOTTO_NUMBER_COUNT;
+        if (isIncorrectSize) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER_COUNT.getContent());
+        }
     }
 }
