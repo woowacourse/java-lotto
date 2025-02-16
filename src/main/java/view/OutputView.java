c987734f6c7f459a87d67f9c65e2bad8dd1a4532
eunseongu <rndmstjs75@gmail.com>
2025-02-16 17:11:11 +0900

    refactor : 불필요한 상수(HYPHEN) 제거

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,7 +9,6 @@ import java.util.Map;
 public class OutputView {
     private static final String PURCHASE_RESULT = "%s개를 구매헀습니다.";
     private static final String WINNING_STATISTIC_MESSAGE = "당첨 통계";
-    private static final String HYPHEN = "-";
     private static final String WINNING_RESULT = "%s개";
     private static final String RATE_MESSAGE = "총 수익률은 %,.2f입니다.";
 
@@ -33,11 +32,11 @@ public class OutputView {
 
     public void printWinningStatistic(Map<Rank, Integer> result) {
         printMessage(WINNING_STATISTIC_MESSAGE);
-        printMessage(HYPHEN.repeat(9));
+        printMessage("-".repeat(9));
 
         for (Rank rank : result.keySet()) {
             if (!rank.getDescription().isBlank()) {
-                System.out.print(rank.getDescription() + " " + HYPHEN + " ");
+                System.out.print(rank.getDescription() + " - ");
                 System.out.printf(WINNING_RESULT, result.get(rank));
                 changeLine();
             }
