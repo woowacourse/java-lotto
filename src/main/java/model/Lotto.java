c8ec506680f8922e0cc1256ef6fb2e6280f3dbb3
unh6unh6 <ruddbs3308@naver.com>
2025-02-17 01:50:57 +0900

    feat(Lotto) : 로또 번호가 6자리가 아니라면 예외를 발생시킨다

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,16 +1,15 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
+    public static final String INVALID_LOTTO_ERROR_MESSAGE = "로또 번호는 중복되지않는 1~45 범위의 6자리 숫자이여야 합니다.";
     public static final int LOTTO_SIZE = 6;
     private final List<Integer> lotto;
 
     public Lotto(final List<Integer> lotto) {
+        validateLotto(lotto);
         this.lotto = lotto;
     }
 
@@ -18,4 +17,21 @@ public class Lotto {
         return Collections.unmodifiableList(lotto);
     }
 
+    private void validateLotto(final List<Integer> lotto) {
+        validateLottoSize(lotto);
+    }
+
+    private void validateLottoSize(final List<Integer> lotto) {
+        if (lotto.size() != 6) {
+            throw new IllegalArgumentException(INVALID_LOTTO_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateLottoNumberRange(final List<Integer> lotto) {
+
+    }
+
+    private void validateLottoNumberNotDuplicated(final List<Integer> lotto) {
+
+    }
 }
--- src/test/java/model/LottoTest.java
+++ src/test/java/model/LottoTest.java
@@ -1,5 +1,8 @@
 package model;
 
+import static model.Lotto.INVALID_LOTTO_ERROR_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -7,17 +10,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 class LottoTest {
 
-    @Disabled
-    @DisplayName("로또 번호 생성 테스트")
+    @DisplayName("로또 번호가 6자리가 아니라면 예외를 발생시킨다")
     @Test
-    void 로또_번호_생성_테스트() {
-        List<Integer> lotto = null;
-        Assertions.assertThat(lotto.size()).isEqualTo(6);
-        for (int number : lotto) {
-            Assertions.assertThat(number).isBetween(1, 45);
-        }
+    void lottoSizeTest() {
+        assertAll(
+                () -> assertThatThrownBy(
+                        () -> new Lotto(List.of(1,2,3,4,5,6,7)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(INVALID_LOTTO_ERROR_MESSAGE),
+                () -> assertThatThrownBy(
+                        () -> new Lotto(List.of(1,2,3,4,5)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(INVALID_LOTTO_ERROR_MESSAGE)
+        );
     }
 }
