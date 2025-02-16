941a0ede8da1e695a6a5155aaacf72fec33ddbad
Jin409 <sualng123@gmail.com>
2025-02-16 18:27:07 +0900

    test : 로또의 결과 등수를 계산할 때 모든 테스트 케이스에 대해서 이뤄지도록 테스트 수정

--- src/test/java/model/LottoRankTest.java
+++ src/test/java/model/LottoRankTest.java
@@ -2,18 +2,34 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import model.rank.LottoRank;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoRankTest {
 
-    @Test
-    void 로또티켓의_당첨_결과를_계산한다() {
+    private static Stream<Arguments> provideOverlappedConditionsAndExpectedRanks() {
+        return Stream.of(
+                Arguments.of(6, false, LottoRank.FIRST),
+                Arguments.of(5, true, LottoRank.SECOND),
+                Arguments.of(5, false, LottoRank.THIRD),
+                Arguments.of(4, false, LottoRank.FOURTH),
+                Arguments.of(3, false, LottoRank.FIFTH),
+                Arguments.of(2, false, null)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideOverlappedConditionsAndExpectedRanks")
+    void 로또티켓의_당첨_결과를_계산한다(int overlappedCount, boolean isBonusNumberOverlapped, LottoRank expected) {
         // when
-        LottoRank rank = LottoRank.findByMatchCondition(5, true);
+        LottoRank actual = LottoRank.findByMatchCondition(overlappedCount, isBonusNumberOverlapped);
 
         // then
-        assertThat(rank).isEqualTo(LottoRank.SECOND);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
