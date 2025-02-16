2a2b46b3ee601ba808b66aef809cf0c49d4b739c
eunseongu <rndmstjs75@gmail.com>
2025-02-16 19:44:47 +0900

    refactor : 메소드명을 구체적으로 변경

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -23,41 +23,41 @@ public class LottoController {
 
     public void run() {
         int purchaseAmount = inputView.purchaseAmountInput();
-        Ticket ticket = ticketProcess(purchaseAmount);
-        List<Lotto> lottoBundle = lottoProcess(ticket);
-        WinningInfo winningInfo = winningInfoProcess();
-        Map<Rank, Integer> rankResult = calculateRankProcess(winningInfo, lottoBundle);
-        profitProcess(rankResult, purchaseAmount);
+        Ticket ticket = generateTicket(purchaseAmount);
+        List<Lotto> lottoBundle = generateLottoBundle(ticket);
+        WinningInfo winningInfo = generateWinningInfo();
+        Map<Rank, Integer> rankResult = calculateMatchingRank(winningInfo, lottoBundle);
+        calculateProfit(rankResult, purchaseAmount);
     }
 
-    private Ticket ticketProcess(int purchaseAmount) {
+    private Ticket generateTicket(int purchaseAmount) {
         Ticket ticket = lottoService.createTicket(purchaseAmount);
         outputView.printPurchaseResult(ticket);
         return ticket;
     }
 
-    private void profitProcess(Map<Rank, Integer> rankResult, int purchaseAmount) {
-        double calculateRate = lottoService.calculateProfit(rankResult, purchaseAmount);
-        outputView.printProfit(calculateRate);
+    private List<Lotto> generateLottoBundle(Ticket ticket) {
+        lottoService.createLottoBundleForTicket(ticket);
+        List<Lotto> lottoBundle = lottoService.getLottoBundle();
+        outputView.printLottos(lottoBundle);
+        return lottoBundle;
     }
 
-    private Map<Rank, Integer> calculateRankProcess(WinningInfo winningInfo, List<Lotto> lottoBundle) {
-        Map<Rank, Integer> rankResult = lottoService.calculateMatchingRank(winningInfo, lottoBundle);
-        outputView.printWinningStatistic(rankResult);
-        return rankResult;
-    }
-
-    private WinningInfo winningInfoProcess() {
+    private WinningInfo generateWinningInfo() {
         String winningNumbers = inputView.winningNumbersInput();
         Lotto lotto = lottoService.createLotto(winningNumbers);
         int bonusNumber = inputView.bonusNumberInput();
         return lottoService.createWinningNumber(lotto, bonusNumber);
     }
 
-    private List<Lotto> lottoProcess(Ticket ticket) {
-        lottoService.createLottoBundleForTicket(ticket);
-        List<Lotto> lottoBundle = lottoService.getLottoBundle();
-        outputView.printLottos(lottoBundle);
-        return lottoBundle;
+    private Map<Rank, Integer> calculateMatchingRank(WinningInfo winningInfo, List<Lotto> lottoBundle) {
+        Map<Rank, Integer> rankResult = lottoService.calculateMatchingRank(winningInfo, lottoBundle);
+        outputView.printWinningStatistic(rankResult);
+        return rankResult;
+    }
+
+    private void calculateProfit(Map<Rank, Integer> rankResult, int purchaseAmount) {
+        double calculateRate = lottoService.calculateProfit(rankResult, purchaseAmount);
+        outputView.printProfit(calculateRate);
     }
 }
