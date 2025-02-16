ccc8e01f7d1f81623ebcf061c5199108f5d2f3ef
yesjuhee <juhee0924@g.skku.edu>
2025-02-16 12:38:15 +0900

    refactor : 사용하지 않는 코드 삭제

--- src/main/java/controller/WinLottoController.java
+++ src/main/java/controller/WinLottoController.java
@@ -3,10 +3,8 @@ package controller;
 import java.util.List;
 import model.WinLotto;
 import view.InputView;
-import view.OutputView;
 
 public class WinLottoController {
-    private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
 
     public WinLotto winLotto() {
