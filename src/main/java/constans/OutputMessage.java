c8b3e910ed4428824e2bae60247d8273766759e9
changuii <rhljh7410@gmail.com>
2025-02-16 09:17:07 +0900

    refactor : constans의 enum의 작성 컨벤션을 통일하라

--- src/main/java/constans/OutputMessage.java
+++ src/main/java/constans/OutputMessage.java
@@ -2,12 +2,12 @@ package constans;
 
 public enum OutputMessage {
 
-    // input
+    // ConsoleInputView
     INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     INPUT_WINNING_NUMBERS("\n지난 주 당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 볼을 입력해 주세요."),
 
-    // output
+    // ConsoleOutputView
     LOTTO_PURCHASE_COUNT("%d개를 구매했습니다.\n"),
     LOTTO_WINNING_RESULT_TITLE("\n당첨 통계\n---------"),
     LOTTO_WINNING_RESULT_MATCH("%d개 일치%s(%d원)- %d개\n"),
