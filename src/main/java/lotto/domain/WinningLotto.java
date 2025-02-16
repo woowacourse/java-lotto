cb3438343c88d1d5e2783446037de90f9ebfe58c
KJungW <sagakkb0707@naver.com>
2025-02-16 18:13:00 +0900

    refactor(winning_lotto): 검증 로직 추가
    작업 내용
    - WinningLotto 관련 검증은 스스로 관리하도록 수정

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.view.constant.ExceptionMessage;
 
 public class WinningLotto {
 
@@ -8,6 +9,8 @@ public class WinningLotto {
     private final int bonusNumber;
 
     public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        validateRange(bonusNumber);
+        validateOverlap(winningNumbers, bonusNumber);
         this.lotto = new Lotto(winningNumbers);
         this.bonusNumber = bonusNumber;
     }
@@ -16,5 +19,20 @@ public class WinningLotto {
         int matches = this.lotto.findMatches(lotto);
         boolean isBonusMatched = lotto.hasNumber(bonusNumber);
         return WinningTier.find(matches, isBonusMatched);
+    }
+
+
+    public static void validateRange(int bonusNumber) {
+        boolean isOutOfRange = bonusNumber < Lotto.MIN_LOTTO_NUMBER || bonusNumber > Lotto.MAX_LOTTO_NUMBER;
+        if (isOutOfRange) {
+            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_RANGE.getContent());
+        }
+    }
+
+    public static void validateOverlap(List<Integer> winningNumbers, int bonusNumber) {
+        boolean isOverlap = winningNumbers.contains(bonusNumber);
+        if (isOverlap) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_NUMBERS.getContent());
+        }
     }
 }
