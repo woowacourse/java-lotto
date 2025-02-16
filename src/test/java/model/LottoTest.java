a36f473a0307d9cb53fe873c4b57d9961607dbaf
junhaa <2171326@hansung.ac.kr>
2025-02-16 16:12:48 +0900

    test: 당첨 번호 검증에 대한 테스트 코드 작성
    - 사용자가 입력한 당첨 번호의 개수, 중복 여부를 검증

--- src/test/java/model/LottoTest.java
+++ src/test/java/model/LottoTest.java
@@ -2,9 +2,7 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,29 +11,11 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
 
-    @Test
-    @DisplayName("당첨 번호가 중복되면 예외가 발생한다.")
-    void 당첨_번호가_중복되면_예외가_발생한다() {
-        List<Integer> winningNumbers = List.of(1, 1, 2, 3, 4, 5);
-        assertThatThrownBy(() -> new Lotto(new HashSet<>(winningNumbers)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .withFailMessage("번호는 중복없이 6개의 숫자여야 합니다.");
-    }
-
-    @Test
-    @DisplayName("당첨 번호가 6개가 아니면 예외가 발생한다.")
-    void 당첨_번호가_6개가_아니면_예외가_발생한다() {
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4);
-        assertThatThrownBy(() -> new Lotto(new HashSet<>(winningNumbers)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .withFailMessage("번호는 중복없이 6개의 숫자여야 합니다.");
-    }
-
-    static Stream<Set<Integer>> outOfRangeNumbers() {
+    static Stream<List<Integer>> outOfRangeNumbers() {
         return Stream.of(
-                Set.of(1, 2, 3, 4, 5, 46),
-                Set.of(-1, 2, 3, 4, 5, 6),
-                Set.of(-1, -2, -3, -4, -5, -6)
+                List.of(1, 2, 3, 4, 5, 46),
+                List.of(-1, 2, 3, 4, 5, 6),
+                List.of(-1, -2, -3, -4, -5, -6)
         );
     }
 
@@ -48,5 +28,22 @@ class LottoTest {
                 .hasMessage("숫자는 1~45 사이여야 합니다.");
     }
 
+    @Test
+    @DisplayName("당첨 번호가 중복되면 예외가 발생한다.")
+    void 당첨_번호가_중복되면_예외가_발생한다() {
+        List<Integer> inputNumbers = List.of(1, 1, 2, 3, 4, 5);
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("당첨 번호는 중복될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("당첨 번호의 수가 6개가 아니면 예외가 발생한다.")
+    void 당첨_번호의_수가_6개가_아니면_예외가_발생한다() {
+        List<Integer> inputNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("번호는 6개의 숫자여야 합니다.");
+    }
 
 }
