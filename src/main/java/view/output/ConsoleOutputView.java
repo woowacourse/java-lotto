147abfd034f85804ea2229ce185abc0eb251c7cc
jeyong <joon6093@naver.com>
2025-02-16 20:20:29 +0900

    refactor: output message를 enum으로 관리하지 않도록 변경

--- src/main/java/view/output/ConsoleOutputView.java
+++ src/main/java/view/output/ConsoleOutputView.java
@@ -1,6 +1,5 @@
 package view.output;
 
-import constans.OutputMessage;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +7,17 @@ import model.LottoRank;
 
 public class ConsoleOutputView implements OutputView {
 
+    private static final String LOTTO_PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String LOTTO_WINNING_RESULT_TITLE_MESSAGE = "\n당첨 통계\n---------";
+    private static final String LOTTO_WINNING_RESULT_MATCH_MESSAGE = "%d개 일치%s(%d원)- %d개\n";
+    private static final String LOTTO_WINNING_RESULT_BONUS_BALL_MESSAGE = ", 보너스 볼 일치";
+    private static final String LOTTO_REVENUE_MESSAGE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)\n";
+    private static final String LOTTO_REVENUE_DAMAGE_MESSAGE = "손해";
+    private static final String LOTTO_REVENUE_PROFIT_MESSAGE = "이익";
+
     @Override
     public void printPurchaseQuantity(final int purchaseQuantity) {
-        System.out.printf(OutputMessage.LOTTO_PURCHASE_COUNT.toString(), purchaseQuantity);
+        System.out.printf(LOTTO_PURCHASE_COUNT_MESSAGE, purchaseQuantity);
     }
 
     @Override
@@ -24,7 +31,7 @@ public class ConsoleOutputView implements OutputView {
         final Map<LottoRank, Integer> lottoRanks,
         final boolean isDamage
     ) {
-        System.out.println(OutputMessage.LOTTO_WINNING_RESULT_TITLE);
+        System.out.println(LOTTO_WINNING_RESULT_TITLE_MESSAGE);
         Arrays.stream(LottoRank.values())
             .filter(rank -> rank != LottoRank.FAIL)
             .forEach(rank -> printLottoResult(rank, lottoRanks));
@@ -37,9 +44,9 @@ public class ConsoleOutputView implements OutputView {
     ) {
         String bonusBallMessage = " ";
         if (rank.isBonusBallMatch()) {
-            bonusBallMessage = OutputMessage.LOTTO_WINNING_RESULT_BONUS_BALL.toString();
+            bonusBallMessage = LOTTO_WINNING_RESULT_BONUS_BALL_MESSAGE;
         }
-        System.out.printf(OutputMessage.LOTTO_WINNING_RESULT_MATCH.toString(), rank.getMatchCount(), bonusBallMessage,
+        System.out.printf(LOTTO_WINNING_RESULT_MATCH_MESSAGE, rank.getMatchCount(), bonusBallMessage,
             rank.getPrizeMoney(), resultRanks.getOrDefault(rank, 0));
     }
 
@@ -47,11 +54,11 @@ public class ConsoleOutputView implements OutputView {
         final double revenueRate,
         final boolean isDamage
     ) {
-        String revenueDescription = OutputMessage.LOTTO_REVENUE_PROFIT.toString();
+        String revenueDescription = LOTTO_REVENUE_PROFIT_MESSAGE;
         if (isDamage) {
-            revenueDescription = OutputMessage.LOTTO_REVENUE_DAMAGE.toString();
+            revenueDescription = LOTTO_REVENUE_DAMAGE_MESSAGE;
         }
-        System.out.printf(OutputMessage.LOTTO_REVENUE.toString(), revenueRate, revenueDescription);
+        System.out.printf(LOTTO_REVENUE_MESSAGE, revenueRate, revenueDescription);
     }
 
     @Override
