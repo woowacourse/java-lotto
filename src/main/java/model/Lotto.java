c9fb6ace1fcad24e9b4893f50048809b03091469
unh6unh6 <ruddbs3308@naver.com>
2025-02-17 01:29:55 +0900

    refactor : Lotto가 로또 번호를 외부에서 받도록 변경

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -10,20 +10,12 @@ public class Lotto {
     public static final int LOTTO_SIZE = 6;
     private final List<Integer> lotto;
 
-    public Lotto() {
-        this.lotto = generateLotto();
+    public Lotto(final List<Integer> lotto) {
+        this.lotto = lotto;
     }
 
     public List<Integer> getLotto() {
         return Collections.unmodifiableList(lotto);
     }
 
-    private List<Integer> generateLotto() {
-        Set<Integer> lotto = new HashSet<>();
-        while (lotto.size() < LOTTO_SIZE) {
-            int number = (int) (Math.random() * 45) + 1;
-            lotto.add(number);
-        }
-        return new ArrayList<>(lotto);
-    }
 }
--- src/test/java/model/LottoTest.java
+++ src/test/java/model/LottoTest.java
@@ -4,14 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+
+    @Disabled
     @DisplayName("로또 번호 생성 테스트")
     @Test
     void 로또_번호_생성_테스트() {
-        List<Integer> lotto = new Lotto().getLotto();
+        List<Integer> lotto = null;
         Assertions.assertThat(lotto.size()).isEqualTo(6);
         for (int number : lotto) {
             Assertions.assertThat(number).isBetween(1, 45);
