73abf2c1d59164a1985b52184534c09080ca77a0
RIWON <121172065+awrion3@users.noreply.github.com>
2025-02-18 00:49:17 +0900

    refactor(ObjectCreator): simplify object creation method

--- src/main/java/lotto/util/ObjectCreator.java
+++ src/main/java/lotto/util/ObjectCreator.java
@@ -1,25 +1,17 @@
 package lotto.util;
 
-import java.util.Optional;
 import java.util.function.Supplier;
 import lotto.view.OutputView;
 
 public class ObjectCreator {
-    public static <T> Optional<T> useInputToCreateObject(Supplier<T> creator) {
-        try {
-            return Optional.of(creator.get());
-        } catch (IllegalArgumentException e) {
-            OutputView.writeErrorMessage(e);
-            return Optional.empty();
+    
+    public static <T> T repeatUntilSuccess(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                OutputView.writeErrorMessage(e);
+            }
         }
-    }
-
-    public static <T> T repeatUntilSuccess(Supplier<Optional<T>> supplier) {
-        Optional<T> result = Optional.empty();
-
-        while (result.isEmpty()) {
-            result = supplier.get();
-        }
-        return result.get();
     }
 }
