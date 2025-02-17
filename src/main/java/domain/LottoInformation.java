f4f2048c5662fc374adbdefc55763cbd3aa04015
supernovaMK <minkikim2002@gmail.com>
2025-02-17 10:31:11 +0900

    refactor(LottoInformation): 중복되는 상수 삭제

--- src/main/java/domain/LottoInformation.java
+++ src/main/java/domain/LottoInformation.java
@@ -6,6 +6,5 @@ public class LottoInformation {
 
     public static final int LOTTO_NUMBER_START = 1;
     public static final int LOTTO_NUMBER_END = 45;
-    public static final int LOTTO_NUMBER_COUNT = 6;
 
 }
