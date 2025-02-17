17003236defe30e8473869465d0c1749b998ca01
taek2222 <oot309@daum.net>
2025-02-17 09:14:46 +0900

    refactor: 금액 예외 처리 이동
    - 독단적 테스트로, 공통 예외 처리에서 객체로 이동했습니다.

--- src/main/java/model/Ticket.java
+++ src/main/java/model/Ticket.java
@@ -3,8 +3,7 @@ package model;
 import dto.LottoResultResponse;
 import dto.TicketAmountResponse;
 
-import static global.utils.Validator.validateDivisibility;
-import static global.utils.Validator.validateRange;
+import static global.utils.Validator.validateNumberRange;
 
 public class Ticket {
 
@@ -37,5 +36,11 @@ public class Ticket {
 
     private double calculateROI(final int totalPrice) {
         return Math.round(((double) totalPrice / purchaseMoney) * 100) / 100.0;
+    }
+
+    private void validateDivisibility(final int number) {
+        if (number % TICKET_PRICE != 0) {
+            throw new IllegalArgumentException("금액은 " + TICKET_PRICE + "원 단위로 나누어 떨어져야 합니다.");
+        }
     }
 }
