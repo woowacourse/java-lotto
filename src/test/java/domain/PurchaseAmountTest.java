fbee8d0af5e319a4f0c82434e51c7df9d02243ac
DongchannN <ldc991104@gmail.com>
2025-02-17 13:22:39 +0900

    test: PurchaseAmount 모델에 대한 테스트 케이스 작성

---
+++ src/test/java/domain/PurchaseAmountTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.properties.LottoProperties;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PurchaseAmountTest {
+
+    @DisplayName("로또 가격에 따른 살 수 있는 로또 개수 반환")
+    @Test
+    void availableLottoQuantityTest() {
+        int money = 12300;
+
+        PurchaseAmount purchaseAmount = PurchaseAmount.of(money);
+        int quantity = purchaseAmount.calculateAvailableQuantity(LottoProperties.PRICE);
+
+        assertThat(quantity).isEqualTo(12);
+    }
+}
