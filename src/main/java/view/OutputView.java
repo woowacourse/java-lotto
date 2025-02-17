f7f3b0363197d6b9cb16accfb08731d1db066688
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-17 17:44:41 +0900

    refactor(LottoController, OutputView): view, model 의존성 제거 리팩토링

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,10 +1,8 @@
 package view;
 
 import domain.Lotto;
-import domain.LottoRank;
+import dto.WinningRecipe;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class OutputView {
@@ -15,51 +13,61 @@ public class OutputView {
     private static final String ASK_INPUT_WINNING_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String ASK_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
     private static final String WINNING_RESULT_HEADER = "당첨 통계" + LINE_SEPARATOR + "---------";
+    private static final String WINNING_RECIPE = "%d개 일치 (%,d원) - %d개";
+    private static final String WINNING_RECIPE_WITH_BONUS = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
     private static final String WINNING_PROFIT = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)";
 
     public void printAskInputMoney() {
-        System.out.println(ASK_INPUT_MONEY);
+        printlnMessage(ASK_INPUT_MONEY);
     }
 
     public void printUserLottos(final List<Lotto> lottos) {
         final String message = lottos.stream()
                 .map(lotto -> lotto.getNumbers().toString())
                 .collect(Collectors.joining(LINE_SEPARATOR));
-        System.out.println(String.format(USER_LOTTOS_HEADER, lottos.size()) + LINE_SEPARATOR + message);
+        printlnMessage(String.format(USER_LOTTOS_HEADER, lottos.size()) + LINE_SEPARATOR + message);
     }
 
     public void printAskInputWiningLotto() {
-        System.out.println(LINE_SEPARATOR + ASK_INPUT_WINNING_LOTTO);
+        printlnMessage(LINE_SEPARATOR + ASK_INPUT_WINNING_LOTTO);
     }
 
     public void printAskInputBonusNumber() {
-        System.out.println(ASK_INPUT_BONUS_NUMBER);
+        printlnMessage(ASK_INPUT_BONUS_NUMBER);
     }
 
-    public void printWinningResult(final Map<LottoRank, Integer> winningResult) {
-        System.out.println(LINE_SEPARATOR + WINNING_RESULT_HEADER);
-        for (Entry<LottoRank, Integer> entry : winningResult.entrySet()) {
-            final LottoRank lottoRank = entry.getKey();
-            final int count = entry.getValue();
-            printLottoRankResult(lottoRank, count);
-        }
+    public void printWinningResult(final List<WinningRecipe> winningRecipes) {
+        printlnMessage(LINE_SEPARATOR + WINNING_RESULT_HEADER);
+        final String message = winningRecipes.stream()
+                .map(this::createWinningRecipeMessage)
+                .collect(Collectors.joining(LINE_SEPARATOR));
+        printlnMessage(message);
     }
 
-    private void printLottoRankResult(final LottoRank lottoRank, final int count) {
-        if (lottoRank == LottoRank.RANK_2) {
-            System.out.printf("%d개 일치, 보너스 볼 일치(%d원) - %d개" + LINE_SEPARATOR, lottoRank.getMatchedCount(),
-                    lottoRank.getPrice(), count);
-            return;
-        }
-        System.out.printf("%d개 일치 (%d원)- %d개" + LINE_SEPARATOR
-                , lottoRank.getMatchedCount(), lottoRank.getPrice(), count);
-    }
 
     public void printWinningProfit(double profitRate) {
         String message = String.format(WINNING_PROFIT, profitRate, "이득");
         if (profitRate < 1) {
             message = String.format(WINNING_PROFIT, profitRate, "손해");
         }
+        printlnMessage(message);
+    }
+
+    private String createWinningRecipeMessage(final WinningRecipe winningRecipe) {
+        String message = WINNING_RECIPE;
+        if (isRankTwo(winningRecipe)) {
+            message = WINNING_RECIPE_WITH_BONUS;
+        }
+        return String.format(message, winningRecipe.matchCount(), winningRecipe.price(),
+                winningRecipe.matchQuantity());
+    }
+
+    private boolean isRankTwo(final WinningRecipe winningRecipe) {
+        return winningRecipe.rank() == 2;
+    }
+
+    private void printlnMessage(final String message) {
         System.out.println(message);
     }
+
 }
