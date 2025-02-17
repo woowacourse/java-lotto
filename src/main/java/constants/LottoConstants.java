1e0b7273b90a9c9d1d88df3dd0655bf0227f4d83
Hacanna42 <dtmdgus901@gmail.com>
2025-02-17 18:44:33 +0900

    refactor: 상수 클래스를 생성할 수 없도록 변경

--- src/main/java/constants/LottoConstants.java
+++ src/main/java/constants/LottoConstants.java
@@ -4,4 +4,7 @@ public class LottoConstants {
     public static final int LOTTO_PRICE = 1000;
     public static final int LOTTO_NUMBERS_SIZE_THRESHOLD = 6;
     public static final int LOTTO_NUMBER_MAX_THRESHOLD = 45;
+
+    private LottoConstants() {
+    }
 }
