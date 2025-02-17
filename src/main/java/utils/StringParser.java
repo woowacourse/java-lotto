860448a8a488bc876ac9416dad0057c1247e9070
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-17 17:19:35 +0900

    refactor(StringParser): 입력 공백 제거하는 부분 추가

--- src/main/java/utils/StringParser.java
+++ src/main/java/utils/StringParser.java
@@ -6,13 +6,15 @@ import java.util.List;
 
 public class StringParser {
 
+    private static final String SPACE = " ";
+
     private StringParser() {
 
     }
 
     public static int parseToNumber(final String input, final ExceptionMessage exceptionMessage) {
         try {
-            return Integer.parseInt(input);
+            return Integer.parseInt(input.replace(SPACE, ""));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(exceptionMessage.getMessage());
         }
@@ -20,7 +22,7 @@ public class StringParser {
 
     public static List<Integer> parseToNumbers(final String input, final String delimiter,
                                                final ExceptionMessage exceptionMessage) {
-        return Arrays.stream(input.split(delimiter))
+        return Arrays.stream(input.replace(SPACE, "").split(delimiter))
                 .map(token -> parseToNumber(token, exceptionMessage))
                 .toList();
     }
