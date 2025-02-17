f6e04cdadc48b53754722fb1f2106e9ed71173e1
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-17 17:51:07 +0900

    test(WinningNumberTest): 1등, 3등, 4등, 5등 테스트 추가

--- src/test/java/domain/WinningNumberTest.java
+++ src/test/java/domain/WinningNumberTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -11,9 +12,24 @@ class WinningNumberTest {
     @DisplayName("로또 당첨 결과 계산을 성공한다")
     @Test
     void calculateRankByLottoTest() {
-        final Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        final Lotto userLotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        final Lotto userLotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        final Lotto userLotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 11));
+        final Lotto userLotto4 = new Lotto(List.of(1, 2, 3, 4, 11, 13));
+        final Lotto userLotto5 = new Lotto(List.of(1, 2, 3, 11, 12, 13));
         final WinningNumber winningNumber = new WinningNumber(new Lotto(List.of(1, 2, 3, 4, 5, 7)), new BonusNumber(6));
-        final LottoRank lottoRank = winningNumber.calculateRankByLotto(userLotto);
-        assertEquals(LottoRank.RANK_2, lottoRank);
+        final LottoRank lottoRank1 = winningNumber.calculateRankByLotto(userLotto1);
+        final LottoRank lottoRank2 = winningNumber.calculateRankByLotto(userLotto2);
+        final LottoRank lottoRank3 = winningNumber.calculateRankByLotto(userLotto3);
+        final LottoRank lottoRank4 = winningNumber.calculateRankByLotto(userLotto4);
+        final LottoRank lottoRank5 = winningNumber.calculateRankByLotto(userLotto5);
+
+        assertAll(
+                () -> assertThat(lottoRank1).isEqualTo(LottoRank.RANK_2),
+                () -> assertThat(lottoRank2).isEqualTo(LottoRank.RANK_1),
+                () -> assertThat(lottoRank3).isEqualTo(LottoRank.RANK_3),
+                () -> assertThat(lottoRank4).isEqualTo(LottoRank.RANK_4),
+                () -> assertThat(lottoRank5).isEqualTo(LottoRank.RANK_5)
+        );
     }
 }
