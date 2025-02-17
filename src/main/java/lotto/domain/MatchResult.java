bff75e84c57f215d5ecf5363fb7937e9a14b217a
YuyoungRhee <bbclara@naver.com>
2025-02-18 00:26:28 +0900

    refactor: 사용하지 않는 객체와 테스트 삭제

--- src/main/java/lotto/domain/MatchResult.java
+++
@@ -1,7 +0,0 @@
-package lotto.domain;
-
-import static lotto.common.exception.ErrorMessage.ERROR_MATCH_NUMBER_NOT_VALID;
-import static lotto.domain.Lotto.LOTTO_SIZE;
-
-public record MatchResult(int matchCount, boolean isBonusMatched) {
-}
