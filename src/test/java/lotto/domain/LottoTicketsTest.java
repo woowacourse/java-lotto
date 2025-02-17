ab76aa674e77fdd37801c222687e1b2a0da628ca
yeonnhuu <yeonnhuu@gmail.com>
2025-02-17 10:42:59 +0900

    :sparkles: feat(LottoTickets): add LottoTicketsTest

---
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTicketsTest {
+
+    @DisplayName("로또 티켓 추가 확인")
+    @Test
+    public void lottoTicketsAdd() {
+        Lotto lottoTicket = new Lotto("1, 2, 3, 4, 5, 6");
+        LottoTickets lottoTickets = new LottoTickets();
+        lottoTickets.addLottoTicket(lottoTicket);
+
+        assertThat(lottoTickets.getLottoTickets()).isNotEmpty();
+    }
+
+    @DisplayName("로또 티켓수 계산 확인")
+    @Test
+    public void lottoTicketsCount() {
+        Lotto lottoTicket1 = new Lotto("1, 2, 3, 4, 5, 6");
+        Lotto lottoTicket2 = new Lotto("1, 2, 3, 4, 5, 7");
+        LottoTickets lottoTickets = new LottoTickets();
+        lottoTickets.addLottoTicket(lottoTicket1);
+        lottoTickets.addLottoTicket(lottoTicket2);
+
+        assertThat(lottoTickets.getLottoTicketsCount()).isEqualTo(2);
+    }
+}
