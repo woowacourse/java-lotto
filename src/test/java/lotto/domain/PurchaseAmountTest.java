e44c51e1d63d6d8441c5aae00d1aa63f92f207d6
horizonpioneer <quantumjump10@gmail.com>
2025-02-17 11:42:20 +0900

    refactor: 구매금액 1000원 단위 테스트 방식을 AssertJ로 변경

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,10 +1,10 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PurchaseAmountTest {
 
@@ -21,12 +21,9 @@ class PurchaseAmountTest {
 
     @Test
     @DisplayName("구매 금액이 1000원 단위가 아닌 경우, 예외를 발생시킨다.")
-    void validateCollectAmount() {
-
-        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            purchaseAmount = new PurchaseAmount(25670);
-        });
-
-        assertThat(exception.getMessage()).isEqualTo("구매 금액은 1000원 단위어야 합니다.");
+    void validateUnitOfThousandWon() {
+        assertThatThrownBy(() -> new PurchaseAmount(25670))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구매 금액은 1000원 단위어야 합니다.");
     }
 }
