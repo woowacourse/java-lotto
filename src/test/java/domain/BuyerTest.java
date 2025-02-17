a37e76e0491bbb6093de6e4d3f047a9587df9d00
JO-eusan <goodsan0330@gmail.com>
2025-02-17 10:45:06 +0900

    test: buyer 테스트 추가

--- src/test/java/domain/BuyerTest.java
+++ src/test/java/domain/BuyerTest.java
@@ -10,20 +10,26 @@ public class BuyerTest {
 
     @Test
     @DisplayName("로또 당첨 개수 세기 테스트")
-    void lottoFactory_countMatchedRanks() {
+    void success_1() {
         WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
 
-        // 로또 3개 생성 (1등, 3등, 꽝)
+        // 로또 6개 생성
         Buyer buyer = new Buyer(
             List.of(
                 new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 2등
                 new Lotto(List.of(1, 2, 3, 4, 5, 10)), // 3등
+                new Lotto(List.of(1, 2, 3, 4, 9, 10)), // 4등
+                new Lotto(List.of(1, 2, 3, 8, 9, 10)), // 5등
                 new Lotto(List.of(10, 11, 12, 13, 14, 15)) // 꽝
             ));
         Map<LottoRank, Integer> result = buyer.countMatchedRanks(winningLotto);
 
         Assertions.assertThat(result.get(LottoRank.FIRST_PLACE)).isEqualTo(1);
+        Assertions.assertThat(result.get(LottoRank.SECOND_PLACE)).isEqualTo(1);
         Assertions.assertThat(result.get(LottoRank.THIRD_PLACE)).isEqualTo(1);
+        Assertions.assertThat(result.get(LottoRank.FOURTH_PLACE)).isEqualTo(1);
+        Assertions.assertThat(result.get(LottoRank.FIFTH_PLACE)).isEqualTo(1);
         Assertions.assertThat(result.get(LottoRank.BOOM)).isEqualTo(1);
     }
 }
