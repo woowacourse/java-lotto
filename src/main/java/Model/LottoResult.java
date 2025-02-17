fa46c32a9dcb00fa33071f63325491ee5e0e87d5
LJM\eueo8 <eueo8259@gmail.com>
2025-02-17 17:48:20 +0900

    refactor: LottoResult에 bonusBall 필드 추가 및 매칭 로직 개선

--- src/main/java/Model/LottoResult.java
+++ src/main/java/Model/LottoResult.java
@@ -2,37 +2,33 @@ package Model;
 
 public enum LottoResult {
 
-    FIFTH(5000, 0, 3),
-    FOURTH(50000, 0, 4 ),
-    THIRD(1500000, 0, 5),
-    SECOND(30000000, 0, 5),
-    FIRST(2000000000, 0, 6);
+    FIFTH(5000, 0, 3, false),
+    FOURTH(50000, 0, 4, true ),
+    THIRD(1500000, 0, 5, false),
+    SECOND(30000000, 0, 5, false),
+    FIRST(2000000000, 0, 6, false);
 
     private final int price;
     private int count;
     private final int correctNumber;
+    private final boolean bonusBall;
 
-    LottoResult(int price, int count, int correctNumber) {
+    LottoResult(int price, int count, int correctNumber, boolean bonusBall) {
         this.price = price;
         this.count = count;
         this.correctNumber = correctNumber;
+        this.bonusBall = bonusBall;
     }
 
     public static void addCount(int count, boolean isBonus) {
-        if (count == LottoResult.FIRST.correctNumber) {
-            FIRST.count++;
+        for(LottoResult lottoResult : LottoResult.values()){
+            isMatchWithBonus(count, isBonus, lottoResult);
         }
-        if (count == LottoResult.SECOND.correctNumber && isBonus) {
-            SECOND.count++;
-        }
-        if (count == LottoResult.THIRD.correctNumber && !isBonus) {
-            THIRD.count++;
-        }
-        if (count == LottoResult.FOURTH.correctNumber) {
-            FOURTH.count++;
-        }
-        if (count == LottoResult.FIFTH.correctNumber) {
-            FIFTH.count++;
+    }
+
+    private static void isMatchWithBonus(int count, boolean isBonus, LottoResult lottoResult) {
+        if (lottoResult.correctNumber == count && lottoResult.bonusBall == isBonus){
+            lottoResult.count++;
         }
     }
 
