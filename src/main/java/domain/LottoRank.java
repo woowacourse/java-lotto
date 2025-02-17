5c652074a5c819134f3449be6b4114f45b0c4ce8
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-17 17:36:23 +0900

    refactor: getter 추가

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -24,7 +24,7 @@ public enum LottoRank {
         this.isBonus = isBonus;
     }
 
-    // 일치하는 번호 갯수와 보너스 번호 일치 여부로 랭크 반환
+
     public static LottoRank findByMatchedCountAndIsBonus(final int matchedCount, final boolean isBonus) {
         return Arrays.stream(LottoRank.values())
                 .filter(rank -> rank.matchedCount == matchedCount)
@@ -33,7 +33,6 @@ public enum LottoRank {
                 .orElse(NONE);
     }
 
-    // 당첨 내역 리스트 반환
     public static List<LottoRank> findByAllWithoutNone() {
         return Arrays.stream(LottoRank.values())
                 .sorted((r1, r2) -> r2.rank - r1.rank)
@@ -46,5 +45,9 @@ public enum LottoRank {
 
     public int getMatchedCount() {
         return matchedCount;
+    }
+
+    public int getRank() {
+        return rank;
     }
 }
