49cc82f352e99d850e87454e32a3dfd47258e827
JO-eusan <goodsan0330@gmail.com>
2025-02-18 02:29:13 +0900

    refactor: 테스트 코드 리팩터링

--- src/test/java/domain/BuyerTest.java
+++ src/test/java/domain/BuyerTest.java
@@ -1,34 +1,36 @@
 package domain;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class BuyerTest {
 
-    @Test
+    @ParameterizedTest
     @DisplayName("로또 당첨 개수 세기 테스트")
-    void success_1() {
+    @CsvSource({
+        "1, 2, 3, 4, 5, 6, FIRST_PLACE",
+        "1, 2, 3, 4, 5, 7, SECOND_PLACE",
+        "1, 2, 3, 4, 5, 10, THIRD_PLACE",
+        "1, 2, 3, 4, 9, 10, FOURTH_PLACE",
+        "1, 2, 3, 8, 9, 10, FIFTH_PLACE",
+        "10, 11, 12, 13, 14, 15, BOOM"
+    })
+    void success_1(int n1, int n2, int n3, int n4, int n5, int n6, LottoRank expectedRank) {
         WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-        // 로또 6개 생성
-        Buyer buyer = new Buyer(
-            List.of(
-                new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
-                new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 2등
-                new Lotto(List.of(1, 2, 3, 4, 5, 10)), // 3등
-                new Lotto(List.of(1, 2, 3, 4, 9, 10)), // 4등
-                new Lotto(List.of(1, 2, 3, 8, 9, 10)), // 5등
-                new Lotto(List.of(10, 11, 12, 13, 14, 15)) // 꽝
-            ));
-        Map<LottoRank, Integer> result = buyer.countMatchedRanks(winningLotto);
+        Buyer buyer = new Buyer(List.of(createLotto(n1, n2, n3, n4, n5, n6)));
 
-        Assertions.assertThat(result.get(LottoRank.FIRST_PLACE)).isEqualTo(1);
-        Assertions.assertThat(result.get(LottoRank.SECOND_PLACE)).isEqualTo(1);
-        Assertions.assertThat(result.get(LottoRank.THIRD_PLACE)).isEqualTo(1);
-        Assertions.assertThat(result.get(LottoRank.FOURTH_PLACE)).isEqualTo(1);
-        Assertions.assertThat(result.get(LottoRank.FIFTH_PLACE)).isEqualTo(1);
-        Assertions.assertThat(result.get(LottoRank.BOOM)).isEqualTo(1);
+        LottoStatistics lottoStatistics = buyer.countMatchedRanks(winningLotto);
+        Map<LottoRank, Integer> result = lottoStatistics.getLottoCounter();
+
+        Assertions.assertThat(result.get(expectedRank)).isEqualTo(1);
+    }
+
+    private static Lotto createLotto(Integer... numbers) {
+        return new Lotto(Arrays.stream(numbers).toList());
     }
 }
