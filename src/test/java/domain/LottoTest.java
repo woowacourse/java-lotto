6a4bf639298f6d4658fa340bcb9d766926088784
mimi <110226866+hongkikii@users.noreply.github.com>
2025-02-17 22:11:26 +0900

    refactor : @MethodSource 사용

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -1,9 +1,8 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
@@ -23,7 +22,7 @@ public class LottoTest {
     @DisplayName("로또 한 개는 여섯 개가 아닌 숫자로 이루어져 있을 시 예외가 발생한다")
     @ParameterizedTest
     @MethodSource
-    void containsSixNumbersInlotto(final List<Integer> numbers) {
+    void containsSixNumbersInlotto(List<Integer> numbers) {
         assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -36,19 +35,20 @@ public class LottoTest {
     }
 
     @DisplayName("로또 번호가 일 이상 사십오 이하의 숫자가 아닐 경우 예외가 발생한다")
-    @Test
-    void containsNumbersInRange() {
-        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 6)))
+    @ParameterizedTest
+    @MethodSource
+    void containsNumbersInRange(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 2_100_000_000)))
-                .isInstanceOf(IllegalArgumentException.class);
+    private static Stream<Arguments> containsNumbersInRange() {
+        return Stream.of(
+                Arguments.of(List.of(-1, 2, 3, 4, 5, 6)),
+                Arguments.of(List.of(0, 2, 3, 4, 5, 6)),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 46)),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 2_100_000_000))
+        );
     }
 
     @DisplayName("로또 번호 사이의 중복이 있을 시 예외가 발생한다.")
@@ -59,35 +59,45 @@ public class LottoTest {
     }
 
     @DisplayName("로또 번호와 일치하는 경우 True를 반환한다")
-    @Test
-    void shouldReturnTrueWhenDuplicateNumbers() {
-        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+    @ParameterizedTest
+    @MethodSource
+    void shouldReturnTrueWhenDuplicateNumbers(List<Integer> numbers) {
+        List<Integer> targetNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
 
-        Assertions.assertThat(lotto1.isSameWith(Arrays.asList(1, 2, 3, 4, 5, 6))).isTrue();
+        assertThat(lotto.isSameWith(targetNumbers)).isTrue();
+    }
 
-        Lotto lotto2 = new Lotto(Arrays.asList(6, 5, 4, 3, 2, 1));
-
-        Assertions.assertThat(lotto2.isSameWith(Arrays.asList(1, 2, 3, 4, 5, 6))).isTrue();
-
-        Lotto lotto3 = new Lotto(Arrays.asList(1, 3, 5, 2, 4, 6));
-
-        Assertions.assertThat(lotto3.isSameWith(Arrays.asList(1, 2, 3, 4, 5, 6))).isTrue();
+    private static Stream<Arguments> shouldReturnTrueWhenDuplicateNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6)),
+                Arguments.of(List.of(6, 5, 4, 3, 2, 1)),
+                Arguments.of(List.of(1, 3, 5, 2, 4, 6))
+        );
     }
 
     @DisplayName("로또 번호와 중복되지 않는 번호를 가진 경우 False를 반환한다")
     @Test
     void shouldReturnFalseWhenNoDuplicateNumbers() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
-        Assertions.assertThat(lotto.isSameWith(Arrays.asList(1, 2, 3, 4, 5, 7))).isFalse();
+        assertThat(lotto.isSameWith(List.of(1, 2, 3, 4, 5, 7))).isFalse();
     }
 
     @DisplayName("로또 번호와 다른 갯수의 숫자가 입력될 경우 False를 반환한다")
-    @Test
-    void shouldReturnFalseWhenCountDifferent() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+    @ParameterizedTest
+    @MethodSource
+    void shouldReturnFalseWhenCountDifferent(List<Integer> targetNumbers) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
-        Assertions.assertThat(lotto.isSameWith(Arrays.asList(1, 2, 3, 4, 5))).isFalse();
-        Assertions.assertThat(lotto.isSameWith(Arrays.asList(1, 2, 3, 4, 5, 6, 7))).isFalse();
+        assertThat(lotto.isSameWith(targetNumbers)).isFalse();
+        assertThat(lotto.isSameWith(targetNumbers)).isFalse();
+    }
+
+    private static Stream<Arguments> shouldReturnFalseWhenCountDifferent() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5)),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7))
+        );
     }
 }
