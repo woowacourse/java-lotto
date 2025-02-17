f873c9409cce4cb71de599b36974b9b5e5b3d04b
supernovaMK <minkikim2002@gmail.com>
2025-02-17 10:28:52 +0900

    feat(InputView): input에 관한 역할을 담당하는 클래스 구현

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,13 +1,11 @@
 package controller;
 
-import enums.LottoRanking;
 import domain.*;
+import enums.LottoRanking;
+import view.InputView;
 import view.OutputView;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -16,60 +14,40 @@ import java.util.stream.Collectors;
 public class LottoController {
 
     public void start() throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        
-        Price price = getPrice(bufferedReader);
+        Price price = InputView.getPrice();
 
         LottoMachine lottoMachine = buyLottos(price);
-        Lottos lottos = printLottos(lottoMachine);
+        Lottos lottos = printLottos(lottoMachine, price);
 
-        LottoResult lottoResult = calculateLottoResult(bufferedReader, lottos);
+        LottoResult lottoResult = calculateLottoResult(lottos);
 
         printLottoResults(lottoResult);
         printRateOfReturn(lottoResult, price);
     }
 
-    private Price getPrice(BufferedReader bufferedReader) throws IOException {
-        System.out.println("구입금액을 입력해 주세요.");
-        return new Price(bufferedReader.readLine());
-    }
-
     private LottoMachine buyLottos(Price price) {
-        LottoMachine lottoMachine = new LottoMachine(price);
-        int ticket = lottoMachine.getTicket();
+        LottoMachine lottoMachine = new LottoMachine();
+        int ticketCount = price.getTicketCount();
 
-        OutputView.printTicketsContMessage(ticket);
+        OutputView.printTicketsContMessage(ticketCount);
 
         return lottoMachine;
     }
 
-    private Lottos printLottos(LottoMachine lottoMachine) {
-        Lottos lottos = lottoMachine.generateLottos();
+    private Lottos printLottos(LottoMachine lottoMachine, Price price) {
+        Lottos lottos = lottoMachine.generateLottos(price.getTicketCount());
 
         OutputView.printLottos(lottos);
 
         return lottos;
     }
 
-    private LottoResult calculateLottoResult(BufferedReader bufferedReader, Lottos lottos) throws IOException {
+    private LottoResult calculateLottoResult(Lottos lottos) throws IOException {
         WinningNumberWithBonusNumber winningNumberWithBonusNumber
-                = new WinningNumberWithBonusNumber(getWinningNumber(bufferedReader), getBonusNumber(bufferedReader));
+                = new WinningNumberWithBonusNumber(InputView.getWinningNumber(), InputView.getBonusNumber());
 
         LottoResult lottoResult = LottoWinningChecker.calculateResult(lottos, winningNumberWithBonusNumber);
         return lottoResult;
-    }
-
-    private Lotto getWinningNumber(BufferedReader bufferedReader) throws IOException {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        Numbers numbers = new Numbers(Arrays.stream(bufferedReader.readLine().split(",")).map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList()));
-        return new Lotto(numbers);
-    }
-
-    private int getBonusNumber(BufferedReader bufferedReader) throws IOException {
-        System.out.println("보너스 볼을 입력해 주세요.");
-        return Integer.parseInt(bufferedReader.readLine());
     }
 
     private void printLottoResults(LottoResult lottoResult) {
@@ -88,6 +66,5 @@ public class LottoController {
         double rateOfReturn = lottoResult.getLottoRate(price.getValue());
 
         OutputView.printRateOfReturn(rateOfReturn);
-
     }
 }
