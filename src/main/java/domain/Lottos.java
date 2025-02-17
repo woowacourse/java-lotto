dee644ddfa93e841f60d738b5c4be4545a32cd8c
Dompoo <dlckdrms0517@gmail.com>
2025-02-17 17:41:26 +0900

    refactor: 구매를 Lottos에서 수행하도록 리팩터링

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -1,30 +1,40 @@
 package domain;
 
 import dto.LottoMatchResult;
-
+import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import util.NumberPicker;
 
 public class Lottos {
-    
+
     private final List<Lotto> lottos;
-    
+
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
     }
-    
+
+    public static Lottos purchase(final int purchaseMoney, final NumberPicker numberPicker) {
+        final int purchaseableCount = Lotto.getPurchaseableCount(purchaseMoney);
+        final List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < purchaseableCount; i++) {
+            lottos.add(Lotto.purchase(numberPicker));
+        }
+        return new Lottos(lottos);
+    }
+
     public EnumMap<LottoPrize, Integer> getStatistics(List<Integer> matchNumbers, int bonusNumber) {
         return calculateStatistics(matchNumbers, bonusNumber);
     }
-    
+
     public double getIncomeRate(List<Integer> matchNumbers, int bonusNumber, int money) {
         EnumMap<LottoPrize, Integer> prizeMap = calculateStatistics(matchNumbers, bonusNumber);
         long totalIncome = calculateTotalIncome(prizeMap);
         return (double) totalIncome / money;
     }
-    
+
     private long calculateTotalIncome(EnumMap<LottoPrize, Integer> prizeMap) {
         long sum = 0L;
         for (Map.Entry<LottoPrize, Integer> entry : prizeMap.entrySet()) {
@@ -32,13 +42,13 @@ public class Lottos {
         }
         return sum;
     }
-    
+
     private EnumMap<LottoPrize, Integer> calculateStatistics(List<Integer> matchNumbers, int bonusNumber) {
         EnumMap<LottoPrize, Integer> lottoPrizeMap = LottoPrize.getInitailizedEnumMap();
         insertLottoPrizeResult(matchNumbers, bonusNumber, lottoPrizeMap);
         return lottoPrizeMap;
     }
-    
+
     private void insertLottoPrizeResult(List<Integer> matchNumbers, int bonusNumber, EnumMap<LottoPrize, Integer> enumMap) {
         for (Lotto lotto : lottos) {
             LottoMatchResult matchResult = lotto.getMatchResult(matchNumbers, bonusNumber);
