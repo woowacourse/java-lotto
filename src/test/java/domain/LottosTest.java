b2ba13724c06f47fae429723a5016493bbdbefcd
Yeongjae Kim <sees77770@gmail.com>
2025-02-18 01:10:48 +0900

    refactor: 로또 발행 테스트 케이스 추가

--- src/test/java/domain/LottosTest.java
+++ src/test/java/domain/LottosTest.java
@@ -1,22 +1,28 @@
 package domain;
 
+import static domain.lotto.Lotto.LOTTO_PRICE;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import domain.lotto.Lottos;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottosTest {
 
-    @Test
-    void 로또를_발행한다() {
+    @ParameterizedTest
+    @ValueSource(ints = {2000, 3000, 4000})
+    void 로또를_발행한다(int money) {
+        List<Integer> dummyLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+
         Lottos lottos = Lottos.issue(
-                2000,
-                (minNumber, maxNumber, maxSize) -> List.of(1, 2, 3, 4, 5, 6)
+                money,
+                (minNumber, maxNumber, maxSize) -> dummyLottoNumbers
         );
 
         assertThat(lottos.getLottos().getFirst().getBallNumbers())
                 .isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lottos.getLottos().size()).isEqualTo(money / LOTTO_PRICE);
     }
 
 }
