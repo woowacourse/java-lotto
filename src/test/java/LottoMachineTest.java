1afbdddb5221389cc9fce9d631562133f9bff363
supernovaMK <minkikim2002@gmail.com>
2025-02-17 10:31:54 +0900

    test(LottoMachine): 클래스 변경으로 불필요한 테스트 삭제

--- src/test/java/LottoMachineTest.java
+++ src/test/java/LottoMachineTest.java
@@ -10,19 +10,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoMachineTest {
 
     @Test
-    void 로또머신은_로또_구매개수를_갖고있다() {
-        //given
-        LottoMachine lottoMachine = new LottoMachine(new Price("10000"));
-        //when & then
-        assertThat(lottoMachine.getTicket()).isEqualTo(10);
-    }
-
-    @Test
     void 로또머신은_로또를_티켓_개수만큼_생성할_수_있다() {
         //given
-        LottoMachine lottoMachine = new LottoMachine(new Price("10000"));
+        Price price = new Price("10000");
+        LottoMachine lottoMachine = new LottoMachine();
 
         //when & then
-        assertThat(lottoMachine.generateLottos().getLottos().size()).isEqualTo(lottoMachine.getTicket());
+        assertThat(lottoMachine.generateLottos(price.getTicketCount()).getLottos().size()).isEqualTo(price.getTicketCount());
     }
 }
