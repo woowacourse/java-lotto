5efb447619990af747f99bfe1f7300d7e173b0bd
Son JunHyung <kingjh1125@gmail.com>
2025-02-18 01:14:30 +0900

    refactor: stream으로 변경

--- src/main/java/domain/LottosFactory.java
+++ src/main/java/domain/LottosFactory.java
@@ -3,16 +3,16 @@ package domain;
 import domain.generator.Generator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class LottosFactory {
 
     public Lottos from(final int count, final Generator generator) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i<count; i++) {
-            lottos.add(Lotto.from(generator.generate()));
-        }
+        List<Lotto> lottos = Stream.generate(generator::generate)
+                .limit(count)
+                .map(Lotto::from)
+                .toList();
 
         return new Lottos(lottos);
     }
-
 }
