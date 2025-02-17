217ff1bf39fc43e10fc6ab29e993d04a6f438dbc
junhaa <2171326@hansung.ac.kr>
2025-02-18 01:53:07 +0900

    test: 테스트 코드 분리
    - 생성자의 로직이 복잡하기 때문에 생성자 테스트 코드를 따로 분리

--- src/test/java/model/WinningStatisticsTest.java
+++ src/test/java/model/WinningStatisticsTest.java
@@ -3,27 +3,45 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningStatisticsTest {
+    private WinningStatistics winningStatistics;
 
-    @Test
-    @DisplayName("로또 당첨 결과로 수익률을 반환한다.")
-    void 로또_당첨_결과로_수익률을_반환한다() {
+    @BeforeEach
+    void setUp() {
         Lotto lotto = new Lotto(new TestLottoNumberGenerator(List.of(1, 2, 3, 4, 5, 6)));
-        BonusNumber bonusNumber = new BonusNumber(6);
+
         // 5개 + 보너스 볼 -> 2등
+        BonusNumber bonusNumber = new BonusNumber(6);
         UserLotto userLotto = new UserLotto(List.of(1, 2, 3, 4, 5, 45), bonusNumber);
-        Wallet wallet = new Wallet(10000);
 
         LottoRepository lottoRepository = new LottoRepository();
         lottoRepository.addLotto(lotto);
 
-        WinningStatistics winningStatistics = new WinningStatistics(lottoRepository, userLotto);
+        winningStatistics = new WinningStatistics(lottoRepository, userLotto);
+    }
+
+    @Test
+    @DisplayName("WinningStatistics가 로또 결과를 정상적으로 저장한다.")
+    void WinningStatistics가_로또_결과를_정상적으로_저장한다() {
+        Map<RankType, Integer> winningResult = winningStatistics.getWinningStatistics();
+
+        assertThat(winningResult.get(RankType.SECOND)).isEqualTo(1);
+        assertThat(winningResult.get(RankType.FIRST)).isEqualTo(0);
+        assertThat(winningResult.get(RankType.THIRD)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("로또 당첨 결과로 수익률을 반환한다.")
+    void 로또_당첨_결과로_수익률을_반환한다() {
+        Wallet wallet = new Wallet(10_000);
 
         // 2등 상금(30_000_000) / 구매 금액(10_000) => 3000
-        assertThat(winningStatistics.calculateWinningRate(wallet)).isEqualTo(3000);
+        assertThat(winningStatistics.calculateWinningRate(wallet)).isEqualTo(3_000);
     }
 
 
