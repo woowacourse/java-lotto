19c3b9e2b9130fbd90d91871fda05c5110f42ca8
JO-eusan <goodsan0330@gmail.com>
2025-02-17 09:20:52 +0900

    fix: 로또 범위를 1~45로 수정

--- src/main/java/util/constant/Values.java
+++ src/main/java/util/constant/Values.java
@@ -4,7 +4,7 @@ public class Values {
 
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_MIN_NUM = 1;
-    public static final int LOTTO_MAX_NUM = 50;
+    public static final int LOTTO_MAX_NUM = 45;
     public static final int LOTTO_UNIT = 1000;
     public static final int MONEY_MIN_NUM = 0;
 }
