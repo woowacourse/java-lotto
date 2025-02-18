1216958c7918562fa0a0c339e9355ee3aa4c24e4
Mint <audwl03071@gmail.com>
2025-02-19 00:01:30 +0900

    fix(RandomLottoGenerator): 로또 생성 개수가 양수가 아닌 경우 예외 발생하도록 수정

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -9,16 +9,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class RandomLottoGenerator {
+public class RandomLottoGenerator implements LottoGenerator {
 
     private static final int START_INDEX = 0;
 
     public List<Lotto> generate(final int count) {
+        validate(count);
         return IntStream.range(0, count)
                 .mapToObj(number -> makeRandomNumbers())
                 .map(HashSet::new)
                 .map(Lotto::new)
                 .toList();
+    }
+
+    private void validate(final int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("생성할 로또의 개수는 양수여야합니다.");
+        }
     }
 
     private List<Integer> makeRandomNumbers() {
--- src/test/java/lotto/domain/RandomLottoGeneratorTest.java
+++ src/test/java/lotto/domain/RandomLottoGeneratorTest.java
@@ -2,14 +2,21 @@ package lotto.domain;
 
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RandomLottoGeneratorTest {
 
+    private RandomLottoGenerator randomLottoGenerator;
+
+    @BeforeEach
+    void setUp() {
+        randomLottoGenerator = new RandomLottoGenerator();
+    }
+
     @Test
     void 로또_개수만큼_로또를_생성한다() {
         // Given
-        final RandomLottoGenerator randomLottoGenerator = new RandomLottoGenerator();
         final int count = 3;
 
         // When
@@ -18,4 +25,16 @@ class RandomLottoGeneratorTest {
         // Then
         Assertions.assertThat(lottos.stream().distinct().count()).isEqualTo(3);
     }
+
+    @Test
+    void 로또_개수가_양수가_아니면_예외가_발생한다() {
+        // Given
+        final int count = -1;
+
+        // When & Then
+        Assertions.assertThatThrownBy(() -> randomLottoGenerator.generate(count))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("생성할 로또의 개수는 양수여야합니다.");
+    }
+
 }
