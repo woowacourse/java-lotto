57919f0888d262fad0dba2d47f4082815c830fd4
julie <7jujubuilds@gmail.com>
2025-02-19 02:22:32 +0900

    test(lotto): `LottoControllerTest` 테스트 코드 이동
    작업 내용
    - `LottoControllerTest`의 테스트 코드를 `LottosTest` 도메인 테스트로 이동

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,10 +1,6 @@
-package lotto.controller;
+package lotto.domain;
 
 import lotto.constant.WinningTier;
-import lotto.domain.Lotto;
-import lotto.domain.WinningLotto;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +9,12 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LottoControllerTest {
+class LottosTest {
 
     @DisplayName("발행된 로또의 당첨 등수를 찾을 수 있다.")
     @Test
     void 발행된_로또의_당첨_등수를_찾을_수_있다() {
-        List<Lotto> lottos = List.of(
+        List<Lotto> sampleLottos = List.of(
                 new Lotto(1, 2, 3, 4, 5, 6),
                 new Lotto(1, 2, 3, 4, 5, 7),
                 new Lotto(1, 2, 3, 4, 5, 8),
@@ -26,9 +22,9 @@ class LottoControllerTest {
                 new Lotto(1, 2, 3, 8, 9, 10),
                 new Lotto(8, 9, 10, 11, 12, 13));
         WinningLotto winningLotto = new WinningLotto(new Lotto(1, 2, 3, 4, 5, 6), 7);
-        LottoController lottoController = new LottoController(new InputView(), new OutputView());
+        Lottos lottos = new Lottos(sampleLottos);
 
-        List<WinningTier> actualTiers = lottoController.getWinningTiers(lottos, winningLotto);
+        List<WinningTier> actualTiers = lottos.getWinningTiers(winningLotto);
 
         assertThat(actualTiers).containsExactlyInAnyOrderElementsOf(Arrays.stream(WinningTier.values()).toList());
     }
