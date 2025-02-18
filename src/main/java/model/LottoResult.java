54ad8285ac586b9b248c9a92ddc56ca6d9a351b7
jhan0121 <jhan0121@gmail.com>
2025-02-18 13:24:55 +0900

    refactor: 숫자 표기법, 변수명, 대상 비교 로직 수정

--- src/main/java/model/LottoResult.java
+++ src/main/java/model/LottoResult.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public enum LottoResult {
     NOT_MATCH(0, false, 0),
-    FIFTH(3, false, 5000),
-    FOURTH(4, false, 50000),
-    THIRD(5, false, 1500000),
-    SECOND(5, true, 30000000),
-    FIRST(6, false, 2000000000);
+    FIFTH(3, false, 5_000),
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000);
 
     private final int targetCount;
     private final boolean isBonus;
@@ -20,11 +20,22 @@ public enum LottoResult {
         this.price = price;
     }
 
-    public static LottoResult findTargetResult(int count, boolean isBonus) {
+    public static LottoResult findTargetResult(int winningNumberCount, boolean isBonus) {
+        if (winningNumberCount == 5) {
+            return findTargetResultUsingBonus(isBonus);
+        }
+
         return Arrays.stream(LottoResult.values())
-                .filter(lottoResult -> lottoResult.targetCount == count && lottoResult.isBonus == isBonus)
+                .filter(lottoResult -> lottoResult.targetCount == winningNumberCount)
                 .findAny()
                 .orElse(NOT_MATCH);
+    }
+
+    private static LottoResult findTargetResultUsingBonus(boolean isBonus) {
+        if (isBonus) {
+            return LottoResult.SECOND;
+        }
+        return LottoResult.THIRD;
     }
 
     public int getPrice() {
