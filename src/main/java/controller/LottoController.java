f8b8871c4e46b74a4fbeee15f1d6dc9ad516a184
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-02-18 17:22:06 +0900

    refactor: 예외 발생시 재입력 받게끔 수정

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -11,6 +11,7 @@ import domain.WinningResult;
 import dto.WinningRecipeGroup;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import utils.RandomNumbersGenerator;
 import view.InputView;
 import view.OutputView;
@@ -43,16 +44,36 @@ public class LottoController {
     }
 
     private WinningNumber requestWinningNumber() {
-        outputView.printAskInputWiningLotto();
-        final List<Integer> numbers = inputView.readWinningNumbers();
-        final Lotto winningLotto = new Lotto(numbers);
-        outputView.printAskInputBonusNumber();
-        final BonusNumber bonusNumber = inputView.readBonusNumber();
-        return new WinningNumber(winningLotto, bonusNumber);
+        final Lotto winningLotto = requestWinningLotto();
+        return tryCatchLoopTemplate(() -> {
+            outputView.printAskInputBonusNumber();
+            final BonusNumber bonusNumber = inputView.readBonusNumber();
+            return new WinningNumber(winningLotto, bonusNumber);
+        });
+    }
+
+    private Lotto requestWinningLotto() {
+        return tryCatchLoopTemplate(() -> {
+            outputView.printAskInputWiningLotto();
+            final List<Integer> numbers = inputView.readWinningNumbers();
+            return new Lotto(numbers);
+        });
     }
 
     private Money requestMoney() {
-        outputView.printAskInputMoney();
-        return inputView.readMoney();
+        return tryCatchLoopTemplate(() -> {
+            outputView.printAskInputMoney();
+            return inputView.readMoney();
+        });
+    }
+
+    private <T> T tryCatchLoopTemplate(Supplier<T> function) {
+        while (true) {
+            try {
+                return function.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
