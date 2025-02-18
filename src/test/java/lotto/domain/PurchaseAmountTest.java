4250b35826fe65d0eb46ec2ac0731bf724a1a7b8
KJungW <sagakkb0707@naver.com>
2025-02-18 09:44:16 +0900

    test(purchase_amount): 구입 금액 테스트
    테스트 내용
    - 단위에 맞지 않는 구매 금액이 입력될 경우 예외가 발생한다

---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import lotto.exceptions.ExceptionMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PurchaseAmountTest {
+
+    @DisplayName("단위에 맞지 않는 구매 금액이 입력될 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, Lotto.LOTTO_PRICE - 1})
+    void 단위에_맞지_않는_구매_금액이_입력될_경우_예외가_발생한다(int invalidAmount) {
+        String messageTemplate = ExceptionMessage.INVALID_PURCHASE_AMOUNT.getContent();
+        String expectedMessage = String.format(messageTemplate, Lotto.LOTTO_PRICE);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new PurchaseAmount(invalidAmount))
+                .withMessage(expectedMessage);
+    }
+}
