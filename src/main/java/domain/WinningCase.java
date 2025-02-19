46861ac835575b7e6b5e1215b1eb0beb357a9b40
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-19 21:28:58 +0900

    test: WinningCase 테스트 추가

--- src/main/java/domain/WinningCase.java
+++ src/main/java/domain/WinningCase.java
@@ -7,11 +7,11 @@ import java.util.Map;
 public enum WinningCase {
 
   ELSE(0, 0, false),
-  THREE_SAME(5000, 3, false),
-  FOUR_SAME(50000, 4, false),
-  FIVE_SAME(1500000, 5, false),
-  FIVE_BONUS_SAME(30000000, 5, true),
-  SIX_SAME(2000000000, 6, false);
+  THREE_SAME(5_000, 3, false),
+  FOUR_SAME(50_000, 4, false),
+  FIVE_SAME(1_500_000, 5, false),
+  FIVE_BONUS_SAME(30_000_000, 5, true),
+  SIX_SAME(2_000_000_000, 6, false);
 
   private final int winningMoney;
   private final int sameCount;
