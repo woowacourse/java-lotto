422357517f65efbfd5c6b48d72de45a1e0e7f7f4
eoehd1ek <eoehd2ek@gmail.com>
2025-02-21 13:49:51 +0900

    test(WinningResult): 누락된 테스트 추가

--- src/test/java/domain/WinningResultTest.java
+++ src/test/java/domain/WinningResultTest.java
@@ -7,6 +7,36 @@ import org.junit.jupiter.api.Test;
 
 class WinningResultTest {
     @Test
+    @DisplayName("순위와 횟수가 주어질 때 그 순위에 대한 횟수가 잘 증가한다")
+    void should_increase_count_by_winningInfo_and_count() {
+        // given
+        WinningResult winningResult = new WinningResult();
+        WinningInfo winningInfo = WinningInfo.FIRST_PRIZE;
+        int count = 2;
+
+        // when
+        winningResult.increaseCount(winningInfo, count);
+
+        // when
+        assertThat(winningResult.getCount(winningInfo)).isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("순위가 주어질 때 그 순위에 대한 등장 횟수를 가져온다")
+    void should_return_count_by_winningInfo() {
+        // given
+        WinningResult winningResult = new WinningResult();
+        WinningInfo winningInfo = WinningInfo.FIRST_PRIZE;
+        int count = 2;
+
+        // when
+        winningResult.increaseCount(winningInfo, count);
+
+        // when
+        assertThat(winningResult.getCount(winningInfo)).isEqualTo(count);
+    }
+
+    @Test
     @DisplayName("당첨 결과를 토대로 총 당첨 금액을 가져온다")
     void should_return_total_prices() {
         // given
